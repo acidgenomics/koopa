95530d8b6762532e22d1b0ad4e9a5a41ea8df675
Michael Steinbaugh <mike@steinbaugh.com>
2016-06-22 08:08:25 -0400

    Add comment

--- sra_fastq_dump.sh
+++ sra_fastq_dump.sh
@@ -1,3 +1,6 @@
+# This requires fastq-dump from sra-tools
+# https://github.com/ncbi/sra-tools
+
 if [ ! -d fastq ]; then
   mkdir fastq
 fi
@@ -6,6 +9,7 @@ while read name; do
   if [ ! -e "${name}.fastq.gz" ] && [ ! -e "${name}_1.fastq.gz" ]; then
     echo "${name}"
     bsub -q priority -W 6:00 fastq-dump --gzip --split-3 --accession "${name}"
-    # rm ~/ncbi/public/sra/{$name}.sra # Clean the SRA cache
+    # Remove SRA cache file upon completion
+    # rm ~/ncbi/public/sra/{$name}.sra
   fi
 done < ../SRR_Acc_List.txt
