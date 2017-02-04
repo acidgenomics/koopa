13606a3436dd5031d2680f1b092e76af27231cf6
Michael Steinbaugh <mike@steinbaugh.com>
2017-02-04 08:46:17 -0500

    Update install_conda.sh

--- bash/install_conda.sh
+++ bash/install_conda.sh
@@ -1,8 +1,3 @@
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
-
-conda update conda
-
-conda config --add channels bioconda
-conda config --add channels r
