3f4f83421e44056af2aa34326a9d90acd51d1653
Michael Steinbaugh <mike@steinbaugh.com>
2017-02-07 15:18:09 -0500

    Update install_conda.sh

--- bash/install_conda.sh
+++ bash/install_conda.sh
@@ -1,3 +1,3 @@
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
