616f513ce3c1a2e7a6a455a0ffd0a5614dd4e538
Michael Steinbaugh <mike@steinbaugh.com>
2017-02-07 10:21:55 -0500

    Ensembl download and version update

--- bash/wget_ensembl.sh
+++ bash/wget_ensembl.sh
@@ -1,40 +1,23 @@
-RELEASE="85"
+RELEASE="87"
 CDNA="$GENOME.cdna.all.fa"
 DNA="$GENOME.dna.$FA_TYPE.fa"
 GTF="$GENOME.$RELEASE.gtf"
 
-cd ~/data
-if [ ! -d $MODEL ]; then
-    mkdir $MODEL
+if [ -d $MODEL ]; then
+    rm -rf $MODEL
 fi
+mkdir $MODEL
 cd $MODEL
-if [ -d genome ]; then
-    rm -rf genome
-fi
-mkdir genome
-cd genome
 
-# Download FASTA and GTF files
-if [ ! -f $CDNA.gz ]; then
-    wget ftp://ftp.ensembl.org/pub/release-$RELEASE/fasta/$MODEL/cdna/$CDNA.gz
-fi
-if [ ! -f $DNA.gz ]; then
-    wget ftp://ftp.ensembl.org/pub/release-$RELEASE/fasta/$MODEL/dna/$DNA.gz
-fi
-if [ ! -f $GTF.gz ]; then
-    wget ftp://ftp.ensembl.org/pub/release-$RELEASE/gtf/$MODEL/$GTF.gz
-fi
+# Download FASTA and GTF files:
+wget ftp://ftp.ensembl.org/pub/release-$RELEASE/fasta/$MODEL/cdna/$CDNA.gz
+wget ftp://ftp.ensembl.org/pub/release-$RELEASE/fasta/$MODEL/dna/$DNA.gz
+wget ftp://ftp.ensembl.org/pub/release-$RELEASE/gtf/$MODEL/$GTF.gz
 
-# Decompress the files if necessary
-if [ ! -f $CDNA ]; then
-    gunzip -cf $CDNA.gz >$CDNA
-fi
-if [ ! -f $DNA ]; then
-    gunzip -cf $DNA.gz >$DNA
-fi
-if [ ! -f $GTF ]; then
-    gunzip -cf $GTF.gz >$GTF
-fi
+# Decompress the files:
+gunzip -cf $CDNA.gz >$CDNA
+gunzip -cf $DNA.gz >$DNA
+gunzip -cf $GTF.gz >$GTF
 
 ln -fs $CDNA cdna.fa
 ln -fs $CDNA.gz cdna.fa.gz
