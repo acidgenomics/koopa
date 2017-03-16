c914c86847b620b25177bf90bf54539df0414926
Michael Steinbaugh <mjsteinbaugh@users.noreply.github.com>
2017-03-15 21:14:47 -0400

    Update 01_miniconda.sh

--- profile/path/01_miniconda.sh
+++ profile/path/01_miniconda.sh
@@ -1,9 +1,7 @@
 # https://conda.io/miniconda.html
-if [ -d "$HOME/miniconda2" ]
-then
+if [[ -d "$HOME/miniconda2" ]]; then
     export PATH="$HOME/miniconda2/bin:$PATH"
 fi
-if [ -d "$HOME/miniconda3" ]
-then
+if [[ -d "$HOME/miniconda3" ]]; then
     export PATH="$HOME/miniconda3/bin:$PATH"
 fi
