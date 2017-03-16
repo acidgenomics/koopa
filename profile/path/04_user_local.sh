fdfa0022b1bfda0c8f6dc36a39ec516b822ceb62
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-15 21:15:49 -0400

    Update 04_user_local.sh

--- profile/path/04_user_local.sh
+++ profile/path/04_user_local.sh
@@ -1,8 +1,7 @@
-if [ -d "$HOME/bin" ]
-then
+if [[ -d "$HOME/bin" ]]; then
     export PATH="$HOME/bin:$PATH"
 fi
-if [ -d "$HOME/lib" ]
-then
+
+if [[ -d "$HOME/lib" ]]; then
     export LD_LIBRARY_PATH="$HOME/lib:$LD_LIBRARY_PATH"
 fi
