a53c6b5128ea7472eb1033e4b7c8f8869bd7607f
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-15 21:16:29 -0400

    Update 01_console_orchestra.sh

--- profile/01_console_orchestra.sh
+++ profile/01_console_orchestra.sh
@@ -6,11 +6,11 @@ if [[ $HOSTNAME =~ ".orchestra" ]] && [[ $(uname -s) = "Linux" ]] && [[ -d /n/da
     
     # Prompt string
     # Set variable identifying the `chroot` you work in
-    if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    if [[ -z "$debian_chroot" ]] && [[ -r /etc/debian_chroot ]]; then
         debian_chroot=$(cat /etc/debian_chroot)
     fi
     # Check for color support
-    if [ $TERM = "xterm-256color" ]; then
+    if [[ $TERM = "xterm-256color" ]]; then
         PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
     else
         PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "
