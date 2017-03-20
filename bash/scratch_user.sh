8dc99a10dcdd0a0f7b2fc28d0824fb6e40589787
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-20 12:54:27 -0400

    Delete

--- bash/scratch_user.sh
+++
@@ -1,6 +0,0 @@
-# Create a user folder on the Orchestra scratch disk
-# Requires an eCommons user identifier
-scratch="/n/scratch2"
-mkdir -p "$scratch"/"$1"
-chmod 700 "$scratch"/"$1"
-ln -s "$scratch"/"$1" ~/scratch
