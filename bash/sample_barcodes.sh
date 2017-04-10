f34d8e4a7a09b528ccfcc1ad206b4c432a9f0bc0
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-10 14:08:21 -0400

    Expand head to top 24

--- bash/sample_barcodes.sh
+++ bash/sample_barcodes.sh
@@ -1,1 +1,1 @@
-gzip -cd *_R3.fastq.* | head -1000000 | awk 'NR == 2 || NR % 4 == 2' | grep -v N | sort | uniq -c | sort -nr | head > sample_barcodes.log
+gzip -cd *_R3.fastq.* | head -1000000 | awk 'NR == 2 || NR % 4 == 2' | grep -v N | sort | uniq -c | sort -nr | head -n 24 > sample_barcodes.log
