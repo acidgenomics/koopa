0ca8f3df6a396c2ed0a52b9aa89b0ba3ba80c158
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-14 11:45:59 -0400

    Linux and macOS system detection

--- bash/install_conda.sh
+++ bash/install_conda.sh
@@ -1,7 +1,20 @@
 # https://bioconda.github.io/
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh
+# Python 3 version
+
+# Linux
+if [[ $(uname -s) = "Linux" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# macOS
+elif [[ $(uname -s) = "Darwin" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+else
+    echo "operating system not supported"
+    exit 1
+fi
+
+bash Miniconda3-*.sh
+rm Miniconda3-*.sh
+
 conda config --add channels conda-forge
 conda config --add channels defaults
 conda config --add channels r
