fc7e3112cc9ea3d06ff1c59f7320dfa11b8e2eb7
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-03 09:29:05 -0400

    Improve paths to work from project root

--- bash/sample_barcodes.sh
+++ bash/sample_barcodes.sh
@@ -1,1 +1,9 @@
-gzip -cd *_R3.fastq.* | head -1000000 | awk 'NR == 2 || NR % 4 == 2' | grep -v N | sort | uniq -c | sort -nr | head -n 24 > sample_barcodes.log
+mkdir -p logs
+gzip -cd data-raw/fastq/*_R3.fastq.* | \
+head -1000000 | \
+awk 'NR == 2 || NR % 4 == 2' | \
+grep -v N | \
+sort | \
+uniq -c | \
+sort -nr | \
+head -n 24 > logs/sample_barcodes.log
