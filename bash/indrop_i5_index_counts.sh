2fc0c11cc04be3761752a8f80d0956d531f98dd7
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-10 15:49:46 -0400

    Rename for better clarity

---
+++ bash/indrop_i5_index_counts.sh
@@ -0,0 +1,13 @@
+# inDrop sample barcode counts from i5 indexes
+# https://iccb.med.harvard.edu/single-cell-core
+# FASTQs must be located at `data-raw/fastq`
+
+mkdir -p logs
+gzip -cd data-raw/fastq/*_R3.* | \
+head -1000000 | \
+awk 'NR == 2 || NR % 4 == 2' | \
+grep -v N | \
+sort | \
+uniq -c | \
+sort -nr | \
+head -n 24 > logs/i5_index_counts.log
--- bash/sample_barcodes.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p logs
-gzip -cd data-raw/fastq/*_R3.* | \
-head -1000000 | \
-awk 'NR == 2 || NR % 4 == 2' | \
-grep -v N | \
-sort | \
-uniq -c | \
-sort -nr | \
-head -n 24 > logs/sample_barcodes.log
