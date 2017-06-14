7d597fe1e29e9b41e03486e41d1f034df24fdc83
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-14 10:05:28 -0400

    Create 01_conda.sh

--- profile/path/01_conda.sh
+++ profile/path/01_conda.sh
@@ -4,7 +4,7 @@ if [[ -d "$HOME/miniconda2" ]]; then
 fi
 if [[ -d "$HOME/miniconda3" ]]; then
     conda_dir="$HOME/miniconda3"
-    export LD_LIBRARY_PATH="$conda_dir/lib:$LD_LIBRARY_PATH"
+    # export LD_LIBRARY_PATH="$conda_dir/lib:$LD_LIBRARY_PATH"
     export PATH="$conda_dir/bin:$PATH"
     export PKG_CONFIG_PATH="$conda_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
 fi
