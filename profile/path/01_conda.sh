5c6c543e21886f0f3d42b7007d637ea0ca28f913
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-14 10:21:53 -0400

    Create 01_conda.sh

--- profile/path/01_conda.sh
+++ profile/path/01_conda.sh
@@ -5,10 +5,12 @@ fi
 if [[ -d "$HOME/miniconda3" ]]; then
     conda_dir="$HOME/miniconda3"
     export PATH="$conda_dir/bin:$PATH"
+    export LD_RUN_PATH="$conda_dir/lib:$LD_RUN_PATH"
     export PKG_CONFIG_PATH="$conda_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
     
     # [fix] Setting LD_LIBRARY_PATH isn't recommended. 
     # https://conda.io/docs/building/shared-libraries.html#shared-libraries-in-linux-and-os-x
-    # export LD_LIBRARY_PATH="$conda_dir/lib:$LD_LIBRARY_PATH"
-    # If R has problems compiling, look for another solution.
+    # https://conda.io/docs/building/environment-vars.html
+    # Setting this fixes some R compilation issues but messes with Orchestra modules.
+    # Look for another solution. Does LD_RUN_PATH fix the issue?
 fi
