646cd815d893b0161e9eae87d8ea8b2ffe082f30
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-14 10:13:41 -0400

    Create 01_conda.sh

--- profile/path/01_conda.sh
+++ profile/path/01_conda.sh
@@ -4,7 +4,11 @@ if [[ -d "$HOME/miniconda2" ]]; then
 fi
 if [[ -d "$HOME/miniconda3" ]]; then
     conda_dir="$HOME/miniconda3"
-    # export LD_LIBRARY_PATH="$conda_dir/lib:$LD_LIBRARY_PATH"
     export PATH="$conda_dir/bin:$PATH"
     export PKG_CONFIG_PATH="$conda_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
+    
+    # [fix] Setting LD_LIBRARY_PATH isn't recommended. 
+    # https://conda.io/docs/building/shared-libraries.html#shared-libraries-in-linux-and-os-x
+    # export LD_LIBRARY_PATH="$conda_dir/lib:$LD_LIBRARY_PATH"
+    # If R has problems compiling, look for another solution.
 fi
