babc838d74a2604c0954d7069b480783c1d97327
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-15 09:03:51 -0400

    Fix quote escaping

--- bash/render.sh
+++ bash/render.sh
@@ -1,2 +1,5 @@
+# Note the use of double quotes in the Rscript command. Single quote usage,
+# which is common in online examples, will not escape the bash variables
+# properly.
 file_name="$1"
-bsub -W 12:00 -q priority -J "$file_name" -n 1 -R rusage[mem=65536] Rscript -e 'rmarkdown::render("$file_name.Rmd")'
+bsub -W 12:00 -q priority -J "$file_name" -n 1 -R rusage[mem=65536] Rscript -e "rmarkdown::render('$file_name.Rmd')"
