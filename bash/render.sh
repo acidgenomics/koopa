06f33121ff1b877686773283250a2ac6cef41a24
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-15 13:07:57 -0400

    Don't include extension

--- bash/render.sh
+++ bash/render.sh
@@ -2,4 +2,4 @@
 # which is common in online examples, will not escape the bash variables
 # properly.
 file_name="$1"
-bsub -W 12:00 -q priority -J "$file_name" -n 1 -R rusage[mem=65536] Rscript -e "rmarkdown::render('$file_name.Rmd')"
+bsub -W 12:00 -q priority -J "$file_name" -n 1 -R rusage[mem=65536] Rscript -e "rmarkdown::render('$file_name')"
