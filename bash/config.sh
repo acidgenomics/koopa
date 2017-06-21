1ecafcd4cd30689bd6f82a81c01c9d66b4eb4b8e
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-21 09:52:08 -0400

    Fix spaces

--- bash/config.sh
+++ bash/config.sh
@@ -3,30 +3,26 @@ echo "PATH exports"
 
 # orchestra
 if [[ -n $ORCHESTRA ]]; then
-    echo "HMS Orchestra modules \
-    ================================================="
+    echo "HMS Orchestra modules ================================================="
     module list
 fi
 
 # conda
 if [[ ! -z $CONDA_DIR ]]; then
-    echo "conda \
-    ================================================================="
+    echo "conda ================================================================="
     conda --version
 fi
 
 
 # aspera connect
 if [[ ! -z $ASPERA_DIR ]]; then
-    echo "Aspera Connect \
-    ========================================================"
+    echo "Aspera Connect ========================================================"
     ascp --version
 fi
 
 
 # bcbio-nextgen
 if [[ ! -z $BCBIO_DIR ]]; then
-    echo "bcbio-nextgen \
-    ========================================================="
+    echo "bcbio-nextgen ========================================================="
     echo $BCBIO_DIR
 fi
