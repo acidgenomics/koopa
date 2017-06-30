374382a95a1f03adfac805f4faf1cafd88953b51
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-30 12:18:28 -0400

    Add bcbio support for O2

--- profile/path/05_bcbio-nextgen.sh
+++ profile/path/05_bcbio-nextgen.sh
@@ -1,9 +1,23 @@
 # https://bcbio-nextgen.readthedocs.io
-bcbio_dir="/opt/bcbio/centos/bin"
+
+if [[ $HPC == "HMS RC O2" ]]; then
+    bcbio_dir="/n/groups/bcbio"
+    bcbio_path="/n/app/bcbio/tools/bin"
+elif [[ $HPC == "HMS RC Orchestra" ]]; then
+    bcbio_dir="/groups/bcbio"
+    bcbio_path="/opt/bcbio/centos/bin"
+fi
+
+# Update paths
 if [[ -d $bcbio_dir ]]; then
+    # Check that directory exists
+    if [[ ! -d $bcbio_dir ]]; then
+        echo "bcbio_dir missing"
+        exit 1
+    fi
     export BCBIO_DIR="$bcbio_dir"
     export PATH="$BCBIO_DIR:$PATH"
     unset PYTHONHOME
     unset PYTHONPATH
+    unset bcbio_dir
 fi
-unset bcbio_dir
