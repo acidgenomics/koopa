521823ad6aeae7149991ce4a029a50e4c0814556
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-06 12:55:11 -0400

    Remove ellipses

--- bash/render.sh
+++ bash/render.sh
@@ -5,7 +5,7 @@ if [[ ! -z $LSF_ENVDIR ]]; then
     ram_gb="$1"
     ram_mb="$(($ram_gb * 1024))"
     file_name="$2"
-    echo "Rendering $file_name with $ram_gb GB RAM..."
+    echo "Rendering $file_name with $ram_gb GB RAM"
     bsub -W 12:00 -q priority -J "$file_name" -n 1 -R rusage[mem="$ram_mb"] \
         Rscript --default-packages="$R_DEFAULT_PACKAGES" \
                  -e "rmarkdown::render('$file_name')"
