971a2232a1cb2fddac52a5e74f87204e9ef575ab
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-08 16:10:58 -0400

    Fix Orchestra modules

--- profile/path/01_hms_rc.sh
+++ profile/path/01_hms_rc.sh
@@ -11,36 +11,37 @@
 # - `module avail`
 # - `module avail stats/R`
 if [[ $HPC == "HMS RC O2" ]]; then
-    module load \
-        bcl2fastq/2.18.0.12 \
-        boost/1.62.0 \
-        cairo/1.14.6 \
-        cmake/3.7.1 \
-        fontconfig/2.12.1 \
-        freetype/2.7 \
-        gcc/6.2.0 \
-        hdf5/1.10.1 \
-        ImageMagick/6.9.1.10 \
-        openblas/0.2.19 \
-        python/3.6.0 \
-        sratoolkit/2.8.1
+    # priority
+    module load gcc/6.2.0
+    # alphabetical
+    module load bcl2fastq/2.18.0.12
+    module load boost/1.62.0
+    module load cairo/1.14.6
+    module load cmake/3.7.1
+    module load fontconfig/2.12.1
+    module load freetype/2.7
+    module load hdf5/1.10.1
+    module load ImageMagick/6.9.1.10
+    module load openblas/0.2.19
+    module load python/3.6.0
+    module load sratoolkit/2.8.1
 elif [[ $HPC == "HMS RC Orchestra" ]]; then
-    module load \
-        dev/boost-1.57.0 \
-        dev/compiler/cmake-3.3.1 \
-        dev/compiler/gcc-4.8.5 \
-        dev/lapack \
-        dev/leiningen/stable-feb032016 \
-        dev/openblas/0.2.14 \
-        dev/python/3.4.2 \
-        dev/ruby/2.2.4 \
-        image/imageMagick/6.9.1 \
-        seq/bcl2fastq/2.17.1.14 \
-        seq/cellranger/2.0.0 \
-        seq/sratoolkit/2.8.1-3 \
-        stats/R/3.3.1 \
-        utils/cairo/1.14.2 \
-        utils/fontconfig/2.11.1 \
-        utils/freetype/2.6 \
-        utils/hdf5/1.8.16
+    # priority
+    module load dev/compiler/gcc-4.8.5
+    # alphabetical
+    module load dev/boost-1.57.0
+    module load dev/compiler/cmake-3.3.1
+    module load dev/lapack
+    module load dev/leiningen/stable-feb032016
+    module load dev/openblas/0.2.14
+    module load dev/python/3.4.2
+    module load dev/ruby/2.2.4
+    module load image/imageMagick/6.9.1
+    module load seq/bcl2fastq/2.17.1.14
+    module load seq/cellranger/2.0.0
+    module load seq/sratoolkit/2.8.1-3
+    module load utils/cairo/1.14.2
+    module load utils/fontconfig/2.11.1
+    module load utils/freetype/2.6
+    module load utils/hdf5/1.8.16
 fi
