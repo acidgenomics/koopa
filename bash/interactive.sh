f0dd98c7489c91a510f2cd3c62bc9eecc46def69
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-14 15:21:09 -0400

    Fix defaults

--- bash/interactive.sh
+++ bash/interactive.sh
@@ -1,4 +1,5 @@
-# Launch an interactive session
+# Launch an interactive session that lasts for 1 hour
+
 if [[ "$#" -gt "0" ]]; then
     cores="$1"
     ram_gb="$2"
@@ -7,14 +8,14 @@ else
     echo "Syntax: interactive CORES RAM_GB"
 fi
 
-# Pass commands to HPC scheduler
 echo "Starting interactive session with $cores cores and $ram_gb GB RAM..."
+
 if [[ ! -z $SLURM_CONF ]]; then
     # Slurm
-    srun -p interactive --pty --mem "$ram_mb" -t 20 /bin/bash
+    srun -p interactive --pty --mem "$ram_mb" --time 1:00:00 /bin/bash
 elif [[ ! -z $LSF_ENVDIR ]]; then
     # LSF    
-    bsub -Is -W 12:00 -q interactive -n "$cores" -R rusage[mem="$ram_mb"] bash
+    bsub -Is -W 1:00 -q interactive -n "$cores" -R rusage[mem="$ram_mb"] bash
 else
     echo "HPC required"
     exit 1
