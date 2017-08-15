e5e51ce8d693d2db28b6547791eb92fece29cd66
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-15 10:39:22 -0400

    Set default name to indrop

--- workflows/bcbio/scrnaseq/harvard_indrop_v3/submit_bcbio.sh
+++ workflows/bcbio/scrnaseq/harvard_indrop_v3/submit_bcbio.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1                  # Number of nodes (keep at 1, except when using MPI queue)
 #SBATCH -t 0-45:59            # Runtime in D-HH:MM format
 #SBATCH -p medium             # Partition (queue)
-#SBATCH -J smith              # Job name
+#SBATCH -J indrop             # Job name
 #SBATCH -o hostname_%j.out    # File to which STDOUT will be written, including job ID
 #SBATCH -e hostname_%j.err    # File to which STDERR will be written, including job ID
 #SBATCH --mem-per-cpu=8G      # Memory needed per core
