253f09baca5b9ffff52a9d9fde4950a10b3f8e39
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-28 11:56:02 -0400

    Fix core message

--- bash/interactive.sh
+++ bash/interactive.sh
@@ -16,7 +16,7 @@ fi
 
 ram_mb="$(($ram_gb * 1024))"
 
-echo "Launching interactive session with ${cores} cores and ${ram_gb} GB RAM"
+echo "Launching interactive session with ${cores} core(s), ${ram_gb} GB RAM"
 
 if [[ $HPC == "HMS RC O2" ]]; then
     srun -p interactive --pty --mem "$ram_mb" --time 0-12:00 /bin/bash
