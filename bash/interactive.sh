ed71c8aa5e7b2edced8e63f051aaf809d52d5759
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-28 11:18:34 -0400

    Improve O2 support

--- bash/interactive.sh
+++ bash/interactive.sh
@@ -1,21 +1,28 @@
-# Launch an interactive session that lasts for 1 hour
+# Launch an interactive session that lasts for 12 hours
+
+# HMS RC O2: $SLURM_CONF is set
+# HMS RC Orchestra: $LSF_ENVDIR is set
+
+# Exit on HPC detection failure
+if [[ -z $HPC ]]; then
+    echo "HPC required"
+    exit 1
+fi
 
 if [[ "$#" -gt "0" ]]; then
     cores="$1"
     ram_gb="$2"
     ram_mb="$(($ram_gb * 1024))"
 else
-    echo "Syntax: interactive CORES RAM_GB"
+    echo "Syntax: interactive <cores> <ram_gb>"
 fi
 
-echo "Starting interactive session with $cores cores and $ram_gb GB RAM..."
+echo "Launching interactive session with ${cores} cores and ${ram_gb} GB RAM"
 
-if [[ ! -z $SLURM_CONF ]]; then
-    # Slurm
-    srun -p interactive --pty --mem "$ram_mb" --time 1:00:00 /bin/bash
-elif [[ ! -z $LSF_ENVDIR ]]; then
-    # LSF    
-    bsub -Is -W 1:00 -q interactive -n "$cores" -R rusage[mem="$ram_mb"] bash
+if [[ $HPC == "HMS RC O2" ]]; then
+    srun -p interactive --pty --mem "$ram_mb" --time 0-12:00 /bin/bash
+elif [[ $HPC == "HMS RC Orchestra" ]]; then
+    bsub -Is -W 12:00 -q interactive -n "$cores" -R rusage[mem="$ram_mb"] bash
 else
     echo "HPC required"
     exit 1
