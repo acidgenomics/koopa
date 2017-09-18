db5d8201c3e2283c3caee52bfe0c359b12d27675
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-18 10:42:28 -0400

    Try loading purge

--- profile/path/03_hms_rc.sh
+++ profile/path/03_hms_rc.sh
@@ -11,7 +11,9 @@
 # - `module avail`
 # - `module avail stats/R`
 if [[ $HPC == "HMS RC O2" ]]; then
+    module purge
     module load bcl2fastq/2.18.0.12
 elif [[ $HPC == "HMS RC Orchestra" ]]; then
+    module purge
     module load seq/bcl2fastq/2.17.1.14
 fi
