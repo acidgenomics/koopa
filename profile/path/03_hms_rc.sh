cb29cf34b273ee29a66eecd278992f6521c1f9e4
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-18 09:35:34 -0400

    Update modules

--- profile/path/03_hms_rc.sh
+++ profile/path/03_hms_rc.sh
@@ -12,8 +12,8 @@
 # - `module avail stats/R`
 if [[ $HPC == "HMS RC O2" ]]; then
     # priority
-    # module load gcc/6.2.0
-    # module load harfbuzz/1.3.4
+    module load gcc/6.2.0
+    module load harfbuzz/1.3.4
     # alphabetical
     module load bcl2fastq/2.18.0.12
     # module load boost/1.62.0
@@ -23,14 +23,15 @@ if [[ $HPC == "HMS RC O2" ]]; then
     # module load freetype/2.7
     # module load hdf5/1.10.1
     # module load ImageMagick/6.9.1.10
-    # module load openblas/0.2.19
+    module load java/jdk-1.8u112
+    module load openblas/0.2.19
     # module load python/3.6.0
     # module load R/3.4.1
     module load sratoolkit/2.8.1
 elif [[ $HPC == "HMS RC Orchestra" ]]; then
     # priority
-    # module load dev/compiler/gcc-4.8.5
-    # module load utils/harfbuzz/1.2.3
+    module load dev/compiler/gcc-4.8.5
+    module load utils/harfbuzz/1.2.3
     # alphabetical
     # module load dev/boost-1.57.0
     # module load dev/compiler/cmake-3.3.1
@@ -47,4 +48,5 @@ elif [[ $HPC == "HMS RC Orchestra" ]]; then
     # module load utils/fontconfig/2.11.1
     # module load utils/freetype/2.6
     # module load utils/hdf5/1.8.16
+    # module load utils/texlive/2015
 fi
