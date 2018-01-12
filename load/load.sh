90d79aed3b674e5c30b5889035685cf05b965b5b
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 14:56:22 -0500

    Check for interactive and avoid double load

---
+++ load/load.sh
@@ -0,0 +1,34 @@
+export SEQCLOUD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check for supported operating system
+if [[ $(uname -s) != "Linux" ]] && [[ $(uname -s) != "Darwin" ]]; then
+    echo "$(uname -s) operating system not supported"
+    exit 1
+fi
+
+# Load profile settings
+. "$SEQCLOUD_DIR"/profile/general.sh
+if [[ "$SEQCLOUD_CONSOLE" != false ]]; then
+    for file in $(find "$SEQCLOUD_DIR"/profile/console \
+        -type f -name "*.sh" ! -name ".*" | sort); do
+        . "$file"
+    done
+    unset -v file
+fi
+if [[ "$SEQCLOUD_PATH" != false ]]; then
+    for file in $(find "$SEQCLOUD_DIR"/profile/path \
+        -type f -name "*.sh" ! -name ".*" | sort); do
+        . "$file"
+    done
+    unset -v file
+fi
+
+# Pass positional parameters to scripts in the `bash` subdirectory
+function seqcloud {
+    local script="$1"
+    shift 1
+    . "$SEQCLOUD_DIR"/scripts/"$script".sh $*
+}
+
+# Login message for interactive session
+. "$SEQCLOUD_DIR"/load/hpc_login_message.sh
