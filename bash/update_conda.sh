bcc993fe4a04c8551d4a8024824df15d1929f926
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 11:58:42 -0500

    Fix for default conda channels update

--- bash/update_conda.sh
+++ bash/update_conda.sh
@@ -1,8 +1,10 @@
 command -v conda >/dev/null 2>&1 || { echo >&2 "conda missing"; exit 1; }
 
-conda config --add channels conda-forge
+# https://bioconda.github.io/#set-up-channels
+# (conda config --add channels r)
 conda config --add channels defaults
-conda config --add channels r
+conda config --add channels conda-forge
 conda config --add channels bioconda
+
 conda update conda
 conda update --all
