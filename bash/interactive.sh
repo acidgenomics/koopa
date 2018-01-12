dc2741f7cffd2c186802094d35c3cddca1774bfb
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 11:56:52 -0500

    Allow user to define cores for srun

--- bash/interactive.sh
+++ bash/interactive.sh
@@ -21,7 +21,7 @@ echo "Launching interactive session with ${cores} core(s), ${ram_gb} GB RAM"
 if [[ $HPC == "HMS RC O2" ]]; then
     command -v srun >/dev/null 2>&1 || { echo >&2 "srun missing"; exit 1; }
     # `--x11` flag before `/bin/bash` requires `~/.ssh/config` set on local machine
-    srun -p interactive --pty --mem "$ram_gb"G --time 0-12:00 --x11 /bin/bash
+    srun -p interactive --pty -n "$cores" --mem "${ram_gb}"G --time 0-12:00 --x11 /bin/bash
 elif [[ $HPC == "HMS RC Orchestra" ]]; then
     command -v bsub >/dev/null 2>&1 || { echo >&2 "bsub missing"; exit 1; }
     bsub -Is -W 12:00 -q interactive -n "$cores" -R rusage[mem="$ram_mb"] bash
@@ -30,7 +30,7 @@ elif [[ $HPC == "Harvard FAS Odyssey" ]]; then
     # https://www.rc.fas.harvard.edu/resources/faq/category/slurm-2/
     command -v srun >/dev/null 2>&1 || { echo >&2 "srun missing"; exit 1; }
     # `--x11` flag before `/bin/bash` requires `~/.ssh/config` set on local machine
-    srun -p test --pty --mem "$ram_gb"G --time 0-8:00 --x11=first /bin/bash
+    srun -p test --pty -n "$cores" --mem "${ram_gb}"G --time 0-8:00 --x11=first /bin/bash
 else
     echo "HPC required"
     exit 1
