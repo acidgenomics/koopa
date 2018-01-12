bafbc93c76f524093e7680535f436e1fef6650cc
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 14:49:31 -0500

    Simplify the login message

--- login/hpc_interactive_message.sh
+++ login/hpc_interactive_message.sh
@@ -1,8 +1,26 @@
-# Interactive message on HPC cluster
-
 if [[ -n "$PS1" && -n "$HPC" ]]; then
     echo "==== seqcloud ============================================================="
-    seqcloud config
+    # bcbio
+    if [[ ! -z $BCBIO_DIR ]]; then
+        echo "# bcbio"
+        echo $BCBIO_DIR
+        echo ""
+    fi
+
+    # Conda
+    if [[ ! -z $CONDA_DIR ]]; then
+        echo "# conda"
+        echo $CONDA_DIR
+        echo ""
+    fi
+
+    # Date
+    echo "Last updated"
+    wd="$PWD"
+    cd "$SEQCLOUD_DIR"
+    git log -1 --format=%cd
+    cd "$wd"
+    unset -v wd
     echo "==========================================================================="
 fi
 
