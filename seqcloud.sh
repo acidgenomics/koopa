476b5a997387e8258f7d441bc2912f9086792eaf
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 14:56:22 -0500

    Check for interactive and avoid double load

--- seqcloud.sh
+++ seqcloud.sh
@@ -5,37 +5,6 @@
 # This software is provided under an MIT License
 # http://seq.cloud
 
-export SEQCLOUD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Check for supported operating system
-if [[ $(uname -s) != "Linux" ]] && [[ $(uname -s) != "Darwin" ]]; then
-    echo "$(uname -s) operating system not supported"
-    exit 1
+if [[ -z "$INTERACTIVE" ]]; then
+    . "$SEQCLOUD_DIR"/load/load.sh
 fi
-
-# Load profile settings
-. "$SEQCLOUD_DIR"/profile/general.sh
-if [[ "$SEQCLOUD_CONSOLE" != false ]]; then
-    for file in $(find "$SEQCLOUD_DIR"/profile/console \
-        -type f -name "*.sh" ! -name ".*" | sort); do
-        . "$file"
-    done
-    unset -v file
-fi
-if [[ "$SEQCLOUD_PATH" != false ]]; then
-    for file in $(find "$SEQCLOUD_DIR"/profile/path \
-        -type f -name "*.sh" ! -name ".*" | sort); do
-        . "$file"
-    done
-    unset -v file
-fi
-
-# Pass positional parameters to scripts in the `bash` subdirectory
-function seqcloud {
-    local script="$1"
-    shift 1
-    . "$SEQCLOUD_DIR"/scripts/"$script".sh $*
-}
-
-# Login message for interactive session
-. "$SEQCLOUD_DIR"/login/hpc_interactive_message.sh
