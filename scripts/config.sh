7262d8728cbaeee795312c8639077e8f63f7fc8c
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 14:28:56 -0500

    Final tweaks

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,23 +1,17 @@
-# HMS RC
-if [[ $HPC =~ "HMS RC" ]]; then
-    echo "Modules ==============================================================="
-    module list
-fi
-
-# Conda
-if [[ ! -z $CONDA_DIR ]]; then
-    echo "Conda ================================================================="
-    conda --version
+# bcbio
+if [[ ! -z $BCBIO_DIR ]]; then
+    echo "# bcbio"
+    echo $BCBIO_DIR
 fi
 
 # Aspera Connect
 if [[ ! -z $ASPERA_DIR ]]; then
-    echo "Aspera Connect ========================================================"
+    echo "# Aspera Connect"
     ascp --version
 fi
 
-# bcbio-nextgen
-if [[ ! -z $BCBIO_DIR ]]; then
-    echo "bcbio-nextgen ========================================================="
-    echo $BCBIO_DIR
+# Conda
+if [[ ! -z $CONDA_DIR ]]; then
+    echo "# conda"
+    conda --version
 fi
