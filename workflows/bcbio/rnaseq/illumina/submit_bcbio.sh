13cdb1317ca63620aa1188945ac6debdb848638c
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-22 11:51:59 -0500

    Create submit_bcbio.sh

---
+++ workflows/bcbio/rnaseq/illumina/submit_bcbio.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# https://slurm.schedmd.com/sbatch.html
+
+#SBATCH --job-name=illumina         # Job name
+#SBATCH --partition=medium          # Partition (queue)
+#SBATCH --time=0-12:00              # Runtime in D-HH:MM format
+
+#SBATCH --nodes=1                   # Number of nodes (keep at 1, except when using MPI queue)
+#SBATCH --ntasks=1                  # Number of cores per node (keep at 1)
+#SBATCH --cpus-per-task=1           # CPUs per task
+#SBATCH --mem-per-cpu=8G            # Memory needed per CPU
+
+#SBATCH --output=hostname_%j.out    # File to which STDOUT will be written, including job ID
+#SBATCH --error=hostname_%j.err     # File to which STDERR will be written, including job ID
+
+#SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
+
+bcbio_nextgen.py ../config/illumina_rnaseq.yaml -n 72 -t ipython -s slurm -q medium -r t=0-12:00
+
+# Use 6x cores per sample
