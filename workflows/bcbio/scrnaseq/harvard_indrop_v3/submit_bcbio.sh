86f106582aacd3c4b3905d9287cbd30e6edfa546
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-22 11:49:00 -0500

    Update submit_bcbio.sh

--- workflows/bcbio/scrnaseq/harvard_indrop_v3/submit_bcbio.sh
+++ workflows/bcbio/scrnaseq/harvard_indrop_v3/submit_bcbio.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#SBATCH -n 1                  # Number of cores per node
-#SBATCH -N 1                  # Number of nodes (keep at 1, except when using MPI queue)
-#SBATCH -t 2-00:00            # Runtime in D-HH:MM format
-#SBATCH -p medium             # Partition (queue)
-#SBATCH -J indrop             # Job name
-#SBATCH -o hostname_%j.out    # File to which STDOUT will be written, including job ID
-#SBATCH -e hostname_%j.err    # File to which STDERR will be written, including job ID
-#SBATCH --mem-per-cpu=8G      # Memory needed per core
-#SBATCH --mail-type=ALL       # Type of email notification (BEGIN, END, FAIL, ALL)
+# https://slurm.schedmd.com/sbatch.html
+
+#SBATCH --job-name=indrop           # Job name
+#SBATCH --partition=medium          # Partition (queue)
+#SBATCH --time=2-00:00              # Runtime in D-HH:MM format
+
+#SBATCH --nodes=1                   # Number of nodes (keep at 1, except when using MPI queue)
+#SBATCH --ntasks=1                  # Number of cores per node (keep at 1)
+#SBATCH --cpus-per-task=1           # CPUs per task
+#SBATCH --mem-per-cpu=8G            # Memory needed per CPU
+
+#SBATCH --output=hostname_%j.out    # File to which STDOUT will be written, including job ID
+#SBATCH --error=hostname_%j.err     # File to which STDERR will be written, including job ID
+
+#SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
 
 bcbio_nextgen.py ../config/indrop_rnaseq.yaml -n 64 -t ipython -s slurm -q medium -r t=2-00:00
