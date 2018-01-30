8b95325c9f76ebb81ff52fbca8d84a2cb46924dd
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-30 14:44:32 -0500

    Add line break to prompt string

--- profile/interactive/hms_rc.sh
+++ profile/interactive/hms_rc.sh
@@ -6,8 +6,8 @@ if [[ $HPC =~ "HMS RC" ]]; then
     
     # Export prompt string
     if [[ $TERM = "xterm-256color" ]]; then
-        PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "
+        PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\n\$ "
     else
-        PS1="${debian_chroot:+($debian_chroot)}\u@\h:\W\$ "
+        PS1="${debian_chroot:+($debian_chroot)}\u@\h:\W\n\$ "
     fi
 fi
