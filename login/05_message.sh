22956494c879f8088cc4a7f0393555b2e8f88b5e
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-18 07:59:28 -0500

    Add aspera to login message

--- login/05_message.sh
+++ login/05_message.sh
@@ -1,12 +1,14 @@
 if [[ -n "$PS1" && -n "$HPC" && -z "$INTERACTIVE_QUEUE" ]]; then
     echo ""
     echo "==== seqcloud ============================================================="
-    # bcbio
+    if [[ -d $ASPERA_DIR ]]; then
+        echo "# Aspera Connect"
+        echo $ASPERA_DIR
+    fi
     if [[ -d $BCBIO_DIR ]]; then
         echo "# bcbio"
         echo $BCBIO_DIR
     fi
-    # Conda
     if [[ -d $CONDA_DIR ]]; then
         echo "# conda"
         echo $CONDA_DIR
