6c013b90bdf14fbee49588a129e57eb16cfa6bbb
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-18 07:56:58 -0500

    Improve optional loading of aspera and conda

--- profile/non-interactive/aspera.sh
+++ profile/non-interactive/aspera.sh
@@ -1,5 +1,7 @@
-aspera_dir="$HOME/.aspera/connect/bin"
-if [[ -d $aspera_dir ]]; then
-    export ASPERA_DIR="$aspera_dir"
+if [[ -z "$ASPERA_DIR" ]]; then
+    aspera_dir="$HOME/.aspera/connect/bin"
+    if [[ -d "$aspera_dir" ]]; then
+        export ASPERA_DIR="$aspera_dir"
+    fi
+    unset -v aspera_dir
 fi
-unset -v aspera_dir
