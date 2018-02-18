67279187a21f719b14a8dcbfae958343aa76a56c
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-18 07:56:58 -0500

    Improve optional loading of aspera and conda

--- profile/non-interactive/miniconda.sh
+++ profile/non-interactive/miniconda.sh
@@ -1,12 +1,14 @@
 # https://conda.io/miniconda.html
 
-if [[ -d "$HOME/miniconda2" ]]; then
-    export CONDA_DIR="$HOME/miniconda2"
-elif [[ -d "$HOME/miniconda3" ]]; then
-    export CONDA_DIR="$HOME/miniconda3"
+if [[ -z "$CONDA_DIR" ]]; then
+    if [[ -d "$HOME/miniconda2" ]]; then
+        export CONDA_DIR="$HOME/miniconda2"
+    elif [[ -d "$HOME/miniconda3" ]]; then
+        export CONDA_DIR="$HOME/miniconda3"
+    fi
 fi
 
-if [[ -n "$CONDA_DIR" ]]; then
+if [[ -d "$CONDA_DIR" ]]; then
     export CONDA_VERSION=$($CONDA_DIR/bin/conda --version)
     # Ensure load script is sourced for v4.4+
     if echo "$CONDA_VERSION" | grep -q "conda 4.4"; then
