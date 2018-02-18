a70d991e7dc568de80b00c02ab17092509147f95
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-18 07:49:55 -0500

    Don't load bcbio if BCBIO_DIR is set false

--- profile/non-interactive/harvard_fas.sh
+++ profile/non-interactive/harvard_fas.sh
@@ -4,5 +4,7 @@ if [[ $HOSTNAME =~ ".rc.fas.harvard.edu" ]] || \
    [[ -d /n/regal/ ]]
 then
     export HPC="Harvard FAS Odyssey"
-    export BCBIO_DIR="/n/regal/hsph_bioinfo/bcbio_nextgen/bin"
+    if [[ -z "$BCBIO_DIR" ]]; then
+        export BCBIO_DIR="/n/regal/hsph_bioinfo/bcbio_nextgen/bin"
+    fi
 fi
