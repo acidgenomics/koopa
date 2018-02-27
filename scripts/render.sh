30ee6dcaec8eed734166d1178041a16b3ea65dad
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-27 07:19:50 -0500

    Draft update, switching to getopts

--- scripts/render.sh
+++ scripts/render.sh
@@ -2,43 +2,56 @@
 # which is common in online examples, will not escape the bash variables
 # properly.
 
-# srun guide
-# https://wiki.rc.hms.harvard.edu/display/O2/Using+Slurm+Basic#UsingSlurmBasic-Thesruncommand
-
 # Exit on HPC detection failure
-if [[ -z $HPC ]]; then
-    echo "HPC required"
+if [[ -z $SCHEDULER ]]; then
+    echo "HPC scheduler required"
     return 1
 fi
 
-if [[ "$#" -gt "0" ]]; then
-    queue="$1"
-    cores="$2"
-    ram_gb="$3"
-    ram_mb="$(($ram_gb * 1024))"
-    file_name="$4"
-else
-    echo "Syntax: render <queue> <cores> <ram_gb> <file_name>"
+usage() {
+    echo "render: [<queue> <cores> <mem> <time> <file>]" 1>&2
     return 1
-fi
+}
 
-echo "Submitting ${file_name} to ${queue} queue with ${cores} core(s), ${ram_gb} GB RAM"
+local OPTIND arg cores file mem queue time
+while getopts "cores:file:mem:queue:time:" arg; do
+    case ${arg} in
+        queue) queue="${OPTARG}";;
+        cores) cores="${OPTARG}";;
+        mem) mem="${OPTARG}";;
+        time) time="${OPTARG}";;
+        file) file="${OPTARG}";;
+        *) usage  # illegal option
+    esac
+done
+shift $((OPTIND-1))
+
+
+
+echo "Submitting ${file} to ${queue} queue with ${cores} core(s), ${mem} GB RAM, ${time} time"
+return 0
+
+
+
 if [[ $SCHEDULER == "slurm" ]]; then
     srun -t 1-00:00 \
         -p "$queue" \
-        -J "$file_name" \
+        -J "$file" \
         -c "$cores" \
-        --mem-per-cpu="${ram_gb}G" \
+        --mem-per-cpu="${mem}G" \
         Rscript --default-packages="$R_DEFAULT_PACKAGES" \
-            -e "rmarkdown::render('$file_name')"
+            -e "rmarkdown::render('$file')"
 elif [[ $SCHEDULER == "lsf" ]]; then
+    mem_mb="$(($mem * 1024))"
     bsub -W 24:00 \
         -q "$queue" \
-        -J "$file_name" \
+        -J "$file" \
         -n "$cores" \
-        -R rusage[mem="$ram_mb"] \
+        -R rusage[mem="$mem_mb"] \
         Rscript --default-packages="$R_DEFAULT_PACKAGES" \
-            -e "rmarkdown::render('$file_name')"
+            -e "rmarkdown::render('$file')"
+        unset -v mem_mb
 fi
 
-unset -v cores file_name queue ram_gb ram_mb
+unset -f usage
+unset -v cores file mem queue time
