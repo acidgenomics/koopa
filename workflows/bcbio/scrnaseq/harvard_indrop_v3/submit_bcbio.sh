ff92fdb931f4003e45b070e55184ee6edc5c890d
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-27 00:07:29 -0500

    Update slurm and lsf scripts

--- workflows/bcbio/scrnaseq/harvard_indrop_v3/submit_bcbio.sh
+++ workflows/bcbio/scrnaseq/harvard_indrop_v3/submit_bcbio.sh
@@ -2,18 +2,19 @@
 
 # https://slurm.schedmd.com/sbatch.html
 
-#SBATCH --job-name=indrop           # Job name
-#SBATCH --partition=medium          # Partition (queue)
-#SBATCH --time=2-00:00              # Runtime in D-HH:MM format
-#SBATCH --nodes=1                   # Number of nodes (keep at 1, except when using MPI queue)
-#SBATCH --ntasks=1                  # Number of cores per node (keep at 1)
-#SBATCH --cpus-per-task=1           # CPUs per task
-#SBATCH --mem-per-cpu=8G            # Memory needed per CPU
-#SBATCH --output=jobid_%j.out       # File to which STDOUT will be written, including job ID
-#SBATCH --error=jobid_%j.err        # File to which STDERR will be written, including job ID
-#SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
+#SBATCH --job-name=indrop              # Job name
+#SBATCH --partition=medium             # Partition name
+#SBATCH --time=2-00:00                 # Runtime in D-HH:MM format
+#SBATCH --nodes=1                      # Number of nodes (keep at 1, except when using MPI queue)
+#SBATCH --ntasks=1                     # Number of cores per node (keep at 1)
+#SBATCH --cpus-per-task=1              # CPU cores requested per task (change for threaded jobs)
+#SBATCH --mem-per-cpu=8G               # Memory needed per CPU
+#SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
+#SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
+#SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
 
 # Use 6x the number of cores per sample
 # 12 x 6 = 72
+n=72
 
-bcbio_nextgen.py ../config/indrop_rnaseq.yaml -n 72 -t ipython -s slurm -q medium -r t=2-00:00 --retries 3 --timeout 1000
+bcbio_nextgen.py ../config/indrop_rnaseq.yaml -n "$n" -t ipython -s slurm -q medium -r t=2-00:00 --retries 3 --timeout 1000
