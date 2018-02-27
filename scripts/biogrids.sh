65c85281852b0ead7c6f2970c6d176476a1859e2
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-27 09:51:28 -0500

    Remove biogrids

--- scripts/biogrids.sh
+++
@@ -1,9 +0,0 @@
-# http://www.biogrids.org
-if [[ $HPC == "HMS RC Orchestra" ]]; then
-    if [[ -f /programs/biogrids.shrc ]]; then
-        source /programs/biogrids.shrc
-    fi
-else
-    echo "Orchestra HPC required"
-    return 1
-fi
