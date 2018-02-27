bd0155107f5c15f03899dbb5375f9f84859e63b6
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-27 10:01:15 -0500

    Clean up scripts

--- scripts/jobs.sh
+++ scripts/jobs.sh
@@ -1,9 +1,7 @@
 if [[ $SCHEDULER == "slurm" ]]; then
-    command -v squeue >/dev/null 2>&1 || { echo >&2 "squeue missing"; return 1; }
-    squeue -u $USER
+    squeue -u "$USER"
 elif [[ $SCHEDULER == "lsf" ]]; then
-    command -v bjobs >/dev/null 2>&1 || { echo >&2 "bjobs missing"; return 1; }
-    bjobs -u $USER
+    bjobs -u "$USER"
 else
     echo "HPC scheduler required"
     return 1
