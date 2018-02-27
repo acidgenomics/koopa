390614a85d0979002af4de11920072b8ac3da58f
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-26 23:36:10 -0500

    Simplify Harvard cluster support

--- profile/non-interactive/harvard_fas_odyssey.sh
+++ profile/non-interactive/harvard_fas_odyssey.sh
@@ -1,7 +1,7 @@
 # HPC environment variable
 if [[ $HOSTNAME =~ ".rc.fas.harvard.edu" ]] || \
-   [[ $(uname -s) = "Linux" ]] && \
-   [[ -d /n/regal/ ]]
+    [[ $(uname -s) = "Linux" ]] && \
+    [[ -d /n/regal/ ]]
 then
     export HPC="Harvard FAS Odyssey"
     if [[ -z "$BCBIO_DIR" ]]; then
