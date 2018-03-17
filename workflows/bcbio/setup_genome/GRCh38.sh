234ccf51a23fff48c483679626b048b022902bc2
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-17 11:37:52 -0400

    Update and rename GRCh38_90.sh to GRCh38.sh

--- workflows/bcbio/setup_genome/GRCh38.sh
+++ workflows/bcbio/setup_genome/GRCh38.sh
@@ -13,7 +13,7 @@
 #SBATCH --nodes=1                      # Number of nodes (keep at 1)
 #SBATCH --ntasks=1                     # Number of tasks per node (keep at 1)
 #SBATCH --cpus-per-task=8              # CPU cores requested per task (change for threaded jobs)
-#SBATCH --mem-per-cpu=8G               # Memory needed per CPU
+#SBATCH --mem-per-cpu=16G              # Memory needed per CPU
 #SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
 #SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
 #SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
