a69130ab1532f4fd573f98258f6d6214a84172f5
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-23 07:30:26 -0400

    Update scripts
    Signed-off-by: Michael Steinbaugh <mike@steinbaugh.com>

--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -1,40 +1,72 @@
 # Launch an interactive session that lasts for 12 hours
 
-# Exit on HPC detection failure
-if [[ -z $HPC ]]; then
-    echo "HPC required"
+usage () {
+    echo "interactive: [-{c}ores 1 -{m}em 8 -{t}ime 0-06:00]" 1>&2
+}
+
+# Early return usage on empty call
+if [[ $# -eq 0 ]]; then
+    usage
     return 1
 fi
 
-if [[ "$#" -gt "0" ]]; then
-    cores="$1"
-    ram_gb="$2"
-else
-    cores="1"
-    ram_gb="1"
+# Early return on HPC detection failure
+if [[ -z $SCHEDULER ]]; then
+    echo "HPC scheduler required"
+    return 1
 fi
 
-ram_mb="$(($ram_gb * 1024))"
+# All arguments are optional
+cores=1
+mem=1
+if [[ "$SCHEDULER" == "slurm" ]]; then
+    time="0-06:00"
+elif [[ "$SCHEDULER" == "lsf" ]]; then
+    time="6:00"
+fi
 
-echo "Launching interactive session with ${cores} core(s), ${ram_gb} GB RAM"
+# Extract options and their arguments into variables
+while getopts ":c:m:t:" opt; do
+    case ${opt} in
+        c) cores="${OPTARG}";;
+        m) mem="${OPTARG}";;
+        t) time="${OPTARG}";;
+        \?) echo "Invalid option: ${OPTARG}" 1>&2;;
+        :) echo "Invalid option: $OPTARG requires an argument" 1>&2;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Inform the user about the job
+echo "Launching interactive session"
+echo "- cores: ${cores}"
+echo "- memory per core: ${mem} GB"
+echo "- time: ${time}"
+
 export INTERACTIVE_QUEUE=true
 
 if [[ $SCHEDULER == "slurm" ]]; then
-    command -v srun >/dev/null 2>&1 || { echo >&2 "srun missing"; return 1; }
     if [[ $HPC == "Harvard FAS Odyssey" ]]; then
         partition="test"
     else
         partition="interactive"
     fi
-    # `--x11` flag before `/bin/bash` requires `~/.ssh/config` on local machine
-    srun -p "$partition" --pty -c "$cores" --mem "${ram_gb}"G --time 0-8:00 --x11=first /bin/bash
+    # x11 requires `~/.ssh/config` on local machine
+    srun -p "$partition" --pty \
+        -c "$cores" \
+        --mem "${ram_gb}"G \
+        --time "$time" \
+        --x11=first \
+        /bin/bash
     unset -v partition
 elif [[ $SCHEDULER == "lsf" ]]; then
-    command -v bsub >/dev/null 2>&1 || { echo >&2 "bsub missing"; return 1; }
-    bsub -Is -W 8:00 -q interactive -n "$cores" -R rusage[mem="$ram_mb"] bash
-else
-    echo "HPC scheduler required"
-    return 1
+    mem_mb="$(($mem * 1024))"
+    bsub -Is -W "$time" \
+        -q interactive \
+        -n "$cores" \
+        -R rusage[mem="$mem_mb"] \
+        bash
+    unset -v mem_mb
 fi
 
-unset -v cores ram_gb
+unset -v cores mem time
