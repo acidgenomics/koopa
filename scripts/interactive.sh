a7e7d00e5baa1732c7f10a43e095c0af55db0c5c
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-23 08:09:12 -0400

    Update interactive.sh

--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -26,7 +26,7 @@ while getopts ":c:m:t:h" opt; do
         c  ) cores=$OPTARG;;
         m  ) mem=$OPTARG;;
         t  ) time=$OPTARG;;
-        h  ) usage; return 0;;
+        h  ) usage; exit;;
         \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
         :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
         *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
