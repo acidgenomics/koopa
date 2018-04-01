37507d4b595155427b912b2536583da83aa7186d
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-01 11:00:57 -0400

    Need to escape names in find query

--- login/01_non-interactive.sh
+++ login/01_non-interactive.sh
@@ -1,6 +1,6 @@
 # Load non-interactive profile scripts
 where="${SEQCLOUD_DIR}/profile/non-interactive"
-for file in $(find "$where" -type f -name *.sh | sort); do
+for file in $(find "$where" -type f -name "*.sh" ! -name ".*" | sort); do
     . "$file"
 done
 unset -v file where
--- login/02_interactive.sh
+++ login/02_interactive.sh
@@ -1,7 +1,7 @@
 # Load interactive profile scripts
 if [[ -n "$PS1" ]]; then
     where="${SEQCLOUD_DIR}/profile/interactive"
-    for file in $(find "$where" -type f -name "*.sh" | sort); do
+    for file in $(find "$where" -type f -name "*.sh" ! -name ".*" | sort); do
         . "$file"
     done
     unset -v file where
