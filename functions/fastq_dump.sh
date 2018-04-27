81b0c0b5b2906d2b23b17220357df5b0c0ae70bb
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-27 14:50:14 -0400

    Update fastq_dump.sh

--- functions/fastq_dump.sh
+++ functions/fastq_dump.sh
@@ -6,9 +6,9 @@ command -v fastq-dump >/dev/null 2>&1 || {
     return 1
 }
 
-while read name; do
-    if [[ ! -e $name.fastq.gz ]] && [[ ! -e $name_1.fastq.gz ]]; then
-        echo "$name"
-        fastq-dump --gzip --split-3 --accession "$name"
+while read accession; do
+    if [[ ! -e $accession.fastq.gz ]] && [[ ! -e $accession_1.fastq.gz ]]; then
+        echo "$accession"
+        fastq-dump --gzip --split-3 "$accession"
     fi
 done < SRR_Acc_List.txt
