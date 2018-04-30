9dfe6534fc84421e508947aa61fd124e508f8613
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-30 12:21:29 -0400

    Simplify internal code

--- functions/gtf.sh
+++ functions/gtf.sh
@@ -1,30 +1,29 @@
 organism="$1"
-ensembl="$ENSEMBL_RELEASE_PATH/gtf"
+ensembl="${ENSEMBL_RELEASE_URL}/gtf"
 
 if [[ $organism == "hsapiens" ]]; then
     echo "Homo sapiens"
     echo "Ensembl GRCh38"
-    request="$ensembl/homo_sapiens/Homo_sapiens.GRCh38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
+    request="${ensembl}/homo_sapiens/Homo_sapiens.GRCh38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
 elif [[ $organism == "mmusculus" ]]; then
     echo "Mus musculus"
     echo "Ensembl GRCm38"
-    request="$ensembl/mus_musculus/Mus_musculus.GRCm38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
+    request="${ensembl}/mus_musculus/Mus_musculus.GRCm38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
 elif [[ $organism == "celegans" ]]; then
-    # Use WormBase annotations instead of Ensembl?
+    # TODO Use WormBase instead of Ensembl?
     echo "Caenorhabditis elegans"
     echo "Ensembl WBcel235"
-    request="$ensembl/caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.${ENSEMBL_RELEASE}.gtf.gz"
+    request="${ensembl}/caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.${ENSEMBL_RELEASE}.gtf.gz"
 elif [[ $organism == "dmelanogaster" ]]; then
     # D. melanogaster Ensembl annotations are out of date.
-    # Use the FlyBase annotations instead.
+    # Using the FlyBase annotations instead.
     echo "Drosophila melanogaster"
-    echo "FlyBase $FLYBASE_RELEASE r${FLYBASE_RELEASE_VERSION}"
-    request="$FLYBASE_RELEASE_PATH/gtf/dmel-all-r${FLYBASE_RELEASE_VERSION}.gtf.gz"
+    echo "FlyBase ${FLYBASE_RELEASE_DATE} ${FLYBASE_RELEASE_VERSION}"
+    request="${FLYBASE_RELEASE_URL}/gtf/dmel-all-${FLYBASE_RELEASE_VERSION}.gtf.gz"
 fi
 
 wget "$request"
 gtf=$(basename "$request")
-# Extract but keep original compressed file
 gunzip -c "$gtf" > "${gtf%.*}"
 
 unset -v ensembl organism request
