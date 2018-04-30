9c202a2fc14854ac7b05dcd3e6402728483ffe6c
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-30 12:43:11 -0400

    Improve variable names in internal code

--- functions/fasta.sh
+++ functions/fasta.sh
@@ -1,62 +1,72 @@
-# https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
-
 organism="$1"
 type="$2"
-ensembl="$ENSEMBL_RELEASE_PATH/fasta"
+
+ensembl="${ENSEMBL_RELEASE_URL}/fasta"
+download=true
+
+# Warn on transcriptome usage
+if [[ $type == "transcriptome" ]]; then
+    echo "Use `cdna` instead of `transcriptome`"
+    type="cdna"
+fi
 
 if [[ $organism == "hsapiens" ]]; then
     echo "Homo sapiens"
     echo "Ensembl GRCh38"
     if [[ $type == "dna" ]]; then
-        request="$ensembl/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
+        remote="${ensembl}/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
     elif [[ $type == "cdna" ]]; then
-        request="$ensembl/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz"
+        remote="${ensembl}/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz"
     fi
 elif [[ $organism == "mmusculus" ]]; then
     echo "Mus musculus"
     echo "Ensembl GRCm38"
     if [[ $type == "dna" ]]; then
-        request="$ensembl/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz"
+        remote="${ensembl}/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz"
     elif [[ $type == "cdna" ]]; then
-        request="$ensembl/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz"
+        remote="${ensembl}/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz"
     fi
 elif [[ $organism == "celegans" ]]; then
-    # Use WormBase annotations instead of Ensembl?
+    # TODO Use WormBase instead of Ensembl?
     echo "Caenorhabditis elegans"
     echo "Ensembl WBcel235"
     if [[ $type == "dna" ]]; then
-        # request="ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/genomic/c_elegans.canonical_bioproject.current.genomic.fa.gz"
-        request="$ensembl/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz"
+        # remote="ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/genomic/c_elegans.canonical_bioproject.current.genomic.fa.gz"
+        remote="${ensembl}/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz"
     elif [[ $type == "cdna" ]]; then
-        # request="ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.canonical_bioproject.current.mRNA_transcripts.fa.gz"
-        request="$ensembl/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz"
+        # remote="ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.canonical_bioproject.current.mRNA_transcripts.fa.gz"
+        remote="${ensembl}/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz"
     fi
 elif [[ $organism == "dmelanogaster" ]]; then
     # D. melanogaster Ensembl annotations are out of date (2014).
-    # Use the FlyBase annotations instead.
+    # Using the FlyBase annotations instead.
     echo "Drosophila melanogaster"
-    echo "FlyBase $FLYBASE_RELEASE r${FLYBASE_RELEASE_VERSION}"
-    flybase="$FLYBASE_RELEASE_PATH/fasta"
+    echo "FlyBase ${FLYBASE_RELEASE_DATE} ${FLYBASE_RELEASE_VERSION}"
+    flybase="${FLYBASE_RELEASE_URL}/fasta"
     if [[ $type == "dna" ]]; then
-        request="$flybase/dmel-all-aligned-r${FLYBASE_RELEASE_VERSION}.fasta.gz"
+        remote="${flybase}/dmel-all-aligned-${FLYBASE_RELEASE_VERSION}.fasta.gz"
     elif [[ $type == "cdna" ]]; then
-        request="$flybase/dmel-all-transcript-r${FLYBASE_RELEASE_VERSION}.fasta.gz"
+        echo "HELLO WORLD"
+        seqcloud flybase_transcriptome
+        download=false
     fi
 elif [[ $organism == "nfurzeri" ]]; then
     echo "Nothobranchius furzeri (turquoise killifish)"
     echo "NFINgb GRZ Assembly"
     nfingb="http://nfingb.leibniz-fli.de/data/raw/notho4"
     if [[ $type == "dna" ]]; then
-        # $request="http://africanturquoisekillifishbrowser.org/NotFur1_genome_draft.fa.tar.gz"
-        request="$nfingb/Nfu_20150522.softmasked_genome.fa.gz"
+        # $remote="http://africanturquoisekillifishbrowser.org/NotFur1_genome_draft.fa.tar.gz"
+        remote="${nfingb}/Nfu_20150522.softmasked_genome.fa.gz"
     elif [[ $type == "cdna" ]]; then
-        request="$nfingb/Nfu_20150522.genes_20150922.transcripts.fa.gz"
+        remote="${nfingb}/Nfu_20150522.genes_20150922.transcripts.fa.gz"
     fi
 fi
 
-wget "$request"
-fasta=$(basename "$request")
-# Extract but keep original compressed file
-gunzip -c "$fasta" > "${fasta%.*}"
+if [[ $download == true ]]; then
+    echo $remote
+    wget "$remote"
+    local=$(basename "$remote")
+    gunzip -c "$local" > "${local%.*}"
+fi
 
-unset -v organism request type
+unset -v download ensembl local organism remote type
--- functions/gtf.sh
+++ functions/gtf.sh
@@ -1,29 +1,30 @@
 organism="$1"
+
 ensembl="${ENSEMBL_RELEASE_URL}/gtf"
 
 if [[ $organism == "hsapiens" ]]; then
     echo "Homo sapiens"
     echo "Ensembl GRCh38"
-    request="${ensembl}/homo_sapiens/Homo_sapiens.GRCh38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
+    remote="${ensembl}/homo_sapiens/Homo_sapiens.GRCh38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
 elif [[ $organism == "mmusculus" ]]; then
     echo "Mus musculus"
     echo "Ensembl GRCm38"
-    request="${ensembl}/mus_musculus/Mus_musculus.GRCm38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
+    remote="${ensembl}/mus_musculus/Mus_musculus.GRCm38.${ENSEMBL_RELEASE}.chr_patch_hapl_scaff.gtf.gz"
 elif [[ $organism == "celegans" ]]; then
     # TODO Use WormBase instead of Ensembl?
     echo "Caenorhabditis elegans"
     echo "Ensembl WBcel235"
-    request="${ensembl}/caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.${ENSEMBL_RELEASE}.gtf.gz"
+    remote="${ensembl}/caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.${ENSEMBL_RELEASE}.gtf.gz"
 elif [[ $organism == "dmelanogaster" ]]; then
     # D. melanogaster Ensembl annotations are out of date.
     # Using the FlyBase annotations instead.
     echo "Drosophila melanogaster"
     echo "FlyBase ${FLYBASE_RELEASE_DATE} ${FLYBASE_RELEASE_VERSION}"
-    request="${FLYBASE_RELEASE_URL}/gtf/dmel-all-${FLYBASE_RELEASE_VERSION}.gtf.gz"
+    remote="${FLYBASE_RELEASE_URL}/gtf/dmel-all-${FLYBASE_RELEASE_VERSION}.gtf.gz"
 fi
 
-wget "$request"
-gtf=$(basename "$request")
-gunzip -c "$gtf" > "${gtf%.*}"
+wget "$remote"
+local=$(basename "$remote")
+gunzip -c "$local" > "${local%.*}"
 
-unset -v ensembl organism request
+unset -v ensembl local organism remote
