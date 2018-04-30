6dbbf85e158ce9e4a8892f75979c8293753202a3
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-30 12:56:00 -0400

    Consolidate FASTA code

--- functions/fasta.sh
+++ functions/fasta.sh
@@ -1,8 +1,8 @@
 organism="$1"
 type="$2"
 
-ensembl="${ENSEMBL_RELEASE_URL}/fasta"
-download=true
+# Default to Ensembl
+base_url="${ENSEMBL_RELEASE_URL}/fasta"
 
 # Warn on transcriptome usage
 if [[ $type == "transcriptome" ]]; then
@@ -14,59 +14,74 @@ if [[ $organism == "hsapiens" ]]; then
     echo "Homo sapiens"
     echo "Ensembl GRCh38"
     if [[ $type == "dna" ]]; then
-        remote="${ensembl}/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
+        remote="${base_url}/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
     elif [[ $type == "cdna" ]]; then
-        remote="${ensembl}/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz"
+        remote="${base_url}/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz"
     fi
 elif [[ $organism == "mmusculus" ]]; then
     echo "Mus musculus"
     echo "Ensembl GRCm38"
     if [[ $type == "dna" ]]; then
-        remote="${ensembl}/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz"
+        remote="${base_url}/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz"
     elif [[ $type == "cdna" ]]; then
-        remote="${ensembl}/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz"
+        remote="${base_url}/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz"
     fi
 elif [[ $organism == "celegans" ]]; then
     # TODO Use WormBase instead of Ensembl?
     echo "Caenorhabditis elegans"
     echo "Ensembl WBcel235"
     if [[ $type == "dna" ]]; then
+        remote="${base_url}/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz"
         # remote="ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/genomic/c_elegans.canonical_bioproject.current.genomic.fa.gz"
-        remote="${ensembl}/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz"
     elif [[ $type == "cdna" ]]; then
+        remote="${base_url}/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz"
         # remote="ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.canonical_bioproject.current.mRNA_transcripts.fa.gz"
-        remote="${ensembl}/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz"
     fi
 elif [[ $organism == "dmelanogaster" ]]; then
-    # D. melanogaster Ensembl annotations are out of date (2014).
+    # D. melanogaster Ensembl annotations are out of date.
     # Using the FlyBase annotations instead.
     echo "Drosophila melanogaster"
     echo "FlyBase ${FLYBASE_RELEASE_DATE} ${FLYBASE_RELEASE_VERSION}"
-    flybase="${FLYBASE_RELEASE_URL}/fasta"
+    
+    base_url="$FLYBASE_RELEASE_URL/fasta"
+    version="$FLYBASE_RELEASE_VERSION"
+    
     if [[ $type == "dna" ]]; then
-        remote="${flybase}/dmel-all-aligned-${FLYBASE_RELEASE_VERSION}.fasta.gz"
+        remote="${base_url}/dmel-all-aligned-${version}.fasta.gz"
     elif [[ $type == "cdna" ]]; then
-        echo "HELLO WORLD"
-        seqcloud flybase_transcriptome
+        wget "${base_url}/dmel-all-transcript-${version}.fasta.gz"
+        wget "${base_url}/dmel-all-miRNA-${version}.fasta.gz"
+        wget "${base_url}/dmel-all-miscRNA-${version}.fasta.gz"
+        wget "${base_url}/dmel-all-ncRNA-${version}.fasta.gz"
+        wget "${base_url}/dmel-all-pseudogene-${version}.fasta.gz"
+        wget "${base_url}/dmel-all-tRNA-${version}.fasta.gz"
+
+        # Concatenate into single FASTA
+        file=dmel-transcriptome-${version}.fasta.gz
+        cat dmel-all-*-${version}.fasta.gz > $file
+
         download=false
+        unset -v version
     fi
 elif [[ $organism == "nfurzeri" ]]; then
     echo "Nothobranchius furzeri (turquoise killifish)"
     echo "NFINgb GRZ Assembly"
-    nfingb="http://nfingb.leibniz-fli.de/data/raw/notho4"
+    base_url="http://nfingb.leibniz-fli.de/data/raw/notho4"
     if [[ $type == "dna" ]]; then
+        remote="${base_url}/Nfu_20150522.softmasked_genome.fa.gz"
         # $remote="http://africanturquoisekillifishbrowser.org/NotFur1_genome_draft.fa.tar.gz"
-        remote="${nfingb}/Nfu_20150522.softmasked_genome.fa.gz"
     elif [[ $type == "cdna" ]]; then
-        remote="${nfingb}/Nfu_20150522.genes_20150922.transcripts.fa.gz"
+        remote="${base_url}/Nfu_20150522.genes_20150922.transcripts.fa.gz"
     fi
 fi
 
-if [[ $download == true ]]; then
-    echo $remote
+if [[ -n $remote ]]; then
+    echo "$remote"
     wget "$remote"
-    local=$(basename "$remote")
-    gunzip -c "$local" > "${local%.*}"
+    file=$(basename "$remote")
 fi
 
-unset -v download ensembl local organism remote type
+# Decompress but keep compressed copy
+gunzip -c "$file" > "${file%.*}"
+
+unset -v base_url download file organism remote type
