f6179328d6d872d8a352285b592ef2485b5de0e5
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-30 09:41:45 -0400

    Update scheduler.sh

--- profile/non-interactive/scheduler.sh
+++ profile/non-interactive/scheduler.sh
@@ -1,5 +1,7 @@
 if [[ ! -z "$SLURM_CONF" ]] || [[ ! -z "$SQUEUE_USER" ]]; then
     export SCHEDULER="slurm"
+    alias j="squeue -u $USER"
 elif [[ ! -z "$LSF_ENVDIR" ]]; then
     export SCHEDULER="lsf"
+    alias j="bjobs"
 fi
