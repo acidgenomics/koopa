56ff8f3128c30081af5335618f61aac0502b92f9
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-07 12:07:12 -0400

    Update prepare_bcbio.sh

--- workflows/bcbio/prepare_bcbio.sh
+++ workflows/bcbio/prepare_bcbio.sh
@@ -1,9 +1,11 @@
 bcbio_nextgen.py -w template bcbio.yaml bcbio.csv *.fastq.gz
 
 cd bcbio/work
-cp ../../submit_bcbio.sh .
-sbatch submit_bcbio.sh
 
+# slurm
+cp ../../sbatch_bcbio.sh .
+sbatch submit_bcbio.sh
 squeue -u $USER
+
 less *.err
 tree
