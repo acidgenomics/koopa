8fc53bd76ec33151f629a499cd9fde03982602d4
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-18 15:06:40 -0400

    Update cellranger scripts

---
+++ workflows/cellranger/cellranger.sh
@@ -0,0 +1,44 @@
+# Cell Ranger
+# 2018-06-18
+
+module load bcl2fastq/2.20.0.422
+module load cellranger/2.1.1
+
+localcores=$SLURM_CPUS_PER_TASK
+localmem=32
+
+
+
+# mkfastq ======================================================================
+# https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/using/mkfastq
+# Point to the Illumina BCL run directory
+# Use a CSV file to specify the samples (see 10X website for examples)
+# cellranger mkfastq --help
+# If you want to remove the Undetermined FASTQs from the output to save space, you can run mkfastq with the --delete-undetermined
+cellranger mkfastq \
+    --run=XXXXXXXXX \
+    --csv=mkfastq.csv \
+    --localcores=$localcores \
+    --localmem=$localmem
+
+
+
+# count ========================================================================
+# https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/using/count
+fastqs="XXXXXXXXX"
+transcriptome="~/refdata-cellranger-mm10-1.2.0"
+
+cellranger count \
+    --id=sample1 \
+    --sample=sample1 \
+    --fastqs=$fastqs  \
+    --transcriptome=$transcriptome \
+    --localcores=$localcores \
+    --localmem=$localmem
+cellranger count \
+    --id=sample2 \
+    --sample=sample2 \
+    --fastqs=$fastqs  \
+    --transcriptome=$transcriptome \
+    --localcores=$localcores \
+    --localmem=$localmem
---
+++ workflows/cellranger/sbatch_cellranger.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# SLURM
+# https://slurm.schedmd.com/sbatch.html
+
+#SBATCH --job-name=cellranger          # Job name
+#SBATCH --partition=medium             # Partition name
+#SBATCH --time=1-00:00                 # Runtime in D-HH:MM format
+#SBATCH --nodes=1                      # Number of nodes (keep at 1, except when using MPI queue)
+#SBATCH --ntasks=1                     # Number of cores per node (keep at 1)
+#SBATCH --cpus-per-task=8              # CPU cores requested per task (change for threaded jobs)
+#SBATCH --mem=64G                      # Memory needed per node (total)
+#SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
+#SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
+#SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
+
+bash cellranger.sh
