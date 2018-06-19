426c2c5cbcd2aa1ae1aadab8bf914e8a9cbc9598
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-19 13:00:55 -0400

    Update sbatch_cellranger.sh

--- workflows/cellranger/sbatch_cellranger.sh
+++ workflows/cellranger/sbatch_cellranger.sh
@@ -5,11 +5,11 @@
 
 #SBATCH --job-name=cellranger          # Job name
 #SBATCH --partition=medium             # Partition name
-#SBATCH --time=2-00:00                 # Runtime in D-HH:MM format
+#SBATCH --time=3-00:00                 # Runtime in D-HH:MM format
 #SBATCH --nodes=1                      # Number of nodes (keep at 1, except when using MPI queue)
 #SBATCH --ntasks=1                     # Number of cores per node (keep at 1)
-#SBATCH --cpus-per-task=32             # CPU cores requested per task (change for threaded jobs)
-#SBATCH --mem=256G                     # Memory needed per node (total)
+#SBATCH --cpus-per-task=16             # CPU cores requested per task (change for threaded jobs)
+#SBATCH --mem=128G                     # Memory needed per node (total)
 #SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
 #SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
 #SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
