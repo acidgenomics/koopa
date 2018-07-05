1e604e062f17d87e7f2454c11987e9cffc0940d8
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-05 12:22:28 -0400

    Update sbatch_GRCm38.sh

--- workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCm38.sh
+++ workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCm38.sh
@@ -3,7 +3,7 @@
 # bcbio_setup_genome.py\
 # Harvard Medical School O2 Cluster
 # Mus musculus
-# 2018-06-27
+# 2018-07-05
 
 # SLURM
 # https://slurm.schedmd.com/sbatch.html
@@ -14,7 +14,7 @@
 #SBATCH --nodes=1                      # Number of nodes (keep at 1)
 #SBATCH --ntasks=1                     # Number of tasks per node (keep at 1)
 #SBATCH --cpus-per-task=8              # CPU cores requested per task (change for threaded jobs)
-#SBATCH --mem-per-cpu=8G               # Memory needed per CPU
+#SBATCH --mem=128G                     # Memory needed per node
 #SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
 #SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
 #SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
