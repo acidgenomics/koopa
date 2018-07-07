6242e9dedb6cb37446de8ddf2f52dc6b07d2fb49
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-07 19:59:27 -0400

    Update sbatch_GRCh38.sh

--- workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCh38.sh
+++ workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCh38.sh
@@ -3,10 +3,13 @@
 # bcbio_setup_genome.py
 # Harvard Medical School O2 Cluster
 # Homo sapiens
-# 2018-07-05
+# 2018-07-07
 
 # SLURM
 # https://slurm.schedmd.com/sbatch.html
+
+# Use highmem partition instead if there are memory issues with hisat2.
+# medium partition has a memory limit of 250 GB.
 
 #SBATCH --job-name=GRCh38              # Job name
 #SBATCH --partition=medium             # Partition name
@@ -14,7 +17,7 @@
 #SBATCH --nodes=1                      # Number of nodes (keep at 1)
 #SBATCH --ntasks=1                     # Number of tasks per node (keep at 1)
 #SBATCH --cpus-per-task=8              # CPU cores requested per task (change for threaded jobs)
-#SBATCH --mem=128G                     # Memory needed per node
+#SBATCH --mem=250G                     # Memory needed per node
 #SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
 #SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
 #SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
