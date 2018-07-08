733126007b1deb300f5bc59aa75af2d3589c262c
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-07 20:02:03 -0400

    Update sbatch_GRCm38.sh

--- workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCm38.sh
+++ workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCm38.sh
@@ -13,7 +13,7 @@
 #SBATCH --time=3-00:00                 # Runtime in D-HH:MM format
 #SBATCH --nodes=1                      # Number of nodes (keep at 1)
 #SBATCH --ntasks=1                     # Number of tasks per node (keep at 1)
-#SBATCH --cpus-per-task=8              # CPU cores requested per task (change for threaded jobs)
+#SBATCH --cpus-per-task=1              # CPU cores requested per task (change for threaded jobs)
 #SBATCH --mem=250G                     # Memory needed per node
 #SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
 #SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
