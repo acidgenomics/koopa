aaea0aa208eea00071aedae129fe8937a8c7edc5
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-09 10:34:22 -0400

    Update sbatch_GRCm38.sh

--- workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCm38.sh
+++ workflows/bcbio/scripts/bcbio_setup_genome/sbatch_GRCm38.sh
@@ -8,13 +8,15 @@
 # SLURM
 # https://slurm.schedmd.com/sbatch.html
 
+# Don't build hisat2 by default, it's very memory intensive
+
 #SBATCH --job-name=GRCm38              # Job name
 #SBATCH --partition=medium             # Partition name
 #SBATCH --time=3-00:00                 # Runtime in D-HH:MM format
 #SBATCH --nodes=1                      # Number of nodes (keep at 1)
 #SBATCH --ntasks=1                     # Number of tasks per node (keep at 1)
 #SBATCH --cpus-per-task=1              # CPU cores requested per task (change for threaded jobs)
-#SBATCH --mem=250G                     # Memory needed per node
+#SBATCH --mem=128G                     # Memory needed per node
 #SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
 #SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
 #SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
@@ -56,4 +58,4 @@ if [[ ! -f "$gtf" ]]; then
     gunzip -c "${gtf}.gz" > "$gtf"
 fi
 
-bcbio_setup_genome.py -c "$cores" -f "$fasta" -g "$gtf" -i bowtie2 bwa hisat2 seq star -n "$name" -b "$build"
+bcbio_setup_genome.py -c "$cores" -f "$fasta" -g "$gtf" -i bowtie2 bwa seq star -n "$name" -b "$build"
