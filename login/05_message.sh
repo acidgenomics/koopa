542d1946a6fb43f6e95be894d4bd6b237b814d52
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-20 08:58:04 -0400

    Update 05_message.sh

--- login/05_message.sh
+++ login/05_message.sh
@@ -1,8 +1,11 @@
+separator_bar="🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢"
+
 # Check for prompt string, SSH connection, and not on interactive node.
 # Only show this message on the login node for HPC.
 if [[ -n "$PS1" && -n "$SSH_CLIENT" && -z "$INTERACTIVE_QUEUE" ]]; then
     echo ""
-    echo "==== koopa shell =========================================================="
+    echo "$separator_bar"
+    echo "koopa shell"
     echo "https://github.com/steinbaugh/koopa"
     if [[ -d "$ASPERA_DIR" ]]; then
         echo "# Aspera Connect"
@@ -16,5 +19,8 @@ if [[ -n "$PS1" && -n "$SSH_CLIENT" && -z "$INTERACTIVE_QUEUE" ]]; then
         echo "# conda"
         echo "$CONDA_DIR"
     fi
-    echo "==========================================================================="
+    echo "$separator_bar"
+    echo ""
 fi
+
+unset -v separator_bar
