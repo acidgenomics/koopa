565cf5690b52986467736c9d4531bafdfb0b15a9
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-28 09:14:15 -0400

    Update functions

--- functions/install_tex.sh
+++ functions/install_tex.sh
@@ -1,5 +1,28 @@
-# https://www.ctan.org/
-brew cask install basictex
+# Install Tex (basictex)
+# https://www.ctan.org
+# This will also attempt to install recommended packages.
+
+# Attempt to install basictex automatically.
+# This contains tlmgr, which manages CTAN packages.
+command -v tlmgr >/dev/null 2>&1 || {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS
+        echo "Installing basictex cask"
+        command -v brew >/dev/null 2>&1 || {
+            echo >&2 "brew missing"
+            return 1
+        }
+        brew cask install basictex 
+    fi
+}
+
+# Check for tlmgr again and error if not installed.
+command -v tlmgr >/dev/null 2>&1 || {
+    echo >&2 "tlmgr missing"
+    return 1
+}
+
+# Now run the updater and install recommended packages.
 sudo tlmgr update --self
 packages=(collection-fontsrecommended  # priority
           collection-latexrecommended  # priority
