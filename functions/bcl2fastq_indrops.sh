a3d032a9bf43ece53f1b095d2dab0156824a3f80
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-08-28 09:14:15 -0400

    Update functions

---
+++ functions/bcl2fastq_indrops.sh
@@ -0,0 +1,10 @@
+command -v bcl2fastq >/dev/null 2>&1 || {
+    echo >&2 "bcl2fastq missing"
+    return 1
+}
+
+bcl2fastq \
+    --use-bases-mask y*,y*,y*,y* \
+    --mask-short-adapter-reads 0 \
+    --minimum-trimmed-read-length 0 \
+    > bcl2fastq.log 2>&1
