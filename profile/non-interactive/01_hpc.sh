cfc0f4037ea660554303571466085fca0742aabe
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-04 17:21:32 -0400

    Update 01_hpc.sh

--- profile/non-interactive/01_hpc.sh
+++ profile/non-interactive/01_hpc.sh
@@ -7,9 +7,9 @@ if [[ $HMS_CLUSTER == "o2" ]] && \
     [[ -d /n/data1/ ]]
 then
     export HPC="HMS RC O2"
-    if [[ -z "$BCBIO_DIR" ]]; then
-        export BCBIO_DIR="/n/app/bcbio/tools/bin"
-    fi
+    # if [[ -z "$BCBIO_DIR" ]]; then
+    #     export BCBIO_DIR="/n/app/bcbio/tools/bin"
+    # fi
 fi
 
 # FAS Odyssey (O3)
@@ -18,7 +18,7 @@ if [[ $HOSTNAME =~ ".rc.fas.harvard.edu" ]] || \
     [[ -d /n/regal/ ]]
 then
     export HPC="Harvard FAS Odyssey"
-    if [[ -z "$BCBIO_DIR" ]]; then
-        export BCBIO_DIR="/n/regal/hsph_bioinfo/bcbio_nextgen/bin"
-    fi
+    # if [[ -z "$BCBIO_DIR" ]]; then
+    #     export BCBIO_DIR="/n/regal/hsph_bioinfo/bcbio_nextgen/bin"
+    # fi
 fi
