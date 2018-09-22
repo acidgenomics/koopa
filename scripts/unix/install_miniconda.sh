b6efcad8a975bc8310f72baf0a3e0c075dd3a70a
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-22 09:56:09 -0400

    Reorganize scripts

---
+++ scripts/unix/install_miniconda.sh
@@ -0,0 +1,28 @@
+# https://conda.io/miniconda.html
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    script="Miniconda3-latest-Linux-x86_64.sh"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    script="Miniconda3-latest-MacOSX-x86_64.sh"
+else
+    echo "${OSTYPE} operating system not supported"
+    return 1
+fi
+
+echo "Downloading Miniconda installer..."
+curl -O "https://repo.continuum.io/miniconda/${script}"
+
+cat << EOF
+koopa is able to load conda automatically.
+Don't allow conda to modify bashrc.
+Instead, export "CONDA_DIR" in bashrc to point to the conda "bin" directory.
+This must be included prior to sourcing koopa.
+EOF
+
+bash "$script"
+
+cat << EOF
+conda installed successfully.
+Export "CONDA_DIR" in bashrc and reload the shell.
+Use "which conda" to check the path.
+EOF
