911960595a478ec5dbab419df3347b56acfb8a86
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-23 10:58:04 -0400

    Update listing script to work on CentOS

--- system/list.sh
+++ system/list.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# List available binaries.
-# 2018-09-22
+# List available scripts.
+# 2018-09-23
 
 find "$KOOPA_BINDIR" \
     -maxdepth 1 -type f \
-    -not -name ".*" -not -name "koopa" \
-    -print0 | \
-    xargs -0 basename | sort
+    -not -name ".*" -not -name "koopa"
+    
+    # -print0 | \
+    # xargs -0 basename | sort
