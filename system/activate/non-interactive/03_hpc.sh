f3035cddf0c92c4afc09572d40697c8d47fb3ee4
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-23 11:22:56 -0400

    Fix config for Odyssey

--- system/activate/non-interactive/03_hpc.sh
+++ system/activate/non-interactive/03_hpc.sh
@@ -10,6 +10,35 @@ else
     return 0
 fi
 
+# Harvard University ===========================================================
+if   [[ $HMS_CLUSTER == "o2" ]] && \
+     [[ $HOSTNAME =~ ".o2.rc.hms.harvard.edu" ]] && \
+     [[ -d /n/data1/ ]]
+then
+    export HPC_NAME="Harvard HMS O2"
+    # https://wiki.rc.hms.harvard.edu/display/O2/Using+Slurm+Basic
+    # Automatically export bcbio into PATH, if necessary.
+    if [[ ! -d "$BCBIO_DIR" ]]; then
+        export BCBIO_DIR="/n/app/bcbio/tools/bin"
+    fi
+elif [[ $HOSTNAME =~ ".rc.fas.harvard.edu" ]] && \
+     [[ -d /n/regal/ ]]
+then
+    export HPC_NAME="Harvard FAS Odyssey"
+    # https://www.rc.fas.harvard.edu/resources/running-jobs/
+    # Automatically export bcbio into PATH, if necessary.
+    if [[ ! -d "$BCBIO_DIR" ]]; then
+        export BCBIO_DIR="/n/regal/hsph_bioinfo/bcbio_nextgen/bin"
+    fi
+    # Change the default partitions, if necessary.
+    if [[ -z "$HPC_PARTITION_DEFAULT" ]]; then
+        export HPC_PARTITION_DEFAULT="shared"
+    fi
+    if [[ -z "$HPC_PARTITION_INTERACTIVE" ]]; then
+        export HPC_PARTITION_INTERACTIVE="test"
+    fi
+fi
+
 # Default configuration ========================================================
 if [[ -z "$HPC_PARTITION_DEFAULT" ]]; then
     export HPC_PARTITION_DEFAULT="short"
@@ -40,34 +69,5 @@ elif [[ "$HPC_SCHEDULER" == "LSF" ]]; then
     alias j="bjobs"
     if [[ -z "$HPC_TIME" ]]; then
         export HPC_TIME="6:00"
-    fi
-fi
-
-# Harvard University ===========================================================
-if   [[ $HMS_CLUSTER == "o2" ]] && \
-     [[ $HOSTNAME =~ ".o2.rc.hms.harvard.edu" ]] && \
-     [[ -d /n/data1/ ]]
-then
-    export HPC_NAME="Harvard HMS O2"
-    # https://wiki.rc.hms.harvard.edu/display/O2/Using+Slurm+Basic
-    # Automatically export bcbio into PATH, if necessary.
-    if [[ ! -d "$BCBIO_DIR" ]]; then
-        export BCBIO_DIR="/n/app/bcbio/tools/bin"
-    fi
-elif [[ $HOSTNAME =~ ".rc.fas.harvard.edu" ]] && \
-     [[ -d /n/regal/ ]]
-then
-    export HPC_NAME="Harvard FAS Odyssey"
-    # https://www.rc.fas.harvard.edu/resources/running-jobs/
-    # Automatically export bcbio into PATH, if necessary.
-    if [[ ! -d "$BCBIO_DIR" ]]; then
-        export BCBIO_DIR="/n/regal/hsph_bioinfo/bcbio_nextgen/bin"
-    fi
-    # Change the default partitions, if necessary.
-    if [[ -z "$HPC_PARTITION_DEFAULT" ]]; then
-        export HPC_PARTITION_DEFAULT="shared"
-    fi
-    if [[ -z "$HPC_PARTITION_INTERACTIVE" ]]; then
-        export HPC_PARTITION_DEFAULT="test"
     fi
 fi
