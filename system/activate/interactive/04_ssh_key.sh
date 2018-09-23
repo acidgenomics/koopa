611de37944ce6a49297bb703ac8515dd001274c7
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-23 08:31:05 -0400

    Update system scripts

--- system/activate/interactive/04_ssh_key.sh
+++ system/activate/interactive/04_ssh_key.sh
@@ -1,7 +1,7 @@
 # Load an SSH key automatically.
 # SCP will fail unless this is interactive only.
 # Only applies when `$SSH_KEY` is set in environment.
-if [[ -n "$PS1" ]] && [[ -f "$SSH_KEY" ]]; then
+if [[ -f "$SSH_KEY" ]]; then
     eval "$(ssh-agent -s)"
     ssh-add "$SSH_KEY"
 fi
