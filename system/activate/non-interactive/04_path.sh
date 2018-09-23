b586f81e6a229753b10ea4354d6699a09d197c54
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-23 10:39:13 -0400

    Draft update to improve Harvard support

--- system/activate/non-interactive/04_path.sh
+++ system/activate/non-interactive/04_path.sh
@@ -31,11 +31,7 @@ fi
 
 # Aspera Connect ===============================================================
 if [[ -z "$ASPERA_DIR" ]]; then
-    aspera_dir="${HOME}/.aspera/connect/bin"
-    if [[ -d "$aspera_dir" ]]; then
-        export ASPERA_DIR="$aspera_dir"
-    fi
-    unset -v aspera_dir
+    ASPERA_DIR="${HOME}/.aspera/connect/bin"
 fi
 if [[ -d "$ASPERA_DIR" ]]; then
     export PATH="${ASPERA_DIR}:${PATH}"
