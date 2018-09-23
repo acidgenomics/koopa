78212ccc5f906db95652e55f5b032809290142fb
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-23 11:15:23 -0400

    Simplify file list method

--- system/list.sh
+++ system/list.sh
@@ -5,7 +5,5 @@
 
 find "$KOOPA_BINDIR" \
     -maxdepth 1 -type f \
-    -not -name ".*" -not -name "koopa"
-    
-    # -print0 | \
-    # xargs -0 basename | sort
+    -not -name ".*" -not -name "koopa" \
+    -printf "%f\n" | sort
