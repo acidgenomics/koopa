2c84168397099bfed8a6e7da1096746fdaf6a6e7
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-22 12:57:39 -0400

    Improve aspera, bcbio, conda handling

--- system/activate/non-interactive/04_path.sh
+++ system/activate/non-interactive/04_path.sh
@@ -13,41 +13,47 @@ fi
 
 # koopa ========================================================================
 # Export general koopa scripts.
-export PATH="${KOOPA_BINDIR}:${PATH}"
+export PATH="${KOOPA_BIN_DIR}:${PATH}"
 
 # Export HPC scheduler scripts.
 if [[ -n "$HPC_SCHEDULER" ]]; then
-    export PATH="${KOOPA_BINDIR}/hpc:${PATH}"
+    export PATH="${KOOPA_BIN_DIR}/hpc:${PATH}"
 fi
 
 # Export additional OS-specific scripts.
 if [[ "$KOOPA_SYSTEM" =~ "Darwin"* ]]; then
     # macOS
-    export PATH="${KOOPA_BINDIR}/darwin:${PATH}"
+    export PATH="${KOOPA_BIN_DIR}/darwin:${PATH}"
 elif [[ "$KOOPA_SYSTEM" =~ "Ubuntu"* ]]; then
     # Ubuntu
-    export PATH="${KOOPA_BINDIR}/ubuntu:${PATH}"
+    export PATH="${KOOPA_BIN_DIR}/ubuntu:${PATH}"
 fi
 
 # Aspera Connect ===============================================================
-if [[ -z "$ASPERA_DIR" ]]; then
-    ASPERA_DIR="${HOME}/.aspera/connect/bin"
+if [[ -z "$ASPERACONNECT_EXE" ]]; then
+    aspera_exe="${HOME}/.aspera/connect/bin/asperaconnect"
+    if [[ -f "$aspera_exe" ]]; then
+        export ASPERACONNECT_EXE="$aspera_exe"
+        unset -v aspera_exe
+    fi
 fi
-if [[ -d "$ASPERA_DIR" ]]; then
-    export PATH="${ASPERA_DIR}:${PATH}"
+if [[ -f "$ASPERACONNECT_EXE" ]]; then
+    aspera_bin_dir="$( dirname "$ASPERACONNECT_EXE" )"
+    export PATH="${aspera_bin_dir}:${PATH}"
+    unset -v aspera_bin_dir
 fi
 
 # Conda ========================================================================
-if [[ -d "$CONDA_DIR" ]]; then
-    source "${CONDA_DIR}/activate"
+if [[ -f "$CONDA_EXE" ]]; then
+    conda_bin_dir="$( dirname "$CONDA_EXE" )"
+    source "${conda_bin_dir}/activate"
+    unset -v conda_bin_dir
 fi
 
 # bcbio ========================================================================
-if [[ -d "$BCBIO_DIR" ]]; then
-    if [[ ! -f "${BCBIO_DIR}/bcbio_nextgen.py" ]]; then
-        echo "bcbio_nextgen.py missing in ${BCBIO_DIR}"
-        exit 1
-    fi
-    export PATH="${BCBIO_DIR}:${PATH}"
+if [[ -f "$BCBIO_EXE" ]]; then
+    bcbio_bin_dir="$( dirname "$BCBIO_EXE" )"
+    export PATH="${bcbio_bin_dir}:${PATH}"
     unset -v PYTHONHOME PYTHONPATH
+    unset -v bcbio_bin_dir
 fi
