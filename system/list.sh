61140018c3bf30ab56de5f49621a92cd79e78d8a
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-22 12:43:29 -0400

    Update internal path variables

--- system/list.sh
+++ system/list.sh
@@ -3,7 +3,7 @@
 # List available scripts.
 # 2018-09-23
 
-find "$KOOPA_BINDIR" \
+find "$KOOPA_BIN_DIR" \
     -maxdepth 1 -type f \
     -not -name ".*" -not -name "koopa" \
     -printf "%f\n" | sort
