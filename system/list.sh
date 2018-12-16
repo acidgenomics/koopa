43469dd1c9b59a4f55ee2c59a32594b4c6b55726
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-15 19:09:41 -0500

    v0.2.2 update

--- system/list.sh
+++ system/list.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
 # List available scripts.
 
-find "$KOOPA_BIN_DIR" \
-    -maxdepth 1 -type f \
-    -not -name ".*" -not -name "koopa" \
-    -printf "%f\n" | sort
+# Note that printf isn't POSIX and doesn't work on macOS.
+# -printf "%p\n"
+
+find "$KOOPA_BIN_DIR" -maxdepth 1 -type f -not -name ".*" -not -name "koopa" | sort
