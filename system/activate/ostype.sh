4c8f205ba6cd7db7e8ad62d9998ba21053149a57
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-07 10:53:15 -0500

    Improve activation

--- system/activate/ostype.sh
+++ system/activate/ostype.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-# Check for supported operating system.
-# Alternatively, can match against $OSTYPE.
+# Operating system variables defined in uname.
+
+# Operating system name.
+OSNAME=$(uname -s)
+export OSNAME
+
+# Host name.
+HOSTNAME=$(uname -n)
+export HOSTNAME
 
 case "$(uname -s)" in
     Darwin) export MACOS=1 && export UNIX=1;;
      Linux) export LINUX=1 && export UNIX=1;;
          *) echo "Unsupported operating system."; return 1;;
 esac
+
+# Microsoft Azure.
+if quiet_expr "$HOSTNAME" "azlabapp"
+then
+    export AZURE=1
+fi
