5bf86d81d794c0c4e3346b8ebb07073f15e1d23c
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-13 13:10:18 -0500

    Improve bcbio detection on Azure

--- system/activate/bcbio.sh
+++ system/activate/bcbio.sh
@@ -6,25 +6,25 @@
 
 if [ -z "$BCBIO_EXE" ]
 then
-    if [ ! -z "$HARVARD_O2" ]
+    if [ ! -z "$AZURE" ]
     then
-        export BCBIO_EXE="/n/app/bcbio/tools/bin/bcbio_nextgen.py"
+        BCBIO_EXE="/data00/bcbio/tools/bin/bcbio_nextgen.py"
+    elif [ ! -z "$HARVARD_O2" ]
+    then
+        BCBIO_EXE="/n/app/bcbio/tools/bin/bcbio_nextgen.py"
     elif [ ! -z "$HARVARD_ODYSSEY" ]
     then
-        export BCBIO_EXE="/n/regal/hsph_bioinfo/bcbio_nextgen/bin/bcbio_nextgen.py"
+        BCBIO_EXE="/n/regal/hsph_bioinfo/bcbio_nextgen/bin/bcbio_nextgen.py"
     fi
 fi
-if [ ! -z "$BCBIO_EXE" ]
+
+# Export in PATH if the binary is accessible.
+if [ -x "$BCBIO_EXE" ]
 then
-    # Check that path is valid.
-    if [ -f "$BCBIO_EXE" ]
-    then
-        bcbio_bin_dir="$( dirname "$BCBIO_EXE" )"
-        export PATH="${bcbio_bin_dir}:${PATH}"
-        unset -v PYTHONHOME PYTHONPATH
-        unset -v bcbio_bin_dir
-    else
-        printf "bcbio does not exist at:\n%s\n" "$BCBIO_EXE"
-        # Don't exit here as this can cause SSH lockout.
-    fi
+    export BCBIO_EXE
+    unset -v PYTHONHOME PYTHONPATH
+    
+    bcbio_bin_dir="$( dirname "$BCBIO_EXE" )"
+    export PATH="${bcbio_bin_dir}:${PATH}"
+    unset -v bcbio_bin_dir
 fi
