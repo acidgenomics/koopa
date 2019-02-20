3a5d6033e5c590f6ff047967909a7a81de87beb4
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-20 11:07:29 -0500

    Improve conda activation

--- system/activate/base/programs/conda.sh
+++ system/activate/base/programs/conda.sh
@@ -5,16 +5,28 @@
 
 # Activate Conda
 #
+# It's no longer recommended to directly export conda in `$PATH`.
+# Instead source the `activate` script.
+#
+# Note that this code will attempt to locate the installation automatically,
+# unless `$CONDA_EXE` is set.
+#
+# Note that an environment will only be activated when `$CONDA_DEFAULT_ENV` is
+# set prior to running this code. Attempts to activate "base" will be ignored.
+
+
+
 # Conda doesn't currently support ksh, and fish support is buggy.
-# Only attempt to autoload for bash or ksh.
-#
-# It's no longer recommended to export conda in PATH.
-# Attempt to locate automatically when not manually defined.
-#
-# Attempt to detect conda path automatically, if not set.
-#
+# Only attempt to autoload for bash or zsh.
+[ "$KOOPA_SHELL" != "bash" ] && \
+    [ "$KOOPA_SHELL" != "zsh" ] && \
+    return
+
+
+
+# Attempt to detect the installation path automatically, if necessary.
+# Use `$CONDA_EXE` to manually set the path, for non-standard installs.
 # Priority:
-#
 # 1. User anaconda3
 #    ~/anaconda3
 # 2. User miniconda3
@@ -23,56 +35,57 @@
 #    /usr/local/anaconda3
 # 4. Shared miniconda3
 #    /usr/local/miniconda3
-#
-# Note that an environment will only be activated when CONDA_DEFAULT_ENV
-# is defined in the shell variables prior to this step.
-#
-# Use CONDA_EXE to manually set the path, for non-standard installs.
-
-
-
-# "$shell" variable inherits from koopa call.
-# SC2154: shell is referenced but not assigned.
-# shellcheck disable=SC2154
-if [ "$KOOPA_SHELL" = "bash" ] || [ "$KOOPA_SHELL" = "zsh" ]
+if [ -z "$CONDA_EXE" ]
 then
-    if [ -z "$CONDA_EXE" ]
+    if [ -f "${HOME}/anaconda3/bin/conda" ]
     then
-        if [ -f "${HOME}/anaconda3/bin/conda" ]
-        then
-            export CONDA_EXE="${HOME}/anaconda3/bin/conda"
-        elif [ -f "${HOME}/miniconda3/bin/conda" ]
-        then
-            export CONDA_EXE="${HOME}/miniconda3/bin/conda"
-        elif [ -f "/usr/local/anaconda3/bin/conda" ]
-        then
-            export CONDA_EXE="/usr/local/anaconda3/bin/conda"
-        elif [ -f "/usr/local/miniconda3/bin/conda" ]
-        then
-            export CONDA_EXE="/usr/local/miniconda3/bin/conda"
-        fi
-    fi
-    if [ ! -z "$CONDA_EXE" ]
+        export CONDA_EXE="${HOME}/anaconda3/bin/conda"
+    elif [ -f "${HOME}/miniconda3/bin/conda" ]
     then
-        # Check that path is valid.
-        if [ -f "$CONDA_EXE" ]
-        then
-            conda_bin_dir="$( dirname "$CONDA_EXE" )"
-            # Activate the default environment automatically, if requested.
-            # Note that this will get redefined as "base" when conda is
-            # activated, so define as an internal variable here.
-            conda_env="$CONDA_DEFAULT_ENV"
-            # shellcheck source=/dev/null
-            . "${conda_bin_dir}/activate"
-            if [ ! -z "$conda_env" ]
-            then
-                conda activate "$conda_env"
-            fi
-            unset -v conda_bin_dir conda_env
-        else
-            printf "conda does not exist at:\n%s\n" "$CONDA_EXE"
-            # Don't exit here, as this can cause SSH lockout.
-        fi
+        export CONDA_EXE="${HOME}/miniconda3/bin/conda"
+    elif [ -f "/usr/local/anaconda3/bin/conda" ]
+    then
+        export CONDA_EXE="/usr/local/anaconda3/bin/conda"
+    elif [ -f "/usr/local/miniconda3/bin/conda" ]
+    then
+        export CONDA_EXE="/usr/local/miniconda3/bin/conda"
     fi
 fi
 
+
+
+# Early return if we don't detect an installation.
+[ -z "$CONDA_EXE" ] && return
+
+
+
+# Early return with error if conda installation is set but not accessible.
+if [ ! -x "$CONDA_EXE" ]
+then
+    printf "conda does not exist at:\n%s\n" "$CONDA_EXE"
+    # Don't exit here, as this can cause SSH lockout.
+    return 1
+fi
+
+
+
+# Activate the default environment automatically, if requested.
+# Note that this will get redefined as "base" when conda is
+# activated, so define as an internal variable here.
+conda_env="$CONDA_DEFAULT_ENV"
+
+# Now we're ready to activate.
+conda_bin_dir="$( dirname "$CONDA_EXE" )"
+# shellcheck source=/dev/null
+. "${conda_bin_dir}/activate"
+
+# Activate custom environment other than base, if desired.
+# Otherwise, clear base environment out of `$PATH`.
+if [ ! -z "$conda_env" ]
+then
+    conda activate "$conda_env"
+else
+    conda deactivate
+fi
+
+unset -v conda_bin_dir conda_env
