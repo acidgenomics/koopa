a8fad0c2d5ed9725d4967cbd09f2463523bafb29
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-19 11:28:24 -0400

    Update cpu-count.sh

--- system/base/exports/cpu-count.sh
+++ system/base/exports/cpu-count.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # shellcheck disable=SC2236
 
-# Count CPUs for Make jobs.
+# Get the number of cores (CPUs) available.
 
 if [ ! -z "$MACOS" ]
 then
@@ -10,6 +10,7 @@ elif [ ! -z "$LINUX" ]
 then
     CPU_COUNT="$(getconf _NPROCESSORS_ONLN)"
 else
+    # Otherwise assume single threaded.
     CPU_COUNT=1
 fi
 export CPU_COUNT
