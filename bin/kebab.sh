4d80b0735326388f4023dd8861d6a322ff96c155
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-03 13:01:26 -0400

    Improve kebab script

--- bin/kebab.sh
+++ bin/kebab.sh
@@ -2,6 +2,9 @@
 set -Eeuo pipefail
 
 # Kebab case
+#
+# See also:
+# - https://stackoverflow.com/questions/152514
 
 # Error if user does not have Perl rename installed.
 if [[ $(rename -V | grep -c "File::Rename") -eq 0 ]]
@@ -10,4 +13,8 @@ then
     exit 1
 fi
 
+# Using `-f` flag here for case insensitive filesystem support (macOS).
+rename -f 'y/A-Z/a-z/' *
+
+# Convert underscores and spaces to dashes.
 rename 's/[_\s]/-/g' *
