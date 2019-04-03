4f1ea61f1af3a3acd1b22a113416c2c1737a735d
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-03 14:37:58 -0400

    Draft update to kebab renamer

--- bin/kebab.sh
+++ bin/kebab.sh
@@ -5,6 +5,7 @@ set -Eeuo pipefail
 #
 # See also:
 # - https://stackoverflow.com/questions/152514
+# - https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
 
 # Error if user does not have Perl rename installed.
 if [[ $(rename -V | grep -c "File::Rename") -eq 0 ]]
@@ -13,8 +14,26 @@ then
     exit 1
 fi
 
-# Using `-f` flag here for case insensitive filesystem support (macOS).
-rename -f 'y/A-Z/a-z/' *
+# Set IFS so we can process files containing spaces.
+ifs=$IFS
+IFS=$(echo -en "\n\b")
 
-# Convert underscores and spaces to dashes.
-rename 's/[_\s]/-/g' *
+files=($@)
+
+# printf "%s\n" "${files[@]}"
+
+for file in "${files[@]}"
+do
+    # Convert underscores and spaces to dashes.
+    rename 's/[_\s]/-/g' "$file"
+
+    # Using `-f` flag here for case insensitive filesystem support (macOS).
+    # rename -f 'y/A-Z/a-z/' "$file"
+
+    # Ensure R scripts end with capital `.R`.
+    # FIXME Improve the file pattern matching here.
+    # rename 's/\.r$/.R' "$file"
+done
+
+IFS=$ifs
+unset -v ifs
