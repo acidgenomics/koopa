16a04aa511fb341470c66d6d760bc9acb0b2ddb8
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-12 10:28:50 -0400

    Improve bcbio custom genome build scripts

--- workflows/bcbio/genomes/refseq-grch38.sh
+++ workflows/bcbio/genomes/refseq-grch38.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -Eeuxo pipefail
 
 # RefSeq GRCh38 genome build
-# Last updated 2019-04-09
+# Last updated 2019-04-12
 # https://www.ncbi.nlm.nih.gov/refseq/
-
-# Prepare biodata directory ====================================================
-biodata_dir="${HOME}/biodata"
-mkdir -p "$biodata_dir"
-cd "$biodata_dir"
 
 # Build parameters =============================================================
 build="GRCh38"
 source="RefSeq"
 release="$REFSEQ_RELEASE"
 
+biodata_dir="${HOME}/biodata"
 bcbio_species_dir="Hsapiens"
-bcbio_build_dir="${build}_${source}_${release}"
 
 cores=$((CPU_COUNT-2))
 
+# Prepare directories ==========================================================
+mkdir -p "$biodata_dir"
+cd "$biodata_dir"
+
+bcbio_build_dir="${build}_${source}_${release}"
 mkdir -p "$bcbio_build_dir"
 cd "$bcbio_build_dir"
 
@@ -34,7 +34,6 @@ then
 fi
 
 # FASTA ------------------------------------------------------------------------
-# GCF_000001405.38_GRCh38.p12_genomic.fna.gz
 fasta="${assembly}_genomic.fna"
 if [[ ! -f "$fasta" ]]
 then
@@ -43,7 +42,6 @@ then
 fi
 
 # GTF --------------------------------------------------------------------------
-# GCF_000001405.38_GRCh38.p12_genomic.gtf
 gtf="${assembly}_genomic.gtf"
 if [[ ! -f "$gtf" ]]
 then
@@ -51,6 +49,7 @@ then
     gunzip -c "${gtf}.gz" > "$gtf"
 fi
 
+# Sanitize GTF =================================================================
 # Note that we're using a sanitized GTF here, otherwise bowtie2 index will fail.
 cp "$gtf" tmp-01-original.gtf
 
@@ -97,10 +96,11 @@ fasta=$(realpath "${bcbio_build_dir}/${fasta}")
 # Note use of clean GTF (see above).
 gtf=$(realpath "${bcbio_build_dir}/${clean_gtf}")
 
-# I recommend skipping HISAT2 indexing here because it's too memory hungry. It
-# fails to index on a virtual machine with 120 GB of RAM. It is recommended to
-# index on a machine with at least 200-400 GB of RAM, if possible. This works
-# on a shared HPC research cluster but is impractical on AWS or Azure.
+# bcbio_setup_genome.py --help
+
+# Note that HISAT2 requires a lot of memory to index (>= 200 GB). This is
+# intended to be run on a high performance cluster but is often impractical on
+# a virtual machine instance (e.g. AWS, Azure).
 
 bcbio_setup_genome.py \
     --cores="$cores" \
@@ -108,6 +108,5 @@ bcbio_setup_genome.py \
     --build="$bcbio_build_dir" \
     --fasta="$fasta" \
     --gtf="$gtf" \
-    --indexes bowtie2 minimap2 star \
+    --indexes bowtie2 minimap2 seq star \
     2>&1 | tee "${log_dir}/bcbio-genomes-$(date "+%Y%m%d_%H%M%S").log"
-
