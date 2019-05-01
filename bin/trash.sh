938089eaa31bd9fae6b4a047cd42922f8628acdc
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-01 16:36:15 -0400

    Fix trash script

--- bin/trash.sh
+++ bin/trash.sh
@@ -4,9 +4,5 @@ set -Eeuxo pipefail
 # Move files to the Trash folder instead of deleting.
 
 trash_dir="${HOME}/.Trash/"
-if [[ -d "$trash_dir" ]]
-then
-    mkdir -p "$trash_dir"
-fi
+mkdir -p "$trash_dir"
 mv "$@" "$trash_dir"
-unset -v trash_dir
