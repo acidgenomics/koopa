3fa0ec8df7b4a42a71e5b5be31c482ab4a5e8dbc
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-06 12:18:41 -0400

    Improve scripts

--- install/redhat/shellcheck.sh
+++ install/redhat/shellcheck.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-# ShellCheck EPEL v0.3.5
+# ShellCheck
 #
 # See also:
 # - Install ShellCheck from source
@@ -23,11 +23,16 @@ then
     exit 1
 fi
 
-echo "Installing EPEL release."
+# Note that EPEL version is super old and many current checks don't work.
+echo "Installing old EPEL version of ShellCheck to /usr/bin."
 sudo yum -y install epel-release
-
-echo "Installing old EPEL of ShellCheck."
 sudo yum install -y ShellCheck
+
+version="v0.6.0"
+echo "Copying newer ${version} binary version to /usr/local/bin."
+wget -qO- "https://storage.googleapis.com/shellcheck/shellcheck-"${version}".linux.x86_64.tar.xz" | \
+    tar -xJv
+sudo cp shellcheck-"${version}"/shellcheck /usr/local/bin/
 
 echo "shellcheck installed successfully."
 command -v shellcheck
