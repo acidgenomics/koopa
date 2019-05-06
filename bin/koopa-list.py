a2b763eb5a80fcfb3af4d8a6114f66d9c18f3a9a
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-06 12:44:34 -0400

    Add python script

--- bin/koopa-list.py
+++ bin/koopa-list.py
@@ -4,6 +4,11 @@ from os import environ, listdir
 from os.path import isfile, join
 
 bin_dir = environ["KOOPA_BIN_DIR"]
+
 scripts = [f for f in listdir(bin_dir) if isfile(join(bin_dir, f))]
 scripts = sorted(scripts)
+
+# Hide invisible files:
+# [n for n in os.listdir(...) if not n.startswith(".")]
+
 print('\n'.join(scripts)) 
