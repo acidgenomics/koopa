0d93c7d1f06c3e8ce48d2a1cb4455cf1f1482aa1
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-10 11:33:30 -0400

    Improve version check script

--- system/extra/check-versions.R
+++ system/extra/check-versions.R
@@ -1,100 +1,164 @@
 # Check installed program versions.
 
-options(error = quote(quit(status = 1L)))
+# options(
+#     error = quote(quit(status = 1L)),
+#     warning = quote(quit(status = 1L))
+# )
 
-# Consider switching to `system2()` in a future update.
-formals(system)[["intern"]] <- TRUE
+check <- function(
+    name,
+    min_version,
+    version_cmd
+) {
+    stopifnot(
+        is.character(name),
+        is.character(min_version)
+    )
+    inform <- warning
+    formals(inform)[["call."]] <- FALSE
 
-check <- function(name, version, required_version) {
-    stopifnot(is.character(name))
-    # Strip trailing character (e.g. "a") if necessary.
+    # Check to see if program is installed.
+    if (identical(unname(Sys.which(name)), "")) {
+        inform(name, "missing")
+        return(invisible())
+    }
+
+    min_version <- package_version(min_version)
+
+    # Run the shell system command to extract the program version.
+    # Consider switching to `system2()` here in a future update.
+    version <- system(
+        command = version_cmd,
+        intern = TRUE
+    )
+    stopifnot(is.character(version))
+
+    # Strip leading and trailing characters (e.g. "v/a") if necessary.
+    version <- sub("^[a-z]+", "", version)
     version <- sub("[a-z]+$", "", version)
     version <- package_version(version)
-    required_version <- package_version(required_version)
-    if (version < required_version) {
-        stop(paste(name, version, "<", required_version), call. = FALSE)
+
+    if (version < min_version) {
+        inform(paste(name, version, "<", min_version))
     }
 }
 
+pipe <- function(...) {
+    paste(..., sep = " | ")
+}
 
+check(
+    name = "emacs",
+    min_version = "26.2",
+    version_cmd = pipe(
+        "emacs --version",
+        "head -n 1",
+        "cut -d ' ' -f 3"
+    )
+)
 
-# Emacs ========================================================================
-name <- "Emacs"
-version <- system("emacs --version | head -n 1 | cut -d ' ' -f 3")
-required_version <- "26.2"
-check(name, version, required_version)
+check(
+    name = "git",
+    min_version = "2.21",
+    version_cmd = pipe(
+        "git --version",
+        "head -n 1",
+        "cut -d ' ' -f 3"
+    )
+)
 
+check(
+    name = "gpg",
+    min_version = "2.2.8",
+    version_cmd = pipe(
+        "gpg --version",
+        "head -n 1",
+        "cut -d ' ' -f 3"
+    )
+)
 
+check(
+    name = "gsl-config",
+    min_version = "2.5",
+    version_cmd = pipe(
+        "gsl-config --version",
+        "head -n 1"
+    )
+)
 
-# Git ==========================================================================
-name <- "Git"
-version <- system("git --version | head -n 1 | cut -d ' ' -f 3")
-required_version <- "2.21"
-check(name, version, required_version)
+check(
+    name = "h5dump",
+    min_version = "1.10",
+    version_cmd = pipe(
+        "h5dump --version",
+        "head -n 1",
+        "cut -d ' ' -f 3"
+    )
+)
 
+check(
+    name = "htop",
+    min_version = "2.2.0",
+    version_cmd = pipe(
+        "htop --version",
+        "head -n 1",
+        "cut -d ' ' -f 2"
+    )
+)
 
+# Requiring the current RHEL 7 version.
+# The cut match is a little tricky here:
+# This is perl 5, version 16, subversion 3 (v5.16.3)
+check(
+    name = "perl",
+    min_version = "5.16.3",
+    version_cmd = pipe(
+        "perl --version",
+        "sed -n '2p'",
+        "cut -d '(' -f 2 | cut -d ')' -f 1"
+    )
+)
 
-# GnuPG ========================================================================
-name <- "GnuPG"
-version <- system("gpg --version | head -n 1 | cut -d ' ' -f 3")
-required_version <- "2.2.8"
-check(name, version, required_version)
-
-
-
-# GSL ==========================================================================
-name <- "GSL"
-version <- system("gsl-config --version | head -n 1")
-required_version <- "2.5"
-check(name, version, required_version)
-
-
-
-# HDF5 =========================================================================
-name <- "HDF5"
-version <- system("h5dump --version | head -n 1 | cut -d ' ' -f 3")
-required_version <- "1.10"
-check(name, version, required_version)
-
-
-
-# htop =========================================================================
-name <- "htop"
-version <- system("htop --version | head -n 1 | cut -d ' ' -f 2")
-required_version <- "2.2.0"
-check(name, version, required_version)
-
-
-
-# Python =======================================================================
 # Now requiring >= 3.7. Python 2 will be phased out by 2020.
 # The user can use either conda or virtualenv.
-name <- "Python"
-version <- system("python --version 2>&1 | head -n 1 | cut -d ' ' -f 2")
-required_version <- "3.7"
-check(name, version, required_version)
+check(
+    name = "python",
+    min_version = "3.7",
+    version_cmd = pipe(
+        "python --version 2>&1",
+        "head -n 1",
+        "cut -d ' ' -f 2"
+    )
+)
 
-
-
-# ShellCheck ===================================================================
 # RHEL 7 still uses super old 0.3.5 release.
-name <- "ShellCheck"
-version <- system("shellcheck --version | sed -n '2p' | cut -d ' ' -f 2")
-required_version <- "0.3.5"
-check(name, version, required_version)
+# This is hard to compile, so keep the dependency relaxed.
+check(
+    name = "shellcheck",
+    min_version = "0.3.5",
+    version_cmd = pipe(
+        "shellcheck --version",
+        "sed -n '2p'",
+        "cut -d ' ' -f 2"
+    )
+)
 
+check(
+    name = "tmux",
+    min_version = "2.9",
+    version_cmd = pipe(
+        "tmux -V",
+        "head -n 1",
+        "cut -d ' ' -f 2"
+    )
+)
 
-
-# tmux =========================================================================
-name <- "tmux"
-version <- system("tmux -V | head -n 1 | cut -d ' ' -f 2")
-required_version <- "2.9"
-check(name, version, required_version)
-
-
-
-# Vim ==========================================================================
-name <- "Vim"
-version <- system("vim --version | head -1 | cut -d ' ' -f 5")
-required_version <- "8.1"
-check(name, version, required_version)
+check(
+    name = "vim",
+    min_version = "8.1",
+    version_cmd = pipe(
+        "vim --version",
+        "head -1",
+        "cut -d ' ' -f 5"
+    )
+)
