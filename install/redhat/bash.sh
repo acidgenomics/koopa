23d04312298944e1bead92146f496b9a43d90263
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-13 13:20:31 -0400

    Improve bash script

--- install/redhat/bash.sh
+++ install/redhat/bash.sh
@@ -1,9 +1,37 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail
+
 # Bash
 # https://www.gnu.org/software/bash/
 
 build_dir="/tmp/bash"
 prefix="/usr/local"
 version="5.0"
+
+# Check for RedHat.
+if [[ ! -f "/etc/redhat-release" ]]
+then
+    echo "Error: RedHat Linux is required." >&2
+    exit 1
+fi
+
+# Error on conda detection.
+if [[ -x "$(command -v conda)" ]] && [[ -n "${CONDA_PREFIX:-}" ]]
+then
+    echo "Error: conda is active." >&2
+    exit 1
+fi
+
+# Require yum to build dependencies.
+if [[ ! -x "$(command -v yum)" ]]
+then
+    echo "Error: yum is required to build dependencies." >&2
+    exit 1
+fi
+
+echo "Installing bash ${version}."
+echo "sudo is required for this script."
+sudo -v
 
 sudo yum install -y yum-utils
 sudo yum-builddep -y bash
