e0754cd02cc669641d9a9b7ec9565b8d691b3a1b
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-13 07:53:31 -0400

    Add OpenSSL install script for RedHat

--- install/redhat/openssl.sh
+++ install/redhat/openssl.sh
@@ -7,17 +7,22 @@ set -Eeuxo pipefail
 build_dir="/tmp/openssl"
 date="2019-02-26"
 version="1.1.1b"
+prefix="/usr/local"
 
-# Check for macOS.
-if [[ "$OSTYPE" != "darwin" ]]
+# Check for RedHat.
+if [[ ! -f "/etc/redhat-release" ]]
 then
-    echo "Error: macOS is required." >&2
+    echo "Error: RedHat Linux is required." >&2
     exit 1
 fi
 
 echo "Installing openssl ${version} (${date})."
 echo "sudo is required for this script."
 sudo -v
+
+# Install dependencies.
+sudo yum -y install yum-utils
+sudo yum-builddep -y openssl
 
 # SC2103: Use a ( subshell ) to avoid having to cd back.
 (
@@ -26,7 +31,7 @@ sudo -v
     curl -O "https://www.openssl.org/source/openssl-${version}.tar.gz"
     tar -xvzf "openssl-${version}.tar.gz"
     cd "openssl-${version}" || exit 1
-    ./Configure darwin64-x86_64-cc
+    ./config --prefix="$prefix"
     make
     make test
     sudo make install
