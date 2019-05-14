5c699aa550c005561e633403ae2a0e19f72cd8fb
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-14 17:39:57 +0000

    Improve init script for RHEL and Ubuntu

--- install/redhat/_init.sh
+++ install/redhat/_init.sh
@@ -5,9 +5,9 @@ echo "sudo access is required for installation."
 sudo -v
 
 # Check for RedHat.
-if [[ ! -f "/etc/redhat-release" ]]
+if ! grep -q "ID=rhel" /etc/os-release
 then
-    echo "Error: RedHat Linux is required." >&2
+    echo "Error: RedHat Enterprise Linux (RHEL) is required." >&2
     exit 1
 fi
 
@@ -26,4 +26,4 @@ then
 fi
 
 # Ensure yum-utils is installed, so we can build dependencies.
-sudo yum install -y yum-utils
+sudo yum -y install yum-utils
