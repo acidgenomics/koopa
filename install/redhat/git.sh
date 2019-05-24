14c333117ae38d2b7391328462b4c65b4e7930b8
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-24 14:20:54 -0400

    Prepare v0.3.4 release

--- install/redhat/git.sh
+++ install/redhat/git.sh
@@ -18,6 +18,7 @@ echo "Installing git ${version}."
 
 # Run preflight initialization checks.
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+# shellcheck source=/dev/null
 . "${script_dir}/_init.sh"
 
 # Install build dependencies.
