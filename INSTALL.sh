e6ee4d3acaeb7364fc658aa44c253d59f533b0e5
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-31 10:08:32 -0400

    Improve installer script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-./INSTALL/submodules.sh
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+git submodule init
+git submodule update
+
+# dotfiles
+(
+    cd dotfiles
+    git submodule init
+    git submodule update
+)
+
+. "${script_dir}/UPDATE.sh"
