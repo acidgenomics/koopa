d142623f21dbf3e93b63c797161f57e845cf9e5f
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-04 09:30:55 -0400

    Rework to pass shellcheck

--- UPDATE.sh
+++ UPDATE.sh
@@ -7,7 +7,12 @@ git fetch --all
 git pull
 git submodule sync --recursive
 git submodule update --init --recursive
-git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+# shellcheck disable=SC2016
+git submodule foreach -q --recursive \
+    'git checkout $( \
+        git config -f $toplevel/.gitmodules submodule.$name.branch || \
+        echo master \
+    )'
 git submodule foreach git pull
 git submodule status
 git status
