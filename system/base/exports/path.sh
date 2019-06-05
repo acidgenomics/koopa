39c78abea21f8554377b19bf4d006fa5c855477c
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-05 19:41:21 +0000

    Improve Debian support

--- system/base/exports/path.sh
+++ system/base/exports/path.sh
@@ -37,15 +37,34 @@ has_sudo && add_to_path_start "${KOOPA_BIN_DIR}/sudo"
 
 os="${KOOPA_OS_NAME}"
 
-# Currently this applies to Fedora/RHEL/CentOS and Amazon Linux 2.
-# In the future, add support here for Debian/Ubuntu.
-# Putting this in PATH before the OS-specific scripts.
-if [ "$os" = "amzn" ]
+if [ ! -z "${LINUX:-}" ]
 then
-    os_bin_dir="${KOOPA_BIN_DIR}/os/rhel"
-    add_to_path_start "$os_bin_dir"
-    has_sudo && add_to_path_start "${os_bin_dir}/sudo"
-    unset -v os_bin_dir
+    id_like="$(cat /etc/os-release | grep ID_LIKE | cut -d "=" -f 2)"
+
+    # - amzn
+    #   ID_LIKE="centos rhel fedora"
+    # - rhel
+    #   ID_LIKE="fedora"
+    # - ubuntu
+    #   ID_LIKE=debian
+
+    # Fedora-like (e.g. RHEL, CentOS, Amazon Linux)
+    if "$(echo "$id_like" | grep -q fedora)"
+    then
+        os_bin_dir="${KOOPA_BIN_DIR}/os/fedora"
+        add_to_path_start "$os_bin_dir"
+        has_sudo && add_to_path_start "${os_bin_dir}/sudo"
+        unset -v os_bin_dir
+    fi
+
+    # Debian-like (e.g. Ubuntu)
+    if "$(echo "$id_like" | grep -q debian)"
+    then
+        os_bin_dir="${KOOPA_BIN_DIR}/os/debian"
+        add_to_path_start "$os_bin_dir"
+        has_sudo && add_to_path_start "${os_bin_dir}/sudo"
+        unset -v os_bin_dir
+    fi
 fi
 
 os_bin_dir="${KOOPA_BIN_DIR}/os/${os}"
