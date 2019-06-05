efc78eefc7d8eda7bbfc69a5a96a8bd956971292
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-05 11:48:20 -0400

    Improve activation

--- system/extra/bash/init.sh
+++ system/extra/bash/init.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Source global definitions.
+# shellcheck source=/dev/null
+[[ -f /etc/profile ]] && source /etc/profile
+
 # shellcheck source=/dev/null
 [[ -f /etc/bashrc ]] && source /etc/bashrc
 
-# Source Bash completions.
 # shellcheck source=/dev/null
 [[ -f /etc/profile.d/bash-completion ]] && \
     source /etc/profile.d/bash-completion
+
 # shellcheck source=/dev/null
 [[ -f /usr/local/etc/bash_completion ]] && \
     source /usr/local/etc/bash_completion
