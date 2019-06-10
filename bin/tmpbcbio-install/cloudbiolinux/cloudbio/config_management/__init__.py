f2945caa4acf6d0d8392d00051c91951d6df8a59
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-10 15:24:49 -0400

    Remove bcbio tmp files

--- bin/tmpbcbio-install/cloudbiolinux/cloudbio/config_management/__init__.py
+++
@@ -1,2 +0,0 @@
-""" This module contains code related to integrating the configuration
-management tools `chef` and `puppet` into CloudBioLinux. """
--- bin/tmpbcbio-install/cloudbiolinux/deploy/deploy_no_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export PROJECT_DIRECTORY="."
-
-# Add cloudbiolinux to python path and run deployment.
-export PYTHONPATH=..:$PYTHONPATH
-python $PROJECT_DIRECTORY/../cloudbio/deploy/main.py "$@"
