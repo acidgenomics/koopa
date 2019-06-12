c06b840682cb9ab1de8579a39837458109aaf429
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-12 16:02:54 -0400

    Improve Rscript handling

--- system/list.R
+++ system/list.R
@@ -7,9 +7,15 @@ options(
     warning = quote(quit(status = 1L))
 )
 
-# Note that this won't pick up in my current RStudio configuration.
+message("koopa programs exported in PATH")
+
+# Note that these won't pick up in my current RStudio configuration.
 koopa_dir <- Sys.getenv("KOOPA_DIR")
-stopifnot(nzchar(koopa_dir))
+path <- Sys.getenv("PATH")
+stopifnot(
+    nzchar(koopa_dir),
+    grepl("koopa", path)
+)
 
 printPrograms <- function(path) {
     path <- normalizePath(path, mustWork = TRUE)
@@ -19,10 +25,16 @@ printPrograms <- function(path) {
         full.names = FALSE
     )
     cat(
+        "",
         paste0(path, ":"),
         paste0("    ", files),
         sep = "\n"
     )
 }
 
-printPrograms(file.path(koopa_dir, "bin"))
+# Split PATH string into a character vector.
+path <- strsplit(x = path, split = ":", fixed = TRUE)[[1L]]
+keep <- grepl(file.path("koopa", "bin"), path)
+path <- path[keep]
+
+invisible(lapply(X = path, FUN = printPrograms))
