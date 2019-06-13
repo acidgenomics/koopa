bbb40157c98beb52bfa0077cb01d49fe4a3bb3ef
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-13 09:37:47 -0400

    Add ipython scripts

---
+++ workflows/bcbio/ipython/harvard-o2/sbatch-indrops-scrnaseq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Schedule bcbio IPython run on HMS O2 cluster.
+# Michael Steinbaugh
+
+# SLURM
+# https://slurm.schedmd.com/sbatch.html
+
+#SBATCH --job-name=scrnaseq            # Job name
+#SBATCH --partition=medium             # Partition name
+#SBATCH --time=2-00:00                 # Runtime in D-HH:MM format
+#SBATCH --nodes=1                      # Number of nodes (keep at 1)
+#SBATCH --ntasks=1                     # Number of tasks per node (keep at 1)
+#SBATCH --cpus-per-task=1              # CPU cores requested per task (change for threaded jobs)
+#SBATCH --mem-per-cpu=8G               # Memory needed per CPU
+#SBATCH --error=jobid_%j.err           # File to which STDERR will be written, including job ID
+#SBATCH --output=jobid_%j.out          # File to which STDOUT will be written, including job ID
+#SBATCH --mail-type=ALL                # Type of email notification (BEGIN, END, FAIL, ALL)
+
+bcbio_nextgen.py ../config/bcbio.yaml \
+    -t ipython \
+    -n 32 \
+    -s slurm -q medium \
+    -r t=2-00:00 --retries 3 --timeout 1000
