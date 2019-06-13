80b78a891d02f439d7dd3e0f5f0745f0960e53ee
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-13 10:45:22 -0400

    Update error messages

--- include/os/debian/sudo.sh
+++ include/os/debian/sudo.sh
@@ -9,28 +9,28 @@ echo "sudo access is required for installation."
 # Check for RedHat.
 if ! grep -q "ID=ubuntu" /etc/os-release
 then
-    echo "Error: Ubuntu Linux is required." >&2
+    >&2 echo "Error: Ubuntu Linux is required."
     exit 1
 fi
 
 # Require apt-get to build dependencies.
 if [[ ! -x "$(command -v apt-get)" ]]
 then
-    echo "Error: apt-get is required to build dependencies." >&2
+    >&2 echo "Error: apt-get is required to build dependencies."
     exit 1
 fi
 
 # Ensure conda is deactivated.
 if [[ -x "$(command -v conda)" ]] && [[ -n "${CONDA_PREFIX:-}" ]]
 then
-    echo "Error: conda is active."
+    >&2 echo "Error: conda is active."
     exit 1
 fi
 
 # Ensure Python virtual environment is deactivated.
 if [[ -x "$(command -v deactivate)" ]]
 then
-    echo "Error: Python virtualenv is active."
+    >&2 echo "Error: Python virtualenv is active."
     exit 1
 fi
 
