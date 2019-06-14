705264f2fd22e11e7fdb5011647991d2c58eff45
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-14 12:29:02 -0400

    Harden permissions

--- include/os/darwin/sudo.sh
+++ include/os/darwin/sudo.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# Source bash functions.
+source "${KOOPA_DIR}/include/shell/bash/functions.sh"
+
+if ! has_sudo
+then
+    echo "Non-interactive (passwordless) sudo is required for this script."
+    exit 1
+fi
+
 # Check for macOS.
 if [[ "$KOOPA_OS_NAME" != "darwin" ]] ||
    [[ -z "${MACOS:-}" ]]
@@ -8,6 +17,3 @@ then
     >&2 echo "Error: macOS is required."
     exit 1
 fi
-
-# Source bash functions.
-source "${KOOPA_DIR}/include/shell/bash/functions.sh"
