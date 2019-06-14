5f90af9245d2f55937fda4e51d65ad9af24bfd5e
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-14 14:49:32 -0400

    Simplify CPI variables

--- config/host/azure/etc/profile.d/constellation.sh
+++ config/host/azure/etc/profile.d/constellation.sh
@@ -1,21 +1,5 @@
 # Constellation profile configuration
-# Last modified 2019-06-13 by Michael Steinbaugh
-
-
-
-# koopa                                                                     {{{1
-# ==============================================================================
-
-# koopa shell
-# https://github.com/acidgenomics/koopa
-BCBIO_EXE="/data00/bcbio/v1.1.5/tools/bin/bcbio_nextgen.py"
-# shellcheck source=/dev/null
-. "/usr/local/koopa/activate"
-
-
-
-# Exports                                                                   {{{1
-# ==============================================================================
+# Last modified 2019-06-14 by Michael Steinbaugh
 
 export D1="/mnt/azbioinfoseq01"
 export D2="/mnt/azbioinfoseq02"
@@ -23,3 +7,5 @@ export D3="/mnt/azbioinfoseq03"
 export D4="/mnt/azbioinfoseq04"
 export D5="/mnt/azbioinfoseq05"
 
+# These exports will be detected by koopa.
+export BCBIO_EXE="/data00/bcbio/v1.1.5/tools/bin/bcbio_nextgen.py"
