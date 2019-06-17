061a8377c030b8d449b4ff319bb8404e3184079a
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-17 17:20:18 -0400

    Improve private dotfile workflow

---
+++ system/install/dotfiles-mike.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+# Mike-specific dot files.
+# Modified 2019-06-17.
+
+private_dir="${KOOPA_CONFIG_DIR}/dotfiles-private"
+if [ ! -d "$private_dir" ]
+then
+    git clone git@github.com:mjsteinbaugh/dotfiles-private.git "$private_dir"
+fi
+unset -v private_dir
+
+dotfile -f forward
+
+if [ "$os" = "darwin" ]
+then
+    dotfile -f os/darwin/gitconfig
+else
+    dotfile -f gitconfig
+fi
+
+dotfile --private Rsecrets
+dotfile --private secrets
