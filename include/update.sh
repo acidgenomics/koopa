7aa0af0c43025ed2740dd19d1fdf3b0bef2b58ef
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-17 16:22:27 -0400

    Fix update script

---
+++ include/update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -Eeu -o pipefail
+
+# Update koopa installation.
+# Modified 2019-06-17.
+
+(
+    cd "$KOOPA_DIR" || exit 1
+    git pull
+    git submodule sync --recursive
+)
+
+cat << EOF
+koopa updated successfully.
+Shell must be reloaded for changes to take effect.
+EOF
