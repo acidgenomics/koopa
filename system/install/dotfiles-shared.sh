5729daeeb42b539c9c8ce86fb6dbd9627ea7a328
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-18 08:49:23 -0400

    Improve dotfiles setup

---
+++ system/install/dotfiles-shared.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install general user dot files.
+# Modified 2019-06-18.
+
+dotfile --force condarc
+dotfile --force gitignore
+dotfile --force screenrc
+dotfile --force spacemacs
+dotfile --force tmux.conf
+dotfile --force vim
+dotfile --force vimrc
+
