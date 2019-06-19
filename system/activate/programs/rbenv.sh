0aad31210a4a96f922166c03de5450e15b73cbce
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-18 22:07:47 -0400

    Improve rbenv activation

--- system/activate/programs/rbenv.sh
+++ system/activate/programs/rbenv.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-# Ruby (rbenv)
-# https://github.com/rbenv/rbenv
+# Ruby environment (rbenv).
+# Modified 2019-06-18.
 
-# Legacy activation methods:
-# export PATH="${HOME}/.rbenv/shims:${PATH}"
-# eval "$(rbenv init -)"
+# See also:
+# - https://github.com/rbenv/rbenv
+# - https://github.com/rbenv/rbenv#how-rbenv-hooks-into-your-shell
+# - https://blakewilliams.me/posts/system-wide-rbenv-install
+# - https://devhints.io/rbenv
 
-quiet_which rbenv && add_to_path_start "$(rbenv root)/shims"
+# Alternate approaches:
+# > add_to_path_start "$(rbenv root)/shims"
+# > add_to_path_start "${HOME}/.rbenv/shims"
+
+# Configure shared installation, if necessary.
+if ! quiet_which rbenv && [ -d "/usr/local/rbenv" ]
+then
+    export RBENV_ROOT="/usr/local/rbenv"
+    add_to_path_start "${RBENV_ROOT}/bin"
+fi
+
+if quiet_which rbenv
+then
+    eval "$(rbenv init -)"
+fi
