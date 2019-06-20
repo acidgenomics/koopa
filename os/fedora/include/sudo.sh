6871ace775e1422d417842ac54f86ad790c93b76
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-20 18:14:36 -0400

    Fix cellar linking

--- os/fedora/include/sudo.sh
+++ os/fedora/include/sudo.sh
@@ -4,13 +4,13 @@ set -Eeu -o pipefail
 # shellcheck source=/dev/null
 source "${KOOPA_DIR}/shell/bash/include/header.sh"
 
-assert_has_sudo
-assert_is_os_fedora
-assert_is_installed yum
-assert_has_no_environments
+_koopa_assert_has_sudo
+_koopa_assert_is_os_fedora
+_koopa_assert_is_installed yum
+_koopa_assert_has_no_environments
 
-build_chgrp /usr/local
-update_ldconfig
+_koopa_build_chgrp /usr/local
+_koopa_update_ldconfig
 
 # Install gcc, if necessary.
 if [[ ! -x "$(command -v gcc)" ]]
