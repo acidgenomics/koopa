5d2990ff4bb2bb6023f3bf489ab9b8af76ba3326
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-20 17:52:40 -0400

    Improve info box

--- system/koopa/info.sh
+++ system/koopa/info.sh
@@ -3,56 +3,24 @@
 # Show koopa installation information (in a box).
 # Modified 2019-06-12.
 
-quiet_command() {
-    command -v "$1" 2>/dev/null
-}
+# shellcheck source=/dev/null
+source "${KOOPA_DIR}/shell/bash/include/header.sh"
 
 array=()
 array+=("$(koopa --version)")
 array+=("https://koopa.acidgenomics.com/")
-
 array+=("")
-
 array+=("## System information")
 array+=("OS: $(python -mplatform)")
 array+=("Current shell: ${KOOPA_SHELL}")
 array+=("Default shell: ${SHELL}")
 array+=("Install path: $KOOPA_DIR")
-
 array+=("")
-
 array+=("## Dependencies")
-
-locate() {
-    local command="$1"
-    local name="${2:-$command}"
-    local path="$(quiet_command "$command")"
-    if [[ -z "$path" ]]
-    then
-        path="[missing]"
-    else
-        path="$(realpath "$path")"
-    fi
-    printf "%s: %s" "$name" "$path"
-}
-
-array+=("$(locate bash Bash)")
-array+=("$(locate R)")
-array+=("$(locate python Python)")
-
-unset -f locate
-
+array+=("$(_koopa_locate bash Bash)")
+array+=("$(_koopa_locate R)")
+array+=("$(_koopa_locate python Python)")
 array+=("")
 array+=("Run 'koopa check' to verify installation.")
 
-# Using unicode box drawings here.
-# Note that we're truncating lines inside the box to 68 characters.
-barpad="$(printf "━%.0s" {1..70})"
-printf "\n  %s%s%s  \n"  "┏" "$barpad" "┓"
-for i in "${array[@]}"
-do
-    printf "  ┃ %-68s ┃  \n"  "${i::68}"
-done
-printf "  %s%s%s  \n\n"  "┗" "$barpad" "┛"
-
-unset -v array barpad
+_koopa_info_box "${array[@]}"
