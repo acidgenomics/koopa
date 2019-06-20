e5b2b8be407b6e7d59081eacd95701131dcd4aaa
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-20 17:19:36 -0400

    Update bash scripts

--- shell/bash/functions/path.sh
+++ shell/bash/functions/path.sh
@@ -1,34 +1,27 @@
 #!/usr/bin/env bash
 
 
-echo "WOOO HOOO"
-
-
-
-# FIXME NEED POSIX
 
 # Add local builds to PATH (e.g. '/usr/local').
 # This will recurse through the local library and find 'bin/' subdirs.
 # Note: read `-a` flag doesn't work on macOS. zsh related?
 # Modified 2019-06-20.
-add_local_bins_to_path() {
+_koopa_bash_add_local_bins_to_path() {
     local dir
     local dirs
     local prefix="$KOOPA_BUILD_PREFIX"
-    add_to_path_start "${prefix}/bin"
+    _koopa_add_to_path_start "${prefix}/bin"
     
     IFS=$'\n'
-    read -r -d '' dirs <<< "$(find_local_bin_dirs)"
+    read -r -d '' dirs <<< "$(_koopa_bash_find_local_bin_dirs)"
     unset IFS
     for dir in "${dirs[@]}"
     do
-        add_to_path_start "$dir"
+        _koopa_add_to_path_start "$dir"
     done
 }
 
 
-
-# FIXME NEED POSIX
 
 # Find local bin directories.
 #
@@ -37,9 +30,12 @@ add_local_bins_to_path() {
 # - https://stackoverflow.com/questions/7442417
 #
 # Modified 2019-06-17.
-find_local_bin_dirs() {
-    local array=()
-    local tmp_file="${KOOPA_TMP_DIR}/find"
+_koopa_bash_find_local_bin_dirs() {
+    local array
+    local tmp_file
+
+    array=()
+    tmp_file="${KOOPA_TMP_DIR}/find"
 
     find "$KOOPA_BUILD_PREFIX" \
         -mindepth 2 \
@@ -67,6 +63,3 @@ find_local_bin_dirs() {
 
     printf "%s\n" "${sorted[@]}"
 }
-
-
-
