9c2dd7a3ebbcc23596ff0f1ba100045f90d46f0e
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-20 08:18:07 -0400

    Reorganize files

---
+++ host/azure/config/etc/profile.d/constellation.sh
@@ -0,0 +1,11 @@
+# Constellation profile configuration
+# Last modified 2019-06-14 by Michael Steinbaugh
+
+export D1="/mnt/azbioinfoseq01"
+export D2="/mnt/azbioinfoseq02"
+export D3="/mnt/azbioinfoseq03"
+export D4="/mnt/azbioinfoseq04"
+export D5="/mnt/azbioinfoseq05"
+
+# These values will be detected in koopa.sh profile.
+export BCBIO_EXE="/data00/bcbio/v1.1.5/tools/bin/bcbio_nextgen.py"
