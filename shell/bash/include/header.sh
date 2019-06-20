0e7888d3064f789b981b91e72f1f0a4c530ec0ec
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-20 17:19:36 -0400

    Update bash scripts

--- shell/bash/include/header.sh
+++ shell/bash/include/header.sh
@@ -2,7 +2,14 @@
 set -Eeu -o pipefail
 
 # Bash internal functions.
-# Modified 2019-06-19.
+# Modified 2019-06-20.
 
-# shellcheck source=/dev/null
 source "${KOOPA_DIR}/system/include/functions.sh"
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
+functions_dir="$(dirname "$script_dir")/functions"
+
+for file in "${functions_dir}/"*".sh"
+do
+    [[ -f "$file" ]] && source "$file"
+done
