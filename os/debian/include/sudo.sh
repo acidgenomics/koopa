edcdb13c49b049f01a7f59307d47b6873064b5c5
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-21 11:11:45 -0400

    Update build scripts

--- os/debian/include/sudo.sh
+++ os/debian/include/sudo.sh
@@ -4,11 +4,11 @@ set -Eeu -o pipefail
 # shellcheck source=/dev/null
 source "${KOOPA_DIR}/shell/bash/include/functions.sh"
 
-assert_has_sudo
-assert_is_os_debian
-assert_is_installed apt-get
-assert_has_no_environments
-build_chgrp /usr/local
-update_ldconfig
+_koopa_assert_has_sudo
+_koopa_assert_is_os_debian
+_koopa_assert_is_installed apt-get
+_koopa_assert_has_no_environments
+_koopa_build_chgrp /usr/local
+_koopa_update_ldconfig
 
 sudo apt-get -y update
