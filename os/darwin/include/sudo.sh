16f17181a94bf2df00bb852818e056bf96dd2066
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-23 09:51:52 -0400

    Improve OS-specific scripts

--- os/darwin/include/sudo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -Eeu -o pipefail
-
-# shellcheck source=/dev/null
-source "${KOOPA_DIR}/shell/bash/include/functions.sh"
-
-_koopa_assert_has_sudo
-_koopa_assert_is_os_darwin
