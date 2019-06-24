18205e8f5d7386ceb6b1558e107dfd0014e423d5
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-24 14:15:25 +0000

    Improve Python install

--- system/include/cellar/python.sh
+++ system/include/cellar/python.sh
@@ -15,9 +15,23 @@ exe_file="${prefix}/bin/python3"
 
 printf "Installing %s %s.\n" "$name" "$version"
 
+rm -rf "$tmp_dir"
+mkdir "$tmp_dir"
+
+# Ensure pip is installed and up to date.
+if _koopa_has_sudo
+then
+    printf "Updating pip.\n"
+    (
+        cd "$tmp_dir"
+        wget https://bootstrap.pypa.io/get-pip.py
+        sudo python get-pip.py
+        sudo pip install -U pip
+        # > sudo pip install -U virtualenv
+    )
+fi
+
 (
-    rm -rf "$tmp_dir"
-    mkdir "$tmp_dir"
     cd "$tmp_dir"
     wget "https://www.python.org/ftp/python/${version}/Python-${version}.tar.xz"
     tar xfv "Python-${version}.tar.xz"
@@ -29,9 +43,9 @@ printf "Installing %s %s.\n" "$name" "$version"
         --enable-shared
     make
     make install
-    rm -rf "$tmp_dir"
 )
 
+rm -rf "$tmp_dir"
 link-cellar "$name" "$version"
 
 command -v "$exe_file"
