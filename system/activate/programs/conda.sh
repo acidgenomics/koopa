694758d60e29b964feb70ef6bf99b7aafbfd6431
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-24 16:28:47 -0400

    Improve conda detection

--- system/activate/programs/conda.sh
+++ system/activate/programs/conda.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2236
 
 # Activate Conda
-# Modified 2019-06-21.
+# Modified 2019-06-24.
 
 # It's no longer recommended to directly export conda in `$PATH`.
 # Instead source the `activate` script.
@@ -21,40 +21,50 @@ echo "$(koopa shell)" | grep -Eq "^(bash|zsh)$" || return
 
 # Attempt to detect the installation path automatically, if necessary.
 # Use `$CONDA_EXE` to manually set the path, for non-standard installs.
+
 # Priority:
-# 1. User anaconda3
-#    ~/anaconda3
-# 2. User miniconda3
-#    ~/miniconda3
-# 3. Shared anaconda3
-#    /usr/local/anaconda3
-# 4. Shared miniconda3
-#    /usr/local/miniconda3
+# - ~/.local/anaconda3
+# - ~/.local/miniconda3
+# - ~/anaconda3
+# - ~/miniconda3
+# - /usr/local/anaconda3
+# - /usr/local/miniconda3
 
 if [ -z "${CONDA_EXE:-}" ]
 then
-    if [ -f "${HOME}/anaconda3/bin/conda" ]
+    if [ -d "${HOME}/.local/anaconda3" ]
     then
-        export CONDA_EXE="${HOME}/anaconda3/bin/conda"
-    elif [ -f "${HOME}/miniconda3/bin/conda" ]
+        conda_home="${HOME}/.local/anaconda3"
+    elif [ -d "${HOME}/.local/miniconda3" ]
     then
-        export CONDA_EXE="${HOME}/miniconda3/bin/conda"
-    elif [ -f "/usr/local/anaconda3/bin/conda" ]
+        conda_home="${HOME}/.local/miniconda3"
+    elif [ -d "${HOME}/anaconda3" ]
     then
-        export CONDA_EXE="/usr/local/anaconda3/bin/conda"
-    elif [ -f "/usr/local/miniconda3/bin/conda" ]
+        conda_home="${HOME}/anaconda3"
+    elif [ -d "${HOME}/miniconda3" ]
     then
-        export CONDA_EXE="/usr/local/miniconda3/bin/conda"
+        conda_home="${HOME}/miniconda3"
+    elif [ -d "/usr/local/anaconda3" ]
+    then
+        conda_home="/usr/local/anaconda3"
+    elif [ -d "/usr/local/miniconda3" ]
+    then
+        conda_home="/usr/local/miniconda3"
+    fi
+
+    if [ ! -z "${conda_home:-}" ]
+    then
+        export CONDA_EXE="${conda_home}/bin/conda"
+        unset -v conda_home
     fi
 fi
 
-if [ -z "${CONDA_EXE:-}" ]
+# Early return if we don't detect an installation.
+[ -z "${CONDA_EXE:-}" ] && return
+
+# Early return with error if conda installation is set but not accessible.
+if [ ! -x "$CONDA_EXE" ]
 then
-    # Early return if we don't detect an installation.
-    return 0
-elif [ ! -x "$CONDA_EXE" ]
-then
-    # Early return with error if conda installation is set but not accessible.
     printf "conda does not exist at:\n%s\n" "$CONDA_EXE"
     return 1
 fi
@@ -66,4 +76,4 @@ conda_bin_dir="$(dirname "$CONDA_EXE")"
 # shellcheck source=/dev/null
 . "${conda_bin_dir}/activate"
 
-unset -v conda_bin_dir conda_env
+unset -v conda_bin_dir
