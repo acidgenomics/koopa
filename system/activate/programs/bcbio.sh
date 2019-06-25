94ffddb6b11a97d6ad9e7c06642be224f585ff12
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-25 12:31:51 -0400

    Improve activation, using internal conda environments

--- system/activate/programs/bcbio.sh
+++ system/activate/programs/bcbio.sh
@@ -2,6 +2,8 @@
 # shellcheck disable=SC2236
 
 # Include bcbio toolkit binaries in PATH, if defined.
+# Modified 2019-06-25.
+
 # Attempt to locate bcbio installation automatically on supported platforms.
 
 if [ -z "${BCBIO_EXE:-}" ]
@@ -12,20 +14,22 @@ then
     elif [ ! -z "${HARVARD_ODYSSEY:-}" ]
     then
         BCBIO_EXE="/n/regal/hsph_bioinfo/bcbio_nextgen/bin/bcbio_nextgen.py"
+    else
+        BCBIO_EXE=
     fi
 fi
 
-# Export in PATH if the binary is accessible.
-if [ -x "${BCBIO_EXE:-}" ]
+# Export in PATH, if accessible.
+if [ -x "$BCBIO_EXE" ]
 then
     export BCBIO_EXE
     unset -v PYTHONHOME PYTHONPATH
-    bcbio_bin_dir="$(dirname "$BCBIO_EXE")"
+    bin_dir="$(dirname "$BCBIO_EXE")"
     # Exporting at the end of PATH so we don't mask gcc or R.
     # This is particularly important to avoid unexpected compilation issues
     # due to compilers in conda masking the system versions.
-    force_add_to_path_end "$bcbio_bin_dir"
-    unset -v bcbio_bin_dir
+    force_add_to_path_end "$bin_dir"
+    unset -v bin_dir
 else
     unset -v BCBIO_EXE
 fi
---
+++ system/activate/programs/conda-envs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# shellcheck disable=SC2236
+
+# Put useful conda environments in PATH.
+# Modified 2019-06-25.
+
+_koopa_is_installed conda || return
+
+# Skip on macOS, where we can use Homebrew instead.
+_koopa_is_linux || return
+
+env_list="$(conda env list --json)"
+
+_koopa_add_conda_env_to_path pandoc "$env_list"
+_koopa_add_conda_env_to_path texlive "$env_list"
+
+unset -v env_list
