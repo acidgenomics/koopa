099da4a55a870271d9ce41f2556334fa8e8a6aca
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-26 12:29:16 -0400

    Update dotfiles-legacy.sh

--- system/uninstall/dotfiles-legacy.sh
+++ system/uninstall/dotfiles-legacy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Delete legacy dot file cruft.
+# Modified 2019-06-26.
+
 _koopa_delete_dotfile atom
 _koopa_delete_dotfile doom.d
+_koopa_delete_dotfile dotfiles
+_koopa_delete_dotfile dotfiles-private
 _koopa_delete_dotfile gitignore_global
