374490eb337e9f2188ac602050d67ad9cce0bf42
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-26 12:06:44 -0400

    Improve comments

--- system/linter/shellcheck.sh
+++ system/linter/shellcheck.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
-# Recursively run shellcheck on all scripts in a directory.
-# Modified 2019-06-26.
+## Recursively run shellcheck on all scripts in a directory.
+## Updated 2019-06-26.
 
 path="${1:-$PWD}"
 
-# Legacy method:
-# > find "$path" -name "*.sh" -exec shellcheck {} \;
+## Legacy method:
+## > find "$path" -name "*.sh" -exec shellcheck {} \;
 
-# This step recursively grep matches files with regular expressions.
-# Here we're checking for the shebang, rather than relying on file extension.
+## This step recursively grep matches files with regular expressions.
+## Here we're checking for the shebang, rather than relying on file extension.
 grep -Elr \
     --binary-files="without-match" \
     --exclude-dir={.git,cellar,conda} \
