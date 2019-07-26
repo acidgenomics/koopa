a5eaa10550cfabd5f598b86d0caf354af4478de2
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-26 12:06:32 -0400

    Improve comments

--- system/activate/exports/general.sh
+++ system/activate/exports/general.sh
@@ -1,62 +1,62 @@
 #!/bin/sh
 
-# General global variable exports.
-# Modified 2019-06-27.
+## General global variable exports.
+## Updated 2019-06-27.
 
 
 
-# Shell type                                                                {{{1
-# ==============================================================================
+## Shell type                                                                {{{1
+## ==============================================================================
 
-# Check if this is a login shell.
+## Check if this is a login shell.
 _koopa_is_login_bash && export LOGIN_BASH=1
 _koopa_is_login_zsh && export LOGIN_ZSH=1
 
-# Check if this is an interactive shell.
+## Check if this is an interactive shell.
 _koopa_is_interactive && export INTERACTIVE=1
 
 
 
-# Standard globals                                                          {{{1
-# ==============================================================================
+## Standard globals                                                          {{{1
+## ==============================================================================
 
-# This variables are used by some koopa scripts, so ensure they're always
-# consistently exported across platforms.
+## This variables are used by some koopa scripts, so ensure they're always
+## consistently exported across platforms.
 
-# HOSTNAME
+## HOSTNAME
 [ -z "${HOSTNAME:-}" ] && HOSTNAME="$(uname -n)" && export HOSTNAME
 
-# OSTYPE
-# Automatically set by bash and zsh.
-# Modified 2019-06-26.
+## OSTYPE
+## Automatically set by bash and zsh.
+## Updated 2019-06-26.
 [ -z "${OSTYPE:-}" ] && 
     OSTYPE="$(uname -s | tr '[:upper:]' '[:lower:]')" &&
     export OSTYPE
 
-# TERM
-# Terminal color mode. This should normally be set by the terminal client.
+## TERM
+## Terminal color mode. This should normally be set by the terminal client.
 [ -z "${TERM:-}" ] && export TERM="screen-256color"
 
-# TODAY
-# Current date. Alternatively, can use `%F` shorthand.
+## TODAY
+## Current date. Alternatively, can use `%F` shorthand.
 [ -z "${TODAY:-}" ] && TODAY="$(date +%Y-%m-%d)" && export TODAY
 
-# USER
+## USER
 [ -z "${USER:-}" ] && USER="$(whoami)" && export USER
 
 
 
-# Interface                                                                  {{{1
-# ==============================================================================
+## Interface                                                                 {{{1
+## ==============================================================================
 
-# Trim the maximum number of directories in prompt (PS1).
-# For bash, requires >= v4.
+## Trim the maximum number of directories in prompt (PS1).
+## For bash, requires >= v4.
 [ -z "${PROMPT_DIRTRIM:-}" ] && export PROMPT_DIRTRIM=4
 
 
 
-# History                                                                   {{{1
-# ==============================================================================
+## History                                                                   {{{1
+## ==============================================================================
 
 [ -z "${HISTFILE:-}" ] && 
     HISTFILE="$HOME/.$(koopa shell)-history" &&
@@ -70,8 +70,8 @@ _koopa_is_interactive && export INTERACTIVE=1
 [ -z "${HISTIGNORE:-}" ] && 
     export HISTIGNORE="&:ls:[bf]g:exit"
 
-# Add the date/time to `history` command output.
-# Note that on macOS bash will fail if `set -e` is set and this isn't exported.
+## Add the date/time to `history` command output.
+## Note that on macOS bash will fail if `set -e` is set and this isn't exported.
 [ -z "${HISTTIMEFORMAT:-}" ] && 
     export HISTTIMEFORMAT="%Y%m%d %T  "
 
@@ -80,23 +80,23 @@ _koopa_is_interactive && export INTERACTIVE=1
 
 
 
-# Locale / encoding                                                         {{{1
-# ==============================================================================
+## Locale / encoding                                                         {{{1
+## ==============================================================================
 
-# Force UTF-8 to avoid encoding issues for users with broken locale settings.
-# https://github.com/Homebrew/brew/blob/master/Library/Homebrew/brew.sh
-# > export LC_ALL="C"
+## Force UTF-8 to avoid encoding issues for users with broken locale settings.
+## https://github.com/Homebrew/brew/blob/master/Library/Homebrew/brew.sh
+## > export LC_ALL="C"
 
 [ "$(locale charmap 2>/dev/null)" != "UTF-8" ] &&
     export LC_ALL="en_US.UTF-8"
 
 
 
-# Editor                                                                    {{{1
-# ==============================================================================
+## Editor                                                                    {{{1
+## ==============================================================================
 
-# Set up text editor, if unset.
-# Using vim instead of emacs by default.
+## Set up text editor, if unset.
+## Using vim instead of emacs by default.
 if [ -z "${EDITOR:-}" ]
 then
     case "$(_koopa_quiet_which)" in
@@ -114,11 +114,11 @@ fi
 
 
 
-# CPU count                                                                 {{{1
-# ==============================================================================
+## CPU count                                                                 {{{1
+## ==============================================================================
 
-# Get the number of cores (CPUs) available.
-# Modified 2019-06-23.
+## Get the number of cores (CPUs) available.
+## Updated 2019-06-23.
 if _koopa_is_darwin
 then
     CPU_COUNT="$(sysctl -n hw.ncpu)"
@@ -126,43 +126,43 @@ elif _koopa_is_linux
 then
     CPU_COUNT="$(getconf _NPROCESSORS_ONLN)"
 else
-    # Otherwise assume single threaded.
+    ## Otherwise assume single threaded.
     CPU_COUNT=1
 fi
 export CPU_COUNT
 
 
 
-# Program-specific                                                          {{{1
-# ==============================================================================
+## Program-specific                                                          {{{1
+## ==============================================================================
 
-# GnuPGP                                                                    {{{2
-# ------------------------------------------------------------------------------
+## GnuPGP                                                                    {{{2
+## ------------------------------------------------------------------------------
 
-# Enable passphrase prompting in terminal.
-# Note that this step will error if tty isn't installed.
+## Enable passphrase prompting in terminal.
+## Note that this step will error if tty isn't installed.
 [ -z "${GPG_TTY:-}" ] && GPG_TTY="$(tty)" && export GPG_TTY
 
-# Ruby                                                                      {{{2
-# ------------------------------------------------------------------------------
+## Ruby                                                                      {{{2
+## ------------------------------------------------------------------------------
 
 [ -d "${HOME}/.gem" ] && export GEM_HOME="${HOME}/.gem"
 
-# rsync                                                                     {{{2
-# ------------------------------------------------------------------------------
+## rsync                                                                     {{{2
+## ------------------------------------------------------------------------------
 
-# Useful flags:
-# -a, --archive               archive mode; equals -rlptgoD (no -H,-A,-X)
-# -z, --compress              compress file data during the transfer
-# -L, --copy-links            transform symlink into referent file/dir
-#     --delete-before         receiver deletes before xfer, not during
-# -h, --human-readable        output numbers in a human-readable format
-#     --iconv=CONVERT_SPEC    request charset conversion of filenames
-#     --progress              show progress during transfer
-#     --dry-run
-#     --one-file-system
-#     --acls --xattrs
-#     --iconv=utf-8,utf-8-mac
+## Useful flags:
+## -a, --archive               archive mode; equals -rlptgoD (no -H,-A,-X)
+## -z, --compress              compress file data during the transfer
+## -L, --copy-links            transform symlink into referent file/dir
+##     --delete-before         receiver deletes before xfer, not during
+## -h, --human-readable        output numbers in a human-readable format
+##     --iconv=CONVERT_SPEC    request charset conversion of filenames
+##     --progress              show progress during transfer
+##     --dry-run
+##     --one-file-system
+##     --acls --xattrs
+##     --iconv=utf-8,utf-8-mac
 
 [ -z "${RSYNC_FLAGS:-}" ] &&
     RSYNC_FLAGS="$(_koopa_rsync_flags)" &&
--- system/activate/programs/conda.sh
+++ system/activate/programs/conda.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
 
-# Activate Conda
-# Modified 2019-06-27.
+## Activate Conda
+## Updated 2019-06-27.
 
-# It's no longer recommended to directly export conda in `$PATH`.
-# Instead source the `activate` script.
-#
-# Note that this code will attempt to locate the installation automatically,
-# unless `$CONDA_EXE` is set.
-#
-# Note that an environment will only be activated when `$CONDA_DEFAULT_ENV` is
-# set prior to running this code. Attempts to activate "base" will be ignored.
+## It's no longer recommended to directly export conda in `$PATH`.
+## Instead source the `activate` script.
+## ## Note that this code will attempt to locate the installation automatically,
+## unless `$CONDA_EXE` is set.
+## ## Note that an environment will only be activated when `$CONDA_DEFAULT_ENV` is
+## set prior to running this code. Attempts to activate "base" will be ignored.
 
-# Conda doesn't currently support ksh, and fish support is buggy.
+## Conda doesn't currently support ksh, and fish support is buggy.
 koopa shell | grep -Eq "^(bash|zsh)$" || return 0
 
-# Attempt to detect the installation path automatically, if necessary.
-# Use `$CONDA_EXE` to manually set the path, for non-standard installs.
+## Attempt to detect the installation path automatically, if necessary.
+## Use `$CONDA_EXE` to manually set the path, for non-standard installs.
 
-# Priority:
-# - ~/.local/anaconda3
-# - ~/.local/miniconda3
-# - ~/anaconda3
-# - ~/miniconda3
-# - /usr/local/anaconda3
-# - /usr/local/miniconda3
+## Priority:
+## - ~/.local/anaconda3
+## - ~/.local/miniconda3
+## - ~/anaconda3
+## - ~/miniconda3
+## - /usr/local/anaconda3
+## - /usr/local/miniconda3
 
 if [ -z "${CONDA_EXE:-}" ]
 then
@@ -59,12 +57,12 @@ then
     unset -v conda_home
 fi
 
-# Run activation script, if accessible.
+## Run activation script, if accessible.
 if [ -x "$CONDA_EXE" ]
 then
     bin_dir="$(dirname "$CONDA_EXE")"
-    # Activation script must run again inside a tmux session.
-    # shellcheck source=/dev/null
+    ## Activation script must run again inside a tmux session.
+    ## shellcheck source=/dev/null
     . "${bin_dir}/activate"
     unset -v bin_dir
     conda deactivate
