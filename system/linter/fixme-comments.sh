b6c9cd7178429044ac04c5e85948837804591695
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-27 13:28:00 -0400

    Improve linter

--- system/linter/fixme-comments.sh
+++ system/linter/fixme-comments.sh
@@ -2,17 +2,25 @@
 set -Eeu -o pipefail
 
 # Find FIXME and TODO comments.
-# Updated 2019-07-10.
+# Updated 2019-07-27.
 
 # Returns with `true` or `false` exit codes.
 
-path="${1:-$PWD}"
+path="${1:-$KOOPA_HOME}"
+
+## Directory exclusion:
+## `exclude-dir={dir1,dir2}` also works.
+## Don't quote this argument.
 
 hits="$( \
     grep -Er \
         --binary-files="without-match" \
-        --exclude-dir={.git,cellar,conda,doom.d,vim} \
         --exclude="fixme-comments.sh" \
+        --exclude-dir=".git" \
+        --exclude-dir="${KOOPA_HOME}/cellar" \
+        --exclude-dir="${KOOPA_HOME}/conda" \
+        --exclude-dir="${KOOPA_HOME}/system/config/dotfiles/doom.d" \
+        --exclude-dir="${KOOPA_HOME}/system/config/dotfiles/vim" \
         "\b(FIXME|TODO)\b" \
         "$path" | \
         sort || echo "" \
