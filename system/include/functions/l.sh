d1b7c0d38e42f85cdb77cb8311041ed1f7f254c0
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-27 13:58:01 -0400

    Remove option to link conda environment. This is a bad idea.

--- system/include/functions/l.sh
+++ system/include/functions/l.sh
@@ -27,34 +27,6 @@ _koopa_link_cellar() {
 
 
 
-# Experimental: may not work well on all systems.
-# Updated 2019-06-27.
-_koopa_link_conda_env() {
-    local env_name
-    local env_prefix
-    local build_prefix
-
-    env_name="$1"
-    env_prefix="$(koopa conda-prefix)/envs/${env_name}"
-    build_prefix="$(koopa build-prefix)"
-
-    printf "Linking %s in %s.\n" "$env_prefix" "$build_prefix"
-
-    _koopa_build_set_permissions "$env_prefix"
-
-    find "$env_prefix" \
-        -maxdepth 1 \
-        -mindepth 1 \
-        ! -name "*conda*" \
-        -print0 |
-        xargs -0 -I {} cp -frsv {} "$build_prefix/".
-
-    _koopa_build_set_permissions "$build_prefix"
-    _koopa_has_sudo && _koopa_update_ldconfig
-}
-
-
-
 # Used by `koopa info`.
 # Updated 2019-07-09.
 _koopa_locate() {
