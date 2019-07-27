d8aeb93ca6e2e90941ca8281b13b8cf683b792ea
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-27 10:19:13 -0400

    Don't attempt to autoconfig screen

--- system/install/dotfiles-shared.sh
+++ system/install/dotfiles-shared.sh
@@ -2,11 +2,10 @@
 set -Eeu -o pipefail
 
 ## Install general user dot files.
-## Updated 2019-06-21.
+## Updated 2019-06-27.
 
 dotfile --force condarc
 dotfile --force gitignore
-dotfile --force screenrc
 dotfile --force spacemacs
 dotfile --force tmux.conf
 dotfile --force vim
