0d8320460e93dc00444cbf94d5fcf2f5cc0b5fdc
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-28 17:28:47 -0400

    Attempt to fix linter to work on Travis

--- system/linter/03-shellcheck.sh
+++ system/linter/03-shellcheck.sh
@@ -2,21 +2,36 @@
 set -Eeu -o pipefail
 
 # Recursively run shellcheck on all scripts in a directory.
-# Updated 2019-07-27.
+# Updated 2019-07-28.
 
 path="${1:-$KOOPA_HOME}"
+
+exclude_dirs=(
+    "${KOOPA_HOME}/cellar"
+    "${KOOPA_HOME}/conda"
+    "${KOOPA_HOME}/system/activate/shell/zsh/fpath"
+    "${KOOPA_HOME}/system/config/dotfiles"
+    ".git"
+)
+
+# Note that full path exclusion doesn't work on Travis CI.
+if [[ -n "${TRAVIS:-}" ]]
+then
+    for i in "${!exclude_dirs[@]}"
+    do
+        exclude_dirs[$i]="$(basename "${exclude_dirs[$i]}")"
+    done
+fi
+
+# Prepend the `--exclude-dir=` flag.
+exclude_dirs=("${exclude_dirs[@]/#/--exclude-dir=}")
 
 # This step recursively grep matches files with regular expressions.
 # Here we're checking for the shebang, rather than relying on file extension.
 
 grep -Elr \
     --binary-files="without-match" \
-    --exclude="fixme-comments.sh" \
-    --exclude-dir=".git" \
-    --exclude-dir="${KOOPA_HOME}/cellar" \
-    --exclude-dir="${KOOPA_HOME}/conda" \
-    --exclude-dir="${KOOPA_HOME}/system/activate/shell/zsh/fpath" \
-    --exclude-dir="${KOOPA_HOME}/system/config/dotfiles" \
+    "${exclude_dirs[@]}" \
     '^#!/.*\b(ba)?sh\b$' \
     "$path" | \
     xargs -I {} shellcheck -x {}
