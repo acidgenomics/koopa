ae27a3f231e6230a33f216afebbd6fa62084092f
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-28 16:45:34 -0400

    Attempt to fix Travis CI build check

--- system/activate/exports/path.sh
+++ system/activate/exports/path.sh
@@ -84,8 +84,9 @@ _koopa_add_bins_to_path "host/$(koopa host-type)"
 # Private scripts                                                          {{{3
 # -----------------------------------------------------------------------------
 
-_koopa_add_to_path_start "$(koopa config-dir)/docker/bin"
-_koopa_add_to_path_start "$(koopa config-dir)/scripts-private/bin"
+# This is breaking on Travis CI.
+# _koopa_add_to_path_start "$(koopa config-dir)/docker/bin"
+# _koopa_add_to_path_start "$(koopa config-dir)/scripts-private/bin"
 
 
 
