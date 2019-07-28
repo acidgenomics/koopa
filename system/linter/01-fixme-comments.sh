7e82fffd6e4d0669f0646d5021c71b1b6da655cb
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-28 17:28:47 -0400

    Attempt to fix linter to work on Travis

--- system/linter/01-fixme-comments.sh
+++ system/linter/01-fixme-comments.sh
@@ -2,25 +2,41 @@
 set -Eeu -o pipefail
 
 # Find FIXME and TODO comments.
-# Updated 2019-07-27.
+# Updated 2019-07-28.
 
 # Returns with `true` or `false` exit codes.
 
 path="${1:-$KOOPA_HOME}"
 dotfiles_dir="${KOOPA_HOME}/system/config/dotfiles"
 
+exclude_dirs=(
+    "${KOOPA_HOME}/cellar"
+    "${KOOPA_HOME}/conda"
+    "${KOOPA_HOME}/system/activate/shell/zsh/fpath"
+    "${dotfiles_dir}/doom.d"
+    "${dotfiles_dir}/dracula"
+    "${dotfiles_dir}/os/darwin/terminal"
+    "${dotfiles_dir}/vim"
+    ".git"
+)
+
+# Note that full path exclusion doesn't work on Travis CI.
+if [[ -n "${TRAVIS:-}" ]]
+then
+    for i in "${!exclude_dirs[@]}"
+    do
+        exclude_dirs[$i]="$(basename "${exclude_dirs[$i]}")"
+    done
+fi
+
+# Prepend the `--exclude-dir=` flag.
+exclude_dirs=("${exclude_dirs[@]/#/--exclude-dir=}")
+
 hits="$( \
-    grep -Er \
+    grep -Elr \
         --binary-files="without-match" \
-        --exclude="${KOOPA_HOME}/system/linter/*fixme-comments.sh" \
-        --exclude-dir=".git" \
-        --exclude-dir="${dotfiles_dir}/doom.d" \
-        --exclude-dir="${dotfiles_dir}/dracula" \
-        --exclude-dir="${dotfiles_dir}/os/darwin/terminal" \
-        --exclude-dir="${dotfiles_dir}/vim" \
-        --exclude-dir="${KOOPA_HOME}/cellar" \
-        --exclude-dir="${KOOPA_HOME}/conda" \
-        --exclude-dir="${KOOPA_HOME}/system/activate/shell/zsh/fpath" \
+        --exclude="$(basename "$0")" \
+        "${exclude_dirs[@]}" \
         "\b(FIXME|TODO)\b" \
         "$path" | \
         sort || echo "" \
