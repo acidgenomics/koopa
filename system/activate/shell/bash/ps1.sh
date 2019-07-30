4c03d128c0cda273fa671641ff8a03c96b9fca73
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-30 06:45:43 -0400

    Add bash version number

--- system/activate/shell/bash/ps1.sh
+++ system/activate/shell/bash/ps1.sh
@@ -3,16 +3,38 @@
 # Define the prompt string.
 # Updated 2019-06-25.
 
-# \#: the command number of this command
-# \!: the history number of this command
-# \H: hostname
-# \W: working directory basename
-# \h: hostname up to the first `.`
-# \n: newline
-# \r: carriage return
-# \s: shell name, the basename of `$0`
-# \u: username
-# \w: working directory
+# Useful variables:
+# https://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html
+
+# \! : the history number of this command
+# \# : the command number of this command
+# \$ : if the effective UID is 0, a #, otherwise a $
+# \@ : the current time in 12-hour am/pm format
+# \A : the current time in 24-hour HH:MM format
+# \D{format} : the format is passed to strftime(3)
+# \H : hostname
+# \T : the current time in 12-hour HH:MM:SS format
+# \V : the release of bash, version + patch level (e.g., 2.00.0)
+# \W : the basename of the current working directory, with $HOME as tilde
+# \[ : begin a sequence of non-printing characters
+# \\ : a backslash
+# \] : end a sequence of non-printing characters
+# \a : an ASCII bell character (07)
+# \d : the date in “Weekday Month Date” format (e.g., “Tue May 26”)
+# \e : an ASCII escape character (033)
+# \h : hostname up to the first `.`
+# \j : the number of jobs currently managed by the shell
+# \l : the basename of the shellâ€™s terminal device name
+# \n : newline
+# \nnn : the character corresponding to the octal number nnn
+# \r : carriage return
+# \s : shell name, the basename of `$0`
+# \t : the current time in 24-hour HH:MM:SS format
+# \u : the username of the current user
+# \v : the version of bash (e.g., 2.00)
+# \w : the current working directory, with $HOME abbreviated with a tilde
+
+# The default value is `\s-\v\$ `.
 
 # Add the conda environment name.
 # Note that we have to source conda first (see shrc.rc above).
@@ -35,7 +57,7 @@ then
 fi
 
 # Shell name.
-shell="$(_koopa_shell)"
+shell="$(_koopa_shell) \v"
 
 # History.
 history="[c\#; h\!]"
