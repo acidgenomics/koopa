9029e76e19163301797604c28fb71d4a508ef7e6
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-14 14:25:53 -0400

    Add user profile configuration step

---
+++ system/activate/user-profile.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Source user configuration profile files.
+# Updated 2019-08-14.
+
+if _koopa_is_login
+then
+    case "$KOOPA_SHELL" in
+        bash)
+            file="${HOME}/.bash_profile"
+            ;;
+        zsh)
+            file="${HOME}/.zprofile"
+    esac
+    if [ -f "$file" ]
+    then
+        . "$file"
+    fi
+    unset -v file
+fi
+
+if _koopa_is_interactive
+then
+    case "$KOOPA_SHELL" in
+        bash)
+            file="${HOME}/.bashrc"
+            ;;
+        zsh)
+            file="${HOME}/.zshrc"
+            ;;
+    esac
+    if [ -f "$file" ]
+    then
+        . "$file"
+    fi
+    unset -v file
+fi
