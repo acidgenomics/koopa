abef2cfff084be24d46979e4348f465136e857c3
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-14 11:00:22 -0400

    Improve coreutils env linkage

--- system/include/cellar/coreutils.sh
+++ system/include/cellar/coreutils.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install GNU core utilities.
-# Updated 2019-06-25.
+# Updated 2019-08-14.
 
 # See also:
 # - https://ftp.gnu.org/gnu/coreutils/
@@ -35,18 +35,9 @@ printf "Installing %s %s.\n" "$name" "$version"
 
 link-cellar "$name" "$version"
 
-if _koopa_has_sudo
-then
-    build_prefix="$(koopa build-prefix)"
-    printf "Replacing '/usr/bin/env' with '/usr/local/bin/env'.\n"
-    if [[ ! -L "/usr/bin/env" ]]
-    then
-        printf "System version will be renamed to 'env.bak'.\n"
-        sudo mv "/usr/bin/env" "/usr/bin/env.bak"
-    else
-        sudo rm -f "/usr/bin/env"
-    fi
-    sudo ln -fns "${build_prefix}/bin/env" "/usr/bin/env"
+# Update '/usr/bin/env'.
+if _koopa_has_sudo then
+    link-coreutils-env
 fi
 
 "$exe_file" --version
