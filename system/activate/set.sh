8fd192f1f6d453af13b1f8b4539a63c243de5fa1
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-14 15:38:00 -0400

    Improve system config

--- system/activate/set.sh
+++ system/activate/set.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-# Set up text editor.
-# Using vi mode instead of emacs by default.
-set -o vi
+# Readline. Currently uses emacs by default.
+# https://unix.stackexchange.com/questions/30454
+case "$EDITOR" in
+    emacs)
+        set -o emacs
+        ;;
+    vi|vim)
+        set -o vi
+        ;;
+esac
