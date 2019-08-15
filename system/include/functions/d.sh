1ac51cb1c7bcc42687723900c09fa335dbaacca4
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-15 08:02:46 -0400

    Add disk check

--- system/include/functions/d.sh
+++ system/include/functions/d.sh
@@ -21,3 +21,32 @@ _koopa_delete_dotfile() {
         printf "Warning: Not symlink: %s\n" "$name"
     fi
 }
+
+
+
+# Check that disk has enough free space.
+# Updated 2019-08-15.
+_koopa_disk_check() {
+    local used
+    local limit
+    used="$(_koopa_disk_pct_used)"
+    limit="80"
+    if [ "$used" -gt "$limit" ]
+    then
+        >&2 printf "Warning: Disk usage is %d%%.\n" "$used"
+    fi
+}
+
+
+
+# Check disk usage on main drive.
+# Updated 2019-08-15.
+_koopa_disk_pct_used() {
+    local disk
+    disk="${1:-/}"
+    df -h "$disk" | \
+        head -n 2  | \
+        sed -n '2p' | \
+        grep -Eo "([.0-9]+%)" | \
+        sed 's/%$//'
+}
