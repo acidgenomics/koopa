09326f2a335ee2e704c9cc1459501aedcc0165a1
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-17 12:25:00 -0400

    Improve zsh config

--- system/activate/shell/zsh/fpath.sh
+++ system/activate/shell/zsh/fpath.sh
@@ -3,7 +3,7 @@
 # Set ZSH functions path.
 # Updated 2019-08-17.
 
-koopa_fpath="${KOOPA_HOME}/shell/zsh/site-functions"
+koopa_fpath="${KOOPA_HOME}/shell/zsh/functions"
 if [[ ! -d "$koopa_fpath" ]]
 then
     >&2 printf "Error: fpath directory is missing: '%s'.\n" "$koopa_fpath"
