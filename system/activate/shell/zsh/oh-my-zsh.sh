1125dd22888b6931aeeb11562b9a31a260b34b98
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-17 12:23:39 -0400

    Improve zsh config

--- system/activate/shell/zsh/oh-my-zsh.sh
+++ system/activate/shell/zsh/oh-my-zsh.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env zsh
 
 # oh-my-zsh configuration
-# Updated 2019-07-29.
+# Updated 2019-08-17.
 
 # See also:
 # - https://github.com/robbyrussell/oh-my-zsh
 
-# If you come from bash you might have to change your `$PATH`.
-# > export PATH="${HOME}/bin:/usr/local/bin:${PATH}"
 
-# Path to your oh-my-zsh installation.
+
+# Install                                                                   {{{1
+# ==============================================================================
+
+# Path to oh-my-zsh installation.
 export ZSH="${HOME}/.oh-my-zsh"
 export ZSH_CUSTOM="${ZSH}/custom"
 
@@ -20,29 +22,63 @@ then
     install-oh-my-zsh
 fi
 
+
+
+# Updates                                                                   {{{1
+# ==============================================================================
+
+# > DISABLE_AUTO_UPDATE="true"
+DISABLE_UPDATE_PROMPT=true
+# > export UPDATE_ZSH_DAYS=14
+
+
+
+# Security                                                                  {{{1
+# ==============================================================================
+
 # Ignore warning about insecure directories identified by compfix.
+# > compaudit | xargs chmod g-w,o-w
+
 ZSH_DISABLE_COMPFIX="true"
 
+
+
+# Theme                                                                     {{{1
+# ==============================================================================
+
 # Set name of the theme to load.
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-# Set to empty string when using pure prompt.
+# Set theme to empty string when using koopa prompt.
+
+# See also:
+# - https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+# - https://github.com/robbyrussell/oh-my-zsh/wiki/Customization#overriding-and-adding-themes
+# - https://github.com/robbyrussell/oh-my-zsh/wiki/External-themes
+
 ZSH_THEME=""
 
-# Uncomment the following line to use case-sensitive completion.
+
+
+# Completion                                                                {{{1
+# ==============================================================================
+
+# Enable case-sensitive completion.
 CASE_SENSITIVE="true"
+
+# Uncomment the following line to display red dots whilst waiting for
+# completion.
+COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line to enable command auto-correction.
+ENABLE_CORRECTION="true"
 
 # Uncomment the following line to use hyphen-insensitive completion.
 # Case-sensitive completion must be off. _ and - will be interchangeable.
 # > HYPHEN_INSENSITIVE="true"
 
-# Uncomment the following line to disable bi-weekly auto-update checks.
-# > DISABLE_AUTO_UPDATE="true"
 
-# Enable automatic upgrade, without prompting.
-DISABLE_UPDATE_PROMPT=true
 
-# Uncomment the following line to change how often to auto-update (in days).
-# > export UPDATE_ZSH_DAYS=13
+# Terminal window                                                           {{{1
+# ==============================================================================
 
 # Uncomment the following line to disable colors in ls.
 # > DISABLE_LS_COLORS="true"
@@ -50,12 +86,11 @@ DISABLE_UPDATE_PROMPT=true
 # Uncomment the following line to disable auto-setting terminal title.
 DISABLE_AUTO_TITLE="true"
 
-# Uncomment the following line to enable command auto-correction.
-# > ENABLE_CORRECTION="true"
 
-# Uncomment the following line to display red dots whilst waiting for
-# completion.
-COMPLETION_WAITING_DOTS="true"
+
+
+# Git                                                                       {{{1
+# ==============================================================================
 
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
@@ -72,35 +107,47 @@ DISABLE_UNTRACKED_FILES_DIRTY="true"
 # See 'man strftime' for details.
 HIST_STAMPS="yyyy-mm-dd"
 
-# Would you like to use another custom folder than $ZSH/custom?
-# > ZSH_CUSTOM=/path/to/new-custom-folder
 
-# Which plugins would you like to load?
-# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
+
+# Plugins                                                                   {{{1
+# ==============================================================================
+
+# Standard plugins can be found in `~/.oh-my-zsh/plugins/`.
+# Custom plugins may be added to `~/.oh-my-zsh/custom/plugins/`.
 
 plugins_dir="${ZSH_CUSTOM}/plugins"
 
-if [[ ! -d "${plugins_dir}/zsh-autosuggestions" ]]
+plugin="zsh-autosuggestions"
+if [[ ! -d "${plugins_dir}/${plugin}" ]]
 then
-    printf "Installing zsh-autosuggestions.\n"
-    git clone https://github.com/zsh-users/zsh-autosuggestions \
-        "${plugins_dir}/zsh-autosuggestions"
+    printf "Installing %s.\n" "$plugin"
+    git clone "https://github.com/zsh-users/${plugin}" \
+        "${plugins_dir}/${plugin}"
+fi
+
+plugin="zsh-syntax-highlighting"
+if [[ ! -d "${plugins_dir}/${plugin}" ]]
+then
+    printf "Installing %s.\n" "$plugin"
+    git clone "https://github.com/zsh-users/${plugin}" \
+        "${plugins_dir}/${plugin}"
 fi
 
 plugins=(
   git
   zsh-autosuggestions
-  # zsh-syntax-highlighting
+  zsh-syntax-highlighting
 )
 
 unset -v plugins_dir
 
+# Load oh-my-zsh.
 source "${ZSH}/oh-my-zsh.sh"
 
 
+
+# Overrides                                                                 {{{1
+# ==============================================================================
 
 # Set personal aliases, overriding those provided by oh-my-zsh libs, plugins,
 # and themes. Aliases can be placed here, though oh-my-zsh users are encouraged
@@ -113,8 +160,10 @@ source "${ZSH}/oh-my-zsh.sh"
 
 # Darken the autosuggest text color.
 # Define using xterm-256 color code.
+#
 # See also:
 # - https://stackoverflow.com/questions/47310537
 # - https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
+#
 # This works well in combo with Dracula theme.
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=240"
--- system/activate/shell/zsh/prompt.sh
+++ system/activate/shell/zsh/prompt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-# Pure prompt.
-# Updated 2019-07-29.
+# Configure prompt.
+# Updated 2019-08-16.
 
 # See also:
 # - https://github.com/sindresorhus/pure
@@ -10,25 +10,6 @@
 # This won't work if an oh-my-zsh theme is enabled.
 # This step must be sourced after oh-my-zsh.
 
-# Quick install using node:
-# > npm install --global pure-prompt
-#
-# Note that npm method requires write access into `/usr/local`.
-# We're configuring manually instead, which also works on remote servers.
-
-# > script_file="${(%):-%N}"
-# > script_dir="$(cd "$(dirname "$script_file")" >/dev/null 2>&1 && pwd)"
-
-koopa_fpath="${KOOPA_HOME}/shell/zsh/functions"
-if [[ ! -d "$koopa_fpath" ]]
-then
-    >&2 printf "Error: fpath directory is missing.\n%s\n" "$koopa_fpath"
-    return 1
-fi
-export FPATH="${koopa_fpath}:${FPATH}"
-
 autoload -U promptinit
 promptinit
-prompt pure
-
-unset -v koopa_fpath
+prompt koopa
