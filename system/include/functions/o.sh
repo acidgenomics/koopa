9f1e2385e63200fcc930f09f6416a23eb3be7f61
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-17 10:55:16 -0400

    Improve prompt string config

--- system/include/functions/o.sh
+++ system/include/functions/o.sh
@@ -3,32 +3,32 @@
 
 
 
-# Updated 2019-06-25.
+# Updated 2019-08-16.
 _koopa_os_type() {
-    local name
+    local id
     if _koopa_is_darwin
     then
-        name="$(uname -s | tr '[:upper:]' '[:lower:]')"
+        id="$(uname -s | tr '[:upper:]' '[:lower:]')"
     elif _koopa_is_linux
     then
-        name="$( \
+        id="$( \
             awk -F= '$1=="ID" { print $2 ;}' /etc/os-release | \
             tr -d '"' \
         )"
-	# Include the major release version for RHEL.
-	if [ "$name" = "rhel" ]
-	then
-        major_version="$( \
-            awk -F= '$1=="VERSION_ID" { print $2 ;}' /etc/os-release | \
-            tr -d '"' | \
-            cut -d '.' -f 1
-        )"
-        name="${name}${major_version}"
-	fi
+        # Include the major release version for RHEL.
+        if [ "$id" = "rhel" ]
+        then
+            version="$( \
+                awk -F= '$1=="VERSION_ID" { print $2 ;}' /etc/os-release | \
+                tr -d '"' | \
+                cut -d '.' -f 1
+            )"
+            id="${id}${version}"
+        fi
     else
-        name=
+        id=""
     fi
-    echo "$name"
+    echo "$id"
 }
 
 
@@ -36,4 +36,30 @@ _koopa_os_type() {
 # Updated 2019-06-22.
 _koopa_os_version() {
     uname -r
+}
+
+
+
+# Updated 2019-08-17.
+_koopa_os_version_prompt_string() {
+    local id
+    local version
+    if _koopa_is_darwin
+    then
+        string="macos"
+    elif _koopa_is_linux
+    then
+        id="$( \
+            awk -F= '$1=="ID" { print $2 ;}' /etc/os-release | \
+            tr -d '"' \
+        )"
+        version="$( \
+            awk -F= '$1=="VERSION_ID" { print $2 ;}' /etc/os-release | \
+            tr -d '"' \
+        )"
+        string="${id} ${version}"
+    else
+        string=""
+    fi
+    echo "$string"
 }
