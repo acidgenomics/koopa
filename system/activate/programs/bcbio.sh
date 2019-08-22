119369c483500e35ccedb6d472458b7f319e7f51
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-21 21:51:27 -0400

    Simplify activation

--- system/activate/programs/bcbio.sh
+++ system/activate/programs/bcbio.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Include bcbio toolkit binaries in PATH, if defined.
-# Updated 2019-06-25.
+# Updated 2019-08-21.
 
 # Attempt to locate bcbio installation automatically on supported platforms.
 
