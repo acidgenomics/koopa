1d45b80e8c76a3429d5b25e899c355e7708b5a8f
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-05 16:53:24 -0400

    Add find text function

--- system/include/functions/f.sh
+++ system/include/functions/f.sh
@@ -3,6 +3,21 @@
 
 
 
+#' Find text in any file.
+#'
+#' @note Updated 2019-09-05.
+#'
+#' @seealso
+#' - https://github.com/stephenturner/oneliners
+#'
+#' @examples
+#' _koopa_find_text "mytext" *.txt
+_koopa_find_text() {
+    find . -name "$2" -exec grep -il "$1" {} \;;
+}
+
+
+
 # Updated 2019-06-27.
 _koopa_force_add_to_path_end() {
     local dir
