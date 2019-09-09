71d63bdbb00264ca206ee806c4509d3a1703cda1
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-09 14:34:18 -0400

    Improve updater

--- system/include/koopa/update.sh
+++ system/include/koopa/update.sh
@@ -4,9 +4,32 @@
 source "$(koopa header bash)"
 
 # Update koopa installation.
-# Updated 2019-08-26.
+# Updated 2019-09-09.
+
+config_dir="$(_koopa_config_dir)"
+
+# spacemacs
+if [[ -x "${config_dir}/spacemacs" ]]
+then
+    printf "Updating spacemacs.\n"
+    (
+        cd "${config_dir}/spacemacs" || exit 1
+        git pull
+    )
+fi
+
+# oh-my-zsh
+if [[ -x "${config_dir}/oh-my-zsh" ]]
+then
+    printf "Updating Oh My Zsh.\n"
+    (
+        cd "${config_dir}/oh-my-zsh" || exit 1
+        git pull
+    )
+fi
 
 # Update repo.
+printf "Updating koopa.\n"
 (
     cd "$KOOPA_HOME" || exit 1
     git fetch --all
