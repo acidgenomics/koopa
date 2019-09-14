ef371fb530801dba656b5236856704980d838599
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-14 06:44:23 -0400

    Add Rust support

---
+++ system/activate/programs/rust.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Rust programming language.
+# Updated 2019-09-14.
+
+# Attempt to locate cargo home and source the env script.
+# This will put the rust cargo programs defined in `bin/` in the PATH.
+
+# Alternatively, can just add `${cargo_home}/bin` to PATH.
+
+cargo_home="${HOME}/.cargo"
+
+if [ -d "$cargo_home" ]
+then
+    source "${cargo_home}/env"
+fi
+
+unset -v cargo_home
