fddc8417b286d67938c8abaf9e7d1e2aa90ed7fc
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-16 09:52:11 -0400

    Add neovim cellar script

---
+++ system/include/cellar/neovim.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Install Neovim.
+# Updated 2019-09-16.
+
+# See also:
+# - https://neovim.io/
+# - https://github.com/neovim/neovim
+# - https://github.com/neovim/neovim/wiki/Installing-Neovim
+# - https://github.com/neovim/neovim/wiki/Building-Neovim
+
+# CMAKE_BUILD_TYPE:
+# - Release: Full compiler optimisations and no debug information. Expect the
+#   best performance from this build type. Often used by package maintainers.
+# - Debug: Full debug information; little optimisations. Use this for
+#   development to get meaningful output from debuggers like gdb or lldb. This
+#   is the default, if CMAKE_BUILD_TYPE is not specified.
+# - RelWithDebInfo ("Release With Debug Info"): Enables many optimisations and
+#   adds enough debug info so that when nvim ever crashes, you can still get a
+#   backtrace.
+
+_koopa_assert_has_no_environments
+
+name="neovim"
+version="$(_koopa_variable "$name")"
+prefix="$(_koopa_cellar_prefix)/${name}/${version}"
+tmp_dir="$(_koopa_tmp_dir)/${name}"
+exe_file="${prefix}/bin/${name}"
+
+printf "Installing %s %s.\n" "$name" "$version"
+
+(
+    rm -fr "$tmp_dir"
+    mkdir -pv "$tmp_dir"
+    cd "$tmp_dir" || exit 1
+    wget "https://github.com/${name}/${name}/archive/v${version}.tar.gz"
+    tar -xzvf "${name}-${version}"
+    cd "${name}-${version}" || exit 1
+    make \
+        --jobs="$CPU_COUNT" \
+        CMAKE_BUILD_TYPE=Release \
+        CMAKE_INSTALL_PREFIX="$prefix"
+    make install
+    rm -fr "$tmp_dir"
+)
+
+link-cellar "$name" "$version"
+"$exe_file" --version
+command -v "$exe_file"
