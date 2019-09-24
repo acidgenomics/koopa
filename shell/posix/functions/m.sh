f10b6c5cf24661d64b66f5f4488d2a0edf513ef4
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-24 17:20:53 -0400

    Add version extraction utility for macOS apps

--- shell/posix/functions/m.sh
+++ shell/posix/functions/m.sh
@@ -3,6 +3,19 @@
 
 
 
+# Extract the version of a macOS application.
+# Updated 2019-09-24.
+_koopa_macos_app_version() {
+    _koopa_assert_is_darwin
+    plutil -p "/Applications/${1}.app/Contents/Info.plist" | \
+        grep CFBundleShortVersionString |
+        awk -F ' => ' '{print $2}' |
+        tr -d '"' |
+        cut -d ' ' -f 1
+}
+
+
+
 # Updated 2019-08-17.
 _koopa_macos_version() {
     _koopa_assert_is_darwin
