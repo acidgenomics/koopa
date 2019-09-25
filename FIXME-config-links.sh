274a594ca2ad990847809ed7903d7f0ccc10e338
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-24 20:30:00 -0400

    Remove FIXME config link script

--- FIXME-config-links.sh
+++
@@ -1,7 +0,0 @@
-_koopa_add_config_link ~/.autojump autojump
-_koopa_add_config_link ~/.emacs.d spacemacs
-_koopa_add_config_link ~/.oh-my-zsh oh-my-zsh
-
-_koopa_add_config_link /usr/local/rbenv rbenv
-
-
