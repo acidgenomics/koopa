bb45e7b55185f57e1cb5a1e02e2914ba0d210d16
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-27 06:59:31 -0400

    Add help check script

---
+++ system/checks/help.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -Eeu -o pipefail
+
+# Check that all scripts support '--help' flag.
+# Updated 2019-09-27.
+find "${KOOPA_HOME}/bin" -type f -print0 | \
+    xargs -0 -I {} nice {} --help > dev/null
