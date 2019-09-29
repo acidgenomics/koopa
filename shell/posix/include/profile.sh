5edf7c2a62cb48f836bf31b2c0788f53edc48c54
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-29 10:59:35 -0400

    Fix typo

--- shell/posix/include/profile.sh
+++ shell/posix/include/profile.sh
@@ -1,5 +1,5 @@
 # koopa shell
 # https://koopa.acidgenomics.com/
 # shellcheck source=/dev/null
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 . "${XDG_CONFIG_HOME}/koopa/activate"
