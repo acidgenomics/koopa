a5beaea3fbe39ebe0d79ca12ad46bf085a08cf3f
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-30 16:22:57 -0400

    Add Lua install support

--- system/include/koopa/list.R
+++ system/include/koopa/list.R
@@ -2,7 +2,7 @@
 ## shebang requires env from coreutils >= 8.30.
 
 ## List user-accessible programs exported in PATH.
-## Updated 2019-07-30.
+## Updated 2019-09-30.
 
 options(
     error = quote(quit(status = 1L)),
