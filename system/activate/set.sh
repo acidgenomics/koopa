d748b30a7c3055851d3be9e84c1c4df428fd4138
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-01 08:11:20 -0400

    Disabling readline configuration based on EDITOR

--- system/activate/set.sh
+++ system/activate/set.sh
@@ -3,11 +3,11 @@
 
 # Readline. Currently uses emacs by default.
 # https://unix.stackexchange.com/questions/30454
-case "$EDITOR" in
-    emacs)
-        set -o emacs
-        ;;
-    vi|vim)
-        set -o vi
-        ;;
-esac
+# > case "$EDITOR" in
+# >     emacs)
+# >         set -o emacs
+# >         ;;
+# >     vi|vim)
+# >         set -o vi
+# >         ;;
+# > esac
