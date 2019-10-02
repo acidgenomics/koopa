9f5a2e2cede70d3ebdbdc8014aeeaff5bb597fc5
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-02 07:56:38 -0400

    Harden R checks against RStudio Server Pro

--- system/include/version/rstudio-server.sh
+++ system/include/version/rstudio-server.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Note that final step removes '-N' patch, which only applies to RStudio Server
+# Pro release version.
+
 rstudio-server version | \
     head -n 1 | \
-    cut -d ' ' -f 1
+    cut -d ' ' -f 1 | \
+    cut -d '-' -f 1
