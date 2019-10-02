493d39d1de7398e9f4fee2caa65a9910fd37f5db
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-02 12:48:04 -0400

    Rework locate program scripts

--- system/activate/os/darwin/aliases.sh
+++ system/activate/os/darwin/aliases.sh
@@ -12,7 +12,7 @@ alias rstudio="open -a rstudio"
 # Use `exa` instead of `ls`, if installed.
 # It has better color support.
 # See also: https://the.exa.website/
-if _koopa_quiet_which exa
+if _koopa_is_installed exa
 then
     alias ls="exa -Fg"
 fi
