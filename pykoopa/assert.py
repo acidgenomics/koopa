95cabc4d0c44200c583a5c59771f6790da3d519b
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-06 13:35:06 -0400

    Add internal Python functions defined in pykoopa

---
+++ pykoopa/assert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+
+
+def assert_is_not_file(x):
+    if os.path.isfile(x):
+        print("Error: File exists: '" + x + "'")
+        sys.exit(0)
+
+
+
+def assert_is_file(x):
+    if not os.path.isfile(x):
+        print("Error: Not file: '" + x + "'")
+        sys.exit(0)
