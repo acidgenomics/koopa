eafa1105621d8552460d3579f65d36b4a56fe8a8
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-07 14:02:54 -0400

    Improve automatic lintr checks on CI

--- system/checks/linter.sh
+++ system/checks/linter.sh
@@ -2,7 +2,7 @@
 set -Eeu -o pipefail
 
 # Linter checks.
-# Updated 2019-09-27.
+# Updated 2019-10-07.
 
 KOOPA_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." \
     >/dev/null 2>&1 && pwd -P)"
@@ -11,6 +11,18 @@ export KOOPA_HOME
 linter_dir="${KOOPA_HOME}/system/linter"
 for file in "${linter_dir}/"*".sh"
 do
-    # shellcheck source=/dev/null
-    [ -f "$file" ] && "$file"
+    if [[ -n "${CI:-}" ]]
+    then
+        case "$file" in
+            python-*|r-*)
+                ;;
+            *)
+                # shellcheck source=/dev/null
+                [ -f "$file" ] && "$file"
+                ;;
+        esac
+    else
+        # shellcheck source=/dev/null
+        [ -f "$file" ] && "$file"
+    fi
 done
