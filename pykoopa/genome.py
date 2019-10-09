38c22f388b168a8f13d154364686f9c919c4ca9b
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-09 10:39:31 -0400

    Generate tx2gene.csv automatically

--- pykoopa/genome.py
+++ pykoopa/genome.py
@@ -3,6 +3,7 @@
 Genome utilities.
 """
 
+import os
 import subprocess
 
 from pykoopa.sys import arg_string
@@ -60,6 +61,27 @@ def refseq_version():
     Updated 2019-10-07.
     """
     return _genome_version("refseq")
+
+
+def tx2gene_from_fasta(source_name, output_dir):
+    """
+    Generate tx2gene.csv mapping file from transcriptome FASTA.
+
+    Note that this function is currently called by genome download scripts, and
+    assumes that output_dir has a specific structure, containing a
+    "transcriptome" subdirectory with the FASTA.
+
+    Updated 2019-10-09.
+    """
+    cmd = "tx2gene-from-" + source_name + "-fasta"
+    transcriptome_dir = os.path.join(output_dir, "transcriptome")
+    input_file = os.path.join(transcriptome_dir, "*.fa.gz")
+    output_file = os.path.join(transcriptome_dir, "tx2gene.csv")
+    if os.path.isfile(output_file):
+        print("File exists: '" + output_file + "'.")
+        return output_file
+    os.system(cmd + " " + input_file + " " + output_file)
+    return output_file
 
 
 def wormbase_version():
