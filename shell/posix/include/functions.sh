311dd4af0cf4110e16ea270283807476aacaef42
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-15 11:36:41 -0400

    Don't attempt to fix echo

--- shell/posix/include/functions.sh
+++ shell/posix/include/functions.sh
@@ -1798,12 +1798,13 @@ _koopa_zsh_version() {
 # Fallback support                                                          {{{1
 # ==============================================================================
 
-if _koopa_is_installed echo
-then
-    echo() {
-        printf "%s\n" "$1"
-    }
-fi
+# This will cause RStudio console to return "-ne" at the top.
+# > if _koopa_is_installed echo
+# > then
+# >     echo() {
+# >         printf "%s\n" "$1"
+# >     }
+# > fi
 
 if ! _koopa_is_installed realpath
 then
