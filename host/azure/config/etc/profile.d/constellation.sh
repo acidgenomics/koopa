a1b6871588de7eeb4142b590962d7b443656329d
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-16 07:53:31 -0400

    Add comments regarding configuration

--- host/azure/config/etc/profile.d/constellation.sh
+++ host/azure/config/etc/profile.d/constellation.sh
@@ -5,6 +5,17 @@
 
 
 
+# Notes                                                                     {{{1
+# ==============================================================================
+
+# Do not set 'LD_LIBRARY_PATH'.
+# Use '/etc/ld.so.conf.d/' method instead.
+# Run 'sudo ldconfig' to update shared libraries.
+
+# Do not set 'R_HOME' or 'JAVA_HOME'.
+
+
+
 # Koopa                                                                     {{{1
 # ==============================================================================
 
