a5dc520e1fd1209a5205af79a35b15ad0af57faf
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-17 15:13:16 -0400

    Improve tmpdir creation

--- shell/bash/include/functions.sh
+++ shell/bash/include/functions.sh
@@ -377,30 +377,3 @@ _koopa_script_name() {
     )"
     basename "$file"
 }
-
-
-
-# T                                                                         {{{1
-# ==============================================================================
-
-_koopa_tmp_dir() {
-    # Create temporary directory.
-    #
-    # Note: macOS requires 'env LC_CTYPE=C'.
-    # Otherwise, you'll see this error: 'tr: Illegal byte sequence'.
-    # This doesn't seem to work reliably, so using timestamp instead.
-    #
-    # See also:
-    # - https://stackoverflow.com/questions/4632028
-    # - https://gist.github.com/earthgecko/3089509
-    #
-    # Alternate approach:
-    # > local unique
-    # > local dir
-    # > unique="$(date "+%Y%m%d-%H%M%S")"
-    # > dir="/tmp/koopa-$(id -u)-${unique}"
-    # > echo "$dir"
-    #
-    # Updated 2019-10-17.
-    mktemp -d
-}
