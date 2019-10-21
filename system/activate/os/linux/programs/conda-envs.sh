c7a2c847c131fe3325b507999537a96dd611e3ca
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-21 11:06:29 -0400

    Don't attempt to use TeX yet

--- system/activate/os/linux/programs/conda-envs.sh
+++ system/activate/os/linux/programs/conda-envs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Put useful conda environments in PATH.
-# Updated 2019-10-18.
+# Updated 2019-10-21.
 
 _koopa_is_installed conda || return 0
 
 _koopa_add_conda_env_to_path pandoc
-_koopa_add_conda_env_to_path texlive-core
+# > _koopa_add_conda_env_to_path texlive-core
