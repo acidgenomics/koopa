69b173ae5b13a2fe1b47c601a50cb120a31e058b
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-01 12:26:03 -0400

    Improve activation and checks

--- system/include/koopa/check.sh
+++ system/include/koopa/check.sh
@@ -3,3 +3,6 @@ set -Eeu -o pipefail
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 Rscript --vanilla "${script_dir}/check.R"
+
+_koopa_disk_check
+_koopa_is_azure && _koopa_check_azure
