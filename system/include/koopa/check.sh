8f57db3dbffdeec058cbcff31e9040483eac820d
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-04 14:16:03 -0500

    Update checks

--- system/include/koopa/check.sh
+++ system/include/koopa/check.sh
@@ -4,8 +4,7 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../../shell/bash/include/header.sh"
 
-_koopa_disk_check
-_koopa_is_azure && _koopa_check_azure
-
 Rscript --vanilla "${script_dir}/check.R"
 
+_koopa_disk_check
+_koopa_is_azure && _koopa_check_azure
