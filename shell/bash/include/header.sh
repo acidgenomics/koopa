419aa563f068e1fe4cbf2563c3576cf3692b62e6
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-09 09:03:29 -0500

    Make bash header smarter and capable of detecting sudo/sbin automatically

--- shell/bash/include/header.sh
+++ shell/bash/include/header.sh
@@ -2,7 +2,7 @@
 set -Eeu -o pipefail
 
 # Bash shared header script.
-# Updated 2019-11-05.
+# Updated 2019-11-09.
 
 KOOPA_BASH_INC="$(cd "$(dirname "${BASH_SOURCE[0]}")" \
     >/dev/null 2>&1 && pwd -P)"
@@ -16,5 +16,11 @@ source "${KOOPA_BASH_INC}/../../posix/include/functions.sh"
 source "${KOOPA_BASH_INC}/functions.sh"
 
 _acid_help "$@"
+
+# Require sudo permission to run 'sbin/' scripts.
+if echo "$0" | grep -q "/sbin/"
+then
+    _acid_assert_has_sudo
+fi
 
 unset -v KOOPA_BASH_INC
