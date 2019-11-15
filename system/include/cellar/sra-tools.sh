cec242ecf2e3ca271d7e84a431e49850d18e6363
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-15 14:42:07 -0500

    Tighten up bash shell

--- system/include/cellar/sra-tools.sh
+++ system/include/cellar/sra-tools.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -Eeu -o pipefail
 
 # RHEL 7 dependencies
 # file-devel : libmagic
--- system/include/cellar/zsh.sh
+++ system/include/cellar/zsh.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -Eeu -o pipefail
 
 name="zsh"
 version="$(_koopa_variable "$name")"
