09a310a1b5955af660837cfd50eb141cf26f5b3b
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-16 12:04:50 -0500

    Improve POSIX shell function checks

---
+++ shell/bash/functions/files.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+_koopa_remove_broken_symlinks() {
+    # """
+    # Remove broken symlinks.
+    # Updated 2019-11-16.
+    # """
+    dir="${1:-"."}"
+    dir="$(realpath "$dir")"
+    _koopa_message "Removing broken symlinks in '${dir}'."
+    mapfile -t arr <<< "$(find-broken-symlinks "$dir")"
+    _koopa_is_array_non_empty "${arr[@]}" || exit 0
+    for file in "${arr[@]}"
+    do
+        [[ -z "$file" ]] && continue
+        rm -v "$file"
+    done
+}
