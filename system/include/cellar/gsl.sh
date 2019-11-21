04f7c875e7a96fefffe556b3cebecaecde9c62fb
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-21 10:09:01 -0500

    Improve consistency of cellar scripts

--- system/include/cellar/gsl.sh
+++ system/include/cellar/gsl.sh
@@ -6,15 +6,13 @@ version="$(_koopa_variable "$name")"
 prefix="$(_koopa_cellar_prefix)/${name}/${version}"
 tmp_dir="$(_koopa_tmp_dir)/${name}"
 build="$(_koopa_make_build_string)"
+jobs="$(_koopa_cpu_count)"
 exe_file="${prefix}/bin/gsl-config"
 
 _koopa_message "Installing ${name} ${version}."
 
 (
-    rm -frv "$prefix"
-    rm -fr "$tmp_dir"
-    mkdir -pv "$tmp_dir"
-    cd "$tmp_dir" || exit 1
+    _koopa_cd_tmp_dir "$tmp_dir"
     file="gsl-${version}.tar.gz"
     url="http://mirror.keystealth.org/gnu/gsl/${file}"
     _koopa_download "$url"
@@ -23,7 +21,7 @@ _koopa_message "Installing ${name} ${version}."
     ./configure \
         --build="$build" \
         --prefix="$prefix"
-    make --jobs="$CPU_COUNT"
+    make --jobs="$jobs"
     make check
     make install
     rm -fr "$tmp_dir"
