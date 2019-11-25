66e417d89809980b193b9d74e19b04ce0946f888
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-25 14:26:37 -0500

    Speed up default koopa update

--- system/include/koopa/update.sh
+++ system/include/koopa/update.sh
@@ -5,32 +5,32 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../../shell/bash/include/header.sh"
 
+koopa_prefix="$(_koopa_prefix)"
+_koopa_message "Updating koopa at '${koopa_prefix}'."
+
+system=0
+
+while (("$#"))
+do
+    case "$1" in
+        --system)
+            system=1
+            shift 1
+            ;;
+        *)
+            _koopa_invalid_arg "$1"
+            ;;
+    esac
+done
+
 # _koopa_remove_broken_symlinks "$(_koopa_config_prefix)"
-
-if _koopa_is_darwin
-then
-    update-homebrew
-    update-r-packages
-    # > if _koopa_has_sudo
-    # > then
-    # >     update-tex
-    # > fi
-fi
-
-if _koopa_is_azure
-then
-    update-azure-vm --all
-else
-    update-conda
-fi
-
-update-venv
-update-rust
 
 # Loop across config directories and update git repos.
 # Consider nesting these under 'app' directory.
-config_dir="$(_koopa_config_prefix)"
-dirs=(
+config_prefix="$(_koopa_config_prefix)"
+_koopa_message "Updating user config at '${config_prefix}'."
+
+repos=(
     Rcheck
     docker
     dotfiles
@@ -40,16 +40,16 @@ dirs=(
     scripts-private
     spacemacs
 )
-for dir in "${dirs[@]}"
+for repo in "${repos[@]}"
 do
     # Skip directories that aren't a git repo.
-    if [[ ! -x "${config_dir}/${dir}/.git" ]]
+    if [[ ! -x "${config_prefix}/${repo}/.git" ]]
     then
         continue
     fi
-    _koopa_message "Updating '${dir}'."
+    _koopa_message "Updating '${repo}'."
     (
-        cd "${config_dir}/${dir}" || exit 1
+        cd "${config_prefix}/${repo}" || exit 1
         # Run updater script, if defined.
         # Otherwise pull the git repo.
         if [[ -x "UPDATE.sh" ]]
@@ -62,36 +62,48 @@ do
     )
 done
 
-# Update repo.
-_koopa_message "Updating koopa."
 (
-    cd "$KOOPA_PREFIX" || exit 1
+    cd "$koopa_prefix" || exit 1
     git fetch --all
     # > git checkout master
     git pull
+    _koopa_set_permissions "$koopa_prefix"
 )
 
-# Clean up legacy files.
-if [[ -d "${KOOPA_PREFIX}/system/config" ]]
-then
-    rm -frv "${KOOPA_PREFIX}/system/config"
-fi
-
-if _koopa_is_linux
-then
-    reset-prefix-permissions
-    prefix="$(_koopa_make_prefix)"
-    remove-broken-symlinks "$prefix"
-    remove-empty-dirs "$prefix"
-    remove-broken-cellar-symlinks
-fi
-
-# > remove-broken-symlinks "$HOME"
-
-# > _koopa_update_r_config
-_koopa_update_profile
-_koopa_update_ldconfig
 _koopa_update_xdg_config
+_koopa_update_ldconfig
+_koopa_update_profile
 
-_koopa_message "koopa update was successful."
+if [[ "$system" -eq 1 ]]
+then
+    _koopa_message "Updating system configuration."
+    if _koopa_is_darwin
+    then
+        update-homebrew
+        update-r-packages
+        # > if _koopa_has_sudo
+        # > then
+        # >     update-tex
+        # > fi
+    fi
+    if _koopa_is_azure
+    then
+        # We're rsyncing config, so don't update conda.
+        update-azure-vm --all
+    else
+        update-conda
+    fi
+    update-venv
+    update-rust
+    if _koopa_is_linux
+    then
+        reset-prefix-permissions
+        prefix="$(_koopa_make_prefix)"
+        remove-broken-symlinks "$prefix"
+        remove-empty-dirs "$prefix"
+        remove-broken-cellar-symlinks
+    fi
+fi
+
+_koopa_success "koopa update was successful."
 _koopa_note "Shell must be reloaded for changes to take effect."
