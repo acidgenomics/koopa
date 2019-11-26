cbb43a78dfbad55b9fde36c0f313e941b9f08e4f
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-26 10:59:41 -0500

    Add more notes about failed LLVM config

--- system/include/cellar/julia.sh
+++ system/include/cellar/julia.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
+# This is currently erroring out if LLVM 7 is installed.
+
 # https://github.com/JuliaLang/julia/blob/master/doc/build/build.md
 # https://github.com/JuliaLang/julia/blob/master/doc/build/linux.md
-
-# This is currently erroring out if LLVM 7 is installed.
+# https://docs.julialang.org/en/v1/devdocs/llvm/
 
 name="julia"
 version="$(_koopa_variable "$name")"
@@ -22,7 +23,17 @@ _koopa_message "Installing ${name} ${version}."
     _koopa_extract "$file"
     cd "julia-${version}" || exit 1
     # Customize 'Make.user' file.
-    echo "prefix=${prefix}" >> Make.user
+    # > echo "prefix=${prefix}" >> "Make.user"
+    cat >"Make.user" <<EOL
+prefix=${prefix}
+
+# This doesn't work.
+# LLVM_VER = 7.0.1
+
+# This doesn't work either.
+# LLVM_DEBUG = Release
+# LLVM_ASSERTIONS = 1
+EOL
     # This step is currently erroring out on RHEL 7 due to LLVM 7.
     make --jobs="$jobs"
     make install
