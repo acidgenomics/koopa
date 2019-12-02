772c2f57357ad801334454fdae26382bdae145e9
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-02 14:57:46 -0500

    Add findutils cellar script

---
+++ system/include/cellar/findutils.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -Eeu -o pipefail
+
+name="findutils"
+version="$(_koopa_variable "$name")"
+prefix="$(_koopa_cellar_prefix)/${name}/${version}"
+tmp_dir="$(_koopa_tmp_dir)/${name}"
+build="$(_koopa_make_build_string)"
+jobs="$(_koopa_cpu_count)"
+
+_koopa_message "Installing ${name} ${version}."
+
+(
+    _koopa_cd_tmp_dir "$tmp_dir"
+    file="${name}-${version}.tar.xz"
+    url="https://ftp.gnu.org/gnu/${name}/${file}"
+    _koopa_download "$url"
+    _koopa_extract "$file"
+    cd "${name}-${version}" || exit 1
+    ./configure \
+        --build="$build" \
+        --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make check
+    make install
+    rm -fr "$tmp_dir"
+)
+
+_koopa_link_cellar "$name" "$version"
