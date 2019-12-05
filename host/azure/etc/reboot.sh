0ee2dfbd6939c7b54b1fb27f3cfbed8a3b00f5b7
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-05 12:04:01 -0500

    Ensure RStudio tmp SSD gets configured correctly

--- host/azure/etc/reboot.sh
+++ host/azure/etc/reboot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # CPI Azure VM start-up script that runs at reboot.
-# Updated 2019-11-14.
+# Updated 2019-12-05.
 #
 # Permissions for '/mnt/' shares are defined in '/etc/fstab'.
 # These are set correctly except for '/mnt/resource/', which is fixed below.
@@ -9,8 +9,9 @@
 # Note that some shares on '/mnt/' are Azure Files mounted via CIFS/Samba and
 # don't support standard Unix permissions.
 
-if [ -e "/mnt/resource" ]
+if [ -e /mnt/resource ]
 then
-    chown root:root "/mnt/resource"
-    chmod 1777 "/mnt/resource"
+    chown root:root /mnt/resource
+    chmod 1777 /mnt/resource
+    sudo ln -fnsv /mnt/resource /tmp/rstudio-tmp
 fi
