17cf7b29099bf495d71180d5a5135c9e3fc88d93
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-05 15:18:32 -0500

    Improve default Perl config

--- system/include/cellar/perl.sh
+++ system/include/cellar/perl.sh
@@ -31,10 +31,10 @@ _koopa_message "Installing ${name} ${version}."
 
 _koopa_link_cellar "$name" "$version"
 
-# Install CPANminus.
-cpan App::cpanminus
+_koopa_message "Installing CPAN Minus."
+"${prefix}/bin/cpan" App::cpanminus
 _koopa_link_cellar "$name" "$version"
 
-# Install File::Rename module.
-cpanm File::Rename
-link-cellar perl
+_koopa_message "Installing 'File::Rename' module."
+"${prefix}/bin/cpanm" File::Rename
+_koopa_link_cellar "$name" "$version"
