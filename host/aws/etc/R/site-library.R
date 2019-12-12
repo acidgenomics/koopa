fb1b5b06a6d93b84b505e4ed8f7153482e7f2ef2
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-12 11:03:36 -0500

    Work on AWS config

---
+++ host/aws/etc/R/site-library.R
@@ -0,0 +1,16 @@
+## AWS packages
+##
+## https://github.com/cloudyr/aws.s3
+## https://github.com/ropensci/webservices
+##
+## aws.batch
+## aws.ec2
+## aws.ec2metadata
+## aws.efs
+## aws.glacier
+## aws.ml
+## aws.signature
+
+install("aws.iam")
+install("aws.lambda")
+install("aws.s3")
