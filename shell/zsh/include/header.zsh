24d9b438fb973f0d7c1822a1af2f7a1f54365f39
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-15 14:21:35 -0500

    Rebase

--- shell/zsh/include/header.zsh
+++ shell/zsh/include/header.zsh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 
 # ZSH shared header script.
-# Updated 2019-11-04.
+# Updated 2020-01-15.
 
 KOOPA_ZSH_INC="$(cd "$(dirname "${(%):-%N}")" >/dev/null 2>&1 && pwd -P)"
 
@@ -13,5 +13,13 @@ source "${KOOPA_ZSH_INC}/../../posix/include/functions.sh"
 # Source ZSH functions.
 # shellcheck source=/dev/null
 # > source "${KOOPA_ZSH_INC}/functions.sh"
+
+_koopa_help "$@"
+
+# Require sudo permission to run 'sbin/' scripts.
+if echo "$0" | grep -q "/sbin/"
+then
+    _koopa_assert_has_sudo
+fi
 
 unset -v KOOPA_ZSH_INC
