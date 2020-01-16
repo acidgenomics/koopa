1e61beff2674d0614c9ea4e00c90a44134882dc2
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-16 08:53:25 -0500

    Update linter checks

--- system/tests/linter.sh
+++ system/tests/linter.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
+# """
 # Check that scripts do not contain lints.
 # Updated 2020-01-14.
+# """
 
 script_dir="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
     >/dev/null 2>&1 && pwd -P)"
@@ -12,7 +14,7 @@ export KOOPA_PREFIX
 # shellcheck source=/dev/null
 source "${KOOPA_PREFIX}/shell/bash/include/header.sh"
 
-_koopa_message "Running lintr checks."
+_koopa_message "Running linter checks."
 
 linter_dir="${script_dir}/linter"
 for file in "${linter_dir}/"*".sh"
