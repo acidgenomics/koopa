fb919eed438db551afcfeab30410b31f5ab7aed8
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-16 10:49:06 -0500

    Improve cross-platform compatibility of exclusion matching

--- system/tests/linter/base-illegal-strings.sh
+++ system/tests/linter/base-illegal-strings.sh
@@ -4,58 +4,50 @@ set -Eeu -o pipefail
 # """
 # Find illegal strings, such as FIXME, TODO, and messed up git merges.
 # Updated 2020-01-16.
+#
+# Use find first, pass to array, and then call grep.
+# This is better supported across platforms.
 # """
 
 # shellcheck source=/dev/null
 source "${KOOPA_PREFIX}/shell/bash/include/header.sh"
 
-path="${1:-$KOOPA_PREFIX}"
+prefix="${1:-$KOOPA_PREFIX}"
 
-exclude_dirs=(
-    "${KOOPA_PREFIX}/dotfiles"
-    "${KOOPA_PREFIX}/shell/zsh/functions"
-    ".git"
-)
-exclude_files=(
-    "$(basename "$0")"
-    ".pylintrc"
-)
-
-# Full path exclusion seems to only work on macOS.
-if ! _koopa_is_macos
-then
-    for i in "${!exclude_dirs[@]}"
-    do
-        exclude_dirs[$i]="$(basename "${exclude_dirs[$i]}")"
-    done
-    for i in "${!exclude_files[@]}"
-    do
-        exclude_files[$i]="$(basename "${exclude_files[$i]}")"
-    done
-fi
-
-# Prepend the '--exclude=' flag.
-exclude_files=("${exclude_files[@]/#/--exclude=}")
-
-# Prepend the '--exclude-dir=' flag.
-exclude_dirs=("${exclude_dirs[@]/#/--exclude-dir=}")
-
+# shellcheck disable=SC2016
 illegal_strings=(
     '<<<<<<<'
     '>>>>>>>'
     '\bFIXME\b'
     '\bTODO\b'
+    '^path='
+    ' path='
+    '\$path'
 )
 grep_pattern="$(_koopa_paste0 '|' "${illegal_strings[@]}")"
 
+array=()
+while IFS= read -r -d $'\0'
+do
+    array+=("$REPLY")
+done < <( \
+    find "$prefix" \
+    -mindepth 1 \
+    -type f \
+    -not -name "$(basename "$0")" \
+    -not -name ".pylintrc" \
+    -not -path "${KOOPA_PREFIX}/.git/*" \
+    -not -path "${KOOPA_PREFIX}/dotfiles/*" \
+    -print0 \
+    | sort
+)
+
 hits="$( \
-    grep -Enr \
+    grep -En \
         --binary-files="without-match" \
-        "${exclude_files[@]}" \
-        "${exclude_dirs[@]}" \
         "$grep_pattern" \
-        "$path" | \
-        sort || echo "" \
+        "${array[@]}" \
+        || echo "" \
 )"
 
 name="$(_koopa_basename_sans_ext "$0")"
