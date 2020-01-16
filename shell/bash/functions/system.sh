53307994e95101b9a008f4a5481c1e47c05ac5b4
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-16 10:36:06 -0500

    Find lint cleanup

--- shell/bash/functions/system.sh
+++ shell/bash/functions/system.sh
@@ -45,7 +45,7 @@ _koopa_find_local_bin_dirs() {                                            # {{{1
         ! -path "*/lib/*" \
         ! -path "*/miniconda3/*" \
         -print0 > "$tmp_file"
-    while IFS=  read -r -d $'\0'
+    while IFS= read -r -d $'\0'
     do
         array+=("$REPLY")
     done < "$tmp_file"
