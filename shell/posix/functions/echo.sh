8a2beb57b603706924d594d313d7e63de35059a3
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-17 19:32:43 -0500

    Improve messages

--- shell/posix/functions/echo.sh
+++ shell/posix/functions/echo.sh
@@ -1,10 +1,80 @@
 #!/bin/sh
 # shellcheck disable=SC2039
 
-_koopa_echo_ansi() {                                                      # {{{1
+_koopa_ansi_escape_code() {                                               # {{{1
+    local escape
+    escape="${1:?}"
+    case "$escape" in
+        nocolor)
+            escape="0"
+            ;;
+        default)
+            escape="0;39"
+            ;;
+        default-bold)
+            escape="1;39"
+            ;;
+        black)
+            escape="0;30"
+            ;;
+        black-bold)
+            escape="1;30"
+            ;;
+        blue)
+            # This looks purple in Dracula config.
+            escape="0;34"
+            ;;
+        blue-bold)
+            escape="1;34"
+            ;;
+        cyan)
+            escape="0;36"
+            ;;
+        cyan-bold)
+            escape="1;36"
+            ;;
+        green)
+            escape="0;32"
+            ;;
+        green-bold)
+            escape="1;32"
+            ;;
+        magenta)
+            escape="0;35"
+            ;;
+        magenta-bold)
+            escape="1;35"
+            ;;
+        red)
+            escape="0;31"
+            ;;
+        red-bold)
+            escape="1;31"
+            ;;
+        yellow)
+            escape="0;33"
+            ;;
+        yellow-bold)
+            escape="1;33"
+            ;;
+        white)
+            escape="0;97"
+            ;;
+        white-bold)
+            escape="1;97"
+            ;;
+        *)
+            >&2 echo "Unsupported ANSI escape code: ${escape}"
+            escape="0"
+            ;;
+    esac
+    echo -e "\033[${escape}m"
+}
+
+_koopa_ansi_echo() {                                                      # {{{1
     # """
-    # Print a colored line in console.
-    # Updated 2020-01-12.
+    # Echo a colored line in console.
+    # Updated 2020-01-17.
     #
     # Currently using ANSI escape codes.
     # This is the classic 8 color terminal approach.
@@ -20,147 +90,210 @@ _koopa_echo_ansi() {                                                      # {{{1
     #
     # See also:
     # - https://en.wikipedia.org/wiki/ANSI_escape_code
+    # - https://misc.flogisoft.com/bash/tip_colors_and_formatting
     # - https://stackoverflow.com/questions/5947742
     # - https://stackoverflow.com/questions/15736223
     # - https://bixense.com/clicolors/
     # """
-    local escape
-    escape="${1:?}"
+    local color
+    color="$(_koopa_ansi_escape_code "${1:?}")"
+    local nocolor
+    nocolor="$(_koopa_ansi_escape_code "nocolor")"
     local string
     string="${2:?}"
-    local nocolor
-    nocolor="\033[0m"
-    local color
-    color="\033[${escape}m"
-    # > printf "%b%s%b\n" "$color" "$nocolor" "$string"
     echo -e "${color}${string}${nocolor}"
 }
 
+
+
+_koopa_echo_default() {                                                   # {{{1
+    _koopa_ansi_echo "default" "${1:?}"
+}
+
+_koopa_echo_default_bold() {                                              # {{{1
+    _koopa_ansi_echo "default-bold" "${1:?}"
+}
+
 _koopa_echo_black() {                                                     # {{{1
-    _koopa_echo_ansi "0;30" "${1:?}"
+    _koopa_ansi_echo "black" "${1:?}"
 }
 
 _koopa_echo_black_bold() {                                                # {{{1
-    _koopa_echo_ansi "1;30" "${1:?}"
+    _koopa_ansi_echo "black-bold" "${1:?}"
 }
 
 _koopa_echo_blue() {                                                      # {{{1
-    _koopa_echo_ansi "0;34" "${1:?}"
+    _koopa_ansi_echo "blue" "${1:?}"
 }
 
 _koopa_echo_blue_bold() {                                                 # {{{1
-    _koopa_echo_ansi "1;34" "${1:?}"
+    _koopa_ansi_echo "blue-bold" "${1:?}"
 }
 
 _koopa_echo_cyan() {                                                      # {{{1
-    _koopa_echo_ansi "0;36" "${1:?}"
+    _koopa_ansi_echo "cyan" "${1:?}"
 }
 
 _koopa_echo_cyan_bold() {                                                 # {{{1
-    _koopa_echo_ansi "1;36" "${1:?}"
+    _koopa_ansi_echo "cyan-bold" "${1:?}"
 }
 
 _koopa_echo_green() {                                                     # {{{1
-    _koopa_echo_ansi "0;32" "${1:?}"
+    _koopa_ansi_echo "green" "${1:?}"
 }
 
 _koopa_echo_green_bold() {                                                # {{{1
-    _koopa_echo_ansi "1;32" "${1:?}"
+    _koopa_ansi_echo "green-bold" "${1:?}"
 }
 
 _koopa_echo_magenta() {                                                   # {{{1
-    _koopa_echo_ansi "0;35" "${1:?}"
+    _koopa_ansi_echo "magenta" "${1:?}"
 }
 
 _koopa_echo_magenta_bold() {                                              # {{{1
-    _koopa_echo_ansi "1;35" "${1:?}"
+    _koopa_ansi_echo "magenta-bold" "${1:?}"
 }
 
 _koopa_echo_red() {                                                       # {{{1
-    _koopa_echo_ansi "0;31" "${1:?}"
+    _koopa_ansi_echo "red" "${1:?}"
 }
 
 _koopa_echo_red_bold() {                                                  # {{{1
-    _koopa_echo_ansi "1;31" "${1:?}"
+    _koopa_ansi_echo "red-bold" "${1:?}"
 }
 
 _koopa_echo_yellow() {                                                    # {{{1
-    _koopa_echo_ansi "0;33" "${1:?}"
+    _koopa_ansi_echo "yellow" "${1:?}"
 }
 
 _koopa_echo_yellow_bold() {                                               # {{{1
-    _koopa_echo_ansi "1;33" "${1:?}"
+    _koopa_ansi_echo "yellow-bold" "${1:?}"
 }
 
 _koopa_echo_white() {                                                     # {{{1
-    _koopa_echo_ansi "0;37" "${1:?}"
+    _koopa_ansi_echo "white" "${1:?}"
 }
 
 _koopa_echo_white_bold() {                                                # {{{1
-    _koopa_echo_ansi "1;37" "${1:?}"
+    _koopa_ansi_echo "white-bold" "${1:?}"
+}
+
+
+
+_koopa_emoji() {                                                          # {{{1
+    # """
+    # Koopa turtle emoji.
+    # Updated 2020-01-17.
+    # """
+    echo "🐢"
 }
 
 _koopa_message() {                                                        # {{{1
     # """
     # General message.
-    # Updated 2020-01-12.
+    # Updated 2020-01-17.
+    #
+    # Alternatives: ==> (Homebrew) / ⇨  / →
     # """
-    _koopa_echo_cyan_bold "${1:?}"
+    local c1 c2 nc pre str
+    pre="➔"
+    pre="$(_koopa_emoji) ${pre}  "
+    str="${1:?}"
+    c1="$(_koopa_ansi_escape_code "blue")"
+    c2="$(_koopa_ansi_escape_code "white-bold")"
+    nc="$(_koopa_ansi_escape_code "nocolor")"
+    echo -e "${c1}${pre}${c2}${str}${nc}"
 }
 
 _koopa_note() {                                                           # {{{1
     # """
-    # General note message.
-    # Updated 2020-01-12.
+    # General note (info).
+    # Updated 2020-01-17.
     # """
-    _koopa_echo_magenta_bold "Note: ${1:?}"
-}
-
-_koopa_status_fail() {                                                    # {{{1
-    # """
-    # Status FAIL.
-    # Updated 2020-01-12.
-    # """
-    _koopa_echo_red "  [FAIL] ${1:?}"
-}
-
-_koopa_status_note() {                                                    # {{{1
-    # """
-    # Status NOTE.
-    # Updated 2020-01-12.
-    # """
-    _koopa_echo_yellow "  [NOTE] ${1:?}"
-}
-
-_koopa_status_ok() {                                                      # {{{1
-    # """
-    # Status OK.
-    # Updated 2020-01-12.
-    # """
-    _koopa_echo_green "    [OK] ${1:?}"
+    local c1 c2 nc pre str
+    pre="ℹ"
+    pre="$(_koopa_emoji) ${pre}  "
+    str="${1:?}"
+    c1="$(_koopa_ansi_escape_code "cyan")"
+    c2="$(_koopa_ansi_escape_code "white")"
+    nc="$(_koopa_ansi_escape_code "nocolor")"
+    echo -e "${c1}${pre}${c2}${str}${nc}"
 }
 
 _koopa_stop() {                                                           # {{{1
     # """
-    # Stop with an error message.
-    # Updated 2020-01-12.
+    # Stop with an error message, and exit.
+    # Updated 2020-01-17.
     # """
-    >&2 _koopa_echo_red_bold "Error: ${1:?}"
-    exit 1
+    local c1 c2 nc pre str
+    pre="🔥"
+    pre="$(_koopa_emoji) ${pre}  ERROR: "
+    str="${1:?}"
+    c1="$(_koopa_ansi_escape_code "red-bold")"
+    c2="$(_koopa_ansi_escape_code "white-bold")"
+    nc="$(_koopa_ansi_escape_code "nocolor")"
+    >&2 echo -e "${c1}${pre}${c2}${str}${nc}"
+    # exit 1
 }
 
 _koopa_success() {                                                        # {{{1
     # """
     # Success message.
-    # Updated 2020-01-12.
+    # Updated 2020-01-17.
     # """
-    _koopa_echo_green_bold "${1:?}"
+    local c1 c2 nc pre str
+    pre="✔"
+    pre="$(_koopa_emoji) ${pre}  "
+    str="${1:?}"
+    c1="$(_koopa_ansi_escape_code "green")"
+    c2="$(_koopa_ansi_escape_code "white-bold")"
+    nc="$(_koopa_ansi_escape_code "nocolor")"
+    echo -e "${c1}${pre}${c2}${str}${nc}"
 }
 
 _koopa_warning() {                                                        # {{{1
     # """
     # Warning message.
-    # Updated 2020-01-12.
+    # Updated 2020-01-17.
     # """
-    >&2 _koopa_echo_yellow_bold "Warning: ${1:?}"
+    local c1 c2 nc pre str
+    pre="!!"
+    pre="$(_koopa_emoji) ${pre} WARNING: "
+    str="${1:?}"
+    c1="$(_koopa_ansi_escape_code "yellow-bold")"
+    c2="$(_koopa_ansi_escape_code "white-bold")"
+    nc="$(_koopa_ansi_escape_code "nocolor")"
+    >&2 echo -e "${c1}${pre}${c2}${str}${nc}"
+}
+
+
+
+_koopa_status_fail() {                                                    # {{{1
+    # """
+    # Status FAIL.
+    # Updated 2020-01-17.
+    # """
+    local str
+    str="  [FAIL] ${1:?}"
+    _koopa_echo_red "$str"
+}
+
+_koopa_status_note() {                                                    # {{{1
+    # """
+    # Status NOTE.
+    # Updated 2020-01-17.
+    # """
+    local str
+    str="  [NOTE] ${1:?}"
+    _koopa_echo_yellow "$str"
+}
+
+_koopa_status_ok() {                                                      # {{{1
+    # """
+    # Status OK.
+    # Updated 2020-01-17.
+    # """
+    local str
+    str="    [OK] ${1:?}"
+    _koopa_echo_green "$str"
 }
