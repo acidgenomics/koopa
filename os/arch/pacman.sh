9784c94cf1a001cd9e2ce6d454c65d573c74b4c6
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-22 11:26:22 -0500

    Add placeholder scripts

--- os/arch/pacman.sh
+++ os/arch/pacman.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
+# """
 # Optional dependencies for r
 #     tk: tcl/tk interface
 #     texlive-bin: latex sty files
 #     gcc-fortran: needed to compile some CRAN packages
 #     openblas: faster linear algebra
-
+#
 # Note that Arch is currently overwriting PS1 for root.
-# This is the source: /etc/profile
-# > Source global bash config
-# > if test "$PS1" && test "$BASH" && test -z ${POSIXLY_CORRECT+x} && 
-# > test -r /etc/bash.bashrc; then
-# >         . /etc/bash.bashrc
-# > fi
+# This is due to configuration in '/etc/profile'.
+# """
 
 pacman -Syyu --noconfirm && \
     pacman-db-upgrade && \
