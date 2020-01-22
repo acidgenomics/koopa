9a9e0b2e291a917caca088272fcb3dbb04b70dc4
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-22 11:04:26 -0500

    Attempt to harden scripts against non-interactive Debian (for Docker)

--- system/include/koopa/list.sh
+++ system/include/koopa/list.sh
@@ -5,4 +5,6 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../../shell/bash/include/header.sh"
 
+_koopa_assert_is_installed Rscript
+
 Rscript --vanilla "${script_dir}/list.R"
