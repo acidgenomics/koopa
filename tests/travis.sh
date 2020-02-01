12de132c9de2c27ee43e6df28dc4720326d6e185
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-01 17:45:35 -0500

    Attempt to re-enable additional checks

--- tests/travis.sh
+++ tests/travis.sh
@@ -10,8 +10,5 @@ tests_dir="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
     >/dev/null 2>&1 && pwd -P)"
 
 "${tests_dir}/linter.sh"
-
-# Disabled until we switch to Dockerized unit tests.
-# > "${tests_dir}/help.sh"
-
+"${tests_dir}/help.sh"
 "${tests_dir}/shunit2.sh"
