a1d0016c20b1e3df544696134a7f12bc7d9037b3
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-01 17:06:57 -0500

    Update unit tests

---
+++ lang/r/include/header.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+options(
+    "error" = quote(quit(status = 1L)),
+    "warning" = quote(quit(status = 1L))
+)
+
+#' Help
+#'
+#' Display help via `man` when `--help` or `-h` flag is detected.
+#'
+#' @note Updated 2020-02-01.
+#' @noRd
+koopaHelp <- function() {
+    args <- commandArgs(trailingOnly = FALSE)
+    if (!isTRUE(any(c("--help", "-h") %in% args))) {
+        return(invisible())
+    }
+    file <- grep(pattern = "--file", x = args)
+    file <- args[file]
+    file <- sub(pattern = "^--file=", replacement = "", x = file)
+    name <- basename(file)
+    system2(command = "man", args = name)
+    quit()
+}
+
+koopaHelp()
