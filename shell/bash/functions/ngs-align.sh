fadb1c7f8fd04c09481ce1a5dbb86639d4f96f54
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-04 15:08:13 -0500

    Draft update to bash functions

--- shell/bash/functions/ngs-align.sh
+++ shell/bash/functions/ngs-align.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Convert these other ones to a general function.
-
 _koopa_bam_filter() {
     # """
     # Perform filtering on a BAM file.
@@ -57,5 +55,90 @@ _koopa_bam_filter_multimappers() {                                        # {{{1
 
 _koopa_bam_filter_unmapped() {
     _koopa_bam_filter --filter="not unmapped" "$@"
+    return 0
+}
+
+_koopa_bowtie2() {                                                        # {{{1
+    # """
+    # Run bowtie2 on paired-end FASTQ files.
+    # Updated 2020-02-04.
+    # """
+    _koopa_assert_is_installed bowtie2
+    while (("$#"))
+    do
+        case "$1" in
+            --fastq-r1=*)
+                local fastq_r1="${1#*=}"
+                shift 1
+                ;;
+            --fastq-r2=*)
+                local fastq_r2="${1#*=}"
+                shift 1
+                ;;
+            --index-prefix=*)
+                local index_prefix="${1#*=}"
+                shift 1
+                ;;
+            --output-dir=*)
+                local output_dir="${1#*=}"
+                shift 1
+                ;;
+            --r1-tail=*)
+                local r1_tail="${1#*=}"
+                shift 1
+                ;;
+            --r2-tail=*)
+                local r2_tail="${1#*=}"
+                shift 1
+                ;;
+            *)
+                _koopa_invalid_arg "$1"
+                ;;
+        esac
+    done
+
+    # FIXME
+    _koopa_assert_is_set \
+        "$fastq_r1" \
+        "$fastq_r2" \
+        "$index_prefix" \
+        "$output_dir" \
+        "$r1_tail" \
+        "$r2_tail"
+
+    # FIXME _koopa_assert_is_set "XXX"
+
+    local id
+    id="$(basename "$fastq_r1")"
+    id="${id/${r1_tail}/}"
+
+    _koopa_info "Aligning '${id}'."
+
+    local sample_output_dir
+    sample_output_dir="${output_dir}/${id}"
+    [ -d "$sample_output_dir" ] && return 1
+    mkdir -pv "$sample_output_dir"
+
+    fastq_r1="${fastq_dir}/${id}${r1_tail}"
+    fastq_r2="${fastq_dir}/${id}${r2_tail}"
+
+    sam_file="${sample_output_dir}/${id}.sam"
+    log_file="${sample_output_dir}/bowtie2.log"
+
+    bowtie2 \
+        --local \
+        --rg "PL:illumina" \
+        --rg "PU:${id}" \
+        --rg "SM:${id}" \
+        --rg-id "$id" \
+        --sensitive-local \
+        -1 "$fastq_r1" \
+        -2 "$fastq_r2" \
+        -S "$sam_file" \
+        -X 2000 \
+        -p "$threads" \
+        -q \
+        -x "$index_prefix" \
+        2>&1 | tee "$log_file"
     return 0
 }
