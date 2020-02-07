904c7cd217e4f694f0aeaaeb3f519025dfdd5195
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-07 09:15:31 -0500

    Draft update to internal version functions

--- shell/posix/functions/versions.sh
+++ shell/posix/functions/versions.sh
@@ -1,11 +1,108 @@
 #!/bin/sh
 # shellcheck disable=SC2039
 
-# FIXME Rework the names: '_koopa_XXX_version'
-# FIXME Need to convert '-' to '_', maybe '.' for names.
-# FIXME Work on this in the internal koopa version functions.
+# """
+# Semantic versioning
+# https://semver.org/
+# MAJOR.MINOR.PATCH
+# """
 
-_koopa_autojump_version() {
+_koopa_version() {                                                        # {{{1
+    # """
+    # Koopa version.
+    # @note Updated 2020-02-07.
+    # """
+    _koopa_variable "koopa"
+}
+
+_koopa_os_version() {                                                     # {{{1
+    # """
+    # Operating system version.
+    # Updated 2020-01-13.
+    #
+    # Note that uname returns Darwin kernel version for macOS.
+    # """
+    if _koopa_is_macos
+    then
+        _koopa_current_version macos
+    else
+        uname -r
+    fi
+}
+
+
+
+_koopa_get_version() {                                                    # {{{1
+    # """
+    # Get the version of an installed program.
+    # Updated 2020-02-07.
+    # """
+    local app
+    app="${1:?}"
+    local fun
+    fun="$(_koopa_snake_case "_koopa_${app}_version")"
+    _koopa_assert_is_function "$fun"
+    "$fun"
+}
+
+_koopa_get_macos_app_version() {                                          # {{{1
+    # """
+    # Extract the version of a macOS application.
+    # Updated 2020-01-12.
+    # """
+    _koopa_assert_is_macos
+    local name
+    name="${1:?}"
+    plutil -p "/Applications/${name}.app/Contents/Info.plist" \
+        | grep CFBundleShortVersionString \
+        | awk -F ' => ' '{print $2}' \
+        | tr -d '"'
+}
+
+_koopa_github_latest_release() {                                          # {{{1
+    # """
+    # Get the latest release version from GitHub.
+    # Updated 2020-01-12.
+    #
+    # Example: _koopa_github_latest_release "acidgenomics/koopa"
+    # """
+    local repo
+    repo="${1:?}"
+    curl -s "https://github.com/${repo}/releases/latest" 2>&1 \
+        | grep -Eo '/tag/[.0-9v]+' \
+        | cut -d '/' -f 3 \
+        | sed 's/^v//'
+}
+
+
+
+_koopa_major_version() {                                                  # {{{1
+    # """
+    # Get the major program version.
+    # Updated 2020-01-12.
+    # """
+    local version
+    version="${1:?}"
+    echo "$version" | cut -d '.' -f 1
+}
+
+_koopa_minor_version() {                                                  # {{{1
+    # """
+    # Get the major program version.
+    # Updated 2020-01-12.
+    # """
+    local version
+    version="${1:?}"
+    echo "$version" | cut -d '.' -f 1-2
+}
+
+
+
+_koopa_autojump_version() {                                               # {{{1
+    # """
+    # Autojump version.
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed autojump || return 1
     autojump --version 2>&1 \
         | head -n 1 \
@@ -13,14 +110,21 @@ _koopa_autojump_version() {
         | sed 's/^v//'
 }
 
-_koopa_azure_cli_version() {
+_koopa_azure_cli_version() {                                              # {{{1
+    # """
+    # Azure CLI version.
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed az || return 1
     az --version \
         | head -n 1 \
         | grep -Eo '[0-9]\.[0-9]\.[0-9]'
 }
 
-_koopa_bash_version() {
+_koopa_bash_version() {                                                   # {{{1
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed bash || return 1
     bash --version \
         | head -n 1 \
@@ -28,12 +132,34 @@ _koopa_bash_version() {
         | cut -d '(' -f 1
 }
 
-_koopa_version_bcbio_nextgen() {
+_koopa_bcbio_nextgen_version() {                                          # {{{1
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed bcbio_nextgen.py || return 1
     bcbio_nextgen.py --version
 }
 
+_koopa_bcbio_nextgen_current_version() {                                  # {{{1
+    # """
+    # Get the latest bcbio-nextgen stable release version.
+    # @note Updated 2020-02-07.
+    #
+    # Alternate approach:
+    # > current="$(_koopa_github_latest_release "bcbio/bcbio-nextgen")"
+    # """
+    local url
+    url="https://raw.githubusercontent.com/bcbio/bcbio-nextgen\
+/master/requirements-conda.txt"
+    curl --silent "$url" \
+        | grep 'bcbio-nextgen=' \
+        | cut -d '=' -f 2
+}
+
 _koopa_version_bcl2fastq() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed bcl2fastq || return 1
     bcl2fastq --version 2>&1 \
         | sed -n '2p' \
@@ -42,11 +168,17 @@ _koopa_version_bcl2fastq() {
 }
 
 _koopa_version_bioconductor() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed Rscript || return 1
     Rscript -e 'cat(as.character(packageVersion("BiocVersion")), "\n")'
 }
 
 _koopa_version_broot() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed broot || return 1
     broot --version \
         | head -n 1 \
@@ -54,6 +186,9 @@ _koopa_version_broot() {
 }
 
 _koopa_version_cargo() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed cargo || return 1
     cargo --version \
         | head -n 1 \
@@ -61,6 +196,9 @@ _koopa_version_cargo() {
 }
 
 _koopa_version_clang() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed clang || return 1
     clang --version \
         | head -n 1 \
@@ -68,6 +206,9 @@ _koopa_version_clang() {
 }
 
 _koopa_version_conda() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed conda || return 1
     conda --version \
         | head -n 1 \
@@ -75,6 +216,9 @@ _koopa_version_conda() {
 }
 
 _koopa_version_coreutils() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     env --version \
         | head -n 1 \
         | cut -d ' ' -f 4 \
@@ -82,6 +226,9 @@ _koopa_version_coreutils() {
 }
 
 _koopa_version_docker() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed docker || return 1
     docker --version \
         | head -n 1 \
@@ -90,6 +237,9 @@ _koopa_version_docker() {
 }
 
 _koopa_version_emacs() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed emacs || return 1
     emacs --version \
         | head -n 1 \
@@ -97,6 +247,9 @@ _koopa_version_emacs() {
 }
 
 _koopa_version_exa() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed exa || return 1
     exa --version \
         | head -n 1 \
@@ -105,6 +258,9 @@ _koopa_version_exa() {
 }
 
 _koopa_version_fd() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed fd || return 1
     fd --version \
         | head -n 1 \
@@ -112,6 +268,9 @@ _koopa_version_fd() {
 }
 
 _koopa_version_fish() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed fish | return 1
     fish --version \
         | head -n 1 \
@@ -119,6 +278,9 @@ _koopa_version_fish() {
 }
 
 _koopa_version_fzf() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed fzf || return 1
     fzf --version \
         | head -n 1 \
@@ -126,6 +288,9 @@ _koopa_version_fzf() {
 }
 
 _koopa_version_gcc() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed gcc || return 1
     gcc --version \
         | head -n 1 \
@@ -135,6 +300,9 @@ _koopa_version_gcc() {
 
 # FIXME Previously labeled with darwin.
 _koopa_version_gcc_macos() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_macos || return 1
     _koopa_is_installed gcc || return 1
     gcc --version 2>&1 \
@@ -143,6 +311,9 @@ _koopa_version_gcc_macos() {
 }
 
 _koopa_version_gdal() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed gdalinfo || return 1
     gdalinfo --version \
         | head -n 1 \
@@ -151,11 +322,17 @@ _koopa_version_gdal() {
 }
 
 _koopa_version_geos() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed geos-config || return 1
     geos-config --version
 }
 
 _koopa_version_git() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed git || return 1
     git --version \
         | head -n 1 \
@@ -163,6 +340,9 @@ _koopa_version_git() {
 }
 
 _koopa_version_gnupg() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed gpg || return 1
     gpg --version \
         | head -n 1 \
@@ -170,6 +350,9 @@ _koopa_version_gnupg() {
 }
 
 _koopa_version_go() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed go || return 1
     go version \
         | grep -Eo "go[.0-9]+" \
@@ -177,13 +360,20 @@ _koopa_version_go() {
 }
 
 _koopa_version_gsl() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed gsl-config || return 1
     gsl-config --version \
         | head -n 1
 }
 
 _koopa_version_hdf5() {
+    # """
+    # @note Updated 2020-02-07.
+    #
     # Debian: 'dpkg -s libhdf5-dev'
+    # """
     _koopa_is_installed h5cc || return 1
     h5cc -showconfig \
         | grep 'HDF5 Version:' \
@@ -191,6 +381,9 @@ _koopa_version_hdf5() {
 }
 
 _koopa_version_homebrew() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed homebrew || return 1
     brew --version 2>&1 \
         | head -n 1 \
@@ -199,6 +392,9 @@ _koopa_version_homebrew() {
 }
 
 _koopa_version_htop() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed htop || return 1
     htop --version \
         | head -n 1 \
@@ -206,6 +402,9 @@ _koopa_version_htop() {
 }
 
 _koopa_version_java() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed java || return 1
     java -version 2>&1 \
         | head -n 1 \
@@ -214,28 +413,29 @@ _koopa_version_java() {
 }
 
 _koopa_version_julia() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed julia || return 1
     julia --version \
         | head -n 1 \
         | cut -d ' ' -f 3
 }
 
-# FIXME This is redundant.
-_koopa_version_koopa() {
-    _koopa_is_installed koopa
-    koopa --version \
-        | head -n 1 \
-        | cut -d ' ' -f 2
-}
-
 _koopa_version_llvm() {
+    # """
+    # @note Updated 2020-02-07.
+    #
     # Note that 'llvm-config' is versioned on most systems.
+    # """
     [ -x "${LLVM_CONFIG:-}" ] || return 1
     "$LLVM_CONFIG" --version
 }
 
 _koopa_lmod_version() {
     # """
+    # @note Updated 2020-02-07.
+    #
     # Alterate approach:
     # > module --version 2>&1 \
     # >     | grep -Eo "Version [.0-9]+" \
@@ -246,6 +446,9 @@ _koopa_lmod_version() {
 }
 
 _koopa_version_lua() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed lua || return 1
     lua -v 2>&1 \
         | head -n 1 \
@@ -253,6 +456,9 @@ _koopa_version_lua() {
 }
 
 _koopa_version_luarocks() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed luarocks || return 1
     luarocks --version \
         | head -n 1 \
@@ -260,11 +466,17 @@ _koopa_version_luarocks() {
 }
 
 _koopa_version_macos() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_macos || return 1
     sw_vers -productVersion
 }
 
 _koopa_version_neofetch() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed neofetch || return 1
     neofetch --version \
         | head -n 1 \
@@ -272,6 +484,9 @@ _koopa_version_neofetch() {
 }
 
 _koopa_version_neovim() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed nvim || return 1
     nvim --version \
         | head -n 1 \
@@ -280,6 +495,9 @@ _koopa_version_neovim() {
 }
 
 _koopa_version_openssl() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed openssl || return 1
     openssl version \
         | head -n 1 \
@@ -288,6 +506,9 @@ _koopa_version_openssl() {
 }
 
 _koopa_version_oracle_instantclient() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed sqlplus || return 1
     sqlplus -v \
         | grep -E '^Version' \
@@ -296,6 +517,9 @@ _koopa_version_oracle_instantclient() {
 }
 
 _koopa_version_pandoc() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed pandoc || return 1
     pandoc --version \
         | head -n 1 \
@@ -303,6 +527,9 @@ _koopa_version_pandoc() {
 }
 
 _koopa_version_perl_file_rename() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed rename || return 1
     local x
     x="$(rename --version | head -n 1)"
@@ -311,6 +538,9 @@ _koopa_version_perl_file_rename() {
 }
 
 _koopa_version_perl() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed perl || return 1
     perl --version \
         | sed -n '2p' \
@@ -319,6 +549,9 @@ _koopa_version_perl() {
 }
 
 _koopa_version_perlbrew() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed perlbrew || return 1
     perlbrew --version \
         | head -n 1 \
@@ -327,6 +560,9 @@ _koopa_version_perlbrew() {
 }
 
 _koopa_version_pip() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed pip3 || return 1
     pip3 --version \
         | head -n 1 \
@@ -334,12 +570,18 @@ _koopa_version_pip() {
 }
 
 _koopa_version_pipx() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed pipx || return 1
     pipx --version \
         | head -n 1
 }
 
 _koopa_version_proj() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed proj || return 1
     proj 2>&1 \
         | head -n 1 \
@@ -348,6 +590,9 @@ _koopa_version_proj() {
 }
 
 _koopa_version_pyenv() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed pyenv || return 1
     pyenv --version \
         | head -n 1 \
@@ -356,6 +601,9 @@ _koopa_version_pyenv() {
 }
 
 _koopa_version_python() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed python3 || return 1
     python3 --version 2>&1 \
         | head -n 1 \
@@ -363,6 +611,9 @@ _koopa_version_python() {
 }
 
 _koopa_version_r() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed R || return 1
     R --version \
         | grep 'R version' \
@@ -370,11 +621,17 @@ _koopa_version_r() {
 }
 
 _koopa_version_r_basejump() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed Rscript || return 1
     Rscript -e 'cat(as.character(packageVersion("basejump")), "\n")'
 }
 
 _koopa_version_rbenv() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed rbenv || return 1
     rbenv --version \
         | head -n 1 \
@@ -383,6 +640,9 @@ _koopa_version_rbenv() {
 }
 
 _koopa_version_ripgrep() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed rg || return 1
     rg --version \
         | head -n 1 \
@@ -390,6 +650,9 @@ _koopa_version_ripgrep() {
 }
 
 _koopa_version_rmate() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed rmate || return 1
     rmate --version \
         | head -n 1 \
@@ -398,6 +661,8 @@ _koopa_version_rmate() {
 
 _koopa_version_rstudio_server() {
     # """
+    # @note Updated 2020-02-07.
+    #
     # Note that final step removes '-N' patch, which only applies to
     # RStudio Server Pro release version.
     # """
@@ -409,6 +674,9 @@ _koopa_version_rstudio_server() {
 }
 
 _koopa_version_ruby() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed ruby || return 1
     ruby --version \
         | head -n 1 \
@@ -417,6 +685,9 @@ _koopa_version_ruby() {
 }
 
 _koopa_version_rust() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed rustc || return 1
     rustc --version \
         | head -n 1 \
@@ -424,6 +695,9 @@ _koopa_version_rust() {
 }
 
 _koopa_version_rustup() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed rustup || return 1
     rustup --version \
         | head -n 1 \
@@ -431,6 +705,9 @@ _koopa_version_rustup() {
 }
 
 _koopa_version_shellcheck() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed shellcheck || return 1
     shellcheck --version \
         | sed -n '2p' \
@@ -438,6 +715,9 @@ _koopa_version_shellcheck() {
 }
 
 _koopa_version_shiny_server() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed shiny-server || return 1
     shiny-server --version \
         | head -n 1 \
@@ -446,11 +726,17 @@ _koopa_version_shiny_server() {
 }
 
 _koopa_version_singularity() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed singularity || return 1
     singularity version
 }
 
 _koopa_version_sqlite() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed sqlite3 || return 1
     sqlite3 --version \
         | head -n 1 \
@@ -459,7 +745,9 @@ _koopa_version_sqlite() {
 
 _koopa_version_tex() {
     # """
-    # Note that we're checking the TeX Live release year here.
+    # @note Updated 2020-02-07.
+    #
+    # We're checking the TeX Live release year here.
     # Here's what it looks like on Debian/Ubuntu:
     # TeX 3.14159265 (TeX Live 2017/Debian)
     # """
@@ -473,6 +761,9 @@ _koopa_version_tex() {
 }
 
 _koopa_version_the_silver_searcher() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed ag || return 1
     ag --version \
         | head -n 1 \
@@ -480,6 +771,9 @@ _koopa_version_the_silver_searcher() {
 }
 
 _koopa_version_tmux() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed tmux || return 1
     tmux -V \
         | head -n 1 \
@@ -487,6 +781,9 @@ _koopa_version_tmux() {
 }
 
 _koopa_version_vim() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed vim || return 1
     local major
     major="$( \
@@ -511,6 +808,9 @@ _koopa_version_vim() {
 }
 
 _koopa_version_zsh() {
+    # """
+    # @note Updated 2020-02-07.
+    # """
     _koopa_is_installed zsh || return 1
     zsh --version \
         | head -n 1 \
