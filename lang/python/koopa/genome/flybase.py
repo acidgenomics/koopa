495bb24d445ef774d9f6eda79165ec8f52396dcc
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-09 08:34:17 -0500

    Reorganize genome functions

---
+++ lang/python/koopa/genome/flybase.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+"""
+FlyBase genome utilities.
+"""
+
+import os
+
+from koopa.sys import decompress_but_keep_original, download, paste_url, _genome_version
+
+
+def download_flybase_genome(release_url, output_dir, decompress, dmel):
+    """
+    Download genome FASTA.
+    Updated 2020-02-09.
+    """
+    output_dir = os.path.join(output_dir, "genome")
+    fasta_url = paste_url(release_url, "fasta")
+    download(url=paste_url(fasta_url, "md5sum.txt"), output_dir=output_dir)
+    download(
+        url=paste_url(fasta_url, "dmel-all-aligned-" + dmel + ".fasta.gz"),
+        output_dir=output_dir,
+        decompress=decompress,
+    )
+
+
+def download_flybase_transcriptome(release_url, output_dir, decompress, dmel):
+    """
+    Download FlyBase transcriptome FASTA.
+    Updated 2020-02-09.
+    """
+    output_dir = os.path.join(output_dir, "transcriptome")
+    cat_dir = os.path.join(output_dir, "cat")
+    output_fasta_file = os.path.join(
+        output_dir, "dmel-transcriptome-" + dmel + ".fasta.gz"
+    )
+    fasta_url = paste_url(release_url, "fasta")
+    download(url=paste_url(fasta_url, "md5sum.txt"), output_dir=cat_dir)
+    download(
+        url=paste_url(fasta_url, "dmel-all-transcript-" + dmel + ".fasta.gz"),
+        output_dir=cat_dir,
+    )
+    download(
+        url=paste_url(fasta_url, "dmel-all-miRNA-" + dmel + ".fasta.gz"),
+        output_dir=cat_dir,
+    )
+    download(
+        url=paste_url(fasta_url, "dmel-all-miscRNA-" + dmel + ".fasta.gz"),
+        output_dir=cat_dir,
+    )
+    download(
+        url=paste_url(fasta_url, "dmel-all-ncRNA-" + dmel + ".fasta.gz"),
+        output_dir=cat_dir,
+    )
+    download(
+        url=paste_url(fasta_url, "dmel-all-pseudogene-" + dmel + ".fasta.gz"),
+        output_dir=cat_dir,
+    )
+    download(
+        url=paste_url(fasta_url, "dmel-all-tRNA-" + dmel + ".fasta.gz"),
+        output_dir=cat_dir,
+    )
+    if not os.path.isfile(output_fasta_file):
+        print("Concatenating '" + output_fasta_file + "'.")
+        fasta_glob = os.path.join(cat_dir, "dmel-all-*.fasta.gz")
+        os.system("cat " + fasta_glob + " > " + output_fasta_file)
+        if decompress is True:
+            decompress_but_keep_original(output_fasta_file)
+
+
+def download_flybase_gtf(release_url, output_dir, decompress, dmel):
+    """
+    Download FlyBase GTF file.
+    Updated 2020-02-09.
+    """
+    output_dir = os.path.join(output_dir, "gtf")
+    gtf_url = paste_url(release_url, "gtf")
+    download(url=paste_url(gtf_url, "md5sum.txt"), output_dir=output_dir)
+    download(
+        url=paste_url(gtf_url, "dmel-all-" + dmel + ".gtf.gz"),
+        output_dir=output_dir,
+        decompress=decompress,
+    )
+
+
+def download_flybase_gff(release_url, output_dir, decompress, dmel):
+    """
+    Download FlyBase GFF3 file.
+    Updated 2020-02-09.
+    """
+    output_dir = os.path.join(output_dir, "gff")
+    gff_url = paste_url(release_url, "gff")
+    download(url=paste_url(gff_url, "md5sum.txt"), output_dir=output_dir)
+    download(
+        url=paste_url(gff_url, "dmel-all-" + dmel + ".gff.gz"),
+        output_dir=output_dir,
+        decompress=decompress,
+    )
+
+
+def flybase_dmel_version():
+    """
+    Current Drosophila melanogaster genome version on FlyBase.
+    Updated 2019-10-07.
+    """
+    return _genome_version("flybase", "--dmel")
+
+
+def flybase_version():
+    """
+    Current FlyBase release version.
+    Updated 2019-10-07.
+    """
+    return _genome_version("flybase")
