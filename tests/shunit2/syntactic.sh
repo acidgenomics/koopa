5fd5ec79767f00976d52fa3789ea4c2615470d4c
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-09 11:09:53 -0500

    Improve syntactic code coverage

---
+++ tests/shunit2/syntactic.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+## shellcheck disable=SC2016
+
+_koopa_is_r_package_installed syntactic || return 0
+
+test_camel_case_bin() {
+    MOCK_INPUT="${SHUNIT_TMPDIR}/foo bar"
+    MOCK_OUTPUT="$(dirname "${MOCK_INPUT}")/fooBar"
+    touch "$MOCK_INPUT"
+    assertTrue "[ -f \"${MOCK_INPUT}\" ]"
+    assertFalse "[ -f \"${MOCK_OUTPUT}\" ]"
+    camel-case "$MOCK_INPUT"
+    assertTrue "[ -f \"${MOCK_OUTPUT}\" ]"
+}
+
+test_kebab_case_bin() {
+    MOCK_INPUT="${SHUNIT_TMPDIR}/foo bar"
+    MOCK_OUTPUT="$(dirname "${MOCK_INPUT}")/foo-bar"
+    touch "$MOCK_INPUT"
+    assertTrue "[ -f \"${MOCK_INPUT}\" ]"
+    assertFalse "[ -f \"${MOCK_OUTPUT}\" ]"
+    kebab-case "$MOCK_INPUT"
+    assertTrue "[ -f \"${MOCK_OUTPUT}\" ]"
+}
+
+test_snake_case_bin() {
+    MOCK_INPUT="${SHUNIT_TMPDIR}/foo bar"
+    MOCK_OUTPUT="$(dirname "${MOCK_INPUT}")/foo_bar"
+    touch "$MOCK_INPUT"
+    assertTrue "[ -f \"${MOCK_INPUT}\" ]"
+    assertFalse "[ -f \"${MOCK_OUTPUT}\" ]"
+    snake-case "$MOCK_INPUT"
+    assertTrue "[ -f \"${MOCK_OUTPUT}\" ]"
+}
