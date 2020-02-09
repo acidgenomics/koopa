7864e38dc90ab75ca22a4b3a9142c9c6aad7d815
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-09 10:44:32 -0500

    Improve code coverage

--- shell/posix/functions/strings.sh
+++ shell/posix/functions/strings.sh
@@ -4,7 +4,7 @@
 _koopa_gsub() {                                                           # {{{1
     # """
     # Global substitution.
-    # Updated 2020-01-12.
+    # @note Updated 2020-01-12.
     # """
     local string
     string="${1:?}"
@@ -35,11 +35,12 @@ _koopa_snake_case() {                                                     # {{{1
 _koopa_strip_left() {                                                     # {{{1
     # """
     # Strip pattern from left side (start) of string.
-    # Updated 2019-09-22.
+    # @note Updated 2019-09-22.
     #
-    # Usage: _koopa_strip_left "string" "pattern"
+    # @usage _koopa_strip_left "string" "pattern"
     #
-    # Example: _koopa_strip_left "The Quick Brown Fox" "The "
+    # @examples
+    # _koopa_strip_left "The Quick Brown Fox" "The "
     # """
     local string
     string="${1:?}"
@@ -51,11 +52,12 @@ _koopa_strip_left() {                                                     # {{{1
 _koopa_strip_right() {                                                    # {{{1
     # """
     # Strip pattern from right side (end) of string.
-    # Updated 2020-01-12.
+    # @note Updated 2020-01-12.
     #
-    # Usage: _koopa_strip_right "string" "pattern"
+    # @usage _koopa_strip_right "string" "pattern"
     #
-    # Example: _koopa_strip_right "The Quick Brown Fox" " Fox"
+    # @examples
+    # _koopa_strip_right "The Quick Brown Fox" " Fox"
     # """
     local string
     string="${1:?}"
@@ -67,10 +69,13 @@ _koopa_strip_right() {                                                    # {{{1
 _koopa_strip_trailing_slash() {                                           # {{{1
     # """
     # Strip trailing slash in file path string.
-    # Updated 2020-01-12.
+    # @note Updated 2020-01-12.
     #
     # Alternate approach using sed:
     # > sed 's/\/$//' <<< "$1"
+    #
+    # @examples
+    # _koopa_strip_trailing_slash "dir/"
     # """
     local file
     file="${1:?}"
@@ -80,8 +85,8 @@ _koopa_strip_trailing_slash() {                                           # {{{1
 _koopa_sub() {                                                            # {{{1
     # """
     # Substitution.
-    # Updated 2020-01-12.
-    # See also: _koopa_gsub (for global matching).
+    # @note Updated 2020-01-12.
+    # @seealso _koopa_gsub (for global matching).
     # """
     local string
     string="${1:?}"
@@ -95,15 +100,14 @@ _koopa_sub() {                                                            # {{{1
 _koopa_trim_ws() {                                                        # {{{1
     # """
     # Trim leading and trailing white-space from string.
-    # Updated 2020-01-12.
+    # @note Updated 2020-01-12.
     #
     # This is an alternative to sed, awk, perl and other tools. The function
     # works by finding all leading and trailing white-space and removing it from
     # the start and end of the string.
     #
-    # Usage: _koopa_trim_ws "   example   string    "
-    #
-    # Example: _koopa_trim_ws "    Hello,  World    "
+    # @examples
+    # _koopa_trim_ws "    Hello,  World    "
     # """
     local string
     string="${1:?}"
