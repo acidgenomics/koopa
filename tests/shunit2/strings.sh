466d81604f1f3b33af03ee53d58b6aeb1a861c43
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-09 10:44:32 -0500

    Improve code coverage

--- tests/shunit2/strings.sh
+++ tests/shunit2/strings.sh
@@ -11,3 +11,33 @@ test_snake_case() {
         "$(_koopa_snake_case "hello world")" \
         "hello_world"
 }
+
+test_strip_left() {
+    assertEquals \
+        "$(_koopa_strip_left "The Quick Brown Fox" "The ")" \
+        "Quick Brown Fox"
+}
+
+test_strip_right() {
+    assertEquals \
+        "$(_koopa_strip_right "The Quick Brown Fox" " Fox")" \
+        "The Quick Brown"
+}
+
+_test_strip_trailing_slash() {
+    assertEquals \
+        "$(_koopa_strip_trailing_slash "https://acidgenomics.com/")" \
+        "https://acidgenomics.com"
+}
+
+test_sub() {
+    assertEquals \
+        "$(_koopa_sub "aaa-aaa-bbb-bbb" "bbb" "ccc")" \
+        "aaa-aaa-ccc-bbb"
+}
+
+test_trim_ws() {
+    assertEquals \
+        "$(_koopa_trim_ws "    Hello,  World    ")" \
+        "Hello,  World"
+}
