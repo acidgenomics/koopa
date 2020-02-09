7e5359dffc0add16dad53648fcffc9f9f38e511e
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-09 16:54:13 -0500

    Pass Python lintr checks

--- lang/python/koopa/argparse.py
+++ lang/python/koopa/argparse.py
@@ -3,15 +3,17 @@
 argparse functions.
 """
 
-
 import argparse
 import os
 
 
 def dir_path(path):
+    """
+    Directory path.
+    Updated 2020-02-09.
+    """
     if os.path.isdir(path):
         return path
-    else:
-        raise argparse.ArgumentTypeError(
-            f"readable_dir:{path} is not a valid path"
-        )
+    raise argparse.ArgumentTypeError(
+        f"readable_dir:{path} is not a valid path"
+    )
