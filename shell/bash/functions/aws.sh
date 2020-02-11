7950e72195abfa09202e155c73c0707f890dab59
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-11 15:07:12 -0500

    Add 'type' flag support

--- shell/bash/functions/aws.sh
+++ shell/bash/functions/aws.sh
@@ -11,6 +11,9 @@ _koopa_aws_s3_ls() {                                                      # {{{1
     # @examples
     # _koopa_aws_s3_ls s3://cpi-bioinfo01/
     # _koopa_aws_s3_ls cpi-bioinfo01/
+    # 
+    # # Directories only:
+    # aws-s3-ls --type=f s3://cpi-bioinfo01/datasets/
     # """
     _koopa_is_installed aws || return 1
 
@@ -20,12 +23,23 @@ _koopa_aws_s3_ls() {                                                      # {{{1
     local pos
     pos=()
 
+    local type
+    type=
+
     while (("$#"))
     do
         case "$1" in
             --recursive)
                 flags+=("--recursive")
                 shift 1
+                ;;
+            --type=*)
+                type="${1#*=}"
+                shift 1
+                ;;
+            --type)
+                type="$2"
+                shift 2
                 ;;
             --)
                 shift 1
@@ -42,6 +56,22 @@ _koopa_aws_s3_ls() {                                                      # {{{1
     done
     set -- "${pos[@]}"
 
+    local dirs files
+    case "${type:-}" in
+        d)
+            dirs=1
+            files=0
+            ;;
+        f)
+            dirs=0
+            files=1
+            ;;
+        *)
+            dirs=1
+            files=1
+            ;;
+    esac
+
     local prefix
     prefix="${1:?}"
     prefix="$(_koopa_strip_trailing_slash "$prefix")"
@@ -50,27 +80,35 @@ _koopa_aws_s3_ls() {                                                      # {{{1
     local x
     x="$(aws s3 ls "${flags[@]}" "$prefix")"
 
-    dirs="$(echo "$x" | grep -Eo '  PRE .+$' || true)"
-    if [[ -n "$dirs" ]]
+    # Directories.
+    if [[ "$dirs" -eq 1 ]]
     then
-        dirs="$( \
-            echo "$dirs" \
-                | sed 's/^  PRE //g' \
-                | sed "s|^|${prefix}|g" \
-        )"
-        echo "$dirs"
+        dirs="$(echo "$x" | grep -Eo '  PRE .+$' || true)"
+        if [[ -n "$dirs" ]]
+        then
+            dirs="$( \
+                echo "$dirs" \
+                    | sed 's/^  PRE //g' \
+                    | sed "s|^|${prefix}|g" \
+            )"
+            echo "$dirs"
+        fi
     fi
 
-    files="$(echo "$x" | grep -E '^[0-9]{4}-[0-9]{2}-[0-9]{2}' || true)"
-    if [[ -n "$files" ]]
+    # Files.
+    if [[ "$files" -eq 1 ]]
     then
-        files="$( \
-            echo "$files" \
-                | grep -Eo '  [0-9]+ .+$' \
-                | sed 's/^  [0-9]* //g' \
-                | sed "s|^|${prefix}|g" \
-        )"
-        echo "$files"
+        files="$(echo "$x" | grep -E '^[0-9]{4}-[0-9]{2}-[0-9]{2}' || true)"
+        if [[ -n "$files" ]]
+        then
+            files="$( \
+                echo "$files" \
+                    | grep -Eo '  [0-9]+ .+$' \
+                    | sed 's/^  [0-9]* //g' \
+                    | sed "s|^|${prefix}|g" \
+            )"
+            echo "$files"
+        fi
     fi
 
     return 0
