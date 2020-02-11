8ef6ab938f982cbb9e36e1f0db30fafd6043f6c2
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-10 22:21:26 -0500

    Rework AWS functions in Bash

--- shell/bash/functions/aws.sh
+++ shell/bash/functions/aws.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # shellcheck disable=SC2039
 
 _koopa_aws_s3_ls() {
@@ -13,15 +13,41 @@ _koopa_aws_s3_ls() {
     # _koopa_aws_s3_ls cpi-bioinfo01/
     # """
     _koopa_is_installed aws || return 1
+
+    local recursive
+    recursive=0
+
+    while (("$#"))
+    do
+        case "$1" in
+            --recursive)
+                recursive=1
+                shift 1
+                ;;
+            *)
+                _koopa_invalid_arg "$1"
+                ;;
+        esac
+    done
+
     local prefix
     prefix="${1:?}"
+    prefix="$(_strip_trailing_slash "$prefix")"
+    prefix="${prefix}/"
+
     local flags
-    flags=
-    #flags="--recursive"
+    flags=()
+
+    if [[ "$recursive" -eq 1 ]]
+    then
+        flags+=("--recursive")
+    fi
+
     local x
-    x="$(aws s3 ls $flags "$prefix")"
+    x="$(aws s3 ls "${flags[@]}" "$prefix")"
+
     dirs="$(echo "$x" | grep -Eo '  PRE .+$')"
-    if [ -n "$dirs" ]
+    if [[ -n "$dirs" ]]
     then
         dirs="$( \
             echo "$dirs" \
@@ -30,8 +56,9 @@ _koopa_aws_s3_ls() {
         )"
         echo "$dirs"
     fi
+
     files="$(echo "$x" | grep -E '^[0-9]{4}-[0-9]{2}-[0-9]{2}')"
-    if [ -n "$files" ]
+    if [[ -n "$files" ]]
     then
         files="$( \
             echo "$files" \
@@ -41,5 +68,6 @@ _koopa_aws_s3_ls() {
         )"
         echo "$files"
     fi
+
     return 0
 }
