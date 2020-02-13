5bdba408de182d06d6c4524cccf0a722f415d564
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-13 10:28:51 -0500

    Rework main activation scripts

--- system/activate/extra.sh
+++ system/activate/extra.sh
@@ -13,18 +13,6 @@
 
 
 
-# Dot files                                                                 {{{1
-# ==============================================================================
-
-dotfiles="$(_koopa_config_prefix)/dotfiles"
-if [ -d "$dotfiles" ]
-then
-    export DOTFILES="$dotfiles"
-fi
-unset -v dotfiles
-
-
-
 # umask                                                                     {{{1
 # ==============================================================================
 
@@ -57,84 +45,6 @@ unset -v dotfiles
 
 
 
-# History                                                                   {{{1
-# ==============================================================================
-
-# See bash(1) for more options.
-# For setting history length, see HISTSIZE and HISTFILESIZE.
-
-# Don't keep duplicate lines in the history.
-# Alternatively, set "ignoreboth" to also ignore lines starting with space.
-if [ -z "${HISTCONTROL:-}" ]
-then
-    export HISTCONTROL="ignoredups"
-fi
-
-# Standardize the history file name across shells.
-if [ -z "${HISTFILE:-}" ]
-then
-    HISTFILE="${HOME}/.$(_koopa_shell)-history"
-    export HISTFILE
-fi
-
-if [ -z "${HISTSIZE:-}" ]
-then
-    export HISTSIZE=100000
-fi
-
-if [ -z "${SAVEHIST:-}" ]
-then
-    export SAVEHIST=100000
-fi
-
-if [ -z "${HISTIGNORE:-}" ]
-then
-    export HISTIGNORE="&:ls:[bf]g:exit"
-fi
-
-# Add the date/time to 'history' command output.
-# Note that on macOS bash will fail if 'set -e' is set and this isn't exported.
-if [ -z "${HISTTIMEFORMAT:-}" ]
-then
-    export HISTTIMEFORMAT="%Y%m%d %T  "
-fi
-
-# For bash users, autojump keeps track of directories by modifying
-# '$PROMPT_COMMAND'. Do not overwrite '$PROMPT_COMMAND' in this case.
-# See also: https://github.com/wting/autojump
-# > export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ;} history -a"
-if [ -z "${PROMPT_COMMAND:-}" ]
-then
-    export PROMPT_COMMAND="history -a"
-fi
-
-
-
-# GNU coreutils                                                             {{{1
-# ==============================================================================
-
-
-if _koopa_is_installed dircolors
-then
-    # This will set the 'LD_COLORS' environment variable.
-    dircolors_file="${KOOPA_PREFIX}/dotfiles/app/coreutils/dircolors"
-    if [ -f "$dircolors_file" ]
-    then
-        eval "$(dircolors "$dircolors_file")"
-    else
-        eval "$(dircolors -b)"
-    fi
-    unset -v dircolors_file
-    alias dir='dir --color=auto'
-    alias egrep='egrep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias grep='grep --color=auto'
-    alias ls='ls --color=auto'
-    alias vdir='vdir --color=auto'
-fi
-
-
-
 # Default text editor                                                       {{{1
 # ==============================================================================
 
@@ -159,49 +69,6 @@ fi
 if [ -z "${PAGER:-}" ]
 then
     export PAGER="less"
-fi
-
-
-
-# Docker                                                                    {{{1
-# ==============================================================================
-
-alias docker-prune='docker system prune --all --force'
-
-
-
-# Emacs                                                                     {{{1
-# ==============================================================================
-
-_koopa_add_to_path_start "${HOME}/.emacs.d/bin"
-
-alias emacs='emacs --no-window-system'
-
-# Use terminal (console) mode by default instead of window system.
-# Allow fast, default mode that skips '.emacs', '.emacs.d', etc.
-alias emacs-default='emacs --no-init-file --no-window-system'
-
-# Run with 24-bit true color support.
-alias emacs24='TERM=xterm-24bit emacs --no-window-system'
-
-
-
-# exa                                                                    {{{1
-# ==============================================================================
-
-
-
-
-# GCC                                                                       {{{1
-# ==============================================================================
-
-# Colored GCC warnings and errors.
-if [ -z "${GCC_COLORS:-}" ]
-then
-    # SC1004: This backslash+linefeed is literal. Break outside single quotes if
-    # you just want to break the line.
-    export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:'\
-'locus=01:quote=01'
 fi
 
 
@@ -262,16 +129,6 @@ fi
 
 
 
-# Ruby                                                                      {{{1
-# ==============================================================================
-
-if [ -d "${HOME}/.gem" ]
-then
-    export GEM_HOME="${HOME}/.gem"
-fi
-
-
-
 # rsync                                                                     {{{1
 # ==============================================================================
 
@@ -294,6 +151,20 @@ _koopa_activate_fzf
 
 # Aliases                                                                   {{{1
 # ==============================================================================
+
+# Improve defaults                                                        # {{{2
+# ------------------------------------------------------------------------------
+
+# Note that macOS ships with a very old version of GNU coreutils.
+# Update these using Homebrew.
+
+alias cp='cp --archive --interactive --verbose'
+alias df='df -H'
+alias du='du -sh'
+alias less='less --ignore-case --raw-control-chars'
+alias mkdir='mkdir --parents --verbose'
+alias mv="mv --interactive --verbose"
+alias rm='rm --dir --interactive="once" --preserve-root --verbose'
 
 # Shortcuts                                                               # {{{2
 # ------------------------------------------------------------------------------
@@ -322,6 +193,15 @@ alias l1='ls -1'
 alias u='clear; cd ../; pwd; l'
 alias d='clear; cd -; l'
 
+alias cls='clear; ls'
+alias df2='df --portability --print-type --si | sort'
+alias lhead='l | head'
+alias ltail='l | tail'
+alias reload='exec "$SHELL" -l'
+alias sha256='shasum -a 256'
+alias tardown='tar -xzvf'
+alias tarup='tar -czvf'
+
 # Navigate up parent directories without 'cd'.
 # These are also supported by autojump.
 # > alias ..='cd ..'
@@ -330,44 +210,52 @@ alias d='clear; cd -; l'
 # > alias .....='cd ../../../../'
 # > alias ......='cd ../../../../../'
 
-
-
-# Improved app defaults                                                   # {{{2
+# Non-standard programs                                                   # {{{2
 # ------------------------------------------------------------------------------
 
-# Note that macOS ships with a very old version of GNU coreutils.
-# Update these using Homebrew.
+if _koopa_is_installed R
+then
+    alias R='R --no-restore --no-save --quiet'
+fi
 
-alias R='R --no-restore --no-save --quiet'
-# Note that 79 characters conforms to PEP8 (see flake8 for details).
-alias black="black --line-length=79"
-alias cp='cp --archive --interactive --verbose'
-alias df='df -H'
-alias du='du -sh'
-alias less='less --ignore-case --raw-control-chars'
-alias mkdir='mkdir --parents --verbose'
-alias mv="mv --interactive --verbose"
-alias rm='rm --dir --interactive="once" --preserve-root --verbose'
+if _koopa_is_installed black
+then
+    # Note that 79 characters conforms to PEP8 (see flake8 for details).
+    alias black="black --line-length=79"
+fi
 
-# App alternates                                                          # {{{2
-# ------------------------------------------------------------------------------
+if _koopa_is_installed docker
+then
+    alias docker-prune='docker system prune --all --force'
+fi
 
-alias cls='clear; ls'
-alias df2='df --portability --print-type --si | sort'
-alias lhead='l | head'
-alias ltail='l | tail'
-# Default mode that doesn't load user config.
-alias nvim-default='nvim -u NONE'
-alias reload='exec "$SHELL" -l'
-alias sha256='shasum -a 256'
-alias tardown='tar -xzvf'
-alias tarup='tar -czvf'
-# Default mode that doesn't load user config.
-alias vim-default='vim -i NONE -u NONE -U NONE'
+if _koopa_is_installed emacs
+then
+    alias emacs='emacs --no-window-system'
+
+    # Use terminal (console) mode by default instead of window system.
+    # Allow fast, default mode that skips '.emacs', '.emacs.d', etc.
+    alias emacs-default='emacs --no-init-file --no-window-system'
+
+    # Run with 24-bit true color support.
+    alias emacs24='TERM=xterm-24bit emacs --no-window-system'
+fi
+
+if _koopa_is_installed nvim
+then
+    # Default mode that doesn't load user config.
+    alias nvim-default='nvim -u NONE'
+fi
 
 if _koopa_is_installed shiny-server
 then
     alias shiny-restart="sudo systemctl restart shiny-server"
     alias shiny-start="sudo systemctl start shiny-server"
     alias shiny-status="sudo systemctl status shiny-server"
+fi
+
+if _koopa_is_installed vim
+then
+    # Default mode that doesn't load user config.
+    alias vim-default='vim -i NONE -u NONE -U NONE'
 fi
