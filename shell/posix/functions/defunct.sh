d301cfb3ebec59dc246314f382f00cd371dd9f3e
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-15 09:50:02 -0500

    Rename internal 'etc/profile.d' updater function

--- shell/posix/functions/defunct.sh
+++ shell/posix/functions/defunct.sh
@@ -4,7 +4,7 @@
 # Note that these are defined primarily to catch errors in private scripts that
 # are defined outside of the koopa package.
 
-_koopa_defunct() {
+_koopa_defunct() {  # {{{1
     # """
     # Make a function defunct.
     # Updated 2020-01-16.
@@ -16,21 +16,28 @@ _koopa_defunct() {
 
 
 
-_koopa_assert_is_darwin() {
+_koopa_assert_is_darwin() {  # {{{1
     # """
     # Updated 2020-01-14.
     # """
     _koopa_defunct "_koopa_assert_is_macos"
 }
 
-_koopa_is_darwin() {
+_koopa_is_darwin() {  # {{{1
     # """
     # Updated 2020-01-14.
     # """
     _koopa_defunct "_koopa_is_macos"
 }
 
-_koopa_update_shells() {
+_koopa_update_profile() {  # {{{1
+    # """
+    # Updated 2020-02-15.
+    # """
+    _koopa_defunct "_koopa_update_etc_profile_d"
+}
+
+_koopa_update_shells() {  # {{{1
     # """
     # Updated 2020-02-11.
     # """
