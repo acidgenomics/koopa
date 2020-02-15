a629ea6364921a3ca2e43e4bc2750ba11679cb12
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-15 12:17:04 -0500

    Update functions.sh

--- shell/posix/include/functions.sh
+++ shell/posix/include/functions.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
+if [ -n "${BASH_VERSION:-}" ]
+then
+    KOOPA_POSIX_SOURCE="${BASH_SOURCE[0]}"
+elif [ -n "${ZSH_VERSION:-}" ]
+then
+    KOOPA_POSIX_SOURCE="${(%):-%N}"
+else
+    >&2 echo "ERROR: Unsupported shell."
+    exit 1
+fi
+
+KOOPA_POSIX_INC="$(cd "$(dirname "$KOOPA_POSIX_SOURCE")" \
+    >/dev/null 2>&1 && pwd -P)"
+
 # Use shell globbing instead of 'find', which doesn't support source.
-for file in "${KOOPA_PREFIX}/shell/posix/functions/"*".sh"
+for file in "${KOOPA_POSIX_INC}/functions/"*".sh"
 do
     # shellcheck source=/dev/null
     [ -f "$file" ] && . "$file"
 done
+
+unset -v KOOPA_POSIX_INC
