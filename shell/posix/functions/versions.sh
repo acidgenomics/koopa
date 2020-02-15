4f86453263b187d753933ae94d74ec65f3e114f0
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-15 08:13:23 -0500

    Rework ordering of versions here, update GitHub parser to use JSON

--- shell/posix/functions/versions.sh
+++ shell/posix/functions/versions.sh
@@ -7,12 +7,260 @@
 # MAJOR.MINOR.PATCH
 # """
 
-_koopa_version() {  # {{{1
+_koopa_bcbio_nextgen_current_version() {  # {{{1
     # """
-    # Koopa version.
+    # Get the latest bcbio-nextgen stable release version.
+    # @note Updated 2020-02-10.
+    #
+    # Alternate approach:
+    # > current="$(_koopa_github_latest_release "bcbio/bcbio-nextgen")"
+    # """
+    _koopa_assert_is_installed curl
+    local url
+    url="https://raw.githubusercontent.com/bcbio/bcbio-nextgen\
+/master/requirements-conda.txt"
+    local x
+    x="$( \
+        curl --silent "$url" \
+            | grep 'bcbio-nextgen=' \
+            | cut -d '=' -f 2 \
+    )"
+    [ -n "$x" ] || return 1
+    echo "$x"
+    return 0
+}
+
+_koopa_bioconductor_version() {  # {{{1
+    # """
+    # Bioconductor version.
     # @note Updated 2020-02-07.
     # """
-    _koopa_variable "koopa"
+    _koopa_r_package_version "BiocVersion"
+    return 0
+}
+
+_koopa_extract_version() {  # {{{1
+    # """
+    # Extract version number.
+    # @note Updated 2020-02-10.
+    # """
+    local x
+    x="${1:?}"
+    x="$( \
+        echo "$x" \
+            | grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+)?(\.[0-9]+)?([a-z])?' \
+            | head -n 1 \
+    )"
+    [ -n "$x" ] || return 1
+    echo "$x"
+    return 0
+}
+
+_koopa_gcc_version() {  # {{{1
+    # """
+    # GCC version.
+    # @note Updated 2020-02-10.
+    # """
+    _koopa_is_installed gcc || return 1
+    if _koopa_is_macos
+    then
+        local x
+        x="$(gcc --version 2>&1 | sed -n '2p')"
+        _koopa_extract_version "$x"
+    else
+        _koopa_return_version "gcc"
+    fi
+    return 0
+}
+
+_koopa_get_homebrew_cask_version() {  # {{{1
+    # """
+    # Get Homebrew Cask version.
+    # @note Updated 2020-02-12.
+    #
+    # @examples _koopa_get_homebrew_cask_version gpg-suite
+    # # 2019.2
+    # """
+    local cask
+    cask="${1:?}"
+    local x
+    x="$(brew cask info "$cask")"
+    _koopa_extract_version "$x"
+    return 0
+}
+
+_koopa_get_macos_app_version() {  # {{{1
+    # """
+    # Extract the version of a macOS application.
+    # @note Updated 2020-01-12.
+    # """
+    _koopa_assert_is_macos
+    local app
+    app="${1:?}"
+    local plist
+    plist="/Applications/${app}.app/Contents/Info.plist"
+    if [ ! -f "$plist" ]
+    then
+        _koopa_stop "'${app}' is not installed."
+    fi
+    local x
+    x="$( \
+        plutil -p "$plist" \
+            | grep 'CFBundleShortVersionString' \
+            | awk -F ' => ' '{print $2}' \
+            | tr -d '"' \
+    )"
+    echo "$x"
+    return 0
+}
+
+_koopa_get_version() {  # {{{1
+    # """
+    # Get the version of an installed program.
+    # @note Updated 2020-02-10.
+    # """
+    local cmd
+    cmd="${1:?}"
+    local fun
+    fun="_koopa_$(_koopa_snake_case "$cmd")_version"
+    if _koopa_is_function "$fun"
+    then
+        "$fun"
+    else
+        _koopa_return_version "$cmd"
+    fi
+    return 0
+}
+
+_koopa_github_latest_release() {  # {{{1
+    # """
+    # Get the latest release version from GitHub.
+    # @note Updated 2020-02-15.
+    #
+    # @examples
+    # _koopa_github_latest_release "acidgenomics/koopa"
+    # # Expected failure:
+    # _koopa_github_latest_release "acidgenomics/acidgenomics.github.io"
+    # """
+    _koopa_assert_is_installed curl
+    local repo
+    repo="${1:?}"
+    local url
+    url="https://api.github.com/repos/${repo}/releases/latest"
+    local json
+    json="$(curl -s "$url" 2>&1 || true)"
+    local tag
+    tag="$( \
+        echo "$json" \
+            | grep '"tag_name":' \
+            | cut -d '"' -f 4 \
+            | sed 's/^v//' \
+    )"
+    [ -n "$tag" ] || return 1
+    echo "$tag"
+    return 0
+}
+
+_koopa_hdf5_version() {  # {{{1
+    # """
+    # HDF5 version.
+    # @note Updated 2020-02-07.
+    #
+    # Debian: 'dpkg -s libhdf5-dev'
+    # """
+    _koopa_is_installed h5cc || return 1
+    local x
+    x="$( \
+        h5cc -showconfig \
+            | grep 'HDF5 Version:' \
+            | sed -E 's/^(.+): //' \
+    )"
+    [ -n "$x" ] || return 1
+    echo "$x"
+    return 0
+}
+
+_koopa_linux_version() {  # {{{1
+    # """
+    # Linux version.
+    # @note Updated 2020-02-07.
+    # """
+    _koopa_is_linux || return 1
+    uname -r
+    return 0
+}
+
+_koopa_llvm_version() {  # {{{1
+    # """
+    # LLVM version.
+    # @note Updated 2020-02-10.
+    #
+    # Note that 'llvm-config' is versioned on most systems.
+    # """
+    [ -x "${LLVM_CONFIG:-}" ] || return 1
+    _koopa_return_version "$LLVM_CONFIG"
+    return 0
+}
+
+_koopa_lmod_version() {  # {{{1
+    # """
+    # Lmod version.
+    # @note Updated 2020-02-07.
+    #
+    # Alterate approach:
+    # > module --version 2>&1 \
+    # >     | grep -Eo "Version [.0-9]+" \
+    # >     | cut -d ' ' -f 2
+    # """
+    [ -n "${LMOD_VERSION:-}" ] || return 1
+    echo "$LMOD_VERSION"
+    return 0
+}
+
+_koopa_macos_version() {  # {{{1
+    # """
+    # macOS version.
+    # @note Updated 2020-02-07.
+    # """
+    _koopa_is_macos || return 1
+    local x
+    x="$(sw_vers -productVersion)"
+    echo "$x"
+    return 0
+}
+
+_koopa_major_version() {  # {{{1
+    # """
+    # Get the major program version.
+    # @note Updated 2020-01-12.
+    # """
+    local version
+    version="${1:?}"
+    echo "$version" | cut -d '.' -f 1
+    return 0
+}
+
+_koopa_minor_version() {  # {{{1
+    # """
+    # Get the major program version.
+    # @note Updated 2020-01-12.
+    # """
+    local version
+    version="${1:?}"
+    echo "$version" | cut -d '.' -f 1-2
+    return 0
+}
+
+_koopa_oracle_instantclient_version() {  # {{{1
+    # """
+    # Oracle InstantClient version.
+    # @note Updated 2020-02-10.
+    # """
+    _koopa_is_installed sqlplus || return 1
+    local x
+    x="$(sqlplus -v | grep -E '^Version')"
+    _koopa_extract_version "$x"
+    return 0
 }
 
 _koopa_os_version() {  # {{{1
@@ -30,77 +278,46 @@ _koopa_os_version() {  # {{{1
         version="$(_koopa_linux_version)"
     fi
     echo "$version"
+    return 0
 }
 
-
-
-_koopa_major_version() {  # {{{1
+_koopa_perl_file_rename_version() {  # {{{1
     # """
-    # Get the major program version.
-    # @note Updated 2020-01-12.
-    # """
-    local version
-    version="${1:?}"
-    echo "$version" | cut -d '.' -f 1
-}
-
-_koopa_minor_version() {  # {{{1
-    # """
-    # Get the major program version.
-    # @note Updated 2020-01-12.
-    # """
-    local version
-    version="${1:?}"
-    echo "$version" | cut -d '.' -f 1-2
-}
-
-_koopa_extract_version() {  # {{{1
-    # """
-    # Extract version number.
+    # Perl File::Rename version.
     # @note Updated 2020-02-10.
     # """
+    _koopa_is_installed rename || return 1
     local x
-    x="${1:?}"
-    x="$( \
-        echo "$x" \
-            | grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+)?(\.[0-9]+)?([a-z])?' \
-            | head -n 1 \
-    )"
-    [ -n "$x" ] || return 1
-    echo "$x"
+    x="$(rename --version | head -n 1)"
+    echo "$x" | grep -q 'File::Rename' || return 1
+    _koopa_extract_version "$x"
+    return 0
 }
 
-_koopa_sanitize_version() {  # {{{1
+_koopa_r_package_version() {  # {{{1
     # """
-    # Sanitize version.
-    # @note Updated 2020-02-11.
+    # R package version.
+    # Updated 2020-02-10.
     # """
+    local pkg
+    pkg="${1:?}"
+    _koopa_is_r_package_installed "$pkg" || return 1
     local x
-    x="${1:?}"
-    local pattern
-    pattern="[.0-9]+"
-    _koopa_is_matching_regex "$x" "$pattern" || return 1
-    x="$(echo "$x" | grep -Eo "$pattern")"
+    x="$(Rscript -e "cat(as.character(packageVersion(\"${pkg}\")), \"\n\")")"
     echo "$x"
+    return 0
 }
 
-
-
-_koopa_get_version() {  # {{{1
+_koopa_r_version() {  # {{{1
     # """
-    # Get the version of an installed program.
+    # R version.
     # @note Updated 2020-02-10.
     # """
-    local cmd
-    cmd="${1:?}"
-    local fun
-    fun="_koopa_$(_koopa_snake_case "$cmd")_version"
-    if _koopa_is_function "$fun"
-    then
-        "$fun"
-    else
-        _koopa_return_version "$cmd"
-    fi
+    _koopa_is_installed R || return 1
+    local x
+    x="$(R --version | grep 'R version')"
+    _koopa_extract_version "$x"
+    return 0
 }
 
 _koopa_return_version() {  # {{{1
@@ -196,224 +413,22 @@ _koopa_return_version() {  # {{{1
     fi
     _koopa_is_installed "$cmd" || return 1
     _koopa_extract_version "$("$cmd" "$flag" 2>&1 || true)"
+    return 0
 }
 
-
-
-_koopa_get_homebrew_cask_version() {  # {{{1
+_koopa_sanitize_version() {  # {{{1
     # """
-    # Get Homebrew Cask version.
-    # @note Updated 2020-02-12.
-    #
-    # @examples _koopa_get_homebrew_cask_version gpg-suite
-    # # 2019.2
+    # Sanitize version.
+    # @note Updated 2020-02-11.
     # """
-    local cask
-    cask="${1:?}"
     local x
-    x="$(brew cask info "$cask")"
-    _koopa_extract_version "$x"
-}
-
-_koopa_get_macos_app_version() {  # {{{1
-    # """
-    # Extract the version of a macOS application.
-    # @note Updated 2020-01-12.
-    # """
-    _koopa_assert_is_macos
-    local app
-    app="${1:?}"
-    local plist
-    plist="/Applications/${app}.app/Contents/Info.plist"
-    if [ ! -f "$plist" ]
-    then
-        _koopa_stop "'${app}' is not installed."
-    fi
-    plutil -p "$plist" \
-        | grep 'CFBundleShortVersionString' \
-        | awk -F ' => ' '{print $2}' \
-        | tr -d '"'
-}
-
-_koopa_github_latest_release() {  # {{{1
-    # """
-    # Get the latest release version from GitHub.
-    # @note Updated 2020-02-07.
-    #
-    # @examples
-    # _koopa_github_latest_release "acidgenomics/koopa"
-    # # Expected failure:
-    # _koopa_github_latest_release "acidgenomics/acidgenomics.github.io"
-    # """
-    _koopa_assert_is_installed curl
-    local repo
-    repo="${1:?}"
-    local x
-    x="$(curl -s "https://github.com/${repo}/releases/latest" 2>&1)"
-    x="$(echo "$x" | grep -Eo '/tag/[-_.A-Za-z0-9]+')"
-    if [ -z "$x" ]
-    then
-        _koopa_stop "'${repo}' does not contain latest release tag."
-    fi
-    echo "$x" \
-        | cut -d '/' -f 3 \
-        | sed 's/^v//'
-}
-
-_koopa_r_package_version() {  # {{{1
-    # """
-    # R package version.
-    # Updated 2020-02-10.
-    # """
-    local pkg
-    pkg="${1:?}"
-    _koopa_is_r_package_installed "$pkg" || return 1
-    local x
-    x="$(Rscript -e "cat(as.character(packageVersion(\"${pkg}\")), \"\n\")")"
+    x="${1:?}"
+    local pattern
+    pattern="[.0-9]+"
+    _koopa_is_matching_regex "$x" "$pattern" || return 1
+    x="$(echo "$x" | grep -Eo "$pattern")"
     echo "$x"
-}
-
-
-
-_koopa_bcbio_nextgen_current_version() {  # {{{1
-    # """
-    # Get the latest bcbio-nextgen stable release version.
-    # @note Updated 2020-02-10.
-    #
-    # Alternate approach:
-    # > current="$(_koopa_github_latest_release "bcbio/bcbio-nextgen")"
-    # """
-    _koopa_assert_is_installed curl
-    local url
-    url="https://raw.githubusercontent.com/bcbio/bcbio-nextgen\
-/master/requirements-conda.txt"
-    local x
-    x="$( \
-        curl --silent "$url" \
-            | grep 'bcbio-nextgen=' \
-            | cut -d '=' -f 2 \
-    )"
-    [ -n "$x" ] || return 1
-    echo "$x"
-}
-
-_koopa_bioconductor_version() {  # {{{1
-    # """
-    # Bioconductor version.
-    # @note Updated 2020-02-07.
-    # """
-    _koopa_r_package_version "BiocVersion"
-}
-
-_koopa_gcc_version() {  # {{{1
-    # """
-    # GCC version.
-    # @note Updated 2020-02-10.
-    # """
-    _koopa_is_installed gcc || return 1
-    if _koopa_is_macos
-    then
-        local x
-        x="$(gcc --version 2>&1 | sed -n '2p')"
-        _koopa_extract_version "$x"
-    else
-        _koopa_return_version "gcc"
-    fi
-}
-
-_koopa_hdf5_version() {  # {{{1
-    # """
-    # HDF5 version.
-    # @note Updated 2020-02-07.
-    #
-    # Debian: 'dpkg -s libhdf5-dev'
-    # """
-    _koopa_is_installed h5cc || return 1
-    local x
-    x="$( \
-        h5cc -showconfig \
-            | grep 'HDF5 Version:' \
-            | sed -E 's/^(.+): //' \
-    )"
-    [ -n "$x" ] || return 1
-    echo "$x"
-}
-
-_koopa_linux_version() {  # {{{1
-    # """
-    # Linux version.
-    # @note Updated 2020-02-07.
-    # """
-    _koopa_is_linux || return 1
-    uname -r
-}
-
-_koopa_llvm_version() {  # {{{1
-    # """
-    # LLVM version.
-    # @note Updated 2020-02-10.
-    #
-    # Note that 'llvm-config' is versioned on most systems.
-    # """
-    [ -x "${LLVM_CONFIG:-}" ] || return 1
-    _koopa_return_version "$LLVM_CONFIG"
-}
-
-_koopa_lmod_version() {  # {{{1
-    # """
-    # Lmod version.
-    # @note Updated 2020-02-07.
-    #
-    # Alterate approach:
-    # > module --version 2>&1 \
-    # >     | grep -Eo "Version [.0-9]+" \
-    # >     | cut -d ' ' -f 2
-    # """
-    [ -n "${LMOD_VERSION:-}" ] || return 1
-    echo "$LMOD_VERSION"
-}
-
-_koopa_macos_version() {  # {{{1
-    # """
-    # macOS version.
-    # @note Updated 2020-02-07.
-    # """
-    _koopa_is_macos || return 1
-    sw_vers -productVersion
-}
-
-_koopa_oracle_instantclient_version() {  # {{{1
-    # """
-    # Oracle InstantClient version.
-    # @note Updated 2020-02-10.
-    # """
-    _koopa_is_installed sqlplus || return 1
-    local x
-    x="$(sqlplus -v | grep -E '^Version')"
-    _koopa_extract_version "$x"
-}
-
-_koopa_perl_file_rename_version() {  # {{{1
-    # """
-    # Perl File::Rename version.
-    # @note Updated 2020-02-10.
-    # """
-    _koopa_is_installed rename || return 1
-    local x
-    x="$(rename --version | head -n 1)"
-    echo "$x" | grep -q 'File::Rename' || return 1
-    _koopa_extract_version "$x"
-}
-
-_koopa_r_version() {  # {{{1
-    # """
-    # R version.
-    # @note Updated 2020-02-10.
-    # """
-    _koopa_is_installed R || return 1
-    local x
-    x="$(R --version | grep 'R version')"
-    _koopa_extract_version "$x"
+    return 0
 }
 
 _koopa_tex_version() {  # {{{1
@@ -437,6 +452,16 @@ _koopa_tex_version() {  # {{{1
     )"
     [ -n "$x" ] || return 1
     echo "$x"
+    return 0
+}
+
+_koopa_version() {  # {{{1
+    # """
+    # Koopa version.
+    # @note Updated 2020-02-07.
+    # """
+    _koopa_variable "koopa"
+    return 0
 }
 
 _koopa_vim_version() {  # {{{1
@@ -467,4 +492,5 @@ _koopa_vim_version() {  # {{{1
         version="$major"
     fi
     echo "$version"
+    return 0
 }
