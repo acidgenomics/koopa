cadcfba891ab7b112e3ace60d55b1dbca2badce4
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-16 14:50:34 -0500

    Improve automatic install, uninstall

--- lang/r/functions/install.R
+++ lang/r/functions/install.R
@@ -1,4 +1,7 @@
-## Wrapper for `BiocManager::install()` that skips installed packages.
+#' Install packages
+#' @note Updated 2020-02-16.
+#'
+#' Wrapper for `BiocManager::install()` that skips installed packages.
 install <- function(pkgs, ...) {
     invisible(lapply(
         X = pkgs,
@@ -8,4 +11,19 @@ install <- function(pkgs, ...) {
             BiocManager::install(pkg, ...)
         }
     ))
+}
+
+
+
+#' Remove packages
+#' @note Updated 2020-02-16.
+#'
+#' Wrapper for `utils::remove.packages()`.
+remove <- function(pkgs) {
+    stopifnot(requireNamespace("utils", quietly = TRUE))
+    pkgs <- intersect(pkgs, rownames(utils::installed.packages()))
+    if (length(pkgs) > 0L) {
+        message("Removing packages: ", toString(pkgs))
+        utils::remove.packages(pkgs = pkgs)
+    }
 }
