f3d33acfeb84bb1075f4490cdeaa5f7dd685e119
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-16 15:41:06 -0500

    Draft update

--- system/include/koopa/update.sh
+++ system/include/koopa/update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
+set -x
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../../shell/bash/include/header.sh"
