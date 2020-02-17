a3065bb1e96ade330bd287c2fab0780815b74dbc
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-16 22:13:54 -0500

    Improve linter checks

--- tests/linter/base-line-width.sh
+++ tests/linter/base-line-width.sh
@@ -2,16 +2,17 @@
 
 # """
 # Find lines containing more than 80 characters.
-# Updated 2020-02-01.
+# Updated 2020-02-16.
 #
 # Use find first, pass to array, and then call grep.
 # This is better supported across platforms.
 # """
 
 # shellcheck source=/dev/null
-source "${KOOPA_PREFIX}/shell/bash/include/header.sh"
+source "$(koopa header bash)"
 
-prefix="${1:-$KOOPA_PREFIX}"
+koopa_prefix="$(_koopa_prefix)"
+prefix="${1:-${koopa_prefix}}"
 
 grep_pattern="^[^\n]{81}"
 
@@ -24,26 +25,34 @@ done < <( \
         -mindepth 1 \
         -type f \
         -not -name "*.md" \
-        -not -path "${KOOPA_PREFIX}/.git/*" \
-        -not -path "${KOOPA_PREFIX}/dotfiles/*" \
-        -not -path "${KOOPA_PREFIX}/shunit2-*" \
+        -not -path "${koopa_prefix}/.git/*" \
+        -not -path "${koopa_prefix}/cellar/*" \
+        -not -path "${koopa_prefix}/coverage/*" \
+        -not -path "${koopa_prefix}/dotfiles/*" \
+        -not -path "${koopa_prefix}/opt/*" \
         -print0 \
     | sort -z
 )
 
-failures="$( \
-    grep -En \
-        --binary-files="without-match" \
-        "$grep_pattern" \
-        "${files[@]}" \
-    || echo "" \
-)"
+failures=()
+for file in "${files[@]}"
+do
+    x="$(
+        grep -En \
+            --binary-files="without-match" \
+            "$grep_pattern" \
+            "$file" \
+        || true \
+    )"
+    [[ -n "$x" ]] && failures+=("$x")
+done
 
 name="$(_koopa_basename_sans_ext "$0")"
-if [[ -n "$failures" ]]
+
+if _koopa_is_array_non_empty "${failures[@]}"
 then
     _koopa_status_fail "$name"
-    echo "$failures"
+    echo "${failures[@]}"
     exit 1
 else
     _koopa_status_ok "$name"
