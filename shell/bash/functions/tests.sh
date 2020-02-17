55a75314e3c92a542a0081733166b48e773f5dc8
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-16 23:17:53 -0500

    Simplify unit tests with functions

---
+++ shell/bash/functions/tests.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+
+_koopa_test_find_files() {  # {{{1
+    # """
+    # Find relevant files for unit tests.
+    # @note Updated 2020-02-16.
+    # """
+    local koopa_prefix
+    koopa_prefix="$(_koopa_prefix)"
+    find "$koopa_prefix" \
+        -mindepth 1 \
+        -type f \
+        -not -name "$(basename "$0")" \
+        -not -name "*.md" \
+        -not -name ".pylintrc" \
+        -not -path "${koopa_prefix}/.git/*" \
+        -not -path "${koopa_prefix}/cellar/*" \
+        -not -path "${koopa_prefix}/coverage/*" \
+        -not -path "${koopa_prefix}/dotfiles/*" \
+        -not -path "${koopa_prefix}/opt/*" \
+        -not -path "${koopa_prefix}/tests/*" \
+        -print \
+    | sort
+    return 0
+}
+
+_koopa_test_find_files_by_ext() {  # {{{1
+    # """
+    # Find relevant test files by extension.
+    # @note Updated 2020-02-16.
+    # """
+    local ext
+    ext="${1:-?}"
+
+    local pattern
+    pattern="\.${ext}$"
+
+    local files
+    mapfile -t files < <(_koopa_test_find_files)
+
+    local x
+    x="$( \
+        printf '%s\n' "${files[@]}" \
+        | grep -Ei "$pattern" \
+    )"
+    echo "$x"
+    return 0
+}
+
+_koopa_test_find_files_by_shebang() {  # {{{1
+    # """
+    # Find relevant test files by shebang.
+    # @note Updated 2020-02-16.
+    # """
+    local pattern
+    pattern="${1:?}"
+
+    local files
+    mapfile -t files < <(_koopa_test_find_files)
+
+    local shebang_files
+    shebang_files=()
+
+    local file
+    for file in "${files[@]}"
+    do
+        local x
+        x="$(
+            grep -El \
+                --binary-files="without-match" \
+                "$pattern" \
+                "$file" \
+            || true \
+        )"
+        [[ -n "$x" ]] && shebang_files+=("$x")
+    done
+
+    printf '%s\n' "${shebang_files[@]}"
+    return 0
+}
+
+_koopa_test_find_failures() {
+    local name
+    name="$(_koopa_basename_sans_ext "$0")"
+
+    local pattern
+    pattern="${1:?}"
+
+    local files
+    mapfile -t files < <(_koopa_test_find_files)
+
+    local failures
+    failures=()
+
+    local file
+    for file in "${files[@]}"
+    do
+        local x
+        x="$(
+            grep -EHn \
+                --binary-files="without-match" \
+                "$pattern" \
+                "$file" \
+            || true \
+        )"
+        [[ -n "$x" ]] && failures+=("$x")
+    done
+
+    if [[ "${#failures[@]}" -gt 0 ]]
+    then
+        _koopa_status_fail "${name} [${#failures[@]}]"
+        printf '%s\n' "${failures[@]}"
+        return 1
+    fi
+
+    _koopa_status_ok "${name} [${#files[@]}]"
+    return 0
+}
--- tests/linter/python-pylint.sh
+++ tests/linter/python-pylint.sh
@@ -1,62 +1,21 @@
 #!/usr/bin/env bash
 
-# """
-# Recursively run pylint on all Python scripts in a directory.
-# Updated 2020-02-01.
-# """
-
 # shellcheck source=/dev/null
-source "${KOOPA_PREFIX}/shell/bash/include/header.sh"
+source "$(koopa header bash)"
 
-name="$(_koopa_basename_sans_ext "$0")"
+# """
+# Run pylint on all Python scripts.
+# Updated 2020-02-16.
+# """
 
-# Skip test if pylint is not installed.
-if ! _koopa_is_installed pylint
-then
-    printf "NOTE | %s\n" "$name"
-    printf "     |   pylint missing.\n"
-    exit 0
-fi
+_koopa_exit_if_not_installed pylint
 
-prefix="${1:-$KOOPA_PREFIX}"
-
-# Find scripts by file extension.
-ext_files=()
-while IFS= read -r -d $'\0'
-do
-    ext_files+=("$REPLY")
-done < <( \
-    find "$prefix" \
-        -mindepth 1 \
-        -type f \
-        -iname "*.py" \
-        -not -path "${KOOPA_PREFIX}/.git/*" \
-        -not -path "${KOOPA_PREFIX}/dotfiles/*" \
-        -not -path "${KOOPA_PREFIX}/shunit2-*" \
-        -print0 \
-)
-
-# Find scripts by shebang.
-mapfile -t shebang_files < <( \
-    find "$prefix" \
-        -mindepth 1 \
-        -type f \
-        -not -path "${KOOPA_PREFIX}/.git/*" \
-        -not -path "${KOOPA_PREFIX}/dotfiles/*" \
-        -not -path "${KOOPA_PREFIX}/shunit2-*" \
-        -print0 \
-    | xargs -0 -I {} \
-    grep -El \
-        --binary-files="without-match" \
-        '^#!/.*\bpython(3)?\b$' \
-        {} \
-)
-
-merge=("${ext_files[@]}" "${shebang_files[@]}")
-files="$(printf "%q\n" "${merge[@]}" | sort -u)"
-mapfile -t files <<< "$files"
+# Find files by shebang.
+grep_pattern='^#!/.*\bpython(3)?\b$'
+mapfile -t files < <(_koopa_test_find_files_by_shebang "$grep_pattern")
 
 # Note that setting '--jobs=0' flag here enables multicore.
 python3 -m pylint --jobs=0 --score=n "${files[@]}"
 
-_koopa_status_ok "$name"
+name="$(_koopa_basename_sans_ext "$0")"
+_koopa_status_ok "${name} [${#files[@]}]"
