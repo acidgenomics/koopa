ee5f6f294811130d1ebda1f38cef2f1ed7def3f6
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-16 23:17:53 -0500

    Simplify unit tests with functions

--- tests/linter/shell-shellcheck.sh
+++ tests/linter/shell-shellcheck.sh
@@ -1,45 +1,20 @@
 #!/usr/bin/env bash
 
+# shellcheck source=/dev/null
+source "$(koopa header bash)"
+
 # """
-# Recursively run shellcheck on all scripts in a directory.
-# Updated 2020-02-01.
-#
-# Use find first, pass to array, and then call grep.
-# This is better supported across platforms.
+# Run ShellCheck on all Bash and Zsh scripts.
+# Updated 2020-02-16.
 # """
 
-# shellcheck source=/dev/null
-source "${KOOPA_PREFIX}/shell/bash/include/header.sh"
+_koopa_exit_if_not_installed shellcheck
+
+# Find files by shebang.
+grep_pattern='^#!/.*\b(ba)?sh\b$'
+mapfile -t files < <(_koopa_test_find_files_by_shebang "$grep_pattern")
+
+shellcheck -x "${files[@]}"
 
 name="$(_koopa_basename_sans_ext "$0")"
-
-# Skip test if shellcheck is not installed.
-# Currently, Travis CI does not have shellcheck installed for macOS.
-if ! _koopa_is_installed shellcheck
-then
-    printf "NOTE | %s\n" "$name"
-    printf "     |   shellcheck missing.\n"
-    exit 0
-fi
-
-prefix="${1:-$KOOPA_PREFIX}"
-
-# Find scripts by shebang.
-mapfile -t shebang_files < <( \
-    find "$prefix" \
-        -mindepth 1 \
-        -type f \
-        -not -path "${KOOPA_PREFIX}/.git/*" \
-        -not -path "${KOOPA_PREFIX}/dotfiles/*" \
-        -not -path "${KOOPA_PREFIX}/shunit2-*" \
-        -print0 \
-    | xargs -0 -I {} \
-    grep -El \
-        --binary-files="without-match" \
-        '^#!/.*\b(ba)?sh\b$' \
-        {} \
-)
-
-shellcheck -x "${shebang_files[@]}"
-
-_koopa_status_ok "$name"
+_koopa_status_ok "${name} [${#files[@]}]"
