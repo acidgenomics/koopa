79aa716c498ae7591453c2c052402f206c263080
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-19 09:54:05 -0500

    Improve broken symlink cleanup

--- shell/bash/functions/files.sh
+++ shell/bash/functions/files.sh
@@ -3,12 +3,12 @@
 _koopa_remove_broken_symlinks() {  # {{{1
     # """
     # Remove broken symlinks.
-    # @note Updated 2020-01-21.
+    # @note Updated 2020-02-19.
     # """
     dir="${1:-"."}"
     _koopa_assert_is_dir "$dir"
     dir="$(realpath "$dir")"
-    _koopa_h2 "Removing broken symlinks in '${dir}'."
+    # > _koopa_h2 "Removing broken symlinks in '${dir}'."
     mapfile -t arr <<< "$(find-broken-symlinks "$dir")"
     _koopa_is_array_non_empty "${arr[@]}" || return 0
     for file in "${arr[@]}"
