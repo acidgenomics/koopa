b1a00fca87355a9df3fd21b40e2b826bde9edced
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-19 13:08:56 -0500

    Harden symlink removal

--- shell/bash/functions/files.sh
+++ shell/bash/functions/files.sh
@@ -8,13 +8,12 @@ _koopa_remove_broken_symlinks() {  # {{{1
     dir="${1:-"."}"
     _koopa_assert_is_dir "$dir"
     dir="$(realpath "$dir")"
-    # > _koopa_h2 "Removing broken symlinks in '${dir}'."
     mapfile -t arr <<< "$(find-broken-symlinks "$dir")"
     _koopa_is_array_non_empty "${arr[@]}" || return 0
     for file in "${arr[@]}"
     do
         [[ -z "$file" ]] && continue
-        rm -v "$file"
+        _koopa_rm "$file"
     done
     return 0
 }
