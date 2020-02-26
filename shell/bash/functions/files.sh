e8e7f2e0d1dc41e1a6b61224c1a87a3296ae1525
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-26 15:38:20 -0500

    Improve internal functions

--- shell/bash/functions/files.sh
+++ shell/bash/functions/files.sh
@@ -3,10 +3,9 @@
 _koopa_remove_broken_symlinks() {  # {{{1
     # """
     # Remove broken symlinks.
-    # @note Updated 2020-02-19.
+    # @note Updated 2020-02-26.
     # """
     dir="${1:-"."}"
-    _koopa_assert_is_dir "$dir"
     dir="$(realpath "$dir")"
     mapfile -t arr <<< "$(find-broken-symlinks "$dir")"
     _koopa_is_array_non_empty "${arr[@]}" || return 0
