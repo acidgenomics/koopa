7adfc499053b502a5af9b0736d0cc4920997cdaa
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-26 11:29:26 -0500

    Improve internal script handling for 'koopa' function

--- system/include/koopa/update.sh
+++ system/include/koopa/update.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-set -Eeu -o pipefail
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
-# shellcheck source=/dev/null
-source "${script_dir}/../../../shell/bash/include/header.sh"
 
 # e.g. /usr/local/koopa
 koopa_prefix="$(_koopa_prefix)"
