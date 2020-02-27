f497790c796cb747ebed51cb6a1915f0b463e0c7
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-27 08:30:20 -0500

    Simplify alpine check

--- shell/posix/functions/boolean.sh
+++ shell/posix/functions/boolean.sh
@@ -106,11 +106,7 @@ _koopa_is_alpine() {  # {{{1
     # Is the operating system Alpine Linux?
     # @note Updated 2020-02-27.
     # """
-    _koopa_is_linux || return 1
-    [ -f /etc/os-release ] || return 1
-    grep "ID=" /etc/os-release | grep -q "alpine" && return 0
-    grep "ID_LIKE=" /etc/os-release | grep -q "alpine" && return 0
-    return 1
+    [ "$(_koopa_os_id)" = "alpine" ]
 }
 
 _koopa_is_amzn() {  # {{{1
