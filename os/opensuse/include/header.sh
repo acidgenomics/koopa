3444d332abc75ef19557ed5ccccc645a5ae8cd73
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-27 12:15:40 -0500

    Update the header

--- os/opensuse/include/header.sh
+++ os/opensuse/include/header.sh
@@ -5,4 +5,4 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../linux/include/header.sh"
 
-_koopa_assert_is_debian
+_koopa_assert_is_opensuse
