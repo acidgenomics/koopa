c03f368552955d03e1579b16d73d073f8a6edb7a
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-01 18:48:34 -0500

    Update internal variable

--- system/include/koopa/check.sh
+++ system/include/koopa/check.sh
@@ -4,6 +4,8 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 
 _koopa_assert_is_installed R Rscript
 
+koopa_prefix="$(_koopa_prefix)"
+
 export KOOPA_FORCE=1
 set +u
 # shellcheck disable=SC1090
