75f57457e632499fa395a5ec7b38783fdc6107af
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-01 18:23:02 -0500

    Workaround for Bioconductor build

--- system/include/koopa/check.sh
+++ system/include/koopa/check.sh
@@ -4,6 +4,12 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 
 _koopa_assert_is_installed R Rscript
 
+export KOOPA_FORCE=1
+set +u
+# shellcheck disable=SC1090
+source "${koopa_prefix}/activate"
+set -u
+
 Rscript --vanilla "${script_dir}/check.R"
 
 _koopa_disk_check
