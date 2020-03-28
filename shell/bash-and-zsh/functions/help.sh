4fa398e29f06f4d3de73c71545d3da36cc591053
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-28 09:09:20 -0400

    Reorganize non-POSIX scripts shared between Bash and Zsh

---
+++ shell/bash-and-zsh/functions/help.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+_koopa_help() {  # {{{1
+    # """
+    # Show usage via '--help' flag.
+    # @note Updated 2020-01-21.
+    #
+    # Note that using 'path' as a local variable here will mess up Zsh.
+    #
+    # Now always calls 'man' to display nicely formatted manual page.
+    #
+    # Bash alternate approach:
+    # > file="${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
+    #
+    # Zsh parameter notes:
+    # - '$0': The name used to invoke the current shell, or as set by the -c
+    #   command line option upon invocation. If the FUNCTION_ARGZERO option is
+    #   set, $0 is set upon entry to a shell function to the name of the
+    #   function, and upon entry to a sourced script to the name of the script,
+    #   and reset to its previous value when the function or script returns.
+    # - 'FUNCTION_ARGZERO': When executing a shell function or sourcing a
+    #   script, set $0 temporarily to the name of the function/script. Note that
+    #   toggling FUNCTION_ARGZERO from on to off (or off to on) does not change
+    #   the current value of $0. Only the state upon entry to the function or
+    #   script has an effect. Compare POSIX_ARGZERO.
+    # - 'POSIX_ARGZERO': This option may be used to temporarily disable
+    #   FUNCTION_ARGZERO and thereby restore the value of $0 to the name used to
+    #   invoke the shell (or as set by the -c command line option). For
+    #   compatibility with previous versions of the shell, emulations use
+    #   NO_FUNCTION_ARGZERO instead of POSIX_ARGZERO, which may result in
+    #   unexpected scoping of $0 if the emulation mode is changed inside a
+    #   function or script. To avoid this, explicitly enable POSIX_ARGZERO in
+    #   the emulate command:
+    #
+    #   emulate sh -o POSIX_ARGZERO
+    #
+    #   Note that NO_POSIX_ARGZERO has no effect unless FUNCTION_ARGZERO was
+    #   already enabled upon entry to the function or script. 
+    #
+    # See also:
+    # - https://stackoverflow.com/questions/192319
+    # - http://zsh.sourceforge.net/Doc/Release/Parameters.html
+    # - https://stackoverflow.com/questions/35677745
+    # """
+    case "${1:-}" in
+        --help|-h)
+            _koopa_assert_is_installed man
+            local file
+            case "$(_koopa_shell)" in
+                bash)
+                    file="$0"
+                    ;;
+                zsh)
+                    # This approach is supported in zsh 5.7.1, but will error
+                    # in older zsh versions, such as on Travis CI. This is the
+                    # same as the value of $0 when the POSIX_ARGZERO option is
+                    # set, but is always available. 
+                    # > file="${ZSH_ARGZERO:?}"
+                    emulate sh -o POSIX_ARGZERO
+                    file="$0"
+                    ;;
+                *)
+                    _koopa_warning "Unsupported shell."
+                    exit 1
+                    ;;
+            esac
+            local name
+            name="${file##*/}"
+            man "$name"
+            exit 0
+            ;;
+    esac
+    return 0
+}
