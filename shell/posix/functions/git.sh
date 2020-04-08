fb28c86cd7d045c436fa3330a8683d08ab080f01
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-08 09:16:23 -0400

    Harden against invalid git repos

--- shell/posix/functions/git.sh
+++ shell/posix/functions/git.sh
@@ -4,7 +4,7 @@
 _koopa_git_branch() {  # {{{1
     # """
     # Current git branch name.
-    # @note Updated 2019-10-13.
+    # @note Updated 2020-04-08.
     #
     # Handles detached HEAD state.
     #
@@ -19,7 +19,7 @@ _koopa_git_branch() {  # {{{1
     # """
     _koopa_is_git || return 1
     local branch
-    branch="$(git symbolic-ref --short -q HEAD)"
+    branch="$(git symbolic-ref --short -q HEAD 2>/dev/null)"
     _koopa_print "$branch"
 }
 
@@ -44,7 +44,7 @@ _koopa_git_clone() {  # {{{1
 _koopa_git_last_commit_local() {  # {{{1
     # """
     # Last git commit of local repository.
-    # @note Updated 2020-02-26.
+    # @note Updated 2020-04-08.
     #
     # Alternate:
     # Can use '%h' for abbreviated commit ID.
@@ -52,20 +52,20 @@ _koopa_git_last_commit_local() {  # {{{1
     # """
     _koopa_is_git || return 1
     local x
-    x="$(git rev-parse HEAD)"
+    x="$(git rev-parse HEAD 2>/dev/null)"
     _koopa_print "$x"
 }
 
 _koopa_git_last_commit_remote() {  # {{{1
     # """
     # Last git commit of remote repository.
-    # @note Updated 2020-02-26.
+    # @note Updated 2020-04-08.
     #
     # Instead of 'HEAD', can use 'refs/heads/master'
     # """
     local url
     url="${1:?}"
     local x
-    x="$(git ls-remote "$url" HEAD)"
+    x="$(git ls-remote "$url" HEAD 2>/dev/null)"
     _koopa_print "$x"
 }
