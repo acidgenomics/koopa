a60d40293a37903af7db109a6704afb84451c749
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-08 09:17:34 -0400

    Migrate functions to acidbase

--- lang/r/functions/boolean.R
+++ lang/r/functions/boolean.R
@@ -14,17 +14,6 @@ hasColor <- function() {
 
 
 
-#' Does the current session have a GitHUB personal access token?
-#'
-#' Required for package installs from GitHub, otherwise will hit rate limit.
-#'
-#' @note Updated 2020-02-18.
-hasGitHubPAT <- function() {
-    isTRUE(nzchar(Sys.getenv("GITHUB_PAT")))
-}
-
-
-
 #' Is a system command cellarized?
 #' @note Updated 2020-02-29.
 isCellar <- function(which) {
@@ -36,62 +25,4 @@ isCellar <- function(which) {
         x = which,
         ignore.case = TRUE
     )
-}
-
-
-
-#' Is a system command installed?
-#' @note Updated 2020-02-06.
-isCommand <- function(which) {
-    nzchar(Sys.which(which))
-}
-
-
-
-#' Is flag?
-#' @note Updated 2020-02-06.
-isFlag <- function(x) {
-    is.logical(x) &&
-        !any(is.na(x)) &&
-        identical(length(x), 1L)
-}
-
-
-
-#' Is session running inside Docker?
-#' @note Updated 2020-02-24.
-isDocker <- function() {
-    ok <- file.exists("/proc/1/cgroup")
-    if (!ok) return(FALSE)
-    x <- readLines("/proc/1/cgroup")
-    any(grepl(pattern = ":/docker/", x = x))
-}
-
-
-
-#' Is an R package installed?
-#' @note Updated 2020-02-16.
-isInstalled <- function(pkgs) {
-    stopifnot(requireNamespace("utils", quietly = TRUE))
-    # Note that GitHub packages are "user/repo", so use basename.
-    basename(pkgs) %in% rownames(installed.packages())
-}
-
-
-
-#' Is macOS?
-#' @note Updated 2020-02-07.
-isMacOS <- function() {
-    grepl(pattern = "darwin", x = R.Version()[["os"]])
-}
-
-
-
-#' Is string?
-#' @note Updated 2020-02-09.
-isString <- function(x) {
-    is.character(x) &&
-        !any(is.na(x)) &&
-        identical(length(x), 1L) &&
-        isTRUE(nzchar(x))
 }
---
+++ lang/r/functions/help.R
@@ -0,0 +1,20 @@
+#' Help
+#'
+#' Display help via `man` when `--help` or `-h` flag is detected.
+#'
+#' @note Updated 2020-02-01.
+#' @noRd
+#'
+#' @return System command when `--help` is set, or invisible `NULL`.
+koopaHelp <- function() {
+    args <- commandArgs(trailingOnly = FALSE)
+    if (!isTRUE(any(c("--help", "-h") %in% args))) {
+        return(invisible())
+    }
+    file <- grep(pattern = "--file", x = args)
+    file <- args[file]
+    file <- sub(pattern = "^--file=", replacement = "", x = file)
+    name <- basename(file)
+    shell(command = "man", args = name)
+    quit()
+}
