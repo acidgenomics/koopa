018a6db36f2e5c0242a1084b5d67bdbf56edd096
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-09 08:39:11 -0400

    Update boolean functions

--- lang/r/functions/boolean.R
+++ lang/r/functions/boolean.R
@@ -1,28 +1,29 @@
 #' Can we use cli methods?
-#' @note Updated 2020-02-16.
+#' @note Updated 2020-04-09.
+#' @noRd
 hasCli <- function() {
-    isInstalled("cli")
+    stopifnot(requireNamespace("goalie", quietly = TRUE))
+    goalie::isInstalled("cli")
 }
 
 
 
 #' Can we output color to the console?
-#' @note Updated 2020-02-16.
+#' @note Updated 2020-04-09.
+#' @noRd
 hasColor <- function() {
-    isInstalled("crayon")
+    stopifnot(requireNamespace("goalie", quietly = TRUE))
+    goalie::isInstalled("crayon")
 }
 
 
 
 #' Is a system command cellarized?
-#' @note Updated 2020-02-29.
+#' @note Updated 2020-04-09.
+#' @noRd
 isCellar <- function(which) {
     which <- Sys.which(which)
     if (!all(nzchar(which))) return(FALSE)
     which <- normalizePath(which)
-    grepl(
-        pattern = "/cellar/",
-        x = which,
-        ignore.case = TRUE
-    )
+    grepl(pattern = "/cellar/", x = which, ignore.case = TRUE)
 }
