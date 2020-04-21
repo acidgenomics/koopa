fc915a548331a1e45afc23f7290168d44d8f465e
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-21 11:31:10 -0400

    Improve consistency of exit calls

--- shell/posix/functions/assert.sh
+++ shell/posix/functions/assert.sh
@@ -846,15 +846,14 @@ _koopa_check_user() {  # {{{1
 _koopa_exit_if_current_version() {  # {{{1
     # """
     # Assert that programs are installed and current.
-    # @note Updated 2020-04-13.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
     do
         if _koopa_is_current_version "$arg"
         then
-            _koopa_note "'${arg}' is current version."
-            exit 0
+            _koopa_exit "'${arg}' is current version."
         fi
     done
     return 0
@@ -863,15 +862,14 @@ _koopa_exit_if_current_version() {  # {{{1
 _koopa_exit_if_dir() {  # {{{1
     # """
     # Exit with note if directory exists.
-    # @note Updated 2020-01-22.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
     do
         if [ -d "$arg" ]
         then
-            _koopa_note "Directory exists: '${arg}'."
-            exit 0
+            _koopa_exit "Directory exists: '${arg}'."
         fi
     done
     return 0
@@ -880,12 +878,11 @@ _koopa_exit_if_dir() {  # {{{1
 _koopa_exit_if_docker() {  # {{{1
     # """
     # Exit with note if running inside Docker.
-    # @note Updated 2020-01-22.
+    # @note Updated 2020-04-21.
     # """
     if _koopa_is_docker
     then
-        _koopa_note "Not supported when running inside Docker."
-        exit 0
+        _koopa_exit "Not supported when running inside Docker."
     fi
     return 0
 }
@@ -893,15 +890,14 @@ _koopa_exit_if_docker() {  # {{{1
 _koopa_exit_if_exists() {  # {{{1
     # """
     # Exit with note if any file type exists.
-    # @note Updated 2020-01-28.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
     do
         if [ -e "$arg" ]
         then
-            _koopa_note "Exists: '${arg}'."
-            exit 0
+            _koopa_exit "Exists: '${arg}'."
         fi
     done
     return 0
@@ -910,7 +906,7 @@ _koopa_exit_if_exists() {  # {{{1
 _koopa_exit_if_installed() {  # {{{1
     # """
     # Exit with note if an app is installed.
-    # @note Updated 2020-02-06.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
@@ -919,8 +915,7 @@ _koopa_exit_if_installed() {  # {{{1
         then
             local where
             where="$(_koopa_which_realpath "$arg")"
-            _koopa_note "'${arg}' is installed at '${where}'."
-            exit 0
+            _koopa_exit "'${arg}' is installed at '${where}'."
         fi
     done
     return 0
@@ -929,15 +924,14 @@ _koopa_exit_if_installed() {  # {{{1
 _koopa_exit_if_not_installed() {  # {{{1
     # """
     # Exit with note if an app is not installed.
-    # @note Updated 2020-01-31.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
     do
         if ! _koopa_is_installed "$arg"
         then
-            _koopa_note "'${arg}' is not installed."
-            exit 0
+            _koopa_exit "'${arg}' is not installed."
         fi
     done
     return 0
@@ -946,15 +940,14 @@ _koopa_exit_if_not_installed() {  # {{{1
 _koopa_exit_if_python_package_not_installed() {  # {{{1
     # """
     # Exit with note if a Python package is not installed.
-    # @note Updated 2020-02-16.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
     do
         if ! _koopa_is_python_package_installed "$arg"
         then
-            _koopa_note "'${arg}' Python package is not installed."
-            exit 0
+            _koopa_exit "'${arg}' Python package is not installed."
         fi
     done
     return 0
@@ -963,15 +956,14 @@ _koopa_exit_if_python_package_not_installed() {  # {{{1
 _koopa_exit_if_r_package_installed() {  # {{{1
     # """
     # Exit with note if a R package is installed.
-    # @note Updated 2020-03-04.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
     do
         if _koopa_is_r_package_installed "$arg"
         then
-            _koopa_note "'${arg}' R package is installed."
-            exit 0
+            _koopa_exit "'${arg}' R package is installed."
         fi
     done
     return 0
@@ -980,15 +972,14 @@ _koopa_exit_if_r_package_installed() {  # {{{1
 _koopa_exit_if_r_package_not_installed() {  # {{{1
     # """
     # Exit with note if a R package is not installed.
-    # @note Updated 2020-03-04.
+    # @note Updated 2020-04-21.
     # """
     [ "$#" -ne 0 ] || return 1
     for arg
     do
         if ! _koopa_is_r_package_installed "$arg"
         then
-            _koopa_note "'${arg}' R package is not installed."
-            exit 0
+            _koopa_exit "'${arg}' R package is not installed."
         fi
     done
     return 0
