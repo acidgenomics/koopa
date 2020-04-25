4611f62b6ba4a038cc091491bf5dc34547776605
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-25 07:02:23 -0400

    Draft update to symlink and dir removal tools

--- shell/bash/functions/files.sh
+++ shell/bash/functions/files.sh
@@ -3,31 +3,25 @@
 _koopa_remove_broken_symlinks() {  # {{{1
     # """
     # Remove broken symlinks.
-    # @note Updated 2020-03-06.
+    # @note Updated 2020-04-25.
     # """
-    local file files
+    local files
     mapfile -t files <<< "$(_koopa_find_broken_symlinks "$@")"
     _koopa_is_array_non_empty "${files[@]}" || return 0
-    for file in "${files[@]}"
-    do
-        [[ -z "$file" ]] && continue
-        _koopa_rm "$file"
-    done
+    _koopa_note "Removing ${#files[@]} broken symlinks."
+    _koopa_rm "${files[@]}"
     return 0
 }
 
 _koopa_remove_empty_dirs() {  # {{{1
     # """
     # Remove empty directories.
-    # @note Updated 2020-03-06.
+    # @note Updated 2020-04-25.
     # """
-    local dir dirs
+    local dirs
     mapfile -t dirs <<< "$(_koopa_find_empty_dirs "$@")"
     _koopa_is_array_non_empty "${dirs[@]}" || return 0
-    for dir in "${dirs[@]}"
-    do
-        [[ -z "$dir" ]] && continue
-        _koopa_rm "$dir"
-    done
+    _koopa_note "Removing ${#dirs[@]} empty directories."
+    _koopa_rm "${dirs[@]}"
     return 0
 }
