88622f8e65cfb784981571e4dc9e93175a75c46d
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-27 09:45:47 -0400

    Improve fallback support for basename and realpath

--- shell/posix/functions/zzz-fallback.sh
+++ shell/posix/functions/zzz-fallback.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 # shellcheck disable=SC2039
 
+if ! _koopa_is_installed basename
+then
+    basename() {  # {{{1
+        _koopa_basename "$@"
+    }
+fi
+
 if ! _koopa_is_installed realpath
 then
     realpath() {  # {{{1
-        # """
-        # Real path to file/directory on disk.
-        # @note Updated 2020-01-13.
-        #
-        # Note that 'readlink -f' doesn't work on macOS.
-        #
-        # See also:
-        # - https://github.com/bcbio/bcbio-nextgen/blob/master/tests/
-        #       run_tests.sh
-        # """
-        if _koopa_is_macos
-        then
-            perl -MCwd -e 'print Cwd::abs_path shift' "$1"
-        else
-            readlink -f "$@"
-        fi
+        _koopa_realpath "$@"
     }
 fi
