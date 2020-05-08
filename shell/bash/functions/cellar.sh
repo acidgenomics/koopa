3d53280fa1c1f429542cf79e275b976cdd0d6b48
Michael Steinbaugh <mike@steinbaugh.com>
2020-05-08 15:15:12 -0400

    Draft update to cellar scripts

--- shell/bash/functions/cellar.sh
+++ shell/bash/functions/cellar.sh
@@ -1,5 +1,59 @@
-#!/bin/sh
-# shellcheck disable=SC2039
+#!/usr/bin/env bash
+
+## FIXME Break out find cellar function.
+## FIXME Create _koopa_unlink_cellar function.
+## FIXME Move this to Bash.
+
+
+_koopa_find_cellar_symlinks() {  # {{{1
+    # """
+    # Find cellar symlinks.
+    # @note Updated 2020-05-08.
+    # """
+    local name
+    name="${1:?}"
+
+    local make_prefix
+    make_prefix="$(_koopa_make_prefix)"
+    _koopa_assert_is_dir "$make_prefix"
+
+    local cellar_prefix
+    cellar_prefix="$(_koopa_cellar_prefix)"
+    _koopa_assert_is_dir "$cellar_prefix"
+
+    # FIXME THIS DOESN"T WORK YET...DUPE?
+
+    return 0
+}
+
+_koopa_find_cellar_version_dir() {  # {{{1
+    # """
+    # Find cellar installation directory.
+    # @note Updated 2020-05-08.
+    # """
+    local name
+    name="${1:?}"
+
+    local version
+    version="${2:?}"
+
+    local cellar_prefix
+    cellar_prefix="$(_koopa_cellar_prefix)"
+    _koopa_assert_is_dir "$cellar_prefix"
+
+    local prefix
+    prefix="$( \
+        find "$cellar_prefix" \
+            -mindepth 1 \
+            -maxdepth 1 \
+            -type d \
+        | sort \
+        | tail -n 1 \
+    )"
+    _koopa_assert_is_dir "$prefix"
+
+    _koopa_print "$prefix"
+}
 
 _koopa_link_cellar() {  # {{{1
     # """
@@ -28,6 +82,7 @@ _koopa_link_cellar() {  # {{{1
     local name
     name="${1:?}"
 
+    # Version is optional and will be detected automatically if necessary.
     local version
     version="${2:-}"
 
@@ -42,41 +97,25 @@ _koopa_link_cellar() {  # {{{1
     _koopa_assert_is_dir "$cellar_prefix"
 
     # Detect the version automatically, if not specified.
-    if [ -n "$version" ]
+    if [[ -n "$version" ]]
     then
         cellar_prefix="${cellar_prefix}/${version}"
     else
-        cellar_prefix="$( \
-            find "$cellar_prefix" \
-                -mindepth 1 \
-                -maxdepth 1 \
-                -type d \
-            | sort \
-            | tail -n 1 \
-        )"
+        cellar_prefix="$(_koopa_find_cellar_version_dir "$name" "$version")"
     fi
     _koopa_assert_is_dir "$cellar_prefix"
 
     _koopa_h2 "Linking '${cellar_prefix}' in '${make_prefix}'."
     _koopa_set_permissions --recursive "$cellar_prefix"
     _koopa_remove_broken_symlinks "$cellar_prefix"
-
-    # Early return cellar-only if Homebrew is installed.
-    if _koopa_is_installed brew
-    then
-        _koopa_note "Homebrew installation detected."
-        _koopa_note "Skipping linkage into '${make_prefix}'."
-        return 0
-    fi
-
     _koopa_remove_broken_symlinks "$make_prefix"
 
     if _koopa_is_shared_install
     then
-        sudo cp -frs "$cellar_prefix/"* "$make_prefix/".
+        sudo cp -frs "${cellar_prefix}/"* "${make_prefix}/".
         _koopa_update_ldconfig
     else
-        cp -frs "$cellar_prefix/"* "$make_prefix/".
+        cp -frs "${cellar_prefix}/"* "${make_prefix}/".
     fi
 
     return 0
