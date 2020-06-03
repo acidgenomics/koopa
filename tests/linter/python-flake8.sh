a900639910fcc0f6218f1e152fc1cdf4fa17c252
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-03 13:07:07 -0400

    Add automatic flake8 linter checks

--- tests/linter/python-flake8.sh
+++ tests/linter/python-flake8.sh
@@ -8,14 +8,13 @@
 # shellcheck source=/dev/null
 source "${KOOPA_PREFIX:?}/shell/bash/include/header.sh"
 
-_koopa_exit_if_not_installed pylint
+_koopa_exit_if_not_installed flake8
 
 # Find files by shebang.
 grep_pattern='^#!/.*\bpython(3)?\b$'
 mapfile -t files < <(_koopa_test_find_files_by_shebang "$grep_pattern")
 
-# Note that setting '--jobs=0' flag here enables multicore.
-pylint --jobs=0 --score=n "${files[@]}"
+flake8 --ignore E402,W503 "${files[@]}"
 
 name="$(_koopa_basename_sans_ext "$0")"
 _koopa_status_ok "${name} [${#files[@]}]"
