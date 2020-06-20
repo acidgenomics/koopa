3c2fee81cfd3399ac1c8ff69d620d06ff0a8c20b
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-20 15:53:24 -0400

    Update roff

--- tests/help/roff.sh
+++ tests/help/roff.sh
@@ -2,7 +2,7 @@
 
 # """
 # Check that all scripts support '--help' flag.
-# Updated 2020-04-12.
+# Updated 2020-06-20.
 # """
 
 KOOPA_PREFIX="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../.." \
@@ -18,11 +18,7 @@ _koopa_h1 "Running manual file '--help' flag checks."
 
 # Put all 'man/' dirs into an array and loop.
 # Pipe GNU find into array.
-man_dirs=()
-while IFS= read -r -d $'\0'
-do
-    man_dirs+=("$REPLY")
-done < <( \
+readarray -t man_dirs <<< "$( \
     find "$KOOPA_PREFIX" \
         -mindepth 1 \
         -type d \
@@ -30,9 +26,9 @@ done < <( \
         -not -path "${KOOPA_PREFIX}/.git/*" \
         -not -path "${KOOPA_PREFIX}/dotfiles/*" \
         -not -path "${KOOPA_PREFIX}/shunit2-*" \
-        -print0 \
-    | sort -z \
-)
+        -print \
+    | sort \
+)"
 
 for dir in "${man_dirs[@]}"
 do
@@ -46,18 +42,14 @@ _koopa_h2 "Checking troff man file formatting."
 
 for dir in "${man_dirs[@]}"
 do
-    files=()
-    while IFS= read -r -d $'\0'
-    do
-        files+=("$REPLY")
-    done < <( \
+    readarray -t files <<< "$( \
         find "$dir" \
             -mindepth 2 \
             -maxdepth 2 \
             -type f \
-            -print0 \
-        | sort -z \
-    )
+            -print \
+        | sort \
+    )"
     for file in "${files[@]}"
     do
         _koopa_info "$file"
@@ -67,19 +59,13 @@ do
     done
 done
 
-exit 0
-
 # '--help' flag support {{{1
 # ==============================================================================
 
 _koopa_h2 "Running exported script '--help' flag checks."
 
 # Put all 'bin/' and/or 'sbin/' dirs into an array and loop.
-bin_dirs=()
-while IFS= read -r -d $'\0'
-do
-    bin_dirs+=("$REPLY")
-done < <( \
+readarray -t bin_dirs <<< "$( \
     find "$KOOPA_PREFIX" \
         -mindepth 1 \
         -type d \
@@ -87,24 +73,20 @@ done < <( \
         -not -path "*/cellar/*" \
         -not -path "*/dotfiles/*" \
         -not -path "${KOOPA_PREFIX}/shunit2-*" \
-        -print0 \
-    | sort -z \
-)
+        -print \
+    | sort \
+)"
 
 for dir in "${bin_dirs[@]}"
 do
-    files=()
-    while IFS= read -r -d $'\0'
-    do
-        files+=("$REPLY")
-    done < <( \
+    readarray -t files <<< "$( \
         find "$dir" \
             -mindepth 1 \
             -maxdepth 1 \
             -type f \
-            -print0 \
-        | sort -z \
-    )
+            -print \
+        | sort \
+    )"
     for file in "${files[@]}"
     do
         _koopa_info "$file"
