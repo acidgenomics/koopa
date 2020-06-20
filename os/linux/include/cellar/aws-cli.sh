55b0d173ed05fb98924f540da2a81d1c6f2b953f
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-20 08:31:43 -0400

    Simplify AWS CLI script

--- os/linux/include/cellar/aws-cli.sh
+++ os/linux/include/cellar/aws-cli.sh
@@ -2,6 +2,9 @@
 # shellcheck disable=SC2154
 
 # """
+# Homebrew recipe, for reference:
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/awscli.rb
+#
 # Note that the AWS bundled installer isn't versioned in the file name, so we
 # need to detect and place in the cellar dynamically instead.
 #
@@ -15,8 +18,8 @@
 # > python3 setup.py install
 # """
 
-file="${version}.tar.gz"
-url="https://github.com/aws/aws-cli/archive/${file}"
+file="awscli-exe-linux-x86_64.zip"
+url="https://awscli.amazonaws.com/${file}"
 _koopa_download "$url"
 _koopa_extract "$file"
 tmp_install_dir="tmp_install"
@@ -24,9 +27,7 @@ tmp_bin_dir="tmp_bin"
 ./aws/install -i "$tmp_install_dir" -b "$tmp_bin_dir" > /dev/null
 _koopa_cd "${tmp_install_dir}/v2"
 # Note that directory structure currently returns differently for Alpine.
-version="$(find . -mindepth 1 -maxdepth 1 -type d -name "2.*")"
-[[ -z "$version" ]] && _koopa_stop "Failed to detect version."
-version="$(basename "$version")"
-prefix="$(_koopa_cellar_prefix)/${name}/${version}"
-_koopa_exit_if_dir "$prefix"
-_koopa_cp "$version" "$prefix"
+version2="$(find . -mindepth 1 -maxdepth 1 -type d -name "2.*")"
+[[ -z "$version2" ]] && _koopa_stop "Failed to detect version."
+version2="$(basename "$version2")"
+_koopa_cp "$version2" "$prefix"
