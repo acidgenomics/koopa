7ba208001486f89204b7a339f28ea32e23b890aa
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-20 17:43:54 -0400

    Update rsync

---
+++ os/linux/include/cellar/rsync.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+
+file="${name}-${version}.tar.gz"
+url="https://download.samba.org/pub/${name}/src/${file}"
+_koopa_download "$url"
+_koopa_extract "$file"
+cd "${name}-${version}" || exit 1
+./configure --prefix="$prefix"
+make --jobs="$jobs"
+make install
