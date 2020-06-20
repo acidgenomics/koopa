cb0fadcde6fca52e6c8e7dc31cf8dd1f1eea6384
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-20 17:53:53 -0400

    Switch to single letter flag

--- os/linux/include/cellar/shunit2.sh
+++ os/linux/include/cellar/shunit2.sh
@@ -8,4 +8,4 @@ _koopa_extract "$file"
 cd "${name}-${version}" || exit 1
 _koopa_mkdir "${prefix}/bin"
 _koopa_set_permissions --recursive "$prefix"
-cp --archive "$name" -t "${prefix}/bin/"
+cp -a "$name" -t "${prefix}/bin/"
