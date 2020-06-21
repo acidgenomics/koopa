c204a6d61c0f47a2bbe123b59e9a202eac50fc9d
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-20 21:37:22 -0400

    Simplify bash installer

--- os/linux/include/cellar/bash.sh
+++ os/linux/include/cellar/bash.sh
@@ -2,21 +2,21 @@
 # shellcheck disable=SC2154
 
 minor_version="$(_koopa_major_minor_version "$version")"
-file="bash-${minor_version}.tar.gz"
-url="${gnu_mirror}/bash/${file}"
+file="${name}-${minor_version}.tar.gz"
+url="${gnu_mirror}/${name}/${file}"
 _koopa_download "$url"
 _koopa_extract "$file"
-cd "bash-${minor_version}" || exit 1
+cd "${name}-${minor_version}" || exit 1
 # Apply patches. 
 patches="$(_koopa_print "$version" | cut -d '.' -f 3)"
 (
     mkdir -pv patches
     cd patches || exit 1
     # Note that GNU mirror doesn't seem to work correctly here.
-    base_url="https://ftp.gnu.org/gnu/bash/bash-${minor_version}-patches"
+    base_url="https://ftp.gnu.org/gnu/${name}/${name}-${minor_version}-patches"
     mv_tr="$(_koopa_print "$minor_version" | tr -d '.')"
     range="$(printf "%03d-%03d" "1" "$patches")"
-    request="${base_url}/bash${mv_tr}-[${range}]"
+    request="${base_url}/${name}${mv_tr}-[${range}]"
     curl "$request" -O
     cd .. || exit 1
     for file in patches/*
@@ -32,4 +32,4 @@ make --jobs="$jobs"
 # > make test
 make install
 
-_koopa_enable_shell bash
+_koopa_enable_shell "$name"
