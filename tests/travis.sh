108fa44079ed76aa032888d947f74cb53f477ac4
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-20 21:45:17 -0400

    Reorganize CI scripts

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,14 +1,1 @@
-#!/usr/bin/env bash
-set -Eeu -o pipefail
-
-# """
-# Continuous integration (CI) tests.
-# Updated 2020-02-04.
-# """
-
-tests_dir="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
-    >/dev/null 2>&1 && pwd -P)"
-
-"${tests_dir}/linter.sh"
-# > "${tests_dir}/help.sh"
-"${tests_dir}/shunit2.sh"
+ci.sh
