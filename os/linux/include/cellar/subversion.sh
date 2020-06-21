449f245db263704a46001f6e9f00013c5a09b07a
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-21 11:24:53 -0400

    Improve subversion config

--- os/linux/include/cellar/subversion.sh
+++ os/linux/include/cellar/subversion.sh
@@ -5,26 +5,12 @@
 # https://subversion.apache.org/download.cgi
 # https://subversion.apache.org/source-code.html
 # https://svn.apache.org/repos/asf/subversion/trunk/INSTALL
+#
+# Requires Apache Portable Runtime (APR) library and Apache Portable Runtime
+# Utility (APRUTIL) library.
 # """
 
-# configure: Apache Portable Runtime (APR) library configuration
-# checking for APR... no
-# configure: WARNING: APR not found
-# The Apache Portable Runtime (APR) library cannot be found.
-# Please install APR on this system and configure Subversion
-# with the appropriate --with-apr option.
-#
-# You probably need to do something similar with the Apache
-# Portable Runtime Utility (APRUTIL) library and then configure
-# Subversion with both the --with-apr and --with-apr-util options.
-#
-# configure: error: no suitable APR found
-
-# configure: error: Subversion requires LZ4 >= r129, or use --with-lz4=internal
-
-# Debian: libapr1-dev libaprutil1-dev
 _koopa_assert_is_installed apr-config apu-config
-
 
 file="${name}-${version}.tar.bz2"
 url="https://mirrors.ocf.berkeley.edu/apache/${name}/${file}"
