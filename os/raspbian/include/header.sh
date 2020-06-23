d40fbb639d5e48c53ae35a879448339f0e19622a
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-23 11:03:38 -0400

    Improve consistency of script headers

--- os/raspbian/include/header.sh
+++ os/raspbian/include/header.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../debian/include/header.sh"
