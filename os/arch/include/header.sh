f81ebaf770ac4b801cde86ce9a14402bd7c5959a
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-23 11:03:38 -0400

    Improve consistency of script headers

--- os/arch/include/header.sh
+++ os/arch/include/header.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../linux/include/header.sh"
 
