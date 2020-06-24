46be6910de5f60763c205e3d7c7a7a145678c80a
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-24 17:30:16 -0400

    Update R header

--- lang/r/include/header.R
+++ lang/r/include/header.R
@@ -132,6 +132,7 @@ if (
 ## =============================================================================
 
 suppressPackageStartupMessages({
+    library(utils)
     library(acidbase)
     library(goalie)
 })
