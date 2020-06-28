e0a92f2deb94ff84b400bd4ee954981c44d43543
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-28 09:28:22 -0400

    Attempt to improve make recipe

--- os/linux/include/cellar/rsync.sh
+++ os/linux/include/cellar/rsync.sh
@@ -6,6 +6,8 @@ url="https://download.samba.org/pub/${name}/src/${file}"
 _koopa_download "$url"
 _koopa_extract "$file"
 cd "${name}-${version}" || exit 1
-./configure --prefix="$prefix"
+./configure \
+    --prefix="$prefix" \
+    --without-included-zlib
 make --jobs="$jobs"
 make install
