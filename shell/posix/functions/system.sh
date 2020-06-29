5aae10ea6ee0471b3d04eb076069a4cc3d173986
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-29 18:24:07 -0400

    Remove FIXME comment

--- shell/posix/functions/system.sh
+++ shell/posix/functions/system.sh
@@ -569,7 +569,6 @@ _koopa_header() {  # {{{1
     return 0
 }
 
-# FIXME NOW THIS ISNT WORKING ON MACOS
 _koopa_host_id() {  # {{{1
     # """
     # Simple host ID string to load up host-specific scripts.
