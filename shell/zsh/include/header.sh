9a7dfdaa03565460c6f8c18a06a3cb4b1d9f2b75
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-30 09:51:41 -0400

    Improve header config

--- shell/zsh/include/header.sh
+++ shell/zsh/include/header.sh
@@ -4,14 +4,13 @@ _koopa_zsh_header() { # {{{1
     # """
     # Zsh header.
     # @note Updated 2020-06-30.
-    #
-    # Don't set local variables here, it will mess up activation.
     # """
-    [[ -n "${KOOPA_VERBOSE:-}" ]] && verbose=1
-    [[ -z "${activate:-}" ]] && activate=0
-    [[ -z "${checks:-}" ]] && checks=1
-    [[ -z "${shopts:-}" ]] && shopts=1
-    [[ -z "${verbose:-}" ]] && verbose=0
+    local file major_version pos
+    [[ -n "${KOOPA_VERBOSE:-}" ]] && local verbose=1
+    [[ -z "${activate:-}" ]] && local activate=0
+    [[ -z "${checks:-}" ]] && local checks=1
+    [[ -z "${shopts:-}" ]] && local shopts=1
+    [[ -z "${verbose:-}" ]] && local verbose=0
     if [[ "$#" -gt 0 ]]
     then
         pos=()
@@ -107,7 +106,7 @@ _koopa_zsh_header() { # {{{1
     then
         unalias -a
     fi
-    unset -v activate file checks major_version pos shopts
+    unset -v activate checks shopts verbose
     return 0
 }
 
