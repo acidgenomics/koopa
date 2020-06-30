8988469ffc3e5af5acc6f04b338e3e5d3d5d8344
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-29 21:22:39 -0400

    Harden functions

--- shell/bash/functions/ngs-align.sh
+++ shell/bash/functions/ngs-align.sh
@@ -3,7 +3,7 @@
 _koopa_bam_filter() {  # {{{1
     # """
     # Perform filtering on a BAM file.
-    # @note Updated 2020-02-05.
+    # @note Updated 2020-06-29.
     #
     # See also:
     # - https://lomereiter.github.io/sambamba/docs/sambamba-view.html
@@ -12,8 +12,8 @@ _koopa_bam_filter() {  # {{{1
     # - https://hbctraining.github.io/In-depth-NGS-Data-Analysis-Course/
     #       sessionV/lessons/03_align_and_filtering.html
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_assert_is_installed sambamba
-
     while (("$#"))
     do
         case "$1" in
@@ -34,29 +34,23 @@ _koopa_bam_filter() {  # {{{1
                 ;;
         esac
     done
-
     _koopa_assert_is_set filter input_bam output_bam
     _koopa_assert_are_not_identical "$input_bam" "$output_bam"
-
     local input_bam_bn
     input_bam_bn="$(basename "$input_bam")"
     local output_bam_bn
     output_bam_bn="$(basename "$output_bam")"
-
     if [[ -f "$output_bam" ]]
     then
         _koopa_note "Skipping '${output_bam_bn}'."
         return 0
     fi
-
     _koopa_h2 "Filtering '${input_bam_bn}' to '${output_bam_bn}'."
     _koopa_assert_is_file "$input_bam"
     _koopa_dl "Filter" "'$filter'"
-
     local threads
     threads="$(_koopa_cpu_count)"
     _koopa_dl "Threads" "$threads"
-
     # Note that sambamba prints version information into stderr.
     sambamba view \
         --filter="$filter" \
@@ -66,19 +60,19 @@ _koopa_bam_filter() {  # {{{1
         --show-progress \
         --with-header \
         "$input_bam"
-
     return 0
 }
 
 _koopa_bam_filter_duplicates() {  # {{{1
     # """
     # Remove duplicates from a duplicate marked BAM file.
-    # @note Updated 2020-02-04.
+    # @note Updated 2020-06-29.
     #
     # See also:
     # https://github.com/bcbio/bcbio-nextgen/blob/master/bcbio/
     #     bam/__init__.py
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_bam_filter --filter="not duplicate" "$@"
     return 0
 }
@@ -86,12 +80,13 @@ _koopa_bam_filter_duplicates() {  # {{{1
 _koopa_bam_filter_multimappers() {  # {{{1
     # """
     # Filter multi-mapped reads from BAM file.
-    # @note Updated 2020-02-04.
+    # @note Updated 2020-06-29.
     #
     # See also:
     # https://github.com/bcbio/bcbio-nextgen/blob/master/bcbio/
     #     chipseq/__init__.py
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_bam_filter --filter="[XS] == null" "$@"
     return 0
 }
@@ -99,8 +94,9 @@ _koopa_bam_filter_multimappers() {  # {{{1
 _koopa_bam_filter_unmapped() {  # {{{1
     # """
     # Filter unmapped reads from BAM file.
-    # @note Updated 2020-02-04.
+    # @note Updated 2020-06-29.
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_bam_filter --filter="not unmapped" "$@"
     return 0
 }
@@ -110,23 +106,19 @@ _koopa_bam_index() {  # {{{1
     # Index BAM file.
     # @note Updated 2020-02-05.
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_assert_is_installed samtools
-
     local bam_file
     bam_file="${1:?}"
-
     _koopa_h2 "Indexing '${bam_file}'."
     _koopa_assert_is_file "$bam_file"
-
     local threads
     threads="$(_koopa_cpu_count)"
     _koopa_dl "Threads" "$threads"
-
     sambamba index \
         --nthreads="$threads" \
         --show-progress \
         "$bam_file"
-
     return 0
 }
 
@@ -141,8 +133,8 @@ _koopa_bam_sort() {  # {{{1
     # See also:
     # https://lomereiter.github.io/sambamba/docs/sambamba-sort.html
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_assert_is_installed sambamba
-
     local unsorted_bam
     unsorted_bam="${1:?}"
     local sorted_bam
@@ -151,20 +143,16 @@ _koopa_bam_sort() {  # {{{1
     unsorted_bam_bn="$(basename "$unsorted_bam")"
     local sorted_bam_bn
     sorted_bam_bn="$(basename "$sorted_bam")"
-
     if [[ -f "$sorted_bam" ]]
     then
         _koopa_note "Skipping '${sorted_bam_bn}'."
         return 0
     fi
-
     _koopa_h2 "Sorting '${unsorted_bam_bn}' to '${sorted_bam_bn}'."
     _koopa_assert_is_file "$unsorted_bam"
-
     local threads
     threads="$(_koopa_cpu_count)"
     _koopa_dl "Threads" "${threads}"
-
     # This is noisy and spits out program version information, so hiding stdout
     # and stderr. Note that simply using '> /dev/null' doesn't work here.
     sambamba sort \
@@ -173,17 +161,16 @@ _koopa_bam_sort() {  # {{{1
         --out="$sorted_bam" \
         --show-progress \
         "$unsorted_bam"
-
     return 0
 }
 
 _koopa_bowtie2() {  # {{{1
     # """
     # Run bowtie2 on paired-end FASTQ files.
-    # @note Updated 2020-02-05.
+    # @note Updated 2020-06-29.
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_assert_is_installed bowtie2
-
     while (("$#"))
     do
         case "$1" in
@@ -216,47 +203,34 @@ _koopa_bowtie2() {  # {{{1
                 ;;
         esac
     done
-
     _koopa_assert_is_set fastq_r1 fastq_r2 index_prefix output_dir \
         r1_tail r2_tail
     _koopa_assert_is_file "$fastq_r1" "$fastq_r2"
-
     local fastq_r1_bn
     fastq_r1_bn="$(basename "$fastq_r1")"
     fastq_r1_bn="${fastq_r1_bn/${r1_tail}/}"
-
     local fastq_r2_bn
     fastq_r2_bn="$(basename "$fastq_r2")"
     fastq_r2_bn="${fastq_r2_bn/${r2_tail}/}"
-
     _koopa_assert_are_identical "$fastq_r1_bn" "$fastq_r2_bn"
-
     local id
     id="$fastq_r1_bn"
-
     local sample_output_dir
     sample_output_dir="${output_dir}/${id}"
-
     if [[ -d "$sample_output_dir" ]]
     then
         _koopa_note "Skipping '${id}'."
         return 0
     fi
-
     _koopa_h2 "Aligning '${id}' into '${sample_output_dir}'."
-
     local threads
     threads="$(_koopa_cpu_count)"
     _koopa_dl "Threads" "$threads"
-
     local sam_file
     sam_file="${sample_output_dir}/${id}.sam"
-
     local log_file
     log_file="${sample_output_dir}/bowtie2.log"
-
     mkdir -pv "$sample_output_dir"
-
     bowtie2 \
         --local \
         --sensitive-local \
@@ -272,18 +246,16 @@ _koopa_bowtie2() {  # {{{1
         -q \
         -x "$index_prefix" \
         2>&1 | tee "$log_file"
-
     return 0
 }
-
 
 _koopa_bowtie2_index() {  # {{{1
     # """
     # Generate bowtie2 index.
     # @note Updated 2020-02-05.
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_assert_is_installed bowtie2-build
-
     while (("$#"))
     do
         case "$1" in
@@ -300,44 +272,36 @@ _koopa_bowtie2_index() {  # {{{1
                 ;;
         esac
     done
-
     _koopa_assert_is_set fasta_file index_dir
     _koopa_assert_is_file "$fasta_file"
-
     if [[ -d "$index_dir" ]]
     then
         _koopa_note "Index exists at '${index_dir}'. Skipping."
         return 0
     fi
-
     _koopa_h2 "Generating bowtie2 index at '${index_dir}'."
-
     local threads
     threads="$(_koopa_cpu_count)"
     _koopa_dl "Threads" "$threads"
-
     # Note that this step adds 'bowtie2.*' to the file names created in the
     # index directory.
     local index_prefix
     index_prefix="${index_dir}/bowtie2"
-
     mkdir -pv "$index_dir"
-
     bowtie2-build \
         --threads="$threads" \
         "$fasta_file" \
         "$index_prefix"
-
     return 0
 }
 
 _koopa_sam_to_bam() {  # {{{1
     # """
     # Convert SAM file to BAM.
-    # @note Updated 2020-02-05.
+    # @note Updated 2020-06-29.
     # """
+    [[ "$#" -gt 0 ]] || return 1
     _koopa_assert_is_installed samtools
-
     while (("$#"))
     do
         case "$1" in
@@ -354,33 +318,26 @@ _koopa_sam_to_bam() {  # {{{1
                 ;;
         esac
     done
-
     _koopa_assert_is_set input_sam output_bam
-
     local sam_bn
     sam_bn="$(basename "$input_sam")"
     local bam_bn
     bam_bn="$(basename "$output_bam")"
-
     if [[ -f "$output_bam" ]]
     then
         _koopa_note "Skipping '${bam_bn}'."
         return 0
     fi
-
     _koopa_h2 "Converting '${sam_bn}' to '${bam_bn}'."
     _koopa_assert_is_file "$input_sam"
-
     local threads
     threads="$(_koopa_cpu_count)"
     _koopa_dl "Threads" "$threads"
-
     samtools view \
         -@ "$threads" \
         -b \
         -h \
         -o "$output_bam" \
         "$input_sam"
-
     return 0
 }
--- shell/bash/functions/tests.sh
+++ shell/bash/functions/tests.sh
@@ -3,43 +3,34 @@
 _koopa_test_find_files_by_ext() {  # {{{1
     # """
     # Find relevant test files by extension.
-    # @note Updated 2020-06-20.
+    # @note Updated 2020-06-29.
     # """
-    local ext
-    ext="${1:-?}"
-
-    local pattern
+    [[ "$#" -gt 0 ]] || return 1
+    local ext files pattern x
+    ext="${1:?}"
     pattern="\.${ext}$"
-
-    local files
     readarray -t files <<< "$(_koopa_test_find_files)"
-
-    local x
     x="$( \
         printf '%s\n' "${files[@]}" \
         | grep -Ei "$pattern" \
     )"
+    [[ -n "$x" ]] || return 1
     _koopa_print "$x"
+    return 0
 }
 
 _koopa_test_find_files_by_shebang() {  # {{{1
     # """
     # Find relevant test files by shebang.
-    # @note Updated 2020-06-20.
+    # @note Updated 2020-06-29.
     # """
-    local pattern
+    [[ "$#" -gt 0 ]] || return 1
+    local file files pattern shebang_files x
     pattern="${1:?}"
-
-    local files
     readarray -t files <<< "$(_koopa_test_find_files)"
-
-    local shebang_files
     shebang_files=()
-
-    local file
     for file in "${files[@]}"
     do
-        local x
         x="$(
             grep -El \
                 --binary-files="without-match" \
@@ -49,7 +40,6 @@ _koopa_test_find_files_by_shebang() {  # {{{1
         )"
         [[ -n "$x" ]] && shebang_files+=("$x")
     done
-
     printf '%s\n' "${shebang_files[@]}"
     return 0
 }
@@ -57,24 +47,15 @@ _koopa_test_find_files_by_shebang() {  # {{{1
 _koopa_test_find_failures() {  # {{{1
     # """
     # Find test failures.
-    # @note Updated 2020-06-20.
+    # @note Updated 2020-06-29.
     # """
-    local name
+    [[ "$#" -gt 0 ]] || return 1
+    local failures file files ignore name pattern x
     name="$(_koopa_basename_sans_ext "$0")"
-
-    local pattern
     pattern="${1:?}"
-
-    local ignore
     ignore="${2:-}"
-
-    local files
-    readarray -t files <<< "$(_koopa_test_find_files)"
-
-    local failures
     failures=()
-
-    local file
+    readarray -t files <<< "$(_koopa_test_find_files)"
     for file in "${files[@]}"
     do
         # Skip ignored files.
@@ -88,8 +69,6 @@ _koopa_test_find_failures() {  # {{{1
                 continue
             fi
         fi
-
-        local x
         x="$(
             grep -EHn \
                 --binary-files="without-match" \
@@ -99,13 +78,12 @@ _koopa_test_find_failures() {  # {{{1
         )"
         [[ -n "$x" ]] && failures+=("$x")
     done
-
     if [[ "${#failures[@]}" -gt 0 ]]
     then
         _koopa_status_fail "${name} [${#failures[@]}]"
         printf '%s\n' "${failures[@]}"
         return 1
     fi
-
     _koopa_status_ok "${name} [${#files[@]}]"
+    return 0
 }
