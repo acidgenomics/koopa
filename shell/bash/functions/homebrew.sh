bd024fdbdf65210a51cb5aee4d2e6240e0c4606c
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-01 14:16:57 -0400

    Improve Homebrew version check

--- shell/bash/functions/homebrew.sh
+++ shell/bash/functions/homebrew.sh
@@ -20,8 +20,23 @@ _koopa_brew_cask_outdated() { # {{{
     local tmp_file x
     tmp_file="$(_koopa_tmp_file)"
     script -q "$tmp_file" brew cask outdated --greedy >/dev/null
-    x="$(grep -v "(latest)" "$tmp_file" | cut -d " " -f 1)"
+    x="$(grep -v "(latest)" "$tmp_file")"
     [[ -n "$x" ]] || return 1
     _koopa_print "$x"
+    return 0
+}
+
+_koopa_brew_outdated() { # {{{
+    # """
+    # Listed outdated Homebrew brews and casks, in a single call.
+    # @note Updated 2020-07-01.
+    # """
+    [[ "$#" -eq 0 ]] || return 1
+    _koopa_h1 "Checking for outdated Homebrew formula."
+    brew update &>/dev/null
+    _koopa_h2 "Brews"
+    brew outdated
+    _koopa_h2 "Casks"
+    _koopa_brew_cask_outdated
     return 0
 }
