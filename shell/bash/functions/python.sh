8bb068b3d76b67ea6352c785481ad907ce756743
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-04 19:43:27 -0400

    Reorganize functions

--- shell/bash/functions/python.sh
+++ shell/bash/functions/python.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+koopa::pip_install() { # {{{1
+    # """
+    # Internal pip install command.
+    # @note Updated 2020-07-03.
+    # """
+    koopa::assert_has_args "$#"
+    local python
+    python="python3"
+    koopa::assert_is_installed "$python"
+    "$python" \
+        -m pip install \
+        --no-warn-script-location \
+        "$@"
+    return 0
+}
+
 koopa::venv_create() {
     # """
     # Create Python virtual environment.
