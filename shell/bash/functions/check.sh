653ba8d1ac3073dcd581a522056186bb8a25a89b
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-04 19:41:00 -0400

    Rework functions

--- shell/bash/functions/check.sh
+++ shell/bash/functions/check.sh
@@ -1,41 +1,40 @@
-#!/bin/sh
-# shellcheck disable=SC2039
+#!/usr/bin/env bash
 
 koopa::check_azure() { # {{{1
     # """
     # Check Azure VM integrity.
-    # @note Updated 2019-10-31.
+    # @note Updated 2020-07-04.
     # """
     koopa::assert_has_no_args "$#"
     koopa::is_azure || return 0
-    if [ -e "/mnt/resource" ]
+    local mount
+    mount='/mnt/resource'
+    if [[ -e "$mount" ]]
     then
-        koopa::check_user "/mnt/resource" "root"
-        koopa::check_group "/mnt/resource" "root"
-        koopa::check_access_octal "/mnt/resource" "1777"
+        koopa::check_user "$mount" 'root'
+        koopa::check_group "$mount" 'root'
+        koopa::check_access_octal "$mount" '1777'
     fi
-    koopa::check_mount "/mnt/rdrive"
+    koopa::check_mount '/mnt/rdrive'
     return 0
 }
 
 koopa::check_access_human() { # {{{1
     # """
     # Check if file or directory has expected human readable access.
-    # @note Updated 2020-01-12.
+    # @note Updated 2020-07-04.
     # """
     koopa::assert_has_args "$#"
-    local file
+    local access code file
     file="${1:?}"
-    local code
     code="${2:?}"
-    if [ ! -e "$file" ]
+    if [[ ! -e "$file" ]]
     then
         koopa::warning "'${file}' does not exist."
         return 1
     fi
-    local access
     access="$(koopa::stat_access_human "$file")"
-    if [ "$access" != "$code" ]
+    if [[ "$access" != "$code" ]]
     then
         koopa::warning "'${file}' current access '${access}' is not '${code}'."
     fi
@@ -45,21 +44,19 @@ koopa::check_access_human() { # {{{1
 koopa::check_access_octal() { # {{{1
     # """
     # Check if file or directory has expected octal access.
-    # @note Updated 2020-01-12.
+    # @note Updated 2020-07-04.
     # """
     koopa::assert_has_args "$#"
-    local file
+    local access code file
     file="${1:?}"
-    local code
     code="${2:?}"
-    if [ ! -e "$file" ]
+    if [[ ! -e "$file" ]]
     then
         koopa::warning "'${file}' does not exist."
         return 1
     fi
-    local access
     access="$(koopa::stat_access_octal "$file")"
-    if [ "$access" != "$code" ]
+    if [[ "$access" != "$code" ]]
     then
         koopa::warning "'${file}' current access '${access}' is not '${code}'."
     fi
@@ -69,21 +66,19 @@ koopa::check_access_octal() { # {{{1
 koopa::check_group() { # {{{1
     # """
     # Check if file or directory has an expected group.
-    # @note Updated 2020-01-12.
+    # @note Updated 2020-07-04.
     # """
     koopa::assert_has_args "$#"
-    local file
+    local code file group
     file="${1:?}"
-    local code
     code="${2:?}"
-    if [ ! -e "$file" ]
+    if [[ ! -e "$file" ]]
     then
         koopa::warning "'${file}' does not exist."
         return 1
     fi
-    local group
     group="$(koopa::stat_group "$file")"
-    if [ "$group" != "$code" ]
+    if [[ "$group" != "$code" ]]
     then
         koopa::warning "'${file}' current group '${group}' is not '${code}'."
         return 1
@@ -95,13 +90,13 @@ koopa::check_mount() { # {{{1
     # """
     # Check if a drive is mounted.
     # Usage of find is recommended over ls here.
-    # @note Updated 2020-06-30.
+    # @note Updated 2020-07-04.
     # """
     koopa::assert_has_args "$#"
     koopa::assert_is_installed find
     local mnt
     mnt="${1:?}"
-    if [ "$(find "$mnt" -mindepth 1 -maxdepth 1 | wc -l)" -eq 0 ]
+    if [[ "$(find "$mnt" -mindepth 1 -maxdepth 1 | wc -l)" -eq 0 ]]
     then
         koopa::warning "'${mnt}' is unmounted."
         return 1
@@ -112,22 +107,20 @@ koopa::check_mount() { # {{{1
 koopa::check_user() { # {{{1
     # """
     # Check if file or directory is owned by an expected user.
-    # @note Updated 2020-01-13.
+    # @note Updated 2020-07-04.
     # """
     koopa::assert_has_args "$#"
-    local file
+    local current_user expected_user file
     file="${1:?}"
-    if [ ! -e "$file" ]
+    if [[ ! -e "$file" ]]
     then
         koopa::warning "'${file}' does not exist on disk."
         return 1
     fi
     file="$(realpath "$file")"
-    local expected_user
     expected_user="${2:?}"
-    local current_user
     current_user="$(koopa::stat_user "$file")"
-    if [ "$current_user" != "$expected_user" ]
+    if [[ "$current_user" != "$expected_user" ]]
     then
         koopa::warning "'${file}' user '${current_user}' is not \
 '${expected_user}'."
--- shell/posix/functions/system.sh
+++ shell/posix/functions/system.sh
@@ -1426,22 +1426,3 @@ koopa::view_latest_tmp_log_file() { # {{{1
     less +G "$log_file"
     return 0
 }
-
-koopa::warn_if_export() { # {{{1
-    # """
-    # Warn if variable is exported in current shell session.
-    # @note Updated 2020-02-20.
-    #
-    # Useful for checking against unwanted compiler settings.
-    # In particular, useful to check for 'LD_LIBRARY_PATH'.
-    # """
-    koopa::assert_has_args "$#"
-    for arg in "$@"
-    do
-        if koopa::is_export "$arg"
-        then
-            koopa::warning "'${arg}' is exported."
-        fi
-    done
-    return 0
-}
