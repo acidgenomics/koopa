8f8ae8177a3777a746f7a25729680f1bd57b4454
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-05 19:37:24 -0400

    Draft update to POSIX functions

--- shell/posix/functions/interactive.sh
+++ shell/posix/functions/interactive.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+_koopa_git_branch() { # {{{1
+    # """
+    # Current git branch name.
+    # @note Updated 2020-07-05.
+    #
+    # This is used in prompt, so be careful with assert checks.
+    #
+    # Handles detached HEAD state.
+    #
+    # Alternatives:
+    # > git name-rev --name-only HEAD
+    # > git rev-parse --abbrev-ref HEAD
+    #
+    # @seealso
+    # - https://git.kernel.org/pub/scm/git/git.git/tree/contrib/completion/
+    #       git-completion.bash?id=HEAD
+    # """
+    # shellcheck disable=SC2039
+    local branch
+    _koopa_is_git || return 0
+    _koopa_is_installed git || return 0
+    branch="$(git symbolic-ref --short -q HEAD 2>/dev/null)"
+    _koopa_print "$branch"
+    return 0
+}
+
 _koopa_tmux_sessions() { # {{{1
     # """
     # Show active tmux sessions.
