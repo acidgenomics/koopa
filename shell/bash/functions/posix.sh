197202585338838297d8d82aedf36331f2dfdba3
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-05 14:25:27 -0400

    Simplify POSIX functions

--- shell/bash/functions/posix.sh
+++ shell/bash/functions/posix.sh
@@ -1,16 +1,75 @@
 #!/usr/bin/env bash
 
 koopa::cpu_count() {
-    koopa::assert_has_no_args "$#"
-    _koopa_cpu_count
+    _koopa_cpu_count "$@"
 }
 
 koopa::group() {
-    koopa::assert_has_no_args "$#"
-    _koopa_group
+    _koopa_group "$@"
 }
 
 koopa::group_id() {
-    koopa::assert_has_no_args "$#"
-    _koopa_group_id
+    _koopa_group_id "$@"
 }
+
+koopa::hostname() {
+    _koopa_hostname "$@"
+}
+
+koopa::host_id() {
+    _koopa_host_id "$@"
+}
+
+koopa::print() {
+    _koopa_print "$@"
+}
+
+# get_minor_version
+# get_version
+# is_debiasn
+# is_installed
+# is_kali
+# is_linux
+# os_codename
+# os_id
+# os_string
+# stop
+# shell
+# user
+# user_id
+
+# kebab_case
+# gsub
+# lowercase
+# snake_case
+# strip_left
+# strip_right
+# strip_trailing_slash
+# sub
+# trim_ws
+
+# info
+# invalid_arg
+# missing_arg
+# note
+# print
+# print_black
+# print_black_bold
+# print_blue
+# print_blue_bold
+# print_cyan
+# print_cyan_bold
+# print_default
+# print_default_bold
+# print_green
+# print_green_bold
+# print_magenta
+# print_magenta_bold
+# print_red
+# print_red_bold
+# print_yellow
+# print_yellow_bold
+# print_white
+# print_white_bold
+# stop
+# warning
