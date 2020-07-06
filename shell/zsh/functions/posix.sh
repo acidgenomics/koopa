b1df6860c9d607353282bdd3b2ff2feef650b613
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-05 22:05:57 -0400

    Improve Zsh function handling

---
+++ shell/zsh/functions/posix.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+koopa::dotfiles_prefix() {
+    _koopa_dotfiles_prefix "$@"
+}
+
+koopa::force_add_to_fpath_start() {
+    _koopa_force_add_to_fpath_start "$@"
+}
+
+koopa::prefix() {
+    _koopa_prefix "$@"
+}
+
+koopa::warning() {
+    _koopa_warning "$@"
+}
