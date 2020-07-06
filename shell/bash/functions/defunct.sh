42ecfb23316fee90a3cd17cef3300aba003b98cd
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-06 08:56:10 -0400

    Improve Bash/POSIX function handling

--- shell/bash/functions/defunct.sh
+++ shell/bash/functions/defunct.sh
@@ -3,7 +3,7 @@
 # Note that these are defined primarily to catch errors in private scripts that
 # are defined outside of the koopa package.
 
-koopa::_defunct() { # {{{1
+koopa::defunct() { # {{{1
     # """
     # Make a function defunct.
     # @note Updated 2020-02-18.
@@ -23,54 +23,54 @@ koopa::assert_is_darwin() { # {{{1
     # """
     # @note Updated 2020-01-14.
     # """
-    koopa::_defunct "koopa::assert_is_macos"
+    koopa::defunct "koopa::assert_is_macos"
 }
 
 koopa::is_darwin() { # {{{1
     # """
     # @note Updated 2020-01-14.
     # """
-    koopa::_defunct "koopa::is_macos"
+    koopa::defunct "koopa::is_macos"
 }
 
 koopa::is_matching_fixed() {  #{{{1
     # """
     # @note Updated 2020-04-29.
     # """
-    koopa::_defunct "koopa::str_match"
+    koopa::defunct "koopa::str_match"
 }
 
 koopa::is_matching_regex() {  #{{{1
     # """
     # @note Updated 2020-04-29.
     # """
-    koopa::_defunct "koopa::str_match_regex"
+    koopa::defunct "koopa::str_match_regex"
 }
 
 koopa::prefix_mkdir() { # {{{1
     # """
     # @note Updated 2020-02-19.
     # """
-    koopa::_defunct "koopa::mkdir"
+    koopa::defunct "koopa::mkdir"
 }
 
 koopa::quiet_cd() { # {{{1
     # """
     # @note Updated 2020-02-16.
     # """
-    koopa::_defunct "koopa::cd"
+    koopa::defunct "koopa::cd"
 }
 
 koopa::update_profile() { # {{{1
     # """
     # @note Updated 2020-02-15.
     # """
-    koopa::_defunct "koopa::update_etc_profile_d"
+    koopa::defunct "koopa::update_etc_profile_d"
 }
 
 koopa::update_shells() { # {{{1
     # """
     # @note Updated 2020-02-11.
     # """
-    koopa::_defunct "koopa::enable_shell"
+    koopa::defunct "koopa::enable_shell"
 }
