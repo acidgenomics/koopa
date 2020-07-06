79301fde6afe9625e5d4743dbc7c99a7ab77d89b
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-05 22:41:33 -0400

    Improve bash reexports

--- shell/zsh/functions/posix.sh
+++ shell/zsh/functions/posix.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env zsh
 
+koopa::add_to_fpath_end() {
+    _koopa_force_add_to_fpath_end "$@"
+}
+
+koopa::add_to_fpath_start() {
+    _koopa_force_add_to_fpath_start "$@"
+}
+
 koopa::dotfiles_prefix() {
     _koopa_dotfiles_prefix "$@"
 }
 
+koopa::force_add_to_fpath_end() {
+    _koopa_force_add_to_fpath_end "$@"
+}
+
 koopa::force_add_to_fpath_start() {
     _koopa_force_add_to_fpath_start "$@"
+}
+
+koopa::remove_from_fpath() {
+    _koopa_remove_from_fpath "$@"
 }
 
 koopa::prefix() {
