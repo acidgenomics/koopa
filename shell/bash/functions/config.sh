8a8d81f09d56f2464e87d6ca31b0ba8a41e68b67
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-07 19:59:39 -0400

    Improve dotfiles installer

--- shell/bash/functions/config.sh
+++ shell/bash/functions/config.sh
@@ -424,7 +424,11 @@ koopa::link_docker() { # {{{1
     return 0
 }
 
-koopa::link_dotfile() {
+koopa::link_dotfile() { # {{{1
+    # """
+    # Link dotfile.
+    # @note Updated 2020-07-07.
+    # """
     local config dot_dir dot_repo force pos private source_name symlink_name
     koopa::assert_has_args "$#"
     config=0
@@ -467,7 +471,6 @@ koopa::link_dotfile() {
     then
         dot_dir="$(koopa::dotfiles_private_config_link)"
     else
-        # FIXME Can I remove and simplify this step?
         # e.g. ~/.config/koopa/dotfiles
         dot_dir="$(koopa::dotfiles_config_link)"
         # Note that this step automatically links into koopa config for users.
@@ -489,33 +492,24 @@ koopa::link_dotfile() {
     # Add link either in HOME (default) or XDG_CONFIG_HOME.
     if [[ "$config" -eq 1 ]]
     then
-        # FIXME REQUIRE THIS. ERROR IF NOT SET.
-        if [[ -z "${XDG_CONFIG_HOME:-}" ]]
-        then
-            XDG_CONFIG_HOME="${HOME}/.config"
-        fi
-        symlink_path="${XDG_CONFIG_HOME}/${symlink_name}"
+        symlink_path="${XDG_CONFIG_HOME:?}/${symlink_name}"
     else
-        symlink_path="${HOME}/.${symlink_name}"
+        symlink_path="${HOME:?}/.${symlink_name}"
     fi
     # Inform the user when nuking a broken symlink.
     if [[ "$force" -eq 1 ]] ||
         { [[ -L "$symlink_path" ]] && [[ ! -e "$symlink_path" ]]; }
     then
-        rm -frv "$symlink_path"
+        koopa::rm "$symlink_path"
     elif [[ -e "$symlink_path" ]]
     then
-        koopa::stop "Existing dotfile: '${symlink_path}'."
-        exit 1
+        koopa::stop "Existing dotfile: \"${symlink_path}\"."
+        return 1
     fi
-    koopa::info "Symlinking '$(basename "$symlink_path")'."
-    # Note that 'ln -fnsv' fails to create subdirectories automatically.
+    koopa::dl "$symlink_path" "$source_path"
     symlink_dn="$(dirname "$symlink_path")"
-    if [[ "$symlink_dn" != "$HOME" ]]
-    then
-        mkdir -pv "$symlink_dn"
-    fi
-    ln -fnsv "$source_path" "$symlink_path"
+    [[ "$symlink_dn" != "${HOME:?}" ]] && koopa::mkdir "$symlink_dn"
+    koopa::ln "$source_path" "$symlink_path"
     return 0
 }
 
