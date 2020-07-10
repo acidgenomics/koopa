bc35cf31493f8f7657c0f6878afebf7b30028211
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-10 09:35:22 -0400

    Wrap up scripts into functions

--- shell/bash/functions/path.sh
+++ shell/bash/functions/path.sh
@@ -1,29 +1,6 @@
 #!/usr/bin/env bash
 
-koopa::add_conda_env_to_path() { # {{{1
-    # """
-    # Add conda environment(s) to PATH.
-    # @note Updated 2020-06-30.
-    # """
-    # shellcheck disable=SC2039
-    local bin_dir name
-    koopa::assert_has_args "$#"
-    koopa::assert_is_installed conda
-    [[ -z "${CONDA_PREFIX:-}" ]] || return 1
-    for name in "$@"
-    do
-        bin_dir="${CONDA_PREFIX}/envs/${name}/bin"
-        if [[ ! -d "$bin_dir" ]]
-        then
-            koopa::warning "Conda environment missing: \"${bin_dir}\"."
-            return 1
-        fi
-        koopa::add_to_path_start "$bin_dir"
-    done
-    return 0
-}
-
-koopa::list_path_priority() { # {{{1
+koopa::_list_path_priority() { # {{{1
     # """
     # Split PATH string by ':' delim into lines.
     # @note Updated 2019-10-27.
@@ -57,7 +34,7 @@ koopa::list_path_priority() { # {{{1
     return 0
 }
 
-koopa::list_path_priority_unique() { # {{{1
+koopa::_list_path_priority_unique() { # {{{1
     # """
     # Split PATH string by ':' delim into lines but only return uniques.
     # @note Updated 2020-07-03.
@@ -65,7 +42,7 @@ koopa::list_path_priority_unique() { # {{{1
     local x
     koopa::assert_is_installed awk tac
     x="$( \
-        koopa::list_path_priority "$@" \
+        koopa::_list_path_priority "$@" \
             | tac \
             | awk '!a[$0]++' \
             | tac \
@@ -74,6 +51,52 @@ koopa::list_path_priority_unique() { # {{{1
     koopa::print "$x"
     return 0
 }
+
+koopa::add_conda_env_to_path() { # {{{1
+    # """
+    # Add conda environment(s) to PATH.
+    # @note Updated 2020-06-30.
+    # """
+    # shellcheck disable=SC2039
+    local bin_dir name
+    koopa::assert_has_args "$#"
+    koopa::assert_is_installed conda
+    [[ -z "${CONDA_PREFIX:-}" ]] || return 1
+    for name in "$@"
+    do
+        bin_dir="${CONDA_PREFIX}/envs/${name}/bin"
+        if [[ ! -d "$bin_dir" ]]
+        then
+            koopa::warning "Conda environment missing: \"${bin_dir}\"."
+            return 1
+        fi
+        koopa::add_to_path_start "$bin_dir"
+    done
+    return 0
+}
+
+koopa::list_path_priority() {
+    # """
+    # List path priority.
+    # @note Updated 2020-07-10.
+    # """
+    local all all_arr n_all n_dupes n_unique unique
+    koopa::assert_is_installed awk
+    all="$(koopa::_list_path_priority "$@")"
+    readarray -t all_arr <<< "$(koopa::print "$all")"
+    unique="$(koopa::print "$all" | awk '!a[$0]++')"
+    readarray -t unique_arr <<< "$(koopa::print "$unique")"
+    n_all="${#all_arr[@]}"
+    n_unique="${#unique_arr[@]}"
+    n_dupes="$((n_all-n_unique))"
+    if [[ "$n_dupes" -gt 0 ]]
+    then
+        koopa::note "${n_dupes} duplicate(s) detected."
+    fi
+    koopa::print "$all"
+    return 0
+}
+
 
 koopa::which() { # {{{1
     # """
