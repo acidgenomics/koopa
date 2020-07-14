a3ff3becb1bc66fd49510aeb96c0bb04cc1c36e8
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-14 08:34:23 -0400

    Improve scripts and conda environment creation

--- shell/bash/functions/conda.sh
+++ shell/bash/functions/conda.sh
@@ -2,8 +2,12 @@
 # shellcheck disable=SC2039
 
 koopa::conda_create_bioinfo_envs() {
-    local all aligners chipseq data_mining file_formats methylation \
-        quality_control rnaseq singlecell trimming variation workflows
+    # """
+    # Create Conda bioinformatics environments.
+    # @note Updated 2020-07-14.
+    # """
+    local all aligners chipseq data_mining envs file_formats methylation \
+        quality_control rnaseq trimming variation workflows
     koopa::assert_is_installed conda
     all=0
     aligners=0
@@ -13,18 +17,18 @@ koopa::conda_create_bioinfo_envs() {
     methylation=0
     quality_control=0
     rnaseq=0
-    singlecell=0
     trimming=0
     variation=0
     workflows=0
     # Set recommended defaults, if necessary.
     if [[ "$#" -eq 0 ]]
     then
+        aligners=1
         chipseq=1
         data_mining=1
         file_formats=1
-        quality_control=1
         rnaseq=1
+        workflows=1
     fi
     while (("$#"))
     do
@@ -61,10 +65,6 @@ koopa::conda_create_bioinfo_envs() {
                 rnaseq=1
                 shift 1
                 ;;
-            --singlecell|--single-cell|--scrnaseq)
-                singlecell=1
-                shift 1
-                ;;
             --trimming)
                 trimming=1
                 shift 1
@@ -91,128 +91,136 @@ koopa::conda_create_bioinfo_envs() {
         methylation=1
         quality_control=1
         rnaseq=1
-        singlecell=1
         trimming=1
         variation=1
         workflows=1
     fi
     koopa::h1 'Installing conda environments for bioinformatics.'
+    envs=()
     if [[ "$file_formats" -eq 1 ]]
     then
         koopa::h2 'File formats'
-        koopa::conda_create_env bamtools                                # 0.043G
-        koopa::conda_create_env bcftools
-        koopa::conda_create_env bedtools                                # 0.082G
-        koopa::conda_create_env bioawk                                  # 0.027G
-        koopa::conda_create_env gffutils                                # 0.222G
-        koopa::conda_create_env htslib                                  # 0.082G
-        koopa::conda_create_env sambamba
-        koopa::conda_create_env samblaster                              # 0.039G
-        koopa::conda_create_env samtools                                # 0.083G
-        koopa::conda_create_env seqtk                                   # 0.027G
+        envs+=(
+            "bamtools=2.5.1"
+            "bcftools=1.10.2"
+            "bedtools=2.29.2"
+            "bioawk=1.0"
+            "gffutils=0.10.1"
+            "htslib=1.10.2"
+            "sambamba=0.7.1"
+            "samblaster=0.1.26"
+            "samtools=1.10"
+            "seqtk=1.3"
+        )
         if koopa::is_linux
         then
-            koopa::conda_create_env biobambam                           # 0.065G
+            envs+=("biobambam=2.0.87")
         fi
     fi
     if [[ "$data_mining" -eq 1 ]]
     then
         koopa::h2 'Data mining'
-        koopa::conda_create_env entrez-direct                           # 0.154G
-        koopa::conda_create_env sra-tools                               # 0.315G
+        envs+=(
+            "entrez-direct=13.3"
+            "sra-tools=2.10.1"
+        )
     fi
     if [[ "$workflows" -eq 1 ]]
     then
         koopa::h2 'Workflows'
-        koopa::conda_create_env cromwell                                # 0.724G
-        koopa::conda_create_env fgbio                                   # 0.628G
-        koopa::conda_create_env gatk4                                   # 0.658G
-        koopa::conda_create_env jupyterlab                              # 0.410G
-        koopa::conda_create_env nextflow                                # 0.518G
-        koopa::conda_create_env snakemake                               # 0.788G
+        # Consider: cromwell
+        envs+=(
+            "fgbio=1.2.0"
+            "gat4k=4.1.8.0"
+            "jupyterlab=2.1.5"
+            "nextflow=20.04.1"
+            "picard=2.23.2"
+            "snakemake=5.20.1"
+        )
     fi
     if [[ "$quality_control" -eq 1 ]]
     then
         koopa::h2 'Quality control'
-        koopa::conda_create_env fastqc                                  # 0.513G
-        koopa::conda_create_env kraken                                  # 0.170G
-        koopa::conda_create_env multiqc                                 # 1.100G
-        koopa::conda_create_env picard                                  # 0.923G
-        koopa::conda_create_env qualimap                                # 1.200G
+        envs+=(
+            "fastqc=0.11.9"
+            "kraken=1.1.1"
+            "multiqc=1.9"
+            "qualimap=2.2.2d"
+        )
     fi
     if [[ "$trimming" -eq 1 ]]
     then
         koopa::h2 'Trimming'
-        koopa::conda_create_env atropos                                 # 0.152G
-        koopa::conda_create_env trimmomatic                             # 0.095G
+        envs+=(
+            "atropos=1.1.28"
+            "trimmomatic=0.39"
+        )
     fi
     if [[ "$aligners" -eq 1 ]]
     then
         koopa::h2 'Aligners'
-        koopa::conda_create_env bowtie2                                 # 0.319G
-        koopa::conda_create_env bwa                                     # 0.105G
-        koopa::conda_create_env hisat2                                  # 0.260G
-        koopa::conda_create_env minimap2                                # 0.045G
-        koopa::conda_create_env novoalign                               # 0.092G
-        koopa::conda_create_env rsem                                    # 0.744G
-        koopa::conda_create_env star                                    # 0.009G
+        # Consider: minimap2, novoalign
+        envs+=(
+            "bowtie2=2.4.1"
+            "bwa=0.7.17"
+            "hisat2=2.2.0"
+            "rsem=1.3.3"
+            "star=2.7.5a"
+        )
+        if koopa::is_linux
+        then
+            envs+=("bwa-mem2=2.0")
+        fi
     fi
     if [[ "$variation" -eq 1 ]]
     then
         koopa::h2 'Variation'
-        koopa::conda_create_env peddy                  # dna-seq        # 1.200G
-        koopa::conda_create_env ericscript             # rna-seq        # 0.728G
-        koopa::conda_create_env oncofuse               # rna-seq        # 0.507G
-        koopa::conda_create_env pizzly                 # rna-seq        # 0.293G
-        koopa::conda_create_env squid                  # rna-seq        # 0.041G
-        koopa::conda_create_env star-fusion            # rna-seq        # 1.600G
-        koopa::conda_create_env vardict                # rna-seq        # 0.396G
+        envs+=(
+            "ericscript=0.5.5"
+            "oncofuse=1.1.1"
+            "peddy=0.4.7"
+            "pizzly=0.37.3"
+            "squid=1.5"
+            "star-fusion=1.9.0"
+            "vardict=2019.06.04"
+        )
         if koopa::is_linux
         then
-            koopa::conda_create_env arriba             # rna-seq        # 0.987G
+            envs+=("arriba=1.2.0")
         fi
     fi
     if [[ "$rnaseq" -eq 1 ]]
     then
         koopa::h2 'RNA-seq'
-        # > koopa::conda_create_env r-bcbiornaseq                       # 2.200G
-        # > koopa::conda_create_env r-deseqanalysis                     # 0.437G
-        koopa::conda_create_env kallisto                                # 0.054G
-        koopa::conda_create_env rapmap                                  # 0.088G
-        koopa::conda_create_env salmon                                  # 0.192G
-        koopa::conda_create_env star                                    # 0.009G
+        # Consider: rapmap
+        envs+=(
+            "kallisto=0.46.2"
+            "salmon=1.3.0"
+        )
     fi
     if [[ "$chipseq" -eq 1 ]]
     then
         koopa::h2 'ChIP-seq'
-        koopa::conda_create_env bowtie2                                 # 0.319G
-        koopa::conda_create_env chromhmm                                # 0.428G
-        koopa::conda_create_env deeptools                               # 1.200G
-        koopa::conda_create_env genrich                                 # 0.000G
-        koopa::conda_create_env homer                                   # 0.263G
-        koopa::conda_create_env macs2                                   # 0.748G
-        koopa::conda_create_env sicer2                                  # 0.748G
-    fi
-    if [[ "$singlecell" -eq 1 ]]
-    then
-        koopa::h2 'Single-cell RNA-seq'
-        # > koopa::conda_create_env r-bcbiosinglecell                   # 0.438G
-        # > koopa::conda_create_env r-monocle3                          # 0.874G
-        # > koopa::conda_create_env r-seurat                            # 0.154G
-        # > koopa::conda_create_env stream                              # 0.998G
-        koopa::conda_create_env kallisto
-        koopa::conda_create_env salmon
+        envs+=(
+            "chromhmm=1.21"
+            "deeptools=3.4.3"
+            "genrich=0.6"
+            "homer=4.11"
+            "macs2=2.2.7.1"
+            "sicer2=1.0.2"
+        )
     fi
     if [[ "$methylation" -eq 1 ]]
     then
         koopa::h2 'Methylation'
-        koopa::conda_create_env bismark                                 # 0.406G
+        envs+=("bismark=0.22.3")
     fi
     if [[ "$all" -eq 1 ]]
     then
         koopa::h2 'Other tools'
-        koopa::conda_create_env igvtools                                # 0.477G
+        envs+=("igvtools=2.5.3")
     fi
+    koopa::conda_create_env "${envs[@]}"
     koopa::sys_set_permissions -r "$(koopa::conda_prefix)"
     conda env list
     return 0
@@ -221,12 +229,11 @@ koopa::conda_create_bioinfo_envs() {
 koopa::conda_create_env() { # {{{1
     # """
     # Create a conda environment.
-    # @note Updated 2020-06-29.
+    # @note Updated 2020-07-14.
     # """
-    local flags force env_name name pos prefix version
+    local conda_prefix force env env_name pos prefix
     koopa::assert_has_args "$#"
     force=0
-    version=
     pos=()
     while (("$#"))
     do
@@ -234,14 +241,6 @@ koopa::conda_create_env() { # {{{1
             --force)
                 force=1
                 shift 1
-                ;;
-            --version=*)
-                version="${1#*=}"
-                shift 1
-                ;;
-            --version)
-                version="$2"
-                shift 2
                 ;;
             --)
                 shift 1
@@ -257,39 +256,32 @@ koopa::conda_create_env() { # {{{1
         esac
     done
     [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
-    name="${1:?}"
-    if [[ -n "${version:-}" ]]
-    then
-        env_name="${name}@${version}"
-    else
-        env_name="$name"
-    fi
-    prefix="$(koopa::conda_prefix)/envs/${env_name}"
-    if [[ "$force" -eq 1 ]]
-    then
-        conda remove --name "$env_name" --all
-    fi
-    if [[ -d "$prefix" ]]
-    then
-        koopa::note "'${env_name}' is installed."
-        return 0
-    fi
-    koopa::info "Creating '${env_name}' conda environment."
+    koopa::assert_has_args "$#"
     koopa::activate_conda
     koopa::assert_is_installed conda
-    flags=(
-        "--name=${env_name}"
-        "--quiet"
-        "--yes"
-    )
-    if [[ -n "${version:-}" ]]
-    then
-        flags+=("${name}=${version}")
-    else
-        flags+=("$name")
-    fi
-    conda create "${flags[@]}"
-    koopa::sys_set_permissions -r "$prefix"
+    conda_prefix="$(koopa::conda_prefix)"
+    for env in "$@"
+    do
+        if ! koopa::str_match "$env" '='
+        then
+            koopa::stop 'Version is required. Specify as "NAME=VERSION".'
+        fi
+        env_name="${env//=/@}"
+        prefix="${conda_prefix}/envs/${env_name}"
+        if [[ -d "$prefix" ]]
+        then
+            if [[ "$force" -eq 1 ]]
+            then
+                conda remove --name "$env_name" --all
+            else
+                koopa::note "Conda environment \"${env_name}\" exists."
+                continue
+            fi
+        fi
+        koopa::info "Creating \"${env_name}\" conda environment."
+        conda create --name="$env_name" --quiet --yes "$env"
+        koopa::sys_set_permissions -r "$prefix"
+    done
     return 0
 }
 
