fb009e2f37bf0f4cc118748faa4492a019d4e4ea
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-14 11:06:57 -0400

    Rework OS-specific functions

--- os/ubuntu/include/header.sh
+++ os/ubuntu/include/header.sh
@@ -3,5 +3,7 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../debian/include/header.sh"
+# shellcheck source=/dev/null
+source "${script_dir}/../../../shell/bash/functions/os/ubuntu.sh"
 
 koopa::assert_is_ubuntu
