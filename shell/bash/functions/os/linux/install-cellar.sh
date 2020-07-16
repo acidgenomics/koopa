7f1c8c26c1dab019c311a5e601edfc2166ddb4d7
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 14:42:01 -0400

    Reorganize functions

--- shell/bash/functions/os/linux/install-cellar.sh
+++ shell/bash/functions/os/linux/install-cellar.sh
@@ -10,6 +10,41 @@ koopa::install_autojump() {
     return 0
 }
 
+koopa::install_automake() {
+    koopa::install_cellar --name="automake" "$@"
+    return 0
+}
+
+koopa::install_aws_cli() {
+    koopa::install_cellar \
+        --name="aws-cli" \
+        --name-fancy="AWS CLI" \
+        --version="latest" \
+        --include-dirs="bin" \
+        "$@"
+    return 0
+}
+
+koopa::install_bash() {
+    koopa::install_cellar --name="bash" --name-fancy="Bash" "$@"
+    return 0
+}
+
+koopa::install_binutils() {
+    koopa::install_cellar --name="binutils" "$@"
+    return 0
+}
+
+koopa::install_cmake() {
+    koopa::install_cellar --name="cmake" --name-fancy="CMake" "$@"
+    return 0
+}
+
+koopa::install_coreutils() {
+    koopa::install_cellar --name="coreutils" "$@"
+    return 0
+}
+
 koopa::install_singularity() {
     koopa::install_cellar --name='singularity' "$@"
     return 0
