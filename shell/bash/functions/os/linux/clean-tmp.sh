8eff38c3c369188531f4051cc24e7455884c8c95
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 14:16:58 -0400

    Rework Linux functions

---
+++ shell/bash/functions/os/linux/clean-tmp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+koopa::linux_clean_tmp() {
+    local dir dirs
+    koopa::assert_has_no_args "$#"
+    dirs=('/tmp')
+    [[ "${TMPDIR:-}" != '/tmp' ]] && dirs+=("$TMPDIR")
+    for dir in "${dirs[@]}"
+    do
+        sudo find "$dir" \
+            -mindepth 1 \
+            -maxdepth 1 \
+            -ctime +30 \
+            -exec rm -frv {} \;
+    done
+    return 0
+}
+
