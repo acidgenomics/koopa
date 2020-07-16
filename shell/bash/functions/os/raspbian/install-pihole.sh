50f9810b5132543348f2f5106f51991cedeeb38f
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 14:08:17 -0400

    Reorganize functions

--- shell/bash/functions/os/raspbian/install-pihole.sh
+++ shell/bash/functions/os/raspbian/install-pihole.sh
@@ -26,27 +26,3 @@ koopa::install_pihole() { # {{{1
     return 0
 }
 
-koopa::install_pivpn() { # {{{1
-    # """
-    # Install PiVPN.
-    # @note Updated 2020-07-14.
-    # @seealso
-    # - https://www.pivpn.io
-    # """
-    local file name_fancy tmp_dir url
-    koopa::assert_has_no_args "$#"
-    name_fancy='PiVPN'
-    koopa::install_start "$name_fancy"
-    tmp_dir="$(koopa::tmp_dir)"
-    (
-        koopa::cd "$tmp_dir"
-        file='pivpn.sh'
-        url='https://install.pivpn.io'
-        chmod +x "$file"
-        "$file"
-    )
-    koopa::rm "$tmp_dir"
-    koopa::install_success "$name_fancy"
-    return 0
-}
-
