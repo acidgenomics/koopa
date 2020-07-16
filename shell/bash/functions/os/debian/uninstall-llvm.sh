f72e3982e693613680fcfa9a06abfd5c1d2546db
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 14:02:05 -0400

    Split out functions

---
+++ shell/bash/functions/os/debian/uninstall-llvm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+koopa::debian_uninstall_llvm() {
+    # """
+    # Uninstall LLVM.
+    # @note Updated 2020-07-16.
+    # """
+    name_fancy='LLVM'
+    koopa::uninstall_start "$name_fancy"
+    koopa::assert_has_no_args "$#"
+    koopa::assert_has_no_envs
+    unset -v LLVM_CONFIG
+    sudo apt-get --yes remove '^clang-[0-9]+.*' '^llvm-[0-9]+.*'
+    sudo apt-get --yes autoremove
+    koopa::rm -S '/etc/apt/sources.list.d/llvm.list'
+    koopa::uninstall_success "$name_fancy"
+    return 0
+}
