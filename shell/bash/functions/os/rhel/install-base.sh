cc8df8d5e9bfa5c76bd8543bb1708d7ad4fb9fe5
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 14:08:17 -0400

    Reorganize functions

--- shell/bash/functions/os/rhel/install-base.sh
+++ shell/bash/functions/os/rhel/install-base.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-koopa::rhel_enable_epel() { # {{{1
-    koopa::assert_has_no_args "$#"
-    if sudo dnf repolist | grep -q 'epel/'
-    then
-        koopa::success 'EPEL is already enabled.'
-        return 0
-    fi
-    sudo dnf install -y \
-        'https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm'
-    return 0
-}
-
 koopa::rhel_install_base() { # {{{1
     # """
     # Install Red Hat Enterprise Linux (RHEL) base system.
