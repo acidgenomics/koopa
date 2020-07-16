17706f61225240d316ab4ea297540483673554cf
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 07:56:48 -0400

    Rework exported macos programs

--- shell/bash/functions/os/macos.sh
+++ shell/bash/functions/os/macos.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+koopa::macos_create_dmg() {
+    # """
+    # Create DMG image.
+    # @note Updated 2020-07-15.
+    # """
+    local dir name
+    koopa::assert_has_args_eq "$#" 1
+    koopa::assert_is_installed hdiutil
+    dir="${1:?}"
+    koopa::assert_is_dir "$dir"
+    dir="$(realpath "$dir")"
+    name="$(basename "$dir")"
+    hdiutil create -volname "$name" -srcfolder "$dir" -ov "${name}.dmg"
+    return 0
+}
+
 koopa::macos_clean_launch_services() { # {{{1
     koopa::assert_has_no_args "$#"
     koopa::h1 'Cleaning LaunchServices "Open With" menu.'
