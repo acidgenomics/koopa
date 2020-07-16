5573c64d33ec9ddeb3dad3062cced4de3d33f9da
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 11:35:31 -0400

    Simplify header

--- os/linux/include/header.sh
+++ os/linux/include/header.sh
@@ -3,4 +3,7 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 # shellcheck source=/dev/null
 source "${script_dir}/../../../shell/bash/include/header.sh"
+# shellcheck source=/dev/null
+source "${script_dir}/../../../shell/bash/functions/os/linux.sh"
+
 koopa::assert_is_linux
