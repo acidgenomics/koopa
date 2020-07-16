e968895dca58ca05bad52c3e8f037bb11cb2be10
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-16 13:41:44 -0400

    Reorganize scripts and improve headers

--- os/opensuse/include/header.sh
+++ os/opensuse/include/header.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
-# shellcheck source=/dev/null
-source "${script_dir}/../../linux/include/header.sh"
-# shellcheck source=/dev/null
-source "${script_dir}/../../../shell/bash/functions/os/opensuse.sh"
+koopa::opensuse_header() {
+    # """
+    # openSUSE Linux header.
+    # @note Updated 2020-07-16.
+    # """
+    local file koopa_prefix
+    koopa_prefix="$( \
+        cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
+        &>/dev/null \
+        && pwd -P \
+    )"
+    # shellcheck source=/dev/null
+    source "${koopa_prefix}/os/linux/include/header.sh"
+    for file in "${koopa_prefix}/shell/bash/functions/os/opensuse/"*'.sh'
+    do
+        # shellcheck source=/dev/null
+        [[ -f "$file" ]] && source "$file"
+    done
+    koopa::assert_is_opensuse
+    return 0
+}
 
-koopa::assert_is_opensuse
+koopa::opensuse_header "$@"
