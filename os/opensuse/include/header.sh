444e0d45b26987d450fe62913f05266dfa27c422
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-17 17:30:34 -0400

    Dont wrap headers in functions

--- os/opensuse/include/header.sh
+++ os/opensuse/include/header.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-koopa::opensuse_header() {
-    # """
-    # openSUSE Linux header.
-    # @note Updated 2020-07-16.
-    # """
-    local file koopa_prefix
-    koopa_prefix="$( \
-        cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
-        &>/dev/null \
-        && pwd -P \
-    )"
-    # shellcheck source=/dev/null
-    source "${koopa_prefix}/os/linux/include/header.sh"
-    for file in "${koopa_prefix}/shell/bash/functions/os/opensuse/"*'.sh'
-    do
-        # shellcheck source=/dev/null
-        [[ -f "$file" ]] && source "$file"
-    done
-    koopa::assert_is_opensuse
-    return 0
-}
+# """
+# openSUSE Linux header.
+# @note Updated 2020-07-17.
+# """
 
-koopa::opensuse_header "$@"
+koopa_prefix="$( \
+    cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
+    &>/dev/null \
+    && pwd -P \
+)"
+# shellcheck source=/dev/null
+source "${koopa_prefix}/os/linux/include/header.sh"
+for file in "${koopa_prefix}/shell/bash/functions/os/opensuse/"*'.sh'
+do
+    # shellcheck source=/dev/null
+    [[ -f "$file" ]] && source "$file"
+done
+unset -v file koopa_prefix
+koopa::assert_is_opensuse
--- os/raspbian/include/header.sh
+++ os/raspbian/include/header.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-koopa::raspbian_header() {
-    # """
-    # Raspbian Linux header.
-    # @note Updated 2020-07-16.
-    # """
-    local file koopa_prefix
-    koopa_prefix="$( \
-        cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
-        &>/dev/null \
-        && pwd -P \
-    )"
-    # shellcheck source=/dev/null
-    source "${koopa_prefix}/os/debian/include/header.sh"
-    for file in "${koopa_prefix}/shell/bash/functions/os/raspbian/"*'.sh'
-    do
-        # shellcheck source=/dev/null
-        [[ -f "$file" ]] && source "$file"
-    done
-    koopa::assert_is_raspbian
-    return 0
-}
+# """
+# Raspbian Linux header.
+# @note Updated 2020-07-17.
+# """
 
-koopa::raspbian_header "$@"
+koopa_prefix="$( \
+    cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
+    &>/dev/null \
+    && pwd -P \
+)"
+# shellcheck source=/dev/null
+source "${koopa_prefix}/os/debian/include/header.sh"
+for file in "${koopa_prefix}/shell/bash/functions/os/raspbian/"*'.sh'
+do
+    # shellcheck source=/dev/null
+    [[ -f "$file" ]] && source "$file"
+done
+unset -v file koopa_prefix
+koopa::assert_is_raspbian
