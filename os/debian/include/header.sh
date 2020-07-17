a53d9c24d532fe302039253e43a0d184deac1887
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-17 17:30:34 -0400

    Dont wrap headers in functions

--- os/debian/include/header.sh
+++ os/debian/include/header.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-koopa::debian_header() {
-    # """
-    # Debian Linux header.
-    # @note Updated 2020-07-16.
-    # """
-    local file koopa_prefix
-    koopa_prefix="$( \
-        cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
-        &>/dev/null \
-        && pwd -P \
-    )"
-    # shellcheck source=/dev/null
-    source "${koopa_prefix}/os/linux/include/header.sh"
-    for file in "${koopa_prefix}/shell/bash/functions/os/debian/"*'.sh'
-    do
-        # shellcheck source=/dev/null
-        [[ -f "$file" ]] && source "$file"
-    done
-    koopa::assert_is_debian
-    return 0
-}
+# """
+# Debian Linux header.
+# @note Updated 2020-07-17.
+# """
 
-koopa::debian_header "$@"
+koopa_prefix="$( \
+    cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
+    &>/dev/null \
+    && pwd -P \
+)"
+# shellcheck source=/dev/null
+source "${koopa_prefix}/os/linux/include/header.sh"
+for file in "${koopa_prefix}/shell/bash/functions/os/debian/"*'.sh'
+do
+    # shellcheck source=/dev/null
+    [[ -f "$file" ]] && source "$file"
+done
+unset -v file koopa_prefix
+koopa::assert_is_debian
--- shell/bash/include/header.sh
+++ shell/bash/include/header.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# """
+# Bash header.
+# @note Updated 2020-07-17.
+# """
+
 if [[ "$#" -gt 0 ]]
 then
     pos=()
@@ -29,85 +34,88 @@ then
         esac
     done
     [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    unset -v pos
 fi
 
-koopa::bash_header() { # {{{1
-    # """
-    # Bash header.
-    # @note Updated 2020-07-16.
-    # """
-    local file major_version pos
-    [[ -n "${KOOPA_VERBOSE:-}" ]] && local verbose=1
-    [[ -z "${activate:-}" ]] && local activate=0
-    [[ -z "${checks:-}" ]] && local checks=1
-    [[ -z "${shopts:-}" ]] && local shopts=1
-    [[ -z "${verbose:-}" ]] && local verbose=0
-    if [[ "$activate" -eq 1 ]]
-    then
-        checks=0
-        shopts=0
-    fi
-    # Customize optional shell behavior.
-    # These are not recommended to be set during koopa activation.
-    if [[ "$shopts" -eq 1 ]]
-    then
-        [[ "$verbose" -eq 1 ]] && set -o xtrace                             # -x
-        # > set -o noglob                                                   # -f
-        set -o errexit                                                      # -e
-        set -o errtrace                                                     # -E
-        set -o nounset                                                      # -u
-        set -o pipefail
-    fi
-    # Requiring Bash >= 4. macOS ships with an ancient version of Bash, due to
-    # licensing. If we're performing a clean install and loading up Homebrew,
-    # this step will fail unless we skip checks.
-    if [[ "$checks" -eq 1 ]]
-    then
-        major_version="$(printf '%s\n' "${BASH_VERSION}" | cut -d '.' -f 1)"
-        if [[ ! "$major_version" -ge 4 ]]
-        then
-            printf '%s\n' 'ERROR: Bash >= 4 is required.' >&2
-            printf '%s: %s\n' 'BASH_VERSION' "$BASH_VERSION" >&2
-            exit 1
-        fi
-        # Check that user's Bash has readarray (mapfile) builtin defined.
-        # We use this a lot to handle arrays.
-        if [[ $(type -t readarray) != 'builtin' ]]
-        then
-            printf '%s\n' 'ERROR: Bash is missing readarray (mapfile).' >&2
-            exit 1
-        fi
-    fi
-    # Ensure koopa prefix is exported, if necessary.
-    if [[ -z "${KOOPA_PREFIX:-}" ]]
-    then
-        KOOPA_PREFIX="$( \
-            cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
-            &>/dev/null \
-            && pwd -P \
-        )"
-        export KOOPA_PREFIX
-    fi
-    # Source POSIX header (which includes functions).
-    # shellcheck source=/dev/null
-    source "${KOOPA_PREFIX}/shell/posix/include/header.sh"
-    # Source Bash functions.
-    for file in "${KOOPA_PREFIX}/shell/bash/functions/"*'.sh'
-    do
-        # shellcheck source=/dev/null
-        [[ -f "$file" ]] && source "$file"
-    done
-    koopa::help "$@"
-    # Require sudo permission to run 'sbin/' scripts.
-    if [[ "$checks" -eq 1 ]]
-    then
-        koopa::str_match "$0" '/sbin' && koopa::assert_has_sudo
-    fi
-    # Disable user-defined aliases.
-    # Primarily intended to reset cp, mv, rf for use inside scripts.
-    [[ "$activate" -eq 0 ]] && unalias -a
-    unset -v activate checks shopts verbose
-    return 0
-}
+[[ -n "${KOOPA_VERBOSE:-}" ]] && verbose=1
+[[ -z "${activate:-}" ]] && activate=0
+[[ -z "${checks:-}" ]] && checks=1
+[[ -z "${shopts:-}" ]] && shopts=1
+[[ -z "${verbose:-}" ]] && verbose=0
 
-koopa::bash_header "$@"
+if [[ "$activate" -eq 1 ]]
+then
+    checks=0
+    shopts=0
+fi
+
+# Customize optional shell behavior.
+# These are not recommended to be set during koopa activation.
+if [[ "$shopts" -eq 1 ]]
+then
+    [[ "$verbose" -eq 1 ]] && set -o xtrace                                 # -x
+    # > set -o noglob                                                       # -f
+    set -o errexit                                                          # -e
+    set -o errtrace                                                         # -E
+    set -o nounset                                                          # -u
+    set -o pipefail
+fi
+
+# Requiring Bash >= 4. macOS ships with an ancient version of Bash, due to
+# licensing. If we're performing a clean install and loading up Homebrew,
+# this step will fail unless we skip checks.
+if [[ "$checks" -eq 1 ]]
+then
+    major_version="$(printf '%s\n' "${BASH_VERSION}" | cut -d '.' -f 1)"
+    if [[ ! "$major_version" -ge 4 ]]
+    then
+        printf '%s\n' 'ERROR: Bash >= 4 is required.' >&2
+        printf '%s: %s\n' 'BASH_VERSION' "$BASH_VERSION" >&2
+        exit 1
+    fi
+    # Check that user's Bash has readarray (mapfile) builtin defined.
+    # We use this a lot to handle arrays.
+    if [[ $(type -t readarray) != 'builtin' ]]
+    then
+        printf '%s\n' 'ERROR: Bash is missing readarray (mapfile).' >&2
+        exit 1
+    fi
+    unset -v major_version
+fi
+
+# Ensure koopa prefix is exported, if necessary.
+if [[ -z "${KOOPA_PREFIX:-}" ]]
+then
+    KOOPA_PREFIX="$( \
+        cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
+        &>/dev/null \
+        && pwd -P \
+    )"
+    export KOOPA_PREFIX
+fi
+
+# Source POSIX header (which includes functions).
+# shellcheck source=/dev/null
+source "${KOOPA_PREFIX}/shell/posix/include/header.sh"
+
+# Source Bash functions.
+for file in "${KOOPA_PREFIX}/shell/bash/functions/"*'.sh'
+do
+    # shellcheck source=/dev/null
+    [[ -f "$file" ]] && source "$file"
+done
+unset -v file
+
+koopa::help "$@"
+
+# Require sudo permission to run 'sbin/' scripts.
+if [[ "$checks" -eq 1 ]]
+then
+    koopa::str_match "$0" '/sbin' && koopa::assert_has_sudo
+fi
+
+# Disable user-defined aliases.
+# Primarily intended to reset cp, mv, rf for use inside scripts.
+[[ "$activate" -eq 0 ]] && unalias -a
+
+unset -v activate checks shopts verbose
