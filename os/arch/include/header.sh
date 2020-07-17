50b4a838532fc781b287acfa68ae04df6d07315a
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-17 17:30:34 -0400

    Dont wrap headers in functions

--- os/arch/include/header.sh
+++ os/arch/include/header.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-koopa::arch_header() {
-    # """
-    # Arch Linux header.
-    # @note Updated 2020-07-16.
-    # """
-    local file koopa_prefix
-    koopa_prefix="$( \
-        cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
-        &>/dev/null \
-        && pwd -P \
-    )"
-    # shellcheck source=/dev/null
-    source "${koopa_prefix}/os/linux/include/header.sh"
-    for file in "${koopa_prefix}/shell/bash/functions/os/arch/"*'.sh'
-    do
-        # shellcheck source=/dev/null
-        [[ -f "$file" ]] && source "$file"
-    done
-    koopa::assert_is_arch
-    return 0
-}
+# """
+# Arch Linux header.
+# @note Updated 2020-07-17.
+# """
 
-koopa::arch_header "$@"
+koopa_prefix="$( \
+    cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
+    &>/dev/null \
+    && pwd -P \
+)"
+# shellcheck source=/dev/null
+source "${koopa_prefix}/os/linux/include/header.sh"
+for file in "${koopa_prefix}/shell/bash/functions/os/arch/"*'.sh'
+do
+    # shellcheck source=/dev/null
+    [[ -f "$file" ]] && source "$file"
+done
+unset -v file koopa_prefix
+koopa::assert_is_arch
--- os/darwin/include/header.sh
+++ os/darwin/include/header.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-koopa::macos_header() {
-    # """
-    # macOS header.
-    # @note Updated 2020-07-16.
-    # """
-    local file koopa_prefix
-    koopa_prefix="$( \
-        cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
-        &>/dev/null \
-        && pwd -P \
-    )"
-    # shellcheck source=/dev/null
-    source "${koopa_prefix}/shell/bash/include/header.sh"
-    for file in "${koopa_prefix}/shell/bash/functions/os/macos/"*'.sh'
-    do
-        # shellcheck source=/dev/null
-        [[ -f "$file" ]] && source "$file"
-    done
-    koopa::assert_is_macos
-    return 0
-}
+# """
+# macOS header.
+# @note Updated 2020-07-17.
+# """
 
-koopa::macos_header "$@"
+koopa_prefix="$( \
+    cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../.." \
+    &>/dev/null \
+    && pwd -P \
+)"
+# shellcheck source=/dev/null
+source "${koopa_prefix}/shell/bash/include/header.sh"
+for file in "${koopa_prefix}/shell/bash/functions/os/macos/"*'.sh'
+do
+    # shellcheck source=/dev/null
+    [[ -f "$file" ]] && source "$file"
+done
+unset -v file koopa_prefix
+koopa::assert_is_macos
