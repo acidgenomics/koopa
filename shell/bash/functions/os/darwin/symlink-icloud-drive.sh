143276072d707ba2cba91c879fdc77959cbd100b
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-17 07:28:16 -0400

    Reorganize macOS functions

--- shell/bash/functions/os/darwin/symlink-icloud-drive.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-koopa::macos_symlink_icloud_drive() {
-    koopa::assert_has_no_args "$#"
-    koopa::ln \
-        "${HOME}/Library/Mobile Documents/com~apple~CloudDocs" \
-        "${HOME}/icloud"
-    return 0
-}
