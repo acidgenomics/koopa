4a4de42f9bca45338f014db7978c0ca007120231
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-17 07:28:16 -0400

    Reorganize macOS functions

--- shell/bash/functions/os/darwin/cache.sh
+++ shell/bash/functions/os/darwin/cache.sh
@@ -15,3 +15,15 @@ LaunchServices.framework/Support/lsregister" \
     return 0
 }
 
+koopa::macos_flush_dns() { # {{{1
+    # """
+    # Flush DNS cache.
+    # @note Updated 2020-07-17.
+    # """
+    koopa::assert_has_no_args "$#"
+    koopa::h1 'Flushing DNS.'
+    sudo dscacheutil -flushcache
+    sudo killall -HUP mDNSResponder
+    koopa::success 'DNS flush was successful.'
+    return 0
+}
