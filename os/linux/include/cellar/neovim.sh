81bfa507972dcc65c7cdbeb2d2c8a9c733470e27
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-20 19:08:25 -0400

    linter fixes

--- os/linux/include/cellar/neovim.sh
+++ os/linux/include/cellar/neovim.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
-# """
-# Currently failing to build on CentOS.
-#
-# make[5]: Leaving directory '/tmp/XXX/neovim-0.4.3/.deps/build/src/libuv-build'
-# make[4]: Leaving directory '/tmp/XXX/neovim-0.4.3/.deps/build/src/libuv-build'
-# [ 62%] Completed 'libuv'
-# make[3]: Leaving directory '/tmp/XXX/neovim-0.4.3/.deps'
-# [ 62%] Built target libuv
-# make[2]: Leaving directory '/tmp/XXX/neovim-0.4.3/.deps'
-# make[1]: *** [Makefile:84: all] Error 2
-# make[1]: Leaving directory '/tmp/XXX/neovim-0.4.3/.deps'
-# make: *** [Makefile:101: deps] Error 2
-# """
-
 # Skip building on CentOS.
 if koopa::is_centos
 then
