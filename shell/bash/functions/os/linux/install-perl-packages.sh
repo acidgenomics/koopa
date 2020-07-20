c5617646ac67396e57dcb3e58da286571f887199
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-20 09:02:09 -0400

    Draft update to Perl package installer

--- shell/bash/functions/os/linux/install-perl-packages.sh
+++ shell/bash/functions/os/linux/install-perl-packages.sh
@@ -3,11 +3,11 @@
 koopa::install_perl_packages() {
     # """
     # Install Perl packages.
-    # @note Updated 2020-07-02.
+    # @note Updated 2020-07-20.
     # """
     local module modules name_fancy
     koopa::assert_is_installed cpan perl
-    name_fancy="Perl packages"
+    name_fancy='Perl packages'
     koopa::install_start "$name_fancy"
     export PERL_MM_USE_DEFAULT=1
     if ! koopa::is_installed cpanm
@@ -20,14 +20,16 @@ koopa::install_perl_packages() {
         modules=("$@")
     else
         modules=(
-            "App::Ack"
-            "File::Rename"
+            'App::Ack'
+            'File::Rename'
         )
     fi
     for module in "${modules[@]}"
     do
+        echo 'FIXME 1'
         koopa::info "${module}"
         cpanm "$module" &>/dev/null
+        echo 'FIXME 2'
     done
     koopa::install_success "$name_fancy"
     return 0
