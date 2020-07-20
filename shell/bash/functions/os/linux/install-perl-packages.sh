1bdf92bf7d4ddf4423ba7b00e06d28fb68f4601d
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-20 09:09:11 -0400

    Improve Perl package installer

--- shell/bash/functions/os/linux/install-perl-packages.sh
+++ shell/bash/functions/os/linux/install-perl-packages.sh
@@ -4,6 +4,10 @@ koopa::install_perl_packages() {
     # """
     # Install Perl packages.
     # @note Updated 2020-07-20.
+    #
+    # CPAN Minus (cpanm) mirror options:
+    # * --mirror http://cpan.cpantesters.org/  # use the fast-syncing mirror
+    # * --from https://cpan.metacpan.org/      # use only the HTTPS mirror
     # """
     local cellar module modules name_fancy
     koopa::assert_is_installed cpan perl
@@ -18,8 +22,8 @@ koopa::install_perl_packages() {
     export PERL_MM_USE_DEFAULT=1
     if ! koopa::is_installed cpanm
     then
-        koopa::info "CPAN Minus"
-        cpan -i "App::cpanminus" &>/dev/null
+        koopa::info 'CPAN Minus'
+        cpan -i 'App::cpanminus' &>/dev/null
     fi
     [[ "$cellar" -eq 1 ]] && koopa::link_cellar perl
     koopa::assert_is_installed cpanm
@@ -34,10 +38,8 @@ koopa::install_perl_packages() {
     fi
     for module in "${modules[@]}"
     do
-        echo 'FIXME 1'
         koopa::info "${module}"
         cpanm "$module" &>/dev/null
-        echo 'FIXME 2'
     done
     [[ "$cellar" -eq 1 ]] && koopa::link_cellar perl
     koopa::install_success "$name_fancy"
