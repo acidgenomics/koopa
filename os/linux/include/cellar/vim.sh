087dc9290dcf4ed81a82bbaae2d8f41b74d68058
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-20 21:13:33 -0400

    Rework python3 handling

--- os/linux/include/cellar/vim.sh
+++ os/linux/include/cellar/vim.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
+# """
 # Installing with Python 3 configuration.
-koopa::assert_is_installed python3
-make_prefix="$(koopa::make_prefix)"
-python3_exe="${make_prefix}/bin/python3"
-koopa::assert_is_file "$python3_exe"
-python3_config_exe="${python3_exe}-config"
-koopa::assert_is_file "$python3_config_exe"
-python3_config_dir="$("$python3_config_exe" --configdir)"
+# """
 
+make_prefix="$(koopa::make_prefix)"
+python3="${make_prefix}/bin/python3"
+python3_config="${python3}-config"
+koopa::assert_is_installed "$python3" "$python3_config"
+python3_config_dir="$("$python3_config" --configdir)"
 file="v${version}.tar.gz"
 url="https://github.com/${name}/${name}/archive/${file}"
 koopa::download "$url"
@@ -18,7 +18,7 @@ koopa::cd "${name}-${version}"
 ./configure \
     --prefix="$prefix" \
     --enable-python3interp='yes' \
-    --with-python3-command="${python3_exe}" \
+    --with-python3-command="$python3" \
     --with-python3-config-dir="${python3_config_dir}" \
     LDFLAGS="-Wl,--rpath=${make_prefix}/lib"
 make --jobs="$jobs"
