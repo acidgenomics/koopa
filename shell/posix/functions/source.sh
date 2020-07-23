b555a83c22e0709e173c90b8f20c0d24bd4efbde
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-23 09:46:28 -0400

    Pass linter checks

--- shell/posix/functions/source.sh
+++ shell/posix/functions/source.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+_koopa_exec_dir() {
+    # """
+    # Execute multiple shell scripts in a directory.
+    # @note Updated 2020-07-23.
+    # """
+    # shellcheck disable=SC2039
+    local dir file
+    dir="${1:?}"
+    [ -d "$dir" ] || return 0
+    for file in "${dir}/"*'.sh'
+    do
+        [ -x "$file" ] || continue
+        # shellcheck source=/dev/null
+        "$file"
+    done
+    return 0
+}
+
 _koopa_source_dir() {
     # """
     # Source multiple shell scripts in a directory.
