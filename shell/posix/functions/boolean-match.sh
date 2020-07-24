772c38b6d49858012332bc2b5c250858a58820e8
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-24 05:56:37 -0400

    Simplify internal string matching code

--- shell/posix/functions/boolean-match.sh
+++ shell/posix/functions/boolean-match.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# FIXME REWORK AND SUPPORT INTERNAL FLAG.
-# FIXME CONSOLIDATE CODE WITH _koopa_str_match_regex
-_koopa_str_match() { # {{{1
+__koopa_str_match() { # {{{1
     # """
-    # Does the input match a fixed string?
-    # @note Updated 2020-05-05.
+    # Does the input match a string?
+    # @note Updated 2020-07-24.
+    #
+    # Modes:
+    # * -E, --extended-regexp
+    # * -F, --fixed strings
+    # * -G, --basic-regexp
+    # * -P, --perl-regexp
+    #
+    # Perl-compatible regular expressions (PCREs) support negative
+    # lookaheads, which are often very useful. However, Perl regex isn't enabled
+    # by default for all installations of grep.
     #
     # Usage of '-q' flag can cause an exit trap in 'set -e' mode.
     # Redirecting output to '/dev/null' works more reliably.
@@ -19,7 +27,26 @@ _koopa_str_match() { # {{{1
     # echo STRING | koopa_str_match PATTERN
     # """
     # shellcheck disable=SC2039
-    local string pattern
+    local OPTIND flag pattern string
+    OPTIND=1
+    while getopts 'EFP' opt
+    do
+        case "$opt" in
+            E)
+                flag='-E'
+                ;;
+            F)
+                flag='-F'
+                ;;
+            P)
+                flag='-P'
+                ;;
+            \?)
+                koopa::invalid_arg
+                ;;
+        esac
+    done
+    shift "$((OPTIND-1))"
     if [ "$#" -eq 2 ]
     then
         # Standard input.
@@ -37,7 +64,27 @@ _koopa_str_match() { # {{{1
     else
         return 1
     fi
-    echo "$string" | grep -Fq "$pattern" >/dev/null
+    echo "$string" | grep "$flag" -q "$pattern" >/dev/null
+}
+
+_koopa_str_match() { #{{{1
+    _koopa_str_match_fixed "$@"
+}
+
+_koopa_str_match_fixed() { # {{{1
+    # """
+    # Does the input match a fixed string?
+    # @note Updated 2020-05-05.
+    # """
+    __koopa_str_match -F "$@"
+}
+
+_koopa_str_match_perl() { # {{{
+    # """
+    # Does the input match a Perl-compatible regulare expression (PCRE)?
+    # @note Updated 2020-07-24.
+    # """
+    __koopa_str_match -P "$@"
 }
 
 _koopa_str_match_posix() { # {{{1
@@ -52,39 +99,10 @@ _koopa_str_match_posix() { # {{{1
     test "${1#*$2}" != "$1"
 }
 
-# FIXME CONSOLIDATE WITH STR_MATCH_FIXED
 _koopa_str_match_regex() { # {{{1
     # """
     # Does the input match a regular expression?
-    # @note Updated 2020-07-20.
-    #
-    # Regex modes:
-    # * -E, --extended-regexp
-    # * -G, --basic-regexp
-    # * -P, --perl-regexp
-    #
-    # Perl-compatible regular expressions (PCREs) support negative
-    # lookaheads, which are often very useful. However, Perl regex isn't enabled
-    # by default for all installations of grep.
+    # @note Updated 2020-07-24.
     # """
-    # shellcheck disable=SC2039
-    local string pattern
-    if [ "$#" -eq 2 ]
-    then
-        # Standard input.
-        # Note that we're allowing empty string input here.
-        string="${1:-}"
-        pattern="${2:?}"
-    elif [ "$#" -eq 1 ]
-    then
-        # Piped input using stdin.
-        pattern="${1:?}"
-        shift 1
-        # Handle string with line breaks '\n'.
-        # shellcheck disable=SC2039
-        read -r -d '' string
-    else
-        return 1
-    fi
-    echo "$string" | grep -Pq "$pattern" >/dev/null
+    __koopa_str_match -E "$@"
 }
