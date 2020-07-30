741293ecc3d36375c47a049c42b4dfc1d12fa735
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-30 16:33:25 -0400

    Attempt to resolve subversion on Fedora

--- os/linux/include/cellar/subversion.sh
+++ os/linux/include/cellar/subversion.sh
@@ -18,6 +18,7 @@ if koopa::is_fedora
 then
     koopa::ln -S '/usr/bin/apr-1-config' '/usr/bin/apr-config'
     koopa::ln -S '/usr/bin/apu-1-config' '/usr/bin/apu-config'
+    koopa::force_add_to_pkg_config_path_start '/usr/lib64/pkgconfig'
 fi
 koopa::assert_is_installed apr-config apu-config sqlite3
 file="${name}-${version}.tar.bz2"
