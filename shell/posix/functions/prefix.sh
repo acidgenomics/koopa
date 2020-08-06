a98d758290ea152cd5460ad8d7a753dd78ea6ebd
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-06 15:51:57 -0400

    Improve site packages detection

--- shell/posix/functions/prefix.sh
+++ shell/posix/functions/prefix.sh
@@ -421,7 +421,8 @@ _koopa_python_site_packages_prefix() { # {{{1
     # """
     # shellcheck disable=SC2039
     local make_prefix python version x
-    python="$(_koopa_python)"
+    python="${1:-}"
+    [ -z "$python" ] && python="$(_koopa_python)"
     _koopa_is_installed "$python" || return 0
     make_prefix="$(_koopa_make_prefix)"
     version="$("$python" --version | head -n 1 | cut -d ' ' -f 2)"
