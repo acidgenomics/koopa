cefe845b6fd186b40263c41cf9d867207c7eb188
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-06 16:16:01 -0400

    Remove extra assert check

--- shell/bash/functions/python.sh
+++ shell/bash/functions/python.sh
@@ -188,12 +188,6 @@ koopa::pip_install() { # {{{1
     done
     [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
     koopa::assert_is_installed "$python"
-
-    # FIXME
-    echo "FIXME"
-    exit 0
-
-    koopa::assert_is_python_package_installed --python="$python" 'pip'
     target="$(koopa::python_site_packages_prefix "$python")"
     koopa::sys_mkdir "$target"
     koopa::dl \
