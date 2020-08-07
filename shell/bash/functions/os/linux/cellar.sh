ba8216f9898da7a7be301cf7385369aa7d128ce7
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-07 10:47:03 -0400

    Rework internal cellar script calls

--- shell/bash/functions/os/linux/cellar.sh
+++ shell/bash/functions/os/linux/cellar.sh
@@ -1,9 +1,75 @@
 #!/usr/bin/env bash
 
-koopa::_install_cellar() { # {{{1
+koopa::find_cellar_version() { # {{{1
+    # """
+    # Find cellar installation directory.
+    # @note Updated 2020-06-30.
+    # """
+    local name prefix x
+    koopa::assert_has_args "$#"
+    name="${1:?}"
+    prefix="$(koopa::cellar_prefix)"
+    koopa::assert_is_dir "$prefix"
+    prefix="${prefix}/${name}"
+    koopa::assert_is_dir "$prefix"
+    x="$( \
+        find "$prefix" \
+            -mindepth 1 \
+            -maxdepth 1 \
+            -type d \
+        | sort \
+        | tail -n 1 \
+    )"
+    koopa::assert_is_dir "$x"
+    x="$(basename "$x")"
+    koopa::print "$x"
+    return 0
+}
+
+koopa::install_autoconf() { # {{{1
+    koopa::install_cellar \
+        --name='autoconf' \
+        "$@"
+}
+
+koopa::install_autojump() { # {{{1
+    koopa::install_cellar \
+        --name='autojump' \
+        "$@"
+}
+
+koopa::install_automake() { # {{{1
+    koopa::install_cellar \
+        --name='automake' \
+        "$@"
+}
+
+koopa::install_aws_cli() { # {{{1
+    koopa::install_cellar \
+        --name='aws-cli' \
+        --name-fancy='AWS CLI' \
+        --version='latest' \
+        --include-dirs='bin' \
+        "$@"
+}
+
+koopa::install_bash() { # {{{1
+    koopa::install_cellar \
+        --name='bash' \
+        --name-fancy='Bash' \
+        "$@"
+}
+
+koopa::install_binutils() { # {{{1
+    koopa::install_cellar \
+        --name='binutils' \
+        "$@"
+}
+
+koopa::install_cellar() { # {{{1
     # """
     # Install cellarized application.
-    # @note Updated 2020-08-05.
+    # @note Updated 2020-08-07.
     # """
     local gnu_mirror include_dirs jobs link_args link_cellar make_prefix name \
         name_fancy pass_args prefix reinstall script_name script_path tmp_dir \
@@ -120,155 +186,89 @@ koopa::_install_cellar() { # {{{1
     return 0
 }
 
-koopa::find_cellar_version() { # {{{1
-    # """
-    # Find cellar installation directory.
-    # @note Updated 2020-06-30.
-    # """
-    local name prefix x
-    koopa::assert_has_args "$#"
-    name="${1:?}"
-    prefix="$(koopa::cellar_prefix)"
-    koopa::assert_is_dir "$prefix"
-    prefix="${prefix}/${name}"
-    koopa::assert_is_dir "$prefix"
-    x="$( \
-        find "$prefix" \
-            -mindepth 1 \
-            -maxdepth 1 \
-            -type d \
-        | sort \
-        | tail -n 1 \
-    )"
-    koopa::assert_is_dir "$x"
-    x="$(basename "$x")"
-    koopa::print "$x"
-    return 0
-}
-
-koopa::install_autoconf() { # {{{1
-    koopa::_install_cellar \
-        --name='autoconf' \
-        "$@"
-}
-
-koopa::install_autojump() { # {{{1
-    koopa::_install_cellar \
-        --name='autojump' \
-        "$@"
-}
-
-koopa::install_automake() { # {{{1
-    koopa::_install_cellar \
-        --name='automake' \
-        "$@"
-}
-
-koopa::install_aws_cli() { # {{{1
-    koopa::_install_cellar \
-        --name='aws-cli' \
-        --name-fancy='AWS CLI' \
-        --version='latest' \
-        --include-dirs='bin' \
-        "$@"
-}
-
-koopa::install_bash() { # {{{1
-    koopa::_install_cellar \
-        --name='bash' \
-        --name-fancy='Bash' \
-        "$@"
-}
-
-koopa::install_binutils() { # {{{1
-    koopa::_install_cellar \
-        --name='binutils' \
-        "$@"
-}
-
 koopa::install_cmake() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='cmake' \
         --name-fancy='CMake' \
         "$@"
 }
 
 koopa::install_coreutils() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='coreutils' \
         "$@"
 }
 
 koopa::install_curl() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='curl' \
         --name-fancy='cURL' \
         "$@"
 }
 
 koopa::install_docker_credential_pass() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='docker-credential-pass' \
         "$@"
 }
 
 koopa::install_emacs() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='emacs' \
         --name-fancy='Emacs' \
         "$@"
 }
 
 koopa::install_findutils() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='findutils' \
         "$@"
 }
 
 koopa::install_fish() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='fish' \
         --name-fancy='Fish' \
         "$@"
 }
 
 koopa::install_gawk() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='gawk' \
         --name-fancy='GNU awk' \
         "$@"
 }
 
 koopa::install_gcc() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='gcc' \
         --name-fancy='GCC' \
         "$@"
 }
 
 koopa::install_gdal() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='gdal' \
         --name-fancy='GDAL' \
         "$@"
 }
 
 koopa::install_geos() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='geos' \
         --name-fancy='GEOS' \
         "$@"
 }
 
 koopa::install_git() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='git' \
         --name-fancy='Git' \
         "$@"
 }
 
 koopa::install_gnupg() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='gnupg' \
         --name-fancy='GnuPG suite' \
         "$@"
@@ -277,13 +277,13 @@ koopa::install_gnupg() { # {{{1
 }
 
 koopa::install_grep() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='grep' \
         "$@"
 }
 
 koopa::install_gsl() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='gsl' \
         --name-fancy='GSL' \
         --cellar-only \
@@ -291,14 +291,14 @@ koopa::install_gsl() { # {{{1
 }
 
 koopa::install_hdf5() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='hdf5' \
         --name-fancy='HDF5' \
         "$@"
 }
 
 koopa::install_htop() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='htop' \
         "$@"
 }
@@ -325,65 +325,71 @@ koopa::install_julia() { # {{{1
         esac
     done
     [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='julia' \
         --name-fancy='Julia' \
         --script-name="julia-${install_type}" \
         "$@"
 }
 
+koopa::install_libevent() { # {{{1
+    koopa::install_cellar \
+        --name='libevent' \
+        "$@"
+}
+
 koopa::install_libtool() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='libtool' \
         "$@"
 }
 
 koopa::install_lua() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='lua' \
         --name-fancy='Lua' \
         "$@"
 }
 
 koopa::install_luarocks() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='luarocks' \
         "$@"
 }
 
 koopa::install_make() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='make' \
         "$@"
 }
 
 koopa::install_ncurses() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='ncurses' \
         "$@"
 }
 
 koopa::install_neofetch() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='neofetch' \
         "$@"
 }
 
 koopa::install_neovim() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='neovim' \
         "$@"
 }
 
 koopa::install_openssh() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='openssh' \
         --name-fancy='OpenSSH' \
         "$@"
 }
 
 koopa::install_openssl() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='openssl' \
         --name-fancy='OpenSSL' \
         --cellar-only \
@@ -391,7 +397,7 @@ koopa::install_openssl() { # {{{1
 }
 
 koopa::install_parallel() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='parallel' \
         "$@"
 }
@@ -401,110 +407,110 @@ koopa::install_password_store() { # {{{1
     # https://www.passwordstore.org/
     # https://git.zx2c4.com/password-store/
     # """
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='password-store' \
         "$@"
 }
 
 koopa::install_patch() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='patch' \
         "$@"
 }
 
 koopa::install_perl() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='perl' \
         --name-fancy='Perl' \
         "$@"
 }
 
 koopa::install_pkg_config() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='pkg-config' \
         "$@"
 }
 
 koopa::install_proj() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='proj' \
         --name-fancy='PROJ' \
         "$@"
 }
 
 koopa::install_pyenv() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='pyenv' \
         "$@"
 }
 
 koopa::install_python() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='python' \
         --name-fancy='Python' \
         "$@"
 }
 
 koopa::install_r() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='r' \
         --name-fancy='R' \
         "$@"
 }
 
 koopa::install_rbenv() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='rbenv' \
         "$@"
 }
 
 koopa::install_rmate() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='rmate' \
         "$@"
 }
 
 koopa::install_rsync() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='rsync' \
         "$@"
 }
 
 koopa::install_ruby() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='ruby' \
         --name-fancy='Ruby' \
         "$@"
 }
 
 koopa::install_sed() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='sed' \
         "$@"
 }
 
 koopa::install_shellcheck() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='shellcheck' \
         --name-fancy='ShellCheck' \
         "$@"
 }
 
 koopa::install_shunit2() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='shunit2' \
         --name-fancy='shUnit2' \
         "$@"
 }
 
 koopa::install_singularity() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='singularity' \
         "$@"
 }
 
 koopa::install_sqlite() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='sqlite' \
         --name-fancy='SQLite' \
         "$@"
@@ -513,50 +519,50 @@ koopa::install_sqlite() { # {{{1
 }
 
 koopa::install_subversion() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='subversion' \
         "$@"
 }
 
 koopa::install_texinfo() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='texinfo' \
         "$@"
 }
 
 koopa::install_the_silver_searcher() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='the-silver-searcher' \
         "$@"
 }
 
 koopa::install_tmux() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='tmux' \
         "$@"
 }
 
 koopa::install_udunits() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='udunits' \
         "$@"
 }
 
 koopa::install_vim() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='vim' \
         --name-fancy='Vim' \
         "$@"
 }
 
 koopa::install_wget() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='wget' \
         "$@"
 }
 
 koopa::install_zsh() { # {{{1
-    koopa::_install_cellar \
+    koopa::install_cellar \
         --name='zsh' \
         --name-fancy='Zsh' \
         "$@"
