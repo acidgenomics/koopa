b058d77b5246b1a6e61f7bbfe29d8d8ae856620c
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-11 18:08:18 -0400

    Draft update to R argparse

--- lang/r/include/header.R
+++ lang/r/include/header.R
@@ -3,9 +3,8 @@
 ## @note Updated 2020-08-11.
 ## """
 
-## FIXME Need a special case for r-koopa install procedure.
-
 .koopa <- new.env()
+.koopa[["minVersion"]] <- package_version("0.0.1")
 .koopa[["vanilla"]] <- isTRUE("--vanilla" %in% commandArgs())
 .koopa[["verbose"]] <- isTRUE("--verbose" %in% commandArgs())
 
@@ -20,82 +19,31 @@ if (isTRUE(.koopa[["verbose"]])) {
 
 stopifnot(packageVersion("base") >= "4.0")
 
-## Check package dependencies {{{1
-## =============================================================================
-
-.koopa[["dependencies"]] <-
-    c(
-        "acidgenomics/acidbase" = "0.1.12",
-        "acidgenomics/acidgenerics" = "0.3.9",
-        "acidgenomics/goalie" = "0.4.6",
-        "acidgenomics/syntactic" = "0.4.2",
-        "acidgenomics/bb8" = "0.2.23"
-    )
-
-## FIXME Need to rework this using new koopa R package.
-
 local({
-    vanilla <- .koopa[["vanilla"]]
-    dependencies <- .koopa[["dependencies"]]
-    installGitHub <- .koopa[[".installGitHub"]]
-    isInstalled <- function(pkgs) {
+    .isInstalled <- function(pkgs) {
         basename(pkgs) %in% rownames(installed.packages())
     }
-    isPackageVersion <- .koopa[["isPackageVersion"]]
-    ok <- isPackageVersion(dependencies)
-    repos <- names(dependencies)[!ok]
-    if (length(repos) > 0L) {
-        if (isTRUE(vanilla)) {
-            stop(paste0(
-                "koopa dependencies are outdated and ",
-                "cannot be updated in '--vanilla' mode."
-            ))
-        }
-        message(sprintf(
-            "Updating koopa dependencies: %s",
-            toString(basename(repos))
-        ))
-        local({
-            repos <- getOption("repos")
-            repos[["CRAN"]] <- "https://cloud.r-project.org"
-            options("repos" = repos)
-        })
-        ## Note that stringi is a dependency for syntactic.
-        invisible(lapply(
-            X = c(
-                "BiocManager",
-                "cli",
-                "crayon",
-                "remotes",
-                "stringi",
-                "stringr"
-            ),
-            FUN = function(pkg) {
-                if (!isTRUE(isInstalled(pkg))) {
-                    install.packages(pkg)
-                }
-            }
-        ))
-        if (isTRUE(nzchar(Sys.getenv("GITHUB_PAT")))) {
-            remotes::install_github(repos, upgrade = "never")
-        } else {
-            installGitHub(repos, reinstall = TRUE)
-        }
+    if (isTRUE(.isInstalled("koopa"))) {
+        ok <- packageVersion("koopa") >= .koopa[["minVersion"]]
+    } else {
+        ok <- FALSE
     }
-    ok <- isPackageVersion(dependencies)
-    if (!all(ok)) {
-        stop(sprintf(
-            "Dependency check failure:\n%s",
-            paste(capture.output(print(ok)), collapse = "\n")
-        ))
+    if (isTRUE(ok)) return()
+    if (isTRUE(.koopa[["vanilla"]])) {
+        stop("Outdated R packages cannot be updated in '--vanilla' mode.")
     }
+    .install <- function(url) {
+        install.packages(pkgs = url, repos = NULL)
+    }
+    message("Installing koopa R package.")
+    .install("https://github.com/acidgenomics/koopa/archive/r.tar.gz")
+    stopifnot(packageVersion("koopa") >= .koopa[["minVersion"]])
 })
-
-.koopa[["koopaHelp"]]()
 
 suppressPackageStartupMessages({
     library(koopa)
 })
-assert(isCleanSystemLibrary())
+stopifnot(isCleanSystemLibrary())
+koopaHelp()
 attach(.koopa)
 koopa <- .koopa[["koopa"]]
