abb3b4b1c7c2f330c6012cac456a83984ec533e6
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-12 18:58:25 -0400

    Reorganize

--- shell/bash/functions/system.sh
+++ shell/bash/functions/system.sh
@@ -51,26 +51,6 @@ koopa::admin_group() { # {{{1
     return 0
 }
 
-koopa::check_exports() { # {{{1
-    # """
-    # Check exported environment variables.
-    # @note Updated 2020-07-05.
-    #
-    # Warn the user if they are setting unrecommended values.
-    # """
-    koopa::assert_has_no_args "$#"
-    koopa::is_rstudio && return 0
-    local vars
-    vars=(
-        'JAVA_HOME'
-        'LD_LIBRARY_PATH'
-        'PYTHONHOME'
-        'R_HOME'
-    )
-    koopa::warn_if_export "${vars[@]}"
-    return 0
-}
-
 koopa::date() { # {{{1
     # """
     # Koopa date.
