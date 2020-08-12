afe4b3a2c08c4ca919b466bdd8c7eacb7f5f844b
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-12 17:36:21 -0400

    Reorganize language-specific scripts

---
+++ shell/bash/functions/lang/python.sh
@@ -0,0 +1,386 @@
+#!/usr/bin/env bash
+
+koopa::install_pip() { # {{{1
+    # """
+    # Install pip for Python.
+    # @note Updated 2020-07-10.
+    # """
+    local file name pos python reinstall tmp_dir url
+    name='pip'
+    python="$(koopa::python)"
+    reinstall=0
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            --python=*)
+                python="${1#*=}"
+                shift 1
+                ;;
+            --python)
+                python="$2"
+                shift 2
+                ;;
+            --reinstall)
+                reinstall=1
+                shift 1
+                ;;
+            '')
+                shift 1
+                ;;
+            --)
+                shift 1
+                break
+                ;;
+            --*|-*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa::assert_has_no_args "$#"
+    koopa::assert_is_installed "$python"
+    if [[ "$reinstall" -eq 0 ]]
+    then
+        if koopa::is_python_package_installed --python="$python" "$name"
+        then
+            koopa::note "Python package '${name}' is already installed."
+            return 0
+        fi
+    fi
+    koopa::install_start "$name"
+    tmp_dir="$(koopa::tmp_dir)"
+    (
+        koopa::cd "$tmp_dir"
+        file='get-pip.py'
+        url="https://bootstrap.pypa.io/${file}"
+        koopa::download "$url"
+        "$python" "$file" --no-warn-script-location
+    )
+    koopa::rm "$tmp_dir"
+    koopa::is_cellar "$python" && koopa::link_cellar python
+    koopa::install_success "$name"
+    return 0
+}
+
+koopa::install_python_packages() { # {{{1
+    # """
+    # Install Python packages.
+    # @note Updated 2020-08-12.
+    # """
+    local install_flags name_fancy pkg pkgs pos python version
+    python="$(koopa::python)"
+    reinstall=0
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            --python=*)
+                python="${1#*=}"
+                shift 1
+                ;;
+            --python)
+                python="$2"
+                shift 2
+                ;;
+            --reinstall)
+                reinstall=1
+                shift 1
+                ;;
+            '')
+                shift 1
+                ;;
+            --)
+                shift 1
+                break
+                ;;
+            --*|-*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa::assert_has_no_envs
+    koopa::assert_is_installed "$python"
+    pkgs=("$@")
+    if [[ "${#pkgs[@]}" -eq 0 ]]
+    then
+        pkgs=(
+            'black'
+            'flake8'
+            'logbook'
+            'numpy'
+            'pandas'
+            'pip'
+            'pipx'
+            'pyflakes'
+            'pylint'
+            'pytest'
+            'ranger-fm'
+            'setuptools'
+            'six'
+            'wheel'
+        )
+        for i in "${!pkgs[@]}"
+        do
+            pkg="${pkgs[$i]}"
+            version="$(koopa::variable "python-${pkg}")"
+            pkgs[$i]="${pkg}==${version}"
+        done
+    fi
+    name_fancy='Python packages'
+    koopa::install_start "$name_fancy"
+    install_flags=("--python=${python}")
+    [[ "$reinstall" -eq 1 ]] && install_flags+=('--reinstall')
+    koopa::python_add_site_packages_to_sys_path "$python"
+    koopa::install_pip "${install_flags[@]}"
+    koopa::pip_install "${install_flags[@]}" "${pkgs[@]}"
+    koopa::install_py_koopa "${install_flags[@]}"
+    koopa::install_success "$name_fancy"
+    return 0
+}
+
+koopa::pip_install() { # {{{1
+    # """
+    # Internal pip install command.
+    # @note Updated 2020-08-06.
+    # """
+    local pip_install_flags pos python reinstall target
+    koopa::assert_has_args "$#"
+    python="$(koopa::python)"
+    reinstall=0
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            --python=*)
+                python="${1#*=}"
+                shift 1
+                ;;
+            --python)
+                python="$2"
+                shift 2
+                ;;
+            --reinstall)
+                reinstall=1
+                shift 1
+                ;;
+            '')
+                shift 1
+                ;;
+            --)
+                shift 1
+                break
+                ;;
+            --*|-*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa::is_installed "$python" || return 0
+    target="$(koopa::python_site_packages_prefix "$python")"
+    koopa::sys_mkdir "$target"
+    koopa::dl \
+        'Packages' "$(koopa::to_string "$@")" \
+        'Target' "$target"
+    pip_install_flags=(
+        "--target=${target}"
+        '--no-warn-script-location'
+        '--upgrade'
+    )
+    if [[ "$reinstall" -eq 1 ]]
+    then
+        pip_flags+=(
+            '--force-reinstall'
+            '--ignore-installed'
+        )
+    fi
+    "$python" -m pip install "${pip_install_flags[@]}" "$@"
+    return 0
+}
+
+koopa::python_add_site_packages_to_sys_path() { # {{{1
+    # """
+    # Add our custom site packages library to sys.path.
+    # @note Updated 2020-08-06.
+    #
+    # @seealso
+    # > "$python" -m site
+    # """
+    local file k_site_pkgs python sys_site_pkgs
+    python="${1:-}"
+    [[ -z "$python" ]] && python="$(koopa::python)"
+    sys_site_pkgs="$(koopa::python_system_site_packages_prefix "$python")"
+    k_site_pkgs="$(koopa::python_site_packages_prefix "$python")"
+    [[ ! -d "$k_site_pkgs" ]] && koopa::sys_mkdir "$k_site_pkgs"
+    file="${sys_site_pkgs}/koopa.pth"
+    if [[ ! -f "$file" ]]
+    then
+        koopa::info "Adding '${file}' path file in '${sys_site_pkgs}'."
+        if koopa::is_cellar "$python"
+        then
+            koopa::write_string "$k_site_pkgs" "$file"
+            koopa::link_cellar python
+        else
+            koopa::sudo_write_string "$k_site_pkgs" "$file"
+        fi
+    fi
+    "$python" -m site
+    return 0
+}
+
+koopa::python_remove_pycache() { # {{{1
+    # """
+    # Remove Python '__pycache__/' from site packages.
+    # @note Updated 2020-08-06.
+    #
+    # These directories can create permission issues when attempting to rsync
+    # installation across multiple VMs.
+    # """
+    local pos prefix python
+    koopa::assert_has_args_le "$#" 1
+    koopa::assert_is_installed find
+    python="$(koopa::python)"
+    while (("$#"))
+    do
+        case "$1" in
+            --python=*)
+                python="${1#*=}"
+                shift 1
+                ;;
+            --python)
+                python="$2"
+                shift 2
+                ;;
+            *)
+                koopa::invalid_arg "$1"
+                ;;
+        esac
+    done
+    koopa::assert_has_no_args "$#"
+    python="$(koopa::which_realpath "$python")"
+    prefix="$(koopa::parent_dir -n 2 "$python")"
+    koopa::info "Removing pycache in '${prefix}'."
+    find "$prefix" \
+        -type d \
+        -name '__pycache__' \
+        -print0 \
+        | xargs -0 -I {} rm -frv '{}'
+    return 0
+}
+
+koopa::venv_create() { # {{{1
+    # """
+    # Create Python virtual environment.
+    # @note Updated 2020-07-21.
+    # """
+    local name prefix python venv_python
+    python="$(koopa::python)"
+    koopa::assert_has_no_envs
+    koopa::assert_is_installed "$python"
+    name="${1:?}"
+    prefix="$(koopa::venv_prefix)/${name}"
+    [[ -d "$prefix" ]] && return 0
+    shift 1
+    koopa::info "Installing Python '${name}' venv at '${prefix}'."
+    koopa::mkdir "$prefix"
+    "$python" -m venv "$prefix"
+    venv_python="${prefix}/bin/python3"
+    "$venv_python" -m pip install --upgrade pip setuptools wheel
+    if [[ "$#" -gt 0 ]]
+    then
+        "$venv_python" -m pip install --upgrade "$@"
+    elif [[ "$name" != 'base' ]]
+    then
+        "$venv_python" -m pip install "$name"
+    fi
+    koopa::sys_set_permissions -r "$prefix"
+    "$venv_python" -m pip list
+    return 0
+}
+
+koopa::venv_create_base() { # {{{1
+    # """
+    # Create base Python virtual environment.
+    # @note Updated 2020-07-20.
+    # """
+    koopa::assert_has_no_args "$#"
+    koopa::venv_create 'base'
+    return 0
+}
+
+koopa::venv_create_r_reticulate() { # {{{1
+    # """
+    # Create Python reticulate environment for R.
+    # @note Updated 2020-07-20.
+    #
+    # Check that LLVM is configured correctly.
+    # umap-learn > numba > llvmlite
+    # Note that llvmlite currently requires LLVM 7+.
+    # https://github.com/numba/llvmlite/issues/523
+    #
+    # macOS compiler flags:
+    # These flags are now required for scikit-learn to compile, which now
+    # requires OpenMP that is unsupported by system default gcc alias.
+    #
+    # Ensure that we're using the correct Clang and LLVM settings.
+    #
+    # Refer to 'system/activate/program.sh' for LLVM_CONFIG export.
+    #
+    # clang: error: unsupported option '-fopenmp'
+    # brew info libomp
+    #
+    # @seealso
+    # - http://llvmlite.pydata.org/
+    # - https://github.com/scikit-learn/scikit-learn/issues/13371
+    # - https://scikit-learn.org/dev/developers/advanced_installation.html
+    # """
+    local name packages
+    koopa::assert_has_no_args "$#"
+    name='r-reticulate'
+    packages=(
+        'Cython'
+        'cwltool'
+        'louvain'
+        'numpy'
+        'pandas'
+        'pip'
+        'pyyaml'
+        'scikit-learn'
+        'scipy'
+        'setuptools'
+        'umap-learn'
+        'wheel'
+    )
+    if koopa::is_macos
+    then
+        export CC='/usr/bin/clang'
+        export CXX='/usr/bin/clang++'
+        export CFLAGS="${CFLAGS:-} -I/usr/local/opt/libomp/include"
+        export CPPFLAGS="${CPPFLAGS:-} -Xpreprocessor -fopenmp"
+        export CXXFLAGS="${CXXFLAGS:-} -I/usr/local/opt/libomp/include"
+        export DYLD_LIBRARY_PATH='/usr/local/opt/libomp/lib'
+        export LDFLAGS="${LDFLAGS:-} -L/usr/local/opt/libomp/lib -lomp"
+    fi
+    if [[ -n "${LLVM_CONFIG:-}" ]]
+    then
+        koopa::info "LLVM_CONFIG: '${LLVM_CONFIG}'."
+    else
+        koopa::note "Export 'LLVM_CONFIG' to locate LLVM llvm-config binary."
+    fi
+    koopa::venv_create "$name" "${packages[@]}"
+    return 0
+}
---
+++ shell/bash/functions/lang/r.sh
@@ -0,0 +1,291 @@
+#!/usr/bin/env bash
+
+koopa::array_to_r_vector() { # {{{1
+    # """
+    # Convert a bash array to an R vector string.
+    # @note Updated 2020-07-20.
+    # """
+    local x
+    koopa::assert_has_args "$#"
+    x="$(printf '"%s", ' "$@")"
+    x="$(koopa::strip_right ', ' "$x")"
+    x="$(printf 'c(%s)\n' "$x")"
+    [[ -n "$x" ]] || return 1
+    koopa::print "$x"
+    return 0
+}
+
+koopa::install_rcheck() { # {{{1
+    # """
+    # Install Rcheck scripts for CI.
+    # @note Updated 2020-07-08.
+    # """
+    local link_name name source_repo target_dir
+    koopa::assert_has_no_args "$#"
+    name='Rcheck'
+    source_repo="https://github.com/acidgenomics/${name}.git"
+    target_dir="$(koopa::local_app_prefix)/${name}"
+    link_name=".${name}"
+    koopa::install_start "$name"
+    if [[ ! -d "$target_dir" ]]
+    then
+        koopa::h2 "Downloading ${name} to '${target_dir}'."
+        (
+            koopa::mkdir "$target_dir"
+            git clone "$source_repo" "$target_dir"
+        )
+    fi
+    koopa::ln "$target_dir" "$link_name"
+    koopa::install_success "$name"
+    return 0
+}
+
+koopa::kill_r() { # {{{1
+    koopa::assert_has_no_args "$#"
+    koopa::assert_is_installed pkill
+    pkill rsession
+}
+
+koopa::link_r_etc() { # {{{1
+    # """
+    # Link R config files inside 'etc/'.
+    # @note Updated 2020-07-06.
+    #
+    # Don't copy Makevars file across machines.
+    # """
+    local file files koopa_prefix os_id r r_etc_source r_etc_target \
+        r_prefix version
+    koopa::assert_has_args_le "$#" 1
+    r="${1:-R}"
+    koopa::is_installed "$r" || return 1
+    r_prefix="$(koopa::r_prefix "$r")"
+    [[ -d "$r_prefix" ]] || return 1
+    version="$(koopa::r_version "$r")"
+    if [[ "$version" != 'devel' ]]
+    then
+        version="$(koopa::major_minor_version "$version")"
+    fi
+    koopa_prefix="$(koopa::prefix)"
+    if koopa::is_linux && koopa::is_cellar "$r_prefix"
+    then
+        os_id='linux'
+    else
+        os_id="$(koopa::os_id)"
+    fi
+    r_etc_source="${koopa_prefix}/os/${os_id}/etc/R/${version}"
+    if [[ ! -d "$r_etc_source" ]]
+    then
+        koopa::warning "Missing R etc source: '${r_etc_source}'."
+        return 1
+    fi
+    if koopa::is_linux && \
+        ! koopa::is_cellar "$r" && \
+        [[ -d '/etc/R' ]]
+    then
+        # This applies to Debian/Ubuntu CRAN binary installs.
+        r_etc_target='/etc/R'
+    else
+        r_etc_target="${r_prefix}/etc"
+    fi
+    files=(
+        'Makevars.site'  # macOS
+        'Renviron.site'
+        'Rprofile.site'
+        'repositories'
+    )
+    for file in "${files[@]}"
+    do
+        [[ -f "${r_etc_source}/${file}" ]] || continue
+        koopa::sys_ln "${r_etc_source}/${file}" "${r_etc_target}/${file}"
+    done
+    return 0
+}
+
+koopa::link_r_site_library() { # {{{1
+    # """
+    # Link R site library.
+    # @note Updated 2020-07-04.
+    # """
+    local app_prefix lib_source lib_target r r_prefix version
+    koopa::assert_has_args_le "$#" 1
+    r="${1:-R}"
+    r_prefix="$(koopa::r_prefix "$r")"
+    [[ -d "$r_prefix" ]] || return 1
+    version="$(koopa::r_version "$r")"
+    if [[ "$version" != 'devel' ]]
+    then
+        version="$(koopa::major_minor_version "$version")"
+    fi
+    app_prefix="$(koopa::app_prefix)"
+    lib_source="${app_prefix}/r/${version}/site-library"
+    lib_target="${r_prefix}/site-library"
+    koopa::sys_mkdir "$lib_source"
+    koopa::sys_ln "$lib_source" "$lib_target"
+    return 0
+}
+
+koopa::r_javareconf() { # {{{1
+    # """
+    # Update R Java configuration.
+    # @note Updated 2020-07-05.
+    #
+    # The default Java path differs depending on the system.
+    #
+    # > R CMD javareconf -h
+    #
+    # Environment variables that can be used to influence the detection:
+    #   JAVA           path to a Java interpreter executable
+    #                  By default first 'java' command found on the PATH
+    #                  is taken (unless JAVA_HOME is also specified).
+    #   JAVA_HOME      home of the Java environment. If not specified,
+    #                  it will be detected automatically from the Java
+    #                  interpreter.
+    #   JAVAC          path to a Java compiler
+    #   JAVAH          path to a Java header/stub generator
+    #   JAR            path to a Java archive tool
+    #
+    # How to check that rJava works:
+    # > library(rJava)
+    # > .jinit()
+    # """
+    local java_flags java_home pos r r2
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            --java-home=*)
+                java_home="${1#*=}"
+                shift 1
+                ;;
+            --java-home)
+                java_home="$2"
+                shift 2
+                ;;
+            --r=*)
+                r="${1#*=}"
+                shift 1
+                ;;
+            --r)
+                r="$2"
+                shift 2
+                ;;
+            --)
+                shift 1
+                break
+                ;;
+            --*|-*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa::assert_has_args_le "$#" 1
+    [[ -n "${1:-}" ]] && r="$1"
+    koopa::is_installed "$r" || return 0
+    r="$(koopa::which_realpath "$r")"
+    if [[ -z "${java_home:-}" ]]
+    then
+        koopa::activate_openjdk
+        java_home="$(koopa::java_prefix)"
+        koopa::is_installed java || return 0
+    fi
+    [[ -d "$java_home" ]] || return 1
+    koopa::h2 'Updating R Java configuration.'
+    koopa::dl 'R' "$r"
+    koopa::dl 'Java home' "$java_home"
+    java_flags=(
+        "JAVA_HOME=${java_home}"
+        "JAVA=${java_home}/bin/java"
+        "JAVAC=${java_home}/bin/javac"
+        "JAVAH=${java_home}/bin/javah"
+        "JAR=${java_home}/bin/jar"
+    )
+    if koopa::is_cellar "$r"
+    then
+        r2=("$r")
+    else
+        koopa::assert_has_sudo
+        r2=('sudo' "$r")
+    fi
+    "${r2[@]}" --vanilla CMD javareconf "${java_flags[@]}"
+    return 0
+}
+
+koopa::r_rebuild_docs() { # {{{1
+    # """
+    # Rebuild R HTML/CSS files in 'docs' directory.
+    # @note Updated 2020-08-11.
+    #
+    # 1. Ensure HTML package index is writable.
+    # 2. Touch an empty 'R.css' file to eliminate additional package warnings.
+    #    Currently we're seeing this inside Fedora Docker images.
+    #
+    # @seealso
+    # HTML package index configuration:
+    # https://stat.ethz.ch/R-manual/R-devel/library/utils/html/
+    #     make.packages.html.html
+    # """
+    local doc_dir html_dir pkg_index r rscript rscript_flags
+    r="${1:-R}"
+    rscript="${r}script"
+    rscript_flags=('--vanilla')
+    koopa::assert_is_installed "$r" "$rscript"
+    koopa::h2 'Updating HTML package index.'
+    doc_dir="$("$rscript" "${rscript_flags[@]}" -e 'cat(R.home("doc"))')"
+    html_dir="${doc_dir}/html"
+    [[ ! -d "$html_dir" ]] && koopa::mkdir -S "$html_dir"
+    pkg_index="${html_dir}/packages.html"
+    koopa::dl 'HTML index' "$pkg_index"
+    [[ ! -f "$pkg_index" ]] && sudo touch "$pkg_index"
+    r_css="${html_dir}/R.css"
+    [[ ! -f "$r_css" ]] && sudo touch "$r_css"
+    koopa::sys_set_permissions "$pkg_index"
+    "$rscript" "${rscript_flags[@]}" -e 'utils::make.packages.html()'
+}
+
+koopa::update_r_config() { # {{{1
+    # """
+    # Update R configuration.
+    # @note Updated 2020-08-11.
+    #
+    # Add shared R configuration symlinks in '${R_HOME}/etc'.
+    # """
+    local etc_prefix make_prefix pkg_index r r_prefix
+    koopa::assert_has_args_le "$#" 1
+    r="${1:-R}"
+    r="$(koopa::which_realpath "$r")"
+    koopa::assert_is_installed "$r"
+    r_prefix="$(koopa::r_prefix "$r")"
+    koopa::h1 'Updating R configuration.'
+    koopa::dl \
+        'R home' "$r_prefix" \
+        'R path' "$r"
+    if koopa::is_cellar "$r"
+    then
+        make_prefix="$(koopa::make_prefix)"
+        etc_prefix="${make_prefix}/lib/R/etc"
+        koopa::sys_set_permissions -r "$r_prefix"
+        # Ensure that (Debian) system 'etc' directories are removed.
+        if [[ -d "$etc_prefix" ]] && [[ ! -L "$etc_prefix" ]]
+        then
+            koopa::sys_rm "$etc_prefix"
+        fi
+        etc_prefix="${make_prefix}/lib64/R/etc"
+        if [[ -d "$etc_prefix" ]] && [[ ! -L "$etc_prefix" ]]
+        then
+            koopa::sys_rm "$etc_prefix"
+        fi
+    else
+        koopa::sys_set_permissions -r "${r_prefix}/library"
+    fi
+    koopa::link_r_etc "$r"
+    koopa::link_r_site_library "$r"
+    koopa::r_javareconf "$r"
+    koopa::r_rebuild_docs "$r"
+    koopa::success 'Update of R configuration was successful.'
+    return 0
+}
