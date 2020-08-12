757abca1674414fbfe380dcc568b04dd3272f1b6
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-12 18:31:15 -0400

    Consolidate Python scripts

--- shell/bash/functions/lang/python.sh
+++ shell/bash/functions/lang/python.sh
@@ -282,6 +282,81 @@ koopa::python_remove_pycache() { # {{{1
     return 0
 }
 
+koopa::update_pyenv() { # {{{1
+    # """
+    # Update pyenv.
+    # @note Updated 2020-07-30.
+    # """
+    koopa::is_installed pyenv || return 0
+    koopa::assert_has_no_args "$#"
+    koopa::assert_has_no_envs
+    koopa::h1 'Updating pyenv.'
+    (
+        koopa::cd "$(pyenv root)"
+        git pull
+    )
+    return 0
+}
+
+koopa::update_python_packages() { # {{{1
+    # """
+    # Update all pip packages.
+    # @note Updated 2020-07-30.
+    # @seealso
+    # - https://github.com/pypa/pip/issues/59
+    # - https://stackoverflow.com/questions/2720014
+    # """
+    local name_fancy pkgs prefix python x
+    koopa::assert_has_no_args "$#"
+    koopa::assert_has_no_envs
+    python="$(koopa::python)"
+    koopa::is_installed "$python" || return 0
+    name_fancy='Python packages'
+    koopa::install_start "$name_fancy"
+    x="$("$python" -m pip list --outdated --format='freeze')"
+    x="$(koopa::print "$x" | grep -v '^\-e')"
+    if [[ -z "$x" ]]
+    then
+        koopa::success 'All Python packages are current.'
+        return 0
+    fi
+    prefix="$(koopa::python_site_packages_prefix)"
+    readarray -t pkgs <<< "$(koopa::print "$x" | cut -d '=' -f 1)"
+    koopa::dl 'Packages' "$(koopa::to_string "${pkgs[@]}")"
+    koopa::dl 'Prefix' "$prefix"
+    "$python" -m pip install --no-warn-script-location --upgrade "${pkgs[@]}"
+    koopa::is_cellar "$python" && koopa::link_cellar python
+    koopa::install_success "$name_fancy"
+    return 0
+}
+
+koopa::update_venv() { # {{{1
+    # """
+    # Update Python virtual environment.
+    # @note Updated 2020-07-20.
+    # """
+    local array lines python
+    koopa::assert_has_no_args "$#"
+    python="$(koopa::python)"
+    koopa::assert_is_installed "$python"
+    if ! koopa::is_venv_active
+    then
+        koopa::note 'No active Python venv detected.'
+        return 0
+    fi
+    koopa::h1 'Updating Python venv.'
+    "$python" -m pip install --upgrade pip
+    lines="$("$python" -m pip list --outdated --format='freeze')"
+    readarray -t array <<< "$lines"
+    koopa::is_array_non_empty "${array[@]}" || return 0
+    koopa::h1 "${#array[@]} outdated packages detected."
+    koopa::print "$lines" \
+        | cut -d '=' -f 1 \
+        | xargs -n1 "$python" -m pip install --upgrade
+    return 0
+}
+
+
 koopa::venv_create() { # {{{1
     # """
     # Create Python virtual environment.
