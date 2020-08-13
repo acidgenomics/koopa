84f8bbe990a14dc57ba9ce1cca32b7e06a5d5c94
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-13 09:36:26 -0400

    Rework BAM scripts

---
+++ shell/bash/functions/ngs/align/bam/convert.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+koopa::sam_to_bam() { # {{{1
+    # """
+    # Convert multiple SAM files in a directory to BAM files.
+    # @note Updated 2020-08-13.
+    # """
+    local bam_file keep_sam pos sam_file sam_files
+    keep_sam=0
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            --keep-sam)
+                keep_sam=1
+                shift 1
+                ;;
+            --)
+                shift 1
+                break
+                ;;
+            --*|-*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    dir="${1:-.}"
+    koopa::assert_is_dir "$dir"
+    dir="$(realpath "$dir")"
+    readarray -t sam_files <<< "$( \
+        find "$dir" \
+            -maxdepth 3 \
+            -mindepth 1 \
+            -type f \
+            -iname '*.sam' \
+            -print \
+        | sort \
+    )"
+    if ! koopa::is_array_non_empty "${sam_files[@]}"
+    then
+        koopa::stop "No SAM files detected in '${dir}'."
+    fi
+    koopa::h1 "Converting SAM files in '${dir}' to BAM format."
+    koopa::activate_conda_env samtools
+    case "$keep_sam" in
+        0)
+            koopa::note 'SAM files will be deleted.'
+            ;;
+        1)
+            koopa::note 'SAM files will be preserved.'
+            ;;
+    esac
+    for sam_file in "${sam_files[@]}"
+    do
+        bam_file="${sam_file%.sam}.bam"
+        koopa::samtools_convert_sam_to_bam \
+            --input-sam="$sam_file" \
+            --output-bam="$bam_file"
+        [[ "$keep_sam" -eq 0 ]] && koopa::rm "$sam_file"
+    done
+    return 0
+}
--- shell/bash/functions/ngs/align/bam/samtools.sh
+++ shell/bash/functions/ngs/align/bam/samtools.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME Need to rename internal functions.
-koopa::sam_to_bam() { # {{{1
+koopa::samtools_convert_sam_to_bam() { # {{{1
     # """
-    # Convert SAM to BAM files.
-    # @note Updated 2020-07-08.
+    # Convert a SAM file to BAM format.
+    # @note Updated 2020-08-13.
     #
     # samtools view --help
     # Useful flags:
@@ -16,76 +15,6 @@ koopa::sam_to_bam() { # {{{1
     # -b                    output BAM
     # -o FILE               output file name [stdout]
     # -u                    uncompressed BAM output (implies -b)
-    # """
-    local bam_file keep_sam pos sam_file sam_files
-    keep_sam=0
-    pos=()
-    while (("$#"))
-    do
-        case "$1" in
-            --keep-sam)
-                keep_sam=1
-                shift 1
-                ;;
-            --)
-                shift 1
-                break
-                ;;
-            --*|-*)
-                koopa::invalid_arg "$1"
-                ;;
-            *)
-                pos+=("$1")
-                shift 1
-                ;;
-        esac
-    done
-    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
-    dir="${1:-.}"
-    koopa::assert_is_dir "$dir"
-    dir="$(realpath "$dir")"
-    # Pipe GNU find into array.
-    readarray -t sam_files <<< "$( \
-        find "$dir" \
-            -maxdepth 3 \
-            -mindepth 1 \
-            -type f \
-            -iname '*.sam' \
-            -print \
-        | sort \
-    )"
-    # Error if file array is empty.
-    if ! koopa::is_array_non_empty "${sam_files[@]}"
-    then
-        koopa::stop "No SAM files detected in '${dir}'."
-    fi
-    koopa::h1 "Converting SAM files in '${dir}' to BAM format."
-    koopa::activate_conda_env samtools
-    case "$keep_sam" in
-        0)
-            koopa::note 'SAM files will be deleted.'
-            ;;
-        1)
-            koopa::note 'SAM files will be preserved.'
-            ;;
-    esac
-    for sam_file in "${sam_files[@]}"
-    do
-        bam_file="${sam_file%.sam}.bam"
-        koopa::_sam_to_bam \
-            --input-sam="$sam_file" \
-            --output-bam="$bam_file"
-        [[ "$keep_sam" -eq 0 ]] && koopa::rm "$sam_file"
-    done
-    return 0
-}
-
-
-# FIXME Previously named _sam_to_bam
-koopa::samtools_convert_sam_to_bam() { # {{{1
-    # """
-    # Convert SAM file to BAM.
-    # @note Updated 2020-08-12.
     # """
     local bam_bn sam_bn threads
     koopa::assert_has_args "$#"
