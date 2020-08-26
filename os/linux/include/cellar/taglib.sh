45df9f2d94fa1adad9453f7576ed00e7c29bef47
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-26 08:42:26 -0400

    Add FIXME comment

--- os/linux/include/cellar/taglib.sh
+++ os/linux/include/cellar/taglib.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
+# FIXME Seeing this error on RHEL UBI when trying to install pytaglib:
+# https://stackoverflow.com/questions/29200461/recompile-with-fpic-flag
+# relocation R_X86_64_32S against symbol `_ZTVN6TagLib4RIFF3WAV4FileE' can not
+# be used when making a shared object; recompile with -fPIC
+
 file="${name}-${version}.tar.gz"
 url="https://github.com/taglib/taglib/releases/download/v${version}/${file}"
 koopa::download "$url"
