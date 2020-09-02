179743753f605457211eb14a7da5427d87f1f2c4
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-02 17:33:09 -0400

    Improve python assert

--- os/linux/include/cellar/htop.sh
+++ os/linux/include/cellar/htop.sh
@@ -4,7 +4,7 @@
 # Repo transferred from https://github.com/hishamhm/htop to 
 # https://github.com/htop-dev/htop in 2020-08.
 
-koopa::assert_is_installed python
+koopa::assert_is_installed python3
 file="${version}.tar.gz"
 url="https://github.com/htop-dev/htop/archive/${file}"
 koopa::download "$url"
