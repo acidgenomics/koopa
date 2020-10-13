892e6b7851f6431691976fcfc866ff1bc53f5491
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-13 07:33:21 -0400

    Skip building R packages on RHEL UBI

--- shell/bash/functions/os/linux/vm/configure.sh
+++ shell/bash/functions/os/linux/vm/configure.sh
@@ -3,7 +3,7 @@
 koopa::configure_vm() { # {{{1
     # """
     # Configure virtual machine.
-    # @note Updated 2020-10-09.
+    # @note Updated 2020-10-13.
     # """
     local app_prefix app_prefix_bn app_prefix_real bioconductor data_disk \
         data_disk_link data_disk_real docker full gb_total \
@@ -326,7 +326,12 @@ koopa::configure_vm() { # {{{1
         then
             venv-create-r-reticulate
         fi
-        install-r-packages
+        # RHEL UBI is currently missing harfbuzz-devel, fribidi-devel, which are
+        # now required to build pkgdown, so skipping for the moment.
+        if ! koopa::is_rhel_ubi
+        then
+            install-r-packages
+        fi
         if [[ "$full" -eq 1 ]]
         then
             install-perl-packages
