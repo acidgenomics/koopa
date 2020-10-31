4558f7746b5e077d7bac8da632a09773bcc85a9f
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-31 08:18:07 -0400

    Update VM config script

--- shell/bash/functions/os/linux/vm/configure.sh
+++ shell/bash/functions/os/linux/vm/configure.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
+# FIXME RETHINK THE VARIABLE AND FLAG SUPPORT HERE.
+# NEED BIOCONDUCTOR BASE IMAGE BUILT ON DEBIAN.
+# --no-r
+# --no-python
+# --no-r-packages
+# --no-python-packages
+# --bioconductor
+# --bioconductor-base
+
 koopa::configure_vm() { # {{{1
     # """
     # Configure virtual machine.
-    # @note Updated 2020-10-29.
+    # @note Updated 2020-10-31.
     # """
-    local app_prefix app_prefix_bn app_prefix_real bioconductor data_disk \
-        data_disk_link data_disk_real docker full gb_total \
-        install_base_flags make_prefix minimal pos prefixes r_version rsync \
-        source_ip
+    local app_prefix app_prefix_bn app_prefix_real bioconductor \
+        data_disk data_disk_link data_disk_real docker full \
+        gb_total install_base_flags install_perl_packages install_python \
+        install_python_packages install_r install_r_packages \
+        install_ruby_packages install_rust_packages make_prefix minimal pos \
+        prefixes python_version r_version rsync source_ip
     koopa::assert_has_no_envs
     # Bioconductor mode, for continuous integration (CI) checks.
+    # This flag is intended installing different R/BioC versions on top of the
+    # Bioconductor (Debian) base image.
     bioconductor=0
-    # Are we building from source inside Docker?
-    docker=0
     # By default, this script skips installation of GNU utils and other
     # dependencies that can take a long time to build from source. This works
     # well for Docker builds but may not support additional software required on
@@ -22,24 +33,52 @@ koopa::configure_vm() { # {{{1
     # Minimal config used for lightweight Docker images. This mode skips all
     # program installation.
     minimal=0
-    # Used for app configuration.
-    data_disk=
-    # Skip rsync mode by default.
-    rsync=0
-    # Used for rsync mode.
-    source_ip=
-    # Set the desired R version automatically.
+
+    install_base_flags=()
+
+    install_aspera_connect=0
+    install_aws_cli=1
+    install_conda=1
+    install_conda_envs=0
+    install_llvm=0
+    install_lmod=0
+    install_openjdk=1
+    install_perl_packages=0
+    install_python=1
+    install_python_packages=1
+    install_r=1
+    install_r_packages=1
+    install_rstudio_server=0
+    install_ruby_packages=0
+    install_rust_packages=0
+    install_shiny_server=0
+
+    passwordless_sudo=1
+    remove_skel=1
+    ssh_key=1
+
+    # Which conda to install (miniconda or anaconda).
+    conda='miniconda'
+
+    # Python version.
+    python_version="$(koopa::variable 'python')"
+    # R version.
     r_version="$(koopa::variable 'r')"
+
+    # Data disk to use for app configuration. Intended for full AWS EC2 or
+    # Azure VM instances.
+    data_disk=
+    # Source IP (for rsync mode).
+    source_ip=
+
+    # Parse arguments.
     pos=()
     while (("$#"))
     do
         case "$1" in
+            # Modes ------------------------------------------------------------
             --bioconductor)
                 bioconductor=1
-                shift 1
-                ;;
-            --data-disk=*)
-                data_disk="${1#*=}"
                 shift 1
                 ;;
             --full)
@@ -50,14 +89,34 @@ koopa::configure_vm() { # {{{1
                 minimal=1
                 shift 1
                 ;;
-            --r-version=*)
-                r_version="${1#*=}"
+            # Rsync settings ---------------------------------------------------
+            --data-disk=*)
+                data_disk="${1#*=}"
                 shift 1
                 ;;
             --source-ip=*)
                 source_ip="${1#*=}"
                 shift 1
                 ;;
+            # Versions ---------------------------------------------------------
+            --python-version=*)
+                python_version="${1#*=}"
+                shift 1
+                ;;
+            --r-version=*)
+                r_version="${1#*=}"
+                shift 1
+                ;;
+            # Overrides --------------------------------------------------------
+            --no-passwordless-sudo)
+                passwordless_sudo=0
+                shift 1
+                ;;
+            --no-remove-skel)
+                remove_skel=0
+                shift 1
+                ;;
+            # Additional parsing -----------------------------------------------
             '')
                 shift 1
                 ;;
@@ -76,8 +135,56 @@ koopa::configure_vm() { # {{{1
     done
     [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
     koopa::assert_has_no_args "$#"
-    [[ -n "$source_ip" ]] && rsync=1
-    koopa::is_docker && docker=1
+
+    # FIXME CHECK FOR INPUT OF SINGLE MODE FLAG.
+
+    # Are we building from source inside Docker?
+    koopa::is_docker && docker=1 || docker=0
+    # Are we rsyncing files from a source machine?
+    [[ -n "$source_ip" ]] && rsync=1 || rsync=0
+    if [[ "$docker" -eq 1 ]]
+    then
+        ssh_key=0
+    fi
+    if [[ "$bioconductor" -eq 1 ]]
+    then
+        install_aws_cli=0
+        install_llvm=1
+        install_python_packages=0
+        install_r=0
+        install_r_packages=0
+        install_rstudio_server=1
+        install_shiny_server=0
+    elif [[ "$full" -eq 1 ]]
+    then
+        conda='anaconda'
+        install_aspera_connect=1
+        install_conda_envs=1
+        install_llvm=1
+        install_lmod=1
+        install_perl_packages=1
+        install_rstudio_server=1
+        install_ruby_packages=1
+        install_rust_packages=1
+        install_shiny_server=1
+        install_base_flags+=('--full')
+    fi
+    if [[ "$rsync" -eq 1 ]]
+    then
+        install_aspera_connect=0
+        install_conda=0
+        install_conda_envs=0
+        install_perl_packages=0
+        install_python_packages=0
+        install_r_packages=0
+        install_ruby_packages=0
+        install_rust_packages=0
+    fi
+    # e.g. '/usr/local'.
+    make_prefix="$(koopa::make_prefix)"
+    # For data disk, linked to '/mnt/data01/n/opt' (see below).
+    # e.g. '/usr/local/opt'.
+    app_prefix="$(koopa::app_prefix)"
 
     # Initial configuration {{{2
     # --------------------------------------------------------------------------
@@ -92,8 +199,11 @@ koopa::configure_vm() { # {{{1
     # Root user and sudo fixes {{{3
     # --------------------------------------------------------------------------
 
-    koopa::enable_passwordless_sudo
-    koopa::fix_sudo_setrlimit_error
+    if [[ "$passwordless_sudo" -eq 1 ]]
+    then
+        koopa::enable_passwordless_sudo
+        koopa::fix_sudo_setrlimit_error
+    fi
 
     # Remove skeleton files {{{3
     # --------------------------------------------------------------------------
@@ -101,7 +211,10 @@ koopa::configure_vm() { # {{{1
     # Remove default user-specific skeleton configuration files. This in
     # particular helps keep shell configuration consistent, especialy for
     # Ubuntu, which sets a lot of config in bashrc.
-    koopa::rm -S '/etc/skel'
+    if [[ "$remove_skel" -eq 1 ]]
+    then
+        koopa::rm -S '/etc/skel'
+    fi
 
     # Early return in minimal mode {{{3
     # --------------------------------------------------------------------------
@@ -111,15 +224,6 @@ koopa::configure_vm() { # {{{1
         koopa::success 'Configuration completed successfully.'
         return 0
     fi
-
-    # Koopa paths {{{3
-    # --------------------------------------------------------------------------
-
-    # e.g. '/usr/local'.
-    make_prefix="$(koopa::make_prefix)"
-    # For data disk, linked to '/mnt/data01/n/opt' (see below).
-    # e.g. '/usr/local/opt'.
-    app_prefix="$(koopa::app_prefix)"
 
     # Local disk configuration {{{3
     # --------------------------------------------------------------------------
@@ -172,53 +276,31 @@ koopa::configure_vm() { # {{{1
     koopa::install_dotfiles
     koopa::mkdir "$make_prefix"
     koopa::assert_is_installed install-base
-    install_base_flags=()
-    [[ "$full" -eq 1 ]] && install_base_flags+=('--full')
     install-base "${install_base_flags[@]:-}"
     koopa::assert_is_installed \
-        'autoconf' \
-        'bc' \
-        'bzip2' \
-        'g++' \
-        'gcc' \
-        'gfortran' \
-        'gzip' \
-        'make' \
-        'man' \
-        'msgfmt' \
-        'tar' \
-        'unzip' \
-        'xml2-config' \
-        'xz'
-    koopa::assert_is_file \
-        '/usr/bin/gcc' \
-        '/usr/bin/g++'
+        'autoconf'    'bc'   'bzip2' 'g++'    'gcc' 'gfortran' \
+        'gzip'        'make' 'man'   'msgfmt' 'tar' 'unzip' \
+        'xml2-config' 'xz'
+    koopa::assert_is_file '/usr/bin/gcc' '/usr/bin/g++'
     sudo ldconfig
 
     # rsync mode {{{2
     # --------------------------------------------------------------------------
 
-    [[ "$rsync" -eq 1 ]] && koopa::rsync_vm --source-ip="$source_ip"
+    [[ "$rsync" -eq 1 ]] && \
+        koopa::rsync_vm --source-ip="$source_ip"
 
     # Programs {{{2
     # --------------------------------------------------------------------------
 
-    install-python
-    if [[ "$full" -eq 1 ]]
-    then
-        install-anaconda
-    else
-        install-miniconda
-    fi
-    # > if [[ "$bioconductor" -eq 1 ]]
-    # > then
-    # >     conda-create-bioinfo-envs --reticulate
-    # > fi
-    install-openjdk
-    if [[ "$bioconductor" -eq 1 ]] || [[ "$full" -eq 1 ]]
-    then
+    [[ "$install_python" -eq 1 ]] && \
+        install-python --version="$python_version"
+    [[ "$install_conda" -eq 1 ]] && \
+        "install-${conda}"
+    [[ "$install_openjdk" -eq 1 ]] && \
+        install-openjdk
+    [[ "$install_llvm" -eq 1 ]] && \
         koopa::run_if_installed install-llvm
-    fi
     if [[ "$full" -eq 1 ]]
     then
         install-curl
@@ -241,15 +323,9 @@ koopa::configure_vm() { # {{{1
         install-parallel
         install-rsync
         install-sed
-    fi
-    if [[ "$full" -eq 1 ]] || koopa::is_rhel_ubi
-    then
         install-libevent
         install-taglib
         install-zsh
-    fi
-    if [[ "$full" -eq 1 ]]
-    then
         install-bash
         install-fish
         install-git
@@ -257,16 +333,10 @@ koopa::configure_vm() { # {{{1
         install-perl
         install-geos
         install-sqlite
-    fi
-    if [[ "$full" -eq 1 ]] || koopa::is_rhel_ubi
-    then
         install-proj
         install-gdal
         install-hdf5
         install-gsl
-    fi
-    if [[ "$full" -eq 1 ]]
-    then
         install-udunits
         install-subversion
         install-go
@@ -280,7 +350,8 @@ koopa::configure_vm() { # {{{1
     install-vim
     install-shellcheck
     install-shunit2
-    install-aws-cli
+    [[ "$install_aws_cli" -eq 1 ]] && \
+        install-aws-cli
     if [[ "$full" -eq 1 ]]
     then
         koopa::run_if_installed \
@@ -300,54 +371,55 @@ koopa::configure_vm() { # {{{1
         # > install-gcc
     fi
     # Install R.
-    if koopa::is_fedora
+    if [[ "$install_r" -eq 1 ]]
     then
-        koopa::assert_is_installed R
-    elif [[ "$r_version" == 'devel' ]]
-    then
-        install-r-devel
-    elif koopa::is_installed install-r-cran-binary
-    then
-        install-r-cran-binary --version="$r_version"
-    else
-        install-r --version="$r_version"
+        if koopa::is_fedora
+        then
+            koopa::assert_is_installed R
+        elif [[ "$r_version" == 'devel' ]]
+        then
+            install-r-devel
+        elif koopa::is_installed install-r-cran-binary
+        then
+            install-r-cran-binary --version="$r_version"
+        else
+            install-r --version="$r_version"
+        fi
+        koopa::update_r_config
     fi
-    if [[ "$bioconductor" -eq 1 ]] || [[ "$full" -eq 1 ]]
-    then
-        koopa::run_if_installed install-rstudio-server install-shiny-server
-    fi
-    # Ensure configuration is current.
-    koopa::update_r_config
-    koopa::update_lmod_config
+    [[ "$install_rstudio_server" -eq 1 ]] && \
+        koopa::run_if_installed install-rstudio-server
+    [[  "$install_shiny_server" -eq 1 ]] && \
+        koopa::run_if_installed install-shiny-server
+    [[ "$install_lmod" -eq 1 ]] && \
+        koopa::update_lmod_config
     sudo ldconfig
 
     # Language-specific packages {{{2
     # --------------------------------------------------------------------------
 
-    if [[ "$rsync" -eq 0 ]]
-    then
-        if [[ "$bioconductor" -eq 0 ]]
-        then
-            install-python-packages
-        fi
+    [[ "$install_python_packages" -eq 1 ]] && \
+        install-python-packages
+    [[ "$install_r_packages" -eq 1 ]] && \
         install-r-packages
-        if [[ "$full" -eq 1 ]]
-        then
-            install-perl-packages
-            install-ruby-packages
-            install-rust-packages
-        fi
-    fi
+    [[ "$install_python_packages" -eq 1 ]] && \
+        install-python-packages
+    [[ "$install_r_packages" -eq 1 ]] && \
+        install-r-packages
+    [[ "$install_perl_packages" -eq 1 ]] && \
+        install-perl-packages
+    [[ "$install_ruby_packages" -eq 1 ]] && \
+        install-ruby-packages
+    [[ "$install_rust_packages" -eq 1 ]] && \
+        install-rust-packages
 
     # Bioinformatics tools {{{2
     # --------------------------------------------------------------------------
 
-    if [[ "$full" -eq 1 ]] && [[ "$rsync" -eq 0 ]]
-    then
+    [[ "$install_aspera_connect" -eq 1 ]] && \
         install-aspera-connect
+    [[ "$install_conda_envs" -eq 1 ]] && \
         conda-create-bioinfo-envs
-        # > install-bcbio
-    fi
 
     # Final steps and clean up {{{2
     # --------------------------------------------------------------------------
@@ -355,7 +427,8 @@ koopa::configure_vm() { # {{{1
     # Generate SSH key {{{3
     # --------------------------------------------------------------------------
 
-    [[ "$docker" -eq 0 ]] && koopa::generate_ssh_key
+    [[ "$ssh_key" -eq 1 ]] && \
+        koopa::generate_ssh_key
 
     # Remove legacy packages {{{3
     # --------------------------------------------------------------------------
@@ -365,7 +438,7 @@ koopa::configure_vm() { # {{{1
         "${app_prefix}/perl" \
         "${app_prefix}/python/pyenv" \
         "${app_prefix}/perl"
-    # Otherwise, ensure permissions are correct:
+    # Otherwise, ensure permissions are correct.
     # > koopa::fix_pyenv_permissions
     # > koopa::fix_rbenv_permissions
 
