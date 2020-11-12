abd1c252dd215521912f2ac3b5e0f5f6b0ce0039
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-12 12:03:47 -0500

    Improve gitignore

---
+++ include/cellar/rsync.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+
+file="${name}-${version}.tar.gz"
+url="https://download.samba.org/pub/${name}/src/${file}"
+koopa::download "$url"
+koopa::extract "$file"
+koopa::cd "${name}-${version}"
+flags=(
+    "--prefix=${prefix}"
+    # '--without-included-zlib'
+    '--disable-zstd'
+)
+if koopa::is_rhel_like
+then
+    flags+=('--disable-xxhash')
+fi
+./configure "${flags[@]}"
+make --jobs="$jobs"
+make install
