91e80870df7e1817c117f7eab0d24217ad3f3861
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-12 12:01:20 -0500

    Reorganize the cellar scripts

--- os/linux/include/cellar/password-store.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2154
-
-file="${name}-${version}.tar.xz"
-url="https://git.zx2c4.com/${name}/snapshot/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-PREFIX="$prefix" make install
--- os/linux/include/cellar/pyenv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2154
-
-koopa::mkdir "$prefix"
-git clone https://github.com/pyenv/pyenv.git "$prefix"
-
