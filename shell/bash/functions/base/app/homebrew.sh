af99f7ed6e13251724117c0e4dd1bdaa5da4f95f
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-12 11:34:00 -0500

    Consolidate Homebrew scripts

--- shell/bash/functions/base/app/homebrew.sh
+++ shell/bash/functions/base/app/homebrew.sh
@@ -1,39 +1,5 @@
 #!/usr/bin/env bash
 
-koopa::brew_cask_outdated() { # {{{
-    # """
-    # List outdated Homebrew casks.
-    # @note Updated 2020-09-08.
-    #
-    # Need help with capturing output:
-    # - https://stackoverflow.com/questions/58344963/
-    # - https://unix.stackexchange.com/questions/253101/
-    #
-    # Syntax changed from 'brew cask outdated' to 'brew outdated --cask' in
-    # 2020-09.
-    #
-    # @seealso
-    # - brew leaves
-    # - brew deps --installed --tree
-    # - brew list --versions
-    # - brew info
-    # """
-    local tmp_file x
-    koopa::assert_has_no_args "$#"
-    koopa::assert_is_installed brew
-    tmp_file="$(koopa::tmp_file)"
-    script -q "$tmp_file" brew outdated --cask --greedy >/dev/null
-    x="$(grep -v '(latest)' "$tmp_file")"
-    [[ -n "$x" ]] || return 0
-    koopa::print "$x"
-    return 0
-}
-
-koopa::brew_cask_quarantine_fix() { # {{{1
-    sudo xattr -r -d com.apple.quarantine /Applications/*.app
-    return 0
-}
-
 koopa::brewfile() { # {{{1
     # """
     # Homebrew Bundle Brewfile path.
@@ -62,61 +28,11 @@ koopa::brew_outdated() { # {{{
     brew update &>/dev/null
     koopa::h2 'Brews'
     brew outdated
-    koopa::h2 'Casks'
-    koopa::brew_cask_outdated
-    return 0
-}
-
-koopa::brew_update() { # {{{1
-    # """
-    # Updated outdated Homebrew brews and casks.
-    # @note Updated 2020-11-06.
-    #
-    # Alternative approaches:
-    # > brew list \
-    # >     | xargs brew reinstall --force-bottle --cleanup \
-    # >     || true
-    # > brew outdated --cask --greedy \
-    # >     | xargs brew reinstall \
-    # >     || true
-    #
-    # @seealso
-    # Refer to useful discussion regarding '--greedy' flag.
-    # https://discourse.brew.sh/t/brew-cask-outdated-greedy/3391
-    # """
-    local casks name_fancy x
-    koopa::assert_has_no_args "$#"
-    koopa::assert_is_installed brew
-    name_fancy='Homebrew'
-    koopa::update_start "$name_fancy"
-    brew analytics off
-    brew update >/dev/null
-    koopa::h2 'Updating brews.'
-    # Use of '--force-bottle' flag here can be helpful, but not all brews have
-    # bottles, so this can error.
-    brew upgrade || true
-    koopa::h2 'Updating casks.'
-    readarray -t casks <<< "$(koopa::brew_cask_outdated)"
-    if koopa::is_array_non_empty "${casks[@]}"
+    if koopa::is_macos
     then
-        koopa::info "${#casks[@]} outdated casks detected."
-        koopa::print "${casks[@]}"
-        for cask in "${casks[@]}"
-        do
-            cask="$(koopa::print "${cask[@]}" | cut -d ' ' -f 1)"
-            case "$cask" in
-                docker)
-                    cask='homebrew/cask/docker'
-                    ;;
-            esac
-            brew reinstall "$cask" || true
-        done
+        koopa::h2 'Casks'
+        koopa::macos_brew_cask_outdated
     fi
-    koopa::h2 'Running cleanup.'
-    brew cleanup -s || true
-    koopa::rm "$(brew --cache)"
-    koopa::update_r_config
-    koopa::update_success "$name_fancy"
     return 0
 }
 
@@ -163,23 +79,6 @@ koopa::install_homebrew() { # {{{1
     ) 2>&1 | tee "$(koopa::tmp_log_file)"
     koopa::rm "$tmp_dir"
     koopa::install_success "$name_fancy"
-    return 0
-}
-
-koopa::macos_install_homebrew_little_snitch() { # {{{1
-    # """
-    # Install Little Snitch via Homebrew Cask.
-    # @note Updated 2020-07-17.
-    # """
-    local dmg_file version
-    koopa::assert_has_no_args "$#"
-    koopa::assert_is_installed hdiutil open
-    version="$(koopa::extract_version "$(brew cask info little-snitch)")"
-    dmg_file="$(koopa::homebrew_prefix)/Caskroom/little-snitch/\
-${version}/LittleSnitch-${version}.dmg"
-    koopa::assert_is_file "$dmg_file"
-    hdiutil attach "$dmg_file" &>/dev/null
-    open "/Volumes/Little Snitch ${version}/Little Snitch Installer.app"
     return 0
 }
 
@@ -254,12 +153,59 @@ koopa::uninstall_homebrew() { # {{{1
     return 0
 }
 
-koopa::macos_update_homebrew() { # {{{1
+koopa::update_homebrew() { # {{{1
     # """
-    # Update Homebrew.
-    # @note Updated 2020-07-30.
+    # Updated outdated Homebrew brews and casks.
+    # @note Updated 2020-11-12.
+    #
+    # Use of '--force-bottle' flag can be helpful, but not all brews have
+    # bottles, so this can error.
+    #
+    # Alternative approaches:
+    # > brew list \
+    # >     | xargs brew reinstall --force-bottle --cleanup \
+    # >     || true
+    # > brew outdated --cask --greedy \
+    # >     | xargs brew reinstall \
+    # >     || true
+    #
+    # @seealso
+    # - Refer to useful discussion regarding '--greedy' flag.
+    # - https://discourse.brew.sh/t/brew-cask-outdated-greedy/3391
     # """
-    koopa::is_installed brew || return 0
-    koopa::brew_update "$@"
+    local casks name_fancy
+    koopa::assert_has_no_args "$#"
+    koopa::assert_is_installed brew
+    name_fancy='Homebrew'
+    koopa::update_start "$name_fancy"
+    brew analytics off
+    brew update >/dev/null
+    koopa::h2 'Updating brews.'
+    brew upgrade || true
+    if koopa::is_macos
+    then
+        koopa::h2 'Updating casks.'
+        readarray -t casks <<< "$(koopa::macos_brew_cask_outdated)"
+        if koopa::is_array_non_empty "${casks[@]}"
+        then
+            koopa::info "${#casks[@]} outdated casks detected."
+            koopa::print "${casks[@]}"
+            for cask in "${casks[@]}"
+            do
+                cask="$(koopa::print "${cask[@]}" | cut -d ' ' -f 1)"
+                case "$cask" in
+                    docker)
+                        cask='homebrew/cask/docker'
+                        ;;
+                esac
+                brew reinstall "$cask" || true
+            done
+        fi
+    fi
+    koopa::h2 'Running cleanup.'
+    brew cleanup -s || true
+    koopa::rm "$(brew --cache)"
+    koopa::update_r_config
+    koopa::update_success "$name_fancy"
     return 0
 }
