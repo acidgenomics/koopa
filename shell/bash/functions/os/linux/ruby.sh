84e90a06f27bced672107c773057e03c2dbc1328
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-12 11:51:45 -0500

    Clean up RStudio functions

--- shell/bash/functions/os/linux/ruby.sh
+++ shell/bash/functions/os/linux/ruby.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-koopa::update_rbenv() { # {{{1
+koopa::linux_update_rbenv() { # {{{1
     # """
     # Update rbenv.
-    # @note Updated 2020-07-30.
+    # @note Updated 2020-11-12.
     # """
     local rbenv_root
-    koopa::is_installed rbenv || return 0
     koopa::assert_has_no_args "$#"
+    koopa::assert_is_linux
     koopa::assert_has_no_envs
+    koopa::is_installed rbenv || return 0
     koopa::h1 'Updating rbenv.'
     rbenv_root="$(rbenv root)"
     koopa::assert_is_dir "$rbenv_root"
