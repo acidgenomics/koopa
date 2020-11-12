682a062226c53023d64f9bb26af1c9ccb39027a3
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-12 12:03:47 -0500

    Improve gitignore

---
+++ include/cellar/bash.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+
+minor_version="$(koopa::major_minor_version "$version")"
+file="${name}-${minor_version}.tar.gz"
+url="${gnu_mirror}/${name}/${file}"
+koopa::download "$url"
+koopa::extract "$file"
+koopa::cd "${name}-${minor_version}"
+# Apply patches. 
+patches="$(koopa::print "$version" | cut -d '.' -f 3)"
+koopa::mkdir patches
+(
+    koopa::cd patches
+    # Note that GNU mirror doesn't seem to work correctly here.
+    base_url="https://ftp.gnu.org/gnu/${name}/${name}-${minor_version}-patches"
+    mv_tr="$(koopa::print "$minor_version" | tr -d '.')"
+    range="$(printf '%03d-%03d' '1' "$patches")"
+    request="${base_url}/${name}${mv_tr}-[${range}]"
+    curl "$request" -O
+    koopa::cd ..
+    for file in 'patches/'*
+    do
+        koopa::info "Applying patch '${file}'."
+        # Alternatively, can pipe curl call directly to 'patch -p0'.
+        # https://stackoverflow.com/questions/14282617
+        patch -p0 --ignore-whitespace --input="$file"
+    done
+)
+./configure --prefix="$prefix"
+make --jobs="$jobs"
+# > make test
+make install
+koopa::enable_shell "$name"
---
+++ include/cellar/binutils.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+
+koopa::assert_is_installed makeinfo  # texinfo
+file="${name}-${version}.tar.xz"
+url="${gnu_mirror}/${name}/${file}"
+koopa::download "$url"
+koopa::extract "$file"
+koopa::cd "${name}-${version}"
+./configure --prefix="$prefix"
+make --jobs="$jobs"
+# > make check
+make install
