0446158042c8aa0787850506d953bc6aa8a460e4
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-16 16:48:53 -0500

    Rework scripts

--- include/cellar/hdf5.sh
+++ include/cellar/hdf5.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
+if koopa::is_macos
+then
+    koopa::add_to_path_start '/usr/local/gfortran/bin'
+fi
 koopa::assert_is_installed gfortran
-
 minor_version="$(koopa::major_minor_version "$version")"
 file="${name}-${version}.tar.gz"
 url="https://support.hdfgroup.org/ftp/HDF5/releases/${name}-${minor_version}/\
