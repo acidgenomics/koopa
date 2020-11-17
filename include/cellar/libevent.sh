672884e79845b38000264dcb1bee4d9ba3614f5b
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-17 13:59:09 -0500

    Add macOS support

--- include/cellar/libevent.sh
+++ include/cellar/libevent.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
+if koopa::is_macos
+then
+    koopa::assert_is_installed brew
+    openssl_pkgconfig="$(brew --prefix)/opt/openssl@1.1/lib/pkgconfig"
+    koopa::assert_is_dir "$openssl_pkgconfig"
+    koopa::add_to_pkg_config_path_start "$openssl_pkgconfig"
+fi
 file="${name}-${version}-stable.tar.gz"
 url="https://github.com/${name}/${name}/releases/download/\
 release-${version}-stable/${file}"
