f39df7b58d57b02c4e11f1cf73ed2cf108170d88
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-17 18:09:34 -0500

    Update Rscript

--- shell/bash/functions/common/system/koopa/check-system.sh
+++ shell/bash/functions/common/system/koopa/check-system.sh
@@ -5,12 +5,8 @@ koopa::check_system() { # {{{1
     # Check system.
     # @note Updated 2020-11-17.
     # """
-    local script
     koopa::assert_has_no_args "$#"
-    koopa::assert_is_installed Rscript
-    script="$(koopa::rscript_prefix)/check-system.R"
-    koopa::assert_is_file "$script"
-    Rscript --vanilla "$script"
+    koopa::rscript_vanilla 'check-system'
     koopa::check_exports
     koopa::check_disk
     koopa::check_data_disk
