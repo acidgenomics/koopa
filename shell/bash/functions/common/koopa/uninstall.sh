4d9348dbae93de8919bacf771e1f28bf42907fb3
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-18 13:42:40 -0500

    Improve uninstaller

--- shell/bash/functions/common/koopa/uninstall.sh
+++ shell/bash/functions/common/koopa/uninstall.sh
@@ -5,19 +5,13 @@ koopa::uninstall() { # {{{1
     # Uninstall commands.
     # @note Updated 2020-11-18.
     # """
-    local fun name
+    local name
     name="${1:-}"
     if [[ -z "$name" ]]
     then
         koopa::stop 'Program name to uninstall is required.'
     fi
-    fun="koopa::uninstall_${name//-/_}"
-    if ! koopa::is_function "$fun"
-    then
-        koopa::stop "No uninstall script available for '${*}'."
-    fi
-    shift 1
-    "$fun" "$@"
+    koopa::_run_function "uninstall_${name}"
     return 0
 }
 
