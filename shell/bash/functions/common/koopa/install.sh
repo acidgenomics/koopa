7c8cc9c29251cb911a8fe8a10930f6e7a4da7885
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-18 13:38:42 -0500

    Improve installer parsing

--- shell/bash/functions/common/koopa/install.sh
+++ shell/bash/functions/common/koopa/install.sh
@@ -5,29 +5,13 @@ koopa::install() { # {{{1
     # Install commands.
     # @note Updated 2020-11-18.
     # """
-    local f fun name
+    local name
     name="${1:-}"
     if [[ -z "$name" ]]
     then
         koopa::stop 'Program name to install is required.'
     fi
-    f="install_${name//-/_}"
-    # FIXME MAKE THIS A FUNCTION.
-    if koopa::is_macos && koopa::is_function "koopa::macos_${f}"
-    then
-        fun="koopa::macos_${f}"
-    elif koopa::is_linux && koopa::is_function "koopa::linux_${f}"
-    then
-        fun="koopa::linux_${f}"
-    else
-        fun="koopa::${f}"
-    fi
-    if ! koopa::is_function "$fun"
-    then
-        koopa::stop "No install script available for '${*}'."
-    fi
-    shift 1
-    "$fun" "$@"
+    koopa::_run_function "install_${name}"
     return 0
 }
 
