04e0be8a55c8a89bd2188d201a65ce7e2dcf1ef4
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-20 10:48:02 -0500

    Rework config scripts

--- shell/bash/functions/os/darwin/configure-system.sh
+++ shell/bash/functions/os/darwin/configure-system.sh
@@ -3,18 +3,18 @@
 koopa::macos_configure_system() { # {{{1
     # """
     # Configure macOS system.
-    # @note Updated 2020-11-18.
+    # @note Updated 2020-11-20.
     # """
     koopa::assert_has_no_args "$#"
     koopa::assert_has_sudo
     koopa::h1 'Configuring macOS system.'
     koopa::enable_passwordless_sudo
-    koopa::macos_install_homebrew
-    koopa::macos_install_homebrew_packages
+    koopa::install_homebrew
+    koopa::install_homebrew_packages
     koopa::install_conda
-    koopa::macos_install_r_cran_gfortran
     koopa::install_python_packages
-    install-r-packages
+    koopa::macos_install_r_cran_gfortran
+    koopa::install_r_packages
     koopa::macos_update_defaults
     koopa::success 'macOS configuration was successful.'
     return 0
