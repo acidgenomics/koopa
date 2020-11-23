68fe5db00105ed635df9614390a896e56a24b0ce
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-23 10:29:03 -0500

    Improve r-koopa installer

--- shell/bash/functions/common/koopa/install.sh
+++ shell/bash/functions/common/koopa/install.sh
@@ -15,29 +15,23 @@ koopa::install() { # {{{1
     return 0
 }
 
-# FIXME DONT ALLOW ARGUMENT PASSTHROUGH HERE.
 koopa::install_py_koopa() { # {{{1
     # """
     # Install Python koopa package.
-    # @note Updated 2020-08-12.
+    # @note Updated 2020-11-23.
     # """
     local url
-    url='https://github.com/acidgenomics/koopa/archive/python.tar.gz'
     koopa::python_add_site_packages_to_sys_path
-    koopa::pip_install "$@" "$url"
+    url='https://github.com/acidgenomics/koopa/archive/python.tar.gz'
+    koopa::pip_install "$url"
     return 0
 }
 
 koopa::install_r_koopa() { # {{{1
     # """
     # Install koopa R package.
-    # @note Updated 2020-11-17.
+    # @note Updated 2020-11-23.
     # """
-    local script
-    koopa::assert_has_no_args "$#"
-    koopa::is_installed Rscript || return 0
-    script="$(koopa::rscript_prefix)/install.R"
-    koopa::assert_is_file "$script"
-    Rscript "$script"
+    koopa::rscript "install-r-koopa"
     return 0
 }
