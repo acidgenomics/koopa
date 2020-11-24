b0d8040eb02cb79f75c9c7c5e297eb66f5617c67
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-24 08:38:40 -0500

    Improve opt linkage

--- shell/bash/functions/common/install/conda.sh
+++ shell/bash/functions/common/install/conda.sh
@@ -3,7 +3,7 @@
 koopa::_install_conda() { # {{{1
     # """
     # Install Conda (or Anaconda).
-    # @note Updated 2020-11-23.
+    # @note Updated 2020-11-24.
     #
     # Assuming installation of Miniconda by default.
     #
@@ -11,7 +11,7 @@ koopa::_install_conda() { # {{{1
     # `conda env list` will return multiprocessing error.
     # https://github.com/conda/conda/issues/9589
     # """
-    local anaconda name_fancy ostype script tmp_dir url version
+    local anaconda name name_fancy ostype prefix script tmp_dir url version
     if koopa::is_installed conda
     then
         koopa::note 'Conda is already installed.'
@@ -52,19 +52,25 @@ koopa::_install_conda() { # {{{1
                 ;;
         esac
     done
-    prefix="$(koopa::conda_prefix)"
-    [[ -d "$prefix" ]] && return 0
     if [[ "$anaconda" -eq 1 ]]
     then
-        [[ -z "$version" ]] && version="$(koopa::variable 'anaconda')"
+        name='anaconda'
         name_fancy='Anaconda'
+        [[ -z "$version" ]] && version="$(koopa::variable "$name")"
         script="Anaconda3-${version}-${ostype}-x86_64.sh"
         url="https://repo.anaconda.com/archive/${script}"
     else
-        [[ -z "$version" ]] && version="$(koopa::variable 'conda')"
+        name='conda'
         name_fancy='Miniconda'
+        [[ -z "$version" ]] && version="$(koopa::variable "$name")"
         script="Miniconda3-py37_${version}-${ostype}-x86_64.sh"
         url="https://repo.continuum.io/miniconda/${script}"
+    fi
+    prefix="$(koopa::app_prefix)/${name}/${version}"
+    if [[ -d "$prefix" ]]
+    then
+        koopa::note "${name_fancy} is already installed at '${prefix}'."
+        return 0
     fi
     koopa::install_start "$name_fancy" "$prefix"
     koopa::mkdir "$prefix"
@@ -75,9 +81,12 @@ koopa::_install_conda() { # {{{1
         bash "$script" -bf -p "$prefix"
     ) 2>&1 | tee "$(koopa::tmp_log_file)"
     koopa::rm "$tmp_dir"
-    koopa::ln "$(koopa::prefix)/os/linux/etc/conda/condarc" "${prefix}/.condarc"
+    koopa::ln \
+        "$(koopa::prefix)/os/linux/etc/conda/condarc" \
+        "${prefix}/.condarc"
     koopa::delete_broken_symlinks "$prefix"
     koopa::sys_set_permissions -r "$prefix"
+    koopa::link_opt "$prefix" 'conda'
     koopa::install_success "$name_fancy"
     koopa::restart
     return 0
@@ -89,6 +98,17 @@ koopa::install_anaconda() { # {{{1
     # @note Updated 2020-10-27.
     # """
     koopa::_install_conda --anaconda "$@"
+    return 0
+}
+
+koopa::install_conda() { # {{{1
+    # """
+    # Install Conda.
+    # @note Updated 2020-11-24.
+    #
+    # Assuming user wants Miniconda by default.
+    # """
+    koopa::install_miniconda "$@"
     return 0
 }
 
