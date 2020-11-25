d2100bd800a52b1b3a2ede4743674e2e55e8d1cd
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-25 07:50:38 -0500

    Reorganize TeX install scripts

---
+++ shell/bash/functions/common/update/tex.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+koopa::update_tex() { # {{{1
+    # """
+    # Update TeX
+    # @note Updated 2020-11-18.
+    # """
+    koopa::assert_has_no_args "$#"
+    koopa::assert_is_installed tlmgr
+    koopa::assert_has_sudo
+    koopa::h1 'Updating TeX Live.'
+    sudo tlmgr update --self
+    sudo tlmgr update --list
+    sudo tlmgr update --all
+    return 0
+}
