af33416d409e37b8f333e6672161dd49cc0bdf56
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-03 16:18:25 -0500

    Improve auto-completer

--- shell/bash/functions/common/koopa/list.sh
+++ shell/bash/functions/common/koopa/list.sh
@@ -4,12 +4,13 @@
 koopa::list() { # {{{1
     # """
     # List exported koopa scripts.
-    # @note Updated 2020-08-12.
+    # @note Updated 2020-12-03.
     # """
     local name
     name="${1:-}"
     case "$name" in
         '')
+            # FIXME THIS NEEDS TO EXCLUDE 'APP' and 'OPT' BETTER.
             koopa::rscript_vanilla 'list'
             ;;
         app-versions)
