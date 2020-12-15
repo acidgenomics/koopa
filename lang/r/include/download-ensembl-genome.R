3cb123c33ddb59727b6c2f92f2148ac60988bca0
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-14 20:30:59 -0500

    parseArgs is erroring on spaces

--- lang/r/include/download-ensembl-genome.R
+++ lang/r/include/download-ensembl-genome.R
@@ -7,8 +7,6 @@ file <- sub(pattern = "^--file=", replacement = "", x = file)
 koopaPrefix <- normalizePath(file.path(dirname(file), "..", "..", ".."))
 source(file.path(koopaPrefix, "lang", "r", "include", "header.R"))
 
-requireNamespaces("AcidGenomes")
-
 # required:
 # - organism
 # - genomeBuild
@@ -28,20 +26,24 @@ requireNamespaces("AcidGenomes")
 # positional
 
 local({
+    # FIXME THIS IS ERRORING WHEN ARGUMENT CONTAINS SPACES.
     input <- parseArgs(
         required = c(
-            "organism",
-            "genome-build"
+            "organism"
+            #"organism",
+            #"genome-build"
         ),
         optional = c(
             "release",
             "type",
             "annotation",
             "output-dir"
-        )
+        ),
         flags = "decompress",
         positional = FALSE
     )
+    return(input)
+
     args <- list()
     args[["organism"]] <- input[["organism"]]
     args[["genomeBuild"]] <- input[["genome-build"]]
@@ -61,6 +63,7 @@ local({
     if (isSubset("decompress", names(input[["flags"]]))) {
         args[["decompress"]] <- TRUE
     }
+    library(AcidGenomes)
     do.call(
         what = AcidGenomes::downloadEnsemblGenome,
         args = args
