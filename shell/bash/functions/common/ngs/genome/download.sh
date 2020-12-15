e80395bb622af66adc4c73d47afab7a83cb9d6d2
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-14 20:11:57 -0500

    Convert these to internal R scripts

--- shell/bash/functions/common/ngs/genome/download.sh
+++ shell/bash/functions/common/ngs/genome/download.sh
@@ -3,39 +3,39 @@
 koopa::download_ensembl_genome() { # {{{1
     # """
     # Download Ensembl genome.
-    # @note Updated 2020-11-19.
+    # @note Updated 2020-12-14.
     # """
     koopa::assert_has_args "$#"
-    koopa::pyscript 'download-ensembl-genome' "$@"
+    koopa::rscript 'download-ensembl-genome' "$@"
     return 0
 }
 
 koopa::download_flybase_genome() { # {{{1
     # """
     # Download FlyBase genome.
-    # @note Updated 2020-11-19.
+    # @note Updated 2020-12-14.
     # """
     koopa::assert_has_args "$#"
-    koopa::pyscript 'download-flybase-genome' "$@"
+    koopa::rscript 'download-flybase-genome' "$@"
     return 0
 }
 
 koopa::download_gencode_genome() { # {{{1
     # """
     # Download GENCODE genome.
-    # @note Updated 2020-11-19.
+    # @note Updated 2020-12-14.
     # """
     koopa::assert_has_args "$#"
-    koopa::pyscript 'download-gencode-genome' "$@"
+    koopa::rscript 'download-gencode-genome' "$@"
     return 0
 }
 
 koopa::download_refseq_genome() { # {{{1
     # """
     # Download RefSeq genome.
-    # @note Updated 2020-11-19.
+    # @note Updated 2020-12-14.
     # """
     koopa::assert_has_args "$#"
-    koopa::pyscript 'download-refseq-genome' "$@"
+    koopa::rscript 'download-refseq-genome' "$@"
     return 0
 }
