222b494891f6881e15a006208ce27ce6065181cb
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-24 10:25:08 -0500

    Update Homebrew cask version parsing

--- shell/bash/functions/os/darwin/install/homebrew.sh
+++ shell/bash/functions/os/darwin/install/homebrew.sh
@@ -3,14 +3,14 @@
 koopa::macos_install_homebrew_little_snitch() { # {{{1
     # """
     # Install Little Snitch via Homebrew Cask.
-    # @note Updated 2020-07-17.
+    # @note Updated 2020-12-24.
     # """
     local dmg_file version
     koopa::assert_has_no_args "$#"
     koopa::assert_is_macos
     koopa::assert_has_sudo
     koopa::assert_is_installed hdiutil open
-    version="$(koopa::extract_version "$(brew cask info little-snitch)")"
+    version="$(koopa::extract_version "$(brew info --cask little-snitch)")"
     dmg_file="$(koopa::homebrew_prefix)/Caskroom/little-snitch/\
 ${version}/LittleSnitch-${version}.dmg"
     koopa::assert_is_file "$dmg_file"
