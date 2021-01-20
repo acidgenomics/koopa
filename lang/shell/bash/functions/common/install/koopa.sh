810cb3c0310b318535be7fe394d32629d2af5198
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-20 08:25:26 -0500

    Rework installers

--- lang/shell/bash/functions/common/install/koopa.sh
+++ lang/shell/bash/functions/common/install/koopa.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# FIXME NEED TO UPDATE THE URL HERE.
 koopa::install_py_koopa() { # {{{1
     # """
     # Install Python koopa package.
-    # @note Updated 2020-11-23.
+    # @note Updated 2021-01-20.
     # """
     local url
     koopa::python_add_site_packages_to_sys_path
-    url='https://github.com/acidgenomics/koopa/archive/python.tar.gz'
+    url='https://github.com/acidgenomics/py-koopa/archive/master.zip'
     koopa::pip_install "$url"
     return 0
 }
