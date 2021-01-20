ee6712a10794a022c80d6f3023cfe08511a73039
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-20 11:10:01 -0500

    Rework bcbio-vm install

--- lang/shell/bash/functions/os/linux/common/install/bcbio.sh
+++ lang/shell/bash/functions/os/linux/common/install/bcbio.sh
@@ -7,8 +7,8 @@ koopa::linux_install_bcbio() { # {{{1
     # """
     local file install_dir name name_fancy prefix \
         python tmp_dir tools_dir url version
-    name='bcbio'
-    name_fancy='bcbio-nextgen'
+    name='bcbio-nextgen'
+    name_fancy="$name"
     version="$(koopa::current_bcbio_version)"
     prefix="$(koopa::app_prefix)/${name}/${version}"
     if [[ -d "$prefix" ]]
@@ -204,12 +204,12 @@ koopa::linux_install_bcbio_vm() { # {{{1
     # Install bcbio-vm.
     # @note Updated 2021-01-20.
     # """
-    local bin_dir data_dir file name name_fancy prefix tmp_dir url
+    local bin_dir conda file make_bin_dir make_prefix name name_fancy prefix url
     koopa::assert_has_no_envs
-    koopa::assert_is_installed docker
-    name='bcbio-vm'
-    name_fancy='bcbio-nextgen-vm'
-    version='rolling'
+    koopa::assert_is_installed conda docker
+    name='bcbio-nextgen-vm'
+    name_fancy="$name_fancy"
+    version="$(koopa::conda_env_latest_version "$name")"
     prefix="$(koopa::app_prefix)/${version}/${name}"
     if [[ -d "$prefix" ]]
     then
@@ -217,8 +217,6 @@ koopa::linux_install_bcbio_vm() { # {{{1
         return 0
     fi
     koopa::install_start "$name_fancy" "$prefix"
-    bin_dir="${prefix}/anaconda/bin"
-    tmp_dir="$(koopa::tmp_dir)"
     # Configure Docker, if necessary.
     if ! koopa::str_match "$(groups)" 'docker'
     then
@@ -228,6 +226,7 @@ koopa::linux_install_bcbio_vm() { # {{{1
         newgrp docker
     fi
     # Download and install Conda.
+    tmp_dir="$(koopa::tmp_dir)"
     (
         koopa::cd "$tmp_dir"
         file='Miniconda3-latest-Linux-x86_64.sh'
@@ -237,22 +236,26 @@ koopa::linux_install_bcbio_vm() { # {{{1
     )
     koopa::rm "$tmp_dir"
     # Ready to install bcbio-vm.
-    "${bin_dir}/conda" install --yes \
+    bin_dir="${prefix}/anaconda/bin"
+    conda="${bin_dir}/conda"
+    "$conda" install --yes \
         --channel='conda-forge' \
         --channel='bioconda' \
-        bcbio-nextgen \
-        bcbio-nextgen-vm
-    koopa::ln -S "${bin_dir}/bcbio_vm.py" '/usr/local/bin/bcbio_vm.py'
-    koopa::ln -S "${bin_dir}/conda" '/usr/local/bin/bcbiovm_conda'
-    sudo chgrp docker '/usr/local/bin/bcbio_vm.py'
-    sudo chmod g+s '/usr/local/bin/bcbio_vm.py'
-    # v1.1.3:
-    # > data_dir="${prefix}/v1.1.3"
-    # > image='quay.io/bcbio/bcbio-vc:1.1.3-v1.1.3'
-    # latest version:
-    data_dir="${prefix}/latest"
-    # > image='quay.io/bcbio/bcbio-vc'
-    "${bin_dir}/bcbio_vm.py" --datadir="$data_dir" saveconfig
+        'bcbio-nextgen' \
+        'bcbio-nextgen-vm'
+    # Symlink into '/usr/local'.
+    make_prefix="$(koopa::make_prefix)"
+    make_bin_dir="${make_prefix}/bin"
+    koopa::ln -S "${bin_dir}/bcbio_vm.py" "${make_bin_dir}/bcbio_vm.py"
+    sudo chgrp docker "${make_bin_dir}/bcbio_vm.py"
+    sudo chmod g+s "${make_bin_dir}/bcbio_vm.py"
+    # Install pinned bcbio-nextgen v1.2.4:
+    # > data_dir="${prefix}/v1.2.4"
+    # > image='quay.io/bcbio/bcbio-vc:1.2.4-517bb34'
+    # Install latest version of bcbio-nextgen:
+    # > data_dir="${prefix}/latest"
+    # > image='quay.io/bcbio/bcbio-vc:latest'
+    # > "${bin_dir}/bcbio_vm.py" --datadir="$data_dir" saveconfig
     # > "${bin_dir}/bcbio_vm.py" install --tools --image "$image"
     koopa::link_opt "$prefix" "$name"
     koopa::install_success "$name_fancy"
