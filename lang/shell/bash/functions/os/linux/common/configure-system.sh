83fb7123a0184970e84fdfa353154b297dd1ef38
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-15 17:10:25 -0500

    Argument fix

--- lang/shell/bash/functions/os/linux/common/configure-system.sh
+++ lang/shell/bash/functions/os/linux/common/configure-system.sh
@@ -138,7 +138,7 @@ koopa::linux_configure_system() { # {{{1
                 mode='bioconductor'
                 shift 1
                 ;;
-            --default)
+            --default|--recommended)
                 mode='default'
                 shift 1
                 ;;
@@ -148,10 +148,6 @@ koopa::linux_configure_system() { # {{{1
                 ;;
             --minimal)
                 mode='minimal'
-                shift 1
-                ;;
-            --recommended)
-                mode='default'
                 shift 1
                 ;;
             # Other variables --------------------------------------------------
