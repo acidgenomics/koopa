7cfdb22e3e65db3615789f53644f955397208ae5
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-16 07:42:14 -0500

    RHEL/CentOS 8 rework

--- lang/shell/bash/functions/os/linux/distro/rhel/base.sh
+++ lang/shell/bash/functions/os/linux/distro/rhel/base.sh
@@ -3,7 +3,7 @@
 koopa::rhel_install_base() { # {{{1
     # """
     # Install Red Hat Enterprise Linux (RHEL) base system.
-    # @note Updated 2020-08-06.
+    # @note Updated 2021-02-16.
     #
     # 'dnf-plugins-core' installs 'config-manager'.
     # """
@@ -12,11 +12,11 @@ koopa::rhel_install_base() { # {{{1
     name_fancy='Red Hat Enterprise Linux (RHEL) base system'
     koopa::install_start "$name_fancy"
     sudo dnf -y install 'dnf-plugins-core' 'util-linux-user'
+    koopa::rhel_enable_epel
     if ! koopa::is_rhel_ubi
     then
         sudo dnf config-manager --set-enabled 'PowerTools'
     fi
-    koopa::rhel_enable_epel
     koopa::fedora_install_base "$@"
     koopa::install_success "$name_fancy"
     return 0
