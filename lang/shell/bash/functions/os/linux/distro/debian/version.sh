b2adef9a7c7dc0123e968a99ba4b04f8e3361805
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-16 15:18:40 -0500

    Add Debian version utility

---
+++ lang/shell/bash/functions/os/linux/distro/debian/version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+koopa::debian_version() {
+    # """
+    # Debian version (including minor version).
+    # @note Updated 2021-02-16.
+    # """
+    local file x
+    file='/etc/debian_version'
+    koopa::assert_is_file "$file"
+    x="$(cat "$file")"
+    koopa::print "$x"
+    return 0
+}
