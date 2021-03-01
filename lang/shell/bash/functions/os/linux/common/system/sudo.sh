87e988bebf8190744325dca813dc065529017595
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-01 14:18:21 -0500

    Improve sudo configuration scripts

---
+++ lang/shell/bash/functions/os/linux/common/system/sudo.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+koopa::linux_fix_sudo_setrlimit_error() { # {{{1
+    # """
+    # Fix bug in recent version of sudo.
+    # @note Updated 2021-03-01.
+    #
+    # This is popping up on Docker builds:
+    # sudo: setrlimit(RLIMIT_CORE): Operation not permitted
+    #
+    # @seealso
+    # - https://ask.fedoraproject.org/t/
+    #       sudo-setrlimit-rlimit-core-operation-not-permitted/4223
+    # - https://bugzilla.redhat.com/show_bug.cgi?id=1773148
+    # """
+    local file
+    koopa::assert_has_no_args "$#"
+    koopa::assert_is_docker
+    string='Set disable_coredump false'
+    file='/etc/sudo.conf'
+    koopa::sudo_append_string "$string" "$file"
+    return 0
+}
