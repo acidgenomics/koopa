817499c9362d0118312e12bab24d8157ff138452
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-01 11:21:36 -0500

    Consolidate installer scripts

---
+++ lang/shell/bash/functions/common/install/r.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+koopa::install_r_cmd_check() { # {{{1
+    # """
+    # Install R CMD check (Rcheck) scripts for CI.
+    # @note Updated 2021-03-01.
+    # """
+    local link_name name source_repo target_dir
+    koopa::assert_has_no_args "$#"
+    name='r-cmd-check'
+    source_repo="https://github.com/acidgenomics/${name}.git"
+    target_dir="$(koopa::local_data_prefix)/${name}"
+    link_name=".Rcheck"
+    koopa::install_start "$name"
+    if [[ ! -d "$target_dir" ]]
+    then
+        koopa::alert "Downloading ${name} to '${target_dir}'."
+        (
+            koopa::mkdir "$target_dir"
+            git clone "$source_repo" "$target_dir"
+        )
+    fi
+    koopa::ln "$target_dir" "$link_name"
+    koopa::install_success "$name"
+    return 0
+}
+
+koopa::install_r_packages() { # {{{1
+    # """
+    # Install R packages.
+    # @note Updated 2021-01-19.
+    # """
+    koopa::h1 'Installing R packages.'
+    koopa::rscript 'installRPackages' "$@"
+    return 0
+}
+
+koopa::update_r_packages() { # {{{1
+    # """
+    # Update R packages.
+    # @note Updated 2021-01-19.
+    # """
+    koopa::h1 'Updating R packages.'
+    koopa::rscript 'updateRPackages' "$@"
+    return 0
+}
--- lang/shell/bash/functions/common/install/tex.sh
+++ lang/shell/bash/functions/common/install/tex.sh
@@ -52,3 +52,18 @@ koopa::install_tex_packages() { # {{{1
     done
     return 0
 }
+
+koopa::update_tex() { # {{{1
+    # """
+    # Update TeX.
+    # @note Updated 2020-11-18.
+    # """
+    koopa::assert_has_no_args "$#"
+    koopa::assert_is_installed tlmgr
+    koopa::assert_has_sudo
+    koopa::h1 'Updating TeX Live.'
+    sudo tlmgr update --self
+    sudo tlmgr update --list
+    sudo tlmgr update --all
+    return 0
+}
