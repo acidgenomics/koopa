46ac8def54bb6ca10a5ee9e8940691876738f65c
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-01 11:57:18 -0500

    Reorganize functions

--- lang/shell/bash/functions/os/linux/common/version.sh
+++ lang/shell/bash/functions/os/linux/common/version.sh
@@ -12,21 +12,3 @@ koopa::linux_version() { # {{{1
     koopa::print "$x"
     return 0
 }
-
-koopa::lmod_version() { # {{{1
-    # """
-    # Lmod version.
-    # @note Updated 2020-06-29.
-    #
-    # Alterate approach:
-    # > module --version 2>&1 \
-    # >     | grep -Eo 'Version [.0-9]+' \
-    # >     | cut -d ' ' -f 2
-    # """
-    local x
-    koopa::assert_has_no_args "$#"
-    x="${LMOD_VERSION:-}"
-    [[ -n "$x" ]] || return 1
-    koopa::print "$x"
-    return 0
-}
