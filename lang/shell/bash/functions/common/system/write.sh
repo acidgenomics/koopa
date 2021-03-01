fd0854c55cca767d9fbc3c66c7689c637c3b51b5
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-01 14:00:55 -0500

    Improve file writer functions

--- lang/shell/bash/functions/common/system/write.sh
+++ lang/shell/bash/functions/common/system/write.sh
@@ -1,17 +1,59 @@
 #!/usr/bin/env bash
 
+koopa::append_string() { # {{{1
+    # """
+    # Append a string at end of file.
+    # @note Updated 2021-03-01.
+    # """
+    local file string
+    koopa::assert_has_args_eq "$#" 2
+    string="${1:?}"
+    file="${2:?}"
+    koopa::mkdir "$(dirname "$file")"
+    koopa::print "$string" >> "$file"
+    return 0
+}
+
+koopa::sudo_append_string() { # {{{1
+    # """
+    # Append a string at end of file as root user.
+    # @note Updated 2021-03-01.
+    #
+    # Alternative approach:
+    # > sudo sh -c "printf '%s\n' '$string' >> '${file}'"
+    # """
+    local file string
+    koopa::assert_has_args_eq "$#" 2
+    koopa::assert_has_sudo
+    koopa::assert_is_installed tee
+    string="${1:?}"
+    file="${2:?}"
+    if [[ ! -d "$(dirname "$file")" ]]
+    then
+        koopa::mkdir -S "$(dirname "$file")"
+    fi
+    koopa::print "$string" | sudo tee -a "$file" >/dev/null
+    return 0
+}
+
 koopa::sudo_write_string() { # {{{1
     # """
     # Write a string to disk using root user.
-    # @note Updated 2020-07-06.
+    # @note Updated 2021-03-01.
     #
-    # Alternatively, 'tee -a' can be used to append file.
+    # Alternative approach:
+    # > sudo sh -c "printf '%s\n' '$string' > '${file}'"
     # """
-    koopa::assert_has_args_eq "$#" 2
     local file string
+    koopa::assert_has_args_eq "$#" 2
+    koopa::assert_has_sudo
+    koopa::assert_is_installed tee
     string="${1:?}"
     file="${2:?}"
-    koopa::mkdir -S "$(dirname "$file")"
+    if [[ ! -d "$(dirname "$file")" ]]
+    then
+        koopa::mkdir -S "$(dirname "$file")"
+    fi
     koopa::print "$string" | sudo tee "$file" >/dev/null
     return 0
 }
@@ -19,10 +61,10 @@ koopa::sudo_write_string() { # {{{1
 koopa::write_string() { # {{{1
     # """
     # Write a string to disk.
-    # @note Updated 2020-08-06.
+    # @note Updated 2021-03-01.
     # """
-    koopa::assert_has_args_eq "$#" 2
     local file string
+    koopa::assert_has_args_eq "$#" 2
     string="${1:?}"
     file="${2:?}"
     koopa::mkdir "$(dirname "$file")"
