5f45b34e7424adf7d1ce9fad630780039087a030
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-24 17:35:08 -0400

    Improve Debian base configuration

--- lang/shell/bash/functions/os/linux/distro/debian/install/base.sh
+++ lang/shell/bash/functions/os/linux/distro/debian/install/base.sh
@@ -3,18 +3,12 @@
 koopa::debian_install_base() { # {{{1
     # """
     # Install Debian base system.
-    # @note Updated 2020-11-10.
+    # @note Updated 2021-03-24.
     #
-    # Flags:
-    # --compact
-    #     This is intended for compact mode configuration in 'configure-vm'
-    #     script, which when disabled builds a lot of GCC packages and other
-    #     programs from source, rather than relying on system binary packages.
-    #
-    # Backup:
+    # Backup package configuration:
     # > sudo dpkg --get-selections > /tmp/dpkglist.txt
     #
-    # Restore:
+    # Restore package configuration:
     # > sudo dpkg --set-selections < /tmp/dpkglist.txt
     # > sudo apt-get -y update
     # > sudo apt-get dselect-upgrade
@@ -28,19 +22,39 @@ koopa::debian_install_base() { # {{{1
     # - How to replicate installed packages across machines.
     #   https://serverfault.com/questions/56848
     # """
-    local apt_installed dev enabled_repos full legacy_pkgs name_fancy pkg pkgs \
-        remove remove_pkgs upgrade
+    local apt_installed legacy_pkgs name_fancy pkg pkgs pos remove_pkgs
     koopa::assert_is_installed apt apt-get sed sudo
-    dev=1
-    full=0
-    remove=0
-    upgrade=1
+    declare -A dict=(
+        [apt_enabled_repos]="$(koopa::apt_enabled_repos)"
+        [apt_installed]="$(sudo apt list --installed 2>/dev/null)"
+        [base]=1
+        [dev]=1
+        [extra]=0
+        [recommended]=1
+        [remove_legacy]=0
+        [upgrade]=1
+    )
     pos=()
     while (("$#"))
     do
         case "$1" in
+            --base-image)
+                dict[base]=1
+                dict[dev]=0
+                dict[extra]=0
+                dict[recommended]=0
+                dict[upgrade]=0
+                ;;
             --full)
-                full=1
+                dict[base]=1
+                dict[dev]=1
+                dict[extra]=1
+                dict[recommended]=1
+                dict[upgrade]=1
+                shift 1
+                ;;
+            --remove-legacy)
+                dict[remove_legacy]=1
                 shift 1
                 ;;
             "")
@@ -61,45 +75,35 @@ koopa::debian_install_base() { # {{{1
     done
     [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
     koopa::assert_has_no_args "$#"
-    [[ "$full" -eq 1 ]] && remove=1
     name_fancy='Debian base system'
     koopa::install_start "$name_fancy"
     # Nuke caches before installing packages.
-    koopa::rm -S '/var/cache/apt/'* '/var/lib/dpkg/available'
+    koopa::rm -S \
+        '/var/cache/apt/'* \
+        '/var/lib/dpkg/available'
     sudo dpkg --clear-avail
     # Debian symlinks '/usr/local/man' to '/usr/local/share/man' by default,
     # which is non-standard and can cause koopa's application link script
     # to break.
     [[ -L '/usr/local/man' ]] && koopa::rm -S '/usr/local/man'
     # Requiring universe repo to be enabled on Ubuntu.
-    enabled_repos="$(koopa::apt_enabled_repos)"
-    if koopa::is_ubuntu && ! koopa::str_match "$enabled_repos" 'universe'
+    if koopa::is_ubuntu && \
+        ! koopa::str_match "${dict[apt_enabled_repos]}" universe
     then
         koopa::stop \
             "The Ubuntu 'universe' repo is disabled." \
             "Check '/etc/apt/sources.list'."
     fi
-
-    # Upgrade {{{2
-    # --------------------------------------------------------------------------
-
-    if [[ "$upgrade" -eq 1 ]]
+    if [[ "${dict[upgrade]}" -eq 1 ]]
     then
         koopa::h2 "Upgrading install via 'dist-upgrade'."
         koopa::apt_get dist-upgrade
     fi
-
-    # Remove packages {{{2
-    # --------------------------------------------------------------------------
-
-    if [[ "$remove" -eq 1 ]]
+    if [[ "${dict[remove_legacy]}" -eq 1 ]]
     then
-        koopa::h2 'Removing legacy packages.'
-        # Alternative approach:
-        # > apt_installed="$(dpkg --get-selections | grep -v deinstall)"
-        # See also:
-        # - https://askubuntu.com/questions/17823
-        apt_installed="$(sudo apt list --installed 2> /dev/null)"
+        # This step is only recommended when cleaning up a persistent virtual
+        # machine that may have a number of old packages installed.
+        koopa::alert 'Removing legacy packages (not generally recommended).'
         legacy_pkgs=(
             'cargo'                   # use 'install-rust'
             'containerd'              # docker legacy
@@ -119,7 +123,7 @@ koopa::debian_install_base() { # {{{1
         remove_pkgs=()
         for pkg in "${legacy_pkgs[@]}"
         do
-            if koopa::str_match_regex "$apt_installed" "^${pkg}/"
+            if koopa::str_match_regex "${dict[apt_installed]}" "^${pkg}/"
             then
                 remove_pkgs+=("$pkg")
             fi
@@ -129,87 +133,97 @@ koopa::debian_install_base() { # {{{1
             sudo apt-get --yes remove "${remove_pkgs[@]}"
         fi
     fi
-
-    # Default {{{2
-    # --------------------------------------------------------------------------
-
-    koopa::h2 'Installing default packages.'
+    # These packages are required to install koopa.
     pkgs=(
-        'apt-listchanges'
-        'apt-transport-https'
-        'apt-utils'
-        'autoconf'
-        'automake'
         'bash'
         'bc'
-        'build-essential'
-        'byacc'
-        'bzip2'
         'ca-certificates'
-        'cmake'
         'coreutils'
         'curl'
-        'diffutils'
-        'dirmngr'
-        'file'
         'findutils'
-        'fortran77-compiler'
-        'g++'
-        'gcc'
-        'gdb'
-        'gdebi-core'
-        'gettext'
-        'gfortran'
         'git'
-        'gnupg'
-        'gpg-agent'
-        'htop'
-        'less'
-        'libtool'
-        'libtool-bin'
-        'locales'
         'lsb-release'
-        'make'
-        'man-db'
-        'nano'
-        'parallel'
-        'pkg-config'
-        'procps'  # ps
-        'psmisc'  # RStudio Server
-        'rsync'
-        'ruby'  # Homebrew
-        'software-properties-common'
-        'subversion'
         'sudo'
-        'texinfo'  # makeinfo
-        'tmux'
-        'tree'
-        'tzdata'
-        'udunits-bin'
-        'unzip'
-        'vim'
-        'wget'
-        'xz-utils'
-        'zip'
-        'zsh'
     )
-
-    # Developer {{{2
-    # --------------------------------------------------------------------------
-
-    if [[ "$dev" -eq 1 ]]
+    # These packages should be included in the Docker base image.
+    if [[ "${dict[base]}" -eq 1 ]]
     then
-        koopa::h2 'Installing developer packages.'
-        if [[ "$full" -eq 0 ]]
-        then
-            pkgs+=(
-                'libgdal-dev'
-                'libgeos-dev'
-                'libproj-dev'
-                'proj-bin'
-            )
-        fi
         pkgs+=(
+            # The 'build-essential' package includes: dpkg-dev, g++, gcc,
+            # libc-dev, and make, which are required to build packages.
+            'build-essential'
+            'bzip2'
+            'gnupg'
+            'less'
+            'libncurses-dev'  # zsh
+            'locales'
+            'tzdata'
+            'unzip'
+            'wget'
+            'xz-utils'
+            'zsh'
+        )
+    fi
+    # These packages will be installed in the Docker recommended image.
+    if [[ "${dict[recommended]}" -eq 1 ]]
+    then
+        pkgs+=(
+            'apt-listchanges'
+            'apt-transport-https'
+            'apt-utils'
+            'autoconf'
+            'automake'
+            'byacc'
+            'cmake'
+            'diffutils'
+            'dirmngr'
+            'file'
+            'fortran77-compiler'
+            'g++'
+            'gcc'
+            'gdb'
+            'gdebi-core'
+            'gettext'
+            'gfortran'
+            'gpg-agent'
+            'htop'
+            'libtool'
+            'libtool-bin'
+            'make'
+            'man-db'
+            'nano'
+            'parallel'
+            'pkg-config'
+            'procps'  # ps
+            'psmisc'  # RStudio Server
+            'rsync'
+            'ruby'  # Homebrew
+            'software-properties-common'
+            'subversion'
+            'sudo'
+            'texinfo'  # makeinfo
+            'tmux'
+            'tree'
+            'udunits-bin'
+            'vim'
+            'zip'
+        )
+    fi
+    # Only include these when not building GDAL, GEOS, and PROJ from source,
+    # which are enabled in full mode.
+    if [[ "${dict[dev]}" -eq 1 ]] && [[ "${dict[extra]}" -eq 0 ]]
+    then
+        pkgs+=(
+            'libgdal-dev'
+            'libgeos-dev'
+            'libproj-dev'
+            'proj-bin'
+        )
+    fi
+    if [[ "${dict[dev]}" -eq 1 ]]
+    then
+        pkgs+=(
+            'dpkg-dev'
             'libacl1-dev'
             'libapparmor-dev'
             'libapr1-dev'  # subversion
@@ -244,8 +258,8 @@ koopa::debian_install_base() { # {{{1
             'liblz4-dev'  # rsync
             'liblzma-dev'
             'libmagick++-dev'
-            # 'libmariadb-dev'
-            # 'libmysqlclient-dev'  # Conflicts with libmariadb-dev (Ubuntu)
+            # > 'libmariadb-dev'
+            # > 'libmysqlclient-dev'  # Conflicts with libmariadb-dev (Ubuntu)
             'libmodule-build-perl'
             'libmpc-dev'
             'libmpfr-dev'
@@ -287,15 +301,10 @@ koopa::debian_install_base() { # {{{1
             'zlib1g-dev'
         )
     fi
-
-    # Extra {{{2
-    # --------------------------------------------------------------------------
-
-    if [[ "$full" -eq 1 ]]
+    if [[ "${dict[extra]}" -eq 1 ]]
     then
-        koopa::h2 'Installing extra recommended packages.'
         pkgs+=(
-            # default-jdk
+            # > default-jdk
             'alien'
             'biber'
             'ggobi'
@@ -324,18 +333,13 @@ koopa::debian_install_base() { # {{{1
         )
         if koopa::is_ubuntu
         then
-            koopa::alert 'Installing Ubuntu-specific packages.'
             pkgs+=('firefox')
         fi
     fi
-
-    # Install packages {{{2
-    # --------------------------------------------------------------------------
-
     koopa::apt_install "${pkgs[@]}"
     koopa::apt_configure_sources
-    sudo apt-get --yes clean
-    sudo apt-get --yes autoremove
+    koopa::apt_clean
+    koopa::debian_set_locale
     koopa::install_success "$name_fancy"
     return 0
 }
