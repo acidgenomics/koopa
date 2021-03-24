47e273424ce6ac5c7806be786826d007bcbd783a
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-24 17:48:10 -0400

    Improve base image support for Alpine and Debian

--- lang/shell/bash/functions/os/linux/distro/alpine/install/base.sh
+++ lang/shell/bash/functions/os/linux/distro/alpine/install/base.sh
@@ -3,48 +3,119 @@
 koopa::alpine_install_base() { # {{{1
     # """
     # Install Alpine Linux base system.
-    # @note Updated 2020-07-16.
+    # @note Updated 2021-03-24.
     #
     # Potentially useful flags:
     # > apk add --no-cache --virtual .build-dependencies
     # """
-    local name_fancy packages
+    local dict name_fancy pkgs
     koopa::assert_is_installed apk sudo
+
+    declare -A dict=(
+        [base]=1
+        [dev]=1
+        [extra]=0
+        [recommended]=1
+        [upgrade]=1
+    )
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            --base-image)
+                dict[base]=1
+                dict[dev]=0
+                dict[extra]=0
+                dict[recommended]=0
+                dict[upgrade]=0
+                shift 1
+                ;;
+            --full)
+                dict[base]=1
+                dict[dev]=1
+                dict[extra]=1
+                dict[recommended]=1
+                dict[upgrade]=1
+                shift 1
+                ;;
+            "")
+                shift 1
+                ;;
+            --)
+                shift 1
+                break
+                ;;
+            --*|-*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa::assert_has_no_args "$#"
     name_fancy='Alpine base system'
     koopa::install_start "$name_fancy"
-    sudo apk update
-    sudo apk upgrade
-    packages=(
-        # .build-dependencies
-        # R-dev
-        # R-doc
-        # fish
-        # pandoc
-        # pandoc-citeproc
-        # texlive
-        # zsh
-        'R'
-        'autoconf'
+    sudo apk --no-cache update
+    if [[ "${dict[upgrade]}" -eq 1 ]]
+    then
+        koopa::alert 'Upgrading system.'
+        sudo apk --no-cache upgrade
+    fi
+    # These packages are required to install koopa.
+    pkgs=(
         'bash'
-        'bash-completion'
-        'build-base'
+        'bc'
         'ca-certificates'
+        'coreutils'
         'curl'
-        'dpkg'
-        'gettext'
+        'findutils'
         'git'
-        'libevent-dev'
-        'libffi-dev'
-        'libxml2-dev'
-        'mdocml'
-        'openssl'
         'shadow'
         'sudo'
-        'tcl'
-        'tree'
-        'wget'
     )
-    sudo apk add --no-cache --virtual "${packages[@]}"
+    # These packages should be included in the Docker base image.
+    if [[ "${dict[base]}" -eq 1 ]]
+    then
+        pkgs+=(
+            'autoconf'
+            'bash-completion'
+            'binutils'
+            'build-base'
+            'dpkg'
+            'zsh'
+        )
+    fi
+    # These packages will be installed in the Docker recommended image.
+    if [[ "${dict[recommended]}" -eq 1 ]]
+    then
+        pkgs+=(
+            # > R-dev
+            # > R-doc
+            # > fish
+            # > pandoc
+            # > pandoc-citeproc
+            # > texlive
+            'R'
+            'gettext'
+            'mdocml'
+            'openssl'
+            'tcl'
+            'tree'
+            'wget'
+        )
+    fi
+    if [[ "${dict[dev]}" -eq 1 ]]
+    then
+        pkgs+=(
+            'libevent-dev'
+            'libffi-dev'
+            'libxml2-dev'
+        )
+    fi
+    sudo apk --no-cache add "${pkgs[@]}"
     koopa::install_success "$name_fancy"
     return 0
 }
