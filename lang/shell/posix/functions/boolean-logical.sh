98cb042d2d2069dcf957fea531e950efeb558162
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-25 16:45:38 -0400

    Improve RHEL 7 config handling

--- lang/shell/posix/functions/boolean-logical.sh
+++ lang/shell/posix/functions/boolean-logical.sh
@@ -417,6 +417,14 @@ _koopa_is_rhel_ubi() { # {{{
     [ -f '/etc/yum.repos.d/ubi.repo' ]
 }
 
+_koopa_is_rhel_7_like() { # {{{1
+    # """
+    # Is the operating system RHEL 7-like?
+    # @note Updated 2021-03-25.
+    # """
+    _koopa_is_rhel_like && _koopa_is_os_version 7
+}
+
 _koopa_is_rhel_8_like() { # {{{1
     # """
     # Is the operating system RHEL 8-like?
