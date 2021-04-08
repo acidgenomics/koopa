dded5218aabc387dbf1dcefecb4e22e9ad0b3f87
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-08 09:34:03 -0400

    Improve cpufetch install support on macos

--- include/install/cpufetch.sh
+++ include/install/cpufetch.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
-# NOTE This is failing to compile on macOS.
+# NOTE v0.94 stable release fails to compile on macOS.
 # use of undeclared identifier 'cpu_set_t'
+# https://github.com/Dr-Noob/cpufetch/issues/38
 
 koopa::assert_is_installed git make
-file="v${version}.tar.gz"
-url="https://github.com/Dr-Noob/cpufetch/archive/refs/tags/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
+case "$version" in
+    0.96)
+        # No stable release for this yet, use master branch.
+        git clone "https://github.com/Dr-Noob/${name}.git"
+        koopa::cd "$name"
+        ;;
+    *)
+        file="v${version}.tar.gz"
+        url="https://github.com/Dr-Noob/cpufetch/archive/refs/tags/${file}"
+        koopa::download "$url"
+        koopa::extract "$file"
+        koopa::cd "${name}-${version}"
+        ;;
+esac
 PREFIX="$prefix" make --jobs="$jobs"
-make install
+PREFIX="$prefix" make install
