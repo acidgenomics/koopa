5211aed15ffc902c639b60a37a6213cdcb18a8f0
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-20 16:21:45 -0400

    Add spacevim installer

--- lang/shell/bash/functions/common/install/vim.sh
+++ lang/shell/bash/functions/common/install/vim.sh
@@ -6,7 +6,16 @@ koopa::install_spacevim() {
     # @note Updated 2021-04-20.
     # https://spacevim.org
     # """
+    local name_fancy
+    name_fancy="SpaceVim"
+    if [[ -d "${HOME}/.SpaceVim" ]]
+    then
+        koopa::alert_note "${name_fancy} is already installed."
+        return 0
+    fi
+    koopa::install_start "$name_fancy"
     curl -sLf https://spacevim.org/install.sh | bash
+    koopa::install_success "$name_fancy"
     return 0
 }
 
@@ -15,9 +24,18 @@ koopa::uninstall_spacevim() { # {{{1
     # Uninstall SpaceVim.
     # @note Updated 2021-04-20.
     # """
+    local name_fancy
+    name_fancy="SpaceVim"
+    if [[ ! -d "${HOME}/.SpaceVim" ]]
+    then
+        koopa::alert_note "${name_fancy} is not installed."
+        return 0
+    fi
+    koopa::uninstall_start "$name_fancy"
     koopa::rm \
         "${HOME}/.SpaceVim" \
         "${HOME}/.SpaceVim.d" \
         "${HOME}/.cache/SpaceVim"
+    koopa::uninstall_success "$name_fancy"
     return 0
 }
