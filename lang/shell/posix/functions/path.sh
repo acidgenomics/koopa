4c3f7009a849968ba7bc10335a11b2bb867d26dd
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-23 17:22:05 -0400

    Speed up path activation

--- lang/shell/posix/functions/path.sh
+++ lang/shell/posix/functions/path.sh
@@ -1,155 +1,53 @@
 #!/bin/sh
 
-# FIXME THIS IS WORKING BETTER...MUCH FASTER.
-
-__koopa_add_to_fpath_end() { # {{{1
+__koopa_add_to_path_string_end() { # {{{1
     # """
-    # Add directory to end of 'FPATH'.
+    # Add a directory to the beginning of a PATH string.
     # @note Updated 2021-04-23.
     # """
-    [ "$#" -eq 1 ] || return 1
-    FPATH="${FPATH:-}:${1:?}"
-    export FPATH
+    local string dir
+    [ "$#" -eq 2 ] || return 1
+    [ -n "${1:-}" ] || return 0
+    string="${1:?}"
+    dir="${2:?}"
+    if _koopa_str_match_posix "$string" ":${dir}"
+    then
+        string="$(__koopa_remove_from_path_string "$string" "$dir")"
+    fi
+    string="${string}:${dir}"
+    _koopa_print "$string"
     return 0
 }
 
-__koopa_add_to_fpath_start() { # {{{1
+__koopa_add_to_path_string_start() { # {{{1
     # """
-    # Add directory to start of 'FPATH'.
+    # Add a directory to the beginning of a PATH string.
     # @note Updated 2021-04-23.
     # """
-    [ "$#" -eq 1 ] || return 1
-    FPATH="${1:?}:${FPATH:-}"
-    export FPATH
+    local string dir
+    [ "$#" -eq 2 ] || return 1
+    [ -n "${1:-}" ] || return 0
+    string="${1:?}"
+    dir="${2:?}"
+    if _koopa_str_match_posix "$string" ":${dir}"
+    then
+        string="$(__koopa_remove_from_path_string "$string" "$dir")"
+    fi
+    string="${dir}:${string}"
+    _koopa_print "$string"
     return 0
 }
 
-__koopa_add_to_manpath_end() { # {{{1
+__koopa_remove_from_path_string() { # {{{1
     # """
-    # Add directory to end of 'MANPATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    MANPATH="${MANPATH:-}:${1:?}"
-    export MANPATH
-    return 0
-}
-
-__koopa_add_to_manpath_start() { # {{{1
-    # """
-    # Add directory to start of 'MANPATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    MANPATH="${1:?}:${MANPATH:-}"
-    export MANPATH
-    return 0
-}
-
-__koopa_add_to_path_end() { # {{{1
-    # """
-    # Add directory to end of 'PATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    PATH="${PATH:-}:${1:?}"
-    export PATH
-    return 0
-}
-
-__koopa_add_to_path_start() { # {{{1
-    # """
-    # Add directory to start of 'PATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    PATH="${1:?}:${PATH:-}"
-    export PATH
-    return 0
-}
-
-__koopa_add_to_pkg_config_path_end() { # {{{1
-    # """
-    # Add directory to end of 'PKG_CONFIG_PATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}:${1:?}"
-    export PKG_CONFIG_PATH
-    return 0
-}
-
-__koopa_add_to_pkg_config_path_start() { # {{{1
-    # """
-    # Add directory to start of 'PKG_CONFIG_PATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    PKG_CONFIG_PATH="${1:?}:${PKG_CONFIG_PATH:-}"
-    export PKG_CONFIG_PATH
-    return 0
-}
-
-# USE A SHARED FUNCTION HERE TO DO REPLACEMENT EASIER...
-__koopa_remove_from_fpath() { # {{{1
-    # """
-    # Remove directory from 'FPATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    FPATH="$( \
-        _koopa_print "${FPATH:-}" \
-        | sed "s|:${1:?}||g" \
-    )"
-    export FPATH
-    return 0
-}
-
-# USE A SHARED FUNCTION HERE TO DO REPLACEMENT EASIER...
-__koopa_remove_from_manpath() { # {{{1
-    # """
-    # Remove directory from 'MANPATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    MANPATH="$( \
-        _koopa_print "${MANPATH:-}" \
-        | sed "s|:${1:?}||g" \
-    )"
-    export MANPATH
-    return 0
-}
-
-# USE A SHARED FUNCTION HERE TO DO REPLACEMENT EASIER...
-__koopa_remove_from_path() { # {{{1
-    # """
-    # Remove directory from 'PATH'.
+    # Remove directory from PATH string with POSIX conventions.
     # @note Updated 2021-04-23.
     #
     # Alternative non-POSIX approach that works on Bash and Zsh:
     # > PATH="${PATH//:$dir/}"
     # """
-    [ "$#" -eq 1 ] || return 1
-    PATH="$( \
-        _koopa_print "${PATH:-}" \
-        | sed "s|:${1:?}||g" \
-    )"
-    export PATH
-    return 0
-}
-
-# USE A SHARED FUNCTION HERE TO DO REPLACEMENT EASIER...
-__koopa_remove_from_pkg_config_path() { # {{{1
-    # """
-    # Remove directory from 'PKG_CONFIG_PATH'.
-    # @note Updated 2021-04-23.
-    # """
-    [ "$#" -eq 1 ] || return 1
-    PKG_CONFIG_PATH="$( \
-        _koopa_print "${PKG_CONFIG_PATH:-}" \
-        | sed "s|:${1:?}||g" \
-    )"
-    export PKG_CONFIG_PATH
+    [ "$#" -eq 2 ] || return 1
+    _koopa_print "${1:?}" | sed "s|:${2:?}||g"
     return 0
 }
 
@@ -164,10 +62,7 @@ _koopa_add_to_fpath_end() { # {{{1
     for dir in "$@"
     do
         [ -d "$dir" ] || continue
-        _koopa_str_match_posix "$FPATH" ":${dir}" && \
-            # FIXME MAKE THIS MORE GENERAL...
-            __koopa_remove_from_fpath "$dir"
-        __koopa_add_to_fpath_end "$dir"
+        FPATH="$(__koopa_add_to_path_string_end "$FPATH" "$dir")"
     done
     export FPATH
     return 0
@@ -180,13 +75,13 @@ _koopa_add_to_fpath_start() { # {{{1
     # """
     local dir
     [ "$#" -gt 0 ] || return 1
+    FPATH="${FPATH:-}"
     for dir in "$@"
     do
         [ -d "$dir" ] || continue
-        _koopa_str_match_posix "${FPATH:-}" ":${dir}" && \
-            __koopa_remove_from_fpath "$dir"
-        __koopa_add_to_fpath_start "$dir"
+        FPATH="$(__koopa_add_to_path_string_start "$FPATH" "$dir")"
     done
+    export FPATH
     return 0
 }
 
@@ -197,13 +92,13 @@ _koopa_add_to_manpath_end() { # {{{1
     # """
     local dir
     [ "$#" -gt 0 ] || return 1
+    MANPATH="${MANPATH:-}"
     for dir in "$@"
     do
         [ -d "$dir" ] || continue
-        _koopa_str_match_posix "${MANPATH:-}" ":${dir}" && \
-            __koopa_remove_from_manpath "$dir"
-        __koopa_add_to_manpath_end "$dir"
+        MANPATH="$(__koopa_add_to_path_string_end "$MANPATH" "$dir")"
     done
+    export MANPATH
     return 0
 }
 
@@ -214,13 +109,13 @@ _koopa_add_to_manpath_start() { # {{{1
     # """
     local dir
     [ "$#" -gt 0 ] || return 1
+    MANPATH="${MANPATH:-}"
     for dir in "$@"
     do
         [ -d "$dir" ] || continue
-        _koopa_str_match_posix "${MANPATH:-}" ":${dir}" && \
-            __koopa_remove_from_manpath "$dir"
-        __koopa_add_to_manpath_start "$dir"
+        MANPATH="$(__koopa_add_to_path_string_start "$MANPATH" "$dir")"
     done
+    export MANPATH
     return 0
 }
 
@@ -231,13 +126,13 @@ _koopa_add_to_path_end() { # {{{1
     # """
     local dir
     [ "$#" -gt 0 ] || return 1
+    PATH="${PATH:-}"
     for dir in "$@"
     do
         [ -d "$dir" ] || continue
-        _koopa_str_match_posix "${PATH:-}" ":${dir}" && \
-            __koopa_remove_from_path "$dir"
-        __koopa_add_to_path_end "$dir"
+        PATH="$(__koopa_add_to_path_string_end "$PATH" "$dir")"
     done
+    export PATH
     return 0
 }
 
@@ -248,12 +143,13 @@ _koopa_add_to_path_start() { # {{{1
     # """
     local dir
     [ "$#" -gt 0 ] || return 1
+    PATH="${PATH:-}"
     for dir in "$@"
     do
-        _koopa_str_match_posix "${PATH:-}" ":${dir}" && \
-            __koopa_remove_from_path "$dir"
-        __koopa_add_to_path_start "$dir"
+        [ -d "$dir" ] || continue
+        PATH="$(__koopa_add_to_path_string_start "$PATH" "$dir")"
     done
+    export PATH
     return 0
 }
 
@@ -264,13 +160,15 @@ _koopa_add_to_pkg_config_path_end() { # {{{1
     # """
     local dir
     [ "$#" -gt 0 ] || return 1
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}"
     for dir in "$@"
     do
         [ -d "$dir" ] || continue
-        _koopa_str_match_posix "${PKG_CONFIG_PATH:-}" ":${dir}" && \
-            __koopa_remove_from_pkg_config_path "$dir"
-        __koopa_add_to_pkg_config_path_end "$dir"
+        PKG_CONFIG_PATH="$( \
+            __koopa_add_to_path_string_end "$PKG_CONFIG_PATH" "$dir" \
+        )"
     done
+    export PKG_CONFIG_PATH
     return 0
 }
 
@@ -281,12 +179,14 @@ _koopa_add_to_pkg_config_path_start() { # {{{1
     # """
     local dir
     [ "$#" -gt 0 ] || return 1
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}"
     for dir in "$@"
     do
         [ -d "$dir" ] || continue
-        _koopa_str_match_posix "${PKG_CONFIG_PATH:-}" ":${dir}" && \
-            __koopa_remove_from_pkg_config_path "$dir"
-        __koopa_add_to_pkg_config_path_start "$dir"
+        PKG_CONFIG_PATH="$( \
+            __koopa_add_to_path_string_start "$PKG_CONFIG_PATH" "$dir" \
+        )"
     done
+    export PKG_CONFIG_PATH
     return 0
 }
