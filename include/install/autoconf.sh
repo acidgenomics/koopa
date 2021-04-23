e3e7cbf1b390adb7cfbb5938e067b36fde14b09d
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-22 21:19:54 -0400

    Draft update, passing shellcheck errors

--- include/install/autoconf.sh
+++ include/install/autoconf.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+gnu_mirror="${INSTALL_GNU_MIRROR:?}"
+jobs="${INSTALL_JOBS:?}"
+name="${INSTALL_NAME:?}"
+prefix="${INSTALL_PREFIX:?}"
+version="${INSTALL_VERSION:?}"
+
 file="${name}-${version}.tar.xz"
 url="${gnu_mirror}/${name}/${file}"
 koopa::download "$url"
