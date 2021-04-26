2cc57987ebe672115a40f847b84504e093e3a655
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-26 15:22:09 -0400

    Update coreutils

--- include/install/coreutils.sh
+++ include/install/coreutils.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-file="${name}-${version}.tar.xz"
-url="${gnu_mirror}/${name}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-# > make check
-make install
+install_coreutils() { # {{{1
+    # """
+    # Install GNU coreutils.
+    # @note Updated 2021-04-26.
+    # """
+    local file gnu_mirror jobs name prefix url version
+    gnu_mirror="${INSTALL_GNU_MIRROR:?}"
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.xz"
+    url="${gnu_mirror}/${name}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make check
+    make install
+    return 0
+}
+
+install_coreutils "$@"
