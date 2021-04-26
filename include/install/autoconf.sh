961cc9da18adbb527b0d4b1cb4b4b8db808dfa1d
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-26 14:24:58 -0400

    Update autoconf

--- include/install/autoconf.sh
+++ include/install/autoconf.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-# FIXME Rework the variable handling in these install scripts.
+install_autoconf() { # {{{1
+    local file gnu_mirror jobs name prefix url version
+    gnu_mirror="${INSTALL_GNU_MIRROR:?}"
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.xz"
+    url="${gnu_mirror}/${name}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make check
+    make install
+    return 0
+}
 
-gnu_mirror="${INSTALL_GNU_MIRROR:?}"
-jobs="${INSTALL_JOBS:?}"
-name="${INSTALL_NAME:?}"
-prefix="${INSTALL_PREFIX:?}"
-version="${INSTALL_VERSION:?}"
-
-file="${name}-${version}.tar.xz"
-url="${gnu_mirror}/${name}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-# > make check
-make install
+install_autoconf "$@"
