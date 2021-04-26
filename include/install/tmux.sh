6bc5769492fd5b0baa5e1732edf4d024d1c185f2
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-26 14:58:59 -0400

    Improve tmux installer

--- include/install/tmux.sh
+++ include/install/tmux.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
-file="${name}-${version}.tar.gz"
-url="https://github.com/${name}/${name}/releases/download/${version}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-make install
+install_tmux() { # {{{1
+    # """
+    # Install Tmux.
+    # @note Updated 2021-04-26.
+    # """
+    local file jobs name prefix url version
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.gz"
+    url="https://github.com/${name}/${name}/releases/download/
+${version}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    make install
+    return 0
+}
+
+install_tmux "$@"
