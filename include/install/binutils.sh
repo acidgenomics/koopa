0210ce5dbc6a375f1c935a434a53525f9636ac90
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-26 14:42:28 -0400

    Improve consistency of build from source scripts

--- include/install/binutils.sh
+++ include/install/binutils.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-koopa::assert_is_installed makeinfo  # texinfo
-file="${name}-${version}.tar.xz"
-url="${gnu_mirror}/${name}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-# > make check
-make install
+install_binutils() { # {{{1
+    # """
+    # Install binutils.
+    # @note Updated 2021-04-26.
+    # """
+    local file gnu_mirror jobs name prefix url version
+    koopa::assert_is_installed makeinfo  # texinfo
+    gnu_mirror="${INSTALL_GNU_MIRROR:?}"
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.xz"
+    url="${gnu_mirror}/${name}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make check
+    make install
+    return 0
+}
+
+install_binutils "$@"
