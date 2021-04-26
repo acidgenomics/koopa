9aebf5f6c449d1b71b5405b12773a9226c48eedb
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-26 15:54:38 -0400

    Update curl installer

--- include/install/curl.sh
+++ include/install/curl.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
-# """
-# https://curl.haxx.se/docs/install.html
-# """
+install_curl() { # {{{1
+    # """
+    # Install cURL.
+    # @note Updated 2021-04-26.
+    # @seealso
+    # - https://curl.haxx.se/docs/install.html
+    # """
+    local file jobs name prefix url version version2
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
 
-file="${name}-${version}.tar.xz"
-version2="${version//./_}"
-url="https://github.com/${name}/${name}/releases/download/\
-${name}-${version2}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-# > make test
-make install
+    file="${name}-${version}.tar.xz"
+    version2="${version//./_}"
+    url="https://github.com/${name}/${name}/releases/download/\
+    ${name}-${version2}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make test
+    make install
+    return 0
+}
+
+install_curl "$@"
