52c06f4c09482c71c84d3b76c95608257e2991c0
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-26 15:10:32 -0400

    Update cmake

--- include/install/cmake.sh
+++ include/install/cmake.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
-# """
-# https://github.com/Kitware/CMake
-# We're enforcing system GCC here to avoid libstdc++ errors.
-# """
+install_cmake() { # {{{1
+    # """
+    # Install CMake.
+    # @note Updated 2021-04-26.
+    #
+    # We're enforcing system GCC here to avoid libstdc++ errors.
+    #
+    # @seealso
+    # - https://github.com/Kitware/CMake
+    # """
+    local file jobs name prefix url version
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    if koopa::is_linux
+    then
+        koopa::assert_is_file '/usr/bin/gcc' '/usr/bin/g++'
+        export CC='/usr/bin/gcc'
+        export CXX='/usr/bin/g++'
+    fi
+    file="cmake-${version}.tar.gz"
+    url="https://github.com/Kitware/CMake/releases/download/v${version}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    # Note that the './configure' script is just a wrapper for './bootstrap'.
+    # > ./bootstrap --help
+    ./bootstrap \
+        --parallel="$jobs" \
+        --prefix="$prefix"
+    make --jobs="$jobs"
+    make install
+    return 0
+}
 
-if koopa::is_linux
-then
-    koopa::assert_is_file /usr/bin/gcc /usr/bin/g++
-    export CC='/usr/bin/gcc'
-    export CXX='/usr/bin/g++'
-fi
-file="cmake-${version}.tar.gz"
-url="https://github.com/Kitware/CMake/releases/download/v${version}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-# Note that the './configure' script is just a wrapper for './bootstrap'.
-# > ./bootstrap --help
-./bootstrap \
-    --parallel="$jobs" \
-    --prefix="$prefix"
-make --jobs="$jobs"
-make install
+install_cmake "$@"
