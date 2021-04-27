1cf833c1567e08058005b12e3448e66299ba71ca
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 09:36:05 -0400

    Update install scripts

--- include/install/emacs.sh
+++ include/install/emacs.sh
@@ -1,34 +1,50 @@
 #!/usr/bin/env bash
 
-# https://github.com/Homebrew/homebrew-core/blob/master/Formula/emacs.rb
+install_emacs() { # {{{1
+    # """
+    # Install Emacs.
+    # @note Updated 2021-04-27.
+    #
+    # Seeing this error on macOS:
+    # Nothing to be done for 'maybe-blessmail'.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/emacs.rb
+    # """
+    local file gnu_mirror jobs name prefix url version
+    gnu_mirror="${INSTALL_GNU_MIRROR:?}"
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.xz"
+    url="${gnu_mirror}/${name}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    flags=("--prefix=${prefix}")
+    if koopa::is_macos
+    then
+        flags+=(
+            '--disable-silent-rules'
+            '--with-gnutls'
+            '--with-modules'
+            '--with-xml2'
+            '--without-dbus'
+            '--without-imagemagick'
+            '--without-ns'
+            '--without-x'
+        )
+    else
+        flags+=(
+            '--with-x-toolkit=no'
+            '--with-xpm=no'
+        )
+    fi
+    ./configure "${flags[@]}"
+    make --jobs="$jobs"
+    make install
+    return 0
+}
 
-# Seeing this error on macOS:
-# Nothing to be done for 'maybe-blessmail'.
-
-file="emacs-${version}.tar.xz"
-url="${gnu_mirror}/emacs/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "emacs-${version}"
-flags=("--prefix=${prefix}")
-if koopa::is_macos
-then
-    flags+=(
-        '--disable-silent-rules'
-        '--with-gnutls'
-        '--with-modules'
-        '--with-xml2'
-        '--without-dbus'
-        '--without-imagemagick'
-        '--without-ns'
-        '--without-x'
-    )
-else
-    flags+=(
-        '--with-x-toolkit=no'
-        '--with-xpm=no'
-    )
-fi
-./configure "${flags[@]}"
-make --jobs="$jobs"
-make install
+install_emacs "$@"
--- include/install/fish.sh
+++ include/install/fish.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 
-# """
-# https://github.com/fish-shell/fish-shell/#building
-# """
-
-file="fish-${version}.tar.gz"
-url="https://github.com/fish-shell/fish-shell/releases/download/\
+install_fish() { # {{{1
+    # """
+    # Install Fish shell.
+    # @note Updated 2021-04-27.
+    # @seealso
+    # - https://github.com/fish-shell/fish-shell/#building
+    # """
+    local file jobs link_app name prefix url version
+    jobs="${INSTALL_JOBS:?}"
+    link_app="${INSTALL_LINK_APP:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.gz"
+    url="https://github.com/fish-shell/fish-shell/releases/download/\
 ${version}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-cmake -DCMAKE_INSTALL_PREFIX="$prefix"
-make --jobs="$jobs"
-# > make test
-make install
-if [[ "${link_app:-0}" -eq 1 ]]
-then
-    koopa::enable_shell "$name"
-fi
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    cmake -DCMAKE_INSTALL_PREFIX="$prefix"
+    make --jobs="$jobs"
+    # > make test
+    make install
+    if [[ "${link_app:-0}" -eq 1 ]]
+    then
+        koopa::enable_shell "$name"
+    fi
+    return 0
+}
+
+install_fish "$@"
