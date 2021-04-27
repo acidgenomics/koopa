34e91f6fd68bd812bad6fe6e0fb361ea3ff0465a
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 09:41:58 -0400

    Update install scripts

--- include/install/gawk.sh
+++ include/install/gawk.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-file="${name}-${version}.tar.xz"
-url="${gnu_mirror}/${name}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-# > make check
-make install
+install_gawk() { # {{{1
+    # """
+    # Install GNU awk.
+    # @note Updated 2021-04-27.
+    # """
+    local file gnu_mirror jobs name prefix url version
+    gnu_mirror="${INSTALL_GNU_MIRROR:?}"
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.xz"
+    url="${gnu_mirror}/${name}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make check
+    make install
+    return 0
+}
+
+install_gawk "$@"
--- include/install/git.sh
+++ include/install/git.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env bash
 
-# """
-# If system doesn't have gettext (msgfmt) installed:
-# Note that this doesn't work on Ubuntu 18 LTS.
-# NO_GETTEXT=YesPlease
-#
-# Git source code releases on GitHub:
-# > file="v${version}.tar.gz"
-# > url="https://github.com/git/${name}/archive/${file}"
-# """
+install_git() { # {{{1
+    # """
+    # Install Git.
+    # @note Updated 2021-04-27.
+    #
+    # If system doesn't have gettext (msgfmt) installed:
+    # Note that this doesn't work on Ubuntu 18 LTS.
+    # NO_GETTEXT=YesPlease
+    #
+    # Git source code releases on GitHub:
+    # > file="v${version}.tar.gz"
+    # > url="https://github.com/git/${name}/archive/${file}"
+    # """
+    local file jobs name openssl prefix url version
+    jobs="${INSTALL_JOBS:?}"
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.gz"
+    url="https://mirrors.edge.kernel.org/pub/software/scm/${name}/${file}"
+    openssl='/bin/openssl'
+    koopa::assert_is_installed "$openssl"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "git-${version}"
+    make configure
+    ./configure \
+        --prefix="$prefix" \
+        --with-openssl="$openssl"
+    make --jobs="$jobs" V=1
+    make install
+    return 0
+}
 
-file="${name}-${version}.tar.gz"
-url="https://mirrors.edge.kernel.org/pub/software/scm/${name}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "git-${version}"
-make configure
-./configure \
-    --prefix="$prefix" \
-    --with-openssl='/bin/openssl'
-make --jobs="$jobs" V=1
-make install
+install_git "$@"
