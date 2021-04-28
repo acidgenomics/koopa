c1c4978177fc9ea01c62a8beed9a4305f6b094cb
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 21:29:49 -0400

    Update installers

--- include/install/neofetch.sh
+++ include/install/neofetch.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-file="${version}.tar.gz"
-url="https://github.com/dylanaraps/${name}/archive/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-koopa::mkdir "$prefix"
-make PREFIX="$prefix" install
+install_neofetch() { # {{{1
+    # """
+    # Install neofetch.
+    # @note Updated 2021-04-27.
+    # """
+    local file name prefix url version
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${version}.tar.gz"
+    url="https://github.com/dylanaraps/${name}/archive/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    koopa::mkdir "$prefix"
+    make PREFIX="$prefix" install
+    return 0
+}
+
+install_neofetch "$@"
