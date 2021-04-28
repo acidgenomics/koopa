d6e6fe61d04abf4ba049d2a8b56d31d62581cf3c
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-28 08:57:39 -0400

    Update installers

--- os/linux/common/include/install/docker-credential-pass.sh
+++ os/linux/common/include/install/docker-credential-pass.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-koopa::assert_is_linux
-arch="$(koopa::arch)"
-file="docker-credential-pass-v${version}-${arch}.tar.gz"
-url="https://github.com/docker/docker-credential-helpers/releases/\
-download/v${version}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-chmod 0775 'docker-credential-pass'
-koopa::mkdir "${prefix}/bin"
-koopa::sys_set_permissions -r "$prefix"
-koopa::cp -t "${prefix}/bin" 'docker-credential-pass'
+install_docker_credential_pass() { # {{{1
+    # """
+    # Install docker-credential-pass.
+    # @note Updated 2021-04-28.
+    # """
+    local arch file name prefix url version
+    koopa::assert_is_linux
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    arch="$(koopa::arch)"
+    file="${name}-v${version}-${arch}.tar.gz"
+    url="https://github.com/docker/docker-credential-helpers/releases/\
+    download/v${version}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    chmod 0775 "$name"
+    koopa::mkdir "${prefix}/bin"
+    koopa::sys_set_permissions -r "$prefix"
+    koopa::cp -t "${prefix}/bin" "$name"
+    return 0
+}
+
+install_docker_credential_pass "$@"
