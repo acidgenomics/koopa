9f766c0b12078be3f1e5defc60edbad31815cf97
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-28 10:36:55 -0400

    Update installers

--- os/linux/common/include/install/lua.sh
+++ os/linux/common/include/install/lua.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-# """
-# See also:
-# - http://www.lua.org/manual/5.3/readme.html
-# """
+install_lua() { # {{{1
+    # """
+    # Install Lua.
+    # @note Updated 2021-04-28.
+    # @seealso
+    # - http://www.lua.org/manual/5.3/readme.html
+    # """
+    local file name prefix url version
+    koopa::assert_is_linux
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.gz"
+    url="http://www.lua.org/ftp/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    make linux
+    make test
+    make install INSTALL_TOP="$prefix"
+    return 0
+}
 
-koopa::assert_is_linux
-file="${name}-${version}.tar.gz"
-url="http://www.lua.org/ftp/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-make linux
-make test
-make install INSTALL_TOP="$prefix"
+install_lua "$@"
