d9418b1b98c32572ca80fcf31b565f259651dddb
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-28 10:44:52 -0400

    Update password store

--- os/linux/common/include/install/password-store.sh
+++ os/linux/common/include/install/password-store.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
-# """
-# https://www.passwordstore.org/
-# https://git.zx2c4.com/password-store/
-# """
+install_password_store() { # {{{1
+    # """
+    # Install Password Store.
+    # @note Updated 2021-04-28.
+    # @seealso
+    # - https://www.passwordstore.org/
+    # - https://git.zx2c4.com/password-store/
+    # """
+    local file name prefix url version
+    koopa::assert_is_linux
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.xz"
+    url="https://git.zx2c4.com/${name}/snapshot/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    PREFIX="$prefix" make install
+    return 0
+}
 
-koopa::assert_is_linux
-file="${name}-${version}.tar.xz"
-url="https://git.zx2c4.com/${name}/snapshot/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-PREFIX="$prefix" make install
+install_password_store "$@"
