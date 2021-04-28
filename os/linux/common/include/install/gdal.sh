f805676b33800e517ea2b7116161a5c046eb781e
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-28 09:17:40 -0400

    Update GDAL

--- os/linux/common/include/install/gdal.sh
+++ os/linux/common/include/install/gdal.sh
@@ -1,36 +1,49 @@
 #!/usr/bin/env bash
 
-# """
-# Use 'configure --help' for build options.
-#
-# If you don't need python support you can suppress it at configure using
-# '--without-python'.
-#
-# Use OpenJPEG instead of Jasper.
-# This is particularly important for CentOS builds.
-# - https://github.com/OSGeo/gdal/issues/2402
-# - https://github.com/OSGeo/gdal/issues/1708
-# """
+install_gdal() { # {{{1
+    # """
+    # Install GDAL.
+    # @note Updated 2021-04-28.
+    #
+    # Use 'configure --help' for build options.
+    #
+    # If you don't need python support you can suppress it at configure using
+    # '--without-python'.
+    #
+    # Use OpenJPEG instead of Jasper.
+    # This is particularly important for CentOS builds.
+    # - https://github.com/OSGeo/gdal/issues/2402
+    # - https://github.com/OSGeo/gdal/issues/1708
+    # """
+    local file jobs make_prefix name prefix reinstall url version
+    koopa::assert_is_linux
+    koopa::assert_is_not_file '/usr/bin/gdal-config'
+    koopa::assert_is_installed proj python3
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    jobs="$(koopa::cpu_count)"    
+    make_prefix="$(koopa::make_prefix)"
+    file="${name}-${version}.tar.gz"
+    url="https://github.com/OSGeo/${name}/releases/download/v${version}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    koopa::alert_coffee_time
+    ./configure \
+        --prefix="$prefix" \
+        --with-openjpeg \
+        --with-proj="$make_prefix" \
+        --with-python='python3' \
+        --with-sqlite3="$make_prefix" \
+        --without-jasper \
+        CFLAGS="-I${make_prefix}/include" \
+        CPPFLAGS="-I${make_prefix}/include" \
+        LDFLAGS="-L${make_prefix}/lib"
+    # Use '-d' flag for more verbose debug mode.
+    make --jobs="$jobs"
+    make install
+    return 0
+}
 
-koopa::assert_is_linux
-[[ "$reinstall" -ne 1 ]] && koopa::assert_is_not_file '/usr/bin/gdal-config'
-koopa::assert_is_installed proj python3
-koopa::alert_coffee_time
-file="${name}-${version}.tar.gz"
-url="https://github.com/OSGeo/${name}/releases/download/v${version}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure \
-    --prefix="$prefix" \
-    --with-openjpeg \
-    --with-proj="$make_prefix" \
-    --with-python='python3' \
-    --with-sqlite3="$make_prefix" \
-    --without-jasper \
-    CFLAGS="-I${make_prefix}/include" \
-    CPPFLAGS="-I${make_prefix}/include" \
-    LDFLAGS="-L${make_prefix}/lib"
-# Use '-d' flag for more verbose debug mode.
-make --jobs="$jobs"
-make install
+install_gdal "$@"
