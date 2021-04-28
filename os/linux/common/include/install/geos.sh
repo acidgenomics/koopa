0b0192af9d9342079c037e5cb7514ea34df0f0fd
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-28 09:20:20 -0400

    Update GEOS

--- os/linux/common/include/install/geos.sh
+++ os/linux/common/include/install/geos.sh
@@ -1,33 +1,45 @@
 #!/usr/bin/env bash
 
-# """
-# Can build with autotools or cmake.
-# See 'INSTALL' file for details.
-# The cmake approach seems to build more reliably inside Docker images.
-#
-# - autotools:
-#   https://trac.osgeo.org/geos/wiki/BuildingOnUnixWithAutotools
-# - cmake:
-#   https://trac.osgeo.org/geos/wiki/BuildingOnUnixWithCMake
-#
-# Alternate autotools approach:
-# > ./autogen.sh
-# > ./configure --prefix="$prefix"
-# > make --jobs="$jobs"
-# > make check
-# """
+install_geos() { # {{{1
+    # """
+    # Install GEOS.
+    # @note Updated 2021-04-28.
+    #
+    # Can build with autotools or cmake.
+    # See 'INSTALL' file for details.
+    # The cmake approach seems to build more reliably inside Docker images.
+    #
+    # - autotools:
+    #   https://trac.osgeo.org/geos/wiki/BuildingOnUnixWithAutotools
+    # - cmake:
+    #   https://trac.osgeo.org/geos/wiki/BuildingOnUnixWithCMake
+    #
+    # Alternate autotools approach:
+    # > ./autogen.sh
+    # > ./configure --prefix="$prefix"
+    # > make --jobs="$jobs"
+    # > make check
+    # """
+    local file jobs name prefix url version
+    koopa::assert_is_linux
+    koopa::assert_is_not_file '/usr/bin/geos-config'
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    jobs="$(koopa::cpu_count)"
+    file="${version}.tar.gz"
+    url="https://github.com/libgeos/${name}/archive/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::mkdir build
+    koopa::cd build
+    cmake "../${name}-${version}" \
+        -DCMAKE_INSTALL_PREFIX="$prefix"
+        # -DGEOS_ENABLE_TESTS=OFF
+    make --jobs="$jobs"
+    # > make test
+    make install
+    return 0
+}
 
-koopa::assert_is_linux
-[[ "$reinstall" -ne 1 ]] && koopa::assert_is_not_file '/usr/bin/geos-config'
-file="${version}.tar.gz"
-url="https://github.com/libgeos/${name}/archive/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::mkdir build
-koopa::cd build
-cmake "../${name}-${version}" \
-    -DCMAKE_INSTALL_PREFIX="$prefix"
-    # -DGEOS_ENABLE_TESTS=OFF
-make --jobs="$jobs"
-# > make test
-make install
+install_geos "$@"
