1df7bfcfb78f4d9091d154a69699f4956fc86e94
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-28 15:10:02 -0400

    Improve Python install

--- os/linux/common/include/install/python.sh
+++ os/linux/common/include/install/python.sh
@@ -5,38 +5,27 @@ install_python() { # {{{1
     # Install Python.
     # @note Updated 2021-04-28.
     #
+    # Check config with:
+    # > ldd /usr/local/bin/python3
+    #
     # Warning: 'make install' can overwrite or masquerade the python3 binary.
     # 'make altinstall' is therefore recommended instead of make install since
     # it only installs 'exec_prefix/bin/pythonversion'.
     #
-    #
     # Ubuntu 18 LTS requires LDFLAGS to be set, otherwise we hit:
-    # python3: error while loading shared libraries: libpython3.8.so.1.0:
-    # cannot open shared object file: No such file or directory
-    #
+    # ## python3: error while loading shared libraries: libpython3.8.so.1.0:
+    # ## cannot open shared object file: No such file or directory
     # This alone works, but I've set other paths, as recommended.
-    # LDFLAGS="-Wl,-rpath ${prefix}/lib"
+    # # > LDFLAGS="-Wl,-rpath ${prefix}/lib"
     #
-    # Check config with:
-    # > ldd /usr/local/bin/python3
-    #
-    #
-    # To resolve this warning:
-    #
-    # > checking for g++... no
-    # > configure:
-    # >
-    # >   By default, distutils will build C++ extension modules with "g++".
-    # >   If this is not intended, then set CXX on the configure command line.
-    #
-    # Specify `CXX` environment variable or `--with-cxx-main=/usr/bin/g++`.
-    #
+    # To customize g++ path, specify `CXX` environment variable
+    # or use `--with-cxx-main=/usr/bin/g++`.
     #
     # See also:
     # - https://docs.python.org/3/using/unix.html
     # - https://stackoverflow.com/questions/43333207
     # """
-    local file jobs name prefix url version
+    local file jobs name name2 prefix url version
     koopa::assert_is_linux
     name="${INSTALL_NAME:?}"
     prefix="${INSTALL_PREFIX:?}"
@@ -44,12 +33,12 @@ install_python() { # {{{1
     minor_version="$(koopa::major_minor_version "$version")"
     jobs="$(koopa::cpu_count)"
     make_prefix="$(koopa::make_prefix)"
-    # FIXME CONVERT THIS TO UPPER.
-    file="Python-${version}.tar.xz"
+    name2="$(koopa::capitalize "$name")"
+    file="${name2}-${version}.tar.xz"
     url="https://www.${name}.org/ftp/${name}/${version}/${file}"
     koopa::download "$url"
     koopa::extract "$file"
-    koopa::cd "Python-${version}"
+    koopa::cd "${name2}-${version}"
     ./configure \
         --enable-optimizations \
         --enable-shared \
