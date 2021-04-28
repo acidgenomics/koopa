fddf9f840ba68192ba22dd3b884b54d5835523e0
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-28 10:40:51 -0400

    Update installers

--- os/linux/common/include/install/openssl.sh
+++ os/linux/common/include/install/openssl.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
-koopa::assert_is_linux
-name='openssl'
-file="${name}-${version}.tar.gz"
-url="https://www.${name}.org/source/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./config \
-    --prefix="$prefix" \
-    --openssldir="$prefix" \
-    shared
-make --jobs="$jobs"
-# > make test
-make install
+install_openssl() { # {{{1
+    # """
+    # Install OpenSSL.
+    # @note Updated 2021-04-28.
+    # """
+    local file name prefix url version
+    koopa::assert_is_linux
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    jobs="$(koopa::cpu_count)"
+    file="${name}-${version}.tar.gz"
+    url="https://www.${name}.org/source/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./config \
+        --prefix="$prefix" \
+        --openssldir="$prefix" \
+        shared
+    make --jobs="$jobs"
+    # > make test
+    make install
+    return 0
+}
+
+install_openssl "$@"
