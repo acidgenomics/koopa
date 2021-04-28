aaa0992b28cfae1d747b2a18735585c0c84fdb63
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 21:13:15 -0400

    Update installers

--- include/install/htop.sh
+++ include/install/htop.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
-# Repo transferred from https://github.com/hishamhm/htop to 
-# https://github.com/htop-dev/htop in 2020-08.
+install_htop() { # {{{1
+    # """
+    # Install htop.
+    # @note Updated 2021-04-27.
+    #
+    # Repo transferred from https://github.com/hishamhm/htop to 
+    # https://github.com/htop-dev/htop in 2020-08.
+    # """
+    local file jobs name prefix url version
+    koopa::assert_is_installed python3
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    jobs="$(koopa::cpu_count)"
+    file="${version}.tar.gz"
+    url="https://github.com/htop-dev/htop/archive/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./autogen.sh
+    ./configure \
+        --disable-unicode \
+        --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make check
+    make install
+    return 0
+}
 
-koopa::assert_is_installed python3
-file="${version}.tar.gz"
-url="https://github.com/htop-dev/htop/archive/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./autogen.sh
-./configure \
-    --disable-unicode \
-    --prefix="$prefix"
-make --jobs="$jobs"
-# > make check
-make install
+install_htop "$@"
