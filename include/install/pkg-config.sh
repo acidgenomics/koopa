563c1e15de0b2145d8ce58df2d3a72a4781af0c6
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 21:36:33 -0400

    Update installers

--- include/install/pkg-config.sh
+++ include/install/pkg-config.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
-# """
-# https://www.freedesktop.org/wiki/Software/pkg-config/
-# https://pkg-config.freedesktop.org/releases/
-# """
+install_pkg_config() { # {{{1
+    # """
+    # Install pkg-config.
+    # @note Updated 2021-04-27.
+    # @seealso
+    # - https://www.freedesktop.org/wiki/Software/pkg-config/
+    # - https://pkg-config.freedesktop.org/releases/
+    # """
+    local file jobs name prefix url version
+    koopa::assert_is_installed cmp diff
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    jobs="$(koopa::cpu_count)"
+    file="${name}-${version}.tar.gz"
+    url="https://${name}.freedesktop.org/releases/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    make install
+    return 0
+}
 
-koopa::assert_is_installed cmp diff
-file="${name}-${version}.tar.gz"
-url="https://${name}.freedesktop.org/releases/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-make install
+install_pkg_config "$@"
