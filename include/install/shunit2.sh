f27d40bf9a91c9a969bbafed12368e37797fcf75
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 21:59:20 -0400

    Update installers

--- include/install/shunit2.sh
+++ include/install/shunit2.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-file="v${version}.tar.gz"
-url="https://github.com/kward/${name}/archive/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}"
-koopa::cp -t "${prefix}/bin" "$name"
+install_shunit2() { # {{{1
+    # """
+    # Install shUnit2.
+    # @note Updated 2021-04-27.
+    # """
+    local file name prefix url version
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="v${version}.tar.gz"
+    url="https://github.com/kward/${name}/archive/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    koopa::cp -t "${prefix}/bin" "$name"
+    return 0
+}
+
+install_shunit2 "$@"
--- include/install/singularity.sh
+++ include/install/singularity.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-koopa::assert_has_sudo
-koopa::assert_is_installed go
-file="${name}-${version}.tar.gz"
-url="https://github.com/sylabs/${name}/releases/download/v${version}/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "$name"
-./mconfig --prefix="$prefix"
-make -C builddir
-sudo make -C builddir install
+install_singularity() { # {{{1
+    # """
+    # Install Singularity.
+    # @note Updated 2021-04-27.
+    # """
+    local file name prefix url version
+    koopa::assert_has_sudo
+    koopa::assert_is_installed go
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file="${name}-${version}.tar.gz"
+    url="https://github.com/sylabs/${name}/releases/download/\
+v${version}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "$name"
+    ./mconfig --prefix="$prefix"
+    make -C builddir
+    sudo make -C builddir install
+    return 0
+}
 
+install_singularity "$@"
