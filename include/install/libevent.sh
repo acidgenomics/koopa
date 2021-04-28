8a7df63dcb9955c07658fdb5d1394625059d9065
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 21:20:37 -0400

    Update installers

--- include/install/libevent.sh
+++ include/install/libevent.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
-if koopa::is_macos
-then
-    koopa::assert_is_installed brew
-    openssl_pkgconfig="$(brew --prefix)/opt/openssl@1.1/lib/pkgconfig"
-    koopa::assert_is_dir "$openssl_pkgconfig"
-    koopa::add_to_pkg_config_path_start "$openssl_pkgconfig"
-fi
-file="${name}-${version}-stable.tar.gz"
-url="https://github.com/${name}/${name}/releases/download/\
+install_libevent() { # {{{1
+    # """
+    # Install libevent.
+    # @note Updated 2021-04-27.
+    # """
+    local file jobs name openssl_pkgconfig prefix url version
+    name="${INSTALL_NAME:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    jobs="$(koopa::cpu_count)"
+    if koopa::is_macos
+    then
+        koopa::assert_is_installed brew
+        openssl_pkgconfig="$(koopa::homebrew_prefix)/opt/\
+openssl@1.1/lib/pkgconfig"
+        koopa::assert_is_dir "$openssl_pkgconfig"
+        koopa::add_to_pkg_config_path_start "$openssl_pkgconfig"
+    fi
+    file="${name}-${version}-stable.tar.gz"
+    url="https://github.com/${name}/${name}/releases/download/\
 release-${version}-stable/${file}"
-koopa::download "$url"
-koopa::extract "$file"
-koopa::cd "${name}-${version}-stable"
-./configure --prefix="$prefix"
-make --jobs="$jobs"
-# > make check
-make install
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}-stable"
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    # > make check
+    make install
+    return 0
+}
+
+install_libevent "$@"
