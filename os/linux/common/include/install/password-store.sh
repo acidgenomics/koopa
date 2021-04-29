b3af3eee0b453b8c18e4e86e6e004143054aa580
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-29 16:55:43 -0400

    Use fewer variables

--- os/linux/common/include/install/password-store.sh
+++ os/linux/common/include/install/password-store.sh
@@ -3,21 +3,20 @@
 install_password_store() { # {{{1
     # """
     # Install Password Store.
-    # @note Updated 2021-04-28.
+    # @note Updated 2021-04-29.
     # @seealso
     # - https://www.passwordstore.org/
     # - https://git.zx2c4.com/password-store/
     # """
-    local file name prefix url version
+    local file prefix url version
     koopa::assert_is_linux
-    name="${INSTALL_NAME:?}"
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
-    file="${name}-${version}.tar.xz"
-    url="https://git.zx2c4.com/${name}/snapshot/${file}"
+    file="password-store-${version}.tar.xz"
+    url="https://git.zx2c4.com/password-store/snapshot/${file}"
     koopa::download "$url"
     koopa::extract "$file"
-    koopa::cd "${name}-${version}"
+    koopa::cd "password-store-${version}"
     PREFIX="$prefix" make install
     return 0
 }
