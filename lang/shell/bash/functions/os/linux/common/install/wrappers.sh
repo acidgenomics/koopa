1e36c6cf8b6a6a35481f22654f93b7c79c6578f9
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-30 12:06:34 -0400

    Update wrappers

--- lang/shell/bash/functions/os/linux/common/install/wrappers.sh
+++ lang/shell/bash/functions/os/linux/common/install/wrappers.sh
@@ -10,23 +10,35 @@ koopa::linux_install_aws_cli() { # {{{1
 }
 
 koopa::linux_install_docker_credential_pass() { # {{{1
-    koopa::linux_install_app --name='docker-credential-pass' "$@"
+    koopa::linux_install_app \
+        --name='docker-credential-pass' \
+        "$@"
 }
 
 koopa::linux_install_gcc() { # {{{1
-    koopa::linux_install_app --name='gcc' --name-fancy='GCC' "$@"
+    koopa::linux_install_app \
+        --name='gcc' \
+        --name-fancy='GCC' "$@"
 }
 
 koopa::linux_install_gdal() { # {{{1
-    koopa::linux_install_app --name='gdal' --name-fancy='GDAL' "$@"
+    koopa::linux_install_app \
+        --name='gdal' \
+        --name-fancy='GDAL' "$@"
 }
 
 koopa::linux_install_geos() { # {{{1
-    koopa::linux_install_app --name='geos' --name-fancy='GEOS' "$@"
+    koopa::linux_install_app \
+        --name='geos' \
+        --name-fancy='GEOS' \
+        "$@"
 }
 
 koopa::linux_install_groff() { # {{{1
-    koopa::linux_install_app --name='groff' --name-fancy='GNU roff' "$@"
+    koopa::linux_install_app \
+        --name='groff' \
+        --name-fancy='GNU roff' \
+        "$@"
 }
 
 koopa::linux_install_julia() { # {{{1
@@ -59,19 +71,29 @@ koopa::linux_install_julia() { # {{{1
 }
 
 koopa::linux_install_lua() { # {{{1
-    koopa::linux_install_app --name='lua' --name-fancy='Lua' "$@"
+    koopa::linux_install_app \
+        --name='lua' \
+        --name-fancy='Lua' \
+        "$@"
 }
 
 koopa::linux_install_luarocks() { # {{{1
-    koopa::linux_install_app --name='luarocks' "$@"
+    koopa::linux_install_app \
+        --name='luarocks' \
+        "$@"
 }
 
 koopa::linux_install_neovim() { # {{{1
-    koopa::linux_install_app --name='neovim' "$@"
+    koopa::linux_install_app \
+        --name='neovim' \
+        "$@"
 }
 
 koopa::linux_install_openssh() { # {{{1
-    koopa::linux_install_app --name='openssh' --name-fancy='OpenSSH' "$@"
+    koopa::linux_install_app \
+        --name='openssh' \
+        --name-fancy='OpenSSH' \
+        "$@"
 }
 
 koopa::linux_install_openssl() { # {{{1
@@ -83,21 +105,33 @@ koopa::linux_install_openssl() { # {{{1
 }
 
 koopa::linux_install_password_store() { # {{{1
-    koopa::linux_install_app --name='password-store' "$@"
+    koopa::linux_install_app \
+        --name='password-store' \
+        "$@"
 }
 
 koopa::linux_install_proj() { # {{{1
-    koopa::linux_install_app --name='proj' --name-fancy='PROJ' "$@"
+    koopa::linux_install_app \
+        --name='proj' \
+        --name-fancy='PROJ' \
+        "$@"
 }
 
 koopa::linux_install_python() { # {{{1
-    koopa::linux_install_app --name='python' --name-fancy='Python' "$@"
+    koopa::linux_install_app \
+        --name='python' \
+        --name-fancy='Python' \
+        "$@"
 }
 
 koopa::linux_install_r() { # {{{1
-    koopa::linux_install_app --name='r' --name-fancy='R' "$@"
+    koopa::linux_install_app \
+        --name='r' \
+        --name-fancy='R' \
+        "$@"
 }
 
+# NOTE Consider changing 'name' to 'r-devel' here?
 koopa::linux_install_r_devel() { # {{{1
     koopa::linux_install_app \
         --name='r' \
@@ -105,22 +139,31 @@ koopa::linux_install_r_devel() { # {{{1
         --version='devel' \
         --script-name='r-devel' \
         "$@"
-    # FIXME MOVE THIS INTO INSTALLER.
-    koopa::configure_r
 }
 
 koopa::linux_install_ruby() { # {{{1
-    koopa::linux_install_app --name='ruby' --name-fancy='Ruby' "$@"
+    koopa::linux_install_app \
+        --name='ruby' \
+        --name-fancy='Ruby' \
+        "$@"
 }
 
 koopa::linux_install_taglib() { # {{{1
-    koopa::linux_install_app --name='taglib' --name-fancy='TagLib' "$@"
+    koopa::linux_install_app \
+        --name='taglib' \
+        --name-fancy='TagLib' \
+        "$@"
 }
 
 koopa::linux_install_udunits() { # {{{1
-    koopa::linux_install_app --name='udunits' "$@"
+    koopa::linux_install_app \
+        --name='udunits' \
+        "$@"
 }
 
 koopa::linux_install_vim() { # {{{1
-    koopa::linux_install_app --name='vim' --name-fancy='Vim' "$@"
+    koopa::linux_install_app \
+        --name='vim' \
+        --name-fancy='Vim' \
+        "$@"
 }
