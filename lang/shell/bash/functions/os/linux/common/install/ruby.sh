53cb94fa17b6dcae301e856a7159ad0844578f2e
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-04 18:01:07 -0400

    Rework installers

--- lang/shell/bash/functions/os/linux/common/install/ruby.sh
+++ lang/shell/bash/functions/os/linux/common/install/ruby.sh
@@ -1,21 +1,39 @@
 #!/usr/bin/env bash
 
-koopa::linux_update_rbenv() { # {{{1
+koopa::linux_install_ruby() { # {{{1
+    koopa::linux_install_app \
+        --name='ruby' \
+        --name-fancy='Ruby' \
+        "$@"
+}
+
+koopa:::linux_install_ruby() { # {{{1
     # """
-    # Update rbenv.
-    # @note Updated 2020-11-12.
+    # Install Ruby.
+    # @note Updated 2021-05-04.
+    # @seealso
+    # - https://www.ruby-lang.org/en/downloads/
     # """
-    local rbenv_root
-    koopa::assert_has_no_args "$#"
+    local file jobs name prefix url version
     koopa::assert_is_linux
-    koopa::assert_has_no_envs
-    koopa::is_installed rbenv || return 0
-    koopa::h1 'Updating rbenv.'
-    rbenv_root="$(rbenv root)"
-    koopa::assert_is_dir "$rbenv_root"
-    (
-        koopa::cd "$rbenv_root"
-        git pull
-    )
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    name='ruby'
+    jobs="$(koopa::cpu_count)"
+    # Ensure '2.7.1p83' becomes '2.7.1' here, for example.
+    version="$(koopa::sanitize_version "$version")"
+    minor_version="$(koopa::major_minor_version "$version")"
+    file="${name}-${version}.tar.gz"
+    url="https://cache.ruby-lang.org/pub/${name}/${minor_version}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    # This will fail on Ubuntu 18 otherwise.
+    # https://github.com/rbenv/ruby-build/issues/156
+    # https://github.com/rbenv/ruby-build/issues/729
+    export RUBY_CONFIGURE_OPTS='--disable-install-doc'
+    ./configure --prefix="$prefix"
+    make --jobs="$jobs"
+    make install
     return 0
 }
--- lang/shell/bash/functions/os/linux/common/install/udunits.sh
+++ lang/shell/bash/functions/os/linux/common/install/udunits.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 
-install_udunits() { # {{{1
+koopa::linux_install_udunits() { # {{{1
+    koopa::linux_install_app \
+        --name='udunits' \
+        "$@"
+}
+
+koopa:::linux_install_udunits() { # {{{1
     # """
     # Install udunits.
-    # @note Updated 2021-04-29.
+    # @note Updated 2021-05-04.
     # """
-    local file jobs prefix url version
+    local file jobs name prefix url version
     koopa::assert_is_linux
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
+    name='udunits'
     jobs="$(koopa::cpu_count)"
-    file="udunits-${version}.tar.gz"
+    file="${name}-${version}.tar.gz"
     # HTTP alternative:
     # > url="https://www.unidata.ucar.edu/downloads/udunits/${file}"
-    url="ftp://ftp.unidata.ucar.edu/pub/udunits/${file}"
+    url="ftp://ftp.unidata.ucar.edu/pub/${name}/${file}"
     koopa::download "$url"
     koopa::extract "$file"
-    koopa::cd "udunits-${version}"
+    koopa::cd "${name}-${version}"
     ./configure --prefix="$prefix"
     make --jobs="$jobs"
     # > make check
     make install
     return 0
 }
-
-install_udunits "$@"
