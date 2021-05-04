064e50e73803f3a9e3961eceb6b1d867dfff9a45
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-04 18:07:29 -0400

    Rework installers

--- lang/shell/bash/functions/os/linux/common/install/wrappers.sh
+++ lang/shell/bash/functions/os/linux/common/install/wrappers.sh
@@ -1,18 +1,2 @@
 #!/usr/bin/env bash
 
-koopa::linux_install_r() { # {{{1
-    koopa::linux_install_app \
-        --name='r' \
-        --name-fancy='R' \
-        "$@"
-}
-
-# NOTE Consider changing 'name' to 'r-devel' here?
-koopa::linux_install_r_devel() { # {{{1
-    koopa::linux_install_app \
-        --name='r' \
-        --name-fancy='R' \
-        --version='devel' \
-        --script-name='r-devel' \
-        "$@"
-}
