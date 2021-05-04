22f2dd9e80efc1c96254673cde4f559624bb4f11
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-04 15:19:48 -0400

    Update installers

---
+++ lang/shell/bash/functions/common/install/fish.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+koopa::install_fish() { # {{{1
+    koopa::install_app \
+        --name='fish' \
+        --name-fancy='Fish' \
+        "$@"
+}
+
+koopa:::install_fish() { # {{{1
+    # """
+    # Install Fish shell.
+    # @note Updated 2021-05-04.
+    # @seealso
+    # - https://github.com/fish-shell/fish-shell/#building
+    # """
+    local file jobs link_app name prefix url version
+    link_app="${INSTALL_LINK_APP:?}"
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    name='fish'
+    jobs="$(koopa::cpu_count)"
+    file="${name}-${version}.tar.gz"
+    url="https://github.com/${name}-shell/${name}-shell/releases/download/\
+${version}/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::cd "${name}-${version}"
+    cmake -DCMAKE_INSTALL_PREFIX="$prefix"
+    make --jobs="$jobs"
+    # > make test
+    make install
+    if [[ "${link_app:-0}" -eq 1 ]]
+    then
+        koopa::enable_shell "$name"
+    fi
+    return 0
+}
