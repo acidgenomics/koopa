53d3c359737df7af0e3d7df42a3ff82ad1abd48f
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-05 11:23:58 -0400

    Simplify the CellRanger installer

--- lang/shell/bash/functions/os/linux/common/install/cellranger.sh
+++ lang/shell/bash/functions/os/linux/common/install/cellranger.sh
@@ -1,52 +1,30 @@
 #!/usr/bin/env bash
 
-# FIXME This needs to call 'install_linux_app'.
-
 koopa::linux_install_cellranger() { # {{{1
+    koopa::linux_install_app \
+        --name='cellranger' \
+        --name-fancy='CellRanger' \
+        --no-link \
+        "$@"
+}
+
+koopa:::linux_install_cellranger() { # {{{1
     # """
     # Install Cell Ranger.
-    # @note Updated 2021-01-20.
+    # @note Updated 2021-05-05.
     #
     # Refdata is accessible here:
     # https://support.10xgenomics.com/single-cell-gene-expression/
     #     software/downloads/latest
     # """
-    local file make_prefix name name_fancy prefix server tmp_dir url version
-    version=
-    while (("$#"))
-    do
-        case "$1" in
-            --version=*)
-                version="${1#*=}"
-                shift 1
-                ;;
-            *)
-                koopa::invalid_arg "$1"
-                ;;
-        esac
-    done
-    koopa::assert_has_no_args "$#"
+    local file name prefix url version
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
     name='cellranger'
-    name_fancy='Cell Ranger'
-    [[ -z "$version" ]] && version="$(koopa::variable "$name")"
-    prefix="$(koopa::app_prefix)/${name}/${version}"
-    [[ -d "$prefix" ]] && return 0
-    koopa::install_start "$name_fancy" "$prefix"
-    tmp_dir="$(koopa::tmp_dir)"
-    (
-        koopa::cd "$tmp_dir"
-        file="${name}-${version}.tar.gz"
-        server='https://seq.cloud'
-        url="${server}/install/cellranger/${file}"
-        koopa::download "$url"
-        koopa::extract "$file"
-        koopa::sys_mv "${name}-${version}" "$prefix"
-    )
-    koopa::rm "$tmp_dir"
-    # Link main 'cellranger' binary into make prefix (e.g. '/usr/local').
-    make_prefix="$(koopa::make_prefix)"
-    koopa::sys_ln -t "${make_prefix}/bin" "${prefix}/cellranger"
-    koopa::link_into_opt "$prefix" "$name"
-    koopa::install_success "$name_fancy"
+    file="${name}-${version}.tar.gz"
+    url="https://seq.cloud/install/cellranger/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    koopa::sys_mv "${name}-${version}" "$prefix"
     return 0
 }
