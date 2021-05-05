3131f835a2b4c06e0f6fdc8276cd2dae0fb3998f
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-05 12:34:34 -0400

    Improve the wget installer

--- lang/shell/bash/functions/common/install/libevent.sh
+++ lang/shell/bash/functions/common/install/libevent.sh
@@ -9,22 +9,14 @@ koopa::install_libevent() { # {{{1
 koopa:::install_libevent() { # {{{1
     # """
     # Install libevent.
-    # @note Updated 2021-05-04.
+    # @note Updated 2021-05-05.
     # """
-    local file jobs name openssl_pkgconfig prefix url version
+    local file jobs name prefix url version
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
     name='libevent'
     jobs="$(koopa::cpu_count)"
-    # FIXME Need to make this a shared function.
-    if koopa::is_macos
-    then
-        koopa::assert_is_installed brew
-        openssl_pkgconfig="$(koopa::homebrew_prefix)/opt/\
-openssl@1.1/lib/pkgconfig"
-        koopa::assert_is_dir "$openssl_pkgconfig"
-        koopa::add_to_pkg_config_path_start "$openssl_pkgconfig"
-    fi
+    koopa::is_macos && koopa::activate_homebrew_pkg_config 'openssl@1.1'
     file="${name}-${version}-stable.tar.gz"
     url="https://github.com/${name}/${name}/releases/download/\
 release-${version}-stable/${file}"
