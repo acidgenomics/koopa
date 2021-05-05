3fdd31da6369c4ef14b227f337ac64055ce7baaa
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-05 11:33:25 -0400

    Update cbl installer

--- lang/shell/bash/functions/os/linux/common/install/cloudbiolinux.sh
+++ lang/shell/bash/functions/os/linux/common/install/cloudbiolinux.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-# FIXME This needs to call 'install_linux_app'.
-
 koopa::linux_install_cloudbiolinux() { # {{{1
+    koopa::linux_install_app \
+        --name='cloudbiolinux' \
+        --name-fancy='CloudBioLinux' \
+        --version='rolling' \
+        --no-link \
+        "$@"
+}
+
+koopa:::linux_install_cloudbiolinux() { # {{{1
     # """
     # Install CloudBioLinux.
-    # @note Updated 2020-11-16.
+    # @note Updated 2021-05-05.
     # """
-    local name_fancy prefix
-    koopa::assert_has_args_le "$#" 1
-    prefix="${1:-cloudbiolinux}"
-    [[ -d "$prefix" ]] && return 0
-    name_fancy='CloudBioLinux'
-    koopa::install_start "$name_fancy"
-    # Using our forked repo, to control for unexpected upstream changes.
-    git clone 'https://github.com/acidgenomics/cloudbiolinux.git' "$prefix"
-    koopa::install_success "$name_fancy"
-    return 0
+    prefix="${INSTALL_PREFIX:?}"
+    url='https://github.com/chapmanb/cloudbiolinux.git'
+    git clone "$url" "$prefix"
 }
