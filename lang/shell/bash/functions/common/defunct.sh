b93075e81e7ce4369a1d229addb89707ca8ec954
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-07 17:31:17 -0400

    Delete some deprecations

--- lang/shell/bash/functions/common/defunct.sh
+++ lang/shell/bash/functions/common/defunct.sh
@@ -178,17 +178,3 @@ koopa::unlink_cellar() { # {{{1
     # """
     koopa::defunct 'koopa::unlink_app'
 }
-
-koopa::update_profile() { # {{{1
-    # """
-    # @note Updated 2020-02-15.
-    # """
-    koopa::defunct 'koopa::update_etc_profile_d'
-}
-
-koopa::update_shells() { # {{{1
-    # """
-    # @note Updated 2020-02-11.
-    # """
-    koopa::defunct 'koopa::enable_shell'
-}
