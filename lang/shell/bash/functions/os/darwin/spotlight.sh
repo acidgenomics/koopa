2ee68bc2299f01f5bcec988496a66d4f04844c63
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-08 10:28:34 -0400

    Update spotlight function

--- lang/shell/bash/functions/os/darwin/spotlight.sh
+++ lang/shell/bash/functions/os/darwin/spotlight.sh
@@ -5,10 +5,11 @@ koopa::macos_spotlight_find() { # {{{1
     # Find files using Spotlight index, instead of GNU find.
     # @note Updated 2021-05-08.
     # """
-    local x
-    koopa::assert_has_args "$#"
+    local pattern x
+    koopa::assert_has_args_eq "$#" 1
     koopa::assert_is_installed mdfind
-    x="$(mdfind -name "$@")"
+    pattern="${1:?}"
+    x="$(mdfind -name "$pattern")"
     [[ -n "$x" ]] || return 1
     koopa::print "$x"
     return 0
