e56e6aa1e8dd7164ecd848ee10073150d24cdb69
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-08 10:22:43 -0400

    Add finder script

---
+++ lang/shell/bash/functions/os/darwin/finder.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+koopa::macos_finder_hide() { # {{{1
+    # """
+    # Hide files from view in the Finder.
+    # @note Updated 2021-05-08.
+    # """
+    koopa::assert_has_args "$#"
+    koopa::assert_is_installed setfile
+    setfile -a V "$@"
+    return 0
+}
+
+koopa::macos_finder_unhide() { # {{{1
+    # """
+    # Unhide files from view in the Finder.
+    # @note Updated 2021-05-08.
+    # """
+    koopa::assert_has_args "$#"
+    koopa::assert_is_installed setfile
+    setfile -a v "$@"
+    return 0
+}
