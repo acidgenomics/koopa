312c3818238a20b4033f9f59a2320eafe718829e
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-10 13:04:02 -0400

    Improve cross-platform config support

--- lang/shell/bash/functions/common/install/gdal.sh
+++ lang/shell/bash/functions/common/install/gdal.sh
@@ -10,7 +10,7 @@ koopa::install_gdal() { # {{{1
 koopa:::install_gdal() { # {{{1
     # """
     # Install GDAL.
-    # @note Updated 2021-04-28.
+    # @note Updated 2021-05-10.
     #
     # Use 'configure --help' for build options.
     #
@@ -22,29 +22,46 @@ koopa:::install_gdal() { # {{{1
     # - https://github.com/OSGeo/gdal/issues/2402
     # - https://github.com/OSGeo/gdal/issues/1708
     # """
-    local file jobs make_prefix name prefix url version
-    koopa::activate_opt_prefix geos proj python
+    local brew_opt_pkgs conf_args file jobs name opt_pkgs prefix url version
+    conf_args=(
+        "--prefix=${prefix}"
+        '--with-openjpeg'
+        '--with-python=python3'
+        '--without-jasper'
+    )
+    opt_pkgs=('geos' 'proj')
+    if koopa::is_linux
+    then
+        # This approach assumes that geos, proj, python, and sqlite are
+        # symlinked into the make prefix.
+        opt_pkgs+=('python' 'sqlite')
+        # > local make_prefix
+        # > make_prefix="$(koopa::make_prefix)"
+        # > conf_args+=(
+        # >    --with-proj="$make_prefix" \
+        # >    --with-sqlite3="$make_prefix" \
+        # >    CFLAGS="-I${make_prefix}/include" \
+        # >    CPPFLAGS="-I${make_prefix}/include" \
+        # >    LDFLAGS="-L${make_prefix}/lib"
+        # > )
+    elif koopa::is_macos
+    then
+        brew_opt_pkgs=('sqlite')
+        koopa::activate_homebrew_opt_prefix "${brew_opt_pkgs[@]}"
+        koopa::macos_activate_python
+    fi
+    koopa::activate_opt_prefix "${opt_pkgs[@]}"
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
     name='gdal'
     jobs="$(koopa::cpu_count)"
-    make_prefix="$(koopa::make_prefix)"
     file="${name}-${version}.tar.gz"
     url="https://github.com/OSGeo/${name}/releases/download/v${version}/${file}"
     koopa::download "$url"
     koopa::extract "$file"
     koopa::cd "${name}-${version}"
     koopa::alert_coffee_time
-    ./configure \
-        --prefix="$prefix" \
-        --with-openjpeg \
-        --with-proj="$make_prefix" \
-        --with-python='python3' \
-        --with-sqlite3="$make_prefix" \
-        --without-jasper \
-        CFLAGS="-I${make_prefix}/include" \
-        CPPFLAGS="-I${make_prefix}/include" \
-        LDFLAGS="-L${make_prefix}/lib"
+    ./configure "${conf_args[@]}"
     # Use '-d' flag for more verbose debug mode.
     make --jobs="$jobs"
     make install
