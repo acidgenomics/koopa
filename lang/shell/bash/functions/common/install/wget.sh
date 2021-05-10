d2204d022a1e57d698f83c2e55e5930327ee9bfd
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-10 10:42:52 -0400

    Switch to activate_homebrew_opt_prefix

--- lang/shell/bash/functions/common/install/wget.sh
+++ lang/shell/bash/functions/common/install/wget.sh
@@ -3,10 +3,14 @@
 # NOTE Currently failing to build on macOS.
 
 koopa::install_wget() { # {{{1
+    # """
+    # Install wget.
+    # @note Updated 2021-05-10.
+    # """
     local conf_args
     if koopa::is_macos
     then
-        koopa::activate_homebrew_pkg_config 'openssl@1.1'
+        koopa::activate_homebrew_opt_prefix 'openssl@1.1'
     fi
     conf_args=('--with-ssl=openssl')
     koopa::install_gnu_app \
