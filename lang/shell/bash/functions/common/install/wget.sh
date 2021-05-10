b335da423ea4de7b8e515219e6df05c3c9296edc
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-10 12:45:06 -0400

    Improve emacs and wget installers

--- lang/shell/bash/functions/common/install/wget.sh
+++ lang/shell/bash/functions/common/install/wget.sh
@@ -1,31 +1,13 @@
 #!/usr/bin/env bash
 
-# NOTE Failing to install on macOS with GCC 11.
-# # utimens.c: In function 'fdutimens':
-# # utimens.c:399:17: warning: 'update_timespec' accessing 16 bytes in a region of size 8 [-Wstringop-overflow=]
-# #   399 |       if (ts && update_timespec (&st, &ts))
-# #       |                 ^~~~~~~~~~~~~~~~~~~~~~~~~~
-# # utimens.c:399:17: note: referencing argument 2 of type 'struct timespec **'
-# # utimens.c:136:1: note: in a call to function 'update_timespec'
-# #   136 | update_timespec (struct stat const *statbuf, struct timespec *ts[2])
-# #       | ^~~~~~~~~~~~~~~
-# #   CC       chdir-long.o
-# #   CC       error.o
-# # utimens.c: In function 'lutimens':
-# # utimens.c:612:17: warning: 'update_timespec' accessing 16 bytes in a region of size 8 [-Wstringop-overflow=]
-# #   612 |       if (ts && update_timespec (&st, &ts))
-# #       |                 ^~~~~~~~~~~~~~~~~~~~~~~~~~
-# # utimens.c:612:17: note: referencing argument 2 of type 'struct timespec **'
-# # utimens.c:136:1: note: in a call to function 'update_timespec'
-# #   136 | update_timespec (struct stat const *statbuf, struct timespec *ts[2])
-# #       | ^~~~~~~~~~~~~~~
-
 koopa::install_wget() { # {{{1
     # """
     # Install wget.
     # @note Updated 2021-05-10.
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/wget.rb
     # """
-    local conf_args gcc_version install_args
+    local conf_args gcc_version install_args pkgs pkgs_string
     install_args=()
     conf_args=()
     if koopa::is_linux
@@ -37,12 +19,26 @@ koopa::install_wget() { # {{{1
     then
         gcc_version="$(koopa::variable 'gcc')"
         gcc_version="$(koopa::major_version "$gcc_version")"
-        # clang currently fails to build this, so use GCC instead.
+        pkgs=(
+            'autoconf'
+            'automake'
+            "gcc@${gcc_version}"
+            'gettext'
+            'gnutls'
+            'libidn2'
+            'openssl'
+            'pkg-config'
+        )
+        pkgs_string="$(koopa::paste0 ',' "${pkgs[@]}")"
         install_args+=(
-            "--homebrew-opt=gcc@${gcc_version},gnutls,libpsl,openssl,pkg-config"
+            "--homebrew-opt=${pkgs_string}"
         )
         conf_args+=(
             "CC=gcc-${gcc_version}"
+            '--disable-debug'
+            '--disable-pcre'
+            '--without-included-regex'
+            '--without-libpsl'
         )
     fi
     koopa::install_gnu_app \
