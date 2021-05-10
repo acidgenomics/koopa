fb72907d299d02f17335d7564dc7e3462d390025
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-10 17:47:15 -0400

    Attempt to improve dircolors handling for rstudio

--- lang/shell/posix/functions/activate-interactive.sh
+++ lang/shell/posix/functions/activate-interactive.sh
@@ -94,6 +94,7 @@ _koopa_activate_dircolors() { # {{{1
     # This will set the 'LD_COLORS' environment variable.
     # """
     local dir dircolors dircolors_file dotfiles_prefix egrep fgrep grep ls vdir
+    echo 'FIXME RSTUDIO'
     _koopa_is_interactive || return 0
     [ -n "${SHELL:-}" ] || return 0
     dir='dir'
