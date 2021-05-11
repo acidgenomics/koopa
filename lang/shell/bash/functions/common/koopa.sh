d7c27e3d42b1ef65e75d40105348cd755895f618
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-11 12:02:13 -0400

    Draft update

--- lang/shell/bash/functions/common/koopa.sh
+++ lang/shell/bash/functions/common/koopa.sh
@@ -119,10 +119,15 @@ koopa:::koopa_install() { # {{{1
     done
     for app in "${apps[@]}"
     do
+        # FIXME THIS IS ERRORING IN THE AMZN DOCKER IMAGE ARGH...
+        # NEED TO FIGURE OUT HOW TO ADDRESS THIS...
+        # FIXME Can we pass "${app_args[@]-}"  here instead?
         if koopa::is_array_non_empty "${app_args[@]}"
         then
+            echo 'FIXME 1'
             koopa:::run_function "install-${app}" "${app_args[@]}"
         else
+            echo 'FIXME2'
             koopa:::run_function "install-${app}"
         fi
     done
