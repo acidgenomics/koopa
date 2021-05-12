964f82ae900525b4269941466638e9934af39467
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-12 12:44:43 -0400

    Install starship

--- lang/shell/bash/functions/common/install/rust.sh
+++ lang/shell/bash/functions/common/install/rust.sh
@@ -108,6 +108,7 @@ koopa::install_rust_packages() { # {{{1
             # Currently failing to build due to cachedir constraint.
             # https://github.com/phiresky/ripgrep-all/issues/88
             # > 'ripgrep-all'
+            'starship'
             'tokei'
             'xsv'
             'zoxide'
