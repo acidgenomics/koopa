1c24e2ad89586ff68224b13e5eaa29513a4ef3f8
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-17 16:46:51 -0400

    Harden activation in Ksh environment

--- lang/shell/posix/include/profile.sh
+++ lang/shell/posix/include/profile.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-# koopa shell
-# https://koopa.acidgenomics.com/
-# shellcheck source=/dev/null
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
-if [ -f "${XDG_CONFIG_HOME}/koopa/activate" ]
-then
-    # shellcheck source=/dev/null
-    . "${XDG_CONFIG_HOME}/koopa/activate"
-fi
+__koopa_activate_user_profile() { # {{{1
+    # """
+    # Activate koopa shell for current user.
+    # @note Updated 2021-05-17.
+    # @seealso https://koopa.acidgenomics.com/
+    # """
+    export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+    if [ -f "${XDG_CONFIG_HOME}/koopa/activate" ]
+    then
+        # shellcheck source=/dev/null
+        . "${XDG_CONFIG_HOME}/koopa/activate"
+    fi
+    return 0
+}
+
+__koopa_activate_user_profile
