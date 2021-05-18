4a82aa3e33f78535e38bc106d71b53125be79141
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-18 12:14:30 -0400

    Draft updates to rethink R configuration

--- lang/shell/bash/functions/os/darwin/install/r.sh
+++ lang/shell/bash/functions/os/darwin/install/r.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
+
+# FIXME Need to get ARM or Intel.
+
 koopa::macos_install_r_cran_gfortran() { # {{{1
     # """
     # Install CRAN gfortran.
-    # @note Updated 2021-04-22.
+    # @note Updated 2021-05-18.
     # @seealso
     # - https://mac.r-project.org/tools/
     # - https://github.com/fxcoudert/gfortran-for-macOS/
     # """
-    local file name os_codename pkg prefix reinstall stem tmp_dir url version
-    # This is compatible with Catalina and Big Sur for 8.2. May need to rework
-    # the variable handling for this in a future update.
-    os_codename='Mojave'
+    local arch file file_stem name os_codename pkg prefix reinstall tmp_dir \
+        url url_stem version
+    arch="$(koopa::arch)"
+    case "$arch" in
+        aarch64)
+            arch='ARM'
+            ;;
+        x86_64)
+            arch='Intel'
+            ;;
+        *)
+            koopa::stop "Unsupported architecture: '${arch}'."
+            ;;
+    esac
     reinstall=0
     version="$(koopa::variable 'r-cran-gfortran')"
     while (("$#"))
@@ -40,17 +53,33 @@ koopa::macos_install_r_cran_gfortran() { # {{{1
         return 0
     fi
     koopa::install_start "$name" "$version" "$prefix"
+    # Example URLs:
+    # - 8.2 Mojave
+    #   https://github.com/fxcoudert/gfortran-for-macOS/releases/download/
+    #     8.2/gfortran-8.2-Mojave.dmg
+    # - 10.2 BigSur
+    #   https://github.com/fxcoudert/gfortran-for-macOS/releases/download/
+    #     10.2-bigsur-intel/gfortran-10.2-BigSur-Intel.dmg
+    url_stem='https://github.com/fxcoudert/gfortran-for-macOS/releases/download'
+    case "$version" in
+        8.2)
+            os_codename='Mojave'
+            file_stem="${name}-${version}-${os_codename}"
+            file="${file_stem}.dmg"
+            url="${url_stem}/${version}/${file}"
+            pkg="/Volumes/${file_stem}/${file_stem}/${name}.pkg"
+            ;;
+        10.2)
+            os_codename="BigSur-${arch}"
+            ;;
+    esac
     tmp_dir="$(koopa::tmp_dir)"
     (
         koopa::cd "$tmp_dir"
-        stem="${name}-${version}-${os_codename}"
-        file="${stem}.dmg"
-        url="https://mac.r-project.org/tools/${file}"
         koopa::download "$url"
         hdiutil mount "$file"
-        pkg="/Volumes/${stem}/${stem}/gfortran.pkg"
         sudo installer -pkg "$pkg" -target /
-        hdiutil unmount "/Volumes/${stem}"
+        hdiutil unmount "/Volumes/${file_stem}"
     ) 2>&1 | tee "$(koopa::tmp_log_file)"
     koopa::rm "$tmp_dir"
     koopa::install_success "$name" "$prefix"
