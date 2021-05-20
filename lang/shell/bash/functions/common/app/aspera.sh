89dcfa912d8532e053c2ddb81a3df5909eb96f02
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-20 18:09:00 -0400

    Harden functions for macOS

--- lang/shell/bash/functions/common/app/aspera.sh
+++ lang/shell/bash/functions/common/app/aspera.sh
@@ -5,16 +5,25 @@ koopa::sra_prefetch_parallel() { # {{{1
     # Prefetch files from SRA in parallel.
     # @note Updated 2021-05-20.
     # """
-    koopa::assert_is_gnu 'find' 'parallel'
+    local brew_prefix find parallel
     koopa::assert_is_installed 'ascp' 'prefetch'
+    find='find'
+    parallel='parallel'
+    if koopa::is_macos
+    then
+        brew_prefix="$(koopa::homebrew_prefix)"
+        find="${brew_prefix}/bin/gfind"
+        parallel="${brew_prefix}/bin/parallel"
+    fi
+    koopa::assert_is_gnu "$find" "$parallel"
     file="${1:-}"
     [ -z "$file" ] && file='SraAccList.txt'
     koopa::assert_is_file "$file"
     jobs="$(koopa::cpu_count)"
     # Delete any temporary files that may have been created by previous run.
-    find . \(-name '*.lock' -o -name '*.tmp'\) -delete
+    "$find" . \(-name '*.lock' -o -name '*.tmp'\) -delete
     sort -u "$file" \
-        | parallel -j "$jobs" \
+        | "$parallel" -j "$jobs" \
             'prefetch --verbose {}'
     return 0
 }
