59687a5765d17f46226ef80e8f7cf2baf3aeda11
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-21 11:12:58 -0400

    Work toward reorganizing POSIX functions

--- lang/shell/posix/functions/strings-variables.sh
+++ lang/shell/posix/functions/strings-variables.sh
@@ -37,6 +37,31 @@ _koopa_cpu_count() { # {{{1
     return 0
 }
 
+_koopa_git_branch() { # {{{1
+    # """
+    # Current git branch name.
+    # @note Updated 2020-07-05.
+    #
+    # This is used in prompt, so be careful with assert checks.
+    #
+    # Handles detached HEAD state.
+    #
+    # Alternatives:
+    # > git name-rev --name-only HEAD
+    # > git rev-parse --abbrev-ref HEAD
+    #
+    # @seealso
+    # - https://git.kernel.org/pub/scm/git/git.git/tree/contrib/completion/
+    #       git-completion.bash?id=HEAD
+    # """
+    local branch
+    _koopa_is_git || return 0
+    _koopa_is_installed git || return 0
+    branch="$(git symbolic-ref --short -q HEAD 2>/dev/null)"
+    _koopa_print "$branch"
+    return 0
+}
+
 _koopa_group() { # {{{1
     # """
     # Current user's default group.
