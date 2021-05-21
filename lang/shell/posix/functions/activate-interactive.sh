ce05f6fdb65fe409360371c58343b34fa6fa990d
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-21 11:10:33 -0400

    Reorganize POSIX functions

--- lang/shell/posix/functions/activate-interactive.sh
+++ lang/shell/posix/functions/activate-interactive.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# shellcheck disable=SC2032
 
 _koopa_activate_aliases() { # {{{1
     # """
@@ -242,6 +243,72 @@ _koopa_activate_starship() { # {{{1
     [ "$nounset" -eq 1 ] && set +u
     eval "$(starship init "$shell")"
     [ "$nounset" -eq 1 ] && set -u
+    return 0
+}
+
+_koopa_activate_tmux_sessions() { # {{{1
+    # """
+    # Show active tmux sessions.
+    # @note Updated 2021-05-21.
+    # """
+    local x
+    _koopa_is_installed tmux || return 0
+    _koopa_is_tmux && return 0
+    # FIXME Rework using new GNU functions.
+    cut='cut'
+    tr='tr'
+    if _koopa_is_macos
+    then
+        brew_prefix="$(_koopa_homebrew_prefix)"
+        cut="${brew_prefix}/bin/gcut"
+        tr="${brew_prefix}/bin/gtr"
+    fi
+    _koopa_is_gnu "$cut" "$tr" || return 0
+    # shellcheck disable=SC2033
+    x="$(tmux ls 2>/dev/null || true)"
+    [ -n "$x" ] || return 0
+    x="$( \
+        _koopa_print "$x" \
+        | "$cut" -d ':' -f 1 \
+        | "$tr" '\n' ' ' \
+    )"
+    _koopa_dl 'tmux' "$x"
+    return 0
+}
+
+_koopa_activate_today_bucket() { # {{{1
+    # """
+    # Create a dated file today bucket.
+    # @note Updated 2020-07-05.
+    #
+    # Also adds a '~/today' symlink for quick access.
+    #
+    # How to check if a symlink target matches a specific path:
+    # https://stackoverflow.com/questions/19860345
+    #
+    # Useful link flags:
+    # -f, --force
+    #        remove existing destination files
+    # -n, --no-dereference
+    #        treat LINK_NAME as a normal file if it is a symbolic link to a
+    #        directory
+    # -s, --symbolic
+    #        make symbolic links instead of hard links
+    # """
+    local bucket_dir today_bucket today_link
+    bucket_dir="${KOOPA_BUCKET:-}"
+    [ -z "$bucket_dir" ] && bucket_dir="${HOME:?}/bucket"
+    # Early return if there's no bucket directory on the system.
+    [ -d "$bucket_dir" ] || return 0
+    today_bucket="$(date '+%Y/%m/%d')"
+    today_link="${HOME:?}/today"
+    # Early return if we've already updated the symlink.
+    _koopa_is_installed readlink || return 0
+    _koopa_str_match "$(readlink "$today_link")" "$today_bucket" && return 0
+    # FIXME Need to harden this using GNU variants..
+    mkdir -p "${bucket_dir}/${today_bucket}"
+    # FIXME Need to harden this using GNU variants..
+    ln -fns "${bucket_dir}/${today_bucket}" "$today_link"
     return 0
 }
 
--- lang/shell/posix/functions/system-coreutils.sh
+++ lang/shell/posix/functions/system-coreutils.sh
@@ -86,6 +86,38 @@ _koopa_realpath() { # {{{1
     return 0
 }
 
+_koopa_umask() { # {{{1
+    # """
+    # Set default file permissions.
+    # @note Updated 2020-06-03.
+    #
+    # - 'umask': Files and directories.
+    # - 'fmask': Only files.
+    # - 'dmask': Only directories.
+    #
+    # Use 'umask -S' to return 'u,g,o' values.
+    #
+    # - 0022: 'u=rwx,g=rx,o=rx'.
+    #         User can write, others can read. Usually default.
+    # - 0002: 'u=rwx,g=rwx,o=rx'.
+    #         User and group can write, others can read.
+    #         Recommended setting in a shared coding environment.
+    # - 0077: 'u=rwx,g=,o='.
+    #         User alone can read/write. More secure.
+    #
+    # Access control lists (ACLs) are sometimes preferable to umask.
+    #
+    # Here's how to use ACLs with setfacl.
+    # > setfacl -d -m group:name:rwx /dir
+    #
+    # @seealso
+    # - https://stackoverflow.com/questions/13268796
+    # - https://askubuntu.com/questions/44534
+    # """
+    umask 0002
+    return 0
+}
+
 _koopa_which() { # {{{1
     # """
     # Locate which program.
