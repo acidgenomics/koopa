a9c297b8e832966658fdfb57fbe925f5bb9c91d1
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-21 08:33:12 -0400

    Rework GNU coreutils approach

--- lang/shell/posix/functions/gnu.sh
+++ lang/shell/posix/functions/gnu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-koopa:::gnu_app() { # {{{1
+__koopa_gnu_app() { # {{{1
     # """
     # GNU app.
     # @note Updated 2021-05-21.
@@ -12,38 +12,51 @@ koopa:::gnu_app() { # {{{1
     koopa::assert_has_args "$#"
     if koopa::is_macos
     then
-        brew_prefix="$(koopa::homebrew_prefix)"
+        brew_prefix="$(_koopa_homebrew_prefix)"
         # > cmd="${brew_prefix}/opt/${brew_opt}/bin/g${cmd}"
         cmd="${brew_prefix}/opt/${brew_opt}/libexec/gnubin/${cmd}"
     fi
-    koopa::assert_is_gnu "$cmd"
+    if [ ! -x "$cmd" ]
+    then
+        _koopa_warning "Missing GNU app: '${cmd}'."
+        return 1
+    fi
     "$cmd" "$@"
     return 0
 }
 
-koopa::gnu_find() { # {{{1
+_koopa_gnu_find() { # {{{1
     # """
     # GNU tr.
     # @note Updated 2021-05-21.
     # """
-    koopa:::gnu_app 'findutils' 'find' "$@"
+    __koopa_gnu_app 'findutils' 'find' "$@"
     return 0
 }
 
-koopa::gnu_tr() { # {{{1
+_koopa_gnu_tr() { # {{{1
     # """
     # GNU tr.
     # @note Updated 2021-05-21.
     # """
-    koopa:::gnu_app 'coreutils' 'tr' "$@"
+    __koopa_gnu_app 'coreutils' 'tr' "$@"
     return 0
 }
 
-koopa::gnu_uname() { # {{{1
+_koopa_gnu_uname() { # {{{1
     # """
     # GNU uname.
     # @note Updated 2021-05-21.
     # """
-    koopa:::gnu_app 'coreutils' 'uname' "$@"
+    __koopa_gnu_app 'coreutils' 'uname' "$@"
+    return 0
+}
+
+_koopa_gnu_wc() { # {{{1
+    # """
+    # GNU wc.
+    # @note Updated 2021-05-21.
+    # """
+    __koopa_gnu_app 'coreutils' 'wc' "$@"
     return 0
 }
