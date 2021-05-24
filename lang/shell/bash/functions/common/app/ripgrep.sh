bb992e9bcedc9df1d015f412a58529a20cc4aeb2
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-24 09:52:29 -0400

    Harden coreutils and update Renviron

--- lang/shell/bash/functions/common/app/ripgrep.sh
+++ lang/shell/bash/functions/common/app/ripgrep.sh
@@ -4,13 +4,18 @@
 koopa::rg_fixme() { # {{{1
     # """
     # ripgrep for 'FIXME' string.
-    # @note Updated 2021-05-08.
+    # @note Updated 2021-05-24.
     # """
     local pattern x
     koopa::assert_has_no_args "$#"
-    koopa::assert_is_installed rg
+    koopa::assert_is_installed 'rg'
     pattern='FIXME'
-    x="$(rg -il "$pattern" | sort)"
+    x="$( \
+        rg \
+        --files-with-matches \
+        --sort 'path' \
+        "$pattern" \
+    )"
     [[ -n "$x" ]] || return 1
     koopa::print "$x"
     return 0
@@ -19,13 +24,18 @@ koopa::rg_fixme() { # {{{1
 koopa::rg_sort() { # {{{1
     # """
     # ripgrep sorted.
-    # @note Updated 2021-05-08.
+    # @note Updated 2021-05-24.
     # """
     local pattern x
     koopa::assert_has_args "$#" 1
-    koopa::assert_is_installed rg
+    koopa::assert_is_installed 'rg'
     pattern="${1:?}"
-    x="$(rg --pretty --sort path "$pattern")"
+    x="$( \
+        rg \
+            --pretty \
+            --sort 'path' \
+            "$pattern" \
+    )"
     [[ -n "$x" ]] || return 1
     koopa::print "$x"
     return 0
@@ -34,18 +44,19 @@ koopa::rg_sort() { # {{{1
 koopa::rg_unique() { # {{{1
     # """
     # ripgrep, but only return a summary of all unique matches.
-    # @note Updated 2021-05-08.
+    # @note Updated 2021-05-24.
     # """
-    local pattern
+    local pattern x
     koopa::assert_has_args_eq "$#" 1
-    koopa::assert_is_installed rg
+    koopa::assert_is_installed 'rg'
     pattern="${1:?}"
     x="$( \
         rg \
             --no-filename \
             --no-line-number \
-            --only-matching "$pattern" \
-        | sort -u \
+            --only-matching \
+            --sort 'path' \
+            "$pattern" \
     )"
     koopa::print "$x"
     return 0
