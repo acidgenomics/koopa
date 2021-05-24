736c6e434922c9837a487385e69a3ebc87a4f3e2
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-24 10:15:08 -0400

    Harden coreutils

--- lang/shell/bash/functions/common/fs/download.sh
+++ lang/shell/bash/functions/common/fs/download.sh
@@ -66,16 +66,22 @@ koopa::download() { # {{{1
 koopa::download_cran_latest() { # {{{1
     # """
     # Download CRAN latest.
-    # @note Updated 2020-07-11.
+    # @note Updated 2021-05-24.
     # """
-    local name pattern url
+    local curl file grep head name pattern url
     koopa::assert_has_args "$#"
-    koopa::assert_is_installed curl grep head
+    curl="$(koopa::locate_curl)"
+    grep="$(koopa::locate_grep)"
+    head="$(koopa::locate_head)"
     for name in "$@"
     do
         url="https://cran.r-project.org/web/packages/${name}/"
         pattern="${name}_[.0-9]+.tar.gz"
-        file="$(curl --silent "$url" | grep -Eo "$pattern" | head -n 1)"
+        file="$( \
+            "$curl" --silent "$url" \
+            | "$grep" -Eo "$pattern" \
+            | "$head" -n 1 \
+        )"
         koopa::download "https://cran.r-project.org/src/contrib/${file}"
     done
     return 0
@@ -88,7 +94,6 @@ koopa::download_github_latest() { # {{{1
     # """
     local api_url curl cut grep repo tag tarball_url tr
     koopa::assert_has_args "$#"
-    basename="$(koopa::locate_basename)"
     curl="$(koopa::locate_curl)"
     cut="$(koopa::locate_cut)"
     grep="$(koopa::locate_grep)"
@@ -102,7 +107,7 @@ koopa::download_github_latest() { # {{{1
             | "$cut" -d ':' -f 2,3 \
             | "$tr" -d ' ,"' \
         )"
-        tag="$("$basename" "$tarball_url")"
+        tag="$(koopa::basename "$tarball_url")"
         koopa::download "$tarball_url" "${tag}.tar.gz"
     done
     return 0
@@ -136,19 +141,21 @@ koopa::download_refdata_scsig() { # {{{1
 koopa::download_sra_accession_list() { # {{{1
     # """
     # Download SRA accession list.
-    # @note Updated 2020-07-02.
+    # @note Updated 2021-05-24.
     # """
-    local file id
+    local cut file id sed
     koopa::assert_has_args_le "$#" 2
-    koopa::activate_conda_env entrez-direct
-    koopa::assert_is_installed esearch efetch
+    cut="$(koopa::locate_cut)"
+    sed="$(koopa::locate_sed)"
+    koopa::activate_conda_env 'entrez-direct'
+    koopa::assert_is_installed 'esearch' 'efetch'
     id="${1:?}"
     file="${2:-SraAccList.txt}"
     koopa::h1 "Downloading SRA '${id}' to '${file}'."
     esearch -db sra -q "$id" \
-        | efetch -format runinfo \
-        | sed 1d \
-        | cut -d ',' -f 1 \
+        | efetch -format 'runinfo' \
+        | "$sed" 1d \
+        | "$cut" -d ',' -f 1 \
         > "$file"
     return 0
 }
@@ -156,10 +163,11 @@ koopa::download_sra_accession_list() { # {{{1
 koopa::download_sra_run_info_table() { # {{{1
     # """
     # Download SRA run info table.
-    # @note Updated 2020-07-02.
+    # @note Updated 2021-05-24.
     # """
-    koopa::activate_conda_env entrez-direct
-    koopa::assert_is_installed esearch efetch
+    koopa::assert_has_args_le "$#" 2
+    koopa::activate_conda_env 'entrez-direct'
+    koopa::assert_is_installed 'esearch' 'efetch'
     id="${1:?}"
     file="${2:-SraRunTable.txt}"
     koopa::h1 "Downloading SRA '${id}' to '${file}'."
@@ -170,9 +178,9 @@ koopa::download_sra_run_info_table() { # {{{1
 }
 
 koopa::ftp_mirror() { # {{{1
-    local dir host user
+    local dir host user wget
     koopa::assert_has_args "$#"
-    koopa::assert_is_installed wget
+    wget="$(koopa::locate_wget)"
     dir=''
     while (("$#"))
     do
@@ -194,35 +202,29 @@ koopa::ftp_mirror() { # {{{1
                 ;;
         esac
     done
-    koopa::assert_is_set host user
+    koopa::assert_is_set 'host' 'user'
     if [[ -n "$dir" ]]
     then
         dir="${host}/${dir}"
     else
         dir="${host}"
     fi
-    wget --ask-password --mirror "ftp://${user}@${dir}/"*
+    "$wget" --ask-password --mirror "ftp://${user}@${dir}/"*
     return 0
 }
 
 koopa::wget_recursive() { # {{{1
     # """
     # Download files with wget recursively.
-    # @note Updated 2021-05-20.
+    # @note Updated 2021-05-24.
     #
     # Note that we need to escape the wildcards in the password.
     # For direct input, can just use single quotes to escape.
     # See also: https://unix.stackexchange.com/questions/379181
     # """
-    local brew_prefix datetime log_file name password url user wget wget_args
+    local datetime log_file name password url user wget wget_args
     koopa::assert_has_args_eq "$#" 3
-    wget='wget'
-    if koopa::is_macos
-    then
-        brew_prefix="$(koopa::homebrew_prefix)"
-        wget="${brew_prefix}/bin/wget"
-    fi
-    koopa::assert_is_installed "$wget"
+    wget="$(koopa::locate_wget)"
     url="${1:?}"
     user="${2:?}"
     password="${3:?}"
