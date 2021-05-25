5a5e75f71f904f0ab7db2a4a240bee10ef7da4a2
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-25 13:24:48 -0400

    Improve our installers

--- lang/shell/bash/functions/common/install/vim.sh
+++ lang/shell/bash/functions/common/install/vim.sh
@@ -10,17 +10,17 @@ koopa::install_vim() { # {{{1
 koopa:::install_vim() { # {{{1
     # """
     # Install Vim.
-    # @note Updated 2021-05-23.
+    # @note Updated 2021-05-25.
     # """
-    local conf_args file jobs name prefix python python_config
+    local conf_args file jobs make name prefix python python_config
     local python_config_dir url version
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
-    name='vim'
+    make="$(koopa::locate_make)"
     jobs="$(koopa::cpu_count)"
     if koopa::is_linux
     then
-        koopa::activate_opt_prefix python
+        koopa::activate_opt_prefix 'python'
     elif koopa::is_macos
     then
         koopa::macos_activate_python
@@ -49,14 +49,15 @@ koopa:::install_vim() { # {{{1
     then
         conf_args+=("LDFLAGS=-Wl,-rpath=${prefix}/lib")
     fi
+    name='vim'
     file="v${version}.tar.gz"
     url="https://github.com/${name}/${name}/archive/${file}"
     koopa::download "$url"
     koopa::extract "$file"
     koopa::cd "${name}-${version}"
     ./configure "${conf_args[@]}"
-    make --jobs="$jobs"
-    # > make test
-    make install
+    "$make" --jobs="$jobs"
+    # > "$make" test
+    "$make" install
     return 0
 }
