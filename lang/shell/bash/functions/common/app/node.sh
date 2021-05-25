e92866e50b76c33637b2ec3fa920c0c8cb73caf0
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-25 11:57:59 -0400

    Improve NPM configuration

--- lang/shell/bash/functions/common/app/node.sh
+++ lang/shell/bash/functions/common/app/node.sh
@@ -6,12 +6,21 @@ koopa::configure_node() { # {{{1
     # @note Updated 2021-05-25.
     # """
     local version
+    koopa::assert_is_installed 'node'
     name_fancy='Node.js'
     version="$(koopa::get_version 'node')"
     version="$(koopa::major_minor_version "$version")"
     prefix="$(koopa::node_packages_prefix "$version")"
     koopa::configure_start "$name_fancy" "$prefix"
-    echo "$prefix"
+    if [[ ! -d "$prefix" ]]
+    then
+        koopa::sys_mkdir "$prefix"
+        (
+            koopa::sys_set_permissions "$(koopa::dirname "$prefix")"
+            koopa::cd "$(koopa::dirname "$prefix")"
+            koopa::sys_ln "$(koopa::basename "$prefix")" 'latest'
+        )
+    fi
     koopa::configure_success "$name_fancy" "$prefix"
     return 0
 }
