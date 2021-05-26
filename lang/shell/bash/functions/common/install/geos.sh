371a220ef11b32c3d4dc1370b62b609d07d602a7
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-26 09:11:59 -0400

    Harden installer paths

--- lang/shell/bash/functions/common/install/geos.sh
+++ lang/shell/bash/functions/common/install/geos.sh
@@ -11,7 +11,7 @@ koopa::install_geos() { # {{{1
 koopa:::install_geos() { # {{{1
     # """
     # Install GEOS.
-    # @note Updated 2021-05-10.
+    # @note Updated 2021-05-26.
     #
     # Can build with autotools or cmake.
     # See 'INSTALL' file for details.
@@ -28,15 +28,17 @@ koopa:::install_geos() { # {{{1
     # > make --jobs="$jobs"
     # > make check
     # """
-    local cmake_args file jobs name prefix url version
+    local cmake cmake_args file jobs make name prefix url version
     if koopa::is_macos
     then
         koopa::activate_homebrew_opt_prefix cmake
     fi
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
-    name='geos'
     jobs="$(koopa::cpu_count)"
+    cmake="$(koopa::locate_cmake)"
+    make="$(koopa::locate_make)"
+    name='geos'
     file="${version}.tar.gz"
     url="https://github.com/lib${name}/${name}/archive/${file}"
     koopa::download "$url"
@@ -48,9 +50,9 @@ koopa:::install_geos() { # {{{1
         "-DCMAKE_INSTALL_PREFIX=${prefix}"
         # > '-DGEOS_ENABLE_TESTS=OFF'
     )
-    cmake "${cmake_args[@]}"
-    make --jobs="$jobs"
-    # > make test
-    make install
+    "$cmake" "${cmake_args[@]}"
+    "$make" --jobs="$jobs"
+    # > "$make" test
+    "$make" install
     return 0
 }
