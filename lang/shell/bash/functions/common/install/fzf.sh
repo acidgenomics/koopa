82b15586b3291fe430679ec88a9a4c419585f943
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-26 08:55:00 -0400

    Harden installer paths

--- lang/shell/bash/functions/common/install/fzf.sh
+++ lang/shell/bash/functions/common/install/fzf.sh
@@ -10,17 +10,18 @@ koopa::install_fzf() { # {{{1
 koopa:::install_fzf() { # {{{1
     # """
     # Install fzf.
-    # @note Updated 2021-05-05.
+    # @note Updated 2021-05-26.
     # @seealso
     # - https://github.com/junegunn/fzf/blob/master/BUILD.md
     # """
-    local file jobs name prefix url version
+    local file jobs make name prefix url version
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
-    name='fzf'
     koopa::activate_go
-    koopa::assert_is_installed go
+    koopa::assert_is_installed 'go'
     jobs="$(koopa::cpu_count)"
+    make="$(koopa::locate_make)"
+    name='fzf'
     koopa::mkdir "$prefix"
     file="${version}.tar.gz"
     url="https://github.com/junegunn/${name}/archive/${file}"
@@ -29,11 +30,11 @@ koopa:::install_fzf() { # {{{1
     koopa::cd "${name}-${version}"
     export FZF_VERSION="$version"
     export FZF_REVISION='tarball'
-    make --jobs="$jobs"
-    # > make test
+    "$make" --jobs="$jobs"
+    # > "$make" test
     # This will copy fzf binary from 'target/' to 'bin/' inside tmp dir.
     # Note that this step does not copy to '/usr/bin/'.
-    make install
+    "$make" install
     # > ./install --help
     ./install --bin --no-update-rc
     # Following approach used in Homebrew recipe here.
