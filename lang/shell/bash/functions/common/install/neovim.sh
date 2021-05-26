0b7c0dd23f175ea7de582e17ec23bb96f5627b1c
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-26 13:56:55 -0400

    Update installers

--- lang/shell/bash/functions/common/install/neovim.sh
+++ lang/shell/bash/functions/common/install/neovim.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# NOTE This is failing to build on macOS.
+
 koopa::install_neovim() { # {{{1
     koopa::install_app \
         --name='neovim' \
@@ -10,18 +12,39 @@ koopa:::install_neovim() { # {{{1
     # """
     # Install Neovim.
     # @note Updated 2021-05-26.
+    # @seealso
+    # - https://github.com/neovim/neovim/wiki/Building-Neovim
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/neovim.rb
+    # - https://github.com/neovim/neovim/issues/11192
     # """
     local file jobs make name prefix url version
+    if koopa::is_macos
+    then
+        koopa::activate_homebrew_opt_prefix \
+            'cmake' \
+            'luarocks' \
+            'pkg-config'
+    fi
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
     jobs="$(koopa::cpu_count)"
     make="$(koopa::locate_make)"
     name='neovim'
-    file="v${version}.tar.gz"
-    url="https://github.com/${name}/${name}/archive/${file}"
+    file="${version}.tar.gz"
+    case "$version" in
+        nightly)
+            ;;
+        *)
+            file="v${file}"
+            ;;
+    esac
+    url="https://github.com/${name}/${name}/archive/refs/tags/${file}"
     koopa::download "$url"
     koopa::extract "$file"
     koopa::cd "${name}-${version}"
+    # > "$make" distclean
+    # Alterantively, can use:
+    # CMAKE_BUILD_TYPE='RelWithDebInfo'
     "$make" \
         --jobs="$jobs" \
         CMAKE_BUILD_TYPE='Release' \
