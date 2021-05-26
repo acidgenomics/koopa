eaa01f2b29bcd5c23897e726c22a772dcae4b507
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-26 09:25:18 -0400

    Harden installer paths

--- lang/shell/bash/functions/common/install/htop.sh
+++ lang/shell/bash/functions/common/install/htop.sh
@@ -9,17 +9,18 @@ koopa::install_htop() { # {{{1
 koopa:::install_htop() { # {{{1
     # """
     # Install htop.
-    # @note Updated 2021-05-06.
+    # @note Updated 2021-05-26.
     #
     # Repo transferred from https://github.com/hishamhm/htop to 
     # https://github.com/htop-dev/htop in 2020-08.
     # """
-    local conf_args file jobs name prefix url version
+    local conf_args file jobs make name prefix url version
     koopa::assert_is_installed python3
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
-    name='htop'
     jobs="$(koopa::cpu_count)"
+    make="$(koopa::locate_make)"
+    name='htop'
     file="${version}.tar.gz"
     url="https://github.com/${name}-dev/${name}/archive/${file}"
     koopa::download "$url"
@@ -31,8 +32,8 @@ koopa:::install_htop() { # {{{1
         '--disable-unicode'
     )
     ./configure "${conf_args[@]}"
-    make --jobs="$jobs"
-    # > make check
-    make install
+    "$make" --jobs="$jobs"
+    # > "$make" check
+    "$make" install
     return 0
 }
