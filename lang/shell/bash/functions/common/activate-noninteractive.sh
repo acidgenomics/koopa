ad5650771805ab63598e70c98cf014f0d1222a6a
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-26 07:14:59 -0400

    Work on cleaning up POSIX functions

--- lang/shell/bash/functions/common/activate-noninteractive.sh
+++ lang/shell/bash/functions/common/activate-noninteractive.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+koopa::activate_ensembl_perl_api() { # {{{1
+    # """
+    # Activate Ensembl Perl API.
+    # @note Updated 2021-05-26.
+    #
+    # Note that this currently requires Perl 5.26.
+    # > perlbrew switch perl-5.26
+    # """
+    local prefix
+    prefix="$(koopa::ensembl_perl_api_prefix)"
+    koopa::assert_is_dir "$prefix"
+    koopa::activate_prefix "${prefix}/ensembl-git-tools"
+    PERL5LIB="${PERL5LIB}:${prefix}/bioperl-1.6.924"
+    PERL5LIB="${PERL5LIB}:${prefix}/ensembl/modules"
+    PERL5LIB="${PERL5LIB}:${prefix}/ensembl-compara/modules"
+    PERL5LIB="${PERL5LIB}:${prefix}/ensembl-variation/modules"
+    PERL5LIB="${PERL5LIB}:${prefix}/ensembl-funcgen/modules"
+    export PERL5LIB
+    return 0
+}
+
 koopa::activate_homebrew_opt_prefix() { # {{{1
     # """
     # Activate Homebrew opt prefix.
