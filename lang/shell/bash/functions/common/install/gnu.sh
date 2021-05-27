494e781ae2a6aba3830d05a60d1c7436f7bb4388
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-27 11:36:44 -0400

    Install fix for findutils on macOS

--- lang/shell/bash/functions/common/install/gnu.sh
+++ lang/shell/bash/functions/common/install/gnu.sh
@@ -85,6 +85,18 @@ koopa::install_coreutils() { # {{{1
 }
 
 koopa::install_findutils() { # {{{1
+    if koopa::is_macos
+    then
+        # Workaround for build failures in 4.8.0.
+        # See also:
+        # - https://github.com/Homebrew/homebrew-core/blob/master/
+        #     Formula/findutils.rb
+        # - https://lists.gnu.org/archive/html/bug-findutils/2021-01/
+        #     msg00050.html
+        # - https://lists.gnu.org/archive/html/bug-findutils/2021-01/
+        #     msg00051.html
+        export CFLAGS='-D__nonnull\(params\)='
+    fi
     koopa::install_gnu_app \
         --name='findutils' \
         "$@"
