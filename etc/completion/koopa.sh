521f350691de3b8f87d65ab523c6ed4a842c5708
Michael Steinbaugh <mike@steinbaugh.com>
2021-05-28 13:40:17 -0400

    Work toward improving completion

--- etc/completion/koopa.sh
+++ etc/completion/koopa.sh
@@ -86,7 +86,7 @@ _koopa_complete() { # {{{1
                     'hdf5'
                     'homebrew'
                     'homebrew-bundle'
-                    'homebrew-packages'
+                    'homebrew-packages'  # FIXME Remove?
                     'htop'
                     'julia'
                     'libevent'
@@ -94,7 +94,7 @@ _koopa_complete() { # {{{1
                     'lua'
                     'luarocks'
                     'make'
-                    'miniconda'
+                    'miniconda'  # FIXME Rename to just conda...
                     'ncurses'
                     'neofetch'
                     'neovim'
@@ -130,8 +130,8 @@ _koopa_complete() { # {{{1
                     'shellcheck'
                     'shunit2'
                     'singularity'
-                    'spacemacs'
-                    'spacevim'
+                    'spacemacs'  # FIXME Need to indicate user here?
+                    'spacevim'  # FIXME Need to indicate user here?
                     'sqlite'
                     'subversion'
                     'taglib'
@@ -167,6 +167,7 @@ _koopa_complete() { # {{{1
                         'homebrew-little-snitch'
                         'python-framework'
                         'r-cran-gfortran'
+                        'r-framework'  # FIXME
                         'xcode-clt'
                     )
                 fi
@@ -214,17 +215,119 @@ _koopa_complete() { # {{{1
                 fi
                 ;;
             uninstall)
-                # FIXME Need to improve this.
                 args=(
+                    'anaconda'  # FIXME
+                    'autoconf'  # FIXME
+                    'automake'  # FIXME
+                    'bash'  # FIXME
+                    'binutils'  # FIXME
+                    'cmake'  # FIXME
+                    'conda'  # FIXME
+                    'coreutils'  # FIXME
+                    'cpufetch'  # FIXME
+                    'curl'  # FIXME
+                    'doom-emacs'  # FIXME
                     'dotfiles'
+                    'emacs'  # FIXME
+                    'ensembl-perl-api'  # FIXME
+                    'findutils'  # FIXME
+                    'fish'  # FIXME
+                    'fzf'  # FIXME
+                    'gawk'  # FIXME
+                    'gcc'  # FIXME
+                    'gdal'  # FIXME
+                    'geos'  # FIXME
+                    'git'  # FIXME
+                    'gnupg'  # FIXME
+                    'go'  # FIXME
+                    'grep'  # FIXME
+                    'groff'  # FIXME
+                    'gsl'  # FIXME
+                    'haskell-stack'  # FIXME
+                    'hdf5'  # FIXME
                     'homebrew'
+                    'htop'  # FIXME
+                    'julia'  # FIXME
                     'koopa'
+                    'libevent'  # FIXME
+                    'libtool'  # FIXME
+                    'lua'  # FIXME
+                    'luarocks'  # FIXME
+                    'make'
+                    'miniconda'  # FIXME Rename to just conda...
+                    'ncurses'  # FIXME
+                    'neofetch'  # FIXME
+                    'neovim'  # FIXME
+                    'openjdk'  # FIXME
+                    'openssh'  # FIXME
+                    'openssl'  # FIXME
+                    'parallel'  # FIXME
+                    'password-store'  # FIXME
+                    'patch'  # FIXME
+                    'perl'  # FIXME
+                    'perl-packages'  # FIXME prompt about this
+                    'perlbrew'  # FIXME
+                    'pkg-config'  # FIXME
+                    'proj'  # FIXME
+                    'pyenv'  # FIXME
+                    'python'  # FIXME
+                    'python-packages'  # FIXME prompt about this
+                    'r'  # FIXME
+                    'r-cmd-check'  # FIXME
+                    'r-devel'  # FIXME
+                    'r-koopa'  # FIXME
+                    'r-packages'  # FIXME prompt about this
+                    'rbenv'  # FIXME
+                    'rbenv-ruby'  # FIXME
+                    'rmate'  # FIXME
+                    'rsync'  # FIXME
+                    'ruby'  # FIXME
+                    'ruby-packages'  # FIXME
+                    'rust'  # FIXME
+                    'rust-packages'  # FIXME prompt about this
+                    'sed'  # FIXME
+                    'shellcheck'  # FIXME
+                    'shunit2'  # FIXME
+                    'singularity'  # FIXME
+                    'spacemacs'  # FIXME
                     'spacevim'
+                    'sqlite'  # FIXME
+                    'subversion'  # FIXME
+                    'taglib'  # FIXME
+                    'tar'  # FIXME
+                    'tex-packages'  # FIXME
+                    'texinfo'  # FIXME
+                    'the-silver-searcher'  # FIXME
+                    'tmux'  # FIXME
+                    'udunits'  # FIXME
+                    'vim'  # FIXME
+                    'wget'  # FIXME
+                    'zsh'  # FIXME
                 )
+                if _koopa_is_linux
+                then
+                    args+=(
+                        'aspera-connect'  # FIXME
+                        'aws-cli'  # FIXME
+                        'bcbio'  # FIXME
+                        'bcl2fastq'  # FIXME
+                        'cellranger'  # FIXME
+                        'cloudbiolinux'  # FIXME
+                        'docker-credential-pass'  # FIXME
+                        'julia'  # FIXME
+                        'lmod'  # FIXME prompt about this?
+                        'rstudio-server'  # FIXME
+                        'shiny-server'  # FIXME
+                    )
+                fi
                 if _koopa_is_macos
                 then
                     args+=(
+                        'homebrew-little-snitch'  # FIXME
+                        'python-framework'  # FIXME
                         'r-cran-gfortran'
+                        'r-framework'  # FIXME
+                        'xcode-clt'  # FIXME
                     )
                 fi
                 ;;
