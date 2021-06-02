6a9ede90678e12bb11520b3c6cadb4c489d6738a
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-02 11:46:51 -0400

    Add support for r-cmd-check

--- lang/shell/bash/functions/common/install/r-cmd-check.sh
+++ lang/shell/bash/functions/common/install/r-cmd-check.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 
-# FIXME Rework the install target here...
 koopa::install_r_cmd_check() { # {{{1
+    koopa::install_app \
+        --name='r-cmd-check' \
+        "$@"
+}
+
+koopa:::install_r_cmd_check() { # {{{1
     # """
     # Install R CMD check (Rcheck) scripts for CI.
-    # @note Updated 2021-03-01.
+    # @note Updated 2021-06-02.
     # """
-    local link_name name source_repo target_dir
-    koopa::assert_has_no_args "$#"
-    name='r-cmd-check'
-    source_repo="https://github.com/acidgenomics/${name}.git"
-    target_dir="$(koopa::local_data_prefix)/${name}"
-    link_name='.Rcheck'
-    koopa::install_start "$name"
-    if [[ ! -d "$target_dir" ]]
-    then
-        koopa::alert "Downloading ${name} to '${target_dir}'."
-        (
-            koopa::mkdir "$target_dir"
-            git clone "$source_repo" "$target_dir"
-        )
-    fi
-    koopa::ln "$target_dir" "$link_name"
-    koopa::install_success "$name"
+    local prefix source_repo
+    prefix="${INSTALL_PREFIX:?}"
+    source_repo="https://github.com/acidgenomics/r-cmd-check.git"
+    koopa::mkdir "$prefix"
+    # FIXME Make this an internal function...
+    git clone --depth 1 "$source_repo" "$prefix"
     return 0
 }
+
+# FIXME Need to include an updater.
