fcc6547391cfdce6958f112b78de51f362245680
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-02 13:21:11 -0400

    Add support for macOS codenames (marketing names)

--- lang/shell/posix/functions/strings-variables.sh
+++ lang/shell/posix/functions/strings-variables.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+__koopa_id() { # {{{1
+    # """
+    # Return ID string.
+    # @note Updated 2021-05-25.
+    # """
+    local x
+    x="$(id "$@")"
+    [ -n "$x" ] || return 1
+    _koopa_print "$x"
+    return 0
+}
+
 _koopa_arch() { # {{{1
     # """
     # Platform architecture.
@@ -43,18 +55,6 @@ _koopa_arch2() { # {{{1
     return 0
 }
 
-__koopa_id() { # {{{1
-    # """
-    # Return ID string.
-    # @note Updated 2021-05-25.
-    # """
-    local x
-    x="$(id "$@")"
-    [ -n "$x" ] || return 1
-    _koopa_print "$x"
-    return 0
-}
-
 _koopa_cpu_count() { # {{{1
     # """
     # Return a usable number of CPU cores.
@@ -79,6 +79,19 @@ _koopa_cpu_count() { # {{{1
     # Subtract some cores for login use on powerful machines.
     [ "$n" -ge 17 ] && n=$((n - 2))
     _koopa_print "$n"
+    return 0
+}
+
+_koopa_debian_os_codename() { # {{{1
+    # """
+    # Debian operating system codename.
+    # @note Updated 2021-06-02.
+    # """
+    local x
+    _koopa_is_installed 'lsb_release' || return 0
+    x="$(lsb_release -cs)"
+    [ -n "$x" ] || return 1
+    _koopa_print "$x"
     return 0
 }
 
@@ -191,7 +204,7 @@ _koopa_host_id() { # {{{1
 
 _koopa_macos_color_mode() { # {{{1
     # """
-    # Return the color mode (dark/light) value.
+    # macOS color mode (dark/light) value.
     # @note Updated 2021-05-07.
     # """
     local x
@@ -202,6 +215,77 @@ _koopa_macos_color_mode() { # {{{1
         x='light'
     fi
     _koopa_print "$x"
+}
+
+_koopa_macos_os_codename() { # {{{1
+    # """
+    # macOS OS codename (marketing name).
+    # @note Updated 2021-06-02.
+    # @seealso
+    # - https://apple.stackexchange.com/questions/333452/
+    # - https://unix.stackexchange.com/questions/234104/
+    # """
+    local version x
+    version="$(_koopa_macos_version)"
+    case "$version" in
+        11.*)
+            x='Big Sur'
+            ;;
+        10.15.*)
+            x='Catalina'
+            ;;
+        10.14.*)
+            x='Mojave'
+            ;;
+        10.13.*)
+            x='High Sierra'
+            ;;
+        10.12.*)
+            x='Sierra'
+            ;;
+        10.11.*)
+            x='El Capitan'
+            ;;
+        10.10.*)
+            x='Yosmite'
+            ;;
+        10.9.*)
+            x='Mavericks'
+            ;;
+        10.8.*)
+            x='Mountain Lion'
+            ;;
+        10.7.*)
+            x='Lion'
+            ;;
+        10.6.*)
+            x='Snow Leopard'
+            ;;
+        10.5.*)
+            x='Leopard'
+            ;;
+        10.4.*)
+            x='Tiger'
+            ;;
+        10.3.*)
+            x='Panther'
+            ;;
+        10.2.*)
+            x='Jaguar'
+            ;;
+        10.1.*)
+            x='Puma'
+            ;;
+        10.0.*)
+            x='Cheetah'
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+    [ -n "$x" ] || return 1
+    _koopa_print "$x"
+    return 0
 }
 
 _koopa_mem_gb() { # {{{1
@@ -236,15 +320,18 @@ _koopa_mem_gb() { # {{{1
 
 _koopa_os_codename() { # {{{1
     # """
-    # Operating system code name.
-    # @note Updated 2020-08-06.
+    # Operating system codename.
+    # @note Updated 2021-06-02.
     # """
-    local x
-    _koopa_is_debian_like || return 0
-    _koopa_is_installed lsb_release || return 0
-    x="$(lsb_release -cs)"
-    [ -n "$x" ] || return 1
-    _koopa_print "$x"
+    if _koopa_is_debian_like
+    then
+        _koopa_debian_os_codename
+    elif _koopa_is_macos
+    then
+        _koopa_macos_os_codename
+    else
+        return 1
+    fi
     return 0
 }
 
