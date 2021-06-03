33a5fb013ab6996df3ac3bcbf7cfa3e062f369b9
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-03 13:25:55 -0400

    Update internal function names

--- lang/shell/bash/functions/common/system/print.sh
+++ lang/shell/bash/functions/common/system/print.sh
@@ -54,6 +54,43 @@ koopa:::alert_process_success() { # {{{1
     return 0
 }
 
+koopa:::status() { # {{{1
+    # """
+    # Koopa status.
+    # @note Updated 2021-06-03.
+    # """
+    local color nocolor label string x
+    koopa::assert_has_args_ge "$#" 3
+    label="$(printf '%10s\n' "${1:?}")"
+    color="$(koopa:::ansi_escape "${2:?}")"
+    nocolor="$(koopa:::ansi_escape 'nocolor')"
+    shift 2
+    for string in "$@"
+    do
+        x="${color}${label}${nocolor} | ${string}"
+        koopa::print "$x"
+    done
+    return 0
+}
+
+koopa::alert_coffee_time() { # {{{1
+    # """
+    # Alert that it's coffee time.
+    # @note Updated 2021-03-31.
+    # """
+    koopa::alert_note 'This step takes a while. Time for a coffee break! ☕'
+    return 0
+}
+
+koopa::alert_restart() { # {{{1
+    # """
+    # Alert the user that they should restart shell.
+    # @note Updated 2021-06-02.
+    # """
+    koopa::alert_note 'Restart the shell.'
+    return 0
+}
+
 koopa::configure_start() { # {{{1
     koopa:::alert_process_start 'Configuring' "$@"
 }
@@ -68,6 +105,74 @@ koopa::install_start() { # {{{1
 
 koopa::install_success() { # {{{1
     koopa:::alert_process_success 'Installation' "$@"
+}
+
+koopa::invalid_arg() { # {{{1
+    # """
+    # Error on invalid argument.
+    # @note Updated 2021-06-02.
+    # """
+    local arg x
+    if [[ "$#" -gt 0 ]]
+    then
+        arg="${1:-}"
+        if koopa::str_match_posix "$arg" '--'
+        then
+            koopa::warning "Use '--arg=VALUE' not '--arg VALUE'."
+        fi
+        x="Invalid argument: '${arg}'."
+    else
+        x='Invalid argument.'
+    fi
+    koopa::stop "$x"
+}
+
+koopa::missing_arg() { # {{{1
+    # """
+    # Error on a missing argument.
+    # @note Updated 2021-06-02.
+    # """
+    koopa::stop 'Missing required argument.'
+}
+
+koopa::status_fail() { # {{{1
+    # """
+    # 'FAIL' status.
+    # @note Updated 2021-06-03.
+    # """
+    koopa:::status 'FAIL' 'red' "$@" >&2
+    return 0
+}
+
+koopa::status_note() { # {{{1
+    # """
+    # 'NOTE' status.
+    # @note Updated 2021-06-03.
+    # """
+    koopa:::status 'NOTE' 'yellow' "$@"
+    return 0
+}
+
+koopa::status_ok() { # {{{1
+    # """
+    # 'OK' status.
+    # @note Updated 2021-06-03.
+    # """
+    koopa:::status 'OK' 'green' "$@"
+    return 0
+}
+
+koopa::stop() { # {{{1
+    # """
+    # Stop with an error message, and exit.
+    # @note Updated 2021-06-03.
+    #
+    # Defining here rather than in POSIX functions library, since we never want
+    # to use 'exit' inside of activation scripts. This can cause unwanted shell
+    # lockout.
+    # """
+    koopa:::msg 'red-bold' 'red' '!! Error:' "$@" >&2
+    exit 1
 }
 
 koopa::uninstall_start() { # {{{1
