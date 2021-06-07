3348f15bcedf90499355d97f126c051b71da0caa
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-07 09:06:19 -0400

    Update r-koopa dependency version

--- lang/r/include/header.R
+++ lang/r/include/header.R
@@ -33,12 +33,12 @@ local({
 
     #' Install R koopa package, if necessary
     #'
-    #' @note Updated 2021-05-11.
+    #' @note Updated 2021-06-07.
     #' @noRd
     installIfNecessary <- function() {
         ## Minimum version of koopa R package.
         ## Ensure that this also gets updated in `koopa system variables`.
-        minVersion <- "0.1.18"
+        minVersion <- "0.1.19"
         minVersion <- package_version(minVersion)
         stopifnot(requireNamespace("utils", quietly = TRUE))
         isInstalled <- function(pkgs) {
