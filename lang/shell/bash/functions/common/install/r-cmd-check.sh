2a18db1ccbd7a78edb8995e8c5b55786e29195d0
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-07 17:15:05 -0400

    Update installers and add uninstall placeholders

--- lang/shell/bash/functions/common/install/r-cmd-check.sh
+++ lang/shell/bash/functions/common/install/r-cmd-check.sh
@@ -8,8 +8,8 @@ koopa::install_r_cmd_check() { # {{{1
 
 koopa:::install_r_cmd_check() { # {{{1
     # """
-    # Install R CMD check (Rcheck) scripts for CI.
-    # @note Updated 2021-06-02.
+    # Install r-cmd-check scripts for CI.
+    # @note Updated 2021-06-07.
     # """
     local prefix source_repo
     prefix="${INSTALL_PREFIX:?}"
@@ -19,4 +19,23 @@ koopa:::install_r_cmd_check() { # {{{1
     return 0
 }
 
-# FIXME Need to include an updater.
+# FIXME Need to add uninstall support here.
+
+koopa::update_r_cmd_check() { # {{{1
+    # """
+    # Update r-cmd-check scripts.
+    # @note Updated 2021-06-07.
+    # """
+    local name name_fancy prefix
+    koopa::assert_has_no_args "$#"
+    name='r-cmd-check'
+    name_fancy="$name"
+    koopa::update_start "$name_fancy"
+    prefix="$(koopa::opt_prefix)/${name}"
+    (
+        koopa::cd "$prefix"
+        koopa::git_pull
+    )
+    koopa::update_success "$name_fancy"
+    return 0
+}
