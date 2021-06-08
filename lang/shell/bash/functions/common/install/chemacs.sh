c50944b73d0f8ae2cfb45e24d40f60a590cc4490
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-08 07:36:41 -0400

    Update installers

--- lang/shell/bash/functions/common/install/chemacs.sh
+++ lang/shell/bash/functions/common/install/chemacs.sh
@@ -21,6 +21,31 @@ koopa:::install_chemacs() { # {{{1
     return 0
 }
 
-# FIXME Add updater.
+koopa::uninstall_chemacs() { # {{{1
+    # """
+    # Uninstall Chemacs2.
+    # @note Updated 2021-06-07.
+    # """
+    koopa::uninstall_app \
+        --name-fancy='Chemacs' \
+        --name='chemacs' \
+        "$@"
+}
 
-# FIXME Add uninstaller.
+koopa::update_chemacs() { # {{{1
+    # """
+    # Update Chemacs2.
+    # @note Updated 2021-06-07.
+    # """
+    local name_fancy prefix
+    name_fancy='Chemacs'
+    prefix="$(koopa::opt_prefix)/chemacs"
+    koopa::assert_is_dir "$prefix"
+    koopa::update_start "$name_fancy" "$prefix"
+    (
+        koopa::cd "$prefix"
+        koopa::git_pull
+    )
+    koopa::update_success "$name_fancy" "$prefix"
+    return 0
+}
