0b4a26221ac33c27269ef7cf652325e82593aeac
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-11 16:39:46 -0400

    Improve uninstaller support

--- lang/shell/bash/functions/os/linux/distro/debian/install/google-cloud-sdk.sh
+++ lang/shell/bash/functions/os/linux/distro/debian/install/google-cloud-sdk.sh
@@ -3,7 +3,7 @@
 koopa::debian_install_google_cloud_sdk() { # {{{1
     # """
     # Install Google Cloud SDK.
-    # @note Updated 2021-06-04.
+    # @note Updated 2021-06-11.
     #
     # Required packages:
     # - apt-transport-https
@@ -13,6 +13,7 @@ koopa::debian_install_google_cloud_sdk() { # {{{1
     # @seealso
     # - https://cloud.google.com/sdk/docs/downloads-apt-get
     # """
+    local name_fancy
     koopa::assert_has_no_args "$#"
     name_fancy='Google Cloud SDK'
     if koopa::is_installed 'gcloud'
@@ -22,7 +23,7 @@ koopa::debian_install_google_cloud_sdk() { # {{{1
     fi
     koopa::install_start "$name_fancy"
     koopa::debian_apt_add_google_cloud_sdk_repo
-    koopa::debian_apt_install google-cloud-sdk
+    koopa::debian_apt_install 'google-cloud-sdk'
     koopa::install_success "$name_fancy"
     return 0
 }
@@ -32,5 +33,16 @@ koopa::debian_uninstall_google_cloud_sdk() { # {{{1
     # Uninstall Google Cloud SDK.
     # @note Updated 2021-06-11.
     # """
-    koopa::stop 'FIXME'
+    local name_fancy
+    koopa::assert_has_no_args "$#"
+    name_fancy='Google Cloud SDK'
+    koopa::uninstall_start "$name_fancy"
+    if ! koopa::is_installed 'gcloud'
+    then
+        koopa::alert_is_not_installed "$name_fancy"
+        return 0
+    fi
+    koopa::debian_apt_remove 'google-cloud-sdk'
+    koopa::uninstall_success "$name_fancy"
+    return 0
 }
