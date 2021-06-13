440e22074c032e737a5694903efdb83a9edac589
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-13 16:26:40 -0400

    Reorganize install-related files

--- lang/shell/bash/functions/common/install/perl.sh
+++ lang/shell/bash/functions/common/install/perl.sh
@@ -2,6 +2,25 @@
 
 # [2021-05-27] macOS success.
 
+koopa::configure_perl() { # {{{1
+    # """
+    # Configure Perl.
+    # @note Updated 2021-06-13.
+    # """
+    local prefix version
+    koopa::assert_is_installed 'perl'
+    version="$(koopa::get_version 'perl')"
+    prefix="$(koopa::perl_packages_prefix "$version")"
+    koopa::link_into_opt "$prefix" 'perl-packages'
+    PERL_MM_OPT="INSTALL_BASE=$prefix" cpan 'local::lib'
+    eval "$( \
+        perl \
+            "-I${prefix}/lib/perl5" \
+            "-Mlocal::lib=${prefix}" \
+    )"
+    return 0
+}
+
 koopa::install_perl() { # {{{1
     koopa::install_app \
         --name-fancy='Perl' \
