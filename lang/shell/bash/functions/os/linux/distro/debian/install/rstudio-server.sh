b12bd9b2fb6f6d8cf22351a7b176c392b84c91cf
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-14 16:56:58 -0400

    Improve workbench support

--- lang/shell/bash/functions/os/linux/distro/debian/install/rstudio-server.sh
+++ lang/shell/bash/functions/os/linux/distro/debian/install/rstudio-server.sh
@@ -60,7 +60,7 @@ koopa::debian_uninstall_rstudio_server() { # {{{1
 koopa::debian_uninstall_rstudio_workbench() { # {{{1
     # """
     # Uninstall RStudio Workbench.
-    # @note Updated 2021-06-11.
+    # @note Updated 2021-06-14.
     # """
     koopa::debian_uninstall_rstudio_server "$@"
 }
