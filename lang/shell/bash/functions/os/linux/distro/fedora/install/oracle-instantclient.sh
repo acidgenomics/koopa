506eac8007e0e5ab100a1938980afa2a99709e94
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-16 13:49:06 -0400

    Draft update to Oracle InstantClient

--- lang/shell/bash/functions/os/linux/distro/fedora/install/oracle-instantclient.sh
+++ lang/shell/bash/functions/os/linux/distro/fedora/install/oracle-instantclient.sh
@@ -6,7 +6,8 @@ koopa::fedora_install_oracle_instantclient() { # {{{1
     # Install Oracle InstantClient.
     # @note Updated 2021-06-16.
     # @seealso
-    # - https://www.oracle.com/database/technologies/instant-client/
+    # - https://www.oracle.com/database/technologies/
+    #     instant-client/downloads.html
     # """
     local arch minor_version name name_fancy stem stems tmp_dir
     local url_prefix version
@@ -17,8 +18,13 @@ koopa::fedora_install_oracle_instantclient() { # {{{1
     minor_version="$(koopa::major_minor_version "$version")"
     arch="$(koopa::arch)"
     koopa::install_start "$name_fancy"
+    koopa::fedora_dnf_install 'libaio-devel'
+    # FIXME Need to create a stripped version string here...
+    # '21.1.0.0.0' to '211000'.
     url_prefix="https://download.oracle.com/otn_software/linux/\
 instantclient/195000"
+    # FIXME Need to update version:
+    # https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-basic-linux.x64-21.1.0.0.0.zip
     stems=('basic' 'devel' 'sqlplus' 'jdbc' 'odbc')
     tmp_dir="$(koopa::tmp_dir)"
     (
@@ -36,7 +42,6 @@ ${minor_version}-${stem}-${version}.${arch}.rpm"
     return 0
 }
 
-# FIXME Need to double check this.
 koopa::fedora_uninstall_oracle_instantclient() { # {{{1
     # """
     # Uninstall Oracle InstantClient.
