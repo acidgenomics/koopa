a92a2d6a5ab1cf181c8f264207778cb9197ecf1e
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-17 09:26:25 -0400

    Work toward improving standardization of deb and rpm install calls

--- lang/shell/bash/functions/os/linux/common/install/shiny-server.sh
+++ lang/shell/bash/functions/os/linux/common/install/shiny-server.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-koopa::debian_install_shiny_server() { # {{{1
+koopa::linux_install_shiny_server() { # {{{1
     # """
-    # Install Shiny Server for Debian/Ubuntu.
-    # @note Updated 2021-05-22.
+    # Install Shiny Server for Linux.
+    # @note Updated 2021-06-16.
+    #
+    # Currently Debian/Ubuntu and Fedora/RHEL are supported.
+    # Currently only "amd64" (x86) architecture is supported here.
+    #
     # @seealso
-    # https://rstudio.com/products/shiny/download-server/ubuntu/
+    # - https://www.rstudio.com/products/shiny/download-server/ubuntu/
+    # - https://www.rstudio.com/products/shiny/download-server/redhat-centos/
     # """
-    local arch arch2 name name_fancy pos r reinstall version
-    # Currently only "amd64" (x86) is supported here.
+    local arch arch2 distro file file_ext install_fun name name_fancy pos
+    local r reinstall url version
     arch="$(koopa::arch)"
     r="$(koopa::locate_r)"
     case "$arch" in
@@ -19,7 +24,21 @@ koopa::debian_install_shiny_server() { # {{{1
             arch2="$arch"
             ;;
     esac
+    if koopa::is_debian_like
+    then
+        distro='ubuntu-14.04'
+        file_ext='deb'
+        install_fun='koopa::debian_install_from_deb'
+    elif koopa::is_fedora_like
+    then
+        distro='centos7'
+        file_ext='rpm'
+        install_fun='koopa::debian_install_from_rpm'
+    else
+        koopa::stop 'Unsupported Linux distro.'
+    fi
     reinstall=0
+    tee="$(koopa::locate_tee)"
     pos=()
     while (("$#"))
     do
@@ -60,11 +79,11 @@ koopa::debian_install_shiny_server() { # {{{1
     fi
     (
         koopa::cd "$tmp_dir"
-        file="shiny-server-${version}-${arch2}.deb"
-        url="https://download3.rstudio.org/ubuntu-14.04/${arch}/${file}"
+        file="${name}-${version}-${arch2}.${file_ext}"
+        url="https://download3.rstudio.org/${distro}/${arch}/${file}"
         koopa::download "$url"
-        sudo gdebi --non-interactive "$file"
-    ) 2>&1 | tee "$(koopa::tmp_log_file)"
+        "$install_fun" "$file"
+    ) 2>&1 | "$tee" "$(koopa::tmp_log_file)"
     koopa::rm "$tmp_dir"
     koopa::install_success "$name_fancy"
     return 0
