fda127454a5d2efd49bb3fcc242013bdfc8029b5
Michael Steinbaugh <mike@steinbaugh.com>
2021-07-12 09:42:45 -0400

    Add note regarding 2FA and DockerHub API

--- lang/shell/bash/functions/common/app/docker.sh
+++ lang/shell/bash/functions/common/app/docker.sh
@@ -360,7 +360,14 @@ koopa::docker_prune_old_images() { # {{{
 koopa::docker_prune_stale_tags() { # {{{1
     # """
     # Prune (delete) all stale tags on DockerHub for a specific image.
-    # @note Updated 2020-03-02.
+    # @note Updated 2020-07-12.
+    #
+    # NOTE This doesn't currently work when 2FA and PAT are enabled.
+    # This issue may be resolved by the end of 2021-07.
+    # See also:
+    # - https://github.com/docker/roadmap/issues/115
+    # - https://github.com/docker/hub-feedback/issues/1914
+    # - https://github.com/docker/hub-feedback/issues/1927
     # """
     koopa::assert_has_args "$#"
     koopa::r_script 'dockerPruneStaleTags' "$@"
