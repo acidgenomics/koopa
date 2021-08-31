0930c9e01a62895158b340a09b169346a82eefb5
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-31 10:49:42 -0400

    Simplify the pager call

--- lang/shell/bash/functions/common/system.sh
+++ lang/shell/bash/functions/common/system.sh
@@ -61,18 +61,18 @@ koopa::info_box() { # {{{1
 koopa::pager() { # {{{1
     # """
     # Run less with support for colors (escape characters).
-    # @note Updated 2021-06-07.
+    # @note Updated 2021-08-31.
     #
     # Detail on handling escape sequences:
     # https://major.io/2013/05/21/
     #     handling-terminal-color-escape-sequences-in-less/
     # """
-    local pager
+    local args
     koopa::assert_has_args "$#"
-    pager="${PAGER:-}"
-    [[ -z "$pager" ]] && pager='less'
-    koopa::assert_is_installed "$pager"
-    "$pager" -R "$@"
+    koopa::assert_is_installed 'less'
+    args=("$@")
+    koopa::assert_is_file "${args[-1]}"
+    less -R "${args[@]}"
     return 0
 }
 
