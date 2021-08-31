c6caf64f77b69bed1abaf5f5418f2aff1ec2ec8f
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-31 10:49:18 -0400

    Harden non-empty array checks

--- lang/shell/bash/functions/common/boolean.sh
+++ lang/shell/bash/functions/common/boolean.sh
@@ -214,15 +214,19 @@ koopa::is_array_empty() { # {{{1
 koopa::is_array_non_empty() { # {{{1
     # """
     # Is the array non-empty?
-    # @note Updated 2020-07-17.
+    # @note Updated 2021-08-31.
     #
     # Particularly useful for checking against readarray return, which currently
     # returns a length of 1 for empty input, due to newlines line break.
+    #
+    # @seealso
+    # - https://serverfault.com/questions/477503/
     # """
     local arr
     [[ "$#" -gt 0 ]] || return 1
     arr=("$@")
-    [[ -z "${arr[0]}" ]] && return 1
+    [[ "${#arr[@]}" -gt 0 ]] || return 1
+    [[ -n "${arr[0]}" ]] || return 1
     return 0
 }
 
