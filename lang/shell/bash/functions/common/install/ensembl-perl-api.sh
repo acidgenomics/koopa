15750b41b5062e671a80f5dcb978b425397f33d0
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-14 16:18:24 -0400

    Improve Ensembl Perl API support

--- lang/shell/bash/functions/common/install/ensembl-perl-api.sh
+++ lang/shell/bash/functions/common/install/ensembl-perl-api.sh
@@ -6,8 +6,7 @@ koopa::install_ensembl_perl_api() { # {{{1
     koopa::install_app \
         --name-fancy='Ensembl Perl API' \
         --name='ensembl-perl-api' \
-        --no-link \
-        --prefix="$(koopa::ensembl_perl_api_prefix)"
+        --no-link
 }
 
 koopa:::install_ensembl_perl_api() { # {{{1
