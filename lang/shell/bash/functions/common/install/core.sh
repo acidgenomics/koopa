33199c7160ae4815ec7fc9b52f554dd3c1a2e4eb
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-15 09:20:55 -0400

    Improve opt linkage handling of prefix in opt

--- lang/shell/bash/functions/common/install/core.sh
+++ lang/shell/bash/functions/common/install/core.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME This step needs to link into opt.
 koopa:::configure_app_packages() { # {{{1
     # """
     # Configure language application.
@@ -481,11 +480,10 @@ koopa::link_app() { # {{{1
     return 0
 }
 
-# FIXME This needs to skip when input is identical (e.g. Python packages).
 koopa::link_into_opt() { # {{{1
     # """
     # Link into koopa opt prefix.
-    # @note Updated 2021-06-11.
+    # @note Updated 2021-09-15.
     # """
     koopa::assert_has_args_eq "$#" 2
     local opt_prefix source_dir target_dir
@@ -493,6 +491,8 @@ koopa::link_into_opt() { # {{{1
     opt_prefix="$(koopa::opt_prefix)"
     [[ ! -d "$opt_prefix" ]] && koopa::mkdir "$opt_prefix"
     target_dir="${opt_prefix}/${2:?}"
+    # This happens during installation of app packages (e.g. Python).
+    [[ "$source_dir" == "$target_dir" ]] && return 0
     koopa::alert "Linking '${source_dir}' in '${target_dir}'."
     [[ ! -d "$source_dir" ]] && koopa::mkdir "$source_dir"
     [[ -d "$target_dir" ]] && koopa::sys_rm "$target_dir"
