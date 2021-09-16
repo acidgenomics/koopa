6575f75554d3cab0517e0168ed99fccbafb2c42f
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-16 14:41:19 -0400

    Add FIXMEs regarding app package configuration

--- lang/shell/bash/functions/common/install/node-packages.sh
+++ lang/shell/bash/functions/common/install/node-packages.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# FIXME This is currently failing due to 'configure_node' call.
 koopa::install_node_packages() { # {{{1
     koopa:::install_app \
         --name-fancy='Node packages' \
@@ -22,12 +23,13 @@ koopa:::install_node_packages() { # {{{1
     # """
     local npm npm_version pkg pkg_lower pkgs prefix version
     koopa::assert_has_no_args "$#"
+    # FIXME This won't configure into app, like we want...argh!!!
+    # FIXME Need to rethink the logic here.
+    prefix="${INSTALL_PREFIX:?}"
+    koopa::configure_node --prefix="$prefix"
     koopa::activate_node
     echo 'FIXME 111'
     # FIXME This step is erroring when called inside install...
-    koopa::configure_node
-    echo 'FIXME 222'
-    prefix="${INSTALL_PREFIX:?}"
     npm='npm'
     koopa::assert_is_installed "$npm"
     # Ensure npm is configured to desired version.
