081030ab153f5d899768c8d2c63d84fd39f5ccaf
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-16 15:27:08 -0400

    Add draft node installer

--- lang/shell/bash/functions/common/install/node.sh
+++ lang/shell/bash/functions/common/install/node.sh
@@ -17,3 +17,12 @@ koopa::configure_node() { # {{{1
 # FIXME Add a Debian binary installer from NodeSource.
 # https://github.com/nodesource/distributions/blob/master/README.md
 # https://nodesource.com/
+
+# Using Ubuntu
+# > curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+# > sudo apt-get install -y nodejs
+
+# Using Debian, as root
+# > curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+# > apt-get install -y nodejs
+
