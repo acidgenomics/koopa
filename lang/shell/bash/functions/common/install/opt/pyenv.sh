4a96765ea7991f2a0b22b4912cdaeef7555c54b5
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-17 16:48:10 -0400

    Improve pyenv handling

--- lang/shell/bash/functions/common/install/opt/pyenv.sh
+++ lang/shell/bash/functions/common/install/opt/pyenv.sh
@@ -4,18 +4,18 @@ koopa::install_pyenv() { # {{{1
     koopa:::install_app \
         --name='pyenv' \
         --no-link \
+        --version='rolling' \
         "$@"
 }
 
 koopa:::install_pyenv() { # {{{1
     # """
     # Install pyenv.
-    # @note Updated 2021-06-02.
+    # @note Updated 2021-09-17.
     # """
     local prefix url
     prefix="${INSTALL_PREFIX:?}"
     url='https://github.com/pyenv/pyenv.git'
-    koopa::mkdir "$prefix"
     koopa::git_clone "$url" "$prefix"
     return 0
 }
@@ -27,27 +27,21 @@ koopa::uninstall_pyenv() { # {{{1
         "$@"
 }
 
-# FIXME This doesn't handle permissions correctly.
-# FIXME Need to rethink using an internal function.
 koopa::update_pyenv() { # {{{1
+    koopa:::update_app --name='pyenv'
+}
+
+koopa:::update_pyenv() { # {{{1
     # """
     # Update pyenv.
-    # @note Updated 2021-05-07.
+    # @note Updated 2021-09-17.
     # """
-    local name_fancy
-    name_fancy='pyenv'
-    if ! koopa::is_installed 'pyenv'
-    then
-        koopa::alert_is_not_installed "$name_fancy"
-        return 0
-    fi
     koopa::assert_has_no_args "$#"
-    koopa::assert_has_no_envs
-    koopa::update_start "$name_fancy"
+    koopa::activate_pyenv
+    koopa::assert_is_installed 'pyenv'
     (
         koopa::cd "$(pyenv root)"
         koopa::git_pull
     )
-    koopa::update_success "$name_fancy"
     return 0
 }
