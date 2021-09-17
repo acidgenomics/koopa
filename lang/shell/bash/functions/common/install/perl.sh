f0d1db21de35df46591c160fd35311670ab272c4
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-17 10:54:50 -0400

    Draft update to Perl configuration

--- lang/shell/bash/functions/common/install/perl.sh
+++ lang/shell/bash/functions/common/install/perl.sh
@@ -2,31 +2,39 @@
 
 # [2021-09-17] macOS success.
 
-# FIXME Need to ensure that correct Perl is in PATH.
 koopa::configure_perl() { # {{{1
     # """
     # Configure Perl.
     # @note Updated 2021-09-17.
+    #
+    # Ignore these unit test errors:
+    # > Failed test 'fish: activate PATH'
+    # > Failed test 'fish: deactivate PATH'
+    #
+    # @seealso:
+    # - https://www.reddit.com/r/perl/comments/i0439v/
+    #   some_perl_modules_doesnt_work_after_update/fzn80k4/
     # """
     local perl prefix yes
     koopa::assert_has_no_args "$#"
     perl="$(koopa::locate_perl)"
+    yes="$(koopa::locate_yes)"
     koopa:::configure_app_packages \
         --name='perl' \
         --name-fancy='Perl' \
         --which-app="$perl"
     prefix="$(koopa::perl_packages_prefix)"
     koopa::assert_is_dir "$prefix"
-    # Ensure we start with a clean CPAN configuration.
-    koopa::rm "${HOME}/.cpan"
+    # Ensure we start with a clean CPAN and CPAN Minus configuration.
+    koopa::rm \
+        "${HOME}/.cpan" \
+        "${HOME}/.cpanm"
     koopa::alert "Setting up 'local::lib' at '${prefix}' using CPAN."
     koopa::add_to_path_start "$(koopa::dirname "$perl")"
-    # FIXME Need to add support for this.
-    yes="$(koopa::locate_yes)"
-    # FIXME This is prompting, which we don't want.
     "$yes" \
         | PERL_MM_OPT="INSTALL_BASE=$prefix" \
-            cpan 'local::lib'
+            cpan -f -i 'local::lib' \
+        || true
     eval "$( \
         "$perl" \
             "-I${prefix}/lib/perl5" \
