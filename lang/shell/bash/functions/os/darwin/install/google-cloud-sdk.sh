41306a043c51233f621126d1f249660ea7a7c2b9
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-20 09:04:40 -0400

    Reorganize installer functions

--- lang/shell/bash/functions/os/darwin/install/google-cloud-sdk.sh
+++ lang/shell/bash/functions/os/darwin/install/google-cloud-sdk.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
 koopa::macos_update_google_cloud_sdk() { # {{{1
-    koopa:::update_app \
-        --name='google-cloud-sdk' \
-        --name-fancy='Google Cloud SDK' \
-        --platform='macos' \
-        "$@"
-}
-
-koopa:::macos_update_google_cloud_sdk() { # {{{1
-    # """
-    # Update Google Cloud SDK.
-    # @note Updated 2021-09-20.
-    # """
-    local brew
+    local brew name_fancy
     koopa::assert_has_no_args "$#"
+    name_fancy='Google Cloud SDK'
+    koopa::update_start "$name_fancy"
     brew="$(koopa::locate_brew)"
     "$brew" reinstall --cask 'google-cloud-sdk'
+    koopa::update_success "$name_fancy"
     return 0
 }
