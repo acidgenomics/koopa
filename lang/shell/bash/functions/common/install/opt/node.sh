f561d238de4975798a8d5bd24e139afab68ec3ab
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-20 12:18:16 -0400

    Draft support for Node.js installer on Debian

--- lang/shell/bash/functions/common/install/opt/node.sh
+++ lang/shell/bash/functions/common/install/opt/node.sh
@@ -13,16 +13,3 @@ koopa::configure_node() { # {{{1
         --which-app="$(koopa::locate_node)" \
         "$@"
 }
-
-# FIXME Add a Debian binary installer from NodeSource.
-# https://github.com/nodesource/distributions/blob/master/README.md
-# https://nodesource.com/
-
-# Using Ubuntu
-# > curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-# > sudo apt-get install -y nodejs
-
-# Using Debian, as root
-# > curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-# > apt-get install -y nodejs
-
