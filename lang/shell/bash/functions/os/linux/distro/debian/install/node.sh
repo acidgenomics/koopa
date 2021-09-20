df5005919e7b0e885896e18bbaee6b4324ee2ae1
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-20 12:18:16 -0400

    Draft support for Node.js installer on Debian

---
+++ lang/shell/bash/functions/os/linux/distro/debian/install/node.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# FIXME Add a Debian binary installer from NodeSource.
+# https://github.com/nodesource/distributions/blob/master/README.md
+# https://nodesource.com/
+
+# Using Ubuntu
+# > curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+# > sudo apt-get install -y nodejs
+
+# Using Debian, as root
+# > curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+# > apt-get install -y nodejs
+
