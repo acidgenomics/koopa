70e977f555a8ca7e6ec63ada5d889a87203e3c23
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-21 13:51:14 -0400

    Resolve FIXME

--- lang/shell/bash/functions/common/koopa.sh
+++ lang/shell/bash/functions/common/koopa.sh
@@ -45,7 +45,6 @@ koopa:::koopa_configure() { # {{{1
         koopa::stop "Missing argument: 'koopa configure <ARG>...'."
     fi
     shift 1
-    echo 'FIXME AAA'
     koopa:::run_function "configure-${name}" "$@"
     return 0
 }
