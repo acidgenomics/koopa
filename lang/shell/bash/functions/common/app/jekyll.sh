89938dc923ee9573fb9b0a476e5cc0cfd069b39a
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-21 09:31:52 -0400

    Improve argparse

--- lang/shell/bash/functions/common/app/jekyll.sh
+++ lang/shell/bash/functions/common/app/jekyll.sh
@@ -3,24 +3,44 @@
 koopa::jekyll_deploy_to_aws() { # {{{1
     # """
     # Deploy Jekyll website to AWS S3 and CloudFront.
-    # @note Updated 20201-01-07.
+    # @note Updated 2021-09-21.
     # """
-    local bucket_prefix distribution_id local_prefix
+    local aws bucket_prefix bundle distribution_id local_prefix profile
     koopa::assert_has_args "$#"
-    koopa::assert_is_installed 'aws' 'bundle'
+    aws="$(koopa::locate_aws)"
+    bundle="$(koopa::locate_bundle)"
+    profile="${AWS_PROFILE:-default}"
     koopa::assert_is_file 'Gemfile'
     local_prefix='_site'
     while (("$#"))
     do
         case "$1" in
+            # Key-value pairs --------------------------------------------------
             '--bucket='*)
                 bucket_prefix="${1#*=}"
                 shift 1
+                ;;
+            '--bucket')
+                bucket_prefix="${2:?}"
+                shift 2
                 ;;
             '--distribution-id='*)
                 distribution_id="${1#*=}"
                 shift 1
                 ;;
+            '--distribution-id')
+                distribution_id="${2:?}"
+                shift 2
+                ;;
+            '--profile='*)
+                profile="${1#*=}"
+                shift 1
+                ;;
+            '--profile')
+                profile="${2:?}"
+                shift 2
+                ;;
+            # Other ------------------------------------------------------------
             *)
                 koopa::invalid_arg "$1"
                 ;;
@@ -31,14 +51,14 @@ koopa::jekyll_deploy_to_aws() { # {{{1
     local_prefix="$(koopa::strip_trailing_slash "$local_prefix")"
     if [[ -f 'Gemfile.lock' ]]
     then
-        bundle update --bundler
+        "$bundle" update --bundler
     fi
-    bundle install
-    bundle exec jekyll build
+    "$bundle" install
+    "$bundle" exec jekyll build
     koopa::aws_s3_sync "${local_prefix}/" "${bucket_prefix}/"
-    aws cloudfront create-invalidation \
+    "$aws" cloudfront create-invalidation \
         --distribution-id="$distribution_id" \
-        --profile='default' \
+        --profile="$profile" \
         --paths '/'
     return 0
 }
@@ -46,18 +66,23 @@ koopa::jekyll_deploy_to_aws() { # {{{1
 koopa::jekyll_serve() { # {{{1
     # """
     # Render Jekyll website.
-    # Updated 2021-01-07.
+    # Updated 2021-09-21.
     # """
-    local dir
+    local bundle dir
     koopa::assert_has_args_le "$#" 1
+    bundle="$(koopa::locate_bundle)"
     koopa::assert_is_file 'Gemfile'
-    koopa::assert_is_installed 'bundle'
     dir="${1:-.}"
+    dir="$(koopa::realpath "$dir")"
+    koopa::alert "Serving Jekyll website in '${dir}'."
     (
         koopa::cd "$dir"
-        [[ -f 'Gemfile.lock' ]] && bundle update --bundler
-        bundle install
-        bundle exec jekyll serve
+        if [[ -f 'Gemfile.lock' ]]
+        then
+            "$bundle" update --bundler
+        fi
+        "$bundle" install
+        "$bundle" exec jekyll serve
     )
     return 0
 }
