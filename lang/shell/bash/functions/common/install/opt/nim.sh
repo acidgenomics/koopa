897fbc065569fef0606e210cf8980071361cae34
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-29 11:46:20 -0400

    Add placeholder support for Nim packages via nimble

---
+++ lang/shell/bash/functions/common/install/opt/nim.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# FIXME Need to be able to install nim from source.
+# FIXME Need to work out how to configure nimble and nim packages.
+# FIXME Need to add support inside of koopa for this -- 'nim-packages'.
