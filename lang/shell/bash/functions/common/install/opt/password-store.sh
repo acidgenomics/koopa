15a036b214b90272bff34fda7e3cd5460ee0924d
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-30 08:12:21 -0400

    Ensure make is hardened

--- lang/shell/bash/functions/common/install/opt/password-store.sh
+++ lang/shell/bash/functions/common/install/opt/password-store.sh
@@ -11,21 +11,22 @@ koopa::install_password_store() { # {{{1
 koopa:::install_password_store() { # {{{1
     # """
     # Install Password Store.
-    # @note Updated 2021-05-04.
+    # @note Updated 2021-09-30.
     # @seealso
     # - https://www.passwordstore.org/
     # - https://git.zx2c4.com/password-store/
     # """
-    local file prefix url version
+    local file make prefix url version
     prefix="${INSTALL_PREFIX:?}"
     version="${INSTALL_VERSION:?}"
+    make="$(koopa::locate_make)"
     name='password-store'
     file="${name}-${version}.tar.xz"
     url="https://git.zx2c4.com/${name}/snapshot/${file}"
     koopa::download "$url"
     koopa::extract "$file"
     koopa::cd "${name}-${version}"
-    PREFIX="$prefix" make install
+    PREFIX="$prefix" "$make" install
     return 0
 }
 
