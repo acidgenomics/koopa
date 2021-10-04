d340028241470b5a3bd1157c541fe5b05cba7f7d
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-04 15:24:39 -0400

    Improve the comment

--- lang/shell/bash/functions/common/app/conda.sh
+++ lang/shell/bash/functions/common/app/conda.sh
@@ -382,7 +382,7 @@ koopa::conda_create_bioinfo_envs() { # {{{1
     return 0
 }
 
-# FIXME Inform the user when we're getting the latest version.
+# FIXME Inform the user when we're getting the latest version of a recipe.
 koopa::conda_create_env() { # {{{1
     # """
     # Create a conda environment.
