779146c1ababfd008a09a906256253602899343d
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-05 08:06:45 -0400

    Draft update

--- lang/shell/bash/functions/common/install/opt/nim.sh
+++ lang/shell/bash/functions/common/install/opt/nim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-koopa::configure_nim() {
+koopa::configure_nim() { # {{{1
     # """
     # Configure Nim.
     # @note Updated 2021-09-29.
@@ -18,3 +18,27 @@ koopa::configure_nim() {
 # FIXME Need to work out how to configure nimble and nim packages.
 # FIXME Need to add support inside of koopa for this -- 'nim-packages'.
 # FIXME Need to add uninstall support.
+
+koopa:::install_nim() { # {{{1
+    # """
+    # Install Nim.
+    # @note Updated 2021-10-05.
+    # """
+    local file prefix url version
+    prefix="${INSTALL_PREFIX:?}"
+    version="${INSTALL_VERSION:?}"
+    file='nim-1.4.8.tar.xz'
+    url="https://nim-lang.org/download/${file}"
+    koopa::download "$url"
+    koopa::extract "$file"
+    (
+        koopa::cd 'FIXME'
+        sh build.sh
+        bin/nim c koch
+        ./koch boot -d:release
+        ./koch tools
+        koopa::cp --target="$prefix" 'bin'
+    )
+    # FIXME Need to copy the 'bin/' to target.
+    return 0
+}
