6d98a399538cdc8d7af6804a9f68f4bf57f96c31
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-25 13:05:22 -0400

    Work on improving internal grep calls, supporting ripgrep

--- lang/shell/bash/functions/common/boolean-grep.sh
+++ lang/shell/bash/functions/common/boolean-grep.sh
@@ -1,85 +1,60 @@
 #!/usr/bin/env bash
 
-# FIXME We may need a koopa::file_match_perl function to replace pcregrep.
-# FIXME Can we make an internal 'koopa:::file_match' variant like our 'str' one?
-
-# FIXME Rename this?
-# FIXME Rework our stdin approach? Use read?
-# FIXME Require explicit arguments here, don't allow positional.
-# FIXME Rename this to include fixed at the end.
-koopa::file_match() { # {{{1
+koopa:::file_match() { # {{{1
     # """
     # Is a string defined in a file?
     # @note Updated 2021-10-25.
     #
     # @examples
     # koopa::file_match_fixed 'FILE' 'PATTERN'
-    #
-    # stdin support:
-    # echo 'FILE' | koopa::file_match_fixed - 'PATTERN'
-    # """
-    local file grep pattern
-    koopa::assert_has_args "$#"
-    if [[ "$#" -eq 2 ]]
-    then
-        # Standard input.
-        file="${1:?}"
-        pattern="${2:?}"
-    elif [[ "$#" -eq 1 ]]
-    then
-        # Piped input using stdin.
-        pattern="${1:?}"
-        shift 1
-        read -r file
-    else
-        return 1
-    fi
-    [[ -f "$file" ]] || return 1
-    # FIXME Rework using 'koopa::grep'.
-    grep="$(koopa::locate_grep)"
-    "$grep" -Fq "$pattern" "$file" >/dev/null
-}
-
-# FIXME Rename this?
-# FIXME Rework our stdin approach? Use read?
-# FIXME Require explicit arguments here, don't allow positional.
-koopa::file_match_regex() { # {{{1
-    # """
-    # Is a string defined in a file?
-    # @note Updated 2021-10-25.
-    #
-    # @examples
     # koopa::file_match_regex 'FILE' '^PATTERN.+$'
     #
     # stdin support:
+    # echo 'FILE' | koopa::file_match_fixed - 'PATTERN'
     # echo 'FILE' | koopa::file_match_regex - '^PATTERN.+$'
     # """
-    local file grep pattern
+    local file grep grep_args pattern pos
     koopa::assert_has_args "$#"
-    if [[ "$#" -eq 2 ]]
-    then
-        # Standard input.
-        file="${1:?}"
-        pattern="${2:?}"
-    elif [[ "$#" -eq 1 ]]
+    # FIXME Add support for ripgrep.
+    grep="$(koopa::locate_grep)"
+    grep_args=()
+    grep_args+=('--quiet')  # -q
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            '--extended-regexp')
+                grep_args+=('--extended-regexp')  # -E
+                shift 1
+                ;;
+            '--fixed-strings')
+                grep_args+=('--fixed-strings')  # -F
+                shift 1
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    if [[ "$#" -eq 1 ]]
     then
         # Piped input using stdin.
         pattern="${1:?}"
         shift 1
-        read -r file
+        read -r -d '' file
     else
-        return 1
+        # Positional variable input.
+        # Note that we're allowing empty string input here.
+        koopa::assert_has_args_eq "$#" 2
+        file="${1:-}"
+        # Alternate piped input using stdin, with string set to '-'.
+        [[ "$file" == '-' ]] && read -r -d '' file
+        pattern="${2:?}"
     fi
-    [[ -f "$file" ]] || return 1
-    # FIXME Rework using 'koopa::grep'.
-    grep="$(koopa::locate_grep)"
-    "$grep" -Eq "$pattern" "$file" >/dev/null
+    "$grep" "${grep_args[@]}" "$pattern" "$file" >/dev/null
 }
-
-
-# FIXME Harden this as bash variants instead.
-# FIXME Consider reworking this to support ripgrep.
-# FIXME Rename this as grep in our Bash library.
 
 koopa:::str_match() { # {{{1
     # """
@@ -99,75 +74,89 @@ koopa:::str_match() { # {{{1
     # Usage of '-q' flag can cause an exit trap in 'set -e' mode.
     # Redirecting output to '/dev/null' works more reliably.
     #
+    # @examples
+    # koopa::str_match_fixed 'STRING' 'PATTERN'
+    # koopa::str_match_regex 'STRING' '^PATTERN.+$'
+    #
+    # stdin support:
+    # echo 'STRING' | koopa::str_match_fixed - 'PATTERN'
+    # echo 'STRING' | koopa::str_match_regex - '^PATTERN.+$'
+    #
     # @seealso
     # - https://bugzilla.redhat.com/show_bug.cgi?id=1589997
     # - https://unix.stackexchange.com/questions/233987
     # """
-    local OPTIND grep flag pattern string
-    [ "$#" -gt 0 ] || return 1
-    grep='grep'
-    OPTIND=1
-    while getopts 'EFP' opt
+    local grep grep_args pattern pos string
+    koopa::assert_has_args "$#"
+    grep="$(koopa::locate_grep)"
+    grep_args=()
+    grep_args+=('--quiet')  # -q
+    pos=()
+    while (("$#"))
     do
-        case "$opt" in
-            'E')
-                flag='-E'
+        case "$1" in
+            '--extended-regexp')
+                grep_args+=('--extended-regexp')  # -E
+                shift 1
                 ;;
-            'F')
-                flag='-F'
+            '--fixed-strings')
+                grep_args+=('--fixed-strings')  # -F
+                shift 1
                 ;;
-            'P')
-                flag='-P'
-                ;;
-            \?)
-                koopa::invalid_arg "$opt"
+            *)
+                pos+=("$1")
+                shift 1
                 ;;
         esac
     done
-    shift "$((OPTIND-1))"
-    if [ "$#" -eq 2 ]
-    then
-        # Standard input.
-        # Note that we're allowing empty string input here.
-        string="${1:-}"
-        pattern="${2:?}"
-    elif [ "$#" -eq 1 ]
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    if [[ "$#" -eq 1 ]]
     then
         # Piped input using stdin.
         pattern="${1:?}"
         shift 1
-        # Handle string with line breaks '\n'.
-        # shellcheck disable=SC3045
         read -r -d '' string
     else
-        return 1
+        # Positional variable input.
+        # Note that we're allowing empty string input here.
+        koopa::assert_has_args_eq "$#" 2
+        string="${1:-}"
+        # Alternate piped input using stdin, with string set to '-'.
+        [[ "$string" == '-' ]] && read -r -d '' string
+        pattern="${2:?}"
     fi
     _koopa_print "$string" \
-        | "$grep" "$flag" -q "$pattern" >/dev/null
+        | "$grep" "${grep_args[@]}" "$pattern" >/dev/null
+}
+
+koopa::file_match_fixed() { # {{{1
+    # """
+    # Does the input file match a fixed string?
+    # @note Updated 2021-10-25.
+    # """
+    koopa:::file_match --fixed-strings "$@"
+}
+
+koopa::file_match_regex() { # {{{1
+    # """
+    # Does the input file match a regular expression?
+    # @note Updated 2021-10-25.
+    # """
+    koopa:::file_match --extended-regexp "$@"
 }
 
 koopa::str_match_fixed() { # {{{1
     # """
     # Does the input match a fixed string?
-    # @note Updated 2020-07-24.
+    # @note Updated 2021-10-25.
     # """
-    koopa:::str_match -F "$@"
-}
-
-# FIXME Should we take this out?
-# FIXME Consider reworking as pcregrep?
-koopa::str_match_perl() { # {{{
-    # """
-    # Does the input match a Perl-compatible regulare expression (PCRE)?
-    # @note Updated 2020-07-24.
-    # """
-    koopa:::str_match -P "$@"
+    koopa:::str_match --fixed-strings "$@"
 }
 
 koopa::str_match_regex() { # {{{1
     # """
     # Does the input match a regular expression?
-    # @note Updated 2020-07-24.
+    # @note Updated 2021-10-25.
     # """
-    koopa:::str_match -E "$@"
+    koopa:::str_match --extended-regexp "$@"
 }
