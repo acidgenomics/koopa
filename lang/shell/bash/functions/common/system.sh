b95c6c7f31bf14111465bed5e81a195c004f21ba
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-25 13:05:22 -0400

    Work on improving internal grep calls, supporting ripgrep

--- lang/shell/bash/functions/common/system.sh
+++ lang/shell/bash/functions/common/system.sh
@@ -3,11 +3,11 @@
 koopa::help() { # {{{1
     # """
     # Show usage via '--help' flag.
-    # @note Updated 2021-06-07.
+    # @note Updated 2021-10-25.
     # """
-    local arg args first_arg last_arg man_file prefix script_name
+    local app arg args first_arg last_arg man_file prefix script_name
     [[ "$#" -eq 0 ]] && return 0
-    [[ "${1:-}" == "" ]] && return 0
+    [[ "${1:-}" == '' ]] && return 0
     first_arg="${1:?}"
     last_arg="${!#}"
     args=("$first_arg" "$last_arg")
@@ -16,20 +16,23 @@ koopa::help() { # {{{1
         case "$arg" in
             '--help' | \
             '-h')
-                koopa::assert_is_installed 'man'
+                declare -A app=(
+                    [head]="$(koopa::locate_head)"
+                    [man]="$(koopa::locate_man)"
+                )
                 file="$(koopa::realpath "$0")"
                 script_name="$(koopa::basename "$file")"
                 prefix="$(koopa::parent_dir --num=2 "$file")"
                 man_file="${prefix}/man/man1/${script_name}.1"
                 if [[ -s "$man_file" ]]
                 then
-                    head -n 10 "$man_file" \
-                        | koopa::str_match_regex '^\.TH ' \
+                    "${app[head]}" -n 10 "$man_file" \
+                        | koopa::str_match_fixed - '.TH ' \
                         || koopa::stop "Invalid documentation at '${man_file}'."
                 else
                     koopa::stop "No documentation for '${script_name}'."
                 fi
-                man "$man_file"
+                "${app[man]}" "$man_file"
                 exit 0
                 ;;
         esac
--- lang/shell/bash/functions/os/darwin/ifactive.sh
+++ lang/shell/bash/functions/os/darwin/ifactive.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# FIXME Rework this using 'koopa::str_match_perl'.
 koopa::macos_ifactive() { # {{{1
     # """
     # Display active interfaces.
     # @note Updated 2021-10-25.
     # """
     local pcregrep x
-    koopa::assert_is_installed 'ifconfig' 'pcregrep'
     pcregrep="$(koopa::locate_pcregrep)"
     x="$( \
         ifconfig \
