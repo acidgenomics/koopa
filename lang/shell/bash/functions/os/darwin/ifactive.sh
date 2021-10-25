190ac236d3b2d691fa030c9a273d650c1089e5ff
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-25 09:41:00 -0400

    Harden pcregrep

--- lang/shell/bash/functions/os/darwin/ifactive.sh
+++ lang/shell/bash/functions/os/darwin/ifactive.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-# FIXME Need to locate pcregrep.
 koopa::macos_ifactive() { # {{{1
     # """
     # Display active interfaces.
     # @note Updated 2021-10-25.
     # """
+    local pcregrep x
     koopa::assert_is_installed 'ifconfig' 'pcregrep'
-    ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'
+    pcregrep="$(koopa::locate_pcregrep)"
+    x="$( \
+        ifconfig \
+            | "$pcregrep" -M -o '^[^\t:]+:([^\n]|\n\t)*status: active' \
+    )"
+    [[ -n "$x" ]] || return 1
+    koopa::print "$x"
     return 0
 }
