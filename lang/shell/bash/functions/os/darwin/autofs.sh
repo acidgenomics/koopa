e019bd591cb9bead625d9c342b469bf166538e0c
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-27 16:00:03 -0400

    Harden against PATH changes

--- lang/shell/bash/functions/os/darwin/autofs.sh
+++ lang/shell/bash/functions/os/darwin/autofs.sh
@@ -3,10 +3,15 @@
 koopa::macos_reload_autofs() { # {{{1
     # """
     # Reload autofs configuration defined in '/etc/auto_master'.
-    # @note Updated 2021-05-08.
+    # @note Updated 2021-10-27.
     # """
+    local app
     koopa::assert_has_no_args "$#"
     koopa::assert_is_admin
-    sudo automount -vc
+    declare -A app=(
+        [automount]="$(koopa::locate_automount)"
+        [sudo]="$(koopa::locate_sudo)"
+    )
+    "${app[sudo]}" "${app[automount]}" -vc
     return 0
 }
