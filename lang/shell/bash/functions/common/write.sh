a51a7b69208085c896b84389d0b4bd9465396c29
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-27 15:54:41 -0400

    Harden sudo commands

--- lang/shell/bash/functions/common/write.sh
+++ lang/shell/bash/functions/common/write.sh
@@ -3,17 +3,19 @@
 koopa::append_string() { # {{{1
     # """
     # Append a string at end of file.
-    # @note Updated 2021-06-17.
+    # @note Updated 2021-10-27.
     # """
-    local file string
+    local app file string
     koopa::assert_has_args_eq "$#" 2
+    declare -A app=(
+        [touch]="$(koopa::locate_touch)"
+    )
     string="${1:?}"
     file="${2:?}"
     if [[ ! -f "$file" ]]
     then
         koopa::mkdir "$(koopa::dirname "$file")"
-        # FIXME Use 'koopa::touch' here.
-        touch "$file"
+        "${app[touch]}" "$file"
     fi
     koopa::print "$string" >> "$file"
     return 0
@@ -22,45 +24,55 @@ koopa::append_string() { # {{{1
 koopa::sudo_append_string() { # {{{1
     # """
     # Append a string at end of file as root user.
-    # @note Updated 2021-06-17.
+    # @note Updated 2021-10-27.
     #
     # Alternative approach:
     # > sudo sh -c "printf '%s\n' '$string' >> '${file}'"
     # """
-    local file parent_dir string tee
+    local app file parent_dir string
     koopa::assert_has_args_eq "$#" 2
     koopa::assert_is_admin
-    tee="$(koopa::locate_tee)"
+    declare -A app=(
+        [sudo]="$(koopa::locate_sudo)"
+        [tee]="$(koopa::locate_tee)"
+        [touch]="$(koopa::locate_touch)"
+    )
     string="${1:?}"
     file="${2:?}"
     if [[ ! -f "$file" ]]
     then
         koopa::mkdir --sudo "$(koopa::dirname "$file")"
-        # FIXME Use 'koopa::touch' here.
-        # Needs to support sudo flag.
-        sudo touch "$file"
+        "${app[sudo]}" "${app[touch]}" "$file"
     fi
-    koopa::print "$string" | sudo "$tee" -a "$file" >/dev/null
+    koopa::print "$string" \
+        | "${app[sudo]}" "${app[tee]}" -a "$file" >/dev/null
     return 0
 }
 
 koopa::sudo_write_string() { # {{{1
     # """
     # Write a string to disk using root user.
-    # @note Updated 2021-05-27.
+    # @note Updated 2021-10-27.
     #
     # Alternative approach:
     # > sudo sh -c "printf '%s\n' '$string' > '${file}'"
     # """
-    local file parent_dir string tee
+    local app file parent_dir string
     koopa::assert_has_args_eq "$#" 2
     koopa::assert_is_admin
-    tee="$(koopa::locate_tee)"
+    declare -A app=(
+        [sudo]="$(koopa::locate_sudo)"
+        [tee]="$(koopa::locate_tee)"
+    )
     string="${1:?}"
     file="${2:?}"
     parent_dir="$(koopa::dirname "$file")"
-    [[ ! -d "$parent_dir" ]] && koopa::mkdir --sudo "$parent_dir"
-    koopa::print "$string" | sudo "$tee" "$file" >/dev/null
+    if [[ ! -d "$parent_dir" ]]
+    then
+        koopa::mkdir --sudo "$parent_dir"
+    fi
+    koopa::print "$string" \
+        | "${app[sudo]}" "${app[tee]}" "$file" >/dev/null
     return 0
 }
 
