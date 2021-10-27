025da1ae1200af7139480516b2d5c34d95424b61
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-27 12:52:34 -0400

    Harden FASTQ utilites

--- lang/shell/bash/functions/common/ngs/fastq.sh
+++ lang/shell/bash/functions/common/ngs/fastq.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# NOTE Need to migrate these functions to r-koopa.
-
 koopa::convert_fastq_to_fasta() { # {{{1
     # """
     # Convert FASTQ files into FASTA format.
-    # @note Updated 2021-09-21.
+    # @note Updated 2021-10-27.
     #
     # Alternate approaches:
     #
@@ -16,14 +14,16 @@ koopa::convert_fastq_to_fasta() { # {{{1
     # >     | awk -v FS="\t" '{print $1"\n"$2}' \
     # >     > "$fasta_file"
     # """
-    local array cut fasta_file fastq_file paste sed source_dir target_dir tr
+    local app fasta_file fastq_file fastq_files source_dir target_dir
     koopa::assert_has_args "$#"
-    cut="$(koopa::locate_cut)"
-    paste="$(koopa::locate_paste)"
-    sed="$(koopa::locate_sed)"
-    tr="$(koopa::locate_tr)"
-    source_dir='.'
-    target_dir='.'
+    declare -A app=(
+        [cut]="$(koopa::locate_cut)"
+        [paste]="$(koopa::locate_paste)"
+        [sed]="$(koopa::locate_sed)"
+        [tr]="$(koopa::locate_tr)"
+    )
+    source_dir="${PWD:?}"
+    target_dir="${PWD:?}"
     while (("$#"))
     do
         case "$1" in
@@ -51,28 +51,28 @@ koopa::convert_fastq_to_fasta() { # {{{1
         esac
     done
     koopa::assert_is_dir "$source_dir"
-    source_dir="$(koopa::strip_trailing_slash "$source_dir")"
-    target_dir="$(koopa::strip_trailing_slash "$target_dir")"
-    # Pipe GNU find into array.
-    # FIXME Rework using 'koopa::find'.
-    readarray -t array <<< "$( \
+    source_dir="$(koopa::realpath "$source_dir")"
+    readarray -t fastq_files <<< "$( \
         koopa::find \
             --glob='*.fastq' \
             --max-depth=1 \
             --min-depth=1 \
             --prefix="$source_dir" \
+            --sort \
             --type='f' \
-        | sort \
     )"
-    [[ "${#array[@]}" -eq 0 ]] && koopa::stop 'No FASTQ files detected.'
-    koopa::mkdir "$target_dir"
-    for fastq_file in "${array[@]}"
+    if [[ "${#fastq_files[@]}" -eq 0 ]]
+    then
+        koopa::stop "No FASTQ files detected in '${source_dir}'."
+    fi
+    target_dir="$(koopa::init_dir "$target_dir")"
+    for fastq_file in "${fastq_files[@]}"
     do
         fasta_file="${fastq_file%.fastq}.fasta"
-        "$paste" - - - - < "$fastq_file" \
-            | "$cut" -f 1,2 \
-            | "$sed" 's/^@/>/' \
-            | "$tr" '\t' '\n' > "$fasta_file"
+        "${app[paste]}" - - - - < "$fastq_file" \
+            | "${app[cut]}" -f '1,2' \
+            | "${app[sed]}" 's/^@/>/' \
+            | "${app[tr]}" '\t' '\n' > "$fasta_file"
     done
     return 0
 }
@@ -80,16 +80,15 @@ koopa::convert_fastq_to_fasta() { # {{{1
 koopa::fastq_lanepool() { # {{{1
     # """
     # Pool lane-split FASTQ files.
-    # @note Updated 2021-09-21.
+    # @note Updated 2021-10-27.
     # """
-    local array basenames cat find head i out prefix source_dir sort
-    local tail target_dir
-    cat="$(koopa::locate_cat)"
-    find="$(koopa::locate_find)"
-    sort="$(koopa::locate_sort)"
+    local app basenames fastq_files i out prefix source_dir target_dir
+    declare -A app=(
+        [cat]="$(koopa::locate_cat)"
+    )
     prefix='lanepool'
-    source_dir='.'
-    target_dir='.'
+    source_dir="${PWD:?}"
+    target_dir="${PWD:?}"
     while (("$#"))
     do
         case "$1" in
@@ -124,27 +123,25 @@ koopa::fastq_lanepool() { # {{{1
                 ;;
         esac
     done
-    source_dir="$(koopa::strip_trailing_slash "$source_dir")"
-    target_dir="$(koopa::strip_trailing_slash "$target_dir")"
-    # Pipe GNU find into array.
-    # FIXME Rework using 'koopa::find'.
-    readarray -t array <<< "$( \
-        "$find" "$source_dir" \
-            -maxdepth 1 \
-            -mindepth 1 \
-            -type 'f' \
-            -iname '*_L001_*.fastq*' \
-            -print \
-        | "$sort" \
+    koopa::assert_is_dir "$source_dir"
+    source_dir="$(koopa::realpath "$source_dir")"
+    readarray -t fastq_files <<< "$( \
+        koopa::find \
+            --glob='*_L001_*.fastq*' \
+            --max-depth=1 \
+            --min-depth=1 \
+            --prefix="$source_dir" \
+            --sort \
+            --type='f' \
     )"
     # Error if file array is empty.
-    if [[ "${#array[@]}" -eq 0 ]]
+    if [[ "${#fastq_files[@]}" -eq 0 ]]
     then
-        koopa::stop 'No lane-split FASTQ files detected.'
+        koopa::stop "No lane-split FASTQ files detected in '${source_dir}'."
     fi
-    koopa::mkdir "$target_dir"
+    target_dir="$(koopa::init_dir "$target_dir")"
     basenames=()
-    for i in "${array[@]}"
+    for i in "${fastq_files[@]}"
     do
         basenames+=("$(koopa::basename "$i")")
     done
@@ -170,7 +167,9 @@ koopa::fastq_lanepool() { # {{{1
     # Loop across the array indices, similar to 'mapply()' approach in R.
     for i in "${!out[@]}"
     do
-        "$cat" "${source_dir}/${head[i]}_L00"[1-9]"_${tail[i]}" > "${out[i]}"
+        "${app[cat]}" \
+            "${source_dir}/${head[i]}_L00"[1-9]"_${tail[i]}" \
+            > "${out[i]}"
     done
     return 0
 }
@@ -178,18 +177,21 @@ koopa::fastq_lanepool() { # {{{1
 koopa::fastq_reads_per_file() { # {{{1
     # """
     # Determine the number of reads per FASTQ file.
-    # @note Updated 2021-09-21.
+    # @note Updated 2021-10-27.
     # """
-    local awk dir wc zcat
-    awk="$(koopa::locate_awk)"
-    wc="$(koopa::locate_wc)"
-    zcat="$(koopa::locate_zcat)"
-    dir="${1:-.}"
+    local app dir
+    declare -A app=(
+        [awk]="$(koopa::locate_awk)"
+        [wc]="$(koopa::locate_wc)"
+        [zcat]="$(koopa::locate_zcat)"
+    )
+    dir="${1:-}"
+    [[ -z "$dir" ]] && dir="${PWD:?}"
     dir="$(koopa::realpath "$dir")"
     # Divide by 4.
     # shellcheck disable=SC2016
-    "$zcat" "${dir}/"*'_R1.fastq.gz' \
-        | "$wc" -l \
-        | "$awk" '{print $1/4}'
+    "${app[zcat]}" "${dir}/"*'_R1.fastq.gz' \
+        | "${app[wc]}" -l \
+        | "${app[awk]}" '{print $1/4}'
     return 0
 }
