33b4f71b8c6b27867d346a891b8e8bf5f3d37cb9
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-30 09:30:54 -0400

    Harden macOS-specific installers

--- lang/shell/bash/functions/os/darwin/install/system/oracle-java.sh
+++ lang/shell/bash/functions/os/darwin/install/system/oracle-java.sh
@@ -1,28 +1,34 @@
 #!/usr/bin/env bash
 
-koopa::macos_uninstall_oracle_java() { # {{{1
+koopa::macos_uninstall_oracle_java() { # {{{
+    koopa:::uninstall_app \
+        --name-fancy='Oracle Java' \
+        --name='oracle-java' \
+        --platform='macos' \
+        --system \
+        "$@"
+}
+
+koopa:::macos_uninstall_oracle_java() { # {{{1
     # """
     # Uninstall Oracle Java.
-    # @note Updated 2020-07-17.
+    # @note Updated 2021-10-30.
     # @seealso
     # - https://www.java.com/en/download/help/mac_uninstall_java.xml
     # """
-    local name_fancy
     koopa::assert_has_no_args "$#"
-    name_fancy='Oracle Java'
-    koopa::uninstall_start "$name_fancy"
-    koopa::rm \
-        "${HOME}/Library/Caches/com.oracle.java.Java-Updater" \
-        "${HOME}/Library/Application Support/Oracle/Java" \
-        "${HOME}/Library/Preferences/com.apple.java.util.prefs.plist" \
-        "${HOME}/Library/Preferences/com.oracle.java.JavaAppletPlugin.plist" \
-        "${HOME}/Library/Safari/LocalStorage/https_www.java.com_0.localstorage"*
+    koopa::assert_is_admin
     koopa::rm --sudo \
         '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin' \
         '/Library/LaunchAgents/com.oracle.java.Java-Updater.plist' \
         '/Library/LaunchDaemons/com.oracle.java.Helper-Tool.plist' \
         '/Library/PreferencePanes/JavaControlPanel.prefPane' \
         '/Library/Preferences/com.oracle.java.Helper-Tool.plist'
-    koopa::uninstall_success "$name_fancy"
+    koopa::rm \
+        "${HOME}/Library/Caches/com.oracle.java.Java-Updater" \
+        "${HOME}/Library/Application Support/Oracle/Java" \
+        "${HOME}/Library/Preferences/com.apple.java.util.prefs.plist" \
+        "${HOME}/Library/Preferences/com.oracle.java.JavaAppletPlugin.plist" \
+        "${HOME}/Library/Safari/LocalStorage/https_www.java.com_0.localstorage"*
     return 0
 }
