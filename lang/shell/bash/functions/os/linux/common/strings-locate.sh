b3a88465ed5d4b9c47c7458eb28fc488a948c74b
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-31 10:35:47 -0400

    Harden ldconfig

--- lang/shell/bash/functions/os/linux/common/strings-locate.sh
+++ lang/shell/bash/functions/os/linux/common/strings-locate.sh
@@ -2,6 +2,33 @@
 
 # FIXME Need to prefix all of these with 'linux'.
 
+# FIXME Need to add these, some of which are distro-specific.
+# - apk  # alpine
+# - apt  # debian
+# - apt-get  # debian
+# - dpkg  # debian
+# - dpkg-reconfigure  # debian
+# - gdebi  # debian
+# - gpasswd
+# - groupadd
+# - ldconfig
+# - locale-gen
+# - rpm  # fedora
+# - service  # fedora? is this on debian too?
+# - unattended-upgrades
+# - update-locale
+# - usermod
+# - zypper  # opensuse
+
+koopa::locate_ldconfig() { # {{{1
+    # """
+    # Locate Linux ldconfig.
+    # @note Updated 2021-10-31.
+    # """
+    koopa::assert_has_no_args "$#"
+    koopa:::locate_app '/sbin/ldconfig'
+}
+
 koopa::locate_systemctl() { # {{{1
     # """
     # Locate Linux systemctl.
