fec7d4f779e4f2fbb395bd06fd4cee6a962897b9
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-01 16:15:31 -0400

    Harden temporary file remover on Linux

--- lang/shell/bash/functions/os/linux/common/system/clean-tmp.sh
+++ lang/shell/bash/functions/os/linux/common/system/clean-tmp.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-# FIXME Need to add support for '-ctime XXX' into our koopa::find function.
-# FIXME Can we also extend this to macOS? May be generally useful.
-# FIXME Need to add '--sudo' flag support to koopa::find.
-
 koopa::linux_clean_tmp() { # {{{1
     # """
     # Clean temporary directory.
     # @note Updated 2021-11-01.
     # """
-    local dir dirs find matches
+    local dir dirs matches
     koopa::assert_has_no_args "$#"
     koopa::assert_is_admin
-    find="$(koopa::locate_find)"
     dirs=('/tmp')
     if [[ "${TMPDIR:-}" != '/tmp' ]]
     then
@@ -20,13 +15,13 @@ koopa::linux_clean_tmp() { # {{{1
     fi
     for dir in "${dirs[@]}"
     do
-        # FIXME Rework using 'koopa::find'.
         readarray -t matches <<< "$( \
-        sudo "$find" "$dir" \
-            -mindepth 1 \
-            -maxdepth 1 \
-            -ctime +30 \
-            -print \
+        koopa::find \
+            --max-depth=1 \
+            --min-days-old=30 \
+            --min-depth=1 \
+            --prefix="$dir" \
+            --sudo \
         )"
         koopa::is_array_non_empty "${matches[@]:-}" || continue
         koopa::rm --sudo "${matches[@]}"
