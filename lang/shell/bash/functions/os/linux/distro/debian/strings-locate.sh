f355e6f9a5f47c53d532018c9a39d7116f46a6f7
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-02 08:17:08 -0400

    Improve support for platform-specific locator functions

---
+++ lang/shell/bash/functions/os/linux/distro/debian/strings-locate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# - apt
+# - apt-get  # debian
+# - dpkg  # debian
+# - dpkg-reconfigure  # debian
+# - gdebi  # debian
+
+koopa::debian_locate_dpkg() { # {{{1
+    # """
+    # Locate Debian 'dpkg'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/bin/dpkg'
+}
+
+koopa::debian_locate_unattended_upgrades() { # {{{1
+    # """
+    # Locate Debian 'unattended-upgrades'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/bin/unattended-upgrades'
+}
