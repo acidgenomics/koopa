858ba67757efdb586538c2e2f40dbf4af29cd1f9
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-02 08:17:08 -0400

    Improve support for platform-specific locator functions

--- lang/shell/bash/functions/os/linux/common/strings-locate.sh
+++ lang/shell/bash/functions/os/linux/common/strings-locate.sh
@@ -3,51 +3,87 @@
 # FIXME Need to prefix all of these with 'linux'.
 
 # FIXME Need to add these, some of which are distro-specific.
-# - apk  # alpine
-# - apt  # debian
-# - apt-get  # debian
-# - dpkg  # debian
-# - dpkg-reconfigure  # debian
-# - gdebi  # debian
-# - groupadd
-# - ldconfig
-# - localedef  # fedora?
-# - locale-gen
 # - rpm  # fedora
-# - service  # fedora? is this on debian too?
-# - unattended-upgrades
-# - update-locale
-# - usermod
 # - zypper  # opensuse
+
+koopa::locate_groupadd() { # {{{1
+    # """
+    # Locate Linux 'groupadd'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/sbin/groupadd'
+}
 
 koopa::locate_gpasswd() { # {{{1
     # """
-    # Locate Linux gpasswd.
-    # @note Updated 2021-11-01.
+    # Locate Linux 'gpasswd'.
+    # @note Updated 2021-11-02.
     # """
-    koopa:::locate_app 'gpasswd'  # FIXME
+    koopa:::locate_app '/usr/bin/gpasswd'
 }
 
 koopa::locate_ldconfig() { # {{{1
     # """
-    # Locate Linux ldconfig.
-    # @note Updated 2021-10-31.
+    # Locate Linux 'ldconfig'.
+    # @note Updated 2021-11-02.
     # """
     koopa:::locate_app '/sbin/ldconfig'
 }
 
+# FIXME Is this on Fedora?
+koopa::locate_localedef() { # {{{1
+    # """
+    # Locate Linux 'localedef'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/bin/localedef'
+}
+
+koopa::locate_locale_gen() { # {{{1
+    # """
+    # Locate Linux 'locale-gen'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/sbin/locale-gen'
+}
+
+koopa::locate_service() { # {{{1
+    # """
+    # Locate Linux 'service'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/sbin/service'
+}
+
 koopa::locate_systemctl() { # {{{1
     # """
-    # Locate Linux systemctl.
-    # @note Updated 2021-10-31.
+    # Locate Linux 'systemctl'.
+    # @note Updated 2021-11-02.
     # """
-    koopa:::locate_app 'systemctl'  # FIXME Harden this
+    koopa:::locate_app '/bin/systemctl'
 }
 
 koopa::locate_update_alternatives() { # {{{1
     # """
-    # Locate Linux update-alternatives.
-    # @note Updated 2021-10-31.
+    # Locate Linux 'update-alternatives'.
+    # @note Updated 2021-11-02.
     # """
-    koopa:::locate_app 'update-alternatives'  # FIXME Harden this
+    koopa:::locate_app '/usr/bin/update-alternatives'
+}
+
+# FIXME Is this on Fedora?
+koopa::locate_update_locale() { # {{{1
+    # """
+    # Locate Linux 'update-locale'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/sbin/update-locale'
+}
+
+koopa::locate_usermod() { # {{{1
+    # """
+    # Locate Linux 'usermod'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app '/usr/sbin/usermod'
 }
---
+++ lang/shell/bash/functions/os/linux/distro/fedora/strings-locate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+koopa::fedora_locate_rpm() { # {{{1
+    # """
+    # Locate Fedora 'rpm'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app 'rpm'  # FIXME
+}
