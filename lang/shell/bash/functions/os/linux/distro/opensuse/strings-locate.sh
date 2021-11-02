959a6deb8a657bf01a52965231a9e163681d7a5d
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-02 08:17:08 -0400

    Improve support for platform-specific locator functions

---
+++ lang/shell/bash/functions/os/linux/distro/opensuse/strings-locate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+koopa::opensuse_locate_zypper() { # {{{1
+    # """
+    # Locate OpenSUSE 'zypper'.
+    # @note Updated 2021-11-02.
+    # """
+    koopa:::locate_app 'zypper'  # FIXME
+}
