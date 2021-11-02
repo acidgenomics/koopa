58b5eccbc3b28b10e743bdd4e1b7523e0b0135e3
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-02 06:45:34 -0400

    Improve support for Python install on macOS 12 Monterey

--- lang/shell/bash/functions/os/darwin/install/system/python.sh
+++ lang/shell/bash/functions/os/darwin/install/system/python.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
-# FIXME Rework this using 'app' and 'dict' array approach.
-# FIXME Finish reworking dict approach here.
-# FIXME Allow the user to pass in version here.
-# FIXME Should we wrap this in 'koopa::install...' call with '--no-custom-prefix' flag set?
-# FIXME Or consider setting a '--system' flag that doesn't attempt to change permissions...
-# FIXME Rework our tmpdir handling here..
-# FIXME Need to rework these using wrappers.
-
 koopa::macos_install_python_framework() { # {{{1
+    koopa:::install_app \
+        --name-fancy='Python framework' \
+        --name='python-framework' \
+        --platform='macos' \
+        --prefix='/Library/Frameworks/Python.framework' \
+        --system \
+        "$@"
+}
+
+koopa::macos_uninstall_python_framework() { # {{{1
+    koopa:::uninstall_app \
+        --name-fancy='Python framework' \
+        --name='python-framework' \
+        --platform='macos' \
+        --system \
+        "$@"
+}
+
+koopa:::macos_install_python_framework() { # {{{1
     # """
     # Install Python framework.
-    # @note Updated 2021-10-29.
+    # @note Updated 2021-11-02.
     # """
-    local app dict file macos_string macos_version major_version name prefix
-    local name_fancy url version
+    local app dict
+    koopa::assert_has_no_args "$#"
     declare -A app=(
         [installer]="$(koopa::locate_installer)"
         [sudo]="$(koopa::locate_sudo)"
@@ -24,34 +35,24 @@ koopa::macos_install_python_framework() { # {{{1
         [macos_version]="$(koopa::macos_version)"
         [name]='python'
         [name_fancy]='Python'
-        [prefix]='/Library/Frameworks/Python.framework'
+        [prefix]="${INSTALL_PREFIX:?}"
         [reinstall]=0
         [tmp_dir]="$(koopa::tmp_dir)"
         [tmp_log_file]="$(koopa::tmp_log_file)"
-        [version]="$(koopa::variable "${dict[name]}")"
+        # FIXME I think this doesn't work because you have to move out of array.
     )
-    while (("$#"))
-    do
-        case "$1" in
-            '--reinstall')
-                dict[reinstall]=1
-                shift 1
-                ;;
-            *)
-                koopa::invalid_arg "$1"
-                ;;
-        esac
-    done
+    dict[version]="$(koopa::variable "${dict[name]}")"
     dict[major_version]="$(koopa::major_version "${dict[version]}")"
     case "${dict[macos_version]}" in
-        '11'*)
+        '11'* | \
+        '12'*)
             dict[macos_string]='macos11'
             ;;
         '10'*)
             dict[macos_string]='macosx10.9'
             ;;
         *)
-            koopa::stop "Unsupported macOS version: '${dict[version]}'."
+            koopa::stop "Unsupported macOS version: '${dict[macos_version]}'."
             ;;
     esac
     if ! koopa::is_current_version "${dict[name]}" || \
@@ -82,23 +83,15 @@ ${dict[version]}/${dict[file]}"
     return 0
 }
 
-# FIXME Rework this as a wrapped install call.
-# FIXME Use '--system' flag to handle this?
-koopa::macos_uninstall_python_framework() { # {{{1
+koopa:::macos_uninstall_python_framework() { # {{{1
     # """
     # Uninstall Python framework.
-    # @note Updated 2021-05-21.
+    # @note Updated 2021-11-02.
     # """
-    local dict
     koopa::assert_has_no_args "$#"
-    declare -A dict=(
-        [name_fancy]='Python framework'
-    )
-    koopa::uninstall_start "${dict[name_fancy]}"
     koopa::rm --sudo \
         '/Applications/Python'* \
         '/Library/Frameworks/Python.framework'
     koopa::delete_broken_symlinks '/usr/local/bin'
-    koopa::uninstall_success "${dict[name_fancy]}"
     return 0
 }
