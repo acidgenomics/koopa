6a0aaccbda88141cd5ce90c2292f06d8421164b4
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-03 10:48:34 -0400

    Harden R installer

--- lang/shell/bash/functions/os/linux/distro/debian/install/system/r-cran-binary.sh
+++ lang/shell/bash/functions/os/linux/distro/debian/install/system/r-cran-binary.sh
@@ -1,30 +1,29 @@
 #!/usr/bin/env bash
 
-# FIXME Installer is erroring with "Not directory: ''." at end of install.
-# FIXME '--reinstall' flag doesn't work here.
-# FIXME Need to wrap inside of system install call.
-
+# FIXME Need to wrap call to 'koopa:::install_app' here.
 koopa::debian_install_r_cran_binary() { # {{{1
     # """
     # Install latest version of R from CRAN.
-    # @note Updated 2021-09-21.
+    # @note Updated 2021-11-03.
     # @seealso
     # - https://cran.r-project.org/bin/linux/debian/
     # - https://cran.r-project.org/bin/linux/ubuntu/README.html
     # """
-    local name_fancy pkgs r version
-    r='/usr/bin/R'
-    koopa::is_installed "$r" && return 0
+    local dict
+    declare -A dict=(
+        [r]='/usr/bin/R'
+        [version]=''
+    )
     while (("$#"))
     do
         case "$1" in
             # Key-value pairs --------------------------------------------------
             '--version='*)
-                version="${1#*=}"
+                dict[version]="${1#*=}"
                 shift 1
                 ;;
             '--version')
-                version="${2:?}"
+                dict[version]="${2:?}"
                 shift 2
                 ;;
             # Other ------------------------------------------------------------
@@ -34,23 +33,23 @@ koopa::debian_install_r_cran_binary() { # {{{1
         esac
     done
     koopa::assert_has_no_args "$#"
-    [[ -z "${version:-}" ]] && version="$(koopa::variable r)"
-    name_fancy="R CRAN ${version} binary package"
-    koopa::install_start "$name_fancy"
-    # This ensures we're starting fresh with the correct apt repo.
+    [[ -z "${dict[version]}" ]] && dict[version]="$(koopa::variable 'r')"
+    dict[name_fancy]="R CRAN ${dict[version]} binary package"
+    if koopa::is_installed "${dict[r]}"
+    then
+        koopa::alert_is_installed "${dict[name_fancy]}"
+        return 0
+    fi
+    koopa::install_start "${dict[name_fancy]}"
     koopa::rm --sudo \
         '/etc/R' \
-        '/etc/apt/sources.list.d/r.list' \
-        '/usr/lib/R/etc'
-    koopa::debian_apt_add_r_repo "$version"
+        '/usr/lib/R/etc' \
+        '/usr/local/lib/R'
+    koopa::debian_apt_add_r_repo "${dict[version]}"
     pkgs=('r-base' 'r-base-dev')
     koopa::debian_apt_install "${pkgs[@]}"
-    # Ensure we don't have a duplicate site library.
-    koopa::rm --sudo '/usr/local/lib/R'
-    echo 'FIXME 1'
-    koopa::configure_r
-    echo 'FIXME 2'
-    koopa::install_success "$name_fancy"
+    koopa::configure_r "${dict[r]}"
+    koopa::install_success "${dict[name_fancy]}"
     return 0
 }
 
