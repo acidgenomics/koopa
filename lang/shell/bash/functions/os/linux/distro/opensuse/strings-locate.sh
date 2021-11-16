15c4fb846976c608d935f9c4ca759150edcfb828
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-16 12:22:59 -0500

    Harden zypper locator

--- lang/shell/bash/functions/os/linux/distro/opensuse/strings-locate.sh
+++ lang/shell/bash/functions/os/linux/distro/opensuse/strings-locate.sh
@@ -3,7 +3,7 @@
 koopa::opensuse_locate_zypper() { # {{{1
     # """
     # Locate OpenSUSE 'zypper'.
-    # @note Updated 2021-11-02.
+    # @note Updated 2021-11-16.
     # """
-    koopa:::locate_app 'zypper'  # FIXME
+    koopa:::locate_app '/usr/bin/zypper'
 }
