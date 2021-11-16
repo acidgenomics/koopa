70e996bddb0e9862256b58207d913cd625763357
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-16 14:48:26 -0500

    Harden Linux-specific string locators

--- lang/shell/bash/functions/os/linux/common/app/bcl2fastq.sh
+++ lang/shell/bash/functions/os/linux/common/app/bcl2fastq.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-# FIXME Need to harden tee call here.
 koopa::linux_bcl2fastq_indrops() { # {{{1
     # """
     # Run bcl2fastq on inDrops sequencing run.
-    # @note Updated 2020-07-20.
+    # @note Updated 2021-11-16.
     # """
+    local app dict
     koopa::assert_has_no_args "$#"
-    koopa::assert_is_installed 'bcl2fastq'
-    bcl2fastq \
+    declare -A app=(
+        [bcl2fastq]="$(koopa::linux_locate_bcl2fastq)"
+        [tee]="$(koopa::locate_tee)"
+    )
+    declare -A dict=(
+        [log_file]='bcl2fastq-indrops.log'
+    )
+    "${app[bcl2fastq]}" \
         --use-bases-mask 'y*,y*,y*,y*' \
         --mask-short-adapter-reads 0 \
         --minimum-trimmed-read-length 0 \
-        2>&1 | tee 'bcl2fastq-indrops.log'
+        2>&1 | "${app[tee]}" "${dict[log_file]}"
     return 0
 }
