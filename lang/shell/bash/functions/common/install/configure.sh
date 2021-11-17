1c133c3047d7372c6b4b2b8a5996d6496123bb2c
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-17 16:31:02 -0500

    Reorganize install scripts

---
+++ lang/shell/bash/functions/common/install/configure.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+koopa::configure_go() { # {{{1
+    koopa:::configure_app_packages \
+        --name-fancy='Go' \
+        --name='go' \
+        --which-app="$(koopa::locate_go)" \
+        "$@"
+}
+
+koopa::configure_julia() { # {{{1
+    koopa:::configure_app_packages \
+        --name-fancy='Julia' \
+        --name='julia' \
+        --which-app="$(koopa::locate_julia)" \
+        "$@"
+}
+
+koopa::configure_nim() { # {{{1
+    koopa:::configure_app_packages \
+        --name-fancy='Nim' \
+        --name='nim' \
+        --which-app="$(koopa::locate_nim)"
+    return 0
+}
+
+koopa::configure_node() { # {{{1
+    koopa:::configure_app_packages \
+        --name-fancy='Node.js' \
+        --name='node' \
+        --which-app="$(koopa::locate_node)" \
+        "$@"
+}
