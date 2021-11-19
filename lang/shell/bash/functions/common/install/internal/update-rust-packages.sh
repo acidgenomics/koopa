5ee66e2fea432b9420a4ef2bf8a58e17867dcd12
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-19 12:44:12 -0500

    Improve installers

---
+++ lang/shell/bash/functions/common/install/internal/update-rust-packages.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+koopa:::update_rust_packages() { # {{{1
+    # """
+    # Update Rust packages.
+    # @note Updated 2021-11-19.
+    # @seealso
+    # - https://crates.io/crates/cargo-update
+    # - https://github.com/nabijaczleweli/cargo-update
+    # """
+    local app
+    declare -A app=(
+        [cargo]="$(koopa::locate_cargo)"
+    )
+    koopa::configure_rust
+    koopa::activate_rust
+    export RUST_BACKTRACE=1
+    koopa::add_to_path_start "$(koopa::dirname "${app[cargo]}")"
+    "${app[cargo]}" install-update -a
+    return 0
+}
