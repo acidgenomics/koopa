960bd446bab43a448be79a1a7c67e1cd3fd4dd4b
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-22 17:33:49 -0500

    Improve Doom Emacs install

---
+++ lang/shell/bash/functions/common/install/internal/update-doom-emacs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+koopa:::update_doom_emacs() { # {{{1
+    # """
+    # Update Doom Emacs.
+    # @note Updated 2021-11-22.
+    #
+    # @seealso
+    # https://github.com/hlissner/doom-emacs/blob/develop/core/cli/upgrade.el
+    # """
+    local app
+    koopa::assert_has_no_args "$#"
+    declare -A app=(
+        [doom]="$(koopa::locate_doom)"
+        [emacs]="$(koopa::locate_emacs)"
+    )
+    koopa::add_to_path_start "$(koopa::dirname "${app[emacs]}")"
+    "${app[doom]}" --yes upgrade --force
+    "${app[doom]}" --yes sync
+    return 0
+}
