2e95dcd768b7676d5d5e82dab87da16d0bd14239
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-22 08:15:38 -0500

    Work on consolidating Homebrew installers

--- lang/shell/bash/functions/common/install/internal/install-tex-packages.sh
+++ lang/shell/bash/functions/common/install/internal/install-tex-packages.sh
@@ -54,21 +54,3 @@ koopa:::install_tex_packages() { # {{{1
     done
     return 0
 }
-
-koopa:::update_tex_packages() { # {{{1
-    # """
-    # Update TeX packages.
-    # @note Updated 2021-11-13.
-    # """
-    local app
-    koopa::assert_has_no_args "$#"
-    koopa::assert_is_admin
-    declare -A app=(
-        [sudo]="$(koopa::locate_sudo)"
-        [tlmgr]="$(koopa::locate_tlmgr)"
-    )
-    "${app[sudo]}" "${app[tlmgr]}" update --self
-    "${app[sudo]}" "${app[tlmgr]}" update --list
-    "${app[sudo]}" "${app[tlmgr]}" update --all
-    return 0
-}
