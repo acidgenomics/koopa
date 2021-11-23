839661caeed49adfa9c99f5a5c8079234f6fe688
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-23 10:46:59 -0500

    Draft update to installers

--- lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
+++ lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
@@ -1,36 +1,10 @@
 #!/usr/bin/env bash
 
-# FIXME Need to wrap this.
-# FIXME Need to run uninstall script with specific Bash.
-koopa::uninstall_dotfiles() { # {{{1
-    # """
-    # Uninstall dot files.
-    # @note Updated 2021-06-14.
-    # """
-    local name name_fancy prefix script
-    koopa::assert_has_no_args "$#"
-    name='dotfiles'
-    name_fancy='dotfiles'
-    prefix="$(koopa::dotfiles_prefix)"
-    if [[ ! -d "$prefix" ]]
-    then
-        koopa::alert_is_not_installed "$name_fancy" "$prefix"
-        return 0
-    fi
-    script="${prefix}/uninstall"
-    koopa::assert_is_file "$script"
-    "$script"
-    koopa::uninstall_app \
-        --name-fancy="$name_fancy" \
-        --name="$name" \
-        "$@"
-    return 0
-}
-
+# FIXME Do we need a separate updater for dotfiles private?
 # FIXME Harden this by using our desired version of Bash, to run the script.
 # FIXME Need to wrap this.
 # FIXME May need to ensure that permissions are correct here.
-koopa::update_dotfiles() { # {{{1
+koopa:::update_dotfiles() { # {{{1
     # """
     # Update dotfiles repo and run install script, if defined.
     # @note Updated 2021-11-18.
