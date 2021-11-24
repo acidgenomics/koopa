af2505e5fabb2e1aee148765071e942e95afd454
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-24 08:21:49 -0500

    Simplify koopa updater

--- lang/shell/bash/functions/common/install/internal/update-koopa.sh
+++ lang/shell/bash/functions/common/install/internal/update-koopa.sh
@@ -3,28 +3,26 @@
 koopa:::update_koopa() { # {{{1
     # """
     # Update koopa installation.
-    # @note Updated 2021-11-22.
+    # @note Updated 2021-11-24.
     #
     # Update of pinned stable releases is intentionally not supported.
+    #
+    # This handles updates to Zsh functions that are changed to group
+    # non-writable permissions, so Zsh passes 'compaudit' checks.
     # """
     local dict
     koopa::assert_has_no_args "$#"
     declare -A dict=(
-        [dotfiles_prefix]="$(koopa::dotfiles_prefix)"
-        [dotfiles_private_prefix]="$(koopa::dotfiles_private_prefix)"
-        [koopa_prefix]="${UPDATE_PREFIX:?}"
+        [prefix]="${UPDATE_PREFIX:?}"
     )
-    if ! koopa::is_git_repo_top_level "${dict[koopa_prefix]}"
+    if ! koopa::is_git_repo_top_level "${dict[prefix]}"
     then
-        koopa::alert_note "Pinned release detected at '${dict[koopa_prefix]}'."
+        koopa::alert_note "Pinned release detected at '${dict[prefix]}'."
         return 1
     fi
-    koopa::sys_set_permissions --recursive "${dict[koopa_prefix]}"
-    koopa::sys_git_pull
-    koopa::update_dotfiles \
-        "${dict[dotfiles_prefix]}" \
-        "${dict[dotfiles_private_prefix]}"
-    koopa::sys_set_permissions --recursive "${dict[koopa_prefix]}"
+    koopa::sys_set_permissions --recursive "${dict[prefix]}"
+    koopa::git_pull "${dict[prefix]}"
+    koopa::sys_set_permissions --recursive "${dict[prefix]}"
     koopa::fix_zsh_permissions
     return 0
 }
