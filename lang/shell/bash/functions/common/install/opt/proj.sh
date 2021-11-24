58ab73c9779df1101520fa68bf23c23b14380ce8
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-24 13:39:11 -0500

    Improve PROJ install

--- lang/shell/bash/functions/common/install/opt/proj.sh
+++ lang/shell/bash/functions/common/install/opt/proj.sh
@@ -3,51 +3,56 @@
 koopa:::install_proj() { # {{{1
     # """
     # Install PROJ.
-    # @note Updated 2021-06-04.
+    # @note Updated 2021-11-24.
     # """
-    local arch conf_args file make_prefix prefix url version
-    prefix="${INSTALL_PREFIX:?}"
-    version="${INSTALL_VERSION:?}"
-    arch="$(koopa::arch)"
-    jobs="$(koopa::cpu_count)"
-    make="$(koopa::locate_make)"
-    name='proj'
+    local app conf_args dict
+    declare -A app=(
+        [make]="$(koopa::locate_make)"
+    )
+    declare -A dict=(
+        [arch]="$(koopa::arch)"
+        [jobs]="$(koopa::cpu_count)"
+        [make_prefix]="$(koopa::make_prefix)"
+        [name]='proj'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
     conf_args=("--prefix=${prefix}")
     if koopa::is_macos
     then
         koopa::activate_homebrew_opt_prefix 'pkg-config' 'libtiff' 'sqlite3'
     elif koopa_is_linux
     then
-        make_prefix="$(koopa::make_prefix)"
         conf_args+=(
-            "CFLAGS=-I${make_prefix}/include"
-            "LDFLAGS=-L${make_prefix}/lib"
+            "CFLAGS=-I${dict[make_prefix]}/include"
+            "LDFLAGS=-L${dict[make_prefix]}/lib"
         )
         # Ensure we're using our custom build of SQLite, in '/usr/local'.
-        SQLITE3_CFLAGS="-I${make_prefix}/include"
-        SQLITE3_LIBS="-L${make_prefix}/lib -lsqlite3"
+        SQLITE3_CFLAGS="-I${dict[make_prefix]}/include"
+        SQLITE3_LIBS="-L${dict[make_prefix]}/lib -lsqlite3"
         # Fix needed to avoid libtiff-4 detection failure.
         # Alternatively, can set '--disable-tiff' configure flag.
         if koopa::is_debian_like
         then
-            # pkg-config: /usr/lib/x86_64-linux-gnu/pkgconfig/libtiff-4.pc
-            TIFF_CFLAGS="/usr/include/${arch}-linux-gnu"
-            TIFF_LIBS="/usr/lib/${arch}-linux-gnu -ltiff"
+            # pkg-config: '/usr/lib/x86_64-linux-gnu/pkgconfig/libtiff-4.pc'.
+            TIFF_CFLAGS="/usr/include/${dict[arch]}-linux-gnu"
+            TIFF_LIBS="/usr/lib/${dict[arch]}-linux-gnu -ltiff"
         elif koopa::is_fedora_like
         then
-            # pkg-config: /usr/lib64/pkgconfig/libtiff-4.pc
+            # pkg-config: '/usr/lib64/pkgconfig/libtiff-4.pc'.
             TIFF_CFLAGS='/usr/include'
             TIFF_LIBS='/usr/lib64 -ltiff'
         fi
         export SQLITE3_CFLAGS SQLITE3_LIBS TIFF_CFLAGS TIFF_LIBS
     fi
-    file="${name}-${version}.tar.gz"
-    url="https://github.com/OSGeo/PROJ/releases/download/${version}/${file}"
-    koopa::download "$url" "$file"
-    koopa::extract "$file"
-    koopa::cd "${name}-${version}"
+    dict[file]="${dict[name]}-${dict[version]}.tar.gz"
+    url="https://github.com/OSGeo/PROJ/releases/download/\
+${dict[version]}/${dict[file]}"
+    koopa::download "${dict[url]}" "${dict[file]}"
+    koopa::extract "${dict[file]}"
+    koopa::cd "${dict[name]}-${dict[version]}"
     ./configure "${conf_args[@]}"
-    "$make" --jobs="$jobs"
-    "$make" install
+    "${app[make]}" --jobs="${dict[jobs]}"
+    "${app[make]}" install
     return 0
 }
