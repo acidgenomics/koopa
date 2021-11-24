16d2981058b6f0173cf57a277d18ee93bd20e01a
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-24 09:02:43 -0500

    Simplify dotfiles updater

--- lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
+++ lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
@@ -1,53 +1,22 @@
 #!/usr/bin/env bash
 
-# FIXME Simplify this to work on our primary dotfiles repo.
-# FIXME Do we need a separate updater for dotfiles private?
-# FIXME Harden this by using our desired version of Bash, to run the script.
-# FIXME Need to wrap this.
-# FIXME May need to ensure that permissions are correct here.
-
 koopa:::update_dotfiles() { # {{{1
     # """
     # Update dotfiles repo and run install script, if defined.
-    # @note Updated 2021-11-18.
+    # @note Updated 2021-11-24.
     # """
-    local app repo repos script
+    local app dict
+    koopa::assert_has_no_args "$#"
     declare -A app=(
         [bash]="$(koopa::locate_bash)"
     )
-    if [[ "$#" -eq 0 ]]
-    then
-        repos=(
-            "$(koopa::dotfiles_prefix)"
-            "$(koopa::dotfiles_private_prefix)"
-        )
-    else
-        repos=("$@")
-    fi
-    # FIXME Need to rework subshell approach here...
-    for repo in "${repos[@]}"
-    do
-        [[ -d "$repo" ]] || continue
-        (
-            koopa::alert_update_start "$repo"
-            koopa::cd "$repo"
-            script="${repo}/update"
-            if [[ -x "$script" ]]
-            then
-                "${app[bash]}" "$script"
-            else
-                # FIXME Can pass directory path in directly.
-                koopa::git_reset
-                # FIXME Can pass directory path in directly.
-                koopa::git_pull
-            fi
-            script="${repo}/install"
-            if [[ -x "$script" ]]
-            then
-                "${app[bash]}" "$script"
-            fi
-            koopa::alert_update_success "$repo"
-        )
-    done
+    declare -A dict=(
+        [prefix]="${UPDATE_PREFIX:?}"
+    )
+    dict[script]="${dict[prefix]}/install"
+    koopa::assert_is_file "${dict[script]}"
+    koopa::git_reset "${dict[prefix]}"
+    koopa::git_pull "${dict[prefix]}"
+    "${app[bash]}" "${dict[script]}"
     return 0
 }
