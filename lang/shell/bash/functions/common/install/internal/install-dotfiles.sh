477feebede2a36de6687b6cda3facd8215e63ea6
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-24 09:11:34 -0500

    Rework installation to call configure at the end

--- lang/shell/bash/functions/common/install/internal/install-dotfiles.sh
+++ lang/shell/bash/functions/common/install/internal/install-dotfiles.sh
@@ -3,25 +3,15 @@
 koopa:::install_dotfiles() { # {{{1
     # """
     # Install dotfiles.
-    # @note Updated 2021-11-23.
+    # @note Updated 2021-11-24.
     # """
-    local app dict
+    local dict
     koopa::assert_has_no_args "$#"
-    declare -A app=(
-        [bash]="$(koopa::locate_bash)"
-    )
     declare -A dict=(
         [prefix]="${INSTALL_PREFIX:?}"
         [url]='https://github.com/acidgenomics/dotfiles.git'
     )
     koopa::git_clone "${dict[url]}" "${dict[prefix]}"
-
-    # FIXME Split this out to separate configure function -----
-    koopa::add_koopa_config_link "${dict[prefix]}" "${dict[name]}"
-    dict[script]="${dict[prefix]}/install"
-    koopa::assert_is_file "${dict[script]}"
-    "${app[bash]}" "${dict[script]}"
-    # FIXME ---------------------------------------------------
-
+    koopa::configure_dotfiles
     return 0
 }
