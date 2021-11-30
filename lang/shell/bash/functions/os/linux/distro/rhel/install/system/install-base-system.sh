f0e2176dedd5646dc29f6b89daeab08e4c6ada52
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-30 10:58:51 -0500

    Improve base system configuration consistency

--- lang/shell/bash/functions/os/linux/distro/rhel/install/system/install-base-system.sh
+++ lang/shell/bash/functions/os/linux/distro/rhel/install/system/install-base-system.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
 
-# FIXME Need to wrap this.
-koopa::rhel_install_base() { # {{{1
+koopa::rhel_install_base_system() { # {{{1
+    koopa::install_app \
+        --name-fancy='Red Hat Enterprise Linux (RHEL) base system' \
+        --name='install-base' \
+        --system \
+        "$@"
+}
+
+koopa:::rhel_install_base_system() { # {{{1
     # """
     # Install Red Hat Enterprise Linux (RHEL) base system.
-    # @note Updated 2021-06-16.
+    # @note Updated 2021-11-30.
     #
     # 'dnf-plugins-core' installs 'config-manager'.
     # """
-    local name_fancy powertools
-    koopa::fedora_install_base "$@"
+    local dict
+    declare -A dict
+    koopa::fedora_install_base_system "$@"
     # Early return for legacy RHEL 7 configs (e.g. Amazon Linux 2).
     if koopa::is_amzn || koopa::is_rhel_7_like
     then
         return 0
     fi
-    koopa::assert_is_installed 'dnf' 'sudo'
-    name_fancy='Red Hat Enterprise Linux (RHEL) base system'
-    koopa::alert_install_start "$name_fancy"
     koopa::fedora_dnf_install 'dnf-plugins-core' 'util-linux-user'
     koopa::rhel_enable_epel
     if koopa::is_centos || koopa::is_rocky
     then
-        powertools='powertools'
+        dict[powertools]='powertools'
     else
-        powertools='PowerTools'
+        dict[powertools]='PowerTools'
     fi
     if ! koopa::is_rhel_ubi
     then
-        koopa::fedora_dnf config-manager --set-enabled "$powertools"
+        koopa::fedora_dnf config-manager --set-enabled "${dict[powertools]}"
     fi
-    koopa::alert_install_success "$name_fancy"
     return 0
 }
