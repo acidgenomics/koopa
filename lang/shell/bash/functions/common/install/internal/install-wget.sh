0f5bdac444e6155a1913145199e22df9838915a9
Michael Steinbaugh <mike@steinbaugh.com>
2021-11-30 08:49:04 -0500

    Update installers

---
+++ lang/shell/bash/functions/common/install/internal/install-wget.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+koopa:::install_wget() { # {{{1
+    # """
+    # Install wget.
+    # @note Updated 2021-11-30.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/wget.rb
+    # """
+    local conf_args dict install_args pkg pkgs
+    install_args=(
+        '--name=wget'
+        '--no-prefix-check'
+        '--quiet'
+    )
+    conf_args=()
+    if koopa::is_linux
+    then
+        conf_args+=(
+            '--with-ssl=openssl'
+        )
+    elif koopa::is_macos
+    then
+        declare -A dict
+        dict[gcc_version]="$(koopa::variable 'gcc')"
+        dict[gcc_maj_ver]="$(koopa::major_version "${dict[gcc_version]}")"
+        pkgs=(
+            "gcc@${dict[gcc_maj_ver]}"
+            'autoconf'
+            'automake'
+            'gettext'
+            'gnutls'
+            'libidn2'
+            'openssl'
+            'pkg-config'
+        )
+        for pkg in "${pkgs[@]}"
+        do
+            install_args+=("--homebrew-opt=${pkg}")
+        done
+        conf_args+=(
+            "CC=gcc-${dict[gcc_maj_ver]}"
+            '--disable-debug'
+            '--disable-pcre'
+            '--without-included-regex'
+            '--without-libpsl'
+        )
+    fi
+    koopa::install_gnu_app \
+        "${install_args[@]}" \
+        "${conf_args[@]}" \
+        "$@"
+}
--- lang/shell/bash/functions/common/strings-processing.sh
+++ lang/shell/bash/functions/common/strings-processing.sh
@@ -3,7 +3,7 @@
 koopa::capitalize() { # {{{1
     # """
     # Capitalize the first letter (only) of a string.
-    # @note Updated 2021-05-21.
+    # @note Updated 2021-11-30.
     #
     # @examples
     # koopa::capitalize 'hello world' 'foo bar'
@@ -11,12 +11,14 @@ koopa::capitalize() { # {{{1
     # @seealso
     # - https://stackoverflow.com/a/12487465
     # """
-    local str tr
+    local app str
     koopa::assert_has_args "$#"
-    tr="$(koopa::locate_tr)"
+    declare -A app=(
+        [tr]="$(koopa::locate_tr)"
+    )
     for str in "$@"
     do
-        str="$("$tr" '[:lower:]' '[:upper:]' <<< "${str:0:1}")${str:1}"
+        str="$("${app[tr]}" '[:lower:]' '[:upper:]' <<< "${str:0:1}")${str:1}"
         koopa::print "$str"
     done
     return 0
@@ -24,32 +26,48 @@ koopa::capitalize() { # {{{1
 
 koopa::paste0() { # {{{1
     # """
-    # Paste arguments (e.g. from an array) into a string separated by delimiter
-    # defined in the first positional argument.
-    # @note Updated 2021-05-21.
+    # Paste arguments into a string separated by delimiter.
+    # @note Updated 2021-11-30.
     #
-    # Note that the 'paste0' name is a reference to the R function.
-    #
-    # @params
-    # $1: The delimiter string
-    # ${@:2}: The arguments to join
-    #
-    # @return
-    # >&1: The arguments separated by the delimiter string
-    #
-    # See also:
-    # - https://stackoverflow.com/questions/13470413
-    # - https://stackoverflow.com/a/57536163/3911732
+    # @seealso
+    # - https://stackoverflow.com/a/57536163/3911732/
+    # - https://stackoverflow.com/questions/13470413/
     # - https://stackoverflow.com/questions/1527049/
+    #
+    # @examples
+    # > koopa::paste0 --sep=', ' 'aaa bbb' 'ccc ddd'
+    # # aaa bbb, ccc ddd
     # """
-    # Require at least the delimiter.
-    (($#)) || return 1
-    local -- delim="$1" str IFS=
-    shift 1
-    # Expand arguments with prefixed delimiter (Empty IFS).
-    str="${*/#/$delim}"
-    # Print without the first delimiter.
-    str="${str:${#delim}}"
+    local IFS pos sep str
+    sep=''
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            # Key-value pairs --------------------------------------------------
+            '--sep='*)
+                sep="${1#*=}"
+                shift 1
+                ;;
+            '--sep')
+                sep="${2:?}"
+                shift 2
+                ;;
+            # Other ------------------------------------------------------------
+            '-'*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa::assert_has_args "$#"
+    IFS=''
+    str="${*/#/$sep}"
+    str="${str:${#sep}}"
     koopa::print "$str"
     return 0
 }
@@ -57,9 +75,13 @@ koopa::paste0() { # {{{1
 koopa::to_string() { # {{{1
     # """
     # Paste arguments to a comma separated string.
-    # @note Updated 2020-06-30.
+    # @note Updated 2021-11-30.
+    #
+    # @examples
+    # > koopa::to_string 'aaa' 'bbb'
+    # # aaa, bbb
     # """
     koopa::assert_has_args "$#"
-    koopa::paste0 ', ' "$@"
+    koopa::paste0 --sep=', ' "$@"
     return 0
 }
