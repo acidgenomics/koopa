f1b3c6906c6e0ee9aac64471a06e37a2781e77b5
Michael Steinbaugh <mike@steinbaugh.com>
2021-12-01 15:57:12 -0500

    Improve julia installer

--- lang/shell/bash/functions/common/install/opt/julia.sh
+++ lang/shell/bash/functions/common/install/opt/julia.sh
@@ -3,7 +3,7 @@
 koopa:::install_julia() { # {{{1
     # """
     # Install Julia (from source).
-    # @note Updated 2021-05-26.
+    # @note Updated 2021-12-01.
     # @seealso
     # - https://github.com/JuliaLang/julia/blob/master/doc/build/build.md
     # - https://github.com/JuliaLang/julia/blob/master/doc/build/linux.md
@@ -11,43 +11,46 @@ koopa:::install_julia() { # {{{1
     # - https://github.com/JuliaLang/julia/blob/master/doc/build/build.md#llvm
     # - https://github.com/JuliaLang/julia/blob/master/Make.inc
     # """
-    local file jobs make name prefix version url
+    local app dict
+    koopa::assert_has_no_args "$#"
     koopa::alert_coffee_time
+    declare -A app=(
+        [make]="$(koopa::locate_make)"
+    )
+    declare -A dict=(
+        [jobs]="$(koopa::cpu_count)"
+        [name]='julia'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    # > dict[file]="v${dict[version]}.tar.gz"
+    # > dict[url]="https://github.com/JuliaLang/${dict[name]}/\
+    # >     archive/${dict[file]}"
+    dict[file]="${dict[name]}-${dict[version]}-full.tar.gz"
+    dict[url]="https://github.com/JuliaLang/${dict[name]}/releases/download/\
+v${dict[version]}/${dict[file]}"
     if koopa::is_macos
     then
-        # NOTE Seeing this pop up on macOS:
-        # # Warning: git information unavailable; versioning information limited
-        # Including 'git' here makes no difference.
         koopa::activate_homebrew_opt_prefix 'gcc'
     fi
-    prefix="${INSTALL_PREFIX:?}"
-    version="${INSTALL_VERSION:?}"
-    jobs="$(koopa::cpu_count)"
-    make="$(koopa::locate_make)"
-    name='julia'
-    # > file="v${version}.tar.gz"
-    # > url="https://github.com/JuliaLang/julia/archive/${file}"
-    file="${name}-${version}-full.tar.gz"
-    url="https://github.com/JuliaLang/${name}/releases/download/\
-v${version}/${file}"
-    koopa::download "$url" "$file"
-    koopa::extract "$file"
-    koopa::cd "${name}-${version}"
+    koopa::download "${dict[url]}" "${dict[file]}"
+    koopa::extract "${dict[file]}"
+    koopa::cd "${dict[name]}-${dict[version]}"
     # If set, this will interfere with internal LLVM build required for
     # Julia. See 'build.md' file for LLVM details.
     unset -v LLVM_CONFIG
     # Customize the 'Make.user' file.
     # Need to ensure we configure internal LLVM build here.
     cat > 'Make.user' << END
-prefix=${prefix}
+prefix=${dict[prefix]}
 # > LLVM_ASSERTIONS=1
 # > LLVM_DEBUG=Release
 # > USE_BINARYBUILDER=0
 USE_LLVM_SHLIB=0
 USE_SYSTEM_LLVM=0
 END
-    "$make" --jobs="$jobs"
-    # > "$make" test
-    "$make" install
+    "${app[make]}" --jobs="${dict[jobs]}"
+    # > "${app[make]}" test
+    "${app[make]}" install
     return 0
 }
