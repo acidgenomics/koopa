4cfd01aa3202deaaaf614e2d81fb8ccd111f90b7
Michael Steinbaugh <mike@steinbaugh.com>
2021-12-07 13:54:25 -0500

    Simplify the PROJ installer

--- lang/shell/bash/functions/common/install/internal/install-proj.sh
+++ lang/shell/bash/functions/common/install/internal/install-proj.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# NOTE Now seeing this warning on Debian:
-# g++: warning: /usr/include/x86_64-linux-gnu: linker input file unused
-# because linking not done
-
 koopa:::install_proj() { # {{{1
     # """
     # Install PROJ.
-    # @note Updated 2021-11-24.
+    # @note Updated 2021-12-07.
     # """
     local app conf_args dict
     declare -A app=(
@@ -27,33 +23,6 @@ koopa:::install_proj() { # {{{1
     then
         koopa::activate_homebrew_opt_prefix 'pkg-config' 'libtiff' 'sqlite3'
     fi
-
-    # FIXME Does this install successfully with these disabled?
-    # > elif koopa::is_linux
-    # > then
-    # >     conf_args+=(
-    # >         "CFLAGS=-I${dict[make_prefix]}/include"
-    # >         "LDFLAGS=-L${dict[make_prefix]}/lib"
-    # >     )
-    # >     # Ensure we're using our custom build of SQLite, in '/usr/local'.
-    # >     SQLITE3_CFLAGS="-I${dict[make_prefix]}/include"
-    # >     SQLITE3_LIBS="-L${dict[make_prefix]}/lib -lsqlite3"
-    # >     # Fix needed to avoid libtiff-4 detection failure.
-    # >     # Alternatively, can set '--disable-tiff' configure flag.
-    # >     if koopa::is_debian_like
-    # >     then
-    # >         # pkg-config: '/usr/lib/x86_64-linux-gnu/pkgconfig/libtiff-4.pc'.
-    # >         TIFF_CFLAGS="/usr/include/${dict[arch]}-${dict[os]}"
-    # >         TIFF_LIBS="/usr/lib/${dict[arch]}-${dict[os]} -ltiff"
-    # >     elif koopa::is_fedora_like
-    # >     then
-    # >         # pkg-config: '/usr/lib64/pkgconfig/libtiff-4.pc'.
-    # >         TIFF_CFLAGS='/usr/include'
-    # >         TIFF_LIBS='/usr/lib64 -ltiff'
-    # >     fi
-    # >     export SQLITE3_CFLAGS SQLITE3_LIBS TIFF_CFLAGS TIFF_LIBS
-    # > fi
-
     dict[file]="${dict[name]}-${dict[version]}.tar.gz"
     dict[url]="https://github.com/OSGeo/PROJ/releases/download/\
 ${dict[version]}/${dict[file]}"
