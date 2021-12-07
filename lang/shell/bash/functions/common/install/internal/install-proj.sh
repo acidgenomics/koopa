a370eb20f154317f26e9d7e21091ddf44f2d60f8
Michael Steinbaugh <mike@steinbaugh.com>
2021-12-07 13:45:57 -0500

    Update PROJ installer

--- lang/shell/bash/functions/common/install/internal/install-proj.sh
+++ lang/shell/bash/functions/common/install/internal/install-proj.sh
@@ -26,30 +26,34 @@ koopa:::install_proj() { # {{{1
     if koopa::is_macos
     then
         koopa::activate_homebrew_opt_prefix 'pkg-config' 'libtiff' 'sqlite3'
-    elif koopa::is_linux
-    then
-        conf_args+=(
-            "CFLAGS=-I${dict[make_prefix]}/include"
-            "LDFLAGS=-L${dict[make_prefix]}/lib"
-        )
-        # Ensure we're using our custom build of SQLite, in '/usr/local'.
-        SQLITE3_CFLAGS="-I${dict[make_prefix]}/include"
-        SQLITE3_LIBS="-L${dict[make_prefix]}/lib -lsqlite3"
-        # Fix needed to avoid libtiff-4 detection failure.
-        # Alternatively, can set '--disable-tiff' configure flag.
-        if koopa::is_debian_like
-        then
-            # pkg-config: '/usr/lib/x86_64-linux-gnu/pkgconfig/libtiff-4.pc'.
-            TIFF_CFLAGS="/usr/include/${dict[arch]}-${dict[os]}"
-            TIFF_LIBS="/usr/lib/${dict[arch]}-${dict[os]} -ltiff"
-        elif koopa::is_fedora_like
-        then
-            # pkg-config: '/usr/lib64/pkgconfig/libtiff-4.pc'.
-            TIFF_CFLAGS='/usr/include'
-            TIFF_LIBS='/usr/lib64 -ltiff'
-        fi
-        export SQLITE3_CFLAGS SQLITE3_LIBS TIFF_CFLAGS TIFF_LIBS
     fi
+
+    # FIXME Does this install successfully with these disabled?
+    # > elif koopa::is_linux
+    # > then
+    # >     conf_args+=(
+    # >         "CFLAGS=-I${dict[make_prefix]}/include"
+    # >         "LDFLAGS=-L${dict[make_prefix]}/lib"
+    # >     )
+    # >     # Ensure we're using our custom build of SQLite, in '/usr/local'.
+    # >     SQLITE3_CFLAGS="-I${dict[make_prefix]}/include"
+    # >     SQLITE3_LIBS="-L${dict[make_prefix]}/lib -lsqlite3"
+    # >     # Fix needed to avoid libtiff-4 detection failure.
+    # >     # Alternatively, can set '--disable-tiff' configure flag.
+    # >     if koopa::is_debian_like
+    # >     then
+    # >         # pkg-config: '/usr/lib/x86_64-linux-gnu/pkgconfig/libtiff-4.pc'.
+    # >         TIFF_CFLAGS="/usr/include/${dict[arch]}-${dict[os]}"
+    # >         TIFF_LIBS="/usr/lib/${dict[arch]}-${dict[os]} -ltiff"
+    # >     elif koopa::is_fedora_like
+    # >     then
+    # >         # pkg-config: '/usr/lib64/pkgconfig/libtiff-4.pc'.
+    # >         TIFF_CFLAGS='/usr/include'
+    # >         TIFF_LIBS='/usr/lib64 -ltiff'
+    # >     fi
+    # >     export SQLITE3_CFLAGS SQLITE3_LIBS TIFF_CFLAGS TIFF_LIBS
+    # > fi
+
     dict[file]="${dict[name]}-${dict[version]}.tar.gz"
     dict[url]="https://github.com/OSGeo/PROJ/releases/download/\
 ${dict[version]}/${dict[file]}"
