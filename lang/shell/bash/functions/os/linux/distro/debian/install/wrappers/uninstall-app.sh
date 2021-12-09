978edcc895543262d6abba63c91c84d961095fb0
Michael Steinbaugh <mike@steinbaugh.com>
2021-12-09 17:09:32 -0500

    Updates to core install handlers

--- lang/shell/bash/functions/os/linux/distro/debian/install/wrappers/uninstall-app.sh
+++ lang/shell/bash/functions/os/linux/distro/debian/install/wrappers/uninstall-app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-koopa::debian_install_docker() { # {{{1
-    koopa::install_app \
+koopa::debian_uninstall_docker() { # {{{1
+    koopa::uninstall_app \
         --name-fancy='Docker' \
         --name='docker' \
         --platform='debian' \
