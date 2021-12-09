c879c3fba8e431c4775be53e0c9bfdf899c772a6
Michael Steinbaugh <mike@steinbaugh.com>
2021-12-09 17:34:50 -0500

    Fix version handling

--- lang/shell/bash/functions/common/install/internal/install-luarocks.sh
+++ lang/shell/bash/functions/common/install/internal/install-luarocks.sh
@@ -26,7 +26,7 @@ koopa:::install_luarocks() { # {{{1
     koopa::cd "${dict[name]}-${dict[version]}"
     ./configure \
         --prefix="${dict[prefix]}" \
-        --lua-version="${dict[lua_version]}" \
+        --lua-version="${dict[lua_min_maj_ver]}" \
         --versioned-rocks-dir
     "${app[make]}" build
     "${app[make]}" install
