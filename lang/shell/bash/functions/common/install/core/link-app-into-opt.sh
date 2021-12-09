3085613a83c10325d71c6a0373aadf320230599e
Michael Steinbaugh <mike@steinbaugh.com>
2021-12-09 17:09:32 -0500

    Updates to core install handlers

--- lang/shell/bash/functions/common/install/core/link-app-into-opt.sh
+++ lang/shell/bash/functions/common/install/core/link-app-into-opt.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-# FIXME Need to rework using dict approach.
 koopa::link_app_into_opt() { # {{{1
     # """
     # Link an application into koopa opt prefix.
-    # @note Updated 2021-11-17.
+    # @note Updated 2021-12-09.
     # """
+    local dict
     koopa::assert_has_args_eq "$#" 2
-    local opt_prefix source_dir target_dir
-    source_dir="${1:?}"
-    opt_prefix="$(koopa::opt_prefix)"
-    [[ ! -d "$opt_prefix" ]] && koopa::mkdir "$opt_prefix"
-    target_dir="${opt_prefix}/${2:?}"
+    declare -A dict=(
+        [opt_prefix]="$(koopa::opt_prefix)"
+        [source_dir]="${1:?}"
+    )
+    dict[target_dir]="${dict[opt_prefix]}/${2:?}"
+    [[ ! -d "${dict[opt_prefix]}" ]] && koopa::mkdir "${dict[opt_prefix]}"
     # This happens during installation of app packages (e.g. Python).
-    [[ "$source_dir" == "$target_dir" ]] && return 0
-    koopa::alert "Linking '${source_dir}' in '${target_dir}'."
-    [[ ! -d "$source_dir" ]] && koopa::mkdir "$source_dir"
-    [[ -d "$target_dir" ]] && koopa::sys_rm "$target_dir"
-    koopa::sys_set_permissions "$opt_prefix"
-    koopa::sys_ln "$source_dir" "$target_dir"
+    [[ "${dict[source_dir]}" == "${dict[target_dir]}" ]] && return 0
+    koopa::alert "Linking '${dict[source_dir]}' in '${dict[target_dir]}'."
+    [[ ! -d "${dict[source_dir]}" ]] && koopa::mkdir "${dict[source_dir]}"
+    [[ -d "${dict[target_dir]}" ]] && koopa::sys_rm "${dict[target_dir]}"
+    koopa::sys_set_permissions "${dict[opt_prefix]}"
+    koopa::sys_ln "${dict[source_dir]}" "${dict[target_dir]}"
     return 0
 }
