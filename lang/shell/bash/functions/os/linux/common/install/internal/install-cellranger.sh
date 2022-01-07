9a84d44da5011b402c5fc6cc5930f5596f7fd6b7
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-07 07:56:35 -0500

    Draft update to installers

--- lang/shell/bash/functions/os/linux/common/install/internal/install-cellranger.sh
+++ lang/shell/bash/functions/os/linux/common/install/internal/install-cellranger.sh
@@ -3,7 +3,7 @@
 koopa:::linux_install_cellranger() { # {{{1
     # """
     # Install Cell Ranger.
-    # @note Updated 2021-11-16.
+    # @note Updated 2022-01-06.
     #
     # Refdata is accessible here:
     # https://support.10xgenomics.com/single-cell-gene-expression/
@@ -12,12 +12,13 @@ koopa:::linux_install_cellranger() { # {{{1
     local dict
     koopa::assert_has_no_args "$#"
     declare -A dict=(
+        [installers_url]="$(koopa::koopa_installers_url)"
         [name]='cellranger'
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
     dict[file]="${dict[name]}-${dict[version]}.tar.gz"
-    dict[url]="https://seq.cloud/install/cellranger/${dict[file]}"
+    dict[url]="${dict[installers_url]}/cellranger/${dict[file]}"
     koopa::download "${dict[url]}" "${dict[file]}"
     koopa::extract "${dict[file]}"
     koopa::sys_mv "${dict[name]}-${dict[version]}" "${dict[prefix]}"
