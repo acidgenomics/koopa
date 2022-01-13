c4fa7f4dbe6aa53f9480c5903a0d42c84b75c2d6
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-13 13:59:27 -0500

    Pin google cloud sdk to python 3.8 on macos

--- lang/shell/posix/functions/activate-noninteractive.sh
+++ lang/shell/posix/functions/activate-noninteractive.sh
@@ -145,17 +145,17 @@ _koopa_activate_homebrew() { # {{{1
 _koopa_activate_homebrew_cask_google_cloud_sdk() { # {{{1
     # """
     # Activate Homebrew Google Cloud SDK.
-    # @note Updated 2021-11-04.
+    # @note Updated 2022-01-13.
     #
-    # The SDK doesn't currently support Python 3.10, so pinning to 3.9.
+    # @seealso
+    # - https://cloud.google.com/sdk/docs/install#mac
     # """
     local brew_prefix prefix python
     [ "$#" -eq 0 ] || return 1
     brew_prefix="$(_koopa_homebrew_prefix)"
     prefix="${brew_prefix}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
     _koopa_activate_prefix "$prefix"
-    # Need to pin to Python 3.9, since 3.10 isn't currently supported.
-    python="${brew_prefix}/opt/python@3.9/bin/python3.9"
+    python="${brew_prefix}/opt/python@3.8/bin/python3.8"
     export CLOUDSDK_PYTHON="$python"
     # Alternate (slower) approach that enables autocompletion.
     # > local shell
