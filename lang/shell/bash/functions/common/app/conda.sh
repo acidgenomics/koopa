3115316c58d7e80a816ac386a613de3698e14c00
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-14 12:23:55 -0500

    Simplify support for conda runners

--- lang/shell/bash/functions/common/app/conda.sh
+++ lang/shell/bash/functions/common/app/conda.sh
@@ -45,6 +45,7 @@ koopa::activate_conda_env() { # {{{1
     return 0
 }
 
+# FIXME Take this out, just move to pinned environment versions.
 koopa::conda_create_bioinfo_envs() { # {{{1
     # """
     # Create Conda bioinformatics environments.
@@ -422,27 +423,43 @@ koopa::conda_create_bioinfo_envs() { # {{{1
 # FIXME This approach is useful, because it enables us to dynamically install
 #       missing conda environments, potentially during
 #       'koopa::locate_conda_XXX' calls.
+# FIXME Add support for '--build' pass-in, which will fix current issue
+#       with mashmap on macOS.
 
 koopa::conda_create_env() { # {{{1
     # """
     # Create a conda environment.
-    # @note Updated 2021-10-05.
+    # @note Updated 2022-01-14.
     #
     # Creates a unique environment for each recipe requested.
     # Supports versioning, which will return as 'star@2.7.5a' for example.
     # """
-    local conda conda_prefix force env_name env_string env_version pos prefix
+    local app dict pos string
     koopa::assert_has_args "$#"
-    force=0
+    declare -A app=(
+        [conda]="$(koopa::locate_conda)"
+        [cut]="$(koopa::locate_cut)"
+    )
+    declare -A dict=(
+        [conda_prefix]="$(koopa::conda_prefix)"
+        [force]=0
+        [latest]=0
+    )
     pos=()
     while (("$#"))
     do
         case "$1" in
+            # Flags ------------------------------------------------------------
             '--force' | \
             '--reinstall')
-                force=1
+                dict[force]=1
                 shift 1
                 ;;
+            '--latest')
+                dict[latest]=1
+                shift 1
+                ;;
+            # Other ------------------------------------------------------------
             '-'*)
                 koopa::invalid_arg "$1"
                 ;;
@@ -454,42 +471,60 @@ koopa::conda_create_env() { # {{{1
     done
     [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
     koopa::assert_has_args "$#"
-    conda="$(koopa::locate_conda)"
-    conda_prefix="$(koopa::conda_prefix)"
-    for env_string in "$@"
+    for string in "$@"
     do
         # Note that we're using 'salmon@1.4.0' for the environment name but
         # must use 'salmon=1.4.0' in the call to conda below.
-        env_string="${env_string//@/=}"
-        # If the version isn't specified, fetch the latest one automatically.
-        if ! koopa::str_detect_fixed "$env_string" '='
+        dict[env_string]="${string//@/=}"
+        if [[ "${dict[latest]}" -eq 1 ]]
         then
-            koopa::alert "Obtaining latest version for '${env_string}'."
-            env_version="$(koopa::conda_env_latest_version "$env_string")"
-            [[ -n "$env_version" ]] || return 1
-            env_string="${env_string}=${env_version}"
-        fi
-        env_name="${env_string//=/@}"
-        prefix="${conda_prefix}/envs/${env_name}"
-        if [[ -d "$prefix" ]]
-        then
-            if [[ "$force" -eq 1 ]]
+            if koopa::str_detect_fixed "${dict[env_string]}" '='
             then
-                koopa::conda_remove_env "$env_name"
+                koopa::stop "Don't specify version when using '--latest'."
+            fi
+            koopa::alert "Obtaining latest version for '${dict[env_string]}'."
+            dict[env_version]="$( \
+                koopa::conda_env_latest_version "${dict[env_string]}" \
+            )"
+            [[ -n "${dict[env_version]}" ]] || return 1
+            dict[env_string]="${dict[env_string]}=${dict[env_version]}"
+        elif ! koopa::str_detect_fixed "${dict[env_string]}" '='
+        then
+            dict[env_version]="$( \
+                koopa::variable "conda-${dict[env_string]}" \
+                || true \
+            )"
+            if [[ -z "${dict[env_version]}" ]]
+            then
+                koopa::stop 'Pinned environment version not defined in koopa.'
+            fi
+            dict[env_string]="${dict[env_string]}=${dict[env_version]}"
+        fi
+        # Ensure we handle edge case of '<NAME>=<VERSION>=<BUILD>' here.
+        dict[env_name]="$( \
+            koopa::print "${dict[env_string]//=/@}" \
+            | "${app[cut]}" -d '@' -f '1-2' \
+        )"
+        dict[env_prefix]="${dict[conda_prefix]}/envs/${dict[env_name]}"
+        if [[ -d "${dict[env_prefix]}" ]]
+        then
+            if [[ "${dict[force]}" -eq 1 ]]
+            then
+                koopa::conda_remove_env "${dict[env_name]}"
             else
-                koopa::alert_note "Conda environment '${env_name}' \
-exists at '${prefix}'."
+                koopa::alert_note "Conda environment '${dict[env_name]}' \
+exists at '${dict[env_prefix]}'."
                 continue
             fi
         fi
-        koopa::alert_install_start "$env_name" "$prefix"
-        "$conda" create \
-            --name="$env_name" \
+        koopa::alert_install_start "${dict[env_name]}" "${dict[env_prefix]}"
+        "${app[conda]}" create \
+            --name="${dict[env_name]}" \
             --quiet \
             --yes \
-            "$env_string"
-        koopa::sys_set_permissions --recursive "$prefix"
-        koopa::alert_install_success "$env_name" "$prefix"
+            "${dict[env_string]}"
+        koopa::sys_set_permissions --recursive "${dict[env_prefix]}"
+        koopa::alert_install_success "${dict[env_name]}" "${dict[env_prefix]}"
     done
     return 0
 }
