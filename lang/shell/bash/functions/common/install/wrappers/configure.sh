571bd3ac51d02a191a3fe566605b25191e286158
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-18 08:48:36 -0500

    Harden our dotfiles installer

--- lang/shell/bash/functions/common/install/wrappers/configure.sh
+++ lang/shell/bash/functions/common/install/wrappers/configure.sh
@@ -6,13 +6,16 @@ koopa::configure_dotfiles() { # {{{1
     # @note Updated 2022-01-18.
     # """
     local app dict
+    koopa::assert_has_args_le "$#" 1
     declare -A app=(
         [bash]="$(koopa::locate_bash)"
     )
     declare -A dict=(
         [name]='dotfiles'
-        [prefix]="$(koopa::dotfiles_prefix)"
+        [prefix]="${1:-}"
     )
+    [[ -z "${dict[prefix]}" ]] && dict[prefix]="$(koopa::dotfiles_prefix)"
+    koopa::assert_is_dir "${dict[prefix]}"
     dict[script]="${dict[prefix]}/install"
     koopa::assert_is_file "${dict[script]}"
     koopa::add_koopa_config_link "${dict[prefix]}" "${dict[name]}"
