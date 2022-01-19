b9b50c485ea991bc06a5230223255a7b36d56da6
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-19 16:18:45 -0500

    Improve FIXME regarding dotfiles updater

--- lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
+++ lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-# FIXME This is now erroring:
+# FIXME This is now erroring in 'configure_dotfiles' step.
+# Is this due to minimal PATH? Why does 'activate' have an unbound variable?
 # /opt/koopa/bin/../lang/shell/bash/include/header.sh: line 114: activate: unbound variable
 # /opt/koopa/opt/dotfiles/install: line 70: : No such file or directory
 
 koopa:::update_dotfiles() { # {{{1
     # """
     # Update dotfiles repo and run install script, if defined.
-    # @note Updated 2021-11-24.
+    # @note Updated 2022-01-19.
     # """
-    local app dict
+    local dict
     koopa::assert_has_no_args "$#"
-    declare -A app=(
-        [bash]="$(koopa::locate_bash)"
-    )
     declare -A dict=(
         [prefix]="${UPDATE_PREFIX:?}"
     )
@@ -21,6 +19,7 @@ koopa:::update_dotfiles() { # {{{1
     koopa::assert_is_file "${dict[script]}"
     koopa::git_reset "${dict[prefix]}"
     koopa::git_pull "${dict[prefix]}"
-    "${app[bash]}" "${dict[script]}"
+    # FIXME Error described above is happening here.
+    koopa::configure_dotfiles "${dict[prefix]}"
     return 0
 }
