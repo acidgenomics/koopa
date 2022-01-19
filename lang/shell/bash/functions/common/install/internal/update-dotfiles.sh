bf2b525e90c8967f2ba579c1d61d3545ec0f302e
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-19 16:33:19 -0500

    Harden our dotfiles installer

--- lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
+++ lang/shell/bash/functions/common/install/internal/update-dotfiles.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME This is now erroring in 'configure_dotfiles' step.
-# Is this due to minimal PATH? Why does 'activate' have an unbound variable?
-# /opt/koopa/bin/../lang/shell/bash/include/header.sh: line 114: activate: unbound variable
-# /opt/koopa/opt/dotfiles/install: line 70: : No such file or directory
-
 koopa:::update_dotfiles() { # {{{1
     # """
     # Update dotfiles repo and run install script, if defined.
@@ -19,7 +14,6 @@ koopa:::update_dotfiles() { # {{{1
     koopa::assert_is_file "${dict[script]}"
     # > koopa::git_reset "${dict[prefix]}"
     koopa::git_pull "${dict[prefix]}"
-    # FIXME Error described above is happening here.
     koopa::configure_dotfiles "${dict[prefix]}"
     return 0
 }
