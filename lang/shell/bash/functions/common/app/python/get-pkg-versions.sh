245b8eb1411ce9fa1544d0870c800aa09ec7553f
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-20 15:16:13 -0500

    Harden functions

--- lang/shell/bash/functions/common/app/python/get-pkg-versions.sh
+++ lang/shell/bash/functions/common/app/python/get-pkg-versions.sh
@@ -3,14 +3,13 @@
 koopa::python_get_pkg_versions() {
     # """
     # Get pinned Python package versions for pip install call.
-    # @note Updated 2021-10-05.
+    # @note Updated 2022-01-20.
     # """
     local i pkg pkgs pkg_lower version
     koopa::assert_has_args "$#"
     pkgs=("$@")
     for i in "${!pkgs[@]}"
     do
-        # FIXME Rework this using dict approach.
         pkg="${pkgs[$i]}"
         pkg_lower="$(koopa::lowercase "$pkg")"
         version="$(koopa::variable "python-${pkg_lower}")"
