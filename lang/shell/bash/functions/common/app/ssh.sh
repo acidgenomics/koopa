6466381fe6ff96e0ac89520d6f8f066ecbc9f778
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-20 10:57:20 -0500

    Update SSH functions

--- lang/shell/bash/functions/common/app/ssh.sh
+++ lang/shell/bash/functions/common/app/ssh.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME Rework using app/dict approach.
 koopa::generate_ssh_key() { # {{{1
     # """
     # Generate SSH key.
-    # @note Updated 2021-09-29.
+    # @note Updated 2022-01-20.
     #
     # This script is called inside our Linux VM configuration function, so
     # don't use assert here.
@@ -19,31 +18,26 @@ koopa::generate_ssh_key() { # {{{1
     # @seealso
     # - https://blog.g3rt.nl/upgrade-your-ssh-keys.html
     # """
-    local comment file flags hostname key_name ssh_keygen user
-    ssh_keygen="$(koopa::locate_ssh_keygen)"
-    user="$(koopa::user)"
-    hostname="$(koopa::hostname)"
-    comment="${user}@${hostname}"
-    # > key_name='id_ed25519'
-    key_name='id_rsa'
+    local app dict ssh_args
+    declare -A app=(
+        [ssh_keygen]="$(koopa::locate_ssh_keygen)"
+    )
+    declare -A dict=(
+        [hostname]="$(koopa::hostname)"
+        [key_name]='id_rsa'  # or 'id_ed25519'
+        [prefix]="${HOME:?}/.ssh"
+        [user]="$(koopa::user)"
+    )
     while (("$#"))
     do
         case "$1" in
             # Key-value pairs --------------------------------------------------
-            '--comment='*)
-                comment="${1#*=}"
-                shift 1
-                ;;
-            '--comment')
-                comment="${2:?}"
-                shift 2
-                ;;
             '--key-name='*)
-                key_name="${1#*=}"
+                dict[key_name]="${1#*=}"
                 shift 1
                 ;;
             '--key-name')
-                key_name="${2:?}"
+                dict[key_name]="${2:?}"
                 shift 2
                 ;;
             # Other ------------------------------------------------------------
@@ -52,56 +46,67 @@ koopa::generate_ssh_key() { # {{{1
                 ;;
         esac
     done
-    file="${HOME:?}/.ssh/${key_name}"
-    if [[ -f "$file" ]]
+    file="${dict[prefix]}/${dict[key_name]}"
+    if [[ -f "${dict[file]}" ]]
     then
-        koopa::alert_note "SSH key exists at '${file}'."
+        koopa::alert_note "SSH key exists at '${dict[file]}'."
         return 0
     fi
-    koopa::alert "Generating SSH key at '${file}'."
-    flags=(
-        '-C' "$comment"
+    koopa::alert "Generating SSH key at '${dict[file]}'."
+    ssh_args=(
+        '-C' "${dict[user]}@${dict[hostname]}"
         '-N' ''
-        '-f' "$file"
+        '-f' "${dict[file]}"
         '-q'
     )
-    if koopa::str_detect_fixed "$file" '_rsa'
-    then
-        # RSA 4096.
-        flags+=(
-            '-b' 4096
-            '-t' 'rsa'
-        )
-    else
-        # Ed25519 (now recommended).
-        flags+=(
-            '-a' 100
-            '-o'
-            '-t' 'ed25519'
-        )
-    fi
+    case "${dict[key_name]}" in
+        *'_ed25519')
+            # Ed25519.
+            ssh_args+=(
+                '-a' 100
+                '-o'
+                '-t' 'ed25519'
+            )
+            ;;
+        *'_rsa')
+            # RSA 4096.
+            ssh_args+=(
+                '-b' 4096
+                '-t' 'rsa'
+            )
+            ;;
+        *)
+            koopa::stop "Unsupported key: '${dict[key_name]}'."
+            ;;
+    esac
     koopa::dl \
-        'ssh-keygen' "$ssh_keygen" \
-        'Flags' "${flags[*]}"
-    "$ssh_keygen" "${flags[@]}"
-    koopa::alert_success "Generated SSH key at '${file}'."
+        'ssh-keygen' "${app[ssh_keygen]}" \
+        'args' "${ssh_args[*]}"
+    "${app[ssh_keygen]}" "${ssh_args[@]}"
+    koopa::alert_success "Generated SSH key at '${dict[file]}'."
     return 0
 }
 
-# FIXME Rework using app/dict approach.
 koopa::ssh_key_info() { # {{{1
     # """
     # Get SSH key information.
-    # @note Updated 2021-09-21.
+    # @note Updated 2022-01-20.
+    #
     # @seealso
     # - https://blog.g3rt.nl/upgrade-your-ssh-keys.html
     # """
-    local keyfile ssh_keygen uniq
-    ssh_keygen="$(koopa::locate_ssh_keygen)"
-    uniq="$(koopa::locate_uniq)"
-    for keyfile in "${HOME:?}/.ssh/id_"*
+    local app dict keyfile
+    declare -A app=(
+        [ssh_keygen]="$(koopa::locate_ssh_keygen)"
+        [uniq]="$(koopa::locate_uniq)"
+    )
+    declare -A dict=(
+        [prefix]="${HOME:?}/.ssh"
+        [stem]='id_'
+    )
+    for keyfile in "${dict[prefix]}/${dict[stem]}"*
     do
-        "$ssh_keygen" -l -f "$keyfile"
-    done | "$uniq"
+        "${app[ssh_keygen]}" -l -f "$keyfile"
+    done | "${app[uniq]}"
     return 0
 }
