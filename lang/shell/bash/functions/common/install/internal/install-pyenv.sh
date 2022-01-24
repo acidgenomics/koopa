b0aefc1ddc23b3331e29cd66ae8ef561d0335e8f
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-24 08:49:02 -0500

    Update installers

--- lang/shell/bash/functions/common/install/internal/install-pyenv.sh
+++ lang/shell/bash/functions/common/install/internal/install-pyenv.sh
@@ -13,17 +13,3 @@ koopa:::install_pyenv() { # {{{1
     koopa::git_clone "${dict[url]}" "${dict[prefix]}"
     return 0
 }
-
-koopa:::update_pyenv() { # {{{1
-    # """
-    # Update pyenv.
-    # @note Updated 2021-11-24.
-    # """
-    local dict
-    koopa::assert_has_no_args "$#"
-    declare -A dict=(
-        [prefix]="${UPDATE_PREFIX:?}"
-    )
-    koopa::git_pull "${dict[prefix]}"
-    return 0
-}
