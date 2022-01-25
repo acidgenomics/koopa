a01cfff5aae8ead03e8db13e072420b644cfabd0
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-25 10:44:47 -0500

    Update R devel installer

--- lang/shell/bash/functions/common/install/internal/install-r-devel.sh
+++ lang/shell/bash/functions/common/install/internal/install-r-devel.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# [2021-05-27] Linux success.
-# [2021-05-27] macOS hitting this install error:
+# [2022-01-25] macOS hitting this install error:
 #
-# make[1]: Entering directory '[...]/svn/r/r-devel/build/doc/manual'
+# configure: WARNING: neither inconsolata.sty nor zi4.sty found: PDF vignettes and package manuals will not be rendered optimally
+# make[1]: Entering directory '/private/var/folders/l1/8y8sjzmn15v49jgrqglghcfr0000gn/T/koopa-501-20220125-104003-k6sD2z8bzY/svn/r/r-devel/build/doc/manual'
 # creating RESOURCES
 # creating FAQ
 # creating doc/html/resources.html
-# make[1]: Leaving directory '[...]/svn/r/r-devel/build/doc/manual'
+# make[1]: Leaving directory '/private/var/folders/l1/8y8sjzmn15v49jgrqglghcfr0000gn/T/koopa-501-20220125-104003-k6sD2z8bzY/svn/r/r-devel/build/doc/manual'
 # ERROR: not an svn checkout
 # make: *** [Makefile:107: svnonly] Error 1
 #
@@ -17,22 +17,26 @@
 #
 # Maybe it's the 'rsync-recommended' step not working on macOS?
 
-# FIXME Rework using app/dict approach.
 koopa:::install_r_devel() { # {{{1
     # """
     # Install R-devel.
-    # @note Updated 2021-05-27.
+    # @note Updated 2022-01-25.
     # """
-    local brew_opt brew_prefix conf_args jobs make name prefix repo_url
-    local revision rtop svn
-    prefix="${INSTALL_PREFIX:?}"
-    revision="${INSTALL_VERSION:?}"
-    jobs="$(koopa::cpu_count)"
-    make="$(koopa::locate_make)"
-    svn="$(koopa::locate_svn)"
-    # Keep this configuration in sync with 'koopa:::install_r'.
+    local app conf_args dict
+    koopa::assert_has_no_args "$#"
+    declare -A app=(
+        [make]="$(koopa::locate_make)"
+        [svn]="$(koopa::locate_svn)"
+    )
+    declare -A dict=(
+        [jobs]="$(koopa::cpu_count)"
+        [name]='r-devel'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [repo_url]='https://svn.r-project.org/R'
+        [revision]="${INSTALL_VERSION:?}"
+    )
     conf_args=(
-        "--prefix=${prefix}"
+        "--prefix=${dict[prefix]}"
         '--enable-R-shlib'
         '--enable-memory-profiling'
         '--with-x=no'
@@ -64,8 +68,8 @@ koopa:::install_r_devel() { # {{{1
         # - https://developer.r-project.org/Blog/public/2020/11/02/
         #     will-r-work-on-apple-silicon/index.html
         # - https://bugs.r-project.org/bugzilla/show_bug.cgi?id=18024
-        brew_prefix="$(koopa::homebrew_prefix)"
-        brew_opt="${brew_prefix}/opt"
+        dict[brew_prefix]="$(koopa::homebrew_prefix)"
+        dict[brew_opt]="${dict[brew_prefix]}/opt"
         koopa::activate_homebrew_opt_prefix \
             'gettext' \
             'jpeg' \
@@ -80,54 +84,51 @@ koopa:::install_r_devel() { # {{{1
         koopa::activate_prefix '/usr/local/gfortran'
         koopa::add_to_path_start '/Library/TeX/texbin'
         conf_args+=(
-            "--with-blas=-L${brew_opt}/openblas/lib -lopenblas"
-            "--with-tcl-config=${brew_opt}/tcl-tk/lib/tclConfig.sh"
-            "--with-tk-config=${brew_opt}/tcl-tk/lib/tkConfig.sh"
+            "--with-blas=-L${dict[brew_opt]}/openblas/lib -lopenblas"
+            "--with-tcl-config=${dict[brew_opt]}/tcl-tk/lib/tclConfig.sh"
+            "--with-tk-config=${dict[brew_opt]}/tcl-tk/lib/tkConfig.sh"
             '--without-aqua'
         )
         export CFLAGS='-Wno-error=implicit-function-declaration'
     fi
-    name='r-devel'
-    # Set the R source code repo URL.
-    repo_url='https://svn.r-project.org/R'
     # Set the desired top-level directory structure.
-    rtop="${PWD}/svn/r"
+    dict[rtop]="$(koopa::init_dir 'svn/r')"
     # Create necessary build directories.
-    koopa::mkdir "${rtop}/${name}/build"
+    koopa::mkdir "${dict[rtop]}/${dict[name]}/build"
     # Check out the latest revision of R-devel.
-    koopa::cd "$rtop"
-    "$svn" checkout \
-        --revision="$revision" \
-        "${repo_url}/trunk" \
-        "${name}/source"
+    koopa::cd "${dict[rtop]}"
+    "${app[svn]}" checkout \
+        --revision="${dict[revision]}" \
+        "${dict[repo_url]}/trunk" \
+        "${dict[name]}/source"
     # > koopa::cd "${rtop}/${name}/source"
     # Draft version information is located in 'VERSION' file.
     # How to ensure that repo is up-to-date.
-    # > "$svn" update
+    # > "${app[svn]}" update
     # Here's how to switch to a different revision inside the source trunk.
     # Somewhere between 80100 and 80200 the version was changed.
-    # > "$svn" checkout \
+    # > "${app[svn]}" checkout \
     # >     --revision='80190' \
-    # >     "${repo_url}/trunk" \
+    # >     "${dict[repo_url]}/trunk" \
     # >     .
     # Get the sources of the recommended packages.
-    koopa::cd "${rtop}/${name}/source/tools"
+    koopa::cd "${dict[rtop]}/${dict[name]}/source/tools"
     ./rsync-recommended
     # Ready to build from source.
-    koopa::cd "${rtop}/${name}/build"
-    koopa::activate_openjdk
-    unset -v R_HOME
+    koopa::cd "${dict[rtop]}/${dict[name]}/build"
     export TZ='America/New_York'
+    unset -v R_HOME
+    koopa::activate_openjdk
     # We build in the separate directory created above,
     # in order not to pollute the source code.
     ../source/configure "${conf_args[@]}"
-    "$make" --jobs="$jobs"
-    # > "$make" check
-    "$make" pdf
-    "$make" info
-    "$make" install
-    r="${prefix}/bin/R"
-    koopa::assert_is_file "$r"
-    koopa::configure_r "$r"
+    "${app[make]}" --jobs="${dict[jobs]}"
+    # > "${app[make]}" check
+    "${app[make]}" pdf
+    "${app[make]}" info
+    "${app[make]}" install
+    app[r]="${dict[prefix]}/bin/R"
+    koopa::assert_is_installed "${app[r]}"
+    koopa::configure_r "${app[r]}"
     return 0
 }
