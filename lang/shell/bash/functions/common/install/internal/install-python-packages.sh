fc24346c81aef05c49aa174962d92347bd08f3d2
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-26 12:53:43 -0500

    Simplify language-specific package installation

--- lang/shell/bash/functions/common/install/internal/install-python-packages.sh
+++ lang/shell/bash/functions/common/install/internal/install-python-packages.sh
@@ -3,37 +3,34 @@
 koopa:::install_python_packages() { # {{{1
     # """
     # Install Python packages.
-    # @note Updated 2021-12-07.
+    # @note Updated 2022-01-26.
     # """
     local pkgs
-    pkgs=("$@")
-    if [[ "${#pkgs[@]}" -eq 0 ]]
-    then
-        # Install essential defaults first.
-        pkgs=(
-            'pip'
-            'setuptools'
-            'wheel'
-        )
-        readarray -t pkgs <<< "$(koopa::python_get_pkg_versions "${pkgs[@]}")"
-        koopa::alert_info 'Ensuring essential defaults are version pinned.'
-        koopa::python_pip_install "${pkgs[@]}"
-        # Now we can install additional recommended extras.
-        pkgs=(
-            'black'
-            'bpytop'
-            'flake8'
-            'glances'
-            'pip2pi'
-            'pipx'
-            'pyflakes'
-            'pylint'
-            'pynvim'
-            'pytaglib'
-            'pytest'
-            'ranger-fm'
-        )
-    fi
+    koopa::assert_has_no_args "$#"
+    # Install essential defaults first.
+    pkgs=(
+        'pip'
+        'setuptools'
+        'wheel'
+    )
+    readarray -t pkgs <<< "$(koopa::python_get_pkg_versions "${pkgs[@]}")"
+    koopa::alert_info 'Ensuring essential defaults are version pinned.'
+    koopa::python_pip_install "${pkgs[@]}"
+    # Now we can install additional recommended extras.
+    pkgs=(
+        'black'
+        'bpytop'
+        'flake8'
+        'glances'
+        'pip2pi'
+        'pipx'
+        'pyflakes'
+        'pylint'
+        'pynvim'
+        'pytaglib'
+        'pytest'
+        'ranger-fm'
+    )
     readarray -t pkgs <<< "$(koopa::python_get_pkg_versions "${pkgs[@]}")"
     koopa::python_pip_install "${pkgs[@]}"
     return 0
--- lang/shell/bash/functions/common/install/internal/install-ruby-packages.sh
+++ lang/shell/bash/functions/common/install/internal/install-ruby-packages.sh
@@ -3,35 +3,28 @@
 koopa:::install_ruby_packages() { # {{{1
     # """
     # Install Ruby packages (gems).
-    # @note Updated 2022-01-25.
+    # @note Updated 2022-01-26.
     #
     # @seealso
     # - https://bundler.io/man/bundle-pristine.1.html
     # - https://www.justinweiss.com/articles/3-quick-gem-tricks/
     # """
     local app dict gem gems
+    koopa::assert_has_no_args "$#"
     koopa::configure_ruby
     koopa::activate_ruby
     declare -A app=(
         [gem]="$(koopa::locate_gem)"
     )
     declare -A dict=(
-        [default]=0
         [gemdir]="$("${app[gem]}" environment 'gemdir')"
     )
-    if [[ "$#" -eq 0 ]]
-    then
-        dict[default]=1
-        gems=(
-            # > 'neovim'
-            'bundler'
-            'bashcov'
-            'ronn'
-        )
-    else
-        dict[default]=0
-        gems=("$@")
-    fi
+    gems=(
+        # > 'neovim'
+        'bundler'
+        'bashcov'
+        'ronn'
+    )
     koopa::dl \
         'Target' "${dict[gemdir]}" \
         'Gems' "$(koopa::to_string "${gems[@]}")"
@@ -39,22 +32,16 @@ koopa:::install_ruby_packages() { # {{{1
     then
         koopa::activate_homebrew_opt_prefix 'libffi'
     fi
-    if [[ "${dict[default]}" -eq 1 ]]
-    then
-        "${app[gem]}" cleanup
-        "${app[gem]}" pristine --all
-        # > if koopa::is_shared_install
-        # > then
-        # >     "${app[gem]}" update --system
-        # > fi
-    fi
+    "${app[gem]}" cleanup
+    "${app[gem]}" pristine --all
+    # > if koopa::is_shared_install
+    # > then
+    # >     "${app[gem]}" update --system
+    # > fi
     for gem in "${gems[@]}"
     do
         "${app[gem]}" install "$gem"
     done
-    if [[ "${dict[default]}" -eq 1 ]]
-    then
-        "${app[gem]}" cleanup
-    fi
+    "${app[gem]}" cleanup
     return 0
 }
