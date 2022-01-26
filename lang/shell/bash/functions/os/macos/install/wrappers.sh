755e305c8b6d06fade2bf05d50ae2db66b0eece0
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-26 16:38:42 -0500

    Consolidate wrappers

--- lang/shell/bash/functions/os/macos/install/wrappers.sh
+++ lang/shell/bash/functions/os/macos/install/wrappers.sh
@@ -1,5 +1,44 @@
 #!/usr/bin/env bash
 
+koopa::macos_install_python_framework() { # {{{1
+    koopa::install_app \
+        --installer='python-framework' \
+        --name-fancy='Python framework' \
+        --name='python' \
+        --platform='macos' \
+        --system \
+        "$@"
+}
+
+koopa::macos_install_r_cran_gfortran() { # {{{1
+    koopa::install_app \
+        --name-fancy='R CRAN gfortran' \
+        --name='r-cran-gfortran' \
+        --platform='macos' \
+        --prefix="$(koopa::macos_gfortran_prefix)" \
+        --system \
+        "$@"
+}
+
+koopa::macos_install_r_framework() { # {{{1
+    koopa::install_app \
+        --installer='r-framework' \
+        --name-fancy='R framework' \
+        --name='r' \
+        --platform='macos' \
+        --system \
+        "$@"
+}
+
+koopa::macos_install_xcode_clt() { # {{{1
+    koopa::install_app \
+        --name-fancy='Xcode Command Line Tools (CLT)' \
+        --name='xcode-clt' \
+        --platform='macos' \
+        --system \
+        "$@"
+}
+
 koopa::macos_uninstall_adobe_creative_cloud() { # {{{1
     koopa::uninstall_app \
         --name-fancy='Adobe Creative Cloud' \
@@ -79,6 +118,15 @@ koopa::macos_uninstall_xcode_clt() { # {{{1
     koopa::uninstall_app \
         --name-fancy='Xcode Command Line Tools (CLT)' \
         --name='xcode-clt' \
+        --platform='macos' \
+        --system \
+        "$@"
+}
+
+koopa::macos_update_google_cloud_sdk() { # {{{1
+    koopa::update_app \
+        --name-fancy='Google Cloud SDK' \
+        --name='google-cloud-sdk' \
         --platform='macos' \
         --system \
         "$@"
