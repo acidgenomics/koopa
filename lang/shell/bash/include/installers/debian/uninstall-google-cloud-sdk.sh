38ac7c7baedf4b0f55e8e1a28aa91c10d2d24137
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-27 17:46:08 -0500

    Improve wrapping of Debian installers

---
+++ lang/shell/bash/include/installers/debian/uninstall-google-cloud-sdk.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+koopa:::debian_uninstall_google_cloud_sdk() { # {{{1
+    # """
+    # Uninstall Google Cloud SDK.
+    # @note Updated 2022-01-27.
+    # """
+    koopa::assert_has_no_args "$#"
+    koopa::debian_apt_remove 'google-cloud-sdk'
+    return 0
+}
