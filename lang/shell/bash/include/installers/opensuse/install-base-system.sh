521c2a48c52dbe220a64d00657787d0826a7ff76
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-27 16:27:05 -0500

    Clean up distro-specific functions

--- lang/shell/bash/include/installers/opensuse/install-base-system.sh
+++ lang/shell/bash/include/installers/opensuse/install-base-system.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-koopa::opensuse_install_base_system() { # {{{1
-    koopa::install_app \
-        --name-fancy='openSUSE base system' \
-        --name='base-system' \
-        --platform='opensuse' \
-        --system \
-        "$@"
-}
-
 koopa:::opensuse_install_base_system() { # {{{1
     # """
     # Install openSUSE base system.
