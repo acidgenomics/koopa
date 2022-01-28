74a1ebf862740b020cceec3dba8602019e165b2b
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-28 15:40:22 -0500

    Draft update to RStudio Server

--- lang/shell/bash/include/installers/linux/install-rstudio-server.sh
+++ lang/shell/bash/include/installers/linux/install-rstudio-server.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME Rework using app/dict approach.
-# FIXME Rework the 'install' variable approach handling here.
-
 koopa:::linux_install_rstudio_server() { # {{{1
     # """
     # Install RStudio Server.
-    # @note Updated 2022-01-27.
+    # @note Updated 2022-01-28.
     #
     # RStudio Server Pro was renamed to Workbench in 2021-06.
     #
@@ -27,20 +24,19 @@ koopa:::linux_install_rstudio_server() { # {{{1
     # - https://hub.docker.com/r/rocker/rstudio/dockerfile
     # - https://github.com/rocker-org/rocker-versioned/tree/master/rstudio
     # """
-    local file install server dict pos url
+    local app dict install
     declare -A app=(
         [r]="$(koopa::locate_r)"
     )
     declare -A dict=(
         [file_ext]=''
         [install]=''
-        [name]='rstudio-server'
+        [name]="${INSTALL_NAME:?}"
         [os_codename]=''
         [platform]=''
-        [version]=''
+        [version]="${INSTALL_VERSION:?}"
         [workbench]=0
     )
-    pos=()
     while (("$#"))
     do
         case "$1" in
@@ -85,58 +81,33 @@ koopa:::linux_install_rstudio_server() { # {{{1
                 dict[version]="${2:?}"
                 shift 2
                 ;;
-            # Flags ------------------------------------------------------------
-           ' --pro' | \
-            '--workbench')
-                dict[workbench]=1
-                shift 1
-                ;;
             # Other ------------------------------------------------------------
-            '-'*)
-                koopa::invalid_arg "$1"
-                ;;
             *)
-                pos+=("$1")
-                shift 1
+                koopa::invalid_arg "$1"
                 ;;
         esac
     done
-    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
-    koopa::assert_has_no_args "$#"
-    if [[ "${dict[workbench]}" -eq 1 ]]
-    then
-        dict[name]='rstudio-workbench'
-    fi
+    koopa::assert_is_set \
+        '--file-ext' "${dict[file_ext]}" \
+        '--install' "${dict[install]}" \
+        '--name' "${dict[name]}" \
+        '--os-codename' "${dict[os_codename]}" \
+        '--platform' "${dict[platform]}" \
+        '--version' "${dict[version]}"
     dict[file_stem]="${dict[name]}"
     if koopa::is_fedora_like
     then
         dict[file_stem]="${dict[file_stem]}-rhel"
     fi
-    if [[ -z "${dict[version]}" ]]
-    then
-        dict[version]="$(koopa::variable "${dict[name]}")"
-    fi
     koopa::add_to_path_start "$(koopa::dirname "${app[r]}")"
-    file="${dict[file_stem]}-${dict[version]}-\
+    dict[file]="${dict[file_stem]}-${dict[version]}-\
 ${dict[platform]}.${dict[file_ext]}"
-    server='download2.rstudio.org'
-    url="https://${server}/server/${dict[os_codename]}/\
-${dict[platform]}/${file}"
+    dict[url]="https://download2.rstudio.org/server/${dict[os_codename]}/\
+${dict[platform]}/${dict[file]}"
     # Ensure '+' gets converted to '%2B'.
-    url="$(koopa::gsub '\+' '%2B' "$url")"
-    koopa::download "$url" "$file"
+    dict[url]="$(koopa::gsub '\+' '%2B' "${dict[url]}")"
+    koopa::download "${dict[url]}" "${dict[file]}"
     IFS=' ' read -r -a install <<< "${dict[install]}"
-    "${install[@]}" "$file"
-    if [[ "${dict[workbench]}" -eq 1 ]]
-    then
-        cat << END
-Activate product license key (if necessary):
-> sudo rstudio-server license-manager activate <product-key>
-
-If you want to move your license of RStudio Server to another system you should
-first deactivate it on the old system with the command:
-> sudo rstudio-server license-manager deactivate
-END
-    fi
+    "${install[@]}" "${dict[file]}"
     return 0
 }
