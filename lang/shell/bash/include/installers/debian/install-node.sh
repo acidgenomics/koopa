6d8e1a8a6bedc8354670338983c663dd92065183
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-28 12:37:27 -0500

    Update Node installer

--- lang/shell/bash/include/installers/debian/install-node.sh
+++ lang/shell/bash/include/installers/debian/install-node.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
 
-# FIXME Need to wrap this.
-koopa::debian_install_node() { # {{{1
+koopa:::debian_install_node() { # {{{1
     # """
     # Install Node.js for Debian using NodeSource.
-    # @note Updated 2021-09-20.
+    # @note Updated 2022-01-28.
     #
     # This will configure apt at '/etc/apt/sources.list.d/nodesource.list'.
     # """
-    local file name_fancy tmp_dir url version
-    name_fancy='Node.js'
-    koopa::alert_install_start "$name_fancy"
-    version="$(koopa::get_version 'node')"
-    version="$(koopa::major_version "$version")"
-    tmp_dir="$(koopa::tmp_dir)"
-    (
-        koopa::cd "$tmp_dir"
-        url="https://deb.nodesource.com/setup_${version}.x"
-        file='setup.sh'
-        koopa::download "$url" "$file"
-        koopa::chmod 'u+x' "$file"
-        sudo "./${file}"
+    local app dict
+    koopa::assert_has_no_args "$#"
+    declare -A app=(
+        [sudo]="$(koopa::locate_sudo)"
     )
-    koopa::rm "$tmp_dir"
+    declare -A dict=(
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[maj_ver]="$(koopa::major_version "${dict[version]}")"
+    dict[url]="https://deb.nodesource.com/setup_${dict[maj_ver]}.x"
+    dict[file]='setup.sh'
+    koopa::download "${dict[url]}" "${dict[file]}"
+    koopa::chmod 'u+x' "${dict[file]}"
+    "${app[sudo]}" "./${dict[file]}"
     koopa::debian_apt_install 'nodejs'
-    koopa::alert_install_success "$name_fancy"
     return 0
 }
