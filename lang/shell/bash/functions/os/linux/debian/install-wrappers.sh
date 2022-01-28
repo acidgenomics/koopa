62e500e4d1bd82404e63b355a01388d0561cae9d
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-28 14:46:02 -0500

    Update installers

--- lang/shell/bash/functions/os/linux/debian/install-wrappers.sh
+++ lang/shell/bash/functions/os/linux/debian/install-wrappers.sh
@@ -9,6 +9,15 @@ koopa::debian_install_azure_cli() { # {{{1
         "$@"
 }
 
+koopa::debian_install_base_system() { # {{{1
+    koopa::install_app \
+        --name-fancy='Debian base system' \
+        --name='base-system' \
+        --platform='debian' \
+        --system \
+        "$@"
+}
+
 koopa::debian_install_bcbio_nextgen_vm() { # {{{1
     koopa::install_app \
         --name='bcbio-nextgen-vm' \
@@ -53,12 +62,30 @@ koopa::debian_install_node() { # {{{1
         "$@"
 }
 
+koopa::debian_install_pandoc() { # {{{1
+    koopa::install_app \
+        --name-fancy='Pandoc' \
+        --name='pandoc' \
+        --platform='debian' \
+        --system \
+        "$@"
+}
+
 koopa::debian_install_r_devel() { # {{{1
     koopa::install_app \
         --name-fancy='R-devel' \
         --name='r-devel' \
         --no-link \
         --platform='debian' \
+        "$@"
+}
+
+koopa::debian_install_wine() { # {{{1
+    koopa::install_app \
+        --name-fancy='Wine' \
+        --name='wine' \
+        --platform='debian' \
+        --system \
         "$@"
 }
 
@@ -105,6 +132,15 @@ koopa::debian_uninstall_llvm() { # {{{1
         "$@"
 }
 
+koopa::debian_uninstall_pandoc() { # {{{1
+    koopa::uninstall_app \
+        --name-fancy='Pandoc' \
+        --name='pandoc' \
+        --platform='debian' \
+        --system \
+        "$@"
+}
+
 koopa::debian_uninstall_r_devel() { # {{{1
     koopa::uninstall_app \
         --name-fancy='R-devel' \
@@ -118,6 +154,15 @@ koopa::debian_uninstall_shiny_server() { # {{{1
     koopa::uninstall_app \
         --name-fancy='Shiny Server' \
         --name='shiny-server' \
+        --platform='debian' \
+        --system \
+        "$@"
+}
+
+koopa::debian_uninstall_wine() { # {{{1
+    koopa::uninstall_app \
+        --name-fancy='Wine' \
+        --name='wine' \
         --platform='debian' \
         --system \
         "$@"
---
+++ lang/shell/bash/include/installers/debian/uninstall-pandoc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+koopa:::debian_uninstall_pandoc() { # {{{1
+    # """
+    # Uninstall Pandoc.
+    # @note Updated 2021-11-02.
+    # May not need (or want) to install 'pandoc-data' here.
+    # """
+    koopa::assert_has_no_args "$#"
+    koopa::debian_apt_remove 'pandoc' 'pandoc-data'
+    return 0
+}
