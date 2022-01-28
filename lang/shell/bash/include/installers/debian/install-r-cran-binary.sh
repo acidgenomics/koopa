26e3f8d5b98b658400fa5801cdb020aca1abf81d
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-28 15:24:42 -0500

    Reorganize Linux distros

--- lang/shell/bash/include/installers/debian/install-r-cran-binary.sh
+++ lang/shell/bash/include/installers/debian/install-r-cran-binary.sh
@@ -28,18 +28,3 @@ koopa:::debian_install_r_cran_binary() { # {{{1
     koopa::configure_r "${app[r]}"
     return 0
 }
-
-koopa:::debian_uninstall_r_cran_binary() { # {{{1
-    # """
-    # Uninstall R CRAN binary.
-    # @note Updated 2022-01-28.
-    # """
-    koopa::assert_has_no_args "$#"
-    koopa::assert_is_admin
-    koopa::rm --sudo \
-        '/etc/R' \
-        '/usr/lib/R/etc'
-    koopa::debian_apt_remove 'r-*'
-    koopa::debian_apt_delete_repo 'r'
-    return 0
-}
