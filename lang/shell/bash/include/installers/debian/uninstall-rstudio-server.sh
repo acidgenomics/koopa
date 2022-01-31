6a5f41624cc665e13051d046429b55fe0b5c7f4d
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-31 09:20:08 -0500

    Improve RStudio installers

--- lang/shell/bash/include/installers/debian/uninstall-rstudio-server.sh
+++ lang/shell/bash/include/installers/debian/uninstall-rstudio-server.sh
@@ -4,6 +4,8 @@ koopa:::debian_uninstall_rstudio_server() { # {{{1
     # """
     # Uninstall RStudio Server.
     # @note Updated 2021-06-14.
+    #
+    # Consider deleting 'rstudio-server' user.
     # """
     koopa::assert_has_no_args "$#"
     koopa::assert_is_admin
