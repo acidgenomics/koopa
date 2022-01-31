9a1d27808a184b4da126544265630acaa4a34084
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-31 09:20:08 -0500

    Improve RStudio installers

--- lang/shell/bash/include/installers/debian/uninstall-shiny-server.sh
+++ lang/shell/bash/include/installers/debian/uninstall-shiny-server.sh
@@ -4,6 +4,8 @@ koopa:::debian_uninstall_shiny_server() { # {{{1
     # """
     # Uninstall Shiny Server.
     # @note Updated 2021-06-14.
+    #
+    # Consider deleting 'shiny' user.
     # """
     koopa::assert_has_no_args "$#"
     koopa::assert_is_admin
