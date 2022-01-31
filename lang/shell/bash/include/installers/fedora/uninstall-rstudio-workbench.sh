df34e22cfbd86e7b9b96f67f7c5112881de45ca4
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-31 09:12:16 -0500

    Work on improving support for RStudio Workbench

--- lang/shell/bash/include/installers/fedora/uninstall-rstudio-workbench.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-koopa:::fedora_uninstall_rstudio_workbench() { # {{{1
-    # """
-    # Uninstall RStudio Workbench.
-    # @note Updated 2022-01-31.
-    # """
-    koopa:::fedora_uninstall_rstudio_server "$@"
-}
