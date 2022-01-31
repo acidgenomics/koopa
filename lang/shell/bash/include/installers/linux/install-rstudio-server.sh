dfb7a30057d0a3b2d810d3cde0d598e104ddd7fc
Michael Steinbaugh <mike@steinbaugh.com>
2022-01-31 14:26:26 -0500

    Add notes on how to improve installer

--- lang/shell/bash/include/installers/linux/install-rstudio-server.sh
+++ lang/shell/bash/include/installers/linux/install-rstudio-server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Improve this installer by informing the user how to connect by default.
+
 koopa:::linux_install_rstudio_server() { # {{{1
     # """
     # Install RStudio Server.
