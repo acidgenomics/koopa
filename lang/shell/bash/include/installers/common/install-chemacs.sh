841d6b047f5a1fb49b3d79c1e04abc4076e3c6f8
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-03 14:25:43 -0500

    Harden our installers

--- lang/shell/bash/include/installers/common/install-chemacs.sh
+++ lang/shell/bash/include/installers/common/install-chemacs.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME This is warning about '/opt/koopa/opt/chemacs' not existing, need
-# to rethink this?
-#
-# FIXME Work on resolving this warning:
-# 🧪 # Installing Chemacs at '/opt/koopa/app/chemacs/rolling'.
-# !! Warning: Does not exist: '/opt/koopa/opt/chemacs'.
-# → Deleting '/opt/koopa/app/chemacs/rolling/.git/branches'.
-
 koopa:::install_chemacs() { # {{{1
     # """
     # Install Chemacs2.
