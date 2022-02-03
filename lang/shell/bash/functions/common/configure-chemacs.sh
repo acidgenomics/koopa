39faaffa60ae32c77e4a8ad02746247772656a74
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-03 14:18:50 -0500

    Resolve FIXMEs

--- lang/shell/bash/functions/common/configure-chemacs.sh
+++ lang/shell/bash/functions/common/configure-chemacs.sh
@@ -3,8 +3,8 @@
 koopa::configure_chemacs() { # {{{1
     # """
     # Configure chemacs.
-    # @note Updated 2022-02-01.
+    # @note Updated 2022-02-03.
     # """
-    koopa::link_dotfile --force --opt 'chemacs'
+    koopa::link_dotfile --opt --overwrite 'chemacs'
     return 0
 }
