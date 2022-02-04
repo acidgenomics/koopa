38e63eddc43b24ec5b6958f88733d8c2049caf3b
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-04 09:42:03 -0500

    Add support for '--delete' flag

--- lang/shell/bash/functions/common/fs/compress.sh
+++ lang/shell/bash/functions/common/fs/compress.sh
@@ -1,18 +1,44 @@
 #!/usr/bin/env bash
 
-# FIXME Add option to delete input directories with '--delete' flag?
-
 koopa::tar_multiple_dirs() { # {{{1
     # """
     # Compress (tar) multiple directories in a single call.
     # @note Updated 2022-02-04.
     # """
-    local app dir dirs
+    local app dict dir dirs pos
     koopa::assert_has_args "$#"
-    koopa::assert_is_dir "$@"
     declare -A app=(
         [tar]="$(koopa::locate_tar)"
     )
+    declare -A dict=(
+        [delete]=0
+    )
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            # Flags ------------------------------------------------------------
+            '--delete')
+                dict[delete]=1
+                shift 1
+                ;;
+            '--no-delete' | \
+            '--keep')
+                dict[delete]=0
+                shift 1
+                ;;
+            # Other ------------------------------------------------------------
+            '-'*)
+                koopa::invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa::assert_is_dir "$@"
     readarray -t dirs <<< "$(koopa::realpath "$@")"
     (
         for dir in "${dirs[@]}"
@@ -22,6 +48,7 @@ koopa::tar_multiple_dirs() { # {{{1
             koopa::alert "Compressing '${dir}'."
             koopa::cd "$(koopa::dirname "$dir")"
             "${app[tar]}" -czf "${bn}.tar.gz" "${bn}/"
+            [[ "${dict[delete]}" -eq 1 ]] && koopa::rm "$dir"
         done
     )
     return 0
