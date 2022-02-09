f1e941fe1cdee173d6faa8df373856a4a07b07f2
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-09 07:43:47 -0500

    Rework Ruby package install config

--- lang/shell/bash/include/installers/common/install-ruby-packages.sh
+++ lang/shell/bash/include/installers/common/install-ruby-packages.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# FIXME Now seeing this error with Ruby 3.1:
-# Building native extensions. This could take a while...
-# ERROR:  While executing gem ... (Errno::EACCES)
-#     Permission denied @ rb_sysopen - /usr/local/Cellar/ruby/3.1.0/lib/ruby/gems/3.1.0/specifications/debug-1.4.0.gemspec
-
 koopa:::install_ruby_packages() { # {{{1
     # """
     # Install Ruby packages (gems).
-    # @note Updated 2022-01-26.
+    # @note Updated 2022-02-09.
     #
     # @seealso
+    # - 'gem pristine --all'
+    # - 'gem update --system'
     # - https://bundler.io/man/bundle-pristine.1.html
     # - https://www.justinweiss.com/articles/3-quick-gem-tricks/
     # """
@@ -38,11 +35,6 @@ koopa:::install_ruby_packages() { # {{{1
         koopa::activate_homebrew_opt_prefix 'libffi'
     fi
     "${app[gem]}" cleanup
-    "${app[gem]}" pristine --all
-    # > if koopa::is_shared_install
-    # > then
-    # >     "${app[gem]}" update --system
-    # > fi
     for gem in "${gems[@]}"
     do
         "${app[gem]}" install "$gem"
