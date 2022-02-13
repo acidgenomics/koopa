100d003a5c28da103bbd9930e5bd5bb6c7035efd
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-13 17:36:55 -0500

    Add EC2 function placeholders

---
+++ lang/shell/bash/functions/common/app/aws/ec2-terminate-instance.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# FIXME Work on adding support for this.
+koopa::aws_ec2_terminate_instance() { # {{{1
+    # """
+    # Terminate an AWS EC2 instance.
+    # @note Updated 2022-02-11.
+    # """
+    return 0
+}
