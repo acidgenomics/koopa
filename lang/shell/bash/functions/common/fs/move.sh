1fa99d5ef420ae3497d789d3d523e1dcbcbed355
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-16 13:38:02 -0500

    Draft updates

--- lang/shell/bash/functions/common/fs/move.sh
+++ lang/shell/bash/functions/common/fs/move.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/env bash
 
-# FIXME Rework using app/dict approach.
 koopa::move_files_in_batch() { # {{{1
-    # Batch move a limited number of files.
-    # @note Updated 2021-05-24.
     # """
-    local find head mv n sort source_dir target_dir xargs
+    # Batch move a limited number of files.
+    # @note Updated 2022-02-16.
+    #
+    # @examples
+    # koopa::move_files_in_batch 100 'source_dir' 'target_dir'
+    # """
+    local app dict
     koopa::assert_has_args_eq "$#" 3
-    find="$(koopa::locate_find)"
-    head="$(koopa::locate_head)"
-    mv="$(koopa::locate_mv)"
-    sort="$(koopa::locate_sort)"
-    xargs="$(koopa::locate_xargs)"
-    n="${1:?}"
-    source_dir="${2:?}"
-    target_dir="${3:?}"
-    koopa::assert_is_dir "$source_dir" "$target_dir"
-    # FIXME Rework using 'koopa::find'.
-    # FIXME Need to add support for '--regex' in addition to '--glob'.
-    "$find" "$source_dir" \
-        -type f \
-        -regex '.+/[^.].+$' \
-        -print0 \
-        | "$sort" -z \
-        | "$head" -z -n "$n" \
-        | "$xargs" -0 "$mv" -t "$target_dir"
+    declare -A app=(
+        [head]="$(koopa::locate_head)"
+        [mv]="$(koopa::locate_gnu_mv)"
+        [xargs]="$(koopa::locate_xargs)"
+    )
+    declare -A dict=(
+        [num]="${1:?}"
+        [source_dir]="${2:?}"
+        [target_dir]="${3:?}"
+    )
+    [[ ! -d "${dict[target_dir]}" ]] && koopa::mkdir "${dict[target_dir]}"
+    koopa::assert_is_dir "${dict[source_dir]}" "${dict[target_dir]}"
+    koopa::find \
+        --max-depth=1 \
+        --min-depth=1 \
+        --prefix="${dict[source_dir]}" \
+        --print0 \
+        --sort \
+        --type='f' \
+    | "${app[head]}" \
+        --lines="${dict[num]}" \
+        --zero-terminated \
+    | "${app[xargs]}" --null \
+        "${app[mv]}" \
+            --target-directory "${dict[target_dir]}" \
+            --verbose
     return 0
 }
 
@@ -51,9 +62,9 @@ koopa::move_files_up_1_level() { # {{{1
 koopa::move_into_dated_dirs_by_filename() { # {{{1
     # """
     # Move into dated directories by filename.
-    # @note Updated 2021-11-04.
+    # @note Updated 2022-02-16.
     # """
-    local day file grep_array grep_string month subdir year
+    local file grep_array grep_string
     koopa::assert_has_args "$#"
     grep_array=(
         '^([0-9]{4})'
@@ -64,18 +75,23 @@ koopa::move_into_dated_dirs_by_filename() { # {{{1
         '([-_])?'
         '(.+)$'
     )
-    grep_string="$(printf '%s' "${grep_array[@]}" $'\n')"
+    grep_string="$(koopa::paste0 "${grep_array[@]}")"
     for file in "$@"
     do
-        if [[ "$file" =~ $grep_string ]]
+        local dict
+        declare -A dict=(
+            [file]="$file"
+        )
+        # NOTE Don't quote '$grep_string' here.
+        if [[ "${dict[file]}" =~ $grep_string ]]
         then
-            year="${BASH_REMATCH[1]}"
-            month="${BASH_REMATCH[3]}"
-            day="${BASH_REMATCH[5]}"
-            subdir="${year}/${month}/${day}"
-            koopa::mv --target-directory="$subdir" "$file"
+            dict[year]="${BASH_REMATCH[1]}"
+            dict[month]="${BASH_REMATCH[3]}"
+            dict[day]="${BASH_REMATCH[5]}"
+            dict[subdir]="${dict[year]}/${dict[month]}/${dict[day]}"
+            koopa::mv --target-directory="${dict[subdir]}" "${dict[file]}"
         else
-            koopa::stop "Does not contain date: '${file}'."
+            koopa::stop "Does not contain date: '${dict[file]}'."
         fi
     done
     return 0
@@ -84,12 +100,13 @@ koopa::move_into_dated_dirs_by_filename() { # {{{1
 koopa::move_into_dated_dirs_by_timestamp() { # {{{1
     # """
     # Move into dated directories by timestamp.
-    # @note Updated 2021-11-16.
+    # @note Updated 2022-02-16.
     # """
-    local file subdir
+    local file
     koopa::assert_has_args "$#"
     for file in "$@"
     do
+        local subdir
         subdir="$(koopa::stat_modified '%Y/%m/%d' "$file")"
         koopa::mv --target-directory="$subdir" "$file"
     done
