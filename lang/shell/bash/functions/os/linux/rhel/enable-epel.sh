c62cf938492dbb914eb609d98492ce80e660cf60
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-17 14:13:25 -0500

    Harden our epel checker

--- lang/shell/bash/functions/os/linux/rhel/enable-epel.sh
+++ lang/shell/bash/functions/os/linux/rhel/enable-epel.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Check that piping to 'str_detect_fixed' works here now with the
-# change to '--pattern' flag.
-
 koopa::rhel_enable_epel() { # {{{1
     # """
     # Enable Extra Packages for Enterprise Linux (EPEL) for Red Hat
@@ -11,7 +8,7 @@ koopa::rhel_enable_epel() { # {{{1
     # """
     koopa::assert_has_no_args "$#"
     if koopa::fedora_dnf repolist \
-        | koopa::str_detect_fixed - --pattern='epel/'
+        | koopa::str_detect_regex - --pattern='^epel'
     then
         koopa::alert_success 'EPEL is already enabled.'
         return 0
