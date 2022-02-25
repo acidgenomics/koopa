f25d32b01015fb8440445c4ddc1dfa946a2fe9e1
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-25 11:03:42 -0500

    Create wrapper function for help2

--- lang/shell/bash/functions/common/system.sh
+++ lang/shell/bash/functions/common/system.sh
@@ -76,6 +76,25 @@ koopa_help() { # {{{1
     exit 0
 }
 
+koopa_help2() { # {{{1
+    # """
+    # Resolve man file for current script, and call help.
+    # @note Updated 2022-02-25.
+    #
+    # Currently used inside shared Bash header.
+    # """
+    local dict
+    declare -A dict
+    dict[script_file]="$(koopa_realpath "$0")"
+    dict[script_name]="$(koopa_basename "${dict[script_file]}")"
+    dict[man_prefix]="$( \
+        koopa_parent_dir --num=2 "${dict[script_file]}" \
+    )"
+    dict[man_file]="${dict[man_prefix]}/man/\
+man1/${dict[script_name]}.1"
+    koopa_help "${dict[man_file]}"
+}
+
 koopa_info_box() { # {{{1
     # """
     # Configuration information box.
