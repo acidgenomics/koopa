8d82f64fe427ebe63985700fe22291fbfe76c0be
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-28 15:56:41 -0500

    Add utility for monitoring out of control spotlight indexing on mac

--- lang/shell/bash/functions/os/macos/spotlight.sh
+++ lang/shell/bash/functions/os/macos/spotlight.sh
@@ -20,3 +20,19 @@ koopa_macos_spotlight_find() { # {{{1
     koopa_print "$x"
     return 0
 }
+
+koopa_macos_spotlight_usage() { # {{{1
+    # """
+    # Monitor current spotlight indexing usage.
+    # @note Updated 2022-02-28.
+    #
+    # Useful for debugging out of control mds_stores that makes my laptop
+    # sound like a jet engine.
+    # """
+    declare -A app=(
+        [fs_usage]="$(koopa_macos_locate_fs_usage)"
+        [sudo]="$(koopa_locate_sudo)"
+    )
+    "${app[sudo]}" "${app[fs_usage]}" -w -f filesys mds
+    return 0
+}
