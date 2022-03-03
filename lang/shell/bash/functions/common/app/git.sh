ec74c4a0ca37e2e887189ddbc4e5698e8e82948a
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-03 15:16:22 -0500

    Harden our master to main branch renamer

--- lang/shell/bash/functions/common/app/git.sh
+++ lang/shell/bash/functions/common/app/git.sh
@@ -548,6 +548,13 @@ koopa_git_rename_master_to_main() { # {{{1
     # Rename default branch from "master" to "main".
     # @note Updated 2022-03-03.
     #
+    # @seealso
+    # - https://hackernoon.com/how-to-rename-your-git-repositories-
+    #     from-master-to-main-6i1u3wsu
+    # - https://www.hanselman.com/blog/easily-rename-your-git-default-branch-
+    #     from-master-to-main
+    # - https://www.git-tower.com/learn/git/faq/git-rename-master-to-main
+    #
     # @examples
     # > koopa_git_rename_master_to_main "${HOME}/git/example"
     # """
@@ -571,18 +578,34 @@ koopa_git_rename_master_to_main() { # {{{1
         do
             koopa_cd "$repo"
             koopa_assert_is_git_repo
+            # Switch to the old branch.
+            "${app[git]}" switch "${dict[old_branch]}"
+            # Rename (move) to the new branch.
             "${app[git]}" branch --move \
                 "${dict[old_branch]}" \
                 "${dict[new_branch]}"
+            # Switch to the new branch.
             "${app[git]}" switch "${dict[new_branch]}"
-            "${app[git]}" fetch "${dict[origin]}"
+            # Get the latest comments (and branches) from the remote.
+            "${app[git]}" fetch --all --prune "${dict[origin]}"
+            # Remove the existing tracking connection.
+            "${app[git]}" branch --unset-upstream
+            # Create a new tracking connection.
             "${app[git]}" branch \
                 --set-upstream-to="${dict[origin]}/${dict[new_branch]}" \
                 "${dict[new_branch]}"
-            "${app[git]}" remote set-head "${dict[origin]}" --auto
+            # Push the renamed branch to remote.
             "${app[git]}" push --set-upstream \
                 "${dict[origin]}" \
                 "${dict[new_branch]}"
+            # Delete the old branch from remote. This may fail if branch is
+            # protected on the remote platform.
+            "${app[git]}" push \
+                "${dict[origin]}" \
+                --delete "${dict[old_branch]}" \
+                || true
+            # Set the remote HEAD.
+            "${app[git]}" remote set-head "${dict[origin]}" --auto
         done
     )
     return 0
