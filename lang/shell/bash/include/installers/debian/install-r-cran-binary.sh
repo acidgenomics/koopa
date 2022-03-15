1f8cfa3e29b5e2ff382f29519f675c85e40b78c8
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-15 10:45:16 -0400

    Don't remove existing directories with our installer

--- lang/shell/bash/include/installers/debian/install-r-cran-binary.sh
+++ lang/shell/bash/include/installers/debian/install-r-cran-binary.sh
@@ -3,11 +3,14 @@
 debian_install_r_cran_binary() { # {{{1
     # """
     # Install latest version of R from CRAN.
-    # @note Updated 2022-01-28.
+    # @note Updated 2022-03-15.
     #
     # In case of missing files in '/etc/R', such as ldpaths or Makeconf:
     # > sudo apt purge r-base-core
     # > sudo apt install r-base-core
+    #
+    # For additional cleanup, consider removing '/etc/R', '/usr/lib/R',
+    # and '/usr/local/lib/R'.
     #
     # @seealso
     # - https://cran.r-project.org/bin/linux/debian/
@@ -22,10 +25,12 @@ debian_install_r_cran_binary() { # {{{1
     declare -A dict=(
         [version]="${INSTALL_VERSION:?}"
     )
-    koopa_rm --sudo \
-        '/etc/R' \
-        '/usr/lib/R/etc' \
-        '/usr/local/lib/R'
+    # These removal steps will mess up existing installation, unless we run
+    # 'sudo apt purge r-base-core' first.
+    # > koopa_rm --sudo \
+    # >     '/etc/R' \
+    # >     '/usr/lib/R/etc' \
+    # >     '/usr/local/lib/R'
     koopa_debian_apt_add_r_repo "${dict[version]}"
     pkgs=('r-base' 'r-base-dev')
     koopa_debian_apt_install "${pkgs[@]}"
