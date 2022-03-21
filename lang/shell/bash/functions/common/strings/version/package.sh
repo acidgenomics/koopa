72184fbb9fe5bdf785f5ceee892b2ba41ffc2867
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-21 15:50:39 -0400

    Improve parameterization

--- lang/shell/bash/functions/common/strings/version/package.sh
+++ lang/shell/bash/functions/common/strings/version/package.sh
@@ -1,70 +1,75 @@
 #!/usr/bin/env bash
 
-# FIXME Need to parameterize.
 koopa_github_latest_release() { # {{{1
     # """
     # Get the latest release version from GitHub.
-    # @note Updated 2022-02-27.
+    # @note Updated 2022-03-21.
     #
     # @examples
     # > koopa_github_latest_release 'acidgenomics/koopa'
     # """
-    local app repo str url
+    local app repo
     koopa_assert_has_args "$#"
     declare -A app=(
         [cut]="$(koopa_locate_cut)"
         [sed]="$(koopa_locate_sed)"
     )
-    repo="${1:?}"
-    url="https://api.github.com/repos/${repo}/releases/latest"
-    str="$( \
-        koopa_parse_url "$url" \
-            | koopa_grep --pattern='"tag_name":' \
-            | "${app[cut]}" --delimiter='"' --fields='4' \
-            | "${app[sed]}" 's/^v//' \
-    )"
-    [[ -n "$str" ]] || return 1
-    koopa_print "$str"
+    for repo in "$@"
+    do
+        local dict
+        declare -A dict
+        dict[repo]="$repo"
+        dict[url]="https://api.github.com/repos/${dict[repo]}/releases/latest"
+        dict[str]="$( \
+            koopa_parse_url "${dict[url]}" \
+                | koopa_grep --pattern='"tag_name":' \
+                | "${app[cut]}" --delimiter='"' --fields='4' \
+                | "${app[sed]}" 's/^v//' \
+        )"
+        [[ -n "${dict[str]}" ]] || return 1
+        koopa_print "${dict[str]}"
+    done
     return 0
 }
 
-# FIXME Need to parameterize.
 koopa_node_package_version() { # {{{1
     # """
     # Node (NPM) package version.
-    # @note Updated 2022-03-15.
+    # @note Updated 2022-03-21.
     #
     # @seealso
     # - https://stackoverflow.com/questions/10972176/
     #
     # @examples
-    # > koopa_node_package_version 'gtop'
+    # > koopa_node_package_version 'npm'
     # """
-    local app dict
-    koopa_assert_has_args_eq "$#" 1
+    local app pkg
+    koopa_assert_has_args "$#"
     declare -A app=(
         [jq]="$(koopa_locate_jq)"
         [npm]="$(koopa_locate_npm)"
     )
-    declare -A dict=(
-        [pkg_name]="${1:?}"
-    )
-    dict[str]="$( \
-        "${app[npm]}" --global --json list "${dict[pkg_name]}" \
-        | "${app[jq]}" \
-            --raw-output \
-            ".dependencies.${dict[pkg_name]}.version" \
-    )"
-    [[ -n "${dict[str]}" ]] || return 1
-    koopa_print "${dict[str]}"
+    for pkg in "$@"
+    do
+        local dict
+        declare -A dict
+        dict[pkg]="$pkg"
+        dict[str]="$( \
+            "${app[npm]}" --global --json list "${dict[pkg]}" \
+            | "${app[jq]}" \
+                --raw-output \
+                ".dependencies.${dict[pkg_name]}.version" \
+        )"
+        [[ -n "${dict[str]}" ]] || return 1
+        koopa_print "${dict[str]}"
+    done
     return 0
 }
 
-# FIXME Need to parameterize.
 koopa_perl_package_version() { # {{{1
     # """
     # Perl package version.
-    # @note Updated 2022-03-18.
+    # @note Updated 2022-03-21.
     #
     # @seealso
     # - https://www.perl.com/article/1/2013/3/24/3-quick-ways-to-find-out-the-
@@ -75,51 +80,56 @@ koopa_perl_package_version() { # {{{1
     # > koopa_perl_package_version 'File::Rename'
     # # 1.30
     # """
-    local app dict
-    koopa_assert_has_args_eq "$#" 1
+    local app pkg
+    koopa_assert_has_args "$#"
     declare -A app=(
         [perl]="$(koopa_locate_perl)"
     )
-    declare -A dict=(
-        [pkg]="${1:?}"
-    )
-    # Note that there cannot be a space after '-M' here.
-    dict[str]="$( \
-        "${app[perl]}" \
-            -M"${dict[pkg]}" \
-            -e "print \$${dict[pkg]}::VERSION .\"\n\";" \
-    )"
-    [[ -n "${dict[str]}" ]] || return 1
-    koopa_print "${dict[str]}"
+    for pkg in "$@"
+    do
+        local dict
+        declare -A dict
+        dict[pkg]="$pkg"
+        # Note that there cannot be a space after '-M' here.
+        dict[str]="$( \
+            "${app[perl]}" \
+                -M"${dict[pkg]}" \
+                -e "print \$${dict[pkg]}::VERSION .\"\n\";" \
+        )"
+        [[ -n "${dict[str]}" ]] || return 1
+        koopa_print "${dict[str]}"
+    done
     return 0
 }
 
-# FIXME Need to parameterize.
 koopa_python_package_version() { # {{{1
     # """
     # Python package version.
-    # @note Updated 2022-03-18.
+    # @note Updated 2022-03-21.
     #
     # @examples
     # > koopa_python_package_version 'pip'
     # # 22.0.4
     # """
-    local app dict
-    koopa_assert_has_args_eq "$#" 1
+    local app pkg
+    koopa_assert_has_args "$#"
     declare -A app=(
         [cut]="$(koopa_locate_cut)"
         [python]="$(koopa_locate_python)"
     )
-    declare -A dict=(
-        [pkg]="${1:?}"
-    )
-    dict[str]="$( \
-        "${app[python]}" -m pip show "${dict[pkg]}" \
+    for pkg in "$@"
+    do
+        local dict
+        declare -A dict
+        dict[pkg]="$pkg"
+        dict[str]="$( \
+            "${app[python]}" -m pip show "${dict[pkg]}" \
             | koopa_grep --pattern='^Version:' --regex \
             | "${app[cut]}" --delimiter=' ' --fields='2' \
-    )"
-    [[ -n "${dict[str]}" ]] || return 1
-    koopa_print "${dict[str]}"
+        )"
+        [[ -n "${dict[str]}" ]] || return 1
+        koopa_print "${dict[str]}"
+    done
     return 0
 }
 
