a3e80fa51185ca2aee66d23e6bf3aa5ae7651b66
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-23 08:59:52 -0400

    Remove warning regarding log.out

--- lang/shell/bash/functions/common/ngs/align/star.sh
+++ lang/shell/bash/functions/common/ngs/align/star.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Ensure 'Log.out' is maintained with index directory.
-# Seeing this warning: Could not move Log.out file
-
 koopa_star_index() { # {{{1
     # """
     # Create a genome index for STAR aligner.
