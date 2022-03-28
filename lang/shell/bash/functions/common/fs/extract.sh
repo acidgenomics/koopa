288547d4f0fa70b9d184e9af7fd938801ede96ec
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-28 19:29:55 -0400

    Draft support for multi file extraction

--- lang/shell/bash/functions/common/fs/extract.sh
+++ lang/shell/bash/functions/common/fs/extract.sh
@@ -111,3 +111,26 @@ koopa_extract() { # {{{1
     done
     return 0
 }
+
+koopa_extract_all() { # {{{1
+    # """
+    # Extract multiple compressed archives in a single call.
+    # @note Updated 2022-03-28.
+    #
+    # @usage koopa_extract_all FILE...
+    #
+    # @examples
+    # > koopa_extract_all 'file1.tar.bz2' 'file2.tar.gz' 'file3.tar.xz'
+    # """
+    local file
+    koopa_assert_has_args_ge "$#" 2
+    koopa_assert_is_file "$@"
+    for file in "$@"
+    do
+        koopa_assert_is_matching_regex \
+            --pattern='\.tar\.(bz2|gz|xz)$' \
+            --string="$file"
+        koopa_extract "$file"
+    done
+    return 0
+}
