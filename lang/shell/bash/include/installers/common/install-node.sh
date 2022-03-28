273d45e6a867987ba7d35b0f717b443c3066cdc0
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-28 13:09:43 -0400

    Rework installer

---
+++ lang/shell/bash/include/installers/common/install-node.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+install_node() { # {{{1
+    # """
+    # Install Node.js.
+    # @note Updated 2022-03-28.
+    #
+    # @seealso
+    # - https://github.com/nodejs/node
+    # """
+    local app dict
+    koopa_assert_has_no_args "$#"
+    declare -A app=(
+        [make]="$(koopa_locate_make)"
+    )
+    declare -A dict=(
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[file]="v${dict[version]}.tar.gz"
+    dict[url]="https://github.com/nodejs/node/archive/refs/tags/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "node-${dict[version]}"
+    ./configure --prefix="${dict[prefix]}"
+    "${app[make]}"
+    "${app[make]}" test
+    "${app[make]}" install
+    return 0
+}
