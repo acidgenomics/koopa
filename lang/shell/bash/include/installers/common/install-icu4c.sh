771d99c5d8500116c206c10c6b8f0aeae260c8b1
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-28 17:50:56 -0400

    Fix navigation

--- lang/shell/bash/include/installers/common/install-icu4c.sh
+++ lang/shell/bash/include/installers/common/install-icu4c.sh
@@ -26,7 +26,7 @@ install_icu4c() { # {{{1
 release-${dict[kebab_version]}/${dict[file]}"
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
-    koopa_cd 'icu'
+    koopa_cd 'icu/source'
     conf_args=(
         "--prefix=${dict[prefix]}"
         '--disable-samples'
