ee57f051864d8c4c785b701d583acaacfe3d7ed9
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-28 12:04:31 -0400

    Dynamically set shared argument

--- lang/shell/bash/include/installers/common/install-openssl.sh
+++ lang/shell/bash/include/installers/common/install-openssl.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
-# FIXME Now seeing this with install on Debian:
-# ./openssl: error while loading shared libraries: libssl.so.3: cannot open
-# shared object file: No such file or directory
-
 install_openssl() { # {{{1
     # """
     # Install OpenSSL.
-    # @note Updated 2022-01-03.
+    # @note Updated 2022-03-28.
     #
     # @seealso
     # - https://wiki.openssl.org/index.php/Compilation_and_Installation
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/openssl@3.rb
     # """
-    local app dict
+    local app conf_args dict
     koopa_assert_has_no_args "$#"
     declare -A app=(
         [make]="$(koopa_locate_make)"
@@ -29,12 +25,20 @@ install_openssl() { # {{{1
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    ./config \
-        --prefix="${dict[prefix]}" \
-        --openssldir="${dict[prefix]}" \
-        no-shared
+    conf_args=(
+        "--prefix=${dict[prefix]}"
+        "--openssldir=${dict[prefix]}"
+    )
+    case "${INSTALL_LINK_APP:-0}" in
+        '0')
+            conf_args+=('no-shared')
+            ;;
+        '1')
+            conf_args+=('shared')
+            ;;
+    esac
+    ./config "${conf_args[@]}"
     "${app[make]}" --jobs="${dict[jobs]}"
-    # > "${app[make]}" test
     "${app[make]}" install
     return 0
 }
