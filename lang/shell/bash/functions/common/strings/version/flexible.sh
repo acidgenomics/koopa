93bd3efade9018714daaa546ba706bf8309c0c39
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-28 06:17:30 -0400

    Fix assert

--- lang/shell/bash/functions/common/strings/version/flexible.sh
+++ lang/shell/bash/functions/common/strings/version/flexible.sh
@@ -79,13 +79,38 @@ koopa_lesspipe_version() { # {{{1
     return 0
 }
 
+koopa_man_version() { # {{{1
+    # """
+    # man-db version.
+    # @note Updated 2022-03-27.
+    # """
+    local app str
+    koopa_assert_has_args_le "$#" 1
+    declare -A app=(
+        [grep]="$(koopa_locate_grep)"
+        [man]="${1:-}"
+    )
+    [[ -z "${app[man]}" ]] && app[man]="$(koopa_locate_man)"
+    str="$( \
+        "${app[grep]}" \
+            --extended-regexp \
+            --only-matching \
+            --text \
+            'lib/man-db/libmandb-[.0-9]+\.dylib' \
+            "${app[man]}" \
+    )"
+    [[ -n "$str" ]] || return 1
+    koopa_extract_version "$str"
+    return 0
+}
+
 koopa_openjdk_version() { # {{{1
     # """
     # Java (OpenJDK) version.
-    # @note Updated 2022-03-18.
+    # @note Updated 2022-03-25.
     # """
     local app str
-    koopa_assert_has_no_args "$#"
+    koopa_assert_has_args_le "$#" 1
     declare -A app=(
         [cut]="$(koopa_locate_cut)"
         [head]="$(koopa_locate_head)"
