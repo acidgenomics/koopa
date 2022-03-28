5781850d94313578c9049805a23b9c24ec669106
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-28 16:39:49 -0400

    Add support for sshd config

--- lang/shell/bash/functions/os/linux/common/config.sh
+++ lang/shell/bash/functions/os/linux/common/config.sh
@@ -168,3 +168,22 @@ koopa_linux_update_ldconfig() { # {{{1
     "${app[sudo]}" "${app[ldconfig]}" || true
     return 0
 }
+
+koopa_linux_update_sshd_config() { # {{{1
+    # """
+    # Update sshd configuration.
+    # @note Updated 2022-03-28.
+    #
+    # Creates a 'koopa.conf' file, which contains passthrough support of
+    # 'KOOPA_COLOR_MODE' environment variable.
+    # """
+    local dict
+    koopa_assert_has_no_args "$#"
+    declare -A dict=(
+        [source_file]="$(koopa_koopa_prefix)/os/linux/common/etc/ssh/\
+sshd_config.d/koopa.conf"
+        [target_file]='/etc/ssh/sshd_config.d/koopa.conf'
+    )
+    koopa_ln --sudo "${dict[source_file]}" "${dict[target_file]}"
+    return 0
+}
