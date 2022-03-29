7f89fd87039e7d2b1a99e2f8a020b991f9e43463
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-29 09:35:52 -0400

    Rework Julia binary completion

--- etc/completion/koopa.sh
+++ etc/completion/koopa.sh
@@ -4,7 +4,7 @@
 __koopa_complete() { # {{{1
     # """
     # Bash/Zsh TAB completion for primary 'koopa' program.
-    # Updated 2022-03-22.
+    # Updated 2022-03-29.
     #
     # Keep all of these commands in a single file.
     # Sourcing multiple scripts doesn't work reliably.
@@ -204,7 +204,7 @@ __koopa_complete() { # {{{1
                             'cloudbiolinux'
                             'docker-credential-pass'
                             'google-cloud-sdk'
-                            'julia'
+                            'julia-binary'
                             'lmod'
                             'pihole'
                             'pivpn'
