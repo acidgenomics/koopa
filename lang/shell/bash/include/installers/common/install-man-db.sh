c19bebeda961beb902b6235763066d6649bbabc6
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-30 11:40:01 -0400

    Add placeholder functions

--- lang/shell/bash/include/installers/common/install-man-db.sh
+++ lang/shell/bash/include/installers/common/install-man-db.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 # FIXME Need to add support for this. Test on Ubuntu instance.
+
+install_man_db() { # {{{1
+    # """
+    # Install man-db.
+    # @note Updated 2022-03-30.
+    #
+    # @seealso
+    # - https://man-db.nongnu.org/development.html
+    # """
+    return 0
+}
