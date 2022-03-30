967431437d1deeb15b0c5c3c8714d6e960e654ed
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-30 11:40:01 -0400

    Add placeholder functions

--- lang/shell/bash/include/installers/common/install-pcre2.sh
+++ lang/shell/bash/include/installers/common/install-pcre2.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 # FIXME Need to add support for this. Test on Ubuntu instance.
+
+install_pcre2() { # {{{1
+    # """
+    # Install PCRE2.
+    # @note Updated 2022-03-30.
+    #
+    # @seealso
+    # - https://www.pcre.org/
+    # """
+    return 0
+}
