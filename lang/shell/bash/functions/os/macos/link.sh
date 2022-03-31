c53a43756a86afc61e001fb56d98ed6f03ebc5bb
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-31 09:48:59 -0400

    Add Python link support

--- lang/shell/bash/functions/os/macos/link.sh
+++ lang/shell/bash/functions/os/macos/link.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Rework 'koopa_sys_link' as 'koopa_link_into_bin' instead.
+
 koopa_macos_link_bbedit() { # {{{1
     # """
     # Link BBEdit.
@@ -22,6 +24,41 @@ koopa_macos_link_google_cloud_sdk() { # {{{1
         "$(koopa_homebrew_prefix)/Caskroom/google-cloud-sdk/latest/\
 google-cloud-sdk/bin/gcloud" \
         "$(koopa_koopa_prefix)/bin/gcloud"
+    return 0
+}
+
+koopa_macos_link_python() { # {{{1
+    # """
+    # Link Python.
+    # @note Updated 2022-03-30.
+    # """
+    local app dict
+    koopa_assert_has_no_args "$#"
+    declare -A app=(
+        [find]="$(koopa_locate_find)"
+    )
+    declare -A dict=(
+        [koopa_prefix]="$(koopa_koopa_prefix)"
+        [python_prefix]="$(koopa_macos_python_prefix)"
+    )
+    app[python]="$( \
+        "${app[find]}" \
+            "${dict[python_prefix]}/bin" \
+            -type 'f' \
+            -executable \
+            -name 'python*' \
+            -not -name '*-*' \
+    )"
+    koopa_assert_is_executable "${app[python]}"
+    dict[version]="$(koopa_get_version "${app[python]}")"
+    dict[maj_min_ver]="$(koopa_major_minor_version "${dict[version]}")"
+    dict[maj_ver]="$(koopa_major_version "${dict[version]}")"
+    koopa_sys_ln \
+        "${app[python]}" \
+        "${dict[koopa_prefix]}/bin/python${dict[maj_min_ver]}"
+    koopa_sys_ln \
+        "${app[python]}" \
+        "${dict[koopa_prefix]}/bin/python${dict[maj_ver]}"
     return 0
 }
 
