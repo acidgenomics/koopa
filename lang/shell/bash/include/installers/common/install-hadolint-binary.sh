56a969582c6f6404a9effbe7dbc3e13f27583400
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-31 15:51:56 -0400

    Draft updates

---
+++ lang/shell/bash/include/installers/common/install-hadolint-binary.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# FIXME Add binary support for macOS and Linux here.
+
+install_hadolint_binary() { # {{{1
+    # """
+    # Install hadolint binary.
+    # @note Updated 2022-03-31.
+    #
+    # Apple Silicon is not yet supported here.
+    #
+    # @seealso
+    # - https://github.com/hadolint/hadolint/releases/
+    # """
+    koopa_stop 'FIXME'
+    # Support all of these:
+    # - https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Darwin-x86_64
+    # - https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-arm64
+    # - https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64
+}
