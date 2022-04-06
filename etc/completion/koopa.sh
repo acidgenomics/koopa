6b54dd68341ebe1083a4834b83962775d6e34a8b
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-06 08:06:21 -0400

    Add FIXME

--- etc/completion/koopa.sh
+++ etc/completion/koopa.sh
@@ -260,6 +260,9 @@ __koopa_complete() { # {{{1
                     ;;
                 'link' | \
                 'unlink')
+                    # FIXME Consider reworking with install / uninstall above
+                    # for consistency. Put install / uninstall system auto-
+                    # completion functions in a separate line.
                     if koopa_is_macos
                     then
                         args+=(
