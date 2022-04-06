8e8ae3838e9a5115ea00d4e5d5243b0a41961a8f
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-06 13:57:20 -0400

    Dont set TZ on Docker

--- lang/shell/bash/functions/os/linux/debian/locale.sh
+++ lang/shell/bash/functions/os/linux/debian/locale.sh
@@ -46,10 +46,15 @@ koopa_debian_set_locale() { # {{{1
 koopa_debian_set_timezone() { # {{{1
     # """
     # Set local timezone.
-    # @note Updated 2022-03-29.
+    # @note Updated 2022-04-06.
+    #
+    # Inside Docker will see this issue:
+    # System has not been booted with systemd as init system (PID 1). Can't
+    # operate. Failed to connect to bus: Host is down.
     # """
     local app dict
     koopa_assert_has_args_le "$#" 1
+    koopa_is_docker && return 0
     declare -A app=(
         [sudo]="$(koopa_locate_sudo)"
         [timedatectl]="$(koopa_debian_locate_timedatectl)"
