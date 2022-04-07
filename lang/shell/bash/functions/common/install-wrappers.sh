5be0a6c2073fe84e554e9f0c41c1cd4e23c59c27
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-07 09:19:56 -0400

    Improve npm linkage

--- lang/shell/bash/functions/common/install-wrappers.sh
+++ lang/shell/bash/functions/common/install-wrappers.sh
@@ -557,10 +557,12 @@ koopa_install_node() { # {{{1
         "$@"
 }
 
-# FIXME Specify other packages to link.
 koopa_install_node_packages() { # {{{1
     koopa_install_app_packages \
+        --link-in-bin='bin/bash-language-server' \
+        --link-in-bin='bin/gtop' \
         --link-in-bin='bin/npm' \
+        --link-in-bin='bin/prettier' \
         --name-fancy='Node' \
         --name='node' \
         "$@"
