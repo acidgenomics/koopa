73a1909b771990712b77f79865386edd4a280c19
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-07 10:01:53 -0400

    Rework our link approach

--- lang/shell/bash/functions/os/macos/link.sh
+++ lang/shell/bash/functions/os/macos/link.sh
@@ -14,6 +14,124 @@
 #            'gnu-which' \
 #            'grep'
 
+koopa_macos_link_homebrew_opt() { # {{{1
+    local dict
+    koopa_assert_has_no_args "$#"
+    declare -A dict=(
+        [brew_opt_prefix]="$(koopa_homebrew_opt_prefix)"
+    )
+    dict[coreutils]="${dict[brew_opt_prefix}/coreutils/libexec/gnubin"
+    koopa_link_in_bin \
+        "${dict[coreutils]}/[" '[' \
+        "${dict[coreutils]}/b2sum" 'b2sum' \
+        "${dict[coreutils]}/base32" 'base32' \
+        "${dict[coreutils]}/base64" 'base64' \
+        "${dict[coreutils]}/basename" 'basename' \
+        "${dict[coreutils]}/basenc" 'basenc' \
+        "${dict[coreutils]}/cat" 'cat' \
+        "${dict[coreutils]}/chcon" 'chcon' \
+        "${dict[coreutils]}/chgrp" 'chgrp' \
+        "${dict[coreutils]}/chmod" 'chmod' \
+        "${dict[coreutils]}/chown" 'chown' \
+        "${dict[coreutils]}/chroot" 'chroot' \
+        "${dict[coreutils]}/cksum" 'cksum' \
+        "${dict[coreutils]}/comm" 'comm' \
+        "${dict[coreutils]}/cp" 'cp' \
+        "${dict[coreutils]}/csplit" 'csplit' \
+        "${dict[coreutils]}/cut" 'cut' \
+        "${dict[coreutils]}/date" 'date' \
+        "${dict[coreutils]}/dd" 'dd' \
+        "${dict[coreutils]}/df" 'df' \
+        "${dict[coreutils]}/dir" 'dir' \
+        "${dict[coreutils]}/dircolors" 'dircolors' \
+        "${dict[coreutils]}/dirname" 'dirname' \
+        "${dict[coreutils]}/du" 'du' \
+        "${dict[coreutils]}/echo" 'echo' \
+        "${dict[coreutils]}/env" 'env' \
+        "${dict[coreutils]}/expand" 'expand' \
+        "${dict[coreutils]}/expr" 'expr' \
+        "${dict[coreutils]}/factor" 'factor' \
+        "${dict[coreutils]}/false" 'false' \
+        "${dict[coreutils]}/fmt" 'fmt' \
+        "${dict[coreutils]}/fold" 'fold' \
+        "${dict[coreutils]}/groups" 'groups' \
+        "${dict[coreutils]}/head" 'head' \
+        "${dict[coreutils]}/hostid" 'hostid' \
+        "${dict[coreutils]}/id" 'id' \
+        "${dict[coreutils]}/install" 'install' \
+        "${dict[coreutils]}/join" 'join' \
+        "${dict[coreutils]}/kill" 'kill' \
+        "${dict[coreutils]}/link" 'link' \
+        "${dict[coreutils]}/ln" 'ln' \
+        "${dict[coreutils]}/logname" 'logname' \
+        "${dict[coreutils]}/ls" 'ls' \
+        "${dict[coreutils]}/md5sum" 'md5sum' \
+        "${dict[coreutils]}/mkdir" 'mkdir' \
+        "${dict[coreutils]}/mkfifo" 'mkfifo' \
+        "${dict[coreutils]}/mknod" 'mknod' \
+        "${dict[coreutils]}/mktemp" 'mktemp' \
+        "${dict[coreutils]}/mv" 'mv' \
+        "${dict[coreutils]}/nice" 'nice' \
+        "${dict[coreutils]}/nl" 'nl' \
+        "${dict[coreutils]}/nohup" 'nohup' \
+        "${dict[coreutils]}/nproc" 'nproc' \
+        "${dict[coreutils]}/numfmt" 'numfmt' \
+        "${dict[coreutils]}/od" 'od' \
+        "${dict[coreutils]}/paste" 'paste' \
+        "${dict[coreutils]}/pathchk" 'pathchk' \
+        "${dict[coreutils]}/pinky" 'pinky' \
+        "${dict[coreutils]}/pr" 'pr' \
+        "${dict[coreutils]}/printenv" 'printenv' \
+        "${dict[coreutils]}/printf" 'printf' \
+        "${dict[coreutils]}/ptx" 'ptx' \
+        "${dict[coreutils]}/pwd" 'pwd' \
+        "${dict[coreutils]}/readlink" 'readlink' \
+        "${dict[coreutils]}/realpath" 'realpath' \
+        "${dict[coreutils]}/rm" 'rm' \
+        "${dict[coreutils]}/rmdir" 'rmdir' \
+        "${dict[coreutils]}/runcon" 'runcon' \
+        "${dict[coreutils]}/seq" 'seq' \
+        "${dict[coreutils]}/sha1sum" 'sha1sum' \
+        "${dict[coreutils]}/sha224sum" 'sha224sum' \
+        "${dict[coreutils]}/sha256sum" 'sha256sum' \
+        "${dict[coreutils]}/sha384sum" 'sha384sum' \
+        "${dict[coreutils]}/sha512sum" 'sha512sum' \
+        "${dict[coreutils]}/shred" 'shred' \
+        "${dict[coreutils]}/shuf" 'shuf' \
+        "${dict[coreutils]}/sleep" 'sleep' \
+        "${dict[coreutils]}/sort" 'sort' \
+        "${dict[coreutils]}/split" 'split' \
+        "${dict[coreutils]}/stat" 'stat' \
+        "${dict[coreutils]}/stdbuf" 'stdbuf' \
+        "${dict[coreutils]}/stty" 'stty' \
+        "${dict[coreutils]}/sum" 'sum' \
+        "${dict[coreutils]}/sync" 'sync' \
+        "${dict[coreutils]}/tac" 'tac' \
+        "${dict[coreutils]}/tail" 'tail' \
+        "${dict[coreutils]}/tee" 'tee' \
+        "${dict[coreutils]}/test" 'test' \
+        "${dict[coreutils]}/timeout" 'timeout' \
+        "${dict[coreutils]}/touch" 'touch' \
+        "${dict[coreutils]}/tr" 'tr' \
+        "${dict[coreutils]}/true" 'true' \
+        "${dict[coreutils]}/truncate" 'truncate' \
+        "${dict[coreutils]}/tsort" 'tsort' \
+        "${dict[coreutils]}/tty" 'tty' \
+        "${dict[coreutils]}/uname" 'uname' \
+        "${dict[coreutils]}/unexpand" 'unexpand' \
+        "${dict[coreutils]}/uniq" 'uniq' \
+        "${dict[coreutils]}/unlink" 'unlink' \
+        "${dict[coreutils]}/uptime" 'uptime' \
+        "${dict[coreutils]}/users" 'users' \
+        "${dict[coreutils]}/vdir" 'vdir' \
+        "${dict[coreutils]}/wc" 'wc' \
+        "${dict[coreutils]}/who" 'who' \
+        "${dict[coreutils]}/whoami" 'whoami' \
+        "${dict[coreutils]}/yes" 'yes'
+    # findutils:
+    # - find
+}
+
 koopa_macos_link_bbedit() { # {{{1
     koopa_assert_has_no_args "$#"
     koopa_link_in_bin \
