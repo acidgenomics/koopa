44b0f6b694e087ee5acdf8bfffd052b2ce1080b7
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-07 19:54:43 -0400

    Update Alpine functions

--- lang/shell/bash/functions/os/linux/alpine/install.sh
+++ lang/shell/bash/functions/os/linux/alpine/install.sh
@@ -9,10 +9,9 @@ koopa_alpine_install_base_system() { # {{{1
         "$@"
 }
 
-koopa_alpine_install_glibc_binary() { # {{{1
+koopa_alpine_install_glibc() { # {{{1
     koopa_install_app \
-        --name-fancy='glibc (binary)' \
-        --name='glibc-binary' \
+        --name='glibc' \
         --platform='alpine' \
         --system \
         --version='2.30-r0' \
