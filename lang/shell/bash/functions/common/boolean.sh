5d541fc88e379b1487ff93008357edabe28d94a4
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-07 16:47:10 -0400

    Add draft support for binary install from our S3 bucket

--- lang/shell/bash/functions/common/boolean.sh
+++ lang/shell/bash/functions/common/boolean.sh
@@ -620,6 +620,57 @@ koopa_is_spacemacs_installed() { # {{{1
     koopa_file_detect_fixed --file="$init_file" --pattern='Spacemacs'
 }
 
+koopa_is_url_active() { # {{{1
+    # """
+    # Check if input is a URL and is active.
+    # @note Updated 2022-04-07.
+    #
+    # @section cURL approach:
+    #
+    # Can also use "--range '0-0'" instead of '--head' here.
+    #
+    # @section wget approach:
+    #
+    # > "${app[wget]}" --spider "$url" 2>/dev/null || return 1
+    #
+    # @seealso
+    # - https://stackoverflow.com/questions/12199059/
+    #
+    # @examples
+    # # TRUE:
+    # > koopa_is_url_active 'https://google.com/'
+    #
+    # # FALSE:
+    # > koopa_is_url_active 'https://google.com/asdf'
+    # """
+    local app url
+    koopa_assert_has_args "$#"
+    declare -A app=(
+        [curl]="$(koopa_locate_curl)"
+    )
+    declare -A dict=(
+        [url_pattern]='://'
+    )
+    for url in "$@"
+    do
+        koopa_str_detect_fixed \
+            --pattern="${dict[url_pattern]}" \
+            --string="$url" \
+            || return 1
+        "${app[curl]}" \
+            --disable \
+            --fail \
+            --head \
+            --location \
+            --output /dev/null \
+            --silent \
+            "$url" \
+            || return 1
+        continue
+    done
+    return 0
+}
+
 koopa_is_variable_defined() { # {{{1
     # """
     # Is the variable defined (and non-empty)?
