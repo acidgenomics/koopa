756a5539be9d686360a7449a6bfb436b22c67561
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-07 06:55:55 -0400

    Rework Homebrew installer

--- lang/shell/bash/functions/common/install-wrappers.sh
+++ lang/shell/bash/functions/common/install-wrappers.sh
@@ -386,6 +386,7 @@ koopa_install_hdf5() { # {{{1
 # FIXME Can we support '--link-in-bin=bin/brew' here?
 koopa_install_homebrew() { # {{{1
     koopa_install_app \
+        --link-in-bin='bin/brew' \
         --name-fancy='Homebrew' \
         --name='homebrew' \
         --system \
