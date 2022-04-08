bb38fa69868ea98a9a7eda88ae51223079a86e87
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-07 20:24:35 -0400

    Improve install wrappers

--- lang/shell/bash/functions/os/linux/alpine/locate-wrappers.sh
+++ lang/shell/bash/functions/os/linux/alpine/locate-wrappers.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
 koopa_alpine_locate_apk() { # {{{1
-    # """
-    # Locate Alpine 'apk'.
-    # @note Updated 2021-11-02.
-    # """
     koopa_locate_app '/sbin/apk'
 }
 
 koopa_alpine_locate_localedef() { # {{{1
-    # """
-    # Locate Alpine 'localedef'.
-    # @note Updated 2021-11-02.
-    # """
     koopa_locate_app '/usr/glibc-compat/bin/localedef'
 }
