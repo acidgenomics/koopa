e08335ad41afcc047fe657d114b5796e25c675a6
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-08 09:44:04 -0400

    Improve cleanup of OpenJDK on Linux

---
+++ lang/shell/bash/include/installers/linux/system/uninstall-openjdk.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+main() { # {{{1
+    local dict
+    koopa_assert_has_no_args "$#"
+    koopa_is_admin || return 0
+    declare -A dict=(
+        [default_java]='/usr/lib/jvm/default-java'
+    )
+    if [[ -d "${dict[default_java]}" ]]
+    then
+        koopa_linux_java_update_alternatives "${dict[default_java]}"
+    fi
+    return 0
+}
