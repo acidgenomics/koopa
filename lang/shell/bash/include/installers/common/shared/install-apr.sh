09956d33023efeb5b461a59d98af07d5f13b5409
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-09 22:50:04 -0400

    Work toward installing apr on macOS

--- lang/shell/bash/include/installers/common/shared/install-apr.sh
+++ lang/shell/bash/include/installers/common/shared/install-apr.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+# FIXME Consider installing GNU patch to resolve here, without prompting.
+
 main() { #{{{1
     # """
     # Install Apache Portable Runtime (APR) library.
     # @note Updated 2022-04-09.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/apr.rb
+    # - macOS build issue:
+    #  https://bz.apache.org/bugzilla/show_bug.cgi?id=64753
     # """
     local app conf_args dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_opt_prefix 'autoconf'
     declare -A app=(
         [make]="$(koopa_locate_make)"
+        [patch]="$(koopa_locate_patch)"
     )
     declare -A dict=(
         [jobs]="$(koopa_cpu_count)"
@@ -21,10 +28,29 @@ main() { #{{{1
     dict[url]="https://archive.apache.org/dist/${dict[name]}/${dict[file]}"
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
-    koopa_cd "${dict[name]}-${dict[version]}"
     conf_args=(
         "--prefix=${dict[prefix]}"
     )
+    # Apply r1871981 which fixes a compile error on macOS 11.0.
+    koopa_cd "${dict[name]}-${dict[version]}"
+    koopa_download \
+        "https://raw.githubusercontent.com/Homebrew/\
+formula-patches/7e2246542543bbd3111a4ec29f801e6e4d538f88/\
+apr/r1871981-macos11.patch" \
+        'patch1.patch'
+    "${app[patch]}" -p0 --input='patch1.patch'
+    # Apply r1882980+1882981 to fix implicit exit() declaration
+    # Remove with the next release, along with the autoconf call & dependency.
+    koopa_cd ..
+    koopa_download \
+        "https://raw.githubusercontent.com/Homebrew/\
+formula-patches/fa29e2e398c638ece1a72e7a4764de108bd09617/apr/\
+r1882980%2B1882981-configure.patch" \
+        'patch2.patch'
+    "${app[patch]}" -p0 --input='patch2.patch'
+    koopa_cd "${dict[name]}-${dict[version]}"
+    # > koopa_rm 'configure'
+    # > autoreconf
     ./configure "${conf_args[@]}"
     "${app[make]}" --jobs="${dict[jobs]}"
     "${app[make]}" install
