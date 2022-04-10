13d40fa16051d6f4f1ece48929fef3c5c8ab73b9
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-10 02:08:10 -0400

    Improve proj config

--- lang/shell/bash/include/installers/common/shared/install-gdal.sh
+++ lang/shell/bash/include/installers/common/shared/install-gdal.sh
@@ -11,29 +11,40 @@ main() { # {{{1
     # This is particularly important for CentOS builds.
     #
     # @seealso
+    # - https://gdal.org/build_hints.html
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/gdal.rb
     # - https://trac.osgeo.org/gdal/wiki/BuildingOnUnixGDAL25dev
     # - https://github.com/OSGeo/gdal/issues/1259
     # - https://github.com/OSGeo/gdal/issues/2402
     # - https://github.com/OSGeo/gdal/issues/1708
     # """
-    local app conf_args dict
+    local app dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_opt_prefix 'geos' 'proj' 'sqlite'
+    koopa_activate_opt_prefix 'cmake' 'geos' 'pkg-config' 'proj' 'sqlite'
     declare -A app=(
+        [cmake]="$(koopa_locate_cmake)"
         [make]="$(koopa_locate_make)"
     )
     declare -A dict=(
         [jobs]="$(koopa_cpu_count)"
         [make_prefix]="$(koopa_make_prefix)"
         [name]='gdal'
+        [opt_prefix]="$(koopa_opt_prefix)"
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
+    dict[file]="${dict[name]}-${dict[version]}.tar.gz"
+    dict[url]="https://github.com/OSGeo/${dict[name]}/releases/download/\
+v${dict[version]}/${dict[file]}"
+    koopa_alert_coffee_time
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
     conf_args=(
         "--prefix=${dict[prefix]}"
         '--with-armadillo=no'
         '--with-openjpeg'
+        "--with-proj=${dict[opt_prefix]}/proj"
         '--with-qhull=no'
         '--without-ecw'
         '--without-exr'
@@ -69,13 +80,6 @@ main() { # {{{1
             '--without-ld-shared'
         )
     fi
-    dict[file]="${dict[name]}-${dict[version]}.tar.gz"
-    dict[url]="https://github.com/OSGeo/${dict[name]}/releases/download/\
-v${dict[version]}/${dict[file]}"
-    koopa_download "${dict[url]}" "${dict[file]}"
-    koopa_extract "${dict[file]}"
-    koopa_cd "${dict[name]}-${dict[version]}"
-    koopa_alert_coffee_time
     ./configure "${conf_args[@]}"
     # Use '-d' flag for more verbose debug mode.
     "${app[make]}" --jobs="${dict[jobs]}"
