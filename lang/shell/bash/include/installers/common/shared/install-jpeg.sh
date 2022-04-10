6884f8e5c1f18314366733bb8d95cabd3c046419
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-10 01:31:31 -0400

    Draft libtiff install

---
+++ lang/shell/bash/include/installers/common/shared/install-jpeg.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+main() { # {{{1
+    # """
+    # Install jpeg.
+    # @note Updated 2022-04-10.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/jpeg.rb
+    # """
+    local app conf_args dict
+    koopa_assert_has_no_args "$#"
+    declare -A app=(
+        [make]="$(koopa_locate_make)"
+    )
+    declare -A dict=(
+        [jobs]="$(koopa_cpu_count)"
+        [name]='jpeg'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[file]="jpegsrc.v${dict[version]}.tar.gz"
+    dict[url]="https://www.ijg.org/files/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
+    conf_args=(
+        "--prefix=${dict[prefix]}"
+        '--disable-dependency-tracking'
+        '--disable-silent-rules'
+    )
+    ./configure "${conf_args[@]}"
+    "${app[make]}" --jobs="${dict[jobs]}"
+    "${app[make]}" install
+    return 0
+}
