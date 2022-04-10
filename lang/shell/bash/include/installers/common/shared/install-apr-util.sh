9e7c0f164a797524efb9855fd4cbf5a170b5cd40
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-09 23:20:07 -0400

    Improve apr config

---
+++ lang/shell/bash/include/installers/common/shared/install-apr-util.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+main() { #{{{1
+    # """
+    # Companion library to apr, the Apache Portable Runtime library.
+    # @note Updated 2022-04-09.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/
+    #     apr-util.rb
+    # """
+    local app conf_args dict
+    koopa_assert_has_no_args "$#"
+    koopa_activate_opt_prefix 'apr' 'openssl'
+    declare -A app=(
+        [make]="$(koopa_locate_make)"
+    )
+    declare -A dict=(
+        [jobs]="$(koopa_cpu_count)"
+        [name]='apr-util'
+        [opt_prefix]="$(koopa_opt_prefix)"
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[file]="${dict[name]}-${dict[version]}.tar.bz2"
+    dict[url]="https://archive.apache.org/dist/apr/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
+    conf_args=(
+        "--prefix=${dict[prefix]}"
+        "--with-apr=${dict[opt_prefix]}/apr/bin/apr-1-config"
+        '--with-crypto'
+        "--with-openssl=${dict[opt_prefix]}/openssl"
+        '--without-pgsql'
+    )
+    ./configure "${conf_args[@]}"
+    "${app[make]}" --jobs="${dict[jobs]}"
+    "${app[make]}" install
+    return 0
+}
--- lang/shell/bash/include/installers/common/shared/install-apr.sh
+++ lang/shell/bash/include/installers/common/shared/install-apr.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Consider installing GNU patch to resolve here, without prompting.
-
-# FIXME Still hitting these issues on macOS:
-# ./include/apr.h:561:2: error: Can not determine the proper size for pid_t
-# ./include/apr_want.h:94:8: error: redefinition of 'iovec'
-
-# FIXME Consider switching to GNU GCC here to build...
-
 main() { #{{{1
     # """
     # Install Apache Portable Runtime (APR) library.
@@ -20,7 +12,9 @@ main() { #{{{1
     # """
     local app conf_args dict
     koopa_assert_has_no_args "$#"
+    koopa_activate_opt_prefix 'autoconf' 'automake' 'libtool'
     declare -A app=(
+        [autoreconf]="$(koopa_locate_autoreconf)"
         [make]="$(koopa_locate_make)"
         [patch]="$(koopa_locate_patch)"
     )
@@ -37,26 +31,32 @@ main() { #{{{1
     conf_args=(
         "--prefix=${dict[prefix]}"
     )
-    # Apply r1871981 which fixes a compile error on macOS 11.0.
-    koopa_cd "${dict[name]}-${dict[version]}"
-    koopa_download \
-        "https://raw.githubusercontent.com/Homebrew/\
+    if koopa_is_macos
+    then
+        # Apply r1871981 which fixes a compile error on macOS 11.0.
+        koopa_cd "${dict[name]}-${dict[version]}"
+        koopa_download \
+            "https://raw.githubusercontent.com/Homebrew/\
 formula-patches/7e2246542543bbd3111a4ec29f801e6e4d538f88/\
 apr/r1871981-macos11.patch" \
-        'patch1.patch'
-    "${app[patch]}" -p0 --input='patch1.patch'
-    # Apply r1882980+1882981 to fix implicit exit() declaration
-    # Remove with the next release, along with the autoconf call & dependency.
-    koopa_cd ..
-    koopa_download \
-        "https://raw.githubusercontent.com/Homebrew/\
+            'patch1.patch'
+        "${app[patch]}" -p0 --input='patch1.patch'
+        # Apply r1882980+1882981 to fix implicit exit() declaration
+        # Remove with the next release, along with autoconf dependency.
+        koopa_cd ..
+        koopa_download \
+            "https://raw.githubusercontent.com/Homebrew/\
 formula-patches/fa29e2e398c638ece1a72e7a4764de108bd09617/apr/\
 r1882980%2B1882981-configure.patch" \
-        'patch2.patch'
-    "${app[patch]}" -p0 --input='patch2.patch'
+            'patch2.patch'
+        "${app[patch]}" -p0 --input='patch2.patch'
+        koopa_cd "${dict[name]}-${dict[version]}"
+        koopa_rm 'configure'
+        # This step requires autoconf, automake, and libtool.
+        "${app[autoreconf]}" --install
+        koopa_cd ..
+    fi
     koopa_cd "${dict[name]}-${dict[version]}"
-    # > koopa_rm 'configure'
-    # > autoreconf
     ./configure "${conf_args[@]}"
     "${app[make]}" --jobs="${dict[jobs]}"
     "${app[make]}" install
