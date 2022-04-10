e43e4537e36d901c42b9acaaf18658149981bc56
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-10 00:16:26 -0400

    Work on SCONS and Serf install

---
+++ lang/shell/bash/include/installers/common/shared/install-scons.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+main() { # {{{1
+    # """
+    # Install SCONS.
+    # @note Updated 2022-04-09.
+    #
+    # Required to install Apache Serf, which is required by subversion for
+    # HTTPS downloads.
+    #
+    # @seealso
+    # - https://scons.org/
+    # - https://gist.github.com/sgykfjsm/1dc5378d0258ae370fca
+    # """
+    local app conf_args dict
+    koopa_assert_has_no_args "$#"
+    declare -A dict=(
+        [name]='SCons'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    koopa_python_create_venv \
+        --prefix="${dict[prefix]}/libexec" \
+        "${dict[name]}==${dict[version]}"
+    koopa_ln \
+        "${dict[prefix]}/libexec/bin/scons" \
+        "${dict[prefix]}/bin/scons"
+    return 0
+}
