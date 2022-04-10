b3f2425397fbddb9c3f5ff1133e936563ff3c053
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-10 18:40:30 -0400

    Improve building of pinentry

--- lang/shell/bash/include/installers/common/shared/install-fltk.sh
+++ lang/shell/bash/include/installers/common/shared/install-fltk.sh
@@ -7,12 +7,16 @@ main() {
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/fltk.rb
+    # - https://courses.cs.washington.edu/courses/csep557/14au/tools/
+    #     fltk_install.html
     # """
-    local app dict
+    local app cmake_args conf_args dict
     koopa_assert_has_no_args "$#"
     declare -A app=(
+        [make]="$(koopa_locate_make)"
     )
     declare -A dict=(
+        [jobs]="$(koopa_cpu_count)"
         [name]='fltk'
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
@@ -20,7 +24,16 @@ main() {
     dict[file]="${dict[name]}-${dict[version]}-source.tar.gz"
     dict[url]="https://www.${dict[name]}.org/pub/${dict[name]}/\
 ${dict[version]}/${dict[file]}"
-
-
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
+    conf_args=(
+        "--prefix=${dict[prefix]}"
+        '--enable-shared'
+        '--enable-threads'
+    )
+    ./configure "${conf_args[@]}"
+    "${app[make]}" --jobs="${dict[jobs]}"
+    "${app[make]}" install
     return 0
 }
