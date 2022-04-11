6b05a321f1a290aa8d40bc5878f66b5b170a0414
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-11 14:21:20 -0400

    Tighten up our compiler settings

--- lang/shell/bash/functions/common/pkg-config.sh
+++ lang/shell/bash/functions/common/pkg-config.sh
@@ -17,67 +17,117 @@ koopa_activate_opt_prefix() { # {{{1
     )
     for name in "$@"
     do
-        local dict2 str
-        declare -A dict2
-        dict2[prefix]="${dict[opt_prefix]}/${name}"
-        koopa_assert_is_dir "${dict2[prefix]}"
-        if koopa_is_empty_dir "${dict2[prefix]}"
+        local prefix
+        prefix="${dict[opt_prefix]}/${name}"
+        koopa_assert_is_dir "$prefix"
+        if koopa_is_empty_dir "$prefix"
         then
-            koopa_stop "'${dict2[prefix]}' is empty."
+            koopa_stop "'${prefix}' is empty."
         fi
-        koopa_alert "Activating '${dict2[prefix]}'."
-        dict2[include]="${dict2[prefix]}/include"
-        dict2[lib64]="${dict2[prefix]}/lib64"
-        dict2[lib]="${dict2[prefix]}/lib"
-        # PATH.
-        koopa_activate_prefix "${dict2[prefix]}"
-        # PKG_CONFIG_PATH.
+        koopa_alert "Activating '${prefix}'."
+        # Set 'PATH' variable.
+        koopa_activate_prefix "${prefix}"
+        # Set 'CPPFLAGS' variable.
+        koopa_add_to_cppflags_start "${prefix}/include"
+        # Set 'LDFLAGS' variable.
+        koopa_add_to_ldflags_start \
+            "${prefix}/lib" \
+            "${prefix}/lib64"
+        # Set 'PKG_CONFIG_PATH' variable.
         koopa_add_to_pkg_config_path_start \
-            "${dict2[prefix]}/lib/pkgconfig" \
-            "${dict2[prefix]}/lib64/pkgconfig" \
-            "${dict2[prefix]}/share/pkgconfig"
-        # CPPFLAGS.
-        if [[ -d "${dict2[include]}" ]]
+            "${prefix}/lib/pkgconfig" \
+            "${prefix}/lib64/pkgconfig" \
+            "${prefix}/share/pkgconfig"
+    done
+    return 0
+}
+
+koopa_add_to_cppflags_start() { # {{{1
+    # """
+    # Append a 'CPPFLAGS' string.
+    # @note Updated 2022-04-11.
+    # """
+    local dir
+    koopa_assert_has_args "$#"
+    CPPFLAGS="${CPPFLAGS:-}"
+    for dir in "$@"
+    do
+        local str
+        [[ -d "$dir" ]] || continue
+        str="-I${dir}"
+        if [[ -n "$CPPFLAGS" ]]
         then
-            str="-I${dict2[include]}"
-            if [[ -n "${dict[cppflags]}" ]]
-            then
-                dict[cppflags]="${dict[cppflags]} ${str}"
-            else
-                dict[cppflags]="$str"
-            fi
-        fi
-        # LDFLAGS.
-        if [[ -d "${dict2[lib]}" ]]
-        then
-            str="-L${dict2[lib]} -Wl,-rpath,${dict2[lib]}"
-            if [[ -n "${dict[ldflags]}" ]]
-            then
-                dict[ldflags]="${dict[ldflags]} ${str}"
-            else
-                dict[ldflags]="$str"
-            fi
-        fi
-        if [[ -d "${dict2[lib64]}" ]]
-        then
-            str="-L${dict2[lib64]} -Wl,-rpath,${dict2[lib64]}"
-            if [[ -n "${dict[ldflags]}" ]]
-            then
-                dict[ldflags]="${dict[ldflags]} ${str}"
-            else
-                dict[ldflags]="$str"
-            fi
+            CPPFLAGS="${str} ${CPPFLAGS}"
+        else
+            CPPFLAGS="$str"
         fi
     done
-    if [[ -n "${dict[cppflags]}" ]]
-    then
-        CPPFLAGS="${dict[cppflags]}"
-        export CPPFLAGS
-    fi
-    if [[ -n "${dict[ldflags]}" ]]
-    then
-        LDFLAGS="${dict[ldflags]}"
-        export LDFLAGS
-    fi
+    export CPPFLAGS
+    return 0
+}
+
+koopa_add_to_ldflags_start() { # {{{1
+    # """
+    # Append an 'LDFLAGS' string.
+    # @note Updated 2022-04-11.
+    #
+    # Use '-rpath,${dir}' here not, '-rpath=${dir}'. This works on both
+    # BSD/Unix (macOS) and Linux systems.
+    # """
+    local dir
+    koopa_assert_has_args "$#"
+    LDFLAGS="${LDFLAGS:-}"
+    for dir in "$@"
+    do
+        local str
+        str="-L${dir} -Wl,-rpath,${dir}"
+        if [[ -n "$LDFLAGS" ]]
+        then
+            LDFLAGS="${str} ${LDFLAGS}"
+        else
+            LDFLAGS="$str"
+        fi
+    done
+    export LDFLAGS
+    return 0
+}
+
+koopa_add_to_pkg_config_path_start() { # {{{1
+    # """
+    # Force add to start of 'PKG_CONFIG_PATH'.
+    # @note Updated 2022-04-11.
+    # """
+    local dir
+    koopa_assert_has_args "$#"
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}"
+    for dir in "$@"
+    do
+        [[ -d "$dir" ]] || continue
+        PKG_CONFIG_PATH="$( \
+            __koopa_add_to_path_string_start "$PKG_CONFIG_PATH" "$dir" \
+        )"
+    done
+    export PKG_CONFIG_PATH
+    return 0
+}
+
+koopa_add_to_pkg_config_path_start_2() { # {{{1
+    # """
+    # Force add to start of 'PKG_CONFIG_PATH' using 'pc_path' variable
+    # lookup from 'pkg-config' program.
+    # @note Updated 2022-04-11.
+    # """
+    local app str
+    koopa_assert_has_args "$#"
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}"
+    for app in "$@"
+    do
+        [[ -x "$app" ]] || continue
+        str="$("$app" --variable 'pc_path' 'pkg-config')"
+        PKG_CONFIG_PATH="$( \
+            __koopa_add_to_path_string_start "$PKG_CONFIG_PATH" "$str" \
+        )"
+    done
+    export PKG_CONFIG_PATH
     return 0
 }
