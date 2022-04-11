43fe8c7e67dd8827590c5f89335d1ec46ce8c172
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-11 10:27:57 -0400

    Rework openssl config

--- lang/shell/bash/include/installers/common/shared/install-curl.sh
+++ lang/shell/bash/include/installers/common/shared/install-curl.sh
@@ -15,8 +15,7 @@ main() { # {{{1
     # """
     local app dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_opt_prefix 'pkg-config'
-    koopa_is_macos && koopa_activate_opt_prefix 'openssl'
+    koopa_activate_opt_prefix 'openssl' 'pkg-config'
     declare -A app=(
         [make]="$(koopa_locate_make)"
     )
