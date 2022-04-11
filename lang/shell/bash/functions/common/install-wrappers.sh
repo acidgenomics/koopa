a92b7c942ba5ab7357704bf9520768ec27ff3f13
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-11 13:14:43 -0400

    Require sqlite

--- lang/shell/bash/functions/common/install-wrappers.sh
+++ lang/shell/bash/functions/common/install-wrappers.sh
@@ -53,6 +53,7 @@ koopa_uninstall_anaconda() { # {{{3
 
 koopa_install_apr() { # {{{3
     koopa_install_app \
+        --activate-opt='sqlite' \
         --name-fancy='Apache Portable Runtime (APR) library' \
         --name='apr' \
         "$@"
