0cb75f53e23e30a51aa75631ec0f4a501cfdaa97
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-12 21:47:12 -0400

    Rework R config on macOS

--- lang/shell/bash/functions/os/macos/link.sh
+++ lang/shell/bash/functions/os/macos/link.sh
@@ -48,10 +48,10 @@ koopa_macos_link_homebrew() { # {{{1
         '/Applications/Emacs.app/Contents/MacOS/Emacs' \
         'emacs'
     # R cask.
-    # > dict[r]="$(koopa_macos_r_prefix)"
-    # > koopa_link_in_bin \
-    # >     "${dict[r]}/bin/R" 'R' \
-    # >     "${dict[r]}/bin/Rscript" 'Rscript'
+    dict[r]="$(koopa_macos_r_prefix)"
+    koopa_link_in_bin \
+        "${dict[r]}/bin/R" 'R' \
+        "${dict[r]}/bin/Rscript" 'Rscript'
     # Visual Studio Code cask.
     koopa_link_in_bin \
         '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' \
