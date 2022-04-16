b77f1a47bfcb4aabd8d8362a040f3ba3d1174bcf
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-15 20:27:44 -0400

    Improve versioning of the Rust installer

--- lang/shell/bash/include/installers/common/shared/install-rust.sh
+++ lang/shell/bash/include/installers/common/shared/install-rust.sh
@@ -1,35 +1,48 @@
 #!/usr/bin/env bash
 
+# FIXME What we want to install is this:
+# rust/rustup/toolchains/1.60.0-x86_64-apple-darwin
+
 main() { # {{{1
     # """
     # Install Rust (via rustup).
-    # @note Updated 2022-04-09.
+    # @note Updated 2022-04-15.
     # """
     local app dict
     koopa_assert_has_no_args "$#"
-    declare -A app
+    declare -A app=(
+        [cut]="$(koopa_locate_cut)"
+        [head]="$(koopa_locate_head)"
+    )
     declare -A dict=(
         [prefix]="${INSTALL_PREFIX:?}"
-        [version]="${INSTALL_VERSION:?}"
-        [toolchain]='stable'
+        [tmp_prefix]='rustup'
+        [version]="${INSTALL_VERSION:?}" # or 'stable' toolchain
     )
-    dict[cargo_home]="${dict[prefix]}"
-    dict[rustup_home]="${dict[prefix]}/rustup"
+    dict[cargo_home]="${dict[tmp_prefix]}"
+    dict[rustup_home]="${dict[tmp_prefix]}"
     CARGO_HOME="${dict[cargo_home]}"
     RUSTUP_HOME="${dict[rustup_home]}"
     export CARGO_HOME RUSTUP_HOME
-    koopa_add_to_path_start "${dict[cargo_home]}/bin"
     koopa_mkdir "${dict[rustup_home]}"
     dict[url]='https://sh.rustup.rs'
     dict[file]='rustup.sh'
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_chmod 'u+x' "${dict[file]}"
-    # Can get the version of install script with '--version' here.
-    "./${dict[file]}" --no-modify-path -v -y
-    app[rustup]="${dict[prefix]}/bin/rustup"
+    "./${dict[file]}" -v -y \
+        --default-toolchain 'none' \
+        --no-modify-path
+    app[rustup]="${dict[tmp_prefix]}/bin/rustup"
     koopa_assert_is_installed "${app[rustup]}"
-    "${app[rustup]}" install "${dict[toolchain]}"
-    "${app[rustup]}" default "${dict[toolchain]}"
-    "${app[rustup]}" toolchain list
+    "${app[rustup]}" install "${dict[version]}"
+    "${app[rustup]}" default "${dict[version]}"
+    dict[toolchain]="$( \
+        "${app[rustup]}" toolchain list \
+        | "${app[head]}" -n 1 \
+        | "${app[cut]}" -d ' ' -f '1' \
+    )"
+    dict[toolchain_prefix]="${dict[tmp_prefix]}/toolchains/${dict[toolchain]}"
+    koopa_assert_is_dir "${dict[toolchain_prefix]}"
+    koopa_cp "${dict[toolchain_prefix]}" "${dict[prefix]}"
     return 0
 }
