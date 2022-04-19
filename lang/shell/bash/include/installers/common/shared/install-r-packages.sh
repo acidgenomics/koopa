8c58961dc8e66f25d6f975123c9a0790f39a7d88
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-19 09:02:28 -0400

    Ensure we pin Bioconductor here

--- lang/shell/bash/include/installers/common/shared/install-r-packages.sh
+++ lang/shell/bash/include/installers/common/shared/install-r-packages.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-# FIXME Configure a pinned version of Bioconductor here.
-
 main() { # {{{1
     # """
     # Install R packages.
-    # @note Updated 2022-04-15.
+    # @note Updated 2022-04-19.
     # """
-    local app
+    local app dict
     koopa_assert_has_no_args "$#"
     declare -A app=(
         [rscript]="$(koopa_locate_rscript)"
     )
+    declare -A dict=(
+        [bioc_version]="$(koopa_variable 'bioconductor')"
+    )
     "${app[rscript]}" -e " \
+        install.packages(pkgs = 'BiocManager', dependencies = NA); \
+        BiocManager::install(version = '${dict[bioc_version]}'); \
         install.packages(pkgs = 'AcidDevTools', dependencies = NA); \
         AcidDevTools::installRecommendedPackages(); \
         install.packages(pkgs = 'koopa', dependencies = TRUE); \
