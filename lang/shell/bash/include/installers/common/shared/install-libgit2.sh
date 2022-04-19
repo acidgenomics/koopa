632835867870c83636200599709a974251731ed3
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-19 16:31:13 -0400

    Add install support for libgit2 and libssh2

---
+++ lang/shell/bash/include/installers/common/shared/install-libgit2.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+main() { # {{{1
+    # """
+    # Install libgit2.
+    # @note Updated 2022-04-19.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/libgit2.rb
+    # """
+    local app cmake_args dict
+    koopa_assert_has_no_args "$#"
+    koopa_activate_opt_prefix \
+        'cmake' \
+        'libssh2' \
+        'openssl' \
+        'pkg-config' \
+        'zlib'
+    declare -A app=(
+        [cmake]="$(koopa_locate_cmake)"
+    )
+    declare -A dict=(
+        [jobs]="$(koopa_cpu_count)"
+        [name]='libgit2'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[file]="v${dict[version]}.tar.gz"
+    dict[url]="https://github.com/${dict[name]}/${dict[name]}/\
+archive/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
+    cmake_args=(
+        "-DCMAKE_INSTALL_PREFIX=${dict[prefix]}"
+        '-DBUILD_CLAR=NO'
+        '-DBUILD_EXAMPLES=YES'
+        '-DBUILD_SHARED_LIBS=ON'
+        '-DUSE_SSH=YES'
+    )
+    "${app[cmake]}" \
+        -S '.' \
+        -B 'build' \
+        "${cmake_args[@]}"
+    "${app[cmake]}" \
+        --build 'build' \
+        --parallel "${dict[jobs]}"
+    "${app[cmake]}" --install 'build'
+    return 0
+}
