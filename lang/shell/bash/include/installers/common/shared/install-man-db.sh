89c04b5ba220005a2d47b96f8990f1e9c4412cd8
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-20 15:48:41 -0400

    Draft update

--- lang/shell/bash/include/installers/common/shared/install-man-db.sh
+++ lang/shell/bash/include/installers/common/shared/install-man-db.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # FIXME Need to add support for this. Test on Ubuntu instance.
+# FIXME Need to add support for GNU pipeline.
 
 main() { # {{{1
     # """
@@ -9,6 +10,29 @@ main() { # {{{1
     #
     # @seealso
     # - https://man-db.nongnu.org/development.html
+    # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/man-db.rb
     # """
-    return 0
+    local install_args
+    install_args=(
+        '--activate-opt=groff'
+        '--activate-opt=libpipeline'
+        '--activate-opt=pkg-config'
+    )
+    install_args+=(
+        # > -D "--with-config-file=#{etc}/man_db.conf"
+        # > -D "--with-systemdsystemunitdir=#{etc}/systemd/system"
+        # > -D "--with-systemdtmpfilesdir=#{etc}/tmpfiles.d"
+        # > -D '--program-prefix=g'
+        -D '--disable-cache-owner'
+        -D '--disable-dependency-tracking'
+        -D '--disable-nls'
+        -D '--disable-setuid'
+        -D '--disable-silent-rules'
+    )
+    koopa_install_gnu_app \
+        --name='man-db' \
+        --no-prefix-check \
+        --quiet \
+        "${install_args[@]}" \
+        "$@"
 }
