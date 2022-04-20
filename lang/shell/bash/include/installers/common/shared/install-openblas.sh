c66073749cf70847155270346d23ae19c2019b8b
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-20 13:44:29 -0400

    Work on adding OpenBLAS and Tcl/Tk build support

--- lang/shell/bash/include/installers/common/shared/install-openblas.sh
+++ lang/shell/bash/include/installers/common/shared/install-openblas.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
 
 main() { # {{{
-    koopa_stop 'FIXME Need to add support for this.'
+    # """
+    # Install OpenBLAS.
+    # @note Updated 2022-04-20.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/
+    #     openblas.rb
+    # - https://ports.macports.org/port/OpenBLAS/details/
+    # - https://iq.opengenus.org/install-openblas-from-source/
+    # """
+    local app dict
+    koopa_assert_has_no_args "$#"
+    if koopa_is_macos
+    then
+        koopa_activate_prefix '/usr/local/gfortran'
+    fi
+    declare -A app=(
+        [make]="$(koopa_locate_make)"
+    )
+    declare -A dict=(
+        [jobs]="$(koopa_cpu_count)"
+        [name]='openblas'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[file]="v${dict[version]}.tar.gz"
+    dict[url]="https://github.com/xianyi/OpenBLAS/archive/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
+    "${app[make]}" \
+        --jobs="${dict[jobs]}" \
+        'FC=gfortran' \
+        'libs' 'netlib' 'shared'
+    "${app[make]}" "PREFIX=${dict[prefix]}" install
     return 0
 }
---
+++ lang/shell/bash/include/installers/common/shared/install-tcl-tk.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+main() { # {{{
+    koopa_stop 'FIXME Need to add support for this.'
+    dict[file]="tcl8.6.12-src.tar.gz"
+    dict[url]="https://downloads.sourceforge.net/project/tcl/Tcl/8.6.12/${dict[file]}"
+    return 0
+}
