211da1b353a94cfe1bf962d01170a5f45c90566f
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-20 16:32:14 -0400

    Improve cross-platform compatiblity

--- lang/shell/bash/include/installers/common/shared/install-serf.sh
+++ lang/shell/bash/include/installers/common/shared/install-serf.sh
@@ -17,9 +17,9 @@ main() { # {{{1
     koopa_activate_opt_prefix \
         'apr' \
         'apr-util' \
+        'openssl' \
         'patch' \
         'scons'
-    koopa_is_macos && koopa_activate_opt_prefix 'openssl'
     declare -A app=(
         [patch]="$(koopa_locate_patch)"
         [scons]="$(koopa_locate_scons)"
@@ -36,30 +36,22 @@ main() { # {{{1
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    if koopa_is_macos
-    then
-        # Download patch required for OpenSSL 3 compatibility.
-        koopa_download "https://www.linuxfromscratch.org/patches/blfs/svn/\
+    # Download patch required for OpenSSL 3 compatibility.
+    koopa_download "https://www.linuxfromscratch.org/patches/blfs/svn/\
 serf-1.3.9-openssl3_fixes-1.patch"
-        "${app[patch]}" -Np1 -i 'serf-1.3.9-openssl3_fixes-1.patch'
-        # These steps require GNU sed.
-        "${app[sed]}" -i.bak "/Append/s:RPATH=libdir,::" 'SConstruct'
-        "${app[sed]}" -i.bak "/Default/s:lib_static,::"  'SConstruct'
-        "${app[sed]}" -i.bak "/Alias/s:install_static,::" 'SConstruct'
-        "${app[sed]}" -i.bak "/  print/{s/print/print(/; s/$/)/}" 'SConstruct'
-        "${app[sed]}" -i.bak "/get_contents()/s/,/.decode()&/" 'SConstruct'
-    fi
+    "${app[patch]}" -Np1 -i 'serf-1.3.9-openssl3_fixes-1.patch'
+    # These steps require GNU sed.
+    "${app[sed]}" -i.bak "/Append/s:RPATH=libdir,::" 'SConstruct'
+    "${app[sed]}" -i.bak "/Default/s:lib_static,::"  'SConstruct'
+    "${app[sed]}" -i.bak "/Alias/s:install_static,::" 'SConstruct'
+    "${app[sed]}" -i.bak "/  print/{s/print/print(/; s/$/)/}" 'SConstruct'
+    "${app[sed]}" -i.bak "/get_contents()/s/,/.decode()&/" 'SConstruct'
     scons_args=(
         "APR=${dict[opt_prefix]}/apr"
         "APU=${dict[opt_prefix]}/apr-util"
+        "OPENSSL=${dict[opt_prefix]}/openssl"
         "PREFIX=${dict[prefix]}"
     )
-    if koopa_is_macos
-    then
-        scons_args+=(
-            "OPENSSL=${dict[opt_prefix]}/openssl"
-        )
-    fi
     "${app[scons]}" "${scons_args[@]}"
     "${app[scons]}" "${scons_args[@]}" install
     return 0
