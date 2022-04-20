fdd232d7e47014dcb7b61893bb452267fa7d20cc
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-20 07:31:23 -0400

    Improve cross platform support for Emacs

--- lang/shell/bash/include/installers/common/shared/install-emacs.sh
+++ lang/shell/bash/include/installers/common/shared/install-emacs.sh
@@ -3,57 +3,24 @@
 main() { # {{{1
     # """
     # Install Emacs.
-    # @note Updated 2022-02-01.
+    # @note Updated 2022-04-20.
     #
     # Consider defining '--enable-locallisppath' and '--infodir' args.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/emacs.rb
     # """
-    local conf_args dict install_args pkg pkgs
-    install_args=(
-        '--name-fancy=Emacs'
-        '--name=emacs'
-        '--no-prefix-check'
-        '--quiet'
-    )
-    conf_args=()
-    if koopa_is_linux
-    then
-        conf_args+=(
-            '--with-x-toolkit=no'
-            '--with-xpm=no'
-        )
-    elif koopa_is_macos
-    then
-        declare -A dict
-        dict[gcc_version]="$(koopa_variable 'gcc')"
-        dict[gcc_maj_ver]="$(koopa_major_version "${dict[gcc_version]}")"
-        pkgs=(
-            "gcc@${dict[gcc_maj_ver]}"
-            'gnutls'
-            'pkg-config'
-        )
-        for pkg in "${pkgs[@]}"
-        do
-            install_args+=("--homebrew-opt=${pkg}")
-        done
-        export "CC=gcc-${dict[gcc_maj_ver]}"
-        conf_args+=(
-            # > "CC=gcc-${dict[gcc_maj_ver]}"
-            '--disable-silent-rules'
-            '--with-gnutls'
-            '--with-modules'
-            '--with-xml2'
-            '--without-dbus'
-            '--without-imagemagick'
-            '--without-ns'
-            '--without-selinux'
-            '--without-x'
-        )
-    fi
     koopa_install_gnu_app \
-        "${install_args[@]}" \
-        "${conf_args[@]}" \
+        --activate-opt='gmp' \
+        --activate-opt='gnutls' \
+        --activate-opt='libtasn1' \
+        --activate-opt='libunistring' \
+        --activate-opt='nettle' \
+        --activate-opt='pkg-config' \
+        --activate-opt='texinfo' \
+        --name-fancy='Emacs' \
+        --name='emacs' \
+        --no-prefix-check \
+        --quiet \
         "$@"
 }
