c761f6c8059361c730e118d66c0cdb1ca4dd34a9
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-20 15:17:07 -0400

    Add support for tcltk

--- lang/shell/bash/include/installers/common/shared/install-tcl-tk.sh
+++ lang/shell/bash/include/installers/common/shared/install-tcl-tk.sh
@@ -6,15 +6,50 @@ main() { # {{{
     # @note Updated 2022-04-20.
     #
     # @seealso
+    # - https://www.tcl.tk/software/tcltk/download.html
+    # - https://www.tcl.tk/doc/howto/compile.html
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/tcl-tk.rb
     # """
-    local app dict
+    local app conf_args dict
     koopa_assert_has_no_args "$#"
     declare -A app=(
         [make]="$(koopa_locate_make)"
     )
-    dict[file]="tcl${dict[version]}-src.tar.gz"
-    dict[url]="https://downloads.sourceforge.net/project/tcl/Tcl/\
-${dict[version]}/${dict[file]}"
+    declare -A dict=(
+        [jobs]="$(koopa_cpu_count)"
+        [prefix]="${INSTALL_PREFIX:?}"
+        [url_stem]='https://prdownloads.sourceforge.net/tcl'
+        [version]="${INSTALL_VERSION:?}"
+    )
+    conf_args=(
+        "--prefix=${dict[prefix]}"
+        '--enable-shared'
+        '--enable-threads'
+        '--enable-64bit'
+    )
+    dict[tcl_file]="tcl${dict[version]}-src.tar.gz"
+    dict[tcl_url]="${dict[url_stem]}/${dict[tcl_file]}"
+    koopa_download "${dict[tcl_url]}" "${dict[tcl_file]}"
+    koopa_extract "${dict[tcl_file]}"
+    (
+        koopa_cd "tcl${dict[version]}/unix"
+        ./configure "${conf_args[@]}"
+        "${app[make]}" --jobs="${dict[jobs]}"
+        "${app[make]}" test
+        "${app[make]}" install
+        "${app[make]}" install-private-headers
+    )
+    dict[tk_file]="tk${dict[version]}-src.tar.gz"
+    dict[tk_url]="${dict[url_stem]}/${dict[tk_file]}"
+    koopa_download "${dict[tk_url]}" "${dict[tk_file]}"
+    koopa_extract "${dict[tk_file]}"
+    (
+        koopa_cd "tk${dict[version]}/unix"
+        ./configure "${conf_args[@]}"
+        "${app[make]}" --jobs="${dict[jobs]}"
+        "${app[make]}" test
+        "${app[make]}" install
+        "${app[make]}" install-private-headers
+    )
     return 0
 }
