f3f6917325de8deaa6c15c9f52fbd368a60de1f8
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-21 12:30:30 -0400

    Work on building X11

--- lang/shell/bash/include/installers/common/shared/install-libpthread-stubs.sh
+++ lang/shell/bash/include/installers/common/shared/install-libpthread-stubs.sh
@@ -2,47 +2,32 @@
 
 main() { # {{{1
     # """
-    # Install libxcb.
+    # Install libpthread-stubs.
     # @note Updated 2022-04-21.
     #
     # @seealso
-    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/libxcb.rb
+    # - https://github.com/Homebrew/homebrew-core/blob/master/
+    #     Formula/libpthread-stubs.rb
     # """
     local app conf_args dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_opt_prefix \
-        'libpthread-stubs' \
-        'libxau' \
-        'libxdmcp' \
-        'pkg-config' \
-        'python' \
-        'xcb-proto' \
-        'xorgproto'
+    koopa_activate_opt_prefix 'pkg-config'
     declare -A app=(
         [make]="$(koopa_locate_make)"
     )
     declare -A dict=(
         [jobs]="$(koopa_cpu_count)"
-        [name]='libxcb'
+        [name]='libpthread-stubs'
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
-    dict[file]="${dict[name]}-${dict[version]}.tar.gz"
+    dict[file]="${dict[name]}-${dict[version]}.tar.bz2"
     dict[url]="https://xcb.freedesktop.org/dist/${dict[file]}"
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
     conf_args=(
         "--prefix=${dict[prefix]}"
-        '--enable-dri3'
-        '--enable-ge'
-        '--enable-xevie'
-        '--enable-xprint'
-        '--enable-selinux'
-        '--disable-dependency-tracking'
-        '--disable-silent-rules'
-        '--enable-devel-docs=no'
-        '--with-doxygen=no'
     )
     ./configure "${conf_args[@]}"
     "${app[make]}" --jobs="${dict[jobs]}"
