04ef7e42348501040e8c422b6d35b227269cea35
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-21 12:18:27 -0400

    Keep building X11 dependencies

--- lang/shell/bash/include/installers/common/shared/install-libxau.sh
+++ lang/shell/bash/include/installers/common/shared/install-libxau.sh
@@ -1,7 +1,44 @@
 #!/usr/bin/env bash
 
+# NOTE May need to address this:
+# Package xorg-macros was not found in the pkg-config search path.
+# Perhaps you should add the directory containing `xorg-macros.pc'
+# to the PKG_CONFIG_PATH environment variable
+# No package 'xorg-macros' found
+# https://github.com/freedesktop/xorg-macros
+
 main() { # {{{1
-    koopa_stop 'FIXME Need to add support for this.'
+    # """
+    # Install libxau.
+    # @note Updated 2022-04-21.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/libxau.rb
+    # """
+    local app conf_args dict
+    koopa_assert_has_no_args "$#"
+    koopa_activate_opt_prefix 'pkg-config' 'xorgproto'
+    declare -A app=(
+        [make]="$(koopa_locate_make)"
+    )
+    declare -A dict=(
+        [jobs]="$(koopa_cpu_count)"
+        [name]='libXau'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[file]="${dict[name]}-${dict[version]}.tar.bz2"
+    dict[url]="https://www.x.org/archive/individual/lib/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
+    conf_args=(
+        "--prefix=${dict[prefix]}"
+        '--disable-dependency-tracking'
+        '--disable-silent-rules'
+    )
+    ./configure "${conf_args[@]}"
+    "${app[make]}" --jobs="${dict[jobs]}"
+    "${app[make]}" install
     return 0
 }
-
