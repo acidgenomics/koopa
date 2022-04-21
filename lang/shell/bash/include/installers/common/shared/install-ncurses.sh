aac11d19abbaf4f3f4145482bf181d48d42d38e9
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-21 08:33:29 -0400

    Rework ncurses installer

---
+++ lang/shell/bash/include/installers/common/shared/install-ncurses.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+main() { # {{{1
+    # """
+    # Install ncurses.
+    # @note Updated 2022-04-21.
+    #
+    # @seealso
+    # - https://github.com/archlinux/svntogit-packages/blob/master/ncurses/
+    #     repos/core-x86_64/PKGBUILD
+    # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/ncurses.rb
+    # - https://github.com/microsoft/vcpkg/issues/22654
+    # """
+    local dict
+    koopa_assert_has_no_args "$#"
+    declare -A dict=(
+        [prefix]="${INSTALL_PREFIX:?}"
+    )
+    dict[pkgconfig_dir]="${dict[prefix]}/lib/pkgconfig"
+    koopa_mkdir "${dict[pkgconfig_dir]}"
+    koopa_install_gnu_app \
+        --activate-opt='pkg-config' \
+        --name='ncurses' \
+        --no-prefix-check \
+        -D '--enable-pc-files' \
+        -D '--enable-widec' \
+        -D '--with-cxx-binding' \
+        -D '--with-cxx-shared' \
+        -D '--with-manpage-format=normal' \
+        -D "--with-pkg-config-libdir=${dict[pkgconfig_dir]}" \
+        -D '--with-shared' \
+        -D '--with-versioned-syms' \
+        -D '--without-ada' \
+        "$@"
+}
