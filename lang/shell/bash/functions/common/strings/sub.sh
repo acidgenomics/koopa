c2d6d1f4d856315779a22b24d351ab4c0af9ac4b
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-21 13:48:59 -0400

    Improve input hardening using quotemeta

--- lang/shell/bash/functions/common/strings/sub.sh
+++ lang/shell/bash/functions/common/strings/sub.sh
@@ -3,7 +3,7 @@
 koopa_gsub() { # {{{1
     # """
     # Global substitution.
-    # @note Updated 2022-02-17.
+    # @note Updated 2022-04-21.
     #
     # @usage koopa_gsub --pattern=PATTERN --replacement=REPLACEMENT STRING...
     #
@@ -11,6 +11,9 @@ koopa_gsub() { # {{{1
     # > koopa_gsub --pattern='a' --replacement='' 'aabb' 'aacc'
     # # bb
     # # cc
+    #
+    # # koopa_gsub --pattern='/' --replacement='|' '/\|/\|'
+    # # |\||\|
     # """
     koopa_sub --global "$@"
 }
@@ -20,12 +23,18 @@ koopa_sub() { # {{{1
     # Single substitution.
     # @note Updated 2022-04-21.
     #
+    # @seealso
+    # - https://perldoc.perl.org/functions/quotemeta
+    #
     # @usage koopa_sub --pattern=PATTERN --replacement=REPLACEMENT STRING...
     #
     # @examples
     # > koopa_sub --pattern='a' --replacement='' 'aabb' 'bbaa'
     # # abb
     # # bba
+    #
+    # # koopa_sub --pattern='/' --replacement='|' '/\|/\|'
+    # # |\|/\|
     # """
     local app dict pos
     declare -A app=(
@@ -35,7 +44,7 @@ koopa_sub() { # {{{1
         [global]=0
         [pattern]=''
         [perl_tail]=''
-        [regex]=1
+        [regex]=0
         [replacement]=''
     )
     pos=()
@@ -87,10 +96,17 @@ koopa_sub() { # {{{1
     [[ "${#pos[@]}" -eq 0 ]] && pos=("$(</dev/stdin)")
     set -- "${pos[@]}"
     koopa_assert_has_args "$#"
-    [[ "${dict[regex]}" -eq 0 ]] && dict[pattern]="\\Q${dict[pattern]}\\E"
     [[ "${dict[global]}" -eq 1 ]] && dict[perl_tail]='g'
-    dict[expr]="s/${dict[pattern]}/${dict[replacement]}/${dict[perl_tail]}"
-    koopa_print "$@" \
-        | "${app[perl]}" -p -e "${dict[expr]}"
+    if [[ "${dict[regex]}" -eq 1 ]]
+    then
+        dict[expr]="s/${dict[pattern]}/${dict[replacement]}/${dict[perl_tail]}"
+    else
+        dict[expr]=" \
+            \$pattern = quotemeta '${dict[pattern]}'; \
+            \$replacement = '${dict[replacement]}'; \
+            s/\$pattern/\$replacement/${dict[perl_tail]}; \
+        "
+    fi
+    koopa_print "$@" | "${app[perl]}" -p -e "${dict[expr]}"
     return 0
 }
