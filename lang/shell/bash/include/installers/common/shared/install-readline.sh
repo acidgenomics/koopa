76da9c88b2b247baa6242b7a79d55a399bf8f372
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-21 08:46:50 -0400

    Rework readline config

--- lang/shell/bash/include/installers/common/shared/install-readline.sh
+++ lang/shell/bash/include/installers/common/shared/install-readline.sh
@@ -15,32 +15,43 @@ main() { # {{{1
     # - https://github.com/archlinux/svntogit-packages/blob/master/readline/
     #     repos/core-x86_64/PKGBUILD
     # """
-    local app dict
+    local app conf_args dict
     koopa_assert_has_no_args "$#"
+    koopa_activate_opt_prefix 'ncurses' 'pkg-config'
+    declare -A app=(
+        [make]="$(koopa_locate_make)"
+    )
     declare -A dict=(
+        [gnu_mirror]="$(koopa_gnu_mirror_url)"
+        [jobs]="$(koopa_cpu_count)"
+        [name]='readline'
         [opt_prefix]="$(koopa_opt_prefix)"
         [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
     )
+    dict[file]="${dict[name]}-${dict[version]}.tar.gz"
+    dict[url]="${dict[gnu_mirror]}/${dict[name]}/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cd "${dict[name]}-${dict[version]}"
+    conf_args=(
+        "--prefix=${dict[prefix]}"
+        '--enable-shared'
+        '--enable-static'
+        '--with-curses'
+    )
+    export CFLAGS='-fPIC'
+    ./configure "${conf_args[@]}"
+    # Alternatively:
+    # > SHLIB_LIBS="${dict[opt_prefix]}/ncurses/lib/libncurses.so"
+    "${app[make]}" \
+        --jobs="${dict[jobs]}" \
+        SHLIB_LIBS='-lncurses'
+    "${app[make]}" install
     if koopa_is_linux
     then
-        export CFLAGS='-fPIC'
-        # > export SHLIB_LIBS='-lncurses'
-        export SHLIB_LIBS="${dict[opt_prefix]}/ncurses/lib/libncurses.so"
-    fi
-    koopa_install_gnu_app \
-        --activate-opt='ncurses' \
-        --name='readline' \
-        --no-prefix-check \
-        -D '--enable-shared' \
-        -D '--enable-static' \
-        -D '--with-curses' \
-        "$@"
-    if koopa_is_linux
-    then
-        declare -A app
         app[ldd]="$(koopa_locate_ldd)"
         "${app[ldd]}" -r "${dict[prefix]}/lib/libreadline.so"
     fi
-    # FIXME On macOS, consider checking with otool -L...
     return 0
 }
