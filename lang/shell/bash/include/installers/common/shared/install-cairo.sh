d5f74e12c4989bccbf05062d320950c8ae200eb9
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-28 17:53:29 -0400

    Improve zlib handling on macOS

--- lang/shell/bash/include/installers/common/shared/install-cairo.sh
+++ lang/shell/bash/include/installers/common/shared/install-cairo.sh
@@ -3,7 +3,7 @@
 main() { # {{{1
     # """
     # Install Cairo.
-    # @note Updated 2022-04-26.
+    # @note Updated 2022-04-28.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/cairo.rb
@@ -14,7 +14,6 @@ main() { # {{{1
     koopa_assert_has_no_args "$#"
     koopa_activate_build_opt_prefix 'pkg-config'
     deps=(
-        # > 'zlib' # FIXME Use system zlib for macOS.
         'gettext'
         'freetype'
         'fontconfig'
@@ -22,18 +21,25 @@ main() { # {{{1
         'pcre'
         'glib'
         'libpng'
-        'libpthread-stubs'
         'lzo'
         'pixman'
-        'xorg-xorgproto'
-        'xorg-xcb-proto'
-        'xorg-libxau'
-        'xorg-libxdmcp'
-        'xorg-libxcb'
-        'xorg-libx11'
-        'xorg-libxext'
-        'xorg-libxrender'
     )
+    if koopa_is_macos
+    then
+        koopa_add_to_pkg_config_path '/opt/X11/pkgconfig'
+    else
+        deps+=(
+            'xorg-xorgproto'
+            'xorg-xcb-proto'
+            'xorg-libpthread-stubs'
+            'xorg-libxau'
+            'xorg-libxdmcp'
+            'xorg-libxcb'
+            'xorg-libx11'
+            'xorg-libxext'
+            'xorg-libxrender'
+        )
+    fi
     koopa_activate_opt_prefix "${deps[@]}"
     declare -A app=(
         [make]="$(koopa_locate_make)"
@@ -49,32 +55,18 @@ main() { # {{{1
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    # Alternative approach that uses meson (from Arch Linux recipe):
-    # How to handle multiple cores here?
-    # > app[meson]="$(koopa_locate_meson)"
-    # > "${app[meson]}" cairo build \
-    # >     -D spectre=disabled \
-    # >     -D tee=enabled \
-    # >     -D tests=disabled \
-    # >     -D symbol-lookup=disabled \
-    # >     -D gtk_doc=true
-    # > "${app[meson]}" compile -C build
-    # > "${app[meson]}" install -C build --destdir "${dict[prefix]}"
     conf_args=(
         "--prefix=${dict[prefix]}"
         '--disable-dependency-tracking'
-        # > '--disable-valgrind'
-        # > '--enable-gobject'
-        # > '--enable-svg'
-        # > '--enable-tee'
-        '--enable-xcb'
-        '--enable-xlib'
-        '--enable-xlib-xrender'
     )
-    # > if koopa_is_macos
-    # > then
-    # >     conf_args+=('--enable-quartz-image')
-    # > fi
+    if ! koopa_is_macos
+    then
+        conf_args+=(
+            '--enable-xcb'
+            '--enable-xlib'
+            '--enable-xlib-xrender'
+        )
+    fi
     ./configure "${conf_args[@]}"
     "${app[make]}" --jobs="${dict[jobs]}"
     "${app[make]}" install
