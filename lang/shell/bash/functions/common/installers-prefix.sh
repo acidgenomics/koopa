af8eec079adbd6262f7cbe350129772ec2219229
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-16 14:31:59 -0400

    Split out functions

---
+++ lang/shell/bash/functions/common/installers-prefix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+koopa_installers_prefix() {
+    # """
+    # Koopa installers prefix.
+    # @note Updated 2022-01-28.
+    # """
+    koopa_print "$(koopa_koopa_prefix)/lang/shell/bash/include/installers"
+    return 0
+}
---
+++ lang/shell/bash/functions/common/man-prefix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+koopa_man_prefix() {
+    # """
+    # man documentation file prefix.
+    # @note Updated 2022-02-15.
+    # """
+    koopa_print "$(koopa_koopa_prefix)/man"
+    return 0
+}
