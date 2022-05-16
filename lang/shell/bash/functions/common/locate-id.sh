fa029a18fb7daa2033f2d611336ddd35c189c67f
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-16 17:08:35 -0400

    Work on splitting out locator and installer wrappers

---
+++ lang/shell/bash/functions/common/locate-id.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_id() {
+    koopa_locate_app \
+        --app-name='id' \
+        --opt-name='coreutils'
+}
---
+++ lang/shell/bash/functions/common/locate-kallisto.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+koopa_locate_kallisto() {
+    koopa_locate_conda_app 'kallisto'
+}
