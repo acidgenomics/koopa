04dec1a2cc58c2ad7c43b6590fe8b9c1bf868604
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-16 17:20:08 -0400

    Work on splitting out locator and installer wrappers

---
+++ lang/shell/bash/functions/common/locate-od.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_od() {
+    koopa_locate_app \
+        --app-name='od' \
+        --opt-name='coreutils'
+}
---
+++ lang/shell/bash/functions/common/locate-parallel.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_parallel() {
+    koopa_locate_app \
+        --app-name='parallel' \
+        --opt-name='coreutils'
+}
