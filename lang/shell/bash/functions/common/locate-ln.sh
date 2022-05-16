7a9c6c34e847654005fb21470c03446087267c51
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-16 17:14:17 -0400

    Work on splitting out locator and installer wrappers

---
+++ lang/shell/bash/functions/common/locate-ln.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+koopa_locate_ln() {
+    koopa_locate_app \
+        --allow-in-path \
+        --app-name='ln' \
+        --opt-name='coreutils'
+}
---
+++ lang/shell/bash/functions/common/locate-ls.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_ls() {
+    koopa_locate_app \
+        --app-name='ls' \
+        --opt-name='coreutils'
+}
