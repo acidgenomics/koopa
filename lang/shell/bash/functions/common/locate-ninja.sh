caa7ed01476046ef5a556bef7d9f66afee3ab518
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-16 17:17:46 -0400

    Work on splitting out locator and installer wrappers

---
+++ lang/shell/bash/functions/common/locate-ninja.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_ninja() {
+    koopa_locate_app \
+        --app-name='ninja' \
+        --opt-name='ninja'
+}
---
+++ lang/shell/bash/functions/common/locate-node.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_node() {
+    koopa_locate_app \
+        --app-name='node' \
+        --opt-name='node'
+}
