b7576d567996f5f6c4433e9bd189a1a9cb4a3059
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-16 17:08:35 -0400

    Work on splitting out locator and installer wrappers

---
+++ lang/shell/bash/functions/common/locate-h5cc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_h5cc() {
+    koopa_locate_app \
+        --app-name='h5cc' \
+        --opt-name='hdf5'
+}
---
+++ lang/shell/bash/functions/common/locate-head.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+koopa_locate_head() {
+    koopa_locate_app \
+        --allow-in-path \
+        --app-name='head' \
+        --opt-name='coreutils'
+}
