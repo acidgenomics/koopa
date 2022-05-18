b18e309368bc5a2a56c420961398e55f78f6fd67
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-18 17:34:17 -0400

    Reorganize OS-specific functions

---
+++ lang/shell/bash/functions/os/linux/common/install-pihole.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+koopa_linux_install_pihole() {
+    koopa_update_app \
+        --name-fancy='Pi-hole' \
+        --name='pihole' \
+        --platform='linux' \
+        --system \
+        "$@"
+}
--- lang/shell/bash/functions/os/linux/opensuse/locate.sh
+++ lang/shell/bash/functions/os/linux/opensuse/locate.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 koopa_opensuse_locate_zypper() {
-    # """
-    # Locate OpenSUSE 'zypper'.
-    # @note Updated 2021-11-16.
-    # """
     koopa_locate_app '/usr/bin/zypper'
 }
