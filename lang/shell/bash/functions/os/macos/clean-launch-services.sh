6c93383da4fb53928fda2473c40efc06aebb7304
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-18 18:25:16 -0400

    Reorganize OS-specific functions

--- lang/shell/bash/functions/os/macos/clean-launch-services.sh
+++ lang/shell/bash/functions/os/macos/clean-launch-services.sh
@@ -24,23 +24,3 @@ koopa_macos_clean_launch_services() {
     koopa_alert_success 'Clean up was successful.'
     return 0
 }
-
-koopa_macos_flush_dns() {
-    # """
-    # Flush DNS cache.
-    # @note Updated 2021-11-16.
-    # """
-    local app
-    koopa_assert_has_no_args "$#"
-    koopa_assert_is_admin
-    declare -A app=(
-        [dscacheutil]="$(koopa_macos_locate_dscacheutil)"
-        [kill_all]="$(koopa_macos_locate_kill_all)"
-        [sudo]="$(koopa_locate_sudo)"
-    )
-    koopa_alert 'Flushing DNS.'
-    "${app[sudo]}" "${app[dscacheutil]}" -flushcache
-    "${app[sudo]}" "${app[kill_all]}" -HUP 'mDNSResponder'
-    koopa_alert_success 'DNS flush was successful.'
-    return 0
-}
