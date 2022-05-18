b1c9dbd3f58d84b1adc410608d91322384b46288
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-18 10:58:30 -0400

    Split out installers

---
+++ lang/shell/bash/functions/common/i/install-libtiff.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_install_libtiff() {
+    koopa_install_app \
+        --name='libtiff' \
+        "$@"
+}
---
+++ lang/shell/bash/functions/common/u/uninstall-libtool.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_uninstall_libtool() {
+    koopa_uninstall_app \
+        --name='libtool' \
+        "$@"
+}
