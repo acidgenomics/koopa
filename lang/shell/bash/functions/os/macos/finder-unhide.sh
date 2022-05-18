b9a8ee35f356d05dfd31b5e7734864600132eaf7
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-18 18:19:22 -0400

    Reorganize OS-specific functions

--- lang/shell/bash/functions/os/macos/finder-unhide.sh
+++ lang/shell/bash/functions/os/macos/finder-unhide.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-koopa_macos_finder_hide() {
-    # """
-    # Hide files from view in the Finder.
-    # @note Updated 2021-05-08.
-    # """
-    koopa_assert_has_args "$#"
-    koopa_assert_is_installed 'setfile'
-    setfile -a V "$@"
-    return 0
-}
+# FIXME Need to locate setfile.
 
 koopa_macos_finder_unhide() {
     # """
