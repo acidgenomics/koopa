0fade71e47e10ad885d01608cfa5155fd5d3a2a5
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-20 17:56:18 -0400

    Work on hardening functions with locators

--- lang/shell/bash/functions/os/macos/d/download-macos.sh
+++ lang/shell/bash/functions/os/macos/d/download-macos.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME Need to locate softwareupdate.
-
 koopa_macos_download_macos() {
     # """
     # Download a full copy of macOS system installer.
-    # @note Updated 2022-05-18.
+    # @note Updated 2022-05-20.
     #
     # Note that you can only download a Catalina installer on Catalina.
     # Attempting to do this on a machine running Big Sur will fail.
@@ -14,10 +12,16 @@ koopa_macos_download_macos() {
     # - https://scriptingosx.com/2019/10/
     #       download-a-full-install-macos-app-with-softwareupdate-in-catalina/
     # """
-    local version
-    version="${1:?}"
-    softwareupdate \
+    local app dict
+    koopa_assert_has_args_eq "$#" 1
+    declare -A app=(
+        [softwareupdate]="$(koopa_macos_locate_softwareupdate)"
+    )
+    declare -A dict=(
+        [version]="${1:?}"
+    )
+    "${app[softwareupdate]}" \
         --fetch-full-installer \
-        --full-installer-version "$version"
+        --full-installer-version "${dict[version]}"
     return 0
 }
