349c077fdb19c60ad9db40618e7f83dbec14ce88
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-20 17:56:18 -0400

    Work on hardening functions with locators

--- lang/shell/bash/functions/common/e/entab.sh
+++ lang/shell/bash/functions/common/e/entab.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-# FIXME Need to locate vim.
-
 koopa_entab() {
     # """
     # Entab files.
-    # @note Updated 2020-07-13.
+    # @note Updated 2022-05-20.
     # """
-    local file
+    local app file
     koopa_assert_has_args "$#"
-    koopa_assert_is_installed 'vim'
+    declare -A app=(
+        [vim]="$(koopa_locate_vim)"
+    )
+    [[ -x "${app[vim]}" ]] || return 1
     koopa_assert_is_file "$@"
     for file in "$@"
     do
-        vim \
+        "${app[vim]}" \
             -c 'set noexpandtab tabstop=4 shiftwidth=4' \
             -c ':%retab!' \
             -c ':wq' \
