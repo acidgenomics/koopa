e04f02d092b0ec27d689855828fd3c150c80d203
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-20 11:39:36 -0400

    Update functions cache

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -3036,6 +3036,27 @@ koopa_brew_upgrade_brews() {
     done
     return 0
 }
+koopa_cache_all_functions() {
+    local dict
+    koopa_assert_has_no_args "$#"
+    declare -A dict=(
+        [koopa_prefix]="$(koopa_koopa_prefix)"
+    )
+    dict[shell_prefix]="${dict[koopa_prefix]}/lang/shell"
+    koopa_cache_functions \
+        "${dict[shell_prefix]}/bash/functions/activate" \
+        "${dict[shell_prefix]}/bash/functions/common" \
+        "${dict[shell_prefix]}/bash/functions/os/linux/alpine" \
+        "${dict[shell_prefix]}/bash/functions/os/linux/arch" \
+        "${dict[shell_prefix]}/bash/functions/os/linux/common" \
+        "${dict[shell_prefix]}/bash/functions/os/linux/debian" \
+        "${dict[shell_prefix]}/bash/functions/os/linux/fedora" \
+        "${dict[shell_prefix]}/bash/functions/os/linux/opensuse" \
+        "${dict[shell_prefix]}/bash/functions/os/linux/rhel" \
+        "${dict[shell_prefix]}/bash/functions/os/macos" \
+        "${dict[shell_prefix]}/posix/functions"
+    return 0
+}
 koopa_cache_functions() {
     local app prefix
     koopa_assert_has_args "$#"
@@ -3834,7 +3855,7 @@ koopa_cli_system() {
             ;;
         'brew-dump-brewfile' | \
         'brew-outdated' | \
-        'cache-functions' | \
+        'cache-all-functions' | \
         'disable-passwordless-sudo' | \
         'enable-passwordless-sudo' | \
         'find-non-symlinked-make-files' | \
