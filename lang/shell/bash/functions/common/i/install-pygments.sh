51b79290f3e8d32daa01db0c71647d44f1553369
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-27 14:01:14 -0400

    Add FLAC and IPython install support

--- lang/shell/bash/functions/common/i/install-pygments.sh
+++ lang/shell/bash/functions/common/i/install-pygments.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# NOTE Consider installing Dracula color support.
-# https://draculatheme.com/pygments
-
 koopa_install_pygments() {
     koopa_install_app \
         --installer='python-venv' \
