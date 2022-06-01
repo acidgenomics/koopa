30075816816d0fc46a2e3f4f39b08034ab81127d
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-01 07:50:49 -0400

    Resolve FIXME

--- lang/shell/bash/functions/common/l/locate-gzip.sh
+++ lang/shell/bash/functions/common/l/locate-gzip.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Need to add updated recipe support for this.
-# https://www.gnu.org/software/gzip/
-
 koopa_locate_gzip() {
     koopa_locate_app \
         --allow-in-path \
