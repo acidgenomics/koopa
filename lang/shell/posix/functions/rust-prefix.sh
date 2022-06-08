d655b350e6230c5276638911f1bf16919a4be2f2
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-08 11:25:09 -0400

    Rework Rust config

--- lang/shell/posix/functions/rust-prefix.sh
+++ lang/shell/posix/functions/rust-prefix.sh
@@ -2,7 +2,7 @@
 
 koopa_rust_prefix() {
     # """
-    # Rust (rustup) install prefix.
+    # Rust install prefix.
     # @note Updated 2021-05-25.
     # """
     koopa_print "$(koopa_opt_prefix)/rust"
