f1a2e507e79c5e8229f672f031463a65408db847
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-13 11:52:49 -0400

    Work on draft asdf install script

--- lang/shell/bash/include/installers/common/shared/install-asdf.sh
+++ lang/shell/bash/include/installers/common/shared/install-asdf.sh
@@ -3,8 +3,19 @@
 main() {
     # """
     # Install asdf.
-    # @note Updated 2022-04-17.
+    # @note Updated 2022-06-13.
     # """
-    koopa_stop 'FIXME Need to add support for this.'
+    local dict
+    declare -A dict=(
+        [name]='asdf'
+        [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
+    )
+    dict[file]="v${dict[version]}.tar.gz"
+    dict[url]="https://github.com/asdf-vm/${dict[name]}/archive/refs/\
+tags/${dict[file]}"
+    koopa_download "${dict[url]}" "${dict[file]}"
+    koopa_extract "${dict[file]}"
+    koopa_cp "${dict[name]}-${dict[version]}" "${dict[prefix]}"
     return 0
 }
