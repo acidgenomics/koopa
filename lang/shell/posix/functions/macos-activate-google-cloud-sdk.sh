5ff86c342b4c0c79caf74e3fe8a01962f7d322bc
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-13 09:52:35 -0400

    Add Google Cloud SDK support

--- lang/shell/posix/functions/macos-activate-google-cloud-sdk.sh
+++ lang/shell/posix/functions/macos-activate-google-cloud-sdk.sh
@@ -3,12 +3,12 @@
 koopa_macos_activate_google_cloud_sdk() {
     # """
     # Activate macOS Google Cloud SDK Homebrew cask.
-    # @note Updated 2022-05-23.
+    # @note Updated 2022-06-13.
     #
     # @seealso
     # - https://cloud.google.com/sdk/docs/install#mac
     # """
-    CLOUDSDK_PYTHON="$(koopa_homebrew_opt_prefix)/python@3.9/bin/python3.9"
+    CLOUDSDK_PYTHON="$(koopa_opt_prefix)/python/bin/python3"
     export CLOUDSDK_PYTHON
     return 0
 }
