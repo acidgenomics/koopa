cf9934c45fea299bcb705bee13d3b0bf0e333e92
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-13 10:08:59 -0400

    Attempt to fix broot installer

--- lang/shell/bash/functions/common/i/install-broot.sh
+++ lang/shell/bash/functions/common/i/install-broot.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 koopa_install_broot() {
+    # """
+    # @seealso
+    # - https://dystroy.org/broot/install/
+    # """
     koopa_install_app \
+        --activate-opt='xorg-xorgproto' \
+        --activate-opt='xorg-xcb-proto' \
+        --activate-opt='xorg-libpthread-stubs' \
+        --activate-opt='xorg-libxau' \
+        --activate-opt='xorg-libxau' \
+        --activate-opt='xorg-libxdmcp' \
+        --activate-opt='xorg-libxcb' \
+        --installer='rust-package' \
         --link-in-bin='bin/broot' \
         --name='broot' \
-        --installer='rust-package' \
         "$@"
 }
