4500efb2809e81fcc14f603e36a64e6f39c2e095
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-14 07:21:27 -0400

    Tighten up man-db install support

--- lang/shell/bash/functions/common/i/install-man-db.sh
+++ lang/shell/bash/functions/common/i/install-man-db.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
 
 koopa_install_man_db() {
-    # """
-    # @seealso
-    # - https://man-db.nongnu.org/development.html
-    # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/man-db.rb
-    # """
     koopa_install_app \
-        --activate-opt='groff' \
-        --activate-opt='libpipeline' \
-        --activate-opt='gdbm' \
-        --installer='gnu-app' \
         --link-in-bin='bin/man' \
         --name='man-db' \
-        -D '--disable-cache-owner' \
-        -D '--disable-dependency-tracking' \
-        -D '--disable-nls' \
-        -D '--disable-setuid' \
-        -D '--disable-silent-rules' \
         "$@"
 }
