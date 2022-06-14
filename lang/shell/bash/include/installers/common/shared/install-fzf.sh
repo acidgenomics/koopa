9dadd7cb413fff1384b53b90ac981f04bcdaa31b
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-14 08:07:02 -0400

    Improve Go permissions cleanup

--- lang/shell/bash/include/installers/common/shared/install-fzf.sh
+++ lang/shell/bash/include/installers/common/shared/install-fzf.sh
@@ -6,32 +6,32 @@
 main() {
     # """
     # Install fzf.
-    # @note Updated 2022-03-30.
+    # @note Updated 2022-06-14.
     # @seealso
     # - https://github.com/junegunn/fzf/blob/master/BUILD.md
     # """
     local app dict
     koopa_assert_has_no_args "$#"
+    koopa_activate_build_opt_prefix 'go'
     declare -A app=(
         [make]="$(koopa_locate_make)"
     )
     declare -A dict=(
+        [gopath]="$(koopa_init_dir 'go')"
         [jobs]="$(koopa_cpu_count)"
         [name]='fzf'
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
-    koopa_activate_opt_prefix 'go'
-    koopa_activate_go
     dict[file]="${dict[version]}.tar.gz"
     dict[url]="https://github.com/junegunn/${dict[name]}/archive/${dict[file]}"
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    export FZF_VERSION="${dict[version]}"
     export FZF_REVISION='tarball'
+    export FZF_VERSION="${dict[version]}"
+    export GOPATH="${dict[gopath]}"
     "${app[make]}" --jobs="${dict[jobs]}"
-    # > "${app[make]}" test
     # This will copy fzf binary from 'target/' to 'bin/'.
     "${app[make]}" install
     # > ./install --help
@@ -39,5 +39,6 @@ main() {
     koopa_cp \
         --target-directory="${dict[prefix]}" \
         'bin' 'doc' 'man' 'plugin' 'shell'
+    koopa_chmod --recursive 'u+rw' "${dict[gopath]}"
     return 0
 }
