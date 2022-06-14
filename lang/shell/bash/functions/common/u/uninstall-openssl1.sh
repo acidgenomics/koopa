44e576e60cce153823f484260415e2cb2163b1a4
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-14 12:54:21 -0400

    Rework openssl config

--- lang/shell/bash/functions/common/u/uninstall-openssl1.sh
+++ lang/shell/bash/functions/common/u/uninstall-openssl1.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-koopa_uninstall_openssl_v1() {
+koopa_uninstall_openssl1() {
     koopa_uninstall_app \
         --name-fancy='OpenSSL (v1)' \
-        --name='openssl-v1' \
+        --name='openssl1' \
         "$@"
 }
