f78fa98dd56348d0213512c8059baf76215a4e8a
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-14 09:06:23 -0400

    Can't build exa

--- lang/shell/bash/functions/common/i/install-exa.sh
+++ lang/shell/bash/functions/common/i/install-exa.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# NOTE 0.10.0 currently fails to build with Rust 1.61.0.
+# https://github.com/ogham/exa/issues/1068
+
 koopa_install_exa() {
     koopa_install_app \
         --link-in-bin='bin/exa' \
