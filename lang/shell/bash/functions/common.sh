a6c43eadad0cc6cab688c5396c8c61ccfc1d27ca
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-15 14:02:49 -0400

    Improve support for version lookup via pkgconfig

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -8570,15 +8570,29 @@ koopa_gcrypt_url() {
 }
 
 koopa_get_version_from_pkg_config() {
-    local app pkg str
+    local app dict
     koopa_assert_has_args_eq "$#" 1
-    pkg="${1:?}"
     declare -A app=(
         [pkg_config]="$(koopa_locate_pkg_config)"
     )
-    str="$("${app[pkg_config]}" --modversion "$pkg")"
-    [[ -n "$str" ]] || return 1
-    koopa_print "$str"
+    declare -A dict=(
+        [opt_prefix]="$(koopa_opt_prefix)"
+        [pkg]="${1:?}"
+        [pkg_config_path]="${PKG_CONFIG_PATH:-}"
+    )
+    dict[pkgconfig]="${dict[opt_prefix]}/${dict[pkg]}/lib/pkgconfig"
+    [[ -d "${dict[pkgconfig]}" ]] || return 1
+    koopa_add_to_pkg_config_path "${dict[pkgconfig]}"
+    dict[str]="$("${app[pkg_config]}" --modversion "${dict[pkg]}")"
+    if [[ -n "${dict[pkg_config_path]}" ]]
+    then
+        PKG_CONFIG_PATH="${dict[pkg_config_path]}"
+        export PKG_CONFIG_PATH
+    else
+        unset -v PKG_CONFIG_PATH
+    fi
+    [[ -n "${dict[str]}" ]] || return 1
+    koopa_print "${dict[str]}"
     return 0
 }
 
--- lang/shell/bash/functions/common/g/get-version-from-pkg-config.sh
+++ lang/shell/bash/functions/common/g/get-version-from-pkg-config.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 
+# NOTE Consider supporting different opt prefix name here.
+
 koopa_get_version_from_pkg_config() {
     # """
-    # Get a library version via pkg-config.
-    # @note Updated 2022-02-27.
+    # Get a library version via 'pkg-config'.
+    # @note Updated 2022-06-15.
     # """
-    local app pkg str
+    local app dict
     koopa_assert_has_args_eq "$#" 1
-    pkg="${1:?}"
     declare -A app=(
         [pkg_config]="$(koopa_locate_pkg_config)"
     )
-    str="$("${app[pkg_config]}" --modversion "$pkg")"
-    [[ -n "$str" ]] || return 1
-    koopa_print "$str"
+    declare -A dict=(
+        [opt_prefix]="$(koopa_opt_prefix)"
+        [pkg]="${1:?}"
+        [pkg_config_path]="${PKG_CONFIG_PATH:-}"
+    )
+    dict[pkgconfig]="${dict[opt_prefix]}/${dict[pkg]}/lib/pkgconfig"
+    [[ -d "${dict[pkgconfig]}" ]] || return 1
+    koopa_add_to_pkg_config_path "${dict[pkgconfig]}"
+    dict[str]="$("${app[pkg_config]}" --modversion "${dict[pkg]}")"
+    if [[ -n "${dict[pkg_config_path]}" ]]
+    then
+        PKG_CONFIG_PATH="${dict[pkg_config_path]}"
+        export PKG_CONFIG_PATH
+    else
+        unset -v PKG_CONFIG_PATH
+    fi
+    [[ -n "${dict[str]}" ]] || return 1
+    koopa_print "${dict[str]}"
     return 0
 }
