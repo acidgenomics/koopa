f76a15ebffbabf4f908328efb9efe45b811e242a
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-15 10:32:42 -0400

    Add FIXMEs regarding conda usage

--- lang/shell/bash/functions/common/l/locate-efetch.sh
+++ lang/shell/bash/functions/common/l/locate-efetch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Rework as app.
+
 koopa_locate_efetch() {
     koopa_locate_conda_app \
         --app-name='efetch' \
--- lang/shell/bash/functions/common/l/locate-gs.sh
+++ lang/shell/bash/functions/common/l/locate-gs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Rework as app.
+
 koopa_locate_gs() {
     koopa_locate_conda_app \
         --app-name='gs' \
