9c4fad3b4ea1fc4e94542a67a21690aafc0bc8b6
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-20 15:11:05 -0400

    Add bedtools install support

--- lang/shell/bash/functions/common/l/locate-bedtools.sh
+++ lang/shell/bash/functions/common/l/locate-bedtools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# FIXME Rework as app.
-
 koopa_locate_bedtools() {
-    koopa_locate_conda_app 'bedtools'
+    koopa_locate_app \
+        --app-name='bedtools' \
+        --opt-name='bedtools'
 }
