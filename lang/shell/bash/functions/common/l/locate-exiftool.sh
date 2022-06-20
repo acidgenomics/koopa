d3d2de192138c453336e5021f95af075e6e80320
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-20 17:27:40 -0400

    Add FIXMEs regarding Perl packages

--- lang/shell/bash/functions/common/l/locate-exiftool.sh
+++ lang/shell/bash/functions/common/l/locate-exiftool.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# FIXME Rework this to split out to separate opt prefix.
+
 koopa_locate_exiftool() {
     koopa_locate_app \
         --app-name='exiftool' \
-        --opt-name='exiftool'
+        --opt-name='perl-packages'
 }
--- lang/shell/bash/functions/common/l/locate-rename.sh
+++ lang/shell/bash/functions/common/l/locate-rename.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Split this out to separate opt prefix.
+
 koopa_locate_rename() {
     koopa_locate_app \
         --app-name='rename' \
