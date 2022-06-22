854112956d39ffb8bae27761a94df625906c2e94
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-22 13:12:30 -0400

    Improve sra-tools install support

--- lang/shell/bash/functions/common/l/locate-exiftool.sh
+++ lang/shell/bash/functions/common/l/locate-exiftool.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# FIXME Rework this to split out to separate opt prefix.
-
 koopa_locate_exiftool() {
     koopa_locate_app \
         --app-name='exiftool' \
-        --opt-name='perl-packages'
+        --opt-name='exiftool'
 }
--- lang/shell/bash/functions/common/l/locate-rename.sh
+++ lang/shell/bash/functions/common/l/locate-rename.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# FIXME Split this out to separate opt prefix.
-
 koopa_locate_rename() {
     koopa_locate_app \
         --app-name='rename' \
-        --koopa-opt-name='perl-packages'
+        --opt-name='rename'
 }
