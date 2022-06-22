520ac112dc5a3e493610c76f562dc2842938b0d8
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-22 11:31:11 -0400

    Improve Doom Emacs and Spacemacs update config

--- lang/shell/bash/functions/common/i/install-libtool.sh
+++ lang/shell/bash/functions/common/i/install-libtool.sh
@@ -3,6 +3,8 @@
 koopa_install_libtool() {
     koopa_install_app \
         --installer='gnu-app' \
+        --link-in-bin='bin/libtool' \
+        --link-in-bin='bin/libtoolize' \
         --name='libtool' \
         "$@"
 }
