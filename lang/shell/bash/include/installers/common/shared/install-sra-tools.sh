037e324f7d12e89529db3573a095eaf9fecf3b81
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-22 15:04:57 -0400

    Draft update that attempts to also install ngs-tools

--- lang/shell/bash/include/installers/common/shared/install-sra-tools.sh
+++ lang/shell/bash/include/installers/common/shared/install-sra-tools.sh
@@ -7,7 +7,7 @@ main() {
     #
     # @seealso
     # - https://github.com/ncbi/sra-tools/wiki/
-    # - https://github.com/ncbi/ncbi-vdb
+    # - https://github.com/ncbi/ncbi-vdb/wiki/
     # - https://github.com/ncbi/ngs-tools
     # - https://hpc.nih.gov/apps/sratoolkit.html
     # - https://github.com/Homebrew/homebrew-core/blob/master/
@@ -17,59 +17,90 @@ main() {
     koopa_assert_has_no_args "$#"
     koopa_activate_build_opt_prefix 'cmake'
     koopa_activate_opt_prefix 'hdf5' 'libxml2' 'python'
-    # Need to use HDF5 1.10 API.
-    export CFLAGS="-DH5_USE_110_API ${CFLAGS:-}"
     declare -A app=(
         [cmake]="$(koopa_locate_cmake)"
     )
     [[ -x "${app[cmake]}" ]] || return 1
     declare -A dict=(
         [base_url]='https://github.com/ncbi'
-        [jobs]="$(koopa_cpu_count)"
+        [opt_prefix]="$(koopa_opt_prefix)"
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
-    # Build NCBI VDB Software Development Kit.
-    dict[name]='ncbi-vdb'
-    dict[file]="${dict[name]}-${dict[version]}.tar.gz"
-    dict[url]="${dict[base_url]}/${dict[name]}/archive/refs/tags/\
+    # CMake configuration will pick up Python Framework on macOS unless we
+    # set this manually.
+    app[python]="$(koopa_realpath "${dict[opt_prefix]}/python/bin/python3")"
+    # Need to use HDF5 1.10 API.
+    export CFLAGS="-DH5_USE_110_API ${CFLAGS:-}"
+    # Build NCBI VDB Software Development Kit (no install).
+    (
+        local dict2
+        declare -A dict2
+        dict2[name]='ncbi-vdb'
+        dict2[file]="${dict2[name]}-${dict[version]}.tar.gz"
+        dict2[url]="${dict[base_url]}/${dict2[name]}/archive/refs/tags/\
 ${dict[version]}.tar.gz"
-    koopa_download "${dict[url]}" "${dict[file]}"
-    koopa_extract "${dict[file]}"
-    "${app[cmake]}" \
-        -S "${dict[name]}-${dict[version]}" \
-        -B "${dict[name]}-${dict[version]}-build" \
-        -DCMAKE_INSTALL_PREFIX="${dict[prefix]}"
-    "${app[cmake]}" --build "${dict[name]}-${dict[version]}-build"
+        koopa_download "${dict2[url]}" "${dict2[file]}"
+        koopa_extract "${dict2[file]}"
+        "${app[cmake]}" \
+            -S "${dict2[name]}-${dict[version]}" \
+            -B "${dict2[name]}-${dict[version]}-build" \
+            -DCMAKE_INSTALL_PREFIX="${dict[prefix]}" \
+            -DPython3_EXECUTABLE="${app[python]}"
+        "${app[cmake]}" --build "${dict2[name]}-${dict[version]}-build"
+    )
     dict[ncbi_vdb_build]="$( \
-        koopa_realpath "${dict[name]}-${dict[version]}-build" \
+        koopa_realpath "ncbi-vdb-${dict[version]}-build" \
     )"
     dict[ncbi_vdb_source]="$( \
-        koopa_realpath "${dict[name]}-${dict[version]}" \
+        koopa_realpath "ncbi-vdb-${dict[version]}" \
     )"
     # Build and install NCBI SRA Toolkit.
-    dict[name]='sra-tools'
-    dict[file]="${dict[name]}-${dict[version]}.tar.gz"
-    dict[url]="${dict[base_url]}/${dict[name]}/archive/refs/tags/\
+    (
+        local dict2
+        declare -A dict2
+        dict2[name]='sra-tools'
+        dict2[file]="${dict2[name]}-${dict[version]}.tar.gz"
+        dict2[url]="${dict[base_url]}/${dict2[name]}/archive/refs/tags/\
 ${dict[version]}.tar.gz"
-    koopa_download "${dict[url]}" "${dict[file]}"
-    koopa_extract "${dict[file]}"
-    # Need to fix '/obj/ngs/ngs-java' path issue in 'CMakeLists.txt' file.
-    koopa_find_and_replace_in_file \
-        --fixed \
-        --pattern='/obj/ngs/ngs-java/' \
-        --replacement='/ngs/ngs-java/' \
-        "${dict[name]}-${dict[version]}/ngs/ngs-java/CMakeLists.txt"
-    "${app[cmake]}" \
-        -S "${dict[name]}-${dict[version]}" \
-        -B "${dict[name]}-${dict[version]}-build" \
-        -DCMAKE_INSTALL_PREFIX="${dict[prefix]}" \
-        -DVDB_BINDIR="${dict[ncbi_vdb_build]}" \
-        -DVDB_INCDIR="${dict[ncbi_vdb_source]}/interfaces" \
-        -DVDB_LIBDIR="${dict[ncbi_vdb_build]}/lib"
-    "${app[cmake]}" --build "${dict[name]}-${dict[version]}-build"
-    "${app[cmake]}" --install "${dict[name]}-${dict[version]}-build"
-    # FIXME Install NCBI NGS Toolkit.
-    # > dict[url]="${dict[base_url]}/ngs-tools/archive/refs/tags/${dict[file]}"
+        koopa_download "${dict2[url]}" "${dict[file]}"
+        koopa_extract "${dict[file]}"
+        # Need to fix '/obj/ngs/ngs-java' path issue in 'CMakeLists.txt' file.
+        # See related: https://github.com/ncbi/sra-tools/pull/664/files
+        koopa_find_and_replace_in_file \
+            --fixed \
+            --pattern='/obj/ngs/ngs-java/' \
+            --replacement='/ngs/ngs-java/' \
+            "${dict[name]}-${dict[version]}/ngs/ngs-java/CMakeLists.txt"
+        "${app[cmake]}" \
+            -S "${dict[name]}-${dict[version]}" \
+            -B "${dict[name]}-${dict[version]}-build" \
+            -DCMAKE_INSTALL_PREFIX="${dict[prefix]}" \
+            -DVDB_BINDIR="${dict[ncbi_vdb_build]}" \
+            -DVDB_INCDIR="${dict[ncbi_vdb_source]}/interfaces" \
+            -DVDB_LIBDIR="${dict[ncbi_vdb_build]}/lib"
+        "${app[cmake]}" --build "${dict[name]}-${dict[version]}-build"
+        "${app[cmake]}" --install "${dict[name]}-${dict[version]}-build"
+    )
+    # Install NCBI NGS Toolkit.
+    (
+        local dict2
+        declare -A dict2
+        dict2[name]='ngs-tools'
+        dict2[file]="${dict2[name]}-${dict[version]}.tar.gz"
+        dict2[url]="${dict[base_url]}/${dict2[name]}/archive/refs/tags/\
+${dict[version]}.tar.gz"
+        koopa_download "${dict2[url]}" "${dict[file]}"
+        koopa_extract "${dict[file]}"
+        "${app[cmake]}" \
+            -S "${dict[name]}-${dict[version]}" \
+            -B "${dict[name]}-${dict[version]}-build" \
+            -DCMAKE_INSTALL_PREFIX="${dict[prefix]}" \
+            -DSRATOOLS_BINDIR="${dict[prefix]}" \
+            -DVDB_INCDIR="${dict[ncbi_vdb_source]}/interfaces" \
+            -DVDB_LIBDIR="${dict[ncbi_vdb_build]}/lib"
+        "${app[cmake]}" --build "${dict[name]}-${dict[version]}-build"
+        "${app[cmake]}" --install "${dict[name]}-${dict[version]}-build"
+    )
     return 0
 }
