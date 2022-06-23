e57e4c4eaf2615f3867d5af0851bb82c1572dcf4
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-23 09:24:32 -0400

    Consider requiring Perl

--- lang/shell/bash/functions/common/i/install-koopa.sh
+++ lang/shell/bash/functions/common/i/install-koopa.sh
@@ -3,9 +3,10 @@
 koopa_install_koopa() {
     # """
     # Install koopa.
-    # @note Updated 2022-05-12.
+    # @note Updated 2022-06-23.
     # """
     local bool dict
+    # NOTE Consider adding 'perl' as a requirement here.
     koopa_assert_is_installed 'cp' 'curl' 'find' 'git' 'grep' 'mkdir' \
         'mktemp' 'mv' 'readlink' 'rm' 'sed' 'tar' 'unzip'
     declare -A bool=(
