1c4fe5e575ae84978fb82f82778c737c6f66f3d4
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-08 14:47:46 -0400

    Rework Ruby package configuration

--- lang/shell/bash/functions/common/u/uninstall-ruby.sh
+++ lang/shell/bash/functions/common/u/uninstall-ruby.sh
@@ -4,6 +4,8 @@ koopa_uninstall_ruby() {
     koopa_uninstall_app \
         --name-fancy='Ruby' \
         --name='ruby' \
+        --unlink-in-bin='bundle' \
+        --unlink-in-bin='bundler' \
         --unlink-in-bin='gem' \
         --unlink-in-bin='ruby' \
         "$@"
