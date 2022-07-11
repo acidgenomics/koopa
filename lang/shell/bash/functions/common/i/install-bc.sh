ac9860ddbfa9ec3aeab267b7e8d0545bf4105d6a
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-11 08:45:18 -0400

    Need to include texinfo

--- lang/shell/bash/functions/common/i/install-bc.sh
+++ lang/shell/bash/functions/common/i/install-bc.sh
@@ -2,6 +2,7 @@
 
 koopa_install_bc() {
     koopa_install_app \
+        --activate-build-opt-prefix='texinfo' \
         --installer='gnu-app' \
         --link-in-bin='bin/bc' \
         --name='bc' \
