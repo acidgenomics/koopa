55d5b6c857e747307b3e02aaed0637bb446d25c1
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-11 08:09:26 -0400

    Ensure setuptools and wheel is always installed

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -17109,6 +17109,10 @@ ${dict[py_maj_min_ver]}"
     "${app[python]}" -m venv "${venv_args[@]}"
     app[venv_python]="${dict[prefix]}/bin/python${dict[py_maj_min_ver]}"
     koopa_assert_is_installed "${app[venv_python]}"
+    koopa_python_pip_install \
+        --python="${app[venv_python]}" \
+        'setuptools==63.1.0' \
+        'wheel==0.37.1'
     if koopa_is_array_non_empty "${pkgs[@]:-}"
     then
         koopa_python_pip_install --python="${app[venv_python]}" "${pkgs[@]}"
