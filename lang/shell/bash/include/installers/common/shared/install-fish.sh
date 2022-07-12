3bfde8bedfd9abd77decc1b96f2601a845619317
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-12 10:17:34 -0400

    Need to improve library location

--- lang/shell/bash/include/installers/common/shared/install-fish.sh
+++ lang/shell/bash/include/installers/common/shared/install-fish.sh
@@ -11,7 +11,7 @@ main() {
     local app cmake_args dict
     koopa_assert_has_no_args "$#"
     koopa_activate_build_opt_prefix 'cmake'
-    koopa_activate_opt_prefix 'ncurses' 'pcre2'
+    koopa_activate_opt_prefix 'ncurses'
     declare -A app=(
         [cmake]="$(koopa_locate_cmake)"
     )
@@ -24,6 +24,12 @@ main() {
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
+    if koopa_is_macos
+    then
+        dict[shared_ext]='dylib'
+    else
+        dict[shared_ext]='so'
+    fi
     dict[ncurses]="$(koopa_realpath "${dict[opt_prefix]}/ncurses")"
     dict[file]="${dict[name]}-${dict[version]}.tar.xz"
     dict[url]="https://github.com/${dict[name]}-shell/${dict[name]}-shell/\
@@ -34,7 +40,7 @@ releases/download/${dict[version]}/${dict[file]}"
     cmake_args=(
         "-DCMAKE_INSTALL_PREFIX=${dict[prefix]}"
         "-DCURSES_INCLUDE_PATH=${dict[ncurses]}/include"
-        "-DCURSES_LIBRARY=${dict[ncurses]}/lib"
+        "-DCURSES_LIBRARY=${dict[ncurses]}/lib/libncursesw.${dict[shared_ext]}"
     )
     "${app[cmake]}" \
         -S '.' \
