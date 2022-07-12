892f413fffd1b3202f13005a8a6b748eaef7d0a0
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-12 07:51:49 -0400

    Remove javah references

---
+++ lang/shell/bash/functions/common/i/install-graphviz.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_install_graphfiz() {
+    koopa_install_app \
+        --name='graphviz' \
+        "$@"
+}
---
+++ lang/shell/bash/functions/common/u/uninstall-graphviz.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_uninstall_graphviz() {
+    koopa_uninstall_app \
+        --name='graphviz' \
+        "$@"
+}
