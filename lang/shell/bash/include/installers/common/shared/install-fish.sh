876020e2d4d95ed4075f1ba8531b4c20e8e27875
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-12 10:10:43 -0400

    Need to locate ncurses

--- lang/shell/bash/include/installers/common/shared/install-fish.sh
+++ lang/shell/bash/include/installers/common/shared/install-fish.sh
@@ -3,12 +3,12 @@
 main() {
     # """
     # Install Fish shell.
-    # @note Updated 2022-04-08.
+    # @note Updated 2022-07-12.
     #
     # @seealso
     # - https://github.com/fish-shell/fish-shell/#building
     # """
-    local app dict
+    local app cmake_args dict
     koopa_assert_has_no_args "$#"
     koopa_activate_build_opt_prefix 'cmake'
     koopa_activate_opt_prefix 'ncurses' 'pcre2'
@@ -20,19 +20,25 @@ main() {
         [jobs]="$(koopa_cpu_count)"
         [link_in_bin]="${INSTALL_LINK_IN_BIN:?}"
         [name]='fish'
+        [opt_prefix]="$(koopa_opt_prefix)"
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
+    dict[ncurses]="$(koopa_realpath "${dict[opt_prefix]}/ncurses")"
     dict[file]="${dict[name]}-${dict[version]}.tar.xz"
     dict[url]="https://github.com/${dict[name]}-shell/${dict[name]}-shell/\
 releases/download/${dict[version]}/${dict[file]}"
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
+    cmake_args=(
+        "-DCMAKE_INSTALL_PREFIX=${dict[prefix]}"
+        "-DCURSES_INCLUDE_PATH=${dict[ncurses]}/include"
+    )
     "${app[cmake]}" \
         -S '.' \
         -B 'build' \
-        -DCMAKE_INSTALL_PREFIX="${dict[prefix]}"
+        "${cmake_args[@]}"
     "${app[cmake]}" \
         --build 'build' \
         --parallel "${dict[jobs]}"
