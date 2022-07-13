b8692af2499f195925ecbae4b5a492bc3043d841
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-12 20:22:42 -0400

    Attempt to debug

--- lang/shell/bash/include/installers/common/shared/install-serf.sh
+++ lang/shell/bash/include/installers/common/shared/install-serf.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Can we use CMake here instead with CMakeList.txt reference?
+# FIXME Failing to locate 'zlib.h' inside of Ubuntu here.
+
 main() {
     # """
     # Install Apache Serf.
@@ -9,10 +12,11 @@ main() {
     #
     # @seealso
     # - https://serf.apache.org/download
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/
+    #     subversion.rb
     # - https://www.linuxfromscratch.org/blfs/view/svn/basicnet/serf.html
     # - https://github.com/apache/serf/blob/trunk/README
-    # - https://raw.githubusercontent.com/Homebrew/homebrew-core/
-    #     0c3d786402ad7d9dd5eb6907e3ed3f2525a0472d/Formula/subversion.rb
+    # - https://fossies.org/diffs/serf/1.3.8_vs_1.3.9/README-diff.html
     # """
     local app dict scons_args
     koopa_assert_has_no_args "$#"
@@ -48,23 +52,36 @@ serf-1.3.9-openssl3_fixes-1.patch"
     "${app[patch]}" -Np1 -i 'serf-1.3.9-openssl3_fixes-1.patch'
     # These steps require GNU sed.
     # Alternatively, can consider using Perl approach here instead.
+    # This is modified from Linux From Scratch tutorial article.
     "${app[sed]}" -i.bak "/Append/s:RPATH=libdir,::" 'SConstruct'
     "${app[sed]}" -i.bak "/Default/s:lib_static,::"  'SConstruct'
     "${app[sed]}" -i.bak "/Alias/s:install_static,::" 'SConstruct'
     "${app[sed]}" -i.bak "/  print/{s/print/print(/; s/$/)/}" 'SConstruct'
     "${app[sed]}" -i.bak "/get_contents()/s/,/.decode()&/" 'SConstruct'
+
+    if koopa_is_linux
+    then
+        # FIXME Need to fix detection of ZLIB.
+        # > inreplace "SConstruct" do |s|
+        # >   s.gsub! "env.Append(LIBPATH=['$OPENSSL\/lib'])",
+        # >   "\\1\nenv.Append(CPPPATH=['$ZLIB\/include'])\nenv.Append(LIBPATH=['$ZLIB/lib'])"
+        # > end
+    fi
+
     # NOTE Refer to 'SConstruct' file for supported arguments.
     scons_args=(
         "APR=${dict[opt_prefix]}/apr"
         "APU=${dict[opt_prefix]}/apr-util"
-        'CC=gcc'
+        # > 'CC=gcc'
         "CFLAGS=${CFLAGS:-}"
         # > "DEBUG=true"
         # > 'GSSAPI=/usr'
-        "LIBS=${LD_LIBRARY_PATH:-}"
+        # > "LIBS=${LD_LIBRARY_PATH:-}"
+        'LIBDIR=lib'
         "LINKFLAGS=${LDFLAGS:-}"
         "OPENSSL=${dict[opt_prefix]}/openssl3"
         "PREFIX=${dict[prefix]}"
+        "SOURCE_LAYOUT=true"
     )
     if koopa_is_linux
     then
