e483eaf12dfee3a7bdb352e14fa4840ed1ac22ce
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-12 20:36:45 -0400

    Draft update

--- lang/shell/bash/include/installers/common/shared/install-serf.sh
+++ lang/shell/bash/include/installers/common/shared/install-serf.sh
@@ -20,7 +20,6 @@ main() {
     # """
     local app dict scons_args
     koopa_assert_has_no_args "$#"
-    koopa_activate_build_opt_prefix 'patch'
     if koopa_is_linux
     then
         koopa_activate_opt_prefix 'zlib'
@@ -28,12 +27,10 @@ main() {
     koopa_activate_opt_prefix \
         'apr' \
         'apr-util' \
-        'openssl3' \
+        'openssl1' \
         'scons'
     declare -A app=(
-        [patch]="$(koopa_locate_patch)"
         [scons]="$(koopa_locate_scons)"
-        [sed]="$(koopa_locate_sed)"
     )
     declare -A dict=(
         [name]='serf'
@@ -46,28 +43,25 @@ main() {
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    # Download patch required for OpenSSL 3 compatibility.
-    koopa_download "https://www.linuxfromscratch.org/patches/blfs/svn/\
-serf-1.3.9-openssl3_fixes-1.patch"
-    "${app[patch]}" -Np1 -i 'serf-1.3.9-openssl3_fixes-1.patch'
-    # These steps require GNU sed.
-    # Alternatively, can consider using Perl approach here instead.
-    # This is modified from Linux From Scratch tutorial article.
-    "${app[sed]}" -i.bak "/Append/s:RPATH=libdir,::" 'SConstruct'
-    "${app[sed]}" -i.bak "/Default/s:lib_static,::"  'SConstruct'
-    "${app[sed]}" -i.bak "/Alias/s:install_static,::" 'SConstruct'
-    "${app[sed]}" -i.bak "/  print/{s/print/print(/; s/$/)/}" 'SConstruct'
-    "${app[sed]}" -i.bak "/get_contents()/s/,/.decode()&/" 'SConstruct'
-
-    # FIXME May need to apply this approach used by Homebrew.
-    # > if koopa_is_linux
-    # > then
-        # FIXME Need to fix detection of ZLIB.
-        # > inreplace "SConstruct" do |s|
-        # >   s.gsub! "env.Append(LIBPATH=['$OPENSSL\/lib'])",
-        # >   "\\1\nenv.Append(CPPPATH=['$ZLIB\/include'])\nenv.Append(LIBPATH=['$ZLIB/lib'])"
-        # > end
-    # > fi
+    koopa_find_and_replace_in_file \
+        --fixed \
+        --pattern="print 'Warning: Used unknown variables:', ', '.join(unknown.keys())" \
+        --replacement="print('Warning: Used unknown variables:', ', '.join(unknown.keys()))" \
+        'SConstruct'
+    koopa_find_and_replace_in_file \
+        --fixed \
+        --pattern='variables=opts,' \
+        --replacement="variables=opts, RPATHPREFIX = '-Wl,-rpath,'," \
+        'SConstruct'
+    if koopa_is_linux
+    then
+        koopa_find_and_replace_in_file \
+            --fixed \
+            --pattern="env.Append(LIBPATH=['\$OPENSSL/lib'])" \
+            --replacement="\\1\nenv.Append(CPPPATH=['\$ZLIB/include'])\nenv.Append(LIBPATH=['\$ZLIB/lib'])" \
+            'SConstruct'
+    fi
+    koopa_stop "${PWD} FIXME"
 
     # NOTE Refer to 'SConstruct' file for supported arguments.
     scons_args=(
@@ -75,14 +69,11 @@ serf-1.3.9-openssl3_fixes-1.patch"
         "APU=${dict[opt_prefix]}/apr-util"
         # > 'CC=gcc'
         "CFLAGS=${CFLAGS:-}"
-        # > "DEBUG=true"
-        # > 'GSSAPI=/usr'
         # > "LIBS=${LD_LIBRARY_PATH:-}"
         'LIBDIR=lib'
         "LINKFLAGS=${LDFLAGS:-}"
-        "OPENSSL=${dict[opt_prefix]}/openssl3"
+        "OPENSSL=${dict[opt_prefix]}/openssl1"
         "PREFIX=${dict[prefix]}"
-        "SOURCE_LAYOUT=true"
     )
     if koopa_is_linux
     then
