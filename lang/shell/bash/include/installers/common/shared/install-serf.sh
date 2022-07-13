0188542c1f5dfa0f7bc2c62a14367f67a6858099
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-13 08:13:18 -0400

    Attempt to use GNU patch to fix install on Linux

--- lang/shell/bash/include/installers/common/shared/install-serf.sh
+++ lang/shell/bash/include/installers/common/shared/install-serf.sh
@@ -17,9 +17,14 @@ main() {
     # - https://www.linuxfromscratch.org/blfs/view/svn/basicnet/serf.html
     # - https://github.com/apache/serf/blob/trunk/README
     # - https://fossies.org/diffs/serf/1.3.8_vs_1.3.9/README-diff.html
+    # - https://www.howtogeek.com/415442/
+    #     how-to-apply-a-patch-to-a-file-and-create-patches-in-linux/
+    # - https://docs.moodle.org/dev/How_to_create_a_patch
+    # - https://docs.moodle.org/dev/How_to_apply_a_patch
     # """
     local app dict scons_args
     koopa_assert_has_no_args "$#"
+    koopa_activate_build_opt_prefix 'patch'
     if koopa_is_linux
     then
         koopa_activate_opt_prefix 'zlib'
@@ -30,8 +35,11 @@ main() {
         'openssl1' \
         'scons'
     declare -A app=(
+        [patch]="$(koopa_locate_patch)"
         [scons]="$(koopa_locate_scons)"
     )
+    [[ -x "${app[patch]}" ]] || return 1
+    [[ -x "${app[scons]}" ]] || return 1
     declare -A dict=(
         [name]='serf'
         [opt_prefix]="$(koopa_opt_prefix)"
@@ -43,35 +51,57 @@ main() {
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    koopa_find_and_replace_in_file \
-        --fixed \
-        --pattern='variables=opts,' \
-        --replacement="variables=opts, RPATHPREFIX = '-Wl,-rpath,'," \
-        'SConstruct'
-    # FIXME Seems like Perl isn't hardened enough for this...
-    koopa_find_and_replace_in_file \
-        --fixed \
-        --pattern="print 'Warning: Used unknown variables:', ', '.join(unknown.keys())" \
-        --replacement="print('Warning: Used unknown variables:', ', '.join(unknown.keys()))" \
-        'SConstruct'
-    # FIXME This doesn't seem to work with Perl engine...another approach?
+    # Patch diff created with:
+    # > diff -Naur 'SConstruct' 'SConstruct-1' > 'sconstruct-1.patch'
+    cat << END > 'sconstruct-1.patch'
+--- SConstruct	2022-07-13 07:51:34.000000000 -0400
++++ SConstruct-1	2022-07-13 07:51:14.000000000 -0400
+@@ -152,7 +152,7 @@
+                  True),
+     )
+ 
+-env = Environment(variables=opts,
++env = Environment(variables=opts, RPATHPREFIX = '-Wl,-rpath,',
+                   tools=('default', 'textfile',),
+                   CPPPATH=['.', ],
+                   )
+@@ -183,7 +183,7 @@
+ 
+ unknown = opts.UnknownVariables()
+ if unknown:
+-  print 'Warning: Used unknown variables:', ', '.join(unknown.keys())
++  print('Warning: Used unknown variables:', ', '.join(unknown.keys()))
+ 
+ apr = str(env['APR'])
+ apu = str(env['APU'])
+END
+    "${app[patch]}" -u 'SConstruct' -i 'sconstruct-1.patch'
     if koopa_is_linux
     then
-        koopa_find_and_replace_in_file \
-            --regex \
-            --pattern="env.Append(LIBPATH=['\$OPENSSL/lib'])" \
-            --replacement="env.Append(LIBPATH=['\$OPENSSL/lib'])\nenv.Append(CPPPATH=['\$ZLIB/include'])\nenv.Append(LIBPATH=['\$ZLIB/lib'])" \
-            'SConstruct'
+        # Patch diff created with:
+        # > diff -Naur 'SConstruct-1' 'SConstruct-2' > 'sconstruct-2.patch'
+        cat << END > 'sconstruct-2.patch'
+--- SConstruct-1	2022-07-13 07:51:14.000000000 -0400
++++ SConstruct-2	2022-07-13 07:53:59.000000000 -0400
+@@ -372,6 +372,8 @@
+ 
+   env.Append(CPPPATH=['$OPENSSL/include'])
+   env.Append(LIBPATH=['$OPENSSL/lib'])
++  env.Append(CPPPATH=['$ZLIB\/include'])
++  env.Append(LIBPATH=['$ZLIB/lib'])
+ 
+ 
+ # If build with gssapi, get its information and define SERF_HAVE_GSSAPI
+END
+        "${app[patch]}" -u 'SConstruct' 'sconstruct-2.patch'
     fi
-    koopa_stop "${PWD} FIXME"
-
-    # NOTE Refer to 'SConstruct' file for supported arguments.
+    # Refer to 'SConstruct' file for supported arguments.
     scons_args=(
+        # > 'CC=gcc'
+        # > "LIBS=${LD_LIBRARY_PATH:-}"
         "APR=${dict[opt_prefix]}/apr"
         "APU=${dict[opt_prefix]}/apr-util"
-        # > 'CC=gcc'
         "CFLAGS=${CFLAGS:-}"
-        # > "LIBS=${LD_LIBRARY_PATH:-}"
         'LIBDIR=lib'
         "LINKFLAGS=${LDFLAGS:-}"
         "OPENSSL=${dict[opt_prefix]}/openssl1"
