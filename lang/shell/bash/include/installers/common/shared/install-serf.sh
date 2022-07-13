c900e88e004a65eac0f8d3eb238cecf7eeaab966
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-12 20:03:53 -0400

    Attempt to harden

--- lang/shell/bash/include/installers/common/shared/install-serf.sh
+++ lang/shell/bash/include/installers/common/shared/install-serf.sh
@@ -11,6 +11,8 @@ main() {
     # - https://serf.apache.org/download
     # - https://www.linuxfromscratch.org/blfs/view/svn/basicnet/serf.html
     # - https://github.com/apache/serf/blob/trunk/README
+    # - https://raw.githubusercontent.com/Homebrew/homebrew-core/
+    #     0c3d786402ad7d9dd5eb6907e3ed3f2525a0472d/Formula/subversion.rb
     # """
     local app dict scons_args
     koopa_assert_has_no_args "$#"
@@ -22,7 +24,7 @@ main() {
     koopa_activate_opt_prefix \
         'apr' \
         'apr-util' \
-        'openssl1' \
+        'openssl3' \
         'scons'
     declare -A app=(
         [patch]="$(koopa_locate_patch)"
@@ -41,22 +43,25 @@ main() {
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
     # Download patch required for OpenSSL 3 compatibility.
-    # > koopa_download "https://www.linuxfromscratch.org/patches/blfs/svn/\
-# > serf-1.3.9-openssl3_fixes-1.patch"
-    # > "${app[patch]}" -Np1 -i 'serf-1.3.9-openssl3_fixes-1.patch'
+    koopa_download "https://www.linuxfromscratch.org/patches/blfs/svn/\
+serf-1.3.9-openssl3_fixes-1.patch"
+    "${app[patch]}" -Np1 -i 'serf-1.3.9-openssl3_fixes-1.patch'
     # These steps require GNU sed.
     # Alternatively, can consider using Perl approach here instead.
-    # > "${app[sed]}" -i.bak "/Append/s:RPATH=libdir,::" 'SConstruct'
-    # > "${app[sed]}" -i.bak "/Default/s:lib_static,::"  'SConstruct'
-    # > "${app[sed]}" -i.bak "/Alias/s:install_static,::" 'SConstruct'
-    # > "${app[sed]}" -i.bak "/  print/{s/print/print(/; s/$/)/}" 'SConstruct'
-    # > "${app[sed]}" -i.bak "/get_contents()/s/,/.decode()&/" 'SConstruct'
+    "${app[sed]}" -i.bak "/Append/s:RPATH=libdir,::" 'SConstruct'
+    "${app[sed]}" -i.bak "/Default/s:lib_static,::"  'SConstruct'
+    "${app[sed]}" -i.bak "/Alias/s:install_static,::" 'SConstruct'
+    "${app[sed]}" -i.bak "/  print/{s/print/print(/; s/$/)/}" 'SConstruct'
+    "${app[sed]}" -i.bak "/get_contents()/s/,/.decode()&/" 'SConstruct'
     # NOTE Refer to 'SConstruct' file for supported arguments.
     scons_args=(
         "APR=${dict[opt_prefix]}/apr"
         "APU=${dict[opt_prefix]}/apr-util"
-        # > 'LIBDIR=lib'
-        "OPENSSL=${dict[opt_prefix]}/openssl1"
+        'CC=gcc'
+        "CFLAGS=${CFLAGS:-}"
+        'GSSAPI=/usr'
+        "LINKFLAGS=${LDFLAGS:-}"
+        "OPENSSL=${dict[opt_prefix]}/openssl3"
         "PREFIX=${dict[prefix]}"
     )
     if koopa_is_linux
