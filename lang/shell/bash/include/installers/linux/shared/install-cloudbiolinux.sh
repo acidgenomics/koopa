3f2329394939c83353bdf4913debf84349ade8d3
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-14 11:59:25 -0400

    Ensure we pin version of cbl

--- lang/shell/bash/include/installers/linux/shared/install-cloudbiolinux.sh
+++ lang/shell/bash/include/installers/linux/shared/install-cloudbiolinux.sh
@@ -3,13 +3,18 @@
 main() {
     # """
     # Install CloudBioLinux.
-    # @note Updated 2021-11-16.
+    # @note Updated 2022-07-14.
     # """
     local dict
     koopa_assert_has_no_args "$#"
     declare -A dict=(
+        [commit]="${INSTALL_VERSION:?}"
         [prefix]="${INSTALL_PREFIX:?}"
         [url]='https://github.com/chapmanb/cloudbiolinux.git'
     )
-    koopa_git_clone "${dict[url]}" "${dict[prefix]}"
+    koopa_git_clone \
+        --commit="${dict[commit]}" \
+        --prefix="${dict[prefix]}" \
+        --url="${dict[url]}"
+    return 0
 }
