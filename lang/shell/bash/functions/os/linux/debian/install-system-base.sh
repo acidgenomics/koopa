af6e8efec57e6eecbe01616587ad9b6360e6834a
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-14 13:37:24 -0400

    Rework base system installers

--- lang/shell/bash/functions/os/linux/debian/install-system-base.sh
+++ lang/shell/bash/functions/os/linux/debian/install-system-base.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-koopa_debian_install_base_system() {
+koopa_debian_install_system_base() {
     koopa_install_app \
         --name-fancy='Debian base system' \
-        --name='base-system' \
+        --name='base' \
         --platform='debian' \
         --system \
         "$@"
