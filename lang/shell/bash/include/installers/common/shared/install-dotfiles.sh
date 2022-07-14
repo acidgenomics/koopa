84d3769c6f8089fe8288fef6d9b1724202f5453f
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-14 12:02:59 -0400

    Pin version

--- lang/shell/bash/include/installers/common/shared/install-dotfiles.sh
+++ lang/shell/bash/include/installers/common/shared/install-dotfiles.sh
@@ -3,15 +3,19 @@
 main() {
     # """
     # Install dotfiles.
-    # @note Updated 2022-01-18.
+    # @note Updated 2022-07-14.
     # """
     local dict
     koopa_assert_has_no_args "$#"
     declare -A dict=(
+        [commit]="${INSTALL_VERSION:?}"
         [prefix]="${INSTALL_PREFIX:?}"
         [url]='https://github.com/acidgenomics/dotfiles.git'
     )
-    koopa_git_clone "${dict[url]}" "${dict[prefix]}"
+    koopa_git_clone \
+        --commit="${dict[commit]}" \
+        --prefix="${dict[prefix]}" \
+        --url="${dict[url]}"
     koopa_configure_dotfiles "${dict[prefix]}"
     return 0
 }
