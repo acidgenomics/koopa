7c0cd841f75212634958ecaab1230bf56f517b3a
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-14 12:44:53 -0400

    Rework mamba config

--- lang/shell/bash/include/installers/common/shared/install-mamba.sh
+++ lang/shell/bash/include/installers/common/shared/install-mamba.sh
@@ -3,13 +3,14 @@
 main() {
     # """
     # Update mamba.
-    # @note Updated 2022-01-25.
+    # @note Updated 2022-07-14.
     # """
     local app dict
     koopa_assert_has_no_args "$#"
     declare -A app=(
         [conda]="$(koopa_locate_conda)"
     )
+    [[ -x "${app[conda]}" ]] || return 1
     declare -A dict=(
         [name]='mamba'
         [version]="${INSTALL_VERSION:?}"
