bcff64de60361615d5422743383ac15fece39616
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-15 11:09:56 -0400

    Improve pinning of Ensembl Perl API

--- lang/shell/bash/include/installers/common/shared/install-ensembl-perl-api.sh
+++ lang/shell/bash/include/installers/common/shared/install-ensembl-perl-api.sh
@@ -3,30 +3,46 @@
 main() {
     # """
     # Install Ensembl Perl API.
-    # @note Updated 2021-11-23.
+    # @note Updated 2022-07-15.
+    #
+    # @seealso
+    # - https://useast.ensembl.org/info/docs/api/api_installation.html
     # """
-    local dict ensembl_repos repo
+    local dict repo repos
     koopa_assert_has_no_args "$#"
     declare -A dict=(
         [prefix]="${INSTALL_PREFIX:?}"
+        [version]="${INSTALL_VERSION:?}"
     )
     koopa_git_clone \
         --branch='release-1-6-924' \
-        'https://github.com/bioperl/bioperl-live.git' \
-        "${dict[prefix]}/bioperl-live"
-    ensembl_repos=(
+        --prefix="${dict[prefix]}/bioperl-live" \
+        --url='https://github.com/bioperl/bioperl-live.git'
+    # Can't version pin here easily.
+    repos=(
+        'ensembl-git-tools'
+    )
+    for repo in "${repos[@]}"
+    do
+        koopa_git_clone \
+            --branch='main' \
+            --prefix="${dict[prefix]}/${repo}" \
+            --url="https://github.com/Ensembl/${repo}.git"
+    done
+    # Pin these to Ensembl release defined as version.
+    repos=(
         'ensembl'
         'ensembl-compara'
         'ensembl-funcgen'
-        'ensembl-git-tools'
         'ensembl-io'
         'ensembl-variation'
     )
-    for repo in "${ensembl_repos[@]}"
+    for repo in "${repos[@]}"
     do
         koopa_git_clone \
-            "https://github.com/Ensembl/${repo}.git" \
-            "${dict[prefix]}/${repo}"
+            --branch="release/${dict[version]}" \
+            --prefix="${dict[prefix]}/${repo}" \
+            --url="https://github.com/Ensembl/${repo}.git"
     done
     return 0
 }
