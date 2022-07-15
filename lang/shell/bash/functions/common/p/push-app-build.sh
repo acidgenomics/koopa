ffe0ee09ac8603c4c40e0d7e81ddb6d037eb3eb3
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-15 15:46:58 -0400

    Simplify app build push

--- lang/shell/bash/functions/common/p/push-app-build.sh
+++ lang/shell/bash/functions/common/p/push-app-build.sh
@@ -1,85 +1,46 @@
 #!/usr/bin/env bash
 
-# NOTE This also should invalidate cloud cache in a future update.
-
-# FIXME Check that application contains files and not only a failed build
-# log file.
-
 koopa_push_app_build() {
     # """
     # Create a tarball of app build, and push to S3 bucket.
-    # @note Updated 2022-03-29.
+    # @note Updated 2022-07-15.
     #
     # @examples
-    # > koopa_push_app_build --app-name='node' --app-version='17.8.0'
-    # # s3://koopa.acidgenomics.com/app/ubuntu-20/amd64/node/17.8.0.tar.gz
+    # > koopa_push_app_build 'emacs' 'vim'
     # """
-    local app dict pos
+    local app dict name
     koopa_assert_has_args "$#"
     declare -A app=(
         [aws]="$(koopa_locate_aws)"
         [tar]="$(koopa_locate_tar)"
     )
+    [[ -x "${app[aws]}" ]] || return 1
+    [[ -x "${app[tar]}" ]] || return 1
     declare -A dict=(
-        [app_name]=''
-        [app_prefix]="$(koopa_app_prefix)"
-        [app_version]=''
-        [arch]="$(koopa_arch2)"
+        [arch]="$(koopa_arch2)" # e.g. 'amd64'.
+        [opt_prefix]="$(koopa_opt_prefix)"
         [os_string]="$(koopa_os_string)"
         [s3_prefix]='s3://koopa.acidgenomics.com/app'
         [s3_profile]='acidgenomics'
         [tmp_dir]="$(koopa_tmp_dir)"
     )
-    pos=()
-    while (("$#"))
+    for name in "$@"
     do
-        case "$1" in
-            # Key-value pairs --------------------------------------------------
-            '--app-name='*)
-                dict[app_name]="${1#*=}"
-                shift 1
-                ;;
-            '--app-name')
-                dict[app_name]="${2:?}"
-                shift 2
-                ;;
-            '--app-version='*)
-                dict[app_version]="${1#*=}"
-                shift 1
-                ;;
-            '--app-version')
-                dict[app_version]="${2:?}"
-                shift 2
-                ;;
-            # Other ------------------------------------------------------------
-            '-'*)
-                koopa_invalid_arg "$1"
-                ;;
-            *)
-                pos+=("$1")
-                shift 1
-                ;;
-        esac
+        local dict2
+        declare -A dict2
+        dict2[name]="$name"
+        dict2[prefix]="$(koopa_realpath "${dict[opt_prefix]}/${dict2[name]}")"
+        dict2[version]="$(koopa_basename "${dict2[prefix]}")"
+        dict2[local_tar]="${dict[tmp_dir]}/\
+${dict2[name]}/${dict2[version]}.tar.gz"
+        dict2[remote_tar]="${dict[s3_prefix]}/${dict[os_string]}/${dict[arch]}/\
+${dict2[name]}/${dict2[version]}.tar.gz"
+        koopa_alert "Pushing '${dict2[prefix]}' to '${dict2[remote_tar]}'."
+        koopa_mkdir "${dict[tmp_dir]}/${dict2[name]}"
+        "${app[tar]}" -Pczf "${dict2[local_tar]}" "${dict2[prefix]}/"
+        "${app[aws]}" --profile="${dict[s3_profile]}" \
+            s3 cp "${dict2[local_tar]}" "${dict2[remote_tar]}"
     done
-    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
-    if [[ "$#" -gt 0 ]]
-    then
-        koopa_assert_has_args_eq "$#" 2
-        dict[app_name]="${1:?}"
-        dict[app_version]="${2:?}"
-    fi
-    koopa_assert_is_set \
-        '--app-name' "${dict[app_name]}" \
-        '--app-version' "${dict[app_version]}"
-    dict[prefix]="${dict[app_prefix]}/${dict[app_name]}/${dict[app_version]}"
-    dict[local_tarball]="${dict[tmp_dir]}/${dict[app_version]}.tar.gz"
-    dict[remote_tarball]="${dict[s3_prefix]}/${dict[os_string]}/${dict[arch]}/\
-${dict[app_name]}/${dict[app_version]}.tar.gz"
-    koopa_alert "Pushing '${dict[prefix]}' to '${dict[remote_tarball]}'."
-    "${app[tar]}" -Pczf "${dict[local_tarball]}" "${dict[prefix]}/"
-    "${app[aws]}" --profile="${dict[s3_profile]}" \
-        s3 cp "${dict[local_tarball]}" "${dict[remote_tarball]}"
     koopa_rm "${dict[tmp_dir]}"
     return 0
 }
-
