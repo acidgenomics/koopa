ce2f1f74a916d19d37c88d84d25ba2c9d35c2517
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-15 06:57:50 -0400

    Rework installers

--- lang/shell/bash/functions/common/u/update-google-cloud-sdk.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-koopa_update_google_cloud_sdk() {
-    koopa_update_app \
-        --name-fancy='Google Cloud SDK' \
-        --name='google-cloud-sdk' \
-        --system \
-        "$@"
-}
