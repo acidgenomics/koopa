087bace39db726e86092ca90696b61074bbe37ee
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-18 09:42:28 -0400

    Simplify base image installer

--- lang/shell/bash/include/installers/alpine/system/install-base.sh
+++ lang/shell/bash/include/installers/alpine/system/install-base.sh
@@ -3,119 +3,66 @@
 main() {
     # """
     # Install Alpine Linux base system.
-    # @note Updated 2021-11-30.
+    # @note Updated 2022-07-18.
     #
     # Use '<pkg>=~<version>' to pin package versions.
+    #
+    # 'build-base' is a meta package containing: binutils, file,
+    # fortify-headers, g++, gcc, libc-dev, path, and remake-make.
     #
     # Potentially useful flags:
     # > apk add --no-cache --virtual .build-dependencies
     # """
-    local app dict pkgs
+    local app pkgs
+    koopa_assert_has_no_args "$#"
     declare -A app=(
         [apk]="$(koopa_alpine_locate_apk)"
         [sudo]="$(koopa_locate_sudo)"
     )
     [[ -x "${app[apk]}" ]] || return 1
     [[ -x "${app[sudo]}" ]] || return 1
-    declare -A dict=(
-        [base]=1
-        [dev]=1
-        [extra]=0
-        [recommended]=1
-        [upgrade]=1
-    )
-    while (("$#"))
-    do
-        case "$1" in
-            '')
-                shift 1
-                ;;
-            '--base-image')
-                dict[base]=1
-                dict[dev]=0
-                dict[extra]=0
-                dict[recommended]=0
-                dict[upgrade]=0
-                shift 1
-                ;;
-            '--full')
-                dict[base]=1
-                dict[dev]=1
-                dict[extra]=1
-                dict[recommended]=1
-                dict[upgrade]=1
-                shift 1
-                ;;
-            *)
-                koopa_invalid_arg "$1"
-                ;;
-        esac
-    done
     "${app[sudo]}" "${app[apk]}" --no-cache update
-    if [[ "${dict[upgrade]}" -eq 1 ]]
-    then
-        koopa_alert 'Upgrading system.'
-        "${app[sudo]}" "${app[apk]}" --no-cache upgrade
-    fi
-    pkgs=()
-    # These packages should be included in base image.
-    if [[ "${dict[base]}" -eq 1 ]]
-    then
-        pkgs+=(
-            # > dpkg-dev
-            'autoconf'
-            'bash'
-            'bash-completion'
-            'bc'
-            # Meta package containing: binutils, file, fortify-headers, g++,
-            # gcc, libc-dev, path, and remake-make.
-            'build-base'
-            'ca-certificates'
-            'coreutils'
-            'curl'
-            'findutils'
-            'gcc'
-            'gettext' # msgfmt
-            'git'
-            'gnupg'
-            'man-db'
-            'ncurses-dev' # zsh
-            'openssl'
-            'patch'
-            'procps' # ps
-            'shadow'
-            'sudo'
-            'tar'
-            'unzip'
-            'xz'
-        )
-    fi
-    # These packages will be installed in the Docker recommended image.
-    if [[ "${dict[recommended]}" -eq 1 ]]
-    then
-        pkgs+=(
-            # > R-dev
-            # > R-doc
-            # > fish
-            # > pandoc
-            # > texlive
-            'R'
-            'mdocml'
-            'tcl'
-            'tree'
-            'wget'
-            'zsh'
-        )
-    fi
-    if [[ "${dict[dev]}" -eq 1 ]]
-    then
-        pkgs+=(
-            'libevent-dev'
-            'libffi-dev'
-            'libxml2-dev'
-            'ncurses-dev'
-        )
-    fi
+    "${app[sudo]}" "${app[apk]}" --no-cache upgrade
+    pkgs=(
+        # > R-dev
+        # > R-doc
+        # > dpkg-dev
+        # > fish
+        # > pandoc
+        # > texlive
+        'R'
+        'autoconf'
+        'bash'
+        'bash-completion'
+        'bc'
+        'build-base'
+        'ca-certificates'
+        'coreutils'
+        'curl'
+        'findutils'
+        'gcc'
+        'gettext' # msgfmt
+        'git'
+        'gnupg'
+        'libevent-dev'
+        'libffi-dev'
+        'libxml2-dev'
+        'man-db'
+        'mdocml'
+        'ncurses-dev' # zsh
+        'openssl'
+        'patch'
+        'procps' # ps
+        'shadow'
+        'sudo'
+        'tar'
+        'tcl'
+        'tree'
+        'unzip'
+        'wget'
+        'xz'
+        'zsh'
+    )
     "${app[sudo]}" "${app[apk]}" --no-cache add "${pkgs[@]}"
     return 0
 }
