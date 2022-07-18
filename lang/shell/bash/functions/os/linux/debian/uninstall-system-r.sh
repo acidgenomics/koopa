f30dbc2b266fa6e88be5a1b684c91359c346deac
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-18 15:30:42 -0400

    Rework

--- lang/shell/bash/functions/os/linux/debian/uninstall-system-r.sh
+++ lang/shell/bash/functions/os/linux/debian/uninstall-system-r.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME Ensure we unlink in koopa bin.
-
 koopa_debian_uninstall_system_r() {
     koopa_uninstall_app \
         --name='r' \
         --platform='debian' \
         --system \
-        --unlink-in-bin='R' \
-        --unlink-in-bin='Rscript' \
         "$@"
 }
