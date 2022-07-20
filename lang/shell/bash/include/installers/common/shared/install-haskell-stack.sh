d90a03447dc30b19c0d550042758561246d1f0cc
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-20 11:56:54 -0400

    Update stack installer

--- lang/shell/bash/include/installers/common/shared/install-haskell-stack.sh
+++ lang/shell/bash/include/installers/common/shared/install-haskell-stack.sh
@@ -2,19 +2,10 @@
 
 # NOTE Not yet suppored for ARM.
 
-# FIXME Can we use these to isolate?
-# '--with-gcc'
-
-# FIXME Can we point to gmp with:
-# --extra-include-dirs
-# --extra-lib-dirs
-
-# FIXME Consider setting '--no-install-ghc'.
-
 main() {
     # """
     # Install Haskell Stack.
-    # @note Updated 2022-07-15.
+    # @note Updated 2022-07-20.
     #
     # @section Required system dependencies:
     #
@@ -47,17 +38,11 @@ main() {
     # """
     local app dict stack_args
     koopa_assert_has_no_args "$#"
-    # > if koopa_is_linux
-    # > then
-    # >     koopa_activate_opt_prefix 'zlib'
-    # > fi
-    # > koopa_activate_opt_prefix 'gmp'
     declare -A app
     declare -A dict=(
         [arch]="$(koopa_arch)" # e.g. 'x86_64'.
         [jobs]="$(koopa_cpu_count)"
         [name]='stack'
-        # > [opt_prefix]="$(koopa_opt_prefix)"
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
@@ -79,24 +64,13 @@ download/v${dict[version]}/${dict[file]}"
     koopa_cp "${dict[file]}" "${app[stack]}"
     unset -v STACK_ROOT
     koopa_rm "${HOME:?}/.stack"
-    # > dict[gmp]="$(koopa_realpath "${dict[opt_prefix]}/gmp")"
     stack_args=(
-        # > "--extra-include-dirs=${dict[gmp]}/include"
-        # > "--extra-lib-dirs=${dict[gmp]}/lib"
         "--jobs=${dict[jobs]}"
         "--stack-root=${dict[root]}"
         '--verbose'
     )
-    # > if koopa_is_linux
-    # > then
-    # >     dict[zlib]="$(koopa_realpath "${dict[opt_prefix]}/zlib")"
-    # >     stack_args+=(
-    # >         "--extra-include-dirs=${dict[zlib]}/include"
-    # >         "--extra-lib-dirs=${dict[zlib]}/lib"
-    # >     )
-    # > fi
     "${app[stack]}" "${stack_args[@]}" setup
-    # NOTE Can install a specific GHC version here with:
+    # Can install a specific GHC version here with:
     # > app[stack]="${dict[prefix]}/bin/stack"
     # > koopa_assert_is_installed "${app[stack]}"
     # > "${app[stack]}" install 'ghc-9.0.2'
