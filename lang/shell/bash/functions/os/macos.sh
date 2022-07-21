9461f6842e63c30245767a088f06a5f98fe54ae6
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-21 09:00:02 -0400

    Work on improving ImageMagick and R config

--- lang/shell/bash/functions/os/macos.sh
+++ lang/shell/bash/functions/os/macos.sh
@@ -656,8 +656,6 @@ koopa_macos_install_system_r_openmp() {
 
 koopa_macos_install_system_r() {
     koopa_install_app \
-        --link-in-bin='R-system' \
-        --link-in-bin='Rscript-system' \
         --name='r' \
         --no-prefix-check \
         --platform='macos' \
