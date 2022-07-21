106314d73b9fe800d3be66d138339e034363a22b
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-21 07:35:35 -0400

    Work toward improving system R config

---
+++ lang/shell/posix/functions/alias-kb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+koopa_alias_kb() {
+    # """
+    # Koopa 'kb' shortcut alias.
+    # @note Updated 2022-07-21.
+    # """
+    cd "$(koopa_koopa_prefix)/lang/shell/bash" || return 1
+}
