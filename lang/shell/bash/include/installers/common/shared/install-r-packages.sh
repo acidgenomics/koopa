acd87c9c457c615880a804b0d3f87298ee16f6d1
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-21 10:48:40 -0400

    Imrpove R config

--- lang/shell/bash/include/installers/common/shared/install-r-packages.sh
+++ lang/shell/bash/include/installers/common/shared/install-r-packages.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-# FIXME Need to rework this for macOS R CRAN binary.
-
 main() {
     # """
     # Install R packages.
-    # @note Updated 2022-07-07.
+    # @note Updated 2022-07-21.
     # """
     local app dict
     koopa_assert_has_no_args "$#"
     declare -A app=(
+        [r]="$(koopa_locate_r)"
         [rscript]="$(koopa_locate_rscript)"
     )
+    [[ -x "${app[r]}" ]] || return 1
     [[ -x "${app[rscript]}" ]] || return 1
+    koopa_configure_r "${app[r]}"
     declare -A dict=(
         [bioc_version]="$(koopa_variable 'bioconductor')"
     )
