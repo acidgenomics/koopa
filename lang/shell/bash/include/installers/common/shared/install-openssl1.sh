fa39fe6602e1fd4bb58d90640d905539580aa14f
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-22 12:21:53 -0400

    Improve openssl install

--- lang/shell/bash/include/installers/common/shared/install-openssl1.sh
+++ lang/shell/bash/include/installers/common/shared/install-openssl1.sh
@@ -11,10 +11,11 @@ main() {
     # - https://wiki.openssl.org/index.php/Compilation_and_Installation
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/
     #     openssl@1.1.rb
+    # - https://stackoverflow.com/questions/4138139/
     # """
     local app conf_args dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_opt_prefix 'zlib'
+    koopa_activate_opt_prefix 'ca-certificates'
     declare -A app=(
         [make]="$(koopa_locate_make)"
     )
@@ -22,6 +23,7 @@ main() {
     declare -A dict=(
         [jobs]="$(koopa_cpu_count)"
         [name]='openssl'
+        [opt_prefix]="$(koopa_opt_prefix)"
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
@@ -36,8 +38,8 @@ main() {
         "-Wl,-rpath,${dict[prefix]}/lib"
         'no-ssl3'
         'no-ssl3-method'
-        'shared' # or 'no-shared'.
-        'zlib'
+        'no-zlib'
+        'shared'
     )
     # The '-fPIC' flag is required for non-prefixed configuration arguments,
     # such as 'no-shared' or 'shared' to be detected correctly.
@@ -46,6 +48,15 @@ main() {
     "${app[make]}" --jobs="${dict[jobs]}"
     # > "${app[make]}" test
     "${app[make]}" install
+    dict[cacert]="$(koopa_realpath "${dict[opt_prefix]}/ca-certificates/share/\
+ca-certificates/cacert.pem")"
+    koopa_assert_is_file "${dict[cacert]}"
+    koopa_ln \
+        "${dict[cacert]}" \
+        "${dict[prefix]}/certs/cacert.pem"
+    app[openssl]="${dict[prefix]}/bin/openssl"
+    koopa_assert_is_installed "${app[openssl]}"
+    "${app[openssl]}" version -d
     # FIXME Rework this as a function, which calls ldd or otool dynamically.
     if koopa_is_linux
     then
