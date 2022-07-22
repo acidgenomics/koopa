58f84319114622ab3c0795c2f5898fe4b1aa53ae
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-22 15:58:12 -0400

    Rework package installation

--- lang/shell/bash/functions/common/i/install-julia-packages.sh
+++ lang/shell/bash/functions/common/i/install-julia-packages.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
 
 koopa_install_julia_packages() {
-    koopa_install_app \
-        --name='julia-packages' \
-        "$@"
+    # """
+    # Install Julia packages.
+    # @note Updated 2022-07-22.
+    #
+    # @seealso
+    # - 'JULIA_DEPOT_PATH' in shell.
+    # - 'DEPOT_PATH' in Julia.
+    # - https://docs.julialang.org/en/v1/
+    # - https://pkgdocs.julialang.org/v1/managing-packages/
+    # - https://pkgdocs.julialang.org/v1/api/
+    # - https://biojulia.net
+    # - https://github.com/BioJulia
+    # - https://stackoverflow.com/questions/36398629/
+    # - https://towardsdatascience.com/
+    #     getting-familiar-with-biojulia-bioinformatics-for-julia-796438aa059
+    # - https://juliaobserver.com/packages
+    # """
+    local app dict
+    koopa_assert_has_no_args "$#"
+    declare -A app=(
+        [julia]="$(koopa_locate_julia)"
+    )
+    [[ -x "${app[julia]}" ]] || return 1
+    declare -A dict=(
+        [script_prefix]="$(koopa_julia_script_prefix)"
+    )
+    dict[script]="${dict[script_prefix]}/install-packages.jl"
+    koopa_assert_is_file "${dict[script]}"
+    koopa_configure_julia "${app[julia]}"
+    koopa_activate_julia
+    "${app[julia]}" "${dict[script]}"
+    return 0
 }
