abfbac14a6b855f3a679b80921e54cb37cbd621a
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-22 08:02:47 -0400

    Attempt to improve koopa updater

--- lang/shell/bash/functions/common/u/update-koopa.sh
+++ lang/shell/bash/functions/common/u/update-koopa.sh
@@ -3,7 +3,7 @@
 koopa_update_koopa() {
     # """
     # Update koopa installation.
-    # @note Updated 2022-04-26.
+    # @note Updated 2022-07-22.
     #
     # Update of pinned stable releases is intentionally not supported.
     #
@@ -14,15 +14,15 @@ koopa_update_koopa() {
     koopa_assert_has_no_args "$#"
     declare -A dict=(
         [prefix]="$(koopa_koopa_prefix)"
+        [user]="$(koopa_user)"
     )
     if ! koopa_is_git_repo_top_level "${dict[prefix]}"
     then
         koopa_alert_note "Pinned release detected at '${dict[prefix]}'."
         return 1
     fi
-    koopa_sys_set_permissions --recursive "${dict[prefix]}"
+    koopa_chown --recursive --sudo "${dict[user]}" "${dict[prefix]}"
     koopa_git_pull "${dict[prefix]}"
-    # NOTE Add a step to recache Bash function library here.
     koopa_sys_set_permissions --recursive "${dict[prefix]}"
     koopa_fix_zsh_permissions
     return 0
