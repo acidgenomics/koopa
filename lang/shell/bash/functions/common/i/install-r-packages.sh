e25bf3186942eae27f8dfa84300c12c3d751ecf0
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-22 09:50:49 -0400

    Need to improve system R package install on Ubuntu 22

--- lang/shell/bash/functions/common/i/install-r-packages.sh
+++ lang/shell/bash/functions/common/i/install-r-packages.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Need to rework this approach for system R.
+# FIXME Run into prefix check issues at the end...
+
 koopa_install_r_packages() {
     koopa_install_app \
         --name='r-packages' \
