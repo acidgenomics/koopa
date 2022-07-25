9c9a1cb41b86a3d6993c94ec63d5263b13a8e867
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-25 09:08:13 -0400

    Revert scp config

--- lang/shell/bash/functions/common/l/locate-scp.sh
+++ lang/shell/bash/functions/common/l/locate-scp.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
 koopa_locate_scp() {
-    local args
-    if koopa_is_macos
-    then
-        # This works better with macOS keychain.
-        args=('/usr/bin/scp')
-    else
-        args=(
-            '--allow-in-path'
-            '--app-name=scp'
-            '--opt-name=openssh'
-        )
-    fi
-    koopa_locate_app "${args[@]}"
+    koopa_locate_app \
+        --allow-in-path \
+        --app-name='scp' \
+        --opt-name='openssh'
 }
