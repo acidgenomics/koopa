b5c80768dfeaab714d8a699f4fb1e1f80e7f0b25
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-27 13:03:42 -0400

    Improve R configuration functions

--- lang/shell/bash/functions/common/i/is-r-package-installed.sh
+++ lang/shell/bash/functions/common/i/is-r-package-installed.sh
@@ -3,7 +3,7 @@
 koopa_is_r_package_installed() {
     # """
     # Is the requested R package installed?
-    # @note Updated 2022-06-15.
+    # @note Updated 2022-07-27.
     #
     # @examples
     # > koopa_is_r_package_installed 'BiocGenerics' 'S4Vectors'
@@ -15,8 +15,7 @@ koopa_is_r_package_installed() {
     )
     [[ -x "${app[r]}" ]] || return 1
     declare -A dict
-    dict[version]="$(koopa_get_version "${app[r]}")"
-    dict[prefix]="$(koopa_r_packages_prefix "${dict[version]}")"
+    dict[prefix]="$(koopa_r_packages_prefix "${app[r]}")"
     for pkg in "$@"
     do
         [[ -d "${dict[prefix]}/${pkg}" ]] || return 1
