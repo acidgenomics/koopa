36d6afbc7287f27c98db405c6aa5a871bb0970ef
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-28 19:12:24 -0400

    Rework our Julia approach

--- lang/shell/bash/functions/common/c/configure-julia.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# FIXME Rework to not configure at '/opt/koopa/opt/julia-packages'.
-
-koopa_configure_julia() {
-    koopa_configure_app_packages \
-        --name='julia' \
-        "$@"
-}
--- lang/shell/posix/functions/activate-julia.sh
+++ lang/shell/posix/functions/activate-julia.sh
@@ -5,22 +5,19 @@ koopa_activate_julia() {
     # Activate Julia.
     # @note Updated 2022-07-28.
     #
+    # Check depot setting with 'Base.DEPOT_PATH'.
     # Check number of cores with 'Threads.nthreads()'.
-    #
-    # How to set 'JULIA_DEPOT_PATH':
-    # > local depot_path
-    # > depot_path="$(koopa_julia_packages_prefix)"
-    # > [ -d "$depot_path" ] || return 0
-    # > export JULIA_DEPOT_PATH="$depot_path"
     #
     # @seealso
     # - https://docs.julialang.org/en/v1/manual/environment-variables/
     # - https://docs.julialang.org/en/v1/manual/multi-threading/
     # - https://github.com/JuliaLang/julia/issues/43949
     # """
-    local num_threads
+    local depot_path num_threads
     [ -x "$(koopa_bin_prefix)/julia" ] || return 0
+    depot_path="$(koopa_julia_packages_prefix)"
     num_threads="$(koopa_cpu_count)"
+    export JULIA_DEPOT_PATH="$depot_path"
     export JULIA_NUM_THREADS="$num_threads"
     return 0
 }
