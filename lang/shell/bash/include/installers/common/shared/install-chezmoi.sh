cc69b5e6dc735a0d3f41a713a4654cc59adebd11
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-28 18:26:13 -0400

    Simplify chezmoi installer

--- lang/shell/bash/include/installers/common/shared/install-chezmoi.sh
+++ lang/shell/bash/include/installers/common/shared/install-chezmoi.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+# FIXME Now seeing this error specifically on macOS, but not Linux:
+#
+# mkdir -p "/opt/koopa/app/chezmoi/2.19.0/bin"
+# install -m 755 --target-directory "/opt/koopa/app/chezmoi/2.19.0/bin" chezmoi
+# install: illegal option -- -
+# usage: install [-bCcpSsv] [-B suffix] [-f flags] [-g group] [-m mode]
+#                [-o owner] file1 file2
+#        install [-bCcpSsv] [-B suffix] [-f flags] [-g group] [-m mode]
+#                [-o owner] file1 ... fileN directory
+#        install -d [-v] [-g group] [-m mode] [-o owner] directory ...
+# make: *** [Makefile:35: install] Error 64
+
 main() {
     # """
     # Install chezmoi.
-    # @note Updated 2022-06-14.
+    # @note Updated 2022-07-28.
     #
     # @seealso
     # - https://www.chezmoi.io/
@@ -15,9 +27,9 @@ main() {
     koopa_assert_has_no_args "$#"
     koopa_activate_build_opt_prefix 'go'
     declare -A app=(
-        [make]="$(koopa_locate_make)"
+        [go]="$(koopa_locate_go)"
     )
-    [[ -x "${app[make]}" ]] || return 1
+    [[ -x "${app[go]}" ]] || return 1
     declare -A dict=(
         [gopath]="$(koopa_init_dir 'go')"
         [name]='chezmoi'
@@ -30,11 +42,10 @@ refs/tags/${dict[file]}"
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    koopa_mkdir "${dict[prefix]}/bin"
-    "${app[make]}" install \
-        GOPATH="${dict[gopath]}" \
-        GO_LDFLAGS="-X main.version=${dict[version]}" \
-        PREFIX="${dict[prefix]}"
+    export GOPATH="${dict[gopath]}"
+    dict[ldflags]="-X main.version=${dict[version]}"
+    "${app[go]}" build -ldflags "${dict[ldflags]}"
+    koopa_cp --target-directory="${dict[prefix]}/bin" 'chezmoi'
     koopa_chmod --recursive 'u+rw' "${dict[gopath]}"
     koopa_configure_chezmoi
     return 0
