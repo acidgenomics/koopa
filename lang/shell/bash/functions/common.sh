abc31c5a0fc98dd4564f70a22b1c9a3cd951ce4c
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-29 13:11:34 -0400

    Ensure we invalidate CloudFront on app upload

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -17759,9 +17759,10 @@ koopa_push_app_build() {
     [[ -x "${app[tar]}" ]] || return 1
     declare -A dict=(
         [arch]="$(koopa_arch2)" # e.g. 'amd64'.
+        [distribution_id]="${KOOPA_AWS_CLOUDFRONT_DISTRIBUTION_ID:?}"
         [opt_prefix]="$(koopa_opt_prefix)"
         [os_string]="$(koopa_os_string)"
-        [s3_prefix]='s3://koopa.acidgenomics.com/app'
+        [s3_bucket]='s3://koopa.acidgenomics.com'
         [s3_profile]='acidgenomics'
         [tmp_dir]="$(koopa_tmp_dir)"
     )
@@ -17775,13 +17776,19 @@ koopa_push_app_build() {
         dict2[version]="$(koopa_basename "${dict2[prefix]}")"
         dict2[local_tar]="${dict[tmp_dir]}/\
 ${dict2[name]}/${dict2[version]}.tar.gz"
-        dict2[remote_tar]="${dict[s3_prefix]}/${dict[os_string]}/${dict[arch]}/\
+        dict2[s3_rel_path]="/app/${dict[os_string]}/${dict[arch]}/\
 ${dict2[name]}/${dict2[version]}.tar.gz"
+        dict2[remote_tar]="${dict[s3_bucket]}${dict[s3_rel_path]}"
         koopa_alert "Pushing '${dict2[prefix]}' to '${dict2[remote_tar]}'."
         koopa_mkdir "${dict[tmp_dir]}/${dict2[name]}"
         "${app[tar]}" -Pczf "${dict2[local_tar]}" "${dict2[prefix]}/"
         "${app[aws]}" --profile="${dict[s3_profile]}" \
             s3 cp "${dict2[local_tar]}" "${dict2[remote_tar]}"
+        "${app[aws]}" --profile="${dict[profile]}" \
+            cloudfront create-invalidation \
+                --distribution-id="${dict[distribution_id]}" \
+                --paths="${dict[s3_rel_path]}" \
+                >/dev/null
     done
     koopa_rm "${dict[tmp_dir]}"
     return 0
