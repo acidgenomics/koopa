a22986f8727fc1aa9e26932b93baa56ef0d53a53
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-29 14:16:49 -0400

    Improve uninstall messages

--- lang/shell/bash/functions/os/macos/u/uninstall-system-python.sh
+++ lang/shell/bash/functions/os/macos/u/uninstall-system-python.sh
@@ -4,7 +4,6 @@ koopa_macos_uninstall_system_python() {
     koopa_uninstall_app \
         --name='python' \
         --platform='macos' \
-        --prefix="$(koopa_macos_python_prefix)" \
         --system \
         "$@"
 }
--- lang/shell/bash/functions/os/macos/u/uninstall-system-r.sh
+++ lang/shell/bash/functions/os/macos/u/uninstall-system-r.sh
@@ -4,7 +4,6 @@ koopa_macos_uninstall_system_r() {
     koopa_uninstall_app \
         --name='r' \
         --platform='macos' \
-        --prefix="$(koopa_macos_r_prefix)" \
         --system \
         --unlink-in-bin='R-system' \
         --unlink-in-bin='Rscript-system' \
