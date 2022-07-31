17e68d592e3e956a6ca70da416c164906d332ac7
Michael Steinbaugh <mike@steinbaugh.com>
2022-07-31 19:25:07 -0400

    Add FIXME

--- lang/shell/bash/functions/common/i/install-chezmoi.sh
+++ lang/shell/bash/functions/common/i/install-chezmoi.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME This is informing about skipping 'chezmoi.1' man link.
+# Need to rethink this approach.
+
 koopa_install_chezmoi() {
     koopa_install_app \
         --link-in-bin='chezmoi' \
