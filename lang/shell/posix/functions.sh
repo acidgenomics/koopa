1311e518fe78ada825857c5402951d7751b9df79
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-01 14:30:12 -0400

    Attempt to fix OpenSSL config

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -185,6 +185,17 @@ koopa_activate_broot() {
     return 0
 }
 
+koopa_activate_ca_certificates() {
+    local prefix ssl_cert_file
+    prefix="$(koopa_opt_prefix)/ca-certificates"
+    [ -d "$prefix" ] || return 0
+    prefix="$(koopa_realpath "$prefix")"
+    ssl_cert_file="${prefix}/share/ca-certificates/cacert.pem"
+    [ -f "$ssl_cert_file" ] || return 0
+    export SSL_CERT_FILE="$ssl_cert_file"
+    return 0
+}
+
 koopa_activate_color_mode() {
     if [ -z "${KOOPA_COLOR_MODE:-}" ]
     then
---
+++ lang/shell/posix/functions/activate-ca-certificates.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+koopa_activate_ca_certificates() {
+    # """
+    # Activate CA certificates for OpenSSL.
+    # @note Updated 2022-08-01.
+    #
+    # This step is currently necessary for Latch SDK on macOS.
+    # """
+    local prefix ssl_cert_file
+    prefix="$(koopa_opt_prefix)/ca-certificates"
+    [ -d "$prefix" ] || return 0
+    prefix="$(koopa_realpath "$prefix")"
+    ssl_cert_file="${prefix}/share/ca-certificates/cacert.pem"
+    [ -f "$ssl_cert_file" ] || return 0
+    export SSL_CERT_FILE="$ssl_cert_file"
+    return 0
+}
