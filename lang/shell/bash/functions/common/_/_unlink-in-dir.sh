5ac048ed599a8312f9f7b98917197dc06c760964
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-02 15:26:18 -0400

    Draft update

--- lang/shell/bash/functions/common/_/_unlink-in-dir.sh
+++ lang/shell/bash/functions/common/_/_unlink-in-dir.sh
@@ -3,13 +3,12 @@
 __koopa_unlink_in_dir() {
     # """
     # Unlink multiple symlinks in a directory.
-    # @note Updated 2022-08-01.
+    # @note Updated 2022-08-02.
     # """
-    local dict file files name names pos
+    local dict name names pos
     koopa_assert_has_args "$#"
     declare -A dict=(
         [prefix]=''
-        [quiet]=0
     )
     pos=()
     while (("$#"))
@@ -23,11 +22,6 @@ __koopa_unlink_in_dir() {
             '--prefix')
                 dict[prefix]="${2:?}"
                 shift 2
-                ;;
-            # Flags ------------------------------------------------------------
-            '--quiet')
-                dict[quiet]=1
-                shift 1
                 ;;
             # Other ------------------------------------------------------------
             '-'*)
@@ -45,20 +39,13 @@ __koopa_unlink_in_dir() {
     koopa_assert_is_dir "${dict[prefix]}"
     dict[prefix]="$(koopa_realpath "${dict[prefix]}")"
     names=("$@")
-    files=()
     for name in "${names[@]}"
     do
-        files+=("${dict[prefix]}/${name}")
+        local file
+        file="${dict[prefix]}/${name}"
+        koopa_assert_is_symlink "$file"
+        koopa_alert "Unlinking '${file}'."
+        koopa_rm "$file"
     done
-    if [[ "${dict[quiet]}" -eq 1 ]]
-    then
-        koopa_rm "${files[@]}"
-    else
-        for file in "${files[@]}"
-        do
-            koopa_alert "Unlinking '${file}'."
-            koopa_rm "$file"
-        done
-    fi
     return 0
 }
