21859af2f45112fbc54cdd87a16c5a701bcb0c06
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-02 15:49:50 -0400

    Simplify

--- lang/shell/bash/functions/common/l/link-in-bin.sh
+++ lang/shell/bash/functions/common/l/link-in-bin.sh
@@ -3,53 +3,19 @@
 koopa_link_in_bin() {
     # """
     # Link a program in koopa 'bin/' directory.
-    # @note Updated 2022-08-01.
+    # @note Updated 2022-08-02.
     #
     # Also updates corresponding 'man1' files automatically, when applicable.
     #
     # @usage
     # > koopa_link_in_bin \
-    # >     SOURCE_FILE_1 TARGET_NAME_1 \
-    # >     SOURCE_FILE_2 TARGET_NAME_2 \
-    # >     ...
+    # >     --source=SOURCE_FILE \
+    # >     --target=TARGET_NAME
     #
     # @examples
     # > koopa_link_in_bin \
-    # >     '/usr/local/bin/emacs' 'emacs' \
-    # >     '/usr/local/bin/vim' 'vim'
+    # >     --name='emacs' \
+    # >     --source='/usr/local/bin/emacs'
     # """
-    local bin_files bin_links dict
-    koopa_assert_has_args "$#"
-    bin_files=()
-    bin_links=()
-    declare -A dict=(
-        [bin_prefix]="$(koopa_bin_prefix)"
-        [man_prefix]="$(koopa_man_prefix)"
-    )
-    while [[ "$#" -ge 2 ]]
-    do
-        bin_files+=("${1:?}")
-        bin_links+=("${2:?}")
-        shift 2
-    done
-    for i in "${!bin_files[@]}"
-    do
-        local dict2
-        declare -A dict2
-        dict2[bin_file]="${bin_files[$i]}"
-        dict2[bin_link]="${bin_links[$i]}"
-        dict2[bin_file_bn]="$(koopa_basename "${dict2[bin_file]}")"
-        dict2[parent_dir]="$(koopa_parent_dir --num=2 "${dict2[bin_file]}")"
-        dict2[man1_file]="${dict2[parent_dir]}/share/man/\
-man1/${dict2[bin_file_bn]}.1"
-        dict2[man1_link]="${dict2[bin_link]}.1"
-        __koopa_link_in_dir \
-            --prefix="${dict[bin_prefix]}" \
-            "${dict2[bin_file]}" "${dict2[bin_link]}"
-        __koopa_link_in_dir \
-            --prefix="${dict[man_prefix]}/man1" \
-            --quiet \
-            "${dict2[man1_file]}" "${dict2[man1_link]}"
-    done
-    return 0
+    __koopa_link_in_dir --prefix="$(koopa_bin_prefix)" "$@"
 }
--- lang/shell/bash/functions/common/u/unlink-in-bin.sh
+++ lang/shell/bash/functions/common/u/unlink-in-bin.sh
@@ -3,31 +3,12 @@
 koopa_unlink_in_bin() {
     # """
     # Unlink a program symlinked in koopa 'bin/ directory.
-    # @note Updated 2022-08-01.
+    # @note Updated 2022-08-02.
     #
     # @usage koopa_unlink_in_bin NAME...
     #
     # @examples
     # > koopa_unlink_in_bin 'R' 'Rscript'
     # """
-    local bin_link bin_links dict man_links
-    koopa_assert_has_args "$#"
-    declare -A dict=(
-        [bin_prefix]="$(koopa_bin_prefix)"
-        [man_prefix]="$(koopa_man_prefix)"
-    )
-    bin_links=("$@")
-    man_links=()
-    for bin_link in "${bin_links[@]}"
-    do
-        man_links+=("${bin_link}.1")
-    done
-    __koopa_unlink_in_dir \
-        --prefix="${dict[bin_prefix]}" \
-        "${bin_links[@]}"
-    __koopa_unlink_in_dir \
-        --prefix="${dict[man_prefix]}/man1" \
-        --quiet \
-        "${man_links[@]}"
-    return 0
+    __koopa_unlink_in_dir --prefix="$(koopa_bin_prefix)" "$@"
 }
