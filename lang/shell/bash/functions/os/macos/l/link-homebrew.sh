29bc9d8f83805174aceefc7f21999ffa0a79f776
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-02 15:43:35 -0400

    Improve legibility of link commands

--- lang/shell/bash/functions/os/macos/l/link-homebrew.sh
+++ lang/shell/bash/functions/os/macos/l/link-homebrew.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
 
 koopa_macos_link_homebrew() {
+    # """
+    # Link additional Homebrew-managed CLI tools into koopa bin.
+    # @note Updated 2022-08-02.
+    # """
     local dict
     declare -A dict
     koopa_assert_has_no_args "$#"
     # BBEdit cask.
     koopa_link_in_bin \
-        '/Applications/BBEdit.app/Contents/Helpers/bbedit_tool' \
-        'bbedit'
+        --name='bbedit' \
+        --source='/Applications/BBEdit.app/Contents/Helpers/bbedit_tool'
     # Emacs cask.
     koopa_link_in_bin \
-        '/Applications/Emacs.app/Contents/MacOS/Emacs' \
-        'emacs'
+        --name='emacs' \
+        --source='/Applications/Emacs.app/Contents/MacOS/Emacs'
     # R cask.
     dict[r]="$(koopa_macos_r_prefix)"
     koopa_link_in_bin \
-        "${dict[r]}/bin/R" 'R' \
-        "${dict[r]}/bin/Rscript" 'Rscript'
+        --name='R' \
+        --source="${dict[r]}/bin/R"
+    koopa_link_in_bin \
+        --name='Rscript' \
+        --source="${dict[r]}/bin/Rscript"
     # Visual Studio Code cask.
     koopa_link_in_bin \
-        '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' \
-        'code'
+        --name='code' \
+        --source="/Applications/Visual Studio Code.app/Contents/Resources/\
+app/bin/code"
 }
 
