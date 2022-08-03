0001857df0a6267eb3291dadf5038f7616fd8fff
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-03 09:05:18 -0400

    Improve man1 linkage

--- lang/shell/bash/functions/common/u/uninstall-bzip2.sh
+++ lang/shell/bash/functions/common/u/uninstall-bzip2.sh
@@ -3,6 +3,17 @@
 koopa_uninstall_bzip2() {
     koopa_uninstall_app \
         --name='bzip2' \
+        --unlink-in-bin='bunzip2' \
+        --unlink-in-bin='bzcat' \
+        --unlink-in-bin='bzcmp' \
+        --unlink-in-bin='bzdiff' \
+        --unlink-in-bin='bzegrep' \
+        --unlink-in-bin='bzfgrep' \
+        --unlink-in-bin='bzgrep' \
+        --unlink-in-bin='bzip2' \
+        --unlink-in-bin='bzip2recover' \
+        --unlink-in-bin='bzless' \
+        --unlink-in-bin='bzmore' \
         "$@"
 }
 
--- lang/shell/bash/functions/common/u/unlink-in-man1.sh
+++ lang/shell/bash/functions/common/u/unlink-in-man1.sh
@@ -3,7 +3,7 @@
 koopa_unlink_in_man1() {
     # """
     # Unlink documentation in koopa 'MANPATH' man1 directory.
-    # @note Updated 2022-08-02.
+    # @note Updated 2022-08-03.
     #
     # @usage
     # > koopa_unlink_in_man1 TARGET_NAME...
@@ -11,5 +11,8 @@ koopa_unlink_in_man1() {
     # @examples
     # > koopa_link_in_man1 'cp.1' 'mv.1'
     # """
-    __koopa_unlink_in_dir --prefix="$(koopa_man_prefix)/man1" "$@"
+    __koopa_unlink_in_dir \
+        --allow-missing \
+        --prefix="$(koopa_man_prefix)/man1" \
+        "$@"
 }
