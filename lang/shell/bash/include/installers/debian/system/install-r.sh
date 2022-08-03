4bef4600fc13fd13852f29b56d4e9867840e831e
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-03 08:18:38 -0400

    Don't link here

--- lang/shell/bash/include/installers/debian/system/install-r.sh
+++ lang/shell/bash/include/installers/debian/system/install-r.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install latest version of R from CRAN.
-    # @note Updated 2022-07-22.
+    # @note Updated 2022-08-02.
     #
     # In case of missing files in '/etc/R', such as ldpaths or Makeconf:
     # > sudo apt purge r-base-core
@@ -35,9 +35,6 @@ main() {
     pkgs=('r-base' 'r-base-dev')
     koopa_debian_apt_install "${pkgs[@]}"
     koopa_assert_is_installed "${app[r]}" "${app[rscript]}"
-    koopa_link_in_bin \
-        "${app[r]}" 'R' \
-        "${app[rscript]}" 'Rscript'
     koopa_configure_r "${app[r]}"
     return 0
 }
