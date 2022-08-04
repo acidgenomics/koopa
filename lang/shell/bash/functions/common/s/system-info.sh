f82ff4d7d8437e3a3b42b15a1f5de3db7d0588db
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-04 11:24:10 -0400

    Improve information on git repo

--- lang/shell/bash/functions/common/s/system-info.sh
+++ lang/shell/bash/functions/common/s/system-info.sh
@@ -3,7 +3,7 @@
 koopa_system_info() {
     # """
     # System information.
-    # @note Updated 2022-06-15.
+    # @note Updated 2022-08-04.
     # """
     local app dict info nf_info
     koopa_assert_has_no_args "$#"
@@ -20,8 +20,6 @@ koopa_system_info() {
         [ascii_turtle_file]="$(koopa_include_prefix)/ascii-turtle.txt"
         [bash_version]="$(koopa_get_version "${app[bash]}")"
         [config_prefix]="$(koopa_config_prefix)"
-        [koopa_date]="$(koopa_koopa_date)"
-        [koopa_github_url]="$(koopa_koopa_github_url)"
         [koopa_prefix]="$(koopa_koopa_prefix)"
         [koopa_url]="$(koopa_koopa_url)"
         [koopa_version]="$(koopa_koopa_version)"
@@ -29,17 +27,23 @@ koopa_system_info() {
         [opt_prefix]="$(koopa_opt_prefix)"
     )
     info=(
-        "koopa ${dict[koopa_version]} (${dict[koopa_date]})"
+        "koopa ${dict[koopa_version]}"
         "URL: ${dict[koopa_url]}"
-        "GitHub URL: ${dict[koopa_github_url]}"
     )
     if koopa_is_git_repo_top_level "${dict[koopa_prefix]}"
     then
-        dict[remote]="$(koopa_git_remote_url "${dict[koopa_prefix]}")"
-        dict[commit]="$(koopa_git_last_commit_local "${dict[koopa_prefix]}")"
+        dict[git_remote]="$(koopa_git_remote_url "${dict[koopa_prefix]}")"
+        dict[git_commit]="$( \
+            koopa_git_last_commit_local "${dict[koopa_prefix]}" \
+        )"
+        dict[git_date]="$(koopa_git_commit_date "${dict[koopa_prefix]}")"
         info+=(
-            "Git Remote: ${dict[remote]}"
-            "Git Commit: ${dict[commit]}"
+            ''
+            'Git repo'
+            '--------'
+            "Remote: ${dict[git_remote]}"
+            "Commit: ${dict[git_commit]}"
+            "Date: ${dict[git_date]}"
         )
     fi
     info+=(
@@ -55,6 +59,7 @@ koopa_system_info() {
     if koopa_is_macos
     then
         app[sw_vers]="$(koopa_macos_locate_sw_vers)"
+        [[ -x "${app[sw_vers]}" ]] || return 1
         dict[os]="$( \
             printf '%s %s (%s)\n' \
                 "$("${app[sw_vers]}" -productName)" \
