22520f68267a12723b5f4bd1a468524b4c007251
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-08 08:37:30 -0400

    Add install support for cheat

--- lang/shell/bash/functions/common/i/install-chezmoi.sh
+++ lang/shell/bash/functions/common/i/install-chezmoi.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME This is informing about skipping 'chezmoi.1' man link.
-# Need to rethink this approach.
-
 koopa_install_chezmoi() {
     koopa_install_app \
         --link-in-bin='chezmoi' \
