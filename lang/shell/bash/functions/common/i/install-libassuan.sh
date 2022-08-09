9f910aaddcf5bcc270dfee40514ff92698ab2361
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-09 12:59:24 -0400

    Rework GnuPG install approach

--- lang/shell/bash/functions/common/i/install-libassuan.sh
+++ lang/shell/bash/functions/common/i/install-libassuan.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-koopa_install_gnutls() {
+koopa_install_libassuan() {
     koopa_install_app \
         --installer='gnupg-gcrypt' \
-        --name='gnutls' \
+        --name='libassuan' \
         "$@"
 }
