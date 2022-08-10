33b0871383ac22e341a481b4a259db9131474ad9
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-10 16:08:28 -0400

    Improve pinentry build

--- lang/shell/bash/functions/os/linux/common/install-pinentry.sh
+++ lang/shell/bash/functions/os/linux/common/install-pinentry.sh
@@ -2,8 +2,6 @@
 
 koopa_linux_install_pinentry() {
     koopa_install_app \
-        --activate-opt='libgpg-error' \
-        --activate-opt='libassuan' \
         --installer='gnupg-gcrypt' \
         --name='pinentry' \
         "$@"
