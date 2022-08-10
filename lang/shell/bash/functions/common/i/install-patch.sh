efebba9f6a0736c13db3d37ebc6c2dd6dd62fab8
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-10 07:43:07 -0400

    Improve cache support

--- lang/shell/bash/functions/common/i/install-patch.sh
+++ lang/shell/bash/functions/common/i/install-patch.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 koopa_install_patch() {
-    koopa_install_app \
-        --installer='gnu-app' \
-        --link-in-bin='patch' \
-        --name='patch' \
-        "$@"
+    local install_args
+    install_args=()
+    if koopa_is_linux
+    then
+        install_args+=('--activate-opt=attr')
+    fi
+    install_args+=(
+        '--installer=gnu-app'
+        '--link-in-bin=patch'
+        '--name=patch'
+    )
+    koopa_install_app "${install_args[@]}" "$@"
 }
