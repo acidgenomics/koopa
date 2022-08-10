668010eb5765effb41f0e78d57c468d1873d6b60
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-10 16:07:06 -0400

    Update dependencies

--- lang/shell/bash/functions/os/linux/common/install-pinentry.sh
+++ lang/shell/bash/functions/os/linux/common/install-pinentry.sh
@@ -2,6 +2,8 @@
 
 koopa_linux_install_pinentry() {
     koopa_install_app \
+        --activate-opt='libgpg-error' \
+        --activate-opt='libassuan' \
         --installer='gnupg-gcrypt' \
         --name='pinentry' \
         "$@"
