1b0763a6c86288d0e423c339cd602c288200389a
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-10 16:11:21 -0400

    Fix typo

--- lang/shell/bash/functions/os/linux/common.sh
+++ lang/shell/bash/functions/os/linux/common.sh
@@ -524,8 +524,6 @@ koopa_linux_install_lmod() {
 
 koopa_linux_install_pinentry() {
     koopa_install_app \
-        --activate-opt='libgpg-error' \
-        --activate-opt='libassuan' \
         --installer='gnupg-gcrypt' \
         --name='pinentry' \
         "$@"
