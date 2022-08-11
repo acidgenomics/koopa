d28ff7f99a03c29bdbca246bcbb44434dd76f9c0
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-11 12:51:34 -0400

    Attempt to rework doom

--- lang/shell/bash/include/installers/common/user/update-doom-emacs.sh
+++ lang/shell/bash/include/installers/common/user/update-doom-emacs.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-# NOTE On macOS, hitting this annoying warning when using GUI Emacs:
-# > /System/Library/Frameworks/Ruby.framework/Versions/2.6/usr/lib/ruby/2.6.0/
-# >   universal-darwin21/rbconfig.rb:230: warning: Insecure world writable dir
-# >   /Applications/Emacs.app/Contents/MacOS in PATH, mode 040777
-
 main() {
     # """
     # Update Doom Emacs.
     # @note Updated 2022-08-11.
+    #
+    # NOTE Consider warning or erroring if user hasn't set up doom configuration
+    # using our dotfiles configuration.
     #
     # @seealso
     # https://github.com/hlissner/doom-emacs/blob/develop/core/cli/upgrade.el
@@ -22,7 +20,7 @@ main() {
     [[ -x "${app[doom]}" ]] || return 1
     [[ -x "${app[emacs]}" ]] || return 1
     koopa_add_to_path_start "$(koopa_dirname "${app[emacs]}")"
-    "${app[doom]}" --force sync
+    # > "${app[doom]}" --force sync
     "${app[doom]}" --force upgrade
     "${app[doom]}" --force sync
     # > "${app[doom]}" --force doctor
