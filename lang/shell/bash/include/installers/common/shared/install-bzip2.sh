908741211a89f7d67ab5e1fe5acfaba09e22900c
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-12 16:59:59 -0400

    Ensure bzip2 has a shared object file

--- lang/shell/bash/include/installers/common/shared/install-bzip2.sh
+++ lang/shell/bash/include/installers/common/shared/install-bzip2.sh
@@ -20,8 +20,10 @@ main() {
     local app dict
     koopa_assert_has_no_args "$#"
     declare -A app=(
+        [cat]="$(koopa_locate_cat)"
         [make]="$(koopa_locate_make)"
     )
+    [[ -x "${app[cat]}" ]] || return 1
     [[ -x "${app[make]}" ]] || return 1
     declare -A dict=(
         [jobs]="$(koopa_cpu_count)"
@@ -36,28 +38,17 @@ main() {
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
-    "${app[make]}" install "PREFIX=${dict[prefix]}"
-    if koopa_is_linux
-    then
-        dict[makefile_shared]='Makefile-libbz2_so'
-    elif koopa_is_macos
+    dict[makefile_shared]="Makefile-libbz2_${dict[shared_ext]}"
+    if koopa_is_macos
     then
         dict[makefile_shared]='Makefile-libbz2_dylib'
         "${app[cat]}" > "${dict[makefile_shared]}" << END
-# This Makefile builds a shared version of the library,
-# libbz2.dylib for MacOSX x86 (10.13.4 or higher),
-# with gcc-2.96 20000731 (Red Hat Linux 7.1 2.96-98).
-# It is a custom Makefile. Use at own risk.
-# Run in your MacOS terminal with the following command:
-# make -f Makefile-libbz2_dylib
-
-PKG_VERSION?=1.0.8
-PREFIX?=/usr/local
+PKG_VERSION=${dict[version]}
 
 SHELL=/bin/sh
 CC=gcc
 BIGFILES=-D_FILE_OFFSET_BITS=64
-CFLAGS=-fpic -fPIC -Wall -Winline -O2 -g $(BIGFILES)
+CFLAGS=-fpic -fPIC -Wall -Winline -O2 -g \$(BIGFILES)
 
 OBJS= blocksort.o  \
 	  huffman.o    \
@@ -67,43 +58,60 @@ OBJS= blocksort.o  \
 	  decompress.o \
 	  bzlib.o
 
-all: $(OBJS)
-	$(CC) -shared -Wl,-install_name -Wl,libbz2.dylib -o libbz2.${PKG_VERSION}.dylib $(OBJS)
-	cp libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/
-	ln -s libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/libbz2.dylib
+all: \$(OBJS)
+	\$(CC) -shared -Wl,-install_name -Wl,libbz2.dylib -o libbz2.\${PKG_VERSION}.dylib \$(OBJS)
 
 clean:
-	rm -f libbz2.dylib libbz2.${PKG_VERSION}.dylib
+	rm -f libbz2.dylib libbz2.\${PKG_VERSION}.dylib
 
 blocksort.o: blocksort.c
-	$(CC) $(CFLAGS) -c blocksort.c
+	\$(CC) \$(CFLAGS) -c blocksort.c
 huffman.o: huffman.c
-	$(CC) $(CFLAGS) -c huffman.c
+	\$(CC) \$(CFLAGS) -c huffman.c
 crctable.o: crctable.c
-	$(CC) $(CFLAGS) -c crctable.c
+	\$(CC) \$(CFLAGS) -c crctable.c
 randtable.o: randtable.c
-	$(CC) $(CFLAGS) -c randtable.c
+	\$(CC) \$(CFLAGS) -c randtable.c
 compress.o: compress.c
-	$(CC) $(CFLAGS) -c compress.c
+	\$(CC) \$(CFLAGS) -c compress.c
 decompress.o: decompress.c
-	$(CC) $(CFLAGS) -c decompress.c
+	\$(CC) \$(CFLAGS) -c decompress.c
 bzlib.o: bzlib.c
-	$(CC) $(CFLAGS) -c bzlib.c
+	\$(CC) \$(CFLAGS) -c bzlib.c
 END
     fi
+    koopa_assert_is_file "${dict[makefile_shared]}"
+    "${app[make]}" install "PREFIX=${dict[prefix]}"
     "${app[make]}" -f "${dict[makefile_shared]}" 'clean'
     "${app[make]}" -f "${dict[makefile_shared]}"
-    koopa_cp \
-        --target-directory="${dict[prefix]}/lib" \
-        "libbz2.${dict[shared_ext]}.${dict[version]}"
-    (
-        koopa_cd "${dict[prefix]}/lib"
-        koopa_ln \
-            "libbz2.${dict[shared_ext]}.${dict[version]}" \
-            "libbz2.${dict[shared_ext]}.${dict[maj_min_ver]}"
-        koopa_ln \
-            "libbz2.${dict[shared_ext]}.${dict[version]}" \
-            "libbz2.${dict[shared_ext]}"
-    )
+    if koopa_is_linux
+    then
+        koopa_cp \
+            --target-directory="${dict[prefix]}/lib" \
+            "libbz2.${dict[shared_ext]}.${dict[version]}"
+        (
+            koopa_cd "${dict[prefix]}/lib"
+            koopa_ln \
+                "libbz2.${dict[shared_ext]}.${dict[version]}" \
+                "libbz2.${dict[shared_ext]}.${dict[maj_min_ver]}"
+            koopa_ln \
+                "libbz2.${dict[shared_ext]}.${dict[version]}" \
+                "libbz2.${dict[shared_ext]}"
+        )
+    elif koopa_is_macos
+    then
+        koopa_cp \
+            --target-directory="${dict[prefix]}/lib" \
+            "libbz2.${dict[version]}.${dict[shared_ext]}"
+        (
+            koopa_cd "${dict[prefix]}/lib"
+            koopa_ln \
+                "libbz2.${dict[version]}.${dict[shared_ext]}" \
+                "libbz2.${dict[maj_min_ver]}.${dict[shared_ext]}"
+            koopa_ln \
+                "libbz2.${dict[version]}.${dict[shared_ext]}" \
+                "libbz2.${dict[shared_ext]}"
+        )
+    fi
     return 0
 }
