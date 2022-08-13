0a308fde32ffa769bffa86535f51974598ae560a
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-12 21:21:03 -0400

    Attempt to harden installer

--- lang/shell/bash/functions/common/i/install-app-internal.sh
+++ lang/shell/bash/functions/common/i/install-app-internal.sh
@@ -1,6 +1,55 @@
 #!/usr/bin/env bash
 
 koopa_install_app_internal() {
+    # """
+    # Internal runner to install an application.
+    # @note Updated 2022-08-12.
+    # """
+    local build_opt_arr opt_arr pos
+    koopa_assert_has_args "$#"
+    build_opt_arr=()
+    opt_arr=()
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            # Key-value pairs --------------------------------------------------
+            '--activate-build-opt='*)
+                build_opt_arr+=("${1#*=}")
+                shift 1
+                ;;
+            '--activate-build-opt')
+                build_opt_arr+=("${2:?}")
+                shift 2
+                ;;
+            '--activate-opt='*)
+                opt_arr+=("${1#*=}")
+                shift 1
+                ;;
+            '--activate-opt')
+                opt_arr+=("${2:?}")
+                shift 2
+                ;;
+            # Other ------------------------------------------------------------
+            '--no-link-in-opt' | \
+            '--no-prefix-check' | \
+            '--quiet')
+                koopa_invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    if koopa_is_array_non_empty "${build_opt_arr[@]:-}"
+    then
+        koopa_activate_build_opt_prefix "${build_opt_arr[@]}"
+    fi
+    if koopa_is_array_non_empty "${opt_arr[@]:-}"
+    then
+        koopa_activate_opt_prefix "${opt_arr[@]}"
+    fi
     koopa_install_app \
         --no-link-in-opt \
         --no-prefix-check \
