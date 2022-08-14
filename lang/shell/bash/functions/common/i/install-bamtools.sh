2647cf29659eddea4f447f6b69e1e29c0717fbd4
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-14 17:19:31 -0400

    Draft updates

--- lang/shell/bash/functions/common/i/install-bamtools.sh
+++ lang/shell/bash/functions/common/i/install-bamtools.sh
@@ -2,7 +2,6 @@
 
 koopa_install_bamtools() {
     koopa_install_app \
-        --installer='conda-env' \
         --link-in-bin='bamtools' \
         --name='bamtools' \
         "$@"
---
+++ lang/shell/bash/functions/common/l/locate-numfmt.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_numfmt() {
+    koopa_locate_app \
+        --app-name='numfmt' \
+        --opt-name='coreutils'
+}
