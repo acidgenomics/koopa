ac2013387d5b33fd5bcf2413671bf0e850300ef8
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-16 19:00:24 -0400

    Add NOTE regarding CMake

--- lang/shell/bash/include/install/common/shared/starship.sh
+++ lang/shell/bash/include/install/common/shared/starship.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 main() {
+    # """
+    # CMake was added as a build dependency in 1.10.
+    # """
     koopa_activate_build_opt_prefix 'cmake'
     koopa_install_app_internal \
         --installer='rust-package' \
