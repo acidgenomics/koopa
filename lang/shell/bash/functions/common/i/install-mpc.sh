8afc6d632d150ccbeb2ffc31f023cce2ff2071db
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-16 06:01:31 -0400

    Rework installers

--- lang/shell/bash/functions/common/i/install-mpc.sh
+++ lang/shell/bash/functions/common/i/install-mpc.sh
@@ -5,18 +5,3 @@ koopa_install_mpc() {
         --name='mpc' \
         "$@"
 }
-
-main() {
-    local dict
-    declare -A dict=(
-        [gmp]="$(koopa_app_prefix 'gmp')"
-        [mpfr]="$(koopa_app_prefix 'mpfr')"
-    )
-    koopa_activate_opt_prefix 'gmp' 'mpfr'
-    koopa_install_app_internal \
-        --installer='gnu-app' \
-        --name='mpc' \
-        -D "--with-gmp=${dict[gmp]}" \
-        -D "--with-mpfr=${dict[mpfr]}" \
-        "$@"
-}
--- lang/shell/bash/include/install/common/shared/npth.sh
+++ lang/shell/bash/include/install/common/shared/npth.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 koopa_install_npth() {
-    koopa_install_app \
+    koopa_install_app_internal \
+        --installer='gnupg-gcrypt' \
         --name='npth' \
         "$@"
 }
