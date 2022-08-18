4aa41dd2795e0ab23bf3112cbf38ed6214adb37a
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-18 12:55:18 -0400

    Improve LD_LIBRARY_PATH handling on macOS

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -18244,14 +18244,13 @@ abort,verbose"
 }
 
 koopa_r_configure_ldpaths() {
-    local app dict key keys ld_lib_arr ld_lib_opt_arr lines
+    local app dict key keys ld_lib_arr ld_lib_app_arr lines
     koopa_assert_has_args_eq "$#" 1
     declare -A app=(
         [r]="${1:?}"
     )
     [[ -x "${app[r]}" ]] || return 1
     koopa_is_koopa_app "${app[r]}" && return 0
-    koopa_is_linux || return 0
     declare -A dict=(
         [arch]="$(koopa_arch)"
         [koopa_prefix]="$(koopa_koopa_prefix)"
@@ -18266,7 +18265,7 @@ koopa_r_configure_ldpaths() {
         ": \${JAVA_HOME=${dict[java_home]}}"
         ": \${R_JAVA_LD_LIBRARY_PATH=\${JAVA_HOME}/libexec/lib/server}"
     )
-    declare -A ld_lib_opt_arr
+    declare -A ld_lib_app_arr
     keys=(
         'fontconfig'
         'freetype'
@@ -18278,20 +18277,31 @@ koopa_r_configure_ldpaths() {
     )
     for key in "${keys[@]}"
     do
-        ld_lib_opt_arr[$key]="$( \
-            koopa_realpath "${dict[opt_prefix]}/${key}/lib" \
-        )"
+        ld_lib_app_arr[$key]="$(koopa_app_prefix "$key")/lib"
     done
-    ld_lib_arr=(
-        "/usr/lib/${dict[arch]}-linux-gnu"
+    ld_lib_arr=()
+    if koopa_is_linux
+    then
+        ld_lib_arr+=("/usr/lib/${dict[arch]}-linux-gnu")
+    fi
+    ld_lib_arr+=(
         "\${R_HOME}/lib"
-        "${ld_lib_opt_arr[@]}"
+        "${ld_lib_app_arr[@]}"
         "\${R_JAVA_LD_LIBRARY_PATH}"
     )
     lines+=(
-        "LD_LIBRARY_PATH=$(printf '%s:' "${ld_lib_arr[@]}")"
+        "LD_LIBRARY_PATH=\"$(printf '%s:' "${ld_lib_arr[@]}")\""
         'export LD_LIBRARY_PATH'
     )
+    if koopa_is_macos
+    then
+        lines+=(
+            "DYLD_FALLBACK_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}\""
+            'export DYLD_FALLBACK_LIBRARY_PATH'
+            "DYLD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}\""
+            'export DYLD_LIBRARY_PATH'
+        )
+    fi
     dict[string]="$(koopa_print "${lines[@]}")"
     koopa_sudo_write_string \
         --file="${dict[file]}" \
