793d09fa73224f9ae0e5fb3a7dc987e700525b3c
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-19 15:20:51 -0400

    Add support for brotli

--- lang/shell/bash/include/install/common/shared/brotli.sh
+++ lang/shell/bash/include/install/common/shared/brotli.sh
@@ -1,48 +1,39 @@
 #!/usr/bin/env bash
 
-# NOTE Consider tightening config for arpack, openblas, and superlu.
-
 main() {
     # """
-    # Install Armadillo.
+    # Install Brotli.
     # @note Updated 2022-08-19.
     #
     # @seealso
     # - http://arma.sourceforge.net/download.html
     # - https://github.com/Homebrew/homebrew-core/blob/master/
     #     Formula/armadillo.rb
-    # - https://git.alpinelinux.org/aports/tree/community/armadillo/APKBUILD
     # """
     local app cmake_args dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_build_opt_prefix 'cmake' 'pkg-config'
-    koopa_activate_opt_prefix 'hdf5'
+    koopa_activate_build_opt_prefix 'cmake'
     declare -A app=(
         [cmake]="$(koopa_locate_cmake)"
+        [make]="$(koopa_locate_make)"
     )
     [[ -x "${app[cmake]}" ]] || return 1
+    [[ -x "${app[make]}" ]] || return 1
     declare -A dict=(
-        [name]='armadillo'
+        [name]='brotli'
         [prefix]="${INSTALL_PREFIX:?}"
         [version]="${INSTALL_VERSION:?}"
     )
-    dict[file]="${dict[name]}-${dict[version]}.tar.xz"
-    dict[url]="http://sourceforge.net/projects/arma/files/${dict[file]}"
+    dict[file]="v${dict[version]}.tar.gz"
+    dict[url]="https://github.com/google/${dict[name]}/archive/${dict[file]}"
     koopa_download "${dict[url]}" "${dict[file]}"
     koopa_extract "${dict[file]}"
     koopa_cd "${dict[name]}-${dict[version]}"
     cmake_args=(
-        # > '-DCMAKE_BUILD_TYPE=MinSizeRel'
-        # > '-DCMAKE_INSTALL_LIBDIR=lib'
         "-DCMAKE_INSTALL_PREFIX:PATH=${dict[prefix]}"
-        '-DDETECT_HDF5=ON'
     )
-    if koopa_is_macos
-    then
-        cmake_args+=('-DALLOW_OPENBLAS_MACOS=ON')
-    fi
-    "${app[cmake]}" -B 'build' "${cmake_args[@]}"
-    "${app[cmake]}" --build 'build'
-    "${app[cmake]}" --install 'build'
+    "${app[cmake]}" . "${cmake_args[@]}"
+    "${app[make]}" VERBOSE=1
+    "${app[make]}" install
     return 0
 }
