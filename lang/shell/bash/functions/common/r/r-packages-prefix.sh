24dacab08bee7f0a142038ce345ade591ada60e8
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-22 10:54:59 -0400

    Rework our app configuration approach

--- lang/shell/bash/functions/common/r/r-packages-prefix.sh
+++ lang/shell/bash/functions/common/r/r-packages-prefix.sh
@@ -1,31 +1,22 @@
 #!/usr/bin/env bash
 
-# FIXME Take this out.
-# FIXME Rework this to just locate the 'site-library' inside the package.
-
 koopa_r_packages_prefix() {
     # """
     # R site library prefix.
-    # @note Updated 2022-07-27.
+    # @note Updated 2022-08-22.
     #
     # @usage
     # > koopa_r_packages_prefix '/opt/koopa/bin/R'
     # # /opt/koopa/app/r-packages/4.2
-    # > koopa_r_packages_prefix '/opt/koopa/bin/R-devel'
-    # # /opt/koopa/app/r-packages/devel
     # """
     local app dict
     declare -A app
     app['r']="${1:?}"
+    [[ -x "${app['r']}" ]] || return 1
     declare -A dict
-    dict['app_prefix']="$(koopa_app_prefix)"
-    dict['name']='r-packages'
-    dict['version']="$(koopa_r_version "${app['r']}")"
-    if [[ "${dict['version']}" != 'devel' ]]
-    then
-        dict['version']="$(koopa_major_minor_version "${dict['version']}")"
-    fi
-    dict['str']="${dict['app_prefix']}/${dict['name']}/${dict['version']}"
+    dict['r_prefix']="$(koopa_r_prefix "${app['r']}")"
+    dict['str']="${dict['r_prefix']}/site-library"
+    [[ -d "${dict['str']}" ]] || return 1
     koopa_print "${dict['str']}"
     return 0
 }
