71289c806a628f5a59f8fbce77781a93a0dd08d1
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-22 08:41:56 -0400

    Update zstd installer

--- lang/shell/bash/functions/common/u/uninstall-zstd.sh
+++ lang/shell/bash/functions/common/u/uninstall-zstd.sh
@@ -3,5 +3,12 @@
 koopa_uninstall_zstd() {
     koopa_uninstall_app \
         --name='zstd' \
+        --unlink-in-bin='pzstd' \
+        --unlink-in-bin='unzstd' \
+        --unlink-in-bin='zstd' \
+        --unlink-in-bin='zstdcat' \
+        --unlink-in-bin='zstdgrep' \
+        --unlink-in-bin='zstdless' \
+        --unlink-in-bin='zstdmt' \
         "$@"
 }
