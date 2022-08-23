6ebdf6e034219d178383762d575fd18e2d16184c
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-23 08:46:53 -0400

    Redefine app variables in JSON

--- lang/shell/bash/include/update/common/shared/r-packages.sh
+++ lang/shell/bash/include/update/common/shared/r-packages.sh
@@ -4,9 +4,10 @@ main() {
     # """
     # Update R packages.
     # @note Updated 2022-02-10.
+    #
+    # Return with success even if 'BiocManager::valid()' check returns false.
     # """
     koopa_assert_has_no_args "$#"
-    # Return with success even if 'BiocManager::valid()' check returns false.
     koopa_r_koopa 'cliUpdateRPackages' "$@" || true
     return 0
 }
