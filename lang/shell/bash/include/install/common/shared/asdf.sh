62cb7686b5825ba740e8f3d9b011dee4c64ae353
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-23 08:26:32 -0400

    Improve asdf install support

--- lang/shell/bash/include/install/common/shared/asdf.sh
+++ lang/shell/bash/include/install/common/shared/asdf.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# NOTE Inform user about 'asdf.sh' shell activation.
+
 main() {
     # """
     # Install asdf.
-    # @note Updated 2022-06-13.
+    # @note Updated 2022-08-23.
     #
     # Be aware that symlink into bin currently won't work (2022-06-13).
     # """
@@ -13,11 +15,15 @@ main() {
         ['prefix']="${INSTALL_PREFIX:?}"
         ['version']="${INSTALL_VERSION:?}"
     )
+    dict['libexec']="${dict['prefix']}/libexec"
     dict['file']="v${dict['version']}.tar.gz"
     dict['url']="https://github.com/asdf-vm/${dict['name']}/archive/refs/\
 tags/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
-    koopa_cp "${dict['name']}-${dict['version']}" "${dict['prefix']}"
+    koopa_cp "${dict['name']}-${dict['version']}" "${dict['libexec']}"
+    koopa_ln \
+        --target-directory="${dict['prefix']}/bin" \
+        "${dict['libexec']}/bin/asdf"
     return 0
 }
