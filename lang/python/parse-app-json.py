039349b8e7b7d2a00495e963b465d971b1e59c6e
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-23 09:38:23 -0400

    Improve our primary JSON parser

--- lang/python/parse-app-json.py
+++ lang/python/parse-app-json.py
@@ -2,22 +2,25 @@
 
 """
 Parse koopa 'app.json' file.
-@note Updated 2022-08-22.
+@note Updated 2022-08-23.
 
 @examples
-./parse-app-json.py '/opt/koopa/include/app.json' 'coreutils' 'bin'
+./parse-app-json.py \
+    --json-file'/opt/koopa/include/app.json' \
+    --app-name='coreutils' \
+    --key='bin'
 """
 
-import argparse
+from argparse import ArgumentParser
 import json
 
 
-def parse_app_json(file, app_name, key):
+def main(json_file, app_name, key):
     """
     Parse the koopa 'app.json' file for defined values.
-    @note Updated 2022-08-22.
+    @note Updated 2022-08-23.
     """
-    with open(file, encoding="utf-8") as con:
+    with open(json_file, encoding="utf-8") as con:
         data = json.load(con)
         keys = data.keys()
         if app_name not in keys:
@@ -30,10 +33,11 @@ def parse_app_json(file, app_name, key):
         return True
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file")
-parser.add_argument("app_name")
-parser.add_argument("key")
+parser = ArgumentParser()
+parser.add_argument("--json-file", required=True, type=str)
+parser.add_argument("--app-name", required=True, type=str)
+parser.add_argument("--key", required=True, type=str)
 args = parser.parse_args()
 
-parse_app_json(file=args.file, app_name=args.app_name, key=args.key)
+
+main(json_file=args.json_file, app_name=args.app_name, key=args.key)
