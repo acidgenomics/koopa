37ac327e9f685c3435f51a7f0778ed3498216e01
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-26 17:44:15 -0400

    Improve alias handling

--- lang/shell/posix/functions/activate-coreutils-aliases.sh
+++ lang/shell/posix/functions/activate-coreutils-aliases.sh
@@ -14,30 +14,11 @@ koopa_activate_coreutils_aliases() {
     # gmv on macOS can run into issues on NFS shares.
     # """
     [ -x "$(koopa_bin_prefix)/gcp" ] || return 0
-    local gcp gcp_args gln gln_args gmkdir gmkdir_args gmv gmv_args grm grm_args
-    gcp='gcp'
-    gln='gln'
-    gmkdir='gmkdir'
-    gmv='gmv'
-    grm='grm'
-    gcp_args='-Riv' # '--interactive --recursive --verbose'.
-    gln_args='-insv' # '--interactive --no-dereference --symbolic --verbose'.
-    gmkdir_args='-pv' # '--parents --verbose'.
-    gmv_args='-iv' # '--interactive --verbose'
-    # Problematic on some file systems: '--dir', '--preserve-root'.
-    # Don't enable '--recursive' here by default, to provide against
-    # accidental deletion of an important directory.
-    grm_args='-iv' # '--interactive=once --verbose'.
-    # shellcheck disable=SC2139
-    alias gcp="${gcp} ${gcp_args}"
-    # shellcheck disable=SC2139
-    alias gln="${gln} ${gln_args}"
-    # shellcheck disable=SC2139
-    alias gmkdir="${gmkdir} ${gmkdir_args}"
-    # shellcheck disable=SC2139
-    alias gmv="${gmv} ${gmv_args}"
-    # shellcheck disable=SC2139
-    alias grm="${grm} ${grm_args}"
+    alias gcp='gcp --interactive --recursive --verbose'
+    alias gln='gln --interactive --no-dereference --symbolic --verbose'
+    alias gmkdir='gmkdir --parents --verbose'
+    alias gmv='gmv --interactive --verbose'
+    alias grm='grm --interactive-once --verbose'
     # Ensure we mask system coreutils.
     alias cp='gcp'
     alias ln='gln'
