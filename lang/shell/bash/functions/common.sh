7e3f5b28fe8f16f609e551679a6292199be19570
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-27 08:06:11 -0400

    Rework version checkers

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -8857,80 +8857,41 @@ koopa_gcrypt_url() {
 }
 
 koopa_get_version() {
-    local dict pos
+    local cmd
     koopa_assert_has_args "$#"
-    declare -A dict=(
-        ['app_name']=''
-        ['opt_name']=''
-        ['opt_prefix']="$(koopa_opt_prefix)"
-    )
-    pos=()
-    while (("$#"))
+    for cmd in "$@"
     do
-        case "$1" in
-            '--app-name='*)
-                dict['app_name']="${1#*=}"
-                shift 1
-                ;;
-            '--app-name')
-                dict['app_name']="${2:?}"
-                shift 2
-                ;;
-            '--opt-name='*)
-                dict['opt_name']="${1#*=}"
-                shift 1
-                ;;
-            '--opt-name')
-                dict['opt_name']="${2:?}"
-                shift 2
-                ;;
-            '-'*)
-                koopa_invalid_arg "$1"
-                ;;
-            *)
-                pos+=("$1")
-                shift 1
-                ;;
-        esac
-    done
-    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
-    if [[ "$#" -gt 0 ]]
-    then
-        koopa_assert_has_args_eq "$#" 1
-        dict['cmd']="${1:?}"
-    else
-        koopa_assert_is_set \
-            '--app-name' "${dict['app_name']}" \
-            '--opt-name' "${dict['opt_name']}"
-        dict['cmd']="${dict['opt_prefix']}/${dict['opt_name']}/bin/${dict['app_name']}"
-    fi
-    dict['bn']="$(koopa_basename "${dict['cmd']}")"
-    dict['bn_snake']="$(koopa_snake_case_simple "${dict['bn']}")"
-    dict['version_arg']="$(__koopa_get_version_arg "${dict['bn']}")"
-    dict['version_fun']="koopa_${dict['bn_snake']}_version"
-    if koopa_is_function "${dict['version_fun']}"
-    then
-        if [[ -x "${dict['cmd']}" ]] && \
-            [[ ! -d "${dict['cmd']}" ]] && \
-            koopa_is_installed "${dict['cmd']}"
+        local dict
+        declare -A dict
+        dict['cmd']="$cmd"
+        dict['bn']="$(koopa_basename "${dict['cmd']}")"
+        dict['bn_snake']="$(koopa_snake_case_simple "${dict['bn']}")"
+        dict['version_arg']="$(__koopa_get_version_arg "${dict['bn']}")"
+        dict['version_fun']="koopa_${dict['bn_snake']}_version"
+        if koopa_is_function "${dict['version_fun']}"
         then
-            dict['str']="$("${dict['version_fun']}" "${dict['cmd']}")"
-        else
-            dict['str']="$("${dict['version_fun']}")"
+            if [[ -x "${dict['cmd']}" ]] && \
+                [[ ! -d "${dict['cmd']}" ]] && \
+                koopa_is_installed "${dict['cmd']}"
+            then
+                dict['str']="$("${dict['version_fun']}" "${dict['cmd']}")"
+            else
+                dict['str']="$("${dict['version_fun']}")"
+            fi
+            [[ -n "${dict['str']}" ]] || return 1
+            koopa_print "${dict['str']}"
+            continue
         fi
+        [[ -x "${dict['cmd']}" ]] || return 1
+        [[ ! -d "${dict['cmd']}" ]] || return 1
+        koopa_is_installed "${dict['cmd']}" || return 1
+        dict['cmd']="$(koopa_realpath "${dict['cmd']}")"
+        dict['str']="$("${dict['cmd']}" "${dict['version_arg']}" 2>&1 || true)"
+        [[ -n "${dict['str']}" ]] || return 1
+        dict['str']="$(koopa_extract_version "${dict['str']}")"
         [[ -n "${dict['str']}" ]] || return 1
         koopa_print "${dict['str']}"
-        return 0
-    fi
-    [[ -x "${dict['cmd']}" ]] || return 1
-    [[ ! -d "${dict['cmd']}" ]] || return 1
-    koopa_is_installed "${dict['cmd']}" || return 1
-    dict['cmd']="$(koopa_realpath "${dict['cmd']}")"
-    dict['str']="$("${dict['cmd']}" "${dict['version_arg']}" 2>&1 || true)"
-    [[ -n "${dict['str']}" ]] || return 1
-    dict['str']="$(koopa_extract_version "${dict['str']}")"
-    [[ -n "${dict['str']}" ]] || return 1
-    koopa_print "${dict['str']}"
+    done
     return 0
 }
 
