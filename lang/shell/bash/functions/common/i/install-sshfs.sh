8a3de25d4c0ff2f2793a25e69e1054f743cd2bc1
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-27 09:15:42 -0400

    Remove sshfs support

--- lang/shell/bash/functions/common/i/install-sshfs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-koopa_install_sshfs() {
-    koopa_install_app \
-        --name='sshfs' \
-        "$@"
-}
--- lang/shell/bash/functions/common/u/uninstall-sshfs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-koopa_uninstall_sshfs() {
-    koopa_uninstall_app \
-        --name='sshfs' \
-        "$@"
-}
