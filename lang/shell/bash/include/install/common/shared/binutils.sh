cfeccc59c44315507c40015ec51d3b8c58988c66
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-30 16:41:26 -0400

    Rework binutils

--- lang/shell/bash/include/install/common/shared/binutils.sh
+++ lang/shell/bash/include/install/common/shared/binutils.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 main() {
-    # """
-    # Consider including zlib.
-    # """
-    koopa_activate_opt_prefix 'texinfo'
+    koopa_activate_build_opt_prefix 'bison'
+    koopa_activate_opt_prefix 'zlib' 'texinfo'
     koopa_install_app_internal \
         --installer='gnu-app' \
         --name='binutils' \
