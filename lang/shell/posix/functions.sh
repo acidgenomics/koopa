daf25876b6712cadcaa950050fac8b5dc1217d62
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-30 13:19:56 -0400

    Allow system passthrough

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -265,6 +265,10 @@ koopa_activate_conda() {
 koopa_activate_coreutils_aliases() {
     local bin_prefix
     bin_prefix="$(koopa_bin_prefix)"
+    if [ -x "${bin_prefix}/gcat" ]
+    then
+        alias cat='gcat'
+    fi
     if [ -x "${bin_prefix}/gcp" ]
     then
         alias gcp='gcp --interactive --recursive --verbose'
@@ -333,6 +337,14 @@ koopa_activate_coreutils_aliases() {
     if [ -x "${bin_prefix}/gtail" ]
     then
         alias tail='gtail'
+    fi
+    if [ -x "${bin_prefix}/gtar" ]
+    then
+        alias tar='gtar'
+    fi
+    if [ -x "${bin_prefix}/gtouch" ]
+    then
+        alias touch='gtouch'
     fi
     if [ -x "${bin_prefix}/gtr" ]
     then
