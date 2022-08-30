ba2c88f2ab340588284e4f376a77f3a6b4c6918c
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-30 13:25:45 -0400

    Attempt to rework

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -21596,7 +21596,16 @@ koopa_system_info() {
         ['bash']="$(koopa_locate_bash --allow-missing)"
         ['cat']="$(koopa_locate_cat --allow-missing)"
     )
-    [[ ! -x "${app['bash']}" ]] && app['bash']='/usr/bin/bash'
+    if [[ ! -x "${app['bash']}" ]]
+    then
+        if [[ -x '/usr/bin/bash' ]]
+        then
+            app['bash']='/usr/bin/bash'
+        elif [[ -x '/bin/bash' ]]
+        then
+            app['bash']='/bin/bash'
+        fi
+    fi
     if [[ ! -x "${app['cat']}" ]]
     then
         if [[ -x '/usr/bin/cat' ]]
