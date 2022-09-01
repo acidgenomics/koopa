a495309cb7a4f552065a376ddcb7ae4c29e88de1
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-01 09:56:14 -0400

    Add Python linters

---
+++ lang/shell/bash/functions/common/i/install-autoflake.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_install_autoflake() {
+    koopa_install_app \
+        --name='autoflake' \
+        "$@"
+}
---
+++ lang/shell/bash/functions/common/i/install-pycodestyle.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_install_pycodestyle() {
+    koopa_install_app \
+        --name='pycodestyle' \
+        "$@"
+}
