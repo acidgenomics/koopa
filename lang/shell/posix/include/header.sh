aa95076bf65e72a7bc5c315c2ef95617cc5be06b
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-02 10:29:14 -0400

    Rework shell activation

--- lang/shell/posix/include/header.sh
+++ lang/shell/posix/include/header.sh
@@ -3,7 +3,7 @@
 __koopa_posix_header() {
     # """
     # POSIX shell header.
-    # @note Updated 2022-08-30.
+    # @note Updated 2022-09-02.
     # """
     [ "$#" -eq 0 ] || return 1
     if [ -z "${KOOPA_PREFIX:-}" ]
@@ -31,96 +31,101 @@ __koopa_posix_header() {
     then
         export KOOPA_DEFAULT_SYSTEM_PATH="${PATH:-}"
     fi
-    if [ "${KOOPA_ACTIVATE:-0}" -eq 0 ]
+    case "${KOOPA_ACTIVATE:-0}" in
+        '0')
+            # Intentionally not including '/usr/local/bin' here.
+            # Consider including koopa bootstrap bin for macOS.
+            export PATH='/usr/bin:/bin'
+            # Ensure these are never set (e.g. inside RStudio terminal).
+            unset -v LD_LIBRARY_PATH PYTHONPATH
+            ;;
+        '1')
+            __koopa_activate_koopa
+            ;;
+    esac
+    if [ "${KOOPA_TEST:-0}" -eq 1 ]
     then
-        # Intentionally not including '/usr/local/bin' here.
-        # Consider including koopa bootstrap bin for macOS.
-        export PATH='/usr/bin:/bin'
+        koopa_duration_stop 'posix' || return 1
     fi
-    # Ensure these are never set (e.g. inside RStudio terminal).
-    # > unset -v \
-    # >     LD_LIBRARY_PATH \
-    # >     PYTHONPATH
+    return 0
+}
+
+__koopa_activate_koopa() {
+    # """
+    # Activate koopa in interactive shell.
+    # @note Updated 2022-09-02.
+    # """
     if [ "${KOOPA_MINIMAL:-0}" -eq 0 ]
     then
         koopa_activate_path_helper || return 1
     fi
     koopa_add_to_path_start "${KOOPA_PREFIX}/bin" || return 1
     koopa_add_to_manpath_start "${KOOPA_PREFIX}/share/man" || return 1
-    if [ "${KOOPA_MINIMAL:-0}" -eq 0 ]
+    [ "${KOOPA_MINIMAL:-0}" -eq 0 ] || return 0
+    koopa_add_to_path_start \
+        '/usr/local/bin'
+    koopa_add_to_manpath_start \
+        '/usr/local/man' \
+        '/usr/local/share/man'
+    koopa_add_to_manpath_end '/usr/share/man'
+    # > koopa_umask || return 1
+    koopa_export_koopa_cpu_count || return 1
+    koopa_export_koopa_shell || return 1
+    # Edge case for RStudio Server terminal to support dircolors.
+    [ -n "${SHELL:-}" ] && export SHELL
+    koopa_activate_xdg || return 1
+    koopa_export_editor || return 1
+    koopa_export_git || return 1
+    koopa_export_gnupg || return 1
+    koopa_export_history || return 1
+    koopa_export_pager || return 1
+    koopa_activate_ca_certificates || return 1
+    koopa_activate_homebrew || return 1
+    koopa_activate_google_cloud_sdk || return 1
+    koopa_activate_ruby || return 1
+    koopa_activate_julia || return 1
+    koopa_activate_python || return 1
+    koopa_activate_pipx || return 1
+    koopa_activate_bcbio_nextgen || return 1
+    koopa_activate_color_mode || return 1
+    koopa_activate_alacritty || return 1
+    koopa_activate_bat || return 1
+    koopa_activate_delta || return 1
+    koopa_activate_difftastic || return 1
+    koopa_activate_dircolors || return 1
+    koopa_activate_fzf || return 0
+    koopa_activate_gcc_colors || return 1
+    koopa_activate_kitty || return 1
+    koopa_activate_lesspipe || return 1
+    koopa_activate_secrets || return 1
+    koopa_activate_ssh_key || return 1
+    koopa_activate_tealdeer || return 1
+    if koopa_is_macos
     then
-        koopa_add_to_path_start \
-            '/usr/local/bin'
-        koopa_add_to_manpath_start \
-            '/usr/local/man' \
-            '/usr/local/share/man'
-        # > koopa_umask || return 1
-        koopa_export_koopa_cpu_count || return 1
-        koopa_export_koopa_shell || return 1
-        # Edge case for RStudio Server terminal to support dircolors correctly.
-        [ -n "${SHELL:-}" ] && export SHELL
-        koopa_activate_xdg || return 1
+        koopa_macos_activate_cli_colors || return 1
+    fi
+    case "$(koopa_shell_name)" in
+        'zsh')
+            alias conda='koopa_alias_conda'
+            ;;
+        *)
+            koopa_activate_conda || return 1
+            ;;
+    esac
+    # Previously included:
+    # > "$(koopa_xdg_local_home)/bin"
+    koopa_add_to_path_start \
+        "$(koopa_scripts_private_prefix)/bin" \
+        || return 1
+    koopa_activate_aliases || return 1
+    if ! koopa_is_subshell
+    then
         koopa_add_config_link \
             "$(koopa_koopa_prefix)" 'home' \
             "$(koopa_koopa_prefix)/activate" 'activate' \
             "$(koopa_dotfiles_prefix)" 'dotfiles' \
             || return 1
-        koopa_add_to_manpath_end '/usr/share/man'
-        koopa_activate_homebrew || return 1
-        koopa_activate_google_cloud_sdk || return 1
-        koopa_activate_ruby || return 1
-        koopa_activate_julia || return 1
-        koopa_activate_python || return 1
-        koopa_activate_pipx || return 1
-        koopa_activate_bcbio_nextgen || return 1
-        if [ "${KOOPA_ACTIVATE:-0}" -eq 1 ]
-        then
-            koopa_export_editor || return 1
-            koopa_export_git || return 1
-            koopa_export_gnupg || return 1
-            koopa_export_history || return 1
-            koopa_export_pager || return 1
-            koopa_activate_ca_certificates || return 1
-            koopa_activate_color_mode || return 1
-            koopa_activate_alacritty || return 1
-            koopa_activate_bat || return 1
-            koopa_activate_delta || return 1
-            koopa_activate_difftastic || return 1
-            koopa_activate_dircolors || return 1
-            koopa_activate_fzf || return 0
-            koopa_activate_gcc_colors || return 1
-            koopa_activate_kitty || return 1
-            koopa_activate_lesspipe || return 1
-            koopa_activate_secrets || return 1
-            koopa_activate_ssh_key || return 1
-            koopa_activate_tealdeer || return 1
-            if koopa_is_macos
-            then
-                koopa_macos_activate_cli_colors || return 1
-            fi
-            case "$(koopa_shell_name)" in
-                'zsh')
-                    alias conda='koopa_alias_conda'
-                    ;;
-                *)
-                    koopa_activate_conda || return 1
-                    ;;
-            esac
-            # Previously included:
-            # > "$(koopa_xdg_local_home)/bin"
-            koopa_add_to_path_start \
-                "$(koopa_scripts_private_prefix)/bin" \
-                || return 1
-            koopa_activate_aliases || return 1
-            if ! koopa_is_subshell
-            then
-                koopa_activate_today_bucket || return 1
-            fi
-        fi
-    fi
-    if [ "${KOOPA_TEST:-0}" -eq 1 ]
-    then
-        koopa_duration_stop 'posix' || return 1
+        koopa_activate_today_bucket || return 1
     fi
     return 0
 }
