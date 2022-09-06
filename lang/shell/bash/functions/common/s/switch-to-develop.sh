d835a57895b02338e1807f306bacfd2828215915
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-06 13:41:22 -0400

    Attempt to improve permission issue

--- lang/shell/bash/functions/common/s/switch-to-develop.sh
+++ lang/shell/bash/functions/common/s/switch-to-develop.sh
@@ -3,7 +3,7 @@
 koopa_switch_to_develop() {
     # """
     # Switch koopa install to development version.
-    # @note Updated 2022-08-30.
+    # @note Updated 2022-09-06.
     #
     # @seealso
     # - https://stackoverflow.com/questions/49297153/
@@ -18,16 +18,16 @@ koopa_switch_to_develop() {
         ['branch']='develop'
         ['origin']='origin'
         ['prefix']="$(koopa_koopa_prefix)"
+        ['user']="$(koopa_user)"
     )
     koopa_alert "Switching koopa at '${dict['prefix']}' to '${dict['branch']}'."
+    if koopa_is_shared_install
+    then
+        koopa_chown --recursive --sudo "${dict['user']}" "${dict['prefix']}"
+    fi
     koopa_sys_set_permissions --recursive "${dict['prefix']}"
     (
         koopa_cd "${dict['prefix']}"
-        # > "${app['git']}" fetch --unshallow || true
-        # > "${app['git']}" checkout \
-        # >     -B "${dict['branch']}" \
-        # >     "${dict['origin']}/${dict['branch']}"
-        # This approach works with shallow clone:
         "${app['git']}" remote set-branches \
             --add "${dict['origin']}" "${dict['branch']}"
         "${app['git']}" fetch "${dict['origin']}"
