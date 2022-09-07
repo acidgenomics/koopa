eeff1d5af5ce50ec92cc66125ab32b46e4e37c87
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-07 15:40:32 -0400

    Add FIXMEs regarding improved shell config

--- lang/shell/bash/functions/common/i/install-nushell.sh
+++ lang/shell/bash/functions/common/i/install-nushell.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Need to configure '/etc/shells' for shared install.
+# FIXME Need to symlink Application Support on macOS (do this in dotfiles).
+
 koopa_install_nushell() {
     koopa_install_app \
         --name='nushell' \
