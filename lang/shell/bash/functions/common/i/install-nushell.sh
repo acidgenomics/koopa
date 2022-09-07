1a72317f00cc23dc6439a9f0feefdd2ab490e4e5
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-07 13:54:03 -0400

    Add nushell support

---
+++ lang/shell/bash/functions/common/i/install-nushell.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_install_nushell() {
+    koopa_install_app \
+        --name='nushell' \
+        "$@"
+}
---
+++ lang/shell/bash/include/install/common/shared/nushell.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# NOTE Consider adding libx11, libxcb.
+
+main() {
+    # """
+    # @seealso
+    # - https://www.nushell.sh/book/installation.html#build-from-source
+    # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/nushell.rb
+    # - https://crates.io/crates/nu
+    # """
+    koopa_activate_opt_prefix 'zlib' 'openssl3'
+    koopa_install_app_passthrough \
+        --name='nushell' \
+        --installer='rust-package' \
+        "$@"
+}
