7a8ac740648a6b6b1a6f1fc495bf9fc0afa0cf86
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-08 13:06:59 -0400

    Draft updates

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -479,6 +479,7 @@ koopa_activate_homebrew() {
     [ -x "${prefix}/bin/brew" ] || return 0
     export HOMEBREW_CLEANUP_MAX_AGE_DAYS=30
     export HOMEBREW_INSTALL_CLEANUP=1
+    export HOMEBREW_INSTALL_FROM_API=1
     export HOMEBREW_NO_ANALYTICS=1
     export HOMEBREW_NO_AUTO_UPDATE=1
     export HOMEBREW_NO_ENV_HINTS=1
