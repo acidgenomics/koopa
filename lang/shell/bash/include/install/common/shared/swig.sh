5b26c9a18353cdf4787c8efdf9521df3d8f71f4d
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-08 11:07:56 -0400

    Work on LLVM install

---
+++ lang/shell/bash/include/install/common/shared/swig.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+main() {
+    # """
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/swig.rb
+    # """
+    local app dict
+    koopa_assert_has_no_args "$#"
+    koopa_activate_opt_prefix 'zlib' 'pcre'
+    declare -A app
+    app['make']="$(koopa_locate_make)"
+    [[ -x "${app['make']}" ]] || return 1
+    declare -A dict=(
+        ['jobs']="$(koopa_cpu_count)"
+        ['name']='swig'
+        ['prefix']="${INSTALL_PREFIX:?}"
+        ['version']="${INSTALL_VERSION:?}"
+    )
+    dict['file']="${dict['name']}-${dict['version']}.tar.gz"
+    dict['url']="https://downloads.sourceforge.net/project/${dict['name']}/\
+${dict['name']}/${dict['name']}-${dict['version']}/${dict['file']}"
+    koopa_download "${dict['url']}" "${dict['file']}"
+    koopa_extract "${dict['file']}"
+    koopa_cd "${dict['name']}-${dict['version']}"
+    conf_args=(
+        "--prefix=${dict['prefix']}"
+        '--disable-dependency-tracking'
+    )
+    ./configure --help
+    ./configure "${conf_args[@]}"
+    "${app['make']}" --jobs="${dict['jobs']}"
+    "${app['make']}" install
+    return 0
+}
