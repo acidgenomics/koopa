1c4e2d1225210381379587f9530efe808d823d93
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-09 18:13:52 -0400

    Work on improving neovim build

---
+++ lang/shell/bash/functions/common/u/uninstall-libtermkey.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_uninstall_libtermkey() {
+    koopa_uninstall_app \
+        --name='libtermkey' \
+        "$@"
+}
---
+++ lang/shell/bash/include/install/common/shared/unibilium.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+main() {
+    # """
+    # Install unibilium.
+    # @note Updated 2022-09-09.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/HEAD/
+    #     Formula/unibilium.rb
+    # """
+    local app dict
+    koopa_activate_build_opt_prefix 'libtool' 'pkg-config'
+    declare -A app=(
+        ['make']="$(koopa_locate_make)"
+    )
+    [[ -x "${app['make']}" ]] || return 1
+    declare -A dict=(
+        ['name']='unibilium'
+        ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+        ['version']="${KOOPA_INSTALL_VERSION:?}"
+    )
+    dict['file']="v${dict['version']}.tar.gz"
+    dict['url']="https://github.com/neovim/${dict['name']}/\
+archive/${dict['file']}"
+    koopa_download "${dict['url']}" "${dict['file']}"
+    koopa_extract "${dict['file']}"
+    koopa_cd "${dict['name']}-${dict['version']}"
+    "${app['make']}"
+    "${app['make']}" install PREFIX="${dict['prefix']}"
+    return 0
+}
