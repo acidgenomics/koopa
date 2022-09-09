4db624984a3a41d6e74b481c63fd733ca419b30e
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-08 22:00:01 -0400

    Work on improving elfutils

--- lang/shell/bash/include/install/linux/shared/elfutils.sh
+++ lang/shell/bash/include/install/linux/shared/elfutils.sh
@@ -10,31 +10,48 @@ main() {
     koopa_activate_opt_prefix \
         'bzip2' \
         'xz' \
-        'zlib'
+        'zlib' \
+        'zstd' \
+        'gettext' \
+        'libiconv'
     declare -A app=(
         ['make']="$(koopa_locate_make)"
     )
     [[ -x "${app['make']}" ]] || return 1
     declare -A dict=(
+        ['gettext']="$(koopa_app_prefix 'gettext')"
         ['jobs']="$(koopa_cpu_count)"
+        ['libiconv']="$(koopa_app_prefix 'libiconv')"
         ['name']='elfutils'
         ['prefix']="${INSTALL_PREFIX:?}"
         ['version']="${INSTALL_VERSION:?}"
+    )
+    koopa_assert_is_dir \
+        "${dict['gettext']}" \
+        "${dict['libiconv']}"
+    conf_args=(
+        # > '--program-prefix=elfutils-'
+        # > '--with-biarch'
+        # > '--with-valgrind'
+        "--prefix=${dict['prefix']}"
+        '--disable-debuginfod'
+        '--disable-debugpred'
+        '--disable-dependency-tracking'
+        '--disable-libdebuginfod'
+        '--disable-silent-rules'
+        '--enable-install-elfh'
+        '--with-bzlib'
+        "--with-libiconv-prefix${dict['libiconv']}"
+        "--with-libintl-prefix=${dict['gettext']}"
+        '--with-zlib'
+        '--with-zstd'
+        '--without-lzma'
     )
     dict['file']="${dict['name']}-${dict['version']}.tar.bz2"
     dict['url']="https://sourceware.org/elfutils/ftp/0.187/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
-    conf_args=(
-        "--prefix=${dict['prefix']}"
-        '--disable-debug'
-        '--disable-debuginfod'
-        '--disable-dependency-tracking'
-        '--disable-libdebuginfod'
-        '--disable-silent-rules'
-        '--program-prefix=elfutils-'
-    )
     ./configure --help
     ./configure "${conf_args[@]}"
     "${app['make']}" --jobs="${dict['jobs']}"
