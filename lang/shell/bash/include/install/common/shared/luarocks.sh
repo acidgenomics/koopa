1b6b07dea3d622567fa381d3dfabb53651bb77f5
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-10 08:09:54 -0400

    Improve configuration for neovim build

--- lang/shell/bash/include/install/common/shared/luarocks.sh
+++ lang/shell/bash/include/install/common/shared/luarocks.sh
@@ -1,25 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME mpack is failing to build.
-#lmpack.c:721:3: error: implicitly declaring library function 'snprintf' with type 'int (char *, unsigned long, const char *, ...)' [-Werror,-Wimplicit-function-declaration]
-#                snprintf(errmsg, 50, "can't serialize object of type %d", type);
-#                ^
-#lmpack.c:721:3: note: include the header <stdio.h> or explicitly provide a declaration for 'snprintf'
-#1 error generated.
-#
-#Error: Build error: Failed compiling object lmpack.o
-#Installing https://luarocks.org/mpack-1.0.9-0.src.rock
-#
-#env MACOSX_DEPLOYMENT_TARGET=11.0 gcc -O2 -fPIC -I/opt/koopa/app/lua/5.4.4/include -c lmpack.c -o lmpack.o
-
 main() {
     # """
     # Install Luarocks.
     # @note Updated 2022-09-10.
-    #
-    # @seealso
-    # - https://github.com/luarocks/luarocks/issues/422
-    # - https://github.com/gphoto/libgphoto2/issues/633
     # """
     local app conf_args dict
     koopa_assert_has_no_args "$#"
@@ -37,7 +21,9 @@ main() {
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
     dict['lua_version']="$(koopa_get_version "${app['lua']}")"
-    dict['lua_maj_min_ver']="$(koopa_major_minor_version "${dict['lua_version']}")"
+    dict['lua_maj_min_ver']="$( \
+        koopa_major_minor_version "${dict['lua_version']}" \
+    )"
     dict['file']="${dict['name']}-${dict['version']}.tar.gz"
     dict['url']="https://luarocks.org/releases/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
@@ -51,31 +37,5 @@ main() {
     ./configure "${conf_args[@]}"
     "${app['make']}" build
     "${app['make']}" install
-    # FIXME Isolate these rocks in lmod and neovim packages instead.
-    # > app['luarocks']="${dict['prefix']}/bin/luarocks"
-    # > koopa_assert_is_installed "${app['luarocks']}"
-    # > if koopa_is_macos
-    # > then
-    # >     # This fix is needed for mpack to build.
-    # >     CFLAGS="-D_DARWIN_C_SOURCE ${CFLAGS:-}"
-    # >     export CFLAGS
-    # > fi
-    # FIXME Harden our config by version pinning here.
-    # > (
-    # >     koopa_cd "${dict['prefix']}"
-    # >     # Lmod dependencies.
-    # >     "${app['luarocks']}" install 'luaposix'
-    # >     "${app['luarocks']}" install 'luafilesystem'
-    # >     # Neovim dependencies.
-    # >     # FIXME Either this is the wrong name or not available for Lua 5.4.
-    # >     # FIXME bit is available for LuaJIT, so maybe we need to use that instead.
-    # >     # https://github.com/neovim/neovim/issues/11352
-    # >     # https://bitop.luajit.org/
-    # >     # FIXME Not available for Lua, need to use LuaJIT.
-    # >     # Not sure how to do this argh....
-    # >     "${app['luarocks']}" install 'bitop'
-    # >     "${app['luarocks']}" install 'lpeg'
-    # >     "${app['luarocks']}" install 'mpack'
-    # > )
     return 0
 }
