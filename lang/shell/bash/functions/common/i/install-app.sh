db1f452d584769deecaab0f3e11bd072e55ed4fd
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-12 10:46:46 -0400

    Improve installers

--- lang/shell/bash/functions/common/i/install-app.sh
+++ lang/shell/bash/functions/common/i/install-app.sh
@@ -3,7 +3,7 @@
 koopa_install_app() {
     # """
     # Install application in a versioned directory structure.
-    # @note Updated 2022-09-08.
+    # @note Updated 2022-09-12.
     # """
     local bin_arr bool dict i man1_arr pos
     koopa_assert_has_args "$#"
