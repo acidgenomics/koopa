2b48ed7d5f917090573ce6204913e4b91db4a995
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-12 18:07:50 -0400

    Improve compiler consistency

--- lang/shell/bash/functions/common/r/r-configure-ldpaths.sh
+++ lang/shell/bash/functions/common/r/r-configure-ldpaths.sh
@@ -53,6 +53,7 @@ koopa_r_configure_ldpaths() {
         'fribidi'
         'gdal'
         'geos'
+        'gettext'
         'graphviz'
         'harfbuzz'
         'icu4c'
@@ -75,10 +76,6 @@ koopa_r_configure_ldpaths() {
         'zlib'
         'zstd'
     )
-    if koopa_is_macos || [[ "${dict['system']}" -eq 0 ]]
-    then
-        keys+=('gettext')
-    fi
     for key in "${keys[@]}"
     do
         local prefix
