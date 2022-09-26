36ce24b9590f2f4a1eb440077efd46d442c6e227
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-26 13:50:28 -0400

    Draft update to Python install

--- lang/shell/bash/include/install/common/shared/python.sh
+++ lang/shell/bash/include/install/common/shared/python.sh
@@ -1,34 +1,49 @@
 #!/usr/bin/env bash
 
 # FIXME Hardened installer is now failing.
-
+#
 # FIXME _bz2 import is failing on Ubuntu 22.
-# Need to resolve this without installing system package.
-
-# NOTE It looks like Python includes '/usr/local' in '-I' and '-L' compilation
-# arguments by default. We should work on restricting this in a future build.
-
+# Need to resolve this without installing system package. 
+#
 # NOTE Consider including support for:
 # - libxcrypt
 # - mpdecimal
 # - unzip
-
+#
 # NOTE Consider cleaning this up on macOS:
 # clang: warning: argument unused during compilation:
 # '-fno-semantic-interposition' [-Wunused-command-line-argument]
-
+#
 # NOTE Likely need to implement this to fix ncurses location on Linux:
 # > inreplace "configure",
 # >     'CPPFLAGS="$CPPFLAGS -I/usr/include/ncursesw"',
 # >     "CPPFLAGS=\"$CPPFLAGS -I#{Formula["ncurses"].opt_include}\""
-
+#
 # NOTE Now seeing this warning:
 # > warning: "_XOPEN_SOURCE" redefined
+#
+# FIXME May need to suppress this on macOS:
+# ld: warning: -undefined dynamic_lookup may not work with chained fixups
+#
+# FIXME Now seeing this error on macOS:
+# Is this due to an unwanted CLT update?
+# gmake[3]: *** [Makefile:637: sharedmods] Hangup: 1
+# gmake[2]: *** [Makefile:531: build_all_generate_profile] Hangup: 1
+# gmake[1]: *** [Makefile:507: profile-gen-stamp] Hangup: 1
+# gmake: *** [Makefile:519: profile-run-stamp] Hangup: 1
+# koopa_macos_xcode_clt_version
+# 14.1.0.0.1.1662869207
+#
+# FIXME Yep still hitting this issue on macOS, argh:
+# gmake: *** [Makefile:637: sharedmods] Hangup: 1
 
 main() {
     # """
     # Install Python.
-    # @note Updated 2022-08-30.
+    # @note Updated 2022-09-26.
+    #
+    # Python includes '/usr/local' in '-I' and '-L' compilation arguments by
+    # default. We should work on restricting this in a future build.
     #
     # Check config with:
     # > ldd /usr/local/bin/python3
@@ -54,23 +69,23 @@ main() {
         # zlib deps: none.
         'zlib'
         # bzip2 deps: none.
-        'bzip2'
+        # > 'bzip2'
         # expat deps: none.
-        'expat'
+        #'expat'
         # libffi deps: none.
-        'libffi'
+        #'libffi'
         # ncurses deps: none.
-        'ncurses'
+        #'ncurses'
         # openssl3 deps: none.
-        'openssl3'
+        #'openssl3'
         # xz deps: none.
-        'xz'
+        #'xz'
         # readline deps: ncurses.
-        'readline'
+        #'readline'
         # gdbm deps: readline.
-        'gdbm'
+        #'gdbm'
         # sqlite deps: readline.
-        'sqlite'
+        #'sqlite'
     )
     koopa_activate_opt_prefix "${deps[@]}"
     declare -A app=(
@@ -101,30 +116,30 @@ ${dict['file']}"
     koopa_cd "Python-${dict['version']}"
     conf_args=(
         "--prefix=${dict['prefix']}"
-        '--enable-ipv6'
-        '--enable-loadable-sqlite-extensions'
-        '--enable-optimizations'
-        '--enable-shared'
-        '--with-dbmliborder=gdbm:ndbm'
-        '--with-ensurepip'
-        '--with-lto'
-        "--with-openssl=${dict['openssl']}"
-        '--with-openssl-rpath=auto'
+        #'--enable-ipv6'
+        #'--enable-loadable-sqlite-extensions'
+        #'--enable-optimizations'
+        #'--enable-shared'
+        #'--with-dbmliborder=gdbm:ndbm'
+        #'--with-ensurepip'
+        #'--with-lto'
+        #"--with-openssl=${dict['openssl']}"
+        #'--with-openssl-rpath=auto'
     )
-    if koopa_is_macos
-    then
-        conf_args+=(
-            '--disable-framework'
-            '--with-dtrace=/usr/sbin/dtrace'
-        )
-    fi
+    #if koopa_is_macos
+    #then
+    #    conf_args+=(
+    #        '--disable-framework'
+    #        '--with-dtrace=/usr/sbin/dtrace'
+    #    )
+    #fi
     # > conf_args+=(
     # >     "CFLAGS=${CFLAGS:-}"
     # >     "LDFLAGS=${LDFLAGS:-}"
     # > )
     koopa_add_rpath_to_ldflags \
-        "${dict['prefix']}/lib" \
-        "${dict['bzip2']}/lib"
+        "${dict['prefix']}/lib"
+        # > "${dict['bzip2']}/lib"
     koopa_print_env
     koopa_dl 'configure args' "${conf_args[*]}"
     ./configure --help
