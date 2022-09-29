e0c92e1508abca83832c2339062042c80eb19e3a
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-29 11:41:03 -0400

    Update grep recipe

--- lang/shell/bash/include/install/common/shared/grep.sh
+++ lang/shell/bash/include/install/common/shared/grep.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME This isn't detecting pcre correctly...
-# checking for PCRE... no
-# checking for pcre2_compile... no
-# configure: WARNING: GNU grep will be built without PCRE support.
-
 main() {
     koopa_activate_opt_prefix 'pcre2'
     koopa_install_app_subshell \
