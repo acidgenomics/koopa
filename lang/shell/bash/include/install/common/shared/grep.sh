365c4b67b0e8e9d0fbd855294738ba32b5cd542f
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-29 11:40:45 -0400

    Improve grep recipe

--- lang/shell/bash/include/install/common/shared/grep.sh
+++ lang/shell/bash/include/install/common/shared/grep.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+# FIXME This isn't detecting pcre correctly...
+# checking for PCRE... no
+# checking for pcre2_compile... no
+# configure: WARNING: GNU grep will be built without PCRE support.
+
 main() {
-    koopa_activate_opt_prefix 'pcre'
+    koopa_activate_opt_prefix 'pcre2'
     koopa_install_app_subshell \
         --installer='gnu-app' \
         --name='grep' \
+        -D '--disable-dependency-tracking' \
+        -D '--disable-nls' \
         -D '--program-prefix=g' \
         "$@"
 }
