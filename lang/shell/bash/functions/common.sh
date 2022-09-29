c206ac902c01093c3f345afb83c81ee7cb103929
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-29 06:12:45 -0400

    Tweak sra-tools recipe

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -14473,33 +14473,6 @@ koopa_is_xcode_clt_installed() {
     return 0
 }
 
-koopa_java_prefix() {
-    local prefix
-    if [[ -n "${JAVA_HOME:-}" ]]
-    then
-        koopa_print "$JAVA_HOME"
-        return 0
-    fi
-    if [[ -d "$(koopa_openjdk_prefix)" ]]
-    then
-        koopa_print "$(koopa_openjdk_prefix)"
-        return 0
-    fi
-    if [[ -x '/usr/libexec/java_home' ]]
-    then
-        prefix="$('/usr/libexec/java_home' || true)"
-        [ -n "$prefix" ] || return 1
-        koopa_print "$prefix"
-        return 0
-    fi
-    if [[ -d "$(koopa_homebrew_opt_prefix)/openjdk" ]]
-    then
-        koopa_print "$(koopa_homebrew_opt_prefix)/openjdk"
-        return 0
-    fi
-    return 1
-}
-
 koopa_jekyll_deploy_to_aws() {
     local app dict
     koopa_assert_has_args "$#"
