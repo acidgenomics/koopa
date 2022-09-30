6df703265b913e9cfcca21640b1c739c5cb9798d
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-30 06:58:29 -0400

    Rework Julia installer

--- lang/shell/bash/include/install/common/shared/julia.sh
+++ lang/shell/bash/include/install/common/shared/julia.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Currently error on macOS after openblas build step.
+
 main() {
     # """
     # Install Julia (from source).
@@ -25,43 +27,46 @@ main() {
     )
     koopa_activate_build_opt_prefix "${build_deps[@]}"
     deps=(
-    # >     # deps: none.
-    # >     'zlib'
-    # >     # deps: none.
-    # >     'zstd'
-    # >     # deps: none.
-    # >     'ca-certificates'
-    # >     # deps: ca-certificates.
-    # >     'openssl3'
-    # >     # deps: ca-certificates, openssl3, zlib, zstd.
-    # >     'curl'
-    # >     # deps: bzip2, zlib.
-    # >     'pcre2'
-    # >     # deps: openssl3.
-    # >     'libssh2'
-    # >     # deps: libssh2, openssl3, pcre, zlib.
-    # >     'libgit2'
-    # >     # deps: m4.
-    # >     'gmp'
-    # >     # deps: gmp.
-    # >     'mpfr'
-    # >     # deps: gmp, mpfr, mpc.
+        # deps: none.
+        'zlib'
+        # deps: none.
+        'zstd'
+        # deps: none.
+        'ca-certificates'
+        # deps: ca-certificates.
+        'openssl3'
+        # deps: ca-certificates, openssl3, zlib, zstd.
+        'curl'
+        # deps: bzip2, zlib.
+        'pcre2'
+        # deps: openssl3.
+        'libssh2'
+        # deps: libssh2, openssl3, pcre, zlib.
+        'libgit2'
+        # deps: m4.
+        'gmp'
+        # deps: gmp.
+        'mpfr'
+        # deps: gmp, mpfr, mpc.
         'gcc' # gfortran
-    # >     # deps: gcc.
-    # >     'lapack'
-    # >     # deps: gcc.
-    # >     'openblas'
-    # >     # deps: none.
-    # >     'utf8proc'
+        # deps: gcc.
+        'lapack'
+        # deps: gcc.
+        'openblas'
+        # deps: none.
+        'utf8proc'
+        'llvm'
     )
-    koopa_activate_opt_prefix "${deps[@]}"
+    if ! koopa_is_macos
+    then
+        koopa_activate_opt_prefix "${deps[@]}"
+    fi
     declare -A app=(
         ['make']="$(koopa_locate_make)"
-        # > [python]="$(koopa_locate_python)"
+        ['python']="$(koopa_locate_python --realpath)"
     )
     [[ -x "${app['make']}" ]] || return 1
-    # > [[ -x "${app['python']}" ]] || return 1
-    # > app['python']="$(koopa_realpath "${app['python']}")"
+    [[ -x "${app['python']}" ]] || return 1
     declare -A dict=(
         ['jobs']="$(koopa_cpu_count)"
         ['name']='julia'
@@ -74,60 +79,78 @@ v${dict['version']}/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
-    # Customize the 'Make.user' file.
-    read -r -d '' "dict[make_user_string]" << END || true
+    # Customize the 'Make.user' file. Refer to 'Make.inc' for supported values.
+    if koopa_is_macos
+    then
+        read -r -d '' "dict[make_user_string]" << END || true
+VERBOSE=1
+USE_BINARYBUILDER=1
+END
+    else
+        read -r -d '' "dict[make_user_string]" << END || true
 prefix=${dict['prefix']}
 libexecdir=${dict['prefix']}/lib
 sysconfdir=${dict['prefix']}/etc
 
 VERBOSE=1
-
 USE_BINARYBUILDER=0
-USE_BLAS64=0
-USE_LLVM_SHLIB=0
 
-USE_SYSTEM_ARPACK=0
-USE_SYSTEM_BLAS=0
-USE_SYSTEM_CSL=0
-USE_SYSTEM_CURL=0 # 1
-USE_SYSTEM_DSFMT=0
-USE_SYSTEM_FFTW=0
-USE_SYSTEM_GMP=0 # 1
-USE_SYSTEM_LAPACK=0 # 1
-USE_SYSTEM_LIBGIT2=0 # 1
-USE_SYSTEM_LIBM=0
-USE_SYSTEM_LIBSSH2=0 # 1
-USE_SYSTEM_LIBSUITESPARSE=0
-USE_SYSTEM_LIBUNWIND=0
-USE_SYSTEM_LIBUV=0
-USE_SYSTEM_LLVM=0
-USE_SYSTEM_MBEDTLS=0
-USE_SYSTEM_MPFR=0 # 1
-USE_SYSTEM_NGHTTP2=0
-USE_SYSTEM_OPENLIBM=0
-USE_SYSTEM_OPENSPECFUN=0
-USE_SYSTEM_P7ZIP=0
-USE_SYSTEM_PATCHELF=0
-USE_SYSTEM_PCRE=0 # 1
-USE_SYSTEM_SUITESPARSE=0
-USE_SYSTEM_UTF8PROC=0 # 1
-USE_SYSTEM_ZLIB=0 # 1
+USE_LLVM_SHLIB=1
 
-# > LIBBLAS=-lopenblas
-# > LIBBLASNAME=libopenblas
-# > LIBLAPACK=-lopenblas
-# > LIBLAPACKNAME=libopenblas
+DISABLE_LIBUNWIND=0
+UNTRUSTED_SYSTEM_LIBM=0
+
+USE_SYSTEM_BLAS=1
+USE_SYSTEM_CSL=1
+USE_SYSTEM_CURL=1
+USE_SYSTEM_DSFMT=1
+USE_SYSTEM_GMP=1
+USE_SYSTEM_LAPACK=1
+USE_SYSTEM_LIBBLASTRAMPOLINE=1
+USE_SYSTEM_LIBGIT2=1
+USE_SYSTEM_LIBM=1
+USE_SYSTEM_LIBSSH2=1
+USE_SYSTEM_LIBSUITESPARSE=1
+USE_SYSTEM_LIBUNWIND=1
+USE_SYSTEM_LIBUV=1
+USE_SYSTEM_LIBWHICH=1
+USE_SYSTEM_LLVM=1
+USE_SYSTEM_MBEDTLS=1
+USE_SYSTEM_MPFR=1
+USE_SYSTEM_NGHTTP2=1
+USE_SYSTEM_OPENLIBM=1
+USE_SYSTEM_OPENSPECFUN=1
+USE_SYSTEM_P7ZIP=1
+USE_SYSTEM_PATCHELF=1
+USE_SYSTEM_PCRE=1
+USE_SYSTEM_UTF8PROC=1
+USE_SYSTEM_ZLIB=1
+
+# FIXME Is this not in use any more?
+# > USE_BLAS64=1
+
+# These options seem to have been removed.
+# > USE_SYSTEM_ARPACK=1
+# > USE_SYSTEM_FFTW=1
+# > USE_SYSTEM_SUITESPARSE=1
+
+LIBBLAS=-lopenblas
+LIBBLASNAME=libopenblas
+LIBLAPACK=-lopenblas
+LIBLAPACKNAME=libopenblas
 
 # > LLVM_CONFIG=\${LLVM_CONFIG}
 # > LLVM_VER=\${LLVM_VER}
 
-# NOTE This doesn't seem to pick up our Python correctly.
-# > PYTHON=\${app['python']}
+PYTHON=\${app['python']}
 END
-    koopa_write_lines \
+    fi
+    koopa_write_string \
         --file='Make.user' \
         --string="${dict['make_user_string']}"
+    koopa_add_to_path_end '/usr/sbin'
     koopa_print_env
+    koopa_print "${dict['make_user_string']}"
     "${app['make']}" --jobs="${dict['jobs']}"
     # > "${app['make']}" test
     "${app['make']}" install
