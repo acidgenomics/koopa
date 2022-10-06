d25b511ce3231156f011e52f86c9afa56f75fa48
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-06 11:09:56 -0400

    Rework pam.d config

--- lang/shell/bash/functions/os/macos/d/disable-touch-id-sudo.sh
+++ lang/shell/bash/functions/os/macos/d/disable-touch-id-sudo.sh
@@ -1,37 +1,35 @@
 #!/usr/bin/env bash
 
+# FIXME Define the code in the function here instead.
+
 koopa_macos_disable_touch_id_sudo() {
     # """
     # Disable sudo authentication via Touch ID PAM.
     # @note Updated 2021-10-30.
     # """
-    local app dict
+    local dict
     koopa_assert_has_no_args "$#"
     koopa_assert_is_admin
-    declare -A app=(
-        ['cp']="$(koopa_locate_cp)"
-        ['sudo']="$(koopa_locate_sudo)"
-    )
-    [[ -x "${app['cp']}" ]] || return 1
-    [[ -x "${app['sudo']}" ]] || return 1
-    declare -A dict=(
-        ['source_file']="$(koopa_koopa_prefix)/os/macos/etc/pam.d/sudo~orig"
-        ['target_file']='/etc/pam.d/sudo'
-    )
-    if [[ -f "${dict['target_file']}" ]] && \
+    declare -A dict
+    dict['file']='/etc/pam.d/sudo'
+    if [[ -f "${dict['file']}" ]] && \
         ! koopa_file_detect_fixed \
-            --file="${dict['target_file']}" \
+            --file="${dict['file']}" \
             --pattern='pam_tid.so'
     then
-        koopa_alert_note "Touch ID not enabled in '${dict['target_file']}'."
+        koopa_alert_note "Touch ID not enabled in '${dict['file']}'."
         return 0
     fi
-    koopa_alert "Disabling Touch ID defined in '${dict['target_file']}'."
-    # NOTE Don't use 'koopa_cp' here, as it will remove the target file
-    # and can cause system lockout in this case.
-    "${app['sudo']}" "${app['cp']}" -v \
-        "${dict['source_file']}" "${dict['target_file']}"
-    koopa_chmod --sudo '0444' "${dict['target_file']}"
+    koopa_alert "Disabling Touch ID defined in '${dict['file']}'."
+    read -r -d '' "dict[string]" << END || true
+# sudo: auth account password session
+auth       sufficient     pam_smartcard.so
+auth       required       pam_opendirectory.so
+account    required       pam_permit.so
+password   required       pam_deny.so
+session    required       pam_permit.so
+END
+    koopa_chmod --sudo '0444' "${dict['file']}"
     koopa_alert_success 'Touch ID disabled for sudo.'
     return 0
 }
