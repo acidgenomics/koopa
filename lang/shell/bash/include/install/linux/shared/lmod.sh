886e3b59742243790bdf87009096bd407d79345c
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-06 16:08:16 -0400

    Attempt to fix lua config

--- lang/shell/bash/include/install/linux/shared/lmod.sh
+++ lang/shell/bash/include/install/linux/shared/lmod.sh
@@ -17,6 +17,7 @@ main() {
         'tcl-tk'
     declare -A app=(
         ['lua']="$(koopa_locate_lua --realpath)"
+        ['luac']="$(koopa_locate_luac --realpath)"
         ['luarocks']="$(koopa_locate_luarocks --realpath)"
         ['make']="$(koopa_locate_make)"
     )
@@ -70,18 +71,28 @@ archive/${dict['file']}"
     dict['lua_ver']="$(koopa_get_version "${app['lua']}")"
     dict['lua_compat_ver']="$(koopa_major_minor_version "${dict['lua_ver']}")"
     lua_path_arr=(
+        # > './?.lua'
         "${dict['libexec']}/share/lua/${dict['lua_compat_ver']}/?.lua"
+        "${dict['libexec']}/share/lua/${dict['lua_compat_ver']}/?/init.lua"
         "${dict['lua']}/share/lua/${dict['lua_compat_ver']}/?.lua"
+        "${dict['lua']}/share/lua/${dict['lua_compat_ver']}/?/init.lua"
     )
     lua_cpath_arr=(
+        # > './?.so'
         "${dict['libexec']}/lib/lua/${dict['lua_compat_ver']}/?.so"
         "${dict['lua']}/lib/lua/${dict['lua_compat_ver']}/?.so"
     )
+    LUAROCKS_PREFIX="${dict['libexec']}"
     LUA_PATH="$(printf '%s;' "${lua_path_arr[@]}")"
     LUA_CPATH="$(printf '%s;' "${lua_cpath_arr[@]}")"
-    export LUA_PATH LUA_CPATH
+    export LUAROCKS_PREFIX LUA_PATH LUA_CPATH
+    "${app['lua']}" -e 'print(package.path)'
+    "${app['lua']}" -e 'print(package.cpath)'
     conf_args=(
         "--prefix=${dict['apps_dir']}"
+        '--with-allowRootUse=no'
+        "--with-lua=${app['lua']}"
+        "--with-luac=${app['luac']}"
         "--with-spiderCacheDir=${dict['data_dir']}/cacheDir"
         "--with-updateSystemFn=${dict['data_dir']}/system.txt"
     )
