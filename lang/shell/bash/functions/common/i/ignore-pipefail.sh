2a4a80b8928ae27888ba195696c039ff9b908cc7
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-11 13:09:29 -0400

    Rework using awk string to avoid pipefail

--- lang/shell/bash/functions/common/i/ignore-pipefail.sh
+++ lang/shell/bash/functions/common/i/ignore-pipefail.sh
@@ -8,7 +8,13 @@ koopa_ignore_pipefail() {
     # This can happen with complex pipes involving GNU coreutils head.
     #
     # @seealso
+    # - "${PIPESTATUS[@]}"
     # - https://stackoverflow.com/questions/22464786/
+    # - https://stackoverflow.com/questions/19120263/
+    # - https://github.com/stedolan/jq/issues/1017/
+    # - https://stackoverflow.com/questions/41516177/
+    # - https://mywiki.wooledge.org/BashFAQ/105
+    # - https://mywiki.wooledge.org/BashFAQ/112
     # """
     local status
     status="${1:?}"
