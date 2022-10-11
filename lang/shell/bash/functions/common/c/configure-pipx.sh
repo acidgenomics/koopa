d7200403982aa865bc26fdff595956de21d82de1
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-11 15:58:48 -0400

    Add support for nanopolish

--- lang/shell/bash/functions/common/c/configure-pipx.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# FIXME Need to add 'koopa_configure_pipx' script here, for configuring
-# pipx versioned into opt.
---
+++ lang/shell/bash/functions/common/i/install-nanopolish.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_install_nanopolish() {
+    koopa_install_app \
+        --name='nanopolish' \
+        "$@"
+}
