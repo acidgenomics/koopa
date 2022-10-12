f9f39be0a66224adfbbcab2d2711fcd907729e68
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-12 08:09:02 -0400

    Fix typo

--- lang/shell/bash/functions/common/l/locate-texi2dvi.sh
+++ lang/shell/bash/functions/common/l/locate-texi2dvi.sh
@@ -2,7 +2,7 @@
 
 koopa_locate_texi2dvi() {
     koopa_locate_app \
-        --app-name='texi2cvi' \
-        --bin-name='texinfo' \
+        --app-name='texinfo' \
+        --bin-name='texi2dvi' \
         "$@"
 }
