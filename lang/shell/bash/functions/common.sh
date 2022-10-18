7c4957673351696f5265f7f27be9be40660e3a1f
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-18 14:56:07 -0400

    Rework revdep install

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -19472,13 +19472,14 @@ koopa_read() {
 }
 
 koopa_reinstall_all_revdeps() {
-    local app_names install_args
+    local app_names install_args revdeps
     koopa_assert_has_args "$#"
-    readarray -t app_names <<< "$(koopa_app_json_revdeps "$@")"
-    koopa_assert_is_array_non_empty "${app_names[@]}"
+    app_names=("$@")
+    readarray -t revdeps <<< "$(koopa_app_json_revdeps "${app_names[@]}")"
+    koopa_assert_is_array_non_empty "${revdeps[@]}"
     install_args=()
     koopa_can_install_binary && install_args+=('--push')
-    install_args+=("${app_names[@]}")
+    install_args+=("${app_names[@]}" "${revdeps[@]}")
     koopa_cli_reinstall "${install_args[@]}"
     return 0
 }
