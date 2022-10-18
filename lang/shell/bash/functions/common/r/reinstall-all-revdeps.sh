96e09d8a489f25e22798490261bf1f2bc8dd9030
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-18 14:31:14 -0400

    Don't require push support here

--- lang/shell/bash/functions/common/r/reinstall-all-revdeps.sh
+++ lang/shell/bash/functions/common/r/reinstall-all-revdeps.sh
@@ -8,11 +8,13 @@ koopa_reinstall_all_revdeps() {
     # @examples
     # > koopa_reinstall_all_revdeps 'python'
     # """
-    local app_names
+    local app_names install_args
     koopa_assert_has_args "$#"
-    koopa_can_install_binary || return 1
     readarray -t app_names <<< "$(koopa_app_json_revdeps "$@")"
     koopa_assert_is_array_non_empty "${app_names[@]}"
-    koopa_cli_reinstall --push "${app_names[@]}"
+    install_args=()
+    koopa_can_install_binary && install_args+=('--push')
+    install_args+=("${app_names[@]}")
+    koopa_cli_reinstall "${install_args[@]}"
     return 0
 }
