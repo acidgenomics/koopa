e8090b30288baa2e48ebae9c966e4f0e460f81f0
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-19 13:54:58 -0400

    Add draft install support for adfr

--- lang/shell/bash/include/install/common/shared/autodock-adfr.sh
+++ lang/shell/bash/include/install/common/shared/autodock-adfr.sh
@@ -1,45 +1,70 @@
 #!/usr/bin/env bash
 
-# FIXME This isn't working due to 2.7 version conflict between conda-forge
-# and the hcc recipe...argh.
-
 main() {
     # """
     # Install ADFR suite.
     # @note Updated 2022-10-19.
+    #
+    # Linux:
+    # - ADFRsuite_x86_64Linux_1.0.tar.gz
+    # - https://ccsb.scripps.edu/adfr/download/1038/
+    # macOS:
+    # - ADFRsuite_x86_64Darwin_1.0.tar.gz
+    # - https://ccsb.scripps.edu/adfr/download/1033/
     # 
     # @seealso
     # - https://ccsb.scripps.edu/adfr/downloads/
-    # - https://anaconda.org/HCC/adfr-suite
-    # - https://github.com/nanome-ai/plugin-docking/blob/master/adfr-suite.yml
-    # - https://conda.io/projects/conda/en/latest/user-guide/tasks/
-    #     manage-environments.html#sharing-an-environment
     # """
-    local dict
-    declare -A dict
-    dict['name']="${KOOPA_INSTALL_NAME:?}"
-    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}/libexec"
-    dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="${dict['name']}.yaml"
-    read -r -d '' "dict[string]" << END || true
-# > name: ${dict['name']}
-channels:
-  - hcc
-  # > - conda-forge
-  # > - anaconda
-dependencies:
-  # > - python=2.7.3
-  - adfr-suite=${dict['version']}
-  # > - numpy=1.15.0
-prefix: ${dict['prefix']}
-END
-    koopa_write_string \
-        --string="${dict['string']}" \
-        --file="${dict['file']}"
-    dict['file']="$(koopa_realpath "${dict['file']}")"
-    koopa_install_app_subshell \
-        --installer='conda-env' \
-        --name='autodock-adfr' \
-        -D "--file=${dict['file']}" \
-        "$@"
+    local app dict
+    koopa_assert_has_no_args "$#"
+    declare -A app
+    app['yes']="$(koopa_locate_yes)"
+    declare -A dict=(
+        ['arch']="$(koopa_arch)" # e.g. 'x86_64'.
+        ['name']='ADFRsuite'
+        ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+        ['version']="${KOOPA_INSTALL_VERSION:?}"
+    )
+    dict['libexec']="$(koopa_init_dir "${dict['prefix']}/libexec")"
+    if koopa_is_macos
+    then
+        dict['platform']='Darwin'
+    else
+        dict['platform']='Linux'
+    fi
+    case "${dict['version']}" in
+        '1.0')
+            case "${dict['platform']}" in
+                'Darwin')
+                    dict['id']='1033'
+                    ;;
+                'Linux')
+                    dict['id']='1038'
+                    ;;
+            esac
+            ;;
+        *)
+            koopa_stop 'Unsupported version.'
+            ;;
+    esac
+    dict['bn']="${dict['name']}_${dict['arch']}${dict['platform']}\
+_${dict['version']}"
+    dict['file']="${dict['bn']}.tar.gz"
+    dict['url']="https://ccsb.scripps.edu/adfr/download/${dict['id']}/"
+    koopa_download "${dict['url']}" "${dict['file']}"
+    koopa_extract "${dict['file']}"
+    koopa_cd "${dict['bn']}"
+    koopa_print_env
+    # Install script options:
+    # * -d: Target directory.
+    # * -c: How to compile the Python files. Use 0 for .pyc or 1 for .pyo.
+    "${app['yes']}" | ./install.sh -d "${dict['libexec']}" -c 0 || true
+    (
+        koopa_cd "${dict['prefix']}"
+        koopa_ln 'libexec/bin' 'bin'
+    )
+    koopa_alert_note "The molecular surface calculation software (MSMS) is \
+freely available for academic research. For obtaining commercial license \
+usage, contact Dr. Sanner at sanner@scripps.edu."
+    return 0
 }
