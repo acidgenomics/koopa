4e4e8a71c7a406c95add1557aa0ea8d2e58dc554
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-19 09:22:22 -0400

    Work on adding adfr support

---
+++ lang/shell/bash/include/install/common/shared/autodock-adfr.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# FIXME Work on adding support for this.
+# https://ccsb.scripps.edu/adfr/downloads/
+# https://anaconda.org/HCC/adfr-suite
+
+# ADFR-suite is required for using Autodock4, as there are steps that require
+# Python 2.7 install this as a separate conda environment.
+# https://github.com/nanome-ai/plugin-docking/blob/master/adfr-suite.yml
+# > name: adfr-suite
+# > channels:
+# >   - conda-forge
+# >   - hcc
+# >   - anaconda
+# > dependencies:
+# >   - python=2.7
+# >   - adfr-suite
+# >   - numpy=1.15.0
+
+main() {
+    # FIXME Need to add support for this.
+    return 0
+}
