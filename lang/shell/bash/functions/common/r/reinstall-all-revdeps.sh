0a0cddb2bef3737d2697ebd3df15ed7ca38273da
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-19 08:31:43 -0400

    Draft update to revdep handling

--- lang/shell/bash/functions/common/r/reinstall-all-revdeps.sh
+++ lang/shell/bash/functions/common/r/reinstall-all-revdeps.sh
@@ -5,17 +5,52 @@ koopa_reinstall_all_revdeps() {
     # Reinstall (and push) all reverse dependencies.
     # @note Updated 2022-10-18.
     #
+    # We're intentionally allowing the passthrough of '--push' here, but the
+    # ability to push a koopa binary is not required.
+    #
     # @examples
-    # > koopa_reinstall_all_revdeps 'python'
+    # > koopa_reinstall_all_revdeps --push 'node' 'python'
     # """
-    local app_names install_args revdeps
+    local app_name flags pos
     koopa_assert_has_args "$#"
-    app_names=("$@")
-    readarray -t revdeps <<< "$(koopa_app_json_revdeps "${app_names[@]}")"
-    koopa_assert_is_array_non_empty "${revdeps[@]}"
-    install_args=()
-    koopa_can_install_binary && install_args+=('--push')
-    install_args+=("${app_names[@]}" "${revdeps[@]}")
-    koopa_cli_reinstall "${install_args[@]}"
+    flags=()
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            # Key-value pairs --------------------------------------------------
+            '--'*)
+                flags+=("$1")
+                shift 1
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa_assert_has_args "$#"
+    for app_name in "$@"
+    do
+        local install_args revdeps
+        install_args=()
+        if koopa_is_array_non_empty "${flags[@]}"
+        then
+            install_args+=("${flags[@]}")
+        fi
+        install_args+=("$app_name")
+        readarray -t revdeps <<< "$(koopa_app_json_revdeps "$app_name")"
+        if koopa_is_array_non_empty "${revdeps[@]}"
+        then
+            install_args+=("${revdeps[@]}")
+            koopa_dl \
+                "${app_name} reverse dependencies" \
+                "$(koopa_to_string "${revdeps[@]}")"
+        else
+            koopa_alert_note "${app_name} has no reverse dependencies."
+        fi
+        koopa_cli_reinstall "${install_args[@]}"
+    done
     return 0
 }
