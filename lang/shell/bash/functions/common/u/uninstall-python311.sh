49481534445f5963fb65e30769ca0523c092da47
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-27 09:46:14 -0400

    Add FIXME regarding symlink handling

--- lang/shell/bash/functions/common/u/uninstall-python311.sh
+++ lang/shell/bash/functions/common/u/uninstall-python311.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Delete symlink at '/opt/koopa/app/python'.
+# FIXME Delete symlink at '/opt/koopa/opt/python'.
+
 koopa_uninstall_python311() {
     koopa_uninstall_app \
         --name='python3.11' \
