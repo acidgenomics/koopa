a2eb591489f2cd3c5f644faaa6a75d80eaf24391
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-27 09:46:14 -0400

    Add FIXME regarding symlink handling

--- lang/shell/bash/functions/common/i/install-python311.sh
+++ lang/shell/bash/functions/common/i/install-python311.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Create symlink at '/opt/koopa/app/python'.
+# FIXME Create symlink at '/opt/koopa/opt/python'.
+
 koopa_install_python311() {
     koopa_install_app \
         --installer='python' \
