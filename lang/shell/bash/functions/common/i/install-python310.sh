ac07c3eb1223b39db3552784520d03bf77d69c85
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-28 11:38:10 -0700

    Improve Python configuration

--- lang/shell/bash/functions/common/i/install-python310.sh
+++ lang/shell/bash/functions/common/i/install-python310.sh
@@ -5,6 +5,7 @@ koopa_install_python310() {
     declare -A dict=(
         ['app_prefix']="$(koopa_app_prefix)"
         ['bin_prefix']="$(koopa_bin_prefix)"
+        ['man1_prefix']="$(koopa_man1_prefix)"
         ['opt_prefix']="$(koopa_opt_prefix)"
     )
     koopa_install_app \
@@ -15,10 +16,13 @@ koopa_install_python310() {
         koopa_alert "Linking 'python' in '${dict['app_prefix']}'."
         koopa_cd "${dict['app_prefix']}"
         koopa_ln 'python3.10' 'python'
-        koopa_alert "Linking 'python' and 'python3' in '${dict['bin_prefix']}'."
+        koopa_alert "Linking 'python' in '${dict['bin_prefix']}'."
         koopa_cd "${dict['bin_prefix']}"
         koopa_ln 'python3.10' 'python3'
         koopa_ln 'python3.10' 'python'
+        koopa_alert "Linking 'python' in '${dict['man1_prefix']}'."
+        koopa_ln 'python3.10.1' 'python3.1'
+        koopa_ln 'python3.10.1' 'python.1'
         koopa_alert "Linking 'python' in '${dict['opt_prefix']}'."
         koopa_cd "${dict['opt_prefix']}"
         koopa_ln 'python3.10' 'python'
