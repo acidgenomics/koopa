b811b3b59099e2523269e80338154f422f74fa25
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-28 11:12:23 -0700

    Improve Python configuration

--- lang/shell/bash/functions/common/i/install-python311.sh
+++ lang/shell/bash/functions/common/i/install-python311.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Create symlink at '/opt/koopa/app/python'.
-# FIXME Create symlink at '/opt/koopa/opt/python'.
-
 koopa_install_python311() {
     koopa_install_app \
         --installer='python' \
