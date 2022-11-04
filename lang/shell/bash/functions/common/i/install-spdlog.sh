c6ffb57f5be65aad2ac21b513c040bd925409154
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-04 11:33:31 -0400

    Rework PATH handling inside koopa app calls

---
+++ lang/shell/bash/functions/common/i/install-spdlog.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_install_spdlog() {
+    koopa_install_app \
+        --name='spdlog' \
+        "$@"
+}
---
+++ lang/shell/bash/functions/common/u/uninstall-spdlog.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_uninstall_spdlog() {
+    koopa_uninstall_app \
+        --name='spdlog' \
+        "$@"
+}
