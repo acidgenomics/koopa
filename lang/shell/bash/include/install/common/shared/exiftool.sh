9fb8825e355659fab6e612764326b9611b4e4357
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-08 12:41:30 -0500

    Update versions

--- lang/shell/bash/include/install/common/shared/exiftool.sh
+++ lang/shell/bash/include/install/common/shared/exiftool.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 main() {
+    # """
+    # Ensure release is tagged production before submitting.
+    # https://exiftool.org/history.html
+    # """
     koopa_install_app_subshell \
         --installer='perl-package' \
         --name='exiftool' \
