02eca4192164cc5e3019c2d78d1b0097a5c02662
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-08 16:09:46 -0500

    Update versions

--- lang/shell/bash/include/install/common/shared/sqlite.sh
+++ lang/shell/bash/include/install/common/shared/sqlite.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install SQLite.
-    # @note Updated 2022-09-28.
+    # @note Updated 2022-11-08.
     #
     # Use autoconf instead of amalgamation.
     #
@@ -36,6 +36,7 @@ main() {
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
     case "${dict['version']}" in
+        '3.39.4' | \
         '3.39.3' | \
         '3.39.2' | \
         '3.38.'* | \
