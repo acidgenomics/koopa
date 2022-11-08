48c93a96e775bbac89142b7e345696db4961afdc
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-08 11:33:53 -0500

    Update versions

--- lang/shell/bash/include/install/common/shared/ghostscript.sh
+++ lang/shell/bash/include/install/common/shared/ghostscript.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Need to rework to build from source, to support 10.0.0.
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/ghostscript.rb
+
 main() {
     koopa_install_app_subshell \
         --installer='conda-env' \
