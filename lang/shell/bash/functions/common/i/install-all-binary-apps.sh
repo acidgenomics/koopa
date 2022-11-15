73fcbce1bfe39b1ce58993126416abc9e86b8095
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-15 11:39:48 -0500

    Update install recipe

--- lang/shell/bash/functions/common/i/install-all-binary-apps.sh
+++ lang/shell/bash/functions/common/i/install-all-binary-apps.sh
@@ -3,7 +3,7 @@
 koopa_install_all_binary_apps() {
     # ""
     # Install all shared apps as binary packages.
-    # @note Updated 2022-11-07.
+    # @note Updated 2022-11-15.
     #
     # This will currently fail for platforms where not all apps can be
     # successfully compiled, such as ARM.
@@ -331,6 +331,7 @@ koopa_install_all_binary_apps() {
             'go'
             'google-cloud-sdk'
             'gseapy'
+            'haskell-cabal'
             'haskell-ghcup'
             'haskell-stack'
             'hisat2'
