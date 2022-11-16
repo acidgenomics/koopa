2738d3f0b0d7572a867fb1433e800ebb834eceda
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-16 07:34:47 -0500

    Debug perl on Ubuntu

--- lang/shell/bash/include/install/common/shared/perl.sh
+++ lang/shell/bash/include/install/common/shared/perl.sh
@@ -3,23 +3,23 @@
 # NOTE Consider including 'expat', 'gdbm', and possibly 'zlib' here?
 # FIXME This is no longer building Perl documentation files.
 
-# FIXME Build is now failing on Ubuntu 22.
-# Compiling Perl properties
-# Creating Perl synonyms
-# Writing tables
-# Making pod file
-# Making test script
-# Updating 'mktables.lst'
-
 main() {
     # """
     # Install Perl.
     # @note Updated 2022-06-21.
     #
+    # @section Regarding parallel build failures on Ubunutu:
+    # make can error at this step when running in parallel.
+    # # Updating 'mktables.lst'
+    # - https://www.nntp.perl.org/group/perl.perl5.porters/2016/
+    #     09/msg239501.html
+    # - https://github.com/Perl/perl5/issues/17541
+    #
     # @seealso
     # - https://www.cpan.org/src/
     # - https://metacpan.org/pod/distribution/perl/INSTALL
     # - https://perlmaven.com/how-to-build-perl-from-source-code
+    #
     # """
     local app dict
     koopa_assert_has_no_args "$#"
@@ -43,6 +43,8 @@ ${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
     koopa_print_env
     ./Configure -des -Dprefix="${dict['prefix']}"
+    # Deparallelize on Linux to avoid error at "Updating 'mktables.lst'".
+    koopa_is_linux && dict['jobs']=1
     "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
     # The installer will warn when you skip this step.
     # > "${app['make']}" test
