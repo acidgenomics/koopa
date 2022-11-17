c090454daaebd6f8f3c832827288ba5c6a0fd0be
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-17 08:16:10 -0500

    Add 'eol-lf' utility program

---
+++ lang/shell/bash/functions/common/e/eol-lf.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+koopa_eol_lf() {
+    # """
+    # Ensure line breaks are Unix LF.
+    # @note Updated 2022-11-17.
+    #
+    # @seealso
+    # - https://gist.github.com/jennybc/0be7717c2b5b30088811
+    # - https://github.com/dfalster/baad/blob/master/scripts/fix-eol.sh
+    # """
+    local app file
+    koopa_assert_has_args "$#"
+    koopa_assert_is_file "$@"
+    declare -A app
+    app['perl']="$(koopa_locate_perl)"
+    [[ -x "${app['perl']}" ]] || return 1
+    for file in "$@"
+    do
+        koopa_alert "Setting EOL as LF in '${file}'."
+        # Convert Windows to Unix.
+        "${app['perl']}" -pi -e 's/\r\n/\n/g' "$file"
+        # Convert old Mac to Unix (Excel).
+        "${app['perl']}" -pi -e 's/\r/\n/g' "$file"
+    done
+}
