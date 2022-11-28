57478cf276a500de7f53ebf27720cb83ca76e066
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-28 10:46:03 -0500

    Rework our bootstrap approach

--- etc/koopa/bootstrap.sh
+++ etc/koopa/bootstrap.sh
@@ -2,7 +2,7 @@
 
 # """
 # Bootstrap core dependencies.
-# @note Updated 2022-10-12.
+# @note Updated 2022-11-28.
 # """
 
 set -o errexit
@@ -10,12 +10,14 @@ set -o nounset
 
 KOOPA_PREFIX="$(cd -- "$(dirname -- "$0")/../.." && pwd)"
 PREFIX="${KOOPA_PREFIX:?}/libexec/bootstrap"
+TMPDIR="${TMPDIR:-/tmp}"
 
+# Restrict the system path.
 PATH='/usr/bin:/bin'
 export PATH
 
-JOBS=2
-TMPDIR="${TMPDIR:-/tmp}"
+# Deparallize, to ensure cross platform compatibility.
+JOBS=1
 
 install_bash() {
     local file name tmp_dir url version
@@ -23,14 +25,14 @@ install_bash() {
     version='5.2'
     file="${name}-${version}.tar.gz"
     url="https://ftp.gnu.org/gnu/${name}/${file}" \
-    tmp_dir="${TMPDIR}/${name}"
+    tmp_dir="${TMPDIR:?}/${name}"
     mkdir -pv "$tmp_dir"
     cd "$tmp_dir" || return 1
     curl "$url" -o "$file"
     tar -xzvf "$file"
     cd "${name}-${version}"
-    ./configure --prefix="$PREFIX"
-    make --jobs="$JOBS"
+    ./configure --prefix="${PREFIX:?}"
+    make --jobs="${JOBS:?}"
     make install
     rm -fr "$tmp_dir"
     return 0
@@ -42,16 +44,16 @@ install_coreutils() {
     version='9.1'
     file="${name}-${version}.tar.gz"
     url="https://ftp.gnu.org/gnu/${name}/${file}" \
-    tmp_dir="${TMPDIR}/${name}"
+    tmp_dir="${TMPDIR:?}/${name}"
     mkdir -pv "$tmp_dir"
     cd "$tmp_dir" || return 1
     curl "$url" -o "$file"
     tar -xzvf "$file"
     cd "${name}-${version}"
     ./configure \
-        --prefix="$PREFIX" \
+        --prefix="${PREFIX:?}" \
         --program-prefix='g'
-    make --jobs="$JOBS"
+    make --jobs="${JOBS:?}"
     make install
     rm -fr "$tmp_dir"
     return 0
@@ -59,8 +61,8 @@ install_coreutils() {
 
 main() {
     rm -fr "${PREFIX:?}"
-    printf "Installing bootstrap into '%s'.\n" "$PREFIX"
-    install_coreutils
+    printf "Installing bootstrap into '%s'.\n" "${PREFIX:?}"
+    # > install_coreutils
     install_bash
 }
 
