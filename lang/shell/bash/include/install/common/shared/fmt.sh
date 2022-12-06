15478a4c90db91711de0d5c4a097729f9d039324
Michael Steinbaugh <mike@steinbaugh.com>
2022-12-06 17:27:04 -0500

    Attempt to build mamba

--- lang/shell/bash/include/install/common/shared/fmt.sh
+++ lang/shell/bash/include/install/common/shared/fmt.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Switch this back to cmake install, rather than using make...
+
 main() {
     # """
     # Install fmt library.
@@ -15,10 +17,8 @@ main() {
     koopa_activate_app --build-only 'cmake'
     declare -A app=(
         ['cmake']="$(koopa_locate_cmake)"
-        ['make']="$(koopa_locate_make)"
     )
     [[ -x "${app['cmake']}" ]] || return 1
-    [[ -x "${app['make']}" ]] || return 1
     declare -A dict=(
         ['jobs']="$(koopa_cpu_count)"
         ['name']='fmt'
@@ -32,20 +32,20 @@ tags/${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
     shared_cmake_args=(
+        # > "-DCMAKE_CXX_FLAGS=${CPPFLAGS:-}"
+        # > "-DCMAKE_EXE_LINKER_FLAGS=${LDFLAGS:-}"
+        # > "-DCMAKE_INSTALL_RPATH=${dict['prefix']}/lib"
+        # > "-DCMAKE_MODULE_LINKER_FLAGS=${LDFLAGS:-}"
+        # > "-DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS:-}"
+        # > '-DFMT_PEDANTIC=ON'
+        # > '-DFMT_SYSTEM_HEADERS=ON'
+        # > '-DFMT_WERROR=ON'
         '-DCMAKE_BUILD_TYPE=Release'
-        "-DCMAKE_CXX_FLAGS=${CPPFLAGS:-}"
-        "-DCMAKE_EXE_LINKER_FLAGS=${LDFLAGS:-}"
         '-DCMAKE_INSTALL_LIBDIR=lib'
         "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
-        "-DCMAKE_INSTALL_RPATH=${dict['prefix']}/lib"
-        "-DCMAKE_MODULE_LINKER_FLAGS=${LDFLAGS:-}"
-        "-DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS:-}"
         '-DFMT_DOC=OFF'
         '-DFMT_INSTALL=ON'
-        '-DFMT_PEDANTIC=ON'
-        '-DFMT_SYSTEM_HEADERS=ON'
         '-DFMT_TEST=ON'
-        '-DFMT_WERROR=ON'
     )
     koopa_print_env
     koopa_dl 'Shared CMake args' "${shared_cmake_args[*]}"
@@ -54,19 +54,19 @@ tags/${dict['file']}"
         -B 'build-shared' \
         "${shared_cmake_args[@]}" \
         -DBUILD_SHARED_LIBS='TRUE'
-    "${app['make']}" \
-        --directory='build-shared' \
-        --jobs="${dict['jobs']}" \
-        install
+    "${app['cmake']}" \
+        --build 'build-shared' \
+        --parallel "${dict['jobs']}"
+    "${app['cmake']}" --install 'build-shared'
     "${app['cmake']}" -LH \
         -S . \
         -B 'build-static' \
         "${shared_cmake_args[@]}" \
         -DBUILD_SHARED_LIBS='FALSE'
-    "${app['make']}" \
-        --directory='build-static' \
-        --jobs="${dict['jobs']}" \
-        install
+    "${app['cmake']}" \
+        --build 'build-static' \
+        --parallel "${dict['jobs']}"
+    "${app['cmake']}" --install 'build-static'
     koopa_assert_is_file "${dict['prefix']}/lib/libfmt.a"
     return 0
 }
