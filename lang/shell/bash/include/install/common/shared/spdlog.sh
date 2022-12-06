675e14ee2a13ab06d0c8d56bcf8a91a73e277915
Michael Steinbaugh <mike@steinbaugh.com>
2022-12-06 17:00:05 -0500

    Work on patching spdlog

--- lang/shell/bash/include/install/common/shared/spdlog.sh
+++ lang/shell/bash/include/install/common/shared/spdlog.sh
@@ -14,13 +14,18 @@ main() {
     # """
     local app dict shared_cmake_args
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'cmake' 'pkg-config'
+    koopa_activate_app --build-only 'cmake' 'patch' 'pkg-config'
     koopa_activate_app 'fmt'
     declare -A app=(
+        ['cat']="$(koopa_locate_cat)"
         ['cmake']="$(koopa_locate_cmake)"
         ['ctest']="$(koopa_locate_ctest)"
+        ['patch']="$(koopa_locate_patch)"
     )
+    [[ -x "${app['cat']}" ]] || return 1
     [[ -x "${app['cmake']}" ]] || return 1
+    [[ -x "${app['ctest']}" ]] || return 1
+    [[ -x "${app['patch']}" ]] || return 1
     declare -A dict=(
         ['fmt']="$(koopa_app_prefix 'fmt')"
         ['jobs']="$(koopa_cpu_count)"
@@ -36,38 +41,46 @@ archive/${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
     shared_cmake_args=(
-        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
+        '-DCMAKE_BUILD_TYPE=Release'
+        "-DCMAKE_CXX_FLAGS=${CPPFLAGS:-}"
+        "-DCMAKE_EXE_LINKER_FLAGS=${LDFLAGS:-}"
         '-DCMAKE_INSTALL_INCLUDEDIR=include'
         '-DCMAKE_INSTALL_LIBDIR=lib'
+        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
         "-DCMAKE_INSTALL_RPATH=${dict['prefix']}/lib"
+        "-DCMAKE_MODULE_LINKER_FLAGS=${LDFLAGS:-}"
+        "-DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS:-}"
         '-DSPDLOG_BUILD_BENCH=OFF'
         '-DSPDLOG_BUILD_TESTS=ON'
         '-DSPDLOG_FMT_EXTERNAL=ON'
-
-        # FIXME Need to add this: -Dpkg_config_libdir=#{lib}
-
         "-Dfmt_DIR=${dict['fmt']}/lib/cmake/fmt"
-        # FIXME Does adding these harden our build?
-        '-DCMAKE_BUILD_TYPE=Release'
-        "-DCMAKE_CXX_FLAGS=${CPPFLAGS:-}"
-        "-DCMAKE_C_FLAGS=${CFLAGS:-}" # FIXME Take out
-        "-DCMAKE_EXE_LINKER_FLAGS=${LDFLAGS:-}"
-        "-DCMAKE_MODULE_LINKER_FLAGS=${LDFLAGS:-}"
-        "-DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS:-}"
+        '-Dpkg_config_libdir=lib'
         # > '-Wno-dev'
     )
     koopa_print_env
-
     # FIXME What does 'ENV.cxx11' from Homebrew do?
     # https://github.com/Homebrew/legacy-homebrew/blob/73a2e7542aa5cf3d8b7bac5bdc5a8330cc1aee5a/Library/Homebrew/extend/ENV/std.rb#L283?
-
-    # FIXME Need a patching step, recommended by Homebrew.
-#    inreplace "include/spdlog/tweakme.h", "// #define SPDLOG_FMT_EXTERNAL", <<~EOS
-#      #ifndef SPDLOG_FMT_EXTERNAL
-#      #define SPDLOG_FMT_EXTERNAL
-#      #endif
-#    EOS
-
+    # Patch diff created with:
+    # > diff -u 'tweakme.h' 'tweakme.h-1' > 'patch-tweakme.patch'
+    "${app['cat']}" << END > 'patch-tweakme.patch'
+--- tweakme.h	2022-12-06 16:50:30
++++ tweakme.h-1	2022-12-06 16:51:32
+@@ -80,6 +80,9 @@
+ //
+ // #define SPDLOG_FMT_EXTERNAL
+ ///////////////////////////////////////////////////////////////////////////////
++#ifndef SPDLOG_FMT_EXTERNAL
++#define SPDLOG_FMT_EXTERNAL
++#endif
+ 
+ ///////////////////////////////////////////////////////////////////////////////
+ // Uncomment to use C++20 std::format instead of fmt. This removes compile
+END
+    "${app['patch']}" \
+        --unified \
+        --verbose \
+        'include/spdlog/tweakme.h' \
+        'patch-tweakme.patch'
     koopa_dl 'Shared CMake args' "${shared_cmake_args[*]}"
     "${app['cmake']}" -LH \
         -S . \
