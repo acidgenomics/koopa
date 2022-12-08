1bcea1d63b7066f6fc5dfd44f9f3ebb07e4e461f
Michael Steinbaugh <mike@steinbaugh.com>
2022-12-08 09:31:28 -0500

    Add link

--- lang/shell/bash/include/install/common/shared/bandwhich.sh
+++ lang/shell/bash/include/install/common/shared/bandwhich.sh
@@ -4,6 +4,7 @@
 # - https://github.com/imsnif/bandwhich/issues/258
 # - https://github.com/imsnif/bandwhich/pull/259
 # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/bandwhich.rb
+# - https://github.com/Homebrew/formula-patches/pull/873
 
 main() {
     koopa_install_app_subshell \
