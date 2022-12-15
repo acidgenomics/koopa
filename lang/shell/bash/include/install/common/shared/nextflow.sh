a893457311eacc41f3565d31797ad207c191d111
Michael Steinbaugh <mike@steinbaugh.com>
2022-12-15 11:01:54 -0500

    Add FIXME

--- lang/shell/bash/include/install/common/shared/nextflow.sh
+++ lang/shell/bash/include/install/common/shared/nextflow.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Rework this to install from https://get.nextflow.io, rather than using
+# bioconda, which is often out of date.
+
 main() {
     koopa_install_app_subshell \
         --installer='conda-env' \
