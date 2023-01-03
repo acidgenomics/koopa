87e1be68a970bb6f7149f18daa10fc17166c8a93
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-03 17:05:40 -0500

    Draft update cellranger

--- lang/shell/bash/include/install/linux/shared/cellranger.sh
+++ lang/shell/bash/include/install/linux/shared/cellranger.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# NOTE Consider informing user interactively regarding license agreement.
+# FIXME Install this into libexec and then just copy the binary.
+# FIXME Consider informing user interactively regarding license agreement.
 
 main() {
     # """
     # Install Cell Ranger.
-    # @note Updated 2022-04-07.
+    # @note Updated 2023-01-03.
     #
     # Refdata is accessible here:
     # https://support.10xgenomics.com/single-cell-gene-expression/
@@ -19,8 +20,8 @@ main() {
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
-    dict['file']="${dict['name']}-${dict['version']}.tar.gz"
-    dict['url']="${dict['installers_url']}/cellranger/${dict['file']}"
+    dict['file']="${dict['name']}-${dict['version']}.tar.xz"
+    dict['url']="${dict['installers_url']}/${dict['name']}/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_mv "${dict['name']}-${dict['version']}" "${dict['prefix']}"
