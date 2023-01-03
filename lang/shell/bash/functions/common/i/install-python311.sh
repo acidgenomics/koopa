d01009dbcb8c52f8414ff2e090649bf71859299b
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-03 13:19:41 -0500

    Rework python install

--- lang/shell/bash/functions/common/i/install-python311.sh
+++ lang/shell/bash/functions/common/i/install-python311.sh
@@ -15,18 +15,18 @@ koopa_install_python311() {
     (
         koopa_alert "Linking 'python' in '${dict['app_prefix']}'."
         koopa_cd "${dict['app_prefix']}"
-        koopa_ln 'python3.10' 'python'
+        koopa_ln 'python3.11' 'python'
         koopa_alert "Linking 'python' in '${dict['bin_prefix']}'."
         koopa_cd "${dict['bin_prefix']}"
-        koopa_ln 'python3.10' 'python3'
-        koopa_ln 'python3.10' 'python'
+        koopa_ln 'python3.11' 'python3'
+        koopa_ln 'python3.11' 'python'
         koopa_alert "Linking 'python' in '${dict['man1_prefix']}'."
         koopa_cd "${dict['man1_prefix']}"
-        koopa_ln 'python3.10.1' 'python3.1'
-        koopa_ln 'python3.10.1' 'python.1'
+        koopa_ln 'python3.11.1' 'python3.1'
+        koopa_ln 'python3.11.1' 'python.1'
         koopa_alert "Linking 'python' in '${dict['opt_prefix']}'."
         koopa_cd "${dict['opt_prefix']}"
-        koopa_ln 'python3.10' 'python'
+        koopa_ln 'python3.11' 'python'
     )
     return 0
 }
