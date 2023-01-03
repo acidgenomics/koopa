4c699b4c7b106f089008a9be90ac3283a6870051
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-03 09:33:53 -0500

    Draft update to OpenBB installer

--- lang/shell/bash/include/install/common/shared/openbb.sh
+++ lang/shell/bash/include/install/common/shared/openbb.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Need to resolve CA certificates issue.
+# Could not find a suitable TLS CA certificate bundle, invalid path: /opt/koopa/app/openbb/2.1.0/libexec/conda/lib/python3.9/site-packages/certifi/cacert.pem
+
 main() {
     # """
     # Install OpenBB terminal.
@@ -8,18 +11,28 @@ main() {
     # @seealso
     # - https://github.com/OpenBB-finance/OpenBBTerminal/blob/main/
     #     openbb_terminal/README.md#anaconda--python
+    # - https://github.com/OpenBB-finance/OpenBBTerminal/blob/main/
+    #     TROUBLESHOOT.md
+    # - https://python-poetry.org/docs/configuration/
     # """
     local app dict
+    koopa_activate_app 'ca-certificates'
     declare -A app
     app['cat']="$(koopa_locate_cat --allow-system)"
     [[ -x "${app['cat']}" ]] || return 1
     declare -A dict=(
+        ['ca_certificates']="$(koopa_app_prefix 'ca-certificates')"
         ['conda_prefix']="$(koopa_conda_prefix)"
         ['name']='OpenBBTerminal'
+        ['poetry_cache_dir']='poetry/cache'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
-    koopa_assert_is_dir "${dict['conda_prefix']}"
+    koopa_assert_is_dir \
+        "${dict['ca_certificates']}" \
+        "${dict['conda_prefix']}"
+    dict['cacert']="${dict['ca_certificates']}/share/ca-certificates/cacert.pem"
+    koopa_assert_is_file "${dict['cacert']}"
     dict['libexec']="${dict['prefix']}/libexec"
     dict['env_prefix']="${dict['libexec']}/conda"
     dict['python_prefix']="${dict['libexec']}/python"
@@ -31,6 +44,7 @@ refs/tags/${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
     koopa_mkdir \
         "${dict['env_prefix']}" \
+        "${dict['poetry_cache_dir']}" \
         "${dict['python_prefix']}"
     dict['env_file']='build/conda/conda-3-9-env.yaml'
     koopa_assert_is_file "${dict['env_file']}"
@@ -39,17 +53,20 @@ refs/tags/${dict['file']}"
         --force \
         --file "${dict['env_file']}" \
         --prefix "${dict['env_prefix']}"
-    conda activate "${dict['env_prefix']}"
-    export PIP_REQUIRE_VIRTUALENV=false
     app['poetry']="${dict['env_prefix']}/bin/poetry"
     [[ -x "${app['poetry']}" ]] || return 1
-    # FIXME This step is hanging on macOS.
-    # NOTE poetry 1.1.13 doesn't support '--no-cache'.
+    conda activate "${dict['env_prefix']}"
+    export DEFAULT_CA_BUNDLE_PATH="${dict['cacert']}"
+    export PIP_REQUIRE_VIRTUALENV=false
+    export POETRY_CACHE_DIR="${dict['poetry_cache_dir']}"
+    # poetry 1.1.13 doesn't support '--no-cache'.
+    # This step may error due to Little Snitch blocking rule on macOS.
     "${app['poetry']}" install --no-interaction --verbose
     # > conda install --yes 'tensorflow'
     koopa_conda_deactivate
     koopa_cp ./* --target-directory="${dict['python_prefix']}"
     dict['bin_file']="${dict['prefix']}/bin/openbb"
+    # FIXME Rework using koopa_write_string'...
     koopa_touch "${dict['bin_file']}"
     "${app['cat']}" > "${dict['bin_file']}" << END
 #!/bin/sh
