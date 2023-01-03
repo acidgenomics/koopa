421c3430b75c794faada7f584328442dc4c1f433
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-03 17:09:06 -0500

    Rework cellranger install

--- lang/shell/bash/include/install/linux/shared/cellranger.sh
+++ lang/shell/bash/include/install/linux/shared/cellranger.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Install this into libexec and then just copy the binary.
-# FIXME Consider informing user interactively regarding license agreement.
-
 main() {
     # """
     # Install Cell Ranger.
@@ -20,10 +17,18 @@ main() {
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
+    dict['libexec']="$(koopa_init_dir "${dict['prefix']}/libexec")"
     dict['file']="${dict['name']}-${dict['version']}.tar.xz"
     dict['url']="${dict['installers_url']}/${dict['name']}/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
-    koopa_mv "${dict['name']}-${dict['version']}" "${dict['prefix']}"
+    koopa_mv "${dict['name']}-${dict['version']}" "${dict['libexec']}"
+    (
+        koopa_cd "${dict['prefix']}"
+        koopa_ln 'libexec/bin/cellranger' 'bin/cellranger'
+    )
+    koopa_alert_note "Installation requires agreement to terms of service at: \
+'https://support.10xgenomics.com/single-cell-gene-expression/\
+software/downloads/latest'."
     return 0
 }
