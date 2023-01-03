a0fb2420c47241d8a251aee2dda8008af552cca8
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-03 13:17:32 -0500

    Rework python install

--- lang/shell/bash/functions/common/i/install-python311.sh
+++ lang/shell/bash/functions/common/i/install-python311.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/env bash
 
 koopa_install_python311() {
+    local dict
+    declare -A dict=(
+        ['app_prefix']="$(koopa_app_prefix)"
+        ['bin_prefix']="$(koopa_bin_prefix)"
+        ['man1_prefix']="$(koopa_man1_prefix)"
+        ['opt_prefix']="$(koopa_opt_prefix)"
+    )
     koopa_install_app \
         --installer='python' \
         --name='python3.11' \
         "$@"
+    (
+        koopa_alert "Linking 'python' in '${dict['app_prefix']}'."
+        koopa_cd "${dict['app_prefix']}"
+        koopa_ln 'python3.10' 'python'
+        koopa_alert "Linking 'python' in '${dict['bin_prefix']}'."
+        koopa_cd "${dict['bin_prefix']}"
+        koopa_ln 'python3.10' 'python3'
+        koopa_ln 'python3.10' 'python'
+        koopa_alert "Linking 'python' in '${dict['man1_prefix']}'."
+        koopa_cd "${dict['man1_prefix']}"
+        koopa_ln 'python3.10.1' 'python3.1'
+        koopa_ln 'python3.10.1' 'python.1'
+        koopa_alert "Linking 'python' in '${dict['opt_prefix']}'."
+        koopa_cd "${dict['opt_prefix']}"
+        koopa_ln 'python3.10' 'python'
+    )
+    return 0
 }
