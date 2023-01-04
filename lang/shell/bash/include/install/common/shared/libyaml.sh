6b50ef7e2033e145ce25ea0ad94d9cbbd8db096c
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-04 07:35:25 -0500

    Work on adding libyaml support

---
+++ lang/shell/bash/include/install/common/shared/libyaml.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# FIXME Seeing this error when running autoconf:
+#
+# autoreconf: running: /opt/koopa/app/autoconf/2.71/bin/autoconf --force
+# configure.ac:62: warning: The macro `AC_HEADER_STDC' is obsolete.
+# configure.ac:62: You should run autoupdate.
+# ./lib/autoconf/headers.m4:704: AC_HEADER_STDC is expanded from...
+# configure.ac:62: the top level
+# configure.ac:56: error: possibly undefined macro: AC_PROG_LIBTOOL
+#       If this token and others are legitimate, please use m4_pattern_allow.
+#       See the Autoconf documentation.
+# autoreconf: error: /opt/koopa/app/autoconf/2.71/bin/autoconf failed with exit status: 1
+
+main() {
+    # """
+    # Install libyaml.
+    # @note Updated 2023-01-04.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/libyaml.rb
+    # """
+    local app dict
+    koopa_assert_has_no_args "$#"
+    koopa_activate_app --build-only \
+        'autoconf' \
+        'automake' \
+        'libtool'
+    declare -A app
+    app['make']="$(koopa_locate_make)"
+    [[ -x "${app['make']}" ]] || return 1
+    declare -A dict=(
+        ['jobs']="$(koopa_cpu_count)"
+        ['name']='libyaml'
+        ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+        ['version']="${KOOPA_INSTALL_VERSION:?}"
+    )
+    dict['file']="${dict['version']}.tar.gz"
+    dict['url']="https://github.com/yaml/${dict['name']}/\
+archive/${dict['file']}"
+    koopa_download "${dict['url']}" "${dict['file']}"
+    koopa_extract "${dict['file']}"
+    koopa_cd "${dict['name']}-${dict['version']}"
+    conf_args=(
+        "--prefix=${dict['prefix']}"
+        '--disable-dependency-tracking'
+    )
+    koopa_print_env
+    koopa_dl 'configure args' "${conf_args[*]}"
+    ./bootstrap
+    ./configure --help
+    ./configure "${conf_args[@]}"
+    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
+    "${app['make']}" install
+    return 0
+}
