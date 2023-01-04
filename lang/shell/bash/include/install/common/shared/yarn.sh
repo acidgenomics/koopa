f29f100376ceab1b4c19f10f550ad2bd7693885c
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-03 21:33:25 -0500

    Version updates

--- lang/shell/bash/include/install/common/shared/yarn.sh
+++ lang/shell/bash/include/install/common/shared/yarn.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 main() {
+    # """
+    # @seealso
+    # - https://github.com/yarnpkg/berry/releases/tag/@yarnpkg/cli/3.3.1
+    # - https://yarnpkg.com/getting-started/migration
+    # """
     koopa_install_app_subshell \
         --installer='node-package' \
         --name='yarn' \
