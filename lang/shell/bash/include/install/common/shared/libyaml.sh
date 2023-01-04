fdbd7a9a18c543a444e486003ee133624c6fafe0
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-04 07:53:49 -0500

    Improve libyaml install

--- lang/shell/bash/include/install/common/shared/libyaml.sh
+++ lang/shell/bash/include/install/common/shared/libyaml.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Seeing this error when running autoconf:
-#
-# autoreconf: running: /opt/koopa/app/autoconf/2.71/bin/autoconf --force
-# configure.ac:62: warning: The macro `AC_HEADER_STDC' is obsolete.
-# configure.ac:62: You should run autoupdate.
-# ./lib/autoconf/headers.m4:704: AC_HEADER_STDC is expanded from...
-# configure.ac:62: the top level
-# configure.ac:56: error: possibly undefined macro: AC_PROG_LIBTOOL
-#       If this token and others are legitimate, please use m4_pattern_allow.
-#       See the Autoconf documentation.
-# autoreconf: error: /opt/koopa/app/autoconf/2.71/bin/autoconf failed with exit status: 1
-#
-# https://stackoverflow.com/questions/53636130/possibly-undefined-macro-ac-prog-libtool
-
 main() {
     # """
     # Install libyaml.
@@ -31,15 +17,22 @@ main() {
         'autoconf' \
         'automake' \
         'libtool'
-    declare -A app
-    app['make']="$(koopa_locate_make)"
+    declare -A app=(
+        ['autoreconf']="$(koopa_locate_autoreconf)"
+        ['autoupdate']="$(koopa_locate_autoupdate)"
+        ['make']="$(koopa_locate_make)"
+    )
+    [[ -x "${app['autoreconf']}" ]] || return 1
+    [[ -x "${app['autoupdate']}" ]] || return 1
     [[ -x "${app['make']}" ]] || return 1
     declare -A dict=(
+        ['libtool']="$(koopa_app_prefix 'libtool')"
         ['jobs']="$(koopa_cpu_count)"
         ['name']='libyaml'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
+    koopa_assert_is_dir "${dict['libtool']}"
     dict['file']="${dict['version']}.tar.gz"
     dict['url']="https://github.com/yaml/${dict['name']}/\
 archive/${dict['file']}"
@@ -52,10 +45,9 @@ archive/${dict['file']}"
     )
     koopa_print_env
     koopa_dl 'configure args' "${conf_args[*]}"
-    # https://superuser.com/questions/565988/autoconf-libtool-and-an-undefined-ac-prog-libtool
-    autoupdate --verbose # FIXME
-    ACLOCAL_PATH='/opt/koopa/app/libtool/2.4.7/share/aclocal' \
-        autoreconf -fvi # FIXME
+    "${app['autoupdate']}" --verbose
+    ACLOCAL_PATH="${dict['libtool']}/share/aclocal" \
+        "${app['autoreconf']}" -fvi
     ./configure --help
     ./configure "${conf_args[@]}"
     "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
