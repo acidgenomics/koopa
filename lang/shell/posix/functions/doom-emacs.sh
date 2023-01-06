6c24704f4bef0afc50ab5ef37eefee64d8de42f2
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-06 15:47:27 -0500

    Harden emacs and vim aliases

--- lang/shell/posix/functions/doom-emacs.sh
+++ lang/shell/posix/functions/doom-emacs.sh
@@ -3,11 +3,15 @@
 koopa_doom_emacs() {
     # """
     # Doom Emacs.
-    # @note Updated 2022-09-16.
+    # @note Updated 2023-01-06.
     # """
     local prefix
     prefix="$(koopa_doom_emacs_prefix)"
-    [ -d "$prefix" ] || return 1
+    if [ ! -d "$prefix" ]
+    then
+        koopa_print "Doom Emacs is not installed at '${prefix}'."
+        return 1
+    fi
     koopa_emacs --with-profile 'doom' "$@"
     return 0
 }
