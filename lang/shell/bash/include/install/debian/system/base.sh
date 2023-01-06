e6e26c156434fe3b5028a30cb0aab4cb5efd2f46
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-06 10:57:00 -0500

    Draft updates to app metadata consolidation

--- lang/shell/bash/include/install/debian/system/base.sh
+++ lang/shell/bash/include/install/debian/system/base.sh
@@ -1,118 +1,10 @@
 #!/usr/bin/env bash
 
-# FIXME Remove this installer file.
-
 main() {
-    # """
-    # Install Debian base system.
-    # @note Updated 2022-07-18.
-    #
-    # The 'build-essential' package includes: dpkg-dev, g++, gcc, libc-dev, and
-    # make, which are required to build packages.
-    #
-    # @section Look up reverse dependencies:
-    # > sudo apt-cache rdepends --installed 'libnode-dev'
-    #
-    # Backup package configuration:
-    # > sudo dpkg --get-selections > /tmp/dpkglist.txt
-    #
-    # Restore package configuration:
-    # > sudo dpkg --set-selections < /tmp/dpkglist.txt
-    # > sudo apt-get -y update
-    # > sudo apt-get dselect-upgrade
-    #
-    # Configure time zone:
-    # > sudo dpkg-reconfigure tzdata
-    #
-    # @seealso:
-    # - Check package source repo.
-    #   https://packages.ubuntu.com/
-    # - How to replicate installed packages across machines.
-    #   https://serverfault.com/questions/56848
-    # """
-    local app dict pkgs
-    declare -A app=(
-        ['dpkg']="$(koopa_debian_locate_dpkg)"
-        ['sudo']="$(koopa_locate_sudo)"
-    )
-    [[ -x "${app['dpkg']}" ]] || return 1
-    [[ -x "${app['sudo']}" ]] || return 1
-    declare -A dict=(
-        ['apt_enabled_repos']="$(koopa_debian_apt_enabled_repos)"
-    )
     pkgs=(
-        # > 'alien'
-        # > 'biber'
-        # > 'ggobi'
-        # > 'ghc' # haskell-stack
-        # > 'gnutls-bin'
-        # > 'graphviz'
-        # > 'jags'
-        # > 'keyboard-configuration'
-        # > 'libgdal-dev' # libspatial
-        # > 'libgeos-dev' # libspatial
-        # > 'libmariadb-dev'
-        # > 'libmysqlclient-dev' # Conflicts with libmariadb-dev (Ubuntu).
-        # > 'libproj-dev' # libspatial
-        # > 'librdf0-dev' # Problematic on 2022-03-29.
-        # > 'mpi-default-bin'
-        # > 'nim'
-        # > 'openmpi-bin'
-        # > 'openmpi-common'
-        # > 'openmpi-doc'
-        # > 'pass'
-        # > 'proj-bin'
-        # > 'protobuf-compiler'
-        # > 'systemd'
-        # > 'tabix'
-        # > 'unattended-upgrades'
-        # > 'xorg'
-        'apt-listchanges'
-        'apt-transport-https'
-        'apt-utils'
-        'attr' # coreutils
-        'autoconf'
-        'automake'
-        'bash'
-        'bash-completion'
-        'bc'
-        'bison' # r-devel
-        'build-essential'
-        'byacc'
-        'bzip2'
-        'ca-certificates'
-        'cmake'
-        'coreutils'
-        'curl'
-        'debhelper' # r-devel
-        'default-jdk'
-        'diffutils'
-        'dirmngr'
-        'expect-dev' # Installs unbuffer.
-        'file'
-        'findutils'
-        'fortran77-compiler'
-        'gdb' # r-devel
-        'gdebi-core'
-        'gettext'
-        'gfortran'
-        'git'
-        'gnupg'
-        'gperf'
-        'gpg-agent'
-        'groff-base' # r-devel
-        'gtk-doc-tools' # harfbuzz
-        'htop'
-        'imagemagick'
-        'jq'
-        'less'
-        'libacl1-dev' # coreutils
-        'libapparmor-dev'
-        'libapr1-dev' # subversion
         'libaprutil1-dev' # subversion
         'libatlas-base-dev' # armadillo
         'libattr1-dev' # coreutils
-        'libbison-dev'
         'libblas-dev'
         'libboost-chrono-dev' # bcl2fastq
         'libboost-date-time-dev' # bcl2fastq
@@ -123,7 +15,6 @@ main() {
         'libboost-thread-dev' # bcl2fastq
         'libboost-timer-dev' # bcl2fastq
         'libbrotli-dev' # node.js
-        'libbz2-dev'
         'libc-ares-dev' # node.js
         'libcairo2-dev' # harfbuzz
         'libcap-dev' # coreutils
@@ -224,65 +115,9 @@ main() {
         'software-properties-common'
         'sqlite3'
         'subversion' # r-devel
-        'sudo'
         'tcl-dev'
         'tcl8.6-dev' # r-devel
         'texinfo' # makeinfo
-        'texlive'
-        'texlive-base' # r-devel
-        'texlive-extra-utils' # r-devel
-        'texlive-fonts-extra' # r-devel
-        'texlive-fonts-recommended' # r-devel
-        'texlive-latex-base' # r-devel
-        'texlive-latex-extra' # r-devel
-        'texlive-latex-recommended' # r-devel
         'tk-dev'
         'tk8.6-dev' # r-devel
-        'tmux'
-        'tree'
-        'tzdata'
-        'udunits-bin'
-        'unzip'
-        'vim'
-        'visidata'
-        'wget'
-        'x11proto-core-dev' # r-devel
-        'xauth' # r-devel
-        'xdg-utils' # r-devel
-        'xfonts-100dpi'
-        'xfonts-75dpi'
-        'xfonts-base' # r-devel
-        'xvfb' # r-devel
-        'xz-utils'
-        'zip'
-        'zlib1g-dev' # r-devel
-        'zsh'
     )
-    if koopa_is_ubuntu
-    then
-        pkgs+=('firefox')
-    fi
-    koopa_rm --sudo \
-        '/var/cache/apt/'* \
-        '/var/lib/dpkg/available'
-    "${app['sudo']}" "${app['dpkg']}" --clear-avail
-    if koopa_is_ubuntu && \
-        ! koopa_str_detect_fixed \
-            --string="${dict['apt_enabled_repos']}" \
-            --pattern='universe'
-    then
-        koopa_stop \
-            "The Ubuntu 'universe' repo is disabled." \
-            "Check '/etc/apt/sources.list'."
-    fi
-    koopa_debian_apt_get 'upgrade'
-    # This can be too aggressive and break some Debian systems.
-    # > koopa_debian_apt_get 'dist-upgrade'
-    koopa_debian_apt_install "${pkgs[@]}"
-    koopa_debian_apt_configure_sources
-    koopa_debian_apt_clean
-    koopa_debian_set_locale
-    koopa_debian_set_timezone
-    koopa_linux_update_system_sshd_config
-    return 0
-}
