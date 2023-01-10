a370a9afd175e270fb24a2588048ed2fe9a7d5e9
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 13:47:50 -0500

    Rework private install approach

--- lang/shell/bash/include/install/linux/shared/cellranger.sh
+++ lang/shell/bash/include/install/linux/shared/cellranger.sh
@@ -1,28 +1,34 @@
 #!/usr/bin/env bash
 
 # FIXME Need to rework to download via s3 instead.
+# s3://private.koopa.acidgenomics.com/installers/cellranger/7.1.0.tar.xz
 
 main() {
     # """
     # Install Cell Ranger.
-    # @note Updated 2023-01-03.
+    # @note Updated 2023-01-10.
     #
     # Refdata is accessible here:
     # https://support.10xgenomics.com/single-cell-gene-expression/
     #     software/downloads/latest
     # """
-    local dict
+    local app dict
     koopa_assert_has_no_args "$#"
+    # FIXME koopa_assert_has_private_access
+    declare -A app
+    app['aws']="$(koopa_locate_aws)"
+    [[ -x "${app['aws']}" ]] || return 1
     declare -A dict=(
         ['installers_url']="$(koopa_private_installers_url)"
-        ['name']='cellranger'
+        ['name']="${KOOPA_INSTALL_NAME:?}"
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
     dict['libexec']="$(koopa_init_dir "${dict['prefix']}/libexec")"
-    dict['file']="${dict['name']}-${dict['version']}.tar.xz"
+    dict['file']="${dict['version']}.tar.xz"
     dict['url']="${dict['installers_url']}/${dict['name']}/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
+    "${app['aws']}" --profile='acidgenomics' \
+        s3 cp "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_mv "${dict['name']}-${dict['version']}" "${dict['libexec']}"
     (
