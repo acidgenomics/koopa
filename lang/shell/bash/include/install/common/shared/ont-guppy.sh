45915f6ded2554cbbba21108ce2378734cbaf01e
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 14:26:20 -0500

    Add guppy install support

--- lang/shell/bash/include/install/common/shared/ont-guppy.sh
+++ lang/shell/bash/include/install/common/shared/ont-guppy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # FIXME s3://private.koopa.acidgenomics.com/installers/ont-guppy/linux/amd64/6.4.2-cpu.tar.gz
+# FIXME s3://private.koopa.acidgenomics.com/installers/ont-guppy/macos/amd64/6.4.2-cpu.zip
 
 main() {
     # """
@@ -9,26 +10,35 @@ main() {
     # """
     local app dict
     koopa_assert_has_no_args "$#"
+    koopa_assert_is_not_aarch64
     # FIXME koopa_assert_has_private_access
     declare -A app
     app['aws']="$(koopa_locate_aws)"
     [[ -x "${app['aws']}" ]] || return 1
     declare -A dict=(
+        ['arch']="$(koopa_arch2)" # e.g. 'amd64'.
+        ['core_type']='cpu' # or 'gpu'.
         ['installers_url']="$(koopa_private_installers_url)"
-        ['name']="${KOOPA_INSTALL_NAME:?}"
+        ['name']='ont-guppy'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
     dict['libexec']="$(koopa_init_dir "${dict['prefix']}/libexec")"
-    dict['file']="${dict['version']}.tar.xz"
-    dict['url']="${dict['installers_url']}/${dict['name']}/${dict['file']}"
+    if koopa_is_macos
+    then
+        dict['platform']='macos'
+        dict['ext']='zip'
+    else
+        dict['platform']='linux'
+        dict['ext']='tar.gz'
+    fi
+    dict['file']="${dict['version']}-${dict['core_type']}.${dict['ext']}"
+    dict['url']="${dict['installers_url']}/${dict['name']}/\
+${dict['platform']}/${dict['arch']}/${dict['file']}"
     "${app['aws']}" --profile='acidgenomics' \
         s3 cp "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
-    koopa_mv "${dict['name']}-${dict['version']}" "${dict['libexec']}"
-    (
-        koopa_cd "${dict['prefix']}"
-        koopa_ln 'libexec/bin' 'bin'
-    )
+    koopa_cd "${dict['name']}-${dict['core_type']}"
+    koopa_cp ./* --target-directory="${dict['prefix']}"
     return 0
 }
