699f015683764160f812afface895881d9cf2e07
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 13:16:44 -0500

    Relax cut checks

--- lang/shell/bash/functions/os/linux/debian/a/apt-space-used-by-grep.sh
+++ lang/shell/bash/functions/os/linux/debian/a/apt-space-used-by-grep.sh
@@ -3,7 +3,7 @@
 koopa_debian_apt_space_used_by_grep() {
     # """
     # Check installed apt package size, with dependencies.
-    # @note Updated 2021-11-02.
+    # @note Updated 2022-01-10.
     #
     # See also:
     # https://askubuntu.com/questions/490945
@@ -13,8 +13,8 @@ koopa_debian_apt_space_used_by_grep() {
     koopa_assert_is_admin
     declare -A app=(
         ['apt_get']="$(koopa_debian_locate_apt_get)"
-        ['cut']="$(koopa_locate_cut)"
-        ['sudo']="$(koopa_locate_sudo)"
+        ['cut']="$(koopa_locate_cut --allow-system)"
+        ['sudo']="$(koopa_locate_sudo --allow-system)"
     )
     [[ -x "${app['apt_get']}" ]] || return 1
     [[ -x "${app['cut']}" ]] || return 1
