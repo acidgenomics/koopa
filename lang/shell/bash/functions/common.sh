56c6a79cd74d5d5a5f9eb60fa2eca9b072638780
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 14:37:02 -0500

    Add guppy install support

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -13236,6 +13236,9 @@ koopa_install_ont_guppy() {
     koopa_install_app \
         --name='ont-guppy' \
         "$@"
+    koopa_alert_note "Installation requires agreement to terms of service at: \
+'https://nanoporetech.com/support/nanopore-sequencing-data-analysis'."
+    return 0
 }
 
 koopa_install_openbb() {
