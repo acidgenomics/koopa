ae06ef0adb894462586750ecf3a0973643114966
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 09:46:06 -0500

    Improve standardization of os-detection functions

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -1619,15 +1619,6 @@ koopa_is_macos() {
     [ "$(uname -s)" = 'Darwin' ]
 }
 
-koopa_is_os_version() {
-    local file grep version
-    file='/etc/os-release'
-    grep='grep'
-    version="${1:?}"
-    [ -f "$file" ] || return 1
-    "$grep" -q "VERSION_ID=\"${version}" "$file"
-}
-
 koopa_is_os() {
     [ "$(koopa_os_id)" = "${1:?}" ]
 }
