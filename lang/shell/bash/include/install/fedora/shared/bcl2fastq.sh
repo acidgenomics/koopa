17ced77382ddffe9bcf860a43dd0e2362bbc0d0c
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 13:23:51 -0500

    Rework installer

--- lang/shell/bash/include/install/fedora/shared/bcl2fastq.sh
+++ lang/shell/bash/include/install/fedora/shared/bcl2fastq.sh
@@ -1,41 +1,28 @@
 #!/usr/bin/env bash
 
-# FIXME Rework to use S3 cp here.
-# s3://private.koopa.acidgenomics.com/installers/bcl2fastq/fedora/amd64/2.20.zip
-
 main() {
     # """
     # Install bcl2fastq binary from Fedora/RHEL RPM file.
     # @note Updated 2023-01-10.
     # """
-    local dict
+    local app dict
     koopa_assert_has_no_args "$#"
+    koopa_can_install_binary || return 1
+    declare -A app
+    app['aws']="$(koopa_locate_aws)"
+    [[ -x "${app['aws']}" ]] || return 1
     declare -A dict=(
-        ['arch']="$(koopa_arch)"
+        ['arch']="$(koopa_arch2)" # e.g. 'amd64'.
         ['installers_url']="$(koopa_private_installers_url)"
-        ['name']='bcl2fastq'
-        ['platform']='linux'
+        ['name']="${KOOPA_INSTALL_NAME:?}"
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
-    dict['arch2']="$(koopa_kebab_case_simple "${dict['arch']}")"
-    dict['platform2']="$(koopa_capitalize "${dict['platform']}")"
-    dict['maj_ver']="$(koopa_major_version "${dict['version']}")"
-    # e.g. '2.20.0.422' to '2-20-0'.
-    dict['version2']="$( \
-        koopa_sub \
-            --pattern='\.[0-9]+$' \
-            --regex \
-            --replacement='' \
-            "${dict['version']}" \
-    )"
-    dict['version2']="$(koopa_kebab_case_simple "${dict['version2']}")"
-    dict['file']="${dict['name']}${dict['maj_ver']}-v${dict['version2']}-\
-${dict['platform']}-${dict['arch2']}.zip"
-    dict['url']="${dict['installers_url']}/${dict['name']}/rpm/${dict['file']}"
-    dict['file2']="${dict['name']}${dict['maj_ver']}-v${dict['version']}-\
-${dict['platform2']}-${dict['arch']}.rpm"
-    koopa_download "${dict['url']}" "${dict['file']}"
+    dict['file']="${dict['version']}.zip"
+# s3://private.koopa.acidgenomics.com/installers/bcl2fastq/fedora/amd64/2.20.zip
+    dict['url']="${dict['installers_url']}/${dict['name']}/fedora/${dict['arch']}/${dict['file']}"
+    "${app['aws']}" --profile='acidgenomics' \
+        s3 cp "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_fedora_install_from_rpm \
         --prefix="${dict['prefix']}" "${dict['file2']}"
