470336662c64e7f0919aea0091588cc0ecc4085d
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 13:16:44 -0500

    Relax cut checks

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -16073,6 +16073,7 @@ koopa_locate_cut() {
     koopa_locate_app \
         --app-name='coreutils' \
         --bin-name='gcut' \
+        --system-bin-name='cut' \
         "$@"
 }
 
--- lang/shell/bash/functions/common/l/locate-cut.sh
+++ lang/shell/bash/functions/common/l/locate-cut.sh
@@ -4,5 +4,6 @@ koopa_locate_cut() {
     koopa_locate_app \
         --app-name='coreutils' \
         --bin-name='gcut' \
+        --system-bin-name='cut' \
         "$@"
 }
