b34cd8bf1e0e7d9644545b19368ebf8e4c162948
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-10 11:23:09 -0500

    Migrate bash-specific functions

--- lang/shell/posix/functions/m/make-prefix.sh
+++ lang/shell/posix/functions/m/make-prefix.sh
@@ -3,13 +3,13 @@
 koopa_make_prefix() {
     # """
     # Return the installation prefix to use.
-    # @note Updated 2022-04-08.
+    # @note Updated 2023-01-10.
     # """
     local prefix
     if [ -n "${KOOPA_MAKE_PREFIX:-}" ]
     then
         prefix="$KOOPA_MAKE_PREFIX"
-    elif koopa_is_local_install
+    elif koopa_is_user_install
     then
         prefix="$(koopa_xdg_local_home)"
     else
