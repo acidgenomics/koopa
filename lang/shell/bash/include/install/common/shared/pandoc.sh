5aa414462c70abd1a9bbd33cb1f8dc1fcfb5b4ec
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-18 19:54:09 -0500

    Attempt to rework using cabal instead of stack

--- lang/shell/bash/include/install/common/shared/pandoc.sh
+++ lang/shell/bash/include/install/common/shared/pandoc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Homebrew formula uses cabal for install..consider switching.
+
 # FIXME Building of 3.0 is not currently supported by stack?
 #2023-01-18 19:43:10.008732: [debug] (SQL) SELECT "id" FROM "last_performed" WHERE _ROWID_=last_insert_rowid(); []
 #2023-01-18 19:43:10.009570: [debug] Not reading lock file
@@ -24,39 +26,54 @@ main() {
     #     Installing-the-development-version-of-pandoc
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/pandoc.rb
     # - https://github.com/commercialhaskell/stack/issues/342
+    # - https://github.com/jgm/pandoc/blob/main/pandoc.cabal
     # """
-    local app dict install_args stack_args
+    local app dict
     koopa_assert_is_not_aarch64
     koopa_activate_app --build-only 'haskell-stack'
     koopa_activate_app 'zlib'
     declare -A app=(
-        ['stack']="$(koopa_locate_stack)"
+        ['cabal']="$(koopa_locate_cabal)"
+        ['ghcup']="$(koopa_locate_ghcup)"
     )
-    [[ -x "${app['stack']}" ]] || return 1
+    [[ -x "${app['cabal']}" ]] || return 1
+    [[ -x "${app['ghcup']}" ]] || return 1
     declare -A dict=(
+        ['cabal_dir']="$(koopa_init_dir 'cabal')"
+        ['ghc_version']='9.4.4'
         ['jobs']="$(koopa_cpu_count)"
         ['name']='pandoc'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
-        ['stack_root']="$(koopa_init_dir 'stack')"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
+    # Avoid wasting space in '~/.cabal'.
+    export CABAL_DIR="${dict['cabal_dir']}"
+    dict['ghc_prefix']="$(koopa_init_dir "ghc-${dict['ghc_version']}")"
+    "${app['ghcup']}" install \
+        'ghc' "${dict['ghc_version']}" \
+            --isolate "${dict['ghc_prefix']}"
+    koopa_assert_is_dir "${dict['ghc_prefix']}/bin"
+    koopa_add_to_path_start "${dict['ghc_prefix']}/bin"
     dict['file']="${dict['name']}-${dict['version']}.tar.gz"
     dict['url']="https://hackage.haskell.org/package/\
 ${dict['name']}-${dict['version']}/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
-    dict['zlib']="$(koopa_app_prefix 'zlib')"
-    stack_args=(
-        "--jobs=${dict['jobs']}"
-        "--stack-root=${dict['stack_root']}"
-        '--verbose'
-        "--extra-include-dirs=${dict['zlib']}/include"
-        "--extra-lib-dirs=${dict['zlib']}/lib"
-    )
-    install_args=(
-        "--local-bin-path=${dict['prefix']}/bin"
-    )
-    "${app['stack']}" "${stack_args[@]}" install "${install_args[@]}"
+    koopa_print_env
+    koopa_init_dir "${dict['prefix']}/bin"
+    # NOTE Can use 'v2-*' commands here instead.
+    "${app['cabal']}" update
+    "${app['cabal']}" configure \
+        --jobs="${dict['jobs']}" \
+        --verbose
+    "${app['cabal']}" build \
+        --jobs="${dict['jobs']}" \
+        --verbose
+    "${app['cabal']}" install \
+        --install-method='copy' \
+        --installdir="${dict['prefix']}/bin" \
+        --jobs="${dict['jobs']}" \
+        --verbose
     return 0
 }
