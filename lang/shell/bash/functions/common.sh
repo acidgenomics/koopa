bf662389b8a1b5fdf1aa589fd8172e2b09a3de25
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-01 12:21:44 -0500

    Initial additional of RSEM index function

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -16878,6 +16878,13 @@ koopa_locate_rscript() {
         "$@"
 }
 
+koopa_locate_rsem_prepare_reference() {
+    koopa_locate_app \
+        --app-name='rsem' \
+        --bin-name='rsem-prepare-reference' \
+        "$@"
+}
+
 koopa_locate_rsync() {
     koopa_locate_app \
         --app-name='rsync' \
@@ -20086,6 +20093,82 @@ koopa_roff() {
         "${app['ronn']}" --roff ./*'.ronn'
         koopa_mv --target-directory="${dict['man_prefix']}/man1" ./*'.1'
     )
+    return 0
+}
+
+koopa_star_index() {
+    local app dict index_args
+    declare -A app=(
+        ['rsem_prepare_reference']="$(koopa_locate_rsem_prepare_reference)"
+    )
+    [[ -x "${app['rsem_prepare_reference']}" ]] || return 1
+    declare -A dict=(
+        ['genome_fasta_file']=''
+        ['gtf_file']=''
+        ['mem_gb']="$(koopa_mem_gb)"
+        ['mem_gb_cutoff']=10
+        ['output_dir']=''
+        ['threads']="$(koopa_cpu_count)"
+        ['tmp_dir']="$(koopa_tmp_dir)"
+    )
+    index_args=()
+    while (("$#"))
+    do
+        case "$1" in
+            '--genome-fasta-file='*)
+                dict['genome_fasta_file']="${1#*=}"
+                shift 1
+                ;;
+            '--genome-fasta-file')
+                dict['genome_fasta_file']="${2:?}"
+                shift 2
+                ;;
+            '--gtf-file='*)
+                dict['gtf_file']="${1#*=}"
+                shift 1
+                ;;
+            '--gtf-file')
+                dict['gtf_file']="${2:?}"
+                shift 2
+                ;;
+            '--output-dir='*)
+                dict['output_dir']="${1#*=}"
+                shift 1
+                ;;
+            '--output-dir')
+                dict['output_dir']="${2:?}"
+                shift 2
+                ;;
+            *)
+                koopa_invalid_arg "$1"
+                ;;
+        esac
+    done
+    koopa_assert_is_set \
+        '--genome-fasta-file' "${dict['genome_fasta_file']}" \
+        '--gtf-file' "${dict['gtf_file']}" \
+        '--output-dir' "${dict['output_dir']}"
+    if [[ "${dict['mem_gb']}" -lt "${dict['mem_gb_cutoff']}" ]]
+    then
+        koopa_stop "RSEM requires ${dict['mem_gb_cutoff']} GB of RAM."
+    fi
+    koopa_assert_is_file \
+        "${dict['genome_fasta_file']}" \
+        "${dict['gtf_file']}"
+    dict['genome_fasta_file']="$(koopa_realpath "${dict['genome_fasta_file']}")"
+    dict['gtf_file']="$(koopa_realpath "${dict['gtf_file']}")"
+    koopa_assert_is_not_dir "${dict['output_dir']}"
+    koopa_alert "Generating RSEM index at '${dict['output_dir']}'."
+    dict['build_name']="$(koopa_basename "${dict['output_dir']}")"
+    index_args+=(
+        '--gtf' "${dict['gtf_file']}"
+        '--num-threads' "${dict['jobs']}"
+        "${dict['genome_fasta_file']}"
+        "${dict['build_name']}"
+    )
+    koopa_dl 'Index args' "${index_args[*]}"
+    "${app['rsem_prepare_reference']}" "${index_args[@]}"
+    koopa_alert_success "RSEM index created at '${dict['output_dir']}'."
     return 0
 }
 
