45a968a3cbadec0740e54baa0a461f596aa83bb7
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-08 12:06:55 -0500

    Update apr on macOS

--- lang/shell/bash/include/install/common/shared/apr.sh
+++ lang/shell/bash/include/install/common/shared/apr.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install Apache Portable Runtime (APR) library.
-    # @note Updated 2022-09-01.
+    # @note Updated 2023-02-08.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/apr.rb
@@ -12,22 +12,11 @@ main() {
     # """
     local app conf_args dict
     koopa_assert_has_no_args "$#"
-    if koopa_is_macos
-    then
-        koopa_activate_app --build-only 'autoconf' 'automake' 'libtool'
-    fi
     koopa_activate_app 'sqlite'
     declare -A app=(
         ['make']="$(koopa_locate_make)"
     )
     [[ -x "${app['make']}" ]] || return 1
-    if koopa_is_macos
-    then
-        app['autoreconf']="$(koopa_locate_autoreconf)"
-        app['patch']="$(koopa_locate_patch)"
-        [[ -x "${app['autoreconf']}" ]] || return 1
-        [[ -x "${app['patch']}" ]] || return 1
-    fi
     declare -A dict=(
         ['jobs']="$(koopa_cpu_count)"
         ['name']='apr'
@@ -39,42 +28,9 @@ main() {
 ${dict['name']}/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
-    conf_args=(
-        "--prefix=${dict['prefix']}"
-    )
-    if koopa_is_macos
-    then
-        # Apply r1871981 which fixes a compile error on macOS 11.0.
-        koopa_cd "${dict['name']}-${dict['version']}"
-        koopa_download \
-            "https://raw.githubusercontent.com/Homebrew/\
-formula-patches/7e2246542543bbd3111a4ec29f801e6e4d538f88/\
-apr/r1871981-macos11.patch" \
-            'patch1.patch'
-        "${app['patch']}" \
-            --input='patch1.patch' \
-            --strip=0 \
-            --verbose
-        # Apply r1882980+1882981 to fix implicit exit() declaration
-        # Remove with the next release, along with autoconf dependency.
-        koopa_cd ..
-        koopa_download \
-            "https://raw.githubusercontent.com/Homebrew/\
-formula-patches/fa29e2e398c638ece1a72e7a4764de108bd09617/apr/\
-r1882980%2B1882981-configure.patch" \
-            'patch2.patch'
-        "${app['patch']}" \
-            --input='patch2.patch' \
-            --strip=0 \
-            --verbose
-        koopa_cd "${dict['name']}-${dict['version']}"
-        koopa_rm 'configure'
-        # This step requires autoconf, automake, and libtool.
-        "${app['autoreconf']}" --install
-        koopa_cd ..
-    fi
     koopa_cd "${dict['name']}-${dict['version']}"
     koopa_print_env
+    conf_args=("--prefix=${dict['prefix']}")
     koopa_dl 'configure args' "${conf_args[*]}"
     ./configure --help
     ./configure "${conf_args[@]}"
