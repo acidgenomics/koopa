d6ff76541bb71620264446b43cd93ade3ada3ac6
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-10 07:53:53 -0500

    Rework dog and mdcat

--- lang/shell/bash/include/install/common/shared/dog.sh
+++ lang/shell/bash/include/install/common/shared/dog.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-# FIXME Consider consolidating openssl code for dog and mdcat in rust-package.
-
 main() {
-    local dict
-    koopa_activate_app 'openssl3'
-    declare -A dict
-    dict['openssl']="$(koopa_app_prefix 'openssl3')"
-    export OPENSSL_DIR="${dict['openssl']}"
-    koopa_add_rpath_to_ldflags "${dict['openssl']}/lib"
     koopa_install_app_subshell \
         --installer='rust-package' \
         --name='dog' \
