eee13a451e3e6708dc8d0f65ba7940a76eb0cf67
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-10 08:27:01 -0500

    Ensure hadolint saves global store

--- lang/shell/bash/include/install/common/shared/hadolint.sh
+++ lang/shell/bash/include/install/common/shared/hadolint.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install hadolint.
-    # @note Updated 2023-01-26.
+    # @note Updated 2023-02-10.
     #
     # @seealso
     # - https://github.com/hadolint/hadolint
@@ -31,6 +31,9 @@ main() {
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
+    dict['cabal_store_dir']="$(\
+        koopa_init_dir "${dict['prefix']}/libexec/cabal/store" \
+    )"
     dict['ghc_prefix']="$(koopa_init_dir "ghc-${dict['ghc_version']}")"
     export CABAL_DIR="${dict['cabal_dir']}"
     export GHCUP_INSTALL_BASE_PREFIX="${dict['ghcup_prefix']}"
@@ -42,6 +45,14 @@ main() {
     koopa_add_to_path_start "${dict['ghc_prefix']}/bin"
     koopa_init_dir "${dict['prefix']}/bin"
     "${app['cabal']}" update
+    dict['cabal_config_file']="${dict['cabal_dir']}/config"
+    koopa_assert_is_file "${dict['cabal_config_file']}"
+    read -r -d '' "dict[cabal_config_string]" << END || true
+store-dir: ${dict['cabal_store_dir']}
+END
+    koopa_append_string \
+        --file="${dict['cabal_config_file']}" \
+        --string="${dict['cabal_config_string']}"
     "${app['cabal']}" install \
         --install-method='copy' \
         --installdir="${dict['prefix']}/bin" \
