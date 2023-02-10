aa876c59facda132fc2df5c0621ca415da6f99af
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-10 07:22:55 -0500

    Update mamba version

--- lang/shell/bash/include/install/common/shared/mdcat.sh
+++ lang/shell/bash/include/install/common/shared/mdcat.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# FIXME Need to resolve openssl issue:
+# mdcat compile fail -- ubuntu 22
+# openssl not found
+# openssl.pc
+
 main() {
     koopa_install_app_subshell \
         --installer='rust-package' \
