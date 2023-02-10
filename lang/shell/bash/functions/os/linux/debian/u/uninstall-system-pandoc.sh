253896b8fd1d38f1fd4a1b8d1bbc4b32a09068e0
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-10 07:35:58 -0500

    Remove system installers for azure-cli, google-cloud-sdk, llvm, and pandoc

--- lang/shell/bash/functions/os/linux/debian/u/uninstall-system-pandoc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-koopa_debian_uninstall_system_pandoc() {
-    koopa_uninstall_app \
-        --name='pandoc' \
-        --platform='debian' \
-        --system \
-        "$@"
-}
--- lang/shell/bash/functions/os/linux/fedora.sh
+++ lang/shell/bash/functions/os/linux/fedora.sh
@@ -1,80 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=all
 
-koopa_fedora_add_azure_cli_repo() {
-    local app dict
-    koopa_assert_has_no_args "$#"
-    koopa_assert_is_admin
-    declare -A app=(
-        ['sudo']="$(koopa_locate_sudo)"
-        ['tee']="$(koopa_locate_tee --allow-system)"
-    )
-    [[ -x "${app['sudo']}" ]] || return 1
-    [[ -x "${app['tee']}" ]] || return 1
-    declare -A dict=(
-        ['file']='/etc/yum.repos.d/azure-cli.repo'
-    )
-    [[ -f "${dict['file']}" ]] && return 0
-    "${app['sudo']}" "${app['tee']}" "${dict['file']}" >/dev/null << END
-[azure-cli]
-name=Azure CLI
-baseurl=https://packages.microsoft.com/yumrepos/azure-cli
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-END
-    return 0
-}
-
-koopa_fedora_add_google_cloud_sdk_repo() {
-    local app dict
-    koopa_assert_has_no_args "$#"
-    koopa_assert_is_admin
-    declare -A app=(
-        ['sudo']="$(koopa_locate_sudo --allow-system)"
-        ['tee']="$(koopa_locate_tee --allow-system)"
-    )
-    [[ -x "${app['sudo']}" ]] || return 1
-    [[ -x "${app['tee']}" ]] || return 1
-    declare -A dict=(
-        ['arch']="$(koopa_arch)"
-        ['enabled']=1
-        ['file']='/etc/yum.repos.d/google-cloud-sdk.repo'
-        ['gpgcheck']=1
-        ['repo_gpgcheck']=0
-    )
-    case "${dict['arch']}" in
-        'x86_64')
-            ;;
-        *)
-            koopa_stop 'Unsupported architecture.'
-            ;;
-    esac
-    if koopa_is_fedora || koopa_is_rhel_8_like
-    then
-        dict['platform']='el8'
-    elif koopa_is_rhel_7_like
-    then
-        dict['platform']='el7'
-    else
-        koopa_stop 'Unsupported platform.'
-    fi
-    dict['baseurl']="https://packages.cloud.google.com/yum/repos/\
-cloud-sdk-${dict['platform']}-${dict['arch']}"
-    [[ -f "${dict['file']}" ]] && return 0
-    "${app['sudo']}" "${app['tee']}" "${dict['file']}" >/dev/null << END
-[google-cloud-sdk]
-name=Google Cloud SDK
-baseurl=${dict['baseurl']}
-enabled=${dict['enabled']}
-gpgcheck=${dict['gpgcheck']}
-repo_gpgcheck=${dict['repo_gpgcheck']}
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-END
-    return 0
-}
-
 koopa_fedora_dnf_delete_repo() {
     local file name
     koopa_assert_has_args "$#"
@@ -107,23 +33,6 @@ koopa_fedora_dnf() {
     return 0
 }
 
-koopa_fedora_import_azure_cli_key() {
-    local app dict
-    koopa_assert_has_no_args "$#"
-    koopa_assert_is_admin
-    declare -A app=(
-        ['rpm']="$(koopa_fedora_locate_rpm)"
-        ['sudo']="$(koopa_locate_sudo)"
-    )
-    [[ -x "${app['rpm']}" ]] || return 1
-    [[ -x "${app['sudo']}" ]] || return 1
-    declare -A dict=(
-        ['key']='https://packages.microsoft.com/keys/microsoft.asc'
-    )
-    "${app['sudo']}" "${app['rpm']}" --import "${dict['key']}"
-    return 0
-}
-
 koopa_fedora_install_bcl2fastq() {
     koopa_install_app \
         --name='bcl2fastq' \
@@ -149,22 +58,6 @@ koopa_fedora_install_from_rpm() {
         --install \
         "$@"
     return 0
-}
-
-koopa_fedora_install_system_azure_cli() {
-    koopa_install_app \
-        --name='azure-cli' \
-        --platform='fedora' \
-        --system \
-        "$@"
-}
-
-koopa_fedora_install_system_google_cloud_sdk() {
-    koopa_install_app \
-        --name='google-cloud-sdk' \
-        --platform='fedora' \
-        --system \
-        "$@"
 }
 
 koopa_fedora_install_system_oracle_instant_client() {
@@ -229,22 +122,6 @@ koopa_fedora_set_locale() {
     "${app['locale']}"
     koopa_alert_success "Locale is defined as '${dict['lang_string']}'."
     return 0
-}
-
-koopa_fedora_uninstall_system_azure_cli() {
-    koopa_uninstall_app \
-        --name='azure-cli' \
-        --platform='fedora' \
-        --system \
-        "$@"
-}
-
-koopa_fedora_uninstall_system_google_cloud_sdk() {
-    koopa_uninstall_app \
-        --name='google-cloud-sdk' \
-        --platform='fedora' \
-        --system \
-        "$@"
 }
 
 koopa_fedora_uninstall_system_oracle_instant_client() {
