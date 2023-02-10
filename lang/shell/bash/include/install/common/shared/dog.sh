923fe7556a6d6387e340e7428ff5d28e582215a2
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-10 08:03:16 -0500

    Rework mdcat

--- lang/shell/bash/include/install/common/shared/dog.sh
+++ lang/shell/bash/include/install/common/shared/dog.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 main() {
+    local dict
+    declare -A dict
+    koopa_activate_app 'openssl1'
+    dict['openssl']="$(koopa_app_prefix 'openssl1')"
+    export OPENSSL_DIR="${dict['openssl']}"
+    koopa_add_rpath_to_ldflags "${dict['openssl']}/lib"
     koopa_install_app_subshell \
         --installer='rust-package' \
         --name='dog' \
