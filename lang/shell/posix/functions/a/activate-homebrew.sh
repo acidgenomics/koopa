b7e099a27469daf3f4eadcca3311631cbe2cd58f
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-16 08:06:56 -0500

    Clean up commented code

--- lang/shell/posix/functions/a/activate-homebrew.sh
+++ lang/shell/posix/functions/a/activate-homebrew.sh
@@ -3,17 +3,12 @@
 koopa_activate_homebrew() {
     # """
     # Activate Homebrew.
-    # @note Updated 2023-01-10.
+    # @note Updated 2023-02-16.
     # """
     local prefix
     prefix="$(koopa_homebrew_prefix)"
     [ -d "$prefix" ] || return 0
     [ -x "${prefix}/bin/brew" ] || return 0
-    # > export HOMEBREW_PREFIX="$prefix"
-    # > [ -z "${HOMEBREW_INSTALL_FROM_API:}" ] && \
-    # >     export HOMEBREW_INSTALL_FROM_API=1
-    # > [ -z "${HOMEBREW_NO_AUTO_UPDATE:-}" ] && \
-    # >     export HOMEBREW_NO_AUTO_UPDATE=1
     [ -z "${HOMEBREW_CLEANUP_MAX_AGE_DAYS:-}" ] && \
         export HOMEBREW_CLEANUP_MAX_AGE_DAYS=30
     [ -z "${HOMEBREW_INSTALL_CLEANUP:-}" ] && \
@@ -22,11 +17,5 @@ koopa_activate_homebrew() {
         export HOMEBREW_NO_ANALYTICS=1
     [ -z "${HOMEBREW_NO_ENV_HINTS:-}" ] && \
         export HOMEBREW_NO_ENV_HINTS=1
-    # > if koopa_is_macos
-    # > then
-    # >     # Alternatively, can add '--no-binaries' here.
-    # >     [ -z "${HOMEBREW_CASK_OPTS:-}" ] && \
-    # >         export HOMEBREW_CASK_OPTS='--no-quarantine'
-    # > fi
     return 0
 }
