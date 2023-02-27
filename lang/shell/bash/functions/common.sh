b76713a9649ae23aace40ac2d31f35e549771ee0
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-27 17:22:49 -0500

    Rework user permissions

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -26353,13 +26353,14 @@ koopa_write_string() {
     return 0
 }
 
-koopa_zsh_compaudit_set_permissions() {
+koopa_zsh_compaudit_set_permission() {
     local dict prefix prefixes
     koopa_assert_has_no_args "$#"
     koopa_assert_is_owner
     declare -A dict=(
         ['koopa_prefix']="$(koopa_koopa_prefix)"
         ['opt_prefix']="$(koopa_opt_prefix)"
+        ['sys_user']="$(koopa_sys_user)"
     )
     prefixes=(
         "${dict['koopa_prefix']}/lang/shell/zsh"
@@ -26368,9 +26369,14 @@ koopa_zsh_compaudit_set_permissions() {
     for prefix in "${prefixes[@]}"
     do
         [[ -d "$prefix" ]] || continue
+        if [[ "$(koopa_stat_user "$prefix")" != "${dict['sys_user']}" ]]
+        then
+            koopa_alert "Fixing ownership at '${prefix}'."
+            koopa_chown --recursive --sudo "${dict['sys_user']}" "$prefix"
+        fi
         if [[ "$(koopa_stat_access_octal "$prefix")" != '755' ]]
         then
-            koopa_alert "Fixing permissions for ZSH compaudit at '${prefix}'."
+            koopa_alert "Fixing write access at '${prefix}'."
             koopa_chmod --recursive 'g-w' "$prefix"
         fi
     done
