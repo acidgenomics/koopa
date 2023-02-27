460c72f23725935a587cdab79bcdc0b66007d39b
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-27 16:34:16 -0500

    Add owner detection and rework zsh permission setting

--- lang/shell/bash/functions/common/u/update-koopa.sh
+++ lang/shell/bash/functions/common/u/update-koopa.sh
@@ -3,7 +3,7 @@
 koopa_update_koopa() {
     # """
     # Update koopa installation.
-    # @note Updated 2022-09-23.
+    # @note Updated 2023-02-27.
     #
     # Update of pinned stable releases is intentionally not supported.
     #
@@ -12,6 +12,7 @@ koopa_update_koopa() {
     # """
     local dict
     koopa_assert_has_no_args "$#"
+    koopa_assert_is_owner
     declare -A dict=(
         ['prefix']="$(koopa_koopa_prefix)"
         ['user']="$(koopa_user)"
@@ -21,14 +22,7 @@ koopa_update_koopa() {
         koopa_alert_note "Pinned release detected at '${dict['prefix']}'."
         return 1
     fi
-    if koopa_is_shared_install
-    then
-        koopa_alert "Resetting permissions at '${dict['prefix']}'."
-        koopa_chown --recursive --sudo "${dict['user']}" "${dict['prefix']}"
-    fi
     koopa_git_pull "${dict['prefix']}"
-    koopa_alert "Resetting permissions at '${dict['prefix']}'."
-    koopa_sys_set_permissions --recursive "${dict['prefix']}"
-    koopa_fix_zsh_permissions
+    koopa_zsh_compaudit_set_permissions
     return 0
 }
