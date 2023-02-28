d2e4044156591ad29f1cb78336b5f0a80e3b880e
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-28 10:10:37 -0500

    Migrate back from bash library

---
+++ lang/shell/posix/functions/o/os-id.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+koopa_os_id() {
+    # """
+    # Operating system ID.
+    # @note Updated 2023-02-28.
+    #
+    # Just return the OS platform ID (e.g. debian).
+    # """
+    local string
+    string="$(koopa_os_string | cut -d '-' -f '1')"
+    [ -n "$string" ] || return 1
+    koopa_print "$string"
+    return 0
+}
