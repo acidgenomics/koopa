0096fb3894f4a121dd65629e906d62ee83060b19
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-28 10:00:05 -0500

    Remove trash app

--- lang/shell/bash/functions/os/macos/i/install-trash.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-koopa_macos_install_trash() {
-    koopa_install_app \
-        --name='trash' \
-        --platform='macos' \
-        "$@"
-}
--- lang/shell/bash/functions/os/macos/u/uninstall-trash.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-koopa_macos_uninstall_trash() {
-    koopa_uninstall_app \
-        --name='trash' \
-        "$@"
-}
