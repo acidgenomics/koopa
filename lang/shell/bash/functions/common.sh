7cd66b2dd5b34512d7547aaebe4620b913c4161b
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-28 09:35:21 -0500

    Rework app updater structure

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -25897,178 +25897,6 @@ koopa_unlink_in_opt() {
         "$@"
 }
 
-koopa_update_app() {
-    local bool dict
-    koopa_assert_has_args "$#"
-    koopa_assert_is_owner
-    koopa_assert_has_no_envs
-    declare -A bool=(
-        ['prefix_check']=1
-        ['quiet']=0
-        ['update_ldconfig']=0
-        ['verbose']=0
-    )
-    declare -A dict=(
-        ['koopa_prefix']="$(koopa_koopa_prefix)"
-        ['mode']='shared'
-        ['name']=''
-        ['opt_prefix']="$(koopa_opt_prefix)"
-        ['platform']='common'
-        ['prefix']=''
-        ['tmp_dir']="$(koopa_tmp_dir)"
-        ['updater_bn']=''
-        ['updater_fun']='main'
-        ['version']=''
-    )
-    while (("$#"))
-    do
-        case "$1" in
-            '--name='*)
-                dict['name']="${1#*=}"
-                shift 1
-                ;;
-            '--name')
-                dict['name']="${2:?}"
-                shift 2
-                ;;
-            '--platform='*)
-                dict['platform']="${1#*=}"
-                shift 1
-                ;;
-            '--platform')
-                dict['platform']="${2:?}"
-                shift 2
-                ;;
-            '--prefix='*)
-                dict['prefix']="${1#*=}"
-                shift 1
-                ;;
-            '--prefix')
-                dict['prefix']="${2:?}"
-                shift 2
-                ;;
-            '--updater='*)
-                dict['updater_bn']="${1#*=}"
-                shift 1
-                ;;
-            '--updater')
-                dict['updater_bn']="${2:?}"
-                shift 2
-                ;;
-            '--version='*)
-                dict['version']="${1#*=}"
-                shift 1
-                ;;
-            '--version')
-                dict['version']="${2:?}"
-                shift 2
-                ;;
-            '--no-prefix-check')
-                bool['prefix_check']=0
-                shift 1
-                ;;
-            '--quiet')
-                bool['quiet']=1
-                shift 1
-                ;;
-            '--system')
-                dict['mode']='system'
-                shift 1
-                ;;
-            '--user')
-                dict['mode']='user'
-                shift 1
-                ;;
-            '--verbose')
-                dict['verbose']=1
-                shift 1
-                ;;
-            *)
-                koopa_invalid_arg "$1"
-                ;;
-        esac
-    done
-    koopa_assert_is_set '--name' "${dict['name']}"
-    [[ "${bool['verbose']}" -eq 1 ]] && set -o xtrace
-    case "${dict['mode']}" in
-        'shared')
-            if [[ -z "${dict['prefix']}" ]]
-            then
-                dict['prefix']="${dict['opt_prefix']}/${dict['name']}"
-            fi
-            ;;
-        'system')
-            koopa_assert_is_admin
-            koopa_is_linux && bool['update_ldconfig']=1
-            ;;
-    esac
-    if [[ -n "${dict['prefix']}" ]]
-    then
-        if [[ ! -d "${dict['prefix']}" ]] && \
-            [[ "${bool['prefix_check']}" -eq 1 ]]
-        then
-            koopa_alert_is_not_installed "${dict['name']}" "${dict['prefix']}"
-            return 1
-        fi
-        dict['prefix']="$(koopa_realpath "${dict['prefix']}")"
-    fi
-    [[ -z "${dict['updater_bn']}" ]] && dict['updater_bn']="${dict['name']}"
-    dict['updater_file']="${dict['koopa_prefix']}/lang/shell/bash/include/\
-update/${dict['platform']}/${dict['mode']}/${dict['updater_bn']}.sh"
-    koopa_assert_is_file "${dict['updater_file']}"
-    source "${dict['updater_file']}"
-    koopa_assert_is_function "${dict['updater_fun']}"
-    if [[ "${bool['quiet']}" -eq 0 ]]
-    then
-        if [[ -d "${dict['prefix']}" ]]
-        then
-            koopa_alert_update_start "${dict['name']}" "${dict['prefix']}"
-        else
-            koopa_alert_update_start "${dict['name']}"
-        fi
-    fi
-    (
-        koopa_cd "${dict['tmp_dir']}"
-        PATH='/usr/bin:/bin'
-        export PATH
-        if koopa_is_linux && \
-            [[ -x '/usr/bin/pkg-config' ]]
-        then
-            koopa_activate_pkg_config \
-                '/usr/bin/pkg-config'
-        fi
-        export UPDATE_PREFIX="${dict['prefix']}"
-        "${dict['updater_fun']}"
-    )
-    koopa_rm "${dict['tmp_dir']}"
-    case "${dict['mode']}" in
-        'shared')
-            [[ -d "${dict['prefix']}" ]] && \
-                koopa_sys_set_permissions \
-                    --recursive "${dict['prefix']}"
-            ;;
-        'system')
-            [[ "${bool['update_ldconfig']}" -eq 1 ]] && \
-                koopa_linux_update_ldconfig
-            ;;
-        'user')
-            [[ -d "${dict['prefix']}" ]] && \
-                koopa_sys_set_permissions \
-                    --recursive --user "${dict['prefix']}"
-            ;;
-    esac
-    if [[ "${bool['quiet']}" -eq 0 ]]
-    then
-        if [[ -d "${dict['prefix']}" ]]
-        then
-            koopa_alert_update_success "${dict['name']}" "${dict['prefix']}"
-        else
-            koopa_alert_update_success "${dict['name']}"
-        fi
-    fi
-    return 0
-}
-
 koopa_update_koopa() {
     local dict prefix prefixes
     koopa_assert_has_no_args "$#"
@@ -26095,10 +25923,9 @@ koopa_update_koopa() {
 }
 
 koopa_update_r_packages() {
-    koopa_update_app \
-        --name='r-packages' \
-        --no-prefix-check \
-        "$@"
+    koopa_assert_has_no_args "$#"
+    koopa_r_koopa 'cliUpdateRPackages'
+    return 0
 }
 
 koopa_update_system_homebrew() {
@@ -26109,10 +25936,21 @@ koopa_update_system_homebrew() {
 }
 
 koopa_update_system_tex_packages() {
-    koopa_update_app \
-        --name='tex-packages' \
-        --system \
-        "$@"
+    local app
+    koopa_assert_has_no_args "$#"
+    declare -A app=(
+        ['sudo']="$(koopa_locate_sudo)"
+        ['tlmgr']="$(koopa_locate_tlmgr)"
+    )
+    [[ -x "${app['sudo']}" ]] || return 1
+    [[ -x "${app['tlmgr']}" ]] || return 1
+    (
+        koopa_activate_app --build-only 'curl' 'gnupg' 'wget'
+        "${app['sudo']}" "${app['tlmgr']}" update --self
+        "${app['sudo']}" "${app['tlmgr']}" update --list
+        "${app['sudo']}" "${app['tlmgr']}" update --all
+    )
+    return 0
 }
 
 koopa_validate_json() {
