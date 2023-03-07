365617f56c643173640717bba5874b76d170f4aa
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-07 09:05:20 -0500

    Rework using direct CPAN install

--- lang/shell/bash/include/install/common/shared/perl-package.sh
+++ lang/shell/bash/include/install/common/shared/perl-package.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# FIXME Alternatively can use cpan -j with our config file here?
+
 main() {
     # """
     # Install Perl package.
-    # @note Updated 2022-09-12.
+    # @note Updated 2023-03-07.
     #
     # Confirm library configuration with 'perl -V' and check '@INC' variable.
     #
@@ -24,23 +26,130 @@ main() {
     # - http://alumni.soe.ucsc.edu/~you/notes/perl-module-install.html
     # - https://docstore.mik.ua/orelly/weblinux2/modperl/ch03_09.htm
     # - https://blogs.iu.edu/ncgas/2019/05/30/installing-perl-modules-locally/
+    # - https://stackoverflow.com/questions/540640/
     # """
     local app bin_file bin_files dict
     koopa_assert_has_no_args "$#"
     koopa_activate_app --build-only 'perl'
     declare -A app=(
+        ['bash']="$(koopa_locate_bash)"
+        ['bzip2']="$(koopa_locate_bzip2)"
+        ['cpan']="$(koopa_locate_cpan)"
+        ['gpg']="$(koopa_locate_gpg)"
+        ['gzip']="$(koopa_locate_gzip)"
+        ['less']="$(koopa_locate_less)"
         ['make']="$(koopa_locate_make)"
+        ['patch']="$(koopa_locate_patch)"
         ['perl']="$(koopa_locate_perl)"
-        ['yes']="$(koopa_locate_yes)"
+        ['tar']="$(koopa_locate_tar)"
+        ['unzip']="$(koopa_locate_unzip)"
+        ['wget']="$(koopa_locate_wget)"
     )
+    [[ -x "${app['bash']}" ]] || return 1
+    [[ -x "${app['bzip2']}" ]] || return 1
+    [[ -x "${app['cpan']}" ]] || return 1
+    [[ -x "${app['gpg']}" ]] || return 1
+    [[ -x "${app['gzip']}" ]] || return 1
+    [[ -x "${app['less']}" ]] || return 1
     [[ -x "${app['make']}" ]] || return 1
+    [[ -x "${app['patch']}" ]] || return 1
     [[ -x "${app['perl']}" ]] || return 1
-    [[ -x "${app['yes']}" ]] || return 1
+    [[ -x "${app['tar']}" ]] || return 1
+    [[ -x "${app['unzip']}" ]] || return 1
+    [[ -x "${app['wget']}" ]] || return 1
     declare -A dict=(
+        ['cpan_prefix']="$(koopa_init_dir 'cpan')"
+        ['jobs']="$(koopa_cpu_count)"
         ['name']="${KOOPA_INSTALL_NAME:?}"
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
+    # Harden against any undesirable variables set by user.
+    unset -v \
+        PERL5LIB \
+        PERL_BASE \
+        PERL_LOCAL_LIB_ROOT \
+        PERL_MB_OPT \
+        PERL_MM_OPT
+    dict['cpan_config_file']="${dict['cpan_prefix']}/CPAN/MyConfig.pm"
+    read -r -d '' "dict[cpan_config_string]" << END || true
+\$CPAN::Config = {
+  'allow_installing_module_downgrades' => q[no],
+  'allow_installing_outdated_dists' => q[no],
+  'applypatch' => q[],
+  'auto_commit' => q[0],
+  'build_cache' => q[100],
+  'build_dir' => q[${dict['cpan_prefix']}/build],
+  'build_dir_reuse' => q[0],
+  'build_requires_install_policy' => q[yes],
+  'bzip2' => q[${app['bzip2']}],
+  'cache_metadata' => q[0],
+  'check_sigs' => q[0],
+  'cleanup_after_install' => q[1],
+  'colorize_output' => q[0],
+  'commandnumber_in_prompt' => q[1],
+  'connect_to_internet_ok' => q[1],
+  'cpan_home' => q[${dict['cpan_prefix']}],
+  'ftp_passive' => q[1],
+  'ftp_proxy' => q[],
+  'getcwd' => q[cwd],
+  'gpg' => q[${app['gpg']}],
+  'gzip' => q[${app['gzip']}],
+  'halt_on_failure' => q[1],
+  'histfile' => q[${dict['cpan_prefix']}/histfile],
+  'histsize' => q[100],
+  'http_proxy' => q[],
+  'inactivity_timeout' => q[0],
+  'index_expire' => q[1],
+  'inhibit_startup_message' => q[1],
+  'keep_source_where' => q[${dict['cpan_prefix']}/sources],
+  'load_module_verbosity' => q[v],
+  'make' => q[${app['make']}],
+  'make_arg' => q[-j${dict['jobs']}],
+  'make_install_arg' => q[-j${dict['jobs']}],
+  'make_install_make_command' => q[${app['make']}],
+  'makepl_arg' => q[INSTALL_BASE=${dict['prefix']}],
+  'mbuild_arg' => q[],
+  'mbuild_install_arg' => q[],
+  'mbuild_install_build_command' => q[./Build],
+  'mbuildpl_arg' => q[--install_base ${dict['prefix']}],
+  'no_proxy' => q[],
+  'pager' => q[${app['less']} -R],
+  'patch' => q[${app['patch']}],
+  'perl5lib_verbosity' => q[v],
+  'prefer_external_tar' => q[1],
+  'prefer_installer' => q[MB],
+  'prefs_dir' => q[${dict['cpan_prefix']}/prefs],
+  'prerequisites_policy' => q[follow],
+  'pushy_https' => q[1],
+  'recommends_policy' => q[1],
+  'scan_cache' => q[never],
+  'shell' => q[${app['bash']}],
+  'show_unparsable_versions' => q[0],
+  'show_upload_date' => q[0],
+  'show_zero_versions' => q[0],
+  'suggests_policy' => q[0],
+  'tar' => q[${app['tar']}],
+  'tar_verbosity' => q[vv],
+  'term_is_latin' => q[1],
+  'term_ornaments' => q[1],
+  'test_report' => q[0],
+  'trust_test_report_history' => q[0],
+  'unzip' => q[${app['unzip']}],
+  'urllist' => [q[http://www.cpan.org/]],
+  'use_prompt_default' => q[1],
+  'use_sqlite' => q[0],
+  'version_timeout' => q[15],
+  'wget' => q[${app['wget']}],
+  'yaml_load_code' => q[0],
+  'yaml_module' => q[YAML],
+};
+1;
+__END__
+END
+    koopa_write_string \
+        --file="${dict['cpan_config_file']}" \
+        --string="${dict['cpan_config_string']}"
     dict['name2']="${dict['name']}"
     dict['version2']="${dict['version']}"
     case "${dict['name']}" in
@@ -68,37 +177,13 @@ main() {
             koopa_stop "Unsupported Perl package: '${dict['name']}'."
             ;;
     esac
-    dict['file']="${dict['name2']}-${dict['version2']}.tar.gz"
-    dict['url']="https://cpan.metacpan.org/authors/id/\
-${dict['author']:0:1}/${dict['author']:0:2}/${dict['author']}/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name2']}-${dict['version2']}"
-    # Harden against any undesirable variables set by user.
-    unset -v \
-        PERL5LIB \
-        PERL_BASE \
-        PERL_LOCAL_LIB_ROOT \
-        PERL_MB_OPT \
-        PERL_MM_OPT
     dict['perl_ver']="$(koopa_get_version "${app['perl']}")"
     dict['perl_maj_ver']="$(koopa_major_version "${dict['perl_ver']}")"
     dict['lib_prefix']="${dict['prefix']}/lib/perl${dict['perl_maj_ver']}"
-    case "${dict['name']}" in
-        'ack')
-            # Install 'File::Next' dependency.
-            "${app['yes']}" | \
-                PERL5LIB="${dict['lib_prefix']}" \
-                "${app['perl']}" -MCPAN -e 'install File::Next' \
-                || true
-            ;;
-    esac
     koopa_print_env
-    koopa_assert_is_file 'Makefile.PL'
-    "${app['perl']}" 'Makefile.PL' INSTALL_BASE="${dict['prefix']}"
-    "${app['make']}"
-    # > "${app['make']}" test
-    "${app['make']}" install
+    "${app['cpan']}" \
+        -j "${dict['cpan_config_file']}" \
+        "${dict['author']}/${dict['name2']}-${dict['version2']}.tar.gz"
     koopa_assert_is_dir "${dict['lib_prefix']}"
     # Ensure we burn Perl library path into executables. This will add a line
     # directly under the shebang.
