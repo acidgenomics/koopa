0313a40f0f216a025283616029ed73b906b58a8f
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-09 09:34:22 -0500

    Rework using _ prefix

--- lang/shell/bash/functions/activate/activate-bash-prompt.sh
+++ lang/shell/bash/functions/activate/activate-bash-prompt.sh
@@ -5,8 +5,8 @@ _koopa_activate_bash_prompt() {
     # Activate Bash prompt.
     # @note Updated 2023-03-09.
     # """
-    koopa_is_root && return 0
-    koopa_activate_starship
+    _koopa_is_root && return 0
+    _koopa_activate_starship
     [[ -n "${STARSHIP_SHELL:-}" ]] && return 0
     PS1="$(_koopa_bash_prompt_string)"
     export PS1
--- lang/shell/bash/functions/activate/activate-bash-reverse-search.sh
+++ lang/shell/bash/functions/activate/activate-bash-reverse-search.sh
@@ -5,6 +5,6 @@ _koopa_activate_bash_reverse_search() {
     # Activate reverse search for Bash.
     # @note Updated 2022-05-16.
     # """
-    koopa_activate_mcfly
+    _koopa_activate_mcfly
     return 0
 }
