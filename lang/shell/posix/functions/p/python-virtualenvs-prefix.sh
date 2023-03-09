931bd4aa0c1a7e47a69a7567b0a25e9fbba5455c
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-09 11:10:48 -0500

    Set this to local user instead

--- lang/shell/posix/functions/p/python-virtualenvs-prefix.sh
+++ lang/shell/posix/functions/p/python-virtualenvs-prefix.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+# FIXME Move this into Bash.
 _koopa_python_virtualenvs_prefix() {
     # """
     # Python virtual environment prefix.
-    # @note Updated 2022-03-30.
+    # @note Updated 2023-03-09.
     # """
-    _koopa_print "$(_koopa_opt_prefix)/python-virtualenvs"
+    _koopa_print "${HOME}/.virtualenvs"
     return 0
 }
