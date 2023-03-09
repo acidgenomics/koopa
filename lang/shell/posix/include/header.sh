b7ffab78b594620e9ae9645a8c81b17d0386471f
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-09 08:22:52 -0500

    Ensure all activation functions are prefixed with underscore

--- lang/shell/posix/include/header.sh
+++ lang/shell/posix/include/header.sh
@@ -26,7 +26,7 @@ __koopa_posix_header() {
     fi
     if [ "${KOOPA_TEST:-0}" -eq 1 ]
     then
-        koopa_duration_start || return 1
+        _koopa_duration_start || return 1
     fi
     if [ -z "${KOOPA_DEFAULT_SYSTEM_PATH:-}" ]
     then
@@ -48,7 +48,7 @@ __koopa_posix_header() {
     esac
     if [ "${KOOPA_TEST:-0}" -eq 1 ]
     then
-        koopa_duration_stop 'posix' || return 1
+        _koopa_duration_stop 'posix' || return 1
     fi
     return 0
 }
@@ -60,52 +60,52 @@ __koopa_activate_koopa() {
     # """
     if [ "${KOOPA_MINIMAL:-0}" -eq 0 ]
     then
-        koopa_activate_path_helper || return 1
+        _koopa_activate_path_helper || return 1
     fi
-    koopa_add_to_path_start "${KOOPA_PREFIX}/bin" || return 1
-    koopa_add_to_manpath_start "${KOOPA_PREFIX}/share/man" || return 1
+    _koopa_add_to_path_start "${KOOPA_PREFIX}/bin" || return 1
+    _koopa_add_to_manpath_start "${KOOPA_PREFIX}/share/man" || return 1
     [ "${KOOPA_MINIMAL:-0}" -eq 0 ] || return 0
-    koopa_add_to_path_start \
+    _koopa_add_to_path_start \
         '/usr/local/bin'
-    koopa_add_to_manpath_start \
+    _koopa_add_to_manpath_start \
         '/usr/local/man' \
         '/usr/local/share/man'
-    koopa_add_to_manpath_end '/usr/share/man'
-    # > koopa_umask || return 1
-    koopa_export_koopa_cpu_count || return 1
-    koopa_export_koopa_shell || return 1
+    _koopa_add_to_manpath_end '/usr/share/man'
+    # > _koopa_umask || return 1
+    _koopa_export_koopa_cpu_count || return 1
+    _koopa_export_koopa_shell || return 1
     # Edge case for RStudio Server terminal to support dircolors.
     [ -n "${SHELL:-}" ] && export SHELL
-    koopa_activate_xdg || return 1
-    koopa_export_editor || return 1
-    koopa_export_gnupg || return 1
-    koopa_export_history || return 1
-    koopa_export_pager || return 1
-    koopa_activate_ca_certificates || return 1
-    koopa_activate_homebrew || return 1
-    koopa_activate_google_cloud_sdk || return 1
-    koopa_activate_ruby || return 1
-    koopa_activate_julia || return 1
-    koopa_activate_python || return 1
-    koopa_activate_pipx || return 1
-    koopa_activate_bcbio_nextgen || return 1
-    koopa_activate_color_mode || return 1
-    koopa_activate_alacritty || return 1
-    koopa_activate_bat || return 1
-    koopa_activate_bottom || return 1
-    koopa_activate_delta || return 1
-    koopa_activate_difftastic || return 1
-    koopa_activate_dircolors || return 1
-    koopa_activate_fzf || return 0
-    koopa_activate_gcc_colors || return 1
-    koopa_activate_kitty || return 1
-    koopa_activate_lesspipe || return 1
-    koopa_activate_secrets || return 1
-    koopa_activate_ssh_key || return 1
-    koopa_activate_tealdeer || return 1
-    if koopa_is_macos
+    _koopa_activate_xdg || return 1
+    _koopa_export_editor || return 1
+    _koopa_export_gnupg || return 1
+    _koopa_export_history || return 1
+    _koopa_export_pager || return 1
+    _koopa_activate_ca_certificates || return 1
+    _koopa_activate_homebrew || return 1
+    _koopa_activate_google_cloud_sdk || return 1
+    _koopa_activate_ruby || return 1
+    _koopa_activate_julia || return 1
+    _koopa_activate_python || return 1
+    _koopa_activate_pipx || return 1
+    _koopa_activate_bcbio_nextgen || return 1
+    _koopa_activate_color_mode || return 1
+    _koopa_activate_alacritty || return 1
+    _koopa_activate_bat || return 1
+    _koopa_activate_bottom || return 1
+    _koopa_activate_delta || return 1
+    _koopa_activate_difftastic || return 1
+    _koopa_activate_dircolors || return 1
+    _koopa_activate_fzf || return 0
+    _koopa_activate_gcc_colors || return 1
+    _koopa_activate_kitty || return 1
+    _koopa_activate_lesspipe || return 1
+    _koopa_activate_secrets || return 1
+    _koopa_activate_ssh_key || return 1
+    _koopa_activate_tealdeer || return 1
+    if _koopa_is_macos
     then
-        koopa_macos_activate_cli_colors || return 1
+        _koopa_macos_activate_cli_colors || return 1
     fi
     case "$(koopa_shell_name)" in
         'zsh')
@@ -115,25 +115,25 @@ __koopa_activate_koopa() {
                 alias mamba='koopa_alias_mamba'
             ;;
         *)
-            koopa_activate_conda || return 1
+            _koopa_activate_conda || return 1
             ;;
     esac
-    koopa_activate_micromamba || return 1
+    _koopa_activate_micromamba || return 1
     # Previously included:
     # > "$(koopa_xdg_local_home)/bin"
-    koopa_add_to_path_start \
+    _koopa_add_to_path_start \
         "$(koopa_scripts_private_prefix)/bin" \
         || return 1
-    if ! koopa_is_subshell
+    if ! _koopa_is_subshell
     then
-        koopa_add_config_link \
+        _koopa_add_config_link \
             "$KOOPA_PREFIX" 'home' \
             "${KOOPA_PREFIX}/activate" 'activate' \
             "$(koopa_dotfiles_prefix)" 'dotfiles' \
             || return 1
-        koopa_activate_today_bucket || return 1
+        _koopa_activate_today_bucket || return 1
     fi
-    koopa_activate_aliases || return 1
+    _koopa_activate_aliases || return 1
     return 0
 }
 
