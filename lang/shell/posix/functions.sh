194791c571e9f7d7167a169421475eebcc2fc635
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-10 16:32:08 -0500

    Improve POSIX compliance of functions

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -853,18 +853,28 @@ _koopa_activate_secrets() {
 }
 
 _koopa_activate_ssh_key() {
-    local key
     _koopa_is_linux || return 0
-    key="${1:-}"
-    if [ -z "$key" ] && [ -n "${SSH_KEY:-}" ]
+    __kvar_key="${1:-}"
+    if [ -z "$__kvar_key" ] && [ -n "${SSH_KEY:-}" ]
     then
-        key="$SSH_KEY"
+        __kvar_key="${SSH_KEY:?}"
     else
-        key="${HOME:?}/.ssh/id_rsa"
+        __kvar_key="${HOME:?}/.ssh/id_rsa"
     fi
-    [ -r "$key" ] || return 0
+    if [ ! -r "$__kvar_key" ]
+    then
+        unset -v __kvar_key
+        return 0
+    fi
+    _koopa_is_installed 'ssh-add' 'ssh-agent' || return 1
+    __kvar_nounset="$(_koopa_boolean_nounset)"
+    [ "$__kvar_nounset" -eq 1 ] && set +o nounset
     eval "$(ssh-agent -s)" >/dev/null 2>&1
-    ssh-add "$key" >/dev/null 2>&1
+    [ "$__kvar_nounset" -eq 1 ] && set -o nounset
+    ssh-add "$__kvar_key" >/dev/null 2>&1
+    unset -v \
+        __kvar_key \
+        __kvar_nounset
     return 0
 }
 
