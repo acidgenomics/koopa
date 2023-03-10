e15670b8c9b3c9c52df687175eaba2833cf1a65b
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-10 16:48:15 -0500

    Improve POSIX compliance of functions

--- lang/shell/posix/functions/a/activate-pyenv.sh
+++ lang/shell/posix/functions/a/activate-pyenv.sh
@@ -6,29 +6,28 @@ _koopa_activate_pyenv() {
     # @note Updated 2023-03-10.
     # """
     [ -n "${PYENV_ROOT:-}" ] && return 0
-    [ -x "$(_koopa_bin_prefix)/pyenv" ] || return 0
     __kvar_prefix="$(_koopa_pyenv_prefix)"
     if [ ! -d "$__kvar_prefix" ]
     then
         unset -v __kvar_prefix
         return 0
     fi
-    __kvar_script="${__kvar_prefix}/bin/pyenv"
-    if [ ! -r "$__kvar_script" ]
+    __kvar_pyenv="${__kvar_prefix}/bin/pyenv"
+    if [ ! -r "$__kvar_pyenv" ]
     then
         unset -v \
             __kvar_prefix \
-            __kvar_script
+            __kvar_pyenv
         return 0
     fi
     export PYENV_ROOT="$__kvar_prefix"
     __kvar_nounset="$(_koopa_boolean_nounset)"
     [ "$__kvar_nounset" -eq 1 ] && set +o nounset
-    eval "$("$__kvar_script" init -)"
+    eval "$("$__kvar_pyenv" init -)"
     [ "$__kvar_nounset" -eq 1 ] && set -o nounset
     unset -v \
         __kvar_nounset \
         __kvar_prefix \
-        __kvar_script
+        __kvar_pyenv
     return 0
 }
