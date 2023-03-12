c2cee0d1a625d781ddf2fef201922438c5b4badc
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-12 11:22:52 -0400

    Migrate Python virtualenv prefix

---
+++ lang/shell/bash/functions/common/p/python-virtualenvs-prefix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+koopa_python_virtualenvs_prefix() {
+    # """
+    # Python virtual environment prefix.
+    # @note Updated 2023-03-12.
+    # """
+    koopa_print "${HOME}/.virtualenvs"
+    return 0
+}
