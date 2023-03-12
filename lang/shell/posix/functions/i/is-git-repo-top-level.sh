1b5ea73844b9316ca7fb75c757747cbbff8ff5af
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-12 19:57:43 -0400

    Support parameterization

--- lang/shell/posix/functions/i/is-git-repo-top-level.sh
+++ lang/shell/posix/functions/i/is-git-repo-top-level.sh
@@ -5,8 +5,12 @@ koopa_is_git_repo_top_level() {
     # Is the working directory the top level of a git repository?
     # @note Updated 2023-03-12.
     # """
-    local prefix
-    koopa_assert_has_args_le "$#" 1
-    prefix="${1:-${PWD:?}}"
-    [[ -e "${prefix}/.git" ]]
+    local arg
+    koopa_assert_has_args "$#"
+    for arg in "$@"
+    do
+        [[ -d "$arg" ]] || return 1
+        [[ -e "${arg}/.git" ]] || return 1
+    done
+    return 0
 }
