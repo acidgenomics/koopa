641c6c11fc78cdfae0f86c977bc553f4f5a97bb9
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-12 11:31:56 -0400

    Support arguments

--- lang/shell/bash/functions/common/a/assert-is-git-repo.sh
+++ lang/shell/bash/functions/common/a/assert-is-git-repo.sh
@@ -2,15 +2,17 @@
 
 koopa_assert_is_git_repo() {
     # """
-    # Assert that current directory is a git repo.
-    # @note Updated 2021-08-19.
-    #
-    # Intentionally doesn't support input of multiple directories here.
+    # Assert that current directory is a Git repo.
+    # @note Updated 2023-03-12.
     # """
-    koopa_assert_has_no_args "$#"
-    if ! koopa_is_git_repo
-    then
-        koopa_stop "Not a Git repo: '${PWD:?}'."
-    fi
+    local arg
+    koopa_assert_has_args "$#"
+    for arg in "$@"
+    do
+        if ! koopa_is_git_repo "$arg"
+        then
+            koopa_stop "Not a Git repo: '${arg}'."
+        fi
+    done
     return 0
 }
