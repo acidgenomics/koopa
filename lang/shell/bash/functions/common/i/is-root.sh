63f8b193a7e6b5b0b51774afb5c24e75cdfc4651
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-13 14:34:57 -0400

    Migrate POSIX reexports to separate files

--- lang/shell/bash/functions/common/i/is-root.sh
+++ lang/shell/bash/functions/common/i/is-root.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # FIXME Create a Bash-specific version of this function.
-koopa_add_config_link() {
-    _koopa_add_config_link "$@"
+koopa_is_root() {
+    _koopa_is_root "$@"
 }
