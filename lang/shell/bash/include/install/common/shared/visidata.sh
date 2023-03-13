239e97295426590367c28b11fb92f34898a206d5
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-13 16:54:06 -0400

    Install as a conda environment instead

--- lang/shell/bash/include/install/common/shared/visidata.sh
+++ lang/shell/bash/include/install/common/shared/visidata.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# FIXME This isn't linking libexec/share/man/man1 correctly.
-
 main() {
     koopa_install_app_subshell \
-        --installer='python-venv' \
+        --installer='conda-env' \
         --name='visidata' \
         "$@"
 }
