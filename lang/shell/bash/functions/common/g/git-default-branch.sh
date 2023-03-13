371ce77b4e554e45cd5a90044d02124580aca287
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-12 21:11:11 -0400

    Harden

--- lang/shell/bash/functions/common/g/git-default-branch.sh
+++ lang/shell/bash/functions/common/g/git-default-branch.sh
@@ -3,7 +3,7 @@
 koopa_git_default_branch() {
     # """
     # Default branch of Git repository.
-    # @note Updated 2022-08-30.
+    # @note Updated 2023-03-12.
     #
     # Alternate approach:
     # > x="$( \
@@ -19,17 +19,16 @@ koopa_git_default_branch() {
     # # main
     # """
     local app dict repos
+    koopa_assert_has_args "$#"
     declare -A app=(
         ['git']="$(koopa_locate_git --allow-system)"
         ['sed']="$(koopa_locate_sed --allow-system)"
     )
     [[ -x "${app['git']}" ]] || return 1
     [[ -x "${app['sed']}" ]] || return 1
-    declare -A dict=(
-        ['remote']='origin'
-    )
+    declare -A dict
+    dict['remote']='origin'
     repos=("$@")
-    koopa_is_array_empty "${repos[@]}" && repos[0]="${PWD:?}"
     koopa_assert_is_dir "${repos[@]}"
     # Using a single subshell here to avoid performance hit during looping.
     # This single subshell is necessary so we don't change working directory.
@@ -37,16 +36,16 @@ koopa_git_default_branch() {
         local repo
         for repo in "${repos[@]}"
         do
-            local x
+            local string
             koopa_cd "$repo"
             koopa_is_git_repo || return 1
-            x="$( \
+            string="$( \
                 "${app['git']}" remote show "${dict['remote']}" \
                     | koopa_grep --pattern='HEAD branch' \
                     | "${app['sed']}" 's/.*: //' \
             )"
-            [[ -n "$x" ]] || return 1
-            koopa_print "$x"
+            [[ -n "$string" ]] || return 1
+            koopa_print "$string"
         done
     )
     return 0
