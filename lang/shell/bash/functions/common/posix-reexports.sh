7e4534ee91ef7a19904041e75c34c6b23867d27a
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-13 09:23:47 -0400

    Work on reorganizing POSIX reexports

--- lang/shell/bash/functions/common/posix-reexports.sh
+++ lang/shell/bash/functions/common/posix-reexports.sh
@@ -1,197 +1,236 @@
 #!/usr/bin/env bash
 
-koopa_activate_conda() {
-    _koopa_activate_conda "$@"
-}
-
-koopa_add_config_link() {
-    _koopa_add_config_link "$@"
-}
-
+# FIXME Create a Bash-specific version of this function.
 koopa_add_to_path_start() {
     _koopa_add_to_path_start "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_add_to_path_string_start() {
     _koopa_add_to_path_string_start "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_arch() {
     _koopa_arch "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_bin_prefix() {
     _koopa_bin_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_boolean_nounset() {
     _koopa_boolean_nounset "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_config_prefix() {
     _koopa_config_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_cpu_count() {
     _koopa_cpu_count "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_default_shell_name() {
     _koopa_default_shell_name "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_doom_emacs_prefix() {
     _koopa_doom_emacs_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_dotfiles_prefix() {
     _koopa_dotfiles_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_emacs_prefix() {
     _koopa_emacs_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_group() {
     _koopa_group "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_homebrew_prefix() {
     _koopa_homebrew_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_alias() {
     _koopa_is_alias "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_alpine() {
     _koopa_is_alpine "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_arch() {
     _koopa_is_arch "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_debian_like() {
     _koopa_is_debian_like "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_fedora_like() {
     _koopa_is_fedora_like "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_installed() {
     _koopa_is_installed "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_linux() {
     _koopa_is_linux "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_macos() {
     _koopa_is_macos "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_opensuse() {
     _koopa_is_opensuse "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_rhel_like() {
     _koopa_is_rhel_like "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_root() {
     _koopa_is_root "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_is_ubuntu_like() {
     _koopa_is_ubuntu_like "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_koopa_prefix() {
     _koopa_koopa_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_local_data_prefix() {
     _koopa_local_data_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_macos_emacs() {
     _koopa_macos_emacs "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_macos_os_version() {
     _koopa_macos_os_version "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_major_minor_patch_version() {
     _koopa_major_minor_patch_version "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_major_minor_version() {
     _koopa_major_minor_version "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_major_version() {
     _koopa_major_version "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_opt_prefix() {
     _koopa_opt_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_os_id() {
     _koopa_os_id "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_os_string() {
     _koopa_os_string "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_prelude_emacs_prefix() {
     _koopa_prelude_emacs_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_prelude_emacs() {
     _koopa_prelude_emacs "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_print() {
     _koopa_print "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_realpath() {
     _koopa_realpath "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_remove_from_path_string() {
     _koopa_remove_from_path_string "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_spacemacs_prefix() {
     _koopa_spacemacs_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_spacevim_prefix() {
     _koopa_spacevim_prefix "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_today() {
     _koopa_today "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_user() {
     _koopa_user "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_user_id() {
     _koopa_user_id "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_xdg_config_home() {
     _koopa_xdg_config_home "$@"
 }
 
+# FIXME Create a Bash-specific version of this function.
 koopa_xdg_data_home() {
     _koopa_xdg_data_home "$@"
 }
