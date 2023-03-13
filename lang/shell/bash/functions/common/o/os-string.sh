8f19be28d5ef64b776cc0bbe05b784fe9a6b1ff7
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-13 15:07:57 -0400

    Migrate POSIX reexports to separate files

---
+++ lang/shell/bash/functions/common/o/os-string.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# FIXME Create a Bash-specific version of this function.
+koopa_os_string() {
+    _koopa_os_string "$@"
+}
---
+++ lang/shell/bash/functions/common/u/user-id.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# FIXME Create a Bash-specific version of this function.
+koopa_user_id() {
+    _koopa_user_id "$@"
+}
