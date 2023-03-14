6153429e15ec2c85706d3890a060f558d37b35f9
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-14 12:29:44 -0400

    Rework our ont guppy uploader

--- lang/shell/bash/functions/common/u/update-ont-guppy-installers.sh
+++ lang/shell/bash/functions/common/u/update-ont-guppy-installers.sh
@@ -21,7 +21,7 @@ koopa_update_ont_guppy_installers() {
         ['s3_profile']='acidgenomics'
     )
     dict['s3_target']="$(koopa_private_installers_s3_uri)/${dict['name']}"
-    dict['version']="$(koopa_app_version "${dict['name']}")"
+    dict['version']="$(koopa_app_json_version "${dict['name']}")"
     koopa_mkdir \
         "${dict['prefix']}/linux/amd64" \
         "${dict['prefix']}/linux/arm64" \
