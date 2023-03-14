c94bb1fc97fd646213403c8092faf88801508f6e
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-14 16:06:05 -0400

    Rework boost

--- lang/shell/bash/include/install/common/shared/boost.sh
+++ lang/shell/bash/include/install/common/shared/boost.sh
@@ -7,6 +7,7 @@ main() {
     #
     # @seealso
     # - https://www.boost.org/users/download/
+    # - https://github.com/conda-forge/boost-feedstock/blob/main/recipe/build.sh
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/boost.rb
     # """
     local b2_args bootstrap_args deps dict
@@ -26,6 +27,12 @@ main() {
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
+    if koopa_is_macos
+    then
+        dict['toolset']='clang'
+    else
+        dict['toolset']='gcc'
+    fi
     dict['snake_version']="$(koopa_snake_case_simple "${dict['version']}")"
     dict['file']="${dict['name']}_${dict['snake_version']}.tar.bz2"
     dict['url']="https://boostorg.jfrog.io/artifactory/main/release/\
@@ -36,21 +43,27 @@ ${dict['version']}/source/${dict['file']}"
     bootstrap_args=(
         "--prefix=${dict['prefix']}"
         "--libdir=${dict['prefix']}/lib"
+        "--with-toolset=${dict['toolset']}"
         "--with-icu=${dict['icu4c']}"
         '--without-libraries=log,mpi,python'
     )
     b2_args=(
         "--prefix=${dict['prefix']}"
         "--libdir=${dict['prefix']}/lib"
-        '-d2'
         "-j${dict['jobs']}"
-        'install'
+        'variant=release'
+        "toolset=${dict['toolset']}"
         'threading=multi'
         'link=shared,static'
+        'runtime-link=shared'
+        "cxxflags=${CPPFLAGS:?}"
+        "linkflags=${LDFLAGS:?}"
+        'install'
     )
     koopa_print_env
     ./bootstrap.sh --help
     ./bootstrap.sh "${bootstrap_args[@]}"
+    # FIXME Can we print out '--help' here?
     ./b2 headers
     ./b2 "${b2_args[@]}"
     return 0
