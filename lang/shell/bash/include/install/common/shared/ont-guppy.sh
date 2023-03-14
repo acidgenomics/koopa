4888035d45589a969ffb6cdba154919caf8745c3
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-14 12:20:58 -0400

    Update guppy installer

--- lang/shell/bash/include/install/common/shared/ont-guppy.sh
+++ lang/shell/bash/include/install/common/shared/ont-guppy.sh
@@ -6,19 +6,18 @@
 main() {
     # """
     # Install Oxford Nanopore guppy caller.
-    # @note Updated 2023-01-10.
+    # @note Updated 2023-03-14.
     # """
     local app dict
     koopa_assert_has_no_args "$#"
-    koopa_assert_is_not_aarch64
-    # FIXME koopa_assert_has_private_access
+    koopa_assert_has_private_access
     declare -A app
     app['aws']="$(koopa_locate_aws)"
     [[ -x "${app['aws']}" ]] || return 1
     declare -A dict=(
         ['arch']="$(koopa_arch2)" # e.g. 'amd64'.
         ['core_type']='cpu' # or 'gpu'.
-        ['installers_url']="$(koopa_private_installers_url)"
+        ['installers_base']="$(koopa_private_installers_s3_uri)"
         ['name']='ont-guppy'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
@@ -33,7 +32,7 @@ main() {
         dict['ext']='tar.gz'
     fi
     dict['file']="${dict['version']}-${dict['core_type']}.${dict['ext']}"
-    dict['url']="${dict['installers_url']}/${dict['name']}/\
+    dict['url']="${dict['installers_base']}/${dict['name']}/\
 ${dict['platform']}/${dict['arch']}/${dict['file']}"
     "${app['aws']}" --profile='acidgenomics' \
         s3 cp "${dict['url']}" "${dict['file']}"
