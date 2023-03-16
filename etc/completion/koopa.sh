397d49ff518c339634602bb7009c08f969d08049
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-16 14:09:09 -0400

    Remove legacy Docker functions intended primarily for DockerHub

--- etc/completion/koopa.sh
+++ etc/completion/koopa.sh
@@ -643,9 +643,7 @@ _koopa_complete() {
                                 'build-all-images'
                                 'build-all-tags'
                                 'prune-all-images'
-                                'prune-all-stale-tags'
                                 'prune-old-images'
-                                'prune-stale-tags'
                                 'remove'
                                 'run'
                             )
