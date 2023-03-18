d7cecddcc65253f6fb55f616b51ae07810a6988a
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-18 19:20:24 -0400

    Add support for BSD stat

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -22223,11 +22223,20 @@ koopa_stat() {
     declare -A app
     app['stat']="$(koopa_locate_stat --allow-system)"
     [[ -x "${app['stat']}" ]] || return 1
-    declare -A dict=(
-        ['format']="${1:?}"
-    )
+    declare -A dict
+    dict['format']="${1:?}"
     shift 1
-    dict['out']="$("${app['stat']}" -c "${dict['format']}" "$@")"
+    if [[ "${app['stat']}" == '/usr/bin/stat' ]] && koopa_is_macos
+    then
+        dict['format_flag']='-f'
+    else
+        dict['format_flag']='--format'
+    fi
+    dict['out']="$( \
+        "${app['stat']}" \
+            "${dict['format_flag']}" "${dict['format']}" \
+            "$@" \
+    )"
     [[ -n "${dict['out']}" ]] || return 1
     koopa_print "${dict['out']}"
     return 0
