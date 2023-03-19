ff9d7ae4cc3360224be108de00f7602cbe747ec5
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-19 12:19:31 -0400

    Add NOTE

--- lang/shell/bash/include/install/common/shared/gcc.sh
+++ lang/shell/bash/include/install/common/shared/gcc.sh
@@ -132,6 +132,7 @@ ${dict['name']}-${dict['version']}/${dict['file']}"
         "--with-zstd=${dict['zstd']}"
         # Ensure linkage is current during bootstrap (stage 2).
         "--with-boot-ldflags=-static-libstdc++ -static-libgcc ${LDFLAGS:?}"
+        # NOTE Do we need to include '-lstdc++' here?
         "--with-boot-libs=${LDLIBS:?}"
     )
     if koopa_is_linux
