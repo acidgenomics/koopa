eeece401dd1b89b2ea4a6d2d51783d9bd42377d2
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-19 14:19:07 -0400

    Fix for OpenBLAS on Apple Silicon

--- lang/shell/bash/include/install/common/shared/openblas.sh
+++ lang/shell/bash/include/install/common/shared/openblas.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+# FIXME This is currently failing on Apple Silicon.
+# FIXME Work on customizing via Makefile.rule.
+
 main() {
     # """
     # Install OpenBLAS.
-    # @note Updated 2022-11-09.
+    # @note Updated 2023-03-19.
     #
     # Attempting to make in parallel can cause installer to crash.
     #
     # @seealso
+    # - https://github.com/conda-forge/openblas-feedstock
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/
     #     openblas.rb
-    # - https://ports.macports.org/port/OpenBLAS/details/
+    # - https://github.com/macports/macports-ports/blob/master/math/
+    #     OpenBLAS/Portfile
     # - https://iq.opengenus.org/install-openblas-from-source/
     # """
     local app dict
@@ -18,12 +23,12 @@ main() {
     koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'gcc'
     declare -A app=(
-        ['gcc']="$(koopa_locate_gcc --realpath)"
-        ['gfortran']="$(koopa_locate_gfortran --realpath)"
+        ['cc']='/usr/bin/gcc'
+        ['fc']="$(koopa_locate_gfortran --realpath)"
         ['make']="$(koopa_locate_make)"
     )
-    [[ -x "${app['gcc']}" ]] || return 1
-    [[ -x "${app['gfortran']}" ]] || return 1
+    [[ -x "${app['cc']}" ]] || return 1
+    [[ -x "${app['fc']}" ]] || return 1
     [[ -x "${app['make']}" ]] || return 1
     declare -A dict=(
         ['name']='OpenBLAS'
@@ -31,26 +36,33 @@ main() {
         ['shared_ext']="$(koopa_shared_ext)"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
+    if koopa_is_macos
+    then
+        # clang doesn't support this currently.
+        dict['use_openmp']=0
+    else
+        dict['use_openmp']=1
+    fi
     dict['file']="v${dict['version']}.tar.gz"
     dict['url']="https://github.com/xianyi/${dict['name']}/archive/\
 ${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_cd "${dict['name']}-${dict['version']}"
-    # Ensure target OS build version is consistency.
-    # > export MACOSX_DEPLOYMENT_TARGET='FIXME'
-    # > export TARGET='FIXME'
-    export DYNAMIC_ARCH=1
-    # Force a large 'NUM_THREADS' to support larger Macs with more cores
-    # available than our builder instance.
-    export NUM_THREADS=56
-    export USE_OPENMP=1
+    read -r -d '' "dict[makefile_string]" << END || true
+CC=${app['cc']}
+FC=${app['fc']}
+NUM_THREADS=56
+USE_OPENMP=${dict['use_openmp']}
+END
+    koopa_append_string \
+        --file='Makefile.rule' \
+        --string="${dict['makefile_string']}"
     koopa_print_env
-    # NOTE Need to deparallelize here, otherwise build will fail on macOS.
     "${app['make']}" VERBOSE=1 --jobs=1 \
-        "CC=${app['gcc']}" \
-        "FC=${app['gfortran']}" \
-        'libs' 'netlib' 'shared'
+        'libs' \
+        'netlib' \
+        'shared'
     "${app['make']}" "PREFIX=${dict['prefix']}" install
     (
         koopa_cd "${dict['prefix']}/lib"
