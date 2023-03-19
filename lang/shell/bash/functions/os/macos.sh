62e50a319570a87d4ffb92b0c50778d24bed574b
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-19 12:53:06 -0400

    Add support for Rosetta install

--- lang/shell/bash/functions/os/macos.sh
+++ lang/shell/bash/functions/os/macos.sh
@@ -654,6 +654,18 @@ koopa_macos_install_system_r() {
         "$@"
 }
 
+koopa_macos_install_system_rosetta() {
+    local app
+    declare -A app=(
+        ['softwareupdate']="$(koopa_macos_locate_softwareupdate)"
+        ['sudo']="$(koopa_locate_sudo)"
+    )
+    [[ -x "${app['softwareupdate']}" ]] || return 1
+    [[ -x "${app['sudo']}" ]] || return 1
+    "${app['sudo']}" "${app['softwareupdate']}" --install-rosetta
+    return 0
+}
+
 koopa_macos_install_system_xcode_clt() {
     koopa_install_app \
         --name='xcode-clt' \
