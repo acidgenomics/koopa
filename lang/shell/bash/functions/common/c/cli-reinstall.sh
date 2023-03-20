59432685bd835dbc1f7e0c2a76a2b604f44b328c
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-20 07:38:36 -0400

    All cURL reverse dependencies

--- lang/shell/bash/functions/common/c/cli-reinstall.sh
+++ lang/shell/bash/functions/common/c/cli-reinstall.sh
@@ -7,6 +7,11 @@ koopa_cli_reinstall() {
             koopa_reinstall_all_revdeps "$@"
             return 0
             ;;
+        '--only-revdeps')
+            shift 1
+            koopa_reinstall_all_revdeps "$@"
+            return 0
+            ;;
     esac
     koopa_cli_install --reinstall "$@"
 }
