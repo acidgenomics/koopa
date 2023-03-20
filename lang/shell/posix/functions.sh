5090c0207e03ab42e3abed66ea7bf8c9a660e753
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-20 08:55:20 -0400

    Rework conda activation

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -306,29 +306,42 @@ _koopa_activate_completion() {
 }
 
 _koopa_activate_conda() {
+    __kvar_deactivate=0
     __kvar_prefix="${1:-}"
-    [ -z "$__kvar_prefix" ] && __kvar_prefix="$(_koopa_conda_prefix)"
+    if [ -z "$__kvar_prefix" ]
+    then
+        __kvar_deactivate=1
+        __kvar_prefix="$(_koopa_conda_prefix)"
+    fi
     if [ ! -d "$__kvar_prefix" ]
     then
-        unset -v __kvar_prefix
+        unset -v \
+            __kvar_deactivate \
+            __kvar_prefix
         return 0
     fi
     __kvar_script="${__kvar_prefix}/bin/activate"
     if [ ! -r "$__kvar_script" ]
     then
-        unset -v __kvar_prefix __kvar_script
+        unset -v \
+            __kvar_deactivate \
+            __kvar_prefix \
+            __kvar_script
         return 0
     fi
     _koopa_is_alias 'conda' && unalias 'conda'
     __kvar_nounset="$(_koopa_boolean_nounset)"
     [ "$__kvar_nounset" -eq 1 ] && set +o nounset
     . "$__kvar_script"
-    if [ "${CONDA_DEFAULT_ENV:-}" = 'base' ] && [ "${CONDA_SHLVL:-0}" -eq 1 ]
+    if [ "$__kvar_deactivate" -eq 1 ] && \
+        [ "${CONDA_DEFAULT_ENV:-}" = 'base' ] && \
+        [ "${CONDA_SHLVL:-0}" -eq 1 ]
     then
         conda deactivate
     fi
     [ "$__kvar_nounset" -eq 1 ] && set -o nounset
     unset -v \
+        __kvar_deactivate \
         __kvar_nounset \
         __kvar_prefix \
         __kvar_script
