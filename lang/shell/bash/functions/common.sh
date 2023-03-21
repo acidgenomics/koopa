ebdee8b3035a63eb6cc8097536ede7c97b7dcfdc
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-21 11:15:52 -0400

    Don't set browser user agent for source forge

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -6286,8 +6286,16 @@ Gecko/20100101 Firefox/111.0"
                 '--output' "${dict['file']}"
                 '--retry' 5
                 '--show-error'
-                '--user-agent' "${dict['user_agent']}"
             )
+            case "${dict['url']}" in
+                *'sourceforge.net/'*)
+                    ;;
+                *)
+                    download_args+=(
+                        '--user-agent' "${dict['user_agent']}"
+                    )
+                    ;;
+            esac
             if [[ "${bool['progress']}" -eq 0 ]]
             then
                 download_args+=('--silent')
