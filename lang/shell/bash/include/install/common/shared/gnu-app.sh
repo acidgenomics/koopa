4031560b53c13fd1169363964dd79b270247e311
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-21 09:01:18 -0400

    Allow install as root

--- lang/shell/bash/include/install/common/shared/gnu-app.sh
+++ lang/shell/bash/include/install/common/shared/gnu-app.sh
@@ -88,6 +88,7 @@ main() {
     koopa_download "${dict['url']}" "${dict['file']}"
     koopa_extract "${dict['file']}"
     koopa_cd "${dict['name2']}-${dict['version']}"
+    export FORCE_UNSAFE_CONFIGURE=1
     koopa_print_env
     koopa_dl 'configure args' "${conf_args[*]}"
     ./configure --help
