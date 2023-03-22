562d4ab28717e974bc9e8cc752a3b9a20b976b72
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-22 08:00:27 -0400

    Work on installing aws-cli from source

---
+++ lang/shell/bash/include/install/common/shared/aws-cli.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+main() {
+    # """
+    # Install AWS CLI.
+    # @note Updated 2023-03-22.
+    #
+    # @seealso
+    # - https://docs.aws.amazon.com/cli/latest/userguide/
+    #     getting-started-source-install.html
+    # - https://github.com/aws/aws-cli/issues/6785
+    # """
+    local app conf_args dict
+    koopa_assert_has_no_args "$#"
+    declare -A app=(
+        ['make']="$(koopa_locate_make)"
+        ['python']="$(koopa_locate_python311)"
+    )
+    [[ -x "${app['make']}" ]] || return 1
+    [[ -x "${app['python']}" ]] || return 1
+    declare -A dict=(
+        ['jobs']="$(koopa_cpu_count)"
+        ['name']='aws-cli'
+        ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+        ['version']="${KOOPA_INSTALL_VERSION:?}"
+    )
+    dict['libexec']="$(koopa_init_dir "${dict['prefix']}/libexec")"
+    dict['file']="${dict['version']}.tar.gz"
+    dict['url']="https://github.com/aws/aws-cli/archive/refs/\
+tags/${dict['file']}"
+    koopa_download "${dict['url']}" "${dict['file']}"
+    koopa_extract "${dict['file']}"
+    koopa_cd "${dict['name']}-${dict['version']}"
+    koopa_python_create_venv \
+        --prefix="${dict['libexec']}" \
+        --python="${app['python']}"
+    koopa_add_to_path_start "${dict['libexec']}/bin"
+    conf_args=(
+        "--prefix=${dict['prefix']}"
+        '--with-download-deps'
+    )
+    koopa_print_env
+    koopa_dl 'configure args' "${conf_args[*]}"
+    ./configure --help
+    ./configure "${conf_args[@]}"
+    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
+    "${app['make']}" install
+    return 0
+}
