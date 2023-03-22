d69cad95dbbafceb941b70dc732cab46809a3659
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-22 05:36:30 -0400

    Convert to FIXME

--- lang/shell/bash/include/install/common/shared/bc.sh
+++ lang/shell/bash/include/install/common/shared/bc.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# NOTE This requires 'ed' to be installed on Ubuntu 20.
+# FIXME This requires 'ed' to be installed on Ubuntu 20.
 
 main() {
+    # """
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/bc.rb
+    # """
     koopa_activate_app --build-only 'texinfo'
     koopa_install_app_subshell \
         --installer='gnu-app' \
