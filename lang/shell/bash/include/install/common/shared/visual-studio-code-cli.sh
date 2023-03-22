a71e6a9f158d81dd6532a08aa19157db2c0baa6c
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-22 17:54:22 -0400

    Work on adding support for code-cli

--- lang/shell/bash/include/install/common/shared/visual-studio-code-cli.sh
+++ lang/shell/bash/include/install/common/shared/visual-studio-code-cli.sh
@@ -10,18 +10,32 @@ main() {
     # - https://github.com/Homebrew/homebrew-core/blob/master/
     #     Formula/code-cli.rb
     # """
-    local dict
-    declare -A dict
-
-    # https://github.com/microsoft/vscode/archive/refs/tags/1.76.2.tar.gz
-    dict['file']='setup.sh'
-    dict['url']="https://aka.ms/install-vscode-server/${dict['file']}"
+    local app dict install_args
+    declare -A app
+    app['cargo']="$(koopa_locate_cargo)"
+    [[ -x "${app['cargo']}" ]] || return 1
+    declare -A dict=(
+        ['cargo_home']="$(koopa_init_dir 'cargo')"
+        ['jobs']="$(koopa_cpu_count)"
+        ['name']='vscode'
+        ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+        ['version']="${KOOPA_INSTALL_VERSION:?}"
+    )
+    dict['file']="${dict['version']}.tar.gz"
+    dict['url']="https://github.com/microsoft/vscode/archive/refs/\
+tags/${dict['file']}"
     koopa_download "${dict['url']}" "${dict['file']}"
-    #ENV["VSCODE_CLI_NAME_LONG"] = "Code OSS"
-    #ENV["VSCODE_CLI_VERSION"] = version
-    #cd "cli" do
-    #  system "cargo", "install", *std_cargo_args
-    #end
-
+    koopa_extract "${dict['file']}"
+    koopa_cd "${dict['name']}-${dict['version']}/cli"
+    export CARGO_HOME="${dict['cargo_home']}"
+    export VSCODE_CLI_NAME_LONG='Code OSS'
+    export VSCODE_CLI_VERSION="${dict['version']}"
+    install_args=(
+        '--jobs' "${dict['jobs']}"
+        '--root' "${dict['prefix']}"
+        '--verbose'
+    )
+    koopa_print_env
+    "${app['cargo']}" install "${install_args[@]}"
     return 0
 }
