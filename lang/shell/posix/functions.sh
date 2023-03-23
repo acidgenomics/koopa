66775501dcdf5f16ab3619bf4af6f0a821844f95
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-23 17:52:04 -0400

    Attempt to harden readlink against old BSD systems

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -2072,10 +2072,34 @@ _koopa_rbenv_prefix() {
 }
 
 _koopa_realpath() {
-    __kvar_string="$(readlink -f "$@")"
-    [ -n "$__kvar_string" ] || return 1
-    _koopa_print "$__kvar_string"
-    unset -v __kvar_string
+    for __kvar_arg in "$@"
+    do
+        __kvar_string="$(readlink -f "$__kvar_arg" 2>/dev/null)"
+        if [ -z "$__kvar_string" ]
+        then
+            __kvar_string="$( \
+                perl -MCwd -le \
+                    'print Cwd::abs_path shift' \
+                    "$__kvar_arg" \
+                2>/dev/null \
+            )"
+        fi
+        if [ -z "$__kvar_string" ]
+        then
+            __kvar_string="$( \
+                python3 -c \
+                    "import os; print(os.path.realpath('${__kvar_arg}'))" \
+                2>/dev/null \
+            )"
+        fi
+        if [ -z "$__kvar_string" ]
+        then
+            unset -v __kvar_arg _kvar_string
+            return 1
+        fi
+        __koopa_print "$__kvar_string"
+    done
+    unset -v __kvar_arg __kvar_string
     return 0
 }
 
