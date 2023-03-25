1eb10d1510d1aa60f08f479eac7cfb36452f40d2
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-25 09:22:19 -0400

    Improve Python scripts

--- lang/python/app-dependencies.py
+++ lang/python/app-dependencies.py
@@ -2,7 +2,7 @@
 
 """
 Solve app dependencies defined in 'app.json' file.
-@note Updated 2023-03-24.
+@note Updated 2023-03-25.
 
 @examples
 ./app-dependencies.py 'mamba'
@@ -11,14 +11,35 @@ Solve app dependencies defined in 'app.json' file.
 from argparse import ArgumentParser
 from json import load
 from os.path import abspath, dirname, join
-from sys import exit as sys_exit
+
+
+def flatten(items, seqtypes=(list, tuple)):
+    """
+    Flatten an arbitrarily nested list.
+    @note Updated 2023-03-25.
+
+    @seealso
+    - https://stackoverflow.com/a/10824086
+    """
+    try:
+        for i, x in enumerate(items):
+            while isinstance(x, seqtypes):
+                items[i:i + 1] = x
+                x = items[i]
+    except IndexError:
+        pass
+    return items
 
 
 def get_deps(app_name: str, json_data: dict) -> list:
     """
     Get unique build dependencies and dependencies in an ordered list.
-    @note Updated 2023-03-24.
+    @note Updated 2023-03-25.
+
+    This makes list unique but keeps order intact, whereas usage of 'set()'
+    can rearrange.
     """
+    assert app_name in json_data
     build_deps = []
     deps = []
     if "build_dependencies" in json_data[app_name]:
@@ -26,47 +47,46 @@ def get_deps(app_name: str, json_data: dict) -> list:
     if "dependencies" in json_data[app_name]:
         deps = json_data[app_name]["dependencies"]
     out = build_deps + deps
-    # This makes list unique but keeps order intact, whereas usage of 'set()'
-    # can rearrange.
     out = list(dict.fromkeys(out))
     return out
 
 
-def main(app_name: str, json_file: str):
+def main(app_name: str, json_file: str) -> bool:
     """
     Parse the koopa 'app.json' file for defined values.
-    @note Updated 2023-03-20.
+    @note Updated 2023-03-25.
     """
     with open(json_file, encoding="utf-8") as con:
         json_data = load(con)
         keys = json_data.keys()
-        if app_name not in keys:
-            sys_exit(1)
+        assert app_name in keys
         deps = get_deps(app_name=app_name, json_data=json_data)
-        if not deps:
+        if len(deps) <= 0:
             return True
         i = 0
-        # FIXME We need to work on appending the list here...need to
-        # use '.append' for that I think...
-        lst = [[]]
-        lst[i] = deps
-        while i + 1:
-            deps2 = lst[i]
-            deps3 = [[]]
-            j = 0
-            for app_name in deps2:
-                print(app_name)
-                print(j)
-                xxx = get_deps(app_name=app_name, json_data=json_data)
-                if xxx:
-                    deps3[j] = xxx
-                j = j + 1
-            print(len(deps3))
-            print(deps3)
-            break
+        lst = []
+        lst.append(deps)
+        while i < len(deps):
+            aaa = []
+            for bbb in lst[i]:
+                if isinstance(bbb, list):
+                    for ccc in bbb:
+                        ddd = get_deps(app_name=ccc, json_data=json_data)
+                        if len(ddd) > 0:
+                            aaa.append(ddd)
+                else:
+                    ccc = get_deps(app_name=bbb, json_data=json_data)
+                    if len(ccc) > 0:
+                        aaa.append(ccc)
+            if len(aaa) <= 0:
+                break
+            lst.append(aaa)
             i = i + 1
-        # print(lst)
-        # print(lst[0])
+        lst.reverse()
+        lst = flatten(lst)
+        lst = list(dict.fromkeys(lst))
+        for val in lst:
+            print(val)
         return True
 
 
