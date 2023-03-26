a937dc25b095cdb6d2d9fabd7de6e84ea585f994
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-26 10:22:01 -0400

    Add vim build dependencies

--- lang/shell/bash/include/install/common/shared/vim.sh
+++ lang/shell/bash/include/install/common/shared/vim.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install Vim.
-    # @note Updated 2022-08-12.
+    # @note Updated 2023-03-26.
     #
     # On Ubuntu, '--enable-rubyinterp' currently causing a false positive error
     # related to ncurses, even when '--with-tlib' is correctly set.
@@ -13,6 +13,7 @@ main() {
     # """
     local app dict
     koopa_assert_has_no_args "$#"
+    koopa_activate_app --build-only 'make' 'pkg-config'
     koopa_activate_app 'ncurses' 'python3.11'
     declare -A app=(
         ['make']="$(koopa_locate_make)"
