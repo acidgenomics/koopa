5ed67b46ff111221553eb832cc5f2156595bbe57
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-26 16:50:57 -0400

    Improve dependencies

--- lang/shell/bash/include/install/common/shared/libjpeg-turbo.sh
+++ lang/shell/bash/include/install/common/shared/libjpeg-turbo.sh
@@ -15,7 +15,7 @@ main() {
     # """
     local app cmake_args dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'pkg-config'
+    koopa_activate_app --build-only 'cmake' 'make' 'pkg-config'
     declare -A app=(
         ['cmake']="$(koopa_locate_cmake)"
         ['make']="$(koopa_locate_make)"
