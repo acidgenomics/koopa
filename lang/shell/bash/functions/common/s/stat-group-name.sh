73bc7f2a74e1ec44e1e9c47b58c71c9e458433c3
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-26 09:36:19 -0400

    Improve stat functions

--- lang/shell/bash/functions/common/s/stat-group-name.sh
+++ lang/shell/bash/functions/common/s/stat-group-name.sh
@@ -6,9 +6,31 @@ koopa_stat_group_name() {
     # @note Updated 2023-03-26.
     #
     # @examples
-    # > koopa_stat_group_name '/tmp'
+    # > koopa_stat_group_name '/tmp' "${HOME:?}"
     # # wheel
+    # # staff
     # """
-    # FIXME BSD '%Sg'
-    koopa_stat '%G' "$@"
+    koopa_assert_has_args "$#"
+    koopa_assert_is_existing "$@"
+    declare -A app dict
+    if koopa_is_macos
+    then
+        app['stat']='/usr/bin/stat'
+        dict['format_flag']='-f'
+        dict['format_string']='%Sg'
+    else
+        app['stat']="$(koopa_locate_stat --allow-system)"
+        dict['format_flag']='--format'
+        dict['format_string']='%G'
+    fi
+    [[ -x "${app['stat']}" ]] || return 1
+    dict['out']="$( \
+        "${app['stat']}" \
+            "${dict['format_flag']}" \
+            "${dict['format_string']}" \
+            "$@" \
+    )"
+    [[ -n "${dict['out']}" ]] || return 1
+    koopa_print "${dict['out']}"
+    return 0
 }
--- lang/shell/bash/functions/common/s/stat-modified.sh
+++ lang/shell/bash/functions/common/s/stat-modified.sh
@@ -3,7 +3,7 @@
 koopa_stat_modified() {
     # """
     # Get file modification time.
-    # @note Updated 2021-11-16.
+    # @note Updated 2023-03-26.
     #
     # @seealso
     # - Convert seconds since Epoch into a useful format.
@@ -11,25 +11,56 @@ koopa_stat_modified() {
     #     Examples-of-date.html
     #
     # @examples
-    # > koopa_stat_modified '%Y-%m-%d' '/tmp'
-    # # 2021-10-17
+    # > koopa_stat_modified --format='%Y-%m-%d' '/tmp' "${HOME:?}"
+    # # 2023-02-09
+    # # 2023-03-26
     # """
-    local app dict timestamp timestamps x
-    koopa_assert_has_args_ge "$#" 2
-    declare -A app=(
-        ['date']="$(koopa_locate_date)"
-    )
+    local app dict pos timestamp timestamps
+    koopa_assert_has_args "$#"
+    declare -A app dict
+    app['date']="$(koopa_locate_date)"
+    app['stat']="$(koopa_locate_stat)"
     [[ -x "${app['date']}" ]] || return 1
-    declare -A dict=(
-        ['format']="${1:?}"
-    )
-    shift 1
-    readarray -t timestamps <<< "$(koopa_stat '%Y' "$@")"
+    [[ -x "${app['stat']}" ]] || return 1
+    dict['format']=''
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            # Key-value pairs --------------------------------------------------
+            '--format='*)
+                dict['format']="${1#*=}"
+                shift 1
+                ;;
+            '--format')
+                dict['format']="${2:?}"
+                shift 2
+                ;;
+            # Other ------------------------------------------------------------
+            '-'*)
+                koopa_invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    koopa_assert_is_set '--format' "${dict['format']}"
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa_assert_has_args "$#"
+    koopa_assert_is_existing "$@"
+    readarray -t timestamps <<< "$( \
+        "${app['stat']}" --format='%Y' "$@" \
+    )"
     for timestamp in "${timestamps[@]}"
     do
-        x="$("${app['date']}" -d "@${timestamp}" +"${dict['format']}")"
-        [[ -n "$x" ]] || return 1
-        koopa_print "$x"
+        local string
+        string="$( \
+            "${app['date']}" -d "@${timestamp}" +"${dict['format']}" \
+        )"
+        [[ -n "$string" ]] || return 1
+        koopa_print "$string"
     done
     return 0
 }
