55b7618a4dd5c248d1bf42e6034239876431ffb8
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-26 08:43:10 -0400

    Draft updates

--- lang/shell/bash/functions/common/s/stat-access-octal.sh
+++ lang/shell/bash/functions/common/s/stat-access-octal.sh
@@ -3,11 +3,16 @@
 koopa_stat_access_octal() {
     # """
     # Get the current access permissions in octal form.
-    # @note Updated 2021-11-16.
+    # @note Updated 2023-03-26.
+    #
+    # @seealso
+    # - https://www.cyberciti.biz/faq/get-octal-file-permissions-from-
+    #     command-line-on-linuxunix/
     #
     # @examples
     # > koopa_stat_access_octal '/tmp'
     # # 755
     # """
+    # FIXME BSD use '%OLp' or '%A'.
     koopa_stat '%a' "$@"
 }
---
+++ lang/shell/bash/functions/common/s/stat-user-name.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+koopa_stat_user_name() {
+    koopa_stop 'FIXME'
+}
