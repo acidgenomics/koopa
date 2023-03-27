0c569879844595f164efd3c77bbcccee71573bbc
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-27 17:16:31 -0400

    Add Python wrappers and harden R header check

---
+++ lang/shell/bash/functions/common/a/app-reverse-dependencies.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+koopa_app_reverse_dependencies() {
+    # """
+    # Get the reverse dependencies of an app.
+    # @note Updated 2023-03-27.
+    #
+    # @examples
+    # koopa_app_reverse_dependencies 'python3.11'
+    # """
+    local app_name cmd
+    koopa_assert_has_args_eq "$#" 1
+    app_name="${1:?}"
+    cmd="$(koopa_koopa_prefix)/lang/python/app-reverse-dependencies.py"
+    koopa_assert_is_executable "$cmd"
+    "$cmd" "$app_name"
+    return 0
+}
