9de7aac5f7d2110892766683fa3235d87610520d
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-27 10:11:11 -0400

    Add support for koopa_stat_user_name

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -22489,7 +22489,29 @@ koopa_stat_user_id() {
 }
 
 koopa_stat_user_name() {
-    koopa_stop 'FIXME'
+    koopa_assert_has_args "$#"
+    koopa_assert_is_existing "$@"
+    declare -A app dict
+    if koopa_is_macos
+    then
+        app['stat']='/usr/bin/stat'
+        dict['format_flag']='-f'
+        dict['format_string']='%Su'
+    else
+        app['stat']="$(koopa_locate_stat --allow-system)"
+        dict['format_flag']='--format'
+        dict['format_string']='%U'
+    fi
+    [[ -x "${app['stat']}" ]] || return 1
+    dict['out']="$( \
+        "${app['stat']}" \
+            "${dict['format_flag']}" \
+            "${dict['format_string']}" \
+            "$@" \
+    )"
+    [[ -n "${dict['out']}" ]] || return 1
+    koopa_print "${dict['out']}"
+    return 0
 }
 
 koopa_status_fail() {
