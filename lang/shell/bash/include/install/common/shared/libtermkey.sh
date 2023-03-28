a55f208a71a2a39f8e8ef78f6551b16a14516931
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-28 19:10:58 -0400

    Improve dependencies in app.json

--- lang/shell/bash/include/install/common/shared/libtermkey.sh
+++ lang/shell/bash/include/install/common/shared/libtermkey.sh
@@ -10,11 +10,10 @@ main() {
     #     Formula/libtermkey.rb
     # """
     local app dict
-    koopa_activate_app --build-only 'libtool' 'pkg-config'
+    koopa_activate_app --build-only 'libtool' 'make' 'pkg-config'
     koopa_activate_app 'ncurses' 'unibilium'
-    declare -A app=(
-        ['make']="$(koopa_locate_make)"
-    )
+    declare -A app
+    app['make']="$(koopa_locate_make)"
     [[ -x "${app['make']}" ]] || return 1
     declare -A dict=(
         ['name']='libtermkey'
