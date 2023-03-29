b10e9d20f857fc50da4612df9dee97d651b735b2
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-29 09:26:32 -0400

    Add support for listing installed app store apps

---
+++ lang/shell/bash/functions/os/macos/l/list-app-store-apps.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+koopa_macos_list_app_store_apps() {
+    # """
+    # List applications installed via the Mac App Store.
+    # @note Updated 2023-03-29.
+    # """
+    local app string
+    declare -A app
+    app['find']="$(koopa_locate_find --allow-system)"
+    app['sed']="$(koopa_locate_sed --allow-system)"
+    app['sort']="$(koopa_locate_sort --allow-system)"
+    [[ -x "${app['find']}" ]] || return 1
+    [[ -x "${app['sed']}" ]] || return 1
+    [[ -x "${app['sort']}" ]] || return 1
+    string="$( \
+        "${app['find']}" \
+            '/Applications' \
+            -maxdepth 4 \
+            -path '*Contents/_MASReceipt/receipt' \
+            -print \
+        | "${app['sed']}" \
+            -e 's#.app/Contents/_MASReceipt/receipt#.app#g' \
+            -e 's#/Applications/##' \
+        | "${app['sort']}" \
+    )"
+    [[ -n "$string" ]] || return 1
+    koopa_print "$string"
+    return 0
+}
