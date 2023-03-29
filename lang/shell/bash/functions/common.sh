d1402cf1f6370a1411918c3764e3a375069d7c78
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-29 12:18:03 -0400

    Improve recursive install

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -9874,11 +9874,8 @@ koopa_install_agat() {
 }
 
 koopa_install_all_apps() {
-    local app app_name app_names dict push_apps
+    local app_name app_names dict push_apps
     koopa_assert_has_no_args "$#"
-    declare -A app
-    app['koopa']="$(koopa_locate_koopa)"
-    [[ -x "${app['koopa']}" ]] || return 1
     declare -A dict=(
         ['mem_gb']="$(koopa_mem_gb)"
         ['mem_gb_cutoff']=6
@@ -9890,10 +9887,11 @@ koopa_install_all_apps() {
     readarray -t app_names <<< "$(koopa_shared_apps)"
     for app_name in "${app_names[@]}"
     do
-        local prefix
-        prefix="$(koopa_app_prefix --allow-missing "$app_name")"
-        [[ -d "$prefix" ]] && continue
-        "${app['koopa']}" install "$app_name"
+        if [[ -d "$(koopa_app_prefix --allow-missing "$app_name")" ]]
+        then
+            continue
+        fi
+        koopa_cli_install "$app_name"
         push_apps+=("$app_name")
     done
     if koopa_can_install_binary && \
@@ -9908,396 +9906,30 @@ koopa_install_all_apps() {
 }
 
 koopa_install_all_binary_apps() {
-    local app app_name apps bool dict
+    local app_name app_names bool
     koopa_assert_has_no_args "$#"
-    declare -A app bool dict
-    app['koopa']="$(koopa_locate_koopa)"
-    [[ -x "${app['koopa']}" ]] || return 1
+    declare -A bool
     bool['bootstrap']=0
-    bool['large']=0
-    koopa_has_large_system_disk && bool['large']=1
-    dict['app_prefix']="$(koopa_app_prefix)"
-    [[ ! -d "${dict['app_prefix']}/aws-cli" ]] && bool['bootstrap']=1
-    apps=()
-    koopa_is_linux && apps+=('attr')
-    apps+=(
-        'zlib'
-        'lz4'
-        'zstd'
-        'bzip2'
-        'ca-certificates'
-        'openssl1'
-        'openssl3'
-        'curl'
-        'curl7'
-        'm4'
-        'gmp'
-        'coreutils'
-        'findutils'
-        'gettext'
-        'libiconv'
-        'pcre'
-        'pcre2'
-        'grep'
-        'sed'
-    )
-    apps+=(
-        'ack'
-        'apr'
-        'apr-util'
-        'armadillo'
-        'asdf'
-        'aspell'
-        'autoconf'
-        'autoflake'
-        'automake'
-        'bash'
-        'bash-language-server'
-        'bashcov'
-        'bat'
-        'bc'
-        'bfg'
-        'binutils'
-        'bison'
-        'black'
-        'boost'
-        'bottom'
-        'bpytop'
-        'broot'
-        'c-ares'
-        'cairo'
-        'cheat'
-        'chemacs'
-        'chezmoi'
-        'cmake'
-        'colorls'
-        'conda'
-        'convmv'
-        'cpufetch'
-        'csvkit'
-        'csvtk'
-        'delta'
-        'diff-so-fancy'
-        'difftastic'
-        'dog'
-        'dotfiles'
-        'du-dust'
-        'ed'
-        'editorconfig'
-        'emacs'
-        'entrez-direct'
-        'exa'
-        'exiftool'
-        'expat'
-        'fd-find'
-        'ffmpeg'
-        'fish'
-        'flac'
-        'flake8'
-        'flex'
-        'fltk'
-        'fontconfig'
-        'freetype'
-        'fribidi'
-        'fzf'
-        'gawk'
-        'gcc'
-        'gdal'
-        'gdbm'
-        'geos'
-        'gh'
-        'git'
-        'git-lfs'
-        'glances'
-        'glib'
-        'gnupg'
-        'gnutls'
-        'go'
-        'gperf'
-        'graphviz'
-        'grex'
-        'groff'
-        'gsl'
-        'gtop'
-        'gum'
-        'gzip'
-        'harfbuzz'
-        'hdf5'
-        'hexyl'
-        'htop'
-        'httpie'
-        'hugo'
-        'hyperfine'
-        'icu4c'
-        'imagemagick'
-        'ipython'
-        'isl'
-        'isort'
-        'jemalloc'
-        'jpeg'
-        'jq'
-        'jupyterlab'
-        'lame'
-        'lapack'
-        'latch'
-        'less'
-        'lesspipe'
-        'libassuan'
-        'libedit'
-        'libev'
-        'libevent'
-        'libffi'
-        'libgcrypt'
-        'libgeotiff'
-        'libgit2'
-        'libgpg-error'
-        'libidn'
-        'libjpeg-turbo'
-        'libksba'
-        'libpipeline'
-        'libpng'
-        'libssh2'
-        'libtasn1'
-        'libtiff'
-        'libtool'
-        'libunistring'
-        'libuv'
-        'libxml2'
-        'libyaml'
-        'libzip'
-        'llama'
-        'lsd'
-        'lua'
-        'luarocks'
-        'lzip'
-        'lzo'
-        'make'
-        'man-db'
-        'markdownlint-cli'
-        'mcfly'
-        'mdcat'
-        'meson'
-        'miller'
-        'mpc'
-        'mpdecimal'
-        'mpfr'
-        'nano'
-        'ncurses'
-        'neofetch'
-        'neovim'
-        'nettle'
-        'nghttp2'
-        'ninja'
-        'nmap'
-        'node'
-        'npth'
-        'nushell'
-        'oniguruma'
-        'openblas'
-        'openjdk'
-        'openssh'
-        'pandoc'
-        'parallel'
-        'password-store'
-        'patch'
-        'perl'
-        'pipx'
-        'pixman'
-        'pkg-config'
-        'poetry'
-        'prettier'
-        'procs'
-        'proj'
-        'py-spy'
-        'pycodestyle'
-        'pyenv'
-        'pyflakes'
-        'pygments'
-        'pylint'
-        'pytaglib'
-        'pytest'
-        'python3.10'
-        'python3.11'
-        'quarto'
-        'r'
-        'radian'
-        'ranger-fm'
-        'rbenv'
-        'readline'
-        'rename'
-        'ripgrep'
-        'ripgrep-all'
-        'rmate'
-        'ronn'
-        'rsync'
-        'ruby'
-        'ruff'
-        'scons'
-        'sd'
-        'serf'
-        'shellcheck'
-        'shunit2'
-        'sox'
-        'sqlite'
-        'starship'
-        'stow'
-        'subversion'
-        'swig'
-        'taglib'
-        'tar'
-        'tcl-tk'
-        'tealdeer'
-        'texinfo'
-        'tmux'
-        'tokei'
-        'tree'
-        'tuc'
-        'udunits'
-        'units'
-        'unzip'
-        'utf8proc'
-        'vim'
-        'visidata'
-        'vulture'
-        'wget'
-        'which'
-        'xorg-libice'
-        'xorg-libpthread-stubs'
-        'xorg-libsm'
-        'xorg-libx11'
-        'xorg-libxau'
-        'xorg-libxcb'
-        'xorg-libxdmcp'
-        'xorg-libxext'
-        'xorg-libxrandr'
-        'xorg-libxrender'
-        'xorg-libxt'
-        'xorg-xcb-proto'
-        'xorg-xorgproto'
-        'xorg-xtrans'
-        'xsv'
-        'xxhash'
-        'xz'
-        'yapf'
-        'yarn'
-        'yq'
-        'yt-dlp'
-        'zellij'
-        'zip'
-        'zoxide'
-        'zsh'
-    )
-    if ! koopa_is_aarch64
+    if [[ ! -d "$(koopa_app_prefix --allow-missing 'aws-cli')" ]]
     then
-        apps+=(
-            'hadolint'
-            'pandoc'
-        )
+        bool['bootstrap']=1
     fi
-    if koopa_is_linux
-    then
-        apps+=(
-            'docker-credential-pass'
-            'elfutils'
-            'pinentry'
-        )
-    fi
-    if [[ "${bool['large']}" -eq 1 ]]
-    then
-        apps+=(
-            'anaconda'
-            'apache-airflow'
-            'apache-spark'
-            'azure-cli'
-            'cli11'
-            'ensembl-perl-api'
-            'fmt'
-            'ghostscript'
-            'google-cloud-sdk'
-            'googletest'
-            'gseapy'
-            'haskell-cabal'
-            'haskell-ghcup'
-            'julia'
-            'libarchive'
-            'libsolv'
-            'llvm'
-            'mamba'
-            'nlohmann-json'
-            'pybind11'
-            'r-devel'
-            'reproc'
-            'rust'
-            'spdlog'
-            'termcolor'
-            'tl-expected'
-            'yaml-cpp'
-        )
-        if ! koopa_is_aarch64
-        then
-            apps+=(
-                'agat'
-                'autodock'
-                'autodock-vina'
-                'bamtools'
-                'bedtools'
-                'bioawk'
-                'bowtie2'
-                'bustools'
-                'deeptools'
-                'fastqc'
-                'ffq'
-                'fgbio'
-                'fq'
-                'fqtk'
-                'gatk'
-                'gffutils'
-                'gget'
-                'hisat2'
-                'htseq'
-                'kallisto'
-                'minimap2'
-                'multiqc'
-                'nanopolish'
-                'nextflow'
-                'openbb'
-                'picard'
-                'rsem'
-                'salmon'
-                'sambamba'
-                'samtools'
-                'scalene'
-                'seqkit'
-                'snakefmt'
-                'snakemake'
-                'sra-tools'
-                'star'
-                'star-fusion'
-                'subread'
-                'umis'
-            )
-        fi
-        if koopa_is_linux
-        then
-            apps+=(
-                'apptainer'
-                'aspera-connect'
-                'lmod'
-            )
-        fi
-    fi
-    koopa_add_to_path_start '/usr/local/bin'
+    readarray -t app_names <<< "$(koopa_shared_apps)"
     if [[ "${bool['bootstrap']}" -eq 1 ]]
     then
-        "${app['koopa']}" install 'aws-cli'
+        koopa_cli_install 'aws-cli'
     fi
-    for app_name in "${apps[@]}"
+    for app_name in "${app_names[@]}"
     do
-        "${app['koopa']}" install --binary "$app_name"
+        if [[ -d "$(koopa_app_prefix --allow-missing "$app_name")" ]]
+        then
+            continue
+        fi
+        koopa_cli_install --binary "$app_name"
     done
     if [[ "${bool['bootstrap']}" -eq 1 ]]
     then
-        "${app['koopa']}" reinstall 'aws-cli'
+        koopa_cli_install --reinstall 'aws-cli'
     fi
     return 0
 }
