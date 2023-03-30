d04a41707ef841015764908cd7b514bc331454d8
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-30 17:31:50 -0400

    Update CMake build scripts

--- lang/shell/bash/include/install/common/shared/brotli.sh
+++ lang/shell/bash/include/install/common/shared/brotli.sh
@@ -3,40 +3,23 @@
 main() {
     # """
     # Install Brotli.
-    # @note Updated 2023-03-24.
+    # @note Updated 2023-03-30.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/brotli.rb
     # """
-    local app cmake_args dict
+    local dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'cmake' 'make'
-    declare -A app=(
-        ['cmake']="$(koopa_locate_cmake)"
-        ['make']="$(koopa_locate_make)"
-    )
-    [[ -x "${app['cmake']}" ]] || return 1
-    [[ -x "${app['make']}" ]] || return 1
+    koopa_activate_app --build-only 'pkg-config'
     declare -A dict=(
-        ['jobs']="$(koopa_cpu_count)"
-        ['name']='brotli'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
-    dict['file']="v${dict['version']}.tar.gz"
-    dict['url']="https://github.com/google/${dict['name']}/\
-archive/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
-    cmake_args=(
-        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
-        '-DCMAKE_VERBOSE_MAKEFILE=ON'
-    )
-    koopa_print_env
-    koopa_dl 'CMake args' "${cmake_args[*]}"
-    "${app['cmake']}" -LH -S . "${cmake_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="https://github.com/google/brotli/archive/\
+v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_cmake_build --prefix="${dict['prefix']}"
     return 0
 }
