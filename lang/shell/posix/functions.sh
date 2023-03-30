b81f1db54cc1647a7759c071f214959b1e982c80
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-30 09:05:10 -0400

    Ensure we define XDG_STATE_HOME

--- lang/shell/posix/functions.sh
+++ lang/shell/posix/functions.sh
@@ -2299,6 +2299,12 @@ _koopa_xdg_local_home() {
 }
 
 _koopa_xdg_state_home() {
-    _koopa_print "$(_koopa_xdg_local_home)/state"
+    __kvar_string="${XDG_STATE_HOME:-}"
+    if [ -z "$__kvar_string" ]
+    then
+        __kvar_string="$(_koopa_xdg_local_home)/state"
+    fi
+    _koopa_print "$__kvar_string"
+    unset -v __kvar_string
     return 0
 }
