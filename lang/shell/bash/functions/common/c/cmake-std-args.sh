c67afd17fc6d3f642c6ec12ac69d865ff8c5795c
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-30 17:16:01 -0400

    Improve standardization of CMake builds

---
+++ lang/shell/bash/functions/common/c/cmake-std-args.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+koopa_cmake_std_args() {
+    # """
+    # Standard CMake arguments.
+    # @note Updated 2023-03-30.
+    #
+    # Potentially useful:
+    # - CMAKE_CXX_FLAGS
+    # - CMAKE_STATIC_LINKER_FLAGS
+    # """
+    local dict prefix
+    koopa_assert_has_args_eq "$#" 1
+    declare -A dict
+    dict['prefix']="${1:?}"
+    args=(
+        '-DCMAKE_BUILD_TYPE=Release'
+        "-DCMAKE_C_FLAGS=${CFLAGS:-}"
+        "-DCMAKE_EXE_LINKER_FLAGS=${LDFLAGS:-}"
+        '-DCMAKE_INSTALL_LIBDIR=lib'
+        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
+        "-DCMAKE_INSTALL_RPATH=${dict['prefix']}/lib"
+        "-DCMAKE_MODULE_LINKER_FLAGS=${LDFLAGS:-}"
+        "-DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS:-}"
+        '-DCMAKE_VERBOSE_MAKEFILE=ON'
+    )
+    koopa_print "${args[@]}"
+    return 0
+}
