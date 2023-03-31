1f9fb98daad4a32905275e71ad8e00be0765ce52
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-31 07:52:39 -0400

    Update CMake build scripts

--- lang/shell/bash/include/install/common/shared/geos.sh
+++ lang/shell/bash/include/install/common/shared/geos.sh
@@ -3,64 +3,27 @@
 main() {
     # """
     # Install GEOS.
-    # @note Updated 2023-03-24.
-    #
-    # Can build with autotools or cmake.
-    # See 'INSTALL' file for details.
-    # The cmake approach seems to build more reliably inside Docker images.
-    #
-    # - cmake:
-    #   https://trac.osgeo.org/geos/wiki/BuildingOnUnixWithCMake
-    # - autotools:
-    #   https://trac.osgeo.org/geos/wiki/BuildingOnUnixWithAutotools
-    #
-    # Alternate autotools approach:
-    # > ./autogen.sh
-    # > ./configure --prefix="${dict['prefix']}"
-    # > "${app['make']}" --jobs="${dict['jobs']}"
-    # > "${app['make']}" check
+    # @note Updated 2023-03-31.
     #
     # @seealso
     # - https://github.com/libgeos/geos/blob/main/INSTALL.md
     # """
-    local app cmake_args dict
+    local cmake_args dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'cmake' 'make' 'pkg-config'
-    declare -A app=(
-        ['cmake']="$(koopa_locate_cmake)"
-        ['make']="$(koopa_locate_make)"
-    )
-    [[ -x "${app['cmake']}" ]] || return 1
-    [[ -x "${app['make']}" ]] || return 1
+    koopa_activate_app --build-only 'pkg-config'
     declare -A dict=(
-        ['jobs']="$(koopa_cpu_count)"
-        ['name']='geos'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
-    dict['file']="${dict['version']}.tar.gz"
-    dict['url']="https://github.com/lib${dict['name']}/${dict['name']}/\
-archive/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
-    koopa_mkdir 'build'
-    koopa_cd 'build'
     cmake_args=(
-        # Standard CMake arguments ---------------------------------------------
-        '-DCMAKE_BUILD_TYPE=Release'
-        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
-        "-DCMAKE_INSTALL_RPATH=${dict['prefix']}/lib"
-        '-DCMAKE_VERBOSE_MAKEFILE=ON'
-        # Build options --------------------------------------------------------
         '-DBUILD_SHARED_LIBS=ON'
         '-DGEOS_ENABLE_TESTS=OFF'
     )
-    koopa_print_env
-    koopa_dl 'CMake args' "${cmake_args[*]}"
-    "${app['cmake']}" -LH -S .. "${cmake_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    # > "${app['make']}" test
-    "${app['make']}" install
+    dict['url']="https://github.com/libgeos/geos/archive/\
+${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_cmake_build --prefix="${dict['prefix']}" "${cmake_args[@]}"
     return 0
 }
