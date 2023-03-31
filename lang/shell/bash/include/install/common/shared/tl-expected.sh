9e811eee7e3b0481a8e477374ac4b1a10eb58aae
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-31 07:32:41 -0400

    Update CMake build scripts

--- lang/shell/bash/include/install/common/shared/tl-expected.sh
+++ lang/shell/bash/include/install/common/shared/tl-expected.sh
@@ -1,50 +1,25 @@
 #!/usr/bin/env bash
 
-# FIXME This is failing to build on Ubuntu 22?
-
 main() {
     # """
     # Install TartanLlama expected (tl-expected).
-    # @note Updated 2023-03-24.
+    # @note Updated 2023-03-31.
     #
     # @seealso
     # - https://github.com/TartanLlama/expected
     # """
-    local app cmake_args dict
+    local cmake_args dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'cmake'
-    declare -A app=(
-        ['cmake']="$(koopa_locate_cmake)"
-    )
-    [[ -x "${app['cmake']}" ]] || return 1
     declare -A dict=(
-        ['jobs']="$(koopa_cpu_count)"
-        ['name']='expected'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
-    dict['file']="v${dict['version']}.tar.gz"
-    dict['url']="https://github.com/TartanLlama/${dict['name']}/archive/\
-refs/tags/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
-    cmake_args=(
-        # Standard CMake arguments ---------------------------------------------
-        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
-        '-DCMAKE_VERBOSE_MAKEFILE=ON'
-        # Build options --------------------------------------------------------
-        '-DEXPECTED_ENABLE_TESTS=OFF'
-    )
-    koopa_print_env
-    koopa_dl 'CMake args' "${cmake_args[*]}"
-    "${app['cmake']}" -LH \
-        -S . \
-        -B 'build' \
-        "${cmake_args[@]}"
-    "${app['cmake']}" \
-        --build 'build' \
-        --parallel "${dict['jobs']}"
-    "${app['cmake']}" --install 'build'
+    cmake_args=('-DEXPECTED_ENABLE_TESTS=OFF')
+    dict['url']="https://github.com/TartanLlama/expected/archive/\
+refs/tags/v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_cmake_build --prefix="${dict['prefix']}" "${cmake_args[@]}"
     return 0
 }
