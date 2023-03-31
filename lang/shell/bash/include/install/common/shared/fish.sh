fd501eff4c265f33ad901f4b231e25eab6642682
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-31 07:19:58 -0400

    Update CMake build scripts

--- lang/shell/bash/include/install/common/shared/fish.sh
+++ lang/shell/bash/include/install/common/shared/fish.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install Fish shell.
-    # @note Updated 2023-03-26.
+    # @note Updated 2023-03-31.
     #
     # @seealso
     # - https://github.com/fish-shell/fish-shell/#building
@@ -11,71 +11,53 @@ main() {
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/fish.rb
     # - https://github.com/fish-shell/fish-shell/blob/master/cmake/PCRE2.cmake
     # """
-    local app cmake_args dict
+    local cmake_args cmake_dict dict
+    declare -A cmake_dict dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'cmake' 'pkg-config'
-    koopa_activate_app 'ncurses' 'pcre2'
-    declare -A app
-    app['cmake']="$(koopa_locate_cmake)"
-    [[ -x "${app['cmake']}" ]] || return 1
-    declare -A dict=(
-        ['bin_prefix']="$(koopa_bin_prefix)"
-        ['jobs']="$(koopa_cpu_count)"
-        ['name']='fish'
-        ['ncurses']="$(koopa_app_prefix 'ncurses')"
-        ['pcre2']="$(koopa_app_prefix 'pcre2')"
-        ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
-        ['shared_ext']="$(koopa_shared_ext)"
-        ['version']="${KOOPA_INSTALL_VERSION:?}"
-    )
-    koopa_assert_is_dir \
-        "${dict['ncurses']}" \
-        "${dict['pcre2']}"
-    dict['curses_include_path']="${dict['ncurses']}/include"
-    dict['curses_library']="${dict['ncurses']}/lib/\
+    koopa_activate_app --build-only 'pkg-config'
+    koopa_activate_app 'gettext' 'ncurses' 'pcre2'
+    dict['gettext']="$(koopa_app_prefix 'gettext')"
+    dict['ncurses']="$(koopa_app_prefix 'ncurses')"
+    dict['pcre2']="$(koopa_app_prefix 'pcre2')"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['shared_ext']="$(koopa_shared_ext)"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    cmake_dict['curses_include_dirs']="${dict['ncurses']}/include"
+    cmake_dict['curses_libraries']="${dict['ncurses']}/lib/\
 libncursesw.${dict['shared_ext']}"
-    dict['sys_pcre2_include_dir']="${dict['pcre2']}/include"
-    dict['sys_pcre2_lib']="${dict['pcre2']}/lib/\
+    cmake_dict['intl_include_dir']="${dict['gettext']}/include"
+    cmake_dict['intl_libraries']="${dict['gettext']}/lib/\
+libintl.${dict['shared_ext']}"
+    cmake_dict['sys_pcre2_include_dir']="${dict['pcre2']}/include"
+    cmake_dict['sys_pcre2_lib']="${dict['pcre2']}/lib/\
 libpcre2-32.${dict['shared_ext']}"
     koopa_assert_is_dir \
-        "${dict['curses_include_path']}" \
-        "${dict['sys_pcre2_include_dir']}"
+        "${cmake_dict['curses_include_dirs']}" \
+        "${cmake_dict['intl_include_dir']}" \
+        "${cmake_dict['sys_pcre2_include_dir']}"
     koopa_assert_is_file \
-        "${dict['curses_library']}" \
-        "${dict['sys_pcre2_lib']}"
-    dict['file']="${dict['name']}-${dict['version']}.tar.xz"
-    dict['url']="https://github.com/${dict['name']}-shell/\
-${dict['name']}-shell/releases/download/${dict['version']}/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
+        "${cmake_dict['curses_libraries']}" \
+        "${cmake_dict['intl_libraries']}" \
+        "${cmake_dict['sys_pcre2_lib']}"
     cmake_args=(
-        # Standard CMake arguments ---------------------------------------------
-        '-DCMAKE_BUILD_TYPE=Release'
-        "-DCMAKE_CXX_FLAGS=${CPPFLAGS:-}"
-        "-DCMAKE_C_FLAGS=${CFLAGS:-}"
-        "-DCMAKE_EXE_LINKER_FLAGS=${LDFLAGS:-}"
-        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
-        "-DCMAKE_MODULE_LINKER_FLAGS=${LDFLAGS:-}"
-        "-DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS:-}"
-        '-DCMAKE_VERBOSE_MAKEFILE=ON'
         # Build options --------------------------------------------------------
+        '-DBUILD_DOCS=OFF'
         '-DFISH_USE_SYSTEM_PCRE2=ON'
+        '-DMAC_CODESIGN_ID=OFF'
+        '-DWITH_GETTEXT=ON'
         # Dependency paths -----------------------------------------------------
-        "-DCURSES_INCLUDE_PATH=${dict['curses_include_path']}"
-        "-DCURSES_LIBRARY=${dict['curses_library']}"
-        "-DSYS_PCRE2_INCLUDE_DIR=${dict['sys_pcre2_include_dir']}"
-        "-DSYS_PCRE2_LIB=${dict['sys_pcre2_lib']}"
+        "-DCURSES_INCLUDE_DIRS=${cmake_dict['curses_include_dirs']}"
+        "-DCURSES_LIBRARIES=${cmake_dict['curses_libraries']}"
+        "-DIntl_INCLUDE_DIR=${cmake_dict['intl_include_dir']}"
+        "-DIntl_LIBRARIES=${cmake_dict['intl_libraries']}"
+        "-DSYS_PCRE2_INCLUDE_DIR=${cmake_dict['sys_pcre2_include_dir']}"
+        "-DSYS_PCRE2_LIB=${cmake_dict['sys_pcre2_lib']}"
     )
-    koopa_print_env
-    koopa_dl 'CMake args' "${cmake_args[*]}"
-    "${app['cmake']}" -LH \
-        -S '.' \
-        -B 'build' \
-        "${cmake_args[@]}"
-    "${app['cmake']}" \
-        --build 'build' \
-        --parallel "${dict['jobs']}"
-    "${app['cmake']}" --install 'build'
+    dict['url']="https://github.com/fish-shell/fish-shell/releases/download/\
+${dict['version']}/fish-${dict['version']}.tar.xz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_cmake_build --prefix="${dict['prefix']}" "${cmake_args[@]}"
     return 0
 }
