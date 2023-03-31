93f9c2471936232bd72874895fa4dd6b0f0c6d61
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-31 07:32:41 -0400

    Update CMake build scripts

--- lang/shell/bash/include/install/common/shared/termcolor.sh
+++ lang/shell/bash/include/install/common/shared/termcolor.sh
@@ -9,37 +9,19 @@ main() {
     # - https://github.com/ikalnytskyi/termcolor
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/termcolor.rb
     # """
-    local app cmake_args dict
+    local dict
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'cmake'
-    declare -A app
-    app['cmake']="$(koopa_locate_cmake)"
-    [[ -x "${app['cmake']}" ]] || return 1
     declare -A dict=(
         ['jobs']="$(koopa_cpu_count)"
         ['name']='termcolor'
         ['prefix']="${KOOPA_INSTALL_PREFIX:?}"
         ['version']="${KOOPA_INSTALL_VERSION:?}"
     )
-    dict['file']="v${dict['version']}.tar.gz"
-    dict['url']="https://github.com/ikalnytskyi/${dict['name']}/\
-archive/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
-    cmake_args=(
-        "-DCMAKE_INSTALL_PREFIX=${dict['prefix']}"
-        '-DCMAKE_VERBOSE_MAKEFILE=ON'
-    )
-    koopa_print_env
-    koopa_dl 'CMake args' "${cmake_args[*]}"
-    "${app['cmake']}" -LH \
-        -S . \
-        -B 'build' \
-        "${cmake_args[@]}"
-    "${app['cmake']}" \
-        --build 'build' \
-        --parallel "${dict['jobs']}"
-    "${app['cmake']}" --install 'build'
+    dict['url']="https://github.com/ikalnytskyi/termcolor/archive/\
+v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_cmake_build --prefix="${dict['prefix']}"
     return 0
 }
