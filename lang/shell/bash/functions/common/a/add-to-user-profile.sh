a5f6dd3ae86e569fd221dc546d58addb1e30fc35
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-03 15:05:17 -0400

    Improve shell activation

--- lang/shell/bash/functions/common/a/add-to-user-profile.sh
+++ lang/shell/bash/functions/common/a/add-to-user-profile.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME Consider skipping koopa activation here for non-interactive shell.
-
 koopa_add_to_user_profile() {
     # """
     # Add koopa configuration to user profile.
-    # @note Updated 2023-03-13.
+    # @note Updated 2023-04-03.
     # """
     local dict
     koopa_assert_has_no_args "$#"
@@ -13,31 +11,30 @@ koopa_add_to_user_profile() {
     dict['file']="$(koopa_find_user_profile)"
     koopa_alert "Adding koopa activation to '${dict['file']}'."
     read -r -d '' "dict[string]" << END || true
-_koopa_activate_user_profile() {
+__koopa_activate_user_profile() {
     # """
     # Activate koopa shell for current user.
-    # @note Updated 2023-03-13.
-    #
-    # @seealso
+    # @note Updated 2023-04-03.
+    # @seealso 
     # - https://koopa.acidgenomics.com/
+    # - https://github.com/acidgenomics/koopa/
     # """
-    local script xdg_config_home
-    [ "\$#" -eq 0 ] || return 1
-    xdg_config_home="\${XDG_CONFIG_HOME:-}"
-    if [ -z "\$xdg_config_home" ]
+    __kvar_xdg_config_home="\${XDG_CONFIG_HOME:-}"
+    if [ -z "\$__kvar_xdg_config_home" ]
     then
-        xdg_config_home="\${HOME:?}/.config"
+        __kvar_xdg_config_home="\${HOME:?}/.config"
     fi
-    script="\${xdg_config_home}/koopa/activate"
-    if [ -r "\$script" ]
+    __kvar_script="\${__kvar_xdg_config_home}/koopa/activate"
+    if [ -r "\$__kvar_script" ]
     then
         # shellcheck source=/dev/null
-        . "\$script"
+        . "\$__kvar_script"
     fi
+    unset -v __kvar_script __kvar_xdg_config_home
     return 0
 }
 
-_koopa_activate_user_profile
+__koopa_activate_user_profile
 END
     koopa_append_string \
         --file="${dict['file']}" \
