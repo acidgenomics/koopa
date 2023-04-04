403c67135fb2d1ba76ccf38fd6acecc20443e27b
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-04 16:25:09 -0400

    Shorten the cmake dict name

--- lang/shell/bash/include/install/common/shared/fish.sh
+++ lang/shell/bash/include/install/common/shared/fish.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install Fish shell.
-    # @note Updated 2023-03-31.
+    # @note Updated 2023-04-04.
     #
     # @seealso
     # - https://github.com/fish-shell/fish-shell/#building
@@ -11,8 +11,8 @@ main() {
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/fish.rb
     # - https://github.com/fish-shell/fish-shell/blob/master/cmake/PCRE2.cmake
     # """
-    local cmake_args cmake_dict dict
-    declare -A cmake_dict dict
+    local cmake cmake_args dict
+    declare -A cmake dict
     koopa_assert_has_no_args "$#"
     koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'gettext' 'ncurses' 'pcre2'
@@ -22,23 +22,23 @@ main() {
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['shared_ext']="$(koopa_shared_ext)"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    cmake_dict['curses_include_dirs']="${dict['ncurses']}/include"
-    cmake_dict['curses_libraries']="${dict['ncurses']}/lib/\
+    cmake['curses_include_dirs']="${dict['ncurses']}/include"
+    cmake['curses_libraries']="${dict['ncurses']}/lib/\
 libncursesw.${dict['shared_ext']}"
-    cmake_dict['intl_include_dir']="${dict['gettext']}/include"
-    cmake_dict['intl_libraries']="${dict['gettext']}/lib/\
+    cmake['intl_include_dir']="${dict['gettext']}/include"
+    cmake['intl_libraries']="${dict['gettext']}/lib/\
 libintl.${dict['shared_ext']}"
-    cmake_dict['sys_pcre2_include_dir']="${dict['pcre2']}/include"
-    cmake_dict['sys_pcre2_lib']="${dict['pcre2']}/lib/\
+    cmake['sys_pcre2_include_dir']="${dict['pcre2']}/include"
+    cmake['sys_pcre2_lib']="${dict['pcre2']}/lib/\
 libpcre2-32.${dict['shared_ext']}"
     koopa_assert_is_dir \
-        "${cmake_dict['curses_include_dirs']}" \
-        "${cmake_dict['intl_include_dir']}" \
-        "${cmake_dict['sys_pcre2_include_dir']}"
+        "${cmake['curses_include_dirs']}" \
+        "${cmake['intl_include_dir']}" \
+        "${cmake['sys_pcre2_include_dir']}"
     koopa_assert_is_file \
-        "${cmake_dict['curses_libraries']}" \
-        "${cmake_dict['intl_libraries']}" \
-        "${cmake_dict['sys_pcre2_lib']}"
+        "${cmake['curses_libraries']}" \
+        "${cmake['intl_libraries']}" \
+        "${cmake['sys_pcre2_lib']}"
     cmake_args=(
         # Build options --------------------------------------------------------
         '-DBUILD_DOCS=OFF'
@@ -46,12 +46,12 @@ libpcre2-32.${dict['shared_ext']}"
         '-DMAC_CODESIGN_ID=OFF'
         '-DWITH_GETTEXT=ON'
         # Dependency paths -----------------------------------------------------
-        "-DCURSES_INCLUDE_DIRS=${cmake_dict['curses_include_dirs']}"
-        "-DCURSES_LIBRARIES=${cmake_dict['curses_libraries']}"
-        "-DIntl_INCLUDE_DIR=${cmake_dict['intl_include_dir']}"
-        "-DIntl_LIBRARIES=${cmake_dict['intl_libraries']}"
-        "-DSYS_PCRE2_INCLUDE_DIR=${cmake_dict['sys_pcre2_include_dir']}"
-        "-DSYS_PCRE2_LIB=${cmake_dict['sys_pcre2_lib']}"
+        "-DCURSES_INCLUDE_DIRS=${cmake['curses_include_dirs']}"
+        "-DCURSES_LIBRARIES=${cmake['curses_libraries']}"
+        "-DIntl_INCLUDE_DIR=${cmake['intl_include_dir']}"
+        "-DIntl_LIBRARIES=${cmake['intl_libraries']}"
+        "-DSYS_PCRE2_INCLUDE_DIR=${cmake['sys_pcre2_include_dir']}"
+        "-DSYS_PCRE2_LIB=${cmake['sys_pcre2_lib']}"
     )
     dict['url']="https://github.com/fish-shell/fish-shell/releases/download/\
 ${dict['version']}/fish-${dict['version']}.tar.xz"
