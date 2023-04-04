6cc88902b280ec99ad05e1d9bb15cef7c5c6c5ad
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-04 14:05:47 -0400

    Improve consistency of R config

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -4668,10 +4668,10 @@ koopa_configure_r() {
                     "${dict['user']}:${dict['group']}" \
                     "${dict['site_library_2']}"
             fi
-            koopa_r_configure_makeconf "${app['r']}"
-            koopa_r_rebuild_docs "${app['r']}"
             ;;
     esac
+    koopa_r_configure_makeconf "${app['r']}"
+    koopa_r_migrate_non_base_packages "${app['r']}"
     koopa_alert_configure_success "${dict['name']}" "${app['r']}"
     if [[ "${dict['system']}" -eq 1 ]] && koopa_is_linux
     then
@@ -18796,53 +18796,6 @@ koopa_r_prefix() {
     )"
     koopa_assert_is_dir "${dict['prefix']}"
     koopa_print "${dict['prefix']}"
-    return 0
-}
-
-koopa_r_rebuild_docs() {
-    local app doc_dir html_dir pkg_index rscript_args
-    declare -A app dict
-    app['r']="${1:?}"
-    app['rscript']="${app['r']}script"
-    [[ -x "${app['r']}" ]] || return 1
-    [[ -x "${app['rscript']}" ]] || return 1
-    dict['system']=0
-    ! koopa_is_koopa_app "${app['r']}" && dict['system']=1
-    rscript_args=('--vanilla')
-    dict['doc_dir']="$( \
-        "${app['rscript']}" "${rscript_args[@]}" -e 'cat(R.home("doc"))' \
-    )"
-    koopa_assert_is_dir "${dict['doc_dir']}"
-    koopa_alert_info "Updating documentation in '${dict['doc_dir']}'."
-    case "${dict['system']}" in
-        '1')
-            app['sudo']="$(koopa_locate_sudo)"
-            [[ -x "${app['sudo']}" ]] || return 1
-            "${app['sudo']}" "${app['rscript']}" \
-                "${rscript_args[@]}" \
-                -e 'utils::make.packages.html()'
-            ;;
-        '0')
-            dict['html_dir']="${dict['doc_dir']}/html"
-            dict['pkg_index']="${dict['html_dir']}/packages.html"
-            dict['r_css']="${dict['html_dir']}/R.css"
-            if [[ ! -d "${dict['html_dir']}" ]]
-            then
-                koopa_mkdir "${dict['html_dir']}"
-            fi
-            if [[ ! -f "${dict['pkg_index']}" ]]
-            then
-                koopa_touch "${dict['pkg_index']}"
-            fi
-            if [[ ! -f "${dict['r_css']}" ]]
-            then
-                koopa_touch "${dict['r_css']}"
-            fi
-            "${app['rscript']}" \
-                "${rscript_args[@]}" \
-                -e 'utils::make.packages.html()'
-            ;;
-    esac
     return 0
 }
 
