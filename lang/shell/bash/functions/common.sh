53bbfaf2e19671de4acea94246d1b94308c9d63f
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-04 14:07:05 -0400

    Improve consistency of R config

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -18269,14 +18269,12 @@ koopa_r_configure_makeconf() {
 koopa_r_configure_makevars() {
     local app conf_dict dict
     local cppflags ldflags lines
+    declare -A app dict
     koopa_assert_has_args_eq "$#" 1
-    declare -A app
     app['r']="${1:?}"
     [[ -x "${app['r']}" ]] || return 1
-    declare -A dict=(
-        ['system']=0
-        ['use_apps']=1
-    )
+    dict['system']=0
+    dict['use_apps']=1
     ! koopa_is_koopa_app "${app['r']}" && dict['system']=1
     if [[ "${dict['system']}" -eq 1 ]] && \
         koopa_is_linux && \
