d017d4f7dec7a5ad74b44833b661fb86ca126a34
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-04 13:54:22 -0400

    Add utility to remove from system library

--- lang/shell/bash/functions/common/r/r-remove-packages-in-system-library.sh
+++ lang/shell/bash/functions/common/r/r-remove-packages-in-system-library.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
 koopa_r_remove_packages_in_system_library() {
-    koopa_stop 'FIXME'
+    # """
+    # Install packages into R site library.
+    # @note Updated 2023-04-04.
+    # """
+    local app dict
+    declare -A app dict
+    koopa_assert_has_args_ge "$#" 2
+    app['r']="${1:?}"
+    app['rscript']="${app['r']}script"
+    [[ -x "${app['r']}" ]] || return 1
+    [[ -x "${app['rscript']}" ]] || return 1
+    shift 1
+    dict['system']=0
+    ! koopa_is_koopa_app "${app['r']}" && dict['system']=1
+    dict['script']="$(koopa_koopa_prefix)/lang/r/\
+remove-packages-in-system-library.R"
+    koopa_assert_is_file "${dict['script']}"
+    case "${dict['system']}" in
+        '0')
+            "${app['rscript']}" "${dict['script']}" "$@"
+            ;;
+        '1')
+            app['sudo']="$(koopa_locate_sudo)"
+            [[ -x "${app['sudo']}" ]] || return 1
+            "${app['sudo']}" "${app['rscript']}" "${dict['script']}" "$@"
+            ;;
+    esac
+    return 0
 }
