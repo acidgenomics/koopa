a74fb485c4ae6cf5ade9c5cbad71d7331839d554
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-04 14:45:20 -0400

    Attempt to add build support of other ONT basecallers

--- lang/shell/bash/include/install/common/shared/ont-dorado.sh
+++ lang/shell/bash/include/install/common/shared/ont-dorado.sh
@@ -5,17 +5,30 @@ main() {
     # Install ONT dorado basecaller.
     # @note Updated 2023-04-04.
     # """
-    local deps dict
+    local build_deps deps dict
     declare -A dict
     koopa_assert_has_no_args "$#"
+    build_deps=('autoconf' 'automake' 'git')
     deps=('hdf5' 'openssl3' 'zstd')
+    koopa_activate_app --build-only "${build_deps[@]}"
     koopa_activate_app "${deps[@]}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['openssl']="$(koopa_app_prefix 'openssl3')"
+    cmake_args=(
+        # Build options --------------------------------------------------------
+        '-DBUILD_KOI_FROM_SOURCE=OFF'
+        '-DGIT_SUBMODULE=ON'
+        # Dependency paths -----------------------------------------------------
+        # > "-DCUDAToolkit_NVCC_EXECUTABLE=FIXME"
+        # > "-DCUDAToolkit_SENTINEL_FILE=FIXME"
+        "-DOPENSSL_ROOT_DIR=${dict['openssl']}"
+    )
     dict['url']="https://github.com/nanoporetech/dorado/archive/refs/tags/\
 v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
-    koopa_cmake_build --prefix="${dict['prefix']}"
+    koopa_cmake_build --prefix="${dict['prefix']}" "${cmake_args[@]}"
+    return 0
 }
