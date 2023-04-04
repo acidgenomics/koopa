d77e44a1422ab19a3251526a48deedbec3b0fc61
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-04 13:46:42 -0400

    Draft updates to R utility functions

---
+++ lang/shell/bash/functions/common/r/r-install-packages-in-site-library.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+koopa_r_install_packages_in_site_library() {
+    # """
+    # Install packages into R site library.
+    # @note Updated 2023-04-04.
+    # """
+    local app dict
+    declare -A app dict
+    koopa_assert_has_args_gt "$#" 1
+    app['r']="${1:?}"
+    app['rscript']="${app['r']}script"
+    [[ -x "${app['r']}" ]] || return 1
+    [[ -x "${app['rscript']}" ]] || return 1
+    shift 1
+    dict['script']="$(koopa_koopa_prefix)/lang/r/\
+install-packages-in-site-library.R"
+    koopa_assert_is_file "${dict['script']}"
+    "${app['rscript']}" "${dict['script']}" "$@"
+    return 0
+}
