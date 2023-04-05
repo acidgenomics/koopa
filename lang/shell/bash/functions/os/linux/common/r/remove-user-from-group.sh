5b1b749a59772b748baef9fdce571c7e5a178ee0
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-05 17:07:34 -0400

    Improve array consistency

--- lang/shell/bash/functions/os/linux/common/r/remove-user-from-group.sh
+++ lang/shell/bash/functions/os/linux/common/r/remove-user-from-group.sh
@@ -3,24 +3,19 @@
 koopa_linux_remove_user_from_group() {
     # """
     # Remove user from group.
-    # @note Updated 2021-11-16.
+    # @note Updated 2023-04-05.
     #
     # @examples
     # > koopa_linux_remove_user_from_group 'docker'
     # """
-    local app dict
+    local -A app dict
     koopa_assert_has_args_le "$#" 2
     koopa_assert_is_admin
-    local -A app=(
-        ['gpasswd']="$(koopa_linux_locate_gpasswd)"
-        ['sudo']="$(koopa_locate_sudo)"
-    )
-    [[ -x "${app['gpasswd']}" ]] || exit 1
-    [[ -x "${app['sudo']}" ]] || exit 1
-    local -A dict=(
-        ['group']="${1:?}"
-        ['user']="${2:-}"
-    )
+    app['gpasswd']="$(koopa_linux_locate_gpasswd)"
+    app['sudo']="$(koopa_locate_sudo)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['group']="${1:?}"
+    dict['user']="${2:-}"
     [[ -z "${dict['user']}" ]] && dict['user']="$(koopa_user_name)"
     "${app['sudo']}" "${app['gpasswd']}" \
         --delete "${dict['user']}" "${dict['group']}"
