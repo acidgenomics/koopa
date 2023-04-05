6cddaf4de1d3a1ecf8cc31c1d97d7373f2428b30
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-05 17:20:47 -0400

    Improve array consistency

--- lang/shell/bash/functions/os/linux/debian/a/apt-space-used-by-grep.sh
+++ lang/shell/bash/functions/os/linux/debian/a/apt-space-used-by-grep.sh
@@ -3,30 +3,27 @@
 koopa_debian_apt_space_used_by_grep() {
     # """
     # Check installed apt package size, with dependencies.
-    # @note Updated 2022-01-10.
+    # @note Updated 2023-04-05.
     #
     # See also:
     # https://askubuntu.com/questions/490945
     # """
-    local app x
+    local -A app
+    local str
     koopa_assert_has_args "$#"
     koopa_assert_is_admin
-    local -A app=(
-        ['apt_get']="$(koopa_debian_locate_apt_get)"
-        ['cut']="$(koopa_locate_cut --allow-system)"
-        ['sudo']="$(koopa_locate_sudo --allow-system)"
-    )
-    [[ -x "${app['apt_get']}" ]] || exit 1
-    [[ -x "${app['cut']}" ]] || exit 1
-    [[ -x "${app['sudo']}" ]] || exit 1
-    x="$( \
+    app['apt_get']="$(koopa_debian_locate_apt_get)"
+    app['cut']="$(koopa_locate_cut --allow-system)"
+    app['sudo']="$(koopa_locate_sudo --allow-system)"
+    koopa_assert_is_executable "${app[@]}"
+    str="$( \
         "${app['sudo']}" "${app['apt_get']}" \
             --assume-no \
             autoremove "$@" \
         | koopa_grep --pattern='freed' \
         | "${app['cut']}" -d ' ' -f '4-5' \
     )"
-    [[ -n "$x" ]] || return 1
-    koopa_print "$x"
+    [[ -n "$str" ]] || return 1
+    koopa_print "$str"
     return 0
 }
