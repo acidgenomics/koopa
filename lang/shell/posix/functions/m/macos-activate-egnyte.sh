ea223a352ac3089077ca9e7c57f6d0012c6d6028
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-07 17:02:06 -0400

    Ensure egnyte cli is accessible

---
+++ lang/shell/posix/functions/m/macos-activate-egnyte.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+_koopa_macos_activate_egnyte() {
+    # """
+    # Activate macOS Egnyte CLI.
+    # @note Updated 2023-04-07.
+    # """
+    _koopa_add_to_path_end "${HOME}/Library/Group Containers/\
+FELUD555VC.group.com.egnyte.DesktopApp/CLI"
+    return 0
+}
