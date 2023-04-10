16a2beda10ccf306e0ac716776e8f7122049565c
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 09:13:08 -0400

    Need to link openssl3

--- lang/shell/bash/include/install/common/shared/nushell.sh
+++ lang/shell/bash/include/install/common/shared/nushell.sh
@@ -9,7 +9,10 @@ main() {
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/nushell.rb
     # - https://crates.io/crates/nu
     # """
+    local -A dict
     koopa_activate_app 'zlib' 'openssl3'
+    dict['openssl']="$(koopa_app_prefix 'openssl3')"
+    export OPENSSL_DIR="${dict['openssl']}"
     koopa_install_app_subshell \
         --installer='rust-package' \
         --name='nushell' \
