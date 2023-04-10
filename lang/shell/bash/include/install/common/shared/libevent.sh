7550df066a6035b48888edc6d0375e418f7fa9f6
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 10:57:57 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/libevent.sh
+++ lang/shell/bash/include/install/common/shared/libevent.sh
@@ -3,39 +3,28 @@
 main() {
     # """
     # Install libevent.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/
     #       Formula/libevent.rb
     # """
-    local -A app dict
+    local -A dict
     local -a conf_args
-    koopa_activate_app --build-only 'make' 'pkg-config'
+    koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'openssl3'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    dict['name']='libevent'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="${dict['name']}-${dict['version']}-stable.tar.gz"
-    dict['url']="https://github.com/${dict['name']}/${dict['name']}/releases/\
-download/release-${dict['version']}-stable/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}-stable"
     conf_args=(
-        "--prefix=${dict['prefix']}"
         '--disable-debug-mode'
         '--disable-dependency-tracking'
+        "--prefix=${dict['prefix']}"
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    # > "${app['make']}" check
-    "${app['make']}" install
+    dict['url']="https://github.com/libevent/libevent/releases/download/\
+release-${dict['version']}-stable/libevent-${dict['version']}-stable.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
