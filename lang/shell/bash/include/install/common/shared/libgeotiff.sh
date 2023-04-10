94cc0a9d9838ec27eed609ba7490d1a0d3b5e90f
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 11:00:14 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/libgeotiff.sh
+++ lang/shell/bash/include/install/common/shared/libgeotiff.sh
@@ -3,17 +3,17 @@
 main() {
     # """
     # Install libgeotiff.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://github.com/OSGeo/libgeotiff
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/
     #     libgeotiff.rb
     # """
-    local -A app dict
+    local -A dict
+    local -a conf_args
     koopa_activate_app --build-only \
         'libtool' \
-        'make' \
         'pkg-config'
     koopa_activate_app \
         'curl' \
@@ -23,28 +23,18 @@ main() {
         'libtiff' \
         'sqlite' \
         'proj'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    dict['name']='libgeotiff'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="${dict['name']}-${dict['version']}.tar.gz"
-    dict['url']="https://github.com/OSGeo/${dict['name']}/releases/download/\
-${dict['version']}/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
     conf_args=(
         '--disable-dependency-tracking'
         "--prefix=${dict['prefix']}"
         '--with-jpeg'
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="https://github.com/OSGeo/libgeotiff/releases/download/\
+${dict['version']}/libgeotiff-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
