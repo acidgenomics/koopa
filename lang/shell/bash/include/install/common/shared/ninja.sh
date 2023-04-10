264ac8075d554adcee20762dab74d4b759d26dce
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 11:57:14 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/ninja.sh
+++ lang/shell/bash/include/install/common/shared/ninja.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install ninja.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://github.com/ninja-build/ninja
@@ -16,12 +16,11 @@ main() {
     dict['name']='ninja'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="v${dict['version']}.tar.gz"
     dict['url']="https://github.com/ninja-build/ninja/archive/refs/\
-tags/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
+tags/v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
     "${app['python']}" ./configure.py --bootstrap
     koopa_cp --target-directory="${dict['prefix']}/bin" 'ninja'
     return 0
