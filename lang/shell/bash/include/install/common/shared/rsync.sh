a7812161d5523b66d2bac1b25f8a27875155f454
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 13:04:43 -0400

    Improve build consistency with conda-forge

--- lang/shell/bash/include/install/common/shared/rsync.sh
+++ lang/shell/bash/include/install/common/shared/rsync.sh
@@ -13,9 +13,8 @@ main() {
     # - https://download.samba.org/pub/rsync/NEWS
     # - https://bugs.gentoo.org/729186
     # """
-    local -A app dict
-    local -a deps
-    koopa_activate_app --build-only 'make'
+    local -A dict
+    local -a conf_args deps
     deps=(
         'zlib'
         'zstd'
@@ -24,23 +23,15 @@ main() {
         'xxhash'
     )
     koopa_activate_app  "${deps[@]}"
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['url']="https://download.samba.org/pub/rsync/src/\
-rsync-${dict['version']}.tar.gz"
-    koopa_download "${dict['url']}"
-    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
-    koopa_cd 'src'
     conf_args=(
-        "--prefix=${dict['prefix']}"
         '--disable-debug'
         '--enable-ipv6'
         '--enable-lz4'
         '--enable-openssl'
         '--enable-xxhash'
+        "--prefix=${dict['prefix']}"
         '--with-included-popt=no'
         '--with-included-zlib=no'
     )
@@ -50,12 +41,11 @@ rsync-${dict['version']}.tar.gz"
     else
         conf_args+=('--enable-zstd')
     fi
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    # > ./prepare-source
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="https://download.samba.org/pub/rsync/src/\
+rsync-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
