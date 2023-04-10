3240c495bd1cd5be06ec4940e1498669d515e449
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 10:19:06 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/isl.sh
+++ lang/shell/bash/include/install/common/shared/isl.sh
@@ -3,41 +3,31 @@
 main() {
     # """
     # Install isl.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://libisl.sourceforge.io/
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/isl.rb
     # """
-    local -A app dict
+    local -A dict
     local -a conf_args
     koopa_assert_has_no_args "$#"
-    koopa_activate_app --build-only 'make' 'pkg-config'
+    koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'gmp'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
     dict['gmp']="$(koopa_app_prefix 'gmp')"
-    dict['jobs']="$(koopa_cpu_count)"
-    dict['name']="${KOOPA_INSTALL_NAME:?}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="${dict['name']}-${dict['version']}.tar.xz"
-    dict['url']="https://libisl.sourceforge.io/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
     conf_args=(
-        "--prefix=${dict['prefix']}"
         '--disable-dependency-tracking'
         '--disable-silent-rules'
+        "--prefix=${dict['prefix']}"
         '--with-gmp=system'
         "--with-gmp-prefix=${dict['gmp']}"
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="https://libisl.sourceforge.io/isl-${dict['version']}.tar.xz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
