2664e152c39f264c0666cdd83edb6a36c5b442be
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 11:22:28 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/libtiff.sh
+++ lang/shell/bash/include/install/common/shared/libtiff.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# Switch to libjpeg-turbo?
-
 main() {
     # """
     # Install libtiff.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/libtiff.rb
@@ -14,34 +12,26 @@ main() {
     # - https://www.linuxfromscratch.org/blfs/view/svn/general/libtiff.html
     # - https://github.com/opentoonz/opentoonz/issues/1566
     # """
-    local -A app dict
+    local -A dict
     local -a conf_args
-    koopa_activate_app --build-only 'make' 'pkg-config'
+    koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'libjpeg-turbo' 'zstd'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="tiff-${dict['version']}.tar.gz"
-    dict['url']="http://download.osgeo.org/libtiff/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "tiff-${dict['version']}"
     conf_args=(
-        "--prefix=${dict['prefix']}"
         '--disable-dependency-tracking'
         '--disable-lzma'
         '--disable-webp'
         '--enable-shared=yes'
         '--enable-static=yes'
+        "--prefix=${dict['prefix']}"
         '--without-x'
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="http://download.osgeo.org/libtiff/\
+tiff-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
--- lang/shell/bash/include/install/common/shared/libuv.sh
+++ lang/shell/bash/include/install/common/shared/libuv.sh
@@ -5,44 +5,33 @@
 main() {
     # """
     # Install libuv.
-    # @note Updated 2023-06-07.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/libuv.rb
     # - https://cran.r-project.org/web/packages/httpuv/index.html
     # """
-    local -A app dict
+    local -A dict
+    local -a conf_args
     koopa_activate_app --build-only \
         'autoconf' \
         'automake' \
         'libtool' \
         'm4' \
-        'make' \
         'pkg-config'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    dict['name']='libuv'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="v${dict['version']}.tar.gz"
-    dict['url']="https://github.com/${dict['name']}/${dict['name']}/\
-archive/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
     conf_args=(
-        "--prefix=${dict['prefix']}"
         '--disable-dependency-tracking'
         '--disable-silent-rules'
+        "--prefix=${dict['prefix']}"
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    # This tries to locate 'glibtoolize'.
+    dict['url']="https://github.com/libuv/libuv/archive/\
+v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
     ./autogen.sh
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
