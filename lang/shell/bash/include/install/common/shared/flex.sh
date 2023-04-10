85f85602e254567a75c0c4a64ee36d0227ac530f
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 09:26:59 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/flex.sh
+++ lang/shell/bash/include/install/common/shared/flex.sh
@@ -5,39 +5,29 @@
 main() {
     # """
     # Install flex.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://github.com/westes/flex/
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/flex.rb
     # """
-    local -A app dict
+    local -A dict
     local -a conf_args
-    koopa_activate_app --build-only 'bison' 'make'
+    koopa_activate_app --build-only 'bison'
     koopa_activate_app 'gettext' 'm4'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    dict['name']='flex'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="flex-2.6.4.tar.gz"
-    dict['url']="https://github.com/westes/${dict['name']}/releases/download/\
-v${dict['version']}/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
     conf_args=(
-        "--prefix=${dict['prefix']}"
         '--disable-dependency-tracking'
         '--disable-silent-rules'
         '--enable-shared'
+        "--prefix=${dict['prefix']}"
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="https://github.com/westes/flex/releases/download/\
+v${dict['version']}/flex-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
