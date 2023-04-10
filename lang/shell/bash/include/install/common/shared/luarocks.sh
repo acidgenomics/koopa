bee4e7af4bf4193ca92c2eddb576a895b9cc368e
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 11:36:04 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/luarocks.sh
+++ lang/shell/bash/include/install/common/shared/luarocks.sh
@@ -3,36 +3,29 @@
 main() {
     # """
     # Install Luarocks.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     # """
     local -A app dict
     local -a conf_args
-    koopa_activate_app --build-only 'make' 'unzip'
+    koopa_activate_app --build-only 'unzip'
     koopa_activate_app 'lua'
     app['lua']="$(koopa_locate_lua)"
-    app['make']="$(koopa_locate_make)"
     koopa_assert_is_executable "${app[@]}"
-    dict['name']='luarocks'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['lua_version']="$(koopa_get_version "${app['lua']}")"
     dict['lua_maj_min_ver']="$( \
         koopa_major_minor_version "${dict['lua_version']}" \
     )"
-    dict['file']="${dict['name']}-${dict['version']}.tar.gz"
-    dict['url']="https://luarocks.org/releases/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
     conf_args=(
-        "--prefix=${dict['prefix']}"
         "--lua-version=${dict['lua_maj_min_ver']}"
+        "--prefix=${dict['prefix']}"
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" build
-    "${app['make']}" install
+    dict['url']="https://luarocks.org/releases/\
+luarocks-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
