52d60f342707b66f45b01321cd70bc62936b5696
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 11:57:14 -0400

    Improve consistency of GNU make builds

--- lang/shell/bash/include/install/common/shared/nano.sh
+++ lang/shell/bash/include/install/common/shared/nano.sh
@@ -1,34 +1,23 @@
 #!/usr/bin/env bash
 
+# NOTE Consider requiring 'libmagic' on Linux.
+
 main() {
     # """
     # Install nano.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-10.
     #
     # @seealso
     # - https://www.nano-editor.org/
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/nano.rb
     # """
-    local -A app dict
+    local -A dict
     local -a conf_args
-    koopa_activate_app --build-only 'make' 'pkg-config'
-    # NOTE Consider requiring 'libmagic' on Linux.
-    koopa_activate_app \
-        'gettext' \
-        'ncurses'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    dict['name']='nano'
+    koopa_activate_app --build-only 'pkg-config'
+    koopa_activate_app 'gettext' 'ncurses'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['maj_ver']="$(koopa_major_version "${dict['version']}")"
-    dict['file']="${dict['name']}-${dict['version']}.tar.xz"
-    dict['url']="https://www.nano-editor.org/dist/\
-v${dict['maj_ver']}/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
     conf_args=(
         '--disable-debug'
         '--disable-dependency-tracking'
@@ -39,11 +28,11 @@ v${dict['maj_ver']}/${dict['file']}"
         '--enable-utf8'
         "--prefix=${dict['prefix']}"
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="https://www.nano-editor.org/dist/v${dict['maj_ver']}/\
+nano-${dict['version']}.tar.xz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
--- lang/shell/bash/include/install/common/shared/nmap.sh
+++ lang/shell/bash/include/install/common/shared/nmap.sh
@@ -3,10 +3,7 @@
 main() {
     # """
     # Install nmap.
-    # @note Updated 2023-04-06.
-    #
-    # Attempting to bundle zlib fails on Ubuntu.
-    # Attempting to bundle pcre fails on macOS.
+    # @note Updated 2023-04-10.
     #
     # May need to include libcap and liblinear here.
     #
@@ -18,63 +15,32 @@ main() {
     # - Check supported Lua version at:
     #   https://github.com/nmap/nmap/tree/master/liblua
     # """
-    local -A app dict 
+    local -A dict
     local -a conf_args deps
-    koopa_activate_app --build-only \
-        'bison' \
-        'flex' \
-        'make'
-    deps=(
-        'zlib'
-        'openssl3'
-        # > 'libssh2'
-        'pcre'
-        # > 'lua'
-    )
+    koopa_activate_app --build-only 'bison' 'flex'
+    deps=('zlib' 'openssl3' 'pcre')
     koopa_activate_app "${deps[@]}"
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    # > dict['libssh2']="$(koopa_app_prefix 'libssh2')"
-    # > dict['lua']="$(koopa_app_prefix 'lua')"
-    dict['name']='nmap'
     dict['openssl']="$(koopa_app_prefix 'openssl3')"
     dict['pcre']="$(koopa_app_prefix 'pcre')"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['zlib']="$(koopa_app_prefix 'zlib')"
-    dict['file']="${dict['name']}-${dict['version']}.tar.bz2"
-    dict['url']="https://nmap.org/dist/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
     conf_args=(
-        # > '--disable-universal'
         "--prefix=${dict['prefix']}"
         '--with-libdnet=included'
         '--with-liblinear=included'
-        # > "--with-liblua=${dict['lua']}"
         '--with-liblua=included'
-        # NOTE May only want to link PCRE on macOS.
-        # > '--with-libpcap=included'
         "--with-libpcre=${dict['pcre']}"
         '--with-libpcre=included'
-        # > "--with-libssh2=${dict['libssh2']}"
         '--with-libssh2=included'
         "--with-libz=${dict['zlib']}"
-        # > '--with-libz=included'
         "--with-openssl=${dict['openssl']}"
-        # > '--without-ncat'
-        # > '--without-ndiff'
-        # > '--without-nmap-update'
-        # > '--without-nping'
         '--without-zenmap'
     )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    dict['url']="https://nmap.org/dist/nmap-${dict['version']}.tar.bz2"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
