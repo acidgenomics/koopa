4cb73a69df9d6a4a5b0b795ab4fe911c5db37c51
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-10 09:14:33 -0400

    Need to link openssl3

--- lang/shell/bash/include/install/common/shared/nushell.sh
+++ lang/shell/bash/include/install/common/shared/nushell.sh
@@ -4,12 +4,16 @@
 
 main() {
     # """
+    # Install nushell.
+    # @note Updated 2023-04-10.
+    #
     # @seealso
     # - https://www.nushell.sh/book/installation.html#build-from-source
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/nushell.rb
     # - https://crates.io/crates/nu
     # """
     local -A dict
+    koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'zlib' 'openssl3'
     dict['openssl']="$(koopa_app_prefix 'openssl3')"
     export OPENSSL_DIR="${dict['openssl']}"
