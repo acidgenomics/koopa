1af03f37448b50617c32431a626404108bfdf7cf
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-11 09:53:59 -0400

    Improve GNU make build consistency

--- lang/shell/bash/include/install/common/shared/xorg-libpthread-stubs.sh
+++ lang/shell/bash/include/install/common/shared/xorg-libpthread-stubs.sh
@@ -3,34 +3,23 @@
 main() {
     # """
     # Install libpthread-stubs.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-11.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/
     #     Formula/libpthread-stubs.rb
     # """
-    local -A app dict
+    local -A dict
     local -a conf_args
-    koopa_activate_app --build-only 'make' 'pkg-config'
-    app['make']="$(koopa_locate_make)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    dict['name']='libpthread-stubs'
+    koopa_activate_app --build-only 'pkg-config'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="${dict['name']}-${dict['version']}.tar.bz2"
-    dict['url']="https://xcb.freedesktop.org/dist/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
-    conf_args=(
-        "--prefix=${dict['prefix']}"
-    )
-    koopa_print_env
-    koopa_dl 'configure args' "${conf_args[*]}"
-    ./configure --help
-    ./configure "${conf_args[@]}"
-    "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    conf_args=("--prefix=${dict['prefix']}")
+    dict['url']="https://xcb.freedesktop.org/dist/\
+libpthread-stubs-${dict['version']}.tar.bz2"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
     return 0
 }
