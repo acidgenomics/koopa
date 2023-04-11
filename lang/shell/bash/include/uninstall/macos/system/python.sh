f711f03b1bbf3056a84c6973eec8f36e28b092f6
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-11 16:50:59 -0400

    Improve system installer cleanup

--- lang/shell/bash/include/uninstall/macos/system/python.sh
+++ lang/shell/bash/include/uninstall/macos/system/python.sh
@@ -5,10 +5,27 @@ main() {
     # Uninstall Python framework binary.
     # @note Updated 2022-04-11.
     # """
+    local -a files
+    [[ -d '/Library/Frameworks/Python.framework' ]] || return 0
+    files=(
+        '/Library/Frameworks/Python.framework'
+        '/usr/local/bin/2to3'
+        '/usr/local/bin/idle3'
+        '/usr/local/bin/pip3'
+        '/usr/local/bin/pydoc3'
+        '/usr/local/bin/python'
+        '/usr/local/bin/python3'
+        '/usr/local/bin/python3-config'
+    )
+    koopa_rm --sudo "${files[@]}"
     koopa_rm --sudo \
         '/Applications/Python'* \
-        '/Library/Frameworks/Python.framework' \
+        '/usr/local/bin/2to3-'* \
+        '/usr/local/bin/idle3.'* \
+        '/usr/local/bin/pip3.'* \
+        '/usr/local/bin/pydoc3.'* \
+        '/usr/local/bin/python3-'* \
+        '/usr/local/bin/python3.'* \
         '/usr/local/lib/python'*
-    koopa_delete_broken_symlinks '/usr/local/bin'
     return 0
 }
