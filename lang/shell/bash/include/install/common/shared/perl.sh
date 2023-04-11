e87caf96fa6360661e7baa2541b5397db6a63334
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-11 07:59:43 -0400

    Update Perl installer

--- lang/shell/bash/include/install/common/shared/perl.sh
+++ lang/shell/bash/include/install/common/shared/perl.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# NOTE Consider including 'expat', 'gdbm', and possibly 'zlib' here?
+# NOTE Consider including 'expat', 'gdbm', and possibly 'zlib' here.
 # FIXME This is no longer building Perl documentation files.
 
 main() {
     # """
     # Install Perl.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-11.
     #
     # @section Regarding parallel build failures on Ubunutu:
     # make can error at this step when running in parallel.
@@ -26,24 +26,19 @@ main() {
     app['make']="$(koopa_locate_make)"
     koopa_assert_is_executable "${app[@]}"
     dict['jobs']="$(koopa_cpu_count)"
-    dict['name']='perl'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    # Deparallelize on Linux to avoid error at "Updating 'mktables.lst'".
     koopa_is_linux && dict['jobs']=1
-    dict['file']="${dict['name']}-${dict['version']}.tar.gz"
     # All Perl 5 releases are currently organized under '5.0'.
     dict['src_maj_min_ver']="$(koopa_major_version "${dict['version']}").0"
     dict['url']="https://www.cpan.org/src/${dict['src_maj_min_ver']}/\
-${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
+perl-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
     koopa_print_env
     ./Configure -des -Dprefix="${dict['prefix']}"
     "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    # The installer will warn when you skip this step.
-    # > "${app['make']}" test
     "${app['make']}" install
     return 0
 }
