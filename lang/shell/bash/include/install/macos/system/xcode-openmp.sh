0587135c1ceadf4c5359d6fc901a1936ef7ac536
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-11 17:08:50 -0400

    Draft app updates

---
+++ lang/shell/bash/include/install/macos/system/xcode-openmp.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+main() {
+    # """
+    # Install OpenMP for Xcode library.
+    # @note Updated 2023-04-11.
+    #
+    # Useful for optimizing performance of 'data.table' package.
+    #
+    # @seealso
+    # - https://mac.r-project.org/openmp/
+    # - https://github.com/Rdatatable/data.table/wiki/Installation
+    # """
+    local -A app dict
+    app['sudo']="$(koopa_locate_sudo)"
+    app['tar']="$(koopa_locate_tar --allow-system)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['platform']='darwin'
+    dict['release']='Release' # or 'Debug'.
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    case "${dict['version']}" in
+        '13.0.0')
+            dict['platform_version']='21'
+            ;;
+        '14.0.6' | \
+        '12.0.1' | \
+        '11.0.1')
+            dict['platform_version']='20'
+            ;;
+        '10.0.0' | \
+        '9.0.1' | \
+        '8.0.1' | \
+        '7.1.0')
+            dict['platform_version']='17'
+            ;;
+        *)
+            koopa_stop "Unsupported version: '${dict['version']}'."
+            ;;
+    esac
+    dict['url']="https://mac.r-project.org/openmp/openmp-${dict['version']}-\
+${dict['platform']}${dict['platform_version']}-${dict['release']}.tar.gz"
+    koopa_download "${dict['url']}"
+    "${app['sudo']}" "${app['tar']}" \
+        -vxz \
+        -f "$(koopa_basename "${dict['url']}")" \
+        -C '/'
+    koopa_assert_is_file \
+        '/usr/local/include/omp-tools.h' \
+        '/usr/local/include/omp.h' \
+        '/usr/local/include/ompt.h' \
+        '/usr/local/lib/libomp.dylib'
+    return 0
+}
+
