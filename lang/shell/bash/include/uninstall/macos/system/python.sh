45c53bb92b246ce13f98716619984e2856a5c7ad
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-11 16:35:28 -0400

    Remove more stuff

--- lang/shell/bash/include/uninstall/macos/system/python.sh
+++ lang/shell/bash/include/uninstall/macos/system/python.sh
@@ -3,11 +3,12 @@
 main() {
     # """
     # Uninstall Python framework binary.
-    # @note Updated 2022-03-30.
+    # @note Updated 2022-04-11.
     # """
     koopa_rm --sudo \
         '/Applications/Python'* \
-        '/Library/Frameworks/Python.framework'
+        '/Library/Frameworks/Python.framework' \
+        '/usr/local/lib/python'*
     koopa_delete_broken_symlinks '/usr/local/bin'
     return 0
 }
