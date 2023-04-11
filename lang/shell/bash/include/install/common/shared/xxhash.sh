fbc0d2fe1428caab2c6efe590648d414addba8ee
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-11 09:42:58 -0400

    Disable static builds where possible

--- lang/shell/bash/include/install/common/shared/xxhash.sh
+++ lang/shell/bash/include/install/common/shared/xxhash.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install xxhash.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-04-11.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/xxhash.rb
@@ -12,16 +12,15 @@ main() {
     koopa_activate_app --build-only 'make' 'pkg-config'
     app['make']="$(koopa_locate_make)"
     koopa_assert_is_executable "${app[@]}"
-    dict['name']='xxHash'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['file']="v${dict['version']}.tar.gz"
-    dict['url']="https://github.com/Cyan4973/${dict['name']}/\
-archive/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
+    dict['url']="https://github.com/Cyan4973/xxHash/\
+archive/v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
     koopa_print_env
     "${app['make']}" install PREFIX="${dict['prefix']}"
+    koopa_rm "${dict['prefix']}/lib/"*'.a'
     return 0
 }
