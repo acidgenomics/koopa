3543145b91932c13d27a1d1e5295e782cae9ad79
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-12 06:59:11 -0400

    Fix openssl handling

--- lang/shell/bash/include/install/common/shared/nushell.sh
+++ lang/shell/bash/include/install/common/shared/nushell.sh
@@ -5,18 +5,17 @@
 main() {
     # """
     # Install nushell.
-    # @note Updated 2023-04-10.
+    # @note Updated 2023-04-12.
     #
     # @seealso
     # - https://www.nushell.sh/book/installation.html#build-from-source
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/nushell.rb
     # - https://crates.io/crates/nu
     # """
-    local -A dict
     koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'zlib' 'openssl3'
-    dict['openssl']="$(koopa_app_prefix 'openssl3')"
-    export OPENSSL_DIR="${dict['openssl']}"
+    OPENSSL_DIR="$(koopa_app_prefix 'openssl3')"
+    export OPENSSL_DIR
     koopa_install_app_subshell \
         --installer='rust-package' \
         --name='nushell'
