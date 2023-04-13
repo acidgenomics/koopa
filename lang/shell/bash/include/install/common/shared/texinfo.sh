3f4c9bfb6622d836213cf40187730f5dc80ae426
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-13 09:01:23 -0400

    Fix texinfo on apple silicon

--- lang/shell/bash/include/install/common/shared/texinfo.sh
+++ lang/shell/bash/include/install/common/shared/texinfo.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME This is now failing to build for mac on Apple Silicon.
-
 main() {
     # """
     # Install texinfo.
@@ -11,17 +9,20 @@ main() {
     # - https://github.com/conda-forge/texinfo-feedstock
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/texinfo.rb
     # """
-    local -A app
-    koopa_activate_app 'gettext' 'ncurses' 'perl'
+    local -A app dict
+    koopa_activate_app 'gettext' 'libiconv' 'ncurses' 'perl'
     app['perl']="$(koopa_locate_perl --realpath)"
     koopa_assert_is_executable "${app[@]}"
+    dict['gettext']="$(koopa_app_prefix 'gettext')"
+    dict['libiconv']="$(koopa_app_prefix 'libiconv')"
     koopa_install_app_subshell \
         --installer='gnu-app' \
         --name='texinfo' \
         -D '--disable-dependency-tracking' \
         -D '--disable-install-warnings' \
-        -D '--disable-perl-api-texi-build' \
         -D '--disable-perl-xs' \
         -D '--disable-silent-rules' \
+        -D "--with-libiconv-prefix=${dict['libiconv']}" \
+        -D "--with-libintl-prefix=${dict['gettext']}" \
         -D "PERL=${app['perl']}"
 }
