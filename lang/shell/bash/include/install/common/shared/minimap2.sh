f6a188e72236d8937ac3760a5d660eb252f3471b
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-01 16:02:10 -0400

    Attempt to improve zlib config

--- lang/shell/bash/include/install/common/shared/minimap2.sh
+++ lang/shell/bash/include/install/common/shared/minimap2.sh
@@ -13,18 +13,28 @@ main() {
     # """
     #
     local -A app dict
+    local -a libs
     koopa_activate_app 'zlib'
     app['make']="$(koopa_locate_make)"
     koopa_assert_is_executable "${app[@]}"
+    dict['jobs']="$(koopa_cpu_count)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['shared_ext']="$(koopa_shared_ext)"
+    dict['zlib']="$(koopa_app_prefix 'zlib')"
     dict['url']="https://github.com/lh3/minimap2/archive/refs/tags/\
 v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
     koopa_print_env
-    "${app['make']}" VERBOSE=1
+    libs=(
+        "${dict['zlib']}/lib/libz.${dict['shared_ext']}"
+    )
+    "${app['make']}" \
+        --jobs="${dict['jobs']}" \
+        LIBS="${libs[*]}" \
+        VERBOSE=1
     koopa_cp --target-directory="${dict['prefix']}/bin" 'minimap2'
     return 0
 }
