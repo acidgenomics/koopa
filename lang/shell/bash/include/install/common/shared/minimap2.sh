1101265c9cee253cd41d4e2d9860ace5ba1e99cf
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-01 12:58:55 -0400

    Build minimap2 from source

--- lang/shell/bash/include/install/common/shared/minimap2.sh
+++ lang/shell/bash/include/install/common/shared/minimap2.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_install_app_subshell \
-        --installer='conda-env' \
-        --name='minimap2'
+    # """
+    # Install minimap2.
+    # @note Updated 2023-05-01.
+    #
+    # @seealso
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/
+    #     minimap2.rb
+    # - https://github.com/bioconda/bioconda-recipes/tree/master/recipes/
+    #     minimap2
+    # """
+    #
+    local -A app dict
+    koopa_activate_app 'zlib'
+    app['make']="$(koopa_locate_make)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['url']="https://github.com/lh3/minimap2/archive/refs/tags/\
+v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_print_env
+    "${app['make']}" VERBOSE=1
+    koopa_cp --target-directory="${dict['prefix']}/bin" 'minimap2'
+    return 0
 }
