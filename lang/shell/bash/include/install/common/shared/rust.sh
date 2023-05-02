9b46a2be85c345c7f104ff300833ae952d59eba8
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-02 06:50:33 -0400

    Debug Rust installer issue on Ubuntu 20

--- lang/shell/bash/include/install/common/shared/rust.sh
+++ lang/shell/bash/include/install/common/shared/rust.sh
@@ -3,6 +3,9 @@
 # FIXME This is hanging on LatchBio Ubuntu 20 instance.
 #   1.69.0-x86_64-unknown-linux-gnu unchanged - rustc 1.69.0 (84c898d65 2023-04-16)
 
+# FIXME This step is the problem on Ubuntu 20:
+# /opt/koopa/bin/gcp -a -f rustup/toolchains/1.69.0-x86_64-unknown-linux-gnu /opt/koopa/app/rust/1.69.0
+
 main() {
     # """
     # Install Rust (via rustup).
@@ -44,6 +47,7 @@ main() {
     dict['toolchain_prefix']="${dict['tmp_prefix']}/toolchains/\
 ${dict['toolchain']}"
     koopa_assert_is_dir "${dict['toolchain_prefix']}"
-    koopa_cp "${dict['toolchain_prefix']}" "${dict['prefix']}"
+    # FIXME This step is hanging on Ubuntu 20.
+    koopa_cp --verbose "${dict['toolchain_prefix']}" "${dict['prefix']}"
     return 0
 }
