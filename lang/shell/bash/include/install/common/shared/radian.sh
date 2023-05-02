a11156fd6ccec0a458a6b6a65d2bc94fc3f98bff
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-02 15:34:06 -0400

    Add FIXME regarding radian install

--- lang/shell/bash/include/install/common/shared/radian.sh
+++ lang/shell/bash/include/install/common/shared/radian.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# FIXME We need to patch the latest release to fix app.py.
+# Run this inside virtual environment:
+# python3 setup.py install
+
 main() {
     koopa_install_app_subshell \
         --installer='python-venv' \
