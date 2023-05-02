e58f587e8cb1c7103e6edf745b53a80e11b9c999
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-02 07:02:54 -0400

    Add links

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -21927,6 +21927,21 @@ koopa_sudo_write_string() {
     return 0
 }
 
+koopa_sudo() {
+    local -A app
+    local -a cmd
+    koopa_assert_has_args "$#"
+    if ! koopa_is_root
+    then
+        app['sudo']="$(koopa_locate_sudo)"
+        koopa_assert_is_executable "${app[@]}"
+        cmd+=("${app['sudo']}")
+    fi
+    cmd+=("$@")
+    "${cmd[@]}"
+    return 0
+}
+
 koopa_switch_to_develop() {
     local -A app dict
     koopa_assert_has_no_args "$#"
