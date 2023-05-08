755037cb6c639ae31a77bb4f02b24599c397841f
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-08 08:04:05 -0400

    Update apps

--- lang/shell/bash/include/install/common/shared/libgcrypt.sh
+++ lang/shell/bash/include/install/common/shared/libgcrypt.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# FIXME This is now failing to build on macOS.
+# Need to apply patch documented here:
+# https://dev.gnupg.org/T6442
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/libgcrypt.rb
+
 main() {
     koopa_install_app_subshell \
         --installer='gnupg-gcrypt' \
