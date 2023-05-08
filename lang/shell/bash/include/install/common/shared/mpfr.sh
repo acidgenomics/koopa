7bb1e90ab6a3432ecbe6b58016c6bd7652b85687
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-08 08:04:05 -0400

    Update apps

--- lang/shell/bash/include/install/common/shared/mpfr.sh
+++ lang/shell/bash/include/install/common/shared/mpfr.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME Need to apply patches for this.
+# https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/mpfr.rb
+
 main() {
     koopa_activate_app 'gmp'
     koopa_install_app_subshell \
