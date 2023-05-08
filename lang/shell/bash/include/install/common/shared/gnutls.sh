eb33732f23b28f0bb7caa5980219076a7be17f36
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-08 09:39:18 -0400

    Update gnutls

--- lang/shell/bash/include/install/common/shared/gnutls.sh
+++ lang/shell/bash/include/install/common/shared/gnutls.sh
@@ -1,7 +1,50 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_install_app_subshell \
-        --installer='gnupg-gcrypt' \
-        --name='gnutls'
+    # """
+    # Install GnuTLS.
+    # @note Updated 2023-05-08.
+    #
+    # @seealso
+    # - https://github.com/conda-forge/gnutls-feedstock
+    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/gnutls.rb
+    # - https://github.com/macports/macports-ports/blob/master/devel/gnutls/
+    # """
+    local -A dict
+    local -a conf_args
+    koopa_activate_app --build-only 'pkg-config'
+    koopa_activate_app \
+        'gmp' \
+        'libtasn1' \
+        'libunistring' \
+        'nettle'
+    dict['gcrypt_url']="$(koopa_gcrypt_url)"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    conf_args=(
+        '--disable-dependency-tracking'
+        '--disable-full-test-suite'
+        '--disable-guile'
+        '--disable-heartbeat-support'
+        '--disable-libdane'
+        '--disable-maintainer-mode'
+        '--disable-silent-rules'
+        '--disable-static'
+        '--enable-openssl-compatibility'
+        "--prefix=${dict['prefix']}"
+        '--with-idn'
+        '--with-included-unistring'
+        '--with-p11-kit'
+        '--without-brotli'
+        '--without-zlib'
+        '--without-zstd'
+    )
+    dict['maj_min_ver']="$(koopa_major_minor_version "${dict['version']}")"
+    dict['url']="${dict['gcrypt_url']}/gnutls/v${dict['maj_min_ver']}/\
+gnutls-${dict['version']}.tar.xz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_make_build "${conf_args[@]}"
+    return 0
 }
