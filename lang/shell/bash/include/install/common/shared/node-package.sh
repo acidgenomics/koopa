30af70e93976bd24f184855e465a9fa5c4c3c9e7
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-09 09:09:48 -0400

    Ensure we burn NODE_PATH into executables

--- lang/shell/bash/include/install/common/shared/node-package.sh
+++ lang/shell/bash/include/install/common/shared/node-package.sh
@@ -11,7 +11,8 @@ main() {
     # - npm config get prefix
     # """
     local -A app dict
-    local -a install_args
+    local -a bin_files install_args
+    local bin_file
     app['node']="$(koopa_locate_node)"
     app['npm']="$(koopa_locate_npm)"
     koopa_assert_is_executable "${app[@]}"
@@ -37,5 +38,27 @@ main() {
     esac
     "${app['npm']}" install "${install_args[@]}" 2>&1
     koopa_rm "${dict['cache_prefix']}"
+    # Ensure we burn 'NODE_PATH' into executables.
+    read -r -d '' "dict[node_path_string]" << END || true
+process.env.NODE_PATH = "${dict['prefix']}/lib/node_modules";
+require("module").Module._initPaths();
+END
+    readarray -t bin_files <<< "$( \
+        koopa_find \
+            --max-depth=1 \
+            --min-depth=1 \
+            --prefix="${dict['prefix']}/bin" \
+            --type='l' \
+    )"
+    koopa_assert_is_array_non_empty "${bin_files[@]:-}"
+    koopa_assert_is_file "${bin_files[@]}"
+    for bin_file in "${bin_files[@]}"
+    do
+        bin_file="$(koopa_realpath "$bin_file")"
+        koopa_insert_at_line_number \
+            --file="$bin_file" \
+            --line-number=2 \
+            --string="${dict['node_path_string']}"
+    done
     return 0
 }
