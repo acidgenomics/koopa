2179dbd7eb8aa150b0596796faf7d8318ce449aa
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-09 08:03:23 -0400

    Harden our Homebrew updater against invalid permissions

--- lang/shell/bash/functions/common/u/update-system-homebrew.sh
+++ lang/shell/bash/functions/common/u/update-system-homebrew.sh
@@ -24,43 +24,48 @@ koopa_update_system_homebrew() {
     dict['prefix']="$(koopa_homebrew_prefix)"
     dict['user_id']="$(koopa_user_id)"
     koopa_assert_is_dir "${dict['prefix']}"
-    if [[ "$(koopa_stat_user_id "${dict['prefix']}")" != "${dict['user_id']}" ]]
+    if koopa_is_macos
     then
-        koopa_stop 'Homebrew is not managed by current user.'
+        koopa_assert_is_dir "${dict['prefix']}/Homebrew"
+        if [[ "$(koopa_stat_user_id "${dict['prefix']}/Homebrew")" \
+            != "${dict['user_id']}" ]]
+        then
+            koopa_stop 'Homebrew is not managed by current user.'
+        fi
+        if ! koopa_macos_is_xcode_clt_installed
+        then
+            koopa_stop \
+                'Xcode Command Line Tools are missing.' \
+                "Run 'koopa install system xcode-clt' to resolve."
+        fi
+        dirs=(
+            "${dict['prefix']}/bin"
+            "${dict['prefix']}/etc"
+            "${dict['prefix']}/etc/bash_completion.d"
+            "${dict['prefix']}/include"
+            "${dict['prefix']}/lib"
+            "${dict['prefix']}/lib/pkgconfig"
+            "${dict['prefix']}/sbin"
+            "${dict['prefix']}/share"
+            "${dict['prefix']}/share/doc"
+            "${dict['prefix']}/share/info"
+            "${dict['prefix']}/share/locale"
+            "${dict['prefix']}/share/man"
+            "${dict['prefix']}/share/man/man1"
+            "${dict['prefix']}/share/zsh"
+            "${dict['prefix']}/share/zsh/site-functions"
+            "${dict['prefix']}/var/homebrew/linked"
+            "${dict['prefix']}/var/homebrew/locks"
+        )
+        for dir in "${dirs[@]}"
+        do
+            [[ "${bool['reset']}" -eq 1 ]] && continue
+            [[ -d "$dir" ]] || continue
+            [[ "$(koopa_stat_user_id "$dir")" == "${dict['user_id']}" ]] \
+                && continue
+            bool['reset']=1
+        done
     fi
-    if koopa_is_macos && ! koopa_macos_is_xcode_clt_installed
-    then
-        koopa_macos_install_system_xcode_clt
-        koopa_stop \
-            'Xcode Command Line Tools are missing.' \
-            "Run 'koopa install system xcode-clt' to resolve."
-    fi
-    dirs=(
-        "${dict['prefix']}/bin"
-        "${dict['prefix']}/etc"
-        "${dict['prefix']}/etc/bash_completion.d"
-        "${dict['prefix']}/include"
-        "${dict['prefix']}/lib"
-        "${dict['prefix']}/lib/pkgconfig"
-        "${dict['prefix']}/sbin"
-        "${dict['prefix']}/share"
-        "${dict['prefix']}/share/doc"
-        "${dict['prefix']}/share/info"
-        "${dict['prefix']}/share/locale"
-        "${dict['prefix']}/share/man"
-        "${dict['prefix']}/share/man/man1"
-        "${dict['prefix']}/share/zsh"
-        "${dict['prefix']}/share/zsh/site-functions"
-        "${dict['prefix']}/var/homebrew/linked"
-        "${dict['prefix']}/var/homebrew/locks"
-    )
-    for dir in "${dirs[@]}"
-    do
-        [[ "${bool['reset']}" -eq 0 ]] || continue
-        [[ -d "$dir" ]] || continue
-        [[ "$(koopa_stat_user_id "$dir")" == "${dict['user_id']}" ]] && continue
-        bool['reset']=1
-    done
     if [[ "${bool['reset']}" -eq 1 ]]
     then
         koopa_brew_reset_permissions
