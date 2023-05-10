a1f05c40feb020c3447783e79e0ea8775c477f72
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-10 12:18:23 -0400

    Work on improving our apt functions

--- lang/shell/bash/functions/os/linux/debian/a/apt-install.sh
+++ lang/shell/bash/functions/os/linux/debian/a/apt-install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# FIXME This step is erroring out for 'r-base' and 'r-base-dev'.
-
 koopa_debian_apt_install() {
     # """
     # Install Debian apt package.
-    # @note Updated 2020-06-30.
+    # @note Updated 2023-05-10.
     # """
     koopa_assert_has_args "$#"
+    koopa_debian_apt_get update
     koopa_debian_apt_get install "$@"
+    return 0
 }
