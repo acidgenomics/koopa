254e432c7fb6cf9611d35726a2f433a889d1d300
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-12 14:57:33 -0400

    Reorganize cli commands

--- lang/shell/bash/functions/os/linux/debian.sh
+++ lang/shell/bash/functions/os/linux/debian.sh
@@ -649,6 +649,61 @@ koopa_debian_apt_space_used_by() {
     return 0
 }
 
+koopa_debian_configure_system_defaults() {
+    local -A app
+    app['cat']="$(koopa_locate_cat --allow-system)"
+    app['debconf_set_selections']="$( \
+        koopa_debian_locate_debconf_set_selections \
+    )"
+    app['echo']="$(koopa_locate_echo --allow-system)"
+    koopa_assert_is_executable "${app[@]}"
+    koopa_debian_apt_get update
+    koopa_debian_apt_get full-upgrade
+    "${app['cat']}" << END \
+        | koopa_sudo "${app['debconf_set_selections']}"
+tzdata tzdata/Areas select America
+tzdata tzdata/Zones/America select New_York
+END
+    koopa_debian_apt_install \
+        'bash' \
+        'ca-certificates' \
+        'coreutils' \
+        'curl' \
+        'findutils' \
+        'g++' \
+        'gcc' \
+        'git' \
+        'libc-dev' \
+        'libgmp-dev' \
+        'locales' \
+        'lsb-release' \
+        'make' \
+        'perl' \
+        'procps' \
+        'sudo' \
+        'systemd' \
+        'tzdata' \
+        'unzip'
+    app['dpkg_reconfigure']="$(koopa_debian_locate_dpkg_reconfigure)"
+    app['locale_gen']="$(koopa_debian_locate_locale_gen)"
+    app['timedatectl']="$(koopa_debian_locate_timedatectl)"
+    app['update_locale']="$(koopa_debian_locate_update_locale)"
+    koopa_assert_is_executable "${app[@]}"
+    koopa_debian_apt_get autoremove
+    koopa_debian_apt_get clean
+    koopa_sudo "${app['timedatectl']}" set-timezone 'America/New_York'
+    koopa_sudo_write_string \
+        --file='/etc/locale.gen' \
+        --string='en_US.UTF-8 UTF-8'
+    koopa_sudo "${app['locale_gen']}" --purge
+    koopa_sudo "${app['dpkg_reconfigure']}" \
+        --frontend='noninteractive' locales
+    koopa_sudo "${app['update_locale']}" LANG='en_US.UTF-8'
+    koopa_debian_needrestart_noninteractive
+    koopa_enable_passwordless_sudo
+    return 0
+}
+
 koopa_debian_debian_version() {
     local file x
     file='/etc/debian_version'
@@ -701,61 +756,6 @@ koopa_debian_install_from_deb() {
     koopa_assert_is_executable "${app[@]}"
     dict['file']="${1:?}"
     koopa_sudo "${app['gdebi']}" --non-interactive "${dict['file']}"
-    return 0
-}
-
-koopa_debian_install_system_builder_base() {
-    local -A app
-    app['cat']="$(koopa_locate_cat --allow-system)"
-    app['debconf_set_selections']="$( \
-        koopa_debian_locate_debconf_set_selections \
-    )"
-    app['echo']="$(koopa_locate_echo --allow-system)"
-    koopa_assert_is_executable "${app[@]}"
-    koopa_debian_apt_get update
-    koopa_debian_apt_get full-upgrade
-    "${app['cat']}" << END \
-        | koopa_sudo "${app['debconf_set_selections']}"
-tzdata tzdata/Areas select America
-tzdata tzdata/Zones/America select New_York
-END
-    koopa_debian_apt_install \
-        'bash' \
-        'ca-certificates' \
-        'coreutils' \
-        'curl' \
-        'findutils' \
-        'g++' \
-        'gcc' \
-        'git' \
-        'libc-dev' \
-        'libgmp-dev' \
-        'locales' \
-        'lsb-release' \
-        'make' \
-        'perl' \
-        'procps' \
-        'sudo' \
-        'systemd' \
-        'tzdata' \
-        'unzip'
-    app['dpkg_reconfigure']="$(koopa_debian_locate_dpkg_reconfigure)"
-    app['locale_gen']="$(koopa_debian_locate_locale_gen)"
-    app['timedatectl']="$(koopa_debian_locate_timedatectl)"
-    app['update_locale']="$(koopa_debian_locate_update_locale)"
-    koopa_assert_is_executable "${app[@]}"
-    koopa_debian_apt_get autoremove
-    koopa_debian_apt_get clean
-    koopa_sudo "${app['timedatectl']}" set-timezone 'America/New_York'
-    koopa_sudo_write_string \
-        --file='/etc/locale.gen' \
-        --string='en_US.UTF-8 UTF-8'
-    koopa_sudo "${app['locale_gen']}" --purge
-    koopa_sudo "${app['dpkg_reconfigure']}" \
-        --frontend='noninteractive' locales
-    koopa_sudo "${app['update_locale']}" LANG='en_US.UTF-8'
-    koopa_debian_needrestart_noninteractive
-    koopa_enable_passwordless_sudo
     return 0
 }
 
