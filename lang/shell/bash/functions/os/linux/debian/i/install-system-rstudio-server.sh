6dd41cc853456386a05c3ec52649b61b8958d276
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-12 16:37:36 -0400

    Improve handling of apt-based installers

--- lang/shell/bash/functions/os/linux/debian/i/install-system-rstudio-server.sh
+++ lang/shell/bash/functions/os/linux/debian/i/install-system-rstudio-server.sh
@@ -3,6 +3,7 @@
 koopa_debian_install_system_rstudio_server() {
     koopa_install_app \
         --name='rstudio-server' \
+        --no-isolate \
         --platform='debian' \
         --system \
         "$@"
