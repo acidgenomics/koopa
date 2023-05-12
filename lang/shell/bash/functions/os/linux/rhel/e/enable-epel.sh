54c27e18c6253fc4de9ba536c6462e8adfd597fd
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-12 14:22:19 -0400

    Resolve FIXMEs

--- lang/shell/bash/functions/os/linux/rhel/e/enable-epel.sh
+++ lang/shell/bash/functions/os/linux/rhel/e/enable-epel.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# FIXME Need to get epel version dynamically here.
-
 koopa_rhel_enable_epel() {
     # """
     # Enable Extra Packages for Enterprise Linux (EPEL).
     # @note Updated 2023-01-06.
+    #
+    # @seealso
+    # - https://docs.fedoraproject.org/en-US/epel/
     # """
-    local rhel_version
+    local -A app dict
     koopa_assert_has_no_args "$#"
     if koopa_fedora_dnf repolist \
         | koopa_str_detect_regex - --pattern='^epel'
@@ -15,8 +16,15 @@ koopa_rhel_enable_epel() {
         koopa_alert_success 'EPEL is already enabled.'
         return 0
     fi
-    rhel_version='9' # FIXME
-    koopa_fedora_dnf_install "https://dl.fedoraproject.org/pub/\
-epel/epel-release-latest-${rhel_version}.noarch.rpm"
+    koopa_assert_is_admin
+    app['sub_mngr']="$(koopa_rhel_locate_subscription_manager)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['arch']="$(koopa_arch)"
+    dict['os_ver']="$(koopa_linux_os_version)"
+    dict['os_maj_ver']="$(koopa_major_version "${dict['os_ver']}")"
+    "${app['sub_mngr']}" repos --enable \
+        "codeready-builder-for-rhel-${dict['os_maj_ver']}-${dict['arch']}-rpms"
+    koopa_fedora_dnf_install "https://dl.fedoraproject.org/pub/epel/\
+epel-release-latest-${os_maj_ver}.noarch.rpm"
     return 0
 }
