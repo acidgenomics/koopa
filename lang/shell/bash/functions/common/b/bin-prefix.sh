54397cdc6e9175702244687fc01018e286ce3ee8
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-12 14:22:19 -0400

    Resolve FIXMEs

--- lang/shell/bash/functions/common/b/bin-prefix.sh
+++ lang/shell/bash/functions/common/b/bin-prefix.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Create a Bash-specific version of this function.
 koopa_bin_prefix() {
     _koopa_bin_prefix "$@"
 }
--- lang/shell/bash/functions/os/linux/rhel.sh
+++ lang/shell/bash/functions/os/linux/rhel.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=all
 
 koopa_rhel_enable_epel() {
-    local rhel_version
+    local -A app dict
     koopa_assert_has_no_args "$#"
     if koopa_fedora_dnf repolist \
         | koopa_str_detect_regex - --pattern='^epel'
@@ -10,8 +10,19 @@ koopa_rhel_enable_epel() {
         koopa_alert_success 'EPEL is already enabled.'
         return 0
     fi
-    rhel_version='9' # FIXME
-    koopa_fedora_dnf_install "https://dl.fedoraproject.org/pub/\
-epel/epel-release-latest-${rhel_version}.noarch.rpm"
+    koopa_assert_is_admin
+    app['sub_mngr']="$(koopa_rhel_locate_subscription_manager)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['arch']="$(koopa_arch)"
+    dict['os_ver']="$(koopa_linux_os_version)"
+    dict['os_maj_ver']="$(koopa_major_version "${dict['os_ver']}")"
+    "${app['sub_mngr']}" repos --enable \
+        "codeready-builder-for-rhel-${dict['os_maj_ver']}-${dict['arch']}-rpms"
+    koopa_fedora_dnf_install "https://dl.fedoraproject.org/pub/epel/\
+epel-release-latest-${os_maj_ver}.noarch.rpm"
     return 0
+}
+
+koopa_rhel_locate_subscription_manager() {
+    koopa_locate_app '/usr/sbin/subscription-manager' "$@"
 }
