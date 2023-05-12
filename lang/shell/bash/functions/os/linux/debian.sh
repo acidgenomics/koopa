57551b3042f77062b687c1c269ccef1d5f2fc190
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-12 15:22:20 -0400

    Improve our regex pattern matching

--- lang/shell/bash/functions/os/linux/debian.sh
+++ lang/shell/bash/functions/os/linux/debian.sh
@@ -890,7 +890,7 @@ koopa_debian_needrestart_noninteractive() {
     [[ -f "${dict['file']}" ]] || return 0
     if koopa_file_detect_regex \
         --file="${dict['file']}" \
-        --pattern="^\$nrconf{restart} = 'a';$"
+        --pattern="^\$nrconf\{restart\} = 'a';$"
     then
         return 0
     fi
@@ -898,8 +898,8 @@ koopa_debian_needrestart_noninteractive() {
     koopa_alert "Modifying '${dict['file']}'."
     koopa_find_and_replace_in_file \
         --regex \
-        --pattern="^.*\$nrconf{restart}.*;$" \
-        --replacement="\$nrconf{restart} = \'a\';" \
+        --pattern="^.*\$nrconf\{restart\}.*;$" \
+        --replacement="\$nrconf\{restart\} = \'a\';" \
         --sudo \
         "${dict['file']}"
     return 0
