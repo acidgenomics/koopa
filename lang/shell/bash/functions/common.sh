dbd518fc8fcfa2180e812e0dc0074bb243797251
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-14 12:26:06 -0400

    Remove deprecated apt-key code

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -12956,17 +12956,6 @@ koopa_is_alpine() {
     _koopa_is_alpine "$@"
 }
 
-koopa_is_anaconda() {
-    local -A app dict
-    koopa_assert_has_args_le "$#" 1
-    app['conda']="${1:-}"
-    [[ -z "${app['conda']}" ]] && app['conda']="$(koopa_locate_conda)"
-    [[ -x "${app['conda']}" ]] || return 1
-    dict['prefix']="$(koopa_parent_dir --num=2 "${app['conda']}")"
-    [[ -x "${dict['prefix']}/bin/anaconda" ]] || return 1
-    return 0
-}
-
 koopa_is_arch() {
     _koopa_is_arch "$@"
 }
