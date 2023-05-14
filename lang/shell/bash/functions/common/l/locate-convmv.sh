165a3a116bc40405f57c20f07eaa7387787ab5ce
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-14 12:21:11 -0400

    Resolve FIXME

--- lang/shell/bash/functions/common/l/locate-convmv.sh
+++ lang/shell/bash/functions/common/l/locate-convmv.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Need to add install support for this.
-
 koopa_locate_convmv() {
     koopa_locate_app \
         --app-name='convmv' \
--- lang/shell/bash/functions/common/s/spacemacs-prefix.sh
+++ lang/shell/bash/functions/common/s/spacemacs-prefix.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Create a Bash-specific version of this function.
 koopa_spacemacs_prefix() {
     _koopa_spacemacs_prefix "$@"
 }
