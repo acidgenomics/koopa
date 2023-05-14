89b5f0bf0a7b06719474fcb974aeadc5a40a5e6c
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-14 12:21:11 -0400

    Resolve FIXME

--- lang/shell/bash/functions/common/l/local-data-prefix.sh
+++ lang/shell/bash/functions/common/l/local-data-prefix.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Create a Bash-specific version of this function.
 koopa_local_data_prefix() {
     _koopa_local_data_prefix "$@"
 }
--- lang/shell/bash/functions/common/o/os-id.sh
+++ lang/shell/bash/functions/common/o/os-id.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Create a Bash-specific version of this function.
 koopa_os_id() {
     _koopa_os_id "$@"
 }
