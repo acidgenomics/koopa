b3bf502bcfac8d9c7cd8f031a05f67b11bb11011
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-14 12:44:10 -0400

    Rework docker-credential-helpers

--- lang/shell/bash/functions/os/linux/common/u/uninstall-docker-credential-pass.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-koopa_linux_uninstall_docker_credential_pass() {
-    koopa_uninstall_app \
-        --name='docker-credential-pass' \
-        --platform='linux' \
-        "$@"
-}
