829d3d7b0e8fa6f5cb8002abbb2f10adffed6ca6
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-15 09:35:09 -0400

    Add back r-koopa

--- lang/shell/bash/functions/common.sh
+++ lang/shell/bash/functions/common.sh
@@ -18213,6 +18213,50 @@ install-packages-in-site-library.R"
     return 0
 }
 
+koopa_r_koopa() {
+    local -A app dict
+    local -a code pos rscript_args
+    koopa_assert_has_args "$#"
+    app['rscript']="$(koopa_locate_rscript)"
+    koopa_assert_is_executable "${app[@]}"
+    rscript_args=()
+    pos=()
+    while (("$#"))
+    do
+        case "$1" in
+            '--vanilla')
+                rscript_args+=('--vanilla')
+                shift 1
+                ;;
+            '--'*)
+                pos+=("$1")
+                shift 1
+                ;;
+            '-'*)
+                koopa_invalid_arg "$1"
+                ;;
+            *)
+                pos+=("$1")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa_assert_has_args "$#"
+    dict['fun']="${1:?}"
+    shift 1
+    dict['header_file']="$(koopa_koopa_prefix)/lang/r/include/header.R"
+    koopa_assert_is_file "${dict['header_file']}"
+    code=("source('${dict['header_file']}');")
+    if [[ "${dict['fun']}" != 'header' ]]
+    then
+        code+=("koopa::${dict['fun']}();")
+    fi
+    pos=("$@")
+    "${app['rscript']}" "${rscript_args[@]}" -e "${code[*]}" "${pos[@]@Q}"
+    return 0
+}
+
 koopa_r_library_prefix() {
     local -A app dict
     koopa_assert_has_args_le "$#" 1
