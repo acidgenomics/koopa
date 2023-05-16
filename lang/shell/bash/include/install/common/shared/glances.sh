503e8c524c70be6cce187fddef0cba6e8088b593
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-16 10:08:07 -0400

    Roll back glances

--- lang/shell/bash/include/install/common/shared/glances.sh
+++ lang/shell/bash/include/install/common/shared/glances.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_activate_app 'ncurses'
     koopa_install_app_subshell \
         --installer='python-venv' \
-        --name='glances' \
-        -D '--no-binary'
+        --name='glances'
 }
