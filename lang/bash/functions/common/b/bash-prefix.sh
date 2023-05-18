8704dcd9f79926692bddbeda74ddd0334230b8ed
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-18 09:14:58 -0400

    Improve lang file path consistency

--- lang/bash/functions/common/b/bash-prefix.sh
+++ lang/bash/functions/common/b/bash-prefix.sh
@@ -5,6 +5,6 @@ koopa_bash_prefix() {
     # Bash functions prefix.
     # @note Updated 2023-05-18.
     # """
-    koopa_print "$(koopa_koopa_prefix)/lang/shell/bash"
+    koopa_print "$(koopa_koopa_prefix)/lang/bash"
     return 0
 }
--- lang/sh/functions/a/alias-kp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-_koopa_alias_kp() {
-    # """
-    # Koopa 'kp' shortcut alias.
-    # @note Updated 2022-07-21.
-    # """
-    cd "$(_koopa_koopa_prefix)/lang/shell/posix" || return 1
-}
