c60e80b56b079dc1442a40cf0cf557b300ae2dac
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-20 09:35:29 -0400

    Harden brew doctor checks

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -2912,6 +2912,37 @@ koopa_bowtie2_index() {
     return 0
 }
 
+koopa_brew_doctor() {
+    local -A app
+    local -a all_checks disabled_checks enabled_checks
+    app['brew']="$(koopa_locate_brew)"
+    app['sort']="$(koopa_locate_sort --allow-system)"
+    app['tr']="$(koopa_locate_tr --allow-system)"
+    app['uniq']="$(koopa_locate_uniq --allow-system)"
+    koopa_assert_is_executable "${app[@]}"
+    disabled_checks=(
+        'check_for_stray_dylibs'
+        'check_for_stray_headers'
+        'check_for_stray_las'
+        'check_for_stray_pcs'
+        'check_for_stray_static_libs'
+        'check_user_path_1'
+        'check_user_path_2'
+        'check_user_path_3'
+    )
+    readarray -t all_checks <<< "$("${app['brew']}" doctor --list-checks)"
+    readarray -t enabled_checks <<< "$( \
+        koopa_print "${all_checks[@]}" "${disabled_checks[@]}" \
+            | "${app['tr']}" ' ' '\n' \
+            | "${app['sort']}" \
+            | "${app['uniq']}" -u \
+    )"
+    koopa_assert_is_array_non_empty "${enabled_checks[@]}"
+    "${app['brew']}" config || true
+    "${app['brew']}" doctor "${enabled_checks[@]}" || true
+    return 0
+}
+
 koopa_brew_dump_brewfile() {
     local -A app
     local today
@@ -24787,8 +24818,7 @@ koopa_update_system_homebrew() {
     "${app['brew']}" cleanup -s || true
     koopa_rm "$("${app['brew']}" --cache)"
     "${app['brew']}" autoremove || true
-    "${app['brew']}" config
-    "${app['brew']}" doctor || true
+    koopa_brew_doctor
     koopa_alert_update_success 'Homebrew' "${dict['prefix']}"
     return 0
 }
