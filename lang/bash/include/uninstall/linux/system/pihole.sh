9a0c7687dfc0fd1b647701c1aef4a3c5baa14733
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-22 07:53:48 -0400

    Draft updates to installers

---
+++ lang/bash/include/uninstall/linux/system/pihole.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# FIXME Need to add a pihole locator function.
+
+main() {
+    # """
+    # Uninstall pihole.
+    # @note Updated 2023-05-21.
+    #
+    # @seealso
+    # - https://docs.pi-hole.net/main/uninstall/
+    # """
+    # pihole uninstall
+    return 0
+}
