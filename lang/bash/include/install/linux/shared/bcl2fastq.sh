98383076d10050a7e548b0cabf22656e57430a5c
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-24 07:41:04 -0400

    Rework bcl2fastq

--- lang/bash/include/install/linux/shared/bcl2fastq.sh
+++ lang/bash/include/install/linux/shared/bcl2fastq.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
+# FIXME This currently requires system zlib on Linux.
+
 main() {
     # """
     # Install bcl2fastq from source.
-    # @note Updated 2023-05-23.
+    # @note Updated 2023-05-24.
     #
-    # ARM is not yet supported.
+    # ARM is not yet supported for this.
     #
-    # @seealso
-    # Conda approach:
+    # @section Conda approach:
+    #
     # - https://anaconda.org/dranew/bcl2fastq/files
     # - https://conda.io/projects/conda/en/latest/user-guide/tasks/
     #     manage-environments.html
     #
-    # Docker image approaches:
+    # @section Docker image approaches:
+    #
     # - https://github.com/Zymo-Research/docker-bcl2fastq/
     # - https://hub.docker.com/r/umccr/bcl2fastq
     # - https://alexandria-scrna-data-library.readthedocs.io/en/latest/
     #     bcl2fastq.html
     #
-    # Building from source (problematic with newer GCC / clang):
-    # - https://sarahpenir.github.io/linux/Installing-bcl2fastq/
+    # @section Building from source:
+    #
     # - https://gist.github.com/jblachly/f8dc0f328d66659d9ee005548a5a2d2e
+    # - https://sarahpenir.github.io/linux/Installing-bcl2fastq/
     # - https://github.com/rossigng/easybuild-easyconfigs/blob/main/
     #     easybuild/easyconfigs/b/bcl2fastq2/
     # - https://github.com/perllb/ctg-wgs/blob/master/
@@ -37,109 +41,101 @@ main() {
     # - https://stackoverflow.com/questions/36195791/
     # """
     local -A app dict
-    local -a cmake_args conf_args
+    local -a conf_args deps
     koopa_assert_is_not_aarch64
+    koopa_activate_app --build-only 'cmake' 'make'
+    deps=('bzip2' 'icu4c' 'xz' 'zlib' 'zstd')
+    koopa_activate_app "${deps[@]}"
     app['aws']="$(koopa_locate_aws --allow-system)"
-    app['conda']="$(koopa_locate_conda --realpath)"
-    app['cp']="$(koopa_locate_cp --allow-system)"
-    app['sort']="$(koopa_locate_sort --allow-system)"
+    app['cmake']="$(koopa_locate_cmake)"
+    app['make']="$(koopa_locate_make)"
     koopa_assert_is_executable "${app[@]}"
+    dict['icu4c']="$(koopa_app_prefix 'icu4c')"
     dict['installers_base']="$(koopa_private_installers_s3_uri)"
     dict['jobs']="$(koopa_cpu_count)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
-    dict['gcc_version']='8.5.0'
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['conda']="$(koopa_init_dir "${dict['prefix']}/libexec")"
-    dict['sysroot']="$(koopa_init_dir "${dict['prefix']}/sysroot")"
-    dict['conda_file']='conda.yaml'
-    read -r -d '' "dict[conda_string]" << END || true
-name: bcl2fastq
-dependencies:
-    # > - curl
-    - bzip2
-    - compiler-rt
-    - gcc==${dict['gcc_version']}
-    - gfortran==${dict['gcc_version']}
-    - gxx==${dict['gcc_version']}
-    - gzip
-    - make
-    - zlib
-END
-    koopa_alert "Preparing conda environment in '${dict['conda']}'."
-    koopa_print "${dict['conda_string']}"
-    koopa_write_string \
-        --file="${dict['conda_file']}" \
-        --string="${dict['conda_string']}"
-    koopa_conda_create_env \
-        --file="${dict['conda_file']}" \
-        --prefix="${dict['conda']}"
-    dict['conda_sysroot']="${dict['conda']}/x86_64-conda-linux-gnu/sysroot"
-    koopa_assert_is_dir "${dict['conda_sysroot']}"
-    (
-        koopa_cd "${dict['sysroot']}"
-        "${app['cp']}" -frsv \
-            --target-directory="${dict['sysroot']}" \
-            "${dict['conda_sysroot']}/"*
-        "${app['cp']}" -frsv \
-            --target-directory="${dict['sysroot']}/usr/bin" \
-            "${dict['conda']}/bin/"*
-        "${app['cp']}" -frsv \
-            --target-directory="${dict['sysroot']}/usr/include" \
-            "${dict['conda']}/include/"*
-        "${app['cp']}" -frsv \
-            --target-directory="${dict['sysroot']}/usr/lib" \
-            "${dict['conda']}/lib/"*
-        koopa_cd "${dict['sysroot']}/usr/bin"
-        koopa_ln 'make' 'gmake'
-    )
-    app['make']="${dict['sysroot']}/usr/bin/gmake"
-    app['cc']="${dict['sysroot']}/usr/bin/gcc"
-    app['cxx']="${dict['sysroot']}/usr/bin/g++"
-    koopa_assert_is_executable "${app[@]}"
+    if koopa_is_linux
+    then
+        dict['arch']="$(koopa_arch)"
+        dict['c_include_path']="/usr/include/${dict['arch']}-linux-gnu"
+        koopa_assert_is_dir "${dict['c_include_path']}"
+        dict['toolset']='gcc'
+    elif koopa_is_macos
+    then
+        dict['toolset']='clang'
+    fi
+    dict['libexec']="$(koopa_init_dir "${dict['prefix']}/libexec")"
     dict['url']="${dict['installers_base']}/bcl2fastq/src/\
 ${dict['version']}.tar.zip"
-    "${app['aws']}" --profile='acidgenomics' s3 cp \
-        "${dict['url']}" "$(koopa_basename "${dict['url']}")"
+    "${app['aws']}" --profile='acidgenomics' \
+        s3 cp "${dict['url']}" "${dict['file']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'unzip'
-    koopa_extract 'unzip/'*'.tar.gz' 'bcl2fastq'
-    koopa_cd 'bcl2fastq'
+    koopa_extract 'unzip/'*'.tar.gz' 'bcl2fastq-src'
+    koopa_rm 'unzip'
+    # Install Boost 1.54.0 from 'redist'.
+    # Refer to 'src/cmake/bootstrap/installBoost.sh'.
+    koopa_cp \
+        'bcl2fastq-src/redist/boost'*'.tar.bz2' \
+        'boost.tar.bz2'
+    koopa_extract 'boost.tar.bz2' 'boost-src'
+    (
+        local -a b2_args bootstrap_args
+        koopa_cd 'boost-src'
+        bootstrap_args=(
+            "--prefix=${dict['libexec']}/boost"
+            "--libdir=${dict['libexec']}/boost/lib"
+            "--with-toolset=${dict['toolset']}"
+            "--with-icu=${dict['icu4c']}"
+            '--without-libraries=log,mpi,python'
+        )
+        b2_args=(
+            '-q'
+            # Show commands as they are executed.
+            '-d+2'
+            "-j${dict['jobs']}"
+            "--prefix=${dict['prefix']}"
+            "--libdir=${dict['prefix']}/lib"
+            "cxxflags=${CPPFLAGS:?}"
+            'link=shared'
+            "linkflags=${LDFLAGS:?}"
+            'runtime-link=shared'
+            'threading=multi'
+            "toolset=${dict['toolset']}"
+            'variant=release'
+            'install'
+        )
+        ./bootstrap.sh --help
+        ./bootstrap.sh "${bootstrap_args[@]}"
+        ./b2 --help
+        ./b2 "${b2_args[@]}"
+        return 0
+    )
+    koopa_cd 'bcl2fastq-src'
     koopa_mkdir 'build'
     koopa_cd 'build'
-    # > export BOOST_ROOT="${dict['conda_boost']}"
-    export CC="${app['cc']}"
-    export CPPFLAGS="-I${dict['sysroot']}/usr/include"
-    export CXX="${app['cxx']}"
-    export C_INCLUDE_PATH="${dict['sysroot']}/usr/include"
-    export LDFLAGS="-L${dict['sysroot']}/usr/lib -L${dict['sysroot']}/lib"
-    export MAKE="${app['make']}"
-    cmake_args=(
-        "-DCMAKE_CXX_FLAGS='${CPPFLAGS:-}'"
-        "-DCMAKE_C_FLAGS='${CPPFLAGS:-}'"
-        "-DCMAKE_EXE_LINKER_FLAGS='${LDFLAGS:-}'"
-        "-DCMAKE_FIND_ROOT_PATH='${dict['sysroot']}'"
-        "-DCMAKE_INCLUDE_PATH='${dict['sysroot']}/usr/include'"
-        "-DCMAKE_LIBRARY_PATH='${dict['sysroot']}/usr/lib'"
-        "-DCMAKE_MODULE_LINKER_FLAGS='${LDFLAGS:-}'"
-        "-DCMAKE_SHARED_LINKER_FLAGS='${LDFLAGS:-}'"
-        "-DCMAKE_SYSROOT='${dict['sysroot']}'"
-        "-DZLIB_INCLUDE_DIR='${dict['sysroot']}/usr/include'"
-        "-DZLIB_LIBRARY='${dict['sysroot']}/usr/lib/libz.so'"
-    )
-    export "CMAKE_OPTIONS=${cmake_args[*]}"
+    readarray -t cmake_args <<< "$( \
+        koopa_cmake_std_args --prefix="${dict['prefix']}"
+    )"
+    export BOOST_ROOT="${dict['libexec']}/boost"
+    export CMAKE_OPTIONS="${cmake_args[*]}"
+    if koopa_is_linux
+    then
+        export C_INCLUDE_PATH="${dict['c_include_path']}"
+    fi
+    koopa_print_env
     conf_args=(
-        # > "--with-cmake=${app['cmake']}"
         '--build-type=Release'
         "--parallel=${dict['jobs']}"
         "--prefix=${dict['prefix']}"
         '--verbose'
+        "--with-cmake=${app['cmake']}"
         '--without-unit-tests'
     )
-    koopa_add_to_path_start "${dict['sysroot']}/usr/bin"
-    # > koopa_conda_activate_env "${dict['conda']}"
-    koopa_print_env
     ../src/configure --help || true
     ../src/configure "${conf_args[@]}"
     "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
     "${app['make']}" install
+    koopa_rm "${dict['prefix']}/bin/test"
     return 0
 }
