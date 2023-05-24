41aad71f2a8f8972c76c33d5c4ee943faf656582
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-24 17:13:19 -0400

    Resolve FIXMEs

--- lang/bash/include/install/common/shared/mpfr.sh
+++ lang/bash/include/install/common/shared/mpfr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# FIXME Need to apply patches for this.
+# NOTE Need to apply patches for this.
 # https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/mpfr.rb
 
 main() {
