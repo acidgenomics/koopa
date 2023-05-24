3359260449bbcf25ce6ae5318d4166bc66248d50
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-24 15:44:21 -0400

    Resolve FIXMEs

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -1818,11 +1818,47 @@ koopa_aws_ecr_login_private() {
     app['aws']="$(koopa_locate_aws)"
     app['docker']="$(koopa_locate_docker)"
     koopa_assert_is_executable "${app[@]}"
-    dict['account_id']="${AWS_ECR_ACCOUNT_ID:?}" # FIXME
-    dict['profile']="${AWS_ECR_PROFILE:?}" # FIXME
-    dict['region']="${AWS_ECR_REGION:?}" # FIXME
-    dict['repo_url']="${dict['account_id']}.dkr.ecr.\
-${dict['region']}.amazonaws.com"
+    dict['account_id']="${AWS_ECR_ACCOUNT_ID:-}"
+    dict['profile']="${AWS_ECR_PROFILE:-}"
+    dict['region']="${AWS_ECR_REGION:-}"
+    dict['repo_url']="${dict['account_id']}.dkr.ecr.${dict['region']}.\
+amazonaws.com"
+    while (("$#"))
+    do
+        case "$1" in
+            '--account-id='*)
+                dict['account_id']="${1#*=}"
+                shift 1
+                ;;
+            '--account-id')
+                dict['account_id']="${2:?}"
+                shift 2
+                ;;
+            '--profile='*)
+                dict['profile']="${1#*=}"
+                shift 1
+                ;;
+            '--profile')
+                dict['profile']="${2:?}"
+                shift 2
+                ;;
+            '--region='*)
+                dict['region']="${1#*=}"
+                shift 1
+                ;;
+            '--region')
+                dict['region']="${2:?}"
+                shift 2
+                ;;
+            *)
+                koopa_invalid_arg "$1"
+                ;;
+        esac
+    done
+    koopa_assert_is_set \
+        '--account-id or AWS_ECR_ACCOUNT_ID' "${dict['account_id']}" \
+        '--profile or AWS_ECR_PROFILE' "${dict['profile']}" \
+        '--region or AWS_ECR_REGION' "${dict['region']}"
     koopa_alert "Logging into '${dict['repo_url']}'."
     "${app['docker']}" logout "${dict['repo_url']}" >/dev/null || true
     "${app['aws']}" --profile="${dict['profile']}" \
@@ -1842,9 +1878,36 @@ koopa_aws_ecr_login_public() {
     app['aws']="$(koopa_locate_aws)"
     app['docker']="$(koopa_locate_docker)"
     koopa_assert_is_executable "${app[@]}"
-    dict['profile']="${AWS_ECR_PROFILE:?}" # FIXME
-    dict['region']="${AWS_ECR_REGION:?}" # FIXME
+    dict['profile']="${AWS_ECR_PROFILE:-}"
+    dict['region']="${AWS_ECR_REGION:-}"
     dict['repo_url']='public.ecr.aws'
+    while (("$#"))
+    do
+        case "$1" in
+            '--profile='*)
+                dict['profile']="${1#*=}"
+                shift 1
+                ;;
+            '--profile')
+                dict['profile']="${2:?}"
+                shift 2
+                ;;
+            '--region='*)
+                dict['region']="${1#*=}"
+                shift 1
+                ;;
+            '--region')
+                dict['region']="${2:?}"
+                shift 2
+                ;;
+            *)
+                koopa_invalid_arg "$1"
+                ;;
+        esac
+    done
+    koopa_assert_is_set \
+        '--profile or AWS_ECR_PROFILE' "${dict['profile']}" \
+        '--region or AWS_ECR_REGION' "${dict['region']}"
     koopa_alert "Logging into '${dict['repo_url']}'."
     "${app['docker']}" logout "${dict['repo_url']}" >/dev/null || true
     "${app['aws']}" --profile="${dict['profile']}" \
