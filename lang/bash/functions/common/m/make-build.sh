c18e3e2c49be198e8751293c4ca49e9b596042e9
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-26 16:48:52 -0400

    Rework openssh

--- lang/bash/functions/common/m/make-build.sh
+++ lang/bash/functions/common/m/make-build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# FIXME Intercept --target argument.
+
 koopa_make_build() {
     # """
     # Build with GNU Make.
     # @note Updated 2023-05-08.
     # """
     local -A app dict
-    local -a conf_args
+    local -a conf_args pos targets
+    local target
     koopa_assert_has_args "$#"
     dict['make']="$(koopa_app_prefix 'make' --allow-missing)"
     if [[ -d "${dict['make']}" ]]
@@ -18,6 +21,26 @@ koopa_make_build() {
     fi
     koopa_assert_is_executable "${app[@]}"
     dict['jobs']="$(koopa_cpu_count)"
+    while (("$#"))
+    do
+        case "$1" in
+            # Key-value pairs --------------------------------------------------
+            '--target='*)
+                targets+=("${1#*=}")
+                shift 1
+                ;;
+            '--target')
+                targets+=("${2:?}")
+                shift 2
+                ;;
+            # Other ------------------------------------------------------------
+            *)
+                pos+=("$1")
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa_is_array_empty "${targets[@]}" && targets+=('install')
     conf_args+=("$@")
     koopa_print_env
     koopa_dl 'configure args' "${conf_args[*]}"
@@ -25,6 +48,9 @@ koopa_make_build() {
     ./configure --help || true
     ./configure "${conf_args[@]}"
     "${app['make']}" VERBOSE=1 --jobs="${dict['jobs']}"
-    "${app['make']}" install
+    for target in "${targets[@]}"
+    do
+        "${app['make']}" "$target"
+    done
     return 0
 }
