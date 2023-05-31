7fcb04e9e9012d4ef5df9501f6aee433754f4930
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-31 08:44:02 -0400

    Improve detection of compressed files

--- lang/bash/functions/common/c/compress-ext-pattern.sh
+++ lang/bash/functions/common/c/compress-ext-pattern.sh
@@ -5,16 +5,13 @@ koopa_compress_ext_pattern() {
     # Compressed file extension pattern.
     # @note Updated 2023-05-31.
     #
-    # Intentionally not including mixed archiving and compression formats here,
-    # such as 7z and zip.
-    #
     # @seealso
     # - https://en.wikipedia.org/wiki/List_of_archive_formats
     # """
     local -a formats
     local str
     koopa_assert_has_no_args "$#"
-    formats=('br' 'bz2' 'gz' 'lz' 'lz4' 'lzma' 'xz' 'Z' 'zst')
+    formats=('7z' 'br' 'bz2' 'gz' 'lz' 'lz4' 'lzma' 'xz' 'z' 'zip' 'zst')
     str="$(koopa_paste --sep='|' "${formats[@]}")"
     str="\.(${str})\$"
     koopa_print "$str"
