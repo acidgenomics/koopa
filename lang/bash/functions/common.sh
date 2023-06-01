18167173e7707e253b6c6499d7eb8cd2f42e8374
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-01 11:19:03 -0400

    Improve handling of decompression mode

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -6967,103 +6967,88 @@ $(koopa_basename_sans_ext "${dict['file']}")"
     )"
     dict['tmpfile']="${dict['tmpdir']}/$(koopa_basename "${dict['file']}")"
     koopa_ln "${dict['file']}" "${dict['tmpfile']}"
+    case "${dict['match']}" in
+        *'.tar' | \
+        *'.tar.'* | \
+        *'.tbz2' | \
+        *'.tgz')
+            local -a tar_cmd_args
+            tar_cmd_args=(
+                '-f' "${dict['tmpfile']}" # '--file'.
+                '-x' # '--extract'.
+            )
+            app['tar']="$(koopa_locate_tar --allow-system)"
+            koopa_assert_is_executable "${app['tar']}"
+            if koopa_is_root && koopa_is_gnu "${app['tar']}"
+            then
+                tar_cmd_args+=(
+                    '--no-same-owner'
+                    '--no-same-permissions'
+                )
+            fi
+            ;;
+    esac
+    case "${dict['match']}" in
+        *'.tar.bz2' | \
+        *'.tar.gz' | \
+        *'.tar.lz' | \
+        *'.tar.xz' | \
+        *'.tbz2' | \
+        *'.tgz')
+            cmd="${app['tar']}"
+            cmd_args=("${tar_cmd_args[@]}")
+            case "${dict['tmpfile']}" in
+                *'.bz2' | *'.tbz2')
+                    app['cmd2']="$(koopa_locate_bzip2 --allow-system)"
+                    koopa_add_to_path_start \
+                        "$(koopa_dirname "${app['cmd2']}")"
+                    cmd_args+=('-j') # '--bzip2'.
+                    ;;
+                *'.gz' | *'.tgz')
+                    app['cmd2']="$(koopa_locate_gzip --allow-system)"
+                    koopa_add_to_path_start \
+                        "$(koopa_dirname "${app['cmd2']}")"
+                    cmd_args+=('-z') # '--gzip'.
+                    ;;
+                *'.lz')
+                    app['cmd2']="$(koopa_locate_lzip --allow-system)"
+                    koopa_add_to_path_start \
+                        "$(koopa_dirname "${app['cmd2']}")"
+                    cmd_args+=('--lzip')
+                    ;;
+                *'.xz')
+                    app['cmd2']="$(koopa_locate_xz --allow-system)"
+                    koopa_add_to_path_start \
+                        "$(koopa_dirname "${app['cmd2']}")"
+                    cmd_args+=('-J') # '--xz'.
+                    ;;
+            esac
+            ;;
+        *'.tar')
+            app['cmd']="${app['tar']}"
+            cmd_args=("${tar_cmd_args[@]}")
+            ;;
+        *'.7z')
+            cmd="$(koopa_locate_7z)"
+            cmd_args=(
+                '-x'
+                "${dict['tmpfile']}"
+            )
+            ;;
+        *'.zip')
+            cmd="$(koopa_locate_unzip --allow-system)"
+            cmd_args=(
+                '-qq'
+                "${dict['tmpfile']}"
+            )
+            ;;
+        *)
+            koopa_stop "Unsupported file: '${dict['file']}'."
+            ;;
+    esac
+    koopa_assert_is_executable "$cmd"
     (
         koopa_cd "${dict['tmpdir']}"
-        case "${dict['match']}" in
-            *'.tar' | \
-            *'.tar.'* | \
-            *'.tbz2' | \
-            *'.tgz')
-                local -a tar_cmd_args
-                tar_cmd_args=(
-                    '-f' "${dict['tmpfile']}" # '--file'.
-                    '-x' # '--extract'.
-                )
-                app['tar']="$(koopa_locate_tar --allow-system)"
-                koopa_assert_is_executable "${app['tar']}"
-                if koopa_is_root && koopa_is_gnu "${app['tar']}"
-                then
-                    tar_cmd_args+=(
-                        '--no-same-owner'
-                        '--no-same-permissions'
-                    )
-                fi
-                ;;
-        esac
-        case "${dict['match']}" in
-            *'.tar.bz2' | \
-            *'.tar.gz' | \
-            *'.tar.lz' | \
-            *'.tar.xz' | \
-            *'.tbz2' | \
-            *'.tgz')
-                cmd="${app['tar']}"
-                cmd_args=("${tar_cmd_args[@]}")
-                case "${dict['tmpfile']}" in
-                    *'.bz2' | *'.tbz2')
-                        app['cmd2']="$(koopa_locate_bzip2 --allow-system)"
-                        koopa_add_to_path_start \
-                            "$(koopa_dirname "${app['cmd2']}")"
-                        cmd_args+=('-j') # '--bzip2'.
-                        ;;
-                    *'.gz' | *'.tgz')
-                        app['cmd2']="$(koopa_locate_gzip --allow-system)"
-                        koopa_add_to_path_start \
-                            "$(koopa_dirname "${app['cmd2']}")"
-                        cmd_args+=('-z') # '--gzip'.
-                        ;;
-                    *'.lz')
-                        app['cmd2']="$(koopa_locate_lzip --allow-system)"
-                        koopa_add_to_path_start \
-                            "$(koopa_dirname "${app['cmd2']}")"
-                        cmd_args+=('--lzip')
-                        ;;
-                    *'.xz')
-                        app['cmd2']="$(koopa_locate_xz --allow-system)"
-                        koopa_add_to_path_start \
-                            "$(koopa_dirname "${app['cmd2']}")"
-                        cmd_args+=('-J') # '--xz'.
-                        ;;
-                esac
-                ;;
-            *'.tar')
-                app['cmd']="${app['tar']}"
-                cmd_args=("${tar_cmd_args[@]}")
-                ;;
-            *'.7z')
-                cmd="$(koopa_locate_7z)"
-                cmd_args=(
-                    '-x'
-                    "${dict['tmpfile']}"
-                )
-                ;;
-            *'.zip')
-                cmd="$(koopa_locate_unzip --allow-system)"
-                cmd_args=(
-                    '-qq'
-                    "${dict['tmpfile']}"
-                )
-                ;;
-            *'.br' | \
-            *'.bz2' | \
-            *'.gz' | \
-            *'.lz' | \
-            *'.lz4' | \
-            *'.lzma' | \
-            *'.xz' | \
-            *'.z' | \
-            *'.zst')
-                cmd='koopa_decompress'
-                cmd_args=("${dict['tmpfile']}")
-                ;;
-            *)
-                koopa_stop "Unsupported file: '${dict['file']}'."
-                ;;
-        esac
-        if ! koopa_is_function "$cmd"
-        then
-            koopa_assert_is_executable "$cmd"
-        fi
         "$cmd" "${cmd_args[@]}" # 2>/dev/null
     )
     koopa_rm "${dict['tmpfile']}"
