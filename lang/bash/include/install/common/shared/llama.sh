dab0296371ee7a324fd86eccd5244a0f8c0b3a52
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-01 13:23:02 -0400

    Update installers

--- lang/bash/include/install/common/shared/llama.sh
+++ lang/bash/include/install/common/shared/llama.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install llama.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-06-01.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/llama.rb
@@ -14,19 +14,17 @@ main() {
     koopa_assert_is_executable "${app[@]}"
     dict['gocache']="$(koopa_init_dir 'gocache')"
     dict['gopath']="$(koopa_init_dir 'go')"
-    dict['name']='llama'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     export GOCACHE="${dict['gocache']}"
     export GOPATH="${dict['gopath']}"
-    dict['file']="v${dict['version']}.tar.gz"
-    dict['url']="https://github.com/antonmedv/${dict['name']}/archive/refs/\
-tags/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
-    dict['ldflags']='-s -w'
+    dict['url']="https://github.com/antonmedv/llama/archive/refs/tags/\
+v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
     koopa_print_env
+    dict['ldflags']='-s -w'
     "${app['go']}" build \
         -ldflags "${dict['ldflags']}" \
         -o "${dict['prefix']}/bin/${dict['name']}"
