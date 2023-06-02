a59fd02d8f0c079fef0c12cad3cb53bb1434ea6f
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-02 10:44:09 -0400

    Rework installers

--- lang/bash/include/install/common/shared/yq.sh
+++ lang/bash/include/install/common/shared/yq.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install yq.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-06-02.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/yq.rb
@@ -15,16 +15,15 @@ main() {
     koopa_assert_is_executable "${app[@]}"
     dict['gocache']="$(koopa_init_dir 'gocache')"
     dict['gopath']="$(koopa_init_dir 'go')"
-    dict['name']='yq'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     export GOCACHE="${dict['gocache']}"
     export GOPATH="${dict['gopath']}"
-    dict['file']="v${dict['version']}.tar.gz"
-    dict['url']="https://github.com/mikefarah/yq/archive/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_extract "${dict['file']}"
-    koopa_cd "${dict['name']}-${dict['version']}"
+    dict['url']="https://github.com/mikefarah/yq/archive/\
+v${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
     "${app['go']}" build \
         -o "${dict['prefix']}/bin/yq"
     koopa_chmod --recursive 'u+rw' "${dict['gopath']}"
