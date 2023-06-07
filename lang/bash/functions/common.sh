87ff54037851e98098ab6b37508ccad3bb478b38
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-07 13:49:16 -0400

    Add minimap2 locator

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -16004,6 +16004,13 @@ koopa_locate_meson() {
         "$@"
 }
 
+koopa_locate_minimap2() {
+    koopa_locate_app \
+        --app-name='minimap2' \
+        --bin-name='minimap2' \
+        "$@"
+}
+
 koopa_locate_mkdir() {
     koopa_locate_app \
         --app-name='coreutils' \
