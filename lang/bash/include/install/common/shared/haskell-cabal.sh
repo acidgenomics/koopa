eecb6ea2a414041b2062c9e1b85c395dd2136c78
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-12 12:00:21 -0400

    Improve installers

--- lang/bash/include/install/common/shared/haskell-cabal.sh
+++ lang/bash/include/install/common/shared/haskell-cabal.sh
@@ -3,16 +3,15 @@
 main() {
     # """
     # Install Haskell Cabal.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-06-12.
     # """
     local -A app dict
     app['ghcup']="$(koopa_locate_ghcup)"
     koopa_assert_is_executable "${app[@]}"
-    dict['name']='cabal'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     "${app['ghcup']}" install \
-        "${dict['name']}" "${dict['version']}" \
+        'cabal' "${dict['version']}" \
         --isolate "${dict['prefix']}/bin"
     return 0
 }
