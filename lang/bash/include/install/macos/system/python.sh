428f4c4e8d9ded24f4338e440a55dc40ff3e94cc
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-12 10:55:44 -0400

    Improve installers

--- lang/bash/include/install/macos/system/python.sh
+++ lang/bash/include/install/macos/system/python.sh
@@ -3,14 +3,13 @@
 main() {
     # """
     # Install Python framework binary.
-    # @note Updated 2022-12-08.
+    # @note Updated 2023-06-12.
     # """
     local -A app dict
     app['installer']="$(koopa_macos_locate_installer)"
     koopa_assert_is_executable "${app[@]}"
     dict['framework_prefix']='/Library/Frameworks/Python.framework'
     dict['macos_version']="$(koopa_macos_os_version)"
-    dict['name']='python'
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     case "${dict['macos_version']}" in
         '10'*)
@@ -20,27 +19,22 @@ main() {
             dict['macos_string']='macos11'
             ;;
     esac
-    dict['major_version']="$(koopa_major_version "${dict['version']}")"
-    dict['maj_min_version']="$(koopa_major_minor_version "${dict['version']}")"
-    dict['prefix']="${dict['framework_prefix']}/Versions/\
-${dict['maj_min_version']}"
-    dict['file']="${dict['name']}-${dict['version']}-\
-${dict['macos_string']}.pkg"
-    dict['url']="https://www.${dict['name']}.org/ftp/${dict['name']}/\
-${dict['version']}/${dict['file']}"
-    koopa_download "${dict['url']}" "${dict['file']}"
-    koopa_sudo "${app['installer']}" -pkg "${dict['file']}" -target /
-    app['python']="${dict['prefix']}/bin/${dict['name']}\
-${dict['major_version']}"
-    koopa_assert_is_installed "${app['python']}"
-    # Ensure 'python' symlink exists.
+    dict['maj_ver']="$(koopa_major_version "${dict['version']}")"
+    dict['maj_min_ver']="$(koopa_major_minor_version "${dict['version']}")"
+    dict['prefix']="${dict['framework_prefix']}/Versions/${dict['maj_min_ver']}"
+    dict['url']="https://www.python.org/ftp/python/${dict['version']}/\
+python-${dict['version']}-${dict['macos_string']}.pkg"
+    koopa_download "${dict['url']}"
+    koopa_sudo "${app['installer']}" \
+        -pkg "$(koopa_basename "${dict['url']}")" \
+        -target '/'
+    app['python']="${dict['prefix']}/bin/python${dict['maj_ver']}"
+    koopa_assert_is_executable "${app['python']}"
     (
         koopa_cd "${dict['prefix']}/bin"
-        koopa_ln --sudo \
-            "${dict['name']}${dict['major_version']}" "${dict['name']}"
+        koopa_ln --sudo "python${dict['maj_ver']}" 'python'
         koopa_cd '/usr/local/bin'
-        koopa_ln --sudo \
-            "${dict['name']}${dict['major_version']}" "${dict['name']}"
+        koopa_ln --sudo "python${dict['maj_ver']}" 'python'
     )
     return 0
 }
