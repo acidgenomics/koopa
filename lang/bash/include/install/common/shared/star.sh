33166dac950c9f1236a56226e62dc52621aa410f
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-16 14:09:27 -0400

    Add support for building STAR from source

--- lang/bash/include/install/common/shared/star.sh
+++ lang/bash/include/install/common/shared/star.sh
@@ -1,7 +1,40 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_install_app_subshell \
-        --installer='conda-env' \
-        --name='star'
+    # """
+    # Install STAR.
+    # @note Updated 2023-06-16.
+    # @seealso
+    # - https://github.com/alexdobin/STAR/
+    # - https://github.com/bioconda/bioconda-recipes/tree/master/recipes/star
+    # """
+    local -A app
+    local -a make_args
+    koopa_activate_app --build-only 'gcc' 'make'
+    app['gcc']="$(koopa_locate_gcc --realpath)"
+    app['make']="$(koopa_locate_make)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['jobs']="$(koopa_cpu_count)"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['url']="https://github.com/alexdobin/STAR/archive/\
+${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src/source'
+    make_args+=(
+        "--jobs=${dict['jobs']}"
+        "CXX=${app['gcc']}"
+        'VERBOSE=1'
+    )
+    if koopa_is_macos
+    then
+        make_args+=('STARforMacStatic' 'STARlongForMacStatic')
+    else
+        make_args+=('STAR' 'STARlong')
+    fi
+    koopa_chmod +x 'STAR' 'STARlong'
+    koopa_cp 'STAR' "${dict['prefix']}/bin/STAR"
+    koopa_cp 'STARlong' "${dict['prefix']}/bin/STARlong"
+    return 0
 }
