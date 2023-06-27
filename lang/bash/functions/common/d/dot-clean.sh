b71876cf34611e926b21431da0cb06b773d71618
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-27 16:23:51 -0400

    Rework using koopa_find

--- lang/bash/functions/common/d/dot-clean.sh
+++ lang/bash/functions/common/d/dot-clean.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-# FIXME Rework using koopa_find instead of fd.
-
 koopa_dot_clean() {
     # """
     # Clean up dot files recursively inside a directory.
-    # @note Updated 2023-05-24.
+    # @note Updated 2023-06-27.
     # """
     local -A app dict
     local -a basenames cruft files
     local i
     koopa_assert_has_args_eq "$#" 1
-    app['fd']="$(koopa_locate_fd)"
-    app['rm']="$(koopa_locate_rm --allow-system)"
-    koopa_assert_is_executable "${app[@]}"
     dict['prefix']="${1:?}"
     koopa_assert_is_dir "${dict['prefix']}"
     dict['prefix']="$(koopa_realpath "${dict['prefix']}")"
-    # > koopa_alert "Cleaning dot files in '${dict['prefix']}'."
     if koopa_is_macos
     then
         app['dot_clean']="$(koopa_macos_locate_dot_clean)"
@@ -26,16 +20,13 @@ koopa_dot_clean() {
         "${app['dot_clean']}" "${dict['prefix']}"
     fi
     readarray -t files <<< "$( \
-        "${app['fd']}" \
-            --absolute-path \
-            --base-directory="${dict['prefix']}" \
-            --glob \
+        koopa_find \
             --hidden \
-            '.*' \
+            --pattern='.*' \
+            --prefix="${dict['prefix']}" \
     )"
     if koopa_is_array_empty "${files[@]}"
     then
-        # > koopa_alert_success "Dot files cleaned in '${dict['prefix']}'."
         return 0
     fi
     cruft=()
@@ -65,6 +56,5 @@ koopa_dot_clean() {
         koopa_print "${cruft[@]}"
         return 1
     fi
-    # > koopa_alert_success "Dot files cleaned in '${dict['prefix']}'."
     return 0
 }
