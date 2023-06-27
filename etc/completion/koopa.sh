6db2333cac2f857a665c11d8b11025a2c8d72b20
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-27 09:21:17 -0400

    Update R dependencies and work on adding support for libaec (dorado)

--- etc/completion/koopa.sh
+++ etc/completion/koopa.sh
@@ -4,7 +4,7 @@
 _koopa_complete() {
     # """
     # Bash/Zsh TAB completion for primary 'koopa' program.
-    # @note Updated 2023-06-16.
+    # @note Updated 2023-06-27.
     #
     # Keep all of these commands in a single file.
     # Sourcing multiple scripts doesn't work reliably.
@@ -229,6 +229,7 @@ _koopa_complete() {
                         'latch'
                         'ldns'
                         'lesspipe'
+                        'libaec'
                         'libarchive'
                         'libassuan'
                         'libcbor'
