597a10067cfcbd8dcdbd15f2206f8c1468aa0e1f
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-27 12:17:26 -0400

    Relax default editor

--- lang/sh/functions.sh
+++ lang/sh/functions.sh
@@ -1545,7 +1545,10 @@ _koopa_emacs() {
 _koopa_export_editor() {
     if [ -z "${EDITOR:-}" ]
     then
-        EDITOR="$(_koopa_bin_prefix)/vim"
+        __kvar_editor="$(_koopa_bin_prefix)/vim"
+        [ -x "$__kvar_editor" ] || __kvar_editor='vim'
+        EDITOR="$__kvar_editor"
+        unset -v __kvar_editor
     fi
     VISUAL="$EDITOR"
     export EDITOR VISUAL
