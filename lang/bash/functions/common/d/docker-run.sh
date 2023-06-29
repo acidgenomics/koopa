89b9a7f07fecfc878cd3f20a8768d360b8cca04f
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-29 07:33:52 -0400

    Relax pull authentication requirement for AWS ECR public images

--- lang/bash/functions/common/d/docker-run.sh
+++ lang/bash/functions/common/d/docker-run.sh
@@ -3,7 +3,7 @@
 koopa_docker_run() {
     # """
     # Run Docker image.
-    # @note Updated 2023-06-01.
+    # @note Updated 2023-06-29.
     #
     # No longer using bind mounts by default.
     # Use named volumes, which have better cross-platform compatiblity, instead.
@@ -11,6 +11,8 @@ koopa_docker_run() {
     # @seealso
     # - https://docs.docker.com/storage/volumes/
     # - https://docs.docker.com/storage/bind-mounts/
+    # - https://docs.aws.amazon.com/AmazonECR/latest/public/
+    #     docker-pull-ecr-image.html
     # """
     local -A app dict
     local -a pos run_args
@@ -58,10 +60,15 @@ koopa_docker_run() {
     dict['image']="${1:?}"
     case "${dict['image']}" in
         *'.dkr.ecr.'*'.amazonaws.com/'*)
+            # Requiring authenticated pulls here.
             koopa_aws_ecr_login_private
             ;;
         'public.ecr.aws/'*)
-            koopa_aws_ecr_login_public
+            # Allowing unauthenticated pulls here.
+            if [[ -n "${AWS_ECR_PROFILE:-}" ]]
+            then
+                koopa_aws_ecr_login_public
+            fi
             ;;
     esac
     "${app['docker']}" pull "${dict['image']}"
