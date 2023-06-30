4a2b801a387c8e36143a164f9610a33a07833aba
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-29 20:40:16 -0400

    Only define aliases when relevant programs are installed

--- lang/sh/functions/a/activate-pyenv.sh
+++ lang/sh/functions/a/activate-pyenv.sh
@@ -3,7 +3,7 @@
 _koopa_activate_pyenv() {
     # """
     # Activate Python version manager (pyenv).
-    # @note Updated 2023-03-10.
+    # @note Updated 2023-06-29.
     # """
     [ -n "${PYENV_ROOT:-}" ] && return 0
     __kvar_prefix="$(_koopa_pyenv_prefix)"
@@ -20,6 +20,7 @@ _koopa_activate_pyenv() {
             __kvar_pyenv
         return 0
     fi
+    _koopa_is_alias 'pyenv' && unalias 'pyenv'
     export PYENV_ROOT="$__kvar_prefix"
     __kvar_nounset="$(_koopa_boolean_nounset)"
     [ "$__kvar_nounset" -eq 1 ] && set +o nounset
