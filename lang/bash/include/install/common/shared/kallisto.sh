158cdfc2e3d348682aada38bc216694cc6a3dc95
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-07 09:59:59 -0400

    Attempt to patch zlib config

--- lang/bash/include/install/common/shared/kallisto.sh
+++ lang/bash/include/install/common/shared/kallisto.sh
@@ -21,9 +21,11 @@ main() {
     # """
     local -A app cmake dict
     local -a cmake_args
-    koopa_activate_app --build-only 'autoconf' 'automake'
+    koopa_activate_app --build-only 'autoconf' 'automake' 'patch'
     koopa_activate_app 'bzip2' 'hdf5' 'xz' 'zlib'
     app['autoreconf']="$(koopa_locate_autoreconf)"
+    app['cat']="$(koopa_locate_cat --allow-system)"
+    app['patch']="$(koopa_locate_patch)"
     app['sed']="$(koopa_locate_sed --allow-system)"
     koopa_assert_is_executable "${app[@]}"
     dict['bzip2']="$(koopa_app_prefix 'bzip2')"
@@ -43,6 +45,7 @@ main() {
         "${cmake['zlib_library']}"
     cmake_args=(
         # Build options --------------------------------------------------------
+        '-DUSE_BAM=ON'
         '-DUSE_HDF5=ON'
         # Dependency paths -----------------------------------------------------
         "-DBZIP2_INCLUDE_DIR=${cmake['bzip2_include_dir']}"
@@ -54,6 +57,28 @@ main() {
 v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    # This patch step is needed for bifrost to pick up zlib correctly.
+    # https://github.com/pachterlab/kallisto/issues/385
+    # Patch diff created with:
+    # > diff -u 'CMakeLists.txt' 'CMakeLists-1.txt' > 'patch-cmakelists.patch'
+    "${app['cat']}" << END > 'patch-cmakelists.patch'
+--- CMakeLists.txt	2023-07-07 09:52:04
++++ CMakeLists-1.txt	2023-07-07 09:54:44
+@@ -72,7 +72,7 @@
+     PREFIX ${PROJECT_SOURCE_DIR}/ext/bifrost
+     SOURCE_DIR ${PROJECT_SOURCE_DIR}/ext/bifrost
+     BUILD_IN_SOURCE 1
+-    CONFIGURE_COMMAND mkdir -p build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_FLAGS=${PROJECT_BIFROST_CMAKE_CXX_FLAGS}
++    CONFIGURE_COMMAND mkdir -p build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_FLAGS=${PROJECT_BIFROST_CMAKE_CXX_FLAGS} -DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIR} -DZLIB_LIBRARY={ZLIB_LIBRARY}
+     BUILD_COMMAND cd build && make
+     INSTALL_COMMAND ""
+ )
+END
+    "${app['patch']}" \
+        --unified \
+        --verbose \
+        'src/CMakeLists.txt' \
+        'patch-cmakelists.patch'
     # This patch step is needed for autoconf 2.69 compatibility.
     # https://github.com/pachterlab/kallisto/issues/303#issuecomment-884612169
     (
