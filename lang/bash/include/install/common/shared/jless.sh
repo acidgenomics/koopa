b1d737c743024f7209c53b3207471dee9bc2670c
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-17 15:26:00 -0400

    Fix jless build recipe on Linux

--- lang/bash/include/install/common/shared/jless.sh
+++ lang/bash/include/install/common/shared/jless.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME This results in shared library issue on Linux argh...this is due
-# to RUSTFLAGS not baking in the lib path correctly.
-
-# jless: error while loading shared libraries: libxcb-shape.so.0: cannot open shared object file: No such file or directory
-
 main() {
     # """
     # Install jless.
