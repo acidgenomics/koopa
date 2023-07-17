dc700d49ac2d5279463e633315bb21500094e549
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-17 14:46:00 -0400

    Fix jless build recipe on Linux

--- lang/bash/include/install/common/shared/jless.sh
+++ lang/bash/include/install/common/shared/jless.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 main() {
+    # """
+    # Install jless.
+    # @note Updated 2023-07-17.
+    #
+    # @seealso
+    # - https://formulae.brew.sh/formula/jless
+    # """
+    if koopa_is_linux
+    then
+        koopa_activate_app --build-only 'python3.11'
+        koopa_activate_app 'xorg-libxcb'
+    fi
     koopa_install_app_subshell \
         --installer='rust-package' \
         --name='jless'
