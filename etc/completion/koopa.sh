b9a19f63abad07e6b072d10e4c59360900da755d
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-17 07:57:39 -0400

    Add support for jless

--- etc/completion/koopa.sh
+++ etc/completion/koopa.sh
@@ -4,7 +4,7 @@
 _koopa_complete() {
     # """
     # Bash/Zsh TAB completion for primary 'koopa' program.
-    # @note Updated 2023-06-29.
+    # @note Updated 2023-07-17.
     #
     # Keep all of these commands in a single file.
     # Sourcing multiple scripts doesn't work reliably.
@@ -217,6 +217,7 @@ _koopa_complete() {
                         'isl'
                         'isort'
                         'jemalloc'
+                        'jless'
                         'jpeg'
                         'jq'
                         'julia'
