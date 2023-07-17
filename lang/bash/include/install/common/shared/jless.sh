86e374012d834500d2cd6556975dd05e9bc4e4ba
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-17 15:08:02 -0400

    Rework rustflag handling

--- lang/bash/include/install/common/shared/jless.sh
+++ lang/bash/include/install/common/shared/jless.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Need to use RUSTFLAGS to pass these correctly.
-
-# FIXME Hitting this build error on Linux:
-#1" "-nodefaultlibs"
-#  = note: /usr/bin/ld: cannot find -lxcb: No such file or directory
-#          /usr/bin/ld: cannot find -lxcb-render: No such file or directory
-#          /usr/bin/ld: cannot find -lxcb-shape: No such file or directory
-#          /usr/bin/ld: cannot find -lxcb-xfixes: No such file or directory
-#          collect2: error: ld returned 1 exit status
-
 main() {
     # """
     # Install jless.
@@ -20,7 +10,6 @@ main() {
     # """
     if koopa_is_linux
     then
-        koopa_activate_app --build-only 'python3.11'
         koopa_activate_app \
             'xorg-xorgproto' \
             'xorg-libxau' \
