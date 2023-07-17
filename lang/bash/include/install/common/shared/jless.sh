84d49828b3cb417fc9234045f074b01b8c74d9fd
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-17 15:00:05 -0400

    Rework comments

--- lang/bash/include/install/common/shared/jless.sh
+++ lang/bash/include/install/common/shared/jless.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 # FIXME Need to use RUSTFLAGS to pass these correctly.
-# https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-reads
-# https://doc.rust-lang.org/cargo/reference/config.html#buildrustflags
-# https://github.com/Homebrew/brew/blob/7ee069ef47e4ea7c2bff705847e9ef647b4e5da3/Library/Homebrew/formula.rb#L1555
-# https://internals.rust-lang.org/t/compiling-rustc-with-non-standard-flags/8950/6
 
 # FIXME Hitting this build error on Linux:
 #1" "-nodefaultlibs"
@@ -30,8 +26,6 @@ main() {
             'xorg-libxau' \
             'xorg-libxdmcp' \
             'xorg-libxcb'
-        RUSTFLAGS="${LDFLAGS:-}"
-        export RUSTFLAGS
     fi
     koopa_install_app_subshell \
         --installer='rust-package' \
