c70018145387a80b41789dc68789fecdda19526b
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-18 12:31:32 -0400

    Initial commit of EC2 identifier to name mapping

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -1754,6 +1754,36 @@ koopa_aws_ec2_instance_id() {
     return 0
 }
 
+koopa_aws_ec2_map_instance_ids_to_names() {
+    local -A app dict
+    local -a ids names out
+    app['aws']="$(koopa_locate_aws)"
+    app['jq']="$(koopa_locate_jq)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['json']="$( \
+        "${app['aws']}" ec2 describe-instances \
+            --output 'json' \
+    )"
+    readarray -t ids <<< "$( \
+        koopa_print "${dict['json']}" \
+        | "${app['jq']}" --raw-output \
+            '.Reservations[] | "\(.Instances[].InstanceId)"' \
+    )"
+    koopa_assert_is_array_non_empty "${ids[@]}"
+    readarray -t names <<< "$( \
+        koopa_print "${dict['json']}" \
+        | "${app['jq']}" --raw-output \
+            '.Reservations[] | "\(.Instances[].Tags[0].Value)"' \
+    )"
+    koopa_assert_is_array_non_empty "${names[@]}"
+    for i in "${!ids[@]}"
+    do
+        out+=("${ids[$i]} : ${names[$i]}")
+    done
+    koopa_print "${out[@]}"
+    return 0
+}
+
 koopa_aws_ec2_suspend() {
     local -A app dict
     app['aws']="$(koopa_locate_aws)"
