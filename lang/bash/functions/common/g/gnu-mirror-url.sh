8cbfb706aeb4ff01691b2d0c58bad639d514022a
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-31 09:29:40 -0400

    Switch GNU mirror back to recommended default

--- lang/bash/functions/common/g/gnu-mirror-url.sh
+++ lang/bash/functions/common/g/gnu-mirror-url.sh
@@ -3,7 +3,7 @@
 koopa_gnu_mirror_url() {
     # """
     # Get GNU FTP mirror URL.
-    # @note Updated 2023-07-30.
+    # @note Updated 2023-07-31.
     #
     # Servers:
     # - https://ftpmirror.gnu.org (primary mirror)
@@ -16,7 +16,7 @@ koopa_gnu_mirror_url() {
     # """
     local server
     koopa_assert_has_no_args "$#"
-    server='https://gnu.mirror.constant.com'
+    server='https://ftpmirror.gnu.org'
     koopa_print "$server"
     return 0
 }
