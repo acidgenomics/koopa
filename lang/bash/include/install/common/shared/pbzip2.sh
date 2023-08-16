6b5ff1addcb5cd16fa7c5c0d2e0c7b428fcbd005
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-16 16:17:59 -0400

    Add pbzip2 support

---
+++ lang/bash/include/install/common/shared/pbzip2.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+main() {
+    # """
+    # Install pbzip2.
+    # @note Updated 2023-08-16.
+    #
+    # @seealso
+    # - https://github.com/conda-forge/pbzip2-feedstock
+    # - https://formulae.brew.sh/formula/pbzip2
+    # """
+    local -A app dict
+    koopa_activate_app --build-only 'make'
+    koopa_activate_app 'bzip2'
+    app['make']="$(koopa_locate_make)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['jobs']="$(koopa_cpu_count)"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['maj_min_ver']="$(koopa_major_minor_version "${dict['version']}")"
+    dict['url']="https://launchpad.net/pbzip2/${dict['maj_min_ver']}/\
+${dict['version']}/+download/pbzip2-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_print_env
+    "${app['make']}" \
+        --jobs="${dict['jobs']}" \
+        CC='/usr/bin/gcc' \
+        CFLAGS="${CFLAGS:-}" \
+        LDFLAGS="${LDFLAGS:-}" \
+        PREFIX="${dict['prefix']}" \
+        VERBOSE=1 \
+        install
+    return 0
+}
