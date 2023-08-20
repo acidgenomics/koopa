23f35155e5b0a679f28b43b33b258f4b82212375
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-20 11:54:03 -0400

    Revert compiler back to gcc on macOS

--- lang/bash/include/install/common/shared/sambamba.sh
+++ lang/bash/include/install/common/shared/sambamba.sh
@@ -13,11 +13,11 @@ main() {
     # """
     local -A app dict
     local -a build_deps deps
-    build_deps=('ldc' 'make' 'python3.11')
+    build_deps=('gcc' 'ldc' 'make' 'python3.11')
     deps=('bzip2' 'lz4' 'xz' 'zlib')
     koopa_activate_app --build-only "${build_deps[@]}"
     koopa_activate_app "${deps[@]}"
-    app['cc']="$(koopa_locate_cc)"
+    app['cc']="$(koopa_locate_gcc)"
     app['make']="$(koopa_locate_make)"
     app['sed']="$(koopa_locate_sed --allow-system)"
     koopa_assert_is_executable "${app[@]}"
@@ -31,23 +31,28 @@ v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
+    # Ensure that we're using our compiler and not baked in 'gcc'.
     koopa_find_and_replace_in_file \
         --pattern='^(CC=gcc)$' \
         --regex \
         --replacement='# \1' \
         'Makefile'
-    koopa_find_and_replace_in_file \
-        --pattern='^(LDFLAGS     = -L=-flto=full)$' \
-        --regex \
-        --replacement='# \1' \
-        'Makefile'
+    # Clang doesn't currently support 'flto=full'.
+    # > koopa_find_and_replace_in_file \
+    # >     --pattern='^(LDFLAGS     = -L=-flto=full)$' \
+    # >     --regex \
+    # >     --replacement='# \1' \
+    # >     'Makefile'
+    koopa_print_env
     "${app['make']}" \
         CC="$CC" \
         LIBRARY_PATH="$LIBRARY_PATH" \
         VERBOSE=1 \
         release
-    koopa_print_env
-    "${app['make']}" check
+    if ! koopa_is_aarch64
+    then
+        "${app['make']}" check
+    fi
     koopa_cp \
         "bin/sambamba-${dict['version']}" \
         "${dict['prefix']}/bin/sambamba"
