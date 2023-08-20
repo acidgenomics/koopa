0b63275a54b413ed4209fc1d0506457f0f55f1f6
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-20 12:14:32 -0400

    Rework sambamba installer

--- lang/bash/functions/common/l/locate-cc.sh
+++ lang/bash/functions/common/l/locate-cc.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
 koopa_locate_cc() {
-    local str
-    if koopa_is_macos
-    then
-        str='/usr/bin/clang'
-    else
-        str='/usr/bin/gcc'
-    fi
-    koopa_locate_app "$str"
+    koopa_locate_app '/usr/bin/gcc'
 }
--- lang/bash/include/install/common/shared/sambamba.sh
+++ lang/bash/include/install/common/shared/sambamba.sh
@@ -19,7 +19,6 @@ main() {
     koopa_activate_app "${deps[@]}"
     app['cc']="$(koopa_locate_gcc)"
     app['make']="$(koopa_locate_make)"
-    app['sed']="$(koopa_locate_sed --allow-system)"
     koopa_assert_is_executable "${app[@]}"
     dict['ldc']="$(koopa_app_prefix 'ldc')"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
@@ -31,18 +30,11 @@ v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
-    # Ensure that we're using our compiler and not baked in 'gcc'.
     koopa_find_and_replace_in_file \
         --pattern='^(CC=gcc)$' \
         --regex \
         --replacement='# \1' \
         'Makefile'
-    # Clang doesn't currently support 'flto=full'.
-    # > koopa_find_and_replace_in_file \
-    # >     --pattern='^(LDFLAGS     = -L=-flto=full)$' \
-    # >     --regex \
-    # >     --replacement='# \1' \
-    # >     'Makefile'
     koopa_print_env
     "${app['make']}" \
         CC="$CC" \
