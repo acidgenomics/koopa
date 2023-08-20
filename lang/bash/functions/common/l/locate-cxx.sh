73ffd57bd7d147bfc27719cce2201f1877437e47
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-20 12:14:32 -0400

    Rework sambamba installer

--- lang/bash/functions/common/l/locate-cxx.sh
+++ lang/bash/functions/common/l/locate-cxx.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
 koopa_locate_cxx() {
-    local str
-    if koopa_is_macos
-    then
-        str='/usr/bin/clang++'
-    else
-        str='/usr/bin/g++'
-    fi
-    koopa_locate_app "$str"
+    koopa_locate_app '/usr/bin/g++'
 }
