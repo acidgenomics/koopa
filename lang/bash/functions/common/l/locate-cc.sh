bd339f44fdf96787b6ebf3c6a262c81863b1c12a
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-21 08:05:13 -0400

    Attempt to fix sambamba build with clang

--- lang/bash/functions/common/l/locate-cc.sh
+++ lang/bash/functions/common/l/locate-cc.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 koopa_locate_cc() {
-    koopa_locate_app '/usr/bin/gcc'
+    local str
+    if koopa_is_macos
+    then
+        str='/usr/bin/clang'
+    else
+        str='/usr/bin/gcc'
+    fi
+    koopa_locate_app "$str"
 }
