8a9e2359b2b5873981593d246775c3145f13c1ff
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-21 14:48:45 -0400

    Rework python install variable handling

--- lang/bash/include/install/common/shared/luigi.sh
+++ lang/bash/include/install/common/shared/luigi.sh
@@ -3,5 +3,6 @@
 main() {
     koopa_install_app_subshell \
         --installer='python-venv' \
-        --name='luigi'
+        --name='luigi' \
+        -D --pip-name='luigi[toml]'
 }
--- lang/bash/include/install/common/shared/ranger-fm.sh
+++ lang/bash/include/install/common/shared/ranger-fm.sh
@@ -3,5 +3,6 @@
 main() {
     koopa_install_app_subshell \
         --installer='python-venv' \
-        --name='ranger-fm'
+        --name='ranger-fm' \
+        -D --package-name='ranger_fm'
 }
