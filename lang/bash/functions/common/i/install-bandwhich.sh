1819aba89c5456ce551cdfc8cffeb9e552798044
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-24 09:56:33 -0400

    Disable bandwhich install

--- lang/bash/functions/common/i/install-bandwhich.sh
+++ lang/bash/functions/common/i/install-bandwhich.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-koopa_install_bandwhich() {
-    koopa_install_app \
-        --name='bandwhich' \
-        "$@"
-}
+# NOTE This app is currently abandoned and no longer builds with Rust.
+# https://github.com/imsnif/bandwhich/issues/264
+
+# > koopa_install_bandwhich() {
+# >     koopa_install_app \
+# >         --name='bandwhich' \
+# >         "$@"
+# > }
