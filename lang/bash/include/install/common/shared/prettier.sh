dd3a0e5aea1e952cb2c8bb9ef86f98657392cf3f
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-25 09:30:42 -0400

    Rework prettier install

--- lang/bash/include/install/common/shared/prettier.sh
+++ lang/bash/include/install/common/shared/prettier.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# FIXME prettier isn't detecting globally installed plugins correctly:
+# prettier --plugin '/opt/koopa/opt/prettier/lib/node_modules/prettier-plugin-sort-json/dist/index.js' --help
+
 main() {
     koopa_install_app_subshell \
         --installer='node-package' \
