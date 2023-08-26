7612aa3745e9411fef5b9ae2957881d543274efd
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-26 09:16:38 -0400

    Change directory for bioconda autobump

--- lang/bash/functions/common/b/bioconda-autobump-recipe.sh
+++ lang/bash/functions/common/b/bioconda-autobump-recipe.sh
@@ -3,7 +3,7 @@
 koopa_bioconda_autobump_recipe() {
     # """
     # Edit a Bioconda autobump recipe.
-    # @note Updated 2023-01-12.
+    # @note Updated 2023-08-26.
     # """
     local -A app dict
     koopa_assert_has_args_eq "$#" 1
@@ -14,20 +14,18 @@ koopa_bioconda_autobump_recipe() {
     dict['repo']="${HOME:?}/git/github/bioconda/bioconda-recipes"
     dict['branch']="${dict['recipe']/-/_}"
     koopa_assert_is_dir "${dict['repo']}"
-    (
-        koopa_cd "${dict['repo']}"
-        "${app['git']}" checkout master
-        "${app['git']}" fetch --all
-        "${app['git']}" pull
-        # Autobump branch:
-        "${app['git']}" checkout \
-            -B "${dict['branch']}" \
-            "origin/bump/${dict['branch']}"
-        # Or create a new branch:
-        # > "${app['git']}" checkout -B "$branch"
-        "${app['git']}" pull origin master
-        koopa_mkdir "recipes/${dict['recipe']}"
-        "${app['vim']}" "recipes/${dict['recipe']}/meta.yaml"
-    )
+    koopa_cd "${dict['repo']}"
+    "${app['git']}" checkout master
+    "${app['git']}" fetch --all
+    "${app['git']}" pull
+    # Autobump branch:
+    "${app['git']}" checkout \
+        -B "${dict['branch']}" \
+        "origin/bump/${dict['branch']}"
+    # Or create a new branch:
+    # > "${app['git']}" checkout -B "$branch"
+    "${app['git']}" pull origin master
+    koopa_mkdir "recipes/${dict['recipe']}"
+    "${app['vim']}" "recipes/${dict['recipe']}/meta.yaml"
     return 0
 }
