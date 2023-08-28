04cc0487f447e35392c4e204402cf348a6075af7
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-28 10:50:30 -0400

    Update ripgrep installer

--- lang/bash/include/install/common/shared/ripgrep.sh
+++ lang/bash/include/install/common/shared/ripgrep.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 main() {
+    # """
+    # Install ripgrep.
+    # @note Updated 2023-08-28.
+    # """
     koopa_activate_app 'pcre2'
     koopa_install_app_subshell \
         --installer='rust-package' \
-        --name='ripgrep'
+        --name='ripgrep' \
+        -D '--features=pcre2'
 }
