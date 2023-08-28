e2ac7d927b51e9913f8b929455cb7207759bdffd
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-28 09:32:46 -0400

    Simplify diff-so-fancy install

--- lang/bash/include/install/common/shared/diff-so-fancy.sh
+++ lang/bash/include/install/common/shared/diff-so-fancy.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-# FIXME Rework to build this from source.
-
 main() {
     # """
     # Install diff-so-fancy.
-    # @note Updated 2023-06-12.
+    # @note Updated 2023-08-28.
+    #
+    # @seealso
+    # - https://formulae.brew.sh/formula/diff-so-fancy
     # """
     local -A dict
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['libexec']="${dict['prefix']}/libexec"
     dict['url']="https://github.com/so-fancy/diff-so-fancy/archive/refs/\
 tags/v${dict['version']}.tar.gz"
+    koopa_mkdir \
+        "${dict['prefix']}/bin" \
+        "${dict['prefix']}/libexec"
     koopa_download "${dict['url']}"
-    koopa_extract \
-        "$(koopa_basename "${dict['url']}")" \
-        "${dict['libexec']}"
-    koopa_mkdir "${dict['prefix']}/bin"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_cp \
+        --target-directory="${dict['prefix']}/libexec" \
+        'diff-so-fancy' \
+        'lib'
     (
         koopa_cd "${dict['prefix']}/bin"
         koopa_ln '../libexec/diff-so-fancy' 'diff-so-fancy'
