fe525a4d40519a3e024446199d1e0672060f11cc
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-28 10:51:24 -0400

    Update tuc installer

--- lang/bash/include/install/common/shared/tuc.sh
+++ lang/bash/include/install/common/shared/tuc.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 main() {
+    # """
+    # Install tuc.
+    # @note Updated 2023-08-28.
+    # """
     koopa_install_app_subshell \
         --installer='rust-package' \
-        --name='tuc'
+        --name='tuc' \
+        -D '--features=regex'
 }
