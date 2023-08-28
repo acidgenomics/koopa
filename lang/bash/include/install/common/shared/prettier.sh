6dcff9c01267b25819b533e8cf292f9ba46cabac
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-28 08:46:53 -0400

    Prettier global extensions no longer recommended

--- lang/bash/include/install/common/shared/prettier.sh
+++ lang/bash/include/install/common/shared/prettier.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# FIXME prettier isn't detecting globally installed plugins correctly:
-# prettier --plugin '/opt/koopa/opt/prettier/lib/node_modules/prettier-plugin-sort-json/dist/index.js' --help
-
 main() {
     koopa_install_app_subshell \
         --installer='node-package' \
         --name='prettier' \
-        -D 'prettier-plugin-sort-json@3.0.0'
+        -D 'prettier-plugin-sort-json@3.0.1'
 }
