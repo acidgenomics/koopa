034bec6fca332fc80509742709eafcfa079ffd09
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-28 10:34:19 -0400

    Need to install from git, as cargo is out of date

--- lang/bash/include/install/common/shared/lsd.sh
+++ lang/bash/include/install/common/shared/lsd.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 main() {
+    local version
+    version="${KOOPA_INSTALL_VERSION:?}"
     koopa_install_app_subshell \
         --installer='rust-package' \
-        --name='lsd'
+        --name='lsd' \
+        -D '--git=https://github.com/lsd-rs/lsd.git' \
+        -D "--tag=v${version}"
 }
