e37873821cbeeaf4f1aa4e2c12b3f6fa5d513a00
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-28 10:38:11 -0400

    Update ripgrep-all

--- lang/bash/include/install/common/shared/ripgrep-all.sh
+++ lang/bash/include/install/common/shared/ripgrep-all.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 main() {
+    local version
+    version="${KOOPA_INSTALL_VERSION:?}"
     koopa_install_app_subshell \
         --installer='rust-package' \
-        --name='ripgrep-all'
+        --name='ripgrep-all' \
+        -D '--cargo-name=ripgrep_all' \
+        -D '--git=https://github.com/phiresky/ripgrep-all.git' \
+        -D "--tag=v${version}"
 }
