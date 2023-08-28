d756300d242939a927c91e77edfaa0e599eb97ad
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-28 10:50:51 -0400

    Update starship installer

--- lang/bash/include/install/common/shared/starship.sh
+++ lang/bash/include/install/common/shared/starship.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 main() {
+    # Install starship.
+    # @note Updated 2023-08-28.
+    # ""
     koopa_activate_app --build-only 'cmake'
     koopa_install_app_subshell \
         --installer='rust-package' \
