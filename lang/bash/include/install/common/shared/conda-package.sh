c82d757ff07936f0e4e10175d6debddaf4f97488
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 09:18:23 -0400

    Rework autodock approach

---
+++ lang/bash/include/install/common/shared/conda-package.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+main() {
+    local -A dict
+    dict['name']="${KOOPA_INSTALL_NAME:?}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    koopa_install_conda_package \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --version="${dict['version']}"
+    return 0
+}
