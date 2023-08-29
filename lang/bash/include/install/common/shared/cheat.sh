b2f51f2ede1ffd2aac90b1120489be96b161e7c3
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 13:19:27 -0400

    Rework go package install approach

--- lang/bash/include/install/common/shared/cheat.sh
+++ lang/bash/include/install/common/shared/cheat.sh
@@ -3,21 +3,24 @@
 main() {
     # """
     # Install cheat.
-    # @note Updated 2023-08-28.
+    # @note Updated 2023-08-29.
     #
     # @seealso
     # - https://github.com/cheat/cheat/
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/cheat.rb
     # """
     local -A dict
+    dict['name']="${KOOPA_INSTALL_NAME:?}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['url']="https://github.com/cheat/cheat/archive/refs/tags/\
 ${dict['version']}.tar.gz"
-    koopa_install_app_subshell \
-        --installer='go-package' \
-        --name='cheat' \
-        -D '--build-cmd=./cmd/cheat' \
-        -D '--mod=vendor' \
-        -D "--url=${dict['url']}"
+    koopa_install_go_package \
+        --build-cmd='./cmd/cheat' \
+        --mod='vendor' \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --url="${dict['url']}" \
+        --version="${dict['version']}"
     return 0
 }
--- lang/bash/include/install/common/shared/go-package.sh
+++ lang/bash/include/install/common/shared/go-package.sh
@@ -1,113 +1,13 @@
 #!/usr/bin/env bash
 
 main() {
-    # """
-    # Install a Go package.
-    # @note Updated 2023-08-28.
-    # """
-    local -A app dict
-    local -a build_args pos
-    koopa_activate_app --build-only 'go'
-    app['go']="$(koopa_locate_go)"
-    koopa_assert_is_executable "${app[@]}"
-    dict['gocache']="$(koopa_init_dir 'gocache')"
-    dict['gopath']="$(koopa_init_dir 'go')"
+    local -A dict
     dict['name']="${KOOPA_INSTALL_NAME:?}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['bin_name']=''
-    dict['build_cmd']=''
-    dict['ldflags']=''
-    dict['mod']=''
-    dict['tags']=''
-    dict['url']=''
-    pos=()
-    while (("$#"))
-    do
-        case "$1" in
-            # Key value pairs --------------------------------------------------
-            '--bin-name='*)
-                dict['bin_name']="${1#*=}"
-                shift 1
-                ;;
-            '--bin-name')
-                dict['bin_name']="${2:?}"
-                shift 2
-                ;;
-            '--build-cmd='*)
-                dict['build_cmd']="${1#*=}"
-                shift 1
-                ;;
-            '--build-cmd')
-                dict['build_cmd']="${2:?}"
-                shift 2
-                ;;
-            '--ldflags='*)
-                dict['ldflags']="${1#*=}"
-                shift 1
-                ;;
-            '--ldflags')
-                dict['ldflags']="${2:?}"
-                shift 2
-                ;;
-            '--mod='*)
-                dict['mod']="${1#*=}"
-                shift 1
-                ;;
-            '--mod')
-                dict['mod']="${2:?}"
-                shift 2
-                ;;
-            '--tags='*)
-                dict['tags']="${1#*=}"
-                shift 1
-                ;;
-            '--tags')
-                dict['tags']="${2:?}"
-                shift 2
-                ;;
-            '--url='*)
-                dict['url']="${1#*=}"
-                shift 1
-                ;;
-            '--url')
-                dict['url']="${2:?}"
-                shift 2
-                ;;
-            # Other ------------------------------------------------------------
-            *)
-                koopa_invalid_arg "$1"
-                ;;
-        esac
-    done
-    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
-    koopa_assert_is_set '--url' "${dict['url']}"
-    export GOCACHE="${dict['gocache']}"
-    export GOPATH="${dict['gopath']}"
-    [[ -z "${dict['bin_name']}" ]] && dict['bin_name']="${dict['name']}"
-    if [[ -n "${dict['ldflags']}" ]]
-    then
-        build_args+=('-ldflags' "${dict['ldflags']}")
-    fi
-    if [[ -n "${dict['mod']}" ]]
-    then
-        build_args+=('-mod' "${dict['mod']}")
-    fi
-    if [[ -n "${dict['tags']}" ]]
-    then
-        build_args+=('-tags' "${dict['tags']}")
-    fi
-    build_args+=('-o' "${dict['prefix']}/bin/${dict['bin_name']}")
-    if [[ -n "${dict['build_cmd']}" ]]
-    then
-        build_args+=("${dict['build_cmd']}")
-    fi
-    koopa_download "${dict['url']}"
-    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
-    koopa_cd 'src'
-    koopa_print_env
-    koopa_dl 'go build args' "${build_args[*]}"
-    "${app['go']}" build "${build_args[@]}"
-    koopa_chmod --recursive 'u+rw' "${dict['gopath']}"
+    koopa_install_go_package \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --version="${dict['version']}"
     return 0
 }
