08b09c0465583ee0a62242956537739a29c3da99
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 15:22:45 -0400

    Rework rust installers

--- lang/bash/include/install/common/shared/ripgrep-all.sh
+++ lang/bash/include/install/common/shared/ripgrep-all.sh
@@ -6,16 +6,13 @@ main() {
     # @note Updated 2023-08-29.
     # """
     local -A dict
-    dict['name']='ripgrep_all'
-    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
-    dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['git']='https://github.com/phiresky/ripgrep-all.git'
+    dict['name']='ripgrep_all'
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['tag']="v${dict['version']}"
     koopa_install_rust_package \
         --git="${dict['git']}" \
         --name="${dict['name']}" \
-        --prefix="${dict['prefix']}" \
-        --tag="${dict['tag']}" \
-        --version="${dict['version']}"
+        --tag="${dict['tag']}"
     return 0
 }
