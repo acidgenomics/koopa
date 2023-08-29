b30e280a6fb765b2e0cbe9e3c15405767c37ad07
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 14:11:14 -0400

    Rework Perl installers

--- lang/bash/include/install/common/shared/exiftool.sh
+++ lang/bash/include/install/common/shared/exiftool.sh
@@ -6,10 +6,11 @@ main() {
     # https://exiftool.org/history.html
     # """
     local -A dict
+    dict['cpan_path']='EXIFTOOL/Image-ExifTool'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     koopa_install_perl_package \
-        --cpan-path='EXIFTOOL/Image-ExifTool' \
+        --cpan-path="${dict['cpan_path']}" \
         --prefix="${dict['prefix']}" \
         --version="${dict['version']}"
     return 0
