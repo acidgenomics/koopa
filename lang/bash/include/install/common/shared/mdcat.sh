b5c6ae30cdbb574336c965b950071c270dd16bd6
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 15:10:16 -0400

    Rework rust installers

--- lang/bash/include/install/common/shared/mdcat.sh
+++ lang/bash/include/install/common/shared/mdcat.sh
@@ -8,9 +8,9 @@ main() {
     local -A dict
     koopa_activate_app 'openssl3'
     dict['name']="${KOOPA_INSTALL_NAME:?}"
+    dict['openssl']="$(koopa_app_prefix 'openssl3')"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['openssl']="$(koopa_app_prefix 'openssl3')"
     OPENSSL_DIR="${dict['openssl']}"
     export OPENSSL_DIR
     koopa_install_rust_package \
--- lang/bash/include/install/common/shared/ripgrep.sh
+++ lang/bash/include/install/common/shared/ripgrep.sh
@@ -3,11 +3,18 @@
 main() {
     # """
     # Install ripgrep.
-    # @note Updated 2023-08-28.
+    # @note Updated 2023-08-29.
     # """
+    local -A dict
     koopa_activate_app 'pcre2'
-    koopa_install_app_subshell \
-        --installer='rust-package' \
-        --name='ripgrep' \
-        -D '--features=pcre2'
+    dict['features']='pcre2'
+    dict['name']="${KOOPA_INSTALL_NAME:?}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    koopa_install_rust_package \
+        --features="${dict['features']}" \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --version="${dict['version']}"
+    return 0
 }
