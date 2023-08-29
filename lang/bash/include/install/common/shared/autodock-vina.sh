0f354e2548e02d456567e6cd0398e2b60e42e6a1
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 09:13:14 -0400

    Rework installer

--- lang/bash/include/install/common/shared/autodock-vina.sh
+++ lang/bash/include/install/common/shared/autodock-vina.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_install_app_subshell \
-        --installer='conda-package' \
-        --name='vina'
+    # """
+    # Install autodock vina.
+    # @note Updated 2023-08-29.
+    # """
+    local -A dict
+    dict['name']="${KOOPA_INSTALL_NAME:?}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    koopa_install_conda_package \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --version="${dict['version']}"
+    return 0
 }
