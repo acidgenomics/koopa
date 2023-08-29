e7941d4d1493127d77e41488518d1dd9f7d8df54
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 13:22:49 -0400

    Update chezmoi

--- lang/bash/include/install/common/shared/chezmoi.sh
+++ lang/bash/include/install/common/shared/chezmoi.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install chezmoi.
-    # @note Updated 2023-08-28.
+    # @note Updated 2023-08-29.
     #
     # @seealso
     # - https://www.chezmoi.io/
@@ -12,13 +12,17 @@ main() {
     # - https://ports.macports.org/port/chezmoi/details/
     # """
     local -A dict
+    dict['name']="${KOOPA_INSTALL_NAME:?}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['ldflags']="-X main.version=${dict['version']}"
     dict['url']="https://github.com/twpayne/chezmoi/archive/refs/tags/\
 v${dict['version']}.tar.gz"
-    koopa_install_app_subshell \
-        --installer='go-package' \
-        --name='chezmoi' \
-        -D "--ldflags=-X main.version=${dict['version']}" \
-        -D "--url=${dict['url']}"
+    koopa_install_go_package \
+        --ldflags="${dict['ldflags']}" \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --url="${dict['url']}" \
+        --version="${dict['version']}"
     return 0
 }
