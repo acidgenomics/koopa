2ff2c62ebe4b36ceb2226399a745a81cf6203f74
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 17:59:46 -0400

    Update GNU app installers

--- lang/bash/include/install/common/shared/coreutils.sh
+++ lang/bash/include/install/common/shared/coreutils.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
 main() {
-    local -a deps
+    # """
+    # Install GNU coreutils.
+    # @note Updated 2023-08-29.
+    # """
+    local -a conf_args deps install_args
+    local conf_arg
     koopa_activate_app --build-only 'gperf'
     deps=()
     koopa_is_linux && deps+=('attr')
     deps+=('gmp')
     koopa_activate_app "${deps[@]}"
-    koopa_install_app_subshell \
-        --installer='gnu-app' \
-        --name='coreutils' \
-        -D '--program-prefix=g' \
-        -D '--with-gmp' \
-        -D '--without-selinux'
+    conf_args=(
+        '--program-prefix=g'
+        '--with-gmp'
+        '--without-selinux'
+    )
+    for conf_arg in "${conf_args[@]}"
+    do
+        install_args+=('-D' "$conf_arg")
+    done
+    koopa_install_gnu_app "${install_args[@]}"
+    return 0
 }
