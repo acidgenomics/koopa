5fa40c6da3a723f4e9058945dc8900dace131ebf
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 13:34:09 -0400

    Rework go installers

--- lang/bash/include/install/common/shared/yq.sh
+++ lang/bash/include/install/common/shared/yq.sh
@@ -10,12 +10,15 @@ main() {
     # - go help build
     # """
     local -A dict
+    dict['name']="${KOOPA_INSTALL_NAME:?}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['url']="https://github.com/mikefarah/yq/archive/\
 v${dict['version']}.tar.gz"
-    koopa_install_app_subshell \
-        --installer='go-package' \
-        --name='yq' \
-        -D "--url=${dict['url']}"
+    koopa_install_go_package \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --url="${dict['url']}" \
+        --version="${dict['version']}"
     return 0
 }
