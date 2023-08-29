e184fccdca06e008732b11a7c1d9572deccf3d1a
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 15:10:16 -0400

    Rework rust installers

--- lang/bash/include/install/common/shared/nushell.sh
+++ lang/bash/include/install/common/shared/nushell.sh
@@ -5,20 +5,27 @@
 main() {
     # """
     # Install nushell.
-    # @note Updated 2023-08-28.
+    # @note Updated 2023-08-29.
     #
     # @seealso
     # - https://www.nushell.sh/book/installation.html#build-from-source
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/nushell.rb
     # - https://crates.io/crates/nu
     # """
+    local -A dict
     koopa_activate_app --build-only 'pkg-config'
     koopa_activate_app 'zlib' 'openssl3'
-    OPENSSL_DIR="$(koopa_app_prefix 'openssl3')"
+    dict['features']='extra'
+    dict['name']='nu'
+    dict['openssl']="$(koopa_app_prefix 'openssl3')"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    OPENSSL_DIR="${dict['openssl']}"
     export OPENSSL_DIR
-    koopa_install_app_subshell \
-        --installer='rust-package' \
-        --name='nushell' \
-        -D '--cargo-name=nu' \
-        -D '--features=extra'
+    koopa_install_rust_package \
+        --features="${dict['features']}" \
+        --name="${dict['name']}" \
+        --prefix="${dict['prefix']}" \
+        --version="${dict['version']}"
+    return 0
 }
