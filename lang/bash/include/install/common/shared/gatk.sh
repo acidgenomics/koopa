c87f48c9b50d6291ced7250bb91866aadc7eda43
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 09:33:51 -0400

    Rework conda package install

--- lang/bash/include/install/common/shared/gatk.sh
+++ lang/bash/include/install/common/shared/gatk.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_install_app_subshell \
-        --installer='conda-package' \
-        --name='gatk4'
+    # """
+    # Install GATK.
+    # @note Updated 2023-08-29.
+    # """
+    local -A dict
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    koopa_install_conda_package \
+        --name='gatk4' \
+        --prefix="${dict['prefix']}" \
+        --version="${dict['version']}"
+    return 0
 }
