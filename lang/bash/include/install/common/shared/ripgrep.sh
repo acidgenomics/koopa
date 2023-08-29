5652e0da033f07067666ccab92f903fcab2ff0cf
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 15:22:45 -0400

    Rework rust installers

--- lang/bash/include/install/common/shared/ripgrep.sh
+++ lang/bash/include/install/common/shared/ripgrep.sh
@@ -5,16 +5,7 @@ main() {
     # Install ripgrep.
     # @note Updated 2023-08-29.
     # """
-    local -A dict
     koopa_activate_app 'pcre2'
-    dict['features']='pcre2'
-    dict['name']="${KOOPA_INSTALL_NAME:?}"
-    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
-    dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    koopa_install_rust_package \
-        --features="${dict['features']}" \
-        --name="${dict['name']}" \
-        --prefix="${dict['prefix']}" \
-        --version="${dict['version']}"
+    koopa_install_rust_package --features='pcre2'
     return 0
 }
