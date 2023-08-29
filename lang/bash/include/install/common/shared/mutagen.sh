1641c75f753adeeaca5498f60af6f87185baa2ff
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 15:55:24 -0400

    Rework mutagen installer

--- lang/bash/include/install/common/shared/mutagen.sh
+++ lang/bash/include/install/common/shared/mutagen.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_install_app_subshell \
-        --installer='python-package' \
-        --name='mutagen' \
-        -D 'tqdm'
+    koopa_install_python_package --extra-package='tqdm'
+    return 0
 }
