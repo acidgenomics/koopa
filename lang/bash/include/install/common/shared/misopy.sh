068505656897a3105d65d03d4452d2b3571e0157
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 09:36:37 -0400

    Switch to symlink

--- lang/bash/include/install/common/shared/misopy.sh
+++ lang/bash/include/install/common/shared/misopy.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-
-# NOTE This is currently slow to solve.
-
-main() {
-    koopa_install_app_subshell \
-        --installer='conda-package' \
-        --name='misopy'
-}
+conda-package.sh
