33bfef511a67761271fd0ae0299c28ca8fff75bd
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 15:34:04 -0400

    Simplify our language-specific package installers

--- lang/bash/include/install/common/shared/rust-package.sh
+++ lang/bash/include/install/common/shared/rust-package.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
 main() {
-    local -A dict
-    dict['name']="${KOOPA_INSTALL_NAME:?}"
-    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
-    dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    koopa_install_rust_package \
-        --name="${dict['name']}" \
-        --prefix="${dict['prefix']}" \
-        --version="${dict['version']}"
+    koopa_install_rust_package
     return 0
 }
