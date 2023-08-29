9306a93e0fe9f683c3be2ed30f9b61195c0a05b4
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-29 14:20:09 -0400

    Rework Ruby package install

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -11411,6 +11411,7 @@ koopa_install_bash() {
 
 koopa_install_bashcov() {
     koopa_install_app \
+        --installer='ruby-package' \
         --name='bashcov' \
         "$@"
 }
@@ -11590,6 +11591,7 @@ koopa_install_cmake() {
 
 koopa_install_colorls() {
     koopa_install_app \
+        --installer='ruby-package' \
         --name='colorls' \
         "$@"
 }
@@ -13653,18 +13655,21 @@ koopa_install_ripgrep() {
 
 koopa_install_rmate() {
     koopa_install_app \
+        --installer='ruby-package' \
         --name='rmate' \
         "$@"
 }
 
 koopa_install_ronn_ng() {
     koopa_install_app \
+        --installer='ruby-package' \
         --name='ronn-ng' \
         "$@"
 }
 
 koopa_install_ronn() {
     koopa_install_app \
+        --installer='ruby-package' \
         --name='ronn' \
         "$@"
 }
@@ -13680,6 +13685,78 @@ koopa_install_rsync() {
     koopa_install_app \
         --name='rsync' \
         "$@"
+}
+
+koopa_install_ruby_package() {
+    local -A app dict
+    app['bundle']="$(koopa_locate_bundle)"
+    app['ruby']="$(koopa_locate_ruby --realpath)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['gemfile']='Gemfile'
+    dict['jobs']="$(koopa_cpu_count)"
+    dict['name']="${KOOPA_INSTALL_NAME:-}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:-}"
+    dict['version']="${KOOPA_INSTALL_VERSION:-}"
+    while (("$#"))
+    do
+        case "$1" in
+            '--name='*)
+                dict['name']="${1#*=}"
+                shift 1
+                ;;
+            '--name')
+                dict['name']="${2:?}"
+                shift 2
+                ;;
+            '--prefix='*)
+                dict['prefix']="${1#*=}"
+                shift 1
+                ;;
+            '--prefix')
+                dict['prefix']="${2:?}"
+                shift 2
+                ;;
+            '--version='*)
+                dict['version']="${1#*=}"
+                shift 1
+                ;;
+            '--version')
+                dict['version']="${2:?}"
+                shift 2
+                ;;
+            *)
+                koopa_invalid_arg "$1"
+                ;;
+        esac
+    done
+    koopa_assert_is_set \
+        '--name' "${dict['name']}" \
+        '--prefix' "${dict['prefix']}" \
+        '--version' "${dict['version']}"
+    read -r -d '' "dict[gemfile_string]" << END || true
+source "https://rubygems.org"
+gem "${dict['name']}", "${dict['version']}"
+END
+    dict['libexec']="${dict['prefix']}/libexec"
+    koopa_mkdir "${dict['libexec']}"
+    koopa_print_env
+    (
+        koopa_cd "${dict['libexec']}"
+        koopa_write_string \
+            --file="${dict['gemfile']}" \
+            --string="${dict['gemfile_string']}"
+        "${app['bundle']}" install \
+            --gemfile="${dict['gemfile']}" \
+            --jobs="${dict['jobs']}" \
+            --retry=3 \
+            --standalone
+        "${app['bundle']}" binstubs \
+            "${dict['name']}" \
+            --path="${dict['prefix']}/bin" \
+            --shebang="${app['ruby']}" \
+            --standalone
+    )
+    return 0
 }
 
 koopa_install_ruby() {
