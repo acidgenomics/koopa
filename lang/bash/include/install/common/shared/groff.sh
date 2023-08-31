da9295b90cbf32bc34cf12507ff625fe49fedbee
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-31 16:00:40 -0400

    Rework groff config

--- lang/bash/include/install/common/shared/groff.sh
+++ lang/bash/include/install/common/shared/groff.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_activate_app 'texinfo'
-    koopa_install_gnu_app
+    # """
+    # Install groff.
+    # @note Updated 2023-08-31.
+    # """
+    local -a conf_args install_args
+    local conf_arg
+    koopa_activate_app --build-only 'bison' 'pkg-config' 'texinfo'
+    koopa_activate_app 'm4' 'perl'
+    conf_args=(
+        '--with-uchardet'
+        '--without-x'
+    )
+    for conf_arg in "${conf_args[@]}"
+    do
+        install_args+=('-D' "$conf_arg")
+    done
+    koopa_install_gnu_app "${install_args[@]}"
     return 0
 }
