b79daa3c4ef9f6a4c36d557d888a85536c4bef7b
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-31 15:13:28 -0400

    Draft gnupg update

--- lang/bash/include/install/common/shared/gnupg.sh
+++ lang/bash/include/install/common/shared/gnupg.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# The installer doesn't honor '--disable-ldap' anymore, which is problematic
+# currently on Linux.
+
 # FIXME 2.4.3 fails to build on Linux if LDAP is not installed.
-# https://crux.nu/bugs/index.php?do=details&task_id=1935&project=1&amp%3Bamp%3Bopened=63&status%5B0%5D=&amp%3Bpagenum=45&order=tasktype&sort=asc&toggleadvanced=1
 
 # /usr/bin/ld: server.o: in function `cmd_ad_query':
 # server.c:(.text+0x1db4): undefined reference to `ks_ldap_help_variables'
@@ -10,33 +12,44 @@
 # 
 # I saw there was a bug related to this that was fixed upstream, but it still seems to be an issue with the port: https://dev.gnupg.org/T6239
 
+# FIXME Consider requiring openldap here.
+# https://github.com/Homebrew/homebrew-core/blob/b124d57c4c711699749c9b1ebb98c21c74588452/Formula/g/gnupg.rb
+
 main() {
     # """
     # Install GnuPG.
-    # @note Updated 2023-05-08.
+    # @note Updated 2023-08-31.
     #
     # @seealso
+    # How to disable ldap requirement ('--disable-ldap'):
     # - https://www.linuxfromscratch.org/blfs/view/svn/postlfs/gnupg.html
+    # - https://www.linuxfromscratch.org/blfs/view/svn/postlfs/gnupg.html
+    # - https://dev.gnupg.org/T6239
     # - https://gitlab.com/goeb/gnupg-static/-/commit/
     #     42665e459192e3ee1bb6461ae2d4336d8f1f023c
+    # - https://crux.nu/bugs/index.php?do=details&task_id=1935
     # """
     local -A app dict
-    local -a conf_args
-    koopa_activate_app --build-only 'pkg-config' 'sed'
-    koopa_activate_app \
-        'zlib' \
-        'bzip2' \
-        'readline' \
-        'nettle' \
-        'libtasn1' \
-        'gnutls' \
-        'sqlite' \
-        'libgpg-error' \
-        'libgcrypt' \
-        'libassuan' \
-        'libksba' \
-        'npth' \
+    local -a build_deps conf_args deps
+    build_deps=('pkg-config' 'sed')
+    # FIXME Add openldap support here.
+    deps=(
+        'zlib'
+        'bzip2'
+        'readline'
+        'nettle'
+        'libtasn1'
+        'gnutls'
+        'sqlite'
+        'libgpg-error'
+        'libgcrypt'
+        'libassuan'
+        'libksba'
+        'npth'
         'pinentry'
+    )
+    koopa_activate_app --build-only "${build_deps[@]}"
+    koopa_activate_app "${deps[@]}"
     app['sed']="$(koopa_locate_sed)"
     koopa_assert_is_executable "${app[@]}"
     dict['bzip2']="$(koopa_app_prefix 'bzip2')"
@@ -71,6 +84,8 @@ main() {
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
+    # FIXME Here's a patch for that, need to rework.
+    # src/dirmngr/server.c:2779: undefined reference to `ks_ldap_help_variables'
     "${app['sed']}" \
         -e '/ks_ldap_free_state/i #if USE_LDAP' \
         -e '/ks_get_state =/a #endif' \
