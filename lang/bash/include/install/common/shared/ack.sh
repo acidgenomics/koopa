45d21a9d7e8157a49603a15383a1f1bf3be496cb
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-31 16:51:58 -0400

    Fix ack verison

--- lang/bash/include/install/common/shared/ack.sh
+++ lang/bash/include/install/common/shared/ack.sh
@@ -3,10 +3,13 @@
 main() {
     # """
     # Install ack.
-    # @note Updated 2023-08-30.
+    # @note Updated 2023-08-31.
     # """
+    local -A dict
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
     koopa_install_perl_package \
         --cpan-path='PETDANCE/ack' \
-        --dependency='File::Next'
+        --dependency='File::Next' \
+        --version="v${dict['version']}"
     return 0
 }
