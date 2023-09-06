2c2a85ae4187e0df452666b01ab2bbed10cf27ab
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-06 09:01:03 -0400

    Rework quarto / pandoc handling for R

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -19661,14 +19661,6 @@ koopa_r_configure_environ() {
             "TZ=\${TZ:-America/New_York}"
         )
     fi
-    if [[ "${bool['system']}" -eq 0 ]] || koopa_is_macos
-    then
-        path_arr+=("${dict['bin_prefix']}")
-    fi
-    path_arr+=(
-        '/usr/bin'
-        '/bin'
-    )
     if koopa_is_linux
     then
         path_arr+=(
@@ -19679,12 +19671,22 @@ koopa_r_configure_environ() {
     elif koopa_is_macos
     then
         path_arr+=(
-            '/Library/TeX/texbin'
-            '/usr/local/MacGPG2/bin'
-            '/opt/X11/bin'
             '/Applications/RStudio.app/Contents/Resources/app/quarto/bin'
             '/Applications/RStudio.app/Contents/Resources/app/quarto/bin/tools'
             '/Applications/RStudio.app/Contents/Resources/app/bin/postback'
+        )
+    fi
+    if [[ "${bool['system']}" -eq 0 ]] || koopa_is_macos
+    then
+        path_arr+=("${dict['bin_prefix']}")
+    fi
+    path_arr+=('/usr/bin' '/bin')
+    if koopa_is_macos
+    then
+        path_arr+=(
+            '/Library/TeX/texbin'
+            '/usr/local/MacGPG2/bin'
+            '/opt/X11/bin'
         )
     fi
     conf_dict['path']="$(printf '%s:' "${path_arr[@]}")"
