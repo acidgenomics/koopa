963081551ce1bfa96d378cf28b5c6e2366ae6c86
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-13 13:20:38 -0400

    Bake in configuration for gcloud

--- lang/bash/include/install/common/shared/google-cloud-sdk.sh
+++ lang/bash/include/install/common/shared/google-cloud-sdk.sh
@@ -12,8 +12,10 @@ main() {
     # - https://saturncloud.io/blog/how-to-resolve-modulenotfounderror-no-
     #     module-named-googlecloud-error/
     # """
-    local -A dict
-    koopa_activate_app --build-only 'python3.11'
+    local -A app dict
+    local -a conf_args
+    app['python']="$(koopa_locate_python311)"
+    koopa_assert_is_executable "${app[@]}"
     dict['arch']="$(koopa_arch)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
@@ -39,5 +41,18 @@ ${dict['arch2']}.tar.gz"
     koopa_extract \
         "$(koopa_basename "${dict['url']}")" \
         "${dict['prefix']}"
+    app['gcloud']="${dict['prefix']}/bin/gcloud"
+    koopa_assert_is_executable "${app['gcloud']}"
+    conf_args=(
+        "export CLOUDSDK_PYTHON=${app['python']}"
+        'export CLOUDSDK_PYTHON_SITEPACKAGES=0'
+        "export PYTHONPATH=${dict['prefix']}/lib"
+    )
+    dict['conf_string']="$(koopa_print "${conf_args[@]}")"
+    koopa_insert_at_line_number \
+        --file="${app['gcloud']}" \
+        --line-number=2 \
+        --string="${dict['conf_string']}"
+    "${app['gcloud']}" --version
     return 0
 }
