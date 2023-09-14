832101f790ecb814e970c84e28be091aaab4964b
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-14 07:59:40 -0400

    Rework using awk for better cross-platform support

--- lang/sh/functions/l/logged-in-user-count.sh
+++ lang/sh/functions/l/logged-in-user-count.sh
@@ -5,6 +5,9 @@ _koopa_logged_in_user_count() {
     # Number of logged in users.
     # @note Updated 2023-09-14.
     #
+    # Usage of 'who -q' is problematic when the same user is connected via
+    # multiple SSH sessions. Need to filter this out.
+    #
     # Return is inconsistent between BSD and Linux.
     #
     # BSD:
@@ -16,7 +19,8 @@ _koopa_logged_in_user_count() {
     # - man who
     # - man w
     # """
-    __kvar_string="$(who -q | tail -n 1 | grep -Eo '[0-9]+$')"
+    __kvar_string="$(_koopa_logged_in_users | wc -l)"
+    [ -n "$__kvar_string" ] || return 1
     _koopa_print "$__kvar_string"
     unset -v __kvar_string
     return 0
