81379a8997163cfa6d6bcd6329ec1427b0df9fe3
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-14 07:40:21 -0400

    Inform about muliple users on EC2

--- lang/sh/functions/l/logged-in-user-count.sh
+++ lang/sh/functions/l/logged-in-user-count.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-_koopa_number_of_logged_in_users() {
+_koopa_logged_in_user_count() {
     # """
     # Number of logged in users.
     # @note Updated 2023-09-14.
+    #
+    # Return is inconsistent between BSD and Linux.
+    #
+    # BSD:
+    # > # users = 1
+    # Linux:
+    # > # users=1
     #
     # @seealso
     # - man who
     # - man w
     # """
-    __kvar_string="$(who -q | tail -n 1 | awk '{ print $NF }')"
+    __kvar_string="$(who -q | tail -n 1 | grep -Eo '[0-9]+$')"
     _koopa_print "$__kvar_string"
     unset -v __kvar_string
     return 0
