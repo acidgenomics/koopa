71814c6e522f80c1a2ee3371c42f7403dfe8748d
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-14 10:36:02 -0400

    Harden against missing python3

--- lang/bash/functions/common/s/shared-apps.sh
+++ lang/bash/functions/common/s/shared-apps.sh
@@ -3,13 +3,14 @@
 koopa_shared_apps() {
     # """
     # Enabled shared apps to be installed by default.
-    # @note Updated 2023-03-27.
+    # @note Updated 2023-09-14.
     #
     # @examples
     # koopa_shared_apps
     # """
     local cmd
     koopa_assert_has_no_args "$#"
+    koopa_assert_is_installed 'python3'
     cmd="$(koopa_koopa_prefix)/lang/python/shared-apps.py"
     koopa_assert_is_executable "$cmd"
     "$cmd"
