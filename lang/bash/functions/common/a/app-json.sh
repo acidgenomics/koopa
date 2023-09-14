57700720939dbbb8099f4e3f071757310a589a77
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-14 10:36:02 -0400

    Harden against missing python3

--- lang/bash/functions/common/a/app-json.sh
+++ lang/bash/functions/common/a/app-json.sh
@@ -3,7 +3,7 @@
 koopa_app_json() {
     # """
     # Parse 'app.json' file using our internal Python JSON parser.
-    # @note Updated 2023-03-30.
+    # @note Updated 2023-09-14.
     #
     # @examples
     # koopa_app_json \
@@ -12,6 +12,7 @@ koopa_app_json() {
     # """
     local cmd
     koopa_assert_has_args "$#"
+    koopa_assert_is_installed 'python3'
     cmd="$(koopa_koopa_prefix)/lang/python/app-json.py"
     koopa_assert_is_executable "$cmd"
     "$cmd" "$@"
