c9d83cd39c13b7adfd71d19d1331e2ddda1276fd
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-14 08:07:14 -0400

    Remove unnecessary comments

--- lang/sh/functions/l/logged-in-users.sh
+++ lang/sh/functions/l/logged-in-users.sh
@@ -4,9 +4,6 @@ _koopa_logged_in_users() {
     # """
     # Logged in users.
     # @note Updated 2023-09-14.
-    #
-    # Can use 'head -n -1' only with GNU coreutils.
-    # Usage of 'tac | tail -n +2' works on Linux, but macOS doesn't bundle tac.
     # """
     __kvar_string="$( \
         who -q \
