37830053b4b773b6b54327345a681dabdb0b14d3
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-15 11:53:16 -0400

    Use a single tmp_dir

--- lang/bash/functions/common/r/r-check.sh
+++ lang/bash/functions/common/r/r-check.sh
@@ -11,21 +11,26 @@ koopa_r_check() {
     # koopa_r_check 'AcidGenomes' 'pipette'
     # """
     local -A app
+    local -A dict
+    local pkg
     koopa_assert_has_args "$#"
     app['rscript']="$(koopa_locate_rscript --only-system)"
     koopa_assert_is_executable "${app[@]}"
+    dict['tmp_dir']="$(koopa_tmp_dir)"
     for pkg in "$@"
     do
-        local -A dict
-        dict['pkg']="$pkg"
-        dict['pkg2']="r-$(koopa_lowercase "${dict['pkg']}")"
-        dict['tmp_dir']="$(koopa_tmp_dir)"
-        dict['tmp_lib']="$(koopa_init_dir "${dict['tmp_dir']}/lib")"
-        dict['tarball']="https://github.com/acidgenomics/\
-${dict['pkg2']}/archive/refs/heads/develop.tar.gz"
-        dict['rscript']="${dict['tmp_dir']}/check.R"
-        read -r -d '' "dict[rscript_string]" << END || true
-.libPaths(new = "${dict['tmp_lib']}", include.site = FALSE)
+        local -A dict2
+        dict2['pkg']="$pkg"
+        dict2['pkg2']="r-$(koopa_lowercase "${dict2['pkg']}")"
+        dict2['tmp_dir']="$( \
+            koopa_init_dir "${dict['tmp_dir']}/${dict2['pkg2']}" \
+        )"
+        dict2['tmp_lib']="$(koopa_init_dir "${dict2['tmp_dir']}/lib")"
+        dict2['tarball']="https://github.com/acidgenomics/\
+${dict2['pkg2']}/archive/refs/heads/develop.tar.gz"
+        dict2['rscript']="${dict2['tmp_dir']}/check.R"
+        read -r -d '' "dict2[rscript_string]" << END || true
+.libPaths(new = "${dict2['tmp_lib']}", include.site = FALSE)
 message("repos")
 print(getOption("repos"))
 message(".libPaths")
@@ -51,9 +56,9 @@ if (!requireNamespace("AcidDevTools", quietly = TRUE)) {
         dependencies = NA
     )
 }
-message("Installing ${dict['pkg']}.")
+message("Installing ${dict2['pkg']}.")
 install.packages(
-    pkgs = "${dict['pkg']}",
+    pkgs = "${dict2['pkg']}",
     repos = c(
         "https://r.acidgenomics.com",
         BiocManager::repositories()
@@ -63,16 +68,17 @@ install.packages(
 AcidDevTools::check("src")
 END
         koopa_write_string \
-            --file="${dict['rscript']}" \
-            --string="${dict['rscript_string']}"
-        koopa_alert "Checking '${dict['pkg']}' package in '${dict['tmp_dir']}'."
+            --file="${dict2['rscript']}" \
+            --string="${dict2['rscript_string']}"
+        koopa_alert "Checking '${dict2['pkg']}' in '${dict2['tmp_dir']}'."
         (
-            koopa_cd "${dict['tmp_dir']}"
-            koopa_download "${dict['tarball']}"
-            koopa_extract "$(koopa_basename "${dict['tarball']}")" 'src'
-            "${app['rscript']}" "${dict['rscript']}"
+            koopa_cd "${dict2['tmp_dir']}"
+            koopa_download "${dict2['tarball']}"
+            koopa_extract "$(koopa_basename "${dict2['tarball']}")" 'src'
+            "${app['rscript']}" "${dict2['rscript']}"
         )
-        koopa_rm "${dict['tmp_dir']}"
+        koopa_rm "${dict2['tmp_dir']}"
     done
+    koopa_rm "${dict['tmp_dir']}"
     return 0
 }
