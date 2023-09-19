dcbf087f038a50c24d7674a4583ec34a4e0a070a
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-19 06:57:46 -0400

    Update bandwhich and chezmoi

--- lang/bash/functions/common/i/install-bandwhich.sh
+++ lang/bash/functions/common/i/install-bandwhich.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# NOTE This app is currently abandoned and no longer builds with Rust.
-# https://github.com/imsnif/bandwhich/issues/264
-
-# > koopa_install_bandwhich() {
-# >     koopa_install_app \
-# >         --installer='rust-package' \
-# >         --name='bandwhich' \
-# >         "$@"
-# > }
+koopa_install_bandwhich() {
+    koopa_install_app \
+        --installer='rust-package' \
+        --name='bandwhich' \
+        "$@"
+}
