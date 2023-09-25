fedba1d75bf36fab147f999d0a37783ea81f4017
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 06:53:41 -0400

    Simplify our bootstrap script

--- etc/koopa/bootstrap.sh
+++ etc/koopa/bootstrap.sh
@@ -1,74 +1,73 @@
 #!/bin/sh
 
-# FIXME Install to temporary directory.
-# FIXME Install Python here.
-# FIXME Return the temporary directory path at end of function.
-
 # """
 # Bootstrap core dependencies.
-# @note Updated 2023-09-23.
+# @note Updated 2023-09-25.
 # """
 
 set -o errexit
 set -o nounset
+set -o xtrace
 
-JOBS=1
 PATH='/usr/bin:/bin'
 TMPDIR="$(mktemp -d)"
 PREFIX="${TMPDIR}/bootstrap"
-export JOBS PATH PREFIX TMPDIR
+export PATH PREFIX TMPDIR
 
 install_bash() {
-    name='bash'
-    version='5.2'
-    file="${name}-${version}.tar.gz"
-    url="https://ftp.gnu.org/gnu/${name}/${file}" \
-    tmp_dir="${TMPDIR:?}/${name}"
-    mkdir -pv "$tmp_dir"
-    cd "$tmp_dir" || return 1
-    curl "$url" -o "$file"
-    tar -xzvf "$file"
-    cd "${name}-${version}"
-    ./configure --prefix="${PREFIX:?}"
-    make --jobs="${JOBS:?}"
-    sudo make install
-    rm -fr "$tmp_dir"
+    mkdir -p "${TMPDIR}/src/bash"
+    cd "${TMPDIR}/src/bash" || return 1
+    curl \
+        'https://ftp.gnu.org/gnu/bash/bash-5.2.15.tar.gz' \
+        -o 'src.tar.gz'
+    tar -xzf 'src.tar.gz'
+    cd 'bash-5.2.15' || return 1
+    ./configure --prefix="$PREFIX"
+    make
+    make install
     return 0
 }
 
 install_coreutils() {
-    name='coreutils'
-    version='9.1'
-    file="${name}-${version}.tar.gz"
-    url="https://ftp.gnu.org/gnu/${name}/${file}" \
-    tmp_dir="${TMPDIR:?}/${name}"
-    mkdir -pv "$tmp_dir"
-    cd "$tmp_dir" || return 1
-    curl "$url" -o "$file"
-    tar -xzvf "$file"
-    cd "${name}-${version}"
-    ./configure \
-        --prefix="${PREFIX:?}" \
-        --program-prefix='g'
-    make --jobs="${JOBS:?}"
-    sudo make install
-    rm -fr "$tmp_dir"
+    mkdir -p "${TMPDIR}/src/coreutils"
+    cd "${TMPDIR}/src/coreutils" || return 1
+    curl \
+        'https://ftp.gnu.org/gnu/coreutils/coreutils-9.4.tar.gz' \
+        -o 'src.tar.gz'
+    tar -xzf 'src.tar.gz'
+    cd 'coreutils-9.4' || return 1
+    ./configure --prefix="${PREFIX:?}" --program-prefix='g'
+    make
+    make install
     return 0
 }
 
-# FIXME Add support for Python
-# install_python
+install_python() {
+    mkdir -p "${TMPDIR}/src/python"
+    cd "${TMPDIR}/src/python"
+    curl \
+        'https://www.python.org/ftp/python/3.11.5/Python-3.11.5.tgz' \
+        'src.tar.gz'
+    tar -xzf 'src.tar.gz'
+    cd 'Python-3.11.5' || return 1
+    ./configure --prefix="$PREFIX"
+    make
+    make install
+    return 0
+}
 
 main() {
-    if [ -x "${PREFIX:?}/bin/bash" ]
-    then
-        printf "Bash is already installed at '%s'.\n" "${PREFIX:?}"
-        return 0
-    fi
-    printf "Installing bootstrap into '%s'.\n" "${PREFIX:?}"
-    # > rm -fr "${PREFIX:?}"
-    # > install_coreutils
-    install_bash
+    printf "Installing bootstrap into '%s'.\n" "$PREFIX"
+    (
+        install_bash
+        install_coreutils
+        install_python
+    )
+    [ -x "${PREFIX}/bin/bash" ] || return 1
+    [ -x "${PREFIX}/bin/gcp" ] || return 1
+    [ -x "${PREFIX}/bin/python3" ] || return 1
+    printf '%s\n' "$PREFIX"
+    return 0
 }
 
 main "$@"
