4e1bb0d73b9f7df187256c75864a996f73e43cf8
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-02 17:24:49 -0400

    Keep gcloud pinned at 3.11

--- lang/bash/include/install/common/shared/google-cloud-sdk.sh
+++ lang/bash/include/install/common/shared/google-cloud-sdk.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+# FIXME Now seeing this error:
+# ModuleNotFoundError: No module named 'imp'
+
+# Traceback (most recent call last):
+#   File "/opt/koopa/app/google-cloud-sdk/446.0.0/libexec/lib/gcloud.py", line 132, in <module>
+#     main()
+#   File "/opt/koopa/app/google-cloud-sdk/446.0.0/libexec/lib/gcloud.py", line 90, in main
+#     from googlecloudsdk.core.util import encoding
+#   File "/opt/koopa/app/google-cloud-sdk/446.0.0/libexec/lib/googlecloudsdk/__init__.py", line 23, in <module>
+#     from googlecloudsdk.core.util import importing
+#   File "/opt/koopa/app/google-cloud-sdk/446.0.0/libexec/lib/googlecloudsdk/core/util/importing.py", line 23, in <module>
+#     import imp
+# ModuleNotFoundError: No module named 'imp'
+
 main() {
     # """
     # Install Google Cloud SDK.
-    # @note Updated 2023-09-13.
+    # @note Updated 2023-10-02.
     #
     # @seealso
     # - https://cloud.google.com/sdk/docs/install
@@ -15,7 +29,7 @@ main() {
     local -A app dict
     local -a conf_args
     koopa_activate_app --build-only 'python3.11'
-    app['python']="$(koopa_locate_python312)"
+    app['python']="$(koopa_locate_python311)"
     koopa_assert_is_executable "${app[@]}"
     dict['arch']="$(koopa_arch)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
