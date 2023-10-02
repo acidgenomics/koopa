18cc4472a2e96045cf68681a28125ed2d6955949
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-02 14:35:56 -0400

    Draft updates to python installer

--- lang/bash/include/install/common/shared/python.sh
+++ lang/bash/include/install/common/shared/python.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME Install without sqlite, for speed.
+
 # NOTE Python 3.12 no longer installs setuptools by default:
 # https://twitter.com/VictorStinner/status/1654124014632321025
 
@@ -26,6 +28,48 @@
 #
 # https://github.com/Homebrew/homebrew-core/pull/149142
 # https://github.com/Homebrew/homebrew-core/pull/149142/files#diff-2b638765de0666dff89595d3c97c19db17cf1f1b33d320ea7ba9e81be89372a1
+
+# How to suppress this for clang?
+# clang: warning: argument unused during compilation: '-fno-semantic-interposition' [-Wunused-command-line-argument]
+
+# Now seeing this with 3.12 update:
+# clang: warning: argument unused during compilation: '-fno-semantic-interposition' [-Wunused-command-line-argument]
+# clang: warning: argument unused during compilation: '-fno-semantic-interposition' [-Wunused-command-line-argument]
+# gcc  -I./Modules/_hacl/include -D_BSD_SOURCE -D_DEFAULT_SOURCE -fno-strict-overflow -Wsign-compare -Wunreachable-code -DNDEBUG -g -O3 -Wall    -fno-semantic-interposition -flto -std=c11 -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wstrict-prototypes -Werror=implicit-function-declaration -fvisibility=hidden -fprofile-instr-generate -I./Include/internal  -I. -I./Include -I/opt/koopa/app/zlib/1.3/include -I/opt/koopa/app/bzip2/1.0.8/include -I/opt/koopa/app/expat/2.5.0/include -I/opt/koopa/app/libffi/3.4.4/include -I/opt/koopa/app/mpdecimal/2.5.1/include -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/openssl3/3.1.3/include -I/opt/koopa/app/xz/5.4.4/include -I/opt/koopa/app/libedit/20230828-3.1/include -I/opt/koopa/app/libedit/20230828-3.1/include/editline -I/opt/koopa/app/gdbm/1.23/include -I/opt/koopa/app/sqlite/3.43.0/include  -I/opt/koopa/app/zlib/1.3/include -I/opt/koopa/app/bzip2/1.0.8/include -I/opt/koopa/app/expat/2.5.0/include -I/opt/koopa/app/libffi/3.4.4/include -I/opt/koopa/app/mpdecimal/2.5.1/include -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/ncurses/6.4/include/ncursesw -I/opt/koopa/app/ncurses/6.4/include -I/opt/koopa/app/openssl3/3.1.3/include -I/opt/koopa/app/xz/5.4.4/include -I/opt/koopa/app/libedit/20230828-3.1/include -I/opt/koopa/app/libedit/20230828-3.1/include/editline -I/opt/koopa/app/gdbm/1.23/include -I/opt/koopa/app/sqlite/3.43.0/include   -c ./Modules/_hacl/Hacl_Hash_SHA1.c -o Modules/_hacl/Hacl_Hash_SHA1.o
+# clang: warning: argument unused during compilation: '-fno-semantic-interposition' [-Wunused-command-line-argument]
+# ./Modules/readline.c:448:21: error: expected expression
+#         (VFunction *)on_completion_display_matches_hook : 0;
+#                     ^
+# ./Modules/readline.c:448:10: error: use of undeclared identifier 'VFunction'; did you mean 'function'?
+#         (VFunction *)on_completion_display_matches_hook : 0;
+#          ^~~~~~~~~
+#          function
+# ./Modules/readline.c:434:61: note: 'function' declared here
+#                                                   PyObject *function)
+#                                                             ^
+# ./Modules/readline.c:448:22: error: expected ':'
+#         (VFunction *)on_completion_display_matches_hook : 0;
+#                      ^
+#                      :
+# ./Modules/readline.c:444:63: note: to match this '?'
+#         readlinestate_global->completion_display_matches_hook ?
+#                                                               ^
+# ./Modules/readline.c:1018:16: warning: a function declaration without a prototype is deprecated in all versions of C [-Wstrict-prototypes]
+# on_startup_hook()
+#                ^
+#                 void
+# ./Modules/readline.c:1033:18: warning: a function declaration without a prototype is deprecated in all versions of C [-Wstrict-prototypes]
+# on_pre_input_hook()
+#                  ^
+#                   void
+# 2 warnings and 3 errors generated.
+# gmake[2]: *** [Makefile:3026: Modules/readline.o] Error 1
+# gmake[2]: *** Waiting for unfinished jobs....
+# clang: warning: argument unused during compilation: '-fno-semantic-interposition' [-Wunused-command-line-argument]
+# gmake[2]: Leaving directory '/private/var/folders/l1/8y8sjzmn15v49jgrqglghcfr0000gn/T/tmp.vs2GHGXd1K/src'
+# gmake[1]: *** [Makefile:798: profile-gen-stamp] Error 2
+# gmake[1]: Leaving directory '/private/var/folders/l1/8y8sjzmn15v49jgrqglghcfr0000gn/T/tmp.vs2GHGXd1K/src'
+# gmake: *** [Makefile:810: profile-run-stamp] Error 2
 
 main() {
     # """
@@ -115,8 +159,8 @@ main() {
         '--with-dbmliborder=gdbm:ndbm'
         '--with-ensurepip=install'
         "--with-openssl=${dict['openssl']}"
-        '--with-readline=editline'
         # FIXME Disabling these with Python 3.12.
+        # > '--with-readline=editline'
         # > '--with-system-expat'
         # > '--with-system-ffi'
         # > '--with-system-libmpdec'
@@ -131,7 +175,7 @@ main() {
         )
     fi
     conf_args+=(
-        'PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1'
+        # > 'PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1'
         # This is defined in the MacPorts recipe.
         # > 'SETUPTOOLS_USE_DISTUTILS=stdlib'
         # Avoid OpenSSL checks that are problematic for Python 3.11.0.
@@ -148,10 +192,10 @@ Python-${dict['version']}.tar.xz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
+    # Override auto-detection of libmpdec, which assumes a universal build.
+    # https://github.com/python/cpython/issues/98557.
     if koopa_is_macos
     then
-        # Override auto-detection of libmpdec, which assumes a universal build.
-        # https://github.com/python/cpython/issues/98557.
         dict['arch']="$(koopa_arch)"
         case "${dict['arch']}" in
             'aarch64' | 'arm64')
@@ -164,17 +208,12 @@ Python-${dict['version']}.tar.xz"
                 koopa_stop 'Unsupported architecture.'
                 ;;
         esac
+        koopa_find_and_replace_in_file \
+            --fixed \
+            --pattern='libmpdec_machine=universal' \
+            --replacement="libmpdec_machine=${dict['decimal_arch']}" \
+            'configure'
         export PYTHON_DECIMAL_WITH_MACHINE="${dict['decimal_arch']}"
-        case "${dict['version']}" in
-            '3.12.'* | \
-            '3.11.'*)
-                koopa_find_and_replace_in_file \
-                    --fixed \
-                    --pattern='libmpdec_machine=universal' \
-                    --replacement="libmpdec_machine=${dict['decimal_arch']}" \
-                    'configure'
-                ;;
-        esac
     fi
     koopa_print_env
     koopa_dl 'configure args' "${conf_args[*]}"
@@ -184,28 +223,19 @@ Python-${dict['version']}.tar.xz"
     "${app['make']}" altinstall
     app['python']="${dict['prefix']}/bin/python${dict['maj_min_ver']}"
     koopa_assert_is_installed "${app['python']}"
-    case "${dict['version']}" in
-        '3.11.'*)
-            koopa_rm "${dict['prefix']}/bin/pip3.10"
-            ;;
-    esac
     "${app['python']}" -m sysconfig
     koopa_check_shared_object --file="${app['python']}"
     koopa_alert 'Checking module integrity.'
-    case "${dict['version']}" in
-        '3.9.'*)
-            ;;
-        *)
-            "${app['python']}" -c 'import _decimal'
-            ;;
-    esac
     "${app['python']}" -c 'import _ctypes'
+    "${app['python']}" -c 'import _decimal'
     "${app['python']}" -c 'import _gdbm'
     "${app['python']}" -c 'import hashlib'
     "${app['python']}" -c 'import pyexpat'
+    "${app['python']}" -c 'import readline'
     "${app['python']}" -c 'import sqlite3'
     "${app['python']}" -c 'import ssl'
     "${app['python']}" -c 'import zlib'
+    # FIXME How to check readline support?
     koopa_alert 'Checking pip configuration.'
     "${app['python']}" -m pip list --format='columns'
     return 0
