1dea70512b3951fa60c7f32db90d6bb7bf7f4949
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-02 17:18:23 -0400

    Update az and gcloud

--- lang/bash/include/install/common/shared/azure-cli.sh
+++ lang/bash/include/install/common/shared/azure-cli.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 main() {
-    koopa_install_python_package --package-name='azure_cli'
+    koopa_install_python_package \
+        --package-name='azure_cli' \
+        --python-version='3.11'
     return 0
 }
