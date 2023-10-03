6e69b95f6740e2584374fa01a8bf95ebdad0400d
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-03 10:54:55 -0400

    Reworking check system and app pruning in Python

--- lang/bash/functions/common/c/check-system.sh
+++ lang/bash/functions/common/c/check-system.sh
@@ -1,22 +1,15 @@
-#!/usr/bin/env bash
+w#!/usr/bin/env bash
 
 koopa_check_system() {
     # """
     # Check system.
-    # @note Updated 2023-05-19.
+    # @note Updated 2023-10-03.
     # """
-    local -A app
-    koopa_assert_has_no_args "$#"
-    app['r']="$(koopa_locate_r --allow-missing)"
-    if [[ ! -x "${app['r']}" ]]
-    then
-        koopa_stop \
-            'koopa R is not installed.' \
-            "Resolve with 'koopa install r'."
-    fi
-    koopa_check_exports || return 1
-    koopa_check_disk '/' || return 1
-    koopa_r_koopa 'cliCheckSystem'
-    koopa_alert_success 'System passed all checks.'
+    koopa_stop 'FIXME Reworking this in Python.'
+    # > koopa_assert_has_no_args "$#"
+    # > koopa_check_exports
+    # > koopa_check_disk '/'
+    # > koopa_r_koopa 'cliCheckSystem'
+    # > koopa_alert_success 'System passed all checks.'
     return 0
 }
