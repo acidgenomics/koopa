ced6a42a7133cd55ff5f5cd7a7446b56f65abb64
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-03 14:54:21 -0400

    Use FIXME comments instead

--- lang/bash/functions/common/c/check-system.sh
+++ lang/bash/functions/common/c/check-system.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+# FIXME Rework this in Python.
+
 koopa_check_system() {
     # """
     # Check system.
     # @note Updated 2023-10-03.
     # """
-    koopa_stop 'FIXME Reworking this in Python.'
-    # > koopa_assert_has_no_args "$#"
-    # > koopa_check_exports
-    # > koopa_check_disk '/'
-    # > koopa_r_koopa 'cliCheckSystem'
-    # > koopa_alert_success 'System passed all checks.'
+    koopa_assert_has_no_args "$#"
+    koopa_check_exports
+    koopa_check_disk '/'
+    koopa_r_koopa 'cliCheckSystem'
+    koopa_alert_success 'System passed all checks.'
     return 0
 }
