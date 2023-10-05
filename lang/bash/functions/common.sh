c94e0b31e5ba24dd21568e02416030e8cd3d344d
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-05 10:28:19 -0400

    Update default apps

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -20106,7 +20106,7 @@ libexec/lib/server}")
             'pcre2'
             'pixman'
             'proj'
-            'python3.11'
+            'python3.12'
             'readline'
             'sqlite'
             'xorg-libice'
