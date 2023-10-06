6215009a90458ce5ee44bee9b9a174a66102fb82
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-06 16:21:45 -0400

    Update uninstall-koopa.sh

--- lang/bash/functions/common/u/uninstall-koopa.sh
+++ lang/bash/functions/common/u/uninstall-koopa.sh
@@ -8,11 +8,14 @@ koopa_uninstall_koopa() {
     local -A dict
     dict['config_prefix']="$(koopa_config_prefix)"
     dict['koopa_prefix']="$(koopa_koopa_prefix)"
+    # FIXME Prompt the user that they want to continue.
     if koopa_is_linux && koopa_is_shared_install
     then
         koopa_rm --sudo '/etc/profile.d/zzz-koopa.sh'
     fi
     koopa_uninstall_dotfiles
+    # FIXME Consider passing --verbose flag here, for better progress.
+    # FIXME Pass '--sudo' for non shared install.
     koopa_rm \
         "${dict['config_prefix']}" \
         "${dict['koopa_prefix']}"
