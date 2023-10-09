a7511948350478af5b85175e399960ef744e8b46
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-09 12:22:55 -0400

    Rework using koopa_append_ldflags

--- lang/bash/functions/common/a/add-rpath-to-ldflags.sh
+++ lang/bash/functions/common/a/add-rpath-to-ldflags.sh
@@ -3,19 +3,17 @@
 koopa_add_rpath_to_ldflags() {
     # """
     # Append 'LDFLAGS' string with an rpath.
-    # @note Updated 2022-06-01.
+    # @note Updated 2023-10-09.
     #
     # Use '-rpath,${dir}' here not, '-rpath=${dir}'. This approach works on
     # both BSD/Unix (macOS) and Linux systems.
     # """
     local dir
     koopa_assert_has_args "$#"
-    LDFLAGS="${LDFLAGS:-}"
     for dir in "$@"
     do
         [[ -d "$dir" ]] || continue
-        LDFLAGS="${LDFLAGS} -Wl,-rpath,${dir}"
+        koopa_append_ldflags "-Wl,-rpath,${dir}"
     done
-    export LDFLAGS
     return 0
 }
