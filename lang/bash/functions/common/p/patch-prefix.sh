ac9a04e37a27901ffc5d71e17613230b03aa3051
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-09 16:21:40 -0400

    Rework zip to build without gcc

---
+++ lang/bash/functions/common/p/patch-prefix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+koopa_patch_prefix() {
+    # """
+    # Patch directory prefix.
+    # @note Updated 2023-10-09.
+    # """
+    koopa_print "$(koopa_koopa_prefix)/lang/bash/include/patch"
+    return 0
+}
