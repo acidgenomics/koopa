1f5d21f5e71e9656d19265ea7577ff8c4d6e21c2
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-10 13:32:27 -0400

    Rework salmon install on apple silicon

--- lang/bash/include/install/common/shared/salmon.sh
+++ lang/bash/include/install/common/shared/salmon.sh
@@ -30,11 +30,9 @@ main() {
     )
     koopa_activate_app --build-only "${build_deps[@]}"
     koopa_activate_app "${deps[@]}"
-    app['cat']="$(koopa_locate_cat --allow-system)"
     app['patch']="$(koopa_locate_patch)"
     app['pkg_config']="$(koopa_locate_pkg_config --realpath)"
     koopa_assert_is_executable "${app[@]}"
-    dict['bzip2']="$(koopa_app_prefix 'bzip2')"
     dict['curl']="$(koopa_app_prefix 'curl')"
     dict['icu4c']="$(koopa_app_prefix 'icu4c')"
     dict['jemalloc']="$(koopa_app_prefix 'jemalloc')"
@@ -45,8 +43,6 @@ main() {
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['xz']="$(koopa_app_prefix 'xz')"
     dict['zlib']="$(koopa_app_prefix 'zlib')"
-    cmake['bzip2_include_dir']="${dict['bzip2']}/include"
-    cmake['bzip2_libraries']="${dict['bzip2']}/lib/libbz2.${dict['shared_ext']}"
     cmake['curl_include_dir']="${dict['curl']}/include"
     cmake['curl_library']="${dict['curl']}/lib/libcurl.${dict['shared_ext']}"
     cmake['htscodec_library']="${dict['staden_io_lib']}/lib/libhtscodecs.a"
@@ -66,13 +62,11 @@ libstaden-read.${dict['shared_ext']}"
     cmake['staden_version']="$(koopa_app_version 'staden-io-lib')"
     cmake['zlib_include_dir']="${dict['zlib']}/include"
     cmake['zlib_library']="${dict['zlib']}/lib/libz.${dict['shared_ext']}"
-    cmake_args=(
+    cmake_args+=(
         # Build options --------------------------------------------------------
         '-DNO_IPO=TRUE'
         '-DUSE_SHARED_LIBS=ON'
         # Dependency paths -----------------------------------------------------
-        "-DBZIP2_INCLUDE_DIR=${cmake['bzip2_include_dir']}"
-        "-DBZIP2_LIBRARIES=${cmake['bzip2_libraries']}"
         "-DCURL_INCLUDE_DIR=${cmake['curl_include_dir']}"
         "-DCURL_LIBRARY=${cmake['curl_library']}"
         "-DHTSCODEC_LIBRARY=${cmake['htscodec_library']}"
@@ -89,45 +83,35 @@ libstaden-read.${dict['shared_ext']}"
         "-DSTADEN_VERSION=${cmake['staden_version']}"
         "-DZLIB_INCLUDE_DIR=${cmake['zlib_include_dir']}"
         "-DZLIB_LIBRARY=${cmake['zlib_library']}"
-        # Disabled dependencies (build from source) ----------------------------
-        # > '-DHTSCODEC_LIBRARY='
-        # > '-Dlibgff_DIR='
     )
+    if ! koopa_is_macos
+    then
+        dict['bzip2']="$(koopa_app_prefix 'bzip2')"
+        cmake['bzip2_include_dir']="${dict['bzip2']}/include"
+        cmake['bzip2_libraries']="${dict['bzip2']}/lib/\
+libbz2.${dict['shared_ext']}"
+        cmake_args+=(
+            # Dependency paths -------------------------------------------------
+            "-DBZIP2_INCLUDE_DIR=${cmake['bzip2_include_dir']}"
+            "-DBZIP2_LIBRARIES=${cmake['bzip2_libraries']}"
+        )
+    fi
     dict['url']="https://github.com/COMBINE-lab/salmon/archive/refs/tags/\
 v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
-    # Patch diff created with:
-    # > diff -u \
-    # >     'Findlibstadenio.cmake' \
-    # >     'Findlibstadenio.cmake-1' \
-    # >     > 'staden.patch'
-    "${app['cat']}" << END > 'staden.patch'
---- Findlibstadenio.cmake	2023-05-01 11:11:46
-+++ Findlibstadenio.cmake-1	2023-05-01 11:13:01
-@@ -16,15 +16,6 @@
- find_library(HTSCODEC_LIBRARY NAMES htscodecs libhtscodecs
-   HINTS \${STADEN_ROOT} ENV STADEN_ROOT PATH_SUFFIXES lib lib64)
- 
--if(STADEN_INCLUDE_DIR)
--  set(_version_regex "^#define[ \\t]+PACKAGE_VERSION[ \\t]+\"([^\\"]+)\\".*")
--  file(STRINGS "\${STADEN_INCLUDE_DIR}/io_lib/io_lib_config.h"
--    STADEN_VERSION REGEX "\${_version_regex}")
--  string(REGEX REPLACE "\${_version_regex}" "\\\1"
--    STADEN_VERSION "\${STADEN_VERSION}")
--  unset(_version_regex)
--endif()
--
- include(FindPackageHandleStandardArgs)
- find_package_handle_standard_args(libstadenio DEFAULT_MSG
-                                   STADEN_LIBRARY 
-END
+    dict['patch_prefix']="$(koopa_patch_prefix)/common/salmon"
+    koopa_assert_is_dir "${dict['patch_prefix']}"
+    dict['patch_file']="${dict['patch_prefix']}/staden.patch"
     "${app['patch']}" \
         --unified \
         --verbose \
         'cmake/Modules/Findlibstadenio.cmake' \
-        'staden.patch'
+        "${dict['patch_file']}"
     koopa_cmake_build --prefix="${dict['prefix']}" "${cmake_args[@]}"
+    app['salmon']="${dict['prefix']}/bin/salmon"
+    koopa_assert_is_executable "${app['salmon']}"
+    "${app['salmon']}" --version
     return 0
 }
