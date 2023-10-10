1f1071bfa32269dd4a02fc8f363d20eea0981b5a
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-10 16:09:46 -0400

    Keep kallisto and salmon as source builds

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -12689,17 +12689,9 @@ koopa_install_jupyterlab() {
 }
 
 koopa_install_kallisto() {
-    if koopa_is_macos && koopa_is_aarch64
-    then
-        koopa_install_app \
-            --name='kallisto' \
-            "$@"
-    else
-        koopa_install_app \
-            --installer='conda-package' \
-            --name='kallisto' \
-            "$@"
-    fi
+    koopa_install_app \
+        --name='kallisto' \
+        "$@"
 }
 
 koopa_install_koopa() {
@@ -14415,17 +14407,9 @@ koopa_install_rust() {
 }
 
 koopa_install_salmon() {
-    if koopa_is_macos && koopa_is_aarch64
-    then
-        koopa_install_app \
-            --name='salmon' \
-            "$@"
-    else
-        koopa_install_app \
-            --installer='conda-package' \
-            --name='salmon' \
-            "$@"
-    fi
+    koopa_install_app \
+        --name='salmon' \
+        "$@"
 }
 
 koopa_install_sambamba() {
