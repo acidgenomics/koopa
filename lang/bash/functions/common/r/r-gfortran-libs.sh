a39aae6637e4bbfd6f0716b742ace0511b702c90
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-11 06:48:42 -0400

    Rework fortran config

--- lang/bash/functions/common/r/r-gfortran-libs.sh
+++ lang/bash/functions/common/r/r-gfortran-libs.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME Use system gcc here on Linux.
-# FIXME Rework to look for libgfortran instead.
-# FIXME Can just return the flags on Linux.
-
 koopa_r_gfortran_libs() {
     # """
     # Define FLIBS for our R gfortran configuration.
@@ -13,43 +9,37 @@ koopa_r_gfortran_libs() {
     # 'lib64' subdirs. See also 'gcc --print-search-dirs'.
     # """
     local -A app dict
-    local -a flibs libs
-    local i
+    local -a flibs libs libs2
+    local lib
     koopa_assert_has_no_args "$#"
-    app['dirname']="$(koopa_locate_dirname --allow-system)"
-    app['sort']="$(koopa_locate_sort --allow-system)"
-    app['xargs']="$(koopa_locate_xargs --allow-system)"
-    koopa_assert_is_executable "${app[@]}"
     dict['arch']="$(koopa_arch)"
     if koopa_is_linux
     then
-        dict['lib_prefix']='/usr/lib'
-
+        app['gfortran']="$(koopa_locate_gfortran --only-system)"
+        koopa_assert_is_executable "${app[@]}"
     elif koopa_is_macos
     then
+        app['dirname']="$(koopa_locate_dirname --allow-system)"
+        app['sort']="$(koopa_locate_sort --allow-system)"
+        app['xargs']="$(koopa_locate_xargs --allow-system)"
+        koopa_assert_is_executable "${app[@]}"
         case "${dict['arch']}" in
             'arm64')
                 dict['arch']='aarch64'
                 ;;
         esac
         dict['lib_prefix']='/opt/gfortran/lib'
-    fi
-    koopa_assert_is_dir "${dict['lib_prefix']}"
-    # FIXME Need to look for libgfortran specifically here.
-    readarray -t libs <<< "$( \
-        koopa_find \
-            --pattern='*.a' \
-            --prefix="${dict['lib_prefix']}" \
-            --type='f' \
-        | "${app['xargs']}" -I '{}' "${app['dirname']}" '{}' \
-        | "${app['sort']}" --unique \
-    )"
-    koopa_assert_is_array_non_empty "${libs[@]:-}"
-    if koopa_is_macos
-    then
+        koopa_assert_is_dir "${dict['lib_prefix']}"
+        readarray -t libs <<< "$( \
+            koopa_find \
+                --pattern='libgfortran.a' \
+                --prefix="${dict['lib_prefix']}" \
+                --type='f' \
+            | "${app['xargs']}" -I '{}' "${app['dirname']}" '{}' \
+            | "${app['sort']}" --unique \
+        )"
+        koopa_assert_is_array_non_empty "${libs[@]:-}"
         # Need to exclude other architectures in the universal build.
-        local -a libs2
-        local lib
         for lib in "${libs[@]}"
         do
             case "$lib" in
@@ -60,12 +50,12 @@ koopa_r_gfortran_libs() {
         done
         koopa_assert_is_array_non_empty "${libs2[@]:-}"
         libs=("${libs2[@]}")
+        libs+=("${dict['lib_prefix']}")
+        for lib in "${libs[@]}"
+        do
+            flibs+=("-L${lib}")
+        done
     fi
-    libs+=("${dict['lib_prefix']}")
-    for i in "${!libs[@]}"
-    do
-        flibs+=("-L${libs[$i]}")
-    done
     flibs+=('-lgfortran')
     if koopa_is_linux
     then
