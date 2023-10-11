d7f4bbf6d8ecf38b4357cb846a037141fc820a68
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-11 06:21:58 -0400

    Revert to installing from conda

--- lang/bash/functions/common/i/install-star.sh
+++ lang/bash/functions/common/i/install-star.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-# FIXME Consider just building from source on aarch64.
-
 koopa_install_star() {
-    # > koopa_assert_is_not_aarch64
-    # > koopa_install_app \
-    # >     --installer='conda-package' \
-    # >     --name='star' \
-    # >     "$@"
+    koopa_assert_is_not_aarch64
     koopa_install_app \
+        --installer='conda-package' \
         --name='star' \
         "$@"
 }
