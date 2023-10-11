a995382f0707d710e39384e66e6b2808e030bb37
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-11 08:23:07 -0400

    Initial version of koopa_str_unique_by_space

--- lang/bash/functions/common/s/str-unique-by-space.sh
+++ lang/bash/functions/common/s/str-unique-by-space.sh
@@ -7,10 +7,31 @@ koopa_str_unique_by_space() {
     #
     # Primarily intended for use during app activation, to sanitize CPPFLAGS,
     # LDFLAGS, and other build variables.
+    #
+    # @seealso
+    # - https://stackoverflow.com/questions/13648410/
     # 
     # @examples
-    # koopa_str_unique_by_space '-I/usr/include -I/usr/include'
+    # koopa_str_unique_by_space \
+    #   '-I/usr/include -I/usr/include' \
+    #   '-L/usr/lib -L/usr/lib'
     # """
-    koopa_stop 'FIXME IN PROGRESS'
+    local -A app
+    local str str2
+    koopa_assert_has_args "$#"
+    app['tr']="$(koopa_locate_tr --allow-system)"
+    app['uniq']="$(koopa_locate_uniq --allow-system)"
+    koopa_assert_is_executable "${app[@]}"
+    for str in "$@"
+    do
+        str2="$( \
+            koopa_print "$str" \
+                | "${app['tr']}" ' ' '\n' \
+                | "${app['uniq']}" \
+                | "${app['tr']}" '\n' ' ' \
+        )"
+        [[ -n "$str2" ]] || return 1
+        koopa_print "$str2"
+    done
     return 0
 }
