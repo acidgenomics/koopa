4344121df2766d028d9ff3e7a1b0403590686c84
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-11 06:21:58 -0400

    Revert to installing from conda

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -14526,7 +14526,9 @@ koopa_install_star_fusion() {
 }
 
 koopa_install_star() {
+    koopa_assert_is_not_aarch64
     koopa_install_app \
+        --installer='conda-package' \
         --name='star' \
         "$@"
 }
