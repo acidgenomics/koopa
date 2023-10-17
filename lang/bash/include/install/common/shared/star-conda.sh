f3485173c2be5e3356ee68e40bda810f3a3358ee
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-17 16:09:10 -0400

    Only patch on linux

--- lang/bash/include/install/common/shared/star-conda.sh
+++ lang/bash/include/install/common/shared/star-conda.sh
@@ -10,26 +10,29 @@ main() {
     koopa_assert_is_executable "${app[@]}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     koopa_install_conda_package
-    dict['patch_file']='patch-script-dir.patch'
-    read -r -d '' "dict[patch_string]" << END || true
+    if koopa_is_linux
+    then
+        dict['patch_file']='patch-script-dir.patch'
+        read -r -d '' "dict[patch_string]" << END || true
 3c3
 < SCRIPT_DIR=\$( cd -- "\$( dirname -- "\${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ---
 > SCRIPT_DIR=${dict['prefix']}
 END
-    koopa_write_lines \
-        --file="${dict['patch_file']}" \
-        --string="${dict['patch_string']}"
-    "${app['patch']}" \
-        --unified \
-        --verbose \
-        "${dict['prefix']}/libexec/bin/STAR" \
-        "${dict['patch_file']}"
-    "${app['patch']}" \
-        --unified \
-        --verbose \
-        "${dict['prefix']}/libexec/bin/STARlong" \
-        "${dict['patch_file']}"
+        koopa_write_lines \
+            --file="${dict['patch_file']}" \
+            --string="${dict['patch_string']}"
+        "${app['patch']}" \
+            --unified \
+            --verbose \
+            "${dict['prefix']}/libexec/bin/STAR" \
+            "${dict['patch_file']}"
+        "${app['patch']}" \
+            --unified \
+            --verbose \
+            "${dict['prefix']}/libexec/bin/STARlong" \
+            "${dict['patch_file']}"
+    fi
     "${dict['prefix']}/bin/STAR" -h
     "${dict['prefix']}/bin/STARlong" -h
     return 0
