16292c2c896919c95122e89e6d71d994a9e1ab7c
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-17 15:33:30 -0400

    Ensure we build kallisto from source to avoid core dump issue on Ubuntu

--- lang/bash/functions/common/i/install-kallisto.sh
+++ lang/bash/functions/common/i/install-kallisto.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 
 koopa_install_kallisto() {
-    # FIXME Need to rework this...
     koopa_install_app \
         --name='kallisto' \
         "$@"
-    return 0
-    if koopa_is_macos && koopa_is_aarch64
-    then
-        koopa_install_app \
-            --name='kallisto' \
-            "$@"
-    else
-        koopa_install_app \
-            --installer='conda-package' \
-            --name='kallisto' \
-            "$@"
-    fi
 }
