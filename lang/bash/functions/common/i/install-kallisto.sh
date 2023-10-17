d2d54ea9d058cdda1396ceb8128d5a19660c7c0e
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-17 15:26:43 -0400

    Attempt to see if we can improve kallisto on ubuntu 22

--- lang/bash/functions/common/i/install-kallisto.sh
+++ lang/bash/functions/common/i/install-kallisto.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 koopa_install_kallisto() {
+    # FIXME Need to rework this...
+    koopa_install_app \
+        --name='kallisto' \
+        "$@"
+    return 0
     if koopa_is_macos && koopa_is_aarch64
     then
         koopa_install_app \
