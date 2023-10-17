a3f94c16493f30cb4837d46d80e7548dc765c27e
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-17 16:08:34 -0400

    Need to patch the script_dir path

--- lang/bash/include/install/common/shared/star-conda.sh
+++ lang/bash/include/install/common/shared/star-conda.sh
@@ -5,9 +5,32 @@ main() {
     # Install STAR from bioconda.
     # @note Updated 2023-10-17.
     # """
-    local -A dict
+    local -A app dict
+    app['patch']="$(koopa_locate_patch)"
+    koopa_assert_is_executable "${app[@]}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     koopa_install_conda_package
-    # FIXME Need to patch SCRIPT_DIR path for STAR and STARlong on Linux.
+    dict['patch_file']='patch-script-dir.patch'
+    read -r -d '' "dict[patch_string]" << END || true
+3c3
+< SCRIPT_DIR=\$( cd -- "\$( dirname -- "\${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+---
+> SCRIPT_DIR=${dict['prefix']}
+END
+    koopa_write_lines \
+        --file="${dict['patch_file']}" \
+        --string="${dict['patch_string']}"
+    "${app['patch']}" \
+        --unified \
+        --verbose \
+        "${dict['prefix']}/libexec/bin/STAR" \
+        "${dict['patch_file']}"
+    "${app['patch']}" \
+        --unified \
+        --verbose \
+        "${dict['prefix']}/libexec/bin/STARlong" \
+        "${dict['patch_file']}"
+    "${dict['prefix']}/bin/STAR" -h
+    "${dict['prefix']}/bin/STARlong" -h
     return 0
 }
