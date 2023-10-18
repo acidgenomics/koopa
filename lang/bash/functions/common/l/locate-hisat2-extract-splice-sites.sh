1d7e946e3ae65f15978446f211becfb1cb2172a2
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-18 09:56:39 -0400

    Add hisat2 locator functions

---
+++ lang/bash/functions/common/l/locate-hisat2-extract-splice-sites.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+koopa_locate_hisat2_extract_splice_sites() {
+    # Alternatively, can use 'extract_splice_sites.py'.
+    koopa_locate_app \
+        --app-name='hisat2' \
+        --bin-name='hisat2_extract_splite_sites.py' \
+        "$@"
+}
---
+++ lang/bash/functions/common/l/locate-hisat2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+koopa_locate_hisat2() {
+    koopa_locate_app \
+        --app-name='hisat2' \
+        --bin-name='hisat2' \
+        "$@"
+}
