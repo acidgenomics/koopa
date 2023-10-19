8f5c4eaef7d793997096bcfeba4c891b4f532da1
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-19 09:35:35 -0400

    Fix typo

--- lang/bash/include/install/common/shared/tree-sitter.sh
+++ lang/bash/include/install/common/shared/tree-sitter.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# NOTE Consider adding support for node and rust in a future update.
-
 main() {
     # """
     # Install tree-sitter.
-    # @note Updated 2023-06-02.
+    # @note Updated 2023-10-19.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/HEAD/
@@ -19,7 +17,7 @@ main() {
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['url']="https://github.com/treesitter/treesitter/archive/\
 v${dict['version']}.tar.gz"
-    koopa_download "${dict['url']}" "${dict['file']}"
+    koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
     koopa_print_env
