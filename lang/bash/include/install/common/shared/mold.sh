f897d129217689f04f12e909a1cdd320f30f6f84
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-19 09:10:20 -0400

    Attempt to fix linux build

--- lang/bash/include/install/common/shared/mold.sh
+++ lang/bash/include/install/common/shared/mold.sh
@@ -7,13 +7,16 @@ main() {
     #
     # @seealso
     # - https://github.com/rui314/mold
+    # - https://formulae.brew.sh/formula/mold
     # """
     local -A dict
     local -a cmake_args deps
     deps+=('mimalloc' 'tbb' 'zlib' 'zstd')
     koopa_activate_app "${deps[@]}"
+    dict['jobs']="$(koopa_cpu_count)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    koopa_is_linux && dict['jobs']=1
     dict['url']="https://github.com/rui314/mold/archive/refs/tags/\
 v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
@@ -26,6 +29,9 @@ v${dict['version']}.tar.gz"
         '-DMOLD_USE_SYSTEM_MIMALLOC=ON'
         '-DMOLD_USE_SYSTEM_TBB=ON'
     )
-    koopa_cmake_build --prefix="${dict['prefix']}" "${cmake_args[@]}"
+    koopa_cmake_build \
+        --jobs="${dict['jobs']}" \
+        --prefix="${dict['prefix']}" \
+        "${cmake_args[@]}"
     return 0
 }
