c6dd31c11c3aeaed60dfb4950d68a6f2ff15a804
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-20 17:34:41 -0400

    Fix typo

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -10626,9 +10626,7 @@ koopa_hisat2_fastq_quality_format() {
     koopa_assert_has_args_eq "$#" 1
     dict['fastq_file']="${1:?}"
     koopa_assert_is_file "${dict['fastq_file']}"
-    dict['format']="$( \
-        koopa_fastq_detect_quality_format "${dict['fastq_file']}" \
-    )"
+    dict['format']="$(koopa_fastq_detect_quality_score "${dict['fastq_file']}")"
     case "${dict['format']}" in
         'Phread+33')
             dict['flag']='--phred33'
--- lang/bash/functions/common/h/hisat2-fastq-quality-format.sh
+++ lang/bash/functions/common/h/hisat2-fastq-quality-format.sh
@@ -3,7 +3,7 @@
 koopa_hisat2_fastq_quality_format() {
     # """
     # Determine whether we should set FASTQ quality score (Phred) flag.
-    # @note Updated 2023-04-06.
+    # @note Updated 2023-10-20.
     #
     # Consider adding support for Solexa sequencing here.
     # """
@@ -11,9 +11,7 @@ koopa_hisat2_fastq_quality_format() {
     koopa_assert_has_args_eq "$#" 1
     dict['fastq_file']="${1:?}"
     koopa_assert_is_file "${dict['fastq_file']}"
-    dict['format']="$( \
-        koopa_fastq_detect_quality_format "${dict['fastq_file']}" \
-    )"
+    dict['format']="$(koopa_fastq_detect_quality_score "${dict['fastq_file']}")"
     case "${dict['format']}" in
         'Phread+33')
             dict['flag']='--phred33'
