2872b97265efcc82038f85babe078b08eaa8df04
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-20 17:38:51 -0400

    Fix typo

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -10628,10 +10628,10 @@ koopa_hisat2_fastq_quality_format() {
     koopa_assert_is_file "${dict['fastq_file']}"
     dict['format']="$(koopa_fastq_detect_quality_score "${dict['fastq_file']}")"
     case "${dict['format']}" in
-        'Phread+33')
+        'Phred+33')
             dict['flag']='--phred33'
             ;;
-        'Phread+64')
+        'Phred+64')
             dict['flag']='--phred64'
             ;;
         *)
