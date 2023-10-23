47e66f0d7d5498048e042cbddf628597249913f6
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-23 10:38:37 -0400

    Debug duplicate file decompression weirdness

--- lang/bash/functions/common/h/hisat2-align-paired-end-per-sample.sh
+++ lang/bash/functions/common/h/hisat2-align-paired-end-per-sample.sh
@@ -178,6 +178,7 @@ koopa_hisat2_align_paired_end_per_sample() {
                 --index-dir="${dict['salmon_index_dir']}" \
         )"
     fi
+    koopa_stop "FIXME $PWD"
     dict['lib_type']="$(koopa_hisat2_fastq_library_type "${dict['lib_type']}")"
     if [[ -n "${dict['lib_type']}" ]]
     then
