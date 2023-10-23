dbac22cf9d85329c2582fb85a3ca4a0641ea0f68
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-23 09:14:51 -0400

    Fix URL

--- lang/bash/include/install/common/shared/pyenv.sh
+++ lang/bash/include/install/common/shared/pyenv.sh
@@ -3,13 +3,13 @@
 main() {
     # """
     # Install pyenv.
-    # @note Updated 2023-10-19.
+    # @note Updated 2023-10-23.
     # """
     local -A dict
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['url']="https://github.com/pyenv/pyenv/archive/refs/tags/\
-${dict['version']}.tar.gz"
+v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract \
         "$(koopa_basename "${dict['url']}")" \
