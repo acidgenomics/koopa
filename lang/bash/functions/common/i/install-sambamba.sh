2385caf89658834f5c19f041ba27689a0d85d97f
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-24 15:59:26 -0400

    Add note

--- lang/bash/functions/common/i/install-sambamba.sh
+++ lang/bash/functions/common/i/install-sambamba.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# NOTE 1.0.1 will build on Apple Silicon, but 1.0 (current conda) is broken.
+
 koopa_install_sambamba() {
     koopa_assert_is_not_aarch64
     koopa_install_app \
