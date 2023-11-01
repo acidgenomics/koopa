0213fffa094b041e57db59738563d306e8536d39
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-01 16:39:26 -0400

    Add rmats support

---
+++ lang/bash/functions/common/i/install-rmats.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+koopa_install_rmats() {
+    koopa_assert_is_not_aarch64
+    koopa_install_app \
+        --name='rmats' \
+        --installer='conda-package' \
+        "$@"
+}
