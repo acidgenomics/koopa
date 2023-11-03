3b8c179c4c1dd65dfcb5049173c0024dfd3ac46d
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-03 09:50:57 -0400

    Add FIXME regarding ld warning on macOS

--- lang/bash/include/install/common/shared/r.sh
+++ lang/bash/include/install/common/shared/r.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# FIXME Need to resolve this duplicate library warnings on macOS?
+# This may be package specific and not easy to resolve...look into it
+# ld: warning: ignoring duplicate libraries: '-lX11', '-lpng16'
+
 main() {
     # """
     # Install R.
-    # @note Updated 2023-10-19.
+    # @note Updated 2023-11-03.
     #
     # @section Compiler settings:
     #
