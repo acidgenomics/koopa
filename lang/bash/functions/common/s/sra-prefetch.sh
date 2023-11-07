c2223d90cd89b4700312f6945f07808a8f28245c
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-07 07:59:24 -0500

    Improve prefetcher and assert that SRA toolkit is configured

--- lang/bash/functions/common/s/sra-prefetch.sh
+++ lang/bash/functions/common/s/sra-prefetch.sh
@@ -2,15 +2,11 @@
 
 koopa_sra_prefetch() {
     # """
-    # Prefetch files from SRA (in parallel).
-    # @note Updated 2023-11-06.
+    # Prefetch files from SRA.
+    # @note Updated 2023-11-07.
     #
     # Alternatively, can sync directly from AWS with:
     # > aws s3 sync s3://sra-pub-run-odp/sra/<SRR_ID>/ ./<SRR_ID>/
-    #
-    # @seealso
-    # - Conda build of sratools prefetch isn't currently working on macOS.
-    #   https://github.com/ncbi/sra-tools/issues/497
     #
     # @examples
     # > koopa_sra_prefetch \
@@ -18,12 +14,10 @@ koopa_sra_prefetch() {
     # >     --output-directory='srp049596-prefetch'
     # """
     local -A app dict
-    local -a prefetch_cmd
-    app['parallel']="$(koopa_locate_parallel --allow-system)"
+    local -a prefetch_args
     app['prefetch']="$(koopa_locate_sra_prefetch)"
     koopa_assert_is_executable "${app[@]}"
     dict['acc_file']=''
-    dict['jobs']="$(koopa_cpu_count)"
     dict['output_dir']='sra'
     while (("$#"))
     do
@@ -56,10 +50,11 @@ koopa_sra_prefetch() {
         '--accession-file' "${dict['acc_file']}" \
         '--output-directory' "${dict['output_dir']}"
     koopa_assert_is_file "${dict['acc_file']}"
+    koopa_assert_is_ncbi_sra_toolkit_configured
     dict['output_dir']="$(koopa_init_dir "${dict['output_dir']}")"
-    koopa_alert "Prefetching SRA files to '${dict['output_dir']}'."
-    prefetch_cmd=(
-        "${app['prefetch']}"
+    koopa_alert "Prefetching SRA samples defined in '${dict['acc_file']}' \
+to '${dict['output_dir']}'."
+    prefetch_args+=(
         '--force' 'no'
         '--max-size' '500G'
         '--output-directory' "${dict['output_dir']}"
@@ -68,15 +63,8 @@ koopa_sra_prefetch() {
         '--type' 'sra'
         '--verbose'
         '--verify' 'yes'
-        '{}'
+        "${dict['acc_file']}"
     )
-    "${app['parallel']}" \
-        --arg-file "${dict['acc_file']}" \
-        --bar \
-        --eta \
-        --jobs "${dict['jobs']}" \
-        --progress \
-        --will-cite \
-        "${prefetch_cmd[*]}"
+    "${app['prefetch']}" "${prefetch_args[@]}"
     return 0
 }
