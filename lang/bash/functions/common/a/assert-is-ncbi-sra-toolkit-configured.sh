4641e692b94f33563f1cab2d80fe0efc7ac7e688
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-07 15:10:48 -0500

    Rename function

--- lang/bash/functions/common/a/assert-is-ncbi-sra-toolkit-configured.sh
+++ lang/bash/functions/common/a/assert-is-ncbi-sra-toolkit-configured.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-koopa_is_ncbi_sra_toolkit_configured() {
+koopa_assert_is_ncbi_sra_toolkit_configured() {
     # """
     # Assert that NCBI SRA Toolkit is configured.
     # @note Updated 2023-11-07.
