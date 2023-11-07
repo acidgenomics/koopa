4f6cae801cf3607cd85bef3bb0c63d59f55506db
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-07 07:59:24 -0500

    Improve prefetcher and assert that SRA toolkit is configured

---
+++ lang/bash/functions/common/a/assert-is-ncbi-sra-toolkit-configured.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+koopa_is_ncbi_sra_toolkit_configured() {
+    # """
+    # Assert that NCBI SRA Toolkit is configured.
+    # @note Updated 2023-11-07.
+    # """
+    local conf_file
+    conf_file="${HOME:?}/.ncbi/user-settings.mkfg"
+    if [[ ! -f "$conf_file" ]]
+    then
+        koopa_stop \
+            "NCBI SRA Toolkit is not configured at '${conf_file}'." \
+            "Run 'vdb-config --interactive' to resolve."
+    fi
+    return 0
+}
