57489a2673bc68fbe14ff18abbd65145fb68ecab
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-07 15:32:23 -0500

    Add back parallel support

--- lang/bash/functions/common/s/sra-prefetch.sh
+++ lang/bash/functions/common/s/sra-prefetch.sh
@@ -8,16 +8,23 @@ koopa_sra_prefetch() {
     # Alternatively, can sync directly from AWS with:
     # > aws s3 sync s3://sra-pub-run-odp/sra/<SRR_ID>/ ./<SRR_ID>/
     #
+    # @seealso
+    # - https://bioinformaticsworkbook.org/dataAcquisition/fileTransfer/sra.html
+    #
     # @examples
     # > koopa_sra_prefetch \
     # >     --accession-file='srp049596-accession-list.txt' \
     # >     --output-directory='srp049596-prefetch'
     # """
     local -A app dict
-    local -a prefetch_args
+    local -a parallel_cmd
+    app['parallel']="$(koopa_locate_parallel --allow-system)"
     app['prefetch']="$(koopa_locate_sra_prefetch)"
     koopa_assert_is_executable "${app[@]}"
     dict['acc_file']=''
+    dict['jobs']="$(koopa_cpu_count)"
+    # Set a maximum of 4 transfers at a time, to avoid hammering SRA.
+    [[ "${dict['jobs']}" -gt 4 ]] &&  dict['jobs']=4
     dict['output_dir']='sra'
     while (("$#"))
     do
@@ -54,7 +61,8 @@ koopa_sra_prefetch() {
     dict['output_dir']="$(koopa_init_dir "${dict['output_dir']}")"
     koopa_alert "Prefetching SRA samples defined in '${dict['acc_file']}' \
 to '${dict['output_dir']}'."
-    prefetch_args+=(
+    parallel_cmd=(
+        "${app['prefetch']}"
         '--force' 'no'
         '--max-size' '500G'
         '--output-directory' "${dict['output_dir']}"
@@ -63,8 +71,15 @@ to '${dict['output_dir']}'."
         '--type' 'sra'
         '--verbose'
         '--verify' 'yes'
-        "${dict['acc_file']}"
+        '{}'
     )
-    "${app['prefetch']}" "${prefetch_args[@]}"
+    "${app['parallel']}" \
+        --arg-file "${dict['acc_file']}" \
+        --bar \
+        --eta \
+        --jobs "${dict['jobs']}" \
+        --progress \
+        --will-cite \
+        "${parallel_cmd[*]}"
     return 0
 }
