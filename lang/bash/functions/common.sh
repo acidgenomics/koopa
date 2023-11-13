9ef6c3a1b8ead78949f7033c8aecaf8ff7d279f2
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-13 11:19:16 -0500

    Detect BAM read type

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -3069,27 +3069,29 @@ koopa_aws_s3_sync() {
 }
 
 koopa_bam_read_type() {
-    local -A app
+    local -A app dict
     local bam_file
     koopa_assert_has_args "$#"
     koopa_assert_is_file "$@"
     app['samtools']="$(koopa_locate_samtools)"
     koopa_assert_is_executable "${app[@]}"
+    dict['threads']="$(koopa_cpu_count)"
     for bam_file in "$@"
     do
         local -A dict2
         dict2['bam_file']="$bam_file"
         dict2['num']="$( \
             "${app['samtools']}" view \
+                -@ "${dict['threads']}" \
                 -c \
                 -f 1 \
                 "${dict2['bam_file']}" \
         )"
         if [[ "${dict2['num']}" -gt 0 ]]
         then
-            dict2['type']='single'
-        else
             dict2['type']='paired'
+        else
+            dict2['type']='single'
         fi
         koopa_print "${dict2['type']}"
     done
--- lang/bash/functions/common/b/bam-read-type.sh
+++ lang/bash/functions/common/b/bam-read-type.sh
@@ -8,27 +8,29 @@ koopa_bam_read_type() {
     # @seealso
     # - https://www.biostars.org/p/178730/
     # """
-    local -A app
+    local -A app dict
     local bam_file
     koopa_assert_has_args "$#"
     koopa_assert_is_file "$@"
     app['samtools']="$(koopa_locate_samtools)"
     koopa_assert_is_executable "${app[@]}"
+    dict['threads']="$(koopa_cpu_count)"
     for bam_file in "$@"
     do
         local -A dict2
         dict2['bam_file']="$bam_file"
         dict2['num']="$( \
             "${app['samtools']}" view \
+                -@ "${dict['threads']}" \
                 -c \
                 -f 1 \
                 "${dict2['bam_file']}" \
         )"
         if [[ "${dict2['num']}" -gt 0 ]]
         then
-            dict2['type']='single'
-        else
             dict2['type']='paired'
+        else
+            dict2['type']='single'
         fi
         koopa_print "${dict2['type']}"
     done
