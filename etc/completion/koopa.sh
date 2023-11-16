4901c050341ed2b6b5a0925f9a04ac41b28b8652
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-16 09:24:02 -0500

    Add cli support for rmats

--- etc/completion/koopa.sh
+++ etc/completion/koopa.sh
@@ -4,7 +4,7 @@
 _koopa_complete() {
     # """
     # Bash/Zsh TAB completion for primary 'koopa' program.
-    # @note Updated 2023-11-15.
+    # @note Updated 2023-11-16.
     #
     # Keep all of these commands in a single file.
     # Sourcing multiple scripts doesn't work reliably.
@@ -55,6 +55,7 @@ _koopa_complete() {
                         'jekyll'
                         'kallisto'
                         'r'
+                        'rmats'
                         'rnaeditingindexer'
                         'rsem'
                         'salmon'
