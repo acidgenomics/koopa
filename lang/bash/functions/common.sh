44cf390bb5a260ae827f737d08a768d1fbd25b52
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-17 14:11:29 -0500

    Add support for MISO index

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -19807,11 +19807,12 @@ koopa_merge_pdf() {
 }
 
 koopa_miso_index() {
-    local -A app dict
+    local -A app bool dict
     koopa_activate_app_conda_env 'misopy'
     app['index_gff']="$(koopa_locate_miso_index_gff)"
     app['tee']="$(koopa_locate_tee --allow-system)"
     koopa_assert_is_executable "${app[@]}"
+    bool['tmp_gff3_file']=0
     dict['gff3_file']=''
     dict['output_dir']=''
     while (("$#"))
@@ -19846,11 +19847,26 @@ koopa_miso_index() {
     dict['gff3_file']="$(koopa_realpath "${dict['gff3_file']}")"
     dict['output_dir']="$(koopa_init_dir "${dict['output_dir']}")"
     dict['log_file']="${dict['output_dir']}/index.log"
+    koopa_alert "Generating MISO index at '${dict['output_dir']}'."
+    if koopa_is_compressed_file "${dict['gff3_file']}"
+    then
+        bool['tmp_gff3_file']=1
+        dict['tmp_gff3_file']="$(koopa_tmp_file_in_wd)"
+        koopa_decompress \
+            --input-file="${dict['gff3_file']}" \
+            --output-file="${dict['tmp_gff3_file']}"
+        dict['gff3_file']="${dict['tmp_gff3_file']}"
+    fi
     "${app['index_gff']}" \
         --index \
         "${dict['gff3_file']}" \
         "${dict['output_dir']}" \
         |& "${app['tee']}" -a "${dict['log_file']}"
+    if [[ "${bool['tmp_gff3_file']}" -eq 1 ]]
+    then
+        koopa_rm "${dict['gff3_file']}"
+    fi
+    koopa_alert_success "MISO index created at '${dict['output_dir']}'."
     return 0
 }
 
