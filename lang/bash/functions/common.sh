c2e9b02a08b74c3b83de774f442732a0cc5db041
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-17 14:16:36 -0500

    Rework miso index approach

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -18827,6 +18827,13 @@ koopa_locate_minimap2() {
         "$@"
 }
 
+koopa_locate_miso_exon_utils() {
+    koopa_locate_app \
+        --app-name='misopy' \
+        --bin-name='exon_utils' \
+        "$@"
+}
+
 koopa_locate_miso_index_gff() {
     koopa_locate_app \
         --app-name='misopy' \
@@ -19809,21 +19816,23 @@ koopa_merge_pdf() {
 koopa_miso_index() {
     local -A app bool dict
     koopa_activate_app_conda_env 'misopy'
+    app['exon_utils']="$(koopa_locate_miso_exon_utils)"
     app['index_gff']="$(koopa_locate_miso_index_gff)"
     app['tee']="$(koopa_locate_tee --allow-system)"
     koopa_assert_is_executable "${app[@]}"
-    bool['tmp_gff3_file']=0
-    dict['gff3_file']=''
+    bool['tmp_gff_file']=0
+    dict['gff_file']=''
+    dict['min_exon_size']=1000
     dict['output_dir']=''
     while (("$#"))
     do
         case "$1" in
-            '--gff3-file='*)
-                dict['gff3_file']="${1#*=}"
+            '--gff-file='*)
+                dict['gff_file']="${1#*=}"
                 shift 1
                 ;;
-            '--gff3-file')
-                dict['gff3_file']="${2:?}"
+            '--gff-file')
+                dict['gff_file']="${2:?}"
                 shift 2
                 ;;
             '--output-dir='*)
@@ -19840,31 +19849,37 @@ koopa_miso_index() {
         esac
     done
     koopa_assert_is_set \
-        '--gff3-file' "${dict['gff3_file']}" \
+        '--gff-file' "${dict['gff_file']}" \
         '--output-dir' "${dict['output_dir']}"
-    koopa_assert_is_file "${dict['gff3_file']}"
+    koopa_assert_is_file "${dict['gff_file']}"
     koopa_assert_is_not_dir "${dict['output_dir']}"
-    dict['gff3_file']="$(koopa_realpath "${dict['gff3_file']}")"
+    dict['gff_file']="$(koopa_realpath "${dict['gff_file']}")"
     dict['output_dir']="$(koopa_init_dir "${dict['output_dir']}")"
     dict['log_file']="${dict['output_dir']}/index.log"
+    dict['exons_dir']="${dict['output_dir']}/exons"
     koopa_alert "Generating MISO index at '${dict['output_dir']}'."
-    if koopa_is_compressed_file "${dict['gff3_file']}"
+    if koopa_is_compressed_file "${dict['gff_file']}"
     then
-        bool['tmp_gff3_file']=1
-        dict['tmp_gff3_file']="$(koopa_tmp_file_in_wd)"
+        bool['tmp_gff_file']=1
+        dict['tmp_gff_file']="$(koopa_tmp_file_in_wd)"
         koopa_decompress \
-            --input-file="${dict['gff3_file']}" \
-            --output-file="${dict['tmp_gff3_file']}"
-        dict['gff3_file']="${dict['tmp_gff3_file']}"
+            --input-file="${dict['gff_file']}" \
+            --output-file="${dict['tmp_gff_file']}"
+        dict['gff_file']="${dict['tmp_gff_file']}"
     fi
     "${app['index_gff']}" \
         --index \
-        "${dict['gff3_file']}" \
+        "${dict['gff_file']}" \
         "${dict['output_dir']}" \
         |& "${app['tee']}" -a "${dict['log_file']}"
-    if [[ "${bool['tmp_gff3_file']}" -eq 1 ]]
+    "${app['exon_utils']}" \
+        --get-const-exons "${dict['gff_file']}" \
+        --min-exon-size "${dict['min_exon_size']}" \
+        --output-dir "${dict['exons_dir']}" \
+        |& "${app['tee']}" -a "${dict['log_file']}"
+    if [[ "${bool['tmp_gff_file']}" -eq 1 ]]
     then
-        koopa_rm "${dict['gff3_file']}"
+        koopa_rm "${dict['gff_file']}"
     fi
     koopa_alert_success "MISO index created at '${dict['output_dir']}'."
     return 0
--- lang/bash/functions/common/m/miso-index.sh
+++ lang/bash/functions/common/m/miso-index.sh
@@ -7,24 +7,27 @@ koopa_miso_index() {
     # """
     local -A app bool dict
     koopa_activate_app_conda_env 'misopy'
+    app['exon_utils']="$(koopa_locate_miso_exon_utils)"
     app['index_gff']="$(koopa_locate_miso_index_gff)"
     app['tee']="$(koopa_locate_tee --allow-system)"
     koopa_assert_is_executable "${app[@]}"
-    bool['tmp_gff3_file']=0
+    bool['tmp_gff_file']=0
     # e.g. 'gencode.v44.annotation.gff3.gz'.
-    dict['gff3_file']=''
+    dict['gff_file']=''
+    # e.g. at least 500.
+    dict['min_exon_size']=1000
     # e.g. 'homo-sapiens-grch38-gencode-44'.
     dict['output_dir']=''
     while (("$#"))
     do
         case "$1" in
             # Key-value pairs --------------------------------------------------
-            '--gff3-file='*)
-                dict['gff3_file']="${1#*=}"
+            '--gff-file='*)
+                dict['gff_file']="${1#*=}"
                 shift 1
                 ;;
-            '--gff3-file')
-                dict['gff3_file']="${2:?}"
+            '--gff-file')
+                dict['gff_file']="${2:?}"
                 shift 2
                 ;;
             '--output-dir='*)
@@ -42,31 +45,37 @@ koopa_miso_index() {
         esac
     done
     koopa_assert_is_set \
-        '--gff3-file' "${dict['gff3_file']}" \
+        '--gff-file' "${dict['gff_file']}" \
         '--output-dir' "${dict['output_dir']}"
-    koopa_assert_is_file "${dict['gff3_file']}"
+    koopa_assert_is_file "${dict['gff_file']}"
     koopa_assert_is_not_dir "${dict['output_dir']}"
-    dict['gff3_file']="$(koopa_realpath "${dict['gff3_file']}")"
+    dict['gff_file']="$(koopa_realpath "${dict['gff_file']}")"
     dict['output_dir']="$(koopa_init_dir "${dict['output_dir']}")"
     dict['log_file']="${dict['output_dir']}/index.log"
+    dict['exons_dir']="${dict['output_dir']}/exons"
     koopa_alert "Generating MISO index at '${dict['output_dir']}'."
-    if koopa_is_compressed_file "${dict['gff3_file']}"
+    if koopa_is_compressed_file "${dict['gff_file']}"
     then
-        bool['tmp_gff3_file']=1
-        dict['tmp_gff3_file']="$(koopa_tmp_file_in_wd)"
+        bool['tmp_gff_file']=1
+        dict['tmp_gff_file']="$(koopa_tmp_file_in_wd)"
         koopa_decompress \
-            --input-file="${dict['gff3_file']}" \
-            --output-file="${dict['tmp_gff3_file']}"
-        dict['gff3_file']="${dict['tmp_gff3_file']}"
+            --input-file="${dict['gff_file']}" \
+            --output-file="${dict['tmp_gff_file']}"
+        dict['gff_file']="${dict['tmp_gff_file']}"
     fi
     "${app['index_gff']}" \
         --index \
-        "${dict['gff3_file']}" \
+        "${dict['gff_file']}" \
         "${dict['output_dir']}" \
         |& "${app['tee']}" -a "${dict['log_file']}"
-    if [[ "${bool['tmp_gff3_file']}" -eq 1 ]]
+    "${app['exon_utils']}" \
+        --get-const-exons "${dict['gff_file']}" \
+        --min-exon-size "${dict['min_exon_size']}" \
+        --output-dir "${dict['exons_dir']}" \
+        |& "${app['tee']}" -a "${dict['log_file']}"
+    if [[ "${bool['tmp_gff_file']}" -eq 1 ]]
     then
-        koopa_rm "${dict['gff3_file']}"
+        koopa_rm "${dict['gff_file']}"
     fi
     koopa_alert_success "MISO index created at '${dict['output_dir']}'."
     return 0
