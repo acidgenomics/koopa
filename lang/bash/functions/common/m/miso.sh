c77e60a08b0679342391f5325b4af28710551386
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-17 12:08:51 -0500

    Improve logging

--- lang/bash/functions/common/m/miso.sh
+++ lang/bash/functions/common/m/miso.sh
@@ -28,6 +28,28 @@ koopa_miso() {
     #
     # This expects a sorted, indexed BAM file as input.
     #
+    # @section Configuring MISO:
+    #
+    # The default configuration file for MISO is available in
+    # 'settings/miso_settings.txt'.
+    #
+    # How to set strandedness:
+    # [data]
+    # strand = fr-unstranded
+    #
+    # The [data] section contains parameters related to the way reads should be
+    # handled. These are:
+    # - filter_results: Specifies whether or not events should be filtered for
+    #   coverage (True or False).
+    # - min_event_reads: What the minimum number of reads that a region must
+    #   have for it to be quantitated. The minimum number of reads is computed
+    #   over the longest genomic region of the gene or alternatively spliced
+    #   event.
+    # - strand (optional): What the strand convention of the input BAM files is.
+    #   Can be set to either fr-unstranded, fr-firststrand, or fr-secondstrand.
+    #   Set to fr-unstranded by default. See Note on strand-specificity in
+    #   RNA-Seq libraries for an explanation of these strand conventions.
+    #
     # May need to set MISO configuration regarding strandedness:
     # - https://mailman.mit.edu/pipermail/miso-users/
     #     Week-of-Mon-20130805/000257.html
@@ -76,15 +98,19 @@ koopa_miso() {
     # e.g. 'FIXME'.
     dict['gtf_file']=''
     dict['index_dir']="$(koopa_tmp_dir_in_wd)"
+    # Using salmon library type conventions here.
+    dict['lib_type']='A'
     dict['num_proc']="$(koopa_cpu_count)"
     # e.g. 150.
     dict['read_length']=''
     # e.g. 'miso/star-gencode/treatment-vs-control'.
     dict['output_dir']=''
     # e.g. 250.
-    dict['paired_insert_length_mean']=''
+    dict['paired_ins_len_mean']=''
     # e.g. 15.
-    dict['paired_insert_length_std_dev']=''
+    dict['paired_ins_len_std_dev']=''
+    # e.g. 'paired'.
+    dict['read_type']=''
     while (("$#"))
     do
         case "$1" in
@@ -161,35 +187,85 @@ koopa_miso() {
         "${dict['bam_file']}" \
         "${dict['genome_fasta_file']}" \
         "${dict['gtf_file']}"
+    koopa_assert_is_matching_regex \
+        --pattern='\.bam$' \
+        --string="${dict['bam_file']}"
     koopa_assert_is_not_dir "${dict['output_dir']}"
     dict['output_dir']="$(koopa_init_dir "${dict['output_dir']}")"
     dict['log_file']="${dict['output_dir']}/miso.log"
+    dict['settings_file']="${dict['output_dir']}/settings.txt"
     koopa_alert "Running MISO analysis in '${dict['output_dir']}'."
+    if [[ "${dict['lib_type']}" == 'A' ]]
+    then
+        koopa_alert 'Detecting BAM library type with salmon.'
+        dict['lib_type']="$( \
+            koopa_salmon_detect_bam_library_type \
+                --bam-file="${dict['bam_file']}" \
+                --fasta-file="${dict['genome_fasta_file']}" \
+        )"
+    fi
+    dict['lib_type']="$( \
+        koopa_salmon_library_type_to_miso "${dict['lib_type']}" \
+    )"
     if [[ -z "${dict['read_length']}" ]]
     then
         koopa_alert 'Detecting BAM read length.'
         dict['read_length']="$(koopa_bam_read_length "${dict['bam_file']}")"
     fi
+    if [[ -z "${dict['read_type']}" ]]
+    then
+        koopa_alert 'Detecting BAM read type.'
+        dict['read_type']="$(koopa_bam_read_type "${dict['bam_file']}")"
+    fi
+    case "${dict['read_type']}" in
+        'paired')
+            bool['paired']=1
+            ;;
+        'single')
+            ;;
+        *)
+            koopa_stop "Unsupported read type: '${dict['read_type']}'."
+            ;;
+    esac
+    if [[ "${bool['paired']}" -eq 1 ]]
+    then
+        # FIXME Need to add suport for calc of these.
+        dict['paired_ins_len_mean']='FIXME'
+        dict['paired_ins_len_std_dev']='FIXME'
+    fi
+    read -r -d '' "dict[settings_string]" << END || true
+[data]
+filter_results = True
+min_event_reads = 20
+strand = ${dict['lib_type']}
+END
+    koopa_write_string \
+        --file="${dict['settings_file']}" \
+        --string="${dict['settings_string']}"
     koopa_alert "Generating MISO index in '${dict['index_dir']}'."
     "${app['index_gff']}" --index "${dict['gtf_file']}" "${dict['index_dir']}"
     miso_args+=(
         '--run' # COMPUTE_GENES_PSI
-            "${dict['index_dir']}" "${dict['bam_file']}"
+            "${dict['index_dir']}"
+            "${dict['bam_file']}"
         '-p' "${dict['num_proc']}"
         '--output-dir' "${dict['output_dir']}"
         '--read-len' "${dict['read_length']}"
+        '--settings-filename' "${dict['settings_file']}"
     )
     if [[ "${bool['paired']}" -eq 1 ]]
     then
         # FIXME Need to calculate these values here.
         miso_args+=(
             '--paired-end'
-                "${dict['paired_insert_length_mean']}"
-                "${dict['paired_insert_length_std_dev']}"
+                "${dict['paired_ins_len_mean']}"
+                "${dict['paired_ins_len_std_dev']}"
         )
     fi
-    koopa_dl 'miso args' "${miso_args[*]}"
+    koopa_dl 'miso' "${miso_args[*]}"
+    koopa_print "${app['miso']} ${miso_args[*]}" \
+        >> "${dict['log_file']}"
     "${app['miso']}" "${miso_args[@]}" \
-        2>&1 | "${app['tee']}" "${dict['log_file']}"
+        |& "${app['tee']}" -a "${dict['log_file']}"
     return 0
 }
