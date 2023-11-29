7cb848c834585589e3fe91a81c60eff5b928ca04
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-29 09:56:51 -0500

    Add verbose mode

--- lang/bash/functions/common/r/rmats.sh
+++ lang/bash/functions/common/r/rmats.sh
@@ -3,7 +3,7 @@
 koopa_rmats() {
     # """
     # Run rMATS analysis on unpaired samples.
-    # @note Updated 2023-11-28.
+    # @note Updated 2023-11-29.
     #
     # @seealso
     # - https://rnaseq-mats.sourceforge.io/
@@ -33,6 +33,7 @@ koopa_rmats() {
     app['tr']="$(koopa_locate_tr --allow-system)"
     koopa_assert_is_executable "${app[@]}"
     bool['tmp_gtf_file']=0
+    bool['verbose']=0
     # e.g. 'b1.txt': control samples.
     dict['b1_file']=''
     # e.g. 'b2.txt': treated samples.
@@ -129,12 +130,18 @@ koopa_rmats() {
                 dict['read_type']="${2:?}"
                 shift 2
                 ;;
+            # Flags ------------------------------------------------------------
+            '--verbose')
+                bool['verbose']=1
+                shift 1
+                ;;
             # Other ------------------------------------------------------------
             *)
                 koopa_invalid_arg "$1"
                 ;;
         esac
     done
+    [[ "${bool['verbose']}" -eq 1 ]] && set -x
     koopa_assert_is_set \
         '--alpha-threshold' "${dict['cstat']}" \
         '--b1-file' "${dict['b1_file']}" \
