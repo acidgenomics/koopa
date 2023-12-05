ce34de819fbc62f1e3cb4bb0b20130abcaac5322
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-05 16:59:30 -0500

    Work on sunsetting r-koopa

--- lang/bash/functions/common/c/check-system.sh
+++ lang/bash/functions/common/c/check-system.sh
@@ -5,12 +5,13 @@
 koopa_check_system() {
     # """
     # Check system.
-    # @note Updated 2023-10-03.
+    # @note Updated 2023-12-05.
     # """
     koopa_assert_has_no_args "$#"
+    koopa_stop 'FIXME REWORKING THIS IN PYTHON.'
+    koopa_alert_start 'Checking system.'
     koopa_check_exports
     koopa_check_disk '/'
-    koopa_r_koopa 'cliCheckSystem'
     koopa_alert_success 'System passed all checks.'
     return 0
 }
--- lang/bash/functions/common/r/rename-camel-case.sh
+++ lang/bash/functions/common/r/rename-camel-case.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# FIXME Rework this as a Rust program.
-
 koopa_rename_camel_case() {
     # """
     # Rename files with camel case formatting.
-    # @note Updated 2023-06-05.
+    # @note Updated 2023-12-05.
     # """
     koopa_assert_has_args "$#"
-    koopa_r_koopa 'cliCamelCase' "$@"
+    koopa_stop 'FIXME REWORKING THIS IN PYTHON.'
+    return 0
 }
