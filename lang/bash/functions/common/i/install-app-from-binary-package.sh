beace545e43198260b1fae47e47d38f505518ad9
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-07 10:06:35 -0500

    Avoid cd step

--- lang/bash/functions/common/i/install-app-from-binary-package.sh
+++ lang/bash/functions/common/i/install-app-from-binary-package.sh
@@ -11,6 +11,7 @@ koopa_install_app_from_binary_package() {
     # >     '/opt/koopa/app/bash/5.1.16'
     # """
     local -A app dict
+    local prefix
     koopa_assert_has_args "$#"
     app['aws']="$(koopa_locate_aws --allow-system)"
     app['tar']="$(koopa_locate_tar --allow-system)"
@@ -29,34 +30,30 @@ install located at '${dict['koopa_prefix']}'. Koopa must be installed at \
 default '${dict['binary_prefix']}' location."
     fi
     koopa_assert_is_dir "$@"
-    (
-        local prefix
-        koopa_cd "${dict['tmp_dir']}"
-        for prefix in "$@"
-        do
-            local -A dict2
-            dict2['prefix']="$(koopa_realpath "$prefix")"
-            dict2['name']="$( \
-                koopa_print "${dict2['prefix']}" \
-                    | koopa_dirname \
-                    | koopa_basename \
-            )"
-            dict2['version']="$(koopa_basename "$prefix")"
-            dict2['tar_file']="${dict['tmp_dir']}/\
-${dict2['name']}-${dict2['version']}.tar.gz"
-            dict2['tar_url']="${dict['s3_bucket']}/${dict['os_string']}/\
+    for prefix in "$@"
+    do
+        local -A dict2
+        dict2['prefix']="$(koopa_realpath "$prefix")"
+        dict2['name']="$( \
+            koopa_print "${dict2['prefix']}" \
+                | koopa_dirname \
+                | koopa_basename \
+        )"
+        dict2['version']="$(koopa_basename "$prefix")"
+        dict2['tar_file']="${dict['tmp_dir']}/${dict2['name']}-\
+${dict2['version']}.tar.gz"
+        dict2['tar_url']="${dict['s3_bucket']}/${dict['os_string']}/\
 ${dict['arch']}/${dict2['name']}/${dict2['version']}.tar.gz"
-            # Can quiet down with '--only-show-errors' here.
-            "${app['aws']}" s3 cp \
-                --profile "${dict['aws_profile']}" \
-                "${dict2['tar_url']}" \
-                "${dict2['tar_file']}"
-            koopa_assert_is_file "${dict2['tar_file']}"
-            # Can increase verbosity with '-v' here.
-            "${app['tar']}" -Pxz -f "${dict2['tar_file']}"
-            koopa_touch "${prefix}/.koopa-binary"
-        done
-    )
+        # Can quiet down with '--only-show-errors' here.
+        "${app['aws']}" s3 cp \
+            --profile "${dict['aws_profile']}" \
+            "${dict2['tar_url']}" \
+            "${dict2['tar_file']}"
+        koopa_assert_is_file "${dict2['tar_file']}"
+        # Can increase verbosity with '-v' here.
+        "${app['tar']}" -Pxz -f "${dict2['tar_file']}"
+        koopa_touch "${prefix}/.koopa-binary"
+    done
     koopa_rm "${dict['tmp_dir']}"
     return 0
 }
