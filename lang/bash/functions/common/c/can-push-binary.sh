6581fde8ee38c5fe22246d665407e2784d835f6b
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-07 09:23:26 -0500

    Make binary install default when supported

--- lang/bash/functions/common/c/can-push-binary.sh
+++ lang/bash/functions/common/c/can-push-binary.sh
@@ -3,10 +3,9 @@
 koopa_can_push_binary() {
     # """
     # Can the current user push a koopa binary?
-    # @note Updated 2023-05-08.
+    # @note Updated 2023-12-07.
     # """
+    [[ "${KOOPA_BUILDER:-0}" -eq 1 ]] || return 1
     [[ -n "${AWS_CLOUDFRONT_DISTRIBUTION_ID:-}" ]] || return 1
-    koopa_can_install_binary || return 1
     return 0
 }
-
