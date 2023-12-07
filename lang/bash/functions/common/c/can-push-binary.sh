fdda7a58434d02670601ab8e2ff23a328c6fc6df
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-07 10:35:13 -0500

    Simplify binary app handling

--- lang/bash/functions/common/c/can-push-binary.sh
+++ lang/bash/functions/common/c/can-push-binary.sh
@@ -5,7 +5,11 @@ koopa_can_push_binary() {
     # Can the current user push a koopa binary?
     # @note Updated 2023-12-07.
     # """
+    local -A app
+    koopa_has_private_access || return 1
     [[ "${KOOPA_BUILDER:-0}" -eq 1 ]] || return 1
     [[ -n "${AWS_CLOUDFRONT_DISTRIBUTION_ID:-}" ]] || return 1
+    app['aws']="$(koopa_locate_aws --allow-missing)"
+    [[ -x "${app['aws']}" ]] || return 1
     return 0
 }
