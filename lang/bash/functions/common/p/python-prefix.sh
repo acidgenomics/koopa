cc68c383d42d222ca1cc82856ca6ff270a6d12a7
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-11 16:31:39 -0500

    Add koopa_python_script function

---
+++ lang/bash/functions/common/p/python-prefix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+koopa_python_prefix() {
+    # """
+    # Python script prefix.
+    # @note Updated 2023-12-11.
+    # """
+    koopa_print "$(koopa_koopa_prefix)/lang/python"
+    return 0
+}
