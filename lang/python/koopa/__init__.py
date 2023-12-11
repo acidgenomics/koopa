793c0806a4e73d14b39f32430e9e987d13ca0f8a
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-11 17:11:48 -0500

    Work on creating shared module

--- lang/python/koopa/__init__.py
+++ lang/python/koopa/__init__.py
@@ -1,4 +1,21 @@
+"""
+koopa module.
+Updated 2023-12-11.
+"""
+
 from os import walk
+from platform import machine
+
+
+def arch() -> str:
+    """
+    Architecture string.
+    Updated 2023-10-16.
+    """
+    string = machine()
+    if string == "x86_64":
+        string = "amd64"
+    return string
 
 
 def list_subdirs(path: str) -> list:
