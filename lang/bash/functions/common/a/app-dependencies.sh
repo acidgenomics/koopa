a560008faebe6aa85bbf44bc1b855bba7ba2aae8
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-11 16:26:18 -0500

    Rework using koopa_python_script function

--- lang/bash/functions/common/a/app-dependencies.sh
+++ lang/bash/functions/common/a/app-dependencies.sh
@@ -3,17 +3,12 @@
 koopa_app_dependencies() {
     # """
     # Get the dependencies of an app.
-    # @note Updated 2023-09-14.
+    # @note Updated 2023-12-11.
     #
     # @examples
     # koopa_app_dependencies 'python3.12'
     # """
-    local app_name cmd
     koopa_assert_has_args_eq "$#" 1
-    koopa_assert_is_installed 'python3'
-    app_name="${1:?}"
-    cmd="$(koopa_koopa_prefix)/lang/python/app-dependencies.py"
-    koopa_assert_is_executable "$cmd"
-    "$cmd" "$app_name"
+    koopa_python_script 'app-dependencies.py' "$@"
     return 0
 }
