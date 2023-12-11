9fff4a863ca0d669a1956329f50f25c4d35bda62
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-11 16:15:39 -0500

    Add docker-build-all-tags

--- lang/python/docker-build-all-tags.py
+++ lang/python/docker-build-all-tags.py
@@ -3,10 +3,16 @@
 """
 Build all Docker tags.
 Updated 2023-12-11.
+
+Example:
+./docker-build-all-tags.py \
+    --local="${HOME}/monorepo/docker/acidgenomics/koopa" \
+    --remote='public.ecr.aws/acidgenomics/koopa'
 """
 
 from argparse import ArgumentParser
-from os.path import abspath, expanduser, isdir
+from os import walk
+from os.path import abspath, expanduser, join, isdir
 from subprocess import run
 from sys import version_info
 
@@ -16,212 +22,15 @@ parser.add_argument("--remote", required=True)
 args = parser.parse_args()
 
 
-"""
-FIXME Need to port this code from R.
-
-#' Is the Docker build recent?
-#'
-#' @export
-#' @note Updated 2023-03-16.
-#'
-#' @param image `character`.
-#' Docker image name (e.g. `repo/image`).
-#' Parameterized, supporting multiple image checks in a single call.
-#'
-#' @param days `integer(1)`.
-#' Maximum number of days to consider build recent.
-#'
-#' @return `logical`.
-#'
-#' @examples
-#' ## > image <- "public.ecr.aws/acidgenomics/koopa:debian"
-#' ## > isDockerBuildRecent(image)
-isDockerBuildRecent <- function(image, days = 2L) {
-    assert(
-        isDockerEnabled(),
-        isCharacter(image),
-        isNumber(days)
-    )
-    bapply(
-        X = image,
-        FUN = function(image) {
-            ok <- tryCatch(
-                expr = {
-                    shell(
-                        command = "docker",
-                        args = c("pull", image),
-                        print = FALSE
-                    )
-                    TRUE
-                },
-                error = function(e) {
-                    FALSE
-                }
-            )
-            if (isFALSE(ok)) {
-                return(FALSE)
-            }
-            x <- tryCatch(
-                expr = {
-                    shell(
-                        command = "docker",
-                        args = c(
-                            "inspect",
-                            "--format='{{json .Created}}'",
-                            image
-                        ),
-                        print = FALSE
-                    )
-                },
-                error = function(e) {
-                    FALSE
-                }
-            )
-            if (isFALSE(x)) {
-                return(FALSE)
-            }
-            assert(
-                is.list(x),
-                isSubset("stdout", names(x))
-            )
-            x <- x[["stdout"]]
-            x <- sub(pattern = "\n$", replacement = "", x = x)
-            ## e.g. "'\"2021-07-12T16:19:01.734591265Z\"'".
-            x <- sub(pattern = "^'\"(.+)\"'$", replacement = "\\1", x = x)
-            ## e.g. "2021-07-12T16:19:01.734591265Z".
-            x <- sub(pattern = "\\.[0-9]+Z$", replacement = "", x = x)
-            ## e.g. "2021-07-12T16:19:01".
-            diffDays <- difftime(
-                time1 = Sys.time(),
-                time2 = as.POSIXct(x, format = "%Y-%m-%dT%H:%M:%S"),
-                units = "days"
-            )
-            diffDays < days
-        },
-        USE.NAMES = TRUE
-    )
-}
-
-#' Build all Docker tags
-#'
-#' @export
-#' @note Updated 2023-03-28.
-#'
-#' @param local `character(1)`.
-#' Docker image repository directory.
-#'
-#' @param remote `character(1)`.
-#' Remote Docker image repository URL (e.g. ECR or DockerHub).
-#'
-#' @param days `numeric(1)`.
-#' Number of days to allow since last build.
-#'
-#' @param force `logical(1)`.
-#' Force rebuild.
-#'
-#' @return Invisible `logical(1)`.
-#'
-#' @examples
-#' ## > local <- file.path(
-#' ## >     "~",
-#' ## >     "monorepo",
-#' ## >     "docker",
-#' ## >     "acidgenomics",
-#' ## >     "koopa"
-#' ## > )
-#' ## > remote <- "public.ecr.aws/acidgenomics/koopa"
-#' ## > dockerBuildAllTags(local = local, remote = remote)
-dockerBuildAllTags <-
-    function(local,
-             remote,
-             days = 2L,
-             force = FALSE) {
-        assert(
-            isADir(local),
-            isString(remote),
-            isNumber(days),
-            isFlag(force)
-        )
-        alert(sprintf("Building all tags: {.var %s}.", remote))
-        ## Build tags in desired order, using "build.txt" file.
-        buildFile <- file.path(local, "build.txt")
-        if (file.exists(buildFile)) {
-            tags <- readLines(buildFile)
-            ## Ensure we ignore comments here.
-            keep <- grepl(pattern = "^[a-z0-9_.-]+$", x = tags)
-            tags <- tags[keep]
-            assert(isCharacter(tags))
-        } else {
-            ## Or build alphabetically (default).
-            tags <- sort(list.dirs(
-                path = local,
-                full.names = FALSE,
-                recursive = FALSE
-            ))
-        }
-        if (length(tags) > 1L) {
-            ## Build "latest" tag automatically at the end.
-            tags <- setdiff(tags, "latest")
-        }
-        assert(hasLength(tags))
-        ## Build the versioned images, defined by `Dockerfile` in the
-        ## subdirectories.
-        invisible(Map(
-            tag = tags,
-            MoreArgs = list(
-                "local" = local,
-                "remote" = remote
-            ),
-            f = function(tag, local, remote) {
-                local <- file.path(local, tag)
-                remote <- paste0(remote, ":", tag)
-                alert(sprintf(
-                    fmt = paste(
-                        "Building {.var %s} at {.path %s}."
-                    ),
-                    remote, local
-                ))
-                if (isFALSE(force)) {
-                    if (isTRUE(
-                        tryCatch(
-                            expr = {
-                                isDockerBuildRecent(
-                                    image = remote,
-                                    days = days
-                                )
-                            },
-                            error = function(e) {
-                                FALSE
-                            }
-                        )
-                    )) {
-                        alertInfo(sprintf(
-                            "{.var %s} was built recently.",
-                            remote
-                        ))
-                        return(TRUE)
-                    }
-                }
-                shell(
-                    command = koopa(),
-                    args = c(
-                        "app", "docker", "build",
-                        "--local", local,
-                        "--remote", remote
-                    ),
-                    print = TRUE
-                )
-            }
-        ))
-        invisible(TRUE)
-    }
-"""
-
-
 def build_tag(local: str, remote: str) -> bool:
     """
     Build a Docker tag.
     Updated 2023-12-11.
+
+    Examples:
+    local = "~/monorepo/docker/acidgenomics/koopa/ubuntu"
+    remote = "public.ecr.aws/acidgenomics/koopa:ubuntu"
+    build_tag(local=local, remote=remote)
     """
     run(
         args=[
@@ -234,14 +43,30 @@ def build_tag(local: str, remote: str) -> bool:
             "--remote",
             remote,
         ],
+        check=True,
     )
     return True
+
+
+def list_subdirs(path: str) -> list:
+    """
+    List subdirectories in a directory.
+    Updated 2023-12-11.
+
+    See also:
+    - https://stackoverflow.com/questions/141291/
+    """
+    out = next(walk(path))[1]
+    return out
 
 
 def main(local: str, remote: str) -> bool:
     """
     Build all Docker images.
     Updated 2023-12-11.
+
+    See also:
+    - https://stackoverflow.com/questions/141291/
 
     Example:
     local = "~/monorepo/docker/acidgenomics/koopa"
@@ -251,8 +76,12 @@ def main(local: str, remote: str) -> bool:
     local = expanduser(local)
     local = abspath(local)
     assert isdir(local)
-    # FIXME Need to find all directories, and then run this in a loop.
-    build_tag(local=local, remote=remote)
+    subdirs = list_subdirs(local)
+    for subdir in subdirs:
+        local2 = join(local, subdir)
+        assert isdir(local2)
+        remote2 = remote + ":" + subdir
+        build_tag(local=local2, remote=remote2)
     return True
 
 
