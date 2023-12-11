d72bf4c41ab38b9c3cb032751199df12a3c1eb02
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-11 17:24:40 -0500

    Rework with koopa module approach

--- lang/python/app-dependencies.py
+++ lang/python/app-dependencies.py
@@ -11,25 +11,17 @@ Examples:
 from argparse import ArgumentParser
 from json import load
 from os.path import abspath, dirname, join
-from platform import machine, system
-from sys import version_info
+from sys import path, version_info
+
+path.insert(0, "koopa")
+
+from koopa import os_id  # noqa
 
 parser = ArgumentParser()
 parser.add_argument("app_name")
 args = parser.parse_args()
 
 _json_file = abspath(join(dirname(__file__), "../../etc/koopa/app.json"))
-
-
-def arch() -> str:
-    """
-    Architecture string.
-    Updated 2023-10-16.
-    """
-    string = machine()
-    if string == "x86_64":
-        string = "amd64"
-    return string
 
 
 def flatten(items: list, seqtypes=(list, tuple)) -> list:
@@ -81,27 +73,6 @@ def get_deps(app_name: str, json_data: dict) -> list:
     all_deps = build_deps + deps
     all_deps = list(dict.fromkeys(all_deps))
     return all_deps
-
-
-def os_id() -> str:
-    """
-    Platform and architecture-specific identifier.
-    Updated 2023-10-16.
-    """
-    string = platform() + "-" + arch()
-    return string
-
-
-def platform() -> str:
-    """
-    Platform string.
-    Updated 2023-03-27.
-    """
-    string = system()
-    string = string.lower()
-    if string == "darwin":
-        string = "macos"
-    return string
 
 
 def print_apps(app_names: list, json_data: dict) -> bool:
--- lang/python/koopa/koopa.py
+++ lang/python/koopa/koopa.py
@@ -4,7 +4,7 @@ Updated 2023-12-11.
 """
 
 from os import walk
-from platform import machine
+from platform import machine, system
 
 
 def arch() -> str:
@@ -18,6 +18,24 @@ def arch() -> str:
     return string
 
 
+def flatten(items: list, seqtypes=(list, tuple)) -> list:
+    """
+    Flatten an arbitrarily nested list.
+    Updated 2023-03-25.
+
+    See also:
+    - https://stackoverflow.com/a/10824086
+    """
+    try:
+        for i, x in enumerate(items):
+            while isinstance(x, seqtypes):
+                items[i : i + 1] = x  # noqa: E203
+                x = items[i]
+    except IndexError:
+        pass
+    return items
+
+
 def list_subdirs(path: str) -> list:
     """
     List subdirectories in a directory.
@@ -29,3 +47,24 @@ def list_subdirs(path: str) -> list:
     lst = next(walk(path))[1]
     lst = lst.sort()
     return lst
+
+
+def os_id() -> str:
+    """
+    Platform and architecture-specific identifier.
+    Updated 2023-10-16.
+    """
+    string = platform() + "-" + arch()
+    return string
+
+
+def platform() -> str:
+    """
+    Platform string.
+    Updated 2023-03-27.
+    """
+    string = system()
+    string = string.lower()
+    if string == "darwin":
+        string = "macos"
+    return string
