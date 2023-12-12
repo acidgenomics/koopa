e2606e7c35448c4d93f7b082019a9f4c97353c38
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-12 06:51:58 -0500

    Add links

--- lang/bash/include/install/common/shared/gseapy.sh
+++ lang/bash/include/install/common/shared/gseapy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 main() {
+    # """
+    # Install GSEAPY.
+    # @note Updated 2023-12-12.
+    #
+    # @seealso
+    # - https://github.com/zqfang/gseapy/
+    # - https://gseapy.readthedocs.io/
+    # - https://bioconda.github.io/recipes/gseapy/README.html
+    # """
     koopa_activate_app --build-only 'rust'
     koopa_install_python_package
     return 0
