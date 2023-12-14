fdabb30d963f65f88ac117278baa4e1d13b8354b
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-14 13:20:20 -0500

    Fix Linux activation error

--- lang/sh/functions/i/is-interactive.sh
+++ lang/sh/functions/i/is-interactive.sh
@@ -4,7 +4,6 @@ _koopa_is_interactive() {
     # """
     # Is the current shell interactive?
     # @note Updated 2023-12-14.
-    # Consider checking for tmux or subshell here.
     # """
     if [ "${KOOPA_INTERACTIVE:-0}" -eq 1 ]
     then
@@ -22,9 +21,9 @@ _koopa_is_interactive() {
     then
         return 0
     fi
-    if [ -n "${SSH_CONNECTION:-}" ] && [ -n "${TMUX:-}" ]
-    then
-        return 0
-    fi
+    # > if [ -n "${SSH_CONNECTION:-}" ] && [ -n "${TMUX:-}" ]
+    # > then
+    # >     return 0
+    # > fi
     return 1
 }
