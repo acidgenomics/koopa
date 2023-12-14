8b7391dcbe425238bbbacf93e29100605a1dff9b
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-14 13:11:01 -0500

    Check for tmux over ssh

--- lang/sh/functions.sh
+++ lang/sh/functions.sh
@@ -1794,10 +1794,26 @@ _koopa_is_installed() {
 }
 
 _koopa_is_interactive() {
-    [ "${KOOPA_INTERACTIVE:-0}" -eq 1 ] && return 0
-    [ "${KOOPA_FORCE:-0}" -eq 1 ] && return 0
-    _koopa_str_detect_posix "$-" 'i' && return 0
-    _koopa_is_tty && return 0
+    if [ "${KOOPA_INTERACTIVE:-0}" -eq 1 ]
+    then
+        return 0
+    fi
+    if [ "${KOOPA_FORCE:-0}" -eq 1 ]
+    then
+        return 0
+    fi
+    if _koopa_str_detect_posix "$-" 'i'
+    then
+        return 0
+    fi
+    if _koopa_is_tty
+    then
+        return 0
+    fi
+    if [ -n "${SSH_CONNECTION:-}" ] && [ -n "${TMUX:-}" ]
+    then
+        return 0
+    fi
     return 1
 }
 
