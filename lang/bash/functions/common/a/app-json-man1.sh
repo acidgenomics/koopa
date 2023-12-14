2a840310ce75461013f3ffcded863ee58403e5c1
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-14 12:56:30 -0500

    Improve our Bash-to-Python handoff

--- lang/bash/functions/common/a/app-json-man1.sh
+++ lang/bash/functions/common/a/app-json-man1.sh
@@ -3,17 +3,17 @@
 koopa_app_json_man1() {
     # """
     # Get 'man1' links from 'app.json' file.
-    # @note Updated 2023-03-30.
+    # @note Updated 2023-12-14.
     #
     # @examples
     # > koopa_app_json_man1 'coreutils' 'binutils'
     # """
-    local app_name
+    local name
     koopa_assert_has_args "$#"
-    for app_name in "$@"
+    for name in "$@"
     do
         koopa_app_json \
-            --app-name="$app_name" \
+            --name="$name" \
             --key='man1'
     done
 }
