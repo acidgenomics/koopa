33a3d6f9e84107becb42ff5153b85a140ba34f29
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-14 10:19:47 -0500

    Improve our JSON processing

--- lang/python/conda-bin-names.py
+++ lang/python/conda-bin-names.py
@@ -6,40 +6,27 @@ Updated 2023-12-14.
 
 Examples:
 ./conda-bin-names.py \
-    /opt/koopa/opt/salmon/libexec/conda-meta/salmon-*.json
+    /opt/koopa/opt/star/libexec/conda-meta/star-*.json
 """
 
 from argparse import ArgumentParser
-from json import load
-from re import compile as re_compile
-from sys import version_info
+from os.path import dirname, join
+from sys import path, version_info
 
 parser = ArgumentParser()
 parser.add_argument("json_file")
 args = parser.parse_args()
 
+path.insert(0, join(dirname(__file__), "koopa"))
 
-def main(json_file: str) -> bool:
+from koopa import print_conda_bin_names
+
+def main(json_file: str) -> None:
     """
-    Parse conda JSON metadata for files to link in 'bin'.
-    Updated 2023-05-11.
+    Main function.
+    Updated 2023-12-14.
     """
-    with open(json_file, encoding="utf-8") as con:
-        json_data = load(con)
-    keys = json_data.keys()
-    if "files" not in keys:
-        raise ValueError("Invalid JSON: '" + json_file + "'.")
-    file_list = json_data["files"]
-    bin_files = []
-    pattern = re_compile(r"^bin/([^/]+)$")
-    for file in file_list:
-        match = pattern.match(file)
-        if match:
-            bin_file = match.group(1)
-            bin_files.append(bin_file)
-    if any(bin_files):
-        for bin_file in bin_files:
-            print(bin_file)
+    print_conda_bin_names(json_file=json_file)
     return True
 
 
