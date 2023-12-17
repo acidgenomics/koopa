513380400139ddbbc92e6007f01d29cff2560112
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-17 14:19:20 -0500

    Add libheif support

---
+++ lang/bash/include/install/common/shared/libheif.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# FIXME Add support for libjpeg-turbo and libpng.
+
+main() {
+    # """
+    # Install libheif.
+    # @note Updated 2023-12-17.
+    #
+    # @seealso
+    # - https://formulae.brew.sh/formula/libheif
+    # """
+    local -A dict
+    koopa_activate_app --build-only 'pkg-config'
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['url']="https://github.com/strukturag/libheif/releases/download/\
+v${dict['version']}/libheif-${dict['version']}.tar.gz"
+    koopa_download "${dict['url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
+    koopa_cd 'src'
+    koopa_cmake_build --prefix="${dict['prefix']}"
+    return 0
+}
