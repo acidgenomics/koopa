a34c03a45ef0efa0bd264a286628c641bc3a5730
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-22 09:44:28 -0500

    Simplify using koopa_install_go_package

--- lang/bash/include/install/common/shared/walk.sh
+++ lang/bash/include/install/common/shared/walk.sh
@@ -3,15 +3,15 @@
 main() {
     # """
     # Install walk.
-    # @note Updated 2023-08-30.
+    # @note Updated 2023-12-22.
     #
     # @seealso
     # - https://github.com/antonmedv/walk/
+    # - https://formulae.brew.sh/formula/walk
     # """
     local -A dict
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    dict['url']="https://github.com/antonmedv/walk/archive/refs/tags/\
-v${dict['version']}.tar.gz"
+    dict['url']="github.com/antonmedv/walk@v${dict['version']}"
     koopa_install_go_package --url="${dict['url']}"
     return 0
 }
