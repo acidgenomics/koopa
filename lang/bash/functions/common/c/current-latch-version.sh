3a36b23bb8050497f1d61d3a89a9b144ebc9bc54
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-22 10:18:09 -0500

    Add support for checking any package at PyPi

--- lang/bash/functions/common/c/current-latch-version.sh
+++ lang/bash/functions/common/c/current-latch-version.sh
@@ -8,20 +8,6 @@ koopa_current_latch_version() {
     # Our awk command removes empty lines with NF, trims whitespace, and then
     # prints the second column in the string, which contains the version.
     # """
-    local -A app
-    local string
-    koopa_assert_has_no_args "$#"
-    app['awk']="$(koopa_locate_awk)"
-    app['curl']="$(koopa_locate_curl)"
-    app['pup']="$(koopa_locate_pup)"
-    koopa_assert_is_executable "${app[@]}"
-    # shellcheck disable=SC2016
-    string="$( \
-        "${app['curl']}" -s 'https://pypi.org/project/latch/' \
-            | "${app['pup']}" 'h1 text{}' \
-            | "${app['awk']}" 'NF {$1=$1; print $2}'
-    )"
-    [[ -n "$string" ]] || return 1
-    koopa_print "$string"
+    koopa_current_pypi_package_version 'latch'
     return 0
 }
