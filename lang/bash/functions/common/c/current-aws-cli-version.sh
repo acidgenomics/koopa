ced63cc1327afa330e7ae6479df34ecbf785b3aa
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-22 10:39:42 -0500

    Simplify our aws-cli livecheck

--- lang/bash/functions/common/c/current-aws-cli-version.sh
+++ lang/bash/functions/common/c/current-aws-cli-version.sh
@@ -4,23 +4,7 @@ koopa_current_aws_cli_version() {
     # """
     # Get the current AWS CLI version.
     # @note Updated 2023-12-22.
-    #
-    # May need to authenticate first with 'gh auth login'.
     # """
-    local -A app
-    local string
-    koopa_assert_has_no_args "$#"
-    app['gh']="$(koopa_locate_gh)"
-    app['head']="$(koopa_locate_head)"
-    app['jq']="$(koopa_locate_jq)"
-    app['sort']="$(koopa_locate_sort)"
-    string="$( \
-        "${app['gh']}" api 'https://api.github.com/repos/aws/aws-cli/tags' \
-            | "${app['jq']}" --raw-output '.[].name' \
-            | "${app['sort']}" -nr \
-            | "${app['head']}" -n 1 \
-    )"
-    [[ -n "$string" ]] || return 1
-    koopa_print "$string"
+    koopa_current_github_tag_version 'aws/aws-cli'
     return 0
 }
