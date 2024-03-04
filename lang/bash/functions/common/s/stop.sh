f4d6d831d989e46a870b8a6dfae366960e096762
Michael Steinbaugh <mike@steinbaugh.com>
2024-03-04 16:24:27 -0500

    Rework our stack trace

--- lang/bash/functions/common/s/stop.sh
+++ lang/bash/functions/common/s/stop.sh
@@ -9,11 +9,17 @@ koopa_stop() {
     # - https://raw.githubusercontent.com/TritonDataCenter/sdc-headnode/
     #     master/buildtools/lib/error_handler.sh
     # """
+    local -A bool
+    bool['verbose']="${KOOPA_VERBOSE:-0}"
     koopa_msg 'red-bold' 'red' 'Error:' "$@" >&2
-    set +o errexit
-    set +o errtrace
-    set +o xtrace
-    trap '' ERR
-    koopa_stack_trace
+    # Include stack trace in verbose mode.
+    if [[ "${bool['verbose']}" -eq 1 ]]
+    then
+        set +o errexit
+        set +o errtrace
+        set +o xtrace
+        trap '' ERR
+        koopa_stack_trace
+    fi
     exit 1
 }
