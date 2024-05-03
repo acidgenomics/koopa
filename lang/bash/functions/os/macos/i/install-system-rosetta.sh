f8b9e149490a2dbf85b593b12e0c7ce2094b16bb
Michael Steinbaugh <mike@steinbaugh.com>
2024-05-03 10:35:21 -0400

    Add agree-to-license flag

--- lang/bash/functions/os/macos/i/install-system-rosetta.sh
+++ lang/bash/functions/os/macos/i/install-system-rosetta.sh
@@ -3,13 +3,18 @@
 koopa_macos_install_system_rosetta() {
     # """
     # Install Rosetta 2.
-    # @note Updated 2023-04-05.
+    # @note Updated 2024-05-03.
+    #
+    # @seealso
+    # - https://github.com/rstudio/rstudio/issues/12791
     # """
     local -A app
     koopa_assert_has_no_args "$#"
     koopa_assert_is_admin
     app['softwareupdate']="$(koopa_macos_locate_softwareupdate)"
     koopa_assert_is_executable "${app[@]}"
-    koopa_sudo "${app['softwareupdate']}" --install-rosetta
+    koopa_sudo "${app['softwareupdate']}" \
+        --install-rosetta \
+        --agree-to-license
     return 0
 }
