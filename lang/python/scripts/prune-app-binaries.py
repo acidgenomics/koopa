cc3a0868a28ab9a459b26a8be5152c84744f0676
Michael Steinbaugh <mike@steinbaugh.com>
2024-05-16 10:41:17 -0400

    Add back support for pruning app binaries

---
+++ lang/python/scripts/prune-app-binaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Prune app binaries
+Updated 2024-05-16.
+
+Examples:
+./prune-app-binaries.py
+"""
+
+from argparse import ArgumentParser, BooleanOptionalAction
+from os.path import dirname, join
+from sys import path, version_info
+
+path.insert(0, join(dirname(__file__), ".."))
+
+from koopa.app import prune_app_binaries
+
+parser = ArgumentParser()
+parser.add_argument("--dry-run", action=BooleanOptionalAction)
+args = parser.parse_args()
+
+
+def main(dry_run=False) -> None:
+    """
+    Main function.
+    Updated 2024-05-16.
+    """
+    prune_app_binaries(dry_run=dry_run)
+    return None
+
+
+if __name__ == "__main__":
+    if not version_info >= (3, 8):
+        raise RuntimeError("Unsupported Python version.")
+    main(dry_run=args.dry_run)
