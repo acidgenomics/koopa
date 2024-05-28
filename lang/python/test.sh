ed9f1e06a52a65d6e2897a039a79b27ba70a1bd9
Michael Steinbaugh <mike@steinbaugh.com>
2024-05-28 15:58:40 -0400

    Rework coverage

--- lang/python/test.sh
+++ lang/python/test.sh
@@ -7,15 +7,15 @@ run_pytest_cov() {
     local -A app dict
     app['pytest']="$(koopa_locate_pytest)"
     koopa_assert_is_executable "${app[@]}"
-    dict['python_prefix']="$(koopa_python_prefix)"
-    koopa_assert_is_dir "${dict['python_prefix']}"
-    export PYTHONPATH="${dict['python_prefix']}"
+    dict['pythonpath']="$(koopa_script_parent_dir)"
+    koopa_assert_is_dir "${dict['pythonpath']}"
+    export PYTHONPATH="${dict['pythonpath']}"
     "${app['pytest']}" \
         --cov='tests' \
         --cov-report='term' \
         --cov-report='html' \
         --cov-fail-under=80 \
-        "${dict['python_prefix']}"
+        "${dict['pythonpath']}"
     return 0
 }
 
