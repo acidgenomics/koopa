12f3940141534846663a501da0546cff9c07b695
Michael Steinbaugh <mike@steinbaugh.com>
2024-05-28 16:01:09 -0400

    Rework to allow user to specify which python

--- lang/bash/functions/common/p/python-script.sh
+++ lang/bash/functions/common/p/python-script.sh
@@ -3,18 +3,44 @@
 koopa_python_script() {
     # """
     # Run a Python script.
-    # @note Updated 2024-04-19.
+    # @note Updated 2024-05-28.
     # """
     local -A app dict
+    local -a pos
     koopa_assert_has_args "$#"
-    app['python']="$(koopa_locate_python3 --allow-system)"
+    app['python']=''
+    while (("$#"))
+    do
+        case "$1" in
+            # Key-value pairs --------------------------------------------------
+            '--python='*)
+                app['python']="${1#*=}"
+                shift 1
+                ;;
+            '--python')
+                app['python']="${2:?}"
+                shift 2
+                ;;
+            # Other ------------------------------------------------------------
+            *)
+                pos+=("${1:?}")
+                shift 1
+                ;;
+        esac
+    done
+    [[ "${#pos[@]}" -gt 0 ]] && set -- "${pos[@]}"
+    koopa_assert_has_args "$#"
+    if [[ -z "${app['python']}" ]]
+    then
+        app['python']="$(koopa_locate_python3 --allow-system)"
+    fi
     koopa_assert_is_installed "${app[@]}"
     dict['prefix']="$(koopa_python_prefix)/scripts"
     koopa_assert_is_dir "${dict['prefix']}"
     dict['cmd_name']="${1:?}"
     shift 1
-    app['script']="${dict['prefix']}/${dict['cmd_name']}"
-    koopa_assert_is_executable "${app['script']}"
-    "${app['python']}" "${app['script']}" "$@"
+    dict['script']="${dict['prefix']}/${dict['cmd_name']}"
+    koopa_assert_is_executable "${dict['script']}"
+    "${app['python']}" "${dict['script']}" "$@"
     return 0
 }
