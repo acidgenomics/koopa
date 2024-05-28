a1b591f7800b9e49be93779ca0ecc8592e5a2fac
Michael Steinbaugh <mike@steinbaugh.com>
2024-05-28 16:21:53 -0400

    Improve handling consistency of R and Python scripts

---
+++ lang/bash/functions/common/r/r-scripts-prefix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+koopa_r_scripts_prefix() {
+    # """
+    # R scripts prefix.
+    # @note Updated 2023-12-11.
+    # """
+    koopa_print "$(koopa_koopa_prefix)/lang/r/scripts"
+    return 0
+}
