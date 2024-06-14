d1317cc72227b95662b28042e0b0f35966ec2ba8
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-14 13:18:09 -0400

    Rework ncbi-sra-tools install script

--- lang/bash/include/install/common/shared/ncbi-sra-tools.sh
+++ lang/bash/include/install/common/shared/ncbi-sra-tools.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+
+# Minimal build reprex:
+# https://github.com/ncbi/sra-tools/issues/937
+# > cd ${TMPDIR}
+# > git clone https://github.com/ncbi/ncbi-vdb.git
+# > git clone https://github.com/ncbi/sra-tools.git
+# > mkdir build
+# > cd build
+# > cmake -S "$(cd ../ncbi-vdb; pwd)" -B ncbi-vdb
+# > cmake --build ncbi-vdb
+ #> cmake -D VDB_LIBDIR="${PWD}/ncbi-vdb/lib" -D CMAKE_INSTALL_PREFIX="${PWD}/sratoolkit" -S "$(cd ../sra-tools; pwd)" -B sra-tools
+# > cmake --build sra-tools --target install
+# > ./sratoolkit/bin/prefetch --version
+
 main() {
     # """
     # Install SRA toolkit.
-    # @note Updated 2024-05-25.
+    # @note Updated 2024-06-14.
+    #
+    # VDB is the database engine that all SRA tools use.
     #
     # Currently, we need to build sra-tools relative to a hard-coded path
     # ('../ncbi-vdb') to ncbi-vdb source code, to ensure that zlib and bzip2
@@ -21,11 +37,14 @@ main() {
     #     recipes/sra-tools
     # - https://github.com/Homebrew/homebrew-core/blob/master/
     #     Formula/sratoolkit.rb
+    # - https://github.com/ncbi/ncbi-vdb/wiki/
+    # - https://github.com/bioconda/bioconda-recipes/tree/master/
+    #     recipes/ncbi-vdb
     # """
     local -A app cmake dict
     local -a build_deps cmake_args deps
-    build_deps+=('bison' 'flex' 'ncbi-vdb' 'python3.12')
-    deps+=('icu4c' 'libxml2')
+    build_deps+=('bison' 'flex' 'python3.12')
+    deps+=('zlib' 'icu4c' 'libxml2' 'hdf5')
     koopa_activate_app --build-only "${build_deps[@]}"
     koopa_activate_app "${deps[@]}"
     app['python']="$(koopa_locate_python312 --realpath)"
@@ -33,27 +52,68 @@ main() {
     dict['libxml2']="$(koopa_app_prefix 'libxml2')"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['shared_ext']="$(koopa_shared_ext)"
-    dict['vdb']="$(koopa_app_prefix 'ncbi-vdb')"
+    dict['temurin']="$(koopa_app_prefix 'temurin')"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     cmake['libxml2_include_dir']="${dict['libxml2']}/include"
     cmake['libxml2_libraries']="${dict['libxml2']}/lib/\
 libxml2.${dict['shared_ext']}"
     cmake['python3_executable']="${app['python']}"
+    koopa_assert_is_dir \
+        "${cmake['libxml2_include_dir']}" \
+        "${dict['temurin']}"
+    koopa_assert_is_file \
+        "${cmake['libxml2_libraries']}" \
+        "${cmake['python3_executable']}"
+    # Build VDB ================================================================
+    koopa_append_cflags '-DH5_USE_110_API'
+    export JAVA_HOME="${dict['temurin']}"
+    cmake_args=(
+        # > '-DLIBS_ONLY=ON'
+        "-DPython3_EXECUTABLE=${cmake['python3_executable']}"
+    )
+    dict['vdb_url']="https://github.com/ncbi/ncbi-vdb/archive/refs/tags/\
+${dict['version']}.tar.gz"
+    koopa_download "${dict['vdb_url']}"
+    koopa_extract "$(koopa_basename "${dict['vdb_url']}")" 'ncbi-vdb'
+    koopa_cd 'ncbi-vdb'
+    if koopa_is_root
+    then
+        # Disable creation of these files:
+        # - /etc/ncbi/
+        # - /etc/profile.d/ncbi-vdb.csh
+        # - /etc/profile.d/ncbi-vdb.sh
+        # shellcheck disable=SC2016
+        koopa_find_and_replace_in_file \
+            --fixed \
+            --pattern='[ "$EUID" -eq 0 ]' \
+            --replacement='[ "$EUID" -eq -1 ]' \
+            'build/install-root.sh'
+        # shellcheck disable=SC2016
+        koopa_find_and_replace_in_file \
+            --fixed \
+            --pattern='[ "$EUID" -eq 0 ]' \
+            --replacement='[ "$EUID" -eq -1 ]' \
+            'libs/kfg/install.sh'
+    fi
+    koopa_cmake_build --prefix="${dict['prefix']}" "${cmake_args[@]}"
+    (
+        koopa_cd "${dict['prefix']}"
+        koopa_ln 'lib' 'lib64'
+    )
+    koopa_cd '..'
+    # Build SRA toolkit ========================================================
+    dict['vdb']="$(koopa_realpath 'ncbi-vdb')"
     cmake['vdb_bindir']="${dict['vdb']}"
     cmake['vdb_libdir']="${dict['vdb']}/lib"
     cmake['vdb_incdir']="${dict['vdb']}/include"
+    koopa_assert_is_dir \
+        "${cmake['vdb_bindir']}" \
+        "${cmake['vdb_incdir']}" \
+        "${cmake['vdb_libdir']}"
     if koopa_is_macos
     then
         cmake['vdb_incdir']="${dict['vdb']}/src/interfaces"
     fi
-    koopa_assert_is_dir \
-        "${cmake['libxml2_include_dir']}" \
-        "${cmake['vdb_bindir']}" \
-        "${cmake['vdb_incdir']}" \
-        "${cmake['vdb_libdir']}"
-    koopa_assert_is_file \
-        "${cmake['libxml2_libraries']}" \
-        "${cmake['python3_executable']}"
     cmake_args=(
         # Build options --------------------------------------------------------
         '-DNO_JAVA=ON'
@@ -65,11 +125,11 @@ libxml2.${dict['shared_ext']}"
         "-DVDB_INCDIR=${cmake['vdb_incdir']}"
         "-DVDB_LIBDIR=${cmake['vdb_libdir']}"
     )
-    dict['url']="https://github.com/ncbi/sra-tools/archive/refs/tags/\
+    dict['sra_url']="https://github.com/ncbi/sra-tools/archive/refs/tags/\
 ${dict['version']}.tar.gz"
-    koopa_download "${dict['url']}"
-    koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
-    koopa_cd 'src'
+    koopa_download "${dict['sra_url']}"
+    koopa_extract "$(koopa_basename "${dict['url']}")" 'sra-tools'
+    koopa_cd 'sra-tools'
     if koopa_is_root
     then
         # Disable creation of these files and directories:
@@ -85,7 +145,6 @@ ${dict['version']}.tar.gz"
     fi
     # If build fails, set '--jobs=1' here for better debugging info.
     koopa_cmake_build \
-        --jobs=1 \
         --prefix="${dict['prefix']}" \
         "${cmake_args[@]}"
     return 0
