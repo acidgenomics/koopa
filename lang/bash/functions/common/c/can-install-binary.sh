4f2ea4a3cc52398e5fad1029621ebc1b642424dc
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-14 16:43:05 -0400

    Rework binary installation detection

--- lang/bash/functions/common/c/can-install-binary.sh
+++ lang/bash/functions/common/c/can-install-binary.sh
@@ -3,10 +3,11 @@
 koopa_can_install_binary() {
     # """
     # Can the current user install and/or push a koopa binary?
-    # @note Updated 2023-12-07.
+    # @note Updated 2024-06-14.
     #
     # Currently requires access to our private S3 bucket.
     # """
+    [[ "${KOOPA_BUILDER:-0}" -eq 1 ]] && return 1
     koopa_has_private_access || return 1
     koopa_can_push_binary && return 1
     return 0
