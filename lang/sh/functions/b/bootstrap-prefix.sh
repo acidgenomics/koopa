3d551ed23e7de592b75ba1c9709a3d02236d9777
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-16 10:37:37 -0400

    Switch ffmpeg to conda and ensure bootstrap gets loaded into shell

---
+++ lang/sh/functions/b/bootstrap-prefix.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+_koopa_bootstrap_prefix() {
+    # """
+    # Koopa bootstrap prefix.
+    # @note Updated 2024-06-16.
+    # """
+    _koopa_print "$(_koopa_xdg_data_home)/koopa-bootstrap"
+    return 0
+}
