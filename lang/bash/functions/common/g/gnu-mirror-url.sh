0f4d50750ae3b77be0a8102b2e23e9684e03f628
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-26 11:31:02 -0400

    Update URL

--- lang/bash/functions/common/g/gnu-mirror-url.sh
+++ lang/bash/functions/common/g/gnu-mirror-url.sh
@@ -17,7 +17,9 @@ koopa_gnu_mirror_url() {
     # """
     local server
     koopa_assert_has_no_args "$#"
-    server='https://koopa.acidgenomics.com/src/gnu'
+    # FIXME Once we have sources, change to:
+    # > server='https://koopa.acidgenomics.com/src/gnu'
+    server='https://ftpmirror.gnu.org'
     koopa_print "$server"
     return 0
 }
