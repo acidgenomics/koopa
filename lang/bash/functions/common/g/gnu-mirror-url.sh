7ab2bd16ea84d74bbb47185b3b577dfb974aa6a7
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-26 13:32:01 -0400

    Revert GNU mirror URL

--- lang/bash/functions/common/g/gnu-mirror-url.sh
+++ lang/bash/functions/common/g/gnu-mirror-url.sh
@@ -17,8 +17,7 @@ koopa_gnu_mirror_url() {
     # """
     local server
     koopa_assert_has_no_args "$#"
-    # > server='https://ftpmirror.gnu.org'
-    server='https://koopa.acidgenomics.com/src/gnu'
+    server='https://ftpmirror.gnu.org'
     koopa_print "$server"
     return 0
 }
