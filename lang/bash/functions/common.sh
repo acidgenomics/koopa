990438573d81faffd4d0029f7fa8d5bcd8f5cdca
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-26 11:13:59 -0400

    Update mirror

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -10320,7 +10320,7 @@ koopa_git_submodule_init() {
 koopa_gnu_mirror_url() {
     local server
     koopa_assert_has_no_args "$#"
-    server='http://ftp.gnu.org/gnu'
+    server='https://ftp.gnu.org/gnu'
     koopa_print "$server"
     return 0
 }
