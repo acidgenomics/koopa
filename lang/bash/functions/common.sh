6448785ff72810b2630d760bfda823b9689e18bc
Michael Steinbaugh <mike@steinbaugh.com>
2024-07-06 08:59:34 -0400

    Simplify ncbi-sra-tools support

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -14552,18 +14552,11 @@ koopa_install_nanopolish() {
 }
 
 koopa_install_ncbi_sra_tools() {
-    if koopa_is_aarch64
-    then
-        koopa_install_app \
-            --installer='ncbi-sra-tools-src' \
-            --name='ncbi-sra-tools' \
-            "$@"
-    else
-        koopa_install_app \
-            --installer='ncbi-sra-tools-conda' \
-            --name='ncbi-sra-tools' \
-            "$@"
-    fi
+    koopa_assert_is_not_aarch64
+    koopa_install_app \
+        --installer='ncbi-sra-tools-conda' \
+        --name='ncbi-sra-tools' \
+        "$@"
 }
 
 koopa_install_ncbi_vdb() {
