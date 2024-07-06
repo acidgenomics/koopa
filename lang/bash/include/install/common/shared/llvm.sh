a0d57392cdea01b965edaa9cda2faa770fc965b2
Michael Steinbaugh <mike@steinbaugh.com>
2024-07-06 07:37:10 -0400

    Attempt to fix linux build

--- lang/bash/include/install/common/shared/llvm.sh
+++ lang/bash/include/install/common/shared/llvm.sh
@@ -3,18 +3,23 @@
 # NOTE Rework this using a cmake dict.
 # NOTE Check that files and dirs exist, where relevant.
 
+# FIXME Build with ninja.
 # FIXME Need to debug build failure on Ubuntu.
+# FIXME Do we need to include swig for build?
 
 main() {
     # """
     # Install LLVM (clang).
-    # @note Updated 2024-07-05.
+    # @note Updated 2024-07-06.
     #
     # @seealso
     # - https://llvm.org/docs/GettingStarted.html
     # - https://llvm.org/docs/CMake.html
-    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/llvm.rb
+    # - https://github.com/conda-forge/llvmdev-feedstock
+    # - https://formulae.brew.sh/formula/llvm
     # - https://github.com/llvm/llvm-project/blob/main/clang/CMakeLists.txt
+    #
+    # Additional configuration:
     # - https://github.com/llvm/llvm-project/blob/main/cmake/
     #     Modules/FindLibEdit.cmake
     # - https://github.com/llvm/llvm-project/blob/main/llvm/cmake/
@@ -35,24 +40,23 @@ main() {
         'git'
         'perl'
         'pkg-config'
+        'ninja'
+        'swig'
     )
     # > koopa_is_linux && build_deps+=('gcc')
     deps=(
+        'xz' # lzma
         'zlib'
         'libedit'
         'libffi'
-        'icu4c' # libxml2
-        'libxml2'
         'ncurses'
         'python3.12'
-        'swig'
     )
     if koopa_is_linux
     then
         deps+=(
             # Needed for 'gold'.
             'binutils'
-            'xz' # elfutils
             'zstd' # elfutils
             # OpenMP requires 'gelf.h'.
             'elfutils'
@@ -67,15 +71,8 @@ main() {
     app['python']="$(koopa_locate_python312 --realpath)"
     app['swig']="$(koopa_locate_swig --realpath)"
     koopa_assert_is_executable "${app[@]}"
-    dict['jobs']="$(koopa_cpu_count)"
-    # FIXME This may only be an issue on Ubuntu 22.
-    # > if koopa_is_linux && [[ "${dict['jobs']}" -gt 4 ]]
-    # > then
-    # >     dict['jobs']=4
-    # > fi
     dict['libedit']="$(koopa_app_prefix 'libedit')"
     dict['libffi']="$(koopa_app_prefix 'libffi')"
-    dict['libxml2']="$(koopa_app_prefix 'libxml2')"
     dict['ncurses']="$(koopa_app_prefix 'ncurses')"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['python']="$(koopa_app_prefix 'python3.12')"
@@ -85,7 +82,6 @@ main() {
     koopa_assert_is_dir \
         "${dict['libedit']}" \
         "${dict['libffi']}" \
-        "${dict['libxml2']}" \
         "${dict['ncurses']}" \
         "${dict['python']}" \
         "${dict['zlib']}"
@@ -103,45 +99,43 @@ main() {
     fi
     dict['py_ver']="$(koopa_get_version "${app['python']}")"
     dict['py_maj_min_ver']="$(koopa_major_minor_version "${dict['py_ver']}")"
-    # FIXME May need to match projects and runtimes with homebrew.
     projects=(
-        # > 'bolt'
-        # > 'cross-project-tests'
-        # > 'pstl'
         'clang'
         'clang-tools-extra'
-        # NOTE flang steps seems to be crashing on Ubuntu 22.
-        # > 'flang'
         'lld'
         'lldb'
         'mlir'
-        'openmp'
         'polly'
     )
     koopa_is_macos && projects+=('flang')
     runtimes=(
         # > 'compiler-rt'
-        # > 'libc'
-        # > 'libclc'
         'libcxx'
         'libcxxabi'
         'libunwind'
     )
+    if koopa_is_macos
+    then
+        runtimes+=('openmp')
+    else
+        projects+=('openmp')
+    fi
     dict['projects']="$(koopa_paste --sep=';' "${projects[@]}")"
     dict['runtimes']="$(koopa_paste --sep=';' "${runtimes[@]}")"
     cmake_args=(
         # Build options --------------------------------------------------------
+        '-DLIBCXX_INSTALL_MODULES=ON'
+        '-DLIBOMP_INSTALL_ALIASES=OFF'
         '-DLLDB_ENABLE_CURSES=ON'
         '-DLLDB_ENABLE_LUA=OFF'
-        '-DLLDB_ENABLE_LZMA=OFF'
+        '-DLLDB_ENABLE_LZMA=ON'
         '-DLLDB_ENABLE_PYTHON=ON'
         '-DLLDB_USE_SYSTEM_DEBUGSERVER=ON'
-        '-DLIBOMP_INSTALL_ALIASES=OFF'
-        '-DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON'
         '-DLLVM_ENABLE_ASSERTIONS=OFF'
+        '-DLLVM_ENABLE_EH=ON'
         '-DLLVM_ENABLE_FFI=ON'
         '-DLLVM_ENABLE_LIBEDIT=ON'
-        '-DLLVM_ENABLE_LIBXML2=ON'
+        '-DLLVM_ENABLE_LIBXML2=OFF'
         "-DLLVM_ENABLE_PROJECTS=${dict['projects']}"
         '-DLLVM_ENABLE_RTTI=ON'
         "-DLLVM_ENABLE_RUNTIMES=${dict['runtimes']}"
@@ -150,11 +144,10 @@ main() {
         '-DLLVM_INCLUDE_DOCS=OFF'
         '-DLLVM_INCLUDE_TESTS=OFF'
         '-DLLVM_INSTALL_UTILS=ON'
+        '-DLLVM_LINK_LLVM_DYLIB=ON'
         '-DLLVM_OPTIMIZED_TABLEGEN=ON'
-        # > "-DLLVM_PARALLEL_COMPILE_JOBS=${dict['jobs']}"  # FIXME
-        # > '-DLLVM_PARALLEL_LINK_JOBS=1'  # FIXME
         '-DLLVM_POLLY_LINK_INTO_TOOLS=ON'
-        '-DLLVM_TARGETS_TO_BUILD=all'
+        '-DLLVM_TARGETS_TO_BUILD=all' # FIXME
         # External dependencies ------------------------------------------------
         "-DCURSES_INCLUDE_DIRS=${dict['ncurses']}/include"
         "-DCURSES_LIBRARIES=${dict['ncurses']}/lib/\
@@ -165,9 +158,6 @@ libncursesw.${dict['shared_ext']}"
         "-DLibEdit_INCLUDE_DIRS=${dict['libedit']}/include"
         "-DLibEdit_LIBRARIES=${dict['libedit']}/lib/\
 libedit.${dict['shared_ext']}"
-        "-DLIBXML2_INCLUDE_DIR=${dict['libxml2']}/include"
-        "-DLIBXML2_LIBRARY=${dict['libxml2']}/lib/\
-libxml2.${dict['shared_ext']}"
         "-DPANEL_LIBRARIES=${dict['ncurses']}/lib/\
 libpanelw.${dict['shared_ext']}"
         "-DPERL_EXECUTABLE=${app['perl']}"
@@ -183,35 +173,41 @@ libncursesw.${dict['shared_ext']}"
         "-DZLIB_INCLUDE_DIR=${dict['zlib']}/include"
         "-DZLIB_LIBRARY=${dict['zlib']}/lib/libz.${dict['shared_ext']}"
         # Additional Python binding fixes --------------------------------------
-        "-DCLANG_PYTHON_BINDINGS_VERSIONS=${dict['py_maj_min_ver']}"
-        "-DLLDB_PYTHON_EXE_RELATIVE_PATH=../../python/${dict['py_ver']}/\
-bin/python${dict['py_maj_min_ver']}"
-        "-DLLDB_PYTHON_RELATIVE_PATH=libexec/python${dict['py_maj_min_ver']}/\
-site-packages"
+# FIXME Are these safe to take out?
+# >         "-DCLANG_PYTHON_BINDINGS_VERSIONS=${dict['py_maj_min_ver']}"
+# >         "-DLLDB_PYTHON_EXE_RELATIVE_PATH=../../python/${dict['py_ver']}/\
+# > bin/python${dict['py_maj_min_ver']}"
+# >         "-DLLDB_PYTHON_RELATIVE_PATH=libexec/python${dict['py_maj_min_ver']}/\
+# > site-packages"
     )
     if koopa_is_linux
     then
         cmake_args+=(
-            # Use our GCC instead of relying on system.
+            '-DCLANG_DEFAULT_CXX_STDLIB=libstdc++'
+            # FIXME Use our GCC instead of relying on system?
             # > "-DCMAKE_C_COMPILER=${app['cc']}"
             # > "-DCMAKE_CXX_COMPILER=${app['cxx']}"
+            # Parts of Polly fail to correctly build with PIC.
+            '-DCMAKE_POSITION_INDEPENDENT_CODE=ON'
             # Ensure OpenMP picks up ELF.
             "-DLIBOMPTARGET_DEP_LIBELF_INCLUDE_DIR=${dict['elfutils']}/include"
             "-DLIBOMPTARGET_DEP_LIBELF_LIBRARIES=${dict['elfutils']}/lib/\
 libelf.${dict['shared_ext']}"
             # Enable llvm gold plugin for LTO.
             "-DLLVM_BINUTILS_INCDIR=${dict['binutils']}/include"
+            '-DLLVM_ENABLE_LIBCXX=OFF'
         )
     elif koopa_is_macos
     then
         dict['sysroot']="$(koopa_macos_sdk_prefix)"
         koopa_assert_is_dir "${dict['sysroot']}"
         cmake_args+=(
-            # > '-DLLVM_BUILD_LLVM_C_DYLIB=ON'
-            # > '-DLLVM_ENABLE_LIBCXX=ON'
-            # > '-DLLVM_LINK_LLVM_DYLIB=ON'
             "-DDEFAULT_SYSROOT=${dict['sysroot']}"
+            '-DLIBCXX_PSTL_CPU_BACKEND=libdispatch'
+            '-DLLVM_BUILD_LLVM_C_DYLIB=ON'
             '-DLLVM_CREATE_XCODE_TOOLCHAIN=OFF'
+            '-DLLVM_ENABLE_LIBCXX=ON'
+            '-DLLVM_LINK_LLVM_DYLIB=ON'
         )
     fi
     dict['url']="https://github.com/llvm/llvm-project/releases/download/\
@@ -220,7 +216,7 @@ llvmorg-${dict['version']}/llvm-project-${dict['version']}.src.tar.xz"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src/llvm'
     koopa_cmake_build \
-        --jobs="${dict['jobs']}" \
+        --ninja \
         --prefix="${dict['prefix']}" \
         "${cmake_args[@]}"
     return 0
