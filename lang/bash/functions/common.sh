bd54fdd3f53254ad1426c3d112de1d812943fcba
Michael Steinbaugh <mike@steinbaugh.com>
2024-07-08 10:35:39 -0400

    Fix for work dotfiles configuration

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -7919,13 +7919,18 @@ koopa_dotfiles_config_link() {
     return 0
 }
 
+koopa_dotfiles_prefix() {
+    _koopa_dotfiles_prefix "$@"
+}
+
 koopa_dotfiles_private_prefix() {
     koopa_print "$(koopa_config_prefix)/dotfiles-private"
     return 0
 }
 
-koopa_dotfiles_prefix() {
-    _koopa_dotfiles_prefix "$@"
+koopa_dotfiles_work_prefix() {
+    koopa_print "$(koopa_config_prefix)/dotfiles-work"
+    return 0
 }
 
 koopa_download_cran_latest() {
