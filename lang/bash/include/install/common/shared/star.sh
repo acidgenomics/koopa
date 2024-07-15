1348b6492f60bc7eaebd14f9b637730c55ffdf2c
Michael Steinbaugh <mike@steinbaugh.com>
2024-07-15 11:58:36 -0400

    Improve conda installation handling

--- lang/bash/include/install/common/shared/star.sh
+++ lang/bash/include/install/common/shared/star.sh
@@ -1,9 +1,52 @@
 #!/usr/bin/env bash
 
-main() {
+install_from_conda() {
     # """
-    # Install STAR.
-    # @note Updated 2023-10-18.
+    # Install STAR from bioconda.
+    # @note Updated 2024-07-15.
+    # """
+    local -A app dict
+    app['patch']="$(koopa_locate_patch)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    koopa_install_conda_package
+    if koopa_is_linux
+    then
+        dict['patch_file']='patch-script-dir.patch'
+        read -r -d '' "dict[patch_string]" << END || true
+--- STAR
++++ STAR-1
+@@ -1,6 +1,6 @@
+ #!/bin/bash
+
+-SCRIPT_DIR=\$( cd -- "\$( dirname -- "\${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
++SCRIPT_DIR=${dict['prefix']}/bin
+ DIR=\$SCRIPT_DIR
+ BASE=\${DIR}/\$(basename "\$0")
+ CMDARGS="\$@"
+END
+        koopa_write_string \
+            --file="${dict['patch_file']}" \
+            --string="${dict['patch_string']}"
+        "${app['patch']}" \
+            --unified \
+            --verbose \
+            "${dict['prefix']}/libexec/bin/STAR" \
+            "${dict['patch_file']}"
+        "${app['patch']}" \
+            --unified \
+            --verbose \
+            "${dict['prefix']}/libexec/bin/STARlong" \
+            "${dict['patch_file']}"
+    fi
+    "${dict['prefix']}/bin/STAR" -h
+    return 0
+}
+
+install_from_source() {
+    # """
+    # Install STAR from source.
+    # @note Updated 2024-07-15.
     #
     # Pull request to use 'SYSTEM_HTSLIB=1' to unbundle htslib:
     # https://github.com/alexdobin/STAR/pull/1586
@@ -101,5 +144,15 @@ ${dict['version']}.tar.gz"
     koopa_cp 'STAR' "${dict['prefix']}/bin/STAR"
     koopa_cp 'STARlong' "${dict['prefix']}/bin/STARlong"
     "${dict['prefix']}/bin/STAR" -h
+    return 0
+}
+
+main() {
+    if koopa_is_aarch64
+    then
+        install_from_source "$@"
+    else
+        install_from_conda "$@"
+    fi
     return 0
 }
