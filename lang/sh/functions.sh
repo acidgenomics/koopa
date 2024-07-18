739403cb6095b58c5470e5b1025e83a3931899e6
Michael Steinbaugh <mike@steinbaugh.com>
2024-07-18 07:38:34 -0400

    Rework activation of secrets and additional profile files

--- lang/sh/functions.sh
+++ lang/sh/functions.sh
@@ -194,6 +194,10 @@ _koopa_activate_aliases() {
     then
         . "${HOME:?}/.aliases-private"
     fi
+    if [ -f "${HOME:?}/.aliases-work" ]
+    then
+        . "${HOME:?}/.aliases-work"
+    fi
     unset -v __kvar_bin_prefix __kvar_xdg_data_home
     return 0
 }
@@ -731,15 +735,35 @@ _koopa_activate_pipx() {
     return 0
 }
 
-_koopa_activate_profile_private() {
-    __kvar_file="${HOME:?}/.profile-private"
-    if [ ! -r "$__kvar_file" ]
+_koopa_activate_profile_files() {
+    if [ -r "${HOME:?}/.profile-personal" ]
     then
-        unset -v __kvar_file
-        return 0
+        . "${HOME:?}/.profile-personal"
     fi
-    . "$__kvar_file"
-    unset -v __kvar_file
+    if [ -r "${HOME:?}/.profile-private" ]
+    then
+        . "${HOME:?}/.profile-private"
+    fi
+    if [ -r "${HOME:?}/.profile-work" ]
+    then
+        . "${HOME:?}/.profile-work"
+    fi
+    if [ -r "${HOME:?}/.secrets" ]
+    then
+        . "${HOME:?}/.secrets"
+    fi
+    if [ -r "${HOME:?}/.secrets-personal" ]
+    then
+        . "${HOME:?}/.secrets-personal"
+    fi
+    if [ -r "${HOME:?}/.secrets-private" ]
+    then
+        . "${HOME:?}/.secrets-private"
+    fi
+    if [ -r "${HOME:?}/.secrets-work" ]
+    then
+        . "${HOME:?}/.secrets-work"
+    fi
     return 0
 }
 
@@ -847,19 +871,6 @@ _koopa_activate_ruby() {
     export GEM_HOME="$__kvar_prefix"
     _koopa_add_to_path_start "${__kvar_prefix}/bin"
     unset -v __kvar_prefix
-    return 0
-}
-
-_koopa_activate_secrets() {
-    __kvar_file="${1:-}"
-    [ -z "$__kvar_file" ] && __kvar_file="${HOME:?}/.secrets"
-    if [ ! -r "$__kvar_file" ]
-    then
-        unset -v __kvar_file
-        return 0
-    fi
-    . "$__kvar_file"
-    unset -v __kvar_file
     return 0
 }
 
