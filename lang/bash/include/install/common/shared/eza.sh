07367faaea1fef033dd1f5ccb934effd8b5dba15
Michael Steinbaugh <mike@steinbaugh.com>
2024-07-27 14:56:46 -0400

    Add link

--- lang/bash/include/install/common/shared/eza.sh
+++ lang/bash/include/install/common/shared/eza.sh
@@ -7,6 +7,7 @@
 # cargo:warning=failed to probe system libgit2: Failed to run `PKG_CONFIG_ALLOW_SYSTEM_CFLAGS="1" PKG_CONFIG_ALLOW_SYSTEM_LIBS="1" PKG_CONFIG_PATH="/opt/koopa/app/libgit2/1.8.1/lib/pkgconfig" "pkg-config" "--libs" "--cflags" "libgit2" "libgit2 >= 1.8.1" "libgit2 < 1.9.0"`: No such file or directory (os error 2)
 #
 # https://github.com/eza-community/eza/issues/1072
+# https://github.com/rust-lang/git2-rs
 
 main() {
     # """
