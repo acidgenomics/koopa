660519bec9398a1e63d9d8ee45a2540ed29dab1a
Michael Steinbaugh <mike@steinbaugh.com>
2024-09-19 14:11:24 -0400

    Draft update to app locator

--- lang/bash/functions/common/l/locate-app.sh
+++ lang/bash/functions/common/l/locate-app.sh
@@ -3,7 +3,7 @@
 koopa_locate_app() {
     # """
     # Locate file system path to an application.
-    # @note Updated 2024-09-17.
+    # @note Updated 2024-09-19.
     #
     # Mode 1: direct executable file path input.
     # Mode 2: '--app-name' and '--bin-name' input.
@@ -20,6 +20,7 @@ koopa_locate_app() {
     bool['allow_bootstrap']=0
     bool['allow_koopa_bin']=1
     bool['allow_missing']=0
+    bool['allow_opt_bin']=1
     bool['allow_system']=0
     bool['only_bootstrap']=0
     bool['only_system']=0
@@ -116,68 +117,81 @@ koopa_locate_app() {
         [[ "${bool['allow_missing']}" -eq 1 ]] && return 0
         koopa_stop "Failed to locate '${dict['app']}'."
     fi
-    if [[ "${bool['only_system']}" -eq 0 ]]
-    then
-        [[ -n "${dict['app_name']}" ]] || return 1
-        [[ -n "${dict['bin_name']}" ]] || return 1
-    fi
+    [[ -n "${dict['app_name']}" ]] || return 1
+    [[ -n "${dict['bin_name']}" ]] || return 1
     if [[ -z "${dict['system_bin_name']}" ]]
     then
         dict['system_bin_name']="${dict['bin_name']}"
+    fi
+    if [[ "${bool['only_system']}" -eq 1 ]]
+    then
+        # FIXME Need to add koopa_remove_from_path_string here.
+        # FIXME Add only system support here.
+        # FIXME Remove koopa from current PATH.
+        koopa_stop 'FIXME'
+        # FIXME Reassign back to expected PATH.
     fi
     if [[ "${bool['allow_bootstrap']}" -eq 1 ]]
     then
         dict['bs_prefix']="$(koopa_bootstrap_prefix)"
         dict['app']="${dict['bs_prefix']}/bin/${dict['bin_name']}"
-    fi
-    if [[ -x "${dict['app']}" ]]
-    then
-        if [[ "${bool['realpath']}" -eq 1 ]]
+        if [[ -x "${dict['app']}" ]]
         then
-            dict['app']="$(koopa_realpath "${dict['app']}")"
+            if [[ "${bool['realpath']}" -eq 1 ]]
+            then
+                dict['app']="$(koopa_realpath "${dict['app']}")"
+            fi
+            koopa_print "${dict['app']}"
+            return 0
         fi
-        koopa_print "${dict['app']}"
-        return 0
     fi
     if [[ "${bool['allow_koopa_bin']}" -eq 1 ]]
     then
         dict['bin_prefix']="$(koopa_bin_prefix)"
         dict['app']="${dict['bin_prefix']}/${dict['bin_name']}"
-    fi
-    if [[ -x "${dict['app']}" ]]
-    then
-        if [[ "${bool['realpath']}" -eq 1 ]]
+        if [[ -x "${dict['app']}" ]]
         then
-            dict['app']="$(koopa_realpath "${dict['app']}")"
+            if [[ "${bool['realpath']}" -eq 1 ]]
+            then
+                dict['app']="$(koopa_realpath "${dict['app']}")"
+            fi
+            koopa_print "${dict['app']}"
+            return 0
         fi
-        koopa_print "${dict['app']}"
-        return 0
     fi
-    if [[ "${bool['only_system']}" -eq 0 ]]
+    if [[ "${bool['allow_opt_bin']}" -eq 1 ]]
     then
         dict['opt_prefix']="$(koopa_opt_prefix)"
         dict['app']="${dict['opt_prefix']}/${dict['app_name']}/\
 bin/${dict['bin_name']}"
+        if [[ -x "${dict['app']}" ]]
+        then
+            if [[ "${bool['realpath']}" -eq 1 ]]
+            then
+                dict['app']="$(koopa_realpath "${dict['app']}")"
+            fi
+            koopa_print "${dict['app']}"
+            return 0
+        fi
     fi
-    # FIXME This is failing to locate correctly on HPC with Lmod.
-    if [[ ! -x "${dict['app']}" ]] && [[ "${bool['allow_system']}" -eq 1 ]]
+    if [[ "${bool['allow_system']}" -eq 1 ]]
     then
         dict['app']="$(koopa_which "${dict['system_bin_name']}" || true)"
-    fi
-    if [[ -x "${dict['app']}" ]]
-    then
-        if [[ "${bool['realpath']}" -eq 1 ]]
+        if [[ -x "${dict['app']}" ]]
         then
-            dict['app']="$(koopa_realpath "${dict['app']}")"
+            if [[ "${bool['realpath']}" -eq 1 ]]
+            then
+                dict['app']="$(koopa_realpath "${dict['app']}")"
+            fi
+            koopa_print "${dict['app']}"
+            return 0
         fi
-        koopa_print "${dict['app']}"
-        return 0
     fi
     [[ "${bool['allow_missing']}" -eq 1 ]] && return 0
     if [[ "${bool['allow_system']}" -eq 1 ]]
     then
         koopa_stop \
-            "Failed to locate '${dict['bin_name']}'."
+            "Failed to locate '${dict['system_bin_name']}'."
     else
         koopa_stop \
             "Failed to locate '${dict['bin_name']}'." \
