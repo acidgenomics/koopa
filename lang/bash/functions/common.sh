fe0f5723e7a7738213304695142097d900eaddaf
Michael Steinbaugh <mike@steinbaugh.com>
2024-09-19 15:39:34 -0400

    Add preliminary build environment assert checks

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -1067,9 +1067,22 @@ koopa_assert_can_install_binary() {
 }
 
 koopa_assert_can_install_from_source() {
+    local -A app version
     koopa_assert_has_no_args "$#"
     koopa_assert_conda_env_is_not_active
-    koopa_assert_is_installed 'python3'
+    app['cc']="$(koopa_locate_cc --only-system)"
+    app['git']="$(koopa_locate_git --allow-system)"
+    app['ld']="$(koopa_locate_ld --only-system)"
+    app['make']="$(koopa_locate_make --only-system)"
+    app['perl']="$(koopa_locate_perl --only-system)"
+    app['python']="$(koopa_locate_python3 --allow-system)"
+    koopa_assert_is_executable "${app[@]}"
+    version['cc']="$(koopa_get_version "${app['cc']}")"
+    version['git']="$(koopa_get_version "${app['git']}")"
+    version['ld']="$(koopa_get_version "${app['ld']}")"
+    version['make']="$(koopa_get_version "${app['make']}")"
+    version['perl']="$(koopa_get_version "${app['perl']}")"
+    version['python']="$(koopa_get_version "${app['python']}")"
     return 0
 }
 
--- lang/bash/functions/common/a/assert-can-install-from-source.sh
+++ lang/bash/functions/common/a/assert-can-install-from-source.sh
@@ -5,14 +5,22 @@ koopa_assert_can_install_from_source() {
     # Assert that current environment supports building from source.
     # @note Updated 2024-09-19.
     # """
-    # FIXME Check for conda.
-    # FIXME Check that specific programs are installed -- python3, cc, ld, etc.
-    # FIXME Check that compiler version is current.
-    # FIXME Consider adding koopa_is_cc_supported check.
+    local -A app version
     koopa_assert_has_no_args "$#"
     koopa_assert_conda_env_is_not_active
-    # Some HPCs run with Python venv loaded, so disabling.
     # > koopa_assert_python_venv_is_not_active
-    koopa_assert_is_installed 'python3'
+    app['cc']="$(koopa_locate_cc --only-system)"
+    app['git']="$(koopa_locate_git --allow-system)"
+    app['ld']="$(koopa_locate_ld --only-system)"
+    app['make']="$(koopa_locate_make --only-system)"
+    app['perl']="$(koopa_locate_perl --only-system)"
+    app['python']="$(koopa_locate_python3 --allow-system)"
+    koopa_assert_is_executable "${app[@]}"
+    version['cc']="$(koopa_get_version "${app['cc']}")"
+    version['git']="$(koopa_get_version "${app['git']}")"
+    version['ld']="$(koopa_get_version "${app['ld']}")"
+    version['make']="$(koopa_get_version "${app['make']}")"
+    version['perl']="$(koopa_get_version "${app['perl']}")"
+    version['python']="$(koopa_get_version "${app['python']}")"
     return 0
 }
