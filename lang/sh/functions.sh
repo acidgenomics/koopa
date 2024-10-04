9b45863e58c1b18344c2aaade66250ce37fdbd2d
Michael Steinbaugh <mike@steinbaugh.com>
2024-10-04 15:49:22 -0400

    Rework bootstrap path handling

--- lang/sh/functions.sh
+++ lang/sh/functions.sh
@@ -251,6 +251,24 @@ _koopa_activate_bat() {
     return 0
 }
 
+_koopa_activate_bootstrap() {
+    __kvar_bootstrap_prefix="$(_koopa_bootstrap_prefix)"
+    if [ ! -d "$(_koopa_bootstrap_prefix)" ]
+    then
+        unset -v __kvar_bootstrap_prefix
+        return 0
+    fi
+    __kvar_opt_prefix="$(_koopa_opt_prefix)"
+    [ -d "${__kvar_opt_prefix}/bash" ] || return 0
+    [ -d "${__kvar_opt_prefix}/coreutils" ] || return 0
+    [ -d "${__kvar_opt_prefix}/openssl3" ] || return 0
+    [ -d "${__kvar_opt_prefix}/python3.12" ] || return 0
+    [ -d "${__kvar_opt_prefix}/zlib" ] || return 0
+    _koopa_add_to_path_start "${__kvar_bootstrap_prefix}/bin"
+    unset -v __kvar_bootstrap_prefix __kvar_opt_prefix
+    return 0
+}
+
 _koopa_activate_bottom() {
     [ -x "$(_koopa_bin_prefix)/btm" ] || return 0
     __kvar_prefix="$(_koopa_xdg_config_home)/bottom"
