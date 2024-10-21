8c2eca3ca118bdc6956de095452dac9ad438f6aa
Michael Steinbaugh <mike@steinbaugh.com>
2024-10-21 12:20:47 -0400

    Fix shared install admin

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -14115,8 +14115,6 @@ koopa_install_koopa() {
             --pattern="^${HOME:?}"
         then
             bool['shared']=0
-        else
-            bool['shared']=1
         fi
         if [[ "${bool['shared']}" -eq 1 ]]
         then
