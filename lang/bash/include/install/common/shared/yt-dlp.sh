943d4ff34c44ba8f08330a7c5ee7ea0cad85e7ff
Michael Steinbaugh <mike@steinbaugh.com>
2024-10-24 07:21:11 -0400

    Add FIXME regarding certificate handling

--- lang/bash/include/install/common/shared/yt-dlp.sh
+++ lang/bash/include/install/common/shared/yt-dlp.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# FIXME Look into tweaking certificate handling in build:
+# https://github.com/yt-dlp/yt-dlp/issues/6892
+# --compat-option no-certifi
+# Or interactively can pass in --no-check-certificate
+
 main() {
     koopa_install_python_package \
         --egg-name='yt_dlp'
