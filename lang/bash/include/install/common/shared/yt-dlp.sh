85fe20c016f7a8c00f0224fb6d2f9e5bdabe297d
Michael Steinbaugh <mike@steinbaugh.com>
2024-10-24 07:23:19 -0400

    Add note regarding config approach

--- lang/bash/include/install/common/shared/yt-dlp.sh
+++ lang/bash/include/install/common/shared/yt-dlp.sh
@@ -5,6 +5,9 @@
 # --compat-option no-certifi
 # Or interactively can pass in --no-check-certificate
 
+# Alternatively, use a configuration file approach instead:
+# https://github.com/yt-dlp/yt-dlp#configuration
+
 main() {
     koopa_install_python_package \
         --egg-name='yt_dlp'
