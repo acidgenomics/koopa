b143751ed3b7b38d9d7abfde3ecb15af33793c9a
Michael Steinbaugh <mike@steinbaugh.com>
2024-11-04 15:35:32 -0500

    Build less from GitHub to avoid server issues

--- lang/bash/include/install/common/shared/less.sh
+++ lang/bash/include/install/common/shared/less.sh
@@ -11,12 +11,15 @@ main() {
     # - https://www.greenwoodsoftware.com/less/
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/less.rb
     # """
-    local -A dict
+    local -A app dict
     local -a conf_args
+    koopa_activate_app --build-only 'autoconf' 'groff'
     koopa_activate_app 'ncurses' 'pcre2'
+    app['make']="$(koopa_locate_make)"
+    koopa_assert_is_executable "${app[@]}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    conf_args=(
+    conf_args+=(
         "--prefix=${dict['prefix']}"
         '--with-regex=pcre2'
     )
@@ -27,6 +30,7 @@ v${dict['version']}-rel.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
+    "${app['make']}" -f 'Makefile.aut' 'distfiles'
     koopa_make_build "${conf_args[@]}"
     return 0
 }
