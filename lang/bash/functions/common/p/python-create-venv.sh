7d1281877db73e01963354028777ccf1800a61f3
Michael Steinbaugh <mike@steinbaugh.com>
2024-11-05 09:47:05 -0500

    Relax version pinning in venv creation

--- lang/bash/functions/common/p/python-create-venv.sh
+++ lang/bash/functions/common/p/python-create-venv.sh
@@ -118,18 +118,9 @@ ${dict['py_maj_min_ver']}"
     koopa_assert_is_installed "${app['venv_python']}"
     if [[ "${bool['pip']}" -eq 1 ]]
     then
-        # 2024-10-14.
-        # > dict['pip_version']='24.2'
-        # > dict['setuptools_version']='75.1.0'
-        # > dict['wheel_version']='0.44.0'
         pip_args+=(
             "--python=${app['venv_python']}"
-            # > "pip==${dict['pip_version']}"
-            # > "setuptools==${dict['setuptools_version']}"
-            # > "wheel==${dict['wheel_version']}"
-            'pip'
-            'setuptools'
-            'wheel'
+            'pip' 'setuptools' 'wheel'
         )
         koopa_python_pip_install "${pip_args[@]}"
     fi
