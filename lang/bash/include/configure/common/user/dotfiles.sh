3f856e2cfefb248fb2fe5562b8a39ca61290edc6
Michael Steinbaugh <mike@steinbaugh.com>
2024-12-03 08:58:12 -0500

    Rework activate script linkage

--- lang/bash/include/configure/common/user/dotfiles.sh
+++ lang/bash/include/configure/common/user/dotfiles.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Configure dotfiles for current user.
-    # @note Updated 2024-09-05.
+    # @note Updated 2024-12-03.
     #
     # This also configures chezmoi to use our koopa managed dotfiles repo.
     #
@@ -27,21 +27,27 @@ main() {
     koopa_assert_is_executable "${app[@]}"
     bool['auto_config']=0
     dict['cm_prefix']="$(koopa_xdg_data_home)/chezmoi"
+    dict['koopa_prefix']="$(koopa_koopa_prefix)"
+    dict['activate_script']="${dict['koopa_prefix']}/activate"
     dict['prefix']="${1:-}"
     if [[ -z "${dict['prefix']}" ]]
     then
-        bool['auto_config']=1``
+        bool['auto_config']=1
         dict['prefix']="$(koopa_dotfiles_prefix)"
     fi
     koopa_assert_is_dir "${dict['prefix']}"
     dict['install_script']="${dict['prefix']}/install"
-    koopa_assert_is_file "${dict['install_script']}"
+    koopa_assert_is_file \
+        "${dict['activate_script']}" \
+        "${dict['install_script']}"
     # Link chezmoi to koopa dotfiles, when appropriate.
     if [[ ! -d "${dict['cm_prefix']}" ]]
     then
         koopa_ln "${dict['prefix']}" "${dict['cm_prefix']}"
     fi
-    koopa_add_config_link "${dict['prefix']}" 'dotfiles'
+    koopa_add_config_link \
+        "${dict['activate_script']}" 'activate' \
+        "${dict['prefix']}" 'dotfiles'
     koopa_add_to_path_start "$(koopa_dirname "${app['bash']}")"
     "${app['bash']}" "${dict['install_script']}"
     if [[ "${bool['auto_config']}" -eq 1 ]]
