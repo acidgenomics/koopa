e488b8fe905b0598ad837372bbcb491bcc4e81b9
Michael Steinbaugh <mike@steinbaugh.com>
2024-12-04 12:42:03 -0500

    Update ca-certs

--- lang/sh/functions.sh
+++ lang/sh/functions.sh
@@ -360,23 +360,27 @@ _koopa_activate_broot() {
 }
 
 _koopa_activate_ca_certificates() {
+    [ -n "${AWS_CA_BUNDLE:-}" ] && return 0
     [ -n "${DEFAULT_CA_BUNDLE_PATH:-}" ] && return 0
+    [ -n "${NODE_EXTRA_CA_CERTS:-}" ] && return 0
     [ -n "${REQUESTS_CA_BUNDLE:-}" ] && return 0
     [ -n "${SSL_CERT_FILE:-}" ] && return 0
-    __kvar_prefix="$(_koopa_opt_prefix)/ca-certificates"
-    if [ ! -d "$__kvar_prefix" ]
+    __kvar_prefix="$(_koopa_xdg_data_home)/ca-certificates"
+    __kvar_file="${__kvar_prefix}/cacert.pem"
+    if [ ! -f "$__kvar_file" ]
     then
-        unset -v __kvar_prefix
-        return 0
+        __kvar_prefix="$(_koopa_opt_prefix)/ca-certificates/share/\
+ca-certificates"
+        __kvar_file="${__kvar_prefix}/cacert.pem"
     fi
-    __kvar_prefix="$(_koopa_realpath "$__kvar_prefix")"
-    __kvar_file="${__kvar_prefix}/share/ca-certificates/cacert.pem"
     if [ ! -f "$__kvar_file" ]
     then
         unset -v __kvar_file __kvar_prefix
         return 0
     fi
+    export AWS_CA_BUNDLE="$__kvar_file"
     export DEFAULT_CA_BUNDLE_PATH="$__kvar_prefix"
+    export NODE_EXTRA_CA_CERTS="$__kvar_file"
     export REQUESTS_CA_BUNDLE="$__kvar_file"
     export SSL_CERT_FILE="$__kvar_file"
     unset -v __kvar_file __kvar_prefix
