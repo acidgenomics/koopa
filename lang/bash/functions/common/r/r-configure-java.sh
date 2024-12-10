973f57fc4d5b915aaef56d582e0c8aedef933a7f
Michael Steinbaugh <mike@steinbaugh.com>
2024-12-10 09:38:01 -0500

    Simplify if statement

--- lang/bash/functions/common/r/r-configure-java.sh
+++ lang/bash/functions/common/r/r-configure-java.sh
@@ -36,10 +36,10 @@ koopa_r_configure_java() {
     bool['system']=0
     bool['use_apps']=1
     ! koopa_is_koopa_app "${app['r']}" && bool['system']=1
-    [[ "${bool['system']}" -eq 1 ]] && bool['use_apps']=0
     if [[ "${bool['system']}" -eq 1 ]]
     then
         koopa_has_standard_umask || return 0
+        bool['use_apps']=0
     fi
     if [[ "${bool['use_apps']}" -eq 1 ]]
     then
