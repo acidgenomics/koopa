ee8e80dba85dd1a63e58ee5ab1e6c42aa80f0481
Michael Steinbaugh <mike@steinbaugh.com>
2024-12-13 09:59:47 -0500

    Improve missing brewfile error message

--- lang/bash/include/install/common/system/homebrew-bundle.sh
+++ lang/bash/include/install/common/system/homebrew-bundle.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install Homebrew packages using Bundle Brewfile.
-    # @note Updated 2023-05-20.
+    # @note Updated 2024-12-13.
     #
     # Custom brewfile is supported using a positional argument.
     #
@@ -23,8 +23,12 @@ main() {
     koopa_assert_is_executable "${app[@]}"
     dict['brewfile']="$(koopa_xdg_config_home)/homebrew/brewfile"
     dict['prefix']="$(koopa_homebrew_prefix)"
-    koopa_assert_is_file "${dict['brewfile']}"
     koopa_assert_is_dir "${dict['prefix']}"
+    if [[ ! -f "${dict['brewfile']}" ]]
+    then
+        koopa_stop "Brewfile at '${dict['brewfile']}' does not exist. \
+Run 'koopa install dotfiles' and 'koopa configure user dotfiles' to resolve."
+    fi
     # Note that cask specific args are handled by 'HOMEBREW_CASK_OPTS' global
     # variable, which is defined in our main Homebrew activation function.
     install_args=(
