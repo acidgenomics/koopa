bef3918769990cc3f35598500a9b76a80e29f5db
Michael Steinbaugh <mike@steinbaugh.com>
2024-12-24 12:31:20 -0500

    Migrate azure-cli to python3.12

--- lang/bash/include/install/common/shared/azure-cli.sh
+++ lang/bash/include/install/common/shared/azure-cli.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 main() {
-    # """
-    # Python 3.12 support status:
-    # https://github.com/Azure/azure-cli/issues/27673
-    # """
     koopa_install_python_package \
         --egg-name='azure_cli' \
-        --python-version='3.11'
+        --python-version='3.12'
     return 0
 }
