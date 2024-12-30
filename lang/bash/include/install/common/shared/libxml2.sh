5733ea2f859b8e013a83f6ab733453df2eb59055
Michael Steinbaugh <mike@steinbaugh.com>
2024-12-30 11:09:04 -0500

    Draft update to libxml2

--- lang/bash/include/install/common/shared/libxml2.sh
+++ lang/bash/include/install/common/shared/libxml2.sh
@@ -1,48 +1,67 @@
 #!/usr/bin/env bash
 
-# NOTE Library paths are currently incorrect for 2.11.2.
-# https://github.com/GNOME/libxml2/commit/3463063001f36c16e5f6ce9ad33cd12a376fc874
+# FIXME This is failing to build with ICU 76 on macOS Apple Silicon.
+# Undefined symbols for architecture arm64:
+#   "_UCNV_FROM_U_CALLBACK_STOP_76", referenced from:
+#       _openIcuConverter in libxml2_la-encoding.o
+#   "_UCNV_TO_U_CALLBACK_STOP_76", referenced from:
+#       _openIcuConverter in libxml2_la-encoding.o
+#
+# https://gitlab.gnome.org/GNOME/libxml2/-/issues/746
+# https://gitlab.gnome.org/GNOME/libxml2/-/commit/b57e022d75425ef8b617a1c3153198ee0a941da8
 
 main() {
     # """
     # Install libxml2.
-    # @note Updated 2023-05-08.
+    # @note Updated 2024-12-30.
     #
     # @seealso
     # - https://github.com/conda-forge/libxml2-feedstock
-    # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/libxml2.rb
+    # - https://formulae.brew.sh/formula/libxml2
     # - https://ports.macports.org/port/libxml2/details/
     # - https://www.linuxfromscratch.org/blfs/view/svn/general/libxml2.html
     # """
-    local -A dict
+    local -A app dict
     local -a build_deps conf_args deps
-    build_deps=('make' 'pkg-config')
+    build_deps=(
+        'autoconf'
+        'automake'
+        'libtool'
+        'make'
+        'pkg-config'
+    )
     deps=(
         'zlib'
         'icu4c'
         'readline'
         'xz'
+        'libiconv'
     )
     koopa_activate_app --build-only "${build_deps[@]}"
     koopa_activate_app "${deps[@]}"
+    app['autoreconf']="$(koopa_locate_autoreconf)"
+    koopa_assert_is_executable "${app['autoreconf']}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['readline']="$(koopa_app_prefix 'readline')"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
+    dict['icu4c']="$(koopa_app_prefix 'icu4c')"
+    dict['libiconv']="$(koopa_app_prefix 'libiconv')"
     dict['xz']="$(koopa_app_prefix 'xz')"
     dict['zlib']="$(koopa_app_prefix 'zlib')"
     dict['maj_min_ver']="$(koopa_major_minor_version "${dict['version']}")"
-    conf_args=(
+    conf_args+=(
         '--disable-dependency-tracking'
         '--enable-static=no'
         "--prefix=${dict['prefix']}"
         '--with-ftp'
         '--with-history'
-        '--with-icu'
+        "--with-iconv=${dict['libiconv']}"
+        "--with-icu=${dict['icu4c']}"
         '--with-legacy'
         "--with-lzma=${dict['xz']}"
         "--with-readline=${dict['readline']}"
+        '--with-tls'
         "--with-zlib=${dict['zlib']}"
-        '--without-iconv'
         '--without-python'
     )
     dict['url']="https://download.gnome.org/sources/libxml2/\
@@ -50,6 +69,8 @@ ${dict['maj_min_ver']}/libxml2-${dict['version']}.tar.xz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
+    # > NOCONFIGURE=1 ./autogen.sh
+    "${app['autoreconf']}" --force --install --verbose
     koopa_make_build "${conf_args[@]}"
     return 0
 }
