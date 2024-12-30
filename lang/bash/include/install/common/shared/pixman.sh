25261a02017b213cb7cf595b026ada7df1fdf7d3
Michael Steinbaugh <mike@steinbaugh.com>
2024-12-30 13:36:33 -0500

    Rework build recipes

--- lang/bash/include/install/common/shared/pixman.sh
+++ lang/bash/include/install/common/shared/pixman.sh
@@ -3,37 +3,41 @@
 main() {
     # """
     # Install pixman.
-    # @note Updated 2023-04-11.
+    # @note Updated 2024-12-30.
     #
     # @seealso
     # - https://github.com/macports/macports-ports/blob/master/graphics/
     #     libpixman/Portfile
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/pixman.rb
     # """
-    local -A dict
-    local -a conf_args
-    koopa_activate_app --build-only 'pkg-config'
+    local -A app dict
+    local -a build_deps meson_args
+    build_deps=(
+        'meson'
+        'ninja'
+        'pkg-config'
+    )
+    koopa_activate_app --build-only "${build_deps[@]}"
+    app['meson']="$(koopa_locate_meson)"
+    app['ninja']="$(koopa_locate_ninja)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['jobs']="$(koopa_cpu_count)"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    conf_args=(
-        '--disable-dependency-tracking'
-        '--disable-gtk'
-        '--disable-silent-rules'
-        '--disable-static'
-        "--prefix=${dict['prefix']}"
-    )
-    if koopa_is_macos
-    then
-        # Disable NEON intrinsic support.
-        # - https://gitlab.freedesktop.org/pixman/pixman/-/issues/59
-        # - https://gitlab.freedesktop.org/pixman/pixman/-/issues/69
-        conf_args+=('--disable-arm-a64-neon')
-    fi
     dict['url']="https://cairographics.org/releases/\
 pixman-${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
-    koopa_make_build "${conf_args[@]}"
+    meson_args+=(
+        '--buildtype=release'
+        '--default-library=shared'
+        "--prefix=${dict['prefix']}"
+    )
+    # FIXME Rework this to use koopa_meson_ninja_build.
+    # Refer to harfbuzz installer for shared code.
+    "${app['meson']}" setup "${meson_args[@]}" 'build'
+    "${app['ninja']}" -v -j "${dict['jobs']}" -C 'build'
+    "${app['ninja']}" -v -j "${dict['jobs']}" -C 'build' install
     return 0
 }
