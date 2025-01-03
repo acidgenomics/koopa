e22cc53a7dc31797b5ee4eff056266ae30246335
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-03 15:42:01 -0500

    Migrate some rust apps to conda

--- lang/bash/functions/common/i/install-difftastic.sh
+++ lang/bash/functions/common/i/install-difftastic.sh
@@ -2,7 +2,7 @@
 
 koopa_install_difftastic() {
     koopa_install_app \
-        --installer='rust-package' \
+        --installer='conda-package' \
         --name='difftastic' \
         "$@"
 }
--- lang/bash/functions/common/i/install-du-dust.sh
+++ lang/bash/functions/common/i/install-du-dust.sh
@@ -2,7 +2,6 @@
 
 koopa_install_du_dust() {
     koopa_install_app \
-        --installer='rust-package' \
         --name='du-dust' \
         "$@"
 }
