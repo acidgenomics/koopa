af67f1f840a7b20e67872a64189dd1c61c4fd91b
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-03 15:59:34 -0500

    Add assert for x86-64

--- lang/bash/functions/common/i/install-difftastic.sh
+++ lang/bash/functions/common/i/install-difftastic.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 koopa_install_difftastic() {
+    if koopa_is_macos
+    then
+        koopa_assert_is_not_x86_64
+    fi
     koopa_install_app \
         --installer='conda-package' \
         --name='difftastic' \
