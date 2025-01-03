cc7d873375820751ce7d9a86159a5a50e5f184d5
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-03 16:46:17 -0500

    Use conda instead of rust builds where possible

--- lang/sh/functions.sh
+++ lang/sh/functions.sh
@@ -950,20 +950,11 @@ _koopa_activate_starship() {
 
 _koopa_activate_tealdeer() {
     [ -x "$(_koopa_bin_prefix)/tldr" ] || return 0
-    if [ -z "${TEALDEER_CACHE_DIR:-}" ]
-    then
-        TEALDEER_CACHE_DIR="$(_koopa_xdg_cache_home)/tealdeer"
-    fi
     if [ -z "${TEALDEER_CONFIG_DIR:-}" ]
     then
         TEALDEER_CONFIG_DIR="$(_koopa_xdg_config_home)/tealdeer"
     fi
-    if [ ! -d "${TEALDEER_CACHE_DIR:?}" ]
-    then
-        _koopa_is_alias 'mkdir' && unalias 'mkdir'
-        mkdir -p "${TEALDEER_CACHE_DIR:?}" >/dev/null
-    fi
-    export TEALDEER_CACHE_DIR TEALDEER_CONFIG_DIR
+    export TEALDEER_CONFIG_DIR
     return 0
 }
 
