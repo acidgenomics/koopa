108b92f9856beb5173c62420266c817605db3498
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-03 17:39:34 -0500

    Install fish with conda

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -13313,9 +13313,9 @@ koopa_install_findutils() {
 
 koopa_install_fish() {
     koopa_install_app \
+        --installer='conda-package' \
         --name='fish' \
         "$@"
-    return 0
 }
 
 koopa_install_flac() {
