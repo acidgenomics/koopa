41bddddf64173655e4aedf6f84f592a753d8a34e
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-03 16:57:49 -0500

    Install zsh with conda

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -16571,7 +16571,10 @@ koopa_install_zoxide() {
 
 koopa_install_zsh() {
     local -A dict
-    koopa_install_app --name='zsh' "$@"
+    koopa_install_app \
+        --installer='conda-package' \
+        --name='zsh' \
+        "$@"
     dict['zsh']="$(koopa_app_prefix 'zsh')"
     koopa_chmod --recursive 'g-w' "${dict['zsh']}/share/zsh"
     return 0
--- lang/bash/functions/common/i/install-zsh.sh
+++ lang/bash/functions/common/i/install-zsh.sh
@@ -2,7 +2,10 @@
 
 koopa_install_zsh() {
     local -A dict
-    koopa_install_app --name='zsh' "$@"
+    koopa_install_app \
+        --installer='conda-package' \
+        --name='zsh' \
+        "$@"
     dict['zsh']="$(koopa_app_prefix 'zsh')"
     koopa_chmod --recursive 'g-w' "${dict['zsh']}/share/zsh"
     return 0
