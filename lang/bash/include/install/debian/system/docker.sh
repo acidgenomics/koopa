5ef2fce5ae6cfe4665fb4e872986e53b0efa4809
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-15 12:10:37 -0500

    Improve Docker config

--- lang/bash/include/install/debian/system/docker.sh
+++ lang/bash/include/install/debian/system/docker.sh
@@ -14,22 +14,29 @@ main() {
     # Install Docker.
     # @note Updated 2025-01-15.
     #
-    # @seealso
-    # - https://docs.docker.com/install/linux/docker-ce/debian/
-    # - https://docs.docker.com/install/linux/docker-ce/ubuntu/
-    #
     # Currently supports overlay2, aufs and btrfs storage drivers.
     #
     # Configures at '/var/lib/docker/'.
+    #
+    # @seealso
+    # - https://docs.docker.com/install/linux/docker-ce/debian/
+    # - https://docs.docker.com/install/linux/docker-ce/ubuntu/
+    # - https://stackoverflow.com/questions/45023363
     # """
+    local -A app
     local pkgs
+    app['service']="$(koopa_debian_locate_service)"
+    koopa_assert_is_executable "${app[@]}"
     koopa_debian_apt_add_docker_repo
     pkgs=(
+        'containerd.io'
+        'docker-buildx-plugin'
         'docker-ce'
         'docker-ce-cli'
-        'containerd.io'
+        'docker-compose-plugin'
     )
     koopa_debian_apt_install "${pkgs[@]}"
     koopa_linux_add_user_to_group 'docker'
+    "${app['service']}" docker start
     return 0
 }
--- lang/bash/include/uninstall/debian/system/docker.sh
+++ lang/bash/include/uninstall/debian/system/docker.sh
@@ -3,13 +3,23 @@
 main() {
     # """
     # Uninstall Docker.
-    # @note Updated 2021-12-09.
+    # @note Updated 2025-01-15.
     # """
     local -a pkgs
     pkgs=(
+        # Official packages ----
         'containerd.io'
+        'docker-buildx-plugin'
         'docker-ce'
         'docker-ce-cli'
+        'docker-compose-plugin'
+        # Conflicting packages ----
+        'containerd'
+        'docker-compose'
+        'docker-doc'
+        'docker.io'
+        'podman-docker'
+        'runc'
     )
     koopa_debian_apt_remove "${pkgs[@]}"
     koopa_debian_apt_delete_repo 'docker'
