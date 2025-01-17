0b649bab3788ca66812e5c272224e1de0f9c82a3
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-17 14:34:37 -0500

    Debug man1 linkage error

--- lang/bash/include/install/common/shared/gentropy.sh
+++ lang/bash/include/install/common/shared/gentropy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # FIXME Saw error message about man1 linkage on EC2, look into debugging.
+# This may only be an issue with Bash 4.2 due to empty array handling?
 
 main() {
     koopa_install_python_package \
