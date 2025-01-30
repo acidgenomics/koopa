e3894b7ca3d62f2ccd758ed0b5faeaa1b519ad95
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-30 10:37:28 -0500

    Add support for direnv activation

--- lang/sh/functions.sh
+++ lang/sh/functions.sh
@@ -579,6 +579,30 @@ _koopa_activate_dircolors() {
     return 0
 }
 
+_koopa_activate_direnv() {
+    __kvar_direnv="$(_koopa_bin_prefix)/direnv"
+    if [ ! -x "$__kvar_direnv" ]
+    then
+        unset -v __kvar_direnv
+        return 0
+    fi
+    __kvar_shell="$(_koopa_shell_name)"
+    __kvar_nounset="$(_koopa_boolean_nounset)"
+    [ "$__kvar_nounset" -eq 1 ] && set +o nounset
+    case "$__kvar_shell" in
+        'bash' | \
+        'zsh')
+            eval "$("$__kvar_direnv" hook "$__kvar_shell")"
+            ;;
+    esac
+    [ "$__kvar_nounset" -eq 1 ] && set -o nounset
+    unset -v \
+        __kvar_direnv \
+        __kvar_nounset \
+        __kvar_shell
+    return 0
+}
+
 _koopa_activate_docker() {
     _koopa_add_to_path_start "${HOME:?}/.docker/bin"
     return 0
