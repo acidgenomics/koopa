a0623417351d293f2142889de3091ff3f105e19a
Michael Steinbaugh <mike@steinbaugh.com>
2025-02-18 07:26:44 -0500

    Debug permissions issue

--- lang/bash/functions/common/r/r-configure-java.sh
+++ lang/bash/functions/common/r/r-configure-java.sh
@@ -84,6 +84,14 @@ Skipping configuration."
         r_cmd=("${app['r']}")
     fi
     koopa_assert_is_executable "${app[@]}"
+    stat /usr/lib/R/etc/ldpaths # FIXME
     "${r_cmd[@]}" --vanilla CMD javareconf "${java_args[@]}"
+    #if [[ "${bool['system']}" -eq 1 ]]
+    #then
+    #    # FIXME Need to set permissions on /usr/lib/R/etc/ldpaths
+    #    koopa_stop 'FIXME'
+    #fi
+    stat /usr/lib/R/etc/ldpaths # FIXME
+    koopa_stop 'FIXME HELLO THERE'
     return 0
 }
