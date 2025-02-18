bcd2877c97a6990c3b71ff3ae371f292f7e209eb
Michael Steinbaugh <mike@steinbaugh.com>
2025-02-18 07:39:15 -0500

    Remove unnecessary umask code

--- lang/bash/include/install/debian/system/r.sh
+++ lang/bash/include/install/debian/system/r.sh
@@ -16,10 +16,8 @@ main() {
     # - https://cran.r-project.org/bin/linux/debian/
     # - https://cran.r-project.org/bin/linux/ubuntu/
     # """
-    local -A app bool dict
+    local -A app dict
     local -a dep_pkgs pkgs
-    # > bool['std_umask']=1
-    # > ! koopa_has_standard_umask && bool['std_umask']=0
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dep_pkgs=(
         'autoconf'
@@ -91,11 +89,6 @@ main() {
         'xz-utils'
         'zlib1g-dev'
     )
-    # > if [[ "${bool['std_umask']}" -eq 0 ]]
-    # > then
-    # >     dict['default_umask']="$(umask)"
-    # >     umask 0022
-    # > fi
     koopa_debian_apt_install "${dep_pkgs[@]}"
     koopa_debian_apt_add_r_repo "${dict['version']}"
     pkgs=('r-base' 'r-base-dev')
@@ -103,9 +96,5 @@ main() {
     app['r']='/usr/bin/R'
     koopa_assert_is_executable "${app['r']}"
     koopa_configure_r "${app['r']}"
-    # > if [[ "${bool['std_umask']}" -eq 0 ]]
-    # > then
-    # >     umask "${dict['default_umask']}"
-    # > fi
     return 0
 }
