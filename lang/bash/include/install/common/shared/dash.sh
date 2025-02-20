e9f36a3f5183d1d7dba6f045faab517f838446ea
Michael Steinbaugh <mike@steinbaugh.com>
2025-02-20 10:12:46 -0500

    Update to use HTTPS

--- lang/bash/include/install/common/shared/dash.sh
+++ lang/bash/include/install/common/shared/dash.sh
@@ -3,7 +3,7 @@
 main() {
     # """
     # Install Dash shell.
-    # @note Updated 2025-01-03.
+    # @note Updated 2025-02-20.
     #
     # @seealso
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/dash.rb
@@ -22,7 +22,9 @@ main() {
     then
         export ac_cv_func_stat64='no'
     fi
-    dict['url']="http://gondor.apana.org.au/~herbert/dash/files/\
+# >     dict['url']="http://gondor.apana.org.au/~herbert/dash/files/\
+# > dash-${dict['version']}.tar.gz"
+    dict['url']="https://git.kernel.org/pub/scm/utils/dash/dash.git/snapshot/\
 dash-${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
