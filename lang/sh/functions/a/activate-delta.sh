930d73e1e186342891c608e5705616b612777614
Michael Steinbaugh <mike@steinbaugh.com>
2025-02-20 12:23:40 -0500

    Quiet down

--- lang/sh/functions/a/activate-delta.sh
+++ lang/sh/functions/a/activate-delta.sh
@@ -3,7 +3,7 @@
 _koopa_activate_delta() {
     # """
     # Activate delta (git-delta) diff tool.
-    # @note Updated 2023-03-09.
+    # @note Updated 2025-02-20.
     # """
     [ -x "$(_koopa_bin_prefix)/delta" ] || return 0
     __kvar_prefix="$(_koopa_xdg_config_home)/delta"
@@ -38,7 +38,10 @@ _koopa_activate_delta() {
         fi
     fi
     _koopa_is_alias 'ln' && unalias 'ln'
-    ln -fns "$__kvar_source_file" "$__kvar_target_file" >/dev/null
+    ln -fns \
+        "$__kvar_source_file" \
+        "$__kvar_target_file" \
+        >/dev/null 2>&1
     unset -v \
         __kvar_prefix \
         __kvar_source_bn \
