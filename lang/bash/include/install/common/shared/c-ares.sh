1dc80b99892e49c01b7f3e3ba14c7dcd6fd7450a
Michael Steinbaugh <mike@steinbaugh.com>
2025-03-08 13:14:23 -0500

    Fix c-ares build

--- lang/bash/include/install/common/shared/c-ares.sh
+++ lang/bash/include/install/common/shared/c-ares.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# FIXME pkg-config isn't set up correctly:
-# prefix=/opt/koopa/app/c-ares/1.34.4
-# exec_prefix=${prefix}//opt/koopa/app/c-ares/1.34.4/bin
-# libdir=${prefix}//opt/koopa/app/c-ares/1.34.4/lib
-# includedir=${prefix}//opt/koopa/app/c-ares/1.34.4/include
-
 main() {
     # """
     # Install c-ares.
@@ -16,6 +10,7 @@ main() {
     # - https://github.com/Homebrew/homebrew-core/blob/master/Formula/c-ares.rb
     # """
     local -A dict
+    local -a cmake_build_args
     koopa_activate_app --build-only 'pkg-config'
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
@@ -24,10 +19,12 @@ v${dict['version']}/c-ares-${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
-    koopa_cmake_build \
-        --bin-dir='bin' \
-        --include-dir='include' \
-        --lib-dir='lib' \
-        --prefix="${dict['prefix']}"
+    cmake_build_args=(
+        '--bin-dir=bin'
+        '--include-dir=include'
+        '--lib-dir=lib'
+        "--prefix=${dict['prefix']}"
+    )
+    koopa_cmake_build "${cmake_build_args[@]}"
     return 0
 }
