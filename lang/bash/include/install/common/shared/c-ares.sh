ca8a89d1c09dd539a89da67a3d768fdf31507b25
Michael Steinbaugh <mike@steinbaugh.com>
2025-03-08 13:13:00 -0500

    Fix pkg-config

--- lang/bash/include/install/common/shared/c-ares.sh
+++ lang/bash/include/install/common/shared/c-ares.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# FIXME pkg-config isn't set up correctly:
+# prefix=/opt/koopa/app/c-ares/1.34.4
+# exec_prefix=${prefix}//opt/koopa/app/c-ares/1.34.4/bin
+# libdir=${prefix}//opt/koopa/app/c-ares/1.34.4/lib
+# includedir=${prefix}//opt/koopa/app/c-ares/1.34.4/include
+
 main() {
     # """
     # Install c-ares.
-    # @note Updated 2024-06-15.
+    # @note Updated 2025-03-08.
     #
     # @seealso
     # - https://c-ares.org/
@@ -18,6 +24,10 @@ v${dict['version']}/c-ares-${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
-    koopa_cmake_build --prefix="${dict['prefix']}"
+    koopa_cmake_build \
+        --bin-dir='bin' \
+        --include-dir='include' \
+        --lib-dir='lib' \
+        --prefix="${dict['prefix']}"
     return 0
 }
