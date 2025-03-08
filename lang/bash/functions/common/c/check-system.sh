fb9a37eab2cdf5a3bb7b01cb88de81268bbc0863
Michael Steinbaugh <mike@steinbaugh.com>
2025-03-08 12:24:50 -0500

    Add FIXME comments

--- lang/bash/functions/common/c/check-system.sh
+++ lang/bash/functions/common/c/check-system.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# FIXME This needs to check system Python and R versions.
 # FIXME This isn't checking man linkage correctly currently.
+# FIXME Check for current clang headers for system R on macOS.
 # FIXME Just move all checks into Python code, for simplicity.
+# TODO Can we check for current Xcode CLT on macOS?
 
 koopa_check_system() {
     # """
