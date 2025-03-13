f9dcb965cbb0b38c563c24a42d4591b4cd340072
Michael Steinbaugh <mike@steinbaugh.com>
2025-03-13 15:39:56 -0400

    Rework true/false check approach

--- etc/koopa/tests/shunit2.d/compare-versions.sh
+++ etc/koopa/tests/shunit2.d/compare-versions.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-test_compare_versions_eq() {
-    assertEquals \
-        '1.0.0 = 1.0.0' \
-        "$( \
-            koopa_compare_versions '1.0.0' -eq '1.0.0' \
-            && echo 'true' || echo 'false' \
-        )" \
-        'true'
-    assertEquals \
-        '1.0.0 = 1.0.1' \
-        "$( \
-            koopa_compare_versions '1.0.0' -eq '1.0.1' \
-            && echo 'true' || echo 'false' \
-        )" \
-        'false'
+test_compare_versions() {
+    (koopa_compare_versions '1.0.0' -eq '1.0.0')
+    assertTrue '1.0.0 = 1.0.0' "$?"
+    (koopa_compare_versions '1.0.0' -eq '1.0.1')
+    assertFalse '1.0.0 = 1.0.1' "$?"
 }
