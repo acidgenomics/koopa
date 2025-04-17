190f788498f112d0a563b6b7776c3210b5221aba
Michael Steinbaugh <mike@steinbaugh.com>
2025-04-17 07:58:34 -0400

    Update dotfiles

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -22059,6 +22059,20 @@ koopa_python_system_packages_prefix() {
     return 0
 }
 
+koopa_python_update_venv() {
+    local -A dict
+    koopa_assert_has_args_eq "$#" 1
+    dict['venv_prefix']="${1:?}"
+    koopa_assert_is_dir "${dict['venv_prefix']}"
+    dict['requirements']="$(koopa_mktemp)"
+    koopa_python_activate_venv "${dict['venv_prefix']}"
+    pip freeze > "$dict['requirements']}"
+    pip install -r "${dict['requirements']}" --upgrade
+    koopa_python_deactivate_venv
+    koopa_rm "${dict['requirements']}"
+    return 0
+}
+
 koopa_python_virtualenvs_prefix() {
     koopa_print "${HOME}/.virtualenvs"
     return 0
