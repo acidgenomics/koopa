fee4b27cdd99d1f6c85a35fa932c44327d5cfe87
Michael Steinbaugh <mike@steinbaugh.com>
2025-05-05 07:49:30 -0400

    Add support for pyenv multiuser

--- lang/bash/include/install/common/shared/pyenv.sh
+++ lang/bash/include/install/common/shared/pyenv.sh
@@ -11,22 +11,33 @@
 main() {
     # """
     # Install pyenv.
-    # @note Updated 2025-04-29.
+    # @note Updated 2025-05-05.
+    #
+    # @seealso
+    # - https://github.com/pyenv/pyenv
+    # - https://github.com/macdub/pyenv-multiuser
     # """
     local -A dict
+    local -a dirs
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['url']="https://github.com/pyenv/pyenv/archive/refs/tags/\
 v${dict['version']}.tar.gz"
-    koopa_download "${dict['url']}"
+    koopa_download "${dict['url']}" 'src.tar.gz'
+    koopa_extract 'src.tar.gz' "${dict['prefix']}"
+    dirs=(
+        "${dict['prefix']}/shims"
+        "${dict['prefix']}/versions"
+    )
+    koopa_mkdir "${dirs[@]}"
+    koopa_chmod 0777 "${dirs[@]}"
+    # Install pyenv-multiuser plugin.
+    dict['multiuser_version']='1.0.8'
+    dict['multiuser_url']="https://github.com/macdub/pyenv-multiuser/archive/\
+refs/tags/${dict['multiuser_version']}.tar.gz"
+    koopa_download "${dict['multiuser_url']}" 'multiuser-src.tar.gz'
     koopa_extract \
-        "$(koopa_basename "${dict['url']}")" \
-        "${dict['prefix']}"
-    koopa_mkdir \
-        "${dict['prefix']}/shims" \
-        "${dict['prefix']}/versions"
-    koopa_chmod 0777 \
-        "${dict['prefix']}/shims" \
-        "${dict['prefix']}/versions"
+        'multiuser-src.tar.gz' \
+        "${dict['prefix']}/plugins/pyenv-multiuser"
     return 0
 }
