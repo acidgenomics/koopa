9194d3fbc53d5c691210cc830bbc94428bef72c4
Michael Steinbaugh <mike@steinbaugh.com>
2025-06-17 11:13:27 -0400

    Update Python and R versions

--- lang/bash/functions/os/macos/i/install-system-python.sh
+++ lang/bash/functions/os/macos/i/install-system-python.sh
@@ -3,7 +3,7 @@
 koopa_macos_install_system_python() {
     koopa_install_app \
         --installer='python' \
-        --name='python3.12' \
+        --name='python3.13' \
         --platform='macos' \
         --prefix="$(koopa_macos_python_prefix)" \
         --system \
