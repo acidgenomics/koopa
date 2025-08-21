25c03ceab6a3d5a2913b4d9d83a138dceb7e7b31
Michael Steinbaugh <mike@steinbaugh.com>
2025-08-21 16:51:30 -0400

    Rework Python locator

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -19912,7 +19912,7 @@ koopa_locate_python() {
     dict['python_version']="$(koopa_python_major_minor_version)"
     koopa_locate_app \
         --app-name="python${dict['python_version']}" \
-        --bin-name="python${dict['python_version']}" \
+        --bin-name='python3' \
         "$@"
 }
 
