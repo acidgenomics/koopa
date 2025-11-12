2467a833b21d378c3a904c40f8b92d7f9ef01834
Michael Steinbaugh <mike@steinbaugh.com>
2025-11-12 08:24:17 -0500

    Set global brewfile

--- lang/sh/functions/m/macos-activate-homebrew.sh
+++ lang/sh/functions/m/macos-activate-homebrew.sh
@@ -3,7 +3,7 @@
 _koopa_macos_activate_homebrew() {
     # """
     # Activate Homebrew on macOS.
-    # @note Updated 2023-12-14.
+    # @note Updated 2025-11-12.
     # """
     __kvar_prefix="$(_koopa_homebrew_prefix)"
     if [ ! -x "${__kvar_prefix}/bin/brew" ]
@@ -11,13 +11,24 @@ _koopa_macos_activate_homebrew() {
         unset -v __kvar_prefix
         return 0
     fi
+    __kvar_brewfile="$(_koopa_xdg_config_home)/homebrew/Brewfile"
     _koopa_add_to_path_start "${__kvar_prefix}/bin"
-    [ -z "${HOMEBREW_CLEANUP_MAX_AGE_DAYS:-}" ] && \
+    if [ -z "${HOMEBREW_BUNDLE_FILE_GLOBAL:-}" ] && [ -f "$__kvar_brewfile" ]
+    then
+        export HOMEBREW_BUNDLE_FILE_GLOBAL="$__kvar_brewfile"
+    fi
+    if [ -z "${HOMEBREW_CLEANUP_MAX_AGE_DAYS:-}" ]
+    then
         export HOMEBREW_CLEANUP_MAX_AGE_DAYS=30
-    [ -z "${HOMEBREW_INSTALL_CLEANUP:-}" ] && \
+    fi
+    if [ -z "${HOMEBREW_INSTALL_CLEANUP:-}" ]
+    then
         export HOMEBREW_INSTALL_CLEANUP=1
-    [ -z "${HOMEBREW_NO_ENV_HINTS:-}" ] && \
+    fi
+    if [ -z "${HOMEBREW_NO_ENV_HINTS:-}" ]
+    then
         export HOMEBREW_NO_ENV_HINTS=1
-    unset -v __kvar_prefix
+    fi
+    unset -v __kvar_brewfile __kvar_prefix
     return 0
 }
