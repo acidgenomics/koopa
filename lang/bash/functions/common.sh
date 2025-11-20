c0fd53667b54b324ec3265ac30f2c0849af3817e
Steinbaugh, Michael <michael.steinbaugh@merck.com>
2025-11-20 16:13:30 +0000

    Update google-cloud-sdk

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -19923,7 +19923,7 @@ koopa_locate_python() {
     dict['python_version']="$(koopa_python_major_minor_version)"
     koopa_locate_app \
         --app-name="python${dict['python_version']}" \
-        --bin-name='python3' \
+        --bin-name="python${dict['python_version']}" \
         "$@"
 }
 
