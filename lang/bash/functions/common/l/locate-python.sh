29bab8fc1b5d46a15f94f0f02159eca5d1f90d1e
Steinbaugh, Michael <michael.steinbaugh@merck.com>
2025-11-20 16:12:15 +0000

    Ensure we're using correct major version

--- lang/bash/functions/common/l/locate-python.sh
+++ lang/bash/functions/common/l/locate-python.sh
@@ -5,6 +5,6 @@ koopa_locate_python() {
     dict['python_version']="$(koopa_python_major_minor_version)"
     koopa_locate_app \
         --app-name="python${dict['python_version']}" \
-        --bin-name='python3' \
+        --bin-name="python${dict['python_version']}" \
         "$@"
 }
