aa8ce31e1b165df22ab63cc571a0fc023c9a52f2
Michael Steinbaugh <mike@steinbaugh.com>
2025-12-01 07:38:51 -0500

    Improve shell support

--- lang/bash/include/install/common/shared/lesspipe.sh
+++ lang/bash/include/install/common/shared/lesspipe.sh
@@ -3,29 +3,30 @@
 main() {
     # """
     # Install lesspipe.
-    # @note Updated 2023-04-10.
+    # @note Updated 2025-12-01.
     #
     # @seealso
     # - https://github.com/wofr06/lesspipe
     # - https://github.com/Homebrew/homebrew-core/blob/master/
     #     Formula/lesspipe.rb
     # """
-    local -A dict
+    local -A app dict
     local -a conf_args
+    app['bash']="$(koopa_locate_bash)"
+    koopa_assert_is_executable "${app['bash']}"
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
-    conf_args=("--prefix=${dict['prefix']}")
+    conf_args=(
+        "--bash-completion-dir=${dict['prefix']}/etc/bash_completion.d"
+        "--prefix=${dict['prefix']}"
+        "--shell=${app['bash']}"
+        "--zsh-completion-dir=${dict['prefix']}/share/zsh/site-functions"
+    )
     dict['url']="https://github.com/wofr06/lesspipe/archive/refs/\
 tags/v${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
-    # shellcheck disable=SC2016
-    koopa_find_and_replace_in_file \
-        --fixed \
-        --pattern='\$(DESTDIR)/etc/bash_completion.d' \
-        --replacement='\$(DESTDIR)\$(PREFIX)/etc/bash_completion.d' \
-        'configure'
     koopa_make_build "${conf_args[@]}"
     return 0
 }
