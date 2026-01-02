b22b560328a8900efd7519df2fcb24d2d47d63fd
Michael Steinbaugh <mike@steinbaugh.com>
2026-01-02 10:40:14 -0500

    Harden custom SSL cert checker

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -11021,11 +11021,14 @@ koopa_has_private_access() {
 }
 
 koopa_has_ssl_cert_file() {
-    if [[ -z "${SSL_CERT_FILE:-}" ]]
+    local -A dict
+    dict['ssl_cert_file']="${SSL_CERT_FILE:-}"
+    if [[ -z "${dict['ssl_cert_file']}" ]]
     then
         return 1
     fi
-    if [[ "${SSL_CERT_FILE:?}" == "${KOOPA_PREFIX:?}/"* ]]
+    dict['koopa_prefix']="$(koopa_koopa_prefix)"
+    if [[ "${dict['ssl_cert_file']}" == "${dict['koopa_prefix']}/"* ]]
     then
         return 1
     fi
