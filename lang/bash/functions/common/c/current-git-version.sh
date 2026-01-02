877e5e04574a2ff78d69144e691120577606d3ae
Michael Steinbaugh <mike@steinbaugh.com>
2026-01-02 09:14:54 -0500

    Add git version checker

--- lang/bash/functions/common/c/current-git-version.sh
+++ lang/bash/functions/common/c/current-git-version.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env bash
 
-koopa_current_gnu_ftp_version() {
+koopa_current_git_version() {
     # """
-    # Get current version from GNU FTP server.
-    # @note Updated 2025-12-30.
-    #
-    # @examples
-    # $ koopa_current_gnu_ftp_version 'coreutils'
+    # Get current Git version.
+    # @note Updated 2026-01-02.
     # """
     local -A app dict
-    koopa_assert_has_args_eq "$#" 1
+    koopa_assert_has_no_args "$#"
     app['curl']="$(koopa_locate_curl)"
     app['cut']="$(koopa_locate_cut)"
     app['grep']="$(koopa_locate_grep)"
-    app['head']="$(koopa_locate_head)"
     app['rev']="$(koopa_locate_rev)"
+    app['sort']="$(koopa_locate_sort)"
+    app['tail']="$(koopa_locate_tail)"
     koopa_assert_is_executable "${app[@]}"
-    dict['name']="${1:?}"
-    dict['url']="https://ftp.gnu.org/gnu/${dict['name']}/?C=M;O=D"
-    dict['grep_string']="${dict['name']}-[.0-9a-z]+.tar"
+    dict['url']='https://mirrors.edge.kernel.org/pub/software/scm/git/'
+    dict['grep_string']='git-[.0-9]+\.tar\.xz'
     dict['version']="$( \
         "${app['curl']}" -s "${dict['url']}" \
             | "${app['grep']}" -Eo "${dict['grep_string']}" \
-            | "${app['head']}" -n 1 \
+            | "${app['sort']}" -u \
+            | "${app['tail']}" -n 1 \
             | "${app['cut']}" -d '-' -f '2' \
             | "${app['rev']}" \
-            | "${app['cut']}" -d '.' -f '2-' \
+            | "${app['cut']}" -d '.' -f '3-' \
             | "${app['rev']}" \
     )"
     koopa_print "${dict['version']}"
