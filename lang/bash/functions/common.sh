ab1f6c863e75e8a908bc0873969d2fd086316b20
Michael Steinbaugh <mike@steinbaugh.com>
2026-01-02 09:29:31 -0500

    Add support for current Python version

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -6891,6 +6891,30 @@ koopa_current_pypi_package_version() {
     return 0
 }
 
+koopa_current_python_version() {
+    local -A app dict
+    koopa_assert_has_no_args "$#"
+    app['curl']="$(koopa_locate_curl)"
+    app['cut']="$(koopa_locate_cut)"
+    app['grep']="$(koopa_locate_grep)"
+    app['head']="$(koopa_locate_head)"
+    app['sort']="$(koopa_locate_sort)"
+    app['tail']="$(koopa_locate_tail)"
+    koopa_assert_is_executable "${app[@]}"
+    dict['url']='https://www.python.org/ftp/python/'
+    dict['grep_string']='3\.[0-9]+\.[0-9]+/'
+    dict['version']="$( \
+        "${app['curl']}" -s "${dict['url']}" \
+            | "${app['grep']}" -Eo "${dict['grep_string']}" \
+            | "${app['cut']}" -d '/' -f 1 \
+            | "${app['sort']}" -Vu \
+            | "${app['tail']}" -n 2 \
+            | "${app['head']}" -n 1 \
+    )"
+    koopa_print "${dict['version']}"
+    return 0
+}
+
 koopa_current_refseq_version() {
     local str url
     koopa_assert_has_no_args "$#"
