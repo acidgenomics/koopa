cd0f2b016f2593877105326b3d13e0461a410c66
Michael Steinbaugh <mike@steinbaugh.com>
2026-01-02 10:36:41 -0500

    Improve SSL checker function

--- lang/bash/functions/common/h/has-ssl-cert-file.sh
+++ lang/bash/functions/common/h/has-ssl-cert-file.sh
@@ -2,8 +2,16 @@
 
 koopa_has_ssl_cert_file() {
     # """
-    # Does the current environment have custom CA certificates loaded?"
-    # @note Updated 2025-12-08.
+    # Does the current environment have custom CA certificates loaded?
+    # @note Updated 2026-01-02.
     # """
-    [[ -n "${SSL_CERT_FILE:-}" ]]
+    if [[ -z "${SSL_CERT_FILE:-}" ]]
+    then
+        return 1
+    fi
+    if [[ "${SSL_CERT_FILE:?}" == "${KOOPA_PREFIX:?}/"* ]]
+    then
+        return 1
+    fi
+    return 0
 }
