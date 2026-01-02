1766b71e9d5ef4910d3c02da80e6225069991342
Michael Steinbaugh <mike@steinbaugh.com>
2026-01-02 10:40:14 -0500

    Harden custom SSL cert checker

--- lang/bash/functions/common/h/has-ssl-cert-file.sh
+++ lang/bash/functions/common/h/has-ssl-cert-file.sh
@@ -5,11 +5,14 @@ koopa_has_ssl_cert_file() {
     # Does the current environment have custom CA certificates loaded?
     # @note Updated 2026-01-02.
     # """
-    if [[ -z "${SSL_CERT_FILE:-}" ]]
+    local -A dict
+    dict['ssl_cert_file']="${SSL_CERT_FILE:-}"
+    if [[ -z "${dict['ssl_cert_file']}" ]]
     then
         return 1
     fi
-    if [[ "${SSL_CERT_FILE:?}" == "${KOOPA_PREFIX:?}/"* ]]
+    dict['koopa_prefix']="$(koopa_koopa_prefix)"
+    if [[ "${dict['ssl_cert_file']}" == "${dict['koopa_prefix']}/"* ]]
     then
         return 1
     fi
