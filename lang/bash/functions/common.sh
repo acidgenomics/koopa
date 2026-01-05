f9fc0b2c579ead569c35f73a462011fecf300fa8
Michael Steinbaugh <mike@steinbaugh.com>
2026-01-05 10:56:17 -0500

    Harden against dotfiles clone inside ~/.config/koopa

--- lang/bash/functions/common.sh
+++ lang/bash/functions/common.sh
@@ -317,6 +317,13 @@ koopa_add_config_link() {
         dict2['dest_name']="${2:?}"
         shift 2
         koopa_assert_is_existing "${dict2['source_file']}"
+        if koopa_str_detect_fixed \
+            --patern="${dict['config_prefix']}" \
+            "${dict2['source_file']}"
+        then
+            koopa_stop "${dict2['source_file']} is sourced \
+inside '${dict['config_prefix']}'."
+        fi
         dict2['dest_file']="${dict['config_prefix']}/${dict2['dest_name']}"
         koopa_is_symlink "${dict2['dest_file']}" && continue
         koopa_ln --verbose "${dict2['source_file']}" "${dict2['dest_file']}"
