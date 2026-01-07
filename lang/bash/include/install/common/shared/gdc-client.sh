8faddcf85614dc77b85f537c21048343a5327a13
Steinbaugh, Michael <michael.steinbaugh@merck.com>
2026-01-07 14:39:04 +0000

    gdc-client install fix

--- lang/bash/include/install/common/shared/gdc-client.sh
+++ lang/bash/include/install/common/shared/gdc-client.sh
@@ -19,21 +19,24 @@ install_from_conda_python312() {
     dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['libexec']="${dict['prefix']}/libexec"
-    set -x
-    koopa_mkdir "${dict['libexec']}"
-    koopa_conda_create_env --prefix="${dict['libexec']}" \
-        --channel='conda-forge' \
-        python==3.12
-    koopa_conda_activate_env "${dict['libexec']}"
-    conda install \
-        --yes \
-        --channel='conda-forge' \
-        --channel='bioconda' \
-        "gdc-client==${dict['version']}"
-    koopa_conda_deactivate
+    dict['conda_env_file']='conda.yaml'
+    read -r -d '' "dict[conda_env_string]" << END || true
+channels:
+  - conda-forge
+  - bioconda
+dependencies:
+  - python =3.12
+  - gdc-client =${dict['version']}
+END
+    koopa_write_string \
+        --file="${dict['conda_env_file']}" \
+        --string="${dict['conda_env_string']}"
+    koopa_install_conda_package --file="${dict['conda_env_file']}"
     koopa_mkdir "${dict['prefix']}/bin"
-    koopa_cd "${dict['prefix']}/bin"
-    koopa_ln '../libexec/bin/gdc-client' 'gdc-client'
+    (
+        koopa_cd "${dict['prefix']}/bin"
+        koopa_ln '../libexec/bin/gdc-client' 'gdc-client'
+    )
     return 0
 
 }
