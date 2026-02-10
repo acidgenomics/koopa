439c01180791ab1dcb8475fc0fa993d4763cd9cc
Michael Steinbaugh <mike@steinbaugh.com>
2026-02-10 09:38:13 -0500

    Add mount-s3 locator

---
+++ lang/bash/functions/common/l/locate-lfs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_lfs() {
+    koopa_locate_app \
+        '/usr/bin/lfs' \
+        "$@"
+}
---
+++ lang/bash/functions/common/l/locate-mount-s3.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+koopa_locate_mount_s3() {
+    koopa_locate_app \
+        '/usr/bin/mount-s3' \
+        "$@"
+}
