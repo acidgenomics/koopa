764d4ab62d2b2b3ec30cc3d09fc9807171f2ff67
Michael Steinbaugh <mike@steinbaugh.com>
2026-03-19 12:56:37 -0400

    Set bin name

--- lang/bash/include/install/common/shared/databricks-cli.sh
+++ lang/bash/include/install/common/shared/databricks-cli.sh
@@ -11,12 +11,8 @@ main() {
     local -A dict
     dict['version']="${KOOPA_INSTALL_VERSION:?}"
     dict['url']="github.com/databricks/cli@v${dict['version']}"
-    #go mod vendor
-    #go build \
-    #    -trimpath \
-    #    -mod vendor \
-    #    -ldflags "-X github.com/databricks/cli/internal/build.buildVersion=${PKG_VERSION}" \
-    #    -o "${BINARY_FILEPATH}"
-    koopa_install_go_package --url="${dict['url']}"
+    koopa_install_go_package \
+        --bin-name='databricks' \
+        --url="${dict['url']}"
     return 0
 }
