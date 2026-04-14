16c67a51d77811c6ea32b85f91a68a3089baf0d6
Michael Steinbaugh <mike@steinbaugh.com>
2026-04-14 19:19:41 +0000

    Add install support for claude-code

--- lang/bash/functions/common/i/install-claude-code.sh
+++ lang/bash/functions/common/i/install-claude-code.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-# FIXME We need to set against repo here:
-# > npm i @anthropic-ai/claude-code
-
 koopa_install_claude_code() {
     koopa_install_app \
-        --installer='node-package' \
         --name='claude-code' \
         "$@"
 }
---
+++ lang/bash/include/install/common/shared/claude-code.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+main() {
+    koopa_install_node_package \
+        --name='@anthropic-ai/claude-code'
+    return 0
+}
