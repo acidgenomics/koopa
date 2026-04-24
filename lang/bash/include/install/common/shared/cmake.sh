9803bb41d579d535b57da91949c3e813621fb4e3
Michael Steinbaugh <mike@steinbaugh.com>
2026-04-24 12:12:09 -0400

    Bump perl

--- lang/bash/include/install/common/shared/cmake.sh
+++ lang/bash/include/install/common/shared/cmake.sh
@@ -53,6 +53,37 @@ v${dict['version']}/cmake-${dict['version']}.tar.gz"
     koopa_download "${dict['url']}"
     koopa_extract "$(koopa_basename "${dict['url']}")" 'src'
     koopa_cd 'src'
+    # Fix bundled curl for OpenSSL 4 compatibility.
+    # OpenSSL 4 made ASN1_STRING an opaque type, so direct struct member
+    # access must be replaced with accessor functions.
+    if [[ -f 'Utilities/cmcurl/lib/vtls/openssl.c' ]]
+    then
+        koopa_find_and_replace_in_file \
+            --regex \
+            --pattern='num->type' \
+            --replacement='ASN1_STRING_type(num)' \
+            'Utilities/cmcurl/lib/vtls/openssl.c'
+        koopa_find_and_replace_in_file \
+            --regex \
+            --pattern='num->length' \
+            --replacement='ASN1_STRING_length(num)' \
+            'Utilities/cmcurl/lib/vtls/openssl.c'
+        koopa_find_and_replace_in_file \
+            --regex \
+            --pattern='num->data' \
+            --replacement='ASN1_STRING_get0_data(num)' \
+            'Utilities/cmcurl/lib/vtls/openssl.c'
+        koopa_find_and_replace_in_file \
+            --regex \
+            --pattern='psig->length' \
+            --replacement='ASN1_STRING_length(psig)' \
+            'Utilities/cmcurl/lib/vtls/openssl.c'
+        koopa_find_and_replace_in_file \
+            --regex \
+            --pattern='psig->data' \
+            --replacement='ASN1_STRING_get0_data(psig)' \
+            'Utilities/cmcurl/lib/vtls/openssl.c'
+    fi
     ./bootstrap --help || true
     ./bootstrap "${bootstrap_args[@]}"
     koopa_print_env
