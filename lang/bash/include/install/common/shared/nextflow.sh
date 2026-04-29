c07e64fc0966ea426578f9bcd76e5b03412be0bb
Steinbaugh, Michael <michael.steinbaugh@merck.com>
2026-04-29 12:19:08 +0000

    Ensure we define java path

--- lang/bash/include/install/common/shared/nextflow.sh
+++ lang/bash/include/install/common/shared/nextflow.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 
-main() {
+install_from_conda() {
+    local -A dict
+    dict['prefix']="${KOOPA_INSTALL_PREFIX:?}"
+    dict['libexec']="${dict['prefix']}/libexec"
+    koopa_install_conda_package
+    koopa_rm "${dict['prefix']}/bin/nextflow"
+    read -r -d '' "dict[bin_string]" << END || true
+#!/bin/sh
+set -o errexit
+set -o nounset
+
+export NXF_JAVA_HOME='${dict['libexec']}'
+'${dict['libexec']}/bin/nextflow' "\$@"
+END
+    koopa_write_string \
+        --file="${dict['prefix']}/bin/nextflow" \
+        --string="${dict['bin_string']}"
+    koopa_chmod +x "${dict['prefix']}/bin/nextflow"
+    "${dict['prefix']}/bin/nextflow" -version
+    return 0
+}
+
+install_from_source() {
     # """
     # Install Nextflow.
-    # @note Updated 2023-04-24.
+    # @note Updated 2026-04-29.
     #
     # @seealso
     # - https://github.com/nextflow-io/nextflow/
@@ -17,19 +39,19 @@ main() {
     koopa_assert_is_dir "${dict['temurin']}"
     dict['libexec']="$(koopa_init_dir "${dict['prefix']}/libexec")"
     koopa_download 'https://get.nextflow.io' 'nextflow'
-    export NXF_HOME="${dict['prefix']}/libexec"
+    export NXF_HOME="${dict['libexec']}"
     export NXF_JAVA_HOME="${dict['temurin']}"
     export NXF_VER="${dict['version']}"
     koopa_chmod +x 'nextflow'
     ./nextflow
-    koopa_cp --target-directory="${dict['libexec']}" 'nextflow'
+    koopa_cp --target-directory="${dict['libexec']}/bin" 'nextflow'
     read -r -d '' "dict[bin_string]" << END || true
 #!/bin/sh
 set -o errexit
 set -o nounset
 
 export NXF_JAVA_HOME='${dict['temurin']}'
-'${dict['libexec']}/nextflow' "\$@"
+'${dict['libexec']}/bin/nextflow' "\$@"
 END
     koopa_write_string \
         --file="${dict['prefix']}/bin/nextflow" \
@@ -37,5 +59,10 @@ END
     koopa_chmod +x "${dict['prefix']}/bin/nextflow"
     "${dict['prefix']}/bin/nextflow" -version
     koopa_rm "${dict['libexec']}/tmp"
+    return 0
+}
+
+main() {
+    install_from_conda
     return 0
 }
