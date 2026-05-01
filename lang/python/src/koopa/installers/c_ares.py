b1223371ce7a1187e9171b3344d7b8dd11d922be
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-01 19:19:35 -0400

    Work on porting complicated installers

---
+++ lang/python/src/koopa/installers/c_ares.py
@@ -0,0 +1,23 @@
+"""Install c-ares."""
+
+from __future__ import annotations
+
+from koopa.build import activate_app, cmake_build
+from koopa.installers._build_helper import download_extract_cd
+
+
+def main(
+    *,
+    name: str,
+    version: str,
+    prefix: str,
+    passthrough_args: list[str] | None = None,
+) -> None:
+    """Install c-ares."""
+    env = activate_app("pkg-config", build_only=True)
+    url = (
+        f"https://github.com/c-ares/c-ares/releases/download/"
+        f"v{version}/c-ares-{version}.tar.gz"
+    )
+    download_extract_cd(url)
+    cmake_build(prefix=prefix, env=env)
