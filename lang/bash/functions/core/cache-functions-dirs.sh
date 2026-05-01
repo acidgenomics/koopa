8abd8f19c50633cd22f4849c54f19773ee7652d9
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-01 10:07:28 -0400

    Simplify lang/bash semantic layout

--- lang/bash/functions/core/cache-functions-dirs.sh
+++ lang/bash/functions/core/cache-functions-dirs.sh
@@ -2,16 +2,16 @@
 
 _koopa_cache_functions_dirs() {
     local -A app dict
-    local dir
-    _koopa_assert_has_args_ge "$#" 2
+    _koopa_assert_has_args_eq "$#" 2
     dict['target_file']="${1:?}"
-    shift 1
+    dict['source_prefix']="${2:?}"
     app['cat']="$(_koopa_locate_cat --allow-system)"
     app['find']="$(_koopa_locate_find --allow-system)"
     app['grep']="$(_koopa_locate_grep --allow-system)"
     app['sort']="$(_koopa_locate_sort --allow-system)"
     app['xargs']="$(_koopa_locate_xargs --allow-system)"
     _koopa_assert_is_executable "${app[@]}"
+    _koopa_assert_is_dir "${dict['source_prefix']}"
     if _koopa_str_detect_fixed \
         --pattern='/bash/' \
         --string="${dict['target_file']}"
@@ -24,15 +24,11 @@ _koopa_cache_functions_dirs() {
     {
         printf '%s\n' "${dict['shebang']}"
         printf '%s\n' '# shellcheck disable=all'
-        for dir in "$@"
-        do
-            [[ -d "$dir" ]] || continue
-            "${app['find']}" "$dir" \
-                -type 'f' -name '*.sh' -print0 \
-            | "${app['sort']}" -z \
-            | "${app['xargs']}" -0 "${app['cat']}" \
-            | "${app['grep']}" -Eiv '^(\s+)?#'
-        done
+        "${app['find']}" "${dict['source_prefix']}" \
+            -type 'f' -name '*.sh' -print0 \
+        | "${app['sort']}" -z \
+        | "${app['xargs']}" -0 "${app['cat']}" \
+        | "${app['grep']}" -Eiv '^(\s+)?#'
     } | "${app['cat']}" -s > "${dict['target_file']}"
     return 0
 }
