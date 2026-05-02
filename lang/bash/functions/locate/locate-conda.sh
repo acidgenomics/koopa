6cff637ca011df364da591f5853502e8bbb4351d
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-02 07:49:24 -0400

    Finish Bash to Python function migration

--- lang/bash/functions/locate/locate-conda.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-_koopa_locate_conda() {
-    _koopa_locate_app \
-        --app-name='conda' \
-        --bin-name='conda' \
-        "$@"
-}
--- lang/bash/functions/prefix/emacs-prefix.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-_koopa_emacs_prefix() {
-    _koopa_print "${HOME:?}/.emacs.d"
-    return 0
-}
