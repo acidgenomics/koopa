6a0d853c55b5b5d544542c77c563272eb16ac21c
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-03 00:28:23 -0400

    Rework tar install

--- lang/python/src/koopa/installers/tar.py
+++ lang/python/src/koopa/installers/tar.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
-import sys
+import os
 
-from koopa.build import activate_app, app_prefix
+from koopa.build import activate_app, app_prefix, locate, make_build
 from koopa.install import install_gnu_app
 
 
@@ -18,15 +18,18 @@ def main(
     """Install tar."""
     env = activate_app("make", build_only=True)
     env = activate_app("libiconv", env=env)
-    env.apply()
     iconv_prefix = app_prefix("libiconv")
-    conf_args = [
-        f"--program-prefix=g",
-        f"--with-libiconv-prefix={iconv_prefix}",
-    ]
+    iconv_lib = os.path.join(iconv_prefix, "lib")
+    env.ldflags.append(f"-L{iconv_lib}")
+    env.ldflags.append(f"-Wl,-rpath,{iconv_lib}")
+    env.apply()
+    os.environ["LIBS"] = f"-liconv {os.environ.get('LIBS', '')}".strip()
     install_gnu_app(
         name=name,
         version=version,
         prefix=prefix,
-        conf_args=conf_args,
+        conf_args=[
+            "--program-prefix=g",
+            f"--with-libiconv-prefix={iconv_prefix}",
+        ],
     )
