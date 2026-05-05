bef293f1104519bcf3b0795961ced4ea78c15e8a
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-05 10:35:46 -0400

    Streamline automatic koopa mirror usage

--- lang/python/src/koopa/installers/isl.py
+++ lang/python/src/koopa/installers/isl.py
@@ -1,8 +1,7 @@
 """Install isl."""
 
 from koopa.build import activate_app, app_prefix, make_build
-from koopa.download import download_with_mirror
-from koopa.installers._build_helper import download_extract_cd, extract_cd
+from koopa.installers._build_helper import download_extract_cd
 
 
 def main(
@@ -11,19 +10,13 @@ def main(
     version: str,
     prefix: str,
     passthrough_args: list[str] | None = None,
-    use_mirror: bool = False,
 ) -> None:
     """Install isl."""
     env = activate_app("pkg-config", build_only=True)
     env = activate_app("gmp", env=env)
     gmp_prefix = app_prefix("gmp")
-    filename = f"isl-{version}.tar.xz"
-    primary_url = f"https://downloads.sourceforge.net/project/libisl/{filename}"
-    if use_mirror:
-        tarball = download_with_mirror(primary_url, name, filename)
-        extract_cd(tarball)
-    else:
-        download_extract_cd(primary_url)
+    url = f"https://downloads.sourceforge.net/project/libisl/isl-{version}.tar.xz"
+    download_extract_cd(url)
     make_build(
         conf_args=[
             "--disable-dependency-tracking",
--- lang/python/src/koopa/installers/sqlite.py
+++ lang/python/src/koopa/installers/sqlite.py
@@ -3,18 +3,12 @@
 import datetime
 
 from koopa.build import activate_app, make_build
-from koopa.download import download_with_mirror
-from koopa.installers._build_helper import download_extract_cd, extract_cd
+from koopa.installers._build_helper import download_extract_cd
 
 
 def _file_version(version: str) -> str:
     parts = version.split(".")
     return f"{parts[0]}{int(parts[1]):02d}{int(parts[2]):02d}00"
-
-
-def _primary_url(file_version: str) -> str:
-    year = datetime.datetime.now(tz=datetime.UTC).year
-    return f"https://www.sqlite.org/{year}/sqlite-autoconf-{file_version}.tar.gz"
 
 
 def main(
@@ -23,19 +17,14 @@ def main(
     version: str,
     prefix: str,
     passthrough_args: list[str] | None = None,
-    use_mirror: bool = False,
 ) -> None:
     """Install sqlite."""
     env = activate_app("pkg-config", build_only=True)
     env = activate_app("zlib", "readline", env=env)
     file_version = _file_version(version)
-    url = _primary_url(file_version)
-    filename = f"sqlite-autoconf-{file_version}.tar.gz"
-    if use_mirror:
-        tarball = download_with_mirror(url, "sqlite", filename)
-        extract_cd(tarball)
-    else:
-        download_extract_cd(url)
+    year = datetime.datetime.now(tz=datetime.UTC).year
+    url = f"https://www.sqlite.org/{year}/sqlite-autoconf-{file_version}.tar.gz"
+    download_extract_cd(url)
     make_build(
         conf_args=[
             "--disable-static",
