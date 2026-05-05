2defee3ca6f1f8520e5c3ae4210fdb69887d9cbc
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-05 15:51:49 -0400

    Fix illumina ica installer

--- lang/python/src/koopa/installers/illumina_ica_cli.py
+++ lang/python/src/koopa/installers/illumina_ica_cli.py
@@ -1,10 +1,11 @@
 """Install illumina-ica-cli."""
 
 import os
+import shutil
 import sys
 
+from koopa.archive import extract
 from koopa.download import download
-from koopa.system import arch
 
 
 def main(
@@ -19,12 +20,20 @@ def main(
         os_id = "darwin"
     else:
         os_id = "linux"
-    machine = arch()
     url = (
         f"https://stratus-documentation-us-east-1-public.s3.amazonaws.com/"
-        f"cli/{version}/{os_id}/{machine}/icav2"
+        f"cli/{version}/ica-{os_id}-amd64.zip"
     )
+    zipfile = download(url)
+    extract(zipfile, "src")
     bin_dir = os.path.join(prefix, "bin")
     os.makedirs(bin_dir, exist_ok=True)
-    download(url, output=os.path.join(bin_dir, "icav2"))
+    icav2_bin = os.path.join("src", "icav2")
+    if not os.path.isfile(icav2_bin):
+        for entry in os.listdir("src"):
+            candidate = os.path.join("src", entry)
+            if os.path.isfile(candidate) and "icav2" in entry:
+                icav2_bin = candidate
+                break
+    shutil.copy2(icav2_bin, os.path.join(bin_dir, "icav2"))
     os.chmod(os.path.join(bin_dir, "icav2"), 0o755)
