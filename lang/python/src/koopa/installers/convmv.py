a1bc57374d8d613675695c008e9d0d0f2847480f
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-05 09:26:10 -0400

    Attempt to wire in smarter mirror usage

--- lang/python/src/koopa/installers/convmv.py
+++ lang/python/src/koopa/installers/convmv.py
@@ -1,10 +1,10 @@
 """Install convmv."""
 
-
 import subprocess
 
 from koopa.build import activate_app, locate
-from koopa.installers._build_helper import download_extract_cd
+from koopa.download import download_with_mirror
+from koopa.installers._build_helper import download_extract_cd, extract_cd
 
 
 def main(
@@ -13,12 +13,18 @@ def main(
     version: str,
     prefix: str,
     passthrough_args: list[str] | None = None,
+    use_mirror: bool = False,
 ) -> None:
     """Install convmv."""
     env = activate_app("make", build_only=True)
     make = locate("make")
-    url = f"https://www.j3e.de/linux/convmv/convmv-{version}.tar.gz"
-    download_extract_cd(url)
+    filename = f"convmv-{version}.tar.gz"
+    url = f"https://www.j3e.de/linux/convmv/{filename}"
+    if use_mirror:
+        tarball = download_with_mirror(url, name, filename)
+        extract_cd(tarball)
+    else:
+        download_extract_cd(url)
     subprocess.run(
         [make, "install", f"PREFIX={prefix}"],
         env=env.to_env_dict(),
--- lang/python/src/koopa/installers/libssh2.py
+++ lang/python/src/koopa/installers/libssh2.py
@@ -1,9 +1,8 @@
 """Install libssh2."""
 
-
 from koopa.build import activate_app, app_prefix, make_build
 from koopa.download import download_with_mirror
-from koopa.installers._build_helper import extract_cd
+from koopa.installers._build_helper import download_extract_cd, extract_cd
 
 
 def main(
@@ -12,6 +11,7 @@ def main(
     version: str,
     prefix: str,
     passthrough_args: list[str] | None = None,
+    use_mirror: bool = False,
 ) -> None:
     """Install libssh2."""
     env = activate_app("pkg-config", build_only=True)
@@ -20,8 +20,11 @@ def main(
     zlib_prefix = app_prefix("zlib")
     filename = f"libssh2-{version}.tar.gz"
     primary_url = f"https://www.libssh2.org/download/{filename}"
-    tarball = download_with_mirror(primary_url, name, filename)
-    extract_cd(tarball)
+    if use_mirror:
+        tarball = download_with_mirror(primary_url, name, filename)
+        extract_cd(tarball)
+    else:
+        download_extract_cd(primary_url)
     make_build(
         conf_args=[
             "--disable-examples-build",
