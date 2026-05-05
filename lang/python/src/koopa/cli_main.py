ff558d95548f61e86c3fa77d7101b62c6f53d9ab
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-05 12:00:36 -0400

    Deprecate usage of koopa header

--- lang/python/src/koopa/cli_main.py
+++ lang/python/src/koopa/cli_main.py
@@ -588,9 +588,14 @@ def _handle_version(_args: argparse.Namespace) -> None:
 
 def _handle_header(_args: argparse.Namespace) -> None:
     """Handle ``koopa header`` subcommand."""
-    from koopa.prefix import bash_prefix
-
-    print(os.path.join(bash_prefix(), "include", "header.sh"))
+    print(
+        "Error: 'koopa header' is deprecated.\n"
+        "Scripts using 'source \"$(koopa header bash)\"' must be updated.\n"
+        "Add 'set -Eeuo pipefail' to your script and source koopa\n"
+        "functions directly instead.",
+        file=sys.stderr,
+    )
+    sys.exit(1)
 
 
 def _handle_install_all_apps(_args: argparse.Namespace) -> None:
