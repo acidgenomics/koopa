c793a3bfe5f9c7fe85af665077f4d3c1c41ef63a
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-06 17:34:44 -0400

    POSIX fixes

--- lang/sh/functions/alias/alias-venv.sh
+++ lang/sh/functions/alias/alias-venv.sh
@@ -8,19 +8,19 @@ _koopa_alias_venv() {
     if [ -f '.venv/bin/activate' ]
     then
         # shellcheck source=/dev/null
-        source '.venv/bin/activate'
+        . '.venv/bin/activate'
     elif [ -f "venv/bin/activate" ]
     then
         # shellcheck source=/dev/null
-        source "venv/bin/activate"
+        . "venv/bin/activate"
     elif [ -f "${HOME}/.venv/bin/activate" ]
     then
         # shellcheck source=/dev/null
-        source "${HOME}/.venv/bin/activate"
+        . "${HOME}/.venv/bin/activate"
     elif [ -f "${HOME}/venv/bin/activate" ]
     then
         # shellcheck source=/dev/null
-        source "${HOME}/venv/bin/activate"
+        . "${HOME}/venv/bin/activate"
     else
         _koopa_print 'Failed to locate Python virtual environment.'
         return 1
