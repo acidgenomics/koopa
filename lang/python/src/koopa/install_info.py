5c3e167f29b5aac458a503368f585ce59c95411a
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-07 06:49:59 -0400

    Need a mechanism to cache stale apps instead we need to restart koopa update

--- lang/python/src/koopa/install_info.py
+++ lang/python/src/koopa/install_info.py
@@ -31,15 +31,22 @@ def write_install_info(output_file: str, name: str, version: str) -> None:
                 d = _resolve_dep_dict(d, sys_dict)
             deps = list(d)
     dep_revisions: dict[str, int] = {}
+    dep_versions: dict[str, str] = {}
     for d in deps:
         resolved_d = d
         d_entry = json_data.get(d, {})
         if isinstance(d_entry, dict) and d_entry.get("alias_of"):
             resolved_d = d_entry["alias_of"]
         resolved_entry = json_data.get(resolved_d, {})
-        rev = int(resolved_entry.get("revision", 0)) if isinstance(resolved_entry, dict) else 0
-        if rev > 0:
-            dep_revisions[resolved_d] = rev
+        if isinstance(resolved_entry, dict):
+            rev = int(resolved_entry.get("revision", 0))
+            if rev > 0:
+                dep_revisions[resolved_d] = rev
+            ver = resolved_entry.get("version", "")
+            if ver:
+                dep_versions[resolved_d] = ver
+        else:
+            rev = 0
     info = {
         "name": name,
         "version": version,
@@ -48,6 +55,7 @@ def write_install_info(output_file: str, name: str, version: str) -> None:
         "build_dependencies": build_deps,
         "dependencies": deps,
         "dep_revisions": dep_revisions,
+        "dep_versions": dep_versions,
         "environ": dict(sorted(os.environ.items())),
     }
     with open(output_file, "w") as fh:
