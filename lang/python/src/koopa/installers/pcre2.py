6b7ecfaecc005011f0f79e18cfdb6e96faea6b5d
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-06 20:28:10 -0400

    Add additional installer optimizations

--- lang/python/src/koopa/installers/pcre2.py
+++ lang/python/src/koopa/installers/pcre2.py
@@ -18,18 +18,20 @@ def main(
     deps = ["zlib"]
     if sys.platform != "darwin":
         deps.append("bzip2")
+    else:
+        deps.append("libedit")
     env = activate_app(*deps, env=env)
     download_extract_cd()
-    make_build(
-        conf_args=[
-            "--disable-dependency-tracking",
-            "--disable-static",
-            "--enable-jit",
-            "--enable-pcre2-16",
-            "--enable-pcre2-32",
-            "--enable-pcre2grep-libbz2",
-            "--enable-pcre2grep-libz",
-            f"--prefix={prefix}",
-        ],
-        env=env,
-    )
+    conf_args = [
+        "--disable-dependency-tracking",
+        "--disable-static",
+        "--enable-jit",
+        "--enable-pcre2-16",
+        "--enable-pcre2-32",
+        "--enable-pcre2grep-libbz2",
+        "--enable-pcre2grep-libz",
+        f"--prefix={prefix}",
+    ]
+    if sys.platform == "darwin":
+        conf_args.append("--enable-pcre2test-libedit")
+    make_build(conf_args=conf_args, env=env)
