b172fa9cc025611b393c2539984246a002bacfbf
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-08 14:15:53 -0400

    Fix zsh activation on starship

--- lang/zsh/include/functions.sh
+++ lang/zsh/include/functions.sh
@@ -822,6 +822,37 @@ _koopa_activate_ruby() {
     return 0
 }
 
+_koopa_activate_starship() {
+    local starship
+    starship="$(_koopa_bin_prefix)/starship"
+    if [[ ! -x "$starship" ]]
+    then
+        return 0
+    fi
+    local shell
+    shell="$(_koopa_shell_name)"
+    case "$shell" in
+        'bash' | \
+        'zsh')
+            ;;
+        *)
+            return 0
+            ;;
+    esac
+    if [[ -n "${STARSHIP_SHELL:-}" ]] && [[ "$STARSHIP_SHELL" != "$shell" ]]
+    then
+        unset -v STARSHIP_SHELL
+    fi
+    local nounset
+    nounset="$(_koopa_boolean_nounset)"
+    if [[ "$nounset" -eq 1 ]]
+    then
+        return 0
+    fi
+    eval "$("$starship" init "$shell")"
+    return 0
+}
+
 _koopa_activate_tealdeer() {
     [[ -x "$(_koopa_bin_prefix)/tldr" ]] || return 0
     if [[ -z "${TEALDEER_CONFIG_DIR:-}" ]]
