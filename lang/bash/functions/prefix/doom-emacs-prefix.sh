c9b27e795bf489fa1b2e6a6beafb1b85b5432b79
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-10 12:45:56 -0400

    Improve emacs and spacevim install support

--- lang/bash/functions/prefix/doom-emacs-prefix.sh
+++ lang/bash/functions/prefix/doom-emacs-prefix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 _koopa_doom_emacs_prefix() {
-    _koopa_print "$(_koopa_xdg_data_home)/doom"
+    _koopa_print "$(_koopa_opt_prefix)/doom-emacs"
     return 0
 }
--- lang/bash/functions/prefix/prelude-emacs-prefix.sh
+++ lang/bash/functions/prefix/prelude-emacs-prefix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 _koopa_prelude_emacs_prefix() {
-    _koopa_print "$(_koopa_xdg_data_home)/prelude"
+    _koopa_print "$(_koopa_opt_prefix)/prelude-emacs"
     return 0
 }
