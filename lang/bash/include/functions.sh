8b0ee0fb7e9a335208dbd4d1eb8d4f44eb9417d2
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-10 16:45:12 -0400

    Modernize our emacs support

--- lang/bash/include/functions.sh
+++ lang/bash/include/functions.sh
@@ -110,7 +110,7 @@ _koopa_activate_aliases() {
         fi
         if [[ -d "${xdg_data_home}/prelude" ]]
         then
-            alias prelude-emacs='_koopa_prelude_emacs'
+            alias emacs-prelude='_koopa_emacs_prelude'
         fi
         if [[ -d "${xdg_data_home}/spacemacs" ]]
         then
@@ -167,10 +167,6 @@ _koopa_activate_aliases() {
         if [[ -x "${bin_prefix}/fzf" ]]
         then
             alias vim-fzf='_koopa_alias_vim_fzf'
-        fi
-        if [[ -d "${xdg_data_home}/spacevim" ]]
-        then
-            alias spacevim='_koopa_spacevim'
         fi
     fi
     if [[ -x "${bin_prefix}/walk" ]]
@@ -429,6 +425,7 @@ ca-certificates"
     export DEFAULT_CA_BUNDLE_PATH="$prefix"
     export NODE_EXTRA_CA_CERTS="$file"
     export REQUESTS_CA_BUNDLE="$file"
+    export GIT_SSL_CAINFO="$file"
     export SSL_CERT_FILE="$file"
     if _koopa_is_linux
     then
@@ -2081,15 +2078,15 @@ ${dict['c2']}${string}${dict['nc']}"
     return 0
 }
 
-_koopa_prelude_emacs() {
-    local prelude_emacs_prefix
-    prelude_emacs_prefix="$(_koopa_prelude_emacs_prefix)"
-    if [[ ! -d "$prelude_emacs_prefix" ]]
+_koopa_emacs_prelude() {
+    local emacs_prelude_prefix
+    emacs_prelude_prefix="$(_koopa_emacs_prelude_prefix)"
+    if [[ ! -d "$emacs_prelude_prefix" ]]
     then
-        _koopa_print 'Prelude Emacs is not installed.'
+        _koopa_print 'Emacs Prelude is not installed.'
         return 1
     fi
-    _koopa_emacs --init-directory="$prelude_emacs_prefix" "$@"
+    _koopa_emacs --init-directory="$emacs_prelude_prefix" "$@"
     return 0
 }
 
@@ -2199,26 +2196,6 @@ _koopa_spacemacs() {
         return 1
     fi
     _koopa_emacs --init-directory="$spacemacs_prefix" "$@"
-    return 0
-}
-
-_koopa_spacevim() {
-    local vim
-    vim='vim'
-    if _koopa_is_macos
-    then
-        local gvim
-        gvim='/Applications/MacVim.app/Contents/bin/gvim'
-        [[ -x "$gvim" ]] && vim="$gvim"
-    fi
-    local vimrc
-    vimrc="$(_koopa_spacevim_prefix)/vimrc"
-    if [[ ! -f "$vimrc" ]]
-    then
-        _koopa_print 'SpaceVim is not installed.'
-        return 1
-    fi
-    "$vim" -u "$vimrc" "$@"
     return 0
 }
 
@@ -2913,7 +2890,7 @@ _koopa_config_prefix() {
 }
 
 _koopa_doom_emacs_prefix() {
-    _koopa_print "$(_koopa_xdg_data_home)/doom"
+    _koopa_print "$(_koopa_opt_prefix)/doom-emacs"
     return 0
 }
 
@@ -2964,8 +2941,8 @@ _koopa_pipx_prefix() {
     return 0
 }
 
-_koopa_prelude_emacs_prefix() {
-    _koopa_print "$(_koopa_xdg_data_home)/prelude"
+_koopa_emacs_prelude_prefix() {
+    _koopa_print "$(_koopa_opt_prefix)/emacs-prelude"
     return 0
 }
 
@@ -2985,14 +2962,10 @@ _koopa_scripts_private_prefix() {
 }
 
 _koopa_spacemacs_prefix() {
-    _koopa_print "$(_koopa_xdg_data_home)/spacemacs"
+    _koopa_print "$(_koopa_opt_prefix)/spacemacs"
     return 0
 }
 
-_koopa_spacevim_prefix() {
-    _koopa_print "$(_koopa_xdg_data_home)/spacevim"
-    return 0
-}
 
 _koopa_xdg_cache_home() {
     local string
--- lang/zsh/functions/prefix/emacs-prelude-prefix.sh
+++ lang/zsh/functions/prefix/emacs-prelude-prefix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-_koopa_prelude_emacs_prefix() {
+_koopa_emacs_prelude_prefix() {
     _koopa_print "$(_koopa_xdg_data_home)/prelude"
     return 0
 }
