d3ba9841d77dcea9dc634d70b56e9f397195ab60
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-12 08:54:42 -0400

    URL formatting fix

--- lang/python/src/koopa/installers/pyenv.py
+++ lang/python/src/koopa/installers/pyenv.py
@@ -21,11 +21,11 @@ def main(
     extract(tarball, prefix)
     plugins_dir = os.path.join(prefix, "plugins")
     os.makedirs(plugins_dir, exist_ok=True)
-    for plugin_name, plugin_ver, plugin_repo in [
-        ("pyenv-multiuser", "1.0.7", "macdub/pyenv-multiuser"),
+    for plugin_name, plugin_ver, plugin_repo, *url_override in [
+        ("pyenv-multiuser", "1.0.7", "macdub/pyenv-multiuser", "https://github.com/macdub/pyenv-multiuser/archive/refs/tags/1.0.7.tar.gz"),
         ("pyenv-virtualenv", "1.4.0", "pyenv/pyenv-virtualenv"),
     ]:
-        purl = f"https://github.com/{plugin_repo}/archive/v{plugin_ver}.tar.gz"
+        purl = url_override[0] if url_override else f"https://github.com/{plugin_repo}/archive/v{plugin_ver}.tar.gz"
         ptarball = download(purl)
         extract(ptarball, os.path.join(plugins_dir, plugin_name))
     for d in ("shims", "versions"):
