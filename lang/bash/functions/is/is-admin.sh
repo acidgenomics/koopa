a1718c849288e2f6013b029a6939d5ff1e88454a
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-26 07:41:43 -0400

    Admin check and pytest configuration fixes

--- lang/bash/functions/is/is-admin.sh
+++ lang/bash/functions/is/is-admin.sh
@@ -43,7 +43,7 @@ _koopa_is_admin() {
     _koopa_is_root && return 0
     # Return false if 'sudo' program is not installed.
     _koopa_is_installed 'sudo' || return 1
-    # Early return true if user has passwordless sudo enabled.
+    # Early return true if user has sudo access.
     _koopa_has_sudo && return 0
     # Check if user is any accepted admin group.
     # Note that this step is very slow for Active Directory domain accounts.
--- lang/bash/include/functions.sh
+++ lang/bash/include/functions.sh
@@ -1768,11 +1768,14 @@ _koopa_grep() {
 
 _koopa_has_sudo() {
     local -A app
+    local stderr
     _koopa_assert_has_no_args "$#"
     app['sudo']="$(_koopa_locate_sudo --allow-missing)"
     [[ -x "${app['sudo']}" ]] || return 1
     _koopa_is_root && return 0
-    "${app['sudo']}" -n true 2>/dev/null && return 0
+    stderr="$("${app['sudo']}" -v -n 2>&1)"
+    [[ $? -eq 0 ]] && return 0
+    [[ "${stderr}" == *'password is required'* ]] && return 0
     return 1
 }
 
