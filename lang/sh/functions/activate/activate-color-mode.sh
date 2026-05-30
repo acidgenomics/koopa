826d8999c9b7657e9d1528e0bc2c4c73292d3a53
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-30 16:22:12 -0400

    Improve color mode activation and BBEdit configuration

--- lang/sh/functions/activate/activate-color-mode.sh
+++ lang/sh/functions/activate/activate-color-mode.sh
@@ -5,7 +5,15 @@ _koopa_activate_color_mode() {
     # Activate dark / light color mode.
     # @note Updated 2022-04-13.
     # """
-    if [ -z "${KOOPA_COLOR_MODE:-}" ]
+    if _koopa_is_macos
+    then
+        if _koopa_is_light_mode
+        then
+            KOOPA_COLOR_MODE='light'
+        else
+            KOOPA_COLOR_MODE='dark'
+        fi
+    elif [ -z "${KOOPA_COLOR_MODE:-}" ]
     then
         KOOPA_COLOR_MODE="$(_koopa_color_mode)"
     fi
--- lang/zsh/functions/activate/activate-color-mode.sh
+++ lang/zsh/functions/activate/activate-color-mode.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env zsh
 
 _koopa_activate_color_mode() {
-    if [[ -z "${KOOPA_COLOR_MODE:-}" ]]
+    if [[ "$OSTYPE" == darwin* ]]
+    then
+        if _koopa_is_light_mode
+        then
+            KOOPA_COLOR_MODE='light'
+        else
+            KOOPA_COLOR_MODE='dark'
+        fi
+    elif [[ -z "${KOOPA_COLOR_MODE:-}" ]]
     then
         KOOPA_COLOR_MODE="$(_koopa_color_mode)"
     fi
