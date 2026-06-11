239b200c5d1704b5ba03ba459de51e299786bed6
Michael Steinbaugh <mike@steinbaugh.com>
2026-06-11 13:15:13 -0400

    Gate against incorrect color mode inside tmux

--- lang/bash/functions/core/is-light-mode.sh
+++ lang/bash/functions/core/is-light-mode.sh
@@ -6,8 +6,19 @@ _koopa_is_light_mode() {
         [[ "$(/usr/bin/defaults read -g 'AppleInterfaceStyle' 2>/dev/null)" != 'Dark' ]]
     elif [[ -n "${TMUX:-}" || "${TERM:-}" == screen* || "${TERM:-}" == tmux* ]]
     then
-        local cache_file="${HOME:?}/.cache/koopa/color-mode"
-        [[ -f "$cache_file" ]] && [[ "$(<"$cache_file")" == 'light' ]]
+        local tmux_mode=''
+        if [[ -n "${TMUX:-}" ]]
+        then
+            tmux_mode="$(tmux show-environment -g KOOPA_COLOR_MODE 2>/dev/null)"
+            tmux_mode="${tmux_mode#KOOPA_COLOR_MODE=}"
+        fi
+        if [[ "$tmux_mode" == 'light' || "$tmux_mode" == 'dark' ]]
+        then
+            [[ "$tmux_mode" == 'light' ]]
+        else
+            local cache_file="${HOME:?}/.cache/koopa/color-mode"
+            [[ -f "$cache_file" ]] && [[ "$(<"$cache_file")" == 'light' ]]
+        fi
     elif [[ "${TERM_PROGRAM:-}" == 'vscode' ]]
     then
         local cache_file="${HOME:?}/.cache/koopa/color-mode"
