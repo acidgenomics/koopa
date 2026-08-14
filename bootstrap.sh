39a2bdce3e8bef4dfb1fce809791e1824c0cf737
Michael Steinbaugh <mike@steinbaugh.com>
2026-08-14 10:43:43 -0400

    App updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -838,7 +838,7 @@ install_zlib() {
 }
 
 install_python_uv() {
-    __kvar_uv_version='0.12.3'
+    __kvar_uv_version='0.12.4'
     __kvar_python_version='3.14.7'
     printf 'Installing python via uv.\n'
     __kvar_tmpdir="$(mktemp -d -t koopa-uv-XXXXXX)"
